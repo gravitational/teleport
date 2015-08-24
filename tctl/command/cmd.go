721a9b6e6954df5e5d5243dcd69febad6586449b
Alex Lyulkov <a@mailgunhq.com>
2015-08-24 18:24:57 +0300

    Fixed variable names

--- tctl/command/cmd.go
+++ tctl/command/cmd.go
@@ -32,39 +32,39 @@ func (cmd *Command) Run(args []string) error {
 	authUrl := app.Flag("auth", "Teleport URL").Default(DefaultTeleportURL).String()
 
 	// Host CA
-	hostca := app.Command("host-ca", "Operations with host certificate authority")
+	hostCa := app.Command("host-ca", "Operations with host certificate authority")
 
-	hostcaReset := hostca.Command("reset", "Reset host certificate authority keys")
-	hostcaResetConfirm := hostcaReset.Flag("confirm", "Automatically apply the operation without confirmation").Bool()
+	hostCaReset := hostCa.Command("reset", "Reset host certificate authority keys")
+	hostCaResetConfirm := hostCaReset.Flag("confirm", "Automatically apply the operation without confirmation").Bool()
 
-	hostcaPubkey := hostca.Command("pub-key", "print host certificate authority public key")
+	hostCaPubKey := hostCa.Command("pub-key", "print host certificate authority public key")
 
 	// User CA
-	userca := app.Command("user-ca", "Operations with user certificate authority")
+	userCa := app.Command("user-ca", "Operations with user certificate authority")
 
-	usercaReset := userca.Command("reset", "Reset user certificate authority keys")
-	usercaResetConfirm := usercaReset.Flag("confirm", "Automatically apply the operation without confirmation").Bool()
+	userCaReset := userCa.Command("reset", "Reset user certificate authority keys")
+	userCaResetConfirm := userCaReset.Flag("confirm", "Automatically apply the operation without confirmation").Bool()
 
-	usercaPubkey := userca.Command("pub-key", "Print user certificate authority public key")
+	userCaPubKey := userCa.Command("pub-key", "Print user certificate authority public key")
 
 	// Remote CA
-	remoteca := app.Command("remote-ca", "Operations with remote certificate authority")
+	remoteCa := app.Command("remote-ca", "Operations with remote certificate authority")
 
-	remotecaUpsert := remoteca.Command("upsert", "Upsert remote certificate to trust")
-	remotecaUpsertId := remotecaUpsert.Flag("id", "Certificate id").Required().String()
-	remotecaUpsertFqdn := remotecaUpsert.Flag("fqdn", "FQDN of the remote party").Required().String()
-	remotecaUpsertType := remotecaUpsert.Flag("type", "Cert type (host or user)").Required().String()
-	remotecaUpsertPath := remotecaUpsert.Flag("path", "Cert path (reads from stdout if omitted)").Required().ExistingFile()
-	remotecaUpsertTtl := remotecaUpsert.Flag("ttl", "ttl for certificate to be trusted").Duration()
+	remoteCaUpsert := remoteCa.Command("upsert", "Upsert remote certificate to trust")
+	remoteCaUpsertID := remoteCaUpsert.Flag("id", "Certificate id").Required().String()
+	remoteCaUpsertFQDN := remoteCaUpsert.Flag("fqdn", "FQDN of the remote party").Required().String()
+	remoteCaUpsertType := remoteCaUpsert.Flag("type", "Cert type (host or user)").Required().String()
+	remoteCaUpsertPath := remoteCaUpsert.Flag("path", "Cert path (reads from stdout if omitted)").Required().ExistingFile()
+	remoteCaUpsertTTL := remoteCaUpsert.Flag("ttl", "ttl for certificate to be trusted").Duration()
 
-	remotecaLs := remoteca.Command("ls", "List trusted remote certificates")
-	remotecaLsFqdn := remotecaLs.Flag("fqdn", "FQDN of the remote party").String()
-	remotecaLsType := remotecaLs.Flag("type", "Cert type (host or user)").Required().String()
+	remoteCaLs := remoteCa.Command("ls", "List trusted remote certificates")
+	remoteCaLsFQDN := remoteCaLs.Flag("fqdn", "FQDN of the remote party").String()
+	remoteCaLsType := remoteCaLs.Flag("type", "Cert type (host or user)").Required().String()
 
-	remotecaRm := remoteca.Command("rm", "Remote remote CA from list of trusted certs")
-	remotecaRmId := remotecaRm.Flag("id", "Certificate id").Required().String()
-	remotecaRmFqdn := remotecaRm.Flag("fqdn", "FQDN of the remote party").Required().String()
-	remotecaRmType := remotecaRm.Flag("type", "Cert type (host or user)").Required().String()
+	remoteCaRm := remoteCa.Command("rm", "Remote remote CA from list of trusted certs")
+	remoteCaRmID := remoteCaRm.Flag("id", "Certificate id").Required().String()
+	remoteCaRmFQDN := remoteCaRm.Flag("fqdn", "FQDN of the remote party").Required().String()
+	remoteCaRmType := remoteCaRm.Flag("type", "Cert type (host or user)").Required().String()
 
 	// Secret
 	secret := app.Command("secret", "Operations with secret tokens")
@@ -75,8 +75,8 @@ func (cmd *Command) Run(args []string) error {
 	token := app.Command("token", "Generates provisioning tokens")
 
 	tokenGenerate := token.Command("generate", "Generate provisioning token for server with fqdn")
-	tokenGenerateFqdn := tokenGenerate.Flag("fqdn", "FQDN of the server").Required().String()
-	tokenGenerateTtl := tokenGenerate.Flag("ttl", "Time to live").Default("120").Duration()
+	tokenGenerateFQDN := tokenGenerate.Flag("fqdn", "FQDN of the server").Required().String()
+	tokenGenerateTTL := tokenGenerate.Flag("ttl", "Time to live").Default("120").Duration()
 	tokenGenerateOutput := tokenGenerate.Flag("output", "Optional output file").String()
 
 	// User
@@ -87,18 +87,18 @@ func (cmd *Command) Run(args []string) error {
 	userDelete := user.Command("delete", "Delete user")
 	userDeleteUser := userDelete.Flag("user", "User to delete").Required().String()
 
-	userUpsertkey := user.Command("upsert-key", "Grant access to the user key, returns signed certificate")
-	userUpsertkeyUser := userUpsertkey.Flag("user", "User holding the key").Required().String()
-	userUpsertkeyKeyid := userUpsertkey.Flag("key-id", "SSH key ID").Required().String()
-	userUpsertkeyKey := userUpsertkey.Flag("key", "Path to public key").Required().ExistingFile()
-	userUpsertkeyTtl := userUpsertkey.Flag("ttl", "Access time to live, certificate and access entry will expire when set").Duration()
+	userUpsertKey := user.Command("upsert-key", "Grant access to the user key, returns signed certificate")
+	userUpsertKeyUser := userUpsertKey.Flag("user", "User holding the key").Required().String()
+	userUpsertKeyKeyID := userUpsertKey.Flag("key-id", "SSH key ID").Required().String()
+	userUpsertKeyKey := userUpsertKey.Flag("key", "Path to public key").Required().ExistingFile()
+	userUpsertKeyTTL := userUpsertKey.Flag("ttl", "Access time to live, certificate and access entry will expire when set").Duration()
 
-	userLskeys := user.Command("ls-keys", "List user's keys registered in teleport")
-	userLskeysUser := userLskeys.Flag("user", "User to list keys form").Required().String()
+	userLsKeys := user.Command("ls-keys", "List user's keys registered in teleport")
+	userLsKeysUser := userLsKeys.Flag("user", "User to list keys form").Required().String()
 
-	userSetpass := user.Command("set-pass", "Set user password")
-	userSetpassUser := userSetpass.Flag("user", "User name").Required().String()
-	userSetpassPass := userSetpass.Flag("pass", "Password").Required().String()
+	userSetPass := user.Command("set-pass", "Set user password")
+	userSetPassUser := userSetPass.Flag("user", "User name").Required().String()
+	userSetPassPass := userSetPass.Flag("pass", "Password").Required().String()
 
 	selectedCommand := kingpin.MustParse(app.Parse(args[1:]))
 
@@ -115,25 +115,25 @@ func (cmd *Command) Run(args []string) error {
 
 	switch selectedCommand {
 	// Host CA
-	case hostcaReset.FullCommand():
-		cmd.resetHostCA(*hostcaResetConfirm)
-	case hostcaPubkey.FullCommand():
+	case hostCaReset.FullCommand():
+		cmd.resetHostCA(*hostCaResetConfirm)
+	case hostCaPubKey.FullCommand():
 		cmd.getHostCAPub()
 
 	// User CA
-	case usercaReset.FullCommand():
-		cmd.resetUserCA(*usercaResetConfirm)
-	case usercaPubkey.FullCommand():
+	case userCaReset.FullCommand():
+		cmd.resetUserCA(*userCaResetConfirm)
+	case userCaPubKey.FullCommand():
 		cmd.getUserCAPub()
 
 	// Remote CA
-	case remotecaUpsert.FullCommand():
-		cmd.upsertRemoteCert(*remotecaUpsertId, *remotecaUpsertFqdn,
-			*remotecaUpsertType, *remotecaUpsertPath, *remotecaUpsertTtl)
-	case remotecaLs.FullCommand():
-		cmd.getRemoteCerts(*remotecaLsFqdn, *remotecaLsType)
-	case remotecaRm.FullCommand():
-		cmd.deleteRemoteCert(*remotecaRmId, *remotecaRmFqdn, *remotecaRmType)
+	case remoteCaUpsert.FullCommand():
+		cmd.upsertRemoteCert(*remoteCaUpsertID, *remoteCaUpsertFQDN,
+			*remoteCaUpsertType, *remoteCaUpsertPath, *remoteCaUpsertTTL)
+	case remoteCaLs.FullCommand():
+		cmd.getRemoteCerts(*remoteCaLsFQDN, *remoteCaLsType)
+	case remoteCaRm.FullCommand():
+		cmd.deleteRemoteCert(*remoteCaRmID, *remoteCaRmFQDN, *remoteCaRmType)
 
 	// Secret
 	case secretNew.FullCommand():
@@ -141,7 +141,7 @@ func (cmd *Command) Run(args []string) error {
 
 	// Token
 	case tokenGenerate.FullCommand():
-		cmd.generateToken(*tokenGenerateFqdn, *tokenGenerateTtl,
+		cmd.generateToken(*tokenGenerateFQDN, *tokenGenerateTTL,
 			*tokenGenerateOutput)
 
 	// User
@@ -149,13 +149,13 @@ func (cmd *Command) Run(args []string) error {
 		cmd.getUsers()
 	case userDelete.FullCommand():
 		cmd.deleteUser(*userDeleteUser)
-	case userUpsertkey.FullCommand():
-		cmd.upsertKey(*userUpsertkeyUser, *userUpsertkeyKeyid,
-			*userUpsertkeyKey, *userUpsertkeyTtl)
-	case userLskeys.FullCommand():
-		cmd.getUserKeys(*userLskeysUser)
-	case userSetpass.FullCommand():
-		cmd.setPass(*userSetpassUser, *userSetpassPass)
+	case userUpsertKey.FullCommand():
+		cmd.upsertKey(*userUpsertKeyUser, *userUpsertKeyKeyID,
+			*userUpsertKeyKey, *userUpsertKeyTTL)
+	case userLsKeys.FullCommand():
+		cmd.getUserKeys(*userLsKeysUser)
+	case userSetPass.FullCommand():
+		cmd.setPass(*userSetPassUser, *userSetPassPass)
 	}
 
 	return nil
