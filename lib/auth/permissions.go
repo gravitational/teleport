e3bf3fd81e62d78ccff462ee95ce40f62a80340e
Alex Lyulkov <alex.lyulkov@gmail.com>
2016-02-29 00:18:29 +0300

    Deleted comented code

--- lib/auth/permissions.go
+++ lib/auth/permissions.go
@@ -65,7 +65,6 @@ func NewStandardPermissions() PermissionChecker {
 	}
 
 	sp.permissions[teleport.RoleWeb] = map[string]bool{
-		//ActionSignIn:           true,
 		ActionCreateWebSession: true,
 		ActionGetWebSession:    true,
 		ActionDeleteWebSession: true,
