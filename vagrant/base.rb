27047b1cebea2c05a7f79e6f5b7f040b54d68a22
Ev Kontsevoy <ev@kontsevoy.com>
2016-06-21 13:22:08 -0700

    Better OSX support for Vagrant

---
+++ vagrant/base.rb
@@ -0,0 +1,88 @@
+# common routines shared by Vagrantfiles for different providers
+
+def configure_teleport(vm)
+  vm.provision "file", source: '../teleport.service', destination: '/tmp/teleport.service'
+  vm.provision "shell", inline: <<-SHELL
+    cp -f /tmp/teleport.service /etc/systemd/system/
+    systemctl daemon-reload
+    systemctl enable teleport.service
+    systemctl start teleport.service
+  SHELL
+end
+
+
+def install_docker(vm, docker_version)
+  vm.provision "file", source: '../docker.service', destination: '/tmp/docker.service'
+  vm.provision "file", source: '../docker.socket', destination: '/tmp/docker.socket'
+
+  vm.provision "shell", inline: <<-SHELL
+    echo "Installing Docker..."
+    groupadd docker
+    gpasswd -a vagrant docker
+    ls /tmp/docker*
+    mv /tmp/docker* /etc/systemd/system/
+    if [ ! -s /usr/bin/docker ]; then
+        echo "Downloading Docker #{docker_version}..."
+        wget -qO /usr/bin/docker https://get.docker.com/builds/Linux/x86_64/docker-#{docker_version} 
+        chmod +x /usr/bin/docker
+    fi
+    systemctl daemon-reload
+    systemctl enable docker.socket
+    systemctl enable docker.service
+    echo "Starting Docker..."
+    systemctl restart docker
+  SHELL
+end
+
+
+# this updates all apt packages (especially important for VirtualBox guest addition packages)
+def apt_update(vm)
+  vm.provision "shell", inline: <<-SHELL
+    if [ ! -f /root/apt.updated ]; then
+        apt-get -y update
+        apt-get -y purge exim4-* libcairo*
+        apt-get -y autoremove
+        apt-get -y upgrade
+        apt-get -y dist-upgrade
+        apt-get -y install htop tree vim aufs-tools screen
+        touch /root/apt.updated
+    fi
+  SHELL
+end
+
+
+
+# basic/recommended configuration of every machine:
+def basic_config(vm)
+  hosts = NODES.map { |hostname, addr| "#{addr} #{hostname}" }.join("\n")
+  bashrc="/home/vagrant/.bashrc"
+  vm.provision "shell", inline: <<-SHELL
+    if ! grep -q "git-core" #{bashrc} ; then 
+        echo "customizing ~/bashrc"
+        echo "\n\n# Customizations from Vagrantfile:" >> #{bashrc}
+        echo "export PS1='\\[\\033[31;1m\\]\\h\\[\\033[0;32m\\] \\w\\[\\033[00m\\]: '" >> #{bashrc}
+        echo export PATH="\$PATH:/usr/lib/git-core:/home/vagrant/teleport/build" >> #{bashrc}
+        echo export GREP_OPTIONS="--color=auto" >> #{bashrc}
+        echo "alias ll='ls -lh'" >> #{bashrc}
+    fi
+    if ! grep -q "Teleport" /etc/hosts ; then 
+        echo "# Teleport entries added by Vagrant:" >> /etc/hosts
+        echo -e "#{hosts}" >> /etc/hosts
+    fi
+    mkdir -p /var/lib/teleport
+    chown vagrant:vagrant /var/lib/teleport
+  SHELL
+end
+
+
+# re-creates clean ~/.ssh on a VM, populated with your (host) ssh credentials
+def configure_ssh(vm)
+  vm.provision "shell", inline: <<-SHELL
+    mkdir -p /home/vagrant/.ssh
+    rm -rf /home/vagrant/.ssh/id_rsa*
+    chown vagrant:vagrant /home/vagrant/.ssh
+  SHELL
+  vm.provision "file", source: '~/.ssh/id_rsa', destination: '~/.ssh/id_rsa'
+  vm.provision "file", source: '~/.ssh/id_rsa.pub', destination: '~/.ssh/id_rsa.pub'
+  vm.provision "file", source: '~/.screenrc', destination: '~/' if File.exists? "~/.screnrc"
+end
