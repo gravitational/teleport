ff50f9380b0f246728233b6f223ada03196886cd
Sasha Klizhentas <klizhentas@gmail.com>
2016-12-13 12:15:16 -0800

    compile new js files

--- web/dist/app/styles.f4d0dfc8ba3482dda019.js
+++ web/dist/app/styles.f4d0dfc8ba3482dda019.js
@@ -3,12 +3,12 @@ webpackJsonp([1],{
 /***/ 0:
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(453);
+	module.exports = __webpack_require__(454);
 
 
 /***/ },
 
-/***/ 453:
+/***/ 454:
 /***/ function(module, exports) {
 
 	// removed by extract-text-webpack-plugin
