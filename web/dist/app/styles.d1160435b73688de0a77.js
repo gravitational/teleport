e65d81a5f60e05b687ecdd99470ebec813083e8c
Alexey Kontsevoy <alex@kontsevoy.com>
2017-02-24 12:26:16 -0500

    (web) dist files

--- web/dist/app/styles.d1160435b73688de0a77.js
+++ web/dist/app/styles.d1160435b73688de0a77.js
@@ -3,12 +3,12 @@ webpackJsonp([1],{
 /***/ 0:
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(462);
+	module.exports = __webpack_require__(476);
 
 
 /***/ },
 
-/***/ 462:
+/***/ 476:
 /***/ function(module, exports) {
 
 	// removed by extract-text-webpack-plugin
