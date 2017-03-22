3ab775c9dfceff77c4d208d64d94ea357f7b0be1
Sasha Klizhentas <klizhentas@gmail.com>
2017-03-22 15:34:24 -0700

    handle expiring nodes

--- lib/services/local/presence.go
+++ lib/services/local/presence.go
@@ -163,17 +163,20 @@ func (s *PresenceService) GetNodes(namespace string) ([]services.Server, error) 
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
-	servers := make([]services.Server, len(keys))
-	for i, key := range keys {
+	servers := make([]services.Server, 0, len(keys))
+	for _, key := range keys {
 		data, err := s.GetVal([]string{namespacesPrefix, namespace, nodesPrefix}, key)
 		if err != nil {
+			if trace.IsNotFound(err) {
+				continue
+			}
 			return nil, trace.Wrap(err)
 		}
 		server, err := services.GetServerMarshaler().UnmarshalServer(data, services.KindNode)
 		if err != nil {
 			return nil, trace.Wrap(err)
 		}
-		servers[i] = server
+		servers = append(servers, server)
 	}
 	// sorting helps with tests and makes it all deterministic
 	sort.Sort(services.SortedServers(servers))
