0dfb45f46519da9af129c0893651bf5d177b81d8
Alexey Kontsevoy <alex@kontsevoy.com>
2017-07-19 11:19:16 -0400

    remove user info from beareToken

--- lib/web/apiserver_test.go
+++ lib/web/apiserver_test.go
@@ -530,7 +530,7 @@ func (s *WebSuite) authPackFromResponse(c *C, re *roundtrip.Response) *authPack 
 	clt := s.client(roundtrip.BearerAuth(sess.Token), roundtrip.CookieJar(jar))
 	jar.SetCookies(s.url(), re.Cookies())
 
-	session, user, err := sess.response()
+	session, err := sess.response()
 	if err != nil {
 		panic(err)
 	}
@@ -538,7 +538,6 @@ func (s *WebSuite) authPackFromResponse(c *C, re *roundtrip.Response) *authPack 
 		c.Errorf("expected expiry time to be in the future but got %v", session.ExpiresIn)
 	}
 	return &authPack{
-		user:    user.GetName(),
 		session: session,
 		clt:     clt,
 		cookies: re.Cookies(),
@@ -587,7 +586,7 @@ func (s *WebSuite) authPack(c *C) *authPack {
 	var rawSess *createSessionResponseRaw
 	c.Assert(json.Unmarshal(re.Bytes(), &rawSess), IsNil)
 
-	sess, _, err := rawSess.response()
+	sess, err := rawSess.response()
 	c.Assert(err, IsNil)
 
 	jar, err := cookiejar.New(nil)
