d83d5d16bb5e261c5afa1e96904e3dfd5e3335b1
Ev Kontsevoy <ev@kontsevoy.com>
2017-09-07 10:03:56 -0700

    Fixed the build updating tctl E
    Source-commit: gravitational/teleport.e@bbfd899e4402fbc46ac570d5a5160755ab10b47a

--- e/tool/tctl/user_command.go
+++ e/tool/tctl/user_command.go
@@ -10,8 +10,8 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/buger/goterm"
 	"github.com/gravitational/kingpin"
+	"github.com/gravitational/teleport/lib/asciitable"
 	"github.com/gravitational/teleport/lib/auth"
 	"github.com/gravitational/teleport/lib/service"
 	"github.com/gravitational/teleport/lib/services"
@@ -75,12 +75,13 @@ func (cmd *UserCommandE) List(client *auth.TunClient) error {
 		fmt.Println("No users found")
 		return nil
 	}
-	t := goterm.NewTable(0, 10, 5, ' ', 0)
-	common.PrintHeader(t, []string{"User", "Roles"})
+	t := asciitable.MakeTable([]string{"User", "Roles"})
 	for _, u := range users {
-		fmt.Fprintf(t, "%v\t%v\n", u.GetName(), strings.Join(u.GetRoles(), ","))
+		t.AddRow([]string{
+			u.GetName(), strings.Join(u.GetRoles(), ","),
+		})
 	}
-	fmt.Println(t.String())
+	fmt.Println(t.AsBuffer().String())
 	return nil
 }
 
