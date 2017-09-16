7bba743efa54f123507dfacb5565b20c8a05faa1
Patrick Robinson <patrick.robinson@envato.com>
2017-09-16 19:59:35 +1000

    More unit tests
    with and without encryption

--- lib/backend/dynamo/dynamodbbk_test.go
+++ lib/backend/dynamo/dynamodbbk_test.go
@@ -20,17 +20,19 @@ limitations under the License.
 package dynamo
 
 import (
-	"reflect"
 	"testing"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/kms"
 	"github.com/aws/aws-sdk-go/service/kms/kmsiface"
 
 	"github.com/gravitational/teleport/lib/backend"
 	"github.com/gravitational/teleport/lib/backend/test"
 	"github.com/gravitational/teleport/lib/utils"
+	"github.com/gravitational/trace"
 
 	. "gopkg.in/check.v1"
 )
@@ -105,8 +107,8 @@ func (s *DynamoDBSuite) TestMigration(c *C) {
 	backend, err := New(cfg)
 	c.Assert(err, IsNil)
 	bk := backend.(*DynamoDBBackend)
-	s.bk.kmsSvc = &mockKmsClient{Datakey: []byte("example key 1234")}
-	s.bk.generateDataKey("alias/teleport")
+	bk.kmsSvc = &mockKmsClient{Datakey: []byte("example key 1234")}
+	bk.generateDataKey("alias/teleport")
 
 	var (
 		legacytable      = "legacy.teleport.t"
@@ -155,7 +157,8 @@ func (s *DynamoDBSuite) TestLock(c *C) {
 func (s *DynamoDBSuite) TestValueAndTTL(c *C) {
 	s.suite.ValueAndTTL(c)
 }
-func TestRecord(t *testing.T) {
+
+func (s *DynamoDBSuite) TestRecord(c *C) {
 	r := record{
 		HashKey:   "teleport",
 		FullPath:  "a/directory/path",
@@ -165,13 +168,61 @@ func TestRecord(t *testing.T) {
 	}
 
 	r.encrypt([]byte("encrypteddatakey"), []byte("example key 1234"), "abcde-12345")
-	if reflect.DeepEqual(r.Value, []byte(`{"some":"json","data":true}`)) {
-		t.Error("Expected record to be encrypted but it was not")
-	}
+	c.Assert(r.Value, Not(DeepEquals), []byte(`{"some":"json","data":true}`))
+
 	firstEncryptedValue := r.Value
 	r.decrypt([]byte("example key 1234"))
 	r.encrypt([]byte("encrypteddatakey"), []byte("example key 1234"), "abcde-12345")
-	if reflect.DeepEqual(r.Value, firstEncryptedValue) {
-		t.Error("Expected record value to be different when encrypted twice, but it was the same")
-	}
+	c.Assert(r.Value, Not(DeepEquals), firstEncryptedValue)
+}
+
+func (s *DynamoDBSuite) TestNoEncryption(c *C) {
+	cfg := make(backend.Params)
+	cfg["type"] = "dynamodb"
+	cfg["table_name"] = "teleport.dynamo.testnoencryption"
+	cfg["endpoint"] = "http://localhost:8000"
+	cfg["access_key"] = "access_key"
+	cfg["secret_key"] = "secret_key"
+	backend, err := New(cfg)
+	c.Assert(err, IsNil)
+	bk := backend.(*DynamoDBBackend)
+
+	bucket := []string{"test", "create"}
+	c.Assert(bk.CreateVal(bucket, "one", []byte("1"), time.Duration(0)), IsNil)
+	err = bk.CreateVal(bucket, "one", []byte("2"), time.Duration(0))
+	c.Assert(trace.IsAlreadyExists(err), Equals, true)
+
+	val, err := bk.GetVal(bucket, "one")
+	c.Assert(err, IsNil)
+	c.Assert(string(val), Equals, "1")
+}
+
+func (s *DynamoDBSuite) TestEncryptionEndToEnd(c *C) {
+	tableName := "teleport.dynamo.testencryptionendtoend"
+	cfg := make(backend.Params)
+	cfg["type"] = "dynamodb"
+	cfg["table_name"] = tableName
+	cfg["endpoint"] = "http://localhost:8000"
+	cfg["access_key"] = "access_key"
+	cfg["secret_key"] = "secret_key"
+	backend, err := New(cfg)
+	c.Assert(err, IsNil)
+	bk := backend.(*DynamoDBBackend)
+	bk.kmsSvc = &mockKmsClient{Datakey: []byte("example key 1234")}
+	bk.generateDataKey("alias/teleport")
+
+	bucket := []string{"test", "create"}
+	c.Assert(bk.CreateVal(bucket, "one", []byte("1"), time.Duration(0)), IsNil)
+
+	av, err := dynamodbattribute.MarshalMap(keyLookup{
+		HashKey:  "teleport",
+		FullPath: "teleport/test/create/one",
+	})
+	c.Assert(err, IsNil)
+	input := dynamodb.GetItemInput{Key: av, TableName: aws.String(tableName)}
+	out, err := bk.svc.GetItem(&input)
+	c.Assert(err, IsNil)
+	var r record
+	dynamodbattribute.UnmarshalMap(out.Item, &r)
+	c.Assert(r.Value, Not(DeepEquals), []byte("1"))
 }
