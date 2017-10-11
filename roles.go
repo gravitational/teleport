954070eaa9bf7a045de1314ad2f303fcf47dcf1b
Russell Jones <russell.valerius.jones@gmail.com>
2017-10-10 17:58:41 -0700

    Fix name of role, has to start with uppercase.

--- roles.go
+++ roles.go
@@ -44,7 +44,7 @@ const (
 	// RoleProvisionToken is a role for nodes authenticated using provisioning tokens
 	RoleProvisionToken Role = "ProvisionToken"
 	// RoleTrustedCluster is a role needed for tokens used to add trusted clusters.
-	RoleTrustedCluster Role = "trusted_cluster"
+	RoleTrustedCluster Role = "Trusted_cluster"
 	// RoleSignup is for first time signing up users
 	RoleSignup Role = "Signup"
 	// RoleNop is used for actions that already using external authz mechanisms
