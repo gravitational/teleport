35e380ac9c703473e7c61c4700c885a39aa47340
Sasha Klizhentas <klizhentas@gmail.com>
2017-10-11 15:31:19 -0700

    add TCP load balancer to use in tests

---
+++ lib/utils/loadbalancer.go
@@ -0,0 +1,216 @@
+/*
+Copyright 2017 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils
+
+import (
+	"context"
+	"io"
+	"net"
+	"sync"
+	"time"
+
+	"github.com/gravitational/trace"
+	log "github.com/sirupsen/logrus"
+)
+
+// NewLoadBalancer returns new load balancer listening on frotend
+// and redirecting requests to backends using round robin algo
+func NewLoadBalancer(ctx context.Context, frontend NetAddr, backends []NetAddr) (*LoadBalancer, error) {
+	if ctx == nil {
+		return nil, trace.BadParameter("missing parameter context")
+	}
+	return &LoadBalancer{
+		frontend:     frontend,
+		ctx:          ctx,
+		backends:     backends,
+		currentIndex: -1,
+		Entry: log.WithFields(log.Fields{
+			trace.Component: "loadbalancer",
+			trace.ComponentFields: log.Fields{
+				"listen": frontend.String(),
+			},
+		}),
+	}, nil
+}
+
+// LoadBalancer implements naive round robin TCP load
+// balancer used in tests.
+type LoadBalancer struct {
+	sync.RWMutex
+	*log.Entry
+	frontend       NetAddr
+	backends       []NetAddr
+	ctx            context.Context
+	currentIndex   int
+	listener       net.Listener
+	listenerClosed bool
+}
+
+// AddBackend adds backend
+func (l *LoadBalancer) AddBackend(b NetAddr) {
+	l.Lock()
+	defer l.Unlock()
+	l.backends = append(l.backends, b)
+	l.Debugf("backends %v", l.backends)
+}
+
+// RemoveBackend removes backend
+func (l *LoadBalancer) RemoveBackend(b NetAddr) {
+	l.Lock()
+	defer l.Unlock()
+	l.currentIndex = -1
+	for i := range l.backends {
+		if l.backends[i].Equals(b) {
+			l.backends = append(l.backends[:i], l.backends[i+1:]...)
+			return
+		}
+	}
+
+}
+
+func (l *LoadBalancer) nextBackend() (*NetAddr, error) {
+	l.Lock()
+	defer l.Unlock()
+	if len(l.backends) == 0 {
+		return nil, trace.ConnectionProblem(nil, "no backends")
+	}
+	l.currentIndex = ((l.currentIndex + 1) % len(l.backends))
+	return &l.backends[l.currentIndex], nil
+}
+
+func (l *LoadBalancer) closeListener() {
+	l.Lock()
+	defer l.Unlock()
+	if l.listener == nil {
+		return
+	}
+	if l.listenerClosed {
+		return
+	}
+	l.listenerClosed = true
+	l.listener.Close()
+}
+
+func (l *LoadBalancer) isClosed() bool {
+	l.RLock()
+	defer l.RUnlock()
+	return l.listenerClosed
+}
+
+func (l *LoadBalancer) Close() error {
+	l.closeListener()
+	return nil
+}
+
+// ListenAndServe starts listening socket and serves connections on it
+func (l *LoadBalancer) ListenAndServe() error {
+	if err := l.Listen(); err != nil {
+		return trace.Wrap(err)
+	}
+	return l.Serve()
+}
+
+// Listen creates a listener on the frontend addr
+func (l *LoadBalancer) Listen() error {
+	var err error
+	l.listener, err = net.Listen(l.frontend.AddrNetwork, l.frontend.Addr)
+	if err != nil {
+		return trace.ConvertSystemError(err)
+	}
+	l.Debugf("created listening socket")
+	return nil
+}
+
+// Serve starts accepting connections
+func (l *LoadBalancer) Serve() error {
+	backoffTimer := time.NewTicker(5 * time.Second)
+	defer backoffTimer.Stop()
+	for {
+		conn, err := l.listener.Accept()
+		if err != nil {
+			if l.isClosed() {
+				return trace.ConnectionProblem(nil, "listener is closed")
+			}
+			select {
+			case <-backoffTimer.C:
+				l.Debugf("backoff on network error")
+			case <-l.ctx.Done():
+				return trace.ConnectionProblem(nil, "context is closing")
+			}
+		}
+		go l.forwardConnection(conn)
+	}
+}
+
+func (l *LoadBalancer) forwardConnection(conn net.Conn) {
+	err := l.forward(conn)
+	if err != nil {
+		l.Warningf("failed to forward connection: %v", err)
+	}
+}
+
+func (l *LoadBalancer) forward(conn net.Conn) error {
+	defer conn.Close()
+
+	backend, err := l.nextBackend()
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	backendConn, err := net.Dial(backend.AddrNetwork, backend.Addr)
+	if err != nil {
+		return trace.ConvertSystemError(err)
+	}
+	defer backendConn.Close()
+
+	logger := l.WithFields(log.Fields{
+		"source": conn.RemoteAddr(),
+		"dest":   backendConn.RemoteAddr(),
+	})
+	logger.Debugf("forward")
+
+	messagesC := make(chan error, 2)
+
+	go func() {
+		defer conn.Close()
+		defer backendConn.Close()
+		_, err := io.Copy(conn, backendConn)
+		messagesC <- err
+	}()
+
+	go func() {
+		defer conn.Close()
+		defer backendConn.Close()
+		_, err := io.Copy(backendConn, conn)
+		messagesC <- err
+	}()
+
+	var lastErr error
+	for i := 0; i < 2; i++ {
+		select {
+		case err := <-messagesC:
+			if err != nil && err != io.EOF {
+				logger.Warningf("connection problem: %v %T", trace.DebugReport(err), err)
+				lastErr = err
+			}
+		case <-l.ctx.Done():
+			return trace.ConnectionProblem(nil, "context is closing")
+		}
+	}
+
+	return lastErr
+}
---
+++ lib/utils/loadbalancer_test.go
@@ -0,0 +1,211 @@
+/*
+Copyright 2017 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+
+	"gopkg.in/check.v1"
+)
+
+type LBSuite struct {
+}
+
+var _ = check.Suite(&LBSuite{})
+
+func (s *LBSuite) SetUpSuite(c *check.C) {
+	InitLoggerForTests()
+}
+
+func (s *LBSuite) TestSingleBackendLB(c *check.C) {
+	backend1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "backend 1")
+	}))
+	defer backend1.Close()
+
+	ports, err := GetFreeTCPPorts(1)
+	c.Assert(err, check.IsNil)
+
+	frontend := localAddr(ports[0])
+
+	lb, err := NewLoadBalancer(context.TODO(), frontend, []NetAddr{urlToNetAddr(backend1.URL)})
+	c.Assert(err, check.IsNil)
+	err = lb.Listen()
+	c.Assert(err, check.IsNil)
+	go lb.Serve()
+	defer lb.Close()
+
+	out, err := roundtrip(frontend.String())
+	c.Assert(err, check.IsNil)
+	c.Assert(out, check.Equals, "backend 1")
+}
+
+func (s *LBSuite) TestTwoBackendsLB(c *check.C) {
+	backend1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "backend 1")
+	}))
+	defer backend1.Close()
+
+	backend2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "backend 2")
+	}))
+	defer backend2.Close()
+
+	backend1Addr, backend2Addr := urlToNetAddr(backend1.URL), urlToNetAddr(backend2.URL)
+
+	ports, err := GetFreeTCPPorts(1)
+	c.Assert(err, check.IsNil)
+
+	frontend := localAddr(ports[0])
+
+	lb, err := NewLoadBalancer(context.TODO(), frontend, nil)
+	c.Assert(err, check.IsNil)
+	err = lb.Listen()
+	c.Assert(err, check.IsNil)
+	go lb.Serve()
+	defer lb.Close()
+
+	// no endpoints
+	_, err = roundtrip(frontend.String())
+	c.Assert(err, check.NotNil)
+
+	lb.AddBackend(backend1Addr)
+	out, err := roundtrip(frontend.String())
+	c.Assert(err, check.IsNil)
+	c.Assert(out, check.Equals, "backend 1")
+
+	lb.AddBackend(backend2Addr)
+	out, err = roundtrip(frontend.String())
+	c.Assert(err, check.IsNil)
+	c.Assert(out, check.Equals, "backend 2")
+}
+
+func (s *LBSuite) TestOneFailingBackend(c *check.C) {
+	backend1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "backend 1")
+	}))
+	defer backend1.Close()
+
+	backend2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "backend 2")
+	}))
+	backend2.Close()
+
+	backend1Addr, backend2Addr := urlToNetAddr(backend1.URL), urlToNetAddr(backend2.URL)
+
+	ports, err := GetFreeTCPPorts(1)
+	c.Assert(err, check.IsNil)
+
+	frontend := localAddr(ports[0])
+
+	lb, err := NewLoadBalancer(context.TODO(), frontend, nil)
+	c.Assert(err, check.IsNil)
+	err = lb.Listen()
+	c.Assert(err, check.IsNil)
+	go lb.Serve()
+	defer lb.Close()
+
+	lb.AddBackend(backend1Addr)
+	lb.AddBackend(backend2Addr)
+
+	out, err := roundtrip(frontend.String())
+	c.Assert(err, check.IsNil)
+	c.Assert(out, check.Equals, "backend 1")
+
+	out, err = roundtrip(frontend.String())
+	c.Assert(err, check.NotNil)
+
+	out, err = roundtrip(frontend.String())
+	c.Assert(err, check.IsNil)
+	c.Assert(out, check.Equals, "backend 1")
+}
+
+func (s *LBSuite) TestClose(c *check.C) {
+	backend1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "backend 1")
+	}))
+	defer backend1.Close()
+
+	ports, err := GetFreeTCPPorts(1)
+	c.Assert(err, check.IsNil)
+
+	frontend := localAddr(ports[0])
+
+	lb, err := NewLoadBalancer(context.TODO(), frontend, []NetAddr{urlToNetAddr(backend1.URL)})
+	c.Assert(err, check.IsNil)
+	err = lb.Listen()
+	c.Assert(err, check.IsNil)
+	go lb.Serve()
+	defer lb.Close()
+
+	out, err := roundtrip(frontend.String())
+	c.Assert(err, check.IsNil)
+	c.Assert(out, check.Equals, "backend 1")
+
+	lb.Close()
+	// second close works
+	lb.Close()
+
+	// requests are failing
+	out, err = roundtrip(frontend.String())
+	c.Assert(err, check.NotNil)
+}
+
+func urlToNetAddr(u string) NetAddr {
+	parsed, err := url.Parse(u)
+	if err != nil {
+		panic(err)
+	}
+	return *MustParseAddr(parsed.Host)
+}
+
+func localURL(port string) string {
+	return fmt.Sprintf("http://127.0.0.1:%v", port)
+}
+
+func localAddr(port string) NetAddr {
+	return *MustParseAddr(fmt.Sprintf("127.0.0.1:%v", port))
+}
+
+// roundtrip is a single connection simplistic HTTP client
+// that allows us to bypass a connection pool to test load balancing
+func roundtrip(addr string) (string, error) {
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+	fmt.Fprintf(conn, "GET / HTTP/1.1\r\nHost: 127.0.0.1\r\n\r\n\r\n")
+
+	re, err := http.ReadResponse(bufio.NewReader(conn), nil)
+	if err != nil {
+		return "", err
+	}
+	defer re.Body.Close()
+	out, err := ioutil.ReadAll(re.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
