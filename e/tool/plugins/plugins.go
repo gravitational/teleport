080bcb52e686ee7ef17039611fb930d8ab60a4e3
Roman Tkachenko <roman@gravitational.com>
2017-10-16 11:41:39 -0700

    Implement enterprise plugins in a single interface
    Source-commit: gravitational/teleport.e@a83b3d6a3de3c99caf79066752d00cb4e4b0c760

--- e/tool/plugins/plugins.go
+++ e/tool/plugins/plugins.go
@@ -3,36 +3,38 @@ package plugins
 import (
 	"fmt"
 
-	enterpriseUI "github.com/gravitational/teleport/e/lib/web"
-
 	"github.com/gravitational/teleport"
 	"github.com/gravitational/teleport/lib/plugins"
-	"github.com/gravitational/teleport/lib/web"
 	"github.com/gravitational/trace"
 )
 
 // SetPlugins installs plugins that provide custom behavior for the
 // enterprise compared to the open-source version
 func SetPlugins() {
-	// injects enterprise web handler plugins
-	web.SetPlugin(&enterpriseUI.Plugin{})
+	plugins.SetPlugins(&enterprisePlugins{})
+}
 
-	// role map is mandatory for trusted clusters in enterprise mode
-	plugins.SetEmptyRolesHandler(func() error {
-		return trace.BadParameter("missing 'role_map' parameter")
-	})
+// enterprisePlugins implements pluggable enterprise teleport logic
+type enterprisePlugins struct{}
 
-	// the default role also has "root" for enterprise users
-	plugins.SetDefaultAllowedLogins(func() []string {
-		return []string{teleport.TraitInternalRoleVariable, teleport.Root}
-	})
+// EmptyRolesHandler is called when a new trusted cluster with empty roles
+// is created, for enterprise it returns an error as roles are mandatory
+func (p *enterprisePlugins) EmptyRolesHandler() error {
+	return trace.BadParameter("missing 'role_map' parameter")
+}
 
-	// version specifies that this is an enterprise build
-	plugins.SetVersionPrinter(func() {
-		ver := fmt.Sprintf("Teleport Enterprise v%s", teleport.Version)
-		if teleport.Gitref != "" {
-			ver = fmt.Sprintf("%s git:%s", ver, teleport.Gitref)
-		}
-		fmt.Println(ver)
-	})
+// DefaultAllowedLogins returns allowed logins for a new admin role, for
+// enterprise it includes "root" as well
+func (p *enterprisePlugins) DefaultAllowedLogins() []string {
+	return []string{teleport.TraitInternalRoleVariable, teleport.Root}
+}
+
+// PrintVersion prints teleport version, for enterprise it includes
+// "Enterprise" in the output
+func (p *enterprisePlugins) PrintVersion() {
+	ver := fmt.Sprintf("Teleport Enterprise v%s", teleport.Version)
+	if teleport.Gitref != "" {
+		ver = fmt.Sprintf("%s git:%s", ver, teleport.Gitref)
+	}
+	fmt.Println(ver)
 }
--- e/tool/teleport/main.go
+++ e/tool/teleport/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"os"
 
+	enterpriseUI "github.com/gravitational/teleport/e/lib/web"
 	"github.com/gravitational/teleport/e/tool/plugins"
 
+	"github.com/gravitational/teleport/lib/web"
 	"github.com/gravitational/teleport/tool/teleport/common"
 )
 
 func main() {
+	web.SetPlugin(&enterpriseUI.Plugin{})
 	plugins.SetPlugins()
 	const testRun = false
 	common.Run(os.Args[1:], testRun)
