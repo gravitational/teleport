f8f58e0d1af533ba05726b4fcbda565202439b49
sokoow <sokoow@gmail.com>
2017-10-23 17:40:27 +0000

    Adding disable-tls flag, fixing https://github.com/gravitational/teleport/issues/1304

--- tool/teleport/common/teleport.go
+++ tool/teleport/common/teleport.go
@@ -71,6 +71,8 @@ func Run(cmdlineArgs []string, testRun bool) (executedCommand string, conf *serv
 	start.Flag("debug", "Enable verbose logging to stderr").
 		Short('d').
 		BoolVar(&ccf.Debug)
+	start.Flag("disable-tls", "Disable TLS use").
+		BoolVar(&ccf.DisableTLS)
 	start.Flag("roles",
 		fmt.Sprintf("Comma-separated list of roles to start with [%s]", strings.Join(defaults.StartRoles, ","))).
 		Short('r').
