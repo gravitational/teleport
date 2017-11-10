461341651cb38e25c8f045e0514d69a38680efbe
Sasha Klizhentas <klizhentas@gmail.com>
2017-11-10 15:22:04 -0800

    adjust session TTL for requested certs

--- lib/auth/oidc.go
+++ lib/auth/oidc.go
@@ -213,7 +213,7 @@ func (a *AuthServer) ValidateOIDCAuthCallback(q url.Values) (*OIDCAuthResponse, 
 
 	if len(req.PublicKey) != 0 {
 		certTTL := utils.MinTTL(utils.ToTTL(a.clock, ident.ExpiresAt), req.CertTTL)
-		allowedLogins, err := roles.CheckLoginDuration(certTTL)
+		allowedLogins, err := roles.CheckLoginDuration(roles.AdjustSessionTTL(certTTL))
 		if err != nil {
 			return nil, trace.Wrap(err)
 		}
