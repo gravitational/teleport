c029ea5dd7c54aae39fcdc037dfdc85a5396090c
Russell Jones <russell.valerius.jones@gmail.com>
2018-01-17 21:30:24 +0000

    During proxy transport, if not remote Auth Servers are found, log a warning and exit.

--- lib/reversetunnel/agent.go
+++ lib/reversetunnel/agent.go
@@ -383,7 +383,11 @@ func (a *Agent) proxyTransport(ch ssh.Channel, reqC <-chan *ssh.Request) {
 	if server == RemoteAuthServer {
 		authServers, err := a.Client.GetAuthServers()
 		if err != nil {
-			a.Warningf("unable to find auth servers: %v", err)
+			a.Warningf("Unable retrieve list of remote Auth Servers: %v.", err)
+			return
+		}
+		if len(authServers) == 0 {
+			a.Warningf("No remote Auth Servers returned by client.")
 			return
 		}
 		for _, as := range authServers {
@@ -393,7 +397,7 @@ func (a *Agent) proxyTransport(ch ssh.Channel, reqC <-chan *ssh.Request) {
 		servers = append(servers, server)
 	}
 
-	a.Debugf("got out of band request %v", servers)
+	a.Debugf("Received out-of-band proxy transport request: %v", servers)
 
 	var conn net.Conn
 	var err error
