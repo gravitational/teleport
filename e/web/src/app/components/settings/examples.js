7407110fe225ddff75293e25d7fb8ae0edd34c85
Alexey Kontsevoy <alex@kontsevoy.com>
2018-01-23 11:16:19 -0500

    fix docker build
    Source-commit: gravitational/teleport.e@97ec947c13cf4ae8c610a70a7f338c4bba9db014

--- e/web/src/app/components/settings/examples.js
+++ e/web/src/app/components/settings/examples.js
@@ -1,6 +1,6 @@
-const roleTemplate = require('raw-loader!./../../../../../../../teleport/examples/resources/role.yaml');
-const authTemplate = require('raw-loader!./../../../../../../../teleport/examples/resources/saml-connector.yaml');
-const trustedClusterTemplate = require('raw-loader!./../../../../../../../teleport/examples/resources/trusted_cluster_enterprise.yaml');
+const roleTemplate = require('raw-loader!./../../../../telebase-examples/resources/role.yaml');
+const authTemplate = require('raw-loader!./../../../../telebase-examples/resources/saml-connector.yaml');
+const trustedClusterTemplate = require('raw-loader!./../../../../telebase-examples/resources/trusted_cluster_enterprise.yaml');
 
 export {
   roleTemplate,
