c6abe4ffd38e1f76e15e34a6406f40e6701da345
Alexey Kontsevoy <alex@kontsevoy.com>
2018-03-06 10:11:16 -0500

    (web) update momentjs lib

--- web/dist/app/styles.3b8ea179f2da4dfc3e86.js
+++ web/dist/app/styles.3b8ea179f2da4dfc3e86.js
@@ -3,12 +3,12 @@ webpackJsonp([1],{
 /***/ 0:
 /***/ (function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(547);
+	module.exports = __webpack_require__(568);
 
 
 /***/ }),
 
-/***/ 547:
+/***/ 568:
 /***/ (function(module, exports) {
 
 	// removed by extract-text-webpack-plugin
