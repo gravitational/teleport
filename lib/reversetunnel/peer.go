ec7e53370df2d80f409da8ebfbeaeb8a18e8947b
Russell Jones <russell.valerius.jones@gmail.com>
2018-10-19 22:20:08 +0000

    Fix formatting issues.

--- lib/reversetunnel/peer.go
+++ lib/reversetunnel/peer.go
@@ -57,7 +57,7 @@ func (p *clusterPeers) pickPeer() (*clusterPeer, error) {
 		}
 	}
 	if currentPeer == nil {
-		return nil, trace.NotFound("no active peers found for %v")
+		return nil, trace.NotFound("no active peers found for %v", p.clusterName)
 	}
 	return currentPeer, nil
 }
@@ -159,7 +159,7 @@ type clusterPeer struct {
 }
 
 func (s *clusterPeer) CachingAccessPoint() (auth.AccessPoint, error) {
-	return nil, trace.ConnectionProblem(nil, "unable to fetch access point, this proxy %v has not been discovered yet, try again later")
+	return nil, trace.ConnectionProblem(nil, "unable to fetch access point, this proxy %v has not been discovered yet, try again later", s)
 }
 
 func (s *clusterPeer) GetClient() (auth.ClientI, error) {
