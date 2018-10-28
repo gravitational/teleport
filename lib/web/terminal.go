55c2bee74fb4819f51a01af8d5197daaceb3d9c7
paihu <paihu@users.noreply.github.com>
2018-10-28 02:31:44 +0000

    add buffer to terminal

--- lib/web/terminal.go
+++ lib/web/terminal.go
@@ -161,6 +161,9 @@ type TerminalHandler struct {
 
 	// decoder is used to decode UTF-8 strings.
 	decoder *encoding.Decoder
+
+	// read buffer
+	buffer []byte
 }
 
 // Serve builds a connect to the remote node and then pumps back two types of
@@ -459,6 +462,15 @@ func (t *TerminalHandler) write(data []byte, ws *websocket.Conn) (n int, err err
 // Read unwraps the envelope and either fills out the passed in bytes or
 // performs an action on the connection (sending window-change request).
 func (t *TerminalHandler) read(out []byte, ws *websocket.Conn) (n int, err error) {
+	if len(t.buffer) > 0 {
+		n := copy(out, t.buffer)
+		if n == len(t.buffer) {
+			t.buffer = []byte{}
+		} else {
+			t.buffer = t.buffer[n:]
+		}
+		return n, nil
+	}
 	var bytes []byte
 	err = websocket.Message.Receive(ws, &bytes)
 	if err != nil {
@@ -486,10 +498,8 @@ func (t *TerminalHandler) read(out []byte, ws *websocket.Conn) (n int, err error
 
 	switch string(envelope.GetType()) {
 	case defaults.WebsocketRaw:
-		if len(out) < len(data) {
-			t.log.Warnf("websocket failed to receive everything: %d vs %d", len(out), len(data))
-		}
-		return copy(out, data), nil
+		t.buffer = data
+		return 0, nil
 	case defaults.WebsocketResize:
 		var e events.EventFields
 		err := json.Unmarshal(data, &e)
