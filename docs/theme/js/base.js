21eaa28c83d894a26c4b213de0e7ad45ce3e0061
Alex Wolfe <thewolfe@gmail.com>
2018-10-29 10:13:05 -0700

    code formatting

--- docs/theme/js/base.js
+++ docs/theme/js/base.js
@@ -64,7 +64,7 @@ $(document).ready(function() {
     new grvlib.TopNav();
     new grvlib.SecondaryNav();
     new grvlib.SideNav({pinned: true});
-    grvlib.buttonSmoothScroll(topPadding);
+    // grvlib.buttonSmoothScroll(topPadding);
     grvlib.buttonRipple();
   }
 });
