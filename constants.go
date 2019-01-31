6b5bfc9eba080e35a08d43e2d103ed5e3316c654
Alexey Kontsevoy <biz.kovoy@gmail.com>
2019-01-31 08:14:47 -0500

    Use xdg-open to open a default browser (#2536)

--- constants.go
+++ constants.go
@@ -488,7 +488,7 @@ const (
 
 const (
 	// OpenBrowserLinux is the command used to open a web browser on Linux.
-	OpenBrowserLinux = "sensible-browser"
+	OpenBrowserLinux = "xdg-open"
 
 	// OpenBrowserDarwin is the command used to open a web browser on macOS/Darwin.
 	OpenBrowserDarwin = "open"
