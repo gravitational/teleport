c72bf13864b3760fdd03b9433d03a6ac6629a727
Sasha Klizhentas <klizhentas@gmail.com>
2019-02-09 20:57:20 -0800

    Marshal YAML as list of documents, fixes #2479
    This commit fixes an issue when tctl
    output in YAML was not compatible with tctl create.
    
    Before:
    
    ```
    $ tctl get users
    
    - User: value
    - User: value
    ```
    
    After:
    
    ```
    $ tctl get users
    
    User: value
    ---
    User: value
    ```

--- lib/utils/jsontools.go
+++ lib/utils/jsontools.go
@@ -18,12 +18,15 @@ package utils
 
 import (
 	"bytes"
+	"io"
+	"reflect"
 	"unicode"
 
 	"github.com/gravitational/trace"
 
 	"github.com/ghodss/yaml"
 	"github.com/json-iterator/go"
+	kyaml "k8s.io/apimachinery/pkg/util/yaml"
 )
 
 // ToJSON converts a single YAML document into a JSON document
@@ -76,4 +79,88 @@ func FastMarshal(v interface{}) ([]byte, error) {
 	}
 
 	return data, nil
+}
+
+const yamlDocDelimiter = "---"
+
+// WriteYAML detects whether value is a list
+// and marshals multiple documents delimited by `---`, otherwise, marshals
+// a single value
+func WriteYAML(w io.Writer, values interface{}) error {
+	if reflect.TypeOf(values).Kind() != reflect.Slice {
+		return trace.Wrap(writeYAML(w, values))
+	}
+	// first pass makes sure that all values are documents (objects or maps)
+	slice := reflect.ValueOf(values)
+	allDocs := func() bool {
+		for i := 0; i < slice.Len(); i++ {
+			if !isDoc(slice.Index(i)) {
+				return false
+			}
+		}
+		return true
+	}
+	if !allDocs() {
+		return trace.Wrap(writeYAML(w, values))
+	}
+	// second pass can marshal documents
+	for i := 0; i < slice.Len(); i++ {
+		err := writeYAML(w, slice.Index(i).Interface())
+		if err != nil {
+			return trace.Wrap(err)
+		}
+		if i != slice.Len()-1 {
+			if _, err := w.Write([]byte(yamlDocDelimiter + "\n")); err != nil {
+				return trace.Wrap(err)
+			}
+		}
+	}
+	return nil
+}
+
+// isDoc detects whether value constitues a document
+func isDoc(val reflect.Value) bool {
+	iterations := 0
+	for val.Kind() == reflect.Interface || val.Kind() == reflect.Ptr {
+		val = val.Elem()
+		// preventing cycles
+		iterations++
+		if iterations > 10 {
+			return false
+		}
+	}
+	return val.Kind() == reflect.Struct || val.Kind() == reflect.Map
+}
+
+// writeYAML writes marshaled YAML to writer
+func writeYAML(w io.Writer, values interface{}) error {
+	data, err := yaml.Marshal(values)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	_, err = w.Write(data)
+	return trace.Wrap(err)
+}
+
+// ReadYAML can unmarshal a stream of documents, used in tests.
+func ReadYAML(reader io.Reader) (interface{}, error) {
+	decoder := kyaml.NewYAMLOrJSONDecoder(reader, 32*1024)
+	var values []interface{}
+	for {
+		var val interface{}
+		err := decoder.Decode(&val)
+		if err != nil {
+			if err == io.EOF {
+				if len(values) == 0 {
+					return nil, trace.BadParameter("no resources found, empty input?")
+				}
+				if len(values) == 1 {
+					return values[0], nil
+				}
+				return values, nil
+			}
+			return nil, trace.Wrap(err)
+		}
+		values = append(values, val)
+	}
 }
