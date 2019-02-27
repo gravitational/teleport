83f47f76ab7208d0543f46bc1b924a59d2e1305c
Gus Luxton <gus@gravitational.com>
2019-02-27 18:56:06 +0000

    Add --force-upgrade flag to Tiller for potentially different Helm versions

--- examples/chart/teleport-demo/gke-init.sh
+++ examples/chart/teleport-demo/gke-init.sh
@@ -2,4 +2,4 @@
 # this is needed to give tiller admin permissions on GKE
 kubectl --namespace kube-system create serviceaccount tiller
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
-helm init --service-account tiller --upgrade --wait
+helm init --service-account tiller --upgrade --force-upgrade --wait
