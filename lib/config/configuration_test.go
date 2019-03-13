gravitational/teleport

commit 6bae91466f00a25f00b2199390d979a91c1a7a29

Do not override syslog target, fixes #2550 (#2551)
Whenever teleport was started with -d flag,
output target was always overwritten to stderr.

This commit makes sure that target supplied
in the configuration is not overwritten
even in case when -d flag is set.

lib/config/configuration.go
@@ -204,7 +204,11 @@ func ApplyFileConfig(fc *FileConfig, cfg *service.Config) error {
 	case "stdout", "out", "1":
 		log.SetOutput(os.Stdout)
 	case teleport.Syslog:
-		utils.SwitchLoggingtoSyslog()
+		err := utils.SwitchLoggingtoSyslog()
+		if err != nil {
+			// this error will go to stderr
+			log.Errorf("Failed to switch logging to syslog: %v.", err)
+		}
 	default:
 		// assume it's a file path:
 		logFile, err := os.Create(fc.Logger.Output)
@@ -220,14 +224,13 @@ func ApplyFileConfig(fc *FileConfig, cfg *service.Config) error {
 		log.SetLevel(log.InfoLevel)
 	case "err", "error":
 		log.SetLevel(log.ErrorLevel)
-	case "debug":
+	case teleport.DebugLevel:
 		log.SetLevel(log.DebugLevel)
 	case "warn", "warning":
 		log.SetLevel(log.WarnLevel)
 	default:
 		return trace.BadParameter("unsupported logger severity: '%v'", fc.Logger.Severity)
 	}
-
 	// apply cache policy for node and proxy
 	cachePolicy, err := fc.CachePolicy.Parse()
 	if err != nil {
@@ -808,6 +811,12 @@ func Configure(clf *CommandLineFlags, cfg *service.Config) error {
 			return trace.Wrap(err)
 		}
 	}
+
+	// apply command line --debug flag to override logger severity
+	if clf.Debug {
+		fileConf.Logger.Severity = teleport.DebugLevel
+	}
+
 	if err = ApplyFileConfig(fileConf, cfg); err != nil {
 		return trace.Wrap(err)
 	}
@@ -826,10 +835,9 @@ func Configure(clf *CommandLineFlags, cfg *service.Config) error {
 		cfg.Proxy.DisableTLS = clf.DisableTLS
 	}
 
-	// apply --debug flag:
+	// apply --debug flag to config:
 	if clf.Debug {
 		cfg.Console = ioutil.Discard
-		utils.InitLogger(utils.LoggingForDaemon, log.DebugLevel)
 		cfg.Debug = clf.Debug
 	}
 

constants.go
@@ -307,6 +307,9 @@ const (
 
 	// HumanDateFormatMilli is a human readable date formatting with milliseconds
 	HumanDateFormatMilli = "Jan _2 15:04:05.000 UTC"
+
+	// DebugLevel is a debug logging level name
+	DebugLevel = "debug"
 )
 
 // Component generates "component:subcomponent1:subcomponent2" strings used

lib/utils/syslog.go
@@ -23,21 +23,23 @@ import (
 	"log/syslog"
 	"os"
 
+	"github.com/gravitational/trace"
 	log "github.com/sirupsen/logrus"
 	logrusSyslog "github.com/sirupsen/logrus/hooks/syslog"
 )
 
 // SwitchLoggingtoSyslog tells the logger to send the output to syslog. This
 // code is behind a build flag because Windows does not support syslog.
-func SwitchLoggingtoSyslog() {
+func SwitchLoggingtoSyslog() error {
 	log.StandardLogger().SetHooks(make(log.LevelHooks))
 	hook, err := logrusSyslog.NewSyslogHook("", "", syslog.LOG_WARNING, "")
 	if err != nil {
-		// syslog not available
+		// syslog is not available
 		log.SetOutput(os.Stderr)
-	} else {
-		// ... and disable stderr:
-		log.AddHook(hook)
-		log.SetOutput(ioutil.Discard)
+		return trace.Wrap(err)
 	}
+	log.AddHook(hook)
+	// ... and disable stderr:
+	log.SetOutput(ioutil.Discard)
+	return nil
 }

commit 7b2f6ef07640e754f43f048a8b7f83924fd9bfe6

Fix a runtime error caused by usage of debug CLI flag
A change introduced in 6bae91466f00a25f00b2199390d979a91c1a7a29
causes Teleport to crash when running with the debug command-line
(`-d`) without any configuration file:

```
panic: runtime error: invalid memory address or nil pointer dereference
[signal SIGSEGV: segmentation violation code=0x1 addr=0x98 pc=0x1608bd9]

goroutine 1 [running]:
github.com/gravitational/teleport/lib/config.Configure(0xc00014a9a0, 0xc0002e9100, 0x5, 0x1)
    /gopath/src/github.com/gravitational/teleport/lib/config/configuration.go:816 +0xa9
github.com/gravitational/teleport/tool/teleport/common.Run(0xc0000b0050, 0x3, 0x3, 0x0, 0x0, 0xc00002ff88, 0xc00008e058)
    /gopath/src/github.com/gravitational/teleport/tool/teleport/common/teleport.go:147 +0x2912
main.main()
    /gopath/src/github.com/gravitational/teleport/tool/teleport/main.go:26 +0x67
```

`fileConf` is nil when we run Teleport without any configuration file.
When we try to turn on debugging from the command-line, it will try to
override the debug configuration in `fileConf`, but it will fail to
do so as it is not initialised.

This commit fixes the problem by ensuring `fileConf` is not nil
before trying to configure it. The behaviour mimics that of `ApplyFileConfig`
which simply short-circuits, and returns early when `fileConf` is nil.

A test was also added to ensure the debug configuration works.
The test would have caught the exception.

lib/config/configuration.go
@@ -813,7 +813,7 @@ func Configure(clf *CommandLineFlags, cfg *service.Config) error {
 	}
 
 	// apply command line --debug flag to override logger severity
-	if clf.Debug {
+	if fileConf != nil && clf.Debug {
 		fileConf.Logger.Severity = teleport.DebugLevel
 	}
 

lib/config/configuration_test.go
@@ -725,6 +725,18 @@ ssh_service:
 	}
 }
 
+// TestDebugFlag ensures that the debug command-line flag is correctly set in the config.
+func (s *ConfigTestSuite) TestDebugFlag(c *check.C) {
+	clf := CommandLineFlags{
+		Debug: true,
+	}
+	cfg := service.MakeDefaultConfig()
+	c.Assert(cfg.Debug, check.Equals, false)
+	err := Configure(&clf, cfg)
+	c.Assert(err, check.IsNil)
+	c.Assert(cfg.Debug, check.Equals, true)
+}
+
 func (s *ConfigTestSuite) TestLicenseFile(c *check.C) {
 	testCases := []struct {
 		path   string