d346f2b124bbf689c0483aeee0295f2228b4ddc4
Joshua Durbin <joshua.durbin@bigcommerce.com>
2019-06-18 13:35:21 -0700

    adds support for GCP HA environments with gcs recording storage, firestore-backed events, and firestore backend storage

---
+++ lib/backend/firestore/doc.go
@@ -0,0 +1,9 @@
+/*
+Package firestoreFirestoreBackend implements Firestore storage backend
+for Teleport auth service, similar to DynamoDB backend.
+
+firestore package implements the FirestoreBackend storage back-end for the
+auth server. Originally contributed by https://github.com/joshdurbin
+
+*/
+package firestore
