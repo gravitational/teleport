1c35089f19c1ff93e69f4b2bae864f7c6733cce3
Alexey Kontsevoy <biz.kovoy@gmail.com>
2019-06-27 17:18:36 -0400

    Fix regression bug with active sessions  (#2809)
    * Fix regression bug with active sessions
    
    * Dist and e-ref

--- web/dist/app/app.d57cf48376d31bb5f8a8.js
+++ web/dist/app/app.d57cf48376d31bb5f8a8.js
@@ -13644,7 +13644,6 @@ var ActiveSessionRec = Object(immutable["Record"])({
   id: undefined,
   namespace: undefined,
   login: undefined,
-  active: undefined,
   created: undefined,
   last_active: undefined,
   server_id: undefined,
@@ -13677,9 +13676,7 @@ function activeSessionStore_receive(state, _ref) {
 
   var jsonArray = json || [];
   var newState = activeSessionStore_defaultState().withMutations(function (newState) {
-    return jsonArray.filter(function (item) {
-      return item.active === true;
-    }).forEach(function (item) {
+    return jsonArray.forEach(function (item) {
       var rec = createSessionRec(siteId, item);
       newState.set(rec.id, rec);
     });
