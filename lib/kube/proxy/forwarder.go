55bf093fa65278a536f9d2ae9dec22738bf0262a
Gus Luxton <gus@gravitational.com>
2019-08-13 07:23:59 -0300

    Update forwarder.go

--- lib/kube/proxy/forwarder.go
+++ lib/kube/proxy/forwarder.go
@@ -284,7 +284,7 @@ func (f *Forwarder) authenticate(req *http.Request) (*authContext, error) {
 	case auth.RemoteUser:
 		isRemoteUser = true
 	case auth.BuiltinRole:
-		f.Warningf("Denying proxy access to unauthenticated user of type %T - this can sometimes be caused by inadvertently sending Kubernetes traffic to the proxy's web port.", userTypeI)
+		f.Warningf("Denying proxy access to unauthenticated user of type %T - this can sometimes be caused by inadvertently using an HTTP load balancer instead of a TCP load balancer on the Kubernetes port.", userTypeI)
 	default:
 		f.Warningf("Denying proxy access to unsupported user type: %T.", userTypeI)
 		return nil, trace.AccessDenied(accessDeniedMsg)
