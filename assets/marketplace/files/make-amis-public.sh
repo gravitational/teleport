92f0cf8c56afceb30e344e155c660c83d07a8bd4
Gus Luxton <gus@gravitational.com>
2019-10-02 20:23:15 -0300

    Update extra variables

--- assets/marketplace/files/make-amis-public.sh
+++ assets/marketplace/files/make-amis-public.sh
@@ -6,7 +6,7 @@ REGION_LIST="eu-west-1 us-east-1 us-east-2 us-west-2"
 
 # Exit if oss/ent parameters not provided
 if [[ "$1" == "" ]]; then
-    echo "Usage: $(basename $0) [oss/ent]"
+    echo "Usage: $(basename $0) [oss/ent/ent-fips]"
     exit 1
 else
     RUN_MODE="$1"
@@ -15,6 +15,14 @@ fi
 ABSPATH=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "${ABSPATH}")
 BUILD_DIR=$(readlink -f "${SCRIPT_DIR}/build")
+
+AMI_TAG="production"
+OUTFILE="amis.txt"
+# Conditionally set tag for FIPS
+if [[ "${RUN_MODE}" == "ent-fips" ]];
+    AMI_TAG="production-fips"
+    OUTFILE="amis-fips.txt"
+fi
 
 # Remove existing AMI ID file if present
 if [ -f "${BUILD_DIR}/amis.txt" ]; then
@@ -29,29 +37,21 @@ if [ ! -f "${TIMESTAMP_FILE}" ]; then
 fi
 BUILD_TIMESTAMP=$(<"${TIMESTAMP_FILE}")
 
-# Read FIPS build timestamp from file
-FIPS_TIMESTAMP_FILE="${BUILD_DIR}/${RUN_MODE}_fips_build_timestamp.txt"
-if [ ! -f "${FIPS_TIMESTAMP_FILE}" ]; then
-    echo 'Cannot find "${TIMESTAMP_FILE}"'
-    exit 1
-fi
-FIPS_BUILD_TIMESTAMP=$(<"${FIPS_TIMESTAMP_FILE}")
-
 # Write AMI ID for each region to AMI ID file
 for REGION in ${REGION_LIST}; do
-    aws ec2 describe-images --region ${REGION} --filters "Name=tag:BuildTimestamp,Values=${BUILD_TIMESTAMP}" "Name=tag:BuildType,Values=production" > "${BUILD_DIR}/${REGION}.json"
+    aws ec2 describe-images --region ${REGION} --filters "Name=tag:BuildTimestamp,Values=${BUILD_TIMESTAMP}" "Name=tag:BuildType,Values=${AMI_TAG}" > "${BUILD_DIR}/${REGION}.json"
     AMI_ID=$(jq --raw-output '.Images[0].ImageId' "${BUILD_DIR}/${REGION}.json")
     if [[ "${AMI_ID}" == "" || "${AMI_ID}" == "null" ]]; then
         echo "Error: cannot get AMI ID for ${REGION}"
         exit 2
     fi
     rm -f "${BUILD_DIR}/${REGION}.json"
-    echo "${REGION}=${AMI_ID}" >> "${BUILD_DIR}/amis.txt"
+    echo "${REGION}=${AMI_ID}" >> "${BUILD_DIR}/${OUTFILE}.txt"
 done
 
 # Make each AMI public (set launchPermission to 'all')
 for REGION in ${REGION_LIST}; do
-    AMI_ID=$(grep ${REGION} "${BUILD_DIR}/amis.txt" | awk -F= '{print $2}')
+    AMI_ID=$(grep ${REGION} "${BUILD_DIR}/${OUTFILE}.txt" | awk -F= '{print $2}')
     if [[ "${AMI_ID}" == "" || "${AMI_ID}" == "null" ]]; then
         echo "Error: cannot get AMI ID for ${REGION}"
         exit 3
@@ -60,30 +60,3 @@ for REGION in ${REGION_LIST}; do
         echo "AMI ID ${AMI_ID} for ${REGION} set to public"
     fi
 done
-
-# Process FIPS AMIs when running in 'ent' mode
-if [[ "${RUN_MODE}" == "ent" ]]; then
-    # Write FIPS AMI ID for each region to FIPS AMI ID file
-    for REGION in ${REGION_LIST}; do
-        aws ec2 describe-images --region ${REGION} --filters "Name=tag:BuildTimestamp,Values=${BUILD_TIMESTAMP}" "Name=tag:BuildType,Values=production-fips" > "${BUILD_DIR}/fips-${REGION}.json"
-        AMI_ID=$(jq --raw-output '.Images[0].ImageId' "${BUILD_DIR}/fips-${REGION}.json")
-        if [[ "${AMI_ID}" == "" || "${AMI_ID}" == "null" ]]; then
-            echo "Error: cannot get AMI ID for ${REGION}"
-            exit 2
-        fi
-        rm -f "${BUILD_DIR}/fips-${REGION}.json"
-        echo "${REGION}=${AMI_ID}" >> "${BUILD_DIR}/amis-fips.txt"
-    done
-
-    # Make each FIPS AMI public (set launchPermission to 'all')
-    for REGION in ${REGION_LIST}; do
-        AMI_ID=$(grep ${REGION} "${BUILD_DIR}/amis-fips.txt" | awk -F= '{print $2}')
-        if [[ "${AMI_ID}" == "" || "${AMI_ID}" == "null" ]]; then
-            echo "Error: cannot get FIPS AMI ID for ${REGION}"
-            exit 3
-        else
-            aws ec2 modify-image-attribute --region ${REGION} --image-id ${AMI_ID} --launch-permission "Add=[{Group=all}]"
-            echo "FIPS AMI ID ${AMI_ID} for ${REGION} set to public"
-        fi
-    done
-fi
