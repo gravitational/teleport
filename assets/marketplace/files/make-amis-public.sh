53a4031ca3beba5cb9e9e2bbe4a14d5b57a43aed
Gus Luxton <gus@gravitational.com>
2020-01-14 11:09:09 -0400

    Add additional AWS regions now supporting encryption at rest

--- assets/marketplace/files/make-amis-public.sh
+++ assets/marketplace/files/make-amis-public.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Define list of regions to run in
-REGION_LIST="eu-west-1 us-east-1 us-east-2 us-west-2"
+REGION_LIST="us-east-1 us-east-2 us-west-1 us-west-2 ap-south-1 ap-northeast-2 ap-southeast-1 ap-southeast-2 ap-northeast-1 ca-central-1 eu-central-1 eu-west-1 eu-west-2 sa-east-1"
 
 # Exit if oss/ent parameters not provided
 if [[ "$1" == "" ]]; then
