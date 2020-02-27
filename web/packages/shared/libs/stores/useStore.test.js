9aded617f36e6506dcf7ead7645f0834858508e6
Lisa Kim <lisa@gravitational.com>
2020-02-27 13:41:02 -0800

    Unit test useStore (#37)

---
+++ web/packages/shared/libs/stores/useStore.test.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2020 Gravitational, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import useStore from './useStore';
+import Store from './store';
+import { render, wait } from 'design/utils/testing';
+
+test('components subscribes to store changes and unsubscribes on unmount', async () => {
+  const store = new Store();
+
+  store.setState({
+    firstname: 'bob',
+    lastname: 'smith',
+  });
+
+  const { unmount, container } = render(<Component store={store} />);
+
+  expect(container.innerHTML).toBe(JSON.stringify(store.state));
+
+  await wait(() => {
+    store.setState({
+      firstname: 'alex',
+    });
+  });
+
+  expect(container.innerHTML).toBe(
+    JSON.stringify({
+      firstname: 'alex',
+      lastname: 'smith',
+    })
+  );
+
+  jest.spyOn(store, 'unsubscribe');
+  unmount();
+  expect(store.unsubscribe).toHaveBeenCalledTimes(1);
+});
+
+function Component({ store }) {
+  // subscribes to store updates
+  useStore(store);
+  return <>{JSON.stringify(store.state)}</>;
+}
