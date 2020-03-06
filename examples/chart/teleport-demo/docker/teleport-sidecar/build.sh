c8687a45772d5207098242ce4c00cec4c5282a5a
Steven Martin <steven@gravitational.com>
2020-03-06 15:40:28 -0500

    Can use project parameter
    Project parameter can be passed for the build and push docker registry

--- examples/chart/teleport-demo/docker/teleport-sidecar/build.sh
+++ examples/chart/teleport-demo/docker/teleport-sidecar/build.sh
@@ -1,15 +1,25 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
+ONE=$1
+TWO=$2
 VERSION=3.2.0
 if [[ "$1" != "" ]]; then
     VERSION=$1
     shift
 fi
+GCPROJECT=kubeadm-167321
+if [[ "" != "${TWO}" ]]; then
+    GCPROJECT=${TWO}
+    shift
+fi
+echo "${VERSION} version"
+echo "${GCPROJECT} project"
+
 docker build --pull \
-    -t gcr.io/kubeadm-167321/teleport-sidecar:${VERSION} \
-    -t gcr.io/kubeadm-167321/teleport-sidecar:latest \
-    --cache-from gcr.io/kubeadm-167321/teleport-sidecar:latest \
+    -t gcr.io/${GCPROJECT}/teleport-sidecar:${VERSION} \
+    -t gcr.io/${GCPROJECT}/teleport-sidecar:latest \
+    --cache-from gcr.io/${GCPROJECT}/teleport-sidecar:latest \
     --build-arg TELEPORT_VERSION=${VERSION} \
-    . $*
-docker push gcr.io/kubeadm-167321/teleport-sidecar:${VERSION}
-docker push gcr.io/kubeadm-167321/teleport-sidecar:latest
+    . 
+docker push gcr.io/${GCPROJECT}/teleport-sidecar:${VERSION}
+docker push gcr.io/${GCPROJECT}/teleport-sidecar:latest
