18745fc57b169472f6b578967ca65ef7c8cc4ac9
Steven Martin <steven@gravitational.com>
2020-03-06 15:33:38 -0500

    Docker build scripts update
    Provides a new parameter option to pass in a different project name for the docker push

--- examples/chart/teleport-demo/docker/build-all.sh
+++ examples/chart/teleport-demo/docker/build-all.sh
@@ -1,15 +1,25 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
+ONE=$1
+TWO=$2
 VERSION=3.2.0
 if [[ "$1" != "" ]]; then
     VERSION=$1
     shift
 fi
-set -e
+GCPROJECT=kubeadm-167321
+if [[ "" != "${TWO}" ]]; then
+    GCPROJECT=${TWO}
+    shift
+fi
+echo "${VERSION} version"
+echo "${GCPROJECT} project"
+
+
 for f in *; do
     if [[ -d $f ]]; then
         pushd $f
-        ./build.sh ${VERSION} "$@"
+        ./build.sh ${VERSION} ${GCPROJECT} "$@"
         popd
     fi
 done
