986c4792ff37b2899ba812978939acd3a0982a62
Steven Martin <steven@gravitational.com>
2020-03-06 15:35:53 -0500

    Uses project parameter
    Can use project parameter as an option for docker push

--- examples/chart/teleport-demo/docker/cloudflare-agent/build.sh
+++ examples/chart/teleport-demo/docker/cloudflare-agent/build.sh
@@ -1,15 +1,31 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
+ONE=$1
+TWO=$2
 VERSION=3.2.0
 if [[ "$1" != "" ]]; then
     VERSION=$1
     shift
 fi
+GCPROJECT=kubeadm-167321
+if [[ "" != "${TWO}" ]]; then
+    GCPROJECT=${TWO}
+    shift
+fi
+echo "${VERSION} version"
+echo "${GCPROJECT} project"
+
 docker pull quay.io/gravitational/debian-grande:buster
+echo "docker build --pull \
+    -t gcr.io/${GCPROJECT}/cloudflare-agent:${VERSION} \
+    -t gcr.io/${GCPROJECT}/cloudflare-agent:latest \
+    --cache-from quay.io/gravitational/debian-grande:buster,gcr.io/${GCPROJECT}/cloudflare-agent:latest \
+    .  "
 docker build --pull \
-    -t gcr.io/kubeadm-167321/cloudflare-agent:${VERSION} \
-    -t gcr.io/kubeadm-167321/cloudflare-agent:latest \
-    --cache-from quay.io/gravitational/debian-grande:buster,gcr.io/kubeadm-167321/cloudflare-agent:latest \
-    . $*
-docker push gcr.io/kubeadm-167321/cloudflare-agent:${VERSION}
-docker push gcr.io/kubeadm-167321/cloudflare-agent:latest
+    -t gcr.io/${GCPROJECT}/cloudflare-agent:${VERSION} \
+    -t gcr.io/${GCPROJECT}/cloudflare-agent:latest \
+    --cache-from quay.io/gravitational/debian-grande:buster,gcr.io/${GCPROJECT}/cloudflare-agent:latest \
+    .   
+docker push gcr.io/${GCPROJECT}/cloudflare-agent:${VERSION}
+docker push gcr.io/${GCPROJECT}/cloudflare-agent:latest
+
