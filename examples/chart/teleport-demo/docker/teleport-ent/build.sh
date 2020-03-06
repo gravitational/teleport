a4beac2de02d00dc790dd27b13093fd8e7310105
Steven Martin <steven@gravitational.com>
2020-03-06 15:39:05 -0500

    Can use project parameter
    Can now pass a project parameter for building and pushing to a specific project docker registory

--- examples/chart/teleport-demo/docker/teleport-ent/build.sh
+++ examples/chart/teleport-demo/docker/teleport-ent/build.sh
@@ -1,15 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
+ONE=$1
+TWO=$2
 VERSION=3.2.0
 if [[ "$1" != "" ]]; then
     VERSION=$1
     shift
 fi
+GCPROJECT=kubeadm-167321
+if [[ "" != "${TWO}" ]]; then
+    GCPROJECT=${TWO}
+    shift
+fi
+
 docker build --pull \
-    -t gcr.io/kubeadm-167321/teleport-ent:${VERSION} \
-    -t gcr.io/kubeadm-167321/teleport-ent:latest \
-    --cache-from gcr.io/kubeadm-167321/teleport-ent:latest \
+    -t gcr.io/${GCPROJECT}/teleport-ent:${VERSION} \
+    -t gcr.io/${GCPROJECT}/teleport-ent:latest \
+    --cache-from gcr.io/${GCPROJECT}/teleport-ent:latest \
     --build-arg TELEPORT_VERSION=${VERSION} \
-    . $*
-docker push gcr.io/kubeadm-167321/teleport-ent:${VERSION}
-docker push gcr.io/kubeadm-167321/teleport-ent:latest
+    . 
+docker push gcr.io/${GCPROJECT}/teleport-ent:${VERSION}
+docker push gcr.io/${GCPROJECT}/teleport-ent:latest
+
