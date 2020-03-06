32837ac448ede7e6523a2cac0ef826d54bd635d1
Steven Martin <steven@gravitational.com>
2020-03-06 15:37:34 -0500

    Uses project parameter
    Can use project parameter  for the docker push

--- examples/chart/teleport-demo/docker/namespace-cleaner/build.sh
+++ examples/chart/teleport-demo/docker/namespace-cleaner/build.sh
@@ -1,15 +1,27 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
+ONE=$1
+TWO=$2
 VERSION=3.2.0
 if [[ "$1" != "" ]]; then
     VERSION=$1
     shift
 fi
+GCPROJECT=kubeadm-167321
+if [[ "" != "${TWO}" ]]; then
+    GCPROJECT=${TWO}
+    shift
+fi
+echo "${VERSION} version"
+echo "${GCPROJECT} project"
+
+
 docker pull quay.io/gravitational/debian-grande:buster
 docker build --pull \
-    -t gcr.io/kubeadm-167321/namespace-cleaner:${VERSION} \
-    -t gcr.io/kubeadm-167321/namespace-cleaner:latest \
-    --cache-from quay.io/gravitational/debian-grande:buster,gcr.io/kubeadm-167321/namespace-cleaner:latest \
-    . $*
-docker push gcr.io/kubeadm-167321/namespace-cleaner:${VERSION}
-docker push gcr.io/kubeadm-167321/namespace-cleaner:latest
+    -t gcr.io/${GCPROJECT}/namespace-cleaner:${VERSION} \
+    -t gcr.io/${GCPROJECT}/namespace-cleaner:latest \
+    --cache-from quay.io/gravitational/debian-grande:buster,gcr.io/${GCPROJECT}kubeadm-167321/namespace-cleaner:latest \
+    . 
+docker push gcr.io/${GCPROJECT}/namespace-cleaner:${VERSION}
+docker push gcr.io/${GCPROJECT}/namespace-cleaner:latest
+
