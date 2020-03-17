90c4258d306948296bdf39645fa930c65552fb43
Steven Martin <steven@gravitational.com>
2020-03-17 16:48:22 -0400

    Made comparison uniform and removed blank lines

--- examples/chart/teleport-demo/docker/teleport-sidecar/build.sh
+++ examples/chart/teleport-demo/docker/teleport-sidecar/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-ONE=$1
 TWO=$2
 VERSION=3.2.0
 if [[ "$1" != "" ]]; then
@@ -8,12 +7,10 @@ if [[ "$1" != "" ]]; then
     shift
 fi
 GCPROJECT=kubeadm-167321
-if [[ "" != "${TWO}" ]]; then
+if [[ "${TWO}" != "" ]]; then
     GCPROJECT=${TWO}
     shift
 fi
-echo "${VERSION} version"
-echo "${GCPROJECT} project"
 
 docker build --pull \
     -t gcr.io/${GCPROJECT}/teleport-sidecar:${VERSION} \
