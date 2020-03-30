e11576576f0594555e9ccb461ca39ae91e004099
Alexey Kontsevoy <biz.kovoy@gmail.com>
2020-03-30 12:30:12 -0400

    Use local user client when resolving user roles

--- lib/web/apiserver.go
+++ lib/web/apiserver.go
@@ -356,7 +356,7 @@ func (h *Handler) getUserStatus(w http.ResponseWriter, r *http.Request, _ httpro
 // GET /webapi/user/context
 //
 func (h *Handler) getUserContext(w http.ResponseWriter, r *http.Request, p httprouter.Params, c *SessionContext, site reversetunnel.RemoteSite) (interface{}, error) {
-	clt, err := c.GetUserClient(site)
+	clt, err := c.GetClient()
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
@@ -386,7 +386,6 @@ func (h *Handler) getUserContext(w http.ResponseWriter, r *http.Request, p httpr
 	}
 
 	userContext.Version = teleport.Version
-
 	return userContext, nil
 }
 
