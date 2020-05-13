17a5b1d7defe53c35cd8f241b956f49066425faa
Travis Swientek <tswientek@gravitational.com>
2020-05-13 16:20:41 -0500

    WIP: Simple Cluster docs and TF scripts.

---
+++ examples/aws/terraform/simple-cluster/dynamo.tf
@@ -0,0 +1,135 @@
+/* 
+DynamoDB is used to store cluster state, event
+metadata, and a simple locking mechanism for SSL 
+cert generation and renewal.
+*/
+
+// DynamoDB table for storing cluster state
+resource "aws_dynamodb_table" "teleport" {
+  name           = var.cluster_name
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "HashKey"
+  range_key      = "FullPath"
+  server_side_encryption {
+    enabled = true
+  }
+
+  lifecycle {
+    ignore_changes = [
+      read_capacity,
+      write_capacity,
+    ]
+  }
+
+  attribute {
+    name = "HashKey"
+    type = "S"
+  }
+
+  attribute {
+    name = "FullPath"
+    type = "S"
+  }
+
+  stream_enabled   = "true"
+  stream_view_type = "NEW_IMAGE"
+
+  ttl {
+    attribute_name = "Expires"
+    enabled        = true
+  }
+
+  tags = {
+    TeleportCluster = var.cluster_name
+  }
+}
+
+// DynamoDB table for storing cluster events
+resource "aws_dynamodb_table" "teleport_events" {
+  name           = "${var.cluster_name}-events"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "SessionID"
+  range_key      = "EventIndex"
+
+  server_side_encryption {
+    enabled = true
+  }
+
+  global_secondary_index {
+    name            = "timesearch"
+    hash_key        = "EventNamespace"
+    range_key       = "CreatedAt"
+    write_capacity  = 10
+    read_capacity   = 10
+    projection_type = "ALL"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      read_capacity,
+      write_capacity,
+    ]
+  }
+
+  attribute {
+    name = "SessionID"
+    type = "S"
+  }
+
+  attribute {
+    name = "EventIndex"
+    type = "N"
+  }
+
+  attribute {
+    name = "EventNamespace"
+    type = "S"
+  }
+
+  attribute {
+    name = "CreatedAt"
+    type = "N"
+  }
+
+  ttl {
+    attribute_name = "Expires"
+    enabled        = true
+  }
+
+  tags = {
+    TeleportCluster = var.cluster_name
+  }
+}
+
+// DynamoDB table for simple locking mechanism
+resource "aws_dynamodb_table" "teleport_locks" {
+  name           = "${var.cluster_name}-locks"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "Lock"
+
+  billing_mode = "PROVISIONED"
+
+  lifecycle {
+    ignore_changes = [
+      read_capacity,
+      write_capacity,
+    ]
+  }
+
+  attribute {
+    name = "Lock"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "Expires"
+    enabled        = true
+  }
+
+  tags = {
+    TeleportCluster = var.cluster_name
+  }
+}
