gravitational/teleport

commit c341d2bc1588f2e3ed72b3ffdfe405b8eac7fe35

fix agent forwarding for multi-session connections
Changes the lifetime of agent forwarding to be scoped
to the underlying ssh connection, instead of the
specific ssh channel which initially passed the agent
forwarding request.

lib/srv/ctx.go
@@ -174,6 +174,8 @@ type ServerContext struct {
 
 	sync.RWMutex
 
+	Parent *sshutils.ConnectionContext
+
 	// env is a list of environment variables passed to the session.
 	env map[string]string
 
@@ -185,12 +187,6 @@ type ServerContext struct {
 
 	// term holds PTY if it was requested by the session.
 	term Terminal
-
-	// agent is a client to remote SSH agent.
-	agent agent.Agent
-
-	// agentCh is SSH channel using SSH agent protocol.
-	agentChannel ssh.Channel
 
 	// session holds the active session (if there's an active one).
 	session *session
@@ -291,7 +287,7 @@ type ServerContext struct {
 
 // NewServerContext creates a new *ServerContext which is used to pass and
 // manage resources.
-func NewServerContext(srv Server, conn *ssh.ServerConn, identityContext IdentityContext) (*ServerContext, error) {
+func NewServerContext(ccx *sshutils.ConnectionContext, srv Server, identityContext IdentityContext) (*ServerContext, error) {
 	clusterConfig, err := srv.GetAccessPoint().GetClusterConfig()
 	if err != nil {
 		return nil, trace.Wrap(err)
@@ -300,13 +296,15 @@ func NewServerContext(srv Server, conn *ssh.ServerConn, identityContext Identity
 	cancelContext, cancel := context.WithCancel(context.TODO())
 
 	ctx := &ServerContext{
+		Parent:            ccx,
 		id:                int(atomic.AddInt32(&ctxID, int32(1))),
 		env:               make(map[string]string),
 		srv:               srv,
-		Conn:              conn,
+		Connection:        ccx.NetConn,
+		Conn:              ccx.ServerConn,
 		ExecResultCh:      make(chan ExecResult, 10),
 		SubsystemResultCh: make(chan SubsystemResult, 10),
-		ClusterName:       conn.Permissions.Extensions[utils.CertTeleportClusterName],
+		ClusterName:       ccx.ServerConn.Permissions.Extensions[utils.CertTeleportClusterName],
 		ClusterConfig:     clusterConfig,
 		Identity:          identityContext,
 		clientIdleTimeout: identityContext.RoleSet.AdjustClientIdleTimeout(clusterConfig.GetClientIdleTimeout()),
@@ -320,8 +318,8 @@ func NewServerContext(srv Server, conn *ssh.ServerConn, identityContext Identity
 	}
 
 	fields := log.Fields{
-		"local":        conn.LocalAddr(),
-		"remote":       conn.RemoteAddr(),
+		"local":        ctx.Conn.LocalAddr(),
+		"remote":       ctx.Conn.RemoteAddr(),
 		"login":        ctx.Identity.Login,
 		"teleportUser": ctx.Identity.TeleportUser,
 		"id":           ctx.id,
@@ -343,7 +341,7 @@ func NewServerContext(srv Server, conn *ssh.ServerConn, identityContext Identity
 			ClientIdleTimeout:     ctx.clientIdleTimeout,
 			Clock:                 ctx.srv.GetClock(),
 			Tracker:               ctx,
-			Conn:                  conn,
+			Conn:                  ctx.Conn,
 			Context:               cancelContext,
 			TeleportUser:          ctx.Identity.TeleportUser,
 			Login:                 ctx.Identity.Login,
@@ -373,6 +371,9 @@ func NewServerContext(srv Server, conn *ssh.ServerConn, identityContext Identity
 	}
 	ctx.AddCloser(ctx.contr)
 	ctx.AddCloser(ctx.contw)
+
+	// gather environment variables from parent.
+	ctx.ImportParentEnv()
 
 	return ctx, nil
 }
@@ -447,30 +448,22 @@ func (c *ServerContext) AddCloser(closer io.Closer) {
 	c.closers = append(c.closers, closer)
 }
 
-// GetAgent returns a agent.Agent which represents the capabilities of an SSH agent.
+// GetAgent returns a agent.Agent which represents the capabilities of an SSH agent,
+// or nil if no agent is available in this context.
 func (c *ServerContext) GetAgent() agent.Agent {
-	c.RLock()
-	defer c.RUnlock()
-	return c.agent
-}
-
-// GetAgentChannel returns the channel over which communication with the agent occurs.
-func (c *ServerContext) GetAgentChannel() ssh.Channel {
-	c.RLock()
-	defer c.RUnlock()
-	return c.agentChannel
-}
-
-// SetAgent sets the agent and channel over which communication with the agent occurs.
-func (c *ServerContext) SetAgent(a agent.Agent, channel ssh.Channel) {
-	c.Lock()
-	defer c.Unlock()
-	if c.agentChannel != nil {
-		c.Infof("closing previous agent channel")
-		c.agentChannel.Close()
+	if c.Parent == nil {
+		return nil
 	}
-	c.agentChannel = channel
-	c.agent = a
+	return c.Parent.GetAgent()
+}
+
+// GetAgentChannel returns the channel over which communication with the agent occurs,
+// or nil if no agent is available in this context.
+func (c *ServerContext) GetAgentChannel() ssh.Channel {
+	if c.Parent == nil {
+		return nil
+	}
+	return c.Parent.GetAgentChannel()
 }
 
 // GetTerm returns a Terminal.
@@ -500,6 +493,12 @@ func (c *ServerContext) GetEnv(key string) (string, bool) {
 	return val, ok
 }
 
+// ImportParentEnv is used to re-synchronize env vars after
+// parent context has been updated.
+func (c *ServerContext) ImportParentEnv() {
+	c.Parent.ExportEnv(c.env)
+}
+
 // takeClosers returns all resources that should be closed and sets the properties to null
 // we do this to avoid calling Close() under lock to avoid potential deadlocks
 func (c *ServerContext) takeClosers() []io.Closer {
@@ -511,10 +510,6 @@ func (c *ServerContext) takeClosers() []io.Closer {
 	if c.term != nil {
 		closers = append(closers, c.term)
 		c.term = nil
-	}
-	if c.agentChannel != nil {
-		closers = append(closers, c.agentChannel)
-		c.agentChannel = nil
 	}
 	closers = append(closers, c.closers...)
 	c.closers = nil

lib/srv/forward/sshserver.go
@@ -88,6 +88,10 @@ type Server struct {
 	// forwarding, subsystems.
 	remoteClient *ssh.Client
 
+	// connectionContext is used to construct ServerContext instances
+	// and supports registration of connection-scoped resource closers.
+	connectionContext *sshutils.ConnectionContext
+
 	// identityContext holds identity information about the user that has
 	// authenticated on sconn (like system login, Teleport username, roles).
 	identityContext srv.IdentityContext
@@ -435,6 +439,8 @@ func (s *Server) Serve() {
 	}
 	s.sconn = sconn
 
+	s.connectionContext = sshutils.NewConnectionContext(s.serverConn, s.sconn)
+
 	// Take connection and extract identity information for the user from it.
 	s.identityContext, err = s.authHandlers.CreateIdentityContext(sconn)
 	if err != nil {
@@ -488,6 +494,7 @@ func (s *Server) Close() error {
 		s.serverConn,
 		s.targetConn,
 		s.remoteClient,
+		s.connectionContext,
 	}
 
 	var errs []error
@@ -646,7 +653,7 @@ func (s *Server) handleChannel(nch ssh.NewChannel) {
 func (s *Server) handleDirectTCPIPRequest(ch ssh.Channel, req *sshutils.DirectTCPIPReq) {
 	// Create context for this channel. This context will be closed when
 	// forwarding is complete.
-	ctx, err := srv.NewServerContext(s, s.sconn, s.identityContext)
+	ctx, err := srv.NewServerContext(s.connectionContext, s, s.identityContext)
 	if err != nil {
 		ctx.Errorf("Unable to create connection context: %v.", err)
 		ch.Stderr().Write([]byte("Unable to create connection context."))
@@ -713,7 +720,7 @@ func (s *Server) handleSessionRequests(ch ssh.Channel, in <-chan *ssh.Request) {
 	// There is no need for the forwarding server to initiate disconnects,
 	// based on teleport business logic, because this logic is already
 	// done on the server's terminating side.
-	ctx, err := srv.NewServerContext(s, s.sconn, s.identityContext)
+	ctx, err := srv.NewServerContext(s.connectionContext, s, s.identityContext)
 	if err != nil {
 		ctx.Errorf("Unable to create connection context: %v.", err)
 		ch.Stderr().Write([]byte("Unable to create connection context."))

commit 8ecf6fc08246a6f13cc974ca8e4a9fe4aeaa4be3

Make agent channel setup lazy.
Changes agent channel setup behavior to be consistent
openssh by having servers lazily request agent channels
when they are needed, rather than immediately starting a
single connection-wide channel as soon as forwarding is
requested.  Fixes an issue introduced in #3613 which
caused openssh clients to hang on exit due to persistent
agent channel.

lib/srv/ctx.go
@@ -293,7 +293,7 @@ func NewServerContext(ccx *sshutils.ConnectionContext, srv Server, identityConte
 		return nil, trace.Wrap(err)
 	}
 
-	cancelContext, cancel := context.WithCancel(context.TODO())
+	cancelContext, cancel := context.WithCancel(ccx)
 
 	ctx := &ServerContext{
 		Parent:            ccx,
@@ -455,15 +455,6 @@ func (c *ServerContext) GetAgent() agent.Agent {
 		return nil
 	}
 	return c.Parent.GetAgent()
-}
-
-// GetAgentChannel returns the channel over which communication with the agent occurs,
-// or nil if no agent is available in this context.
-func (c *ServerContext) GetAgentChannel() ssh.Channel {
-	if c.Parent == nil {
-		return nil
-	}
-	return c.Parent.GetAgentChannel()
 }
 
 // GetTerm returns a Terminal.

lib/srv/forward/sshserver.go
@@ -439,7 +439,7 @@ func (s *Server) Serve() {
 	}
 	s.sconn = sconn
 
-	s.connectionContext = sshutils.NewConnectionContext(s.serverConn, s.sconn)
+	s.connectionContext = sshutils.NewConnectionContext(s.closeContext, s.serverConn, s.sconn)
 
 	// Take connection and extract identity information for the user from it.
 	s.identityContext, err = s.authHandlers.CreateIdentityContext(sconn)

lib/srv/regular/proxy.go
@@ -46,12 +46,11 @@ import (
 // remote hosts to a proxy client (AKA port mapping)
 type proxySubsys struct {
 	proxySubsysConfig
-	log          *logrus.Entry
-	closeC       chan struct{}
-	error        error
-	closeOnce    sync.Once
-	agent        agent.Agent
-	agentChannel ssh.Channel
+	log       *logrus.Entry
+	closeC    chan struct{}
+	error     error
+	closeOnce sync.Once
+	agent     agent.Agent
 }
 
 // parseProxySubsys looks at the requested subsystem name and returns a fully configured
@@ -172,9 +171,8 @@ func newProxySubsys(cfg proxySubsysConfig) (*proxySubsys, error) {
 			trace.Component:       teleport.ComponentSubsystemProxy,
 			trace.ComponentFields: map[string]string{},
 		}),
-		closeC:       make(chan struct{}),
-		agent:        cfg.ctx.GetAgent(),
-		agentChannel: cfg.ctx.GetAgentChannel(),
+		closeC: make(chan struct{}),
+		agent:  cfg.ctx.GetAgent(),
 	}, nil
 }
 

lib/srv/regular/sshserver.go
@@ -34,7 +34,6 @@ import (
 	"time"
 
 	"golang.org/x/crypto/ssh"
-	"golang.org/x/crypto/ssh/agent"
 
 	"github.com/gravitational/teleport"
 	"github.com/gravitational/teleport/lib/auth"
@@ -765,8 +764,9 @@ func (s *Server) serveAgent(ctx *srv.ServerContext) error {
 		return trace.ConvertSystemError(err)
 	}
 
-	// start an agent on a unix socket
-	agentServer := &teleagent.AgentServer{Agent: ctx.Parent.GetAgent()}
+	// start an agent server on a unix socket.  each incoming connection
+	// will result in a separate agent request.
+	agentServer := &teleagent.AgentServer{Getter: teleagent.AgentGetter(ctx.Parent.StartAgentChannel)}
 	err = agentServer.ListenUnixSocket(socketPath, uid, gid, 0600)
 	if err != nil {
 		return trace.Wrap(err)
@@ -778,7 +778,7 @@ func (s *Server) serveAgent(ctx *srv.ServerContext) error {
 	// ensure that SSHAuthSock and SSHAgentPID are imported into
 	// the current child context.
 	ctx.ImportParentEnv()
-	ctx.Debugf("Opened agent channel for Teleport user %v and socket %v.", ctx.Identity.TeleportUser, socketPath)
+	ctx.Debugf("Starting agent server for Teleport user %v and socket %v.", ctx.Identity.TeleportUser, socketPath)
 	go agentServer.Serve()
 
 	return nil
@@ -1174,14 +1174,7 @@ func (s *Server) handleAgentForwardNode(req *ssh.Request, ctx *srv.ServerContext
 		return trace.Wrap(err)
 	}
 
-	// open a channel to the client where the client will serve an agent
-	authChannel, _, err := ctx.Conn.OpenChannel(sshutils.AuthAgentRequest, nil)
-	if err != nil {
-		return trace.Wrap(err)
-	}
-
-	// save the agent in the context so it can be used later
-	ctx.Parent.SetAgent(agent.NewClient(authChannel), authChannel)
+	ctx.Parent.SetForwardAgent(true)
 
 	// serve an agent on a unix socket on this node
 	err = s.serveAgent(ctx)
@@ -1210,16 +1203,7 @@ func (s *Server) handleAgentForwardProxy(req *ssh.Request, ctx *srv.ServerContex
 		return trace.Wrap(err)
 	}
 
-	// Open a channel to the client where the client will serve an agent.
-	authChannel, _, err := ctx.Conn.OpenChannel(sshutils.AuthAgentRequest, nil)
-	if err != nil {
-		return trace.Wrap(err)
-	}
-
-	// Save the agent so it can be used when making a proxy subsystem request
-	// later. It will also be used when building a remote connection to the
-	// target node.
-	ctx.Parent.SetAgent(agent.NewClient(authChannel), authChannel)
+	ctx.Parent.SetForwardAgent(true)
 
 	return nil
 }

lib/sshutils/ctx.go
@@ -17,9 +17,12 @@ limitations under the License.
 package sshutils
 
 import (
+	"context"
 	"io"
 	"net"
 	"sync"
+
+	"github.com/gravitational/teleport/lib/teleagent"
 
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/agent"
@@ -29,6 +32,7 @@ import (
 
 // ConnectionContext manages connection-level state.
 type ConnectionContext struct {
+	context.Context
 	// NetConn is the base connection object.
 	NetConn net.Conn
 
@@ -42,24 +46,57 @@ type ConnectionContext struct {
 	// set for all channels.
 	env map[string]string
 
-	// agent is a client to remote SSH agent.
-	agent agent.Agent
+	// forwardAgent indicates that agent forwarding has
+	// been requested for this connection.
+	forwardAgent bool
 
-	// agentCh is SSH channel using SSH agent protocol.
-	agentChannel ssh.Channel
 	// closers is a list of io.Closer that will be called when session closes
 	// this is handy as sometimes client closes session, in this case resources
 	// will be properly closed and deallocated, otherwise they could be kept hanging.
 	closers []io.Closer
+
+	// closed indicates that closers have been run.
+	closed bool
+
+	// cancel cancels the context.Context scope associated with this ConnectionContext.
+	cancel context.CancelFunc
 }
 
 // NewConnectionContext creates a new ConnectionContext instance.
-func NewConnectionContext(nconn net.Conn, sconn *ssh.ServerConn) *ConnectionContext {
+func NewConnectionContext(ctx context.Context, nconn net.Conn, sconn *ssh.ServerConn) *ConnectionContext {
+	ctx, cancel := context.WithCancel(ctx)
 	return &ConnectionContext{
+		Context:    ctx,
 		NetConn:    nconn,
 		ServerConn: sconn,
 		env:        make(map[string]string),
+		cancel:     cancel,
 	}
+}
+
+// StartAgentChannel sets up a new agent forwarding channel against this connection.  The channel
+// is automatically closed when either ConnectionContext, or the supplied context.Context
+// gets canceled.
+func (c *ConnectionContext) StartAgentChannel(ctx context.Context) (agent.Agent, error) {
+	// refuse to start an agent if forwardAgent has not yet been set.
+	if !c.getForwardAgent() {
+		return nil, trace.AccessDenied("agent forwarding not requested or not authorized")
+	}
+	// open a agent channel to client
+	agentChannel, _, err := c.ServerConn.OpenChannel(AuthAgentRequest, nil)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	go func() {
+		// agent channel may live as long as the broader connection context,
+		// or be scoped to some shorter context.
+		select {
+		case <-ctx.Done():
+		case <-c.Done():
+		}
+		agentChannel.Close()
+	}()
+	return agent.NewClient(agentChannel), nil
 }
 
 // SetEnv sets a environment variable within this context.
@@ -88,30 +125,29 @@ func (c *ConnectionContext) ExportEnv(m map[string]string) {
 }
 
 // GetAgent returns a agent.Agent which represents the capabilities of an SSH agent,
-// or nil if no agent is available in this context.
+// or nil if no agent is available in this context.  This is a legacy method used in
+// parts of Teleport which still expect persistent agent connections that are greedily
+// initialized as soon as forwarding capabilities are requested. Prefer using the
+// StartAgentChannel method directly where possible.
 func (c *ConnectionContext) GetAgent() agent.Agent {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	return c.agent
+	if c.getForwardAgent() {
+		return teleagent.Lazy(c.Context, c.StartAgentChannel)
+	}
+	return nil
 }
 
-// GetAgentChannel returns the channel over which communication with the agent occurs,
-// or nil if no agent is available in this context.
-func (c *ConnectionContext) GetAgentChannel() ssh.Channel {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	return c.agentChannel
-}
-
-// SetAgent sets the agent and channel over which communication with the agent occurs.
-func (c *ConnectionContext) SetAgent(a agent.Agent, channel ssh.Channel) {
+// SetForwardAgent configures this context to support agent forwarding.
+func (c *ConnectionContext) SetForwardAgent(forwardAgent bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	if c.agentChannel != nil {
-		c.agentChannel.Close()
-	}
-	c.agentChannel = channel
-	c.agent = a
+	c.forwardAgent = forwardAgent
+}
+
+// getForwardAgent loads the forwardAgent flag with lock.
+func (c *ConnectionContext) getForwardAgent() bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.forwardAgent
 }
 
 // AddCloser adds any closer in ctx that will be called
@@ -119,6 +155,14 @@ func (c *ConnectionContext) SetAgent(a agent.Agent, channel ssh.Channel) {
 func (c *ConnectionContext) AddCloser(closer io.Closer) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+	// if context was already closed, run the closer immediately
+	// in the background.
+	if c.closed {
+		go func() {
+			closer.Close()
+		}()
+		return
+	}
 	c.closers = append(c.closers, closer)
 }
 
@@ -131,16 +175,16 @@ func (c *ConnectionContext) takeClosers() []io.Closer {
 
 	closers := c.closers
 	c.closers = nil
-	if c.agentChannel != nil {
-		closers = append(closers, c.agentChannel)
-		c.agentChannel = nil
-	}
+	c.closed = true
+
 	return closers
 }
 
 // Close closes associated resources (e.g. agent channel).
 func (c *ConnectionContext) Close() error {
 	var errs []error
+
+	c.cancel()
 
 	closers := c.takeClosers()
 

lib/sshutils/server.go
@@ -432,7 +432,11 @@ func (s *Server) HandleConnection(conn net.Conn) {
 	defer keepAliveTick.Stop()
 	keepAlivePayload := [8]byte{0}
 
-	ccx := NewConnectionContext(wconn, sconn)
+	// NOTE: we deliberately don't use s.closeContext here because the server's
+	// closeContext field is used to trigger starvation on cancellation by halting
+	// the acceptance of new connections; it is not intended to halt in-progress
+	// connection handling, and is therefore orthogonal to the role of ConnectionContext.
+	ccx := NewConnectionContext(context.TODO(), wconn, sconn)
 	defer ccx.Close()
 
 	for {
@@ -461,6 +465,8 @@ func (s *Server) HandleConnection(conn net.Conn) {
 			if err != nil {
 				log.Errorf("Failed sending keepalive request: %v", err)
 			}
+		case <-ccx.Done():
+			log.Debugf("Connection context canceled: %v -> %v", conn.RemoteAddr(), conn.LocalAddr())
 		}
 	}
 }

lib/teleagent/agent.go
@@ -1,6 +1,7 @@
 package teleagent
 
 import (
+	"context"
 	"io"
 	"net"
 	"os"
@@ -14,9 +15,13 @@ import (
 	"golang.org/x/crypto/ssh/agent"
 )
 
+// AgentGetter is a function used to get an agent instance.
+type AgentGetter func(context.Context) (agent.Agent, error)
+
 // AgentServer is implementation of SSH agent server
 type AgentServer struct {
 	agent.Agent
+	Getter   AgentGetter
 	listener net.Listener
 	path     string
 }
@@ -24,6 +29,33 @@ type AgentServer struct {
 // NewServer returns new instance of agent server
 func NewServer() *AgentServer {
 	return &AgentServer{Agent: agent.NewKeyring()}
+}
+
+// getAgent gets an agent instance
+func (a *AgentServer) getAgent(ctx context.Context) (agent.Agent, error) {
+	if a.Agent != nil {
+		return a.Agent, nil
+	}
+	return a.Getter(ctx)
+}
+
+// startServe starts serving agent protocol against conn
+func (a *AgentServer) startServe(ctx context.Context, conn net.Conn) error {
+	ctx, cancel := context.WithCancel(ctx)
+	instance, err := a.getAgent(ctx)
+	if err != nil {
+		cancel()
+		return trace.Wrap(err)
+	}
+	go func() {
+		defer cancel()
+		if err := agent.ServeAgent(instance, conn); err != nil {
+			if err != io.EOF {
+				log.Errorf(err.Error())
+			}
+		}
+	}()
+	return nil
 }
 
 // ListenUnixSocket starts listening and serving agent assuming that
@@ -47,8 +79,13 @@ func (a *AgentServer) ListenUnixSocket(path string, uid, gid int, mode os.FileMo
 
 // Serve starts serving on the listener, assumes that Listen was called before
 func (a *AgentServer) Serve() error {
+	ctx, cancel := context.WithCancel(context.TODO())
+	defer cancel()
 	if a.listener == nil {
 		return trace.BadParameter("Serve needs a Listen call first")
+	}
+	if a.Agent == nil && a.Getter == nil {
+		return trace.BadParameter("An agent or agent getter must be supplied.")
 	}
 	var tempDelay time.Duration // how long to sleep on accept failure
 	for {
@@ -77,13 +114,10 @@ func (a *AgentServer) Serve() error {
 			continue
 		}
 		tempDelay = 0
-		go func() {
-			if err := agent.ServeAgent(a.Agent, conn); err != nil {
-				if err != io.EOF {
-					log.Errorf(err.Error())
-				}
-			}
-		}()
+		if err := a.startServe(ctx, conn); err != nil {
+			log.Errorf("Failed to start serving agent: %v", err)
+			return trace.Wrap(err)
+		}
 	}
 }
 

lib/teleagent/lazy.go
@@ -1,0 +1,112 @@
+package teleagent
+
+import (
+	"context"
+	"sync"
+
+	"github.com/gravitational/trace"
+
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
+)
+
+// lazyAgent is an agent.Agent instance which is lazily initialized
+// via a call to an AgentGetter.  Helpful for dealing with APIs
+// which require, but may not actually use, an agent.
+type lazyAgent struct {
+	ctx  context.Context
+	get  AgentGetter
+	once sync.Once
+	ref  agent.Agent
+	err  error
+}
+
+// Lazy wraps an AgentGetter in an agent.Agent interface which lazily
+// initializes the agent on first method invocation.
+func Lazy(ctx context.Context, get AgentGetter) agent.Agent {
+	return &lazyAgent{
+		ctx: ctx,
+		get: get,
+	}
+}
+
+// agent gets the lazily initialized agent
+func (l *lazyAgent) agent() (agent.Agent, error) {
+	l.once.Do(func() {
+		l.ref, l.err = l.get(l.ctx)
+	})
+	return l.ref, l.err
+}
+
+// List returns the identities known to the agent.
+func (l *lazyAgent) List() ([]*agent.Key, error) {
+	a, err := l.agent()
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	return a.List()
+}
+
+// Sign has the agent sign the data using a protocol 2 key as defined
+// in [PROTOCOL.agent] section 2.6.2.
+func (l *lazyAgent) Sign(key ssh.PublicKey, data []byte) (*ssh.Signature, error) {
+	a, err := l.agent()
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	return a.Sign(key, data)
+}
+
+// Add adds a private key to the agent.
+func (l *lazyAgent) Add(key agent.AddedKey) error {
+	a, err := l.agent()
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	return a.Add(key)
+}
+
+// Remove removes all identities with the given public key.
+func (l *lazyAgent) Remove(key ssh.PublicKey) error {
+	a, err := l.agent()
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	return a.Remove(key)
+}
+
+// RemoveAll removes all identities.
+func (l *lazyAgent) RemoveAll() error {
+	a, err := l.agent()
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	return a.RemoveAll()
+}
+
+// Lock locks the agent. Sign and Remove will fail, and List will empty an empty list.
+func (l *lazyAgent) Lock(passphrase []byte) error {
+	a, err := l.agent()
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	return a.Lock(passphrase)
+}
+
+// Unlock undoes the effect of Lock
+func (l *lazyAgent) Unlock(passphrase []byte) error {
+	a, err := l.agent()
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	return a.Unlock(passphrase)
+}
+
+// Signers returns signers for all the known keys.
+func (l *lazyAgent) Signers() ([]ssh.Signer, error) {
+	a, err := l.agent()
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	return a.Signers()
+}