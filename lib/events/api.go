656636bcb37c0e11d3e36c76c40261ff126ef472
Lisa Kim <lisa@gravitational.com>
2020-05-18 19:24:36 -0700

    Add code and event names

--- lib/events/api.go
+++ lib/events/api.go
@@ -300,6 +300,18 @@ const (
 	TrustedClusterCreateEvent = "trusted_cluster.create"
 	// TrustedClusterDeleteEvent is the event for removing a trusted cluster.
 	TrustedClusterDeleteEvent = "trusted_cluster.delete"
+	// GithubConnectorCreatedEvent fires when a Github connector is created/updated.
+	GithubConnectorCreatedEvent = "github.created"
+	// GithubConnectorDeletedEvent fires when a Github connector is deleted.
+	GithubConnectorDeletedEvent = "github.deleted"
+	// OIDCConnectorCreatedEvent fires when OIDC connector is created/updated.
+	OIDCConnectorCreatedEvent = "oidc.created"
+	// OIDCConnectorDeletedEvent fires when OIDC connector is deleted.
+	OIDCConnectorDeletedEvent = "oidc.deleted"
+	// SAMLConnectorCreatedEvent fires when SAML connector is created/updated.
+	SAMLConnectorCreatedEvent = "saml.created"
+	// SAMLConnectorDeletedEvent fires when SAML connector is deleted.
+	SAMLConnectorDeletedEvent = "saml.deleted"
 )
 
 const (
