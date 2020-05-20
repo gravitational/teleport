34be857268a83c1ab7720b6d049abe859be1e356
Andrew Lytvynov <andrew@gravitational.com>
2020-05-20 09:19:01 -0700

    Extract common fields of various client.SSHLogin* structs
    A bit of deduplication, no functional changes.

--- lib/client/api.go
+++ lib/client/api.go
@@ -2052,16 +2052,18 @@ func (tc *TeleportClient) directLogin(ctx context.Context, secondFactorType stri
 
 	// ask the CA (via proxy) to sign our public key:
 	response, err := SSHAgentLogin(ctx, SSHLoginDirect{
-		ProxyAddr:      tc.WebProxyAddr,
-		User:           tc.Config.Username,
-		Password:       password,
-		OTPToken:       otpToken,
-		PubKey:         pub,
-		TTL:            tc.KeyTTL,
-		Insecure:       tc.InsecureSkipVerify,
-		Pool:           loopbackPool(tc.WebProxyAddr),
-		Compatibility:  tc.CertificateFormat,
-		RouteToCluster: tc.SiteName,
+		SSHLogin: SSHLogin{
+			ProxyAddr:      tc.WebProxyAddr,
+			PubKey:         pub,
+			TTL:            tc.KeyTTL,
+			Insecure:       tc.InsecureSkipVerify,
+			Pool:           loopbackPool(tc.WebProxyAddr),
+			Compatibility:  tc.CertificateFormat,
+			RouteToCluster: tc.SiteName,
+		},
+		User:     tc.Config.Username,
+		Password: password,
+		OTPToken: otpToken,
 	})
 
 	return response, trace.Wrap(err)
@@ -2072,17 +2074,19 @@ func (tc *TeleportClient) ssoLogin(ctx context.Context, connectorID string, pub 
 	log.Debugf("samlLogin start")
 	// ask the CA (via proxy) to sign our public key:
 	response, err := SSHAgentSSOLogin(ctx, SSHLoginSSO{
-		ConnectorID:    connectorID,
-		PubKey:         pub,
-		TTL:            tc.KeyTTL,
-		Protocol:       protocol,
-		Compatibility:  tc.CertificateFormat,
-		BindAddr:       tc.BindAddr,
-		ProxyAddr:      tc.WebProxyAddr,
-		Insecure:       tc.InsecureSkipVerify,
-		Pool:           loopbackPool(tc.WebProxyAddr),
-		Browser:        tc.Browser,
-		RouteToCluster: tc.SiteName,
+		SSHLogin: SSHLogin{
+			ProxyAddr:      tc.WebProxyAddr,
+			PubKey:         pub,
+			TTL:            tc.KeyTTL,
+			Insecure:       tc.InsecureSkipVerify,
+			Pool:           loopbackPool(tc.WebProxyAddr),
+			Compatibility:  tc.CertificateFormat,
+			RouteToCluster: tc.SiteName,
+		},
+		ConnectorID: connectorID,
+		Protocol:    protocol,
+		BindAddr:    tc.BindAddr,
+		Browser:     tc.Browser,
 	})
 	return response, trace.Wrap(err)
 }
@@ -2101,15 +2105,17 @@ func (tc *TeleportClient) u2fLogin(ctx context.Context, pub []byte) (*auth.SSHLo
 	}
 
 	response, err := SSHAgentU2FLogin(ctx, SSHLoginU2F{
-		ProxyAddr:      tc.WebProxyAddr,
-		User:           tc.Config.Username,
-		Password:       password,
-		PubKey:         pub,
-		TTL:            tc.KeyTTL,
-		Insecure:       tc.InsecureSkipVerify,
-		Pool:           loopbackPool(tc.WebProxyAddr),
-		Compatibility:  tc.CertificateFormat,
-		RouteToCluster: tc.SiteName,
+		SSHLogin: SSHLogin{
+			ProxyAddr:      tc.WebProxyAddr,
+			PubKey:         pub,
+			TTL:            tc.KeyTTL,
+			Insecure:       tc.InsecureSkipVerify,
+			Pool:           loopbackPool(tc.WebProxyAddr),
+			Compatibility:  tc.CertificateFormat,
+			RouteToCluster: tc.SiteName,
+		},
+		User:     tc.Config.Username,
+		Password: password,
 	})
 
 	return response, trace.Wrap(err)
