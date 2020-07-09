91de076f5248a852c1bae3ba0e8016008b07b28f
Gus Luxton <gus@gravitational.com>
2020-07-09 17:13:21 -0300

    TMPDIR -> PACKAGE_TEMPDIR and set TMPDIR variable for mktemp

--- build.assets/build-package.sh
+++ build.assets/build-package.sh
@@ -257,11 +257,11 @@ fi
 
 # create a temporary directory and download specified Teleport version
 pushd $(mktemp -d)
-TMPDIR=$(pwd)
+PACKAGE_TEMPDIR=$(pwd)
 # automatically clean up on exit
 # TODO(gus): testing
-trap "ls -lR /tmp && rm -rf ${TMPDIR}" EXIT
-mkdir -p ${TMPDIR}/buildroot
+trap "ls -lR ${TMPDIR} && rm -rf ${PACKAGE_TEMPDIR}" EXIT
+mkdir -p ${PACKAGE_TEMPDIR}/buildroot
 
 # implement a rudimentary download cache for repeat builds on the same host
 mkdir -p ${TARBALL_DIRECTORY}
@@ -284,20 +284,20 @@ tar -C $(pwd) -xvzf ${TARBALL_DIRECTORY}/${TARBALL_FILENAME} ${FILE_LIST}
 # move files into correct locations before building the package
 if [[ "${PACKAGE_TYPE}" != "pkg" ]]; then
     if [[ "${LINUX_BINARY_FILE_LIST}" != "" ]]; then
-        mkdir -p ${TMPDIR}/buildroot${LINUX_BINARY_DIR}
-        mv -v ${LINUX_BINARY_FILE_LIST} ${TMPDIR}/buildroot${LINUX_BINARY_DIR}
+        mkdir -p ${PACKAGE_TEMPDIR}/buildroot${LINUX_BINARY_DIR}
+        mv -v ${LINUX_BINARY_FILE_LIST} ${PACKAGE_TEMPDIR}/buildroot${LINUX_BINARY_DIR}
     fi
     if [[ "${LINUX_SYSTEMD_FILE_LIST}" != "" ]]; then
-        mkdir -p ${TMPDIR}/buildroot${LINUX_SYSTEMD_DIR}
-        mv -v ${LINUX_SYSTEMD_FILE_LIST} ${TMPDIR}/buildroot${LINUX_SYSTEMD_DIR}
+        mkdir -p ${PACKAGE_TEMPDIR}/buildroot${LINUX_SYSTEMD_DIR}
+        mv -v ${LINUX_SYSTEMD_FILE_LIST} ${PACKAGE_TEMPDIR}/buildroot${LINUX_SYSTEMD_DIR}
     fi
     if [[ "${LINUX_CONFIG_FILE}" != "" ]]; then
-        mkdir -p ${TMPDIR}/buildroot${LINUX_CONFIG_DIR}
-        mv -v ${LINUX_CONFIG_FILE} ${TMPDIR}/buildroot${LINUX_CONFIG_DIR}
+        mkdir -p ${PACKAGE_TEMPDIR}/buildroot${LINUX_CONFIG_DIR}
+        mv -v ${LINUX_CONFIG_FILE} ${PACKAGE_TEMPDIR}/buildroot${LINUX_CONFIG_DIR}
         CONFIG_FILE_STANZA="--config-files /src/buildroot${LINUX_CONFIG_DIR}/${LINUX_CONFIG_FILE} "
     fi
     # /var/lib/teleport
-    mkdir -p -m0700 ${TMPDIR}/buildroot${LINUX_DATA_DIR}
+    mkdir -p -m0700 ${PACKAGE_TEMPDIR}/buildroot${LINUX_DATA_DIR}
 fi
 popd
 
@@ -313,13 +313,13 @@ if [[ "${PACKAGE_TYPE}" == "pkg" ]]; then
                 -v \
                 --timestamp \
                 --options runtime \
-                ${TMPDIR}/${FILE}
+                ${PACKAGE_TEMPDIR}/${FILE}
         done
     fi
 
     # build the package for OS X
     pkgbuild \
-        --root ${TMPDIR}/${TAR_PATH} \
+        --root ${PACKAGE_TEMPDIR}/${TAR_PATH} \
         --identifier ${BUNDLE_ID} \
         --version ${TELEPORT_VERSION} \
         --install-location /usr/local/bin \
@@ -352,10 +352,10 @@ if [[ "${PACKAGE_TYPE}" == "pkg" ]]; then
                 \"username\": \"${APPLE_USERNAME}\",
                 \"password\": \"${APPLE_PASSWORD}\"
             }
-        }" > ${TMPDIR}/gon-config.json
+        }" > ${PACKAGE_TEMPDIR}/gon-config.json
 
         # notarise built package using gon
-        gon ${TMPDIR}/gon-config.json
+        gon ${PACKAGE_TEMPDIR}/gon-config.json
     fi
 
     # checksum created packages
@@ -367,7 +367,7 @@ else
     rm -vf ${OUTPUT_FILENAME}
 
     # build for other platforms
-    docker run -v ${TMPDIR}:/src --rm ${DOCKER_IMAGE} \
+    docker run -v ${PACKAGE_TEMPDIR}:/src --rm ${DOCKER_IMAGE} \
         fpm \
         --input-type dir \
         --output-type ${PACKAGE_TYPE} \
@@ -389,7 +389,7 @@ else
         ${FILE_PERMISSIONS_STANZA} .
 
     # copy created package back to current directory
-    cp ${TMPDIR}/*.${PACKAGE_TYPE} .
+    cp ${PACKAGE_TEMPDIR}/*.${PACKAGE_TYPE} .
 
     # checksum created packages
     for FILE in *.${PACKAGE_TYPE}; do
