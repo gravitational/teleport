aea4f98e8c4a9d21124e7e7456eb8d48cd01d82e
Lisa Kim <lisa@gravitational.com>
2020-07-24 12:54:47 -0700

    Remove closing when pings fail to send

--- lib/web/terminal.go
+++ lib/web/terminal.go
@@ -320,7 +320,6 @@ func (t *TerminalHandler) startPingLoop(ws *websocket.Conn) {
 			// https://github.com/golang/net/blob/master/websocket/hybi.go#L291
 			if err := codec.Send(ws, nil); err != nil {
 				t.log.Errorf("Unable to send ping frame to web client: %v.", err)
-				t.Close()
 				return
 			}
 		case <-t.terminalContext.Done():
