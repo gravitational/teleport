a05bda61ed5d6cdb58d3d1b728a8b959e9f98da5
Lisa Kim <lisa@gravitational.com>
2020-10-09 18:11:40 -0700

    Fix github success event code

--- lib/auth/github.go
+++ lib/auth/github.go
@@ -152,7 +152,7 @@ func (a *Server) ValidateGithubAuthCallback(q url.Values) (*GithubAuthResponse, 
 		a.emitter.EmitAuditEvent(a.closeCtx, event)
 		return nil, trace.Wrap(err)
 	}
-	event.Code = events.UserSSOLoginFailureCode
+	event.Code = events.UserSSOLoginCode
 	event.Status.Success = true
 	event.User = re.auth.Username
 	if err := a.emitter.EmitAuditEvent(a.closeCtx, event); err != nil {
