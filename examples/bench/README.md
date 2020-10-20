2800c1ea2efc4d2e46336cac4f95db45ce74516d
Quin Quintero <quinq@gravitational.com>
2020-10-20 13:01:00 -0700

    docs and example

---
+++ examples/bench/README.md
@@ -0,0 +1,12 @@
+
+# Tsh Bench
+
+
+## Linear Benchmark Generator
+
+### What is this
+A linear generator generates benchmarks between a lower and upper bound using a fixed step as configured by the user. 
+
+
+### Use case
+Linear generators are useful when benchmarking setups with understood performance profiles or generating graphs for user-facing materials.
---
+++ examples/bench/example.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gravitational/teleport/lib/benchmark"
+)
+
+func main() {
+	linear := &benchmark.Linear{
+		LowerBound:          10,
+		UpperBound:          50,
+		Step:                10,
+		MinimumMeasurements: 1000,
+		MinimumWindow:       30 * time.Second,
+		Threads:             10,
+	}
+
+	results, err := benchmark.Run(linear, "ls -l /", "ec2-3-15-147-120.us-east-2.compute.amazonaws.com", "ec2-user", "ec2-3-15-147-120.us-east-2.compute.amazonaws.com")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for i, res := range results {
+		fmt.Printf("Benchmark #%v\n", i)
+		fmt.Printf("Duration: %v\n", res.Duration)
+		fmt.Printf("Requests Originated: %v\n", res.RequestsOriginated)
+		fmt.Printf("Requests Failed: %v\n", res.RequestsFailed)
+
+	}
+
+}
