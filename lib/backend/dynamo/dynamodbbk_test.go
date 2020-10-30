2bf6f0411cbb1921476fcd702fc0d3ea94ea65f2
Russell Jones <rjones@gravitational.com>
2020-10-30 18:23:40 +0000

    Added support for DynamoDB Auto Scaling.

--- lib/backend/dynamo/dynamodbbk_test.go
+++ lib/backend/dynamo/dynamodbbk_test.go
@@ -14,7 +14,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
 */
 
 package dynamo
@@ -34,7 +33,7 @@ import (
 func TestDynamoDB(t *testing.T) { check.TestingT(t) }
 
 type DynamoDBSuite struct {
-	bk        *Backend
+	bk        *DynamoDBBackend
 	suite     test.BackendSuite
 	tableName string
 }
@@ -54,16 +53,14 @@ func (s *DynamoDBSuite) SetUpSuite(c *check.C) {
 	}
 	bk, err := newBackend()
 	c.Assert(err, check.IsNil)
-	s.bk = bk.(*Backend)
+	s.bk = bk.(*DynamoDBBackend)
 	s.suite.B = s.bk
 	s.suite.NewBackend = newBackend
 }
 
-// TearDownSuite stops the backend.
-//
-// TODO: This function should delete all tables created during tests.
 func (s *DynamoDBSuite) TearDownSuite(c *check.C) {
 	if s.bk != nil && s.bk.svc != nil {
+		//		s.bk.deleteTable(context.Background(), s.tableName, false)
 		c.Assert(s.bk.Close(), check.IsNil)
 	}
 }
@@ -104,16 +101,20 @@ func (s *DynamoDBSuite) TestLocking(c *check.C) {
 	s.suite.Locking(c)
 }
 
-// TestContinuousBackups verifies that the continuous backup state is updated
-// upon start of the DynamoDB backend.
+// TestContinuousBackups verifies that the continuous backup state is set upon
+// startup of DynamoDB.
 func (s *DynamoDBSuite) TestContinuousBackups(c *check.C) {
+	if !hasLocalDynamoDB() {
+		c.Skip("No local DynamoDB found.")
+	}
+
 	var tests = []struct {
 		enabled bool
 		desc    check.CommentInterface
 	}{
 		{
 			enabled: true,
-			desc:    check.Commentf("enable continuous backups"),
+			desc:    check.Commentf("enabled continuous backups"),
 		},
 		{
 			enabled: false,
@@ -135,5 +136,72 @@ func (s *DynamoDBSuite) TestContinuousBackups(c *check.C) {
 		ok, err := d.getContinuousBackups(context.Background())
 		c.Assert(err, check.IsNil, tt.desc)
 		c.Assert(ok, check.Equals, tt.enabled, tt.desc)
+	}
+}
+
+// TestContinuousBackups verifies that auto scaling is enabled and disabled
+// upon startup of DynamoDB.
+func (s *DynamoDBSuite) TestAutoScaling(c *check.C) {
+	if !hasLocalDynamoDB() {
+		c.Skip("No local DynamoDB found.")
+	}
+
+	var tests = []struct {
+		inEnabled          bool
+		inReadMinCapacity  int
+		inReadMaxCapacity  int
+		inReadTargetValue  float64
+		inWriteMinCapacity int
+		inWriteMaxCapacity int
+		inWriteTargetValue float64
+		desc               check.CommentInterface
+	}{
+		{
+			inEnabled:          true,
+			inReadMinCapacity:  10,
+			inReadMaxCapacity:  20,
+			inReadTargetValue:  50.0,
+			inWriteMinCapacity: 10,
+			inWriteMaxCapacity: 20,
+			inWriteTargetValue: 50.0,
+			desc:               check.Commentf("enabled auto scaling"),
+		},
+		{
+			inEnabled:          false,
+			inReadMinCapacity:  0,
+			inReadMaxCapacity:  0,
+			inReadTargetValue:  0.0,
+			inWriteMinCapacity: 0,
+			inWriteMaxCapacity: 0,
+			inWriteTargetValue: 0.0,
+			desc:               check.Commentf("disabled auto scaling"),
+		},
+	}
+	for _, tt := range tests {
+		tableName := "teleport.dynamo.continuous.backups"
+		newBackend := func() (backend.Backend, error) {
+			return New(context.Background(), map[string]interface{}{
+				"table_name":         tableName,
+				"auto_scaling":       tt.inEnabled,
+				"read_min_capacity":  tt.inReadMinCapacity,
+				"read_max_capacity":  tt.inReadMaxCapacity,
+				"read_target_value":  tt.inReadTargetValue,
+				"write_min_capacity": tt.inWriteMinCapacity,
+				"write_max_capacity": tt.inWriteMaxCapacity,
+				"write_target_value": tt.inWriteTargetValue,
+			})
+		}
+		bk, err := newBackend()
+		c.Assert(err, check.IsNil, tt.desc)
+		d := bk.(*Backend)
+
+		resp, err := d.getAutoScaling(context.Background())
+		c.Assert(err, check.IsNil, tt.desc)
+		c.Assert(resp.readMinCapacity, check.Equals, tt.inReadMinCapacity)
+		c.Assert(resp.readMaxCapacity, check.Equals, tt.inReadMaxCapacity)
+		c.Assert(resp.readTargetValue, check.Equals, tt.inReadTargetValue)
+		c.Assert(resp.writeMinCapacity, check.Equals, tt.inWriteMinCapacity)
+		c.Assert(resp.writeMaxCapacity, check.Equals, tt.inWriteMaxCapacity)
+		c.Assert(resp.writeTargetValue, check.Equals, tt.inWriteTargetValue)
 	}
 }
