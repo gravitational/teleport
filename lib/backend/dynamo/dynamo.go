17941307041e9ffd58665a9d3c354df338a1aa22
Russell Jones <rjones@gravitational.com>
2020-10-30 18:23:40 +0000

    Added support for DynamoDB Auto Scaling.

--- lib/backend/dynamo/dynamo.go
+++ lib/backend/dynamo/dynamo.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2015-2019 Gravitational, Inc.
+Copyright 2015-2020 Gravitational, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
 */
 
 package dynamo
