e6681abe6a7113cfd2da507f05581b7bdf398540
Sasha Klizhentas <klizhentas@gmail.com>
2020-11-03 20:46:32 -0800

    Fan out events in async mode for async recordings.
    This commit fixes #4695.
    
    Teleport in async recording mode sends all events to disk,
    and uploads them to the server later.
    
    It uploads some events synchronously to the audit log so
    they show up in the global event log right away.
    
    However if the auth server is slow, the fanout blocks the session.
    
    This commit makes the fanout of some events to be fast,
    but nonblocking and never fail so sessions will not hang
    unless the disk writes hang.
    
    It adds a backoff period and timeout after which some
    events will be lost, but session will continue without locking.

--- lib/defaults/defaults.go
+++ lib/defaults/defaults.go
@@ -308,6 +308,10 @@ var (
 	// usually is slow, e.g. once in 30 seconds
 	NetworkBackoffDuration = time.Second * 30
 
+	// AuditBackoffTimeout is a time out before audit logger will
+	// start loosing events
+	AuditBackoffTimeout = 5 * time.Second
+
 	// NetworkRetryDuration is a standard retry on network requests
 	// to retry quickly, e.g. once in one second
 	NetworkRetryDuration = time.Second
@@ -387,6 +391,9 @@ var (
 	// connections. These pings are needed to avoid timeouts on load balancers
 	// that don't respect TCP keep-alives.
 	SPDYPingPeriod = 30 * time.Second
+
+	// AsyncBufferSize is a default buffer size for async emitters
+	AsyncBufferSize = 1024
 )
 
 // Default connection limits, they can be applied separately on any of the Teleport
