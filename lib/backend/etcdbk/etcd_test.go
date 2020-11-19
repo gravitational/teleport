a73e7e2c956fe4269574b9e38ebff6ef161a43c7
dmitri <deemok@gmail.com>
2020-11-19 13:59:27 +0100

    Backport of https://github.com/gravitational/teleport/pull/4800.

--- lib/backend/etcdbk/etcd_test.go
+++ lib/backend/etcdbk/etcd_test.go
@@ -27,10 +27,11 @@ import (
 	"github.com/gravitational/teleport/lib/backend"
 	"github.com/gravitational/teleport/lib/backend/test"
 	"github.com/gravitational/teleport/lib/utils"
-	"github.com/jonboulle/clockwork"
-	"go.etcd.io/etcd/clientv3"
 
 	"github.com/gravitational/trace"
+	"github.com/stretchr/testify/require"
+	"github.com/jonboulle/clockwork"
+	"go.etcd.io/etcd/clientv3"
 	"gopkg.in/check.v1"
 )
 
@@ -379,4 +380,33 @@ func (s *EtcdSuite) TestSyncLegacyPrefix(c *check.C) {
 		backupPrefix + "/2":       "c2",
 	})
 	s.reset(c)
+}
+
+// TestCompareAndSwapOversizedValue ensures that the backend reacts with a proper
+// error message if client sends a message exceeding the configured size maximum
+// See https://github.com/gravitational/teleport/issues/4786
+func TestCompareAndSwapOversizedValue(t *testing.T) {
+	// setup
+	const maxClientMsgSize = 128
+	bk, err := New(context.Background(), backend.Params{
+		"peers":                          []string{"https://127.0.0.1:2379"},
+		"prefix":                         "/teleport",
+		"tls_key_file":                   "../../../examples/etcd/certs/client-key.pem",
+		"tls_cert_file":                  "../../../examples/etcd/certs/client-cert.pem",
+		"tls_ca_file":                    "../../../examples/etcd/certs/ca-cert.pem",
+		"dial_timeout":                   500 * time.Millisecond,
+		"etcd_max_client_msg_size_bytes": maxClientMsgSize,
+	})
+	require.NoError(t, err)
+	prefix := test.MakePrefix()
+	// Explicitly exceed the message size
+	value := make([]byte, maxClientMsgSize+1)
+
+	// verify
+	_, err = bk.CompareAndSwap(context.Background(),
+		backend.Item{Key: prefix("one"), Value: []byte("1")},
+		backend.Item{Key: prefix("one"), Value: value},
+	)
+	require.True(t, trace.IsLimitExceeded(err))
+	require.Regexp(t, ".*ResourceExhausted.*", err)
 }
