e1b267a6d75da018612825eb6a4ba05ecd8d020a
Gus Luxton <gus@gravitational.com>
2020-12-03 16:34:44 -0400

    Update SSM paths

--- examples/aws/terraform/ha-autoscale-cluster/ssm.tf
+++ examples/aws/terraform/ha-autoscale-cluster/ssm.tf
@@ -6,7 +6,7 @@ resource "aws_ssm_parameter" "license" {
   count     = var.license_path != "" ? 1 : 0
   name      = "/teleport/${var.cluster_name}/license"
   type      = "SecureString"
-  value     = file("${path.module}/${var.license_path}")
+  value     = file(var.license_path)
   overwrite = true
 }
 
