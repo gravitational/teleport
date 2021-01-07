02b8731d62ac8b53c58fed0bf39f2feef7d129c9
Gus Luxton <gus@goteleport.com>
2021-01-07 15:10:56 -0400

    Add support for proxy NLB alias to AMI

--- examples/aws/terraform/ha-autoscale-cluster/proxy_asg.tf
+++ examples/aws/terraform/ha-autoscale-cluster/proxy_asg.tf
@@ -99,17 +99,17 @@ resource "aws_launch_configuration" "proxy" {
   user_data                   = templatefile(
     "${path.module}/proxy-user-data.tpl",
     {
-      region               = var.region
-      cluster_name         = var.cluster_name
-      auth_server_addr     = aws_lb.auth.dns_name
-      proxy_server_lb_addr = aws_lb.proxy.dns_name
-      influxdb_addr        = "http://${aws_lb.monitor.dns_name}:8086"
-      email                = var.email
-      domain_name          = var.route53_domain
-      s3_bucket            = var.s3_bucket_name
-      telegraf_version     = var.telegraf_version
-      teleport_uid         = var.teleport_uid
-      use_acm              = var.use_acm
+      region                  = data.aws_region.current.name
+      cluster_name            = var.cluster_name
+      auth_server_addr        = aws_lb.auth.dns_name
+      proxy_server_lb_addr    = aws_lb.proxy.dns_name
+      proxy_server_nlb_alias  = var.route53_domain_acm_nlb_alias
+      influxdb_addr           = "http://${aws_lb.monitor.dns_name}:8086"
+      email                   = var.email
+      domain_name             = var.route53_domain
+      s3_bucket               = var.s3_bucket_name
+      telegraf_version        = var.telegraf_version
+      use_acm                 = var.use_acm
     }
   )
   key_name                    = var.key_name
