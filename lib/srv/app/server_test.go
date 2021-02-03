b48832c0a5f969519dadc0058b393ae935d46188
dmitri <deemok@gmail.com>
2021-02-03 19:10:02 +0100

    Backport of https://github.com/gravitational/teleport/pull/5079.

--- lib/srv/app/server_test.go
+++ lib/srv/app/server_test.go
@@ -29,6 +29,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
@@ -297,8 +298,14 @@ func (s *Suite) TestHandleConnection(c *check.C) {
 		},
 	}
 
+	var wg sync.WaitGroup
+	wg.Add(1)
+
 	// Handle the connection in another goroutine.
-	go s.appServer.HandleConnection(pw)
+	go func() {
+		s.appServer.HandleConnection(pw)
+		wg.Done()
+	}()
 
 	// Issue request.
 	resp, err := httpClient.Get("https://" + teleport.APIDomain)
@@ -315,6 +322,10 @@ func (s *Suite) TestHandleConnection(c *check.C) {
 	// error here.
 	err = s.appServer.Close()
 	c.Assert(err, check.NotNil)
+
+	// Wait for the application server to actually stop serving before
+	// closing the test. This will make sure the server removes the listeners
+	wg.Wait()
 }
 
 // TestAuthorize verifies that only authorized requests are handled.
