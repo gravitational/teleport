60379295bcdf42ba97e71dfe80e14f097c21c9d9
Steven Martin <steven@gravitational.com>
2021-02-03 21:42:44 -0500

    Update output.tf

--- examples/aws/terraform/ha-autoscale-cluster/output.tf
+++ examples/aws/terraform/ha-autoscale-cluster/output.tf
@@ -6,7 +6,7 @@
 output "z_bastion_instance_public_ip" {
   value = {
   for instance in aws_instance.bastion:
-  instance.id => format("ip %s %s %s %s",instance.public_ip, ", Add identity file via ssh agent (ssh-add identityfile.pem)\n and you can jump host to servers. \nEx:\nssh -J ec2-user@", instance.public_ip, " ec2-user@<proxy/auth/node ip>") 
+  instance.id => format("ip %s %s%s %s",instance.public_ip, ", Add identity file via ssh agent (ssh-add identityfile.pem)\n and you can jump host to servers. \nEx:\nssh -J ec2-user@", instance.public_ip, " ec2-user@<proxy/auth/node ip>") 
 }
   description = "AWS Bastion EC2 IP. Add AWS identity file to your ssh agent and you can jump host through the bastion.  ssh -J <bastion-ip> <proxy/auth ip>"
 }
@@ -28,7 +28,7 @@ output "monitor_autoscaling_groups" {
   value = aws_autoscaling_group.monitor.*.id
 }
 output "retrieving_autoscaling_group_ips" {
-  value = format("To get the private IPs of the autoscaling ec2 instances set the ASG_VALUE to a above scaling group.\nEx:\nASG_VALUE=teleportoss-auth\naws ec2 describe-instances     --filters \"Name=tag-value,Values=%s\" --region %s  --query 'Reservations[*].Instances[*].[InstanceId,PrivateIpAddress]' --output text", "$${ASG_VALUE}", var.region)
+  value = format("To get the private IPs of the autoscaling ec2 instances set the ASG_VALUE to a above scaling group and run the aws command.\nEx:\nASG_VALUE=teleportoss-auth\naws ec2 describe-instances     --filters \"Name=tag-value,Values=%s\" --region %s  --query 'Reservations[*].Instances[*].[InstanceId,PrivateIpAddress]' --output text", "$${ASG_VALUE}", var.region)
 
 }
 
