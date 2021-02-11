7a9e59001ea1e73b8a5e3da57c5adf3d951f1924
Isaiah Becker-Mayer <isaiah@goteleport.com>
2021-02-11 09:08:23 -0800

    adding check for compressed images

--- lib/web/gziphandler.go
+++ lib/web/gziphandler.go
@@ -25,8 +25,8 @@ func (w gzipResponseWriter) Write(b []byte) (int, error) {
 // MakeGzipHandler adds support for gzip compression for given handler.
 func MakeGzipHandler(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Check if the client can accept the gzip encoding.
-		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+		// Check if the client can accept the gzip encoding and that this is not an image asset.
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") || isCompressedImageRequest(r) {
 			handler.ServeHTTP(w, r)
 			return
 		}
@@ -38,4 +38,22 @@ func MakeGzipHandler(handler http.Handler) http.Handler {
 		gzw := gzipResponseWriter{Writer: gz, ResponseWriter: w}
 		handler.ServeHTTP(gzw, r)
 	})
+}
+
+// isCompressedImageRequest checks whether a request is for a png or jpeg
+func isCompressedImageRequest(r *http.Request) bool {
+	pathLen := len(r.URL.Path)
+
+	// Path must be at least 5 characters long to be asking for a compressed image
+	if pathLen < len("a.png") {
+		return false
+	}
+
+	b := pathLen
+	a := pathLen - len(".png") // or ".jpg"
+	if r.URL.Path[a:b] == ".png" || r.URL.Path[a:b] == ".jpg" || r.URL.Path[a-1:b] == ".jpeg" {
+		return true
+	}
+
+	return false
 }
