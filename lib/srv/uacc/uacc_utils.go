324ccda21f68c35eafc00d48551974cf81351995
Acrimon <joel.wejdenstal@gmail.com>
2021-02-15 20:43:39 +0100

    Implemented utmp/wtmp support.

---
+++ lib/srv/uacc/uacc_utils.go
@@ -0,0 +1,39 @@
+/*
+Copyright 2021 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package uacc
+
+import (
+	"encoding/binary"
+	"net"
+
+	"github.com/gravitational/trace"
+)
+
+// PrepareAddr parses and transforms a net.Addr into a format usable by other uacc functions.
+func PrepareAddr(addr net.Addr) ([4]int32, error) {
+	stringIP, _, err := net.SplitHostPort(addr.String())
+	if err != nil {
+		return [4]int32{}, trace.Wrap(err)
+	}
+	ip := net.ParseIP(stringIP)
+	rawV6 := ip.To16()
+	groupedV6 := [4]int32{}
+	for i := range groupedV6 {
+		groupedV6[i] = int32(binary.LittleEndian.Uint32(rawV6[i*4 : (i+1)*4]))
+	}
+	return groupedV6, nil
+}
