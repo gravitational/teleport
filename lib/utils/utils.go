f835ba124ec3a8818a9d3861c08642d3f51065a5
Andrej Tokarčík <andrej@goteleport.com>
2021-03-01 15:24:22 +0100

    Introduce utils.ReadAtMost to prevent resource exhaustion

--- lib/utils/utils.go
+++ lib/utils/utils.go
@@ -537,6 +537,23 @@ func FileExists(fp string) bool {
 	return true
 }
 
+// ReadAtMost reads up to limit bytes from r, and reports an error
+// when limit bytes are read.
+func ReadAtMost(r io.Reader, limit int64) ([]byte, error) {
+	limitedReader := &io.LimitedReader{R: r, N: limit}
+	data, err := ioutil.ReadAll(limitedReader)
+	if err != nil {
+		return data, err
+	}
+	if limitedReader.N <= 0 {
+		return data, ErrLimitReached
+	}
+	return data, nil
+}
+
+// ErrLimitReached means that the read limit is reached.
+var ErrLimitReached = &trace.LimitExceededError{Message: "the read limit is reached"}
+
 const (
 	// CertTeleportUser specifies teleport user
 	CertTeleportUser = "x-teleport-user"
