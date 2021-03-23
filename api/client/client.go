7b2baf757f98d70cbc4bf41e7862f2ce6b04cbe1
Brian Joerger <bjoerger@goteleport.com>
2021-03-22 18:05:01 -0700

    Convert Trusted Cluster CRUD endpoints to gRPC.

--- api/client/client.go
+++ api/client/client.go
@@ -970,3 +970,51 @@ func (c *Client) DeleteGithubConnector(ctx context.Context, name string) error {
 	_, err := c.grpc.DeleteGithubConnector(ctx, &types.ResourceRequest{Name: name})
 	return trail.FromGRPC(err)
 }
+
+// GetTrustedCluster returns a Trusted Cluster by name.
+func (c *Client) GetTrustedCluster(ctx context.Context, name string) (types.TrustedCluster, error) {
+	if name == "" {
+		return nil, trace.BadParameter("cannot get trusted cluster, missing name")
+	}
+	req := &types.ResourceRequest{Name: name}
+	resp, err := c.grpc.GetTrustedCluster(ctx, req)
+	if err != nil {
+		return nil, trail.FromGRPC(err)
+	}
+	return resp, nil
+}
+
+// GetTrustedClusters returns a list of Trusted Clusters.
+func (c *Client) GetTrustedClusters(ctx context.Context) ([]types.TrustedCluster, error) {
+	resp, err := c.grpc.GetTrustedClusters(ctx, &empty.Empty{})
+	if err != nil {
+		return nil, trail.FromGRPC(err)
+	}
+	trustedClusters := make([]types.TrustedCluster, len(resp.TrustedClusters))
+	for i, trustedCluster := range resp.TrustedClusters {
+		trustedClusters[i] = trustedCluster
+	}
+	return trustedClusters, nil
+}
+
+// UpsertTrustedCluster creates or updates a Trusted Cluster.
+func (c *Client) UpsertTrustedCluster(ctx context.Context, trusedCluster types.TrustedCluster) (types.TrustedCluster, error) {
+	trustedCluster, ok := trusedCluster.(*types.TrustedClusterV2)
+	if !ok {
+		return nil, trace.BadParameter("invalid type %T", trusedCluster)
+	}
+	resp, err := c.grpc.UpsertTrustedCluster(ctx, trustedCluster)
+	if err != nil {
+		return nil, trail.FromGRPC(err)
+	}
+	return resp, nil
+}
+
+// DeleteTrustedCluster deletes a Trusted Cluster by name.
+func (c *Client) DeleteTrustedCluster(ctx context.Context, name string) error {
+	if name == "" {
+		return trace.BadParameter("cannot delete trusted cluster, missing name")
+	}
+	_, err := c.grpc.DeleteTrustedCluster(ctx, &types.ResourceRequest{Name: name})
+	return trail.FromGRPC(err)
+}
