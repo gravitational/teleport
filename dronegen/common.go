026d3419c2454163678de9b43d5c69b81702fb7f
Gus Luxton <gus@gravitational.com>
2021-03-22 22:32:45 -0300

    dronegen: drone config generator (#6071)

---
+++ dronegen/common.go
@@ -0,0 +1,100 @@
+package main
+
+import "fmt"
+
+var (
+	triggerPullRequest = trigger{
+		Event: triggerRef{Include: []string{"pull_request"}},
+		Repo:  triggerRef{Include: []string{"gravitational/*"}},
+	}
+	triggerPush = trigger{
+		Event:  triggerRef{Include: []string{"push"}, Exclude: []string{"pull_request"}},
+		Branch: triggerRef{Include: []string{"master", "branch/*"}},
+		Repo:   triggerRef{Include: []string{"gravitational/*"}},
+	}
+	triggerTag = trigger{
+		Event: triggerRef{Include: []string{"tag"}},
+		Ref:   triggerRef{Include: []string{"refs/tags/v*"}},
+		Repo:  triggerRef{Include: []string{"gravitational/*"}},
+	}
+
+	volumeDocker = volume{
+		Name: "dockersock",
+		Temp: &volumeTemp{},
+	}
+	volumeTmpfs = volume{
+		Name: "tmpfs",
+		Temp: &volumeTemp{Medium: "memory"},
+	}
+	volumeTmpDind = volume{
+		Name: "tmp-dind",
+		Temp: &volumeTemp{},
+	}
+	volumeTmpIntegration = volume{
+		Name: "tmp-integration",
+		Temp: &volumeTemp{},
+	}
+
+	volumeRefTmpfs = volumeRef{
+		Name: "tmpfs",
+		Path: "/tmpfs",
+	}
+	volumeRefDocker = volumeRef{
+		Name: "dockersock",
+		Path: "/var/run",
+	}
+	volumeRefTmpDind = volumeRef{
+		Name: "tmp-dind",
+		Path: "/tmp",
+	}
+	volumeRefTmpIntegration = volumeRef{
+		Name: "tmp-integration",
+		Path: "/tmp",
+	}
+
+	// TODO(gus): Set this from `make -C build.assets print-runtime-version` or similar rather
+	// than hardcoding it. Also remove the usage of RUNTIME as a pipeline-level environment variable
+	// (as support for these varies among Drone runners) and only set it for steps that need it.
+	goRuntime = value{raw: "go1.15.5"}
+)
+
+type buildType struct {
+	os      string
+	arch    string
+	fips    bool
+	centos6 bool
+}
+
+// dockerService generates a docker:dind service
+// It includes the Docker socket volume by default, plus any extra volumes passed in
+func dockerService(v ...volumeRef) service {
+	return service{
+		Name:    "Start Docker",
+		Image:   "docker:dind",
+		Volumes: append(v, volumeRefDocker),
+	}
+}
+
+// dockerVolumes returns a slice of volumes
+// It includes the Docker socket volume by default, plus any extra volumes passed in
+func dockerVolumes(v ...volume) []volume {
+	return append(v, volumeDocker)
+}
+
+// dockerVolumeRefs returns a slice of volumeRefs
+// It includes the Docker socket volumeRef as a default, plus any extra volumeRefs passed in
+func dockerVolumeRefs(v ...volumeRef) []volumeRef {
+	return append(v, volumeRefDocker)
+}
+
+// releaseMakefileTarget gets the correct Makefile target for a given arch/fips/centos6 combo
+func releaseMakefileTarget(b buildType) string {
+	makefileTarget := fmt.Sprintf("release-%s", b.arch)
+	if b.centos6 {
+		makefileTarget += "-centos6"
+	}
+	if b.fips {
+		makefileTarget += "-fips"
+	}
+	return makefileTarget
+}
---
+++ dronegen/types.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"runtime"
+	"strings"
+)
+
+// Types to mirror the YAML fields of the drone config.
+// See https://docs.drone.io/pipeline/kubernetes/syntax/ and https://docs.drone.io/pipeline/exec/syntax/.
+
+type pipeline struct {
+	comment string
+
+	Kind        string           `yaml:"kind"`
+	Type        string           `yaml:"type"`
+	Name        string           `yaml:"name"`
+	Environment map[string]value `yaml:"environment,omitempty"`
+	Trigger     trigger          `yaml:"trigger"`
+	Workspace   workspace        `yaml:"workspace,omitempty"`
+	Platform    platform         `yaml:"platform,omitempty"`
+	Clone       clone            `yaml:"clone,omitempty"`
+	DependsOn   []string         `yaml:"depends_on,omitempty"`
+	Concurrency concurrency      `yaml:"concurrency,omitempty"`
+	Steps       []step           `yaml:"steps"`
+	Services    []service        `yaml:"services,omitempty"`
+	Volumes     []volume         `yaml:"volumes,omitempty"`
+}
+
+func newKubePipeline(name string) pipeline {
+	return pipeline{
+		comment: generatedComment(),
+		Kind:    "pipeline",
+		Type:    "kubernetes",
+		Name:    name,
+		Clone:   clone{Disable: true},
+	}
+}
+
+//nolint:deadcode,unused
+func newExecPipeline(name string) pipeline {
+	return pipeline{
+		comment: generatedComment(),
+		Kind:    "pipeline",
+		Type:    "exec",
+		Name:    name,
+		Clone:   clone{Disable: true},
+	}
+}
+
+func generatedComment() string {
+	c := `################################################
+# Generated using dronegen, do not edit by hand!
+# Use 'make dronegen' to update.
+`
+	_, file, line, ok := runtime.Caller(2)
+	if ok {
+		// Trim off the local path to the repo.
+		i := strings.LastIndex(file, "dronegen")
+		if i > 0 {
+			file = file[i:]
+		}
+		c += fmt.Sprintf("# Generated at %s:%d\n", file, line)
+	}
+	c += "################################################\n\n"
+	return c
+}
+
+type trigger struct {
+	Event  triggerRef `yaml:"event,omitempty"`
+	Cron   triggerRef `yaml:"cron,omitempty"`
+	Target triggerRef `yaml:"target,omitempty"`
+	Ref    triggerRef `yaml:"ref,omitempty"`
+	Repo   triggerRef `yaml:"repo"`
+	Branch triggerRef `yaml:"branch,omitempty"`
+}
+
+type triggerRef struct {
+	Include []string `yaml:"include,omitempty"`
+	Exclude []string `yaml:"exclude,omitempty"`
+}
+
+// UnmarshalYAML parses trigger references as either a list of strings, or as
+// include/exclude lists. Both are allowed by drone.
+func (v *triggerRef) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var rawItems []string
+	if err := unmarshal(&rawItems); err == nil {
+		v.Include = rawItems
+		return nil
+	}
+	var regular struct {
+		Include []string `yaml:"include,omitempty"`
+		Exclude []string `yaml:"exclude,omitempty"`
+	}
+	if err := unmarshal(&regular); err == nil {
+		v.Include = regular.Include
+		v.Exclude = regular.Exclude
+		return nil
+	}
+	return errors.New("can't unmarshal the value as either string or from_secret reference")
+}
+
+type workspace struct {
+	Path string `yaml:"path"`
+}
+
+type clone struct {
+	Disable bool `yaml:"disable"`
+}
+
+type platform struct {
+	OS   string `yaml:"os"`
+	Arch string `yaml:"arch"`
+}
+
+type concurrency struct {
+	Limit int `yaml:"limit"`
+}
+
+type volume struct {
+	Name  string       `yaml:"name"`
+	Temp  *volumeTemp  `yaml:"temp,omitempty"`
+	Claim *volumeClaim `yaml:"claim,omitempty"`
+}
+
+type volumeTemp struct {
+	Medium string `yaml:"medium,omitempty"`
+}
+
+type volumeClaim struct {
+	Name string `yaml:"name"`
+}
+
+type service struct {
+	Name    string      `yaml:"name"`
+	Image   string      `yaml:"image"`
+	Volumes []volumeRef `yaml:"volumes"`
+}
+
+type volumeRef struct {
+	Name string `yaml:"name"`
+	Path string `yaml:"path"`
+}
+
+type step struct {
+	Name        string           `yaml:"name"`
+	Image       string           `yaml:"image"`
+	Commands    []string         `yaml:"commands"`
+	Environment map[string]value `yaml:"environment,omitempty"`
+	Volumes     []volumeRef      `yaml:"volumes,omitempty"`
+	Settings    map[string]value `yaml:"settings,omitempty"`
+	Template    []string         `yaml:"template,omitempty"`
+	When        *condition       `yaml:"when,omitempty"`
+	Failure     string           `yaml:"failure,omitempty"`
+}
+
+type condition struct {
+	Status []string `yaml:"status,omitempty"`
+}
+
+// value is a string value for key:value pairs like "environment" or
+// "settings". Values can be either inline strings (raw) or references to
+// secrets stored in Drone (fromSecret).
+type value struct {
+	raw        string
+	fromSecret string
+}
+
+type valueFromSecret struct {
+	FromSecret string `yaml:"from_secret"`
+}
+
+func (v value) MarshalYAML() (interface{}, error) {
+	if v.raw != "" && v.fromSecret != "" {
+		return nil, fmt.Errorf("value %+v has both raw and fromSecret set, can only have one", v)
+	}
+	if v.raw != "" {
+		return v.raw, nil
+	}
+	if v.fromSecret != "" {
+		return valueFromSecret{FromSecret: v.fromSecret}, nil
+	}
+	return nil, fmt.Errorf("value has neither raw nor fromSecret set, need one")
+}
+
+func (v *value) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	if err := unmarshal(&v.raw); err == nil {
+		return nil
+	}
+	var fs valueFromSecret
+	if err := unmarshal(&fs); err == nil {
+		v.fromSecret = fs.FromSecret
+		return nil
+	}
+	return errors.New("can't unmarshal the value as either string or from_secret reference")
+}
