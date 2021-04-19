b32128f4b7c73b49ee15238e6d2fdf284cf6d579
Rom DuPlain <rom.duplain@gmail.com>
2021-04-19 14:19:19 -0700

    Move "Not logged in." message.

--- tool/tsh/tsh.go
+++ tool/tsh/tsh.go
@@ -1925,7 +1925,7 @@ func onStatus(cf *CLIConf) error {
 	printProfiles(cf.Debug, profile, profiles)
 
 	if profile == nil {
-		return trace.NotFound("No active profile.")
+		return trace.NotFound("Not logged in.")
 	}
 
 	duration := time.Until(profile.ValidUntil)
@@ -1938,7 +1938,6 @@ func onStatus(cf *CLIConf) error {
 
 func printProfiles(debug bool, profile *client.ProfileStatus, profiles []*client.ProfileStatus) {
 	if profile == nil && len(profiles) == 0 {
-		fmt.Printf("Not logged in.\n")
 		return
 	}
 
