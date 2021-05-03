f3fdcbe2693fe8636f885225f1e8e38b2eab5c22
Jane Quintero <quinq@gravitational.com>
2021-05-03 15:54:25 -0700

    add various prometheus metrics

--- lib/services/server.go
+++ lib/services/server.go
@@ -19,6 +19,7 @@ package services
 import (
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/lib/defaults"
@@ -528,4 +529,10 @@ func MarshalServers(s []Server) ([]byte, error) {
 	}
 
 	return bytes, nil
+}
+
+// NodeHasMissedKeepAlives checks if node has missed its keep alive
+func NodeHasMissedKeepAlives(s Server) bool {
+	serverExpiry := s.Expiry()
+	return serverExpiry.Before(time.Now().Add(defaults.ServerAnnounceTTL - (defaults.ServerKeepAliveTTL * 2)))
 }
