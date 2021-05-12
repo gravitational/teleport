8ac634153782182cb387b15cfa6d03882e61da33
Forrest Marshall <forrest@gravitational.com>
2021-05-12 14:43:03 -0700

    add missing event variant

--- lib/auth/grpcserver.go
+++ lib/auth/grpcserver.go
@@ -401,6 +401,10 @@ func eventToGRPC(in types.Event) (*proto.Event, error) {
 		out.Resource = &proto.Event_ClusterNetworkingConfig{
 			ClusterNetworkingConfig: r,
 		}
+	case *types.AuthPreferenceV2:
+		out.Resource = &proto.Event_AuthPreference{
+			AuthPreference: r,
+		}
 	default:
 		return nil, trace.BadParameter("resource type %T is not supported", in.Resource)
 	}
