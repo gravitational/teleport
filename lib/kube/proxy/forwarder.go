40554680259e115f0ec0b5d85a3be1766c94ccb6
Jane Quintero <quinq@gravitational.com>
2021-05-11 17:10:35 -0700

    add session recording type to session end

--- lib/kube/proxy/forwarder.go
+++ lib/kube/proxy/forwarder.go
@@ -913,6 +913,7 @@ func (f *Forwarder) exec(ctx *authContext, w http.ResponseWriter, req *http.Requ
 				KubernetesClusterMetadata: ctx.eventClusterMeta(),
 				KubernetesPodMetadata:     eventPodMeta,
 				InitialCommand:            request.cmd,
+				SessionRecording:          ctx.clusterConfig.GetSessionRecording(),
 			}
 			if err := emitter.EmitAuditEvent(f.ctx, sessionEndEvent); err != nil {
 				f.log.WithError(err).Warn("Failed to emit session end event.")
