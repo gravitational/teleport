9a368274cf390e88dc7d33706c3cac124e71115b
xacrimon <joel.wejdenstal@gmail.com>
2021-05-13 21:32:14 +0200

    implemented ToEventFields and GetSessionID

--- lib/events/dynamic.go
+++ lib/events/dynamic.go
@@ -16,14 +16,46 @@ limitations under the License.
 
 package events
 
+import (
+	"github.com/gravitational/teleport/lib/utils"
+	"github.com/gravitational/trace"
+
+	"encoding/json"
+)
+
+// FromEventFields converts from the typed dynamic representation
+// to the new typed interface-style representation.
+//
+// This is mainly used to convert from the backend format used by
+// our various event backends.
 func FromEventFields(fields EventFields) (AuditEvent, error) {
 	panic("unimplemented")
 }
 
-func GetSessionID(event AuditEvent) string {
-	panic("unimplemented")
+// GetSessionID pulls the session ID from the events that have a
+// SessionMetadata. For other events an empty string is returned.
+func GetSessionID(event AuditEvent) (string, error) {
+	fields, err := ToEventFields(event)
+	if err != nil {
+		return "", trace.Wrap(err)
+	}
+
+	return fields.GetString(SessionEventID), nil
 }
 
+// ToEventFields converts from the typed interface-style event representation
+// to the old dynamic map style representation in order to provide outer compatability
+// with existing public API routes when the backend is updated with the typed events.
 func ToEventFields(event AuditEvent) (EventFields, error) {
-	panic("unimplemented")
+	encoded, err := utils.FastMarshal(event)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	var fields EventFields
+	if err := json.Unmarshal(encoded, &fields); err != nil {
+		return nil, trace.BadParameter("failed to unmarshal event %v", err)
+	}
+
+	return fields, nil
 }
