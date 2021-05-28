722558dac898619f34ce77786806513e7fb6d341
Nic Klaassen <nic@goteleport.com>
2021-05-27 18:46:27 -0700

    send client version in grpc metadata

---
+++ api/metadata/metadata.go
@@ -0,0 +1,66 @@
+/*
+Copyright 2021 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package metadata
+
+import (
+	"context"
+
+	"github.com/gravitational/teleport/api/constants"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+const versionKey = "version"
+
+var versionValue = constants.Version
+
+// SetVersion is used only for tests to set the version value that the client
+// will send in the GRPC metadata.
+func SetVersion(version string) {
+	versionValue = version
+}
+
+func addMetadataToContext(ctx context.Context) context.Context {
+	return metadata.AppendToOutgoingContext(ctx, versionKey, versionValue)
+}
+
+// StreamClientInterceptor intercepts a GRPC client stream call and adds metadata to the context
+func StreamClientInterceptor(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
+	ctx = addMetadataToContext(ctx)
+	return streamer(ctx, desc, cc, method, opts...)
+}
+
+// UnaryClientInterceptor intercepts a GRPC client unary call and adds metadata to the context
+func UnaryClientInterceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	ctx = addMetadataToContext(ctx)
+	return invoker(ctx, method, req, reply, cc, opts...)
+}
+
+// VersionFromContext can be called from a GRPC server method to return the
+// client version that was added to the GRPC metadata by
+// StreamClientInterceptor or UnaryClientInterceptor on the client.
+func VersionFromContext(ctx context.Context) (string, bool) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return "", false
+	}
+	versionList, ok := md[versionKey]
+	if !ok || len(versionList) != 1 {
+		return "", false
+	}
+	return versionList[0], true
+}
