caafaa56b5199c32f5fbb319504d9ed9e3862646
xacrimon <joel.wejdenstal@gmail.com>
2021-06-28 10:46:14 +0200

    add comment about default

--- lib/web/apiserver.go
+++ lib/web/apiserver.go
@@ -1895,6 +1895,7 @@ func toFieldsSlice(rawEvents []apievents.AuditEvent) ([]events.EventFields, erro
 //               include=session.start,session.end, all are returned if empty
 //   "order":    optional ordering of events. Can be either "asc" or "desc"
 //               for ascending and descending respectively.
+//               If no order is provided it defaults to ascending.
 func (h *Handler) clusterSearchEvents(w http.ResponseWriter, r *http.Request, p httprouter.Params, ctx *SessionContext, site reversetunnel.RemoteSite) (interface{}, error) {
 	values := r.URL.Query()
 
