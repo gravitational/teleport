c6cb86c571ac1f60933781e88e17a81533c33680
Brian Joerger <bjoerger@goteleport.com>
2021-07-02 13:27:06 -0700

    Add unimplemented authservice server and fix other merge conflicts.

--- api/client/client_test.go
+++ api/client/client_test.go
@@ -23,7 +23,6 @@ import (
 	"net"
 	"strconv"
 	"testing"
-	"time"
 
 	"github.com/gravitational/teleport/api/client/proto"
 	"github.com/gravitational/teleport/api/defaults"
@@ -102,35 +101,35 @@ const fiveMBNode = "fiveMBNode"
 func testNodes(namespace string) ([]types.Server, error) {
 	switch namespace {
 	case fiveMBNode:
-		node, err := types.NewServerWithLabels(
-			"fiveMBNode",
-			types.KindNode,
-			types.ServerSpecV2{},
-			map[string]string{
-				// Artificially make a node ~ 5MB to force
-				// ListNodes to fail regardless of chunk size.
-				"label": string(make([]byte, 5000000)),
+		node := &types.ServerV2{
+			Version: types.V2,
+			Kind:    types.KindNode,
+			Metadata: types.Metadata{
+				Name:      "node1",
+				Namespace: defaults.Namespace,
+				Labels: map[string]string{
+					// Artificially make a node ~ 5MB to force
+					// ListNodes to fail regardless of chunk size.
+					"label": string(make([]byte, 5000000)),
+				},
 			},
-		)
-		if err != nil {
-			return nil, trace.Wrap(err)
 		}
 		return []types.Server{node}, nil
 	default:
-		var err error
 		nodes := make([]types.Server, 50)
 		for i := 0; i < 50; i++ {
-			if nodes[i], err = types.NewServerWithLabels(
-				fmt.Sprintf("node%v", i),
-				types.KindNode,
-				types.ServerSpecV2{},
-				map[string]string{
-					// Artificially make each node ~ 100KB to force
-					// ListNodes to fail with chunks of >= 40.
-					"label": string(make([]byte, 100000)),
+			nodes[i] = &types.ServerV2{
+				Version: types.V2,
+				Kind:    types.KindNode,
+				Metadata: types.Metadata{
+					Name:      "node1",
+					Namespace: defaults.Namespace,
+					Labels: map[string]string{
+						// Artificially make each node ~ 100KB to force
+						// ListNodes to fail with chunks of >= 40.
+						"label": string(make([]byte, 100000)),
+					},
 				},
-			); err != nil {
-				return nil, trace.Wrap(err)
 			}
 		}
 		return nodes, nil
@@ -152,142 +151,6 @@ func (mc *mockInsecureTLSCredentials) TLSConfig() (*tls.Config, error) {
 
 func (mc *mockInsecureTLSCredentials) SSHClientConfig() (*ssh.ClientConfig, error) {
 	return nil, trace.NotImplemented("no ssh config")
-}
-
-func TestNew(t *testing.T) {
-	t.Parallel()
-	ctx := context.Background()
-	addr := "localhost:3025"
-	startMockServer(t, addr)
-
-	tests := []struct {
-		desc      string
-		config    Config
-		assertErr require.ErrorAssertionFunc
-	}{{
-		desc: "successfully dial tcp address.",
-		config: Config{
-			Addrs: []string{addr},
-			Credentials: []Credentials{
-				&mockInsecureTLSCredentials{}, // TODO(Joerger) replace insecure credentials
-			},
-			DialOpts: []grpc.DialOption{
-				grpc.WithInsecure(), // TODO(Joerger) remove insecure dial option
-			},
-		},
-		assertErr: require.NoError,
-	}, {
-		desc: "synchronously dial addr/cred pairs and succeed with the 1 good pair.",
-		config: Config{
-			Addrs: []string{"bad addr", addr, "bad addr"},
-			Credentials: []Credentials{
-				&tlsConfigCreds{nil},
-				&mockInsecureTLSCredentials{}, // TODO(Joerger) replace insecure credentials
-				&tlsConfigCreds{nil},
-			},
-			DialOpts: []grpc.DialOption{
-				grpc.WithInsecure(), // TODO(Joerger) remove insecure dial option
-			},
-		},
-		assertErr: require.NoError,
-	}, {
-		desc: "fail to dial with a bad address.",
-		config: Config{
-			DialTimeout: time.Second,
-			Addrs:       []string{"bad addr"},
-			Credentials: []Credentials{
-				&mockInsecureTLSCredentials{}, // TODO(Joerger) replace insecure credentials
-			},
-			DialOpts: []grpc.DialOption{
-				grpc.WithInsecure(), // TODO(Joerger) remove insecure dial option
-			},
-		},
-		assertErr: func(t require.TestingT, err error, _ ...interface{}) {
-			require.EqualError(t, err, "all auth methods failed\n\tcontext deadline exceeded")
-		},
-	}}
-
-	for _, tt := range tests {
-		t.Run(tt.desc, func(t *testing.T) {
-			t.Parallel()
-			clt, err := New(ctx, tt.config)
-			tt.assertErr(t, err)
-
-			if err == nil {
-				defer clt.Close()
-				// requests to the server should succeed.
-				_, err = clt.Ping(ctx)
-				require.NoError(t, err)
-			}
-		})
-	}
-}
-
-func TestNewDialBackground(t *testing.T) {
-	t.Parallel()
-	ctx := context.Background()
-	addr := "localhost:4025"
-
-	// Create client before the server is listening.
-	clt, err := New(ctx, Config{
-		DialInBackground: true,
-		Addrs:            []string{addr},
-		Credentials: []Credentials{
-			&mockInsecureTLSCredentials{}, // TODO(Joerger) replace insecure credentials
-		},
-		DialOpts: []grpc.DialOption{
-			grpc.WithInsecure(), // TODO(Joerger) remove insecure dial option
-		},
-	})
-	require.NoError(t, err)
-	defer clt.Close()
-
-	// requests to the server will result in a connection error.
-	_, err = clt.Ping(ctx)
-	require.Error(t, err)
-	require.True(t, trace.IsConnectionProblem(err))
-
-	// Start the server and wait for the client connection to be ready.
-	startMockServer(t, addr)
-	require.NoError(t, clt.waitForConnectionReady(ctx))
-
-	// requests to the server should succeed.
-	_, err = clt.Ping(ctx)
-	require.NoError(t, err)
-}
-
-func TestWaitForConnectionReady(t *testing.T) {
-	t.Parallel()
-	ctx := context.Background()
-	addr := "localhost:5025"
-
-	// Create client before the server is listening.
-	clt, err := New(ctx, Config{
-		DialInBackground: true,
-		Addrs:            []string{addr},
-		Credentials: []Credentials{
-			&mockInsecureTLSCredentials{}, // TODO(Joerger) replace insecure credentials
-		},
-		DialOpts: []grpc.DialOption{
-			grpc.WithInsecure(), // TODO(Joerger) remove insecure dial option
-		},
-	})
-	require.NoError(t, err)
-	defer clt.Close()
-
-	// WaitForConnectionReady should return false once the
-	// context is canceled if the server isn't open to connections.
-	cancelCtx, cancel := context.WithTimeout(ctx, time.Second*3)
-	defer cancel()
-	require.Error(t, clt.waitForConnectionReady(cancelCtx))
-
-	// WaitForConnectionReady should return nil if the server is open to connections.
-	startMockServer(t, addr)
-	require.NoError(t, clt.waitForConnectionReady(ctx))
-
-	// WaitForConnectionReady should return an error if the grpc connection is closed.
-	require.NoError(t, clt.GetConnection().Close())
-	require.Error(t, clt.waitForConnectionReady(ctx))
 }
 
 func TestLimitExceeded(t *testing.T) {
