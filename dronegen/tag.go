f08237d7d0ed06dddf62b7c41420e31d3edfc56b
Justinas Stankevicius <justinas@goteleport.com>
2021-07-20 15:17:07 +0300

    Fix product var usage, fix expected status code

--- dronegen/tag.go
+++ dronegen/tag.go
@@ -280,8 +280,8 @@ func tagCreateReleaseAssetCommands(b buildType) []string {
 for file in $(find . -type f ! -iname '*.sha256'); do
   product="$(basename "$file" | sed 's/-v[0-9].*//')" # extract part before -vX.Y.Z
   status_code=$(curl -s -o /tmp/curl_out.txt -w "%%{http_code}" -F product=$product -F version=$VERSION -F notes_md="# Teleport $VERSION" -F status=draft $RELEASE_HOST/releases)
-  [ $status_code = 200 ] || [ $status_code = 303 ] || (echo "curl HTTP status: $status_code"; cat /tmp/curl_out.txt)
-  curl -F description="TODO" -F os="%s" -F arch="%s" -F file=@$file -F sha256="$(cat $file.sha256)" -F release_id="teleport@$VERSION" $RELEASE_HOST/assets;
+  [ $status_code = 200 ] || [ $status_code = 409 ] || (echo "curl HTTP status: $status_code"; cat /tmp/curl_out.txt)
+  curl -F description="TODO" -F os="%s" -F arch="%s" -F file=@$file -F sha256="$(cat $file.sha256)" -F release_id="$product@$VERSION" $RELEASE_HOST/assets;
 done`,
 			b.os, b.arch /* TODO: fips */),
 	}
