1d0f74c62bb11988ccda5790cb9f7c98302f56c7
Andrej Tokarčík <andrej@goteleport.com>
2021-07-21 12:51:23 +0200

    Remove GetLock methods from Cache/ReadAccessPoint (#7593)
    The cache system does not provide freshness guarantees required
    for proper lock propagation.

--- lib/cache/collections.go
+++ lib/cache/collections.go
@@ -177,11 +177,6 @@ func setupCollections(c *Cache, watches []types.WatchKind) (map[resourceKind]col
 				return nil, trace.BadParameter("missing parameter Presence")
 			}
 			collections[resourceKind] = &databaseServer{watch: watch, Cache: c}
-		case types.KindLock:
-			if c.Access == nil {
-				return nil, trace.BadParameter("missing parameter Access")
-			}
-			collections[resourceKind] = &lock{watch: watch, Cache: c}
 		case types.KindNetworkRestrictions:
 			if c.Restrictions == nil {
 				return nil, trace.BadParameter("missing parameter Restrictions")
@@ -2008,69 +2003,6 @@ func (c *sessionRecordingConfig) processEvent(ctx context.Context, event types.E
 }
 
 func (c *sessionRecordingConfig) watchKind() types.WatchKind {
-	return c.watch
-}
-
-type lock struct {
-	*Cache
-	watch types.WatchKind
-}
-
-func (c *lock) erase(ctx context.Context) error {
-	err := c.accessCache.DeleteAllLocks(ctx)
-	if err != nil && !trace.IsNotFound(err) {
-		return trace.Wrap(err)
-	}
-	return nil
-}
-
-func (c *lock) fetch(ctx context.Context) (apply func(ctx context.Context) error, err error) {
-	resources, err := c.Access.GetLocks(ctx)
-	if err != nil {
-		return nil, trace.Wrap(err)
-	}
-	return func(ctx context.Context) error {
-		if err := c.erase(ctx); err != nil {
-			return trace.Wrap(err)
-		}
-		for _, resource := range resources {
-			c.setTTL(resource)
-			if err := c.accessCache.UpsertLock(ctx, resource); err != nil {
-				return trace.Wrap(err)
-			}
-		}
-		return nil
-	}, nil
-}
-
-func (c *lock) processEvent(ctx context.Context, event types.Event) error {
-	switch event.Type {
-	case types.OpDelete:
-		err := c.accessCache.DeleteLock(ctx, event.Resource.GetName())
-		if err != nil {
-			// Resource could be missing in the cache, expired or not created,
-			// if the first consumed event is delete.
-			if !trace.IsNotFound(err) {
-				c.WithError(err).Warn("Failed to delete lock.")
-				return trace.Wrap(err)
-			}
-		}
-	case types.OpPut:
-		resource, ok := event.Resource.(types.Lock)
-		if !ok {
-			return trace.BadParameter("unexpected type %T", event.Resource)
-		}
-		c.setTTL(resource)
-		if err := c.accessCache.UpsertLock(ctx, resource); err != nil {
-			return trace.Wrap(err)
-		}
-	default:
-		c.Warnf("Skipping unsupported event type %v.", event.Type)
-	}
-	return nil
-}
-
-func (c *lock) watchKind() types.WatchKind {
 	return c.watch
 }
 
