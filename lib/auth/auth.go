2b47fe1d8de5ca995a92f4cde53418a7054231ad
Tim Buckley <tim@goteleport.com>
2021-08-04 11:57:50 -0600

    Do not deny logins in `isMFARequired` (#7739)
    * Do not deny logins in `isMFARequired`
    
    This changes `isMFARequired` to return `false` rather than an error
    (e.g. not found) when a user is not authorized to access a particular
    resource. This defers the authorization denial until such time as the
    client actually attempts to connect.
    
    Before Teleport v6.2, clients would immediately attempt to connect to
    nodes, and if denied by RBAC rules, would print an "access denied"
    error.
    
    As of Teleport v6.2, clients first call `isMFARequired` and, if
    unauthorized, abort the connection and print a "node not found"
    error. As no connection is ever attempted, no audit log entry is
    generated.
    
    Fixes #7652
    
    * Remove dead code
    
    * Ensure users are still asked for MFA if multiple nodes match.
    
    * Remove unnecessary debug log
    
    * Update lib/auth/auth.go
    
    Co-authored-by: Brian Joerger <bjoerger@gravitational.com>
    
    * Improve clarity per review feedback
    
    * Fix improper error log
    
    Co-authored-by: Brian Joerger <bjoerger@gravitational.com>

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -2282,7 +2282,6 @@ func (a *Server) isMFARequired(ctx context.Context, checker services.AccessCheck
 	var noMFAAccessErr, notFoundErr error
 	switch t := req.Target.(type) {
 	case *proto.IsMFARequiredRequest_Node:
-		notFoundErr = trace.NotFound("node %q not found", t.Node)
 		if t.Node.Node == "" {
 			return nil, trace.BadParameter("empty Node field")
 		}
@@ -2322,7 +2321,9 @@ func (a *Server) isMFARequired(ctx context.Context, checker services.AccessCheck
 		// If at least one node requires MFA, we'll catch it.
 		for _, n := range matches {
 			err := checker.CheckAccessToServer(t.Node.Login, n, services.AccessMFAParams{AlwaysRequired: false, Verified: false})
-			if err != nil {
+
+			// Ignore other errors; they'll be caught on the real access attempt.
+			if err != nil && errors.Is(err, services.ErrSessionMFARequired) {
 				noMFAAccessErr = err
 				break
 			}
@@ -2385,17 +2386,14 @@ func (a *Server) isMFARequired(ctx context.Context, checker services.AccessCheck
 	// Errors other than ErrSessionMFARequired mean something else is wrong,
 	// most likely access denied.
 	if !errors.Is(noMFAAccessErr, services.ErrSessionMFARequired) {
-		if trace.IsAccessDenied(noMFAAccessErr) {
-			log.Infof("Access to resource denied: %v", noMFAAccessErr)
-			// notFoundErr should always be set by this point, but check it
-			// just in case.
-			if notFoundErr == nil {
-				notFoundErr = trace.NotFound("target resource not found")
-			}
-			// Mask access denied errors to prevent resource name oracles.
-			return nil, trace.Wrap(notFoundErr)
+		if !trace.IsAccessDenied(noMFAAccessErr) {
+			log.WithError(noMFAAccessErr).Warn("Could not determine MFA access")
 		}
-		return nil, trace.Wrap(noMFAAccessErr)
+
+		// Mask the access denied errors by returning false to prevent resource
+		// name oracles. Auth will be denied (and generate an audit log entry)
+		// when the client attempts to connect.
+		return &proto.IsMFARequiredResponse{Required: false}, nil
 	}
 	// If we reach here, the error from AccessChecker was
 	// ErrSessionMFARequired.
