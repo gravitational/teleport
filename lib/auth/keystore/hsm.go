c72303f35cec4896690fcbf1da81ce336962646b
Nic Klaassen <nic@goteleport.com>
2021-08-18 22:52:54 -0700

    fix api/v7

--- lib/auth/keystore/hsm.go
+++ lib/auth/keystore/hsm.go
@@ -24,7 +24,7 @@ import (
 	"golang.org/x/crypto/ssh"
 
 	"github.com/gravitational/teleport"
-	"github.com/gravitational/teleport/api/v7/types"
+	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/lib/sshutils"
 	"github.com/gravitational/trace"
 
