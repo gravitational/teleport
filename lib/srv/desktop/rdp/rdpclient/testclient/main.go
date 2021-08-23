b9e9b5384d8cae0a1b06c90bcb8ae60b8002de6d
Andrew Lytvynov <andrew@goteleport.com>
2021-08-23 21:03:52 +0000

    RDP client implementation (#7824)
    * RDP client implementation
    
    This is a minimal RDP client on top of the rdp-rs Rust crate.
    The crate is wrapped with an FFI Rust adapter, statically compiled and
    called from Go via CGO.
    
    This PR also has toy web client to test the RDP code, and just enough of
    the desktop wire protocol implementation to make it work.
    
    I excluded the RDP client from the build via tags for now to avoid
    bloating the teleport binaries.
    
    Many more things missing that will come in later PRs, to keep this one
    reviewable.
    
    * flag tweaks and updated readme for macos
    
    * Switch to C-style strings between Go and Rust
    
    * Use regular top-level C function instead ofthe cgo jump function
    
    * Fix bitmap buffer memory leak
    
    * Consistent naming for CGO types
    
    * Pass rust object reference to Go
    
    * Clean up FFI error string management
    
    * Extract a thin C wrapper for read_rdp_output
    
    * Use the log crate in rust
    
    * Small rust cleanups
    
    * Fix shellcheck nit in run.sh
    
    * Fix RDP client memory release
    
    * Allow Rust code to compile on any unix
    
    * Force Alpha channel to 100% always
    
    For some reason, on Windows 10 with bitmap decompression the Alpha
    always ends up as 0. This makes everything transparent.
    
    * Implement screen size and credential negotiation
    
    * desktop protocol: remove password prompt
    
    It was decided that supporting passwords natively is a bad product
    decision. We will only support certificate-based authn and only in
    ActiveDirectory environments.
    
    Until we implement certificate support, passwords can be injected via an
    environment variable for testing. It will be removed before the beta
    release.
    
    * Address review feedback
    
    Co-authored-by: Isaiah Becker-Mayer <isaiah@goteleport.com>

---
+++ lib/srv/desktop/rdp/rdpclient/testclient/main.go
@@ -0,0 +1,82 @@
+//+build desktop_access_beta
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"golang.org/x/net/websocket"
+
+	"github.com/gravitational/teleport/lib/srv/desktop/deskproto"
+	"github.com/gravitational/teleport/lib/srv/desktop/rdp/rdpclient"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		log.Fatalf("usage: TELEPORT_DEV_RDP_PASSWORD=password %s host:port user", os.Args[0])
+	}
+	addr := os.Args[1]
+	username := os.Args[2]
+	if os.Getenv("TELEPORT_DEV_RDP_PASSWORD") == "" {
+		log.Fatal("missing TELEPORT_DEV_RDP_PASSWORD env var")
+	}
+
+	assetPath := filepath.Join(exeDir(), "testclient")
+	log.Printf("serving assets from %q", assetPath)
+	http.Handle("/", http.FileServer(http.Dir(assetPath)))
+	http.Handle("/connect", handleConnect(addr, username))
+
+	log.Println("listening on http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe failed:", err)
+	}
+}
+
+func handleConnect(addr, username string) http.Handler {
+	return websocket.Handler(func(conn *websocket.Conn) {
+		usernameSent := false
+		c, err := rdpclient.New(rdpclient.Config{
+			Addr: addr,
+			OutputMessage: func(m deskproto.Message) error {
+				data, err := m.Encode()
+				if err != nil {
+					return fmt.Errorf("failed to encode output message: %w", err)
+				}
+				return websocket.Message.Send(conn, data)
+			},
+			InputMessage: func() (deskproto.Message, error) {
+				// Inject username as the first message.
+				if !usernameSent {
+					usernameSent = true
+					return deskproto.ClientUsername{Username: username}, nil
+				}
+				var buf []byte
+				if err := websocket.Message.Receive(conn, &buf); err != nil {
+					return nil, fmt.Errorf("failed to read input message: %w", err)
+				}
+				return deskproto.Decode(buf)
+			},
+		})
+		if err != nil {
+			log.Printf("failed to create rdpclient: %v", err)
+			return
+		}
+		if err := c.Wait(); err != nil {
+			log.Printf("failed to wait for rdpclient to finish: %v", err)
+			return
+		}
+	})
+}
+
+func exeDir() string {
+	exe, err := os.Executable()
+	if err != nil {
+		log.Println("failed to find executable path:", err)
+		return "."
+	}
+	return filepath.Dir(exe)
+}
