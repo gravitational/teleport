3ccddeb48d3647e705edc7f91793a6c1303730d0
Marek Smolinski <marek@goteleport.com>
2021-08-30 15:26:57 +0200

    update

--- lib/srv/alpnproxy/conn.go
+++ lib/srv/alpnproxy/conn.go
@@ -20,6 +20,8 @@ import (
 	"io"
 	"net"
 	"time"
+
+	"github.com/gravitational/teleport/lib/utils"
 )
 
 // newBufferedConn creates new instance of bufferedConn.
@@ -80,8 +82,8 @@ type readOnlyConn struct {
 func (conn readOnlyConn) Read(p []byte) (int, error)         { return conn.reader.Read(p) }
 func (conn readOnlyConn) Write(p []byte) (int, error)        { return 0, io.ErrClosedPipe }
 func (conn readOnlyConn) Close() error                       { return nil }
-func (conn readOnlyConn) LocalAddr() net.Addr                { return nil }
-func (conn readOnlyConn) RemoteAddr() net.Addr               { return nil }
+func (conn readOnlyConn) LocalAddr() net.Addr                { return &utils.NetAddr{} }
+func (conn readOnlyConn) RemoteAddr() net.Addr               { return &utils.NetAddr{} }
 func (conn readOnlyConn) SetDeadline(t time.Time) error      { return nil }
 func (conn readOnlyConn) SetReadDeadline(t time.Time) error  { return nil }
 func (conn readOnlyConn) SetWriteDeadline(t time.Time) error { return nil }
