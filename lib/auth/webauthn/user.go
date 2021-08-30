91449219e798e1440f1553738a224d50b5ac9ba1
Alan Parra <alan.parra@goteleport.com>
2021-08-30 16:10:10 -0300

    Adapt lib/auth/webauthn to Identity and type changes (#8082)
    Adjust lib/auth/webauthn to correctly match Identity methods (interfaces
    deviated a little during reviews) and make use of WebauthnLocalAuth to create
    and record Webauthn user IDs.
    
    * Adjust lib/auth/webauthn to Identity and configs
    * Apply default Webauthn challenge timeout
    * Create and make use of Webauthn ID

--- lib/auth/webauthn/user.go
+++ lib/auth/webauthn/user.go
@@ -17,44 +17,72 @@ limitations under the License.
 package webauthn
 
 import (
+	"context"
+
+	"github.com/google/uuid"
 	"github.com/gravitational/teleport/api/types"
+	"github.com/gravitational/trace"
 
 	wan "github.com/duo-labs/webauthn/webauthn"
 )
+
+// userIDStorage is used to keep getOrCreateUserWebauthnID as focused as
+// possible, since it's used both by login and registration.
+type userIDStorage interface {
+	UpsertWebauthnLocalAuth(ctx context.Context, user string, wla *types.WebauthnLocalAuth) error
+	GetWebauthnLocalAuth(ctx context.Context, user string) (*types.WebauthnLocalAuth, error)
+}
+
+func getOrCreateUserWebauthnID(ctx context.Context, user string, identity userIDStorage) ([]byte, error) {
+	wla, err := identity.GetWebauthnLocalAuth(ctx, user)
+	switch {
+	case trace.IsNotFound(err): // first-time user, create a new ID
+		webID := uuid.New()
+		err := identity.UpsertWebauthnLocalAuth(ctx, user, &types.WebauthnLocalAuth{
+			UserID: webID[:],
+		})
+		return webID[:], trace.Wrap(err)
+	case err != nil:
+		return nil, trace.Wrap(err)
+	default:
+		return wla.UserID, nil
+	}
+}
 
 // webUser implements a WebAuthn protocol user.
 // It is used to provide user information to WebAuthn APIs, but has no direct
 // counterpart in storage nor in other packages.
 type webUser struct {
 	credentials []wan.Credential
-	user        types.User
+	name        string
+	webID       []byte
 }
 
-func newWebUser(user types.User, idOnly bool, devices []*types.MFADevice) *webUser {
+func newWebUser(name string, webID []byte, credentialIDOnly bool, devices []*types.MFADevice) *webUser {
 	var credentials []wan.Credential
 	for _, dev := range devices {
-		c, ok := deviceToCredential(dev, idOnly)
+		c, ok := deviceToCredential(dev, credentialIDOnly)
 		if ok {
 			credentials = append(credentials, c)
 		}
 	}
 	return &webUser{
 		credentials: credentials,
-		user:        user,
+		name:        name,
+		webID:       webID,
 	}
 }
 
 func (w *webUser) WebAuthnID() []byte {
-	// TODO(codingllama): Create and initialize WebAuthn ID
-	return []byte(w.user.GetName())
+	return w.webID
 }
 
 func (w *webUser) WebAuthnName() string {
-	return w.user.GetName()
+	return w.name
 }
 
 func (w *webUser) WebAuthnDisplayName() string {
-	return w.user.GetName()
+	return w.name
 }
 
 func (w *webUser) WebAuthnIcon() string {
