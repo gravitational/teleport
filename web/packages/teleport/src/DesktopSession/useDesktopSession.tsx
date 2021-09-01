6a97c77c87d3f175dfbcb7fbf0d30426e4b719ab
Isaiah Becker-Mayer <isaiah@goteleport.com>
2021-08-31 18:57:39 -0700

    move connection string wrangling to hook

--- web/packages/teleport/src/DesktopSession/useDesktopSession.tsx
+++ web/packages/teleport/src/DesktopSession/useDesktopSession.tsx
@@ -15,32 +15,18 @@ limitations under the License.
 */
 
 import { useRef, useState, useEffect } from 'react';
-import { useRouteMatch } from 'react-router';
 import useAttempt from 'shared/hooks/useAttemptNext';
-import cfg, { UrlDesktopParams } from 'teleport/config';
 import TdpClient from 'teleport/lib/tdp/client';
-import { getAccessToken, getHostName } from 'teleport/services/api';
+import useConnectionString from './useConnectionString';
 
 export default function useDesktopSession() {
   const { attempt, setAttempt } = useAttempt('processing');
-  const desktopRouteMatch = useRouteMatch<UrlDesktopParams>(cfg.routes.desktop);
+  const { addr, username } = useConnectionString();
   const tdpClientRef = useRef<TdpClient>();
   // Flag for alerting the DesktopSession component that the TdpClient is initialized.
   const [tdpClientInitialized, setTdpClientInitialized] = useState(false);
 
   useEffect(() => {
-    if (!desktopRouteMatch) {
-      throw new Error('route did not match');
-    }
-
-    // Build the websocket address from the route's url parameters.
-    const { clusterId, username, desktopId } = desktopRouteMatch.params;
-    const addr = cfg.api.desktopWsAddr
-      .replace(':fqdm', getHostName())
-      .replace(':clusterId', clusterId)
-      .replace(':desktopId', desktopId)
-      .replace(':token', getAccessToken());
-
     // Create the TdpClient reference with the ws address and the username from the route url.
     tdpClientRef.current = new TdpClient(addr, username);
 
