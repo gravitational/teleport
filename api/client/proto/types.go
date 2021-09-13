94e80c6f3f65367458573bc9de12dce18fb40d0c
Zac Bergquist <zac.bergquist@goteleport.com>
2021-09-13 13:28:41 -0600

    Add godoc for package proto

--- api/client/proto/types.go
+++ api/client/proto/types.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package proto provides the protobuf API specification for Teleport.
 package proto
 
 import (
