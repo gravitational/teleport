ff383a14665517673df09206dc34bdd2d92c8c47
Russell Jones <rjones@gravitational.com>
2021-09-24 02:38:48 +0000

    Release 7.2.0-dev.6.

--- api/version.go
+++ api/version.go
@@ -3,7 +3,7 @@
 package api
 
 const (
-	Version = "7.2.0-dev.5"
+	Version = "7.2.0-dev.6"
 )
 
 // Gitref variable is automatically set to the output of git-describe
--- version.go
+++ version.go
@@ -3,7 +3,7 @@
 package teleport
 
 const (
-	Version = "7.2.0-dev.5"
+	Version = "7.2.0-dev.6"
 )
 
 // Gitref variable is automatically set to the output of git-describe
