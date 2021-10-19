b7c8986aadac9495ed85c924d2299025bcfb6a4d
Justinas Stankevicius <justinas@goteleport.com>
2021-10-20 00:39:14 +0300

    Tag 8.0.0-dev.6

--- api/version.go
+++ api/version.go
@@ -3,7 +3,7 @@
 package api
 
 const (
-	Version = "8.0.0-dev.5"
+	Version = "8.0.0-dev.6"
 )
 
 // Gitref variable is automatically set to the output of git-describe
--- version.go
+++ version.go
@@ -3,7 +3,7 @@
 package teleport
 
 const (
-	Version = "8.0.0-dev.5"
+	Version = "8.0.0-dev.6"
 )
 
 // Gitref variable is automatically set to the output of git-describe
