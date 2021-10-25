8cbcee72c88118bb5d5dcda3726f54d88585a340
Justinas Stankevicius <justinas@goteleport.com>
2021-10-25 17:29:50 +0300

    Add missing env vars for darwin

--- dronegen/mac_pkg.go
+++ dronegen/mac_pkg.go
@@ -74,6 +74,7 @@ func darwinPkgPipeline(name, makeTarget string, pkgGlobs []string) pipeline {
 			Name:     "Register artifacts",
 			Commands: tagCreateReleaseAssetCommands(b),
 			Environment: map[string]value{
+				"WORKSPACE_DIR": {raw: p.Workspace.Path},
 				"RELEASES_CERT": value{fromSecret: "RELEASES_CERT"},
 				"RELEASES_KEY":  value{fromSecret: "RELEASES_KEY"},
 			},
