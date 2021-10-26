f123006d2d013c1db6023d74054003f6e52837da
Justinas Stankevicius <justinas@goteleport.com>
2021-10-26 18:03:28 +0300

    Fail with body when asset upload fails

--- dronegen/tag.go
+++ dronegen/tag.go
@@ -328,7 +328,7 @@ for file in $(find . -type f ! -iname '*.sha256'); do
   shasum="$(cat "$file.sha256" | cut -d ' ' -f 1)"
   status_code=$(curl $CREDENTIALS -o $WORKSPACE_DIR/curl_out.txt -w "%%{http_code}" -F product=$product -F version=$VERSION -F notesMd="# Teleport $VERSION" -F status=draft $RELEASES_HOST/releases)
   [ $status_code = 200 ] || [ $status_code = 409 ] || (echo "curl HTTP status: $status_code"; cat $WORKSPACE_DIR/curl_out.txt && exit 1)
-  curl $CREDENTIALS --fail -o /dev/null -F description="TODO" -F os="%s" -F arch="%s" -F file=@$file -F sha256="$shasum" -F releaseId="$product@$VERSION" $RELEASES_HOST/assets;
+  curl $CREDENTIALS --fail-with-body -o /dev/null -F description="TODO" -F os="%s" -F arch="%s" -F file=@$file -F sha256="$shasum" -F releaseId="$product@$VERSION" $RELEASES_HOST/assets;
 done`,
 			b.os, b.arch /* TODO: fips */),
 	}
