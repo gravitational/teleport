15090b03302a77b05c68a9308204f91f62ec34f1
Russell Jones <rjones@gravitational.com>
2021-10-27 21:20:57 +0000

    Release 8.0.0-beta.2.

--- api/version.go
+++ api/version.go
@@ -3,7 +3,7 @@
 package api
 
 const (
-	Version = "8.0.0-dev.33"
+	Version = "8.0.0-beta.2"
 )
 
 // Gitref variable is automatically set to the output of git-describe
--- version.go
+++ version.go
@@ -3,7 +3,7 @@
 package teleport
 
 const (
-	Version = "8.0.0-dev.33"
+	Version = "8.0.0-beta.2"
 )
 
 // Gitref variable is automatically set to the output of git-describe
