1ab047e57f18e891763e81ea799377011dd861c5
xacrimon <joel.wejdenstal@gmail.com>
2021-11-08 15:43:49 +0100

    impl exec command run impl

--- tool/tsh/kube.go
+++ tool/tsh/kube.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"os/exec"
 	"strings"
 	"time"
 
@@ -108,9 +109,10 @@ func newKubeExecCommand(parent *kingpin.CmdClause) *kubeExecCommand {
 	return c
 }
 
-// TODO(joel): impl run
 func (c *kubeExecCommand) run(cf *CLIConf) error {
-	return nil
+	cmdStrings := kubeExecCommandAssembler(c)
+	cmd := exec.Command(cmdStrings[0], cmdStrings[1:]...)
+	return trace.Wrap(cmd.Run())
 }
 
 type kubeSessionsCommand struct {
