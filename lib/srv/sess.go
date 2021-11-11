1ee4b19345fd4b0e4ae5e6eb7cc1ad7c0d88d73c
xacrimon <joel.wejdenstal@gmail.com>
2021-11-11 17:56:30 +0100

    fix recheck on leave

--- lib/srv/sess.go
+++ lib/srv/sess.go
@@ -1119,7 +1119,6 @@ func (s *session) removePartyMember(party *party) {
 	defer s.mu.Unlock()
 
 	delete(s.parties, party.id)
-	s.checkAccess <- struct{}{}
 }
 
 // removeParty removes the party from the in-memory map that holds all party
@@ -1131,7 +1130,7 @@ func (s *session) removeParty(p *party) error {
 	s.removePartyMember(p)
 
 	s.writer.deleteWriter(string(p.id))
-
+	s.checkAccess <- struct{}{}
 	return nil
 }
 
