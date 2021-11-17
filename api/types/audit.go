b7dd4b6864cb5b68736c334d1ebd9c0841c35dc1
Andrew Burke <andrew.burke@goteleport.com>
2021-11-17 15:47:48 -0800

    Fix missed merge conflict

--- api/types/audit.go
+++ api/types/audit.go
@@ -228,16 +228,9 @@ func (c *ClusterAuditConfigV2) WriteTargetValue() float64 {
 	return c.Spec.WriteTargetValue
 }
 
-<<<<<<< HEAD
-// BucketOwnerFullControl is used to enable the S3 bucket owner to take control of audit
-// session objects uploaded to it.
-func (c *ClusterAuditConfigV2) BucketOwnerFullControl() bool {
-	return c.Spec.BucketOwnerFullControl
-=======
 // Clone performs a deep copy.
 func (c *ClusterAuditConfigV2) Clone() ClusterAuditConfig {
 	return proto.Clone(c).(*ClusterAuditConfigV2)
->>>>>>> master
 }
 
 // setStaticFields sets static resource header and metadata fields.
