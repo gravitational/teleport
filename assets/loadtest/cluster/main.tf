427fc174fedb818eb0d9f2a5f6b7d2b1bdc17c66
Tim Ross <tim.ross@goteleport.com>
2021-11-24 13:17:32 -0500

    allow gke cluster to pull images from gcr

--- assets/loadtest/cluster/main.tf
+++ assets/loadtest/cluster/main.tf
@@ -44,6 +44,7 @@ resource "google_container_node_pool" "loadtest" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.read_only",
     ]
   }
 }
