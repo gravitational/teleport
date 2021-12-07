aa799642491a19bfa8154fc0aba6158d29fac423
Tim Ross <tim.ross@goteleport.com>
2021-12-07 08:40:11 -0500

    make timeout test more robust

--- lib/auth/clt_test.go
+++ lib/auth/clt_test.go
@@ -33,30 +33,47 @@ func TestClient_DialTimeout(t *testing.T) {
 	}{
 		{
 			desc:    "dial timeout set to valid value",
-			timeout: 1 * time.Second,
+			timeout: 500 * time.Millisecond,
 		},
 		{
-			desc: "defaults prevent infinite timeout",
+			desc:    "defaults prevent infinite timeout",
+			timeout: 0,
 		},
 	}
 
 	for _, tt := range cases {
 		t.Run(tt.desc, func(t *testing.T) {
+			tt := tt
 			t.Parallel()
 
-			// create a client that will attempt to connect a blackholed address. The address is reserved
+			// create a client that will attempt to connect to a blackholed address. The address is reserved
 			// for benchmarking by RFC 6890.
-			clt, err := NewClient(apiclient.Config{
+			cfg := apiclient.Config{
 				DialTimeout: tt.timeout,
 				Addrs:       []string{"198.18.0.254:1234"},
 				Credentials: []apiclient.Credentials{
 					apiclient.LoadTLS(&tls.Config{}),
 				},
-			})
+			}
+			clt, err := NewClient(cfg)
 			require.NoError(t, err)
 
-			// try to create a session - this will timeout after the DialTimeout threshold is exceeded
-			require.Error(t, clt.CreateSession(session.Session{Namespace: "test"}))
+			// call this so that the DialTimeout gets updated, if necessary, so that we know how long to
+			// wait before failing this test
+			require.NoError(t, cfg.CheckAndSetDefaults())
+
+			errChan := make(chan error, 1)
+			go func() {
+				// try to create a session - this will timeout after the DialTimeout threshold is exceeded
+				errChan <- clt.CreateSession(session.Session{Namespace: "test"})
+			}()
+
+			select {
+			case err := <-errChan:
+				require.Error(t, err)
+			case <-time.After(cfg.DialTimeout + (cfg.DialTimeout / 2)):
+				t.Fatal("Timed out waiting for dial to complete")
+			}
 		})
 	}
 }
