018eeac173fdcb89a1c926d12ecf261c8d11a475
Isaiah Becker-Mayer <isaiah@goteleport.com>
2021-12-16 17:01:48 -0500

    Remove old `PlayerNext` (#513)
    * Removes unused PlayerNext, revises the player path
    
    * updates snapshots with new href

--- web/packages/teleport/src/PlayerNext/fixtures/npm.js
+++
@@ -1,5 +0,0 @@
-import ttyEvents from './npm.tty.events.json';
-import auditEvents from './npm.audit.events.json';
-import ttyStream from 'raw-loader!./npm.tty.stream';
-
-export default { ttyEvents, ttyStream, auditEvents };
