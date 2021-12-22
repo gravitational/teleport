722796db0e5c6d309909cfc4ceb7658583a8bf02
Trent Clarke <trent@goteleport.com>
2021-12-22 17:13:44 +1100

    Linter appeasement

--- dronegen/common.go
+++ dronegen/common.go
@@ -37,17 +37,9 @@ var (
 		Name: "dockersock",
 		Temp: &volumeTemp{},
 	}
-	volumeDockerTmpfs = volume{
-		Name: "dockertmpfs",
-		Temp: &volumeTemp{},
-	}
 	volumeTmpfs = volume{
 		Name: "tmpfs",
 		Temp: &volumeTemp{Medium: "memory"},
-	}
-	volumeTmpIntegration = volume{
-		Name: "tmp-integration",
-		Temp: &volumeTemp{},
 	}
 
 	volumeRefTmpfs = volumeRef{
@@ -57,14 +49,6 @@ var (
 	volumeRefDocker = volumeRef{
 		Name: "dockersock",
 		Path: "/var/run",
-	}
-	volumeRefDockerTmpfs = volumeRef{
-		Name: "dockertmpfs",
-		Path: "/var/lib/docker",
-	}
-	volumeRefTmpIntegration = volumeRef{
-		Name: "tmp-integration",
-		Path: "/tmp",
 	}
 
 	// TODO(gus): Set this from `make -C build.assets print-runtime-version` or similar rather
