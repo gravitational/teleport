a92e67c308f24cc6c28b279dcc7dfb4e2f076aa3
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2021-12-28 13:44:34 +0100

    Use a custom RequestID field instead of ResourceMetadata in AccessRequestDelete
    This is for consistency with AccessRequestCreate and other FooDelete
    events for resources that don't use the Name field in Metadata to
    identify the resource.

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -2460,9 +2460,7 @@ func (a *Server) DeleteAccessRequest(ctx context.Context, name string) error {
 			User:         ClientUsername(ctx),
 			Impersonator: ClientImpersonator(ctx),
 		},
-		ResourceMetadata: apievents.ResourceMetadata{
-			Name: name,
-		},
+		RequestID: name,
 	}); err != nil {
 		log.WithError(err).Warn("Failed to emit access request delete event.")
 	}
