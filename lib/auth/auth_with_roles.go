cfbdb175f9f9b45c3f9bce981e7c868265e5c4ba
Zac Bergquist <zac.bergquist@goteleport.com>
2021-12-28 15:03:54 -0700

    Record desktop sessions
    Here we introduce a new protobuf type (DesktopRecording) that contains
    an encoded TDP message, and update AuditWriter to treat these similarly
    to SessionPrint events (which are used for SSH session recordings).
    
    We also add desktop session playback endpoint, temporarily located at
    /webapi/sites/:site/desktopplaybacktest/:session
    which streams TDP messages from a recorded session over
    a websocket interface.

--- lib/auth/auth_with_roles.go
+++ lib/auth/auth_with_roles.go
@@ -2185,7 +2185,6 @@ func (a *ServerWithRoles) ResumeAuditStream(ctx context.Context, sid session.ID,
 	}, nil
 }
 
-// streamWithRoles verifies every event
 type streamWithRoles struct {
 	a        *ServerWithRoles
 	serverID string
