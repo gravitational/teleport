c759820d592937ef047cff157d682729f2404582
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2021-12-28 17:34:14 +0100

    fix: pass access requests through PreAuthenticatedSignIn

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -1065,9 +1065,10 @@ func (a *Server) PreAuthenticatedSignIn(user string, identity tlsca.Identity) (t
 		return nil, trace.Wrap(err)
 	}
 	sess, err := a.NewWebSession(types.NewWebSessionRequest{
-		User:   user,
-		Roles:  roles,
-		Traits: traits,
+		User:           user,
+		Roles:          roles,
+		Traits:         traits,
+		AccessRequests: identity.ActiveRequests,
 	})
 	if err != nil {
 		return nil, trace.Wrap(err)
