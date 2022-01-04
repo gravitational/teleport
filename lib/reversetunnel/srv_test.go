ea176c2b3c9034be014ee5e980b94a4a6d32f706
Trent Clarke <trent@goteleport.com>
2022-01-05 10:42:07 +1100

    Attempts to make CI integration test logs more useful (#9626)
    Actually tracking down the cause of a failure in the integration tests can 
    be hard:
    
    * It's hard to get an overall summary of what failed
    * The tests sometimes emit no output before timing out, meaning any 
      diagnostic info is lost
    * The emitted logs are too voluminous for a human to parse
    * The emitted logs can present information out of order
    * It's often hard to tell where the output from one test ends 
      and the next one begins
    
    This patch attempts to address these concerns without attempting to rewrite 
    any of the underlying teleport logging.
    
     * It improves the render-tests script to (optionally) report progress per-
       test, rather than on a per-package basis. My working hypothesis on the
       tests that time out with no output is that go test ./integration is
       waiting for the entire set of integration tests tests to be complete
       before reporting success or failure. Reporting on a per-test cycle gives
       faster feedback and means that any timed-out builds should give at least
       some idea of where they are stuck.
    
     * Adds the render-tests filter to the integration and integration-root make
       targets. This will show an overall summary of test results, as well as
        - Discarding log output from passing tests to increase signal-to-noise 
          ratio, and
        - Strongly delimiting the output from each failed test, making failures 
          easier to find.
    
     * Removes the notion of a failure-only logger in favour of post-processing
       the log events with render-tests. The failure-only logger catches log
       output from the tests and only forwards it to the console if the test 
       fails. Unfortunately, not all log output is guaranteed to pass through
       this logger (some teleport packages do not honour the configured logger,
       and reports from the go race detector certainly don't), meaning some 
       output is presented at the time it happens, and other output is batched
       and displayed at the end of the test. This makes working out what 
       happened where harder than it need be.
    
    In addition, this patch also promotes the render-tests script into a fully-
    fledged program, with appropriate makefile targets, make clean support, etc. 
    It is now also more robust in the face on non-JSON output from go test 
    (which happens if a package fails to compile).

--- lib/reversetunnel/srv_test.go
+++ lib/reversetunnel/srv_test.go
@@ -28,7 +28,6 @@ import (
 	"github.com/gravitational/teleport/lib/defaults"
 	"github.com/gravitational/teleport/lib/services"
 	"github.com/gravitational/teleport/lib/utils"
-	"github.com/gravitational/teleport/lib/utils/testlog"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/require"
@@ -58,7 +57,7 @@ func TestServerKeyAuth(t *testing.T) {
 	require.NoError(t, err)
 
 	s := &server{
-		log: testlog.FailureOnly(t),
+		log: utils.NewLoggerForTests(),
 		localAccessPoint: mockAccessPoint{
 			ca: ca,
 		},
