c9a0ed275cd7ee6921dfe8edb3f06f2ab818369b
Justinas Stankevicius <justinas@goteleport.com>
2022-01-06 22:25:52 +0200

    Add asset descriptions

--- dronegen/mac.go
+++ dronegen/mac.go
@@ -136,7 +136,7 @@ func darwinTagPipeline() pipeline {
 		},
 		{
 			Name:     "Register artifacts",
-			Commands: tagCreateReleaseAssetCommands(b),
+			Commands: tagCreateReleaseAssetCommands(b, ""),
 			Failure:  "ignore",
 			Environment: map[string]value{
 				"WORKSPACE_DIR": {raw: p.Workspace.Path},
