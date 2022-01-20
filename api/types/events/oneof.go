f73ad8171753a95783793d7b0288a8f543e1e233
Edoardo Spadolini <edoardo.spadolini@gmail.com>
2022-01-20 10:55:42 +0100

    Add the `access_request.delete` event (#9552) (#9786)
    * Add the `access_request.delete` event
    
    * fix typo
    
    Co-authored-by: Marek Smoliński <marek@goteleport.com>
    
    * Use a custom RequestID field instead of ResourceMetadata in AccessRequestDelete
    
    This is for consistency with AccessRequestCreate and other FooDelete
    events for resources that don't use the Name field in Metadata to
    identify the resource.
    
    Co-authored-by: Marek Smoliński <marek@goteleport.com>
    
    Co-authored-by: Marek Smoliński <marek@goteleport.com>

--- api/types/events/oneof.go
+++ api/types/events/oneof.go
@@ -233,6 +233,10 @@ func ToOneOf(in AuditEvent) (*OneOf, error) {
 		out.Event = &OneOf_LockDelete{
 			LockDelete: e,
 		}
+	case *AccessRequestDelete:
+		out.Event = &OneOf_AccessRequestDelete{
+			AccessRequestDelete: e,
+		}
 	default:
 		return nil, trace.BadParameter("event type %T is not supported", in)
 	}
@@ -340,6 +344,8 @@ func FromOneOf(in OneOf) (AuditEvent, error) {
 	} else if e := in.GetLockCreate(); e != nil {
 		return e, nil
 	} else if e := in.GetLockDelete(); e != nil {
+		return e, nil
+	} else if e := in.GetAccessRequestDelete(); e != nil {
 		return e, nil
 	} else {
 		if in.Event == nil {
