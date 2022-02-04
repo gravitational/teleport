82777c73a00ed584bac027032c0f17cedff4a590
Andrew Burke <31974658+atburke@users.noreply.github.com>
2022-02-04 10:00:24 -0800

    Revert "Emit event when connecting to non-Teleport server (#9370)"
    This reverts commit 570a659f39f916c3691d209035a9de78d86a7ca0.

--- lib/events/dynamic.go
+++ lib/events/dynamic.go
@@ -440,12 +440,6 @@ func FromEventFields(fields EventFields) (apievents.AuditEvent, error) {
 			return nil, trace.Wrap(err)
 		}
 		return &e, nil
-	case SessionConnectEvent:
-		var e events.SessionConnect
-		if err := utils.FastUnmarshal(data, &e); err != nil {
-			return nil, trace.Wrap(err)
-		}
-		return &e, nil
 	case AccessRequestDeleteEvent:
 		var e events.AccessRequestDelete
 		if err := utils.FastUnmarshal(data, &e); err != nil {
