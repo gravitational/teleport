e982779f0a3248712c3d23d76968d2dc30312424
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2022-02-04 14:18:30 -0500

    Dynamically resolve reverse tunnel address (#9958) (#10139)
    * Dynamically resolve reverse tunnel address
    
    The reverse tunnel address is currently a static string that is
    retrieved from config and passed around for the duration of a
    services lifetime. When the `tunnel_public_address` is changed
    on the proxy and the proxy is then restarted, all established
    reverse tunnels over the old address will fail indefinintely.
    As a means to get around this, #8102 introduced a mechanism
    that would cause nodes to restart if their connection to the
    auth server was down for a period of time. While this did
    allow the nodes to pickup the new address after the nodes
    restarted it was meant to be a stop gap until a more robust
    solution could be applid.
    
    Instead of using a static address, the reverse tunnel address
    is now resolved via a `reversetunnel.Resolver`. Anywhere that
    previoulsy relied on the static proxy address now will fetch
    the actual reverse tunnel address via the webclient by using
    the Resolver. In addition this builds on the refactoring done
    in #4290 to further simplify the reversetunnel package. Since
    we no longer track multiple proxies, all the left over bits
    that did so have been removed to accomodate using a dynamic
    reverse tunnel address.
    
    (cherry picked from commit 6cb13715ba831d6c54618d232af417783855b298)

--- lib/cache/cache.go
+++ lib/cache/cache.go
@@ -334,7 +334,7 @@ type Cache struct {
 
 	// fnCache is used to perform short ttl-based caching of the results of
 	// regularly called methods.
-	fnCache *fnCache
+	fnCache *utils.FnCache
 
 	trustCache           services.Trust
 	clusterConfigCache   services.ClusterConfiguration
@@ -604,6 +604,14 @@ func New(config Config) (*Cache, error) {
 		return nil, trace.Wrap(err)
 	}
 
+	fnCache, err := utils.NewFnCache(utils.FnCacheConfig{
+		TTL:   time.Second,
+		Clock: config.Clock,
+	})
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
 	ctx, cancel := context.WithCancel(config.Context)
 	cs := &Cache{
 		wrapper:              wrapper,
@@ -612,7 +620,7 @@ func New(config Config) (*Cache, error) {
 		Config:               config,
 		generation:           atomic.NewUint64(0),
 		initC:                make(chan struct{}),
-		fnCache:              newFnCache(time.Second),
+		fnCache:              fnCache,
 		trustCache:           local.NewCAService(wrapper),
 		clusterConfigCache:   clusterConfigCache,
 		provisionerCache:     local.NewProvisioningService(wrapper),
