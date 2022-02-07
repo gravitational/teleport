e4f908a03057c545b94d0bce040da9f29fc9ec0c
Lisa Kim <lisa@goteleport.com>
2022-02-06 23:04:16 -0800

    Define resource matcher given various filters

--- lib/services/matchers.go
+++ lib/services/matchers.go
@@ -18,6 +18,7 @@ package services
 
 import (
 	"github.com/gravitational/teleport/api/types"
+	"github.com/gravitational/trace"
 
 	"github.com/sirupsen/logrus"
 )
@@ -55,6 +56,124 @@ func MatchResourceLabels(matchers []ResourceMatcher, resource types.ResourceWith
 		}
 	}
 	return false
+}
+
+// MatchResourceByFilters returns true if all filter values given matched against the resource.
+func MatchResourceByFilters(resource types.ResourceWithLabels, filter MatchResourceFilter) (bool, error) {
+	if filter.Labels == nil && len(filter.PredicateExpression) == 0 && len(filter.SearchKeywords) == 0 {
+		return true, nil
+	}
+
+	var specResource types.ResourceWithLabels
+
+	// We assume when filtering for services like KubeService, AppServer, and DatabaseServer
+	// the user is wanting to filter the contained resource ie. KubeClusters, Application, and Database.
+	switch filter.ResourceKind {
+	case types.KindNode:
+		specResource = resource
+
+	case types.KindKubeService:
+		return matchAndFilterKubeClusters(resource, filter)
+
+	case types.KindAppServer:
+		server, ok := resource.(types.AppServer)
+		if !ok {
+			return false, trace.BadParameter("expected types.AppServer, got %T", resource)
+		}
+		specResource = server.GetApp()
+
+	case types.KindDatabaseServer:
+		server, ok := resource.(types.DatabaseServer)
+		if !ok {
+			return false, trace.BadParameter("expected types.DatabaseServer, got %T", resource)
+		}
+		specResource = server.GetDatabase()
+
+	default:
+		return false, trace.NotImplemented("filtering for resource kind %q not supported", filter.ResourceKind)
+	}
+
+	return matchResourceByFilters(specResource, filter)
+}
+
+func matchResourceByFilters(resource types.ResourceWithLabels, filter MatchResourceFilter) (bool, error) {
+	if len(filter.PredicateExpression) > 0 {
+		parser, err := NewResourceParser(resource)
+		if err != nil {
+			return false, trace.Wrap(err)
+		}
+
+		switch match, err := parser.EvalBoolPredicate(filter.PredicateExpression); {
+		case err != nil:
+			return false, trace.Wrap(err)
+		case !match:
+			return false, nil
+		}
+	}
+
+	if !types.MatchLabels(resource, filter.Labels) {
+		return false, nil
+	}
+
+	if !resource.MatchSearch(filter.SearchKeywords) {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+// matchAndFilterKubeClusters is similar to MatchResourceByFilters, but does two things in addition:
+//  1) handles kube service having a 1-N relationship (service-clusters)
+//     so each kube cluster goes through the filters
+//  2) filters out the non-matched clusters on the kube service and the kube service is
+//     updated in place with only the matched clusters
+//  3) only returns true if the service contained any matched cluster
+func matchAndFilterKubeClusters(resource types.ResourceWithLabels, filter MatchResourceFilter) (bool, error) {
+	server, ok := resource.(types.Server)
+	if !ok {
+		return false, trace.BadParameter("expected types.Server, got %T", resource)
+	}
+
+	kubeClusters := server.GetKubernetesClusters()
+
+	// Get the list of kube clusters.
+	resources := make([]types.ResourceWithLabels, len(kubeClusters))
+	for i, kube := range kubeClusters {
+		k8sV3, err := types.NewKubernetesClusterV3FromLegacyCluster(server.GetNamespace(), kube)
+		if err != nil {
+			return false, trace.Wrap(err)
+		}
+		resources[i] = k8sV3
+	}
+
+	// Apply filter to each kube cluster.
+	filtered := make([]*types.KubernetesCluster, 0, len(kubeClusters))
+	for i, resource := range resources {
+		switch match, err := matchResourceByFilters(resource, filter); {
+		case err != nil:
+			return false, trace.Wrap(err)
+		case match:
+			filtered = append(filtered, kubeClusters[i])
+		}
+	}
+
+	// Update in place with the filtered clusters.
+	server.SetKubernetesClusters(filtered)
+
+	// Length of 0 means this service does not contain any matches.
+	return len(filtered) > 0, nil
+}
+
+// MatchResourceFilter holds the filter values to match against a resource.
+type MatchResourceFilter struct {
+	// ResourceKind is the resource kind and is used to fine tune the filtering.
+	ResourceKind string
+	// Labels are the labels to match.
+	Labels map[string]string
+	// SearchKeywords is a list of search keywords to match.
+	SearchKeywords []string
+	// PredicateExpression holds boolean conditions that must be matched.
+	PredicateExpression string
 }
 
 const (
