c7879afbd5876e798f37f984aaec630d7494a897
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2022-02-08 12:13:35 +0100

    Add the `cert.create` event (#9822)
    * Add the `cert.create` event
    
    For now, this is only emitted for user certificate issuance.
    
    * Make `cert_type` a string rather than an enum
    
    * Match field names and json tags in events.Identity
    
    * Change events.Identity.Traits to be a wrappers.LabelValues/wrappers.Traits
    
    * Event code shouldn't be under T10xx anymore

--- lib/events/dynamic.go
+++ lib/events/dynamic.go
@@ -374,6 +374,12 @@ func FromEventFields(fields EventFields) (apievents.AuditEvent, error) {
 			return nil, trace.Wrap(err)
 		}
 		return &e, nil
+	case CertificateCreateEvent:
+		var e events.CertificateCreate
+		if err := utils.FastUnmarshal(data, &e); err != nil {
+			return nil, trace.Wrap(err)
+		}
+		return &e, nil
 	default:
 		return nil, trace.BadParameter("unknown event type: %q", eventType)
 	}
