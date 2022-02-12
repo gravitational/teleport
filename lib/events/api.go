cd3bf06fca77e2fb7f73556c669c5c579b013ce5
Zac Bergquist <zac.bergquist@goteleport.com>
2022-02-12 09:12:37 -0700

    Fix fake streamer implementation to match the real one
    The godoc for StreamSessionEvents read as if the errors channel is
    closed when no error occurs, so the fake streamer used for tests
    closed it, but this created non-determinism with select statements.
    
    Fix the fake streamer to match reality (the errors channel is **not**
    closed on the happy path) and update the godocs that led to the mistake.

--- lib/events/api.go
+++ lib/events/api.go
@@ -685,7 +685,7 @@ type IAuditLog interface {
 	WaitForDelivery(context.Context) error
 
 	// StreamSessionEvents streams all events from a given session recording. An error is returned on the first
-	// channel if one is encountered. Otherwise it is simply closed when the stream ends.
+	// channel if one is encountered. Otherwise the event channel is closed when the stream ends.
 	// The event channel is not closed on error to prevent race conditions in downstream select statements.
 	StreamSessionEvents(ctx context.Context, sessionID session.ID, startIndex int64) (chan apievents.AuditEvent, chan error)
 }
