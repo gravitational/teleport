abbe4b1359121f90f52a353ebb3bbdce7b5a82f2
xacrimon <joel.wejdenstal@gmail.com>
2022-02-15 18:04:56 +0100

    version + e ref update

--- api/version.go
+++ api/version.go
@@ -3,7 +3,7 @@
 package api
 
 const (
-	Version = "8.3.0-alpha1"
+	Version = "8.3.0-alpha.1"
 )
 
 // Gitref variable is automatically set to the output of git-describe
--- version.go
+++ version.go
@@ -3,7 +3,7 @@
 package teleport
 
 const (
-	Version = "8.3.0-alpha1"
+	Version = "8.3.0-alpha.1"
 )
 
 // Gitref variable is automatically set to the output of git-describe
