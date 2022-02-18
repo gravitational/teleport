4280d9cc88134a8f2a8eb12b8c52e8649c3173aa
Justinas Stankevicius <justinas@goteleport.com>
2022-02-18 15:57:34 +0200

    Fix drone signing: add tdr, use tsh v8

--- shell.nix
+++ shell.nix
@@ -1,7 +1,23 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs> { }
+, pkgsUnstable ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/48d63e924a2666baf37f4f14a18f19347fbd54a2.tar.gz") { }
+, buildGoModule ? pkgs.buildGoModule
+}:
+let tdr = buildGoModule rec {
+  pname = "tdr";
+  version = "0.0.0-${src.rev}";
+  # https://unix.stackexchange.com/questions/557977
+  src = builtins.fetchGit {
+    url = "git@github.com:gravitational/tdr.git";
+    rev = "5a17e035aec3f014553290fbd295dcdfeef8179c";
+  };
+  vendorSha256 = "07cbhyisv510jyqp4srcc5256xzclcfnalfn3h9a34x9j8qa70ph";
+};
+in
 pkgs.mkShell {
   buildInputs = with pkgs; [
     drone-cli
+    pkgsUnstable.teleport # drone app is not listed when using tsh v7
+    tdr
     zip
 
     go_1_17
