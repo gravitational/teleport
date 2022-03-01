943645d9c99d16e1491336bb1065a06c105ec053
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2022-03-01 02:59:29 -0500

    Go 1.17 build fix.

--- lib/srv/db/redis/protocol/resp2.go
+++ lib/srv/db/redis/protocol/resp2.go
@@ -102,7 +102,7 @@ func WriteCmd(wr *redis.Writer, vals interface{}) error {
 		var err error
 		v := reflect.ValueOf(val)
 
-		if v.Kind() == reflect.Pointer {
+		if v.Kind() == reflect.Ptr {
 			v = v.Elem()
 		}
 
