f671be59d6e6103bff05e9d801728ef23160f44b
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2022-03-07 21:26:29 -0500

    Enable BPF tests in CI

--- .cloudbuild/scripts/cmd/unit-tests/main.go
+++ .cloudbuild/scripts/cmd/unit-tests/main.go
@@ -177,11 +177,16 @@ func run() error {
 }
 
 func runUnitTests(workspace string) error {
+	enableTests := []string{
+		"TELEPORT_ETCD_TEST=yes",
+		"TELEPORT_XAUTH_TEST=yes",
+		"TELEPORT_BPF_TEST=yes",
+	}
+
 	cmd := exec.Command("make", "test")
 	cmd.Dir = workspace
 	cmd.Env = os.Environ()
-	cmd.Env = append(cmd.Env, "TELEPORT_ETCD_TEST=yes")
-	cmd.Env = append(cmd.Env, "TELEPORT_XAUTH_TEST=yes")
+	cmd.Env = append(cmd.Env, enableTests...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
