48771e24c9300112555a40f3003c1d315ed21f3c
Victor Sokolov <gzigzigzeo@gmail.com>
2022-03-09 20:19:18 +0300

    Latest reference docs for Terraform and event-handler

--- examples/resources/terraform/terraform-user-role.tf
+++ examples/resources/terraform/terraform-user-role.tf
@@ -15,18 +15,19 @@ provider "teleport" {
 }
 
 resource "teleport_role" "terraform-test" {
-  metadata {
+  metadata = {
     name        = "terraform-test"
     description = "Terraform test role"
+
     labels = {
-      example  = "yes"
+      example  = "yes"      
     }
   }
-  
-  spec {
-    options {
+
+  spec = {
+    options = {
       forward_agent           = false
-      max_session_ttl         = "30m"
+      max_session_ttl         = "7m"
       port_forwarding         = false
       client_idle_timeout     = "1h"
       disconnect_expired_cert = true
@@ -34,47 +35,50 @@ resource "teleport_role" "terraform-test" {
       request_access          = "denied"
     }
 
-    allow {
+    deny = {
+      logins = ["anonymous"]
+    }
+
+    allow = {
       logins = ["this-user-does-not-exist"]
 
-      rules {
+      rules = [{
         resources = ["user", "role"]
         verbs = ["list"]
-      }
+      }]
 
-      request {
+      request = {
         roles = ["example"]
-        claims_to_roles {
+        claims_to_roles = [{
           claim = "example"
           value = "example"
           roles = ["example"]
-        }
+        }]
       }
 
-      node_labels {
-         key = "example"
-         value = ["yes"]
+      node_labels = {
+        example = "yes"
       }
-    }
-
-    deny {
-      logins = ["anonymous"]
     }
   }
 }
 
 resource "teleport_user" "terraform-test" {
-  metadata {
+  depends_on = [
+    teleport_role.example
+  ]
+
+  metadata = {
     name        = "terraform-test"
     description = "Test terraform user"
-    expires     = "2022-10-12T07:20:50.52Z"
+    expires     = "2023-10-12T07:20:50Z"
 
     labels = {
       test      = "true"
     }
   }
 
-  spec {
+  spec = {
     roles = ["terraform-test"]
   }
 }
