31ebcbc9cd21049637753058068c86a9b57408f3
Fred Heinecke <fred.heinecke@goteleport.com>
2022-03-10 10:23:10 -0600

    Attempted fix of RPM naming issue

--- build.assets/build-package.sh
+++ build.assets/build-package.sh
@@ -192,11 +192,6 @@ elif [[ "${ARCH}" == "arm64" ]]; then
     TEXT_ARCH="ARMv8/ARM64"
 fi
 
-# amd64 RPMs should use CentOS 7 compatible artifacts
-if [[ "${PACKAGE_TYPE}" == "rpm" && "${ARCH}" == "x86_64" ]]; then
-    OPTIONAL_RUNTIME_SECTION+="-centos7"
-fi
-
 # set optional runtime section for filename
 if [[ "${RUNTIME}" == "fips" ]]; then
     OPTIONAL_RUNTIME_SECTION+="-fips"
