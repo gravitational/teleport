c22100760eb78bb19faaf5fa57d17b20aeb94b18
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2022-03-11 18:54:32 -0500

    `tsh` ignores `TELEPORT_HOME`
    * `tsh db connect` do not respect TELEPORT_HOME (#9226)
    
    Fix the issue where `tsh db connect` looks for home directory in `~/.tsh` even when TELEPORT_HOME environment variable is set.
    
    * Fix custom tsh home dir for some tsh commands. (#9240)
    
    TELEPORT_HOME now should work for all tsh commands.
    
    * Retry with re-login ignores TELEPORT_HOME. (#9436)
    
    Set correct home directory after `tsh` re-logins the user.

--- tool/tsh/app.go
+++ tool/tsh/app.go
@@ -40,7 +40,7 @@ func onAppLogin(cf *CLIConf) error {
 	if err != nil {
 		return trace.Wrap(err)
 	}
-	profile, err := client.StatusCurrent("", cf.Proxy)
+	profile, err := client.StatusCurrent(cf.HomePath, cf.Proxy)
 	if err != nil {
 		return trace.Wrap(err)
 	}
@@ -107,7 +107,7 @@ func onAppLogout(cf *CLIConf) error {
 	if err != nil {
 		return trace.Wrap(err)
 	}
-	profile, err := client.StatusCurrent("", cf.Proxy)
+	profile, err := client.StatusCurrent(cf.HomePath, cf.Proxy)
 	if err != nil {
 		return trace.Wrap(err)
 	}
@@ -150,7 +150,7 @@ func onAppConfig(cf *CLIConf) error {
 	if err != nil {
 		return trace.Wrap(err)
 	}
-	profile, err := client.StatusCurrent("", cf.Proxy)
+	profile, err := client.StatusCurrent(cf.HomePath, cf.Proxy)
 	if err != nil {
 		return trace.Wrap(err)
 	}
@@ -198,7 +198,7 @@ Key:       %v
 // If logged into multiple apps, returns an error unless one was specified
 // explicitly on CLI.
 func pickActiveApp(cf *CLIConf) (*tlsca.RouteToApp, error) {
-	profile, err := client.StatusCurrent("", cf.Proxy)
+	profile, err := client.StatusCurrent(cf.HomePath, cf.Proxy)
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
