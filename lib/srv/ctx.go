89ead4100298613d493fe0ff577e5e8d56eeb651
STeve Huang <xin.huang@goteleport.com>
2022-03-14 12:18:29 -0400

    fix panic child.Close() called without logger initialized

--- lib/srv/ctx.go
+++ lib/srv/ctx.go
@@ -369,6 +369,9 @@ func NewServerContext(ctx context.Context, parent *sshutils.ConnectionContext, s
 		cancel:                 cancel,
 	}
 
+	// Pre-setup log entry in case early failures need to log something.
+	child.initLogEntry()
+
 	authPref, err := srv.GetAccessPoint().GetAuthPreference(ctx)
 	if err != nil {
 		childErr := child.Close()
@@ -379,23 +382,8 @@ func NewServerContext(ctx context.Context, parent *sshutils.ConnectionContext, s
 		child.disconnectExpiredCert = identityContext.CertValidBefore
 	}
 
-	fields := log.Fields{
-		"local":        child.ServerConn.LocalAddr(),
-		"remote":       child.ServerConn.RemoteAddr(),
-		"login":        child.Identity.Login,
-		"teleportUser": child.Identity.TeleportUser,
-		"id":           child.id,
-	}
-	if !child.disconnectExpiredCert.IsZero() {
-		fields["cert"] = child.disconnectExpiredCert
-	}
-	if child.clientIdleTimeout != 0 {
-		fields["idle"] = child.clientIdleTimeout
-	}
-	child.Entry = log.WithFields(log.Fields{
-		trace.Component:       srv.Component(),
-		trace.ComponentFields: fields,
-	})
+	// Update log entry with updated values like child.disconnectExpiredCert.
+	child.initLogEntry()
 
 	lockTargets, err := ComputeLockTargets(srv, identityContext)
 	if err != nil {
@@ -454,6 +442,29 @@ func NewServerContext(ctx context.Context, parent *sshutils.ConnectionContext, s
 	child.AddCloser(child.x11rdyw)
 
 	return ctx, child, nil
+}
+
+// initLogEntry initialize log entry.
+func (c *ServerContext) initLogEntry() {
+	fields := log.Fields{
+		"login":        c.Identity.Login,
+		"teleportUser": c.Identity.TeleportUser,
+		"id":           c.id,
+	}
+	if c.ServerConn != nil {
+		fields["local"] = c.ServerConn.LocalAddr()
+		fields["remote"] = c.ServerConn.RemoteAddr()
+	}
+	if !c.disconnectExpiredCert.IsZero() {
+		fields["cert"] = c.disconnectExpiredCert
+	}
+	if c.clientIdleTimeout != 0 {
+		fields["idle"] = c.clientIdleTimeout
+	}
+	c.Entry = log.WithFields(log.Fields{
+		trace.Component:       c.srv.Component(),
+		trace.ComponentFields: fields,
+	})
 }
 
 // Parent grants access to the connection-level context of which this
