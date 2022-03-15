2630883996db9d14cd291db72c34e6fb40bffc6d
Alex McGrath <alex.mcgrath@goteleport.com>
2022-03-15 12:43:07 +0000

    Reslove comments, move all occurences of teleport.dev to use a constant

--- api/types/constants.go
+++ api/types/constants.go
@@ -299,11 +299,11 @@ const (
 const (
 	// TeleportNamespace is used as the namespace prefix for any
 	// labels defined by teleport
-	TeleportNamespace = "teleport.dev/"
+	TeleportNamespace = "teleport.dev"
 
 	// OriginLabel is a resource metadata label name used to identify a source
 	// that the resource originates from.
-	OriginLabel = "teleport.dev/origin"
+	OriginLabel = TeleportNamespace + "/origin"
 
 	// OriginConfigFile is an origin value indicating that the resource was
 	// constructed as a default value.
--- tool/tsh/tsh.go
+++ tool/tsh/tsh.go
@@ -353,8 +353,6 @@ const (
 	// establishment of a TCP connection, rather than the full HTTP round-
 	// trip that we measure against, so some tweaking may be needed.
 	proxyDefaultResolutionTimeout = 2 * time.Second
-
-	teleportNamespace = "teleport.dev"
 )
 
 // cliOption is used in tests to inject/override configuration within Run
@@ -1455,7 +1453,7 @@ func sortedLabels(labels map[string]string) string {
 	var namespaced []string
 	var result []string
 	for key, val := range labels {
-		if strings.HasPrefix(key, teleportNamespace+"/") {
+		if strings.HasPrefix(key, types.TeleportNamespace+"/") {
 			teleportNamespaced = append(teleportNamespaced, key)
 			continue
 		}
