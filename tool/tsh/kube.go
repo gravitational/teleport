c92fb5f94f0a9c25a515de9abbda8faef8bc38e0
Joel Wejdenstål <jwejdenstal@icloud.com>
2022-03-22 19:43:35 +0100

    sort sessions by creation date

--- tool/tsh/kube.go
+++ tool/tsh/kube.go
@@ -23,6 +23,7 @@ import (
 	"net"
 	"net/url"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -489,8 +490,24 @@ func (c *kubeSessionsCommand) run(cf *CLIConf) error {
 		}
 	}
 
+	sortSessions := sortSessions(filteredSessions)
+	sort.Sort(&sortSessions)
 	printSessions(filteredSessions)
 	return nil
+}
+
+type sortSessions []types.SessionTracker
+
+func (s *sortSessions) Len() int {
+	return len(*s)
+}
+
+func (s *sortSessions) Less(i, j int) bool {
+	return (*s)[i].GetCreated().Before((*s)[j].GetCreated())
+}
+
+func (s *sortSessions) Swap(i, j int) {
+	(*s)[i], (*s)[j] = (*s)[j], (*s)[i]
 }
 
 func printSessions(sessions []types.SessionTracker) {
