9a57ed6f63f7503b1c4c20d3a04dcd963a38ad39
Przemko Robakowski <przemko.robakowski@goteleport.com>
2022-04-02 02:50:40 +0200

    Support proxy protocol v2

--- lib/multiplexer/multiplexer_test.go
+++ lib/multiplexer/multiplexer_test.go
@@ -189,7 +189,7 @@ func TestMux(t *testing.T) {
 		require.Equal(t, out, remoteAddr.String())
 	})
 
-	// ProxyLine tests proxy line protocol
+	// ProxyLineV2 tests proxy protocol v2
 	t.Run("ProxyLineV2", func(t *testing.T) {
 		listener, err := net.Listen("tcp", "127.0.0.1:0")
 		require.Nil(t, err)
