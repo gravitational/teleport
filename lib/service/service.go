8aa0e78b952f5d8cfe669dddb2172ad948a3a8f4
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2022-04-08 17:26:39 +0200

    Close listeners immediately in proxy.shutdown

--- lib/service/service.go
+++ lib/service/service.go
@@ -3247,16 +3247,8 @@ func (process *TeleportProcess) initProxyEndpoint(conn *Connector) error {
 
 	// execute this when process is asked to exit:
 	process.OnExit("proxy.shutdown", func(payload interface{}) {
+		listeners.Close()
 		rcWatcher.Close()
-		defer listeners.Close()
-		// Need to shut down this listener first, because
-		// in case of graceful shutdown, if tls server was not called
-		// the shutdown could be doing nothing, as server has not
-		// started tracking the listener first. It's ok to close listener
-		// several times.
-		if listeners.kube != nil {
-			listeners.kube.Close()
-		}
 		if asyncEmitter != nil {
 			warnOnErr(asyncEmitter.Close(), log)
 		}
