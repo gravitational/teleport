14efae0a4ec41d40298bb28df9415350fdb5961f
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2022-04-14 15:57:04 +0200

    Close listeners early and emitters late

--- lib/service/service.go
+++ lib/service/service.go
@@ -3235,9 +3235,6 @@ func (process *TeleportProcess) initProxyEndpoint(conn *Connector) error {
 	process.OnExit("proxy.shutdown", func(payload interface{}) {
 		listeners.Close()
 		rcWatcher.Close()
-		if asyncEmitter != nil {
-			warnOnErr(asyncEmitter.Close(), log)
-		}
 		if payload == nil {
 			log.Infof("Shutting down immediately.")
 			if tsrv != nil {
@@ -3282,6 +3279,7 @@ func (process *TeleportProcess) initProxyEndpoint(conn *Connector) error {
 				warnOnErr(alpnServer.Close(), log)
 			}
 		}
+		warnOnErr(asyncEmitter.Close(), log)
 		warnOnErr(conn.Close(), log)
 		log.Infof("Exited.")
 	})
