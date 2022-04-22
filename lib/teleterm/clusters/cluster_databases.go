3933e03874fc1e22cff4408c6f3d0413a1123507
Rafał Cieślak <rafal.cieslak@goteleport.com>
2022-04-22 17:55:06 +0200

    Pass database name when creating certs and CLI command

--- lib/teleterm/clusters/cluster_databases.go
+++ lib/teleterm/clusters/cluster_databases.go
@@ -89,7 +89,7 @@ func (c *Cluster) GetDatabases(ctx context.Context) ([]Database, error) {
 }
 
 // ReissueDBCerts issues new certificates for specific DB access
-func (c *Cluster) ReissueDBCerts(ctx context.Context, user string, db types.Database) error {
+func (c *Cluster) ReissueDBCerts(ctx context.Context, user string, db types.Database, databaseName string) error {
 	// When generating certificate for MongoDB access, database username must
 	// be encoded into it. This is required to be able to tell which database
 	// user to authenticate the connection as.
@@ -103,6 +103,7 @@ func (c *Cluster) ReissueDBCerts(ctx context.Context, user string, db types.Data
 			ServiceName: db.GetName(),
 			Protocol:    db.GetProtocol(),
 			Username:    user,
+			Database:    databaseName,
 		},
 		AccessRequests: c.status.ActiveRequests.AccessRequests,
 	})
