b3db804fc72ae0a47452b72d415b52d7509dfd8a
Rafał Cieślak <rafal.cieslak@goteleport.com>
2022-04-26 11:38:56 +0200

    Teleport Connect: Accept database name when setting up proxy (#12173)
    * Add target_subresource_name to proto files
    
    * Pass database name when creating certs and CLI command

--- lib/teleterm/clusters/cluster_databases.go
+++ lib/teleterm/clusters/cluster_databases.go
@@ -89,7 +89,7 @@ func (c *Cluster) GetDatabases(ctx context.Context) ([]Database, error) {
 }
 
 // ReissueDBCerts issues new certificates for specific DB access
-func (c *Cluster) ReissueDBCerts(ctx context.Context, user string, db types.Database) error {
+func (c *Cluster) ReissueDBCerts(ctx context.Context, user, dbName string, db types.Database) error {
 	// When generating certificate for MongoDB access, database username must
 	// be encoded into it. This is required to be able to tell which database
 	// user to authenticate the connection as.
@@ -103,6 +103,7 @@ func (c *Cluster) ReissueDBCerts(ctx context.Context, user string, db types.Data
 			ServiceName: db.GetName(),
 			Protocol:    db.GetProtocol(),
 			Username:    user,
+			Database:    dbName,
 		},
 		AccessRequests: c.status.ActiveRequests.AccessRequests,
 	})
@@ -115,6 +116,7 @@ func (c *Cluster) ReissueDBCerts(ctx context.Context, user string, db types.Data
 		ServiceName: db.GetName(),
 		Protocol:    db.GetProtocol(),
 		Username:    user,
+		Database:    dbName,
 	}, c.status)
 	if err != nil {
 		return trace.Wrap(err)
