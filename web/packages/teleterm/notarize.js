9dbca93f9d7b5f4ddf32c1aa64726481534c6725
Zac Bergquist <zmb3@users.noreply.github.com>
2022-04-26 09:52:26 -0600

    Add support for notarization (#770)
    Notarization is performed via electron-notarize when the
    APPLE_USERNAME and APPLE_PASSWORD environment variables are set.

---
+++ web/packages/teleterm/notarize.js
@@ -0,0 +1,25 @@
+const { notarize } = require('electron-notarize');
+const package = require('./package.json');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  if (!process.env.APPLE_USERNAME || !process.env.APPLE_PASSWORD) {
+    console.warn(
+      'missing $APPLE_USERNAME and/or $APPLE_PASSWORD: notarization will be skipped'
+    );
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    appBundleId: package.build.appId,
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_USERNAME,
+    appleIdPassword: process.env.APPLE_PASSWORD,
+  });
+};
