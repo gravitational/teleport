402acb85f6e004ef35fe842f49f4797122d9be45
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2022-05-04 16:14:04 -0400

    Do not sync Database CA if leaf 9.0 (#12040)
    Database CA should not be synced when adding trusted cluster with leaf not in version lower than 10.0.

--- lib/reversetunnel/remotesite.go
+++ lib/reversetunnel/remotesite.go
@@ -428,7 +428,7 @@ func (s *remoteSite) compareAndSwapCertAuthority(ca types.CertAuthority) error {
 	return trace.CompareFailed("remote certificate authority rotation has been updated")
 }
 
-func (s *remoteSite) updateCertAuthorities(retry utils.Retry) {
+func (s *remoteSite) updateCertAuthorities(retry utils.Retry, remoteClusterVersion string) {
 	s.Debugf("Watching for cert authority changes.")
 
 	for {
@@ -441,7 +441,7 @@ func (s *remoteSite) updateCertAuthorities(retry utils.Retry) {
 			return
 		}
 
-		err := s.watchCertAuthorities()
+		err := s.watchCertAuthorities(remoteClusterVersion)
 		if err != nil {
 			switch {
 			case trace.IsNotFound(err):
@@ -460,7 +460,12 @@ func (s *remoteSite) updateCertAuthorities(retry utils.Retry) {
 	}
 }
 
-func (s *remoteSite) watchCertAuthorities() error {
+func (s *remoteSite) watchCertAuthorities(remoteClusterVersion string) error {
+	localWatchedTypes, err := s.getLocalWatchedCerts(remoteClusterVersion)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
 	localWatcher, err := services.NewCertAuthorityWatcher(s.ctx, services.CertAuthorityWatcherConfig{
 		ResourceWatcherConfig: services.ResourceWatcherConfig{
 			Component: teleport.ComponentProxy,
@@ -468,7 +473,7 @@ func (s *remoteSite) watchCertAuthorities() error {
 			Clock:     s.clock,
 			Client:    s.localAccessPoint,
 		},
-		WatchCertTypes: []types.CertAuthType{types.HostCA, types.UserCA, types.DatabaseCA},
+		WatchCertTypes: localWatchedTypes,
 	})
 	if err != nil {
 		return trace.Wrap(err)
@@ -503,8 +508,7 @@ func (s *remoteSite) watchCertAuthorities() error {
 		case cas := <-localWatcher.CertAuthorityC:
 			for _, localCA := range cas {
 				if localCA.GetClusterName() != s.srv.ClusterName ||
-					(localCA.GetType() != types.HostCA &&
-						localCA.GetType() != types.UserCA && localCA.GetType() != types.DatabaseCA) {
+					!localWatcher.IsWatched(localCA.GetType()) {
 					continue
 				}
 
@@ -543,6 +547,25 @@ func (s *remoteSite) watchCertAuthorities() error {
 			}
 		}
 	}
+}
+
+// getLocalWatchedCerts returns local certificates types that should be watched by the cert authority watcher.
+func (s *remoteSite) getLocalWatchedCerts(remoteClusterVersion string) ([]types.CertAuthType, error) {
+	localWatchedTypes := []types.CertAuthType{types.HostCA, types.UserCA}
+
+	// Delete in 11.0.
+	ver10orAbove, err := utils.MinVerWithoutPreRelease(remoteClusterVersion, constants.DatabaseCAMinVersion)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	if ver10orAbove {
+		localWatchedTypes = append(localWatchedTypes, types.DatabaseCA)
+	} else {
+		s.Debugf("Connected to remote cluster of version %s. Database CA won't be propagated.", remoteClusterVersion)
+	}
+
+	return localWatchedTypes, nil
 }
 
 func (s *remoteSite) updateLocks(retry utils.Retry) {
