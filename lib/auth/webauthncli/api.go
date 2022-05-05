c2005254994e26e27690b151bd0c4faf7b34680a
Alan Parra <alan.parra@goteleport.com>
2022-05-05 17:28:02 -0300

    Add support for touch ID in wancli.Login

--- lib/auth/webauthncli/api.go
+++ lib/auth/webauthncli/api.go
@@ -16,8 +16,11 @@ package webauthncli
 
 import (
 	"context"
+	"errors"
 
 	"github.com/gravitational/teleport/api/client/proto"
+	"github.com/gravitational/teleport/lib/auth/touchid"
+	"github.com/gravitational/trace"
 
 	wanlib "github.com/gravitational/teleport/lib/auth/webauthn"
 	log "github.com/sirupsen/logrus"
@@ -64,6 +67,36 @@ func Login(
 	ctx context.Context,
 	origin string, assertion *wanlib.CredentialAssertion, prompt LoginPrompt, opts *LoginOpts,
 ) (*proto.MFAAuthenticateResponse, string, error) {
+	var attachment AuthenticatorAttachment
+	var user string
+	if opts != nil {
+		attachment = opts.AuthenticatorAttachment
+		user = opts.User
+	}
+
+	switch attachment {
+	case AttachmentCrossPlatform:
+		log.Debug("Cross-platform login")
+		return crossPlatformLogin(ctx, origin, assertion, prompt, opts)
+	case AttachmentPlatform:
+		log.Debug("Platform login")
+		return platformLogin(origin, user, assertion)
+	default:
+		log.Debug("Attempting platform login")
+		resp, credentialUser, err := platformLogin(origin, user, assertion)
+		if !errors.Is(err, &touchid.ErrAttemptFailed{}) {
+			return resp, credentialUser, trace.Wrap(err)
+		}
+
+		log.WithError(err).Debug("Platform login failed, falling back to cross-platform")
+		return crossPlatformLogin(ctx, origin, assertion, prompt, opts)
+	}
+}
+
+func crossPlatformLogin(
+	ctx context.Context,
+	origin string, assertion *wanlib.CredentialAssertion, prompt LoginPrompt, opts *LoginOpts,
+) (*proto.MFAAuthenticateResponse, string, error) {
 	if IsFIDO2Available() {
 		log.Debug("FIDO2: Using libfido2 for assertion")
 		return FIDO2Login(ctx, origin, assertion, prompt, opts)
@@ -72,6 +105,18 @@ func Login(
 	prompt.PromptTouch()
 	resp, err := U2FLogin(ctx, origin, assertion)
 	return resp, "" /* credentialUser */, err
+}
+
+func platformLogin(origin, user string, assertion *wanlib.CredentialAssertion) (*proto.MFAAuthenticateResponse, string, error) {
+	resp, credentialUser, err := touchid.AttemptLogin(origin, user, assertion)
+	if err != nil {
+		return nil, "", err
+	}
+	return &proto.MFAAuthenticateResponse{
+		Response: &proto.MFAAuthenticateResponse_Webauthn{
+			Webauthn: wanlib.CredentialAssertionResponseToProto(resp),
+		},
+	}, credentialUser, nil
 }
 
 // Register performs client-side, U2F-compatible, Webauthn registration.
