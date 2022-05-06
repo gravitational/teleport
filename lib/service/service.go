e41709daf00c7daa880bd7cc65c460962d60a2cd
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2022-05-06 18:30:23 +0200

    yolo close of all registeredListeners at shutdown

--- lib/service/service.go
+++ lib/service/service.go
@@ -3795,6 +3795,37 @@ func (process *TeleportProcess) WaitWithContext(ctx context.Context) {
 // completion, returns context that will be closed once the shutdown is done
 func (process *TeleportProcess) StartShutdown(ctx context.Context) context.Context {
 	process.BroadcastEvent(Event{Name: TeleportExitEvent, Payload: ctx})
+
+	for _, l := range process.registeredListeners {
+		switch fl := l.listener.(type) {
+		case *net.TCPListener:
+			fd, err := fl.File()
+			if err != nil {
+				process.log.WithError(err).Warnf("Failed to get file descriptor for listener %q at shutdown.", l.typ)
+				continue
+			}
+			newListener, err := net.FileListener(fd)
+			fd.Close()
+			if err != nil {
+				go func() {
+					for {
+						c, err := newListener.Accept()
+						if err != nil {
+							process.log.WithError(err).Warnf("Failed to accept connection on listener %q at shutdown.", l.typ)
+							time.Sleep(5 * time.Second)
+							continue
+						}
+						process.log.Errorf("=========== RECEIVED CONNECTION DURING SHUTDOWN ON %q ===========", l.typ)
+						c.Close()
+					}
+				}()
+			}
+		default:
+			process.log.Warnf("Unknown listener type encountered during shutdown: %T", l.listener)
+		}
+		l.listener.Close()
+	}
+
 	localCtx, cancel := context.WithCancel(ctx)
 	go func() {
 		defer cancel()
