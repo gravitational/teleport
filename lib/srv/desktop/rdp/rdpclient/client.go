7fe5b95d9a375362dd5408a7fccb8bfacd5cba6c
Isaiah Becker-Mayer <isaiah@goteleport.com>
2022-05-09 15:35:25 -0400

    Debugf

--- lib/srv/desktop/rdp/rdpclient/client.go
+++ lib/srv/desktop/rdp/rdpclient/client.go
@@ -291,7 +291,7 @@ func (c *Client) start() {
 						wheel:  C.PointerWheelNone,
 					},
 				); err != C.ErrCodeSuccess {
-					c.cfg.Log.Warningf("write_rdp_pointer failed on the Rust side")
+					c.cfg.Log.Debugf("write_rdp_pointer failed on the Rust side")
 					return
 				}
 			case tdp.MouseButton:
@@ -317,7 +317,7 @@ func (c *Client) start() {
 						wheel:  C.PointerWheelNone,
 					},
 				); err != C.ErrCodeSuccess {
-					c.cfg.Log.Warningf("write_rdp_pointer failed on the Rust side")
+					c.cfg.Log.Debugf("write_rdp_pointer failed on the Rust side")
 					return
 				}
 			case tdp.MouseWheel:
@@ -346,7 +346,7 @@ func (c *Client) start() {
 						wheel_delta: C.int16_t(m.Delta),
 					},
 				); err != C.ErrCodeSuccess {
-					c.cfg.Log.Warningf("write_rdp_pointer failed on the Rust side")
+					c.cfg.Log.Debugf("write_rdp_pointer failed on the Rust side")
 					return
 				}
 			case tdp.KeyboardButton:
@@ -357,7 +357,7 @@ func (c *Client) start() {
 						down: m.State == tdp.ButtonPressed,
 					},
 				); err != C.ErrCodeSuccess {
-					c.cfg.Log.Warningf("write_rdp_keyboard failed on the Rust side")
+					c.cfg.Log.Debugf("write_rdp_keyboard failed on the Rust side")
 					return
 				}
 			case tdp.ClipboardData:
@@ -367,7 +367,7 @@ func (c *Client) start() {
 						(*C.uint8_t)(unsafe.Pointer(&m[0])),
 						C.uint32_t(len(m)),
 					); err != C.ErrCodeSuccess {
-						c.cfg.Log.Warningf("update_clipboard failed on the Rust side")
+						c.cfg.Log.Debugf("update_clipboard failed on the Rust side")
 						return
 					}
 				} else {
