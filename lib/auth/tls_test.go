b70009ab086366a64e1fba234902f977384b693a
Krzysztof Skrzętnicki <krzysztof.skrzetnicki@goteleport.com>
2022-05-24 11:04:50 +0200

    Fix TestGetCurrentUser test, stabilize it.

--- lib/auth/tls_test.go
+++ lib/auth/tls_test.go
@@ -940,7 +940,7 @@ func TestGetCurrentUser(t *testing.T) {
 			Description: "",
 			Labels:      nil,
 			Expires:     nil,
-			ID:          12,
+			ID:          currentUser.GetMetadata().ID,
 		},
 		Spec: types.UserSpecV2{
 			Roles: []string{"user:user1"},
