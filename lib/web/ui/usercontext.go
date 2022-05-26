daf5259bb705bdec27a59a0954d0bce047f952a0
Isaiah Becker-Mayer <isaiah@goteleport.com>
2022-05-26 17:04:06 -0400

    changes Sessions to RecordedSessions for clarity's sake

--- lib/web/ui/usercontext.go
+++ lib/web/ui/usercontext.go
@@ -50,8 +50,8 @@ type AccessCapabilities struct {
 }
 
 type userACL struct {
-	// Sessions defines access to recorded sessions.
-	Sessions access `json:"sessions"`
+	// RecordedSessions defines access to recorded sessions.
+	RecordedSessions access `json:"recordedSessions"`
 	// ActiveSessions defines access to active sessions.
 	ActiveSessions access `json:"activeSessions"`
 	// AuthConnectors defines access to auth.connectors.
@@ -201,7 +201,7 @@ func getAccessStrategy(roleset services.RoleSet) accessStrategy {
 // NewUserContext returns user context
 func NewUserContext(user types.User, userRoles services.RoleSet, features proto.Features, desktopRecordingEnabled bool) (*UserContext, error) {
 	ctx := &services.Context{User: user}
-	sessionAccess := newAccess(userRoles, ctx, types.KindSession)
+	recordedSessionAccess := newAccess(userRoles, ctx, types.KindSession)
 	activeSessionAccess := newAccess(userRoles, ctx, types.KindSSHSession)
 	roleAccess := newAccess(userRoles, ctx, types.KindRole)
 	authConnectors := newAccess(userRoles, ctx, types.KindAuthConnector)
@@ -235,7 +235,7 @@ func NewUserContext(user types.User, userRoles services.RoleSet, features proto.
 		Desktops:                desktopAccess,
 		AuthConnectors:          authConnectors,
 		TrustedClusters:         trustedClusterAccess,
-		Sessions:                sessionAccess,
+		RecordedSessions:        recordedSessionAccess,
 		ActiveSessions:          activeSessionAccess,
 		Roles:                   roleAccess,
 		Events:                  eventAccess,
