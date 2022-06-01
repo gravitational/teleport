fd5bf9a9af179d7c270f5867e4802ef4194fe0f2
Forrest Marshall <forrest@gravitational.com>
2022-06-01 00:37:31 +0000

    inventory control stream & certs

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -788,7 +788,7 @@ func testUUIDBasedProxy(t *testing.T, suite *integrationTestSuite) {
 			return "", trace.Wrap(err)
 		}
 
-		return ident.ID.HostID()
+		return ident.ID.HostID(), nil
 	}
 
 	// add two nodes with the same hostname.
