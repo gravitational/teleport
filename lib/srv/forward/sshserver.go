08072e4cb681b2a8ff494630d7755f1d4d982c41
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2022-06-02 09:28:30 -0400

    Span forwarding (#12980)
    * Span forwarding
    
    Modifies the auth grpc server to implement the OTLP Collector
    RegisterTraceServiceServer API (https://github.com/open-telemetry/opentelemetry-proto/blob/main/opentelemetry/proto/collector/trace/v1/trace_service.proto).
    This allows the auth server to receive spans from other services
    like `tsh`, `tctl`, and `tbot`. Any spans received by the auth
    server will be forwarded to the exporter configured via the
    `tracing_service` if it is enabled. All received spans will be
    dropped in the event that the`tracing_service` disabled. By
    forwarding spans to the auth server, `tsh` doesn't need to
    be provided with any of the telemetry backend information
    to have its spans exported.
    
    Adds a new `--trace` flag to `tsh` to enable collecting and
    forwarding spans to the auth server. When set, the tracing
    provider is initialized with a sampling rate of 1.0 to force
    all spans to be recorded. Teleport respects the sampling rate
    from remote spans, which means that when `--trace` is set, all
    spans from `tsh` and any downstream Teleport services will be
    recorded and exported regardless of the sampling rate that each
    Teleport service is configured with.

--- lib/srv/forward/sshserver.go
+++ lib/srv/forward/sshserver.go
@@ -43,8 +43,8 @@ import (
 	"github.com/gravitational/teleport/lib/sshutils/x11"
 	"github.com/gravitational/teleport/lib/teleagent"
 	"github.com/gravitational/teleport/lib/utils"
-	"github.com/gravitational/trace"
 
+	"github.com/gravitational/trace"
 	"github.com/jonboulle/clockwork"
 	"github.com/pborman/uuid"
 	"github.com/sirupsen/logrus"
@@ -894,6 +894,8 @@ func (s *Server) dispatch(ctx context.Context, ch ssh.Channel, req *ssh.Request,
 	scx.Debugf("Handling request %v, want reply %v.", req.Type, req.WantReply)
 
 	switch req.Type {
+	case tracessh.TracingRequest:
+		return s.handleTracingRequest(req, scx)
 	case sshutils.ExecRequest:
 		return s.termHandlers.HandleExec(ctx, ch, req, scx)
 	case sshutils.PTYRequest:
@@ -1067,6 +1069,14 @@ func (s *Server) handleSubsystem(ctx context.Context, ch ssh.Channel, req *ssh.R
 			Err:  trace.Wrap(err),
 		})
 	}()
+
+	return nil
+}
+
+func (s *Server) handleTracingRequest(req *ssh.Request, ctx *srv.ServerContext) error {
+	if _, err := ctx.RemoteSession.SendRequest(req.Type, false, req.Payload); err != nil {
+		s.log.WithError(err).Debugf("Unable to set forward tracing context")
+	}
 
 	return nil
 }
