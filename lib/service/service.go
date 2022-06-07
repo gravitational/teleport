2e67b1505d25e3a9e8a1aa7a00a39c18471545a4
Tim Ross <tim.ross@goteleport.com>
2022-06-07 18:01:08 -0400

    Manually instrument `backend.Backend`
    Adds a `trace.Tracer` to the `backend.Reporter`
    wrapper so that all `bakend.Backend` implementations
    can be traced. Further instrumentation of each specific
    backend will be added at a later date to see how long
    each sql query, or call to dynamo/etcd took within
    each backend operation.

--- lib/service/service.go
+++ lib/service/service.go
@@ -1679,6 +1679,7 @@ func (process *TeleportProcess) newAccessCache(cfg accessCacheConfig) (*cache.Ca
 	reporter, err := backend.NewReporter(backend.ReporterConfig{
 		Component: teleport.ComponentCache,
 		Backend:   mem,
+		Tracer:    process.TracingProvider.Tracer(teleport.ComponentCache),
 	})
 	if err != nil {
 		return nil, trace.Wrap(err)
@@ -4091,6 +4092,7 @@ func (process *TeleportProcess) initAuthStorage() (bk backend.Backend, err error
 	reporter, err := backend.NewReporter(backend.ReporterConfig{
 		Component: teleport.ComponentBackend,
 		Backend:   backend.NewSanitizer(bk),
+		Tracer:    process.TracingProvider.Tracer(teleport.ComponentBackend),
 	})
 	if err != nil {
 		return nil, trace.Wrap(err)
