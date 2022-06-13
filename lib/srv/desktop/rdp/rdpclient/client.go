83ca5466db8ce16568706f0cec0ccc792b15b091
Isaiah Becker-Mayer <isaiah@goteleport.com>
2022-06-13 14:52:29 -0400

    Fixing error handling for sharedDirectory*Requests when directory sharing is disabled

--- lib/srv/desktop/rdp/rdpclient/client.go
+++ lib/srv/desktop/rdp/rdpclient/client.go
@@ -544,8 +544,10 @@ func (c *Client) sharedDirectoryAcknowledge(ack tdp.SharedDirectoryAcknowledge) 
 			c.cfg.Log.Errorf("failed to send SharedDirectoryAcknowledge: %v", err)
 			return C.ErrCodeFailure
 		}
+		return C.ErrCodeSuccess
 	}
-	return C.ErrCodeSuccess
+
+	return C.ErrCodeFailure
 }
 
 //export tdp_sd_info_request
@@ -563,8 +565,10 @@ func (c *Client) sharedDirectoryInfoRequest(req tdp.SharedDirectoryInfoRequest) 
 			c.cfg.Log.Errorf("failed to send SharedDirectoryAcknowledge: %v", err)
 			return C.ErrCodeFailure
 		}
+		return C.ErrCodeSuccess
 	}
-	return C.ErrCodeSuccess
+
+	return C.ErrCodeFailure
 }
 
 //export tdp_sd_create_request
@@ -583,8 +587,10 @@ func (c *Client) sharedDirectoryCreateRequest(req tdp.SharedDirectoryCreateReque
 			c.cfg.Log.Errorf("failed to send SharedDirectoryAcknowledge: %v", err)
 			return C.ErrCodeFailure
 		}
+		return C.ErrCodeSuccess
 	}
-	return C.ErrCodeSuccess
+
+	return C.ErrCodeFailure
 }
 
 //export tdp_sd_delete_request
@@ -602,8 +608,10 @@ func (c *Client) sharedDirectoryDeleteRequest(req tdp.SharedDirectoryDeleteReque
 			c.cfg.Log.Errorf("failed to send SharedDirectoryAcknowledge: %v", err)
 			return C.ErrCodeFailure
 		}
+		return C.ErrCodeSuccess
 	}
-	return C.ErrCodeSuccess
+
+	return C.ErrCodeFailure
 }
 
 //export tdp_sd_list_request
@@ -622,8 +630,10 @@ func (c *Client) sharedDirectoryListRequest(req tdp.SharedDirectoryListRequest) 
 			c.cfg.Log.Errorf("failed to send SharedDirectoryAcknowledge: %v", err)
 			return C.ErrCodeFailure
 		}
+		return C.ErrCodeSuccess
 	}
-	return C.ErrCodeSuccess
+
+	return C.ErrCodeFailure
 }
 
 // Wait blocks until the client disconnects and runs the cleanup.
