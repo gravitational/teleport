28f9146d3d0edc4763c76f172517941b717168e0
Joel Wejdenstål <jwejdenstal@icloud.com>
2022-06-14 10:23:45 +0200

    Revert useless change

--- lib/web/apiserver.go
+++ lib/web/apiserver.go
@@ -2126,7 +2126,7 @@ func (h *Handler) siteSessionsGet(w http.ResponseWriter, r *http.Request, p http
 		}
 	}
 
-	return siteSessionsGetResponse{sessions}, nil
+	return siteSessionsGetResponse{Sessions: sessions}, nil
 }
 
 // siteSessionGet gets the list of site session by id
