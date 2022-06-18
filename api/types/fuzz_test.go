0ab9716aac8cc99fa717649afd539d98168d741c
Norbert Szetei <59439874+nszetei@users.noreply.github.com>
2022-06-18 23:28:07 +0200

    oss fuzz integration (#13473)
    * Update api/types/fuzz_test.go
    * do not fail if the file is missing
    * missing go-118-fuzz-build fix
    
    Co-authored-by: Zac Bergquist <zac.bergquist@goteleport.com>

--- api/types/fuzz_test.go
+++ api/types/fuzz_test.go
@@ -14,24 +14,18 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package protocol
+package types
 
 import (
-	"bytes"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func FuzzMongoRead(f *testing.F) {
-	f.Add([]byte{})
-	f.Add([]byte("000\xa4000000000000"))
-
-	f.Fuzz(func(t *testing.T, msgBytes []byte) {
-		msg := bytes.NewReader(msgBytes)
-
+func FuzzParseDuration(f *testing.F) {
+	f.Fuzz(func(t *testing.T, s string) {
 		require.NotPanics(t, func() {
-			_, _ = ReadMessage(msg)
+			parseDuration(s)
 		})
 	})
 }
