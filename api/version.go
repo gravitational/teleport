1d050ac6917b4f69ed03b610981b8bef8366851c
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2022-06-24 19:10:41 +0200

    v10.0.0-a2ppeer.1: delay hb on growth

--- api/version.go
+++ api/version.go
@@ -3,7 +3,7 @@
 package api
 
 const (
-	Version = "10.0.0-alpha.2"
+	Version = "10.0.0-a2ppeer.1"
 )
 
 // Gitref variable is automatically set to the output of git-describe
--- version.go
+++ version.go
@@ -3,7 +3,7 @@
 package teleport
 
 const (
-	Version = "10.0.0-alpha.2"
+	Version = "10.0.0-a2ppeer.1"
 )
 
 // Gitref variable is automatically set to the output of git-describe
