6b7779748cc9acf92a867251e1096ec56f7c82c4
Justinas Stankevicius <justinas@goteleport.com>
2022-06-27 17:40:25 +0300

    Assign Connect to both OSS and Enterprise releases

--- dronegen/tag.go
+++ dronegen/tag.go
@@ -353,13 +353,13 @@ func tagCreateReleaseAssetCommands(b buildType, packageType string, extraQualifi
 		`CREDENTIALS="--cert $WORKSPACE_DIR/releases.crt --key $WORKSPACE_DIR/releases.key"`,
 		`which curl || apk add --no-cache curl`,
 		fmt.Sprintf(`cd "$WORKSPACE_DIR/go/artifacts"
-for file in $(find . -type f ! -iname '*.sha256' ! -iname '*-unsigned.zip*'); do
+find . -type f ! -iname '*.sha256' ! -iname '*-unsigned.zip*' | while read -r file; do
   # Skip files that are not results of this build
   # (e.g. tarballs from which OS packages are made)
   [ -f "$file.sha256" ] || continue
 
   name="$(basename "$file" | sed -E 's/(-|_)v?[0-9].*$//')" # extract part before -vX.Y.Z
-  if [ "$name" = "tsh" ]; then
+  if [ "$name" = "tsh" -o "$name" = "Teleport Connect" ]; then
     products="teleport teleport-ent";
   else
     products="$name"
