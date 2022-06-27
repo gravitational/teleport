a7760a4f71c7a0faeb05bb4f15cdc99f6e65107c
Rafał Cieślak <rafal.cieslak@goteleport.com>
2022-06-27 15:43:48 +0200

    Teleport Connect: Add SetGatewayTargetSubresourceName RPC (#13476)
    * clusters.CreateGateway: Trim spaces when building a CLI command
    
    If cliCommand.Env is empty, the returned command has an empty space at
    the front.
    
    * Do not pass TargetSubresourceName when fetching certs for gateway
    
    Since we want to be able to freely specify the db name after the gateway
    creation, let's not pass the db name when creating the gateway to show
    the intent more clearly.
    
    * Make CLICommand a method on Gateway rather than a field
    
    * Extract getting cluster by resource URI to Storage

--- lib/teleterm/clusters/cluster_databases.go
+++ lib/teleterm/clusters/cluster_databases.go
@@ -98,7 +98,7 @@ func (c *Cluster) GetDatabases(ctx context.Context) ([]Database, error) {
 }
 
 // ReissueDBCerts issues new certificates for specific DB access
-func (c *Cluster) ReissueDBCerts(ctx context.Context, user, dbName string, db types.Database) error {
+func (c *Cluster) ReissueDBCerts(ctx context.Context, user string, db types.Database) error {
 	// When generating certificate for MongoDB access, database username must
 	// be encoded into it. This is required to be able to tell which database
 	// user to authenticate the connection as.
@@ -123,7 +123,6 @@ func (c *Cluster) ReissueDBCerts(ctx context.Context, user, dbName string, db ty
 				ServiceName: db.GetName(),
 				Protocol:    db.GetProtocol(),
 				Username:    user,
-				Database:    dbName,
 			},
 			AccessRequests: c.status.ActiveRequests.AccessRequests,
 		})
@@ -142,7 +141,6 @@ func (c *Cluster) ReissueDBCerts(ctx context.Context, user, dbName string, db ty
 		ServiceName: db.GetName(),
 		Protocol:    db.GetProtocol(),
 		Username:    user,
-		Database:    dbName,
 	}, c.status)
 	if err != nil {
 		return trace.Wrap(err)
