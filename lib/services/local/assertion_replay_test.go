3379527c4a209b0fa24b4623132527a13b68c188
Joel Wejdenstål <jwejdenstal@icloud.com>
2022-06-28 15:25:22 +0200

    doc

--- lib/services/local/assertion_replay_test.go
+++ lib/services/local/assertion_replay_test.go
@@ -40,11 +40,19 @@ func TestAssertionReplayService(t *testing.T) {
 		id[i] = uuid.New().String()
 	}
 
+	// first time foo
 	require.NoError(t, service.RecognizeSSOAssertion(ctx, id[0], "foo", delay(time.Hour)))
+
+	// second time foo
 	require.Error(t, service.RecognizeSSOAssertion(ctx, id[0], "foo", delay(time.Hour)))
 
+	// first time bar
 	require.NoError(t, service.RecognizeSSOAssertion(ctx, id[1], "bar", delay(time.Millisecond)))
 	time.Sleep(time.Second)
+
+	// assertion has expired, no risk of replay
 	require.NoError(t, service.RecognizeSSOAssertion(ctx, id[1], "bar", delay(time.Hour)))
+
+	// assertion should still exist
 	require.Error(t, service.RecognizeSSOAssertion(ctx, id[1], "bar", delay(time.Hour)))
 }
