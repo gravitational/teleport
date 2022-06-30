9ca9082cb080a83b449217aa14aa70c42b72b72c
Nic Klaassen <nic@goteleport.com>
2022-06-30 13:42:09 -0700

    Fix resource access requests for apps (#13955)

--- tool/tsh/access_request.go
+++ tool/tsh/access_request.go
@@ -26,7 +26,6 @@ import (
 	"github.com/gravitational/teleport"
 	"github.com/gravitational/teleport/api/client"
 	"github.com/gravitational/teleport/api/client/proto"
-	apidefaults "github.com/gravitational/teleport/api/defaults"
 	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/lib/asciitable"
 	"github.com/gravitational/teleport/lib/auth"
@@ -392,7 +391,7 @@ func onRequestSearch(cf *CLIConf) error {
 	clusterName := clusterNameResource.GetClusterName()
 
 	req := proto.ListResourcesRequest{
-		ResourceType:        searchKindFixup(cf.ResourceKind),
+		ResourceType:        services.MapResourceKindToListResourcesType(cf.ResourceKind),
 		Labels:              tc.Labels,
 		PredicateExpression: cf.PredicateExpression,
 		SearchKeywords:      tc.SearchKeywords,
@@ -406,11 +405,11 @@ func onRequestSearch(cf *CLIConf) error {
 
 	var resources types.ResourcesWithLabels
 	for _, result := range results {
-		fixedResources, err := resultKindFixup(result, cf.ResourceKind)
+		leafResources, err := services.MapListResourcesResultToLeafResource(result, cf.ResourceKind)
 		if err != nil {
 			return trace.Wrap(err)
 		}
-		resources = append(resources, fixedResources...)
+		resources = append(resources, leafResources...)
 	}
 
 	rows := [][]string{}
@@ -449,45 +448,4 @@ To request access to these resources, run
 	}
 
 	return nil
-}
-
-func searchKindFixup(kind string) string {
-	// Some resource kinds don't support search directly, run the search on the
-	// parent kind instead.
-	switch kind {
-	case types.KindApp:
-		return types.KindAppServer
-	case types.KindDatabase:
-		return types.KindDatabaseServer
-	case types.KindKubernetesCluster:
-		return types.KindKubeService
-	default:
-		return kind
-	}
-}
-
-func resultKindFixup(resource types.ResourceWithLabels, hint string) (types.ResourcesWithLabels, error) {
-	// The inverse of searchKindFixup, after the search map the result back to
-	// the kind we really want.
-	switch r := resource.(type) {
-	case types.AppServer:
-		return types.ResourcesWithLabels{r.GetApp()}, nil
-	case types.DatabaseServer:
-		return types.ResourcesWithLabels{r.GetDatabase()}, nil
-	case types.Server:
-		if hint == types.KindKubernetesCluster {
-			kubeClusters := r.GetKubernetesClusters()
-			resources := make(types.ResourcesWithLabels, 0, len(kubeClusters))
-			for _, kubeCluster := range kubeClusters {
-				resource, err := types.NewKubernetesClusterV3FromLegacyCluster(apidefaults.Namespace, kubeCluster)
-				if err != nil {
-					return nil, trace.Wrap(err)
-				}
-				resources = append(resources, resource)
-			}
-			return resources, nil
-		}
-	default:
-	}
-	return types.ResourcesWithLabels{resource}, nil
 }
