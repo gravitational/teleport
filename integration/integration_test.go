4b3c5e81c0273c61d2525366318c8f97f7c02fef
Gabriel Corado <gabrielcorado@mail.com>
2022-07-04 15:15:17 -0300

    test(integration): assert connection close

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -3284,7 +3284,7 @@ func waitAppServerTunnel(t *testing.T, tunnel reversetunnel.Server, clusterName,
 			return false
 		}
 
-		conn.Close()
+		require.NoError(t, conn.Close())
 		return true
 	}, 10*time.Second, time.Second)
 }
