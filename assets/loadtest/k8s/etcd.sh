f6938830cbbe45a5a2e9a8463187f503f52474a1
Tim Ross <tim.ross@goteleport.com>
2022-07-05 14:11:32 -0400

    fix linter

--- assets/loadtest/k8s/etcd.sh
+++ assets/loadtest/k8s/etcd.sh
@@ -5,10 +5,10 @@ set -x
 
 PEERS="etcd-0=https://etcd-0.etcd:2380,etcd-1=https://etcd-1.etcd:2380,etcd-2=https://etcd-2.etcd:2380"
 exec etcd \
-  --name ${HOSTNAME} \
-  --advertise-client-urls https://${HOSTNAME}.etcd:2379 \
+  --name ${POD_NAME} \
+  --advertise-client-urls https://${POD_NAME}.etcd:2379 \
   --listen-client-urls https://0.0.0.0:2379 \
-  --initial-advertise-peer-urls https://${HOSTNAME}.etcd:2380 \
+  --initial-advertise-peer-urls https://${POD_NAME}.etcd:2380 \
   --listen-peer-urls https://0.0.0.0:2380 \
   --initial-cluster ${PEERS} \
   --trusted-ca-file=/etc/etcd/certs/ca-cert.pem \
