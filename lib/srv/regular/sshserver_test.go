08f5d45673c1974798105be66811c4a434f1486b
Zac Bergquist <zac.bergquist@goteleport.com>
2022-07-06 18:00:41 -0600

    Apply fix to TestProxyRoundRobin

--- lib/srv/regular/sshserver_test.go
+++ lib/srv/regular/sshserver_test.go
@@ -1133,8 +1133,7 @@ func noCache(clt auth.ClientI, cacheName []string) (auth.RemoteProxyAccessPoint,
 func TestProxyRoundRobin(t *testing.T) {
 	t.Parallel()
 
-	log.Infof("[TEST START] TestProxyRoundRobin")
-	f := newFixture(t)
+	f := newFixtureWithoutDiskBasedLogging(t)
 	ctx := context.Background()
 
 	proxyClient, _ := newProxyClient(t, f.testSrv)
