579411af9f54b9874bd3d2b8a51060091dfd2e8b
Tim Ross <tim.ross@goteleport.com>
2022-07-11 15:09:38 -0400

    Allow traces to be exported to files
    Adds support for exporting traces to a file. While not recommended
    for production use, some folks may need to collect traces without
    having any telemetry infrastructure in place to store them. To do
    so they can simply update their tracing_service to point to a
    directory, as seen in the following config snippet.
    
    ```yaml
    tracing_service:
       exporter_url: "file:///var/lib/teleport/traces"
    ```
    
    The file contents will contain one json encoded otlp trace per line.
    Files written by the exporter will all follow the following naming
    convention:  <unix_timestamp>-<random_number>.trace
    
    To prevent a trace file from growing unbound forever, there is a
    default limit of 100MB, after which, the file will be rotated for
    a brand new file. Users can adjust the file size limit by adding
    a query paramter to the exporter url like: `?limit=12345`.
    
    Part of #12241

--- lib/observability/tracing/tracing_test.go
+++ lib/observability/tracing/tracing_test.go
@@ -218,11 +218,23 @@ func TestNewExporter(t *testing.T) {
 			errAssertion:      require.NoError,
 			exporterAssertion: require.NotNil,
 		},
+		{
+			name: "file exporter",
+			config: Config{
+				Service:     "test",
+				ExporterURL: "file://" + t.TempDir(),
+			},
+			errAssertion:      require.NoError,
+			exporterAssertion: require.NotNil,
+		},
 	}
 
 	for _, tt := range cases {
 		t.Run(tt.name, func(t *testing.T) {
 			exporter, err := NewExporter(context.Background(), tt.config)
+			if exporter != nil {
+				t.Cleanup(func() { require.NoError(t, exporter.Shutdown(context.Background())) })
+			}
 			tt.errAssertion(t, err)
 			tt.exporterAssertion(t, exporter)
 		})
@@ -476,6 +488,26 @@ func TestConfig_CheckAndSetDefaults(t *testing.T) {
 				Host:   "localhost:8080",
 			},
 		},
+		{
+			name: "file exporter",
+			cfg: Config{
+				Service:      "test",
+				SamplingRate: 1.0,
+				ExporterURL:  "file:///var/lib/teleport",
+				DialTimeout:  time.Millisecond,
+			},
+			errorAssertion: require.NoError,
+			expectedCfg: Config{
+				Service:      "test",
+				ExporterURL:  "file:///var/lib/teleport",
+				SamplingRate: 1.0,
+				DialTimeout:  time.Millisecond,
+			},
+			expectedURL: &url.URL{
+				Scheme: "file",
+				Host:   "/var/lib/teleport",
+			},
+		},
 	}
 
 	for _, tt := range cases {
@@ -535,6 +567,26 @@ func TestConfig_Endpoint(t *testing.T) {
 				DialTimeout:  time.Millisecond,
 			},
 			expected: "collector.opentelemetry.svc:4317",
+		},
+		{
+			name: "file exporter",
+			cfg: Config{
+				Service:      "test",
+				ExporterURL:  "file:///var/lib/teleport",
+				SamplingRate: 1.0,
+				DialTimeout:  time.Millisecond,
+			},
+			expected: "/var/lib/teleport",
+		},
+		{
+			name: "file exporter with limit",
+			cfg: Config{
+				Service:      "test",
+				ExporterURL:  "file:///var/lib/teleport?limit=200",
+				SamplingRate: 1.0,
+				DialTimeout:  time.Millisecond,
+			},
+			expected: "/var/lib/teleport",
 		},
 	}
 
