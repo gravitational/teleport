2b06db474705b87ec937cc55656f17745bc344ab
Nic Klaassen <nic@goteleport.com>
2022-07-11 13:56:11 -0700

    use v5 role for cert authority

--- lib/services/role.go
+++ lib/services/role.go
@@ -165,7 +165,7 @@ func RoleForUser(u types.User) types.Role {
 
 // RoleForCertAuthority creates role using types.CertAuthority.
 func RoleForCertAuthority(ca types.CertAuthority) types.Role {
-	role, _ := types.NewRoleV3(RoleNameForCertAuthority(ca.GetClusterName()), types.RoleSpecV5{
+	role, _ := types.NewRole(RoleNameForCertAuthority(ca.GetClusterName()), types.RoleSpecV5{
 		Options: types.RoleOptions{
 			MaxSessionTTL: types.NewDuration(defaults.MaxCertDuration),
 		},
