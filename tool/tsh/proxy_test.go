4d62388aa8d9ab1bbe9bcebc48b974c9b00cf390
Krzysztof Skrzętnicki <krzysztof.skrzetnicki@goteleport.com>
2022-07-14 11:49:34 +0200

    Debugging testing discrepancy.

--- tool/tsh/proxy_test.go
+++ tool/tsh/proxy_test.go
@@ -82,7 +82,12 @@ func TestTSHSSH(t *testing.T) {
 }
 
 func testRootClusterSSHAccess(t *testing.T, s *suite) {
-	err := Run(context.Background(), []string{
+	run := func(ctx context.Context, args []string, opts ...cliOption) error {
+		t.Logf("Running Run() with args=%v, opts=%v", args, opts)
+		return Run(ctx, args, opts...)
+	}
+
+	err := run(context.Background(), []string{
 		"login",
 		"--insecure",
 		"--debug",
@@ -93,7 +98,7 @@ func testRootClusterSSHAccess(t *testing.T, s *suite) {
 		return nil
 	})
 	require.NoError(t, err)
-	err = Run(context.Background(), []string{
+	err = run(context.Background(), []string{
 		"ssh",
 		s.root.Config.Hostname,
 		"echo", "hello",
@@ -101,7 +106,7 @@ func testRootClusterSSHAccess(t *testing.T, s *suite) {
 	require.NoError(t, err)
 
 	identityFile := path.Join(t.TempDir(), "identity.pem")
-	err = Run(context.Background(), []string{
+	err = run(context.Background(), []string{
 		"login",
 		"--insecure",
 		"--debug",
@@ -114,7 +119,7 @@ func testRootClusterSSHAccess(t *testing.T, s *suite) {
 	})
 	require.NoError(t, err)
 
-	err = Run(context.Background(), []string{
+	err = run(context.Background(), []string{
 		"--proxy", s.root.Config.Proxy.WebAddr.String(),
 		"--insecure",
 		"-i", identityFile,
