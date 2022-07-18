b2dcbbdb54f50187e3b1c33063c9aa79adb65944
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2022-07-18 14:05:29 +0200

    Fix tests

--- lib/teleterm/teleterm_test.go
+++ lib/teleterm/teleterm_test.go
@@ -17,19 +17,27 @@ package teleterm_test
 import (
 	"context"
 	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 	"time"
 
+	"github.com/gravitational/teleport/lib/fixtures"
 	"github.com/gravitational/teleport/lib/teleterm"
-
 	"github.com/stretchr/testify/require"
 )
 
 func TestStart(t *testing.T) {
 	homeDir := t.TempDir()
+	certsDir := t.TempDir()
+
+	require.NoError(t, os.WriteFile(filepath.Join(certsDir, "cert.crt"), []byte(fixtures.TLSCACertPEM), 0600))
+	require.NoError(t, os.WriteFile(filepath.Join(certsDir, "cert.key"), []byte(fixtures.TLSCAKeyPEM), 0600))
+
 	cfg := teleterm.Config{
-		Addr:    fmt.Sprintf("unix://%v/teleterm.sock", homeDir),
-		HomeDir: fmt.Sprintf("%v/", homeDir),
+		Addr:     fmt.Sprintf("unix://%v/teleterm.sock", homeDir),
+		HomeDir:  homeDir,
+		CertsDir: certsDir,
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
