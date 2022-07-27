174578582b9e5b9a0fb241eccf3a495e4eb5a732
Justinas Stankevicius <justinas@goteleport.com>
2022-07-27 16:47:59 +0300

    Tag v10.0.3-justinas.1

--- api/version.go
+++ api/version.go
@@ -3,7 +3,7 @@
 package api
 
 const (
-	Version = "10.0.2"
+	Version = "10.0.2-justinas.1"
 )
 
 // Gitref variable is automatically set to the output of git-describe
--- version.go
+++ version.go
@@ -3,7 +3,7 @@
 package teleport
 
 const (
-	Version = "10.0.2"
+	Version = "10.0.2-justinas.1"
 )
 
 // Gitref variable is automatically set to the output of git-describe
