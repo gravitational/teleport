7d80611de966973dd5ac6a98bbebb6019e98b390
Krzysztof Skrzętnicki <krzysztof.skrzetnicki@goteleport.com>
2022-07-27 13:14:55 +0200

    Alias support for `tsh` (#13305)
    * aliases are read from global and user configs.
    * we prevent Kingpin from terminating `tsh`; we handle parsing errors better.
    * added support for `TELEPORT_DEBUG` env variable, changed how logging is initialized.
    * debugging aliases is possible via `TELEPORT_DEBUG=1` env variable; `--debug` is ineffective as it comes into play too late.
    * if alias definition calls `tsh`, we call the `Run()` function directly instead of spawning fresh `tsh`; this improves the UX.
    * alias loops are detected and a proper error is shown.
    * all flags are made repeatable; if only one value for a given flag is possible, the last instance of the flag will be effective.
    
    Co-authored-by: Marek Smoliński <marek@goteleport.com>

--- lib/utils/cli.go
+++ lib/utils/cli.go
@@ -286,6 +286,9 @@ func Consolef(w io.Writer, log log.FieldLogger, component, msg string, params ..
 func InitCLIParser(appName, appHelp string) (app *kingpin.Application) {
 	app = kingpin.New(appName, appHelp)
 
+	// make all flags repeatable, this makes the CLI easier to use.
+	app.AllRepeatable(true)
+
 	// hide "--help" flag
 	app.HelpFlag.Hidden()
 	app.HelpFlag.NoEnvar()
