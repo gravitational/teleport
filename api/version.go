125fa091b5977b53300fd568f3222ec174a9e2d3
Rafał Cieślak <rafal.cieslak@goteleport.com>
2022-07-29 12:41:39 +0200

    Update version to 11.0.0-dev.11

--- api/version.go
+++ api/version.go
@@ -3,7 +3,7 @@
 package api
 
 const (
-	Version = "11.0.0-dev.9"
+	Version = "11.0.0-dev.11"
 )
 
 // Gitref variable is automatically set to the output of git-describe
--- version.go
+++ version.go
@@ -3,7 +3,7 @@
 package teleport
 
 const (
-	Version = "11.0.0-dev.9"
+	Version = "11.0.0-dev.11"
 )
 
 // Gitref variable is automatically set to the output of git-describe
