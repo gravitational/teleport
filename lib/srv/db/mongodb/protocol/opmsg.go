b56cfe66d6be4cbad6e16e87f29b105c2773a8b0
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2022-08-01 10:34:08 -0400

    Fix wrong formatting

--- lib/srv/db/mongodb/protocol/opmsg.go
+++ lib/srv/db/mongodb/protocol/opmsg.go
@@ -281,11 +281,11 @@ func readOpMsg(header MessageHeader, payload []byte) (*MessageOpMsg, error) {
 
 // validateDocumentSize validates document length encoded in the message.
 func validateDocumentSize(src []byte) error {
-        const headerLen = 4
+	const headerLen = 4
 	if len(src) < headerLen {
 		return trace.BadParameter("document is too short")
 	}
-	
+
 	// document length is encoded in the first 4 bytes
 	documentLength := int(int32(src[0]) | int32(src[1])<<8 | int32(src[2])<<16 | int32(src[3])<<24)
 
