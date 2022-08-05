87168a27ef811db0f4d33e3841991404e15ab73a
Joel Wejdenstål <jwejdenstal@icloud.com>
2022-08-05 12:23:18 +0200

    fix delete race condition

--- lib/kube/proxy/forwarder.go
+++ lib/kube/proxy/forwarder.go
@@ -1235,7 +1235,9 @@ func (f *Forwarder) exec(ctx *authContext, w http.ResponseWriter, req *http.Requ
 	}
 
 	<-party.closeC
+	f.mu.Lock()
 	delete(f.sessions, session.id)
+	f.mu.Unlock()
 	return nil, nil
 }
 
