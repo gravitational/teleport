159ca422f5fea405b9a094e45b8cb87f25bcdfb8
Joel Wejdenstål <jwejdenstal@icloud.com>
2022-08-05 12:24:14 +0200

    fix join race condition

--- lib/kube/proxy/forwarder.go
+++ lib/kube/proxy/forwarder.go
@@ -784,7 +784,9 @@ func (f *Forwarder) join(ctx *authContext, w http.ResponseWriter, req *http.Requ
 		return nil, trace.Wrap(err)
 	}
 
+	f.mu.Lock()
 	session := f.sessions[sessionID]
+	f.mu.Unlock()
 	if session == nil {
 		return nil, trace.NotFound("session %v not found", sessionID)
 	}
