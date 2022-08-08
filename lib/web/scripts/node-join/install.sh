6f199595e8cd3f8b7679cb2288088101881c0416
Marco André Dinis <marco.dinis@goteleport.com>
2022-08-08 23:59:24 +0100

    [v10] Add Suggested Labels to Provision Tokens (#15114) (#15319)
    Add Suggested Labels to Provision Tokens (#15114)
    
    For Teleport Discover, the user will be able to test connecting to a
    resource right after adding it.
    
    The flow should look like this:
    - User selects the resource type - in this case Server/Nodes
    - WebUI generates a new Token
    - WebUI shows the `sudo bash .../<token>/install.sh` to the user
    - When the user runs this command, and assuming everything works out,
      the WebUI should be able to receive the Server/Node that was
      generated from that specific Token.
    
    To achieve this, here's a more detailed flow, which this PR implements
    - User selects the resource type - in this case Server/Nodes
    - WebUI generates a new Token
      This generates a Provision Token which contains a RefResourceID
      WebUI receives back that ID
    - WebUI shows the `sudo bash .../<token>/install.sh` to the user
      This generates a script which includes setting the labels as part of
      the `teleport configure` command:
      `$ teleport configure ... --labels teleport.internal/resource-id=<refResourceID> ...`
    - User runs the provided command on the target host
    - WebUI should be able to query the Servers/Nodes which contain that
      specific `<resource-id>` and allow the user to connect to it.
    
    Demo:
    ![image](https://user-images.githubusercontent.com/689271/182440692-97c75ae0-1e14-4f76-b9ff-d41060c73ed0.png)

--- lib/web/scripts/node-join/install.sh
+++ lib/web/scripts/node-join/install.sh
@@ -41,7 +41,13 @@ TARGET_PORT='{{.port}}'
 JOIN_TOKEN='{{.token}}'
 JOIN_METHOD='{{.joinMethod}}'
 JOIN_METHOD_FLAG=""
-[ ! -z "$JOIN_METHOD" ] && JOIN_METHOD_FLAG="--join-method ${JOIN_METHOD}"
+[ -n "$JOIN_METHOD" ] && JOIN_METHOD_FLAG="--join-method ${JOIN_METHOD}"
+
+# inject labels into the configuration
+LABELS='{{.labels}}'
+LABELS_FLAG=""
+[ -n "$LABELS" ] && LABELS_FLAG=(--labels "${LABELS}")
+
 # When all stanza generators have been updated to use the new
 # `teleport <service> configure` commands CA_PIN_HASHES can be removed along
 # with the script passing it in in `join_tokens.go`.
@@ -439,6 +445,7 @@ install_teleport_node_config() {
       ${JOIN_METHOD_FLAG} \
       --ca-pin ${CA_PINS} \
       --auth-server ${TARGET_HOSTNAME}:${TARGET_PORT} \
+      "${LABELS_FLAG[@]}" \
       --output ${TELEPORT_CONFIG_PATH}
 }
 # checks whether the given host is running MacOS
