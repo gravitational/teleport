12f3d74ba9b0fb3d8f15560080b6a9170af4d48c
Trent Clarke <trent@goteleport.com>
2022-08-09 16:49:21 +1000

    Find tsh test

--- dronegen/windows.go
+++ dronegen/windows.go
@@ -205,7 +205,8 @@ func buildWindowsTshStep(workspace string) step {
 			`Enable-Go -ToolchainDir "` + windowsToolchainDir + `"`,
 			`cd $TeleportSrc`,
 			`$Env:GCO_ENABLED=1`,
-			`go build -v -o build/tsh ./tool/tsh`,
+			`go build -o build/tsh.exe ./tool/tsh`,
+			`tree build`,
 		},
 	}
 }
