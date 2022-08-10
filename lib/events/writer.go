26f58384be0f99a0e11444c38854e8ef8f05ba69
Vitor Enes <vitor.duarte@goteleport.com>
2022-08-10 15:47:36 +0100

    Implement `StreamEvents` for `WriterLog` and `MultiLog`

--- lib/events/writer.go
+++ lib/events/writer.go
@@ -94,6 +94,13 @@ func (w *WriterLog) SearchSessionEvents(fromUTC, toUTC time.Time, limit int, ord
 	return nil, "", trace.NotImplemented("not implemented")
 }
 
+// StreamSessionEvents TODO
+func (w *WriterLog) StreamEvents(ctx context.Context, startKey string) (chan apievents.StreamEvents, chan error) {
+	c, e := make(chan apievents.StreamEvents), make(chan error, 1)
+	e <- trace.NotImplemented("not implemented")
+	return c, e
+}
+
 // StreamSessionEvents streams all events from a given session recording. An error is returned on the first
 // channel if one is encountered. Otherwise the event channel is closed when the stream ends.
 // The event channel is not closed on error to prevent race conditions in downstream select statements.
