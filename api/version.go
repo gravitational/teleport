0982050af5fe5ef167f029fc4245732ad1196589
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2022-08-12 12:24:09 +0200

    Fix

--- api/version.go
+++ api/version.go
@@ -3,7 +3,7 @@
 package api
 
 const (
-	Version = "11.0.0-gzdunek.12"
+	Version = "11.0.0-gzdunek.13"
 )
 
 // Gitref variable is automatically set to the output of git-describe
--- version.go
+++ version.go
@@ -3,7 +3,7 @@
 package teleport
 
 const (
-	Version = "11.0.0-gzdunek.12"
+	Version = "11.0.0-gzdunek.13"
 )
 
 // Gitref variable is automatically set to the output of git-describe
