7c904714e9a88b215f111095b9d56efb4ef025e9
Noah Stride <noah.stride@goteleport.com>
2022-08-12 13:06:38 +0100

    Machine ID support for Logins trait (#15117)
    * Support user traits in impersonated role certificates
    
    * Add param to tctl bots add for specifying logins
    
    * Update comment on desiredAccessInfoForRoleRequest to be mroe accurate
    
    * Update docs for --logins support with Machine ID
    
    * Rearrange bot tests and name them more in-line with standards
    
    * Add test coverage for Server.createBot
    
    * Test role impersonation preserves user traits
    
    * pass traits directly into desiredAccessInfoForRoleRequests
    
    * Drop underscores from test names
    
    * Remove unnecessary initialization of traits
    
    * Address PR comments
    
    * Update docs/pages/machine-id/getting-started.mdx
    
    Co-authored-by: Paul Gottschling <paul.gottschling@goteleport.com>
    
    * Update docs/pages/machine-id/getting-started.mdx
    
    Co-authored-by: Paul Gottschling <paul.gottschling@goteleport.com>
    
    * Update docs/pages/machine-id/getting-started.mdx
    
    Co-authored-by: Paul Gottschling <paul.gottschling@goteleport.com>
    
    Co-authored-by: Paul Gottschling <paul.gottschling@goteleport.com>

--- lib/auth/auth_with_roles.go
+++ lib/auth/auth_with_roles.go
@@ -30,6 +30,7 @@ import (
 	apidefaults "github.com/gravitational/teleport/api/defaults"
 	"github.com/gravitational/teleport/api/types"
 	apievents "github.com/gravitational/teleport/api/types/events"
+	"github.com/gravitational/teleport/api/types/wrappers"
 	apiutils "github.com/gravitational/teleport/api/utils"
 	"github.com/gravitational/teleport/lib/backend"
 	"github.com/gravitational/teleport/lib/defaults"
@@ -1385,7 +1386,6 @@ func (k *kubeChecker) CanAccess(resource types.Resource) error {
 	default:
 		return trace.BadParameter("unexpected resource type %T", resource)
 	}
-
 }
 
 // canAccessKubernetesLegacy checks if the user has access to Kubernetes Cluster when represented by `types.ServerV2`.
@@ -2245,7 +2245,7 @@ func (a *ServerWithRoles) desiredAccessInfo(ctx context.Context, req *proto.User
 			log.WithError(err).Warn()
 			return nil, err
 		}
-		return a.desiredAccessInfoForRoleRequest(req)
+		return a.desiredAccessInfoForRoleRequest(req, user.GetTraits())
 	}
 	return a.desiredAccessInfoForUser(ctx, req, user)
 }
@@ -2260,7 +2260,7 @@ func (a *ServerWithRoles) desiredAccessInfoForImpersonation(req *proto.UserCerts
 }
 
 // desiredAccessInfoForRoleRequest returns the desired roles for a role request.
-func (a *ServerWithRoles) desiredAccessInfoForRoleRequest(req *proto.UserCertsRequest) (*services.AccessInfo, error) {
+func (a *ServerWithRoles) desiredAccessInfoForRoleRequest(req *proto.UserCertsRequest, traits wrappers.Traits) (*services.AccessInfo, error) {
 	// If UseRoleRequests is set, make sure we don't return unusable certs: an
 	// identity without roles can't be parsed.
 	// DEPRECATED: consider making role requests without UseRoleRequests set an
@@ -2274,9 +2274,12 @@ func (a *ServerWithRoles) desiredAccessInfoForRoleRequest(req *proto.UserCertsRe
 	// are intended to reduce allowed permissions so we'll accept them
 	// as-is for now (and ensure the user is allowed to assume them
 	// later).
-	// Note: traits are not currently set for role impersonation.
+	//
+	// Traits are copied across from the impersonating user so that role
+	// variables within the impersonated role behave as expected.
 	return &services.AccessInfo{
-		Roles: req.RoleRequests,
+		Roles:  req.RoleRequests,
+		Traits: traits,
 	}, nil
 }
 
