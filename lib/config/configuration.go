ca4974b97d47570ee81b1cff2b47e14dd73ab8b1
Nic Klaassen <nic@goteleport.com>
2022-08-12 16:51:16 -0700

    add test

--- lib/config/configuration.go
+++ lib/config/configuration.go
@@ -2126,8 +2126,6 @@ func applyTokenConfig(fc *FileConfig, cfg *service.Config) error {
 	if fc.AuthToken != "" {
 		cfg.JoinMethod = types.JoinMethodToken
 		cfg.SetToken(fc.AuthToken)
-
-		return nil
 	}
 
 	if fc.JoinParams != (JoinParams{}) {
@@ -2144,5 +2142,6 @@ func applyTokenConfig(fc *FileConfig, cfg *service.Config) error {
 			return trace.BadParameter(`unknown value for join_params.method: %q, expected one of %v`, fc.JoinParams.Method, []types.JoinMethod{types.JoinMethodEC2, types.JoinMethodIAM, types.JoinMethodToken})
 		}
 	}
+
 	return nil
 }
