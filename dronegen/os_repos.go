151a2f489e3116fc7ce8f55e056529361d3233a6
Fred Heinecke <fred.heinecke@goteleport.com>
2022-08-15 14:26:14 -0500

    Added migrations for v10.1.4, v10.1.2, and v9.3.14

--- dronegen/os_repos.go
+++ dronegen/os_repos.go
@@ -83,13 +83,16 @@ func artifactMigrationPipeline() []pipeline {
 		// "v9.3.10",
 		// "v9.3.12",
 		// "v9.3.13",
+		"v9.3.14",
 		// "v10.0.0",
 		// "v10.0.1",
 		// "v10.0.2",
+		"v10.1.2",
+		"v10.1.4",
 	}
 	// Pushing to this branch will trigger the listed versions to be migrated. Typically this should be
 	// the branch that these changes are being committed to.
-	migrationBranch := "" // "rfd/0058-package-distribution"
+	migrationBranch := "fred/os-package-fix-1" // "rfd/0058-package-distribution"
 
 	aptPipeline := migrateAptPipeline(migrationBranch, migrationVersions)
 	yumPipeline := migrateYumPipeline(migrationBranch, migrationVersions)
