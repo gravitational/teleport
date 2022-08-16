07a9b9489760f2b1bbe025b3ab6379b82d9f0dc5
Gavin Frazar <gavin.frazar@goteleport.com>
2022-08-15 18:26:35 -0700

    Update azure test

--- lib/cloud/azure/azure_test.go
+++ lib/cloud/azure/azure_test.go
@@ -2,55 +2,74 @@ package azure
 
 import (
 	"fmt"
+	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/mysql/armmysql"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/postgresql/armpostgresql"
-	"github.com/gravitational/teleport/lib/defaults"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 func TestServerConversion(t *testing.T) {
 	tests := []struct {
-		name    string
-		dbType  string
-		version string
-		state   string
+		name          string
+		provider      string
+		version       string
+		state         string
+		wantAvailable bool
+		wantSupported bool
 	}{
 		{
-			name:    "mysql conversion",
-			dbType:  "mysql",
-			version: "5.7",
-			state:   "Ready",
+			name:          "mysql available and supported",
+			provider:      MySQLNamespace,
+			version:       "5.7",
+			state:         "Ready",
+			wantAvailable: true,
+			wantSupported: true,
 		},
 		{
-			name:    "postgres conversion",
-			dbType:  "postgres",
-			version: "11",
-			state:   "Ready",
+			name:          "mysql unavailable and unsupported",
+			provider:      MySQLNamespace,
+			version:       "5.6",
+			state:         "",
+			wantAvailable: false,
+			wantSupported: false,
+		},
+		{
+			name:          "postgres available and supported",
+			provider:      PostgreSQLNamespace,
+			version:       "11",
+			state:         "Ready",
+			wantAvailable: true,
+			wantSupported: true,
+		},
+		{
+			name:          "postgres unavailable and unsupported",
+			provider:      PostgreSQLNamespace,
+			version:       "unknown",
+			state:         "Disabled",
+			wantAvailable: false,
+			wantSupported: false,
 		},
 	}
+
+	region := "eastus"
+	dbName := "dbname"
+	tags := map[string]string{"foo": "bar", "baz": "qux"}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			var (
-				server     Server
+				server     *DBServer
 				err        error
-				provider   string
-				port       string
 				fqdn       string
 				serverType string
 			)
-			region := "eastus"
-			dbName := "dbname"
-			typeFmt := "%v/servers"
-			idFmt := "/subscriptions/subid/resourceGroups/group/providers/%v/dbname"
-			tags := map[string]string{"foo": "bar", "baz": "qux"}
-			switch tt.dbType {
-			case defaults.ProtocolMySQL:
-				provider = "Microsoft.DBforMySQL"
-				port = "3306"
-				serverType = fmt.Sprintf(typeFmt, provider)
-				id := fmt.Sprintf(idFmt, serverType)
-				fqdn = fmt.Sprintf("dbname.%v.database.azure.com", tt.dbType)
+			switch tt.provider {
+			case MySQLNamespace:
+				serverType = fmt.Sprintf("%v/servers", tt.provider)
+				id := fmt.Sprintf("/subscriptions/subid/resourceGroups/group/providers/%v/dbname", serverType)
+				fqdn = fmt.Sprintf("%v.mysql.database.azure.com", dbName)
 				server, err = ServerFromMySQLServer(
 					&armmysql.Server{
 						Location: &region,
@@ -64,12 +83,10 @@ func TestServerConversion(t *testing.T) {
 						Name: &dbName,
 						Type: &serverType,
 					})
-			case defaults.ProtocolPostgres:
-				provider = "Microsoft.DBforPostgreSQL"
-				port = "5432"
-				serverType = fmt.Sprintf(typeFmt, provider)
-				id := fmt.Sprintf(idFmt, serverType)
-				fqdn = fmt.Sprintf("dbname.%v.database.azure.com", tt.dbType)
+			case PostgreSQLNamespace:
+				serverType = fmt.Sprintf("%v/servers", tt.provider)
+				id := fmt.Sprintf("/subscriptions/subid/resourceGroups/group/providers/%v/dbname", serverType)
+				fqdn = fmt.Sprintf("%v.postgres.database.azure.com", dbName)
 				server, err = ServerFromPostgresServer(
 					&armpostgresql.Server{
 						Location: &region,
@@ -84,27 +101,32 @@ func TestServerConversion(t *testing.T) {
 						Type: &serverType,
 					})
 			default:
-				require.FailNow(t, "unknown db protocol specified by test")
+				require.FailNow(t, "unknown db namespace specified by test")
 			}
 			require.NoError(t, err)
 
-			require.Equal(t, tt.dbType, server.Protocol())
-			require.Equal(t, tt.state, server.State())
-			require.Equal(t, tt.version, server.Version())
-			require.Equal(t, provider, server.ID().ProviderNamespace)
-			require.Equal(t, "group", server.ID().ResourceGroup)
-			require.Equal(t, "subid", server.ID().SubscriptionID)
-			require.Equal(t, "servers", server.ID().ResourceType)
-			require.Equal(t, dbName, server.ID().ResourceName)
-			require.Equal(t, dbName, server.Name())
-			require.Equal(t, fqdn+":"+port, server.Endpoint())
-			require.Equal(t, region, server.Region())
-			require.Equal(t, tags, server.Tags())
+			rid, err := arm.ParseResourceID(server.ID)
+			require.NoError(t, err)
+
+			require.Equal(t, dbName, server.Name)
+			require.Equal(t, dbName, rid.Name)
+			require.Equal(t, tt.provider+"/servers", server.Type)
+			require.Equal(t, region, server.Location)
+			require.Equal(t, tags, server.Tags)
+			require.Equal(t, fqdn, server.Properties.FullyQualifiedDomainName)
+			require.Equal(t, tt.state, server.Properties.UserVisibleState)
+			require.Equal(t, tt.version, server.Properties.Version)
+			require.Equal(t, "group", rid.ResourceGroupName)
+			require.Equal(t, "subid", rid.SubscriptionID)
+			require.Equal(t, tt.provider, rid.ResourceType.Namespace)
+			require.Equal(t, "servers", rid.ResourceType.Type)
+			require.Equal(t, tt.wantAvailable, server.IsAvailable())
+			require.Equal(t, tt.wantSupported, server.IsVersionSupported())
 		})
 	}
 }
 
-// makeAzureTags is a test helper util function
+// makeAzureTags is a helper function for making azure db server tags
 func makeAzureTags(m map[string]string) map[string]*string {
 	result := make(map[string]*string, len(m))
 	for k, v := range m {
