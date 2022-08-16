89d291de64749790c4b9305894bf76d246af5d65
Gavin Frazar <gavin.frazar@goteleport.com>
2022-08-15 18:19:28 -0700

    Update azure constants

--- lib/cloud/azure/constants.go
+++ lib/cloud/azure/constants.go
@@ -1,9 +1,28 @@
-// TODO(gavin)
+/*
+Copyright 2022 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package azure
 
 const (
-	// Azure managed mysql server port is always 3306
+	// MySQLPort is the Azure managed MySQL server port
 	MySQLPort = "3306"
-	// Azure managed mysql server port is always 5432
+	// PostgresPort is the Azure managed PostgreSQL server port
 	PostgresPort = "5432"
+	// MySQLNamespace is the namespace for Azure managed MySQL DBs
+	MySQLNamespace = "Microsoft.DBforMySQL"
+	// PostgreSQLNamespace is the namespace for Azure managed PostgreSQL DBs
+	PostgreSQLNamespace = "Microsoft.DBforPostgreSQL"
 )
