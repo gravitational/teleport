c88b5e42455ab8dfe7006544c2f44c4420e2957a
Vitor Enes <vitor.duarte@goteleport.com>
2022-08-18 15:51:32 +0100

    Allow teleport to run without services

--- lib/service/service.go
+++ lib/service/service.go
@@ -4557,8 +4557,8 @@ func (process *TeleportProcess) Close() error {
 
 func validateConfig(cfg *Config) error {
 	if !cfg.Auth.Enabled && !cfg.SSH.Enabled && !cfg.Proxy.Enabled && !cfg.Kube.Enabled && !cfg.Apps.Enabled && !cfg.Databases.Enabled && !cfg.WindowsDesktop.Enabled {
-		return trace.BadParameter(
-			"config: enable at least one of auth_service, ssh_service, proxy_service, app_service, database_service, kubernetes_service or windows_desktop_service")
+		cfg.Log.Warn(
+			"config: all services (auth_service, ssh_service, proxy_service, app_service, database_service, kubernetes_service or windows_desktop_service) are disabled")
 	}
 
 	if cfg.DataDir == "" {
