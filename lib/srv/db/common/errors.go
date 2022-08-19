f6fe1bc036d3f57d1a9526638aec37288b36f00c
Gavin Frazar <gavin.frazar@goteleport.com>
2022-08-18 18:55:29 -0700

    Add azure response error conversion

--- lib/srv/db/common/errors.go
+++ lib/srv/db/common/errors.go
@@ -21,6 +21,8 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/go-mysql-org/go-mysql/mysql"
 	"github.com/gravitational/trace"
@@ -32,6 +34,7 @@ import (
 	"google.golang.org/grpc/status"
 
 	awslib "github.com/gravitational/teleport/lib/cloud/aws"
+	azurelib "github.com/gravitational/teleport/lib/cloud/azure"
 	"github.com/gravitational/teleport/lib/defaults"
 )
 
@@ -58,6 +61,8 @@ func ConvertError(err error) error {
 		return convertGCPError(e)
 	case awserr.RequestFailure:
 		return awslib.ConvertRequestFailureError(e)
+	case *azcore.ResponseError:
+		return azurelib.ConvertResponseError(e)
 	case *pgconn.PgError:
 		return convertPostgresError(e)
 	case *mysql.MyError:
