1682fc64d09457f7bdecbbba809a2da5aece1260
Isaiah Becker-Mayer <isaiah@goteleport.com>
2022-08-24 16:28:25 -0400

    removes the directory_sharing build flag and associated components

--- lib/srv/desktop/windows_server.go
+++ lib/srv/desktop/windows_server.go
@@ -839,13 +839,12 @@ func (s *WindowsService) connectRDP(ctx context.Context, log logrus.FieldLogger,
 		GenerateUserCert: func(ctx context.Context, username string, ttl time.Duration) (certDER, keyDER []byte, err error) {
 			return s.generateCredentials(ctx, username, desktop.GetDomain(), ttl)
 		},
-		CertTTL:        windowsDesktopCertTTL,
-		Addr:           desktop.GetAddr(),
-		Conn:           tdpConn,
-		AuthorizeFn:    authorize,
-		AllowClipboard: authCtx.Checker.DesktopClipboard(),
-		// AllowDirectorySharing() ensures this setting is modulated by build flag while in development
-		AllowDirectorySharing: authCtx.Checker.DesktopDirectorySharing() && AllowDirectorySharing(),
+		CertTTL:               windowsDesktopCertTTL,
+		Addr:                  desktop.GetAddr(),
+		Conn:                  tdpConn,
+		AuthorizeFn:           authorize,
+		AllowClipboard:        authCtx.Checker.DesktopClipboard(),
+		AllowDirectorySharing: authCtx.Checker.DesktopDirectorySharing(),
 	})
 	if err != nil {
 		s.onSessionStart(ctx, sw, &identity, sessionStartTime, windowsUser, string(sessionID), desktop, err)
