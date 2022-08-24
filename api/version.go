4ecdf2380bfa5742bd494cf0718c1fcd4c47d690
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2022-08-24 16:07:18 +0200

    Update to 11.0.0-gzdunek.36

--- api/version.go
+++ api/version.go
@@ -3,7 +3,7 @@
 package api
 
 const (
-	Version = "11.0.0-gzdunek.35"
+	Version = "11.0.0-gzdunek.36"
 )
 
 // Gitref variable is automatically set to the output of git-describe
--- version.go
+++ version.go
@@ -3,7 +3,7 @@
 package teleport
 
 const (
-	Version = "11.0.0-gzdunek.35"
+	Version = "11.0.0-gzdunek.36"
 )
 
 // Gitref variable is automatically set to the output of git-describe
