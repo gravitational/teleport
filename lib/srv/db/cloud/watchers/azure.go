3f11462efb8fb4644bb1afff3600fdf6e582d6f1
Gavin Frazar <gavinfrazar@gmail.com>
2022-08-24 16:18:28 -0700

    Update lib/srv/db/cloud/watchers/azure.go
    Co-authored-by: Marek Smoliński <marek@goteleport.com>

--- lib/srv/db/cloud/watchers/azure.go
+++ lib/srv/db/cloud/watchers/azure.go
@@ -115,9 +115,8 @@ func (f *azureFetcher) Get(ctx context.Context) (types.Databases, error) {
 func (f *azureFetcher) getDBServers(ctx context.Context) ([]*azure.DBServer, error) {
 	if f.cfg.ResourceGroup == types.Wildcard {
 		return f.cfg.Client.ListAll(ctx, common.MaxPages)
-	} else {
-		return f.cfg.Client.ListWithinGroup(ctx, f.cfg.ResourceGroup, common.MaxPages)
 	}
+	return f.cfg.Client.ListWithinGroup(ctx, f.cfg.ResourceGroup, common.MaxPages)
 }
 
 // getDatabases returns a list of database resources representing Azure database servers.
