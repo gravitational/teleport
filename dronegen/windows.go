0f7123b25239d299e2659365c7a3d13d429bfea7
Trent Clarke <trent@goteleport.com>
2022-08-25 12:30:42 +1000

    Fine, stay unsigned. See if I care.

--- dronegen/windows.go
+++ dronegen/windows.go
@@ -208,9 +208,9 @@ func buildWindowsTshStep(workspace string) step {
 			`cd $TeleportSrc`,
 			`$Env:GCO_ENABLED=1`,
 			`go build -o build/tsh.exe ./tool/tsh`,
-			`Convert-Base64 -Data $Env:WINDOWS_SIGNING_CERT -FilePath "windows-signing-cert.pfx"`,
-			`osslsigncode sign -pkcs12 windows-signing-cert.pfx -n "Teleport" -i https://goteleport.com -t http://timestamp.digicert.com -h sha2 -in build/tsh.exe -out build/tsh.signed.exe`,
-			`Rename-Item -Force -Path build/tsh.signed.exe -NewName build/tsh.exe`,
+			// `Convert-Base64 -Data $Env:WINDOWS_SIGNING_CERT -FilePath "windows-signing-cert.pfx"`,
+			// `osslsigncode sign -pkcs12 windows-signing-cert.pfx -n "Teleport" -i https://goteleport.com -t http://timestamp.digicert.com -h sha2 -in build/tsh.exe -out build/tsh.signed.exe`,
+			// `Rename-Item -Force -Path build/tsh.signed.exe -NewName build/tsh.exe`,
 		},
 	}
 }
