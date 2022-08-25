0e07d86734b038c58524cec843f6157cacd6ae00
Alan Parra <alan.parra@goteleport.com>
2022-08-25 14:59:34 -0300

    Generate protos using Buf

--- build.assets/genproto.sh
+++ build.assets/genproto.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 #
 # Builds, formats, lints and generates protos for teleport and teleport/api.
-#
-# Env variables:
-# - PROTO_INCLUDE: `protoc -I` extension with path to imported protos
-#   (eg, Gogo import path).
 set -eu
 
 echoed() {
@@ -20,22 +16,14 @@ main() {
   buf lint
   buf format -w
 
-  # Build legacy protos.
-  # Output path is the same as their old path, inside Go packages.
-  for p in client/proto types/{events,webauthn,wrappers,}; do
-    echoed protoc -I="./api/proto/:$PROTO_INCLUDE" \
-      --gogofast_out='plugins=grpc:.' \
-      "api/proto/teleport/legacy/$p"/*.proto
-  done
-
   # Generated protos are written to
   # <teleport-root>/github.com/gravitational/teleport/..., so we copy them to
   # the correct relative path.
   # TODO(codingllama): Find a way to generate in the correct path.
+  buf generate
   for f in $(find github.com -name '*.pb.go'); do
     mv "$f" "${f#github.com/gravitational/teleport/}"
   done
-
   rm -fr github.com/  # Remove empty generated root.
 }
 
