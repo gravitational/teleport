430e34253b0b755d6f1cc192c0c408368e7c34b4
Nic Klaassen <nic@goteleport.com>
2022-08-29 12:41:13 -0700

    add missing aws helpers

--- lib/utils/ec2.go
+++ lib/utils/ec2.go
@@ -19,10 +19,7 @@ package utils
 import (
 	"context"
 	"fmt"
-	"fmt"
-	"io""
-	"strings"
-	"time
+	"io"
 	"regexp"
 	"strings"
 	"time"
@@ -30,7 +27,12 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/feature/ec2/imds"
 	"github.com/gravitational/trace"
+
+	"github.com/gravitational/teleport/lib/cloud/aws"
 )
+
+// metadataReadLimit is the largest number of bytes that will be read from imds responses.
+const metadataReadLimit = 1_000_000
 
 // GetEC2IdentityDocument fetches the PKCS7 RSA2048 InstanceIdentityDocument
 // from the IMDS for this EC2 instance.
@@ -80,7 +82,11 @@ func GetEC2NodeID() (string, error) {
 //   https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/resource-ids.html
 var ec2NodeIDRE = regexp.MustCompile("^[0-9]{12}-i-[0-9a-f]{8,}$")
 
-// IsEC2NodeID returns true if the given ID looks like an EC2 node ID
+// IsEC2NodeID returns true if the given ID looks like an EC2 node ID. Uses a
+// simple regex to check. Node IDs are almost always UUIDs when set
+// automatically, but can be manually overridden by admins. If someone manually
+// sets a host ID that looks like one of our generated EC2 node IDs, they may be
+// able to trick this function, so don't use it for any critical purpose.
 func IsEC2NodeID(id string) bool {
 	return ec2NodeIDRE.MatchString(id)
 }
