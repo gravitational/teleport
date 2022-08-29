11fa9f6b614ef95673baeee15b520dfe044dd78a
Trent Clarke <trent@goteleport.com>
2022-08-29 16:00:02 +1000

    Remove scaffolding

--- dronegen/windows.go
+++ dronegen/windows.go
@@ -34,15 +34,8 @@ func newWindowsPipeline(name string) pipeline {
 func windowsTagPipeline() pipeline {
 	p := newWindowsPipeline("build-native-windows-amd64")
 
-	//p.Trigger = triggerTag
-	p.Trigger = trigger{
-		Event:  triggerRef{Include: []string{"push"}, Exclude: []string{"pull_request"}},
-		Branch: triggerRef{Include: []string{"master", "branch/*", "tcsc/build-windows*"}},
-		Repo:   triggerRef{Include: []string{"gravitational/*"}},
-	}
-
-	// TODO(tcsc): restore before merge
-	//p.DependsOn = []string{ "build-windows-amd64" }
+	p.Trigger = triggerTag
+	p.DependsOn = []string{"build-windows-amd64"}
 
 	p.Steps = []step{
 		cloneWindowsRepositoriesStep(p.Workspace.Path),
@@ -54,22 +47,16 @@ func windowsTagPipeline() pipeline {
 		{
 			Name: "Upload Artifacts",
 			Environment: map[string]value{
-				"WORKSPACE_DIR": {raw: p.Workspace.Path},
-				// TODO(tcsc): aid during dev, revert before merge
-				"AWS_REGION":            {raw: "ap-southeast-2"},
-				"AWS_S3_BUCKET":         {raw: "trents-mock-dronestorage"},
-				"AWS_ACCESS_KEY_ID":     {fromSecret: "MOCK_AWS_ACCESS_KEY_ID"},
-				"AWS_SECRET_ACCESS_KEY": {fromSecret: "MOCK_AWS_SECRET_ACCESS_KEY"},
-				// "AWS_REGION":            {raw: "us-west-2"},
-				// "AWS_S3_BUCKET":         {fromSecret: "AWS_S3_BUCKET"},
-				// "AWS_ACCESS_KEY_ID":     {fromSecret: "AWS_ACCESS_KEY_ID"},
-				// "AWS_SECRET_ACCESS_KEY": {fromSecret: "AWS_SECRET_ACCESS_KEY"},
+				"WORKSPACE_DIR":         {raw: p.Workspace.Path},
+				"AWS_REGION":            {raw: "us-west-2"},
+				"AWS_S3_BUCKET":         {fromSecret: "AWS_S3_BUCKET"},
+				"AWS_ACCESS_KEY_ID":     {fromSecret: "AWS_ACCESS_KEY_ID"},
+				"AWS_SECRET_ACCESS_KEY": {fromSecret: "AWS_SECRET_ACCESS_KEY"},
 			},
 			Commands: []string{
 				`$Workspace = "` + perBuildWorkspace + `"`,
 				`$TeleportSrc = "$Workspace` + teleportSrc + `"`,
 				`$WebappsSrc = "$Workspace` + webappsSrc + `"`,
-				`$Env:DRONE_TAG="v10.1.2"`, // TODO(tcsc): aid during dev, remove before merge
 				`$TeleportVersion=$Env:DRONE_TAG.TrimStart('v')`,
 				`$OutputsDir="$Workspace/outputs"`,
 				`New-Item -Path "$OutputsDir" -ItemType 'Directory' | Out-Null`,
@@ -89,7 +76,7 @@ func windowsPushPipeline() pipeline {
 	p := newWindowsPipeline("push-build-native-windows-amd64")
 	p.Trigger = trigger{
 		Event:  triggerRef{Include: []string{"push"}, Exclude: []string{"pull_request"}},
-		Branch: triggerRef{Include: []string{"master", "branch/*", "tcsc/build-windows-tcon"}},
+		Branch: triggerRef{Include: []string{"master", "branch/*"}},
 		Repo:   triggerRef{Include: []string{"gravitational/*"}},
 	}
 
@@ -204,9 +191,6 @@ func buildWindowsTshStep(workspace string) step {
 			`cd $TeleportSrc`,
 			`$Env:GCO_ENABLED=1`,
 			`go build -o build/tsh.exe ./tool/tsh`,
-			// `Convert-Base64 -Data $Env:WINDOWS_SIGNING_CERT -FilePath "windows-signing-cert.pfx"`,
-			// `osslsigncode sign -pkcs12 windows-signing-cert.pfx -n "Teleport" -i https://goteleport.com -t http://timestamp.digicert.com -h sha2 -in build/tsh.exe -out build/tsh.signed.exe`,
-			// `Rename-Item -Force -Path build/tsh.signed.exe -NewName build/tsh.exe`,
 		},
 	}
 }
