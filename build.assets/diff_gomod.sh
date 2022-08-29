e1396540e1ec7dce2df58a379d1a99f3a2ddd064
Alan Parra <alan.parra@goteleport.com>
2022-08-29 14:28:13 -0300

    Print status and diff on failures

--- build.assets/diff_gomod.sh
+++ build.assets/diff_gomod.sh
@@ -8,6 +8,10 @@ main() {
     pushd "$d" >/dev/null
     go mod tidy
     if [[ -n "$(git status --porcelain)" ]]; then
+      # Print status and diff to allow easier debugging.
+      git status --porcelain >&2
+      git diff "$f" >&2
+
       echo -e "Found untidy Go Module at $f." \
         "Please run the following command in your workspace try again:"\
         "\n\tcd $d && go mod tidy" >&2
