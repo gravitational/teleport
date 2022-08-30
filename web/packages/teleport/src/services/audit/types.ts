68bb1346912a0a26738eb40d1df348697c7f8969
Ryan Clark <ryan@ryanclark.me>
2022-08-30 21:21:09 +0100

    Add an event for when a session recording is accessed (#1133)

--- web/packages/teleport/src/services/audit/types.ts
+++ web/packages/teleport/src/services/audit/types.ts
@@ -127,6 +127,7 @@ export const eventCodes = {
   SESSION_START: 'T2000I',
   SESSION_UPLOAD: 'T2005I',
   SESSION_CONNECT: 'T2010I',
+  SESSION_RECORDING_ACCESS: 'T2012I',
   SUBSYSTEM_FAILURE: 'T3001E',
   SUBSYSTEM: 'T3001I',
   TERMINAL_RESIZE: 'T2002I',
@@ -751,6 +752,13 @@ export type RawEvents = {
     typeof eventCodes.UPGRADE_WINDOW_UPDATED,
     {
       upgrade_window_start: string;
+    }
+  >;
+  [eventCodes.SESSION_RECORDING_ACCESS]: RawEvent<
+    typeof eventCodes.SESSION_RECORDING_ACCESS,
+    {
+      sid: string;
+      user: string;
     }
   >;
 };
