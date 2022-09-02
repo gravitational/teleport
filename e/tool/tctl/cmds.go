633609ee13a221902ce1da7cd6859feff01c6f73
Forrest Marshall <forrest@goteleport.com>
2022-09-02 03:56:56 +0000

    rework cmd registration
    Source-commit: gravitational/teleport.e@3724d90deda03309b29707791ca28dea5c332e3d

---
+++ e/tool/tctl/cmds.go
@@ -0,0 +1,33 @@
+/*
+Copyright 2022 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package main
+
+import (
+	"github.com/gravitational/teleport/e/tool/tctl/sso/configure"
+	"github.com/gravitational/teleport/e/tool/tctl/sso/tester"
+	"github.com/gravitational/teleport/tool/tctl/common"
+)
+
+// ENTCommands returns the ent variants of commands that use different variants
+// for oss and ent, and commands that are unique to ent.
+func ENTCommands() []common.CLICommand {
+	return []common.CLICommand{
+		&ResourceCommandE{},
+		&SAMLCommand{},
+		&configure.SSOConfigureCommandE{},
+		&tester.SSOTestCommandE{},
+	}
+}
--- e/tool/tctl/main.go
+++ e/tool/tctl/main.go
@@ -7,33 +7,15 @@ package main
 
 import (
 	"github.com/gravitational/teleport/e/tool/modules"
-	"github.com/gravitational/teleport/e/tool/tctl/sso/configure"
-	"github.com/gravitational/teleport/e/tool/tctl/sso/tester"
 	"github.com/gravitational/teleport/tool/tctl/common"
 )
 
 func main() {
 	modules.SetModules(nil)
 
-	commands := []common.CLICommand{
-		&common.UserCommand{},
-		&common.NodeCommand{},
-		&common.TokensCommand{},
-		&common.AuthCommand{},
-		&ResourceCommandE{},
-		&common.StatusCommand{},
-		&common.TopCommand{},
-		&common.AccessRequestCommand{},
-		&SAMLCommand{},
-		&configure.SSOConfigureCommandE{},
-		&tester.SSOTestCommandE{},
-		&common.AppsCommand{},
-		&common.DBCommand{},
-		&common.KubeCommand{},
-		&common.DesktopCommand{},
-		&common.LockCommand{},
-		&common.BotsCommand{},
-		&common.InventoryCommand{},
-	}
+	// aggregate common and ent-specific commands
+	commands := common.Commands()
+	commands = append(commands, ENTCommands()...)
+
 	common.Run(commands)
 }
