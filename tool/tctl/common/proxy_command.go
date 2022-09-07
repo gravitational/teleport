13f8ed0815c5d62a811be069c98e6323ccf8552f
Christopher Cooper <chris.cooper@goteleport.com>
2022-09-07 09:30:15 -0400

    review comments

--- tool/tctl/common/proxy_command.go
+++ tool/tctl/common/proxy_command.go
@@ -1,14 +1,29 @@
+// Copyright 2022 Gravitational, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package common
 
 import (
 	"context"
+	"io"
+	"os"
+
 	"github.com/gravitational/kingpin"
 	"github.com/gravitational/teleport"
 	"github.com/gravitational/teleport/lib/auth"
 	"github.com/gravitational/teleport/lib/service"
 	"github.com/gravitational/trace"
-	"io"
-	"os"
 )
 
 // ProxyCommand returns information about connected proxies
@@ -25,8 +40,8 @@ type ProxyCommand struct {
 func (p *ProxyCommand) Initialize(app *kingpin.Application, config *service.Config) {
 	p.config = config
 
-	auth := app.Command("proxy", "Operations with information for cluster proxies").Hidden()
-	p.lsCmd = auth.Command("ls", "List connected auth servers")
+	authCommand := app.Command("proxy", "Operations with information for cluster proxies").Hidden()
+	p.lsCmd = authCommand.Command("ls", "List connected auth servers")
 	p.lsCmd.Flag("format", "Output format: 'yaml', 'json' or 'text'").Default(teleport.YAML).StringVar(&p.format)
 
 	if p.stdout == nil {
@@ -60,6 +75,10 @@ func (p *ProxyCommand) TryRun(ctx context.Context, cmd string, client auth.Clien
 	switch cmd {
 	case p.lsCmd.FullCommand():
 		err = p.ListProxies(ctx, client)
+		if err != nil {
+			return false, err
+		}
+	default:
 		return false, nil
 	}
 	return true, trace.Wrap(err)
