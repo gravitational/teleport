f43e661cf122c7faee2b2d88f1319dcfc79a93b3
Anton Miniailo <anton@goteleport.com>
2022-09-13 10:58:03 -0400

    Refactor our `utils/fs` locking to use `flock` library and add support for windows
    Flock works differently on linx/mac vs windows. On windows it's impossible to
    read/write file after locking, so we need to create separate lockfile.

--- lib/utils/fs_unix.go
+++ lib/utils/fs_unix.go
@@ -19,48 +19,14 @@ limitations under the License.
 
 package utils
 
-import (
-	"os"
-	"syscall"
+// On non-windows we lock the target file itself.
+const lockPostfix = ""
 
-	"github.com/gravitational/trace"
-)
-
-// FSWriteLock grabs Flock-style filesystem lock on an open file
-// in exclusive mode.
-func FSWriteLock(f *os.File) error {
-	if err := syscall.Flock(int(f.Fd()), syscall.LOCK_EX); err != nil {
-		return trace.ConvertSystemError(err)
-	}
-	return nil
-}
-
-// FSTryWriteLock tries to grab write lock, returns CompareFailed
-// if lock is already acquired
-func FSTryWriteLock(f *os.File) error {
-	err := syscall.Flock(int(f.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
-	if err != nil {
-		if err == syscall.EWOULDBLOCK {
-			return trace.CompareFailed("lock %v is acquired by another process", f.Name())
+var unlockWrapper = func(unlockFn func() error, path string) func() error {
+	return func() error {
+		if unlockFn == nil {
+			return nil
 		}
-		return trace.ConvertSystemError(err)
+		return unlockFn()
 	}
-	return nil
-}
-
-// FSReadLock grabs Flock-style filesystem lock on an open file
-// in read (shared) mode
-func FSReadLock(f *os.File) error {
-	if err := syscall.Flock(int(f.Fd()), syscall.LOCK_SH); err != nil {
-		return trace.ConvertSystemError(err)
-	}
-	return nil
-}
-
-// FSUnlock unlcocks Flock-style filesystem lock
-func FSUnlock(f *os.File) error {
-	if err := syscall.Flock(int(f.Fd()), syscall.LOCK_UN); err != nil {
-		return trace.ConvertSystemError(err)
-	}
-	return nil
 }
