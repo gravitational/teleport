22591eb8e421c300b86bb5ff4775fcdb9bbae88b
Gavin Frazar <16846283+GavinFrazar@users.noreply.github.com>
2022-09-13 12:07:16 -0700

    Update lib/client/db/dbcmd/dbcmd_test.go
    Co-authored-by: Marek Smoliński <marek@goteleport.com>

--- lib/client/db/dbcmd/dbcmd_test.go
+++ lib/client/db/dbcmd/dbcmd_test.go
@@ -739,6 +739,7 @@ func TestGetConnectCommandNoAbsPathIsNoopWhenGivenRelativePath(t *testing.T) {
 }
 
 func TestConvertCommandError(t *testing.T) {
+        t.Parallel()
 	homePath := t.TempDir()
 	conf := &client.Config{
 		HomePath:     homePath,
