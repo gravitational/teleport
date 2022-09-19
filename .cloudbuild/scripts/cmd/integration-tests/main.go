gravitational/teleport

commit 9bb0255a073e3641324877b2c4bf524c65458c7e

Update toolchain to Go 1.19 (#16479)
Update Go toolchain, allowing for a possible go.mod bump to 1.19.

Since Go 1.19 BoringCrypto is no longer a separate branch, but instead it's
enabled by a [GOEXPERIMENT][1].

Release notes: https://tip.golang.org/doc/go1.19.

[1]: https://cs.opensource.google/go/go/+/refs/tags/go1.19.1:src/internal/goexperiment/exp_boringcrypto_on.go;l=3

* Update Go in build.assets/
* Update Go in Drone
* Appease .sh linter
* Update FIPS images
* Update e/
* Simplify Centos7 FIPS image

.drone.yml
@@ -39,7 +39,7 @@ name: push-build-linux-amd64
 environment:
   BUILDBOX_VERSION: teleport11
   GID: "1000"
-  RUNTIME: go1.18.6
+  RUNTIME: go1.19.1
   UID: "1000"
 trigger:
   event:
@@ -150,7 +150,7 @@ name: push-build-linux-386
 environment:
   BUILDBOX_VERSION: teleport11
   GID: "1000"
-  RUNTIME: go1.18.6
+  RUNTIME: go1.19.1
   UID: "1000"
 trigger:
   event:
@@ -253,7 +253,7 @@ name: push-build-linux-amd64-fips
 environment:
   BUILDBOX_VERSION: teleport11
   GID: "1000"
-  RUNTIME: go1.18.6
+  RUNTIME: go1.19.1
   UID: "1000"
 trigger:
   event:
@@ -360,7 +360,7 @@ name: push-build-windows-amd64
 environment:
   BUILDBOX_VERSION: teleport11
   GID: "1000"
-  RUNTIME: go1.18.6
+  RUNTIME: go1.19.1
   UID: "1000"
 trigger:
   event:
@@ -525,7 +525,7 @@ steps:
   - tar -C  /tmp/build-$DRONE_BUILD_NUMBER-$DRONE_BUILD_CREATED/toolchains -xzf $RUNTIME.darwin-amd64.tar.gz
   - rm -rf $RUNTIME.darwin-amd64.tar.gz
   environment:
-    RUNTIME: go1.18.6
+    RUNTIME: go1.19.1
 - name: Install Rust Toolchain
   commands:
   - set -u
@@ -960,7 +960,7 @@ name: push-build-linux-arm
 environment:
   BUILDBOX_VERSION: teleport11
   GID: "1000"
-  RUNTIME: go1.18.6
+  RUNTIME: go1.19.1
   UID: "1000"
 trigger:
   event:
@@ -1063,7 +1063,7 @@ name: push-build-linux-arm64
 environment:
   BUILDBOX_VERSION: teleport11
   GID: "1000"
-  RUNTIME: go1.18.6
+  RUNTIME: go1.19.1
   UID: "1000"
 trigger:
   event:
@@ -1694,7 +1694,7 @@ type: kubernetes
 name: build-linux-amd64-centos7
 environment:
   BUILDBOX_VERSION: teleport11
-  RUNTIME: go1.18.6
+  RUNTIME: go1.19.1
 trigger:
   event:
     include:
@@ -1854,7 +1854,7 @@ type: kubernetes
 name: build-linux-amd64-centos7-fips
 environment:
   BUILDBOX_VERSION: teleport11
-  RUNTIME: go1.18.6
+  RUNTIME: go1.19.1
 trigger:
   event:
     include:
@@ -2013,7 +2013,7 @@ type: kubernetes
 name: build-linux-amd64
 environment:
   BUILDBOX_VERSION: teleport11
-  RUNTIME: go1.18.6
+  RUNTIME: go1.19.1
 trigger:
   event:
     include:
@@ -2185,7 +2185,7 @@ type: kubernetes
 name: build-linux-amd64-fips
 environment:
   BUILDBOX_VERSION: teleport11
-  RUNTIME: go1.18.6
+  RUNTIME: go1.19.1
 trigger:
   event:
     include:
@@ -3078,7 +3078,7 @@ type: kubernetes
 name: build-linux-386
 environment:
   BUILDBOX_VERSION: teleport11
-  RUNTIME: go1.18.6
+  RUNTIME: go1.19.1
 trigger:
   event:
     include:
@@ -3661,7 +3661,7 @@ steps:
   - tar -C  /tmp/build-$DRONE_BUILD_NUMBER-$DRONE_BUILD_CREATED/toolchains -xzf $RUNTIME.darwin-amd64.tar.gz
   - rm -rf $RUNTIME.darwin-amd64.tar.gz
   environment:
-    RUNTIME: go1.18.6
+    RUNTIME: go1.19.1
 - name: Install Rust Toolchain
   commands:
   - set -u
@@ -4178,7 +4178,7 @@ type: kubernetes
 name: build-linux-arm
 environment:
   BUILDBOX_VERSION: teleport11
-  RUNTIME: go1.18.6
+  RUNTIME: go1.19.1
 trigger:
   event:
     include:
@@ -4335,7 +4335,7 @@ type: kubernetes
 name: build-linux-arm64
 environment:
   BUILDBOX_VERSION: teleport11
-  RUNTIME: go1.18.6
+  RUNTIME: go1.19.1
 trigger:
   event:
     include:
@@ -5228,7 +5228,7 @@ type: kubernetes
 name: build-windows-amd64
 environment:
   BUILDBOX_VERSION: teleport11
-  RUNTIME: go1.18.6
+  RUNTIME: go1.19.1
 trigger:
   event:
     include:
@@ -7054,6 +7054,6 @@ steps:
     WORKSPACE_DIR: /tmp/build-darwin-amd64-connect
 ---
 kind: signature
-hmac: 122d13c57fd07c1ea1baf781df66fe8f867338f089eb2a4636a7aef23a3dee30
+hmac: 6820f97d5d9c565d4a77e1f2c6a0b8091df655a1540a4133a5860f31f8414ff3
 
 ...

build.assets/Dockerfile-centos7-fips
@@ -34,9 +34,6 @@ ENV LANGUAGE=en_US.UTF-8 \
     LC_ALL=en_US.UTF-8 \
     LC_CTYPE=en_US.UTF-8
 
-ARG BORINGCRYPTO_RUNTIME
-ARG GO_BOOTSTRAP_RUNTIME=go1.9.7
-
 ARG UID
 ARG GID
 RUN (groupadd ci --gid=$GID -o && useradd ci --uid=$UID --gid=$GID --create-home --shell=/bin/sh && \
@@ -63,7 +60,7 @@ RUN yum groupinstall -y 'Development Tools' && \
     which \
     zip \
     # required by PIV integration
-    pcsc-lite-devel \  
+    pcsc-lite-devel \
     # required by libbpf
     zlib-static && \
     yum clean all
@@ -72,22 +69,15 @@ RUN yum groupinstall -y 'Development Tools' && \
 RUN (curl -L https://github.com/coreos/etcd/releases/download/v3.3.9/etcd-v3.3.9-linux-amd64.tar.gz | tar -xz && \
      cp etcd-v3.3.9-linux-amd64/etcd* /bin/)
 
-# BoringCrypto (unlike regular Go) requires glibc 2.14, so we have to build from source.
-# 1) Install older binary Go runtime for bootstrapping
-# 2) Get source for the correct Go boringcrypto runtime and compile it with Go bootstrap runtime
-# 3) Erase Go bootstrap runtime and create build directories
-# 4) Print compiled Go version
-RUN mkdir -p /go-bootstrap && cd /go-bootstrap && curl https://dl.google.com/go/${GO_BOOTSTRAP_RUNTIME}.linux-amd64.tar.gz | tar xz && \
-    mkdir -p /opt && cd /opt && curl https://go-boringcrypto.storage.googleapis.com/${BORINGCRYPTO_RUNTIME}.src.tar.gz | tar xz && \
-    cd /opt/go/src && GOROOT_BOOTSTRAP=/go-bootstrap/go ./make.bash && \
-    rm -rf /go-bootstrap && \
+# Install Go.
+ARG GOLANG_VERSION
+RUN mkdir -p /opt && cd /opt && curl https://storage.googleapis.com/golang/$GOLANG_VERSION.linux-amd64.tar.gz | tar xz && \
     mkdir -p /go/src/github.com/gravitational/teleport && \
     chmod a+w /go && \
     chmod a+w /var/lib && \
-    chmod a-w / && \
-    /opt/go/bin/go version
-
-ENV GOPATH="/go" \
+    chmod a-w /
+ENV GOEXPERIMENT=boringcrypto \
+    GOPATH="/go" \
     GOROOT="/opt/go" \
     PATH="/opt/llvm/bin:$PATH:/opt/go/bin:/go/bin:/go/src/github.com/gravitational/teleport/build"
 

build.assets/Dockerfile-fips
@@ -57,13 +57,14 @@ RUN (curl -L https://github.com/coreos/etcd/releases/download/v3.3.9/etcd-v3.3.9
      cp etcd-v3.3.9-linux-amd64/etcd* /bin/)
 
 # Install Go.
-ARG BORINGCRYPTO_RUNTIME
-RUN mkdir -p /opt && cd /opt && curl https://go-boringcrypto.storage.googleapis.com/${BORINGCRYPTO_RUNTIME}.linux-amd64.tar.gz | tar xz && \
+ARG GOLANG_VERSION
+RUN mkdir -p /opt && cd /opt && curl https://storage.googleapis.com/golang/$GOLANG_VERSION.linux-amd64.tar.gz | tar xz && \
     mkdir -p /go/src/github.com/gravitational/teleport && \
     chmod a+w /go && \
     chmod a+w /var/lib && \
     chmod a-w /
-ENV GOPATH="/go" \
+ENV GOEXPERIMENT=boringcrypto \
+    GOPATH="/go" \
     GOROOT="/opt/go" \
     PATH="$PATH:/opt/go/bin:/go/bin:/go/src/github.com/gravitational/teleport/build"
 

build.assets/Makefile
@@ -23,18 +23,11 @@ OS ?= linux
 ARCH ?= amd64
 BUILDBOX_VERSION ?= teleport11
 
-# NOTE: Windows Drone scripts hardcode $GOLANG_VERSION rather than drawing it
-#       from here at build time, so any changes here must be reflected in
-#       `dronegen/windows.go`
-GOLANG_VERSION ?= go1.18.6
+GOLANG_VERSION ?= go1.19.1
 
-# NOTE: Windows Drone scripts hardcode $NODE_VERSION rather than drawing it
-#       from here at build time, so any changes here must be reflected in
-#       `dronegen/windows.go`
 NODE_VERSION ?= 16.13.2
 
 RUST_VERSION ?= 1.63.0 # don't bump this without checking GLIBC compatibility
-BORINGCRYPTO_RUNTIME=$(GOLANG_VERSION)b7
 LIBBPF_VERSION ?= 0.7.0-teleport
 
 UID := $$(id -u)
@@ -160,7 +153,7 @@ buildbox-fips:
 		docker build \
 			--build-arg UID=$(UID) \
 			--build-arg GID=$(GID) \
-			--build-arg BORINGCRYPTO_RUNTIME=$(BORINGCRYPTO_RUNTIME) \
+			--build-arg GOLANG_VERSION=$(GOLANG_VERSION) \
 			--build-arg LIBBPF_VERSION=$(LIBBPF_VERSION) \
 			--cache-from $(BUILDBOX_FIPS) \
 			--tag $(BUILDBOX_FIPS) -f Dockerfile-fips . ; \
@@ -192,7 +185,7 @@ buildbox-centos7-fips:
 	docker build \
 		--build-arg UID=$(UID) \
 		--build-arg GID=$(GID) \
-		--build-arg BORINGCRYPTO_RUNTIME=$(BORINGCRYPTO_RUNTIME) \
+		--build-arg GOLANG_VERSION=$(GOLANG_VERSION) \
 		--build-arg RUST_VERSION=$(RUST_VERSION) \
 		--build-arg LIBBPF_VERSION=$(LIBBPF_VERSION) \
 		--cache-from $(BUILDBOX_CENTOS7_FIPS) \

build.assets/webapps/webapps-version.sh
@@ -2,7 +2,7 @@
 
 # If this build was triggered from a tag on the teleport repo,
 # then assume we can use the same tag for the webapps repo.
-if [ ! -z "$DRONE_TAG" ]
+if [ -n "$DRONE_TAG" ]
 then
     echo "$DRONE_TAG"
     exit 0

commit 428c171b911ee20f15fd15b7b252126631695742

Set and manage GOCACHE for integration tests (#16523)
Fixes integration test breakage caused by #16479.

* Set and manage GOCACHE for integration tests

.cloudbuild/scripts/cmd/integration-tests/main.go
@@ -37,9 +37,9 @@ import (
 )
 
 const (
-	gomodcacheDir = ".gomodcache-ci"
-	nonrootUID    = 1000
-	nonrootGID    = 1000
+	goCachesRootName = "gocache"
+	nonrootUID       = 1000
+	nonrootGID       = 1000
 )
 
 // main is just a stub that prints out an error message and sets a nonzero exit
@@ -78,8 +78,11 @@ func innerMain() error {
 		}
 	}
 
-	moduleCacheDir := filepath.Join(os.TempDir(), gomodcacheDir)
-	gomodcache := fmt.Sprintf("GOMODCACHE=%s", moduleCacheDir)
+	goCacheRoot := filepath.Join(os.TempDir(), goCachesRootName)
+	env := map[string]string{
+		"GOCACHE":    filepath.Join(goCacheRoot, "go-build"),
+		"GOMODCACHE": filepath.Join(goCacheRoot, "pkg"),
+	}
 
 	log.Println("Analyzing code changes")
 	ch, err := changes.Analyze(args.workspace, args.targetBranch, args.commitSHA)
@@ -102,7 +105,7 @@ func innerMain() error {
 	}()
 
 	log.Printf("Running root-only integration tests...")
-	err = runRootIntegrationTests(args.workspace, gomodcache)
+	err = runRootIntegrationTests(args.workspace, env)
 	if err != nil {
 		return trace.Wrap(err, "Root-only integration tests failed")
 	}
@@ -121,7 +124,7 @@ func innerMain() error {
 		}
 
 		log.Printf("Reconfiguring module cache for nonroot user")
-		err = chownR(moduleCacheDir, nonrootUID, nonrootGID)
+		err = chownR(goCacheRoot, nonrootUID, nonrootGID)
 		if err != nil {
 			return trace.Wrap(err, "failed reconfiguring module cache")
 		}
@@ -142,7 +145,7 @@ func innerMain() error {
 	defer etcdSvc.Stop()
 
 	log.Printf("Running nonroot integration tests...")
-	err = runNonrootIntegrationTests(args.workspace, nonrootUID, nonrootGID, gomodcache)
+	err = runNonrootIntegrationTests(args.workspace, nonrootUID, nonrootGID, env)
 	if err != nil {
 		return trace.Wrap(err, "Nonroot integration tests failed")
 	}
@@ -152,12 +155,13 @@ func innerMain() error {
 	return nil
 }
 
-func runRootIntegrationTests(workspace string, env ...string) error {
+func runRootIntegrationTests(workspace string, env map[string]string) error {
 	// Run root integration tests
 	cmd := exec.Command("make", "rdpclient", "integration-root")
 	cmd.Dir = workspace
-	if len(env) > 0 {
-		cmd.Env = append(os.Environ(), env...)
+	cmd.Env = os.Environ()
+	for k, v := range env {
+		cmd.Env = append(cmd.Env, fmt.Sprintf("%v=%v", k, v))
 	}
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -165,10 +169,13 @@ func runRootIntegrationTests(workspace string, env ...string) error {
 	return cmd.Run()
 }
 
-func runNonrootIntegrationTests(workspace string, uid, gid int, env ...string) error {
+func runNonrootIntegrationTests(workspace string, uid, gid int, env map[string]string) error {
 	cmd := exec.Command("make", "integration")
 	cmd.Dir = workspace
-	cmd.Env = append(append(os.Environ(), "TELEPORT_ETCD_TEST=yes"), env...)
+	cmd.Env = append(os.Environ(), "TELEPORT_ETCD_TEST=yes")
+	for k, v := range env {
+		cmd.Env = append(cmd.Env, fmt.Sprintf("%v=%v", k, v))
+	}
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 