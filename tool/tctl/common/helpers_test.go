f0b6d0c67030be5e46c49eb8b5b5cf18e9af6fca
Gavin Frazar <16846283+GavinFrazar@users.noreply.github.com>
2022-09-19 10:28:27 -0700

    Fix tctl auth server flag (#16130) (#16263)

--- tool/tctl/common/helpers_test.go
+++ tool/tctl/common/helpers_test.go
@@ -78,7 +78,7 @@ func runResourceCommand(t *testing.T, fc *config.FileConfig, args []string, opts
 	ccf.ConfigString = mustGetBase64EncFileConfig(t, fc)
 	ccf.Insecure = options.Insecure
 
-	clientConfig, err := applyConfig(&ccf, cfg)
+	clientConfig, err := ApplyConfig(&ccf, cfg)
 	require.NoError(t, err)
 
 	if options.CertPool != nil {
