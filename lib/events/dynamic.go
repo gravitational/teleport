e2fab63ba95af13b1a00852ef2def74ae65935d7
Alex McGrath <alex.mcgrath@goteleport.com>
2022-09-21 13:23:06 +0100

    Introduce discovery_service and automatically run an SSM Document on discovered EC2 nodes  (#14094)
    * Add initial version of installer
    
    * Resolve comments
    
    - Use aws waiters when checking commands
    - Use SSMRunRequest rather than passing instances
    - General comments
    
    * Resolve comments, (rebase) pass scriptname parameter
    
    This resolves comments regarding running on multiple ec2 instances at
    once by adding state to the instances cache to check if the instance
    is known about and how far into installation it is
    
    * Revert cache
    
    * Dont cache on non discovery nodes
    
    * Resolve some comments
    
    * Move discovery out to its own serviec
    
    * Add a `discovery_service` section
    
    * Fix messed up conflict merge
    
    * Make starting a standalone discovery agent work
    
    * Resolve comments
    
    * Resolve comments
    
    - use a regular events.Emitter
    - resolve a thousand typos :)
    
    * Resolve comments
    
    * resolve comments, fix a bad merge
    
    * Fail when a non ec2 matcher type is configured
    
    * fix lint-go
    
    * Resolve comments
    
    * Resolve comments, add initial test (currently broken)
    
    * Fix log string so only 1 pair of [] are used
    
    * Chunk instances for sending commands
    
    * add 'isInitialized' to watchers
    
    * Add test for chunked discovery, log output
    
    * lints
    
    * explicetly set matcher.Tags to "*":"*" if its unset

--- lib/events/dynamic.go
+++ lib/events/dynamic.go
@@ -251,6 +251,8 @@ func FromEventFields(fields EventFields) (events.AuditEvent, error) {
 		e = &events.UpgradeWindowStartUpdate{}
 	case SessionRecordingAccessEvent:
 		e = &events.SessionRecordingAccess{}
+	case SSMRunEvent:
+		e = &events.SSMRun{}
 	case UnknownEvent:
 		e = &events.Unknown{}
 	default:
