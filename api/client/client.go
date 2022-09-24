515824c2d16a0ff13a76d7fc128a4e7a7d2ebec0
Zac Bergquist <zac.bergquist@goteleport.com>
2022-09-23 18:07:46 -0600

    [v10] Misc Backports (#16674)
    * Init an uploader service for Windows Desktop Service
    
    This will result in multiple uploader services running if there are
    other Teleport services running in the same process, but that will
    be fixed by #14521.
    
    Updates #12549
    
    * buddy: Fix incorrect use of loop variables (#16306)
    
    * Fix incorrect use of loop variables
    
    This commit fixes a few occurrences of loop variables being
    incorrectly used in the context of Go-routines or (most frequently)
    parallel tests. To fix the issues, we create a local copy of the range
    variables before the parallel tests (or Go-routine), as suggested in
    the documentation of the `testing` package:
    
    https://pkg.go.dev/testing#hdr-Subtests_and_Sub_benchmarks
    
    Issues were found using the `loopvarcapture` linter.
    
    Signed-off-by: Roman Tkachenko <roman@goteleport.com>
    
    * fix TestTraceProvider/spans_exported_with_gRPC+TLS
    
    * run TestSSH serially
    
    * operator: Conserve 'created_by' data in user spec
    
    Signed-off-by: Roman Tkachenko <roman@goteleport.com>
    Co-authored-by: Renato Costa <renato@cockroachlabs.com>
    Co-authored-by: Tim Ross <tim.ross@goteleport.com>
    Co-authored-by: Hugo Hervieux <hugo.hervieux@goteleport.com>
    
    * Correctly set the end time when completing an upload
    
    The upload completer would set the session end time based on the last
    known event in the session, but it left the timestamp in the event's
    metadata blank, which caused it to default to the time the upload was
    completed.
    
    Fixes #16555
    
    * Speed up TestTerminalPing (#16481)
    
    This test takes between 10-20 seconds to run, because it waits for
    a websocket ping message and pings run on a 10s timer by default.
    
    It turns out, we allow specifying the keepalive interval in the
    terminal request, but due to a bug we would always overwrite that
    value with what's in the cluster networking config.
    
    Fix the bug and speed up the test by overriding the keepalive interval
    to something shorter for this test.
    
    Before:
    
        ➜ go test -run TestTerminalPing -race ./lib/web -count=1
        ok      github.com/gravitational/teleport/lib/web       13.122s
    
    After:
    
        ➜ go test -run TestTerminalPing -race ./lib/web -count=1
        ok      github.com/gravitational/teleport/lib/web       3.394s
    
    * Add labels to Windows Desktop Service, add endpoint for searching them (#16436)
    
    * export GetResourcesByResourceIDs
    
    * add WithCluserClientProvider
    
    * Improve unit tests in CI (#16698)
    
    - Increase timeout
    - Detect whether operator, CI, Rust, or Helm tests are necessary
    
    * Update `webassets` ref to latest `teleport-v10`
    
    Signed-off-by: Roman Tkachenko <roman@goteleport.com>
    Co-authored-by: Roman Tkachenko <roman@goteleport.com>
    Co-authored-by: Renato Costa <renato@cockroachlabs.com>
    Co-authored-by: Tim Ross <tim.ross@goteleport.com>
    Co-authored-by: Hugo Hervieux <hugo.hervieux@goteleport.com>
    Co-authored-by: Ryan Clark <ryan@ryanclark.me>
    Co-authored-by: Nic Klaassen <nic@goteleport.com>
    Co-authored-by: Ryan Clark <ryan.clark@goteleport.com>

--- api/client/client.go
+++ api/client/client.go
@@ -2653,6 +2653,8 @@ func (c *Client) ListResources(ctx context.Context, req proto.ListResourcesReque
 			resources[i] = respResource.GetKubeService()
 		case types.KindWindowsDesktop:
 			resources[i] = respResource.GetWindowsDesktop()
+		case types.KindWindowsDesktopService:
+			resources[i] = respResource.GetWindowsDesktopService()
 		case types.KindKubernetesCluster:
 			resources[i] = respResource.GetKubeCluster()
 		default:
