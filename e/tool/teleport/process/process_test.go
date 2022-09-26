48295e9fc1cff6f17dfa80d516e4d0c00402fb32
Ryan Clark <ryan.clark@goteleport.com>
2022-09-26 18:26:54 +0100

    Revert "Use new Config API for setting the auth address on test setup (#534)" (#537)
    This reverts commit c7a5de20964f9d4a2109500629015efab7b84de6.
    
    Source-commit: gravitational/teleport.e@aef79429029ac2fcad897d730a2c56ce0eaa36e0
    Source-pull-requests: gravitational/teleport.e#534, gravitational/teleport.e#537

--- e/tool/teleport/process/process_test.go
+++ e/tool/teleport/process/process_test.go
@@ -21,10 +21,11 @@ func TestProxyWithoutLicense(t *testing.T) {
 		Proxy: service.ProxyConfig{
 			Enabled: true,
 		},
+		AuthServers: []utils.NetAddr{
+			*utils.MustParseAddr("tcp://127.0.0.1:8080"),
+		},
 		Log: utils.WrapLogger(logrus.WithField("test", t.Name())),
 	}
-
-	config.SetAuthServerAddress(*utils.MustParseAddr("tcp://127.0.0.1:8080"))
 
 	_, err := NewTeleport(config)
 	require.Nil(t, err)
@@ -52,10 +53,11 @@ func TestMissingLicenseError(t *testing.T) {
 			ListenAddr:   *utils.MustParseAddr("tcp://127.0.0.1:0"),
 		},
 		Hostname: "localhost",
-		Log:      utils.WrapLogger(logrus.WithField("test", t.Name())),
+		AuthServers: []utils.NetAddr{
+			*utils.MustParseAddr("tcp://127.0.0.1:8080"),
+		},
+		Log: utils.WrapLogger(logrus.WithField("test", t.Name())),
 	}
-
-	config.SetAuthServerAddress(*utils.MustParseAddr("tcp://127.0.0.1:8080"))
 
 	_, err = NewTeleport(config)
 	require.True(t, trace.IsAccessDenied(err))
