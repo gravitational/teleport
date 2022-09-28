e5d56886ac402b56da9cc3f9140e6f11d96a1ff7
Justinas Stankevičius <justinas@users.noreply.github.com>
2022-09-28 13:59:33 +0300

    [v9] Clean up old artifacts when retrying a tag build (#16669)

--- dronegen/tag.go
+++ dronegen/tag.go
@@ -24,6 +24,10 @@ const (
 	rpmPackage = "rpm"
 	// debPackage is the DEB package type
 	debPackage = "deb"
+
+	// tagCleanupPipelineName is the name of the pipeline that cleans up
+	// artifacts from a previous partially-failed build
+	tagCleanupPipelineName = "clean-up-previous-build"
 )
 
 const releasesHost = "https://releases-staging.platform.teleport.sh"
@@ -199,6 +203,8 @@ func tagPipelines() []pipeline {
 	ps = append(ps, tagPipeline(buildType{os: "linux", arch: "amd64", centos7: true, fips: true}))
 
 	ps = append(ps, darwinTagPipeline(), darwinTeleportPkgPipeline(), darwinTshPkgPipeline())
+
+	ps = append(ps, tagCleanupPipeline())
 	return ps
 }
 
@@ -243,6 +249,7 @@ func tagPipeline(b buildType) pipeline {
 		"RUNTIME":          goRuntime,
 	}
 	p.Trigger = triggerTag
+	p.DependsOn = []string{tagCleanupPipelineName}
 	p.Workspace = workspace{Path: "/go"}
 	p.Volumes = dockerVolumes()
 	p.Services = []service{
@@ -458,7 +465,7 @@ func tagPackagePipeline(packageType string, b buildType) pipeline {
 
 	p := newKubePipeline(pipelineName)
 	p.Trigger = triggerTag
-	p.DependsOn = []string{dependentPipeline}
+	p.DependsOn = []string{dependentPipeline, tagCleanupPipelineName}
 	p.Workspace = workspace{Path: "/go"}
 	p.Volumes = packageDockerVolumes
 	p.Services = []service{
@@ -515,4 +522,8 @@ func tagPackagePipeline(packageType string, b buildType) pipeline {
 		},
 	}
 	return p
+}
+
+func tagCleanupPipeline() pipeline {
+	return relcliPipeline(triggerTag, tagCleanupPipelineName, "Clean up previously built artifacts", "relcli auto_destroy -f -v 6")
 }
