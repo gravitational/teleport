93795d24b6a30f9250de4ff3276e9fd075c41f0d
Tim Ross <tim.ross@goteleport.com>
2022-09-28 17:34:41 -0400

    Fix label based tsh when per session mfa is enabled via role
    Label based tsh was sending UUID:0 as the node name to the
    IsMFARequired check. The server was checking this name against each
    nodes name and uuid. However the port included in the name prevented
    any matches from being found. Which resulted in the ssh session being
    rejected with an access denied message by the node because mfa was
    required but never performed.
    
    This removes the :0 from the node name to the IsMFARequired check and
    adds a test case to TestSSHOnMultipleNodes that uses role based
    per session mfa.

--- tool/tsh/tsh_test.go
+++ tool/tsh/tsh_test.go
@@ -794,7 +794,6 @@ func TestSSHOnMultipleNodes(t *testing.T) {
 	// Running ssh 'echo test' command should work when referencing
 	// multiple nodes by labels, without and with mfa_per_session.
 	t.Parallel()
-	tmpHomePath := t.TempDir()
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
@@ -810,6 +809,15 @@ func TestSSHOnMultipleNodes(t *testing.T) {
 		},
 	})
 	require.NoError(t, err)
+
+	perSessionMFARole, err := types.NewRoleV3("mfa-login", types.RoleSpecV5{
+		Allow: types.RoleConditions{
+			Logins: []string{user.Username},
+		},
+		Options: types.RoleOptions{RequireSessionMFA: true},
+	})
+	require.NoError(t, err)
+
 	alice, err := types.NewUser("alice")
 	require.NoError(t, err)
 	alice.SetRoles([]string{"access", "ssh-login"})
@@ -819,9 +827,7 @@ func TestSSHOnMultipleNodes(t *testing.T) {
 	require.NoError(t, err)
 	device.SetPasswordless()
 
-	rootAuth, rootProxy := makeTestServers(t,
-		withBootstrap(connector, alice, sshLoginRole),
-	)
+	rootAuth, rootProxy := makeTestServers(t, withBootstrap(connector, alice, sshLoginRole, perSessionMFARole))
 
 	authAddr, err := rootAuth.AuthAddr()
 	require.NoError(t, err)
@@ -927,6 +933,7 @@ func TestSSHOnMultipleNodes(t *testing.T) {
 		name            string
 		hostLabels      string
 		authPreference  types.AuthPreference
+		roles           []string
 		setup           func(t *testing.T)
 		errAssertion    require.ErrorAssertionFunc
 		stdoutAssertion require.ValueAssertionFunc
@@ -1054,10 +1061,32 @@ func TestSSHOnMultipleNodes(t *testing.T) {
 			deviceSignCount: 0,
 			stdoutAssertion: require.Empty,
 		},
+		{
+			name: "role requires per session mfa - 2 stage nodes",
+			authPreference: &types.AuthPreferenceV2{
+				Spec: types.AuthPreferenceSpecV2{
+					Type:         constants.Local,
+					SecondFactor: constants.SecondFactorOptional,
+					Webauthn: &types.Webauthn{
+						RPID: "127.0.0.1",
+					},
+				},
+			},
+			roles:      []string{"access", sshLoginRole.GetName(), perSessionMFARole.GetName()},
+			setup:      registerPasswordlessDeviceWithWebauthnSolver,
+			hostLabels: "env=stage",
+			stdoutAssertion: func(t require.TestingT, i interface{}, i2 ...interface{}) {
+				require.Equal(t, "test\ntest\n", i, i2...)
+			},
+			deviceSignCount: 2,
+			errAssertion:    require.NoError,
+		},
 	}
 
 	for _, tt := range cases {
 		t.Run(tt.name, func(t *testing.T) {
+			tmpHomePath := t.TempDir()
+
 			require.NoError(t, rootAuth.GetAuthServer().SetAuthPreference(ctx, tt.authPreference))
 			t.Cleanup(func() {
 				require.NoError(t, rootAuth.GetAuthServer().SetAuthPreference(ctx, defaultPreference))
@@ -1065,6 +1094,16 @@ func TestSSHOnMultipleNodes(t *testing.T) {
 
 			if tt.setup != nil {
 				tt.setup(t)
+			}
+
+			if tt.roles != nil {
+				roles := alice.GetRoles()
+				t.Cleanup(func() {
+					alice.SetRoles(roles)
+					require.NoError(t, rootAuth.GetAuthServer().UpsertUser(alice))
+				})
+				alice.SetRoles(tt.roles)
+				require.NoError(t, rootAuth.GetAuthServer().UpsertUser(alice))
 			}
 
 			err = Run(ctx, []string{
