cfc523f74d12e4de4ccab6f18d4c72361155ef41
Justinas Stankevičius <justinas@users.noreply.github.com>
2022-09-28 13:59:33 +0300

    [v7] Clean up old artifacts when retrying a tag build (#16669)

--- dronegen/main.go
+++ dronegen/main.go
@@ -22,6 +22,7 @@ func main() {
 	pipelines = append(pipelines, promoteBuildPipeline())
 	pipelines = append(pipelines, updateDocsPipeline())
 	pipelines = append(pipelines, buildboxPipeline())
+	pipelines = append(pipelines, publishReleasePipeline())
 
 	if err := writePipelines(".drone.yml", pipelines); err != nil {
 		fmt.Println("failed writing drone pipelines:", err)
