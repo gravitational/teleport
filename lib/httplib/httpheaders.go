1942589bf1909708323ec2914aa6809036053c1d
Ryan Clark <ryan.clark@goteleport.com>
2022-10-07 17:04:14 +0100

    Change the application access auth flow to use a CORS POST request

--- lib/httplib/httpheaders.go
+++ lib/httplib/httpheaders.go
@@ -81,6 +81,11 @@ func GetDefaultContentSecurityPolicy() string {
 	}, ";")
 }
 
+// SetDefaultContentSecurityPolicy provides a starting Content Security Policy with safe defaults.
+func SetDefaultContentSecurityPolicy(h http.Header) {
+	h.Set("Content-Security-Policy", GetDefaultContentSecurityPolicy())
+}
+
 // SetWebConfigHeaders sets headers for webConfig.js
 func SetWebConfigHeaders(h http.Header) {
 	h.Set("Content-Type", "application/javascript")
