9044a3bf245a4ecc949cfe7ef21f113f0516f1a9
Michael Myers <michael.myers@goteleport.com>
2022-10-10 20:57:29 -0500

    recurse submodules teleport connect windows

--- dronegen/windows.go
+++ dronegen/windows.go
@@ -150,9 +150,8 @@ func cloneWindowsRepositoriesStep(workspace string) step {
 			`git checkout $TeleportRev`,
 			`New-Item -Path $WebappsSrc -ItemType Directory | Out-Null`,
 			`cd $WebappsSrc`,
-			`git clone https://github.com/gravitational/webapps.git .`,
+			`git --recurse-submodules clone https://github.com/gravitational/webapps.git .`,
 			`git checkout $(& $TeleportSrc/build.assets/webapps/webapps-version.ps1)`,
-			`git submodule update --init packages/webapps.e`,
 		},
 	}
 }
