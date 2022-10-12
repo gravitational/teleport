6b48a0332a88482f443fddfd9cfe018f89ff5422
Michael Myers <michael.myers@goteleport.com>
2022-10-12 08:18:14 -0500

    removed ability to fail webapps.e and recursive

--- dronegen/mac.go
+++ dronegen/mac.go
@@ -273,7 +273,7 @@ func pushCheckoutCommandsDarwin(artifactConfig darwinArtifactConfig) []string {
 		// this is allowed to fail because pre-4.3 Teleport versions don't use the webassets submodule
 		`GIT_SSH_COMMAND='ssh -i $WORKSPACE_DIR/.ssh/id_rsa -o UserKnownHostsFile=$WORKSPACE_DIR/.ssh/known_hosts -F /dev/null' git submodule update --init --recursive webassets || true`,
 		`cd $WORKSPACE_DIR/go/src/github.com/gravitational/webapps`,
-		`GIT_SSH_COMMAND='ssh -i $WORKSPACE_DIR/.ssh/id_rsa -o UserKnownHostsFile=$WORKSPACE_DIR/.ssh/known_hosts -F /dev/null' git submodule update --init --recursive packages/webapps.e || true`,
+		`GIT_SSH_COMMAND='ssh -i $WORKSPACE_DIR/.ssh/id_rsa -o UserKnownHostsFile=$WORKSPACE_DIR/.ssh/known_hosts -F /dev/null' git submodule update --init packages/webapps.e`,
 		`rm -rf $WORKSPACE_DIR/.ssh`,
 		`mkdir -p $WORKSPACE_DIR/go/cache`,
 	)
