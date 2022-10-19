675cec9e6284ff307d57bc66632d9ce89a45b33f
Tim Ross <tim.ross@goteleport.com>
2022-10-19 11:21:35 -0400

    Version 10.2.6-dev.iam.2

--- api/version.go
+++ api/version.go
@@ -3,7 +3,7 @@
 package api
 
 const (
-	Version = "10.2.6-dev.iam.1"
+	Version = "10.2.6-dev.iam.2"
 )
 
 // Gitref variable is automatically set to the output of git-describe
--- version.go
+++ version.go
@@ -3,7 +3,7 @@
 package teleport
 
 const (
-	Version = "10.2.6-dev.iam.1"
+	Version = "10.2.6-dev.iam.2"
 )
 
 // Gitref variable is automatically set to the output of git-describe
