73fc5bc162ab63eb9ea67441543b14ee24886c33
Nic Klaassen <nic@goteleport.com>
2022-10-26 21:29:33 -0700

    shrink/remove keystore interface
    This commit introduces the keystore.Manager type to handle all
    interaction between CA and the keystore backends.
    
    Why:
    
    * reduces the code that needs to be implemented per keystore backend to
      only the necessary operations
    * separate concerns of managing key material and handling CA data
      structures
    * define interfaces where they're used, not implemented
    * delete net 245 lines of code
    * reduce keystore.KeyStore stutter

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -201,7 +201,7 @@ func NewServer(cfg *InitConfig, opts ...ServerOption) (*Server, error) {
 		native.PrecomputeKeys()
 		cfg.KeyStoreConfig.Software.RSAKeyPairSource = native.GenerateKeyPair
 	}
-	keyStore, err := keystore.NewKeyStore(cfg.KeyStoreConfig)
+	keyStore, err := keystore.NewManager(cfg.KeyStoreConfig)
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
@@ -429,9 +429,9 @@ type Server struct {
 	// session related streams
 	streamer events.Streamer
 
-	// keyStore is an interface for interacting with private keys in CAs which
-	// may be backed by HSMs
-	keyStore keystore.KeyStore
+	// keyStore manages all CA private keys, which  may or may not be backed by
+	// HSMs
+	keyStore *keystore.Manager
 
 	// lockWatcher is a lock watcher, used to verify cert generation requests.
 	lockWatcher *services.LockWatcher
@@ -913,7 +913,7 @@ func (a *Server) generateHostCert(p services.HostCertParams) ([]byte, error) {
 }
 
 // GetKeyStore returns the KeyStore used by the auth server
-func (a *Server) GetKeyStore() keystore.KeyStore {
+func (a *Server) GetKeyStore() *keystore.Manager {
 	return a.keyStore
 }
 
@@ -3728,11 +3728,11 @@ func mergeKeySets(a, b types.CAKeySet) types.CAKeySet {
 
 // addAdditionalTrustedKeysAtomic performs an atomic CompareAndSwap to update
 // the given CA with newKeys added to the AdditionalTrustedKeys
-func (a *Server) addAddtionalTrustedKeysAtomic(
+func (a *Server) addAdditionalTrustedKeysAtomic(
 	ctx context.Context,
 	currentCA types.CertAuthority,
 	newKeys types.CAKeySet,
-	needsUpdate func(types.CertAuthority) bool,
+	needsUpdate func(types.CertAuthority) (bool, error),
 ) error {
 	for {
 		select {
@@ -3740,7 +3740,9 @@ func (a *Server) addAddtionalTrustedKeysAtomic(
 			return trace.Wrap(a.closeCtx.Err())
 		default:
 		}
-		if !needsUpdate(currentCA) {
+		if updateRequired, err := needsUpdate(currentCA); err != nil {
+			return trace.Wrap(err)
+		} else if !updateRequired {
 			return nil
 		}
 
@@ -3770,7 +3772,7 @@ func (a *Server) addAddtionalTrustedKeysAtomic(
 
 // newKeySet generates a new sets of keys for a given CA type.
 // Keep this function in sync with lib/service/suite/suite.go:NewTestCAWithConfig().
-func newKeySet(keyStore keystore.KeyStore, caID types.CertAuthID) (types.CAKeySet, error) {
+func newKeySet(keyStore *keystore.Manager, caID types.CertAuthID) (types.CAKeySet, error) {
 	var keySet types.CAKeySet
 	switch caID.Type {
 	case types.UserCA, types.HostCA:
@@ -3806,7 +3808,11 @@ func newKeySet(keyStore keystore.KeyStore, caID types.CertAuthID) (types.CAKeySe
 // ensureLocalAdditionalKeys adds additional trusted keys to the CA if they are not
 // already present.
 func (a *Server) ensureLocalAdditionalKeys(ctx context.Context, ca types.CertAuthority) error {
-	if a.keyStore.HasLocalAdditionalKeys(ca) {
+	hasUsableKeys, err := a.keyStore.HasUsableAdditionalKeys(ca)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	if hasUsableKeys {
 		// nothing to do
 		return nil
 	}
@@ -3816,8 +3822,9 @@ func (a *Server) ensureLocalAdditionalKeys(ctx context.Context, ca types.CertAut
 		return trace.Wrap(err)
 	}
 
-	err = a.addAddtionalTrustedKeysAtomic(ctx, ca, newKeySet, func(ca types.CertAuthority) bool {
-		return !a.keyStore.HasLocalAdditionalKeys(ca)
+	err = a.addAdditionalTrustedKeysAtomic(ctx, ca, newKeySet, func(ca types.CertAuthority) (bool, error) {
+		hasUsableKeys, err := a.keyStore.HasUsableAdditionalKeys(ca)
+		return hasUsableKeys, trace.Wrap(err)
 	})
 	if err != nil {
 		return trace.Wrap(err)
