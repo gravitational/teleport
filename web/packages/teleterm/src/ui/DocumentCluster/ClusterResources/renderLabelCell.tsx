19e44e7b8e3d6612bd808b132758d15027f1c520
Rafał Cieślak <rafal.cieslak@goteleport.com>
2022-10-31 18:16:03 +0100

    Connect: Use gap instead of margins for <Label> groups (#1316)
    This avoids extra space where it's not necessary.

--- web/packages/teleterm/src/ui/DocumentCluster/ClusterResources/renderLabelCell.tsx
+++ web/packages/teleterm/src/ui/DocumentCluster/ClusterResources/renderLabelCell.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
+
 import { makeLabelTag } from 'teleport/components/formatters';
 import { Cell } from 'design/DataTable';
-import { Label as SingleLabel } from 'design';
-import { Label } from 'teleport/types';
+import { Label, Flex } from 'design';
+import * as types from 'teleport/types';
 
-export function renderLabelCell<T extends { labelsList: Label[] }>(props: T) {
+export function renderLabelCell<T extends { labelsList: types.Label[] }>(
+  props: T
+) {
   const labels = props.labelsList.map(makeLabelTag);
   const $labels = labels.map(label => (
-    <SingleLabel mb="1" mr="1" key={label} kind="secondary">
+    <Label key={label} kind="secondary">
       {label}
-    </SingleLabel>
+    </Label>
   ));
 
-  return <Cell>{$labels}</Cell>;
+  return (
+    <Cell>
+      <Flex flexWrap="wrap" gap={1}>
+        {$labels}
+      </Flex>
+    </Cell>
+  );
 }
