5eff104e41567661c910bfd12988b75a54d61ce5
Michael Wilson <mike@mdwn.dev>
2022-11-01 12:00:41 -0400

    [v11] Prioritize HTTP/1.1 over HTTP/2. (#18002)
    Due to a bug in Chrome, secure websockets in Teleport work intermittently on
    the Chrome browser when using HTTP/2. Prioritizing HTTP/1.1 fixes this issue,
    though the reasons for this are not 100% clear. When or if
    https://bugs.chromium.org/p/chromium/issues/detail?id=1379017 is resolved, we
    should be able to revert this. When https://github.com/golang/go/issues/49918
    is implemented, we may be able to revert this if enabling HTTP/2 websockets
    fixes the issue on our end.

--- lib/srv/alpnproxy/common/protocols.go
+++ lib/srv/alpnproxy/common/protocols.go
@@ -65,7 +65,6 @@ const (
 	// be included in the list of ALPN header for the proxy server to handle the connection properly.
 	ProtocolReverseTunnelV2 Protocol = "teleport-reversetunnelv2"
 
-	// ProtocolHTTP is TLS ALPN protocol value used to indicate HTTP2 protocol
 	// ProtocolHTTP is TLS ALPN protocol value used to indicate HTTP 1.1 protocol
 	ProtocolHTTP Protocol = "http/1.1"
 
@@ -98,8 +97,14 @@ const (
 var SupportedProtocols = append(
 	ProtocolsWithPing(ProtocolsWithPingSupport...),
 	append([]Protocol{
-		ProtocolHTTP2,
+		// HTTP needs to be prioritized over HTTP2 due to a bug in Chrome:
+		// https://bugs.chromium.org/p/chromium/issues/detail?id=1379017
+		// If Chrome resolves this, we can switch the prioritization. We may
+		// also be able to get around this if https://github.com/golang/go/issues/49918
+		// is implemented and we can enable HTTP2 websockets on our end, but
+		// it's less clear this will actually fix the issue.
 		ProtocolHTTP,
+		ProtocolHTTP2,
 		ProtocolProxySSH,
 		ProtocolReverseTunnel,
 		ProtocolAuth,
