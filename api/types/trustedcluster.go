cc5cfb8beeb3cf83de22cd380443adac8f2bfae5
EdwardDowling <edward.dowling@goteleport.com>
2022-11-04 17:40:21 +0000

    Allow updating of trusted cluster role maps

--- api/types/trustedcluster.go
+++ api/types/trustedcluster.go
@@ -249,11 +249,12 @@ func (c *TrustedClusterV2) CanChangeStateTo(t TrustedCluster) error {
 	if !slices.Equal(c.GetRoles(), t.GetRoles()) {
 		return immutableFieldErr("roles")
 	}
+	roleMapUpdated := false
 	if !cmp.Equal(c.GetRoleMap(), t.GetRoleMap()) {
-		return immutableFieldErr("role_map")
+		roleMapUpdated = true
 	}
 
-	if c.GetEnabled() == t.GetEnabled() {
+	if c.GetEnabled() == t.GetEnabled() && !roleMapUpdated {
 		if t.GetEnabled() {
 			return trace.AlreadyExists("leaf cluster is already enabled, this update would have no effect")
 		}
