df7959571b1743a2ab5c86acb5f09267a56e05e6
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2022-11-06 20:29:41 -0500

    Cleanup Add workaround for OpenSSH 7

--- lib/sshutils/signer.go
+++ lib/sshutils/signer.go
@@ -18,6 +18,7 @@ package sshutils
 
 import (
 	"crypto"
+	"io"
 
 	"github.com/gravitational/teleport/api/utils/sshutils"
 
@@ -25,6 +26,20 @@ import (
 
 	"github.com/gravitational/trace"
 )
+
+// LegacySHA1Signer always forces use of SHA-1 for signing. It should be not
+// be used until necessary.
+type LegacySHA1Signer struct {
+	Signer ssh.AlgorithmSigner
+}
+
+func (s *LegacySHA1Signer) PublicKey() ssh.PublicKey {
+	return s.Signer.PublicKey()
+}
+
+func (s *LegacySHA1Signer) Sign(rand io.Reader, data []byte) (*ssh.Signature, error) {
+	return s.Signer.SignWithAlgorithm(rand, data, ssh.KeyAlgoRSA)
+}
 
 // NewSigner returns new ssh Signer from private key + certificate pair.  The
 // signer can be used to create "auth methods" i.e. login into Teleport SSH
