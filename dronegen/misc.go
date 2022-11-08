def609a2bd9d88a2995bf5aa316ab8485528ff9f
fheinecke <23390735+fheinecke@users.noreply.github.com>
2022-11-08 16:59:08 -0600

    Backport multiarch container images to v11 (#18222)
    * Added multiarch build support for teleport-operator (#16688)
    
    * Added multiarch build support for Teleport (#17597)
    
    * Added product min version

--- dronegen/misc.go
+++ dronegen/misc.go
@@ -18,13 +18,3 @@ func updateDocsPipeline() pipeline {
 	// TODO: migrate
 	return pipeline{}
 }
-
-func verifyTaggedBuildStep() step {
-	return step{
-		Name:  "Verify build is tagged",
-		Image: "alpine:latest",
-		Commands: []string{
-			"[ -n ${DRONE_TAG} ] || (echo 'DRONE_TAG is not set. Is the commit tagged?' && exit 1)",
-		},
-	}
-}
