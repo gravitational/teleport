a62f464244b865a51193c0fa7960024b796b9d41
Justinas Stankevicius <justinas@goteleport.com>
2022-11-09 16:31:17 +0200

    Bump relcli (attach assets atomically)

--- dronegen/windows.go
+++ dronegen/windows.go
@@ -24,8 +24,8 @@ const (
 	teleportSrc       = `/go/src/github.com/gravitational/teleport`
 	webappsSrc        = `/go/src/github.com/gravitational/webapps`
 
-	relcliURL    = `https://cdn.teleport.dev/relcli-v1.1.75-windows.exe`
-	relcliSha256 = `664a48f349791b51c3fb5610ff282b050d5c3d74523730bb229256d2cc9ea0d7`
+	relcliURL    = `https://cdn.teleport.dev/relcli-v1.1.76-beta.1-windows.exe`
+	relcliSha256 = `7cf8ef6919f1945fa9484ee74e9e0862488a85c51b238b402ec6b73bcaaa9f76`
 )
 
 func newWindowsPipeline(name string) pipeline {
