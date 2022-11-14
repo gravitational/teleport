38015e9f3861d5efd976db9dcc317464f6752a4c
Tiago Silva <tiago.silva@goteleport.com>
2022-11-14 15:08:07 +0000

    Fix concurrency issues when `tsh login` is invoked in parallel tests (#18333) (#18426)
    When `tsh login` is called, it manipulates the user's kubeconfig. Kubeconfig location comes from $KUBECONFIG env var or, by default,  `$HOME/.kube/config`.
    
    When we call `tsh login` and `tsh logout`, they manipulate the kubeconfig to update some fields. While doing it in parallel tests, concurrency issues appear, and two or more writes happen concurrently. It results in an invalid kubeconfig that subsequently causes other tests to fail.
    
    This PR introduces a custom path to define the kubeconfig when running parallel tests to avoid concurrency issues.
    
    Fixes #15490

--- tool/tsh/tsh_test.go
+++ tool/tsh/tsh_test.go
@@ -2365,6 +2365,13 @@ func setHomePath(path string) cliOption {
 	}
 }
 
+func setKubeConfigPath(path string) cliOption {
+	return func(cf *CLIConf) error {
+		cf.kubeConfigPath = path
+		return nil
+	}
+}
+
 func setIdentity(path string) cliOption {
 	return func(cf *CLIConf) error {
 		cf.IdentityFileIn = path
