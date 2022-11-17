ec5ab3c1ebb0fd5fdda02bfdc8e63e20950be2a5
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2022-11-17 15:44:23 -0500

    Add comments.

--- lib/srv/db/snowflake/engine_test.go
+++ lib/srv/db/snowflake/engine_test.go
@@ -24,6 +24,8 @@ import (
 	"testing"
 
 	// Register Snowflake database driver.
+	// Do not move this dependency outside _test.go file. Doing so will create
+	// ocsp_response_cache.json in random places.
 	_ "github.com/snowflakedb/gosnowflake"
 	"github.com/stretchr/testify/require"
 )
