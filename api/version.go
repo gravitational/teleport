82fb0e99317faf09f1fbc17606338a37334a8ea0
mcbattirola <matheus.battirola@goteleport.com>
2022-11-21 11:15:35 -0300

    Revert "Make version"
    This reverts commit b0dacf827011ca241e64732c78087e3780aef6c7.

--- api/version.go
+++ api/version.go
@@ -3,7 +3,7 @@
 package api
 
 const (
-	Version = "12.0.0-mcbattirola-3"
+	Version = "12.0.0-dev"
 )
 
 // Gitref variable is automatically set to the output of git-describe
--- version.go
+++ version.go
@@ -3,7 +3,7 @@
 package teleport
 
 const (
-	Version = "12.0.0-mcbattirola-3"
+	Version = "12.0.0-dev"
 )
 
 // Gitref variable is automatically set to the output of git-describe
