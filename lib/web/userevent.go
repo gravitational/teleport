a4500a5a40170eea5207bb4c8cd7875a18f8c0b7
Michelle Bergquist <michelle.bergquist@goteleport.com>
2022-11-21 21:01:09 -0700

    unauthenticated user events

--- lib/web/userevent.go
+++ lib/web/userevent.go
@@ -23,6 +23,19 @@ const (
 	addFirstResourceLaterClickEvent = "tp.ui.onboard.addFirstResourceLater.click"
 )
 
+// createPreUserEventRequest contains the event and properties associated with a user event
+// the usageReporter convert event function will later set the timestamp
+// and anonymize/set the cluster name
+// the username is required for pre-user events
+type createPreUserEventRequest struct {
+	// Event describes the event being capture
+	Event string `json:"event"`
+	// Alert is a banner click event property
+	Alert string `json:"alert"`
+	// Username token is set for unauthenticated event requests
+	Username string `json:"username"`
+}
+
 // createUserEventRequest contains the event and properties associated with a user event
 // the usageReporter convert event function will later set the timestamp
 // and anonymize/set the cluster name
@@ -42,7 +55,81 @@ func (r *createUserEventRequest) CheckAndSetDefaults() error {
 	return nil
 }
 
+// CheckAndSetDefaults validates the Request has the required fields.
+func (r *createPreUserEventRequest) CheckAndSetDefaults() error {
+	if r.Event == "" {
+		return trace.BadParameter("missing required parameter Event")
+	}
+
+	if r.Username == "" {
+		return trace.BadParameter("missing required parameter Username")
+	}
+
+	return nil
+}
+
+// createPreUserEventHandle sends a user event to the UserEvent service
+// this handler is for on-boarding user events pre-session
+func (h *Handler) createPreUserEventHandle(w http.ResponseWriter, r *http.Request, p httprouter.Params) (interface{}, error) {
+	var req createPreUserEventRequest
+	if err := httplib.ReadJSON(r, &req); err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	if err := req.CheckAndSetDefaults(); err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	client := h.cfg.ProxyClient()
+
+	typedEvent := v1.UsageEventOneOf{}
+	switch req.Event {
+	case getStartedClickEvent:
+		typedEvent.Event = &v1.UsageEventOneOf_UiOnboardGetStartedClick{
+			UiOnboardGetStartedClick: &v1.UIOnboardGetStartedClickEvent{
+				// todo mberg add to event
+				// UserName: req.Username,
+			},
+		}
+	case setCredentialSubmitEvent:
+		typedEvent.Event = &v1.UsageEventOneOf_UiOnboardSetCredentialSubmit{
+			UiOnboardSetCredentialSubmit: &v1.UIOnboardSetCredentialSubmitEvent{
+				// todo mberg add to event
+				//UserName: req.Username,
+			},
+		}
+	case registerChallengeSubmitEvent:
+		typedEvent.Event = &v1.UsageEventOneOf_UiOnboardRegisterChallengeSubmit{
+			UiOnboardRegisterChallengeSubmit: &v1.UIOnboardRegisterChallengeSubmitEvent{
+				// todo mberg add to event
+				//UserName: req.Username,
+			},
+		}
+	case recoveryCodesContinueClickEvent:
+		typedEvent.Event = &v1.UsageEventOneOf_UiOnboardRecoveryCodesContinueClick{
+			UiOnboardRecoveryCodesContinueClick: &v1.UIOnboardRecoveryCodesContinueClickEvent{
+				// todo mberg add to event
+				//UserName: req.Username,
+			},
+		}
+	default:
+		return nil, trace.BadParameter("invalid event %s", req.Event)
+	}
+
+	event := &proto.SubmitUsageEventRequest{
+		Event: &typedEvent,
+	}
+
+	err := client.SubmitUsageEvent(r.Context(), event)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	return nil, nil
+}
+
 // createUserEventHandle sends a user event to the UserEvent service
+// this handler is for user events with a session
 func (h *Handler) createUserEventHandle(w http.ResponseWriter, r *http.Request, params httprouter.Params, sctx *SessionContext) (interface{}, error) {
 	var req createUserEventRequest
 	if err := httplib.ReadJSON(r, &req); err != nil {
@@ -65,22 +152,6 @@ func (h *Handler) createUserEventHandle(w http.ResponseWriter, r *http.Request, 
 			UiBannerClick: &v1.UIBannerClickEvent{
 				Alert: req.Alert,
 			},
-		}
-	case getStartedClickEvent:
-		typedEvent.Event = &v1.UsageEventOneOf_UiOnboardGetStartedClick{
-			UiOnboardGetStartedClick: &v1.UIOnboardGetStartedClickEvent{},
-		}
-	case setCredentialSubmitEvent:
-		typedEvent.Event = &v1.UsageEventOneOf_UiOnboardSetCredentialSubmit{
-			UiOnboardSetCredentialSubmit: &v1.UIOnboardSetCredentialSubmitEvent{},
-		}
-	case registerChallengeSubmitEvent:
-		typedEvent.Event = &v1.UsageEventOneOf_UiOnboardRegisterChallengeSubmit{
-			UiOnboardRegisterChallengeSubmit: &v1.UIOnboardRegisterChallengeSubmitEvent{},
-		}
-	case recoveryCodesContinueClickEvent:
-		typedEvent.Event = &v1.UsageEventOneOf_UiOnboardRecoveryCodesContinueClick{
-			UiOnboardRecoveryCodesContinueClick: &v1.UIOnboardRecoveryCodesContinueClickEvent{},
 		}
 	case addFirstResourceClickEvent:
 		typedEvent.Event = &v1.UsageEventOneOf_UiOnboardAddFirstResourceClick{
