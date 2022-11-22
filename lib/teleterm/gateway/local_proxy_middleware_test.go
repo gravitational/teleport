6f0cb3f5fc6a9b27ea048eb5fa6866ede8b0be95
Rafał Cieślak <rafal.cieslak@goteleport.com>
2022-11-22 13:35:25 +0100

    Remove tt := tt from serial tests

--- lib/teleterm/gateway/local_proxy_middleware_test.go
+++ lib/teleterm/gateway/local_proxy_middleware_test.go
@@ -96,7 +96,6 @@ func TestLocalProxyMiddleware_OnNewConnection(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			ctx, cancel := context.WithCancel(context.Background())
 			defer cancel()
