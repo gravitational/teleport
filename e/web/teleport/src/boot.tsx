5f32ecf8cda51a2e44be11244d0cc7e336a21eab
Jeff Pihach <hatched@users.noreply.github.com>
2022-11-28 13:59:04 -0600

    Allow telemetry to be enabled in enterprise (#438)
    Source-commit: gravitational/teleport.e@d1422dab06631a8a22a53f19bb213d04eb08532d
    Source-pull-requests: gravitational/teleport.e#438

--- e/web/teleport/src/boot.tsx
+++ e/web/teleport/src/boot.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import history from 'teleport/services/history';
+import { instantiateTelemetry } from 'teleport/telemetry-boot';
 
 import cfg from 'e-teleport/config';
 
@@ -12,6 +13,10 @@ cfg.init(window['GRV_CONFIG']);
 
 // use browser history
 history.init();
+
+if (localStorage.getItem('enable-telemetry') === 'true') {
+  instantiateTelemetry();
+}
 
 const teleportContextE = new TeleportContextE();
 
