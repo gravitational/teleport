4f833fbe5eb29b125888f926a79b8908768d7c44
Vitor Enes <vitor.duarte@goteleport.com>
2022-11-28 20:28:41 +0000

    Add `server_hostname` to `session.*` events (#18812)
    With this commit, `session.*` events now also contain a `server_hostname` field.
    
    Example:
    
    ```json
    {
      "argv": null,
      "cgroup_id": 6926,
      "cluster_name": "vitor.teleport.sh",
      "code": "T4000I",
      "ei": 17,
      "event": "session.command",
      "login": "root",
      "namespace": "default",
      "path": "/usr/bin/date",
      "pid": 98679,
      "ppid": 98668,
      "program": "date",
      "return_code": 0,
      "server_hostname": "ip-172-31-10-157",
      "server_id": "8369d1d1-eea5-4074-8f64-0103f1e1fb00",
      "sid": "a40353c4-93dc-4d6b-a481-5546ecafb83e",
      "time": "2022-11-28T16:32:34.342Z",
      "uid": "cbd9e562-8932-405d-974c-11637f91d711",
      "user": "vitor@goteleport.com"
    }
    ```

--- lib/restrictedsession/restricted_test.go
+++ lib/restrictedsession/restricted_test.go
@@ -169,15 +169,16 @@ func setupBPFContext(t *testing.T) *bpfContext {
 	require.NoError(t, err)
 
 	// Create the SessionContext used by both enhanced recording and us (restricted session)
-	tt.ctx = &bpf.SessionContext{
-		Namespace: apidefaults.Namespace,
-		SessionID: uuid.New().String(),
-		ServerID:  uuid.New().String(),
-		Login:     "foo",
-		User:      "foo@example.com",
-		PID:       os.Getpid(),
-		Emitter:   &tt.emitter,
-		Events:    map[string]bool{},
+	bpfCtx.ctx = &bpf.SessionContext{
+		Namespace:      apidefaults.Namespace,
+		SessionID:      uuid.New().String(),
+		ServerID:       uuid.New().String(),
+		ServerHostname: "ip-172-31-11-148",
+		Login:          "foo",
+		User:           "foo@example.com",
+		PID:            os.Getpid(),
+		Emitter:        &tt.emitter,
+		Events:         map[string]bool{},
 	}
 
 	// Create enhanced recording session to piggy-back on.
@@ -301,6 +302,7 @@ func (tt *bpfContext) expectedAuditEvent(ver int, ip string, op apievents.Sessio
 		},
 		ServerMetadata: apievents.ServerMetadata{
 			ServerID:        tt.ctx.ServerID,
+			ServerHostname:  tt.ctx.ServerHostname,
 			ServerNamespace: tt.ctx.Namespace,
 		},
 		SessionMetadata: apievents.SessionMetadata{
