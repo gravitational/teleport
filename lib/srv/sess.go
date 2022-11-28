3b5fea2c74ed450cc877214338a240aa74a27d57
Steven Martin <steven@goteleport.com>
2022-11-28 10:24:15 -0800

    Include ssh protocol in start, end audit events
    The SSH protocol was not recorded in the audit events for the start and end.  This prevents other clients from listing the type of protocol correctly.

--- lib/srv/sess.go
+++ lib/srv/sess.go
@@ -673,6 +673,7 @@ func (s *session) emitSessionStartEvent(ctx *ServerContext) {
 		UserMetadata: ctx.Identity.GetUserMetadata(),
 		ConnectionMetadata: apievents.ConnectionMetadata{
 			RemoteAddr: ctx.ServerConn.RemoteAddr().String(),
+			Protocol:   events.EventProtocolSSH,
 		},
 		SessionRecording: ctx.SessionRecordingConfig.GetMode(),
 		InitialCommand:   initialCommand,
@@ -803,6 +804,10 @@ func (s *session) emitSessionEndEvent() {
 			SessionID: string(s.id),
 		},
 		UserMetadata:      ctx.Identity.GetUserMetadata(),
+                ConnectionMetadata: apievents.ConnectionMetadata{
+                        RemoteAddr: ctx.ServerConn.RemoteAddr().String(),
+                        Protocol:   events.EventProtocolSSH,
+                },
 		EnhancedRecording: s.hasEnhancedRecording,
 		Interactive:       s.term != nil,
 		StartTime:         start,
