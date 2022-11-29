b625a48cac22215e54b8f82b30832b555d1d4acd
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2022-11-29 16:28:36 -0500

    Add missing license.

--- lib/auth/keygen/keygen_test.go
+++ lib/auth/keygen/keygen_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2022 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package keygen
 
 import (
@@ -24,13 +40,13 @@ type nativeContext struct {
 	suite *test.AuthSuite
 }
 
-func setupNativeContext(ctx context.Context, t *testing.T) *nativeContext {
+func setupNativeContext(ctx context.Context, _ *testing.T) *nativeContext {
 	var tt nativeContext
 
 	clock := clockwork.NewFakeClockAt(time.Date(2016, 9, 8, 7, 6, 5, 0, time.UTC))
 
 	tt.suite = &test.AuthSuite{
-		A:      New(context.Background(), SetClock(clock)),
+		A:      New(ctx, SetClock(clock)),
 		Keygen: native.GenerateKeyPair,
 		Clock:  clock,
 	}
