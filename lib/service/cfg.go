0aa7a5d6f8d4a9dea5953b8f994e29ddc964ceae
Forrest Marshall <forrest@goteleport.com>
2022-12-02 21:51:13 +0000

    instance heartbeats

--- lib/service/cfg.go
+++ lib/service/cfg.go
@@ -154,7 +154,7 @@ type Config struct {
 	Trust services.Trust
 
 	// Presence service is a discovery and hearbeat tracker
-	Presence services.Presence
+	Presence services.PresenceInternal
 
 	// Events is events service
 	Events types.Events
--- lib/services/presets.go
+++ lib/services/presets.go
@@ -75,6 +75,7 @@ func NewPresetEditorRole() types.Role {
 					types.NewRule(types.KindInstaller, RW()),
 					types.NewRule(types.KindLicense, RO()),
 					types.NewRule(types.KindDownload, RO()),
+					types.NewRule(types.KindInstance, RO()),
 					// Please see defaultAllowRules when adding a new rule.
 				},
 			},
@@ -119,6 +120,7 @@ func NewPresetAccessRole() types.Role {
 						Verbs:     []string{types.VerbRead, types.VerbList},
 						Where:     "contains(session.participants, user.metadata.name)",
 					},
+					types.NewRule(types.KindInstance, RO()),
 					// Please see defaultAllowRules when adding a new rule.
 				},
 			},
