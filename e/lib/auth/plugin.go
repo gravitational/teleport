fd2abd30b1c0f9009a0fed36c6abd54ffc49afe1
Cam Hutchison <camh@goteleport.com>
2022-12-07 10:22:53 +1100

    auth: Remove handler checks (#635)
    Remove the checks for the SAML and OIDC handlers to see if the handlers
    are already installed. The handlers have now been removed from the OSS
    repository, so there is no more double-registration of these handlers.
    
    Source-commit: gravitational/teleport.e@3aa4ec7f09bef26fa602f0257ee547d8261ea4af
    Source-pull-requests: gravitational/teleport.e#635

--- e/lib/auth/plugin.go
+++ e/lib/auth/plugin.go
@@ -148,18 +148,8 @@ func (p *Plugin) RegisterAuthWebHandlers(handler interface{}) error {
 	}
 
 	apiServer.GET("/:version/license/status", httplib.MakeHandler(p.getLicenseCheckResult))
-
-	// Temporary check for the existence of this endpoint in Teleport OSS.
-	// Once removed from there, this check can be removed here.
-	if h, _, _ := apiServer.Lookup("POST", "/:version/saml/requests/validate"); h == nil {
-		apiServer.POST("/:version/saml/requests/validate", apiServer.WithAuth(validateSAMLResponseWeb))
-	}
-
-	// Temporary check for the existence of this endpoint in Teleport OSS.
-	// Once removed from there, this check can be removed here.
-	if h, _, _ := apiServer.Lookup("POST", "/:version/oidc/requests/validate"); h == nil {
-		apiServer.POST("/:version/oidc/requests/validate", apiServer.WithAuth(validateOIDCAuthCallbackWeb))
-	}
+	apiServer.POST("/:version/saml/requests/validate", apiServer.WithAuth(validateSAMLResponseWeb))
+	apiServer.POST("/:version/oidc/requests/validate", apiServer.WithAuth(validateOIDCAuthCallbackWeb))
 
 	return nil
 }
