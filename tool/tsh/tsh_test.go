1263814c714360d9d317dedbcd594d8b558ce2ca
mcbattirola <matheus.battirola@goteleport.com>
2022-12-06 13:46:13 -0300

    Fix test module not implementing module interface

--- tool/tsh/tsh_test.go
+++ tool/tsh/tsh_test.go
@@ -148,6 +148,10 @@ func (p *cliModules) AttestHardwareKey(_ context.Context, _ interface{}, _ keys.
 	return keys.PrivateKeyPolicyNone, nil
 }
 
+func (p *cliModules) EnableRecoveryCodes() {
+	return
+}
+
 func TestAlias(t *testing.T) {
 	testExecutable, err := os.Executable()
 	require.NoError(t, err)
