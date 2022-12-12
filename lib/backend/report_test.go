5dacf719763ed60fca5211ffe9584a18b5ed3941
Lisa Kim <lisa@goteleport.com>
2022-12-12 09:30:47 -0800

    Obscure token ID that's part of the usertoken backend prefix

--- lib/backend/report_test.go
+++ lib/backend/report_test.go
@@ -84,3 +84,17 @@ func TestBuildKeyLabel(t *testing.T) {
 		require.Equal(t, tc.masked, buildKeyLabel(tc.input, sensitivePrefixes))
 	}
 }
+
+func TestBuildLabelKey_SensitiveBackendPrefixes(t *testing.T) {
+	testCases := []struct {
+		input  string
+		masked string
+	}{
+		{"/tokens/1234-5678", "/tokens/******678"},
+		{"/usertoken/1234-5678", "/usertoken/******678"},
+		{"/access_requests/1234-5678", "/access_requests/******678"},
+	}
+	for _, tc := range testCases {
+		require.Equal(t, tc.masked, buildKeyLabel(tc.input, sensitiveBackendPrefixes))
+	}
+}
