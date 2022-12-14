c80855b4484babd68a0254ded42a9e757b46d8ec
Lisa Kim <lisa@goteleport.com>
2022-12-14 09:46:02 -0800

    Add back u2f prefix for backwards comp

--- lib/backend/report.go
+++ lib/backend/report.go
@@ -408,6 +408,7 @@ func buildKeyLabel(key string, sensitivePrefixes []string) string {
 // sensitive values.
 var sensitiveBackendPrefixes = []string{
 	"tokens",
+	"adduseru2fchallenges",
 	"usertoken",
 	// Global passwordless challenges, keyed by challenge, as per
 	// https://github.com/gravitational/teleport/blob/01775b73f138ff124ff0351209d629bb01836869/lib/services/local/users.go#L1510.
