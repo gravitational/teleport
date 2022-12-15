c727912e48ab4bcd2439c381770cb2f031e434d3
Tim Ross <tim.ross@goteleport.com>
2022-12-15 12:15:45 -0500

    Allow custom trace exporter for tsh
    Trace forwarding via `tsh --trace` only works to date if Auth is
    configured with the `tracing_service` enabled. In all other scenarios
    the traces are still forwarded to Auth but are silently dropped.
    This makes it difficult to capture valuable traces from customers
    with latency issues as they are first required to setup a Telemetry
    backend and enable tracing in their cluster.
    
    A new `--trace-exporter` flag is added to `tsh` to make it possible
    to direct traces from `tsh` to a file or local instance of jaeger
    without hacing to modify their Teleport cluster. The url must follow
    the same semantics as the config file equivalent.
    
    One important caveat is that **only** the `tsh` spans will be captured.
    Any corresponding `teleport` spans are exported acording to the
    `tracing_service`. While this only paints half the picture, it is
    still a good indicator of where `tsh` may be experiencing latency.
    
    An example usage to send traces to local files:
    
    ```bash
    tsh --trace --trace-exporter=file:///some/path/traces ssh user@foo
    ```

--- lib/observability/tracing/collector.go
+++ lib/observability/tracing/collector.go
@@ -45,6 +45,7 @@ type Collector struct {
 
 	spanLock sync.RWMutex
 	spans    []*otlp.ScopeSpans
+	exported chan struct{}
 }
 
 // CollectorConfig configures how a Collector should be created.
@@ -54,12 +55,12 @@ type CollectorConfig struct {
 
 // NewCollector creates a new Collector based on the provided config.
 func NewCollector(cfg CollectorConfig) (*Collector, error) {
-	grpcLn, err := net.Listen("tcp4", "")
+	grpcLn, err := net.Listen("tcp4", "127.0.0.1:0")
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
 
-	httpLn, err := net.Listen("tcp4", "")
+	httpLn, err := net.Listen("tcp4", "127.0.0.1:0")
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
@@ -76,6 +77,7 @@ func NewCollector(cfg CollectorConfig) (*Collector, error) {
 		httpLn:     httpLn,
 		grpcServer: grpc.NewServer(grpc.Creds(creds)),
 		tlsConfing: tlsConfig,
+		exported:   make(chan struct{}, 1),
 	}
 
 	c.httpServer = &http.Server{Handler: c, TLSConfig: tlsConfig.Clone()}
@@ -186,14 +188,26 @@ func (c *Collector) Export(ctx context.Context, req *coltracepb.ExportTraceServi
 		c.spans = append(c.spans, span.ScopeSpans...)
 	}
 
+	select {
+	case c.exported <- struct{}{}:
+	default:
+	}
+
 	return &coltracepb.ExportTraceServiceResponse{}, nil
 }
 
+func (c *Collector) WaitForExport() {
+	<-c.exported
+}
+
+// Spans returns all collected spans and resets the collector
 func (c *Collector) Spans() []*otlp.ScopeSpans {
-	c.spanLock.RLock()
-	defer c.spanLock.RUnlock()
+	c.spanLock.Lock()
+	defer c.spanLock.Unlock()
 	spans := make([]*otlp.ScopeSpans, len(c.spans))
 	copy(spans, c.spans)
+
+	c.spans = nil
 
 	return spans
 }
