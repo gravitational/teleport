12975be1e9a9a3d1102bc6c3c006d76a30028c66
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2022-12-15 15:59:40 -0500

    Skip TestBot_Run_CARotation (#19404)

--- lib/tbot/tbot_test.go
+++ lib/tbot/tbot_test.go
@@ -34,7 +34,7 @@ import (
 	libutils "github.com/gravitational/teleport/lib/utils"
 )
 
-func rotate(
+func rotate( //nolint:unused // used in skipped test
 	ctx context.Context, t *testing.T, log logrus.FieldLogger, svc *service.TeleportProcess, phase string,
 ) {
 	t.Helper()
@@ -120,6 +120,9 @@ func setupServerForCARotationTest(ctx context.Context, log utils.Logger, t *test
 // TestCARotation is a heavy integration test that through a rotation, the bot
 // receives credentials for a new CA.
 func TestBot_Run_CARotation(t *testing.T) {
+	// TODO(jakule): Re-enable this test https://github.com/gravitational/teleport/issues/19403
+	t.Skip("Temporary disable until it's fixed - flaky")
+
 	t.Parallel()
 	if testing.Short() {
 		t.Skip("test skipped when -short provided")
