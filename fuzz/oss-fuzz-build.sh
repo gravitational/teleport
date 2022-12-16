78f652d850311980433910acf38c954c6bd16a2d
Mike Jensen <mike.jensen@goteleport.com>
2022-12-16 09:12:26 -0700

    Disable FuzzMongoRead from oss-fuzz due to instability

--- fuzz/oss-fuzz-build.sh
+++ fuzz/oss-fuzz-build.sh
@@ -77,8 +77,9 @@ build_teleport_fuzzers() {
   compile_native_go_fuzzer $TELEPORT_PREFIX/lib/srv/db/sqlserver/protocol \
     FuzzMSSQLLogin fuzz_mssql_login
 
-  compile_native_go_fuzzer $TELEPORT_PREFIX/lib/srv/db/mongodb/protocol \
-   FuzzMongoRead fuzz_mongo_read
+# Disabled until we can update the mongoDB driver
+#  compile_native_go_fuzzer $TELEPORT_PREFIX/lib/srv/db/mongodb/protocol \
+#   FuzzMongoRead fuzz_mongo_read
 
   compile_native_go_fuzzer $TELEPORT_PREFIX/lib/auth/webauthn \
     FuzzParseCredentialCreationResponseBody fuzz_parse_credential_creation_response_body
