42411175d0dc14e09b53a5b8b570e87b12130b2c
Sam Freiberg <sfreiberg@goteleport.com>
2022-12-22 16:14:27 -0600

    Add unit test

--- lib/web/apiserver.go
+++ lib/web/apiserver.go
@@ -3076,11 +3076,11 @@ func (h *Handler) WithLimiter(fn httplib.HandlerFunc) httprouter.Handle {
 // should be used when you need to nest this inside another HandlerFunc.
 func (h *Handler) WithLimiterHandlerFunc(fn httplib.HandlerFunc) httplib.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) (interface{}, error) {
-		host, _, err := net.SplitHostPort(r.RemoteAddr)
+		remote, _, err := net.SplitHostPort(r.RemoteAddr)
 		if err != nil {
 			return nil, trace.Wrap(err)
 		}
-		err = h.limiter.RegisterRequest(host, nil /* customRate */)
+		err = h.limiter.RegisterRequest(remote, nil /* customRate */)
 		// MaxRateError doesn't play well with errors.Is, hence the cast.
 		if _, ok := err.(*ratelimit.MaxRateError); ok {
 			return nil, trace.LimitExceeded(err.Error())
