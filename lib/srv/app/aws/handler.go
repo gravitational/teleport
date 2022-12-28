576988ad8bf0d66a08f2981aa15a6759700b2003
Gavin Frazar <gavin.frazar@goteleport.com>
2022-12-28 08:53:10 -0800

    Remove api error header from backport

--- lib/srv/app/aws/handler.go
+++ lib/srv/app/aws/handler.go
@@ -100,7 +100,6 @@ func NewAWSSignerHandler(ctx context.Context, config SignerHandlerConfig) (http.
 // formatForwardResponseError converts an error to a status code and writes the code to a response.
 func (s *signerHandler) formatForwardResponseError(rw http.ResponseWriter, r *http.Request, err error) {
 	s.Log.WithError(err).Debugf("Failed to process request.")
-	common.SetTeleportAPIErrorHeader(rw, err)
 
 	// Convert trace error type to HTTP and write response.
 	code := trace.ErrorToCode(err)
