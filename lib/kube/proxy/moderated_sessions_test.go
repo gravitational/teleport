43e7e37b524688d85c9e156298d470ee6037e57e
Tiago Silva <tiago.silva@goteleport.com>
2022-12-29 22:49:22 +0000

    Update Kubernetes Dependencies (#19695)
    This PR replaces the following PRs opened by dependabot:
    - #19678
    - #19677
    
    It also bumps:
    
    - Bump k8s.io/api from v0.25.4 to v0.26.0
    - Bump k8s.io/apiextensions-apiserver from v0.25.4 to v0.26.0
    - Bump k8s.io/apimachinery from v0.25.4 to v0.26.0
    - Bump k8s.io/apiserver from v0.25.4 to v0.26.0
    - Bump k8s.io/cli-runtime from v0.25.4 to v0.26.0
    - Bump k8s.io/kubectl from v0.25.4 to v0.26.0
    - Bump k8s.io/utils v0.0.0-20220728103510-ee6ede2d64ed to v0.0.0-20221128185143-99ec85e7a448
    - Bump sigs.k8s.io/controller-runtime from v0.13.1 to v0.14.1

--- lib/kube/proxy/moderated_sessions_test.go
+++ lib/kube/proxy/moderated_sessions_test.go
@@ -415,7 +415,7 @@ func TestModeratedSessions(t *testing.T) {
 					stdoutWritter.Close()
 				}()
 				// start user session.
-				err := exec.Stream(streamOpts)
+				err := exec.StreamWithContext(testCtx.ctx, streamOpts)
 				// ignore closed pipes error.
 				if errors.Is(err, io.ErrClosedPipe) {
 					return nil
