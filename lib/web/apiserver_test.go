5e2ad29d568e28d9b50c86d074af680353cb2ac3
Sam Freiberg <sfreiberg@goteleport.com>
2022-12-30 16:22:20 -0600

    Remove unnecessary addition

--- lib/web/apiserver_test.go
+++ lib/web/apiserver_test.go
@@ -6901,7 +6901,7 @@ func TestWithLimiterHandlerFunc(t *testing.T) {
 		_, err = hf(nil, r, nil)
 		require.NoError(t, err)
 	}
-	r.RemoteAddr = fmt.Sprintf("127.0.0.1:%v", burst+1)
+	r.RemoteAddr = fmt.Sprintf("127.0.0.1:%v", burst)
 	_, err = hf(nil, r, nil)
 	require.Error(t, err)
 	require.True(t, trace.IsLimitExceeded(err))
