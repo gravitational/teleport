1ba330c66d239549316c5186612f974c301dc8a4
joerger <bjoerger@goteleport.com>
2023-01-03 13:26:56 -0800

    split TestMarshalUnmarshalKnownHosts.

--- lib/sshutils/marshal_test.go
+++ lib/sshutils/marshal_test.go
@@ -19,49 +19,51 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-
-	"github.com/gravitational/teleport/lib/auth/native"
 )
 
-func TestMarshalUnmarshalKnownHosts(t *testing.T) {
-	priv, err := native.GeneratePrivateKey()
-	require.NoError(t, err)
-	authorizedKey := priv.MarshalSSHPublicKey()
-	knownHosts := []KnownHost{
-		{
-			AuthorizedKey: authorizedKey,
-			ProxyHost:     "proxy.example.com",
-			Hostname:      "cluster1",
-			Comment: map[string][]string{
-				"logins": {"root"},
-			},
-		}, {
-			AuthorizedKey: authorizedKey,
-			ProxyHost:     "proxy.example.com",
-			Hostname:      "cluster2",
-		}, {
-			AuthorizedKey: authorizedKey,
-			Hostname:      "cluster3",
-		},
-	}
-
-	var knownHostsFile [][]byte
+func TestMarshalKnownHost(t *testing.T) {
+	var file [][]byte
 	for _, kh := range knownHosts {
 		line, err := MarshalKnownHost(kh)
 		require.NoError(t, err)
-		knownHostsFile = append(knownHostsFile, []byte(line))
+		file = append(file, []byte(line))
 	}
+	require.Equal(t, knownHostsFile, file)
+}
 
+func TestUnmarshalKnownHosts(t *testing.T) {
 	parsedKnownHosts, err := UnmarshalKnownHosts(knownHostsFile)
 	require.NoError(t, err)
 	for i, parsed := range parsedKnownHosts {
-		// type comment should default to host
-		if knownHosts[i].Comment == nil {
-			knownHosts[i].Comment = map[string][]string{"type": {"host"}}
+		require.Equal(t, knownHosts[i].AuthorizedKey, parsed.AuthorizedKey)
+		require.Equal(t, knownHosts[i].ProxyHost, parsed.ProxyHost)
+		require.Equal(t, knownHosts[i].Hostname, parsed.Hostname)
+		for key, val := range knownHosts[i].Comment {
+			require.Equal(t, knownHosts[i].Comment[key], val)
 		}
-		if knownHosts[i].Comment["type"] == nil {
-			knownHosts[i].Comment["type"] = []string{"host"}
-		}
-		require.Equal(t, knownHosts[i], parsed)
 	}
+}
+
+var knownHosts = []KnownHost{
+	{
+		AuthorizedKey: []byte("ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEk4cVIiydp9xSPIb8UqXpShY8zPlk/lpR69UL+0+RnNXtQl7GcQUZsrXDB2gOCfj+doKZj8Pt8oQVSDJF/vKhr+KS2Z+LC2Gyt8D5IY/acyyhSN5VoIo0JzIOr5CPGJNpLChREFuveV30hLihSfY52cqSvu7N5u34BlZ29WTLeBD9WssAG5HZUES8Xo3neHBl4SOck+mdiUvOIPhcnPiYRmYltOI3GJRu5y1xGemoPU3MnMziQMqnKCc2+To6IC8CkeQqa8D//BxLjenjSgn1K/SLUHraMb5qCmf77fyshj6A9jamgo0UOaOqem+jyg8idnz6JbVfXwW0nEaSyPzX\n"),
+		ProxyHost:     "proxy.example.com",
+		Hostname:      "cluster1",
+		Comment: map[string][]string{
+			"logins": {"root"},
+		},
+	}, {
+		AuthorizedKey: []byte("ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEk4cVIiydp9xSPIb8UqXpShY8zPlk/lpR69UL+0+RnNXtQl7GcQUZsrXDB2gOCfj+doKZj8Pt8oQVSDJF/vKhr+KS2Z+LC2Gyt8D5IY/acyyhSN5VoIo0JzIOr5CPGJNpLChREFuveV30hLihSfY52cqSvu7N5u34BlZ29WTLeBD9WssAG5HZUES8Xo3neHBl4SOck+mdiUvOIPhcnPiYRmYltOI3GJRu5y1xGemoPU3MnMziQMqnKCc2+To6IC8CkeQqa8D//BxLjenjSgn1K/SLUHraMb5qCmf77fyshj6A9jamgo0UOaOqem+jyg8idnz6JbVfXwW0nEaSyPzX\n"),
+		ProxyHost:     "proxy.example.com",
+		Hostname:      "cluster2",
+	}, {
+		AuthorizedKey: []byte("ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEk4cVIiydp9xSPIb8UqXpShY8zPlk/lpR69UL+0+RnNXtQl7GcQUZsrXDB2gOCfj+doKZj8Pt8oQVSDJF/vKhr+KS2Z+LC2Gyt8D5IY/acyyhSN5VoIo0JzIOr5CPGJNpLChREFuveV30hLihSfY52cqSvu7N5u34BlZ29WTLeBD9WssAG5HZUES8Xo3neHBl4SOck+mdiUvOIPhcnPiYRmYltOI3GJRu5y1xGemoPU3MnMziQMqnKCc2+To6IC8CkeQqa8D//BxLjenjSgn1K/SLUHraMb5qCmf77fyshj6A9jamgo0UOaOqem+jyg8idnz6JbVfXwW0nEaSyPzX\n"),
+		Hostname:      "cluster3",
+	},
+}
+
+var knownHostsFile = [][]byte{
+	[]byte("@cert-authority proxy.example.com,cluster1,*.cluster1 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEk4cVIiydp9xSPIb8UqXpShY8zPlk/lpR69UL+0+RnNXtQl7GcQUZsrXDB2gOCfj+doKZj8Pt8oQVSDJF/vKhr+KS2Z+LC2Gyt8D5IY/acyyhSN5VoIo0JzIOr5CPGJNpLChREFuveV30hLihSfY52cqSvu7N5u34BlZ29WTLeBD9WssAG5HZUES8Xo3neHBl4SOck+mdiUvOIPhcnPiYRmYltOI3GJRu5y1xGemoPU3MnMziQMqnKCc2+To6IC8CkeQqa8D//BxLjenjSgn1K/SLUHraMb5qCmf77fyshj6A9jamgo0UOaOqem+jyg8idnz6JbVfXwW0nEaSyPzX logins=root&type=host\n"),
+	[]byte("@cert-authority proxy.example.com,cluster2,*.cluster2 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEk4cVIiydp9xSPIb8UqXpShY8zPlk/lpR69UL+0+RnNXtQl7GcQUZsrXDB2gOCfj+doKZj8Pt8oQVSDJF/vKhr+KS2Z+LC2Gyt8D5IY/acyyhSN5VoIo0JzIOr5CPGJNpLChREFuveV30hLihSfY52cqSvu7N5u34BlZ29WTLeBD9WssAG5HZUES8Xo3neHBl4SOck+mdiUvOIPhcnPiYRmYltOI3GJRu5y1xGemoPU3MnMziQMqnKCc2+To6IC8CkeQqa8D//BxLjenjSgn1K/SLUHraMb5qCmf77fyshj6A9jamgo0UOaOqem+jyg8idnz6JbVfXwW0nEaSyPzX type=host\n"),
+	[]byte("@cert-authority cluster3,*.cluster3 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEk4cVIiydp9xSPIb8UqXpShY8zPlk/lpR69UL+0+RnNXtQl7GcQUZsrXDB2gOCfj+doKZj8Pt8oQVSDJF/vKhr+KS2Z+LC2Gyt8D5IY/acyyhSN5VoIo0JzIOr5CPGJNpLChREFuveV30hLihSfY52cqSvu7N5u34BlZ29WTLeBD9WssAG5HZUES8Xo3neHBl4SOck+mdiUvOIPhcnPiYRmYltOI3GJRu5y1xGemoPU3MnMziQMqnKCc2+To6IC8CkeQqa8D//BxLjenjSgn1K/SLUHraMb5qCmf77fyshj6A9jamgo0UOaOqem+jyg8idnz6JbVfXwW0nEaSyPzX type=host\n"),
 }
