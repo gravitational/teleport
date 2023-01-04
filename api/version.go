3a7f2854d3daef74816e0f6190cd0024ea19a58d
Jeff Pihach <jeff.pihach@goteleport.com>
2023-01-04 14:57:02 -0600

    bump version

--- api/version.go
+++ api/version.go
@@ -3,7 +3,7 @@
 package api
 
 const (
-	Version = "12.0.0-dev.hatchmerge.10"
+	Version = "12.0.0-dev.hatchmerge.11"
 )
 
 // Gitref variable is automatically set to the output of git-describe
--- version.go
+++ version.go
@@ -3,7 +3,7 @@
 package teleport
 
 const (
-	Version = "12.0.0-dev.hatchmerge.10"
+	Version = "12.0.0-dev.hatchmerge.11"
 )
 
 // Gitref variable is automatically set to the output of git-describe
