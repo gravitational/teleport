7851f7df26b704385bf29d7f78bd3ae80e9f1ae1
Vitor Enes <vitor.duarte@goteleport.com>
2023-01-04 18:57:27 +0000

    Fix typo

--- lib/utils/parse/ast.go
+++ lib/utils/parse/ast.go
@@ -5,8 +5,6 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -401,7 +399,7 @@ func buildRegexpReplaceExpr(args []Expr) (Expr, error) {
 	replacement, ok := args[2].(*StringLitExpr)
 	if !ok {
 		return nil, trace.BadParameter(
-			"found function \"%s.%s\" with 2nd argument that is not a string",
+			"found function \"%s.%s\" with 3rd argument that is not a string",
 			RegexpNamespace,
 			RegexpReplaceFnName,
 		)
