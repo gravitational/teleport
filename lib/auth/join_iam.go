gravitational/teleport

commit 0433a9d5cb8ed7cf0eb9d498f52efcbcc3dd5eda

Add InstanceJoin/BotJoin audit events (#19044)
* Introduce NodeJoin/BotJoin events with GH support

* Remove Node -> Instance for clarity

* Add CircleCI join audit attributes

* Tidy implementation

* More tidying of implementation

* Add tests for audit event emission

* Appease linter with AmericaniZed spellings

* Fix imports

* Fix missing errcheck in Join test

* Update token safe name to omit entire token for shorter tokens

lib/auth/join_iam.go
@@ -414,7 +414,7 @@ func (a *Server) RegisterUsingIAMMethod(ctx context.Context, challengeResponse c
 		return nil, trace.Wrap(err)
 	}
 
-	certs, err := a.generateCerts(ctx, provisionToken, req.RegisterUsingTokenRequest)
+	certs, err := a.generateCerts(ctx, provisionToken, req.RegisterUsingTokenRequest, nil)
 	return certs, trace.Wrap(err)
 }
 

api/proto/teleport/legacy/types/events/events.proto
@@ -2957,7 +2957,7 @@ message CertificateCreate {
 }
 
 // RenewableCertificateGenerationMismatch is emitted when a renewable
-// certificiate's generation counter fails to validate, possibly indicating a
+// certificate's generation counter fails to validate, possibly indicating a
 // stolen certificate and an invalid renewal attempt.
 message RenewableCertificateGenerationMismatch {
   // Metadata is a common event metadata.
@@ -2972,6 +2972,65 @@ message RenewableCertificateGenerationMismatch {
     (gogoproto.nullable) = false,
     (gogoproto.embed) = true,
     (gogoproto.jsontag) = ""
+  ];
+}
+
+// BotJoin records a bot join event.
+message BotJoin {
+  // Metadata is a common event metadata.
+  Metadata Metadata = 1 [
+    (gogoproto.nullable) = false,
+    (gogoproto.embed) = true,
+    (gogoproto.jsontag) = ""
+  ];
+  // Status contains common command or operation status fields.
+  Status Status = 2 [
+    (gogoproto.nullable) = false,
+    (gogoproto.embed) = true,
+    (gogoproto.jsontag) = ""
+  ];
+  // BotName is the name of the bot which has joined.
+  string BotName = 3 [(gogoproto.jsontag) = "bot_name,omitempty"];
+  // Method is the event field indicating what join method was used.
+  string Method = 4 [(gogoproto.jsontag) = "method,omitempty"];
+  // TokenName is the name of the provision token used to join.
+  string TokenName = 5 [(gogoproto.jsontag) = "token_name,omitempty"];
+  // Attributes is a map of attributes received from the join method provider.
+  google.protobuf.Struct Attributes = 6 [
+    (gogoproto.jsontag) = "attributes,omitempty",
+    (gogoproto.casttype) = "Struct"
+  ];
+}
+
+// InstanceJoin records an instance join event.
+message InstanceJoin {
+  // Metadata is a common event metadata.
+  Metadata Metadata = 1 [
+    (gogoproto.nullable) = false,
+    (gogoproto.embed) = true,
+    (gogoproto.jsontag) = ""
+  ];
+  // Status contains common command or operation status fields.
+  Status Status = 2 [
+    (gogoproto.nullable) = false,
+    (gogoproto.embed) = true,
+    (gogoproto.jsontag) = ""
+  ];
+  // HostID is the unique host ID of the instance which attempted to join.
+  string HostID = 3 [(gogoproto.jsontag) = "host_id,omitempty"];
+  // NodeName is the name of the instance which attempted to join.
+  string NodeName = 4 [(gogoproto.jsontag) = "node_name,omitempty"];
+  // Role is the role that the node requested when attempting to join.
+  string Role = 5 [(gogoproto.jsontag) = "role,omitempty"];
+  // Method is the event field indicating what join method was used.
+  string Method = 6 [(gogoproto.jsontag) = "method,omitempty"];
+  // TokenName is the name of the token used to join. This will be omitted
+  // for the 'token' join method where the token name is a secret value.
+  string TokenName = 7 [(gogoproto.jsontag) = "token_name,omitempty"];
+  // Attributes is a map of attributes received from the join method provider.
+  google.protobuf.Struct Attributes = 8 [
+    (gogoproto.jsontag) = "attributes,omitempty",
+    (gogoproto.casttype) = "Struct"
   ];
 }
 
@@ -3106,6 +3165,8 @@ message OneOf {
     events.DesktopSharedDirectoryRead DesktopSharedDirectoryRead = 107;
     events.DesktopSharedDirectoryWrite DesktopSharedDirectoryWrite = 108;
     events.DynamoDBRequest DynamoDBRequest = 109;
+    events.BotJoin BotJoin = 110;
+    events.InstanceJoin InstanceJoin = 111;
   }
 }
 

api/types/events/oneof.go
@@ -471,6 +471,14 @@ func ToOneOf(in AuditEvent) (*OneOf, error) {
 		out.Event = &OneOf_DesktopSharedDirectoryWrite{
 			DesktopSharedDirectoryWrite: e,
 		}
+	case *BotJoin:
+		out.Event = &OneOf_BotJoin{
+			BotJoin: e,
+		}
+	case *InstanceJoin:
+		out.Event = &OneOf_InstanceJoin{
+			InstanceJoin: e,
+		}
 	default:
 		log.Errorf("Attempted to convert dynamic event of unknown type \"%v\" into protobuf event.", in.GetType())
 		unknown := &Unknown{}

api/types/provisioning.go
@@ -105,6 +105,11 @@ type ProvisionToken interface {
 	V1() *ProvisionTokenV1
 	// String returns user friendly representation of the resource
 	String() string
+
+	// GetSafeName returns the name of the token, sanitized appropriately for
+	// join methods where the name is secret. This should be used when logging
+	// the token name.
+	GetSafeName() string
 }
 
 // NewProvisionToken returns a new provision token with the given roles.
@@ -363,14 +368,37 @@ func (p *ProvisionTokenV2) Expiry() time.Time {
 	return p.Metadata.Expiry()
 }
 
-// GetName returns server name
+// GetName returns the name of the provision token. This value can be secret!
+// Use GetSafeName where the name may be logged.
 func (p *ProvisionTokenV2) GetName() string {
 	return p.Metadata.Name
 }
 
-// SetName sets the name of the TrustedCluster.
+// SetName sets the name of the provision token.
 func (p *ProvisionTokenV2) SetName(e string) {
 	p.Metadata.Name = e
+}
+
+// GetSafeName returns the name of the token, sanitized appropriately for
+// join methods where the name is secret. This should be used when logging
+// the token name.
+func (p *ProvisionTokenV2) GetSafeName() string {
+	name := p.GetName()
+	if p.GetJoinMethod() != JoinMethodToken {
+		return name
+	}
+
+	// If the token name is short, we just blank the whole thing.
+	if len(name) < 16 {
+		return strings.Repeat("*", len(name))
+	}
+
+	// If the token name is longer, we can show the last 25% of it to help
+	// the operator identify it.
+	hiddenBefore := int(0.75 * float64(len(name)))
+	name = name[hiddenBefore:]
+	name = strings.Repeat("*", hiddenBefore) + name
+	return name
 }
 
 // String returns the human readable representation of a provisioning token.

api/types/provisioning_test.go
@@ -404,3 +404,34 @@ func TestProvisionTokenV2_CheckAndSetDefaults(t *testing.T) {
 		})
 	}
 }
+
+func TestProvisionTokenV2_GetSafeName(t *testing.T) {
+	t.Run("token join method (short)", func(t *testing.T) {
+		tok, err := NewProvisionToken("1234", []SystemRole{RoleNode}, time.Now())
+		require.NoError(t, err)
+		got := tok.GetSafeName()
+		require.Equal(t, "****", got)
+	})
+	t.Run("token join method (long)", func(t *testing.T) {
+		tok, err := NewProvisionToken("0123456789abcdef", []SystemRole{RoleNode}, time.Now())
+		require.NoError(t, err)
+		got := tok.GetSafeName()
+		require.Equal(t, "************cdef", got)
+	})
+	t.Run("non-token join method", func(t *testing.T) {
+		tok, err := NewProvisionTokenFromSpec("12345678", time.Now(), ProvisionTokenSpecV2{
+			Roles:      []SystemRole{RoleNode},
+			JoinMethod: JoinMethodKubernetes,
+			Kubernetes: &ProvisionTokenSpecV2Kubernetes{
+				Allow: []*ProvisionTokenSpecV2Kubernetes_Rule{
+					{
+						ServiceAccount: "namespace:my-service-account",
+					},
+				},
+			},
+		})
+		require.NoError(t, err)
+		got := tok.GetSafeName()
+		require.Equal(t, "12345678", got)
+	})
+}

lib/auth/join.go
@@ -25,8 +25,9 @@ import (
 
 	"github.com/gravitational/teleport/api/client/proto"
 	"github.com/gravitational/teleport/api/types"
-	"github.com/gravitational/teleport/lib/backend"
+	apievents "github.com/gravitational/teleport/api/types/events"
 	"github.com/gravitational/teleport/lib/defaults"
+	"github.com/gravitational/teleport/lib/events"
 )
 
 // tokenJoinMethod returns the join method of the token with the given tokenName
@@ -82,6 +83,12 @@ func (a *Server) checkTokenJoinRequestCommon(ctx context.Context, req *types.Reg
 	return provisionToken, nil
 }
 
+type joinAttributeSourcer interface {
+	// JoinAuditAttributes returns a series of attributes that can be inserted into
+	// audit events related to a specific join.
+	JoinAuditAttributes() (map[string]interface{}, error)
+}
+
 // RegisterUsingToken returns credentials for a new node to join the Teleport
 // cluster using a previously issued token.
 //
@@ -99,6 +106,7 @@ func (a *Server) RegisterUsingToken(ctx context.Context, req *types.RegisterUsin
 		return nil, trace.Wrap(err)
 	}
 
+	var joinAttributeSrc joinAttributeSourcer
 	switch a.tokenJoinMethod(ctx, req.Token) {
 	case types.JoinMethodEC2:
 		if err := a.checkEC2JoinRequest(ctx, req); err != nil {
@@ -110,13 +118,17 @@ func (a *Server) RegisterUsingToken(ctx context.Context, req *types.RegisterUsin
 			"join method but the node has connected to the wrong endpoint, make " +
 			"sure your node is configured to use the IAM join method")
 	case types.JoinMethodGitHub:
-		if err := a.checkGitHubJoinRequest(ctx, req); err != nil {
+		claims, err := a.checkGitHubJoinRequest(ctx, req)
+		if err != nil {
 			return nil, trace.Wrap(err)
 		}
+		joinAttributeSrc = claims
 	case types.JoinMethodCircleCI:
-		if err := a.checkCircleCIJoinRequest(ctx, req); err != nil {
+		claims, err := a.checkCircleCIJoinRequest(ctx, req)
+		if err != nil {
 			return nil, trace.Wrap(err)
 		}
+		joinAttributeSrc = claims
 	case types.JoinMethodKubernetes:
 		if err := a.checkKubernetesJoinRequest(ctx, req); err != nil {
 			return nil, trace.Wrap(err)
@@ -136,63 +148,106 @@ func (a *Server) RegisterUsingToken(ctx context.Context, req *types.RegisterUsin
 		return nil, trace.Wrap(err)
 	}
 
-	certs, err := a.generateCerts(ctx, provisionToken, req)
+	// With all elements of the token validated, we can now generate & return
+	// certificates.
+	if req.Role == types.RoleBot {
+		certs, err := a.generateCertsBot(ctx, provisionToken, req, joinAttributeSrc)
+		return certs, trace.Wrap(err)
+	}
+	certs, err := a.generateCerts(ctx, provisionToken, req, joinAttributeSrc)
 	return certs, trace.Wrap(err)
 }
 
-func (a *Server) generateCerts(ctx context.Context, provisionToken types.ProvisionToken, req *types.RegisterUsingTokenRequest) (*proto.Certs, error) {
-	if req.Role == types.RoleBot {
-		// bots use this endpoint but get a user cert
-		// botResourceName must be set, enforced in CheckAndSetDefaults
-		botName := provisionToken.GetBotName()
+func (a *Server) generateCertsBot(
+	ctx context.Context,
+	provisionToken types.ProvisionToken,
+	req *types.RegisterUsingTokenRequest,
+	joinAttributeSrc joinAttributeSourcer,
+) (*proto.Certs, error) {
+	// bots use this endpoint but get a user cert
+	// botResourceName must be set, enforced in CheckAndSetDefaults
+	botName := provisionToken.GetBotName()
+	joinMethod := provisionToken.GetJoinMethod()
+	// Append `bot-` to the bot name to derive its username.
+	botResourceName := BotResourceName(botName)
 
-		// Append `bot-` to the bot name to derive its username.
-		botResourceName := BotResourceName(botName)
-		expires := a.GetClock().Now().Add(defaults.DefaultRenewableCertTTL)
-		if req.Expires != nil {
-			expires = *req.Expires
-		}
+	expires := a.GetClock().Now().Add(defaults.DefaultRenewableCertTTL)
+	if req.Expires != nil {
+		expires = *req.Expires
+	}
 
-		joinMethod := provisionToken.GetJoinMethod()
+	// Repeatable join methods (e.g IAM) should not produce renewable
+	// certificates. Ephemeral join methods (e.g Token) should produce
+	// renewable certificates, but the token should be deleted after use.
+	var renewable bool
+	var shouldDeleteToken bool
+	switch joinMethod {
+	case types.JoinMethodToken:
+		shouldDeleteToken = true
+		renewable = true
+	case types.JoinMethodIAM,
+		types.JoinMethodGitHub,
+		types.JoinMethodCircleCI,
+		types.JoinMethodKubernetes:
+		shouldDeleteToken = false
+		renewable = false
+	default:
+		return nil, trace.BadParameter(
+			"unsupported join method %q for bot", joinMethod,
+		)
+	}
+	certs, err := a.generateInitialBotCerts(
+		ctx, botResourceName, req.PublicSSHKey, expires, renewable,
+	)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
 
-		// Repeatable join methods (e.g IAM) should not produce renewable
-		// certificates. Ephemeral join methods (e.g Token) should produce
-		// renewable certificates, but the token should be deleted after use.
-		var renewable bool
-		var shouldDeleteToken bool
-		switch joinMethod {
-		case types.JoinMethodToken:
-			shouldDeleteToken = true
-			renewable = true
-		case types.JoinMethodIAM,
-			types.JoinMethodGitHub,
-			types.JoinMethodCircleCI,
-			types.JoinMethodKubernetes:
-			shouldDeleteToken = false
-			renewable = false
-		default:
-			return nil, trace.BadParameter(
-				"unsupported join method %q for bot", joinMethod,
+	if shouldDeleteToken {
+		// delete ephemeral bot join tokens so they can't be re-used
+		if err := a.DeleteToken(ctx, provisionToken.GetName()); err != nil {
+			log.WithError(err).Warnf("Could not delete bot provision token %q after generating certs",
+				provisionToken.GetSafeName(),
 			)
 		}
-		certs, err := a.generateInitialBotCerts(
-			ctx, botResourceName, req.PublicSSHKey, expires, renewable,
-		)
-		if err != nil {
-			return nil, trace.Wrap(err)
-		}
-
-		if shouldDeleteToken {
-			// delete ephemeral bot join tokens so they can't be re-used
-			if err := a.DeleteToken(ctx, provisionToken.GetName()); err != nil {
-				log.WithError(err).Warnf("Could not delete bot provision token %q after generating certs",
-					string(backend.MaskKeyName(provisionToken.GetName())))
-			}
-		}
-
-		log.Infof("Bot %q has joined the cluster.", botName)
-		return certs, nil
 	}
+
+	// Emit audit event for bot join.
+	log.Infof("Bot %q has joined the cluster.", botName)
+	joinEvent := &apievents.BotJoin{
+		Metadata: apievents.Metadata{
+			Type: events.BotJoinEvent,
+			Code: events.BotJoinCode,
+		},
+		Status: apievents.Status{
+			Success: true,
+		},
+		BotName:   provisionToken.GetBotName(),
+		Method:    string(joinMethod),
+		TokenName: provisionToken.GetSafeName(),
+	}
+	if joinAttributeSrc != nil {
+		attributes, err := joinAttributeSrc.JoinAuditAttributes()
+		if err != nil {
+			log.WithError(err).Warn("Unable to fetch join attributes from join method.")
+		}
+		joinEvent.Attributes, err = apievents.EncodeMap(attributes)
+		if err != nil {
+			log.WithError(err).Warn("Unable to encode join attributes for audit event.")
+		}
+	}
+	if err := a.emitter.EmitAuditEvent(ctx, joinEvent); err != nil {
+		log.WithError(err).Warn("Failed to emit bot join event.")
+	}
+	return certs, nil
+}
+
+func (a *Server) generateCerts(
+	ctx context.Context,
+	provisionToken types.ProvisionToken,
+	req *types.RegisterUsingTokenRequest,
+	joinAttributeSrc joinAttributeSourcer,
+) (*proto.Certs, error) {
 	if req.Expires != nil {
 		return nil, trace.BadParameter("'expires' cannot be set on join for non-bot certificates")
 	}
@@ -226,6 +281,35 @@ func (a *Server) generateCerts(ctx context.Context, provisionToken types.Provisi
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
+
+	// Emit audit event
 	log.Infof("Node %q [%v] has joined the cluster.", req.NodeName, req.HostID)
+	joinEvent := &apievents.InstanceJoin{
+		Metadata: apievents.Metadata{
+			Type: events.InstanceJoinEvent,
+			Code: events.InstanceJoinCode,
+		},
+		Status: apievents.Status{
+			Success: true,
+		},
+		NodeName:  req.NodeName,
+		Role:      string(req.Role),
+		Method:    string(provisionToken.GetJoinMethod()),
+		TokenName: provisionToken.GetSafeName(),
+		HostID:    req.HostID,
+	}
+	if joinAttributeSrc != nil {
+		attributes, err := joinAttributeSrc.JoinAuditAttributes()
+		if err != nil {
+			log.WithError(err).Warn("Unable to fetch join attributes from join method.")
+		}
+		joinEvent.Attributes, err = apievents.EncodeMap(attributes)
+		if err != nil {
+			log.WithError(err).Warn("Unable to encode join attributes for audit event.")
+		}
+	}
+	if err := a.emitter.EmitAuditEvent(ctx, joinEvent); err != nil {
+		log.WithError(err).Warn("Failed to emit instance join event.")
+	}
 	return certs, nil
 }

lib/auth/join_circleci.go
@@ -26,17 +26,17 @@ import (
 	"github.com/gravitational/teleport/lib/circleci"
 )
 
-func (a *Server) checkCircleCIJoinRequest(ctx context.Context, req *types.RegisterUsingTokenRequest) error {
+func (a *Server) checkCircleCIJoinRequest(ctx context.Context, req *types.RegisterUsingTokenRequest) (*circleci.IDTokenClaims, error) {
 	if req.IDToken == "" {
-		return trace.BadParameter("IDToken not provided for %q join request", types.JoinMethodCircleCI)
+		return nil, trace.BadParameter("IDToken not provided for %q join request", types.JoinMethodCircleCI)
 	}
 	pt, err := a.GetToken(ctx, req.Token)
 	if err != nil {
-		return trace.Wrap(err)
+		return nil, trace.Wrap(err)
 	}
 	token, ok := pt.(*types.ProvisionTokenV2)
 	if !ok {
-		return trace.BadParameter("%q join method only support ProvisionTokenV2, '%T' was provided", types.JoinMethodCircleCI, pt)
+		return nil, trace.BadParameter("%q join method only support ProvisionTokenV2, '%T' was provided", types.JoinMethodCircleCI, pt)
 	}
 
 	claims, err := a.circleCITokenValidate(
@@ -45,10 +45,10 @@ func (a *Server) checkCircleCIJoinRequest(ctx context.Context, req *types.Regist
 		req.IDToken,
 	)
 	if err != nil {
-		return trace.Wrap(err)
+		return nil, trace.Wrap(err)
 	}
 
-	return trace.Wrap(checkCircleCIAllowRules(token, claims))
+	return claims, trace.Wrap(checkCircleCIAllowRules(token, claims))
 }
 
 func checkCircleCIAllowRules(token *types.ProvisionTokenV2, claims *circleci.IDTokenClaims) error {

commit c78107c2e98c9fd60b9a217cb14f8e125c92311e

Fix bot IAM joining
This fixes a small regression in #19044 that broke IAM-based joining
for bots. Bots now have slightly different joining logic, but IAM
joining uses a different entrypoint that wasn't updated to reflect
this.

lib/auth/join_iam.go
@@ -414,6 +414,10 @@ func (a *Server) RegisterUsingIAMMethod(ctx context.Context, challengeResponse c
 		return nil, trace.Wrap(err)
 	}
 
+	if req.RegisterUsingTokenRequest.Role == types.RoleBot {
+		certs, err := a.generateCertsBot(ctx, provisionToken, req.RegisterUsingTokenRequest, nil)
+		return certs, trace.Wrap(err)
+	}
 	certs, err := a.generateCerts(ctx, provisionToken, req.RegisterUsingTokenRequest, nil)
 	return certs, trace.Wrap(err)
 }