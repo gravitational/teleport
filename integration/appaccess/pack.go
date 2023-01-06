fcacf0732b3e243d5862128bcabfa04a04a81299
Gabriel Corado <gabriel.oliveira@goteleport.com>
2023-01-06 18:00:18 -0300

    test(appaccess): delete app sessions on test cleanup

--- integration/appaccess/pack.go
+++ integration/appaccess/pack.go
@@ -286,6 +286,17 @@ func (p *Pack) CreateAppSession(t *testing.T, publicAddr, clusterName string) []
 	err = json.Unmarshal(body, &casResp)
 	require.NoError(t, err)
 
+	// Drop the session on cleanup. This helps maintaining the number of app
+	// sessions low. Having a large number of sessions might impact time
+	// sensitive tests.
+	t.Cleanup(func() {
+		// Do not assert the error because some tests might directly delete the
+		// session causing it to return a session not found error.
+		_ = p.rootCluster.Process.GetAuthServer().DeleteAppSession(context.Background(), types.DeleteAppSessionRequest{
+			SessionID: casResp.CookieValue,
+		})
+	})
+
 	return []*http.Cookie{
 		{
 			Name:  app.CookieName,
