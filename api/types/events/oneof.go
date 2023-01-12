33b546c50be619d8c8791aa44506d23d0305c922
Lisa Kim <lisa@goteleport.com>
2023-01-12 13:37:17 -0800

    Add event type conversion

--- api/types/events/oneof.go
+++ api/types/events/oneof.go
@@ -275,6 +275,10 @@ func ToOneOf(in AuditEvent) (*OneOf, error) {
 		out.Event = &OneOf_MFADeviceDelete{
 			MFADeviceDelete: e,
 		}
+	case *DeviceTrust:
+		out.Event = &OneOf_DeviceTrust{
+			DeviceTrust: e,
+		}
 	case *BillingCardCreate:
 		out.Event = &OneOf_BillingCardCreate{
 			BillingCardCreate: e,
