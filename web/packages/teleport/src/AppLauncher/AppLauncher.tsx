260c3526f7719e6b2703c05f4343db0f1a18ac14
Ryan Clark <ryan.clark@goteleport.com>
2023-01-12 22:51:51 +0000

    Change the application access authentication flow (#1515)

--- web/packages/teleport/src/AppLauncher/AppLauncher.tsx
+++ web/packages/teleport/src/AppLauncher/AppLauncher.tsx
@@ -14,25 +14,93 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
+
+import { useLocation, useParams } from 'react-router';
+
 import { Flex, Indicator } from 'design';
+
 import { AccessDenied } from 'design/CardError';
 
-import useAppLauncher from './useAppLauncher';
+import useAttempt from 'shared/hooks/useAttemptNext';
 
-export default function Container() {
-  const state = useAppLauncher();
-  return <AppLauncher {...state} />;
-}
+import { UrlLauncherParams } from 'teleport/config';
+import service from 'teleport/services/apps';
 
-export function AppLauncher(props: ReturnType<typeof useAppLauncher>) {
-  if (props.status === 'failed') {
-    return <AccessDenied message={props.statusText} />;
+export function AppLauncher() {
+  const { attempt, setAttempt } = useAttempt('processing');
+
+  const params = useParams<UrlLauncherParams>();
+  const { search } = useLocation();
+  const queryParams = new URLSearchParams(search);
+
+  const createAppSession = useCallback(async (params: UrlLauncherParams) => {
+    try {
+      let fqdn = params.fqdn;
+      if (!fqdn) {
+        const app = await service.getAppFqdn(params);
+
+        fqdn = app.fqdn;
+      }
+
+      const port = location.port ? `:${location.port}` : '';
+      const session = await service.createAppSession(params);
+
+      await fetch(`https://${fqdn}${port}/x-teleport-auth`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'X-Cookie-Value': session.cookieValue,
+          'X-Subject-Cookie-Value': session.subjectCookieValue,
+        },
+      });
+
+      let path = '';
+      if (queryParams.has('path')) {
+        path = decodeURIComponent(queryParams.get('path'));
+
+        if (!path.startsWith('/')) {
+          path = `/${path}`;
+        }
+      }
+
+      window.location.replace(`https://${fqdn}${port}${path}`);
+    } catch (err) {
+      let statusText = 'Something went wrong';
+      if (err instanceof Error) {
+        statusText = err.message;
+      }
+
+      setAttempt({
+        status: 'failed',
+        statusText,
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    createAppSession(params);
+  }, [params]);
+
+  if (attempt.status === 'failed') {
+    return <AppLauncherAccessDenied statusText={attempt.statusText} />;
   }
 
+  return <AppLauncherProcessing />;
+}
+
+export function AppLauncherProcessing() {
   return (
     <Flex height="180px" justifyContent="center" alignItems="center" flex="1">
       <Indicator />
     </Flex>
   );
+}
+
+interface AppLauncherAccessDeniedProps {
+  statusText: string;
+}
+
+export function AppLauncherAccessDenied(props: AppLauncherAccessDeniedProps) {
+  return <AccessDenied message={props.statusText} />;
 }
--- web/packages/teleport/src/AppLauncher/index.ts
+++ web/packages/teleport/src/AppLauncher/index.ts
@@ -14,5 +14,4 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import AppLauncher from './AppLauncher';
-export default AppLauncher;
+export { AppLauncher as default } from './AppLauncher';
