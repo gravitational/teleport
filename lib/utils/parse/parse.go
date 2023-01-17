ae1ce37dda027045b873d57d7d45b7c89e109085
Vitor Enes <vitor.duarte@goteleport.com>
2023-01-17 11:48:09 +0000

    Remove TODO

--- lib/utils/parse/parse.go
+++ lib/utils/parse/parse.go
@@ -13,9 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
-// TODO(awly): combine Expression and Matcher. It should be possible to write:
-// `{{regexp.match(email.local(external.trait_name))}}`
 package parse
 
 import (
