273795cb52486bffbe90bfa8ecc6a583ff36cc1f
Mike Wilson <michael.wilson@goteleport.com>
2023-01-20 11:28:38 -0500

    Conditionally build the UI if there are changes.
    `ensure-webassets` and `ensure-webassets-e` will now build the UI if the
    hash-of-hashes has changed since the last time the webassets were built.

---
+++ build.assets/build-webassets-if-changed.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Rebuilds a UI make target based on whether the given directories contents have changed
+# minus any node-modules.
+#
+
+set -eo pipefail
+
+ROOT_PATH="$(realpath "$(dirname "$0")/..")"
+MAKE="${MAKE:-make}"
+
+if [ "$#" -lt 4 ]; then
+  echo "Usage: $0 <type> <last-sha-file> <build-target> <directories...>"
+  exit 1
+fi
+
+TYPE="$1"
+LAST_SHA_FILE="$ROOT_PATH/$2"
+BUILD_TARGET="$3"
+shift 3
+SRC_DIRECTORIES=("$@")
+
+# Calculate the current hash-of-hashes of the given source directories. Adds in yarn.lock and package.json as well.
+# This excludes node_modules, as the yarn.lock/package.json differences should handle this.
+CURRENT_SHA="$( (for dir in "${SRC_DIRECTORIES[@]}"; do
+  find "$ROOT_PATH/$dir" -type f
+done && echo "$ROOT_PATH/yarn.lock" && echo "$ROOT_PATH/package.json") | 
+  grep -v "node_modules" | xargs -I{} -n1 -P8 sha256sum {} | sort -k 2 | sha256sum |
+  cut -f1 -d' ')"
+
+BUILD=true
+
+# If the LAST_SHA_FILE exists, test whether it's equivalent to the current calculated SHA. If it is,
+# set BUILD to false.
+if [ -f "$LAST_SHA_FILE" ]; then
+  LAST_SHA="$(cat "$LAST_SHA_FILE")"
+  if [ "$LAST_SHA" = "$CURRENT_SHA" ]; then
+    BUILD=false
+  fi
+fi
+
+# If BUILD is true, make the build target. This assumes using the root Makefile.
+if [ "$BUILD" = "true" ]; then \
+  "$MAKE" -C "$ROOT_PATH" "$BUILD_TARGET"; \
+  # Record the current SHA into the LAST_SHA_FILE. The make target is expected to have
+  # created any necessary directories here.
+  echo "$CURRENT_SHA" > "$LAST_SHA_FILE"
+  echo "$TYPE webassets successfully updated."
+else
+  echo "$TYPE webassets up to date."
+fi
