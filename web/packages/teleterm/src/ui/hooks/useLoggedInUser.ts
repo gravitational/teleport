2e0c29164719a04a46136a49a777fa99eb35f095
Rafał Cieślak <rafal.cieslak@goteleport.com>
2023-01-24 12:51:06 +0100

    Reword comments

--- web/packages/teleterm/src/ui/hooks/useLoggedInUser.ts
+++ web/packages/teleterm/src/ui/hooks/useLoggedInUser.ts
@@ -3,8 +3,8 @@ import { useWorkspaceContext } from 'teleterm/ui/Documents';
 import { LoggedInUser } from 'teleterm/services/tshd/types';
 
 /**
- * useLoggedInUser returns the logged in user of the root cluster of the currently active workspace.
- * The return value changes depending on the active workspace.
+ * useLoggedInUser returns the user logged into the root cluster of the active workspace. The return
+ * value changes depending on the active workspace.
  *
  * It should be used within components that reside outside of WorkspaceContext, typically anything
  * that's outside of Document-type components.
@@ -27,9 +27,8 @@ export function useLoggedInUser(): LoggedInUser | undefined {
 }
 
 /**
- * useWorkspaceLoggedInUser returns the logged in user of the root cluster of the workspace
- * specified by WorkspaceContext. The returned value won't change when the UI switches between
- * workspaces.
+ * useWorkspaceLoggedInUser returns the user logged into the root cluster of the workspace specified
+ * by WorkspaceContext. The returned value won't change when the UI switches between workspaces.
  *
  * It should be used for components which are bound to a particular workspace and which don't change
  * their workspace over their lifecycle; typically those are Document-type components and anything
