cd2e796023ea807b1e9fc7504f0911635d1c8b45
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2023-01-27 12:35:28 +0100

    Add integration/teleterm_test with tests for get* and list* clusters
    Co-authored-by: Rafał Cieślak <rafal.cieslak@goteleport.com>

---
+++ integration/teleterm_test.go
@@ -0,0 +1,208 @@
+package integration
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+
+	"github.com/gravitational/teleport/api/types"
+	dbhelpers "github.com/gravitational/teleport/integration/db"
+	"github.com/gravitational/teleport/integration/helpers"
+	"github.com/gravitational/teleport/lib/client"
+	"github.com/gravitational/teleport/lib/service"
+	api "github.com/gravitational/teleport/lib/teleterm/api/protogen/golang/v1"
+	"github.com/gravitational/teleport/lib/teleterm/api/uri"
+	"github.com/gravitational/teleport/lib/teleterm/apiserver/handler"
+	"github.com/gravitational/teleport/lib/teleterm/clusters"
+	"github.com/gravitational/teleport/lib/teleterm/daemon"
+)
+
+func TestTeletermRPCService(t *testing.T) {
+	pack := dbhelpers.SetupDatabaseTest(t,
+		dbhelpers.WithListenerSetupDatabaseTest(helpers.SingleProxyPortSetup),
+		dbhelpers.WithLeafConfig(func(config *service.Config) {
+			config.Auth.NetworkingConfig.SetProxyListenerMode(types.ProxyListenerMode_Multiplex)
+		}),
+		dbhelpers.WithRootConfig(func(config *service.Config) {
+			config.Auth.NetworkingConfig.SetProxyListenerMode(types.ProxyListenerMode_Multiplex)
+		}),
+	)
+	pack.WaitForLeaf(t)
+
+	creds, err := helpers.GenerateUserCreds(helpers.UserCredsRequest{
+		Process:  pack.Root.Cluster.Process,
+		Username: pack.Root.User.GetName(),
+	})
+	require.NoError(t, err)
+
+	t.Run("list root clusters returning logged in user", func(t *testing.T) {
+		t.Parallel()
+
+		testIfListRootClustersReturnsLoggedInUser(t, pack, creds)
+	})
+	t.Run("get cluster returns properties from auth server", func(t *testing.T) {
+		t.Parallel()
+
+		testIfGetClusterReturnsPropertiesFromAuthServer(t, pack)
+	})
+}
+
+func testIfListRootClustersReturnsLoggedInUser(t *testing.T, pack *dbhelpers.DatabasePack, creds *helpers.UserCreds) {
+	tc, err := simulateLogin(pack.Root.User.GetName(), pack, creds)
+	require.NoError(t, err)
+	// The profile on disk created by NewClientWithCreds doesn't have WebProxyAddr set.
+	tc.WebProxyAddr = pack.Root.Cluster.Web
+	tc.SaveProfile(false /* makeCurrent */)
+
+	storage, err := clusters.NewStorage(clusters.Config{
+		Dir:                tc.KeysDir,
+		InsecureSkipVerify: tc.InsecureSkipVerify,
+	})
+	require.NoError(t, err)
+
+	daemonService, err := daemon.New(daemon.Config{
+		Storage: storage,
+	})
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		daemonService.Stop()
+	})
+
+	handler, err := handler.New(
+		handler.Config{
+			DaemonService: daemonService,
+		},
+	)
+	require.NoError(t, err)
+
+	response, err := handler.ListRootClusters(context.Background(), &api.ListClustersRequest{})
+	require.NoError(t, err)
+
+	require.Equal(t, 1, len(response.Clusters))
+	require.Equal(t, pack.Root.User.GetName(), response.Clusters[0].LoggedInUser.Name)
+}
+
+func testIfGetClusterReturnsPropertiesFromAuthServer(t *testing.T, pack *dbhelpers.DatabasePack) {
+	authServer := pack.Root.Cluster.Process.GetAuthServer()
+
+	// Use random names to not collide with other tests.
+	uuid := uuid.NewString()
+	suggestedReviewer := "suggested-reviewer"
+	requestableRoleName := fmt.Sprintf("%s-%s", "requested-role", uuid)
+	userName := fmt.Sprintf("%s-%s", "user", uuid)
+	roleName := fmt.Sprintf("%s-%s", "get-cluster-role", uuid)
+
+	requestableRole, err := types.NewRole(requestableRoleName, types.RoleSpecV6{})
+	require.NoError(t, err)
+
+	// Create user role with ability to request role
+	userRole, err := types.NewRole(roleName, types.RoleSpecV6{
+		Options: types.RoleOptions{},
+		Allow: types.RoleConditions{
+			Logins: []string{
+				userName,
+			},
+			NodeLabels: types.Labels{types.Wildcard: []string{types.Wildcard}},
+			Request: &types.AccessRequestConditions{
+				Roles:              []string{requestableRoleName},
+				SuggestedReviewers: []string{suggestedReviewer},
+			},
+		},
+	})
+	require.NoError(t, err)
+
+	// add role that user can request
+	err = authServer.UpsertRole(context.Background(), requestableRole)
+	require.NoError(t, err)
+
+	// add role that allows to request "requestableRole"
+	err = authServer.UpsertRole(context.Background(), userRole)
+	require.NoError(t, err)
+
+	user, err := types.NewUser(userName)
+	user.AddRole(userRole.GetName())
+	require.NoError(t, err)
+
+	watcher, err := authServer.NewWatcher(context.Background(), types.Watch{
+		Kinds: []types.WatchKind{
+			{Kind: types.KindUser},
+		},
+	})
+	require.NoError(t, err)
+	defer watcher.Close()
+
+	select {
+	case <-time.After(time.Second * 30):
+		t.Fatalf("Timeout waiting for OpInit event.")
+	case event := <-watcher.Events():
+		if event.Type != types.OpInit {
+			t.Fatalf("Unexpected event type.")
+		}
+		require.Equal(t, event.Type, types.OpInit)
+	case <-watcher.Done():
+		t.Fatal(watcher.Error())
+	}
+
+	err = authServer.UpsertUser(user)
+	require.NoError(t, err)
+
+	WaitForResource(t, watcher, user.GetKind(), user.GetName())
+
+	creds, err := helpers.GenerateUserCreds(helpers.UserCredsRequest{
+		Process:  pack.Root.Cluster.Process,
+		Username: userName,
+	})
+	require.NoError(t, err)
+
+	tc, err := simulateLogin(userName, pack, creds)
+	require.NoError(t, err)
+	// The profile on disk created by NewClientWithCreds doesn't have WebProxyAddr set.
+	tc.WebProxyAddr = pack.Root.Cluster.Web
+	tc.SaveProfile(false /* makeCurrent */)
+
+	storage, err := clusters.NewStorage(clusters.Config{
+		Dir:                tc.KeysDir,
+		InsecureSkipVerify: tc.InsecureSkipVerify,
+	})
+	require.NoError(t, err)
+
+	daemonService, err := daemon.New(daemon.Config{
+		Storage: storage,
+	})
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		daemonService.Stop()
+	})
+
+	handler, err := handler.New(
+		handler.Config{
+			DaemonService: daemonService,
+		},
+	)
+	require.NoError(t, err)
+
+	rootClusterName, _, err := net.SplitHostPort(pack.Root.Cluster.Web)
+	require.NoError(t, err)
+
+	response, err := handler.GetCluster(context.Background(), &api.GetClusterRequest{
+		ClusterUri: uri.NewClusterURI(rootClusterName).String(),
+	})
+	require.NoError(t, err)
+
+	require.Equal(t, userName, response.LoggedInUser.Name)
+	require.ElementsMatch(t, []string{requestableRoleName}, response.LoggedInUser.RequestableRoles)
+	require.ElementsMatch(t, []string{suggestedReviewer}, response.LoggedInUser.SuggestedReviewers)
+}
+
+func simulateLogin(userName string, pack *dbhelpers.DatabasePack, creds *helpers.UserCreds) (*client.TeleportClient, error) {
+	return pack.Root.Cluster.NewClientWithCreds(helpers.ClientConfig{
+		Login:   userName,
+		Cluster: pack.Root.Cluster.Secrets.SiteName,
+	}, *creds)
+
+}
