6f7b59a4106fa3408f8bda542a7e1442eba656b2
mcbattirola <matheus.battirola@goteleport.com>
2023-01-30 12:30:20 -0300

    Update godoc

--- lib/web/apiserver.go
+++ lib/web/apiserver.go
@@ -3620,8 +3620,8 @@ func (h *Handler) authExportPublic(w http.ResponseWriter, r *http.Request, p htt
 
 // getIsDashboard returns a bool indicating if the cluster is a
 // dashboard cluster.
-// Dashboards are clusters running on cloud infrastructure that aren't
-// Teleport Cloud users
+// Dashboards are clusters running on cloud infrastructure that
+// aren't Teleport Cloud clusters
 func getIsDashboard(features proto.Features) bool {
 	return !features.GetCloud() && features.GetRecoveryCodes()
 }
