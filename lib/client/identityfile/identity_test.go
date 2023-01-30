49e7512aa97db1756e79ab384f9941b450414574
Brian Joerger <bjoerger@goteleport.com>
2023-01-30 09:50:56 -0800

    Update lib/client/identityfile/identity_test.go
    Co-authored-by: Jakub Nyckowski <jakub.nyckowski@goteleport.com>

--- lib/client/identityfile/identity_test.go
+++ lib/client/identityfile/identity_test.go
@@ -329,8 +329,8 @@ func TestKeyFromIdentityFile(t *testing.T) {
 	})
 	require.NoError(t, err)
 
-	proxyHost := "proxy.example.com"
-	cluster := "cluster"
+	const proxyHost = "proxy.example.com"
+	const cluster = "cluster"
 
 	// parsed key is unchanged from original with proxy and cluster provided.
 	parsedKey, err := KeyFromIdentityFile(identityFilePath, proxyHost, cluster)
