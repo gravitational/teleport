9f81042c431a137d941c318152bdbb788ba4c01c
Jeff Pihach <jeff.pihach@goteleport.com>
2023-01-31 14:26:08 -0600

    Use the webassets directory at the root of the project for the web ui.

--- webassets_embed_ent.go
+++ webassets_embed_ent.go
@@ -1,15 +1,11 @@
-//go:build webassets_embed
-// +build webassets_embed
+//go:build webassets_embed && webassets_ent
 
 /*
 Copyright 2021 Gravitational, Inc.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package web
+package teleport
 
 import (
 	"embed"
@@ -27,13 +23,13 @@ import (
 	"github.com/gravitational/trace"
 )
 
-//go:embed build/webassets
-var webassetFS embed.FS
+//go:embed webassets/e/teleport
+var embedded embed.FS
 
-// NewStaticFileSystem returns the initialized implementation of http.FileSystem
-// interface which can be used to serve Teleport Proxy Web UI
-func NewStaticFileSystem() (http.FileSystem, error) {
-	wfs, err := fs.Sub(webassetFS, "build/webassets")
+// NewWebAssetsFilesystem returns the initialized implementation of
+// http.FileSystem interface which can be used to serve Teleport Proxy Web UI
+func NewWebAssetsFilesystem() (http.FileSystem, error) {
+	wfs, err := fs.Sub(embedded, "webassets/e/teleport")
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
