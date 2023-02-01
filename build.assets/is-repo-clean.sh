acd67fae6b910c693bf6013a5f708be68025d579
Mike Wilson <michael.wilson@goteleport.com>
2023-02-01 13:27:47 -0500

    Restoring the script.

---
+++ build.assets/is-repo-clean.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Detects whether protobufs are up to date.
+#
+
+GIT="${GIT:-git}"
+
+main() {
+  if ! command -v "$GIT" >/dev/null; then
+    echo "Unable to find git command."
+    exit 1
+  fi
+  
+  if ! "$GIT" diff --quiet; then
+    echo "The repository is dirty."
+    exit 1
+  fi
+}
+
+main "$@"
