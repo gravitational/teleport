b63473c4bc50cf1958abe2f2ba63aecbab4448da
Jeff Pihach <jeff.pihach@goteleport.com>
2023-02-02 11:18:44 -0600

    ignore type check on e imports.

--- web/packages/build/.eslintrc.js
+++ web/packages/build/.eslintrc.js
@@ -112,7 +112,7 @@ module.exports = {
     'import/no-named-as-default': 0,
     'import/default': 2,
     'import/named': 2,
-    // Change to a 2 once e pkg imports are removed from teleterm.
+    // XXX Change to a 2 once e pkg imports are removed from teleterm.
     'import/no-unresolved': 1,
     'no-underscore-dangle': 0,
     'no-case-declarations': 0,
