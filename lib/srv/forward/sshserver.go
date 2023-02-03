8337429706a3feab4ea781fdd3d2ecada08f1e7f
Gus Luxton <gus@goteleport.com>
2023-02-03 17:25:43 -0400

    Also handle windadj in forwarding server

--- lib/srv/forward/sshserver.go
+++ lib/srv/forward/sshserver.go
@@ -998,12 +998,16 @@ func (s *Server) handleSessionChannel(ctx context.Context, nch ssh.NewChannel) {
 				),
 			)
 
-			if err := s.dispatch(ctx, ch, req, scx); err != nil {
+			// dispatch returns an additional bool which indicates whether the reply has already been handled inside
+			// the dispatch function itself. if this is set to true, we avoid sending a second response in violation
+			// of the SSH protocol specifcation.
+			err, replyAlreadyHandled := s.dispatch(ctx, ch, req, scx)
+			if err != nil {
 				s.replyError(ch, req, err)
 				span.End()
 				return
 			}
-			if req.WantReply {
+			if !replyAlreadyHandled && req.WantReply {
 				if err := req.Reply(true, nil); err != nil {
 					scx.Errorf("failed sending OK response on %q request: %v", req.Type, err)
 				}
@@ -1026,7 +1030,7 @@ func (s *Server) handleSessionChannel(ctx context.Context, nch ssh.NewChannel) {
 	}
 }
 
-func (s *Server) dispatch(ctx context.Context, ch ssh.Channel, req *ssh.Request, scx *srv.ServerContext) error {
+func (s *Server) dispatch(ctx context.Context, ch ssh.Channel, req *ssh.Request, scx *srv.ServerContext) (err error, replyHandled bool) {
 	scx.Debugf("Handling request %v, want reply %v.", req.Type, req.WantReply)
 
 	// Certs with a join-only principal can only use a
@@ -1034,20 +1038,20 @@ func (s *Server) dispatch(ctx context.Context, ch ssh.Channel, req *ssh.Request,
 	if scx.JoinOnly {
 		switch req.Type {
 		case tracessh.TracingRequest:
-			return s.handleTracingRequest(ctx, req, scx)
+			return s.handleTracingRequest(ctx, req, scx), false
 		case sshutils.PTYRequest:
-			return s.termHandlers.HandlePTYReq(ctx, ch, req, scx)
+			return s.termHandlers.HandlePTYReq(ctx, ch, req, scx), false
 		case sshutils.ShellRequest:
-			return s.termHandlers.HandleShell(ctx, ch, req, scx)
+			return s.termHandlers.HandleShell(ctx, ch, req, scx), false
 		case sshutils.WindowChangeRequest:
-			return s.termHandlers.HandleWinChange(ctx, ch, req, scx)
+			return s.termHandlers.HandleWinChange(ctx, ch, req, scx), false
 		case teleport.ForceTerminateRequest:
-			return s.termHandlers.HandleForceTerminate(ch, req, scx)
+			return s.termHandlers.HandleForceTerminate(ch, req, scx), false
 		case sshutils.EnvRequest:
 			// We ignore all SSH setenv requests for join-only principals.
 			// SSH will send them anyway but it seems fine to silently drop them.
 		case sshutils.SubsystemRequest:
-			return s.handleSubsystem(ctx, ch, req, scx)
+			return s.handleSubsystem(ctx, ch, req, scx), false
 		case sshutils.AgentForwardRequest:
 			// to maintain interoperability with OpenSSH, agent forwarding requests
 			// should never fail, all errors should be logged and we should continue
@@ -1056,31 +1060,31 @@ func (s *Server) dispatch(ctx context.Context, ch ssh.Channel, req *ssh.Request,
 			if err != nil {
 				s.log.Debug(err)
 			}
-			return nil
+			return nil, false
 		default:
-			return trace.AccessDenied("attempted %v request in join-only mode", req.Type)
+			return trace.AccessDenied("attempted %v request in join-only mode", req.Type), false
 		}
 	}
 
 	switch req.Type {
 	case tracessh.TracingRequest:
-		return s.handleTracingRequest(ctx, req, scx)
+		return s.handleTracingRequest(ctx, req, scx), false
 	case sshutils.ExecRequest:
-		return s.termHandlers.HandleExec(ctx, ch, req, scx)
+		return s.termHandlers.HandleExec(ctx, ch, req, scx), false
 	case sshutils.PTYRequest:
-		return s.termHandlers.HandlePTYReq(ctx, ch, req, scx)
+		return s.termHandlers.HandlePTYReq(ctx, ch, req, scx), false
 	case sshutils.ShellRequest:
-		return s.termHandlers.HandleShell(ctx, ch, req, scx)
+		return s.termHandlers.HandleShell(ctx, ch, req, scx), false
 	case sshutils.WindowChangeRequest:
-		return s.termHandlers.HandleWinChange(ctx, ch, req, scx)
+		return s.termHandlers.HandleWinChange(ctx, ch, req, scx), false
 	case teleport.ForceTerminateRequest:
-		return s.termHandlers.HandleForceTerminate(ch, req, scx)
+		return s.termHandlers.HandleForceTerminate(ch, req, scx), false
 	case sshutils.EnvRequest:
-		return s.handleEnv(ctx, ch, req, scx)
+		return s.handleEnv(ctx, ch, req, scx), false
 	case sshutils.SubsystemRequest:
-		return s.handleSubsystem(ctx, ch, req, scx)
+		return s.handleSubsystem(ctx, ch, req, scx), false
 	case sshutils.X11ForwardRequest:
-		return s.handleX11Forward(ctx, ch, req, scx)
+		return s.handleX11Forward(ctx, ch, req, scx), false
 	case sshutils.AgentForwardRequest:
 		// to maintain interoperability with OpenSSH, agent forwarding requests
 		// should never fail, all errors should be logged and we should continue
@@ -1089,10 +1093,16 @@ func (s *Server) dispatch(ctx context.Context, ch ssh.Channel, req *ssh.Request,
 		if err != nil {
 			s.log.Debug(err)
 		}
-		return nil
+		return nil, false
+	case sshutils.PuTTYWinadjRequest:
+		// PuTTY sends this request along with some SSH_MSG_CHANNEL_WINDOW_ADJUST messages as part of its window-size
+		// tuning. It can be sent on any type of channel. There is no message-specific data. Servers MUST treat it
+		// as an unrecognized request and respond with SSH_MSG_CHANNEL_FAILURE.
+		// https://the.earth.li/~sgtatham/putty/0.76/htmldoc/AppendixG.html#sshnames-channel
+		return s.handlePuTTYWinadj(ch, req), true
 	default:
 		return trace.BadParameter(
-			"%v doesn't support request type '%v'", s.Component(), req.Type)
+			"%v doesn't support request type '%v'", s.Component(), req.Type), false
 	}
 }
 
@@ -1296,4 +1306,14 @@ func parseSubsystemRequest(req *ssh.Request, ctx *srv.ServerContext) (*remoteSub
 	}
 
 	return parseRemoteSubsystem(context.Background(), r.Name, ctx), nil
+}
+
+// handlePuTTYWinadj replies with failure to a PuTTY winadj request as required.
+// it returns an error if the reply fails.
+func (s *Server) handlePuTTYWinadj(ch ssh.Channel, req *ssh.Request) error {
+	if err := req.Reply(false, []byte{}); err != nil {
+		s.log.Warnf("Failed to reply to %q request: %v", req.Type, err)
+		return err
+	}
+	return nil
 }
