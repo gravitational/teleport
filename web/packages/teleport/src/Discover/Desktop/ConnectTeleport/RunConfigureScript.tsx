6850b1924b2ef95f499c92fa7db8c34823eb1159
Lisa Kim <lisa@goteleport.com>
2023-02-06 10:40:38 -0800

    Remove timeouts from discover flows, fix desktop discovery (#20829) (#21327)

--- web/packages/teleport/src/Discover/Desktop/ConnectTeleport/RunConfigureScript.tsx
+++ web/packages/teleport/src/Discover/Desktop/ConnectTeleport/RunConfigureScript.tsx
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 import * as Icons from 'design/Icon';
 
 import { ButtonPrimary } from 'design/Button';
-import { Text, Box } from 'design';
 
 import {
   StepContent,
@@ -16,8 +15,7 @@ import TextSelectCopy from 'teleport/components/TextSelectCopy';
 import { generateCommand } from 'teleport/Discover/Shared/generateCommand';
 
 import cfg from 'teleport/config';
-import { Timeout } from 'teleport/Discover/Shared/Timeout';
-import { useJoinToken } from 'teleport/Discover/Shared/JoinTokenContext';
+import { useJoinTokenSuspender } from 'teleport/Discover/Shared/useJoinTokenSuspender';
 import { ResourceKind } from 'teleport/Discover/Shared';
 
 import loading from './run-configure-script-loading.svg';
@@ -29,35 +27,9 @@ interface RunConfigureScriptProps {
 export function RunConfigureScript(
   props: React.PropsWithChildren<RunConfigureScriptProps>
 ) {
-  const { joinToken, reloadJoinToken, timeout, timedOut } = useJoinToken(
-    ResourceKind.Desktop
-  );
+  const { joinToken } = useJoinTokenSuspender(ResourceKind.Desktop);
 
-  let content;
-  if (timedOut) {
-    content = (
-      <StepInstructions>
-        <Text mb={4}>That script expired.</Text>
-
-        <ButtonPrimary onClick={reloadJoinToken}>
-          Generate another
-        </ButtonPrimary>
-      </StepInstructions>
-    );
-  } else {
-    const command = generateCommand(cfg.getConfigureADUrl(joinToken.id));
-
-    content = (
-      <StepInstructions>
-        <TextSelectCopy text={command} mt={2} mb={5} bash allowMultiline />
-
-        <ButtonPrimary onClick={() => props.onNext()}>Next</ButtonPrimary>
-        <Box mt={4}>
-          <Timeout timeout={timeout} />
-        </Box>
-      </StepInstructions>
-    );
-  }
+  const command = generateCommand(cfg.getConfigureADUrl(joinToken.id));
 
   return (
     <StepContent>
@@ -68,7 +40,11 @@ export function RunConfigureScript(
         1. Run the configure Active Directory script
       </StepTitle>
 
-      {content}
+      <StepInstructions>
+        <TextSelectCopy text={command} mt={2} mb={5} bash allowMultiline />
+
+        <ButtonPrimary onClick={() => props.onNext()}>Next</ButtonPrimary>
+      </StepInstructions>
     </StepContent>
   );
 }
