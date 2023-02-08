6eff75b3fa4e00f4d95761fb8db7e92f6eefacbe
Rafał Cieślak <rafal.cieslak@goteleport.com>
2023-02-08 15:46:03 +0100

    Remove code related to restarting lib/teleterm gateways
    The Electron app no longer asks tshd to restart gateways. This was changed
    in gravitational/webapps#1383.
    
    This PR merely removes the tshd implementation of that RPC.

--- lib/teleterm/apiserver/handler/handler_gateways.go
+++ lib/teleterm/apiserver/handler/handler_gateways.go
@@ -93,16 +93,6 @@ func newAPIGateway(gateway gateway.Gateway) (*api.Gateway, error) {
 	}, nil
 }
 
-// RestartGateway stops a gateway and starts a new with identical parameters but fresh certs,
-// keeping the original URI.
-func (s *Handler) RestartGateway(ctx context.Context, req *api.RestartGatewayRequest) (*api.EmptyResponse, error) {
-	if err := s.DaemonService.RestartGateway(ctx, req.GatewayUri); err != nil {
-		return nil, trace.Wrap(err)
-	}
-
-	return &api.EmptyResponse{}, nil
-}
-
 // SetGatewayTargetSubresourceName changes the TargetSubresourceName field of gateway.Gateway
 // and returns the updated version of gateway.Gateway.
 //
