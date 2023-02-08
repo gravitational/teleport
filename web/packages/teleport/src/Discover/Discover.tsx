5556af4a71580ba778f8a1566cc72ca4706b3b21
Isaiah Becker-Mayer <isaiah@goteleport.com>
2023-02-08 10:13:01 -0800

    Fix uncaught errors in Desktop's Discover flow (#21343)
    * Adds values for when labels aren't defined
    
    * fixes formatting and prevents usePingTeleport from rerunning the poll when used twice within a single context
    
    * Adds comments, pushes start logic into start itself
    
    * polishing usePingTeleport api such that it reflects that either the join token or search term is used for search
    
    * simplify and fix searchTerm bug

--- web/packages/teleport/src/Discover/Discover.tsx
+++ web/packages/teleport/src/Discover/Discover.tsx
@@ -68,8 +68,7 @@ function DiscoverContent() {
           selectedResource={selectedResource}
           views={views}
         />
-
-        {content}
+          {content}
       </FeatureBox>
 
       <Prompt
