95ea1021489f05021e063de5627c1cbfb41d54fc
Tim Ross <tim.ross@goteleport.com>
2023-02-08 08:28:01 -0500

    Improve listing resources across clusters
    Previously doing an `ls -R` would list all resources per profile in
    parallel. This had little benefit when one profile needed to list
    multiple clusters as those were still done serially. To mitigate this
    things are broken up into two separate steps:
    
      1) Connect to all clusters the user has credentials for in parallel
      2) List resources for each cluster in parallel
    
    This ensures that by the time a client is established for all available
    clusters that resource listing can happen in parallel regardless of
    profile. All operations are still restricted to an upper bound, however
    those limits were also bumped to help performance.

--- lib/client/client.go
+++ lib/client/client.go
@@ -746,6 +746,7 @@ func (proxy *ProxyClient) GetClusterAlerts(ctx context.Context, req types.GetClu
 
 // FindNodesByFiltersForCluster returns list of the nodes in a specified cluster which have filters matched.
 func (proxy *ProxyClient) FindNodesByFiltersForCluster(ctx context.Context, req proto.ListResourcesRequest, cluster string) ([]types.Server, error) {
+	req.ResourceType = types.KindNode
 	ctx, span := proxy.Tracer.Start(
 		ctx,
 		"proxyClient/FindNodesByFiltersForCluster",
@@ -759,8 +760,6 @@ func (proxy *ProxyClient) FindNodesByFiltersForCluster(ctx context.Context, req 
 		),
 	)
 	defer span.End()
-
-	req.ResourceType = types.KindNode
 
 	site, err := proxy.ConnectToCluster(ctx, cluster)
 	if err != nil {
@@ -962,6 +961,7 @@ func (proxy *ProxyClient) FindDatabaseServersByFilters(ctx context.Context, req 
 
 // FindDatabaseServersByFiltersForCluster returns all registered database proxy servers in the provided cluster.
 func (proxy *ProxyClient) FindDatabaseServersByFiltersForCluster(ctx context.Context, req proto.ListResourcesRequest, cluster string) ([]types.DatabaseServer, error) {
+	req.ResourceType = types.KindDatabaseServer
 	ctx, span := proxy.Tracer.Start(
 		ctx,
 		"proxyClient/FindDatabaseServersByFiltersForCluster",
@@ -976,7 +976,6 @@ func (proxy *ProxyClient) FindDatabaseServersByFiltersForCluster(ctx context.Con
 	)
 	defer span.End()
 
-	req.ResourceType = types.KindDatabaseServer
 	authClient, err := proxy.ConnectToCluster(ctx, cluster)
 	if err != nil {
 		return nil, trace.Wrap(err)
