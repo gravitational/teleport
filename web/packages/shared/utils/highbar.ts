66707af2aed9db253d8d104e7e120400812f035f
Jeff Pihach <jeff.pihach@goteleport.com>
2023-02-09 13:25:44 -0600

    Create new local util lib to replace lodash.

---
+++ web/packages/shared/utils/highbar.ts
@@ -0,0 +1,112 @@
+/**
+ * Copyright 2023 Gravitational, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export type DebouncedFunc<T extends (...args: any[]) => any> = {
+  (...args: Parameters<T>): ReturnType<T> | undefined;
+  cancel(): void;
+};
+
+export function isInteger(checkVal: any): boolean {
+  return Number.isInteger(checkVal) || checkVal == parseInt(checkVal);
+}
+
+// Lift & Shift from lodash
+export function debounce<T extends (...args: any) => any>(
+  func: T,
+  wait: number | undefined
+): DebouncedFunc<T> {
+  let lastArgs, lastThis, result, timerId, lastCallTime;
+
+  function invokeFunc() {
+    const args = lastArgs;
+    const thisArg = lastThis;
+
+    lastArgs = lastThis = undefined;
+    result = func.apply(thisArg, args);
+    return result;
+  }
+
+  function remainingWait(time) {
+    const timeSinceLastCall = time - lastCallTime;
+    const timeWaiting = wait - timeSinceLastCall;
+    return timeWaiting;
+  }
+
+  function shouldInvoke(time) {
+    const timeSinceLastCall = time - lastCallTime;
+
+    // Either this is the first call, activity has stopped and we're at the
+    // trailing edge, the system time has gone backwards and we're treating
+    // it as the trailing edge, or we've hit the `maxWait` limit.
+    return (
+      lastCallTime === undefined ||
+      timeSinceLastCall >= wait ||
+      timeSinceLastCall < 0
+    );
+  }
+
+  function timerExpired() {
+    const time = Date.now();
+    if (shouldInvoke(time)) {
+      return trailingEdge();
+    }
+    // Restart the timer.
+    timerId = setTimeout(timerExpired, remainingWait(time));
+  }
+
+  function trailingEdge() {
+    timerId = undefined;
+
+    // Only invoke if we have `lastArgs` which means `func` has been
+    // debounced at least once.
+    if (lastArgs) {
+      return invokeFunc();
+    }
+    lastArgs = lastThis = undefined;
+    return result;
+  }
+
+  function cancel() {
+    if (timerId !== undefined) {
+      clearTimeout(timerId);
+    }
+    lastArgs = lastCallTime = lastThis = timerId = undefined;
+  }
+
+  function debounced() {
+    const time = Date.now();
+    const isInvoking = shouldInvoke(time);
+
+    lastArgs = arguments;
+    lastThis = this;
+    lastCallTime = time;
+
+    if (isInvoking) {
+      if (timerId === undefined) {
+        // Start the timer for the trailing edge.
+        timerId = setTimeout(timerExpired, wait);
+        return result;
+      }
+    }
+    if (timerId === undefined) {
+      timerId = setTimeout(timerExpired, wait);
+    }
+    return result;
+  }
+
+  debounced.cancel = cancel;
+  return debounced;
+}
