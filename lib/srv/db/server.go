0755c75b7a1d597e9a6770e21a60c775c46d4962
Alan Parra <alan.parra@goteleport.com>
2023-02-10 18:48:12 -0300

    Appease linter

--- lib/srv/db/server.go
+++ lib/srv/db/server.go
@@ -14,9 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package db
-
 //nolint:goimports // Ignored because of inline comments.
+package db
 
 import (
 	"context"
