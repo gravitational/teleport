cc5fcab984a65fc0626efd716959a9d34d9b65cc
STeve (Xin) Huang <xin.huang@goteleport.com>
2023-02-14 08:40:29 -0500

    AWS IAM role matching for database users (#20610) (#21251)

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -4072,7 +4072,7 @@ func (a *Server) isMFARequired(ctx context.Context, checker services.AccessCheck
 		}
 
 		dbRoleMatchers := role.DatabaseRoleMatchers(
-			db.GetProtocol(),
+			db,
 			t.Database.Username,
 			t.Database.GetDatabase(),
 		)
