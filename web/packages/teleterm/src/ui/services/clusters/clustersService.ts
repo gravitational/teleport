335732e7bb57517f95c3c4b93cfbf6817ec97e35
Rafał Cieślak <rafal.cieslak@goteleport.com>
2023-02-16 12:38:49 +0100

    Add TODO comments for further improvements

--- web/packages/teleterm/src/ui/services/clusters/clustersService.ts
+++ web/packages/teleterm/src/ui/services/clusters/clustersService.ts
@@ -424,10 +424,12 @@ export class ClustersService extends ImmutableStore<types.ClustersServiceState> 
     return this.getClusters().filter(c => !c.leaf);
   }
 
+  // TODO(ravicious): Use ResourcesService instead.
   async fetchKubes(params: ServerSideParams) {
     return await this.client.getKubes(params);
   }
 
+  // TODO(ravicious): Move to ResourceService.
   async getDbUsers(dbUri: uri.DatabaseUri): Promise<string[]> {
     return await this.client.listDatabaseUsers(dbUri);
   }
