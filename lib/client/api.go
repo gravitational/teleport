2080eb872e84d9ef7612cd21a38233ef4f2c533f
Steven Martin <steven@goteleport.com>
2023-02-19 18:29:38 -0500

    Remove log statement and update comment

--- lib/client/api.go
+++ lib/client/api.go
@@ -4165,7 +4165,7 @@ func (tc *TeleportClient) applyProxySettings(proxySettings webclient.ProxySettin
 				proxySettings.DB.PostgresPublicAddr)
 		}
 		tc.PostgresProxyAddr = net.JoinHostPort(addr.Host(), strconv.Itoa(addr.Port(tc.WebProxyPort())))
-		// When in TLS routing mode the web port should be used if no Postgres Public Addr is set
+		// Listen address port applies if set and not in TLS routing mode.
 	case proxySettings.DB.PostgresListenAddr != "" && !proxySettings.TLSRoutingEnabled:
 		addr, err := utils.ParseAddr(proxySettings.DB.PostgresListenAddr)
 		if err != nil {
@@ -4174,7 +4174,6 @@ func (tc *TeleportClient) applyProxySettings(proxySettings webclient.ProxySettin
 		}
 		tc.PostgresProxyAddr = net.JoinHostPort(tc.WebProxyHost(), strconv.Itoa(addr.Port(defaults.PostgresListenPort)))
 	default:
-		log.Debugf("Returning default")
 		webProxyHost, webProxyPort := tc.WebProxyHostPort()
 		tc.PostgresProxyAddr = net.JoinHostPort(webProxyHost, strconv.Itoa(webProxyPort))
 	}
