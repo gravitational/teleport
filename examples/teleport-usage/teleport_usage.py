3135f1eeb98458c71700efc13e40bc88665736e8
Russell Jones <rjones@goteleport.com>
2023-02-22 13:35:21 -0800

    Cleaned up usage script and documentation

--- examples/teleport-usage/teleport_usage.py
+++ examples/teleport-usage/teleport_usage.py
@@ -1,9 +1,12 @@
 """
 Copyright 2023 Gravitational, Inc.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,7 +37,7 @@ if not start_date:
     now = datetime.datetime.now()
     start_date = now - datetime.timedelta(days=30)
 else:
-    start_date = datetime.datetime.strptime(start_date,'%Y-%m-%d')    
+    start_date = datetime.datetime.strptime(start_date,'%Y-%m-%d')
 
 end_date = start_date + datetime.timedelta(days=30)
 
@@ -58,7 +61,7 @@ end_date_str = end_date.strftime("%b %d %y")
 day_range = (end_date - start_date).days
 d_range = (end_date - start_date).days + 1
 print("""
-NOTE: This tool queries usage for the last 30 days") 
+NOTE: This tool queries usage for the last 30 days")
 For a custom start date use ENV START_DATE (format YYYY-MM-DD)")
 """)
 print(f"Gathering data for unique users who accessed each protocol between {start_date_str} to {end_date_str} ({day_range} days): ")
@@ -119,5 +122,3 @@ if print_users == "YES":
     print(json.dumps(all_users, indent=4, separators=(',', ':')))
 else:
     print("If you would like to print all unique users per protocol set ENV SHOW_USERS=YES in a future run")
-
-
