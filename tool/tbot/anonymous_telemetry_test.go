b27aba5483ce9e53b46cf55f964ee04188b25a2d
Noah <noah.stride@goteleport.com>
2023-03-01 13:02:31 +0000

    Finish automated test

--- tool/tbot/anonymous_telemetry_test.go
+++ tool/tbot/anonymous_telemetry_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"github.com/gravitational/teleport"
+	"github.com/gravitational/teleport/api/types"
 	"testing"
 
 	"github.com/bufbuild/connect-go"
@@ -13,13 +15,14 @@ import (
 )
 
 type mockReportingServiceClient struct {
-	eventRequest v1alpha.SubmitTbotEventRequest
+	eventRequest *v1alpha.SubmitTbotEventRequest
 }
 
 func (mrsc *mockReportingServiceClient) SubmitTbotEvent(
-	context.Context,
-	*connect.Request[v1alpha.SubmitTbotEventRequest],
+	ctx context.Context,
+	req *connect.Request[v1alpha.SubmitTbotEventRequest],
 ) (*connect.Response[v1alpha.SubmitTbotEventResponse], error) {
+	mrsc.eventRequest = req.Msg
 	return connect.NewResponse(&v1alpha.SubmitTbotEventResponse{}), nil
 }
 
@@ -31,13 +34,91 @@ func mockEnvGetter(data map[string]string) envGetter {
 
 func TestSendTelemetry(t *testing.T) {
 	ctx := context.Background()
-	mockClient := &mockReportingServiceClient{}
-	err := sendTelemetry(
-		ctx,
-		mockClient,
-		mockEnvGetter(map[string]string{}),
-		utils.NewLogger(),
-		&config.BotConfig{},
-	)
-	require.NoError(t, err)
+	log := utils.NewLoggerForTests()
+
+	t.Run("sends telemetry when enabled", func(t *testing.T) {
+		mockClient := &mockReportingServiceClient{}
+		env := map[string]string{
+			helperEnv:                    "test",
+			helperVersionEnv:             "13.37.0",
+			anonymousTelemetryEnabledEnv: "1",
+		}
+		cfg := &config.BotConfig{
+			Oneshot: true,
+			Onboarding: &config.OnboardingConfig{
+				JoinMethod: types.JoinMethodGitHub,
+			},
+			Destinations: []*config.DestinationConfig{
+				{
+					DestinationMixin: config.DestinationMixin{
+						Directory: &config.DestinationDirectory{},
+					},
+				},
+				{
+					DestinationMixin: config.DestinationMixin{
+						Directory: &config.DestinationDirectory{},
+					},
+					KubernetesCluster: &config.KubernetesCluster{
+						ClusterName: "foo",
+					},
+				},
+				{
+					DestinationMixin: config.DestinationMixin{
+						Directory: &config.DestinationDirectory{},
+					},
+					App: &config.App{
+						App: "bar",
+					},
+				},
+				{
+					DestinationMixin: config.DestinationMixin{
+						Directory: &config.DestinationDirectory{},
+					},
+					Database: &config.Database{
+						Database: "biz",
+					},
+				},
+			},
+		}
+		err := sendTelemetry(
+			ctx,
+			mockClient,
+			mockEnvGetter(env),
+			log,
+			cfg,
+		)
+		require.NoError(t, err)
+		require.NotNil(t, mockClient.eventRequest)
+		require.NotZero(t, mockClient.eventRequest.Timestamp)
+		require.NotZero(t, mockClient.eventRequest.DistinctId)
+		require.Equal(t, &v1alpha.SubmitTbotEventRequest_Start{
+			Start: &v1alpha.TbotStartEvent{
+				RunMode:  v1alpha.TbotStartEvent_RUN_MODE_ONE_SHOT,
+				JoinType: string(types.JoinMethodGitHub),
+				Version:  teleport.Version,
+
+				Helper:        env[helperEnv],
+				HelperVersion: env[helperVersionEnv],
+
+				DestinationsApplication: 1,
+				DestinationsKubernetes:  1,
+				DestinationsDatabase:    1,
+				DestinationsOther:       1,
+			},
+		}, mockClient.eventRequest.Event)
+	})
+	t.Run("does not send telemetry when not explicitly enabled", func(t *testing.T) {
+		mockClient := &mockReportingServiceClient{}
+		env := map[string]string{}
+		cfg := &config.BotConfig{}
+		err := sendTelemetry(
+			ctx,
+			mockClient,
+			mockEnvGetter(env),
+			log,
+			cfg,
+		)
+		require.NoError(t, err)
+		require.Nil(t, mockClient.eventRequest)
+	})
 }
