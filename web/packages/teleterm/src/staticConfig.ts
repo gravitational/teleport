4262be42050790a3c606eaf700b31a796d17dcfd
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2023-03-02 18:15:23 +0100

    Provide prehog address for prod env

--- web/packages/teleterm/src/staticConfig.ts
+++ web/packages/teleterm/src/staticConfig.ts
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'development') {
 
 if (process.env.NODE_ENV === 'production') {
   staticConfig = {
-    prehogAddress: '',
+    prehogAddress: 'https://reporting.teleportinfra.sh',
     feedbackAddress: 'https://usage.teleport.dev',
   };
 }
