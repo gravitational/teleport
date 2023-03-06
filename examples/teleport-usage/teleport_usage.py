2f47ac02f20603e38c771d9ba7f266efa10a63d1
Yael Jay Perez <48836604+yjperez@users.noreply.github.com>
2023-03-05 20:29:58 -0600

    Update teleport_usage.py
    Refactored the dynamoDB query to reduce it by 80% from 150 queries to 30 queries. Changed parsing logic to no over count Kube exec sessions as SSH sessions.

--- examples/teleport-usage/teleport_usage.py
+++ examples/teleport-usage/teleport_usage.py
@@ -25,7 +25,7 @@ from boto3.dynamodb.conditions import Key, Attr
 # Check for AWS DynamoDB table name and region ENV, if none prompt
 table_name = os.getenv("TABLE_NAME")
 if not table_name:
-    TABLE_NAME = input("DynamoDB events table name: ")
+    table_name = input("DynamoDB events table name: ")
 
 aws_region = os.getenv("AWS_REGION")
 if not aws_region:
@@ -43,17 +43,7 @@ end_date = start_date + datetime.timedelta(days=30)
 
 # Configure DynamoDB for query
 dynamodb = boto3.resource("dynamodb", region_name=aws_region)
-table = dynamodb.Table(TABLE_NAME)
-
-# variables for protocols
-app = 'app.session.start'
-db = 'db.session.start'
-kube = 'kube.request'
-ssh = 'session.start'
-win = 'windows.desktop.session.start'
-
-protocols = ['app', 'db', 'kube', 'win']
-all_users = []
+table = dynamodb.Table(table_name)
 
 # Generating data message
 start_date_str = start_date.strftime("%b %d %y")
@@ -66,7 +56,15 @@ For a custom start date use ENV START_DATE (format YYYY-MM-DD)")
 """)
 print(f"Gathering data for unique users who accessed each protocol between {start_date_str} to {end_date_str} ({day_range} days): ")
 
-unique_users = set()
+# Creating a set per protocol to gather unique users
+unique_users = {
+    'app.session.start': set(),
+    'db.session.start': set(),
+    'kube.request': set(),
+    'session.start': set(),
+    'windows.desktop.session.start': set(),
+    
+}
 
 # Run Dynamodb query to retrieve all login for SSH for specified date range
 for i in range(d_range):
@@ -74,51 +72,39 @@ for i in range(d_range):
 
     date = current_date.strftime("%Y-%m-%d")
     response = table.query(
-        TableName=TABLE_NAME,
+        TableName=table_name,
         IndexName="timesearchV2",
         KeyConditionExpression=Key("CreatedAtDate").eq(date),
-        FilterExpression=Attr("EventType").begins_with(ssh),
+        FilterExpression=Attr("EventType").contains('session.start' or 'kube.request'),
     )
-    # Calculate unique users
-    for item in response["Items"]:
-        user = item.get("FieldsMap").get("user")
-        if user:
-            unique_users.add(user)
-print(f"ssh: {len(unique_users)}")
-if len(unique_users) != 0:
-    all_users.append(f"ssh: {unique_users}")
+    # Parsing the response from DynamoDB. Separating users by event type
+    for item in response['Items']:
+        event_type = item['FieldsMap']['event']
+        user = item['FieldsMap']['user']
+        if event_type in unique_users:
+            # For SSH access, ensuring there is not a double count the Kube execs
+            if event_type == 'session.start' and item['FieldsMap'].get('kubernetes_cluster', ''):
+                continue  # skip if kubernetes_cluster exists and is not empty
+            unique_users[event_type].add(user)
 
-# Loop through the protocols in the list
-for protocol in protocols:
-  # Create a set for the unique users of this protocol
-  unique_users = set()
+alias_dict = {
+    'app.session.start': 'App Access',
+    'db.session.start': 'DB Access',
+    'kube.request': 'Kube Access',
+    'session.start': 'SSH Access',
+    'windows.desktop.session.start': 'Windows Access'
+}
+for event_type, users in unique_users.items():
+    print(f'{alias_dict.get(event_type, event_type)}: {len(users)} unique users')
 
-  # Run Dynamodb query to retrieve all login event types for specified date range
-    for i in range(d_range):
-        current_date = start_date + datetime.timedelta(i)
-        date = current_date.strftime("%Y-%m-%d")
-        response = table.query(
-            TableName=TABLE_NAME,
-            IndexName="timesearchV2",
-            KeyConditionExpression=Key("CreatedAtDate").eq(date),
-            FilterExpression=Attr("EventType").begins_with(protocol),
-        )
-        # Calculate unique users
-        for item in response["Items"]:
-            user = item.get("FieldsMap").get("user")
-            if user:
-                unique_users.add(user)
-
-    # Print the count of unique users for this protocol
-    print(f"{protocol}: {len(unique_users)}")
-    if len(unique_users) != 0:
-        all_users.append(f"{protocol}: {unique_users}")
 
 # By default will not print all users, to print use ENV  SHOW_USERS=YES
 print_users = os.getenv("SHOW_USERS")
 if print_users == "YES":
     # Print the unique users, separated by a comma and a space
     print("Printing all unique users per protocol: ")
-    print(json.dumps(all_users, indent=4, separators=(',', ':')))
+    for event_type, users in unique_users.items():
+        if len(users) != 0:
+            print(f'{alias_dict.get(event_type, event_type)}: {users}')
 else:
     print("If you would like to print all unique users per protocol set ENV SHOW_USERS=YES in a future run")
