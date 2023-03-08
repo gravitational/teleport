707a4de0e12681e801bbf1788bfd61ede2d78828
Jonathan Lassoff <jof@goteleport.com>
2023-03-08 15:41:51 -0800

    Move a bunch of tf files to test

--- examples/aws/terraform/starter-cluster/ssm.tf
+++
@@ -1,8 +0,0 @@
-// SSM for Teleport Enterprise license storage and retrieval
-resource "aws_ssm_parameter" "license" {
-  count     = var.license_path != "" ? 1 : 0
-  name      = "/teleport/${var.cluster_name}/license"
-  type      = "SecureString"
-  value     = file(var.license_path)
-  overwrite = true
-}
