4e0f9f24542421e9b24aa510b2ee000f31fe4502
Tim Ross <tim.ross@goteleport.com>
2023-03-13 14:49:46 -0400

    buildbox:teleport12-dev-go120

--- dronegen/container_image_products.go
+++ dronegen/container_image_products.go
@@ -122,7 +122,7 @@ func NewTeleportOperatorProduct(cloneDirectory string) *Product {
 				compilerName = "arm-linux-gnueabihf-gcc"
 			}
 
-			buildboxName += ":teleport12"
+			buildboxName += ":teleport12-dev-go120"
 
 			return []string{
 				fmt.Sprintf("BUILDBOX=%s", buildboxName),
