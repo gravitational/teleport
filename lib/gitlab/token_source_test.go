601d2dc25b94b4f7dcf7e60719ca918a10041526
Noah <noah.stride@goteleport.com>
2023-03-13 12:09:10 +0100

    Add tests for IDTokenSource

---
+++ lib/gitlab/token_source_test.go
@@ -0,0 +1,35 @@
+package gitlab
+
+import (
+	"github.com/gravitational/trace"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestIDTokenSource_GetIDToken(t *testing.T) {
+	t.Run("value present", func(t *testing.T) {
+		its := &IDTokenSource{
+			getEnv: func(key string) string {
+				if key == "TBOT_GITLAB_JWT" {
+					return "foo"
+				}
+				return ""
+			},
+		}
+		tok, err := its.GetIDToken()
+		require.NoError(t, err)
+		require.Equal(t, "foo", tok)
+	})
+
+	t.Run("value missing", func(t *testing.T) {
+		its := &IDTokenSource{
+			getEnv: func(key string) string {
+				return ""
+			},
+		}
+		tok, err := its.GetIDToken()
+		require.Error(t, err)
+		require.True(t, trace.IsBadParameter(err))
+		require.Equal(t, "", tok)
+	})
+}
