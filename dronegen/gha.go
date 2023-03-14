eb8bf4211ccdf6fcd6ef85e6c160f135592abcd3
Trent Clarke <trent@goteleport.com>
2023-03-14 22:06:52 +1100

    disable workflow tagging

--- dronegen/gha.go
+++ dronegen/gha.go
@@ -41,7 +41,6 @@ func ghaBuildPipeline(b ghaBuildType) pipeline {
 	cmd.WriteString(`go run ./cmd/gh-trigger-workflow `)
 	cmd.WriteString(`-owner ${DRONE_REPO_OWNER} `)
 	cmd.WriteString(`-repo teleport.e `)
-	cmd.WriteString(`-tag-workflow `)
 	fmt.Fprintf(&cmd, `-workflow %s `, b.ghaWorkflow)
 	fmt.Fprintf(&cmd, `-workflow-ref=%s `, b.workflowRef)
 
