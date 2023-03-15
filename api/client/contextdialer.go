eaa4ffcf9cb3b1084e3a0489bc867663bf4f9f7f
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2023-03-15 09:41:56 -0400

    Add a dedicated client to communicate with the Proxy SSH server (#22629)
    A new `api/client/proxy/Client` has been added to interact with
    the SSH and gRPC servers that the Proxy serves on its SSH port.
    The client will first try connecting to the gRPC server and if
    that fails it will fall back to the SSH server.
    
    Much of the SSH functionality mimics the existing behavior of the
    `ProxyClient` in `lib/client`. This is the first part of phasing
    out that client in favor of the new client. There will be a follow
    up PR that migrates `lib/client` to make use of the new client instead.
    
    Part of #19812

--- api/client/contextdialer.go
+++ api/client/contextdialer.go
@@ -26,11 +26,11 @@ import (
 	oteltrace "go.opentelemetry.io/otel/trace"
 	"golang.org/x/crypto/ssh"
 
-	"github.com/gravitational/teleport/api/client/proxy"
 	"github.com/gravitational/teleport/api/client/webclient"
 	"github.com/gravitational/teleport/api/constants"
 	"github.com/gravitational/teleport/api/observability/tracing"
 	tracessh "github.com/gravitational/teleport/api/observability/tracing/ssh"
+	"github.com/gravitational/teleport/api/utils"
 	"github.com/gravitational/teleport/api/utils/sshutils"
 )
 
@@ -81,7 +81,7 @@ func tracedDialer(ctx context.Context, fn ContextDialerFunc) ContextDialerFunc {
 func NewDialer(ctx context.Context, keepAlivePeriod, dialTimeout time.Duration) ContextDialer {
 	return tracedDialer(ctx, func(ctx context.Context, network, addr string) (net.Conn, error) {
 		dialer := newDirectDialer(keepAlivePeriod, dialTimeout)
-		if proxyURL := proxy.GetProxyURL(addr); proxyURL != nil {
+		if proxyURL := utils.GetProxyURL(addr); proxyURL != nil {
 			return DialProxyWithDialer(ctx, proxyURL, addr, dialer)
 		}
 		return dialer.DialContext(ctx, network, addr)
