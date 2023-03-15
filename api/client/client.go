e2074f999967001827ade90cf70e847286ff656a
Noah <noah.stride@goteleport.com>
2023-03-15 11:45:10 +0100

    Fix GetCertAuthorities gRPC error wrapping

--- api/client/client.go
+++ api/client/client.go
@@ -3311,7 +3311,7 @@ func (c *Client) GetCertAuthorities(ctx context.Context, caType types.CertAuthTy
 		IncludeKey: loadKeys,
 	})
 	if err != nil {
-		return nil, trace.Wrap(err)
+		return nil, trail.FromGRPC(err)
 	}
 
 	cas := make([]types.CertAuthority, 0, len(resp.CertAuthoritiesV2))
@@ -3319,7 +3319,7 @@ func (c *Client) GetCertAuthorities(ctx context.Context, caType types.CertAuthTy
 		cas = append(cas, ca)
 	}
 
-	return cas, trail.FromGRPC(err)
+	return cas, nil
 }
 
 // UpdateHeadlessAuthenticationState updates a headless authentication state.
