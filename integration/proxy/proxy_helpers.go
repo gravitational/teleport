b0dc78610e3ed3c8d59eb9178b29bc12a769a372
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2023-03-16 10:46:43 -0400

    Migrate upserting CAs to gRPC (#23140)
    Adds `UpsertCertAuthority` to the `teleport.v1.trust.TrustService`
    and deprecates the HTTP api. The `services.Trust` interface was
    also updated to include a `context.Context` in both
    `CreateCertAuthority` and `UpsertCertAuthority.
    
    To ensure backwards compatibility the `auth.Client` will first
    attempt to use the gRPC client and if that fails with a not
    implemented error it will fallback to using HTTP.
    
    Part of #6394

--- integration/proxy/proxy_helpers.go
+++ integration/proxy/proxy_helpers.go
@@ -357,17 +357,18 @@ func createTestRole(username string) types.Role {
 func withStandardRoleMapping() proxySuiteOptionsFunc {
 	return func(options *suiteOptions) {
 		options.updateRoleMappingFunc = func(t *testing.T, suite *Suite) {
+			ctx := context.Background()
 			rc := suite.root
 			lc := suite.leaf
 			role := suite.root.Secrets.Users[helpers.MustGetCurrentUser(t).Username].Roles[0]
-			ca, err := lc.Process.GetAuthServer().GetCertAuthority(context.Background(), types.CertAuthID{
+			ca, err := lc.Process.GetAuthServer().GetCertAuthority(ctx, types.CertAuthID{
 				Type:       types.UserCA,
 				DomainName: rc.Secrets.SiteName,
 			}, false)
 			require.NoError(t, err)
 			ca.SetRoles(nil) // Reset roles, otherwise they will take precedence.
 			ca.SetRoleMap(types.RoleMap{{Remote: role.GetName(), Local: []string{role.GetName()}}})
-			err = lc.Process.GetAuthServer().UpsertCertAuthority(ca)
+			err = lc.Process.GetAuthServer().UpsertCertAuthority(ctx, ca)
 			require.NoError(t, err)
 		}
 	}
