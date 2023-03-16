d617762cbc951e249c1208b4f61d988366229942
mcbattirola <matheus.battirola@goteleport.com>
2023-03-16 10:38:28 -0300

    Add new feature source field to license

--- api/types/license.go
+++ api/types/license.go
@@ -116,7 +116,17 @@ type License interface {
 	// GetAccountID returns Account ID.
 	//  Note: This is not applicable to all Cloud licenses
 	GetAccountID() string
+
+	// GetFeatureSource returns where the features should be loaded from.
+	GetFeatureSource() FeatureSource
 }
+
+type FeatureSource string
+
+const (
+	FeatureSourceLicense FeatureSource = "license"
+	FeatureSourceCloud   FeatureSource = "cloud"
+)
 
 // NewLicense is a convenience method to create LicenseV3.
 func NewLicense(name string, spec LicenseSpecV3) (License, error) {
@@ -416,6 +426,17 @@ func (c *LicenseV3) String() string {
 	return strings.Join(features, ",")
 }
 
+// GetFeatureSource returns the source Teleport should use to
+// read the features
+func (c *LicenseV3) GetFeatureSource() FeatureSource {
+	// defaults to License for backward compatibility
+	if c.Spec.FeatureSource == "" {
+		return FeatureSourceLicense
+	}
+
+	return c.Spec.FeatureSource
+}
+
 // LicenseSpecV3 is the actual data we care about for LicenseV3. When changing
 // this, keep in mind that other consumers of teleport/api (Houston, Sales
 // Center) might still need to generate or parse licenses for older versions of
@@ -450,4 +471,6 @@ type LicenseSpecV3 struct {
 	SupportsResourceAccessRequests Bool `json:"resource_access_requests,omitempty"`
 	// Trial is true for trial licenses
 	Trial Bool `json:"trial,omitempty"`
+	// FeatureSource is the source of the set of enabled feature
+	FeatureSource FeatureSource `json:"feature_source"`
 }
