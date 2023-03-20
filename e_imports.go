2447ba230eb4f133c6edb8d04d106af43baf8d48
Mike Wilson <michael.wilson@goteleport.com>
2023-03-20 14:22:51 -0400

    Add Okta client import for Okta service.
    The Okta client import has been added for the Okta service.

--- e_imports.go
+++ e_imports.go
@@ -45,12 +45,12 @@ comm -13 <(
 import (
 	_ "github.com/beevik/etree"
 	_ "github.com/coreos/go-oidc/oidc"
-	_ "github.com/crewjam/saml/samlidp"
 	_ "github.com/go-piv/piv-go/piv"
 	_ "github.com/google/go-attestation/attest"
 	_ "github.com/gravitational/form"
 	_ "github.com/gravitational/teleport-plugins/access/common"
 	_ "github.com/gravitational/teleport-plugins/access/slack"
+	_ "github.com/okta/okta-sdk-golang/v2/okta"
 	_ "google.golang.org/api/admin/directory/v1"
 	_ "google.golang.org/api/cloudidentity/v1"
 	_ "google.golang.org/genproto/googleapis/rpc/status"
