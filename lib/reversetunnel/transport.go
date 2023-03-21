ebed3dee07f9dfe4e834bbe39271ec77de74f3fc
Anton Miniailo <anton@goteleport.com>
2023-03-21 13:00:09 -0400

    Send signed proxy header to the kube service
    Because it was checking version, which was empty, signed headers were not sent,
     when we contacted leaf cluster's kube service

--- lib/reversetunnel/transport.go
+++ lib/reversetunnel/transport.go
@@ -325,7 +325,8 @@ func (p *transport) start() {
 		clientDst = dst
 	}
 	var signedHeader []byte
-	if shouldSendSignedPROXYHeader(p.proxySigner, dreq.TeleportVersion, useTunnel, dreq.Address != RemoteAuthServer, clientSrc, clientDst) {
+	isKubeOrAuth := dreq.ConnType == types.KubeTunnel || dreq.Address == RemoteAuthServer
+	if shouldSendSignedPROXYHeader(p.proxySigner, dreq.TeleportVersion, useTunnel, !isKubeOrAuth, clientSrc, clientDst) {
 		signedHeader, err = p.proxySigner.SignPROXYHeader(clientSrc, clientDst)
 		if err != nil {
 			errorMessage := fmt.Sprintf("connection rejected - could not create signed PROXY header: %v", err)
