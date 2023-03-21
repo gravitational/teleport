4382a898781b0fb6e8f3df12a2b6ef184255a705
Tim Ross <tim.ross@goteleport.com>
2023-03-21 13:58:32 -0400

    Validate proxy peer identity
    Closes #23230
    
    Proxy peer clients now validate that any connections established to
    a peer have the expected UUID. This prevents clients from connecting
    to incorrect peers if the `peer_public_addr` is shared by multiple
    proxies, or is a load balancer.
    
    In the event that a connection is established to an incorrect peer
    the connection is closed and a error is written to the logs. The
    gRPC connection should be attempted multiple times and eventually
    land on the correct peer. However there may be some degradation
    of service until the peer connections are all properly established.

--- lib/service/service.go
+++ lib/service/service.go
@@ -3453,6 +3453,7 @@ func (process *TeleportProcess) initProxyEndpoint(conn *Connector) error {
 				TLSConfig:   clientTLSConfig,
 				Log:         process.log,
 				Clock:       process.Clock,
+				ClusterName: clusterName,
 			})
 			if err != nil {
 				return trace.Wrap(err)
@@ -3679,6 +3680,7 @@ func (process *TeleportProcess) initProxyEndpoint(conn *Connector) error {
 			TLSConfig:     serverTLSConfig,
 			ClusterDialer: clusterdial.NewClusterDialer(tsrv),
 			Log:           log,
+			ClusterName:   clusterName,
 		})
 		if err != nil {
 			return trace.Wrap(err)
