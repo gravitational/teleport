3f54935ffd4879b809bef48126f9d45aafb0e622
Mike Wilson <michael.wilson@goteleport.com>
2023-03-21 12:15:10 -0400

    Set serviceStarted if enterprise services are enabled.
    If enterprise only services have been enabled, set serviceStarted so that
    the process does not exit when none of the OSS services are not running.

--- lib/service/service.go
+++ lib/service/service.go
@@ -1110,6 +1110,12 @@ func NewTeleport(cfg *Config) (*TeleportProcess, error) {
 		warnOnErr(process.closeImportedDescriptors(teleport.ComponentDiscovery), process.log)
 	}
 
+	// Enterprise services will be handled by the enterprise binary. We'll let these set serviceStarted
+	// to serviceStarted and let the enterprise binary error if need be.
+	if process.enterpriseServicesEnabled() {
+		serviceStarted = true
+	}
+
 	process.RegisterFunc("common.rotate", process.periodicSyncRotationState)
 
 	// run one upload completer per-process
@@ -1143,6 +1149,11 @@ func NewTeleport(cfg *Config) (*TeleportProcess, error) {
 	go process.notifyParent()
 
 	return process, nil
+}
+
+// enterpriseServicesEnabled will return true of any enterprise services are enabled.
+func (process *TeleportProcess) enterpriseServicesEnabled() bool {
+	return modules.GetModules().BuildType() == modules.BuildEnterprise && (process.Config.Okta.Enabled)
 }
 
 // notifyParent notifies parent process that this process has started
