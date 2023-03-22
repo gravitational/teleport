a6946d76f5c0377f8ef1a27fdbec6621d68a9eb2
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2023-03-22 10:28:45 -0400

    Address PR comments

--- web/packages/teleport/src/components/HeadlessRequestDialog/HeadlessRequestDialog.tsx
+++ web/packages/teleport/src/components/HeadlessRequestDialog/HeadlessRequestDialog.tsx
@@ -31,61 +31,6 @@ export default function HeadlessRequestDialog({
   onReject,
   errorText,
 }: Props) {
-  const dialogContent = () => {
-    const getText = (errorText: string) => {
-      if (errorText) {
-        return (
-          <Text textAlign="center">
-            The requested session doesn't exist or is invalid. Please generate a
-            new request.
-            <br />
-            <br />
-            You can close this window.
-          </Text>
-        );
-      }
-
-      return (
-        <Text textAlign="center">
-          Someone has initiated a command from {ipAddress}. If it was not you,
-          click reject and contact your administrator.
-          <br />
-          <br />
-          If it was you, please use your hardware key to approve.
-        </Text>
-      );
-    };
-
-    const getButtons = (errorText: string) => {
-      if (errorText) {
-        return;
-      }
-
-      return (
-        <>
-          <ButtonPrimary onClick={onAccept} autoFocus mr={3} width="130px">
-            Approve
-          </ButtonPrimary>
-          <ButtonSecondary onClick={onReject}>Reject</ButtonSecondary>
-        </>
-      );
-    };
-
-    return (
-      <>
-        <DialogContent mb={6}>
-          {errorText && (
-            <Danger mt={2} width="100%">
-              {errorText}
-            </Danger>
-          )}
-          {getText(errorText)}
-        </DialogContent>
-        <DialogFooter textAlign="center">{getButtons(errorText)}</DialogFooter>
-      </>
-    );
-  };
-
   return (
     <Dialog dialogCss={() => ({ width: '400px' })} open={true}>
       <DialogHeader style={{ flexDirection: 'column' }}>
@@ -93,7 +38,42 @@ export default function HeadlessRequestDialog({
           Host {ipAddress} wants to execute a command
         </DialogTitle>
       </DialogHeader>
-      {dialogContent()}
+      <DialogContent mb={6}>
+        {errorText && (
+          <Danger mt={2} width="100%">
+            {errorText}
+          </Danger>
+        )}
+        <Text textAlign="center">
+          {errorText ? (
+            <>
+              The requested session doesn't exist or is invalid. Please generate
+              a new request.
+              <br />
+              <br />
+              You can close this window.
+            </>
+          ) : (
+            <>
+              Someone has initiated a command from {ipAddress}. If it was not
+              you, click Reject and contact your administrator.
+              <br />
+              <br />
+              If it was you, please use your hardware key to approve.
+            </>
+          )}
+        </Text>
+      </DialogContent>
+      <DialogFooter textAlign="center">
+        {!errorText && (
+          <>
+            <ButtonPrimary onClick={onAccept} autoFocus mr={3} width="130px">
+              Approve
+            </ButtonPrimary>
+            <ButtonSecondary onClick={onReject}>Reject</ButtonSecondary>
+          </>
+        )}
+      </DialogFooter>
     </Dialog>
   );
 }
