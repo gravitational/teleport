ae098bd6a641496b63e3515212fac5e98d15f13c
Tim Ross <tim.ross@goteleport.com>
2023-03-23 14:35:32 -0400

    Intern compiled regular expressions
    Profiles of the benchmark test revealed that the `regexp.Compile`
    done within `utils.matchString` was the most cpu and memory intensive
    portion of the tests. By leveraging a `lru.Cache` to intern the
    compiled regular expressions we get quite a performance improvement.

--- lib/auth/auth_with_roles_test.go
+++ lib/auth/auth_with_roles_test.go
@@ -1154,10 +1154,10 @@ func TestSessionRecordingConfigRBAC(t *testing.T) {
 // pkg: github.com/gravitational/teleport/lib/auth
 // cpu: Intel(R) Core(TM) i9-9880H CPU @ 2.30GHz
 // BenchmarkListNodes
-// BenchmarkListNodes-16    	       1	8214856209 ns/op	10052464312 B/op	103378939 allocs/op
+// BenchmarkListNodes-16    	       1	1191177699 ns/op	611828912 B/op	10555825 allocs/op
 // PASS
-// ok  	github.com/gravitational/teleport/lib/auth	10.985s
-// go test ./lib/auth -bench=. -run=^$ -v  52.00s user 6.32s system 242% cpu 24.085 total
+// ok  	github.com/gravitational/teleport/lib/auth	4.522s
+// go test ./lib/auth -bench=. -run=^$ -v  28.93s user 5.99s system 157% cpu 22.191 total
 func BenchmarkListNodes(b *testing.B) {
 	const nodeCount = 50_000
 	const roleCount = 32
