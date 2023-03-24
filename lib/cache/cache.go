d1708a3095b5f6c752df59144b8adcca9cba9245
Michael Wilson <mike@mdwn.dev>
2023-03-24 13:19:30 -0400

    Refactor UserGroups local service to use generic service. (#23548)
    * Refactor UserGroups local service to use generic service.
    
    The UserGroups local service now uses the generic service. This came about
    due to a bug in the pagination logic for the user group listing. This fixes
    that issue and simplifies the user group service logic.
    
    * Turn *Svc vars into *Cache vars to make things uniform.

--- lib/cache/cache.go
+++ lib/cache/cache.go
@@ -802,7 +802,13 @@ func New(config Config) (*Cache, error) {
 		return nil, trace.Wrap(err)
 	}
 
-	oktaSvc, err := local.NewOktaService(config.Backend)
+	userGroupsCache, err := local.NewUserGroupService(config.Backend)
+	if err != nil {
+		cancel()
+		return nil, trace.Wrap(err)
+	}
+
+	oktaCache, err := local.NewOktaService(config.Backend)
 	if err != nil {
 		cancel()
 		return nil, trace.Wrap(err)
@@ -833,8 +839,8 @@ func New(config Config) (*Cache, error) {
 		webTokenCache:                local.NewIdentityService(config.Backend).WebTokens(),
 		windowsDesktopsCache:         local.NewWindowsDesktopService(config.Backend),
 		samlIdPServiceProvidersCache: samlIdPServiceProvidersCache,
-		userGroupsCache:              local.NewUserGroupService(config.Backend),
-		oktaCache:                    oktaSvc,
+		userGroupsCache:              userGroupsCache,
+		oktaCache:                    oktaCache,
 		eventsFanout:                 services.NewFanoutSet(),
 		Logger: log.WithFields(log.Fields{
 			trace.Component: config.Component,
