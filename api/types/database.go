b4649477341a1130bbff7bedd363e514e7598100
Gavin Frazar <gavin.frazar@goteleport.com>
2023-03-29 13:35:54 -0700

    support readable enum values in database tls mode (#23601)
    * support readable enum values in database tls mode
    
    * propagate process.ExitContext() to reversetunnel server
    
    * fix lint
    
    * fix test

--- api/types/database.go
+++ api/types/database.go
@@ -17,6 +17,7 @@ limitations under the License.
 package types
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 	"time"
@@ -845,3 +846,91 @@ func (d Databases) Less(i, j int) bool { return d[i].GetName() < d[j].GetName() 
 
 // Swap swaps two databases.
 func (d Databases) Swap(i, j int) { d[i], d[j] = d[j], d[i] }
+
+// MarshalJSON marshals DatabaseTLSMode to string.
+func (d DatabaseTLSMode) MarshalJSON() ([]byte, error) {
+	val, err := d.encode()
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	out, err := json.Marshal(val)
+	return out, trace.Wrap(err)
+}
+
+// UnmarshalJSON supports parsing DatabaseTLSMode from number or string.
+func (d *DatabaseTLSMode) UnmarshalJSON(data []byte) error {
+	type loopBreaker DatabaseTLSMode
+	var val loopBreaker
+	// try as number first.
+	if err := json.Unmarshal(data, &val); err == nil {
+		*d = DatabaseTLSMode(val)
+		return nil
+	}
+
+	// fallback to string.
+	var s string
+	if err := json.Unmarshal(data, &s); err != nil {
+		return trace.Wrap(err)
+	}
+	return d.decodeName(s)
+}
+
+// MarshalYAML marshals DatabaseTLSMode to string.
+func (d DatabaseTLSMode) MarshalYAML() (interface{}, error) {
+	val, err := d.encode()
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	return val, nil
+}
+
+// UnmarshalYAML supports parsing DatabaseTLSMode from number or string.
+func (d *DatabaseTLSMode) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	// try as number first.
+	type loopBreaker DatabaseTLSMode
+	var val loopBreaker
+	if err := unmarshal(&val); err == nil {
+		*d = DatabaseTLSMode(val)
+		return nil
+	}
+
+	// fallback to string.
+	var s string
+	if err := unmarshal(&s); err != nil {
+		return trace.Wrap(err)
+	}
+	return d.decodeName(s)
+}
+
+// decodeName decodes DatabaseTLSMode from a string. This is necessary for
+// allowing tctl commands to work with the same names as documented in Teleport
+// configuration, rather than requiring it be specified as an unreadable enum
+// number.
+func (d *DatabaseTLSMode) decodeName(name string) error {
+	switch name {
+	case "verify-full", "":
+		*d = DatabaseTLSMode_VERIFY_FULL
+		return nil
+	case "verify-ca":
+		*d = DatabaseTLSMode_VERIFY_CA
+		return nil
+	case "insecure":
+		*d = DatabaseTLSMode_INSECURE
+		return nil
+	}
+	return trace.BadParameter("DatabaseTLSMode invalid value %v", d)
+}
+
+// encode RequireMFAType into a string. This allows users to see a readable,
+// documented value for the setting using tsh db ls or tctl get db.
+func (d DatabaseTLSMode) encode() (string, error) {
+	switch d {
+	case DatabaseTLSMode_VERIFY_FULL:
+		return "verify-full", nil
+	case DatabaseTLSMode_VERIFY_CA:
+		return "verify-ca", nil
+	case DatabaseTLSMode_INSECURE:
+		return "insecure", nil
+	}
+	return "", trace.BadParameter("DatabaseTLSMode invalid value %v", d)
+}
