32549933a8ce8b0a33ac9448b8df82e91ee66bc9
Alan Parra <alan.parra@goteleport.com>
2023-03-31 12:47:05 -0300

    Test device conversions

--- api/types/device_test.go
+++ api/types/device_test.go
@@ -16,9 +16,16 @@ package types
 
 import (
 	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/testing/protocmp"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/gravitational/teleport/api/defaults"
-	"github.com/stretchr/testify/require"
+	devicepb "github.com/gravitational/teleport/api/gen/proto/go/teleport/devicetrust/v1"
 )
 
 // TestUnmarshalDevice tests that devices can be successfully
@@ -88,5 +95,56 @@ func TestUnmarshalDevice(t *testing.T) {
 			require.NoError(t, err, "UnmarshalDevice returned unexpected error")
 			require.Equal(t, tc.expected, out, "unmarshalled device  does not match what was expected")
 		})
+	}
+}
+
+func TestDeviceConversions_toAndFrom(t *testing.T) {
+	t1 := time.UnixMilli(1680276526972000) // Fri Mar 31 2023 15:28:46 UTC
+	t11 := t1.Add(100 * time.Millisecond)
+	t2 := t1.Add(1 * time.Second)
+	t22 := t1.Add(100 * time.Millisecond)
+
+	const osType = devicepb.OSType_OS_TYPE_MACOS
+	const assetTag = "llama14"
+	dev := &devicepb.Device{
+		ApiVersion:   "v1",
+		Id:           "0af7c335-5f2c-4756-8266-9965a47ccbd3",
+		OsType:       osType,
+		AssetTag:     assetTag,
+		CreateTime:   timestamppb.New(t1),
+		UpdateTime:   timestamppb.New(t2),
+		EnrollStatus: devicepb.DeviceEnrollStatus_DEVICE_ENROLL_STATUS_ENROLLED,
+		Credential: &devicepb.DeviceCredential{
+			Id:           "557762f0-4cd4-4b75-aaee-575c57237c0b",
+			PublicKeyDer: []byte("insert public key here"),
+		},
+		CollectedData: []*devicepb.DeviceCollectedData{
+			{
+				CollectTime:  timestamppb.New(t1),
+				RecordTime:   timestamppb.New(t11),
+				OsType:       osType,
+				SerialNumber: assetTag,
+			},
+			{
+				CollectTime:  timestamppb.New(t2),
+				RecordTime:   timestamppb.New(t22),
+				OsType:       osType,
+				SerialNumber: assetTag,
+			},
+		},
+	}
+
+	gotRes := DeviceToResource(dev)
+	// Assert some of the more "unusual" or missing fields.
+	// We know other information isn't lost because of the conversion below,
+	// therefore it must be present in the resource.
+	assert.Equal(t, dev.ApiVersion, gotRes.Version, "resource.Version is not the ApiVersion")
+	assert.Equal(t, dev.Id, gotRes.Metadata.Name, "resource.Metadata.Name is not the Id")
+	assert.NotEmpty(t, gotRes.Metadata.Namespace, "resource.Metadata.Namespace")
+
+	gotDev, err := DeviceFromResource(gotRes)
+	require.NoError(t, err, "DeviceFromResource failed")
+	if diff := cmp.Diff(dev, gotDev, protocmp.Transform()); diff != "" {
+		t.Errorf("DeviceFromResource mismatch (-want +got)\n%s", diff)
 	}
 }
