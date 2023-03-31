34e9502269052e50abfc81fed62259de18c82075
Justinas Stankevičius <justinas@users.noreply.github.com>
2023-03-31 14:08:27 +0300

    [v12] Add gRPC service definition for Plugin resources (#21750) (#23780)
    * Add plugin exchange service
    
    * Add Plugin methods to auth
    
    * Add gRPC-layer methods for Plugin
    
    * Add RBAC presets for Plugin
    
    * Test GetPlugin()/NoSecrets access
    
    * Make error assertions more correct in role test
    
    * Deny setting credentials if user can not read them
    
    * gofmt
    
    * Apply minor suggestions from code review
    
    
    
    
    * Move dependency into an existing block in go.mod
    
    * Improve error messages for failed type assertions
    
    * DRY WithSecrets access checks for Plugins
    
    * Run new tests in parallel
    
    * Improve error assertions in auth_with_roles_test
    
    * TestGetPluginWithSecrets: split cases to subtests
    
    * Clean up test servers and clients
    
    * Add proto for plugin service
    
    * Remove Plugin methods from auth service
    
    Moved to a dedicated service
    
    * Remove plugin-related auth methods
    
    Moved to a dedicated service in Enterprise
    
    * Remove CreatePlugin test from auth_with_roles_test
    
    Moved to a dedicated service in Enterprise
    
    * Pass "backend getter" to local plugins service
    
    This pattern is used in Enterprise to set up secondary services
    before auth (and backend) are created.
    
    * Rename InitialCredentials to BootstrapCredentials
    
    * Add plugins service to genproto.sh
    
    * Reformat generated proto
    
    * Remove obsolete PluginExchangeService
    
    The equivalent of this is now in Enterprise
    
    * Add kube service to genproto.sh; regenerate
    
    * Add ListPlugins to plugin backend service
    
    * Reimplement GetPlugins on top of ListPlugins
    
    This is a "convenience" implementation for the backend service layer.
    
    * Replace GetPlugins with ListPlugins in gRPC schema
    
    * Fix ListKubernetesResources unit test
    
    * Simplify plugin pagination key to just the name
    
    * Use existing constant for page size
    
    * Make dummy clients return errors instead of panic
    
    * Remove obsolete field
    
    * Ensure go.mod is valid for corresponding e changes
    
    * Fix passing mutex ref
    
    * Move teleport-plugins import to e_imports
    
    * Revert oauth change in go.mod
    
    * Use limit+1 to look-ahead when paginating plugins
    
    * Test plugin pagination with pageSize > numPlugins
    
    * Add descriptive messages to gRPC dummy clients
    
    * Plugin: add RW for editor; remove secrets from gRPC
    
    * Make message more descriptive for dummy gRPC conn
    
    ---------
    
    Co-authored-by: rosstimothy <39066650+rosstimothy@users.noreply.github.com>
    Co-authored-by: Tiago Silva <tiago.silva@goteleport.com>

--- lib/auth/auth_with_roles_test.go
+++ lib/auth/auth_with_roles_test.go
@@ -4396,3 +4396,31 @@ func modifyAndWaitForEvent(t *testing.T, errFn require.ErrorAssertionFunc, clien
 	}
 	return nil
 }
+
+func TestUnimplementedClients(t *testing.T) {
+	ctx := context.Background()
+	testAuth, err := NewTestAuthServer(TestAuthServerConfig{Dir: t.TempDir()})
+	server := &ServerWithRoles{
+		authServer: testAuth.AuthServer,
+	}
+
+	require.NoError(t, err)
+
+	t.Run("DevicesClient", func(t *testing.T) {
+		_, err := server.DevicesClient().ListDevices(ctx, nil)
+		require.Error(t, err)
+		require.True(t, trace.IsNotImplemented(err), err)
+	})
+
+	t.Run("LoginRuleClient", func(t *testing.T) {
+		_, err := server.LoginRuleClient().ListLoginRules(ctx, nil)
+		require.Error(t, err)
+		require.True(t, trace.IsNotImplemented(err), err)
+	})
+
+	t.Run("PluginClient", func(t *testing.T) {
+		_, err := server.PluginsClient().ListPlugins(ctx, nil)
+		require.Error(t, err)
+		require.True(t, trace.IsNotImplemented(err), err)
+	})
+}
