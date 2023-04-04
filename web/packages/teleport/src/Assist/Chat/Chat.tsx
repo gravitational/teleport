251b2957e72448088d9f341c1412cabfca0ed3fa
Ryan Clark <ryan.clark@goteleport.com>
2023-04-04 15:18:32 +0100

    Add license headers

--- web/packages/teleport/src/Assist/Chat/Chat.tsx
+++ web/packages/teleport/src/Assist/Chat/Chat.tsx
@@ -1,11 +1,23 @@
+/*
+Copyright 2023 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 import React, { useCallback, useEffect, useRef } from 'react';
 import styled, { keyframes } from 'styled-components';
 
-import { Dots } from 'teleport/Assist/Dots';
-
 import { useMessages } from '../contexts/messages';
-
-import { Author, Type } from '../services/messages';
 
 import { ChatBox } from './ChatBox';
 import { ChatItem } from './ChatItem';
