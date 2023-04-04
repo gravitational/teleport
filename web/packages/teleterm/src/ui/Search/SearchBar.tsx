b3718fe781bb5f919a56bc6230c421f776c5f60d
Rafał Cieślak <rafal.cieslak@goteleport.com>
2023-04-04 15:42:11 +0200

    Connect: Add SearchBar
    This commit adds an MVP of the search bar to Connect. Currently it's
    behind a feature flag (`feature.searchBar`) but we'll enable it by
    default before the release. The plan was to merge the code ASAP,
    potentially even straight to v12 when we have the chance, which didn't
    exactly pan out but there's no harm to having this feature flag for now.
    
    On top of that, this commit adds a new shortcut to open the search bar
    (this replaces the current shortcut to open the command bar) and a
    shortcut to open a new terminal tab.
    
    The search works by essentially making a `ListResources` request for
    each supported resource type to every cluster the user is logged in to.
    We repurposed the old command palette UI for that but rewritten it to
    use React context and hooks rather than a class and a store. This
    allowed us to be a little bit more flexible as the old approach required
    every picker to conform to the same interface, both in terms of UI and
    code.
    
    This implementation has two main pickers so far:
    
    * `ActionPicker` which is the main one. It searches for resources but at
      the moment it also supports applying filters. In the future, we plan
      to add more actions to it such as "Open a new tab" or "Install tsh".
    * `ParameterPicker` is activated when you pick an action from the
      `ActionPicker` that requires an additional parameter. Think choosing
      an SSH server or a db – you need to provide an SSH login or a db user
      for those item. In those situations, `ActionPicker` will switch to
      `ParameterPicker` and let you pick a relevant item from the list.
    
    Everything is contained within `web/packages/teleterm/src/ui/Search`.
    Arguably, `useSearch` could be refactored a little bit to maybe make its
    structure a little more clear as it handles both the resource search and
    the filter search. However, at the moment we're not totally sure how the
    search bar will evolve, so we want to leave any bigger refactors for
    later. We added a couple of basic tests for regressions that happened so
    far. We also have stories for the items from the action picker.
    
    Error handling will be added in an upcoming PR. Docs updates will be
    done in a separate PR as well.
    
    Co-authored-by: Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>

---
+++ web/packages/teleterm/src/ui/Search/SearchBar.tsx
@@ -0,0 +1,172 @@
+/**
+ * Copyright 2023 Gravitational, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { useRef, useEffect } from 'react';
+import styled from 'styled-components';
+import { Box, Flex } from 'design';
+import { space, width, color, height } from 'styled-system';
+
+import {
+  SearchContextProvider,
+  useSearchContext,
+} from 'teleterm/ui/Search/SearchContext';
+import { KeyboardShortcutAction } from 'teleterm/services/config';
+import {
+  useKeyboardShortcutFormatters,
+  useKeyboardShortcuts,
+} from 'teleterm/ui/services/keyboardShortcuts';
+
+import { useAppContext } from '../appContextProvider';
+
+const OPEN_COMMAND_BAR_SHORTCUT_ACTION: KeyboardShortcutAction =
+  'openCommandBar';
+
+export function SearchBarConnected() {
+  const { workspacesService } = useAppContext();
+  workspacesService.useState();
+
+  if (!workspacesService.getRootClusterUri()) {
+    return null;
+  }
+
+  return (
+    <SearchContextProvider>
+      <SearchBar />
+    </SearchContextProvider>
+  );
+}
+
+function SearchBar() {
+  const containerRef = useRef<HTMLElement>();
+  const { getAccelerator } = useKeyboardShortcutFormatters();
+  const {
+    activePicker,
+    inputValue,
+    onInputValueChange,
+    inputRef,
+    opened,
+    open,
+    close,
+  } = useSearchContext();
+  const ctx = useAppContext();
+  ctx.clustersService.useState();
+
+  useKeyboardShortcuts({
+    [OPEN_COMMAND_BAR_SHORTCUT_ACTION]: () => {
+      open();
+    },
+  });
+
+  useEffect(() => {
+    const onClickOutside = e => {
+      if (!e.composedPath().includes(containerRef.current)) {
+        close();
+      }
+    };
+    if (opened) {
+      window.addEventListener('click', onClickOutside);
+      return () => window.removeEventListener('click', onClickOutside);
+    }
+  }, [close, opened]);
+
+  function handleOnFocus(e: React.FocusEvent) {
+    open(e.relatedTarget);
+  }
+
+  const defaultInputProps = {
+    ref: inputRef,
+    role: 'searchbox',
+    placeholder: activePicker.placeholder,
+    value: inputValue,
+    onChange: e => {
+      onInputValueChange(e.target.value);
+    },
+    spellCheck: false,
+  };
+
+  return (
+    <Flex
+      css={`
+        position: relative;
+        flex: 4;
+        flex-shrink: 1;
+        min-width: calc(${props => props.theme.space[7]}px * 2);
+        height: 100%;
+        background: ${props => props.theme.colors.levels.surface};
+        border: 1px ${props => props.theme.colors.action.disabledBackground}
+          solid;
+      `}
+      justifyContent="center"
+      ref={containerRef}
+      onFocus={handleOnFocus}
+    >
+      {!opened && (
+        <>
+          <Input {...defaultInputProps} />
+          <Shortcut>
+            {getAccelerator(OPEN_COMMAND_BAR_SHORTCUT_ACTION)}
+          </Shortcut>
+        </>
+      )}
+      {opened && (
+        <activePicker.picker
+          // autofocusing cannot be done in `open` function as it would focus the input from closed state
+          input={<Input {...defaultInputProps} autoFocus={true} />}
+        />
+      )}
+    </Flex>
+  );
+}
+
+const Input = styled.input(props => {
+  const { theme } = props;
+  return {
+    height: '100%',
+    background: theme.colors.levels.sunkenSecondary,
+    boxSizing: 'border-box',
+    color: theme.colors.text.primary,
+    width: '100%',
+    outline: 'none',
+    border: 'none',
+    padding: `${theme.space[1]}px ${theme.space[2]}px`,
+    '&:hover, &:focus': {
+      color: theme.colors.text.contrast,
+      background: theme.colors.levels.surface,
+
+      opacity: 1,
+    },
+    '::placeholder': {
+      color: theme.colors.text.secondary,
+    },
+
+    ...space(props),
+    ...width(props),
+    ...height(props),
+    ...color(props),
+  };
+});
+
+const Shortcut = styled(Box).attrs({ p: 1 })`
+  position: absolute;
+  right: ${props => props.theme.space[2]}px;
+  top: 50%;
+  transform: translate(0, -50%);
+  color: ${({ theme }) => theme.colors.text.secondary};
+  background-color: ${({ theme }) => theme.colors.levels.surface};
+  line-height: 12px;
+  font-size: 12px;
+  border-radius: 2px;
+`;
