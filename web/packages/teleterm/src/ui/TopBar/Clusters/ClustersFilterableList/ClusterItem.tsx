d3c41495839a4f735310577e6c8dd6b49f6cd4f1
Rafał Cieślak <rafal.cieslak@goteleport.com>
2023-04-06 10:17:03 +0000

    ClusterItem: Remove usage of colors.secondary.lighter (#24182)

--- web/packages/teleterm/src/ui/TopBar/Clusters/ClustersFilterableList/ClusterItem.tsx
+++ web/packages/teleterm/src/ui/TopBar/Clusters/ClustersFilterableList/ClusterItem.tsx
@@ -91,16 +91,10 @@ function getBackgroundColor(props) {
     }
     return props.theme.colors.brand.main;
   }
-  if (props.isActive) {
-    return props.theme.colors.secondary.lighter;
-  }
 }
 
 function getHoverBackgroundColor(props) {
   if (props.isSelected) {
     return props.theme.colors.brand.accent;
-  }
-  if (props.isActive) {
-    return props.theme.colors.secondary.lighter;
   }
 }
