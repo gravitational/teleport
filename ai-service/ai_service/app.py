0fb031c582e59339d162c1249adc188dc06aaf95
Joel Wejdenstål <jwejdenstal@icloud.com>
2023-04-12 18:48:15 +0200

    expose as flask

--- ai-service/ai_service/app.py
+++ ai-service/ai_service/app.py
@@ -1,4 +1,5 @@
 import json
+from flask import Flask, request
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain import PromptTemplate, LLMChain
@@ -8,31 +9,32 @@ from langchain.prompts.chat import (
     AIMessagePromptTemplate,
     HumanMessagePromptTemplate,
 )
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+import ai_service.model as model
 
-llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0.3)
-chat_llm = ChatOpenAI(temperature=0.3)
+app = Flask(__name__)
 
-def get_messages():
-    messages = []
-    with open("messages.json", "r") as f:
-        raw = json.load(f)
-        for raw_message in raw["messages"]:
-            match raw_message["kind"]:
-                case "human":
-                    messages.append(HumanMessage(content=raw_message["text"]))
-                case "ai":
-                    messages.append(AIMessage(content=raw_message["text"]))
-                case "system":
-                    messages.append(SystemMessage(content=raw_message["text"]))
 
-    return messages
+@app.route("/")
+def root():
+    return "Hello, World!"
 
-def run():
-    messages = get_messages()
+
+llm = OpenAI(model_name="gpt-4", temperature=0.1)
+chat_llm = ChatOpenAI(temperature=0.1)
+
+
+@app.route("/assistant_query", methods=["POST"])
+def assistant_query():
+    messages = model.context(username=request.json["username"])
+    for raw_message in request.json["messages"]:
+        match raw_message["kind"]:
+            case "human":
+                messages.append(HumanMessage(content=raw_message["text"]))
+            case "ai":
+                messages.append(AIMessage(content=raw_message["text"]))
+            case "system":
+                messages.append(SystemMessage(content=raw_message["text"]))
+
     completion = chat_llm(messages)
-    print(completion.content)
+    return completion.content
