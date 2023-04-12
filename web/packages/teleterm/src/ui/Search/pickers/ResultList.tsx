9539b0042f19dc87db030620ee070219dec19ae3
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2023-04-12 16:51:52 +0200

    Use `active` prop only for `InteractiveItem`

--- web/packages/teleterm/src/ui/Search/pickers/ResultList.tsx
+++ web/packages/teleterm/src/ui/Search/pickers/ResultList.tsx
@@ -21,7 +21,7 @@ import React, {
   useRef,
   useState,
 } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { Attempt } from 'shared/hooks/useAsync';
 
@@ -119,7 +119,7 @@ export function ResultList<T>(props: ResultListProps<T>) {
             <InteractiveItem
               ref={isActive ? activeItemRef : null}
               role="menuitem"
-              $active={isActive}
+              active={isActive}
               key={key}
               onClick={() => props.onPick(r)}
             >
@@ -145,18 +145,24 @@ export const NonInteractiveItem = styled.div`
 
   padding: ${props => props.theme.space[2]}px;
   color: ${props => props.theme.colors.text.contrast};
-  background: ${props =>
-    props.$active
-      ? props.theme.colors.levels.elevated
-      : props.theme.colors.levels.surface};
+  background: ${props => props.theme.colors.levels.surface};
 `;
 
 const InteractiveItem = styled(NonInteractiveItem)`
+  cursor: pointer;
+
   &:hover,
   &:focus {
-    cursor: pointer;
     background: ${props => props.theme.colors.levels.elevated};
   }
+
+  ${props => {
+    if (props.active) {
+      return css`
+        background: ${props => props.theme.colors.levels.elevated};
+      `;
+    }
+  }}
 `;
 
 function getNext(selectedIndex = 0, max = 0) {
