43df598511e38de397643c2b283e6668fce02fc8
Lisa Kim <lisa@goteleport.com>
2023-04-12 09:51:36 -0700

    Add a success dialog after creating integration

--- web/packages/teleport/src/Integrations/Enroll/AwsOidc/instructions/SeventhStageInstructions.tsx
+++ web/packages/teleport/src/Integrations/Enroll/AwsOidc/instructions/SeventhStageInstructions.tsx
@@ -1,47 +1,79 @@
-import React, { useCallback, useState } from 'react';
-
-import Text from 'design/Text';
-import Box from 'design/Box';
-
-import { ButtonPrimary } from 'design';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router';
+import { Link } from 'react-router-dom';
+import { Danger } from 'design/Alert';
+import { CircleCheck } from 'design/Icon';
+import { ButtonPrimary, ButtonSecondary, Text, Flex, Box } from 'design';
+import Dialog, {
+  DialogHeader,
+  DialogContent,
+  DialogFooter,
+} from 'design/Dialog';
 
 import FieldInput from 'shared/components/FieldInput';
-import Validation from 'shared/components/Validation';
+import Validation, { Validator } from 'shared/components/Validation';
+import useAttempt from 'shared/hooks/useAttemptNext';
 
 import { requiredField } from 'shared/components/Validation/rules';
 
+import {
+  IntegrationKind,
+  integrationService,
+} from 'teleport/services/integrations';
+import cfg from 'teleport/config';
+
 import { InstructionsContainer } from './common';
 
-import type { CommonInstructionsProps } from './common';
+export function SeventhStageInstructions() {
+  const location = useLocation<{ discoverEventId: string }>();
 
-export function SeventhStageInstructions(props: CommonInstructionsProps) {
-  const [roleARN, setRoleARN] = useState('');
+  const { attempt, setAttempt } = useAttempt('');
+  const [showConfirmBox, setShowConfirmBox] = useState(false);
+  const [roleArn, setRoleArn] = useState('');
   const [name, setName] = useState('');
 
-  const handleSubmit = useCallback(() => {
-    // TODO: create the integration
-  }, [roleARN]);
+  function handleSubmit(validator: Validator) {
+    if (!validator.validate()) {
+      return;
+    }
+
+    setAttempt({ status: 'processing' });
+    integrationService
+      .createIntegration({
+        name,
+        subKind: IntegrationKind.AwsOidc,
+        awsoidc: { roleArn },
+      })
+      .then(() => setShowConfirmBox(true))
+      .catch((err: Error) =>
+        setAttempt({ status: 'failed', statusText: err.message })
+      );
+  }
 
   return (
     <InstructionsContainer>
+      {attempt.status === 'failed' && (
+        <Danger mb={5}>{attempt.statusText}</Danger>
+      )}
       <Text>From the list of roles, select the role you just created</Text>
-
-      <Text mt={5}>Copy the role ARN and paste it below</Text>
 
       <Validation>
         {({ validator }) => (
           <>
-            <Box mt={5}>
+            <Text mt={5}>Copy the role ARN and paste it below</Text>
+            <Box mt={2}>
               <FieldInput
-                onChange={e => setRoleARN(e.target.value)}
-                value={roleARN}
+                label="Role ARN"
+                onChange={e => setRoleArn(e.target.value)}
+                value={roleArn}
                 placeholder="Role ARN"
                 rule={requiredField('Role ARN is required')}
               />
             </Box>
             <Text mt={5}>Give this AWS integration a name</Text>
-            <Box mt={5}>
+            <Box mt={2}>
               <FieldInput
+                label="Name"
                 onChange={e => setName(e.target.value)}
                 value={name}
                 placeholder="Integration name"
@@ -49,11 +81,79 @@ export function SeventhStageInstructions(props: CommonInstructionsProps) {
               />
             </Box>
             <Box mt={5}>
-              <ButtonPrimary onClick={() => handleSubmit()}>Next</ButtonPrimary>
+              <ButtonPrimary
+                onClick={() => handleSubmit(validator)}
+                disabled={attempt.status === 'processing'}
+              >
+                Next
+              </ButtonPrimary>
             </Box>
           </>
         )}
       </Validation>
+      {showConfirmBox && (
+        <SuccessfullyAddedIntegrationDialog
+          discoverEventId={location.state?.discoverEventId}
+          integrationName={name}
+        />
+      )}
     </InstructionsContainer>
+  );
+}
+
+export function SuccessfullyAddedIntegrationDialog({
+  discoverEventId,
+  integrationName,
+}: {
+  discoverEventId: string;
+  integrationName: string;
+}) {
+  return (
+    <Dialog
+      dialogCss={() => ({ maxWidth: '500px', width: '100%' })}
+      disableEscapeKeyDown={true}
+      onClose={close}
+      open={true}
+    >
+      <DialogHeader css={{ margin: '0 auto' }}>
+        <CircleCheck mb={4} fontSize={60} color="success" />
+      </DialogHeader>
+      <DialogContent>
+        <Text textAlign="center">
+          AWS integration "{integrationName}" successfully added
+        </Text>
+      </DialogContent>
+      <DialogFooter css={{ margin: '0 auto' }}>
+        {discoverEventId ? (
+          <ButtonPrimary
+            size="large"
+            as={Link}
+            to={{
+              pathname: cfg.routes.discover,
+              state: {
+                integrationSubKind: IntegrationKind.AwsOidc,
+                integrationName,
+                discoverEventId,
+              },
+            }}
+          >
+            Begin RDS Enrollment
+          </ButtonPrimary>
+        ) : (
+          <Flex gap="3">
+            <ButtonPrimary as={Link} to={cfg.routes.integrations} size="large">
+              Go to Integration List
+            </ButtonPrimary>
+            <ButtonSecondary
+              as={Link}
+              to={cfg.getIntegrationEnrollRoute(null)}
+              size="large"
+            >
+              Add Another Integration
+            </ButtonSecondary>
+          </Flex>
+        )}
+      </DialogFooter>
+    </Dialog>
   );
 }
