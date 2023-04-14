295bf731e49532f5f464c2ff1f9ab09873d1d71c
Marek Smoliński <marek@goteleport.com>
2023-04-14 20:37:05 +0200

    Try to fix TestAgentPoolConnectionCount (#24616)

--- lib/reversetunnel/agentpool.go
+++ lib/reversetunnel/agentpool.go
@@ -379,7 +379,7 @@ func (p *AgentPool) isAgentRequired() bool {
 		return true
 	}
 
-	return p.active.len() < p.runtimeConfig.connectionCount
+	return p.active.len() < p.runtimeConfig.getConnectionCount()
 }
 
 // disconnectAgents handles disconnecting agents that are no longer required.
@@ -624,6 +624,12 @@ func (c *agentPoolRuntimeConfig) restrictConnectionCount() bool {
 		return false
 	}
 	return c.tunnelStrategyType == types.ProxyPeering
+}
+
+func (c *agentPoolRuntimeConfig) getConnectionCount() int {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.connectionCount
 }
 
 // useReverseTunnelV2 returns true if reverse tunnel should be used.
