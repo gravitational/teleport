9f5a81b3f1aa3e0f3de937d5e6c8e087debc4f12
Brian Joerger <bjoerger@goteleport.com>
2023-04-17 13:35:15 -0700

    Fix `TestHeadlessAuthenticationWatcher` flakiness (#24562)
    * Extend time between put events in headless watcher test to avoid marking waiters as stale by accident.
    
    * Restructure headless authentication watcher to eliminate race conditions in tests.
    
    * Resolve comments.
    
    * Resolve comments.

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -5109,13 +5109,19 @@ func (a *Server) GetHeadlessAuthentication(ctx context.Context, name string) (*t
 		return nil, trace.Wrap(err)
 	}
 
-	// wait for the headless authentication to be updated with valid login details
-	// by the login process. If the headless authentication is already updated,
-	// Wait will return it immediately.
+	sub, err := a.headlessAuthenticationWatcher.Subscribe(ctx, name)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	defer sub.Close()
+
 	waitCtx, cancel := context.WithTimeout(ctx, defaults.HTTPRequestTimeout)
 	defer cancel()
 
-	headlessAuthn, err := a.headlessAuthenticationWatcher.Wait(waitCtx, name, func(ha *types.HeadlessAuthentication) (bool, error) {
+	// wait for the headless authentication to be updated with valid login details
+	// by the login process. If the headless authentication is already updated,
+	// Wait will return it immediately.
+	headlessAuthn, err := a.headlessAuthenticationWatcher.WaitForUpdate(waitCtx, sub, func(ha *types.HeadlessAuthentication) (bool, error) {
 		return services.ValidateHeadlessAuthentication(ha) == nil, nil
 	})
 	return headlessAuthn, trace.Wrap(err)
