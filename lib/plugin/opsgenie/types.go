0022ee0fe2749c3c9cc392b7b705cabcb8b3f290
EdwardDowling <edward.dowling@goteleport.com>
2023-04-18 14:07:06 +0100

    Add missing godocs to opsgenie types

--- lib/plugin/opsgenie/types.go
+++ lib/plugin/opsgenie/types.go
@@ -18,32 +18,47 @@ package opsgenie
 
 // Alert represents an Opsgenie alert
 type Alert struct {
-	ID       string  `json:"id"`
-	Title    string  `json:"title"`
-	Status   string  `json:"status"`
-	AlertKey string  `json:"alert_key"`
-	Body     Details `json:"body"`
+	// ID is the id of the Opsgenie alert.
+	ID string `json:"id"`
+	// Title is the title of the Opsgenie alert.
+	Title string `json:"title"`
+	// Status is the curerent status of the Opsgenie alert.
+	Status string `json:"status"`
+	// AlertKey is the key of the Opsgenie alert.
+	AlertKey string `json:"alert_key"`
+	// Details are a map of key-value pairs to use as custom properties of the alert.
+	Detials map[string]string `json:"details"`
 }
 
 // AlertBody represents and Opsgenie alert body
 type AlertBody struct {
-	Message     string      `json:"message,omitempty"`
-	Alias       string      `json:"alias,omitempty"`
-	Description string      `json:"description,omitempty"`
-	Responders  []Responder `json:"responders,omitempty"`
-	Priority    string      `json:"priority,omitempty"`
+	// Message is the message the alert is created with.
+	Message string `json:"message,omitempty"`
+	// Alias is the client-defined identifier of the alert.
+	Alias string `json:"alias,omitempty"`
+	// Description field of the alert.
+	Description string `json:"description,omitempty"`
+	// Responders are the teams/users that the alert will be routed to send notifications.
+	Responders []Responder `json:"responders,omitempty"`
+	// Priority is the priority the alert is created with.
+	Priority string `json:"priority,omitempty"`
 }
 
 // Responder represents an Opsgenie responder
 type Responder struct {
-	Name     string `json:"name,omitempty"`
+	// Name is the name of the responder.
+	Name string `json:"name,omitempty"`
+	// Username is the opsgenie username of the responder.
 	Username string `json:"username,omitempty"`
-	Type     string `json:"type,omitempty"`
-	ID       string `json:"id,omitempty"`
+	// Type is the type of responder team/user
+	Type string `json:"type,omitempty"`
+	// ID is the ID of the responder.
+	ID string `json:"id,omitempty"`
 }
 
 // RespondersResult represents a group of Opsgenie responders
 type RespondersResult struct {
+	// Data is a wrapper around the OnCallRecipients.
 	Data struct {
 		OnCallRecipients []string `json:"onCallRecipients,omitempty"`
 	} `json:"data,omitempty"`
@@ -51,18 +66,16 @@ type RespondersResult struct {
 
 // AlertResult is a wrapper around Alert
 type AlertResult struct {
+	// Alert contians the actual alert data.
 	Alert Alert `json:"alert"`
 }
 
 // AlertNote represents an Opsgenie alert note
 type AlertNote struct {
-	User   string `json:"user"`
+	// User is the user that created the note.
+	User string `json:"user"`
+	// Source is the display name of the request source.
 	Source string `json:"source"`
-	Note   string `json:"note"`
-}
-
-// Details represents the details field of an Opsgenie alert
-type Details struct {
-	Type    string `json:"type,omitempty"`
-	Details string `json:"details,omitempty"`
+	// Note is the alert note.
+	Note string `json:"note"`
 }
