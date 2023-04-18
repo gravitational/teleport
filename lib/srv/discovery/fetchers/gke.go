bf5f33a59f603bf7f09f3cb19a23b3b73341c81c
Tiago Silva <tiago.silva@goteleport.com>
2023-04-18 10:33:14 +0100

    Prevent multiple discovery agents to race against each other (#24214)
    Currently, when multiple discovery services are running with different
    selectors, they will race against each other and delete the other
    resources.
    
    This PR fixes that behavior if the databases or kube clusters existent
    in auth do not match the current selectors, they will be ignored.
    
    This has a slight problem when a user stops a discovery agent, he must
    manually delete the resources created, otherwise they will be available
    forever.
    
    Fixes #23699

--- lib/srv/discovery/fetchers/gke.go
+++ lib/srv/discovery/fetchers/gke.go
@@ -18,6 +18,7 @@ package fetchers
 
 import (
 	"context"
+	"fmt"
 
 	containerpb "cloud.google.com/go/container/apiv1/containerpb"
 	"github.com/gravitational/trace"
@@ -114,17 +115,9 @@ func (a *gkeFetcher) Cloud() string {
 	return types.CloudGCP
 }
 
-// gcpLabelsToTeleportLabels converts GKE labels to a labels map.
-func (a *gkeFetcher) gcpLabelsToTeleportLabels(tags map[string]string) map[string]string {
-	labels := make(map[string]string)
-	for key, val := range tags {
-		if types.IsValidLabelKey(key) {
-			labels[key] = val
-		} else {
-			a.Log.Debugf("Skipping GKE tag %q, not a valid label key.", key)
-		}
-	}
-	return labels
+func (a *gkeFetcher) String() string {
+	return fmt.Sprintf("gkeFetcher(ProjectID=%v, Location=%v, FilterLabels=%v)",
+		a.ProjectID, a.Location, a.FilterLabels)
 }
 
 // getMatchingKubeCluster checks if the GKE cluster tags matches the GCP matcher
@@ -132,9 +125,12 @@ func (a *gkeFetcher) gcpLabelsToTeleportLabels(tags map[string]string) map[strin
 // If any cluster does not match the filtering criteria, this function returns
 // a “trace.CompareFailed“ error to distinguish filtering and operational errors.
 func (a *gkeFetcher) getMatchingKubeCluster(gkeCluster gcp.GKECluster) (types.KubeCluster, error) {
-	gkeCluster.Labels = a.gcpLabelsToTeleportLabels(gkeCluster.Labels)
+	cluster, err := services.NewKubeClusterFromGCPGKE(gkeCluster)
+	if err != nil {
+		return nil, trace.WrapWithMessage(err, "Unable to create types.KubernetesClusterV3 cluster from gcp.GKECluster.")
+	}
 
-	if match, reason, err := services.MatchLabels(a.FilterLabels, gkeCluster.Labels); err != nil {
+	if match, reason, err := services.MatchLabels(a.FilterLabels, cluster.GetAllLabels()); err != nil {
 		return nil, trace.WrapWithMessage(err, "Unable to match GKE cluster labels against match labels.")
 	} else if !match {
 		return nil, trace.CompareFailed("GKE cluster %q labels does not match the selector: %s", gkeCluster.Name, reason)
@@ -146,9 +142,5 @@ func (a *gkeFetcher) getMatchingKubeCluster(gkeCluster gcp.GKECluster) (types.Ku
 		return nil, trace.CompareFailed("GKE cluster %q not enrolled due to its current status: %s", gkeCluster.Name, st)
 	}
 
-	cluster, err := services.NewKubeClusterFromGCPGKE(gkeCluster)
-	if err != nil {
-		return nil, trace.WrapWithMessage(err, "Unable to create types.KubernetesClusterV3 cluster from gcp.GKECluster.")
-	}
 	return cluster, nil
 }
