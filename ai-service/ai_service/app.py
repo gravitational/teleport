b1911c1eddbfb118440811d7bb9075c7f39c48d2
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2023-04-19 00:30:02 -0400

    Convert python GRPC server to async (#24620)

--- ai-service/ai_service/app.py
+++ ai-service/ai_service/app.py
@@ -1,24 +1,24 @@
+import asyncio
 import json
 import logging
-from concurrent import futures
 
 import grpc
+from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
 
+import ai_service.gen.teleport.assistant.v1.assistant_pb2_grpc as assistant_grpc
+import ai_service.model as model
 from ai_service.gen.teleport.assistant.v1.assistant_pb2 import (
     CompleteRequest,
     CompletionResponse,
 )
-import ai_service.gen.teleport.assistant.v1.assistant_pb2_grpc as assistant_grpc
-import ai_service.model as model
-
-chat_llm = ChatOpenAI(model_name="gpt-4", temperature=0.5)
 
 DEFAULT_HELLO_MESSAGE = "Hey, I'm Teleport - a powerful tool that can assist you in managing your Teleport cluster via ChatGPT."
 
 
-def assistant_query(
+async def assistant_query(
+    chat_llm: ChatOpenAI,
     request: CompleteRequest,
 ) -> CompletionResponse:
     logging.debug(request)
@@ -39,7 +39,8 @@ def assistant_query(
                 messages.append(SystemMessage(content=raw_message.content))
 
     model.add_try_extract(messages)
-    completion = chat_llm(messages).content
+    result = await chat_llm.agenerate([messages])
+    completion = result.generations[0][0].message.content
 
     try:
         data = json.loads(completion)
@@ -54,20 +55,30 @@ def assistant_query(
 
 
 class AssistantServicer(assistant_grpc.AssistantServiceServicer):
-    def Complete(self, request, context):
-        return assistant_query(request)
+    def __init__(self) -> None:
+        self.chat_llm = ChatOpenAI(model_name="gpt-4", temperature=0.5)
+
+    async def Complete(
+        self, request: CompleteRequest, context: grpc.aio.ServicerContext
+    ):
+        return await assistant_query(self.chat_llm, request)
 
 
-def serve():
-    logging.info("Assist service started")
+async def serve():
+    logging.info("Starting Assist service")
 
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.aio.server()
     assistant_grpc.add_AssistantServiceServicer_to_server(AssistantServicer(), server)
-    server.add_insecure_port("127.0.0.1:50052")
-    server.start()
-    server.wait_for_termination()
+    listen_addr = "127.0.0.1:50051"
+    server.add_insecure_port(listen_addr)
+
+    logging.info("Starting server on %s", listen_addr)
+    await server.start()
+    await server.wait_for_termination()
 
 
 if __name__ == "__main__":
+    load_dotenv(verbose=True)
+
     logging.basicConfig(level=logging.DEBUG)
-    serve()
+    asyncio.run(serve())
