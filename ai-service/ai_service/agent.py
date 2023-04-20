822d8b315249574e67b818d7c9dd4dff1d00a793
Joel Wejdenstål <jwejdenstal@icloud.com>
2023-04-20 12:46:16 +0200

    move graph and tool configs to agent section

--- ai-service/ai_service/agent.py
+++ ai-service/ai_service/agent.py
@@ -1,4 +1,4 @@
-import ai_service.data as data
+from ai_service.data import llm_predictor, graph, node_index, docs_index
 from langchain.agents import Tool
 from langchain.chains.conversation.memory import ConversationBufferMemory
 from langchain.chat_models import ChatOpenAI
@@ -12,6 +12,55 @@ from llama_index.langchain_helpers.agents import (
     GraphToolConfig,
 )
 
+# configuration for node tool
+node_config = IndexToolConfig(
+    index=node_index, 
+    name=f"Vector Index Nodes",
+    description=f"useful for when you want to answer queries about Cluster Nodes",
+    index_query_kwargs={"similarity_top_k": 3},
+    tool_kwargs={"return_direct": True}
+)
+
+# configuration for docs tool
+docs_config = IndexToolConfig(
+    index=docs_index, 
+    name=f"Vector Index Docs",
+    description=f"useful for when you want to answer queries about Teleport",
+    index_query_kwargs={"similarity_top_k": 3},
+    tool_kwargs={"return_direct": True}
+)
+
+# define a decompose transform for the graph
 decompose_transform = DecomposeQueryTransform(
     llm_predictor, verbose=True
+)
+
+# define query configs for graph 
+graph_query_configs = [
+    {
+        "index_struct_type": "simple_dict",
+        "query_mode": "default",
+        "query_kwargs": {
+            "similarity_top_k": 1,
+            # "include_summary": True
+        },
+        "query_transform": decompose_transform
+    },
+    {
+        "index_struct_type": "list",
+        "query_mode": "default",
+        "query_kwargs": {
+            "response_mode": "tree_summarize",
+            "verbose": True
+        }
+    },
+]
+
+# graph tool config
+graph_config = GraphToolConfig(
+    graph=graph,
+    name=f"Graph Index",
+    description="useful for when you want to answer questions about Teleport with nodes",
+    query_configs=graph_query_configs,
+    tool_kwargs={"return_direct": True}
 )
--- ai-service/ai_service/data.py
+++ ai-service/ai_service/data.py
@@ -9,14 +9,6 @@ from pathlib import Path
 from llama_index import LLMPredictor, ServiceContext
 from langchain import OpenAI
 from llama_index.indices.composability import ComposableGraph
-from llama_index.indices.query.query_transform.base import DecomposeQueryTransform
-from llama_index.langchain_helpers.agents import (
-    LlamaToolkit,
-    create_llama_chat_agent,
-    IndexToolConfig,
-    GraphToolConfig,
-)
-
 
 # used via LlamaHub to parse Teleport docs
 UnstructuredReader = download_loader("UnstructuredReader", refresh_cache=True)
@@ -32,13 +24,6 @@ node_index = GPTSimpleVectorIndex.from_documents(
     node_documents, service_context=service_context
 )
 node_summary = "Cluster Nodes"
-nodeconfig = IndexToolConfig(
-    index=node_index, 
-    name=f"Vector Index Nodes",
-    description=f"useful for when you want to answer queries about Cluster Nodes",
-    index_query_kwargs={"similarity_top_k": 3},
-    tool_kwargs={"return_direct": True}
-)
 
 # load Teleport docs for additional context
 docs_documents = unstructured_loader.load_data(
@@ -48,13 +33,6 @@ docs_index = GPTSimpleVectorIndex.from_documents(
     docs_documents, service_context=service_context
 )
 docs_summary = "Teleport Documentation"
-docs_config = IndexToolConfig(
-    index=docs_index, 
-    name=f"Vector Index Docs",
-    description=f"useful for when you want to answer queries about Teleport",
-    index_query_kwargs={"similarity_top_k": 3},
-    tool_kwargs={"return_direct": True}
-)
 
 # compose a graph allowing cross-analysis of node data and Teleport docs
 graph = ComposableGraph.from_indices(
@@ -62,39 +40,4 @@ graph = ComposableGraph.from_indices(
     [node_index, docs_index],
     index_summaries=[node_summary, docs_summary],
     service_context=service_context,
-)
-
-# define a decompose transform for the graph
-decompose_transform = DecomposeQueryTransform(
-    llm_predictor, verbose=True
-)
-
-# define query configs for graph 
-graph_query_configs = [
-    {
-        "index_struct_type": "simple_dict",
-        "query_mode": "default",
-        "query_kwargs": {
-            "similarity_top_k": 1,
-            # "include_summary": True
-        },
-        "query_transform": decompose_transform
-    },
-    {
-        "index_struct_type": "list",
-        "query_mode": "default",
-        "query_kwargs": {
-            "response_mode": "tree_summarize",
-            "verbose": True
-        }
-    },
-]
-
-# graph tool config
-graph_config = GraphToolConfig(
-    graph=graph,
-    name=f"Graph Index",
-    description="useful for when you want to answer questions about Teleport with nodes",
-    query_configs=graph_query_configs,
-    tool_kwargs={"return_direct": True}
 )
