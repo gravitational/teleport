c41c56ee181f6f1bcbbecad0e98dfe4f13f64bcf
Joel Wejdenstål <jwejdenstal@icloud.com>
2023-04-20 11:54:27 +0200

    load from knowledge dir

--- ai-service/ai_service/data.py
+++ ai-service/ai_service/data.py
@@ -1,16 +1,15 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain.document_loaders import DirectoryLoader
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import TokenTextSplitter
 
-loader = TextLoader('state_of_the_union.txt', encoding='utf8')
+loader = DirectoryLoader('./knowledge', loader_cls=TextLoader)
 index_builder = VectorstoreIndexCreator(
     vectorstore_cls=Chroma, 
     embedding=OpenAIEmbeddings(),
-    text_splitter=CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    text_splitter=TokenTextSplitter(chunk_size=500, chunk_overlap=20)
 )
 index = index_builder.from_loaders([loader])
 retreiver = index.vectorstore.as_retriever()
