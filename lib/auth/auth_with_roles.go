a8b35654de960f5b6ade083d2e2f2ac76433bbaf
Tiago Silva <tiago.silva@goteleport.com>
2023-04-24 18:08:54 +0100

    [v13] Fixes cache init problem with missing `kube_service` (#25006)
    * Fixes cache init problem with missing `kube_service`
    
    This PR fixes a cache init problem when Teleport 12 Kube/Proxy clients connect to a
    Teleport 13 cluster and expect to watch the unsupported `kube_service`
    object.
    
    It allows the operation but ignores the resources being watched.
    
    * fix check

--- lib/auth/auth_with_roles.go
+++ lib/auth/auth_with_roles.go
@@ -1511,15 +1511,6 @@ func (a *ServerWithRoles) GetNodes(ctx context.Context, namespace string) ([]typ
 	return filteredNodes, nil
 }
 
-const (
-	// kubeService is a special resource type that is used to keep compatibility
-	// with Teleport 12 clients.
-	// Teleport 13 no longer supports kube_service resource type, but Teleport 12
-	// clients still expect it to be present in the server.
-	// TODO(tigrato): DELETE in 14.0.0
-	kubeService = "kube_service"
-)
-
 // ListResources returns a paginated list of resources filtered by user access.
 func (a *ServerWithRoles) ListResources(ctx context.Context, req proto.ListResourcesRequest) (*types.ListResourcesResponse, error) {
 	// kubeService is a special resource type that is used to keep compatibility
@@ -1527,7 +1518,7 @@ func (a *ServerWithRoles) ListResources(ctx context.Context, req proto.ListResou
 	// Teleport 13 no longer supports kube_service resource type, but Teleport 12
 	// clients still expect it to be present in the server.
 	// TODO(tigrato): DELETE in 14.0.0
-	if req.ResourceType == kubeService {
+	if req.ResourceType == types.KindKubeService {
 		return &types.ListResourcesResponse{}, nil
 	}
 
