48e14a92e117f63e5a1e0e2a57f5f1641e224670
mcbattirola <matheus.battirola@goteleport.com>
2023-04-24 14:37:47 -0300

    Add comments clarifying that ctas are not used currently

--- web/packages/teleport/src/services/cta/cta.ts
+++ web/packages/teleport/src/services/cta/cta.ts
@@ -1,3 +1,5 @@
+// ctaService dictates if some CTAs should be hidden or visible in the app.
+// Currently, no CTAs are active.
 export const ctaService = {
   showAuthConnectorsCTA: false,
   showTrustedDevicesCTA: false,
