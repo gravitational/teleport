8c1b5c9f14f459e67531ed29177f8f87ef702f31
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2023-04-26 10:59:20 +0200

    Add shadow for the tabs

--- web/packages/teleterm/src/ui/Tabs/Tabs.tsx
+++ web/packages/teleterm/src/ui/Tabs/Tabs.tsx
@@ -69,7 +69,19 @@ export function Tabs(props: Props) {
     : $emptyTab;
 
   return (
-    <StyledTabs as="nav" typography="h5" bold {...styledProps}>
+    <StyledTabs
+      as="nav"
+      typography="h5"
+      bold
+      {...styledProps}
+      css={`
+        position: relative;
+        z-index: 1;
+        box-shadow: 0px 1px 10px 0px rgba(0, 0, 0, 0.12),
+          0px 4px 5px 0px rgba(0, 0, 0, 0.14),
+          0px 2px 4px -1px rgba(0, 0, 0, 0.2);
+      `}
+    >
       {$items}
       <ButtonIcon
         ml="1"
