03691ebd5de5b0d4e814ad821ae1177a6081666a
Rafał Cieślak <rafal.cieslak@goteleport.com>
2023-04-27 11:06:11 +0200

    Ensure the test process prints to stdout before exiting

---
+++ web/packages/teleterm/src/mainProcess/testProcess.mjs
@@ -0,0 +1,17 @@
+import process from 'process';
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const waitTime = parseInt(process.argv[2]);
+if (waitTime) {
+  await sleep(waitTime);
+}
+
+const shouldExit = process.argv[3];
+if (shouldExit) {
+  process.exit(1);
+}
+
+console.log('Lorem ipsum dolor sit amet');
+console.log('{CONNECT_GRPC_PORT: 1337}');
+console.log('Lorem ipsum dolor sit amet');
