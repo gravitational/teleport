e63f90ff320a12c72f1faa18eaba3ccfbae00e96
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2023-04-27 16:51:54 +0200

    Use the correct emitter in auth.TLSServer

--- lib/service/service.go
+++ lib/service/service.go
@@ -1666,7 +1666,7 @@ func (process *TeleportProcess) initAuthService() error {
 		Authorizer:     authorizer,
 		AuditLog:       process.auditLog,
 		PluginRegistry: process.PluginRegistry,
-		Emitter:        checkingEmitter,
+		Emitter:        authServer,
 		MetadataGetter: uploadHandler,
 	}
 
