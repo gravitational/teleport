11774e1c09cf5f78e2376eb1c228eada9d8a7178
Alan Parra <alan.parra@goteleport.com>
2023-04-28 16:52:19 -0300

    Use new device event layout in Web UI

--- web/packages/teleport/src/Audit/fixtures/index.ts
+++ web/packages/teleport/src/Audit/fixtures/index.ts
@@ -2522,15 +2522,11 @@ export const events = [
       os_type: 2,
     },
     ei: 0,
-    event: 'device',
-    status: {
-      success: true,
-    },
+    event: 'device.create',
+    success: true,
     time: '2023-01-12T19:28:36.842Z',
     uid: '94d33b77-82cd-4558-8893-0320699bf755',
-    user: {
-      user: '3827e8ad-7cbe-4423-a80f-dfc89e83eb86.im-a-cluster-name',
-    },
+    user: '3827e8ad-7cbe-4423-a80f-dfc89e83eb86.im-a-cluster-name',
   },
   {
     cluster_name: 'im-a-cluster-name',
@@ -2541,15 +2537,11 @@ export const events = [
       os_type: 2,
     },
     ei: 0,
-    event: 'device',
-    status: {
-      success: false, // failed
-    },
+    event: 'device.create',
+    success: false, // failed
     time: '2023-01-12T19:28:36.842Z',
     uid: '94d33b77-82cd-4558-8893-0320699bf755',
-    user: {
-      user: '3827e8ad-7cbe-4423-a80f-dfc89e83eb86.im-a-cluster-name',
-    },
+    user: '3827e8ad-7cbe-4423-a80f-dfc89e83eb86.im-a-cluster-name',
   },
   {
     cluster_name: 'im-a-cluster-name',
@@ -2558,15 +2550,11 @@ export const events = [
       device_id: '99d39707-efdd-436c-94f3-6a1aeef1fbf2',
     },
     ei: 0,
-    event: 'device',
-    status: {
-      success: true,
-    },
+    event: 'device.delete',
+    success: true,
     time: '2023-01-12T20:33:20.527Z',
     uid: 'a12e693e-1c45-43e4-a9d1-5fd8399e303c',
-    user: {
-      user: '3827e8ad-7cbe-4423-a80f-dfc89e83eb86.im-a-cluster-name',
-    },
+    user: '3827e8ad-7cbe-4423-a80f-dfc89e83eb86.im-a-cluster-name',
   },
   {
     cluster_name: 'im-a-cluster-name',
@@ -2575,15 +2563,11 @@ export const events = [
       device_id: '99d39707-efdd-436c-94f3-6a1aeef1fbf2',
     },
     ei: 0,
-    event: 'device',
-    status: {
-      success: false,
-    },
+    event: 'device.delete',
+    success: false,
     time: '2023-01-12T20:33:20.527Z',
     uid: 'a12e693e-1c45-43e4-a9d1-5fd8399e303c',
-    user: {
-      user: '3827e8ad-7cbe-4423-a80f-dfc89e83eb86.im-a-cluster-name',
-    },
+    user: '3827e8ad-7cbe-4423-a80f-dfc89e83eb86.im-a-cluster-name',
   },
   {
     cluster_name: 'im-a-cluster-name',
@@ -2592,15 +2576,11 @@ export const events = [
       device_id: '99d39707-efdd-436c-94f3-6a1aeef1fbf2',
     },
     ei: 0,
-    event: 'device',
-    status: {
-      success: true,
-    },
+    event: 'device.token.create',
+    success: true,
     time: '2023-01-12T19:51:54.168Z',
     uid: '24cce2a0-57b7-494e-a196-c7fd2482b10c',
-    user: {
-      user: '3827e8ad-7cbe-4423-a80f-dfc89e83eb86.im-a-cluster-name',
-    },
+    user: '3827e8ad-7cbe-4423-a80f-dfc89e83eb86.im-a-cluster-name',
   },
   {
     cluster_name: 'im-a-cluster-name',
@@ -2609,15 +2589,11 @@ export const events = [
       device_id: '99d39707-efdd-436c-94f3-6a1aeef1fbf2',
     },
     ei: 0,
-    event: 'device',
-    status: {
-      success: false,
-    },
+    event: 'device.token.create',
+    success: false,
     time: '2023-01-12T19:51:54.168Z',
     uid: '24cce2a0-57b7-494e-a196-c7fd2482b10c',
-    user: {
-      user: '3827e8ad-7cbe-4423-a80f-dfc89e83eb86.im-a-cluster-name',
-    },
+    user: '3827e8ad-7cbe-4423-a80f-dfc89e83eb86.im-a-cluster-name',
   },
   {
     cluster_name: 'im-a-cluster-name',
@@ -2628,15 +2604,11 @@ export const events = [
       os_type: 2,
     },
     ei: 0,
-    event: 'device',
-    status: {
-      success: true,
-    },
+    event: 'device.token.spent',
+    success: true,
     time: '2023-01-12T21:31:29.191Z',
     uid: 'bbbc496f-820b-4f49-ae0d-1c1b29faee85',
-    user: {
-      user: 'lisa',
-    },
+    user: 'lisa',
   },
   {
     cluster_name: 'im-a-cluster-name',
@@ -2647,15 +2619,11 @@ export const events = [
       os_type: 2,
     },
     ei: 0,
-    event: 'device',
-    status: {
-      success: false,
-    },
+    event: 'device.token.spent',
+    success: false,
     time: '2023-01-12T21:31:29.191Z',
     uid: 'bbbc496f-820b-4f49-ae0d-1c1b29faee85',
-    user: {
-      user: 'lisa',
-    },
+    user: 'lisa',
   },
   {
     cluster_name: 'im-a-cluster-name',
@@ -2666,15 +2634,11 @@ export const events = [
       os_type: 2,
     },
     ei: 0,
-    event: 'device',
-    status: {
-      success: true,
-    },
+    event: 'device.enroll',
+    success: true,
     time: '2023-01-12T21:31:29.191Z',
     uid: 'bbbc496f-820b-4f49-ae0d-1c1b29faee85',
-    user: {
-      user: 'lisa',
-    },
+    user: 'lisa',
   },
   {
     cluster_name: 'im-a-cluster-name',
@@ -2685,43 +2649,31 @@ export const events = [
       os_type: 2,
     },
     ei: 0,
-    event: 'device',
-    status: {
-      success: false,
-    },
+    event: 'device.enroll',
+    success: false,
     time: '2023-01-12T21:31:29.191Z',
     uid: 'bbbc496f-820b-4f49-ae0d-1c1b29faee85',
-    user: {
-      user: 'lisa',
-    },
+    user: 'lisa',
   },
   {
     cluster_name: 'im-a-cluster-name',
     code: 'TV006I',
     ei: 0,
-    event: 'device',
-    status: {
-      success: true,
-    },
+    event: 'device.authenticate',
+    success: true,
     time: '2023-01-12T19:34:48.1Z',
     uid: 'fa279611-91d8-47b5-9fad-b8ea3e5286e0',
-    user: {
-      user: 'lisa',
-    },
+    user: 'lisa',
   },
   {
     cluster_name: 'im-a-cluster-name',
     code: 'TV006I',
     ei: 0,
-    event: 'device',
-    status: {
-      success: false,
-    },
+    event: 'device.authenticate',
+    success: false,
     time: '2023-01-12T19:34:48.1Z',
     uid: 'fa279611-91d8-47b5-9fad-b8ea3e5286e0',
-    user: {
-      user: 'lisa',
-    },
+    user: 'lisa',
   },
   {
     cluster_name: 'im-a-cluster-name',
@@ -2732,15 +2684,11 @@ export const events = [
       os_type: 2,
     },
     ei: 0,
-    event: 'device',
-    status: {
-      success: true,
-    },
+    event: 'device.update',
+    success: true,
     time: '2023-01-12T21:31:29.191Z',
     uid: 'bbbc496f-820b-4f49-ae0d-1c1b29faee85',
-    user: {
-      user: 'lisa',
-    },
+    user: 'lisa',
   },
   {
     cluster_name: 'im-a-cluster-name',
