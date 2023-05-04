e0326a786297299e858db92914e9b286b9bb8221
matheus <matheus.battirola@goteleport.com>
2023-05-04 11:57:48 -0300

    Add IsUsageBased to features and send it to web UI (#25465)
    * Add IsUsageBased to features and send to web UI
    
    * Update flag name
    
    * Improve comment
    
    * Add comments and improve field name
    
    * Remove duplicated property

--- web/packages/teleport/src/config.ts
+++ web/packages/teleport/src/config.ts
@@ -40,6 +40,8 @@ const cfg = {
   isDashboard: false,
   tunnelPublicAddress: '',
   recoveryCodesEnabled: false,
+  // IsUsageBasedBilling determines if the user subscription is usage-based (pay-as-you-go).
+  isUsageBasedBilling: false,
 
   configDir: '$HOME/.config',
 
