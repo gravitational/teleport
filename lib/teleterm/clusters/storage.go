38049d07109f543b6011d81e9e572e63897a40d8
STeve Huang <xin.huang@goteleport.com>
2023-05-04 13:42:56 -0400

    update/add comments

--- lib/teleterm/clusters/storage.go
+++ lib/teleterm/clusters/storage.go
@@ -160,7 +160,11 @@ func (s *Storage) addCluster(ctx context.Context, dir, webProxyAddress string) (
 		return nil, trace.Wrap(err)
 	}
 
+	// Do the ALPN handshake test to decide if connection upgrades are required
+	// for TLS Routing. Only do the test once Ping verifies the cluster is
+	// reachable.
 	clusterClient.TLSRoutingConnUpgradeRequired = apiclient.IsALPNConnUpgradeRequired(webProxyAddress, s.InsecureSkipVerify)
+
 	if err := clusterClient.SaveProfile(false); err != nil {
 		return nil, trace.Wrap(err)
 	}
