bc28311bbbe66163d5f523a506821018de9d0058
matheus <matheus.battirola@goteleport.com>
2023-05-04 11:57:48 -0300

    Add IsUsageBased to features and send it to web UI (#25465)
    * Add IsUsageBased to features and send to web UI
    
    * Update flag name
    
    * Improve comment
    
    * Add comments and improve field name
    
    * Remove duplicated property

--- lib/web/apiserver.go
+++ lib/web/apiserver.go
@@ -1357,6 +1357,7 @@ func (h *Handler) getWebConfig(w http.ResponseWriter, r *http.Request, p httprou
 		RecoveryCodesEnabled: h.ClusterFeatures.GetRecoveryCodes(),
 		UI:                   h.getUIConfig(r.Context()),
 		IsDashboard:          isDashboard(h.ClusterFeatures),
+		IsUsageBasedBilling:  h.ClusterFeatures.GetIsUsageBased(),
 	}
 
 	resource, err := h.cfg.ProxyClient.GetClusterName()
