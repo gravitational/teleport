d94db816c0a533d7117c6cd5da568e5f5696e5d8
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2023-05-08 13:40:16 -0400

    Add ability to enable trace logging level (#25800)
    Closes #25737

--- lib/config/configuration.go
+++ lib/config/configuration.go
@@ -538,6 +538,8 @@ func applyLogConfig(loggerConfig Log, cfg *service.Config) error {
 		logger.SetLevel(log.DebugLevel)
 	case "warn", "warning":
 		logger.SetLevel(log.WarnLevel)
+	case "trace":
+		logger.SetLevel(log.TraceLevel)
 	default:
 		return trace.BadParameter("unsupported logger severity: %q", loggerConfig.Severity)
 	}
@@ -1903,7 +1905,9 @@ func Configure(clf *CommandLineFlags, cfg *service.Config) error {
 			log.SetLevel(log.DebugLevel)
 			cfg.Log.SetLevel(log.DebugLevel)
 		} else {
-			fileConf.Logger.Severity = teleport.DebugLevel
+			if fileConf.Logger.Severity != "trace" {
+				fileConf.Logger.Severity = teleport.DebugLevel
+			}
 		}
 	}
 
