47f9dc5bc740afb109c9836b13e73bb0a203683b
Isaiah Becker-Mayer <isaiah@goteleport.com>
2023-05-09 09:51:15 -0700

    Fixes a SharedDirectoryAnnounce incompatibility
    The SharedDirectoryAnnounce message was changed to remove an unnecessary
    completionID field, however this change caused an incompatibility between
    the proxy and previous versions of the wds. This commit reverts that specific
    change and adds notes to the code explaining the situation.
    
    The original change is here: https://github.com/gravitational/teleport/pull/25260/files#diff-98a4bee57beb7f007614e4810d2cf8413bddf48484c8aad6dd0756a218797c36R677-R678

--- lib/srv/desktop/tdp/proto.go
+++ lib/srv/desktop/tdp/proto.go
@@ -675,8 +675,16 @@ func (s SharedDirectoryAnnounce) Encode() ([]byte, error) {
 }
 
 func decodeSharedDirectoryAnnounce(in io.Reader) (SharedDirectoryAnnounce, error) {
+	// TODO(isaiah): The completionID here is a copy-paste error, but we need to keep it
+	// for now in order that the proxy stay compatible with previous versions of the wds.
+	var completionID uint32
+	err := binary.Read(in, binary.BigEndian, &completionID)
+	if err != nil {
+		return SharedDirectoryAnnounce{}, trace.Wrap(err)
+	}
+
 	var directoryID uint32
-	err := binary.Read(in, binary.BigEndian, &directoryID)
+	err = binary.Read(in, binary.BigEndian, &directoryID)
 	if err != nil {
 		return SharedDirectoryAnnounce{}, trace.Wrap(err)
 	}
