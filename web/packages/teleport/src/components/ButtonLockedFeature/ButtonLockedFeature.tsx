c2027a5fe8f437813d6e08cf9b43e4160e8e843f
matheus <matheus.battirola@goteleport.com>
2023-05-09 15:24:24 -0300

    [v13] Team plan CTAs (#25073) (#25701)
    * Team plan CTAs (#25073)
    
    * (wip) add unlock feature button to support page
    
    * Fix typo
    
    * (wip) add locked feature join session
    
    * Improve session join style
    
    * Add session join lock button
    
    * Remove hover on locked join menu
    
    * Update copy
    
    * Only show `join as...` when appropriate
    
    * (wip)
    
    * Make ButtonLockedFeature theme sensitive
    
    * Remove debug stuff
    
    * Adjust feature button height
    
    * Fix session join button on locked
    
    * Remove unused import
    
    * typo
    
    * Add link to cta button
    
    * Remove teams flag
    
    * Add cta service
    
    * Update snapshots
    
    * Lock button style
    
    * Add cta to context
    
    * Rename cta to ctaService
    
    * Stories and tests
    
    * Add comments clarifying that ctas are not used currently
    
    * add showActiveSessionsCTA to useSessions
    
    * Licenses
    
    * Lint license
    
    * Remove commented out code
    
    * Add missing types
    
    * Replace cta service for an object with cta on context
    
    * Fix menu hack
    
    * small refactor in styled stuff
    
    * Update SessionJoinBtn to use master code
    
    * Snapshot update
    
    * Minor stuff
    
    * update snapshots
    
    * Update snapshots
    
    * Remove duplicated prop

---
+++ web/packages/teleport/src/components/ButtonLockedFeature/ButtonLockedFeature.tsx
@@ -0,0 +1,55 @@
+/*
+Copyright 2023 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import React from 'react';
+import styled from 'styled-components';
+import { ButtonPrimary } from 'design/Button';
+import { Unlock } from 'design/Icon';
+import Flex from 'design/Flex';
+
+export type Props = {
+  children: React.ReactNode;
+  noIcon?: boolean;
+  [index: string]: any;
+};
+
+const salesUrl = 'https://goteleport.com/signup/enterprise/';
+
+export function ButtonLockedFeature({ children, noIcon = false, ...rest }) {
+  return (
+    <StyledButton onClick={() => window.open(salesUrl, 'blank')} {...rest}>
+      <Flex alignItems="center">
+        {!noIcon && <UnlockIcon />}
+        {children}
+      </Flex>
+    </StyledButton>
+  );
+}
+
+const StyledButton = styled(ButtonPrimary)`
+  text-transform: none;
+  width: 100%;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  font-size: 12px;
+`;
+
+const UnlockIcon = styled(Unlock)`
+  color: inherit;
+  font-weight: 500;
+  font-size: 15px;
+  margin-right: 4px;
+`;
