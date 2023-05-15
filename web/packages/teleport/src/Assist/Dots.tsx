bff31e7ccb981710859ac40b244abbf1d7be0062
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2023-05-15 09:06:34 -0400

    [v12][Assist] Fix 'React is not defined' error (#26208)
    * [v12][Assist] Fix 'React is not defined' error
    
    * yarn prettier-write

--- web/packages/teleport/src/Assist/Dots.tsx
+++ web/packages/teleport/src/Assist/Dots.tsx
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import React from 'react';
 import styled, { keyframes } from 'styled-components';
 
 const loader = keyframes`
