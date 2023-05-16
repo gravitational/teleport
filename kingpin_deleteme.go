e55c1f8ca22f73d4272e4cdc92721d11569db3c9
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2023-05-16 16:14:53 +0200

    Add a bogus kingpin/v2 import to satisfy linter

---
+++ kingpin_deleteme.go
@@ -0,0 +1,5 @@
+// TODO(gzdunek): Delete this file after landing #26238 and
+// https://github.com/gravitational/teleport.e/pull/1402.
+package teleport
+
+import _ "github.com/alecthomas/kingpin/v2"
