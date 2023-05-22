5931638c639c354af88debd724ff9ecb4509f34d
Alan Parra <alan.parra@goteleport.com>
2023-05-22 14:27:04 -0300

    Rename babybox to grpcbox

--- build.assets/grpcbox.mk
+++ build.assets/grpcbox.mk
@@ -1,33 +1,33 @@
-# Makefile for babybox related targets and variables.
+# Makefile for grpcbox targets and variables.
 #
-# The babybox is a leaner, meaner, faster buildbox meant exclusively for
+# The grpcbox is a leaner, meaner, faster buildbox meant exclusively for
 # codegen.
 # It is not guaranteed to have tooling parity with the buildbox.
 #
-# See Dockerfile-babybox.
+# See Dockerfile-grpcbox.
 
-BABYBOX_BASE_NAME ?= teleport-babybox
+GRPCBOX_BASE_NAME ?= teleport-grpcbox
 
 ifeq ($(BUILDBOX_VERSION), "")
-BABYBOX ?= $(BABYBOX_BASE_NAME)
+GRPCBOX ?= $(GRPCBOX_BASE_NAME)
 else
-BABYBOX ?= $(BABYBOX_BASE_NAME):$(BUILDBOX_VERSION)
+GRPCBOX ?= $(GRPCBOX_BASE_NAME):$(BUILDBOX_VERSION)
 endif
 
-# BABYRUN has the necessary invocation to run a command inside the babybox.
+# GRPCBOX_RUN has the necessary invocation to run a command inside the grpcbox.
 # Use this variable to run it from other Makefiles.
-BABYRUN := docker run -it --rm -v "$$(pwd)/../:/workdir" -w /workdir $(BABYBOX)
+GRPCBOX_RUN := docker run -it --rm -v "$$(pwd)/../:/workdir" -w /workdir $(GRPCBOX)
 
-# babybox builds a codegen-focused buildbox.
+# grpcbox builds a codegen-focused buildbox.
 # It's leaner, meaner, faster and not supposed to compile code.
-.PHONY: babybox
-babybox:
+.PHONY: grpcbox
+grpcbox:
 	DOCKER_BUILDKIT=1 docker build \
 		--build-arg BUF_VERSION=$(BUF_VERSION) \
 		--build-arg GOGO_PROTO_TAG=$(GOGO_PROTO_TAG) \
 		--build-arg NODE_GRPC_TOOLS_VERSION=$(NODE_GRPC_TOOLS_VERSION) \
 		--build-arg NODE_PROTOC_TS_VERSION=$(NODE_PROTOC_TS_VERSION) \
 		--build-arg PROTOC_VER=$(PROTOC_VER) \
-		-f Dockerfile-babybox \
-		-t "$(BABYBOX)" \
+		-f Dockerfile-grpcbox \
+		-t "$(GRPCBOX)" \
 		../
