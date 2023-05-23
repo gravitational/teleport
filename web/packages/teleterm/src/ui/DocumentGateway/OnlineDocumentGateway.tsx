643877b5509e49e045ffbb6a450f8416ddbaad5d
Rafał Cieślak <rafal.cieslak@goteleport.com>
2023-05-23 10:38:11 +0200

    Preserve backwards compatibility of teleport.lib.teleterm.v1.Gateway

--- web/packages/teleterm/src/ui/DocumentGateway/OnlineDocumentGateway.tsx
+++ web/packages/teleterm/src/ui/DocumentGateway/OnlineDocumentGateway.tsx
@@ -105,7 +105,7 @@ export function OnlineDocumentGateway(props: OnlineDocumentGatewayProps) {
         </Validation>
       </Flex>
       <CliCommand
-        cliCommand={props.gateway.cliCommand.preview}
+        cliCommand={props.gateway.gatewayCliCommand.preview}
         isLoading={isPortOrDbNameProcessing}
         onRun={props.runCliCommand}
       />
