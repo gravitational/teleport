6bf1271c4b90c3d90df74e3f0eec08d4bac1ba72
Rafał Cieślak <rafal.cieslak@goteleport.com>
2023-05-25 17:05:03 +0200

    [v11] Spawn gateway CLI client directly (#26755)
    * Connect: Create dedicated functions for connecting to resources
    
    * Create dedicated functions for connecting to resources
    
    * Do not use spread operator
    
    * Rename `connectToNode` -> `connectToServer`
    
    * Remove unused imports
    
    * Spawn gateway CLI client directly
    
    * Return os.exec.Cmd as gateway CLI command
    
    * Remove separate Props type from DocumentTerminal
    
    * Refactor Kind type exported from documentsService
    
    * Export makeRuntimeSettings from MainProcess mock
    
    * PtyProcess: Join args in logger name
    
    * ptyHostService: Pass ptyOptions explicitly instead of using spread
    
    I noticed that we pass both argsList and args to the PtyProcess constructor.
    While TypeScript allows that, it is a bit confusing when inspecting the
    actual values received in the constructor.
    
    * Add empty DocumentGatewayCliClient
    
    * Start terminal from DocumentGatewayCliClient
    
    * Add waiting state for DocumentGatewayCliClient
    
    * Remove targetSubresourceName from DocumentGatewayCliClient title
    
    * Preserve backwards compatibility of teleport.lib.teleterm.v1.Gateway
    
    * Capture the error if the gateway CLI client executable is not found
    
    * Add onStartError to PTY process
    
    * Add logging to frontend PtyEventsStreamHandler
    
    * Return cleanup functions from pty process onEvent functions
    
    * Extract DocumentReconnect from DocumentTerminal
    
    The Reconnect component will be useful when handling startError.
    
    * Add getPtyId to PtyProcess
    
    This will act as the key for the Terminal component, allowing us to
    make sure we mount it only once for any given PTY process.
    
    * Capture startError and display an error state
    
    * Terminal/ctrl: Cleanup ptyProcess onData listener
    
    * Include command name and args in startError error message
    
    * Remove ptyId field from PtyProcess
    
    ---------
    
    Co-authored-by: Grzegorz Zdunek <gzdunek@users.noreply.github.com>

---
+++ web/packages/teleterm/src/ui/DocumentGatewayCliClient/DocumentGatewayCliClient.tsx
@@ -0,0 +1,154 @@
+/**
+ * Copyright 2023 Gravitational, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { Flex, Text, ButtonPrimary } from 'design';
+
+import Document from 'teleterm/ui/Document';
+import * as types from 'teleterm/ui/services/workspacesService';
+import { useAppContext } from 'teleterm/ui/appContextProvider';
+import { DocumentTerminal } from 'teleterm/ui/DocumentTerminal';
+import { useWorkspaceContext } from 'teleterm/ui/Documents';
+import { connectToDatabase } from 'teleterm/ui/services/workspacesService';
+
+/**
+ * DocumentGatewayCliClient creates a terminal session that targets the given gateway.
+ *
+ * It waits for the gateway to be created before starting the terminal session. This typically
+ * happens only during the app restart. We assume that most of the time both the DocumentGateway and
+ * DocumentGatewayCliClient tabs will be reopened together. In that case, DocumentGatewayCliClient
+ * will wait for DocumentGateway to create the gateway first before attempting to start the client.
+ *
+ * However, if the user closes just the DocumentGateway tab and then restarts the app with just the
+ * DocumentGatewayCliClient tab present, the gateway will never be created. In that case, the user
+ * will be able to click "Open the connection" to manually open a new DocumentGateway tab.
+ */
+export const DocumentGatewayCliClient = (props: {
+  visible: boolean;
+  doc: types.DocumentGatewayCliClient;
+}) => {
+  const { clustersService } = useAppContext();
+  clustersService.useState();
+
+  const { doc, visible } = props;
+  const [hasRenderedTerminal, setHasRenderedTerminal] = useState(false);
+
+  const gateway = clustersService.findGatewayByConnectionParams(
+    doc.targetUri,
+    doc.targetUser
+  );
+
+  // Once we render the terminal, we want to keep it visible. Otherwise removing the gateway would
+  // mean that this document would immediately unmount DocumentTerminal and close the PTY.
+  //
+  // After the gateway is closed, the CLI client will not be able to interact with the gateway
+  // target, but the user might still want to inspect the output.
+  if (gateway || hasRenderedTerminal) {
+    if (!hasRenderedTerminal) {
+      setHasRenderedTerminal(true);
+    }
+
+    return <DocumentTerminal doc={doc} visible={visible} />;
+  }
+
+  return <WaitingForGateway doc={doc} visible={visible} />;
+};
+
+const TIMEOUT_SECONDS = 10;
+
+const WaitingForGateway = (props: {
+  doc: types.DocumentGatewayCliClient;
+  visible: boolean;
+}) => {
+  const { doc, visible } = props;
+  const ctx = useAppContext();
+  const { documentsService } = useWorkspaceContext();
+  // If we depended on doc.status for hasTimedOut instead of using a separate state, then on reopen
+  // the doc would have status set to 'connected' on 'error' and it'd be updated from useEffect,
+  // meaning that there would be a brief flash of old state.
+  const [hasTimedOut, setHasTimedOut] = useState(false);
+
+  useEffect(() => {
+    // Update the doc state to make the progress bar show up in the tab bar.
+    // Once DocumentTerminal is mounted, it is going to update the status to 'connected' or 'error'.
+    documentsService.update(doc.uri, { status: 'connecting' });
+
+    const timeoutId = setTimeout(() => {
+      setHasTimedOut(true);
+      documentsService.update(doc.uri, { status: 'error' });
+    }, TIMEOUT_SECONDS * 1000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const openConnection = () => {
+    connectToDatabase(ctx, {
+      uri: doc.targetUri,
+      name: doc.targetName,
+      protocol: doc.targetProtocol,
+      dbUser: doc.targetUser,
+    });
+  };
+
+  return (
+    <Document visible={visible} px={2}>
+      <WaitingForGatewayContent
+        doc={doc}
+        hasTimedOut={hasTimedOut}
+        openConnection={openConnection}
+      />
+    </Document>
+  );
+};
+
+export const WaitingForGatewayContent = ({
+  doc,
+  hasTimedOut,
+  openConnection,
+}: {
+  doc: types.DocumentGatewayCliClient;
+  hasTimedOut: boolean;
+  openConnection: () => void;
+}) => (
+  <Flex gap={4} flexDirection="column" mx="auto" alignItems="center" mt={100}>
+    {hasTimedOut ? (
+      <div>
+        <StyledText>
+          A connection to <strong>{doc.targetName}</strong> as{' '}
+          <strong>{doc.targetUser}</strong> has not been opened up within{' '}
+          {TIMEOUT_SECONDS} seconds.
+        </StyledText>
+        <StyledText>Please try to open the connection manually.</StyledText>
+      </div>
+    ) : (
+      <StyledText>
+        Waiting for a db connection to <strong>{doc.targetName}</strong> as{' '}
+        <strong>{doc.targetUser}</strong> to be opened up.
+      </StyledText>
+    )}
+
+    <ButtonPrimary onClick={openConnection}>Open the connection</ButtonPrimary>
+  </Flex>
+);
+
+const StyledText = styled(Text).attrs({
+  typography: 'h5',
+  textAlign: 'center',
+})``;
