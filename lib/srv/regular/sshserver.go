b3f1915861c56bb44eed57c61631620a86c5314b
Alex McGrath <alex.mcgrath@goteleport.com>
2023-05-25 13:32:32 +0100

    Add option to allow for host users not to be deleted
    This adds a new role option called create_host_users_mode which allows
    for it to be configured to not delete users when a session ends. The
    old `create_host_user` option will be deprecated with this.
    
    If the deprecated option is set, the new mode option will default to
    dropping users as is the current behavior.
    
    add generated protos
    
    use combined output for commands, dont set CreateHostUser
    
    fix tests
    
    ci fixes
    
    add godocs to HostUserMode constants, fix tests
    
    remain -> keep
    
    Use an enum instead of a string
    
    fix merge errors
    
    Add tests & resolve comments
    
    Resolve issues
    
    fmt

--- lib/srv/regular/sshserver.go
+++ lib/srv/regular/sshserver.go
@@ -1642,14 +1642,14 @@ func (s *Server) dispatch(ctx context.Context, ch ssh.Channel, req *ssh.Request,
 	case tracessh.TracingRequest:
 		return nil
 	case sshutils.ExecRequest:
-		if _, err := s.termHandlers.SessionRegistry.TryCreateHostUser(serverContext); err != nil {
+		if err := s.termHandlers.SessionRegistry.TryCreateHostUser(serverContext); err != nil {
 			return trace.Wrap(err)
 		}
 		return s.termHandlers.HandleExec(ctx, ch, req, serverContext)
 	case sshutils.PTYRequest:
 		return s.termHandlers.HandlePTYReq(ctx, ch, req, serverContext)
 	case sshutils.ShellRequest:
-		if _, err := s.termHandlers.SessionRegistry.TryCreateHostUser(serverContext); err != nil {
+		if err := s.termHandlers.SessionRegistry.TryCreateHostUser(serverContext); err != nil {
 			return trace.Wrap(err)
 		}
 		return s.termHandlers.HandleShell(ctx, ch, req, serverContext)
@@ -1674,7 +1674,7 @@ func (s *Server) dispatch(ctx context.Context, ch ssh.Channel, req *ssh.Request,
 		// https://tools.ietf.org/html/draft-ietf-secsh-agent-02
 		// the open ssh proto spec that we implement is here:
 		// http://cvsweb.openbsd.org/cgi-bin/cvsweb/src/usr.bin/ssh/PROTOCOL.agent
-		if _, err := s.termHandlers.SessionRegistry.TryCreateHostUser(serverContext); err != nil {
+		if err := s.termHandlers.SessionRegistry.TryCreateHostUser(serverContext); err != nil {
 			s.Logger.Warn(err)
 			return nil
 		}
