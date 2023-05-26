274f79d0ad3712a787cbdbae4c1aba4a8c9ebb30
Mike Wilson <michael.wilson@goteleport.com>
2023-05-26 15:26:44 -0400

    Introduce the Plugin Static Credentials object.
    The Plugin Static Credentials object has been introduced to support storing
    static credentials for plugins.
    
    Related RFD: https://github.com/gravitational/teleport/pull/26599

---
+++ api/types/plugin_static_credentials.go
@@ -0,0 +1,145 @@
+/*
+Copyright 2023 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package types
+
+import "github.com/gravitational/trace"
+
+// PluginStaticCredentials are static credentials for plugins.
+type PluginStaticCredentials interface {
+	// ResourceWithSecrets provides common resource methods.
+	ResourceWithLabels
+
+	// GetAPIToken will return a flag indicating whether this object contains an API token
+	// credentials and the attached API token if possible.
+	GetAPIToken() (bool, string)
+
+	// GetAPIToken will return a flag indicating whether this object contains basic auth
+	// credentials and the attached username and password if possible.
+	GetBasicAuth() (bool, string, string)
+
+	// GetOAuthClientSecret will return a flag indicating whether this object contains OAuth
+	// client secret credentials and the attached client ID and client secret.
+	GetOAuthClientSecret() (bool, string, string)
+}
+
+// NewPluginStaticCredentialsV1 creates a new PluginStaticCredentialsV1 resource.
+func NewPluginStaticCredentialsV1(metadata Metadata, spec PluginStaticCredentialsSpecV1) (PluginStaticCredentials, error) {
+	p := &PluginStaticCredentialsV1{
+		ResourceHeader: ResourceHeader{
+			Metadata: metadata,
+		},
+		Spec: spec,
+	}
+
+	if err := p.CheckAndSetDefaults(); err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	return p, nil
+}
+
+// CheckAndSetDefaults checks validity of all parameters and sets defaults.
+func (p *PluginStaticCredentialsV1) CheckAndSetDefaults() error {
+	p.setStaticFields()
+
+	if err := p.Metadata.CheckAndSetDefaults(); err != nil {
+		return trace.Wrap(err)
+	}
+
+	switch credentials := p.Spec.Credentials.(type) {
+	case *PluginStaticCredentialsSpecV1_APIToken:
+		if credentials.APIToken == nil {
+			return trace.BadParameter("api token object is missing")
+		}
+
+		if credentials.APIToken.APIToken == "" {
+			return trace.BadParameter("token string is empty")
+		}
+	case *PluginStaticCredentialsSpecV1_BasicAuth:
+		if credentials.BasicAuth == nil {
+			return trace.BadParameter("basic auth object is missing")
+		}
+
+		if credentials.BasicAuth.Username == "" {
+			return trace.BadParameter("username is empty")
+		}
+
+		if credentials.BasicAuth.Password == "" {
+			return trace.BadParameter("password is empty")
+		}
+	case *PluginStaticCredentialsSpecV1_OauthClientSecret:
+		if credentials.OauthClientSecret == nil {
+			return trace.BadParameter("oauth client secret object is missing")
+		}
+
+		if credentials.OauthClientSecret.ClientId == "" {
+			return trace.BadParameter("client ID is empty")
+		}
+
+		if credentials.OauthClientSecret.ClientSecret == "" {
+			return trace.BadParameter("client secret is empty")
+		}
+	default:
+		return trace.BadParameter("credentials are not set or have an unknown type")
+	}
+
+	return nil
+}
+
+// setStaticFields sets static fields for the object.
+func (p *PluginStaticCredentialsV1) setStaticFields() {
+	p.Kind = KindPluginStaticCredentials
+	p.Version = V1
+}
+
+// GetAPIToken will return a flag indicating whether this object contains an API token
+// credentials and the attached API token if possible.
+func (p *PluginStaticCredentialsV1) GetAPIToken() (ok bool, apiToken string) {
+	credentials, ok := p.Spec.Credentials.(*PluginStaticCredentialsSpecV1_APIToken)
+	if !ok {
+		return false, ""
+	}
+
+	return true, credentials.APIToken.APIToken
+}
+
+// GetAPIToken will return a flag indicating whether this object contains basic auth
+// credentials and the attached username and password if possible.
+func (p *PluginStaticCredentialsV1) GetBasicAuth() (ok bool, username string, password string) {
+	credentials, ok := p.Spec.Credentials.(*PluginStaticCredentialsSpecV1_BasicAuth)
+	if !ok {
+		return false, "", ""
+	}
+
+	return true, credentials.BasicAuth.Username, credentials.BasicAuth.Password
+}
+
+// GetOAuthClientSecret will return a flag indicating whether this object contains OAuth
+// client secret credentials and the attached client ID and client secret.
+func (p *PluginStaticCredentialsV1) GetOAuthClientSecret() (ok bool, clientID string, clientSecret string) {
+	credentials, ok := p.Spec.Credentials.(*PluginStaticCredentialsSpecV1_OauthClientSecret)
+	if !ok {
+		return false, "", ""
+	}
+
+	return true, credentials.OauthClientSecret.ClientId, credentials.OauthClientSecret.ClientSecret
+}
+
+// MatchSearch is a dummy value as credentials are not searchable.
+func (p *PluginStaticCredentialsV1) MatchSearch(_ []string) bool {
+	return false
+}
