f5ebf2f811ce08ade057a76a340178acf1f91fea
Rafał Cieślak <rafal.cieslak@goteleport.com>
2023-05-26 08:51:52 +0200

    [v11] Spawn gateway CLI client directly (#26755)
    * Connect: Create dedicated functions for connecting to resources
    
    * Create dedicated functions for connecting to resources
    
    * Do not use spread operator
    
    * Rename `connectToNode` -> `connectToServer`
    
    * Remove unused imports
    
    * Spawn gateway CLI client directly
    
    * Return os.exec.Cmd as gateway CLI command
    
    * Remove separate Props type from DocumentTerminal
    
    * Refactor Kind type exported from documentsService
    
    * Export makeRuntimeSettings from MainProcess mock
    
    * PtyProcess: Join args in logger name
    
    * ptyHostService: Pass ptyOptions explicitly instead of using spread
    
    I noticed that we pass both argsList and args to the PtyProcess constructor.
    While TypeScript allows that, it is a bit confusing when inspecting the
    actual values received in the constructor.
    
    * Add empty DocumentGatewayCliClient
    
    * Start terminal from DocumentGatewayCliClient
    
    * Add waiting state for DocumentGatewayCliClient
    
    * Remove targetSubresourceName from DocumentGatewayCliClient title
    
    * Preserve backwards compatibility of teleport.lib.teleterm.v1.Gateway
    
    * Capture the error if the gateway CLI client executable is not found
    
    * Add onStartError to PTY process
    
    * Add logging to frontend PtyEventsStreamHandler
    
    * Return cleanup functions from pty process onEvent functions
    
    * Extract DocumentReconnect from DocumentTerminal
    
    The Reconnect component will be useful when handling startError.
    
    * Add getPtyId to PtyProcess
    
    This will act as the key for the Terminal component, allowing us to
    make sure we mount it only once for any given PTY process.
    
    * Capture startError and display an error state
    
    * Terminal/ctrl: Cleanup ptyProcess onData listener
    
    * Include command name and args in startError error message
    
    * Remove ptyId field from PtyProcess
    
    ---------
    
    Co-authored-by: Grzegorz Zdunek <gzdunek@users.noreply.github.com>

---
+++ web/packages/teleterm/src/ui/services/workspacesService/documentsService/connectToKube.ts
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2023 Gravitational, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { KubeUri, routing } from 'teleterm/ui/uri';
+import { IAppContext } from 'teleterm/ui/types';
+import { TrackedKubeConnection } from 'teleterm/ui/services/connectionTracker';
+
+export async function connectToKube(
+  ctx: IAppContext,
+  target: { uri: KubeUri }
+): Promise<void> {
+  const rootClusterUri = routing.ensureRootClusterUri(target.uri);
+  const documentsService =
+    ctx.workspacesService.getWorkspaceDocumentService(rootClusterUri);
+  const kubeDoc = documentsService.createTshKubeDocument({
+    kubeUri: target.uri,
+  });
+  const connection = ctx.connectionTracker.findConnectionByDocument(
+    kubeDoc
+  ) as TrackedKubeConnection;
+
+  await ctx.workspacesService.setActiveWorkspace(rootClusterUri);
+
+  documentsService.add({
+    ...kubeDoc,
+    kubeConfigRelativePath:
+      connection?.kubeConfigRelativePath || kubeDoc.kubeConfigRelativePath,
+  });
+  documentsService.open(kubeDoc.uri);
+}
