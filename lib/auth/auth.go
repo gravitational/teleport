00d422b2052303fccb7d6f9684a8601896fce618
cui fliter <imcusg@gmail.com>
2023-05-29 13:17:50 +0800

    Fix: time.Since should not be used directly after a defer statement
    Signed-off-by: cui fliter <imcusg@gmail.com>

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -3243,7 +3243,7 @@ func (a *Server) GenerateHostCerts(ctx context.Context, req *proto.HostCertsRequ
 
 	// only observe latencies for non-throttled requests
 	start := a.clock.Now()
-	defer generateRequestsLatencies.Observe(time.Since(start).Seconds())
+	defer func() { generateRequestsLatencies.Observe(time.Since(start).Seconds()) }()
 
 	generateRequestsCount.Inc()
 	generateRequestsCurrent.Inc()
