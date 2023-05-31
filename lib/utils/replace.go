6c5a5741cea1f3b03abd4aba2a27c1f453d6215e
Nic Klaassen <nic@goteleport.com>
2023-05-31 09:32:27 -0700

    [v13] feat: label expression parser (#26970)
    Backport #26176 to branch/v13
    
    This PR builds on https://github.com/gravitational/teleport/pull/26177
    to implement the label expression parser as defined in
    [RFD 116](https://github.com/gravitational/teleport/blob/master/rfd/0116-label-expressions.md#expression-syntax).

--- lib/utils/replace.go
+++ lib/utils/replace.go
@@ -128,6 +128,25 @@ func SliceMatchesRegex(input string, expressions []string) (bool, error) {
 	return false, nil
 }
 
+// RegexMatchesAny returns true if [expression] matches any element of
+// [inputs]. [expression] support globbing ("env-*") or normal regexp syntax if
+// surrounded with ^$ ("^env-.*$").
+func RegexMatchesAny(inputs []string, expression string) (bool, error) {
+	expr, err := compileRegexCached(expression)
+	if err != nil {
+		return false, trace.Wrap(err)
+	}
+	for _, input := range inputs {
+		// Since the expression is always surrounded by ^ and $ this is an exact
+		// match for either a plain string (for example ^hello$) or for a regexp
+		// (for example ^hel*o$).
+		if expr.MatchString(input) {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
 // mustCache initializes a new [lru.Cache] with the provided size.
 // A panic will be triggered if the creation of the cache fails.
 func mustCache[K comparable, V any](size int) *lru.Cache[K, V] {
