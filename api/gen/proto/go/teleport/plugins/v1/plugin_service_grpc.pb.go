dd3e38c11af11168305049e8962174fd54a71409
Mike Wilson <michael.wilson@goteleport.com>
2023-06-01 14:57:20 -0400

    Add plugin static credentials getter.
    The plugins service now supports getting plugin static credentials through
    gRPC. This is necessary for supporting static credentials storage and
    reading in our plugins.

--- api/gen/proto/go/teleport/plugins/v1/plugin_service_grpc.pb.go
+++ api/gen/proto/go/teleport/plugins/v1/plugin_service_grpc.pb.go
@@ -35,13 +35,14 @@ import (
 const _ = grpc.SupportPackageIsVersion7
 
 const (
-	PluginService_CreatePlugin_FullMethodName            = "/teleport.plugins.v1.PluginService/CreatePlugin"
-	PluginService_GetPlugin_FullMethodName               = "/teleport.plugins.v1.PluginService/GetPlugin"
-	PluginService_DeletePlugin_FullMethodName            = "/teleport.plugins.v1.PluginService/DeletePlugin"
-	PluginService_ListPlugins_FullMethodName             = "/teleport.plugins.v1.PluginService/ListPlugins"
-	PluginService_SetPluginCredentials_FullMethodName    = "/teleport.plugins.v1.PluginService/SetPluginCredentials"
-	PluginService_SetPluginStatus_FullMethodName         = "/teleport.plugins.v1.PluginService/SetPluginStatus"
-	PluginService_GetAvailablePluginTypes_FullMethodName = "/teleport.plugins.v1.PluginService/GetAvailablePluginTypes"
+	PluginService_CreatePlugin_FullMethodName               = "/teleport.plugins.v1.PluginService/CreatePlugin"
+	PluginService_GetPlugin_FullMethodName                  = "/teleport.plugins.v1.PluginService/GetPlugin"
+	PluginService_DeletePlugin_FullMethodName               = "/teleport.plugins.v1.PluginService/DeletePlugin"
+	PluginService_ListPlugins_FullMethodName                = "/teleport.plugins.v1.PluginService/ListPlugins"
+	PluginService_SetPluginCredentials_FullMethodName       = "/teleport.plugins.v1.PluginService/SetPluginCredentials"
+	PluginService_SetPluginStatus_FullMethodName            = "/teleport.plugins.v1.PluginService/SetPluginStatus"
+	PluginService_GetAvailablePluginTypes_FullMethodName    = "/teleport.plugins.v1.PluginService/GetAvailablePluginTypes"
+	PluginService_GetPluginStaticCredentials_FullMethodName = "/teleport.plugins.v1.PluginService/GetPluginStaticCredentials"
 )
 
 // PluginServiceClient is the client API for PluginService service.
@@ -63,6 +64,9 @@ type PluginServiceClient interface {
 	// GetAvailablePluginTypes returns the types of plugins
 	// that the auth server supports onboarding.
 	GetAvailablePluginTypes(ctx context.Context, in *GetAvailablePluginTypesRequest, opts ...grpc.CallOption) (*GetAvailablePluginTypesResponse, error)
+	// GetPluginStaticCredentials returns static credentials that are requested. Only accessible by RoleAdmin and,
+	// in the case of Teleport Assist, RoleProxy.
+	GetPluginStaticCredentials(ctx context.Context, in *GetPluginStaticCredentialsRequest, opts ...grpc.CallOption) (*GetPluginStaticCredentialsResponse, error)
 }
 
 type pluginServiceClient struct {
@@ -136,6 +140,15 @@ func (c *pluginServiceClient) GetAvailablePluginTypes(ctx context.Context, in *G
 	return out, nil
 }
 
+func (c *pluginServiceClient) GetPluginStaticCredentials(ctx context.Context, in *GetPluginStaticCredentialsRequest, opts ...grpc.CallOption) (*GetPluginStaticCredentialsResponse, error) {
+	out := new(GetPluginStaticCredentialsResponse)
+	err := c.cc.Invoke(ctx, PluginService_GetPluginStaticCredentials_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // PluginServiceServer is the server API for PluginService service.
 // All implementations must embed UnimplementedPluginServiceServer
 // for forward compatibility
@@ -155,6 +168,9 @@ type PluginServiceServer interface {
 	// GetAvailablePluginTypes returns the types of plugins
 	// that the auth server supports onboarding.
 	GetAvailablePluginTypes(context.Context, *GetAvailablePluginTypesRequest) (*GetAvailablePluginTypesResponse, error)
+	// GetPluginStaticCredentials returns static credentials that are requested. Only accessible by RoleAdmin and,
+	// in the case of Teleport Assist, RoleProxy.
+	GetPluginStaticCredentials(context.Context, *GetPluginStaticCredentialsRequest) (*GetPluginStaticCredentialsResponse, error)
 	mustEmbedUnimplementedPluginServiceServer()
 }
 
@@ -182,6 +198,9 @@ func (UnimplementedPluginServiceServer) SetPluginStatus(context.Context, *SetPlu
 }
 func (UnimplementedPluginServiceServer) GetAvailablePluginTypes(context.Context, *GetAvailablePluginTypesRequest) (*GetAvailablePluginTypesResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetAvailablePluginTypes not implemented")
+}
+func (UnimplementedPluginServiceServer) GetPluginStaticCredentials(context.Context, *GetPluginStaticCredentialsRequest) (*GetPluginStaticCredentialsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetPluginStaticCredentials not implemented")
 }
 func (UnimplementedPluginServiceServer) mustEmbedUnimplementedPluginServiceServer() {}
 
@@ -322,6 +341,24 @@ func _PluginService_GetAvailablePluginTypes_Handler(srv interface{}, ctx context
 	return interceptor(ctx, in, info, handler)
 }
 
+func _PluginService_GetPluginStaticCredentials_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetPluginStaticCredentialsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(PluginServiceServer).GetPluginStaticCredentials(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: PluginService_GetPluginStaticCredentials_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(PluginServiceServer).GetPluginStaticCredentials(ctx, req.(*GetPluginStaticCredentialsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // PluginService_ServiceDesc is the grpc.ServiceDesc for PluginService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -356,6 +393,10 @@ var PluginService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetAvailablePluginTypes",
 			Handler:    _PluginService_GetAvailablePluginTypes_Handler,
+		},
+		{
+			MethodName: "GetPluginStaticCredentials",
+			Handler:    _PluginService_GetPluginStaticCredentials_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
