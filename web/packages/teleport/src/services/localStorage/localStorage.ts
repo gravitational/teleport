851a11fb91ec94699456a61b7b60cf95a281aa4d
Lisa Kim <lisa@goteleport.com>
2023-06-01 16:52:54 -0700

    Fix local storage clearing

--- web/packages/teleport/src/services/localStorage/localStorage.ts
+++ web/packages/teleport/src/services/localStorage/localStorage.ts
@@ -29,13 +29,15 @@ const KEEP_LOCALSTORAGE_KEYS_ON_LOGOUT = [
 
 const storage = {
   clear() {
+    const keys = [];
     for (let i = 0; i < window.localStorage.length; i++) {
-      const key = window.localStorage.key(i);
-
+      keys.push(window.localStorage.key(i));
+    }
+    keys.forEach(key => {
       if (!KEEP_LOCALSTORAGE_KEYS_ON_LOGOUT.includes(key)) {
         window.localStorage.removeItem(key);
       }
-    }
+    });
   },
 
   subscribe(fn) {
