e4ae7cca4ef22b995ef31306bc1b8a2612711c03
Mike Wilson <michael.wilson@goteleport.com>
2023-06-02 10:51:41 -0400

    Make sure proxy can create sessions.

--- lib/auth/auth_with_roles_test.go
+++ lib/auth/auth_with_roles_test.go
@@ -5014,6 +5014,10 @@ func TestCreateSAMLIdPSession(t *testing.T) {
 		identity  TestIdentity
 		assertErr require.ErrorAssertionFunc
 	}{
+		"as proxy user": {
+			identity:  TestBuiltin(types.RoleProxy),
+			assertErr: require.NoError,
+		},
 		"as session user": {
 			identity:  TestUser(alice),
 			assertErr: require.NoError,
