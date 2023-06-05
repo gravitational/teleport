d77a1bdabb1b6b77b79502415039b3ea2da3d303
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2023-06-05 19:06:21 -0400

    Move more code.

--- web/packages/teleport/src/Assist/contexts/messages.tsx
+++ web/packages/teleport/src/Assist/contexts/messages.tsx
@@ -88,6 +88,7 @@ interface ExecEvent {
   event: EventType.EXEC;
   exitError?: string;
 }
+
 type SessionEvent = ExecEvent | { event: string };
 
 const convertToQuery = (cmd: ExecuteRemoteCommandPayload): string => {
@@ -253,14 +254,15 @@ async function convertServerMessage(
 
     const eventsResp = await api.fetch(sessionUrl + '/events');
     const sessionExists = eventsResp.status === 200;
-    const eventsData = (await eventsResp.json()) as {
-      events: SessionEvent[];
-    };
 
     let msg;
     let errorMsg;
     if (sessionExists) {
-      // eventsData.events can be empty if the command execution failed.
+      // Get events only if the session exists. Otherwise, eventsData.events can be empty
+      // if the command execution failed.
+      const eventsData = (await eventsResp.json()) as {
+        events: SessionEvent[];
+      };
       const execEvent = eventsData.events.find(isExecEvent);
       // The offset here is set base on A/B test that was run between me, myself and I.
       const stream = await api.fetch(
