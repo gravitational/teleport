1be7bc72c69077ea2f7e94e262e639361bb7bc4b
Alan Parra <alan.parra@goteleport.com>
2023-06-05 11:39:56 -0300

    Implement the MDM sync scheduler (#1554)
    The MDM sync scheduler calculates the sync schedule, taking a list of inventory
    entries as input (Jamf, Intune, etc).
    
    A single entry can specify periods for both PARTIAL and FULL syncs, with the
    latter taking precedence. In this case, it's expected for PARTIAL syncs to be
    more frequent than FULL, so they are "squeezed" in-between FULL runs. (If
    PARTIALs are less frequent than FULLs they would never run - in practice this
    configuration is [not allowed][1].)
    
    Each entry represents its own separate schedule and does not interfere in other
    entries.
    
    The scheduler itself is designed to be sleep free - the waiting is done by
    whatever is using it, if desired. In our case, the specific MDM services will
    code the wait using `time.Timer` or a similar mechanism.
    
    #826
    
    [1]: https://github.com/gravitational/teleport/blob/38bd6f00994efa983e436109c25755e702b135e2/api/types/jamf.go#L87-L89
    
    Source-commit: gravitational/teleport.e@9b8185ca122db3f146ffaa31f6eb37b9c23d72a5
    Source-pull-requests: gravitational/teleport.e#1554

---
+++ e/lib/mdm/scheduler.go
@@ -0,0 +1,194 @@
+package mdm
+
+import (
+	"errors"
+	"sort"
+	"time"
+
+	"github.com/gravitational/trace"
+
+	devicepb "github.com/gravitational/teleport/api/gen/proto/go/teleport/devicetrust/v1"
+)
+
+// ErrScheduleNoEntries is returned when no entries are provided to the
+// [SyncScheduler].
+var ErrScheduleNoEntries = errors.New("no schedule entries provided")
+
+// ErrScheduleEmpty is returned when all schedule entries are inactive.
+var ErrScheduleEmpty = errors.New("schedule has no active entries")
+
+// ScheduleEntry is an entry in the sync schedule.
+type ScheduleEntry[E any] struct {
+	// Entry is the underlying sync entry.
+	Entry E
+	// Mode is the sync mode to be used for the entry.
+	Mode devicepb.SyncInventoryMode
+
+	avoidReschedule bool
+	offset          time.Duration
+}
+
+// ScheduleEntryInfo is a subset of the information in sync inventory entries,
+// like [types.JamfInventoryEntry].
+type ScheduleEntryInfo struct {
+	// SyncPeriodPartial is the PARTIAL sync period.
+	// Zero or negavite disables PARTIAL syncs.
+	SyncPeriodPartial time.Duration
+	// SyncPeriodFull is the FULL sync period.
+	// Zero or negavite disables FULL syncs.
+	SyncPeriodFull time.Duration
+}
+
+// SyncScheduler calculates the sync schedule for MDM services.
+//
+// Call [NextOffset] to discover when the next sync entry is due. Call [Next] to
+// "advance" time to the next entry and pop it from the schedule.
+//
+// Sync schedules are endless and will always return more entries.
+type SyncScheduler[E any] struct {
+	// schedule are the scheduled entries, sorted by ascending offset.
+	// Guaranteed to always have at least one entry.
+	schedule []ScheduleEntry[E]
+	infoFn   func(E) ScheduleEntryInfo
+}
+
+// NewSyncScheduler creates a new MDM sync scheduler.
+//
+// `initialDelayFn` calculates the initial sync delay for entries. Guaranteed to
+// be invoked once per entry in the `entries` slice, in input order.
+//
+// `infoFn` extracts a [ScheduleEntryInfo] from the underlying entry.
+func NewSyncScheduler[E any](entries []E, initialDelayFn func() time.Duration, infoFn func(E) ScheduleEntryInfo) (*SyncScheduler[E], error) {
+	switch {
+	case len(entries) == 0:
+		return nil, trace.Wrap(ErrScheduleNoEntries)
+	case initialDelayFn == nil:
+		return nil, trace.BadParameter("initialDelayFn required")
+	case infoFn == nil:
+		return nil, trace.BadParameter("infoFn required")
+	}
+
+	t := &SyncScheduler[E]{
+		infoFn: infoFn,
+	}
+	t.initializeSchedule(entries, initialDelayFn)
+	if len(t.schedule) == 0 {
+		return nil, trace.Wrap(ErrScheduleEmpty)
+	}
+
+	return t, nil
+}
+
+// NextOffset returns when the next [ScheduleEntry] is due.
+func (t *SyncScheduler[E]) NextOffset() time.Duration {
+	return t.schedule[0].offset
+}
+
+// Next advances the schedule to the next [ScheduleEntry] and returns it.
+func (t *SyncScheduler[E]) Next() ScheduleEntry[E] {
+	// Pop next entry.
+	entry := t.schedule[0]
+	t.schedule = t.schedule[1:]
+
+	// "Advance" schedule.
+	for i := range t.schedule {
+		t.schedule[i].offset -= entry.offset
+	}
+
+	// Reschedule future entries.
+	if !entry.avoidReschedule {
+		t.reschedule(entry.Entry, 0 /* initialDelay */, false /* initialSync */)
+		t.sort()
+	}
+
+	return entry
+}
+
+func (t *SyncScheduler[E]) initializeSchedule(entries []E, initialDelayFn func() time.Duration) {
+	for _, e := range entries {
+		t.reschedule(e, initialDelayFn(), true /* initialSync */)
+	}
+	t.sort()
+}
+
+func (t *SyncScheduler[E]) reschedule(entry E, initialDelay time.Duration, initialSync bool) {
+	// There are 2 "types" of schedules:
+	//
+	// 1) FULL + PARTIAL (PARTIAL < FULL)
+	//    T0 ---- FULL ---- PARTIAL 1 ---- PARTIAL n (as many as can fit) ---- FULL ---- (Repeat)
+	//
+	//    Reschedule for PARTIALs and FULLs happens when time a FULL entry is
+	//    popped, with exception of the initial FULL entry.
+	//
+	// 2) FULL only or PARTIAL only
+	//    T0 ---- FULL or PARTIAL ---- (Repeat)
+	//
+	//    Reschedule happens whenever an entry is popped.
+
+	info := t.infoFn(entry)
+	hasFull := info.SyncPeriodFull > 0
+	hasPartial := info.SyncPeriodPartial > 0
+
+	switch {
+	case hasFull && hasPartial && info.SyncPeriodPartial < info.SyncPeriodFull: // type "1"
+		// Schedule initial FULL sync.
+		if initialSync {
+			t.schedule = append(t.schedule, ScheduleEntry[E]{
+				Entry:           entry,
+				Mode:            devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_FULL,
+				avoidReschedule: true,         // another FULL is in the schedule.
+				offset:          initialDelay, // sync "immediately"
+			})
+		}
+
+		// Schedule delayed FULL sync.
+		fullOffset := initialDelay + info.SyncPeriodFull
+		t.schedule = append(t.schedule, ScheduleEntry[E]{
+			Entry:  entry,
+			Mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_FULL,
+			offset: fullOffset,
+		})
+
+		// Schedule PARTIAL syncs in-between.
+		partialOffset := initialDelay + info.SyncPeriodPartial
+		for partialOffset < fullOffset {
+			t.schedule = append(t.schedule, ScheduleEntry[E]{
+				Entry:           entry,
+				Mode:            devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_PARTIAL,
+				avoidReschedule: true, // reschedule only on FULL.
+				offset:          partialOffset,
+			})
+			partialOffset += info.SyncPeriodPartial
+		}
+	case hasFull || hasPartial: // type "2", FULL or PARTIAL
+		var mode devicepb.SyncInventoryMode
+		var offset time.Duration
+
+		// FULL takes precedence.
+		if hasFull {
+			offset = info.SyncPeriodFull
+			mode = devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_FULL
+		} else {
+			offset = info.SyncPeriodPartial
+			mode = devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_PARTIAL
+		}
+
+		if initialSync {
+			offset = initialDelay // sync "immediately"
+		} else {
+			offset += initialDelay
+		}
+
+		t.schedule = append(t.schedule, ScheduleEntry[E]{
+			Entry:  entry,
+			Mode:   mode,
+			offset: offset,
+		})
+	}
+}
+
+func (t *SyncScheduler[E]) sort() {
+	sort.Slice(t.schedule, func(i, j int) bool {
+		return t.schedule[i].offset < t.schedule[j].offset
+	})
+}
---
+++ e/lib/mdm/scheduler_test.go
@@ -0,0 +1,362 @@
+package mdm_test
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+
+	devicepb "github.com/gravitational/teleport/api/gen/proto/go/teleport/devicetrust/v1"
+	"github.com/gravitational/teleport/api/types"
+	"github.com/gravitational/teleport/e/lib/mdm"
+)
+
+func TestSyncScheduler_completeSchedule(t *testing.T) {
+	entries := []*types.JamfInventoryEntry{
+		{
+			FilterRsql:        "full+partial", // Abuse RSQL strings for easier debugging.
+			SyncPeriodPartial: types.Duration(6 * time.Hour),
+			SyncPeriodFull:    types.Duration(18 * time.Hour),
+		},
+		{
+			FilterRsql:        "full only",
+			SyncPeriodPartial: types.Duration(8 * time.Hour), // partial>=full means only full happens.
+			SyncPeriodFull:    types.Duration(8 * time.Hour),
+		},
+		{
+			FilterRsql:        "partial only",
+			SyncPeriodPartial: types.Duration(5 * time.Hour),
+			SyncPeriodFull:    0, // disabled
+		},
+		{
+			FilterRsql:        "none",
+			SyncPeriodPartial: -1, // disabled, same as zero
+			SyncPeriodFull:    -1, // disabled, same as zero
+		},
+	}
+	fullPartial := entries[0]
+	fullOnly := entries[1]
+	partialOnly := entries[2]
+	// "none" is never scheduled
+
+	scheduler, err := mdm.NewSyncScheduler(entries, incDelay(), jamfInfo)
+	if err != nil {
+		t.Fatalf("NewSyncScheduler failed: %v", err)
+	}
+
+	assertSchedule(t, scheduler, []wantSchedule{
+		// t=0, initial sync
+		{
+			offset: 0,
+			entry:  fullPartial,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_FULL,
+		},
+		// t=1, initial sync
+		{
+			offset: 1,
+			entry:  fullOnly,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_FULL,
+		},
+		// t=2, initial sync
+		{
+			offset: 1,
+			entry:  partialOnly,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_PARTIAL,
+		},
+		// t=5h
+		{
+			offset: 5 * time.Hour,
+			entry:  partialOnly,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_PARTIAL,
+		},
+		// t=6h
+		{
+			offset: 1 * time.Hour,
+			entry:  fullPartial,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_PARTIAL,
+		},
+		// t=8h
+		{
+			offset: 2 * time.Hour,
+			entry:  fullOnly,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_FULL,
+		},
+		// t=10h
+		{
+			offset: 2 * time.Hour,
+			entry:  partialOnly,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_PARTIAL,
+		},
+		// t=12h
+		{
+			offset: 2 * time.Hour,
+			entry:  fullPartial,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_PARTIAL,
+		},
+		// t=15h
+		{
+			offset: 3 * time.Hour,
+			entry:  partialOnly,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_PARTIAL,
+		},
+		// t=16h
+		{
+			offset: 1 * time.Hour,
+			entry:  fullOnly,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_FULL,
+		},
+		// t=18h
+		{
+			offset: 2 * time.Hour,
+			entry:  fullPartial,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_FULL,
+		},
+		// t=20h
+		{
+			offset: 2 * time.Hour,
+			entry:  partialOnly,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_PARTIAL,
+		},
+		// t=24h
+		{
+			offset: 4 * time.Hour,
+			entry:  fullPartial,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_PARTIAL,
+		},
+		{
+			offset: 1,
+			entry:  fullOnly,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_FULL,
+		},
+		// and so on...
+	})
+}
+
+func TestSyncScheduler_partialNotDivisor(t *testing.T) {
+	// SyncPeriodPartial is not a divisor of SyncPeriodFull, meaning the schedules
+	// don't line up perfectly.
+	entry := &types.JamfInventoryEntry{
+		SyncPeriodPartial: types.Duration(5 * time.Hour),
+		SyncPeriodFull:    types.Duration(12 * time.Hour),
+	}
+
+	scheduler, err := mdm.NewSyncScheduler(
+		[]*types.JamfInventoryEntry{entry}, zeroDelay, jamfInfo)
+	if err != nil {
+		t.Fatalf("NewSyncScheduler failed: %v", err)
+	}
+
+	assertSchedule(t, scheduler, []wantSchedule{
+		{
+			entry: entry,
+			mode:  devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_FULL,
+		},
+		// t=5h
+		{
+			offset: 5 * time.Hour,
+			entry:  entry,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_PARTIAL,
+		},
+		// t=10h
+		{
+			offset: 5 * time.Hour,
+			entry:  entry,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_PARTIAL,
+		},
+		// t=12h
+		{
+			offset: 2 * time.Hour,
+			entry:  entry,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_FULL,
+		},
+		// t=17
+		{
+			offset: 5 * time.Hour,
+			entry:  entry,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_PARTIAL,
+		},
+		// t=22h
+		{
+			offset: 5 * time.Hour,
+			entry:  entry,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_PARTIAL,
+		},
+		// t=24
+		{
+			offset: 2 * time.Hour,
+			entry:  entry,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_FULL,
+		},
+		// t=29
+		{
+			offset: 5 * time.Hour,
+			entry:  entry,
+			mode:   devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_PARTIAL,
+		},
+		// and so on...
+	})
+}
+
+func TestSyncScheduler_scheduleCantBeExhausted(t *testing.T) {
+	entries := []*types.JamfInventoryEntry{
+		{
+			FilterRsql:        "full+partial", // Abuse RSQL strings for easier debugging.
+			SyncPeriodPartial: types.Duration(1 * time.Minute),
+			SyncPeriodFull:    types.Duration(2 * time.Minute),
+		},
+		{
+			FilterRsql:        "partial",
+			SyncPeriodPartial: types.Duration(2 * time.Minute),
+		},
+		{
+			FilterRsql:     "full",
+			SyncPeriodFull: types.Duration(2 * time.Minute),
+		},
+	}
+
+	scheduler, err := mdm.NewSyncScheduler(entries, incDelay(), jamfInfo)
+	if err != nil {
+		t.Fatalf("NewSyncScheduler failed: %v", err)
+	}
+
+	// Skip initial entries, so we get to the "loop".
+	scheduler.Next()
+	scheduler.Next()
+	scheduler.Next()
+
+	want := []wantSchedule{
+		// t=1
+		{
+			entry: entries[0],
+			mode:  devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_PARTIAL,
+		},
+		// t=2
+		{
+			entry: entries[0],
+			mode:  devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_FULL,
+		},
+		{
+			entry: entries[1],
+			mode:  devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_PARTIAL,
+		},
+		{
+			entry: entries[2],
+			mode:  devicepb.SyncInventoryMode_SYNC_INVENTORY_MODE_FULL,
+		},
+		// and then loops perfectly.
+	}
+
+	// Run an arbitrarily high number of iterations, showing that the schedule
+	// loops perfectly and can't be exhausted.
+	const iterations = 400
+	for i := 0; i < iterations; i++ {
+		w := want[i%len(want)]
+		got := scheduler.Next()
+		if diff := cmp.Diff(w.entry, got.Entry); diff != "" {
+			t.Fatalf("Next(%v) mismatch (-want +got)\n%s", i, diff)
+		}
+		if got.Mode != w.mode {
+			t.Errorf("Next(%v) mode=%v, want %v", i, got.Mode, w.mode)
+		}
+	}
+}
+
+func TestNewSyncScheduler_errors(t *testing.T) {
+	tests := []struct {
+		name    string
+		entries []*types.JamfInventoryEntry
+		wantErr error
+	}{
+		{
+			name:    "empty entries",
+			wantErr: mdm.ErrScheduleNoEntries,
+		},
+		{
+			name: "empty schedule",
+			entries: []*types.JamfInventoryEntry{
+				{
+					SyncPeriodPartial: 0, // both syncs disabled
+					SyncPeriodFull:    0,
+				},
+				{
+					SyncPeriodPartial: -1, // both syncs disabled
+					SyncPeriodFull:    -1,
+				},
+			},
+			wantErr: mdm.ErrScheduleEmpty,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_, err := mdm.NewSyncScheduler(test.entries, zeroDelay, jamfInfo)
+			if !errors.Is(err, test.wantErr) {
+				t.Errorf("NewSyncScheduler returned err=%v, want %q", err, test.wantErr)
+			}
+		})
+	}
+}
+
+// incDelay returns a function with a monotonically-increasing delay.
+// Useful to space out starting entries evenly.
+func incDelay() func() time.Duration {
+	var delay time.Duration
+	return func() time.Duration {
+		delay++
+		return delay
+	}
+}
+
+func zeroDelay() time.Duration {
+	return 0
+}
+
+func jamfInfo(e *types.JamfInventoryEntry) mdm.ScheduleEntryInfo {
+	return mdm.ScheduleEntryInfo{
+		SyncPeriodPartial: time.Duration(e.SyncPeriodPartial),
+		SyncPeriodFull:    time.Duration(e.SyncPeriodFull),
+	}
+}
+
+type wantSchedule struct {
+	// offset is the offset of this entry in relation to the preceding entry.
+	offset time.Duration
+	entry  *types.JamfInventoryEntry
+	mode   devicepb.SyncInventoryMode
+}
+
+func assertSchedule(t *testing.T, scheduler *mdm.SyncScheduler[*types.JamfInventoryEntry], wantEntries []wantSchedule) {
+	var elapsed time.Duration
+	for i, want := range wantEntries {
+		// Ignore nanos if the offset doesn't include any.
+		// Makes testing simpler.
+		var maxDiff time.Duration
+		if want.offset%1000 == 0 {
+			maxDiff = 2 // Up to 2ns of difference is OK.
+		}
+
+		// Assert NextOffset().
+		gotOffset := scheduler.NextOffset()
+		if got := scheduler.NextOffset(); want.offset-got > maxDiff {
+			t.Errorf("NextOffset(%v) = %v, want %v", i, got, want.offset)
+		}
+		elapsed += gotOffset
+
+		// Assert Next() schedule entry.
+		t.Run(fmt.Sprintf("t=%v", elapsed), func(t *testing.T) {
+			gotEntry := scheduler.Next()
+			wantEntry := mdm.ScheduleEntry[*types.JamfInventoryEntry]{
+				Entry: want.entry,
+				Mode:  want.mode,
+			}
+			if diff := cmp.Diff(
+				wantEntry, gotEntry,
+				cmpopts.IgnoreFields(gotEntry, "avoidReschedule", "offset"),
+			); diff != "" {
+				t.Errorf("Next(%v) mismatch (-want +got)\n%s", i, diff)
+			}
+		})
+	}
+}
