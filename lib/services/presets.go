a3ba8e41884bc3b55a3892c9ca68c5a2d6b492c0
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2023-06-05 16:03:03 -0400

    Fixed test

--- lib/services/presets.go
+++ lib/services/presets.go
@@ -143,6 +143,7 @@ func NewPresetAccessRole() types.Role {
 						Where:     "contains(session.participants, user.metadata.name)",
 					},
 					types.NewRule(types.KindInstance, RO()),
+					types.NewRule(types.KindAssistant, append(RW(), types.VerbUse)),
 					// Please see defaultAllowRules when adding a new rule.
 				},
 			},
