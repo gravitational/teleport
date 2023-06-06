01e9286f5c8f3a984e5fc4ccf17952b1cd1b641a
Noah <noah.stride@goteleport.com>
2023-06-06 23:32:28 +0100

    `make fix-imports`

--- lib/tbot/config/output_database.go
+++ lib/tbot/config/output_database.go
@@ -19,11 +19,11 @@ package config
 import (
 	"context"
 	"fmt"
-	"github.com/gravitational/teleport/api/types"
 
 	"github.com/gravitational/trace"
 	"golang.org/x/exp/slices"
 
+	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/lib/tbot/bot"
 	"github.com/gravitational/teleport/lib/tbot/identity"
 )
