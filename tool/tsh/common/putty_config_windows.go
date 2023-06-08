243a375914794f9e6c82f15deb920a62387308cf
Gus Luxton <gus@goteleport.com>
2023-06-08 12:01:55 -0300

    Fix up putty_config_windows

--- tool/tsh/common/putty_config_windows.go
+++ tool/tsh/common/putty_config_windows.go
@@ -21,20 +21,20 @@ package common
 import (
 	"fmt"
 	"net"
-	"runtime"
-	"sort"
 	"strings"
 	"syscall"
 
 	"github.com/gravitational/trace"
 	"golang.org/x/crypto/ssh"
-	"golang.org/x/exp/slices"
 
 	"github.com/gravitational/teleport/api/constants"
 	"github.com/gravitational/teleport/api/profile"
+	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/api/utils/keypaths"
 	"github.com/gravitational/teleport/lib/auth"
-	"github.com/gravitational/teleport/lib/client"
+	"github.com/gravitational/teleport/lib/puttyhosts"
+	"github.com/gravitational/teleport/lib/sshutils"
+	"github.com/gravitational/teleport/lib/utils/registry"
 )
 
 // the key should not include HKEY_CURRENT_USER
@@ -54,6 +54,9 @@ const puttyDefaultProxyPort = 0 // no need to set the proxy port as it's abstrac
 const puttyDwordPresent = `00000001`
 const puttyDwordProxyMethod = `00000005`    // run a local command
 const puttyDwordProxyLogToTerm = `00000002` // only until session starts
+const puttyPermitRSASHA1 = `00000000`
+const puttyPermitRSASHA256 = `00000001`
+const puttyPermitRSASHA512 = `00000001`
 
 // despite the strings/ints in struct, these are stored in the registry as DWORDs
 type puttyRegistrySessionDwords struct {
@@ -77,18 +80,24 @@ type puttyRegistrySessionStrings struct {
 }
 
 // addPuTTYSession adds a PuTTY session for the given host/port to the Windows registry
-func addPuTTYSession(proxyHostname string, hostname string, port int, login string, ppkFilePath string, certificateFilePath string, commandToRun string) (bool, error) {
-	puttySessionName := fmt.Sprintf(`%v%%20(%v)`, hostname, proxyHostname)
+func addPuTTYSession(proxyHostname string, hostname string, port int, login string, ppkFilePath string, certificateFilePath string, commandToRun string, leafClusterName string) error {
+	// note: the use of ` and double % signs here is intentional
+	// the registry key is named "hostname.example.com%20(proxy:teleport.example.com)"
+	// this produces a session name which displays in PuTTY as "hostname.example.com (proxy:teleport.example.com)"
+	puttySessionName := fmt.Sprintf(`%v%%20(proxy:%v)`, hostname, proxyHostname)
+	if leafClusterName != "" {
+		// the registry key is named "hostname.example.com%20(leaf:leaf.example.com,proxy:teleport.example.com)"
+		// this produces a session name which displays in PuTTY as "hostname.example.com (leaf:leaf.example.com,proxy:teleport.example.com)"
+		puttySessionName = fmt.Sprintf(`%v%%20(leaf:%v,proxy:%v)`, hostname, leafClusterName, proxyHostname)
+	}
 	registryKey := fmt.Sprintf(`%v\%v`, puttyRegistrySessionsKey, puttySessionName)
-	sessionDwords := puttyRegistrySessionDwords{}
-	sessionStrings := puttyRegistrySessionStrings{}
 
 	// if the port passed is 0, this means "use server default" so we override it to 3022
 	if port == 0 {
 		port = puttyDefaultSSHPort
 	}
 
-	sessionDwords = puttyRegistrySessionDwords{
+	sessionDwords := puttyRegistrySessionDwords{
 		Present:        puttyDwordPresent,
 		PortNumber:     port,
 		ProxyPort:      puttyDefaultProxyPort,
@@ -96,7 +105,7 @@ func addPuTTYSession(proxyHostname string, hostname string, port int, login stri
 		ProxyLogToTerm: puttyDwordProxyLogToTerm,
 	}
 
-	sessionStrings = puttyRegistrySessionStrings{
+	sessionStrings := puttyRegistrySessionStrings{
 		Hostname:            hostname,
 		Protocol:            puttyProtocol,
 		ProxyHost:           proxyHostname,
@@ -109,66 +118,66 @@ func addPuTTYSession(proxyHostname string, hostname string, port int, login stri
 	}
 
 	// now check for and create the individual session key
-	pk, err := getRegistryKey(registryKey)
+	pk, err := registry.GetOrCreateRegistryKey(registryKey)
 	if err != nil {
-		return false, trace.Wrap(err)
+		return trace.Wrap(err)
 	}
 	defer pk.Close()
 
 	// write dwords
-	if ok, err := registryWriteDword(pk, "Present", sessionDwords.Present); !ok {
-		return false, trace.Wrap(err)
+	if err := registry.WriteDword(pk, "Present", sessionDwords.Present); err != nil {
+		return trace.Wrap(err)
 	}
-	if ok, err := registryWriteDword(pk, "PortNumber", fmt.Sprintf("%v", sessionDwords.PortNumber)); !ok {
-		return false, trace.Wrap(err)
+	if err := registry.WriteDword(pk, "PortNumber", fmt.Sprintf("%v", sessionDwords.PortNumber)); err != nil {
+		return trace.Wrap(err)
 	}
-	if ok, err := registryWriteDword(pk, "ProxyPort", fmt.Sprintf("%v", sessionDwords.ProxyPort)); !ok {
-		return false, trace.Wrap(err)
+	if err := registry.WriteDword(pk, "ProxyPort", fmt.Sprintf("%v", sessionDwords.ProxyPort)); err != nil {
+		return trace.Wrap(err)
 	}
-	if ok, err := registryWriteDword(pk, "ProxyMethod", sessionDwords.ProxyMethod); !ok {
-		return false, trace.Wrap(err)
+	if err := registry.WriteDword(pk, "ProxyMethod", sessionDwords.ProxyMethod); err != nil {
+		return trace.Wrap(err)
 	}
-	if ok, err := registryWriteDword(pk, "ProxyLogToTerm", sessionDwords.ProxyLogToTerm); !ok {
-		return false, trace.Wrap(err)
+	if err := registry.WriteDword(pk, "ProxyLogToTerm", sessionDwords.ProxyLogToTerm); err != nil {
+		return trace.Wrap(err)
 	}
 
 	// write strings
-	if ok, err := registryWriteString(pk, "Hostname", sessionStrings.Hostname); !ok {
-		return false, trace.Wrap(err)
+	if err := registry.WriteString(pk, "Hostname", sessionStrings.Hostname); err != nil {
+		return trace.Wrap(err)
 	}
-	if ok, err := registryWriteString(pk, "Protocol", sessionStrings.Protocol); !ok {
-		return false, trace.Wrap(err)
+	if err := registry.WriteString(pk, "Protocol", sessionStrings.Protocol); err != nil {
+		return trace.Wrap(err)
 	}
-	if ok, err := registryWriteString(pk, "ProxyHost", sessionStrings.ProxyHost); !ok {
-		return false, trace.Wrap(err)
+	if err := registry.WriteString(pk, "ProxyHost", sessionStrings.ProxyHost); err != nil {
+		return trace.Wrap(err)
 	}
-	if ok, err := registryWriteString(pk, "ProxyUsername", sessionStrings.ProxyUsername); !ok {
-		return false, trace.Wrap(err)
+	if err := registry.WriteString(pk, "ProxyUsername", sessionStrings.ProxyUsername); err != nil {
+		return trace.Wrap(err)
 	}
-	if ok, err := registryWriteString(pk, "ProxyTelnetCommand", sessionStrings.ProxyTelnetCommand); !ok {
-		return false, trace.Wrap(err)
+	if err := registry.WriteString(pk, "ProxyTelnetCommand", sessionStrings.ProxyTelnetCommand); err != nil {
+		return trace.Wrap(err)
 	}
-	if ok, err := registryWriteString(pk, "PublicKeyFile", sessionStrings.PublicKeyFile); !ok {
-		return false, trace.Wrap(err)
+	if err := registry.WriteString(pk, "PublicKeyFile", sessionStrings.PublicKeyFile); err != nil {
+		return trace.Wrap(err)
 	}
-	if ok, err := registryWriteString(pk, "DetachedCertificate", sessionStrings.DetachedCertificate); !ok {
-		return false, trace.Wrap(err)
+	if err := registry.WriteString(pk, "DetachedCertificate", sessionStrings.DetachedCertificate); err != nil {
+		return trace.Wrap(err)
 	}
-	if ok, err := registryWriteString(pk, "UserName", sessionStrings.UserName); !ok {
-		return false, trace.Wrap(err)
+	if err := registry.WriteString(pk, "UserName", sessionStrings.UserName); err != nil {
+		return trace.Wrap(err)
 	}
 
-	return true, nil
+	return nil
 }
 
 // addHostCAPublicKey adds a host CA to the registry with a set of space-separated hostnames
-func addHostCAPublicKey(keyName string, publicKey string, hostnames ...string) (bool, error) {
+func addHostCAPublicKey(keyName string, publicKey string, hostname string) error {
 	registryKeyName := fmt.Sprintf(`%v\%v`, puttyRegistrySSHHostCAsKey, keyName)
 
 	// get the subkey with the host CA key name
-	registryKey, err := getRegistryKey(registryKeyName)
+	registryKey, err := registry.GetOrCreateRegistryKey(registryKeyName)
 	if err != nil {
-		return false, trace.Wrap(err)
+		return trace.Wrap(err)
 	}
 	hostList, _, err := registryKey.GetStringsValue("MatchHosts")
 	if err != nil {
@@ -176,48 +185,38 @@ func addHostCAPublicKey(keyName string, publicKey string, hostnames ...string) (
 		// exist and it must be created
 		if err != syscall.ERROR_FILE_NOT_FOUND {
 			log.Debugf("Can't get registry value %v: %T", registryKeyName, err)
-			return false, trace.Wrap(err)
+			return trace.Wrap(err)
 		}
-	} else {
-		// initialise empty hostlist if no value returned
-		if len(hostList) == 0 {
-			hostList = []string{}
-		}
+	}
+	// initialize an empty hostlist if there isn't one stored under the registry key
+	if len(hostList) == 0 {
+		hostList = []string{}
 	}
 	defer registryKey.Close()
 
-	// iterate over the list of hostnames provided
-	// if an FQDN is provided, see whether it can be covered by a wildcard hostname
-	// that already exists in the list and skip adding it.
-	for _, host := range hostnames {
-		if strings.Contains(host, ".") && !strings.HasPrefix(host, "*.") {
-			fullHostname := strings.Split(host, ".")
-			wildcardDomain := fmt.Sprintf("*.%s", strings.Join(fullHostname[1:], "."))
-			if !slices.Contains(hostList, wildcardDomain) {
-				log.Debugf("Adding wildcard %s to hostList", wildcardDomain)
-				hostList = append(hostList, wildcardDomain)
-			} else {
-				log.Debugf("Not adding %s because it's already covered by %s", host, wildcardDomain)
-				continue
-			}
-		} else {
-			if !slices.Contains(hostList, host) {
-				log.Debugf("Adding %s to hostList", host)
-				hostList = append(hostList, host)
-			}
-		}
-	}
-	sort.Strings(hostList)
+	// add the new hostname to the existing hostList from the registry key (if one exists)
+	hostList = puttyhosts.AddHostToHostList(hostList, hostname)
 
 	// write strings to subkey
-	if ok, err := registryWriteMultiString(registryKey, "MatchHosts", hostList); !ok {
-		return false, trace.Wrap(err)
+	if err := registry.WriteMultiString(registryKey, "MatchHosts", hostList); err != nil {
+		return trace.Wrap(err)
 	}
-	if ok, err := registryWriteString(registryKey, "PublicKey", publicKey); !ok {
-		return false, trace.Wrap(err)
+	if err := registry.WriteString(registryKey, "PublicKey", publicKey); err != nil {
+		return trace.Wrap(err)
 	}
 
-	return true, nil
+	// write dwords for signature acceptance
+	if err := registry.WriteDword(registryKey, "PermitRSASHA1", puttyPermitRSASHA1); err != nil {
+		return trace.Wrap(err)
+	}
+	if err := registry.WriteDword(registryKey, "PermitRSASHA256", puttyPermitRSASHA256); err != nil {
+		return trace.Wrap(err)
+	}
+	if err := registry.WriteDword(registryKey, "PermitRSASHA512", puttyPermitRSASHA512); err != nil {
+		return trace.Wrap(err)
+	}
+
+	return nil
 }
 
 // formatLocalCommandString replaces placeholders in a constant with actual values
@@ -225,19 +224,31 @@ func formatLocalCommandString(tshPath string, cluster string) string {
 	// replace the placeholder "%cluster" with the actual cluster name as passed to the function
 	clusterString := strings.ReplaceAll(puttyProxyTelnetCommand, "%cluster", cluster)
 	// PuTTY needs its paths to be double-escaped i.e. C:\\Users\\User\\tsh.exe
-	escapedTshPath := strings.ReplaceAll(tshPath, `\`, `\\`)
-	return strings.ReplaceAll(clusterString, "%tsh", escapedTshPath)
+	escapedTSHPath := strings.ReplaceAll(tshPath, `\`, `\\`)
+	return strings.ReplaceAll(clusterString, "%tsh", escapedTSHPath)
 }
 
 // onPuttyConfig handles the `tsh config putty` subcommand
 func onPuttyConfig(cf *CLIConf) error {
-	if runtime.GOOS != constants.WindowsOS {
-		return trace.NotImplemented("PuTTY config is only implemented on Windows")
-	}
-
-	tc, err := makeClient(cf, true)
+	tc, err := makeClient(cf)
 	if err != nil {
 		return trace.Wrap(err)
+	}
+
+	// validate hostname against a naive regex to make sure it doesn't contain obviously illegal characters due
+	// to typos or similar. setting an "invalid" key in the registry makes it impossible to delete via the PuTTY
+	// UI and requires registry edits, so it's much better to error out early here.
+	hostname := tc.Config.Host
+	if !puttyhosts.NaivelyValidateHostname(hostname) {
+		return trace.BadParameter("provided hostname %v does not look like a valid hostname. Make sure it doesn't contain illegal characters.", hostname)
+	}
+
+	port := tc.Config.HostPort
+	userHostString := hostname
+	login := ""
+	if tc.Config.HostLogin != "" {
+		login = tc.Config.HostLogin
+		userHostString = fmt.Sprintf("%v@%v", login, userHostString)
 	}
 
 	// connect to proxy to fetch cluster info
@@ -253,61 +264,97 @@ func onPuttyConfig(cf *CLIConf) error {
 		return trace.Wrap(err)
 	}
 
-	// get the public key of the Teleport host CA so we can add it to PuTTY's list of trusted keys
-	var hostCAKnownHostsLine string
+	// get root cluster name and set keypaths
+	rootClusterName, err := proxyClient.RootClusterName(cf.Context)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	keysDir := profile.FullProfilePath(tc.Config.KeysDir)
+	ppkFilePath := keypaths.PPKFilePath(keysDir, proxyHost, tc.Config.Username)
+	certificateFilePath := keypaths.SSHCertPath(keysDir, proxyHost, tc.Config.Username, rootClusterName)
+
+	// if a leaf cluster is requested, we need to check whether we can find it while running the CA loop below
+	wantLeaf := cf.LeafClusterName != ""
+	leafExists := false
+
+	// get all CAs for the cluster (including trusted clusters)
+	var cas []types.CertAuthority
 	err = tc.WithRootClusterClient(cf.Context, func(clt auth.ClientI) error {
-		hostCAKnownHostsLine, err = client.ExportAuthorities(cf.Context, clt, client.ExportAuthoritiesRequest{AuthType: "host"})
+		cas, err = clt.GetCertAuthorities(cf.Context, types.HostCA, false /* exportSecrets */)
 		if err != nil {
 			return trace.Wrap(err)
 		}
 		return nil
 	})
-	_, _, hostCAPublicKeyBytes, _, _, err := ssh.ParseKnownHosts([]byte(hostCAKnownHostsLine))
-	if err != nil {
-		return trace.Wrap(err)
+	// iterate over all the CAs
+	hostCAPublicKeys := make(map[string][]string)
+	for _, ca := range cas {
+		// if this is either the root or the requested leaf cluster, process it
+		if ca.GetName() == rootClusterName || (wantLeaf && ca.GetName() == cf.LeafClusterName) {
+			// if we've found the requested leaf cluster, mark that we've found it
+			if ca.GetName() == cf.LeafClusterName {
+				leafExists = true
+			}
+			for i, key := range ca.GetTrustedSSHKeyPairs() {
+				log.Debugf("%v CA [%v]: %v", ca.GetName(), i, key)
+				kh, err := sshutils.MarshalKnownHost(sshutils.KnownHost{
+					Hostname:      ca.GetClusterName(),
+					AuthorizedKey: key.PublicKey,
+				})
+				if err != nil {
+					return trace.Wrap(err)
+				}
+				_, _, hostCABytes, _, _, err := ssh.ParseKnownHosts([]byte(kh))
+				if err != nil {
+					return trace.Wrap(err)
+				}
+				hostCAPublicKeys[ca.GetName()] = append(hostCAPublicKeys[ca.GetName()], strings.TrimPrefix(strings.TrimSpace(string(ssh.MarshalAuthorizedKey(hostCABytes))), constants.SSHRSAType+" "))
+			}
+		}
 	}
-	hostCAPublicKey := strings.TrimSuffix(string(ssh.MarshalAuthorizedKey(hostCAPublicKeyBytes)), "\n")
-
-	// get root cluster name
-	rootClusterName, rootErr := proxyClient.RootClusterName(cf.Context)
-	// TODO(gus): figure out what to do (if anything) about leaf clusters
-	//leafClusters, leafErr := proxyClient.GetLeafClusters(cf.Context)
-	_, leafErr := proxyClient.GetLeafClusters(cf.Context)
-	if err := trace.NewAggregate(rootErr, leafErr); err != nil {
-		return trace.Wrap(err)
+	// update the cert path if a leaf cluster was requested
+	proxyCommandClusterName := rootClusterName
+	if wantLeaf {
+		// if we haven't found the requested leaf cluster, error out
+		if !leafExists {
+			return trace.NotFound("Cannot find registered leaf cluster %q. Use the leaf cluster name as it appears in the output of `tsh clusters`.", cf.LeafClusterName)
+		}
+		proxyCommandClusterName = cf.LeafClusterName
+		certificateFilePath = keypaths.SSHCertPath(keysDir, proxyHost, tc.Config.Username, cf.LeafClusterName)
 	}
 
-	keysDir := profile.FullProfilePath(tc.Config.KeysDir)
-	ppkFilePath := keypaths.PPKFilePath(keysDir, proxyHost, tc.Config.Username)
-	certificateFilePath := keypaths.SSHCertPath(keysDir, proxyHost, tc.Config.Username, rootClusterName)
+	// add all host CA public keys for cluster
+	for cluster, hostCAs := range hostCAPublicKeys {
+		keyName := fmt.Sprintf(`TeleportHostCA-%v`, cluster)
+		for i, publicKey := range hostCAs {
+			// append indices to entries if we have multiple public keys for a CA
+			if len(hostCAs) > 1 {
+				keyName = fmt.Sprintf(`%v-%d`, keyName, i)
+			}
 
-	hostname := tc.Config.Host
-	port := tc.Config.HostPort
-	userHostString := hostname
-	login := ""
-	if tc.Config.HostLogin != "" {
-		login = tc.Config.HostLogin
-		userHostString = fmt.Sprintf("%v@%v", login, userHostString)
+			if err := addHostCAPublicKey(keyName, publicKey, hostname); err != nil {
+				log.Errorf("Failed to add host CA key for %v: %T", cluster, err)
+				return trace.Wrap(err)
+			}
+			log.Debugf("Added/updated host CA key %d for %v", i, cluster)
+		}
 	}
 
 	// format local command string (to run 'tsh proxy ssh')
-	localCommandString := formatLocalCommandString(cf.executablePath, rootClusterName)
-
-	keyName := fmt.Sprintf(`teleportHostCA-%v`, proxyHost)
-	if ok, err := addHostCAPublicKey(keyName, hostCAPublicKey, hostname); !ok {
-		log.Infof("Failed to add host CA key: %T", err)
-		return trace.Wrap(err)
-	} else {
-		log.Debugf("Added/updated host CA key for %v", hostname)
-	}
+	localCommandString := formatLocalCommandString(cf.executablePath, proxyCommandClusterName)
 
 	// add session to registry
-	if ok, err := addPuTTYSession(proxyHost, tc.Config.Host, port, login, ppkFilePath, certificateFilePath, localCommandString); !ok {
-		log.Infof("Failed to add PuTTY session for %v: %T\n", userHostString, err)
+	if err := addPuTTYSession(proxyHost, tc.Config.Host, port, login, ppkFilePath, certificateFilePath, localCommandString, cf.LeafClusterName); err != nil {
+		log.Errorf("Failed to add PuTTY session for %v: %T\n", userHostString, err)
 		return trace.Wrap(err)
-	} else {
-		fmt.Printf("Added PuTTY session for %v [via %v]\n", userHostString, proxyHost)
 	}
 
+	// handle leaf clusters
+	if wantLeaf {
+		fmt.Printf("Added PuTTY session for %v [leaf:%v,proxy:%v]\n", userHostString, cf.LeafClusterName, proxyHost)
+		return nil
+	}
+
+	fmt.Printf("Added PuTTY session for %v [proxy:%v]\n", userHostString, proxyHost)
 	return nil
 }
