aa914c153e288b00b3a1e6f44f459730efe94ed9
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2023-06-09 22:37:32 -0400

    Set the minimal ping time to one second

--- lib/web/apiserver_test.go
+++ lib/web/apiserver_test.go
@@ -1512,7 +1512,7 @@ func isResizeEventEnvelope(e *Envelope) bool {
 func TestTerminalPing(t *testing.T) {
 	t.Parallel()
 	s := newWebSuite(t)
-	ws, _, err := s.makeTerminal(t, s.authPack(t, "foo"), withKeepaliveInterval(500*time.Millisecond))
+	ws, _, err := s.makeTerminal(t, s.authPack(t, "foo"), withKeepaliveInterval(time.Second))
 	require.NoError(t, err)
 	t.Cleanup(func() { require.NoError(t, ws.Close()) })
 
