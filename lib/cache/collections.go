e104761a4f5c6fa82ebc82a82e5a77cdd101ebd6
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2023-06-13 15:25:15 +0200

    [v13] Delete proxy heartbeats on graceful shutdown (#27786)
    * Pass the context through DeleteProxy
    
    * Delete proxy heartbeats on graceful shutdown
    
    * Pass context through UpsertProxy
    
    * Tweak logging
    
    * Move heartbeat deletion to proxy.shutdown

--- lib/cache/collections.go
+++ lib/cache/collections.go
@@ -780,7 +780,7 @@ func (proxyExecutor) getAll(ctx context.Context, cache *Cache, loadSecrets bool)
 }
 
 func (proxyExecutor) upsert(ctx context.Context, cache *Cache, resource types.Server) error {
-	return cache.presenceCache.UpsertProxy(resource)
+	return cache.presenceCache.UpsertProxy(ctx, resource)
 }
 
 func (proxyExecutor) deleteAll(ctx context.Context, cache *Cache) error {
@@ -788,7 +788,7 @@ func (proxyExecutor) deleteAll(ctx context.Context, cache *Cache) error {
 }
 
 func (proxyExecutor) delete(ctx context.Context, cache *Cache, resource types.Resource) error {
-	return cache.presenceCache.DeleteProxy(resource.GetName())
+	return cache.presenceCache.DeleteProxy(ctx, resource.GetName())
 }
 
 func (proxyExecutor) isSingleton() bool { return false }
