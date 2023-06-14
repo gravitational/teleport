94963aa1bee868c9a1f8ca62b03748da3a14fb23
Joel Wejdenstål <jwejdenstal@icloud.com>
2023-06-14 13:16:11 +0200

    swap api wrapper version temporarily

--- lib/ai/model/agent.go
+++ lib/ai/model/agent.go
@@ -217,7 +217,7 @@ func (a *Agent) plan(ctx context.Context, state *executionState) (*agentAction, 
 	resp, err := state.llm.CreateChatCompletion(
 		ctx,
 		openai.ChatCompletionRequest{
-			Model:    openai.GPT4,
+			Model:    openai.GPT3Dot5Turbo0613,
 			Messages: prompt,
 		},
 	)
