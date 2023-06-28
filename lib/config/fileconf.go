3179bc3382c11d9b4780113dd1dd18b646762464
STeve (Xin) Huang <xin.huang@goteleport.com>
2023-06-28 09:09:24 -0400

    Teleport Proxy Behind ALB support for IP Pinning (#26623)
    * Teleport Proxy Behind ALB support for IP Pinning
    
    * move X-Forwarded-For handling to a middleware
    
    * fix where middleware should be nil
    
    * minor refactor based on review comments
    
    * remove TODO and use contains
    
    * use_x_forwarded_for -> trust_x_forwarded_for
    
    * add NoopHTTPMiddleware

--- lib/config/fileconf.go
+++ lib/config/fileconf.go
@@ -2121,6 +2121,11 @@ type Proxy struct {
 
 	// Assist is a set of options related to the Teleport Assist feature.
 	Assist *AssistOptions `yaml:"assist,omitempty"`
+
+	// TrustXForwardedFor enables the service to take client source IPs from
+	// the "X-Forwarded-For" headers for web APIs received from layer 7 load
+	// balancers or reverse proxies.
+	TrustXForwardedFor types.Bool `yaml:"trust_x_forwarded_for,omitempty"`
 }
 
 // UIConfig provides config options for the web UI served by the proxy service.
