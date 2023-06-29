wfetut/wdqfefe

Generalize Kubernetes Resource RBAC
This PR generalizes the work introduced by #19613 to other resources
existent in the Kubernetes API.

It allows restricting access to different Kubernetes resources using a
single `kubernetes_group`.

`kubernetes_resource.kind` supports `*` and the following resources:
  * Deployments
  * Statefulset
  * Secrets
  * Configmap
  * DaemonSet
  * Job
  * CronJob
  * Namespace
  * Services
  * Nodes
  * ReplicaSets
  * Roles
  * RoleBindings
  * ClusterRoles
  * ClusterRoleBindings
  * PersistentVolumes
  * PersistentVolumeClaims
  * Ingress
  * Certificates
  * ServiceAccounts

Part of #20811

api/types/resource_ids_test.go
@@ -144,6 +144,281 @@ func TestResourceIDs(t *testing.T) {
 			expected:         `["/one/pod/cluster"]`,
 			expectParseError: true,
 		},
+		{
+			desc: "pod resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubePod,
+				Name:            "cluster",
+				SubResourceName: "namespace/pod*",
+			}},
+			expected: `["/one/pod/cluster/namespace/pod*"]`,
+		},
+		{
+			desc: "secret resource name with missing namespace",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeSecret,
+				Name:            "cluster",
+				SubResourceName: "/secret*",
+			}},
+			expected:         `["/one/secret/cluster//secret*"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "secret resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubeSecret,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/secret/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "secret resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeSecret,
+				Name:            "cluster",
+				SubResourceName: "namespace/secret*",
+			}},
+			expected: `["/one/secret/cluster/namespace/secret*"]`,
+		},
+		{
+			desc: "configmap resource name with missing namespace",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeConfigmap,
+				Name:            "cluster",
+				SubResourceName: "/configmap*",
+			}},
+			expected:         `["/one/configmap/cluster//configmap*"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "configmap resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubeConfigmap,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/configmap/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "configmap resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeConfigmap,
+				Name:            "cluster",
+				SubResourceName: "namespace/configmap*",
+			}},
+			expected: `["/one/configmap/cluster/namespace/configmap*"]`,
+		},
+		{
+			desc: "service resource name with missing namespace",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeService,
+				Name:            "cluster",
+				SubResourceName: "/service*",
+			}},
+			expected:         `["/one/service/cluster//service*"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "service resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubeService,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/service/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "service resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeService,
+				Name:            "cluster",
+				SubResourceName: "namespace/service*",
+			}},
+			expected: `["/one/service/cluster/namespace/service*"]`,
+		},
+		{
+			desc: "service_account resource name with missing namespace",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeServiceAccount,
+				Name:            "cluster",
+				SubResourceName: "/service_account*",
+			}},
+			expected:         `["/one/service_account/cluster//service_account*"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "service_account resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubeServiceAccount,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/service_account/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "service_account resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeServiceAccount,
+				Name:            "cluster",
+				SubResourceName: "namespace/service_account*",
+			}},
+			expected: `["/one/service_account/cluster/namespace/service_account*"]`,
+		},
+		{
+			desc: "persistent_volume_claim resource name with missing namespace",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubePersistentVolumeClaim,
+				Name:            "cluster",
+				SubResourceName: "/persistent_volume_claim*",
+			}},
+			expected:         `["/one/persistent_volume_claim/cluster//persistent_volume_claim*"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "persistent_volume_claim resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubePersistentVolumeClaim,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/persistent_volume_claim/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "namespace resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubeNamespace,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/namespace/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "namespace resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeNamespace,
+				Name:            "cluster",
+				SubResourceName: "namespace*",
+			}},
+			expected: `["/one/namespace/cluster/namespace*"]`,
+		},
+		{
+			desc: "kube_node resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubeNode,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/kube_node/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "kube_node resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeNode,
+				Name:            "cluster",
+				SubResourceName: "kube_node*",
+			}},
+			expected: `["/one/kube_node/cluster/kube_node*"]`,
+		},
+		{
+			desc: "persistent_volume resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubePersistentVolume,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/persistent_volume/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "persistent_volume resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubePersistentVolume,
+				Name:            "cluster",
+				SubResourceName: "persistent_volume*",
+			}},
+			expected: `["/one/persistent_volume/cluster/persistent_volume*"]`,
+		},
+		{
+			desc: "cluster_role resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubeClusterRole,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/cluster_role/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "cluster_role resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeClusterRole,
+				Name:            "cluster",
+				SubResourceName: "cluster_role*",
+			}},
+			expected: `["/one/cluster_role/cluster/cluster_role*"]`,
+		},
+		{
+			desc: "cluster_role_binding resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubeClusterRoleBinding,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/cluster_role_binding/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "cluster_role_binding resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeClusterRoleBinding,
+				Name:            "cluster",
+				SubResourceName: "cluster_role_binding*",
+			}},
+			expected: `["/one/cluster_role_binding/cluster/cluster_role_binding*"]`,
+		},
+		{
+			desc: "certificate_signing_request resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubeCertificateSigningRequest,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/certificate_signing_request/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "certificate_signing_request resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeCertificateSigningRequest,
+				Name:            "cluster",
+				SubResourceName: "certificate_signing_request*",
+			}},
+			expected: `["/one/certificate_signing_request/cluster/certificate_signing_request*"]`,
+		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.desc, func(t *testing.T) {

gravitational/teleport

api/types/resource_ids_test.go
@@ -144,6 +144,281 @@ func TestResourceIDs(t *testing.T) {
 			expected:         `["/one/pod/cluster"]`,
 			expectParseError: true,
 		},
+		{
+			desc: "pod resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubePod,
+				Name:            "cluster",
+				SubResourceName: "namespace/pod*",
+			}},
+			expected: `["/one/pod/cluster/namespace/pod*"]`,
+		},
+		{
+			desc: "secret resource name with missing namespace",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeSecret,
+				Name:            "cluster",
+				SubResourceName: "/secret*",
+			}},
+			expected:         `["/one/secret/cluster//secret*"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "secret resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubeSecret,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/secret/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "secret resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeSecret,
+				Name:            "cluster",
+				SubResourceName: "namespace/secret*",
+			}},
+			expected: `["/one/secret/cluster/namespace/secret*"]`,
+		},
+		{
+			desc: "configmap resource name with missing namespace",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeConfigmap,
+				Name:            "cluster",
+				SubResourceName: "/configmap*",
+			}},
+			expected:         `["/one/configmap/cluster//configmap*"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "configmap resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubeConfigmap,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/configmap/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "configmap resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeConfigmap,
+				Name:            "cluster",
+				SubResourceName: "namespace/configmap*",
+			}},
+			expected: `["/one/configmap/cluster/namespace/configmap*"]`,
+		},
+		{
+			desc: "service resource name with missing namespace",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeService,
+				Name:            "cluster",
+				SubResourceName: "/service*",
+			}},
+			expected:         `["/one/service/cluster//service*"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "service resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubeService,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/service/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "service resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeService,
+				Name:            "cluster",
+				SubResourceName: "namespace/service*",
+			}},
+			expected: `["/one/service/cluster/namespace/service*"]`,
+		},
+		{
+			desc: "service_account resource name with missing namespace",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeServiceAccount,
+				Name:            "cluster",
+				SubResourceName: "/service_account*",
+			}},
+			expected:         `["/one/serviceaccount/cluster//service_account*"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "service_account resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubeServiceAccount,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/serviceaccount/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "service_account resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeServiceAccount,
+				Name:            "cluster",
+				SubResourceName: "namespace/service_account*",
+			}},
+			expected: `["/one/serviceaccount/cluster/namespace/service_account*"]`,
+		},
+		{
+			desc: "persistent_volume_claim resource name with missing namespace",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubePersistentVolumeClaim,
+				Name:            "cluster",
+				SubResourceName: "/persistent_volume_claim*",
+			}},
+			expected:         `["/one/persistentvolumeclaim/cluster//persistent_volume_claim*"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "persistent_volume_claim resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubePersistentVolumeClaim,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/persistentvolumeclaim/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "namespace resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubeNamespace,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/namespace/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "namespace resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeNamespace,
+				Name:            "cluster",
+				SubResourceName: "namespace*",
+			}},
+			expected: `["/one/namespace/cluster/namespace*"]`,
+		},
+		{
+			desc: "kube_node resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubeNode,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/kube_node/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "kube_node resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeNode,
+				Name:            "cluster",
+				SubResourceName: "kube_node*",
+			}},
+			expected: `["/one/kube_node/cluster/kube_node*"]`,
+		},
+		{
+			desc: "persistent_volume resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubePersistentVolume,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/persistentvolume/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "persistent_volume resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubePersistentVolume,
+				Name:            "cluster",
+				SubResourceName: "persistent_volume*",
+			}},
+			expected: `["/one/persistentvolume/cluster/persistent_volume*"]`,
+		},
+		{
+			desc: "cluster_role resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubeClusterRole,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/clusterrole/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "cluster_role resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeClusterRole,
+				Name:            "cluster",
+				SubResourceName: "cluster_role*",
+			}},
+			expected: `["/one/clusterrole/cluster/cluster_role*"]`,
+		},
+		{
+			desc: "cluster_role_binding resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubeClusterRoleBinding,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/clusterrolebinding/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "cluster_role_binding resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeClusterRoleBinding,
+				Name:            "cluster",
+				SubResourceName: "cluster_role_binding*",
+			}},
+			expected: `["/one/clusterrolebinding/cluster/cluster_role_binding*"]`,
+		},
+		{
+			desc: "certificate_signing_request resource name with missing namespace and pod name",
+			in: []ResourceID{{
+				ClusterName: "one",
+				Kind:        KindKubeCertificateSigningRequest,
+				Name:        "cluster",
+			}},
+			expected:         `["/one/certificatesigningrequest/cluster"]`,
+			expectParseError: true,
+		},
+		{
+			desc: "certificate_signing_request resource name in cluster with slash",
+			in: []ResourceID{{
+				ClusterName:     "one",
+				Kind:            KindKubeCertificateSigningRequest,
+				Name:            "cluster",
+				SubResourceName: "certificate_signing_request*",
+			}},
+			expected: `["/one/certificatesigningrequest/cluster/certificate_signing_request*"]`,
+		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.desc, func(t *testing.T) {