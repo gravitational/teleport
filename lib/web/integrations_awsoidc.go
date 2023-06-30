57583b33213cf96b788748192583e5a2d809b27f
Marco Dinis <marco.dinis@goteleport.com>
2023-06-30 18:30:49 +0100

    DeployService: auto upsert IAM Join Token
    When using the DeployService, the deployed services (database service
    only for now) will join the Teleport Cluster using the IAM Join Method.
    
    In order to do so, we require an IAM Token that allows the AWS Account
    ID and ARN of the assumed-role.
    Instead of asking the user to create it, we do it for them.
    
    This PR creates or updates the IAM Join Token.

--- lib/web/integrations_awsoidc.go
+++ lib/web/integrations_awsoidc.go
@@ -130,7 +130,12 @@ func (h *Handler) awsOIDCDeployService(w http.ResponseWriter, r *http.Request, p
 		return nil, trace.Wrap(err)
 	}
 
-	deployDBServiceClient, err := awsoidc.NewDeployServiceClient(ctx, awsClientReq)
+	clt, err := sctx.GetUserClient(ctx, site)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	deployDBServiceClient, err := awsoidc.NewDeployServiceClient(ctx, awsClientReq, clt)
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
@@ -142,6 +147,7 @@ func (h *Handler) awsOIDCDeployService(w http.ResponseWriter, r *http.Request, p
 
 	deployServiceResp, err := awsoidc.DeployService(ctx, deployDBServiceClient, awsoidc.DeployServiceRequest{
 		Region:                        req.Region,
+		AccountID:                     req.AccountID,
 		SubnetIDs:                     req.SubnetIDs,
 		ClusterName:                   req.ClusterName,
 		ServiceName:                   req.ServiceName,
