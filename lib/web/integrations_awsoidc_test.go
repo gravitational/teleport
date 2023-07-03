7787f2d8ed78d62fb16102891172b8f1cfb5574f
Marco Dinis <marco.dinis@goteleport.com>
2023-07-03 17:49:59 +0100

    Script to configure IAM for the DeployService

---
+++ lib/web/integrations_awsoidc_test.go
@@ -0,0 +1,146 @@
+/*
+Copyright 2023 Gravitational, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package web
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"testing"
+
+	"github.com/gravitational/trace"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBuildDeployServiceConfigureIAMScript(t *testing.T) {
+	isBadParamErrFn := func(tt require.TestingT, err error, i ...interface{}) {
+		require.True(tt, trace.IsBadParameter(err), "expected bad parameter, got %v", err)
+	}
+
+	ctx := context.Background()
+	env := newWebPack(t, 1)
+
+	// Unauthenticated client for script downloading.
+	publicClt := env.proxies[0].newClient(t)
+	pathVars := []string{
+		"webapi",
+		"scripts",
+		"integrations",
+		"configure",
+		"deployservice-iam.sh",
+	}
+	endpoint := publicClt.Endpoint(pathVars...)
+
+	tests := []struct {
+		name                 string
+		reqRelativeURL       string
+		reqQuery             url.Values
+		errCheck             require.ErrorAssertionFunc
+		expectedTeleportArgs string
+	}{
+		{
+			name: "valid",
+			reqQuery: url.Values{
+				"awsRegion":       []string{"us-east-1"},
+				"role":            []string{"myRole"},
+				"taskRole":        []string{"taskRole"},
+				"integrationName": []string{"myintegration"},
+			},
+			errCheck: require.NoError,
+			expectedTeleportArgs: "integration configure deployservice-iam " +
+				`--cluster="localhost" ` +
+				`--name="myintegration" ` +
+				`--aws-region="us-east-1" ` +
+				`--role="myRole" ` +
+				`--task-role="taskRole"`,
+		},
+		{
+			name: "valid with symbols in role",
+			reqQuery: url.Values{
+				"awsRegion":       []string{"us-east-1"},
+				"role":            []string{"Test+1=2,3.4@5-6_7"},
+				"taskRole":        []string{"taskRole"},
+				"integrationName": []string{"myintegration"},
+			},
+			errCheck: require.NoError,
+			expectedTeleportArgs: "integration configure deployservice-iam " +
+				`--cluster="localhost" ` +
+				`--name="myintegration" ` +
+				`--aws-region="us-east-1" ` +
+				`--role="Test+1=2,3.4@5-6_7" ` +
+				`--task-role="taskRole"`,
+		},
+		{
+			name: "missing aws-region",
+			reqQuery: url.Values{
+				"role":            []string{"myRole"},
+				"taskRole":        []string{"taskRole"},
+				"integrationName": []string{"myintegration"},
+			},
+			errCheck: isBadParamErrFn,
+		},
+		{
+			name: "missing role",
+			reqQuery: url.Values{
+				"awsRegion":       []string{"us-east-1"},
+				"taskRole":        []string{"taskRole"},
+				"integrationName": []string{"myintegration"},
+			},
+			errCheck: isBadParamErrFn,
+		},
+		{
+			name: "missing task role",
+			reqQuery: url.Values{
+				"awsRegion":       []string{"us-east-1"},
+				"role":            []string{"role"},
+				"integrationName": []string{"myintegration"},
+			},
+			errCheck: isBadParamErrFn,
+		},
+		{
+			name: "missing integration name",
+			reqQuery: url.Values{
+				"awsRegion": []string{"us-east-1"},
+				"role":      []string{"role"},
+				"taskRole":  []string{"taskRole"},
+			},
+			errCheck: isBadParamErrFn,
+		},
+		{
+			name: "trying to inject escape sequence into query params",
+			reqQuery: url.Values{
+				"awsRegion":       []string{"us-east-1"},
+				"role":            []string{"role"},
+				"taskRole":        []string{"taskRole"},
+				"integrationName": []string{"'; rm -rf /tmp/dir; echo '"},
+			},
+			errCheck: isBadParamErrFn,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			resp, err := publicClt.Get(ctx, endpoint, tc.reqQuery)
+			tc.errCheck(t, err)
+			if err != nil {
+				return
+			}
+
+			require.Contains(t, string(resp.Bytes()),
+				fmt.Sprintf("teleportArgs='%s'\n", tc.expectedTeleportArgs),
+			)
+		})
+	}
+}
