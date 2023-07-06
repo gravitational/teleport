2935cd58a3acd29821dd0d9e017fc2b7f4361bf8
Steven Martin <steven@goteleport.com>
2023-07-06 09:40:58 -0700

    skip error on public addresses

--- lib/service/service.go
+++ lib/service/service.go
@@ -5306,7 +5306,8 @@ func initSelfSignedHTTPSCert(cfg *servicecfg.Config) (err error) {
 		for _, element := range cfg.Proxy.PublicAddrs {
 			proxyHost, _, err := net.SplitHostPort(element.String())
 			if err != nil {
-				return trace.Wrap(err)
+        // ignore error since this is a nice to have
+        continue
 			}
 
 			if net.ParseIP(proxyHost) == nil {
