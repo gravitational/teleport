3abcbd20bf0d936f81633a92b081715b7a4a44eb
Noah <noah.stride@goteleport.com>
2023-07-10 15:30:10 +0100

    Use old style SVG icon components

--- web/packages/design/src/SVGIcon/GitLab.tsx
+++ web/packages/design/src/SVGIcon/GitLab.tsx
@@ -16,13 +16,17 @@ limitations under the License.
 
 import React from 'react';
 
-import { SVGIcon } from './SVGIcon';
-
 import type { SVGIconProps } from './common';
 
 export function GitLabIcon({ size = 80, fill }: SVGIconProps) {
   return (
-    <SVGIcon viewBox="90 90 200 200" size={size} fill={fill}>
+    <svg
+      viewBox="90 90 200 200"
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      fill={fill}
+    >
       <g id="LOGO">
         <path
           d="m282.83 170.73-.27-.69-26.14-68.22a6.81 6.81 0 0 0-2.69-3.24 7 7 0 0 0-8 .43 7 7 0 0 0-2.32 3.52l-17.65 54h-71.47l-17.65-54a6.86 6.86 0 0 0-2.32-3.53 7 7 0 0 0-8-.43 6.87 6.87 0 0 0-2.69 3.24L97.44 170l-.26.69a48.54 48.54 0 0 0 16.1 56.1l.09.07.24.17 39.82 29.82 19.7 14.91 12 9.06a8.07 8.07 0 0 0 9.76 0l12-9.06 19.7-14.91 40.06-30 .1-.08a48.56 48.56 0 0 0 16.08-56.04Z"
@@ -41,6 +45,6 @@ export function GitLabIcon({ size = 80, fill }: SVGIconProps) {
           d="M132.58 185.84A88.19 88.19 0 0 0 97.44 170l-.26.69a48.54 48.54 0 0 0 16.1 56.1l.09.07.24.17 39.82 29.82L190 229.21Z"
         />
       </g>
-    </SVGIcon>
+    </svg>
   );
 }
