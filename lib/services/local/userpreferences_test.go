4137b8cb1f4416fe96a35ff610202a8c8e62dcf3
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2023-07-11 17:02:18 -0400

    Fix panic when fetching user preferences (#28751)
    * Fix panic when fetching user preferences
    
    Closes https://github.com/gravitational/teleport/issues/28740
    
    * Prevent overwriting mismatched types in user preferences
    
    * Add error handling to user preferences service
    
    * GCI imports
    
    * Refactor user preferences test and logic
    
    This commit updates the approach for comparing equality in user preferences test. It introduces the use of the "go-cmp" library which provides more flexibility in handling comparison of struct elements, thus helping to catch any unexpected changes that could be overlooked with the standard equality checking. We also revised the logic to handle default preferences by overwriting values for better efficiency and readability, also removing unnecessary checking and merging of values.
    
    * Add comments to overwriteValues method in userpreferences.go
    
    Added detailed comments to better explain the overwriteValues function in userpreferences.go file. The comments clarify how the function uses proto.Ranges to iterate over fields and only overwrite non-nil/empty fields.
    
    * Apply some magic to preferences test
    
    * Change the import to avoid go.mod changed and match our other imports

--- lib/services/local/userpreferences_test.go
+++ lib/services/local/userpreferences_test.go
@@ -18,12 +18,17 @@ package local_test
 
 import (
 	"context"
+	"encoding/json"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/jonboulle/clockwork"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/testing/protocmp"
 
 	userpreferencesv1 "github.com/gravitational/teleport/api/gen/proto/go/userpreferences/v1"
+	"github.com/gravitational/teleport/lib/backend"
 	"github.com/gravitational/teleport/lib/backend/memory"
 	"github.com/gravitational/teleport/lib/services/local"
 )
@@ -39,6 +44,8 @@ func newUserPreferencesService(t *testing.T) *local.UserPreferencesService {
 }
 
 func TestUserPreferencesCRUD2(t *testing.T) {
+	t.Parallel()
+
 	ctx := context.Background()
 	defaultPref := local.DefaultUserPreferences()
 	username := "something"
@@ -74,6 +81,9 @@ func TestUserPreferencesCRUD2(t *testing.T) {
 				Preferences: &userpreferencesv1.UserPreferences{
 					Assist: &userpreferencesv1.AssistUserPreferences{
 						PreferredLogins: []string{"foo", "bar"},
+					},
+					Onboard: &userpreferencesv1.OnboardUserPreferences{
+						PreferredResources: []userpreferencesv1.Resource{},
 					},
 				},
 			},
@@ -152,14 +162,18 @@ func TestUserPreferencesCRUD2(t *testing.T) {
 	}
 
 	for _, test := range tests {
+		test := test
 		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
 			identity := newUserPreferencesService(t)
 
 			res, err := identity.GetUserPreferences(ctx, &userpreferencesv1.GetUserPreferencesRequest{
 				Username: username,
 			})
 			require.NoError(t, err)
-			require.Equal(t, defaultPref, res.Preferences)
+			// Clone the proto as the accessing fields for some reason modifies the state.
+			require.Empty(t, cmp.Diff(defaultPref, proto.Clone(res.Preferences), protocmp.Transform()))
 
 			if test.req != nil {
 				err := identity.UpsertUserPreferences(ctx, test.req)
@@ -171,7 +185,43 @@ func TestUserPreferencesCRUD2(t *testing.T) {
 			})
 
 			require.NoError(t, err)
-			require.Equal(t, test.expected, res.Preferences)
+			require.Empty(t, cmp.Diff(test.expected, res.Preferences, protocmp.Transform()))
 		})
 	}
+}
+
+func TestLayoutUpdate(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	identity := newUserPreferencesService(t)
+
+	outdatedPrefs := &userpreferencesv1.UserPreferences{
+		Assist: &userpreferencesv1.AssistUserPreferences{
+			PreferredLogins: []string{"foo", "bar"},
+		},
+	}
+	val, err := json.Marshal(outdatedPrefs)
+	require.NoError(t, err)
+
+	// Insert the outdated preferences directly into the backend
+	// to simulate a previous version of the preferences.
+	_, err = identity.Put(ctx, backend.Item{
+		Key:   backend.Key("user_preferences", "test"),
+		Value: val,
+	})
+	require.NoError(t, err)
+
+	// Get the preferences and ensure that the layout is updated.
+	prefs, err := identity.GetUserPreferences(ctx, &userpreferencesv1.GetUserPreferencesRequest{
+		Username: "test",
+	})
+	require.NoError(t, err)
+	// The layout should be updated to the latest version (values should not be nil).
+	require.NotNil(t, prefs.Preferences.Onboard)
+	// Non-existing values should be set to the default value.
+	require.Equal(t, userpreferencesv1.AssistViewMode_ASSIST_VIEW_MODE_DOCKED, prefs.Preferences.Assist.ViewMode)
+	require.Equal(t, userpreferencesv1.Theme_THEME_LIGHT, prefs.Preferences.Theme)
+	// Existing values should be preserved.
+	require.Equal(t, []string{"foo", "bar"}, prefs.Preferences.Assist.PreferredLogins)
 }
