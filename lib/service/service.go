49a181247837c688dddb9fc0255dc4df74bed7fd
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2023-07-11 09:46:01 +0200

    Add `ProxyGroup` support to reverse tunnels (#27574)
    * Set and distribute proxy group ID and generation
    
    * Replace proxy tracker with a proxygroup-aware one
    
    * Eager proxy deletion
    
    * godocs
    
    * restore and update tracker_test
    
    * Mixed workload test with proxy groups
    
    * limit connectionCount if there's fewer proxies
    
    * package docs
    
    * use ints for the generation
    
    * Explicitly zero out the Atoi return value on error
    
    * Check TELEPORT_UNSTABLE_PROXYGROUP_GEN at startup
    
    It's now required to be empty or a valid uint64
    
    * Pointer receivers for discoveryRequest, add docs
    
    * track docs
    
    * expand and document lease field names
    
    * remove proxy deletion via gossip
    
    * Poll the tracker
    
    * restore TestAgentPoolConnectionCount
    
    * Improve and deflake tracker tests
    
    * Update track package docs
    
    * Update cannotLease docs
    
    Co-authored-by: Forrest <30576607+fspmarshall@users.noreply.github.com>
    
    * docs for TryAcquire
    
    * TODO for logging
    
    ---------
    
    Co-authored-by: Forrest <30576607+fspmarshall@users.noreply.github.com>

--- lib/service/service.go
+++ lib/service/service.go
@@ -36,6 +36,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"strconv"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -3968,6 +3969,17 @@ func (process *TeleportProcess) initProxyEndpoint(conn *Connector) error {
 		})
 	}
 
+	staticLabels := make(map[string]string, 2)
+	if cfg.Proxy.ProxyGroupID != "" {
+		staticLabels[types.ProxyGroupIDLabel] = cfg.Proxy.ProxyGroupID
+	}
+	if cfg.Proxy.ProxyGroupGeneration != 0 {
+		staticLabels[types.ProxyGroupGenerationLabel] = strconv.FormatUint(cfg.Proxy.ProxyGroupGeneration, 10)
+	}
+	if len(staticLabels) > 0 {
+		log.Infof("Enabling proxy group labels: group ID = %q, generation = %v.", cfg.Proxy.ProxyGroupID, cfg.Proxy.ProxyGroupGeneration)
+	}
+
 	sshProxy, err := regular.New(
 		process.ExitContext(),
 		cfg.SSH.Addr,
@@ -3999,6 +4011,7 @@ func (process *TeleportProcess) initProxyEndpoint(conn *Connector) error {
 		regular.SetIngressReporter(ingress.SSH, ingressReporter),
 		regular.SetPROXYSigner(proxySigner),
 		regular.SetPublicAddrs(cfg.Proxy.PublicAddrs),
+		regular.SetLabels(staticLabels, services.CommandLabels(nil), labels.Importer(nil)),
 	)
 	if err != nil {
 		return trace.Wrap(err)
