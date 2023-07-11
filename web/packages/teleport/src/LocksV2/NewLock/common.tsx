gravitational/teleport

commit 106af761e2da5253979295e75fa55a727454f403

Add UI `node` lock to use `server_id` instead (#27395)
PR #27018 generalized locks to other roles besides `RoleNode` by adding
a new field `server_id` that forbids the agent to connect to the cluster
independently of the roles it has associated.

This PR converts the UI lock of nodes from `node` to the new `server_id`
field.

web/packages/teleport/src/LocksV2/NewLock/common.tsx
@@ -45,6 +45,7 @@ export type LockResourceKind =
   | 'role'
   | 'login'
   | 'node'
+  | 'server_id'
   | 'mfa_device'
   | 'windows_desktop'
   | 'access_request'
@@ -69,6 +70,7 @@ export function getEmptyResourceMap(): LockResourceMap {
     login: {},
     access_request: {},
     device: {},
+    server_id: {},
   };
 }
 
@@ -116,7 +118,7 @@ export const baseResourceKindOpts: LockResourceOption[] = [
     listKind: 'logins',
   },
   {
-    value: 'node',
+    value: 'server_id',
     label: 'Servers',
     listKind: 'server-side',
   },

web/packages/teleport/src/LocksV2/NewLock/ResourceList/ServerSideSupportedList/Nodes.tsx
@@ -54,12 +54,14 @@ export function Nodes(props: ServerSideListProps & { nodes: Node[] }) {
         {
           altKey: 'action-btn',
           render: agent =>
-            renderActionCell(Boolean(selectedResources.node[agent.id]), () =>
-              toggleSelectResource({
-                kind: 'node',
-                targetValue: agent.id,
-                friendlyName: agent.hostname,
-              })
+            renderActionCell(
+              Boolean(selectedResources.server_id[agent.id]),
+              () =>
+                toggleSelectResource({
+                  kind: 'server_id',
+                  targetValue: agent.id,
+                  friendlyName: agent.hostname,
+                })
             ),
         },
       ]}

commit e682c08542c468520c72b0ec3622b8c81f94fbf0

Fix listing servers when creating a new lock via webui
This PR fixes the property `value` used by Servers when listing
connected nodes. The value was incorrectly switched from `node` to
`server_id` by mistake which crashed the list of Servers.

This regression was introduced in #27395.

Fixes #28948

web/packages/teleport/src/LocksV2/NewLock/common.tsx
@@ -118,7 +118,7 @@ export const baseResourceKindOpts: LockResourceOption[] = [
     listKind: 'logins',
   },
   {
-    value: 'server_id',
+    value: 'node',
     label: 'Servers',
     listKind: 'server-side',
   },