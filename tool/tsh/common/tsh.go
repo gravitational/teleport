e1a058f163ce4e9ba584e0129a0adf8669a7badd
Steven Martin <steven@goteleport.com>
2023-07-14 10:34:32 -0400

    verbiage update
    Co-authored-by: Zac Bergquist <zac.bergquist@goteleport.com>

--- tool/tsh/common/tsh.go
+++ tool/tsh/common/tsh.go
@@ -757,7 +757,7 @@ func Run(ctx context.Context, args []string, opts ...CliOption) error {
 	lsRecordings.Flag("last", "Duration into the past from which session recordings should be listed. Format 5h30m40s").StringVar(&cf.recordingsSince)
 	exportRecordings := recordings.Command("export", "Export recorded desktop sesions to video.")
 	exportRecordings.Flag("out", "Override output file name").StringVar(&cf.OutFile)
-	exportRecordings.Arg("session-id", "ID of the session to export to video").Required().StringVar(&cf.SessionID)
+	exportRecordings.Arg("session-id", "ID of the session to export").Required().StringVar(&cf.SessionID)
 
 	// Local TLS proxy.
 	proxy := app.Command("proxy", "Run local TLS proxy allowing connecting to Teleport in single-port mode.")
