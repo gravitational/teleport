d9ea2061d79b6ceb9484f423c4922836130db9c0
Michael Wilson <mike@mdwn.dev>
2023-07-18 11:29:22 -0400

    Add the AccessList to the cache. (#29119)
    * Add the AccessList to the cache.
    
    The AccessList has been added into the cache. The AccessList resource
    and its supporting resources had to be moved back into api for this so that
    it can be properly referenced in api/client/events.go, which is necessary
    for cache propagation.
    
    * Address remaining lib/types references.
    
    * Add in additional test to events_test.go.
    
    * GCI.
    
    * Add back in client wrapper so that the cache will work properly.
    
    * Traits ToProto order is deterministic.
    
    * Add in compile time access list client check.

--- lib/service/service.go
+++ lib/service/service.go
@@ -2105,6 +2105,7 @@ func (process *TeleportProcess) newAccessCache(cfg accessCacheConfig) (*cache.Ca
 		SAMLIdPServiceProviders: cfg.services,
 		UserGroups:              cfg.services,
 		Okta:                    cfg.services.OktaClient(),
+		AccessLists:             cfg.services.AccessListClient(),
 		Integrations:            cfg.services,
 		WebSession:              cfg.services.WebSessions(),
 		WebToken:                cfg.services.WebTokens(),
