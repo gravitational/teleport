b76a697ee1d932bbfa1ca4de0cd29e6d86955727
Bartosz Leper <bartosz.leper@goteleport.com>
2023-07-18 14:59:14 +0200

    Lint and test fixes

--- web/packages/design/src/ResourceIcons/ResourceIcons.story.tsx
+++ web/packages/design/src/ResourceIcons/ResourceIcons.story.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { ResourceIcon, iconNames } from 'design/ResourceIcons';
 import { Flex } from '..';
 
--- web/packages/design/src/ResourceIcons/index.tsx
+++ web/packages/design/src/ResourceIcons/index.tsx
@@ -1,4 +1,5 @@
 import React, { ComponentProps } from 'react';
+
 import { useTheme } from 'styled-components';
 import { Image } from 'design';
 
