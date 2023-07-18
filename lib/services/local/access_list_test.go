fe925901802afdc04d0ef652c54c1b8fc37630cc
Michael Wilson <mike@mdwn.dev>
2023-07-18 13:13:42 -0400

    Access list backend service and marshal/unmarshal. (#29253)
    The access list backend service and marshaling/unmarshaling functions have
    been implemented. This will allow for CRUD operations for access lists.

---
+++ lib/services/local/access_list_test.go
@@ -0,0 +1,177 @@
+/*
+Copyright 2023 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package local
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/gravitational/trace"
+	"github.com/jonboulle/clockwork"
+	"github.com/stretchr/testify/require"
+
+	"github.com/gravitational/teleport/api/types"
+	"github.com/gravitational/teleport/api/types/header"
+	"github.com/gravitational/teleport/lib/backend/memory"
+)
+
+// TestAccessListCRUD tests backend operations with access list resources.
+func TestAccessListCRUD(t *testing.T) {
+	ctx := context.Background()
+	clock := clockwork.NewFakeClock()
+
+	backend, err := memory.New(memory.Config{
+		Context: ctx,
+		Clock:   clock,
+	})
+	require.NoError(t, err)
+
+	service, err := NewAccessListService(backend, clock)
+	require.NoError(t, err)
+
+	// Create a couple access lists.
+	accessList1 := newAccessList(t, "accessList1")
+	accessList2 := newAccessList(t, "accessList2")
+
+	// Initially we expect no access lists.
+	out, err := service.GetAccessLists(ctx)
+	require.NoError(t, err)
+	require.Empty(t, out)
+
+	// Create both access lists.
+	err = service.UpsertAccessList(ctx, accessList1)
+	require.NoError(t, err)
+	err = service.UpsertAccessList(ctx, accessList2)
+	require.NoError(t, err)
+
+	// Fetch all access lists.
+	out, err = service.GetAccessLists(ctx)
+	require.NoError(t, err)
+	require.Empty(t, cmp.Diff([]*types.AccessList{accessList1, accessList2}, out,
+		cmpopts.IgnoreFields(header.Metadata{}, "ID"),
+	))
+
+	// Fetch a specific access list.
+	accessList, err := service.GetAccessList(ctx, accessList2.GetName())
+	require.NoError(t, err)
+	require.Empty(t, cmp.Diff(accessList2, accessList,
+		cmpopts.IgnoreFields(header.Metadata{}, "ID"),
+	))
+
+	// Try to fetch an access list that doesn't exist.
+	_, err = service.GetAccessList(ctx, "doesnotexist")
+	require.True(t, trace.IsNotFound(err), "expected not found error, got %v", err)
+
+	// Update an access list.
+	accessList1.SetExpiry(clock.Now().Add(30 * time.Minute))
+	err = service.UpsertAccessList(ctx, accessList1)
+	require.NoError(t, err)
+	accessList, err = service.GetAccessList(ctx, accessList1.GetName())
+	require.NoError(t, err)
+	require.Empty(t, cmp.Diff(accessList1, accessList,
+		cmpopts.IgnoreFields(header.Metadata{}, "ID"),
+	))
+
+	// Delete an access list.
+	err = service.DeleteAccessList(ctx, accessList1.GetName())
+	require.NoError(t, err)
+	out, err = service.GetAccessLists(ctx)
+	require.NoError(t, err)
+	require.Empty(t, cmp.Diff([]*types.AccessList{accessList2}, out,
+		cmpopts.IgnoreFields(header.Metadata{}, "ID"),
+	))
+
+	// Try to delete an access list that doesn't exist.
+	err = service.DeleteAccessList(ctx, "doesnotexist")
+	require.True(t, trace.IsNotFound(err), "expected not found error, got %v", err)
+
+	// Delete all access lists.
+	err = service.DeleteAllAccessLists(ctx)
+	require.NoError(t, err)
+	out, err = service.GetAccessLists(ctx)
+	require.NoError(t, err)
+	require.Empty(t, out)
+}
+
+func newAccessList(t *testing.T, name string) *types.AccessList {
+	t.Helper()
+
+	accessList, err := types.NewAccessList(
+		header.Metadata{
+			Name: name,
+		},
+		types.AccessListSpec{
+			Description: "test access list",
+			Owners: []types.AccessListOwner{
+				{
+					Name:        "test-user1",
+					Description: "test user 1",
+				},
+				{
+					Name:        "test-user2",
+					Description: "test user 2",
+				},
+			},
+			Audit: types.AccessListAudit{
+				Frequency: time.Hour,
+			},
+			MembershipRequires: types.AccessListRequires{
+				Roles: []string{"mrole1", "mrole2"},
+				Traits: map[string][]string{
+					"mtrait1": {"mvalue1", "mvalue2"},
+					"mtrait2": {"mvalue3", "mvalue4"},
+				},
+			},
+			OwnershipRequires: types.AccessListRequires{
+				Roles: []string{"orole1", "orole2"},
+				Traits: map[string][]string{
+					"otrait1": {"ovalue1", "ovalue2"},
+					"otrait2": {"ovalue3", "ovalue4"},
+				},
+			},
+			Grants: types.AccessListGrants{
+				Roles: []string{"grole1", "grole2"},
+				Traits: map[string][]string{
+					"gtrait1": {"gvalue1", "gvalue2"},
+					"gtrait2": {"gvalue3", "gvalue4"},
+				},
+			},
+			Members: []types.AccessListMember{
+				{
+					Name:    "member1",
+					Joined:  time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC),
+					Expires: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+					Reason:  "because",
+					AddedBy: "test-user1",
+				},
+				{
+					Name:    "member2",
+					Joined:  time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC),
+					Expires: time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC),
+					Reason:  "because again",
+					AddedBy: "test-user2",
+				},
+			},
+		},
+	)
+	require.NoError(t, err)
+
+	return accessList
+}
