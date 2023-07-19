a66eab9adc6a12ed0655b429d8c237d8feecb8e7
Gavin Frazar <gavin.frazar@goteleport.com>
2023-07-19 11:10:50 -0700

    [v12] update discovery labels (#29271)
    Backport #28917 to branch/v12
    
    * update discovery labels and name overrides
    * move all labels into api/types/constants.go
    * add name override labels for GKE and AKS that are allowed tag names in
      GCP and Azure
    * support legacy and cloud-agnostic name override labels for AWS
    * add tests for creating kube clusters from cloud kube clusters

--- lib/services/resource_test.go
+++ lib/services/resource_test.go
@@ -179,3 +179,59 @@ func TestParseShortcut(t *testing.T) {
 		})
 	}
 }
+
+func Test_setResourceName(t *testing.T) {
+	tests := []struct {
+		name           string
+		meta           types.Metadata
+		overrideLabels []string
+		firstNamePart  string
+		extraNameParts []string
+		want           types.Metadata
+	}{
+		{
+			name:           "no override, one part name",
+			meta:           types.Metadata{},
+			firstNamePart:  "foo",
+			extraNameParts: nil,
+			want:           types.Metadata{Name: "foo"},
+		},
+		{
+			name:           "no override, multi part name",
+			meta:           types.Metadata{},
+			firstNamePart:  "foo",
+			extraNameParts: []string{"bar", "baz"},
+			want:           types.Metadata{Name: "foo-bar-baz"},
+		},
+		{
+			name:           "override by generic cloud label, one part name",
+			meta:           types.Metadata{Labels: map[string]string{types.AWSDatabaseNameOverrideLabels[0]: "gizmo"}},
+			overrideLabels: types.AWSDatabaseNameOverrideLabels,
+			firstNamePart:  "foo",
+			extraNameParts: nil,
+			want:           types.Metadata{Name: "gizmo", Labels: map[string]string{types.AWSDatabaseNameOverrideLabels[0]: "gizmo"}},
+		},
+		{
+			name:           "override by original AWS label, one part name",
+			meta:           types.Metadata{Labels: map[string]string{types.AWSDatabaseNameOverrideLabels[1]: "gizmo"}},
+			overrideLabels: types.AWSDatabaseNameOverrideLabels,
+			firstNamePart:  "foo",
+			extraNameParts: nil,
+			want:           types.Metadata{Name: "gizmo", Labels: map[string]string{types.AWSDatabaseNameOverrideLabels[1]: "gizmo"}},
+		},
+		{
+			name:           "override, multi part name",
+			meta:           types.Metadata{Labels: map[string]string{types.AzureDatabaseNameOverrideLabel: "gizmo"}},
+			overrideLabels: []string{types.AzureDatabaseNameOverrideLabel},
+			firstNamePart:  "foo",
+			extraNameParts: []string{"bar", "baz"},
+			want:           types.Metadata{Name: "gizmo-bar-baz", Labels: map[string]string{types.AzureDatabaseNameOverrideLabel: "gizmo"}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := setResourceName(tt.overrideLabels, tt.meta, tt.firstNamePart, tt.extraNameParts...)
+			require.Equal(t, tt.want, result)
+		})
+	}
+}
