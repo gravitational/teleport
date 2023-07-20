wfetut/wdqfefe

add saml apps to webui apps list

api/client/client.go
@@ -3039,6 +3039,8 @@ func (c *Client) ListResources(ctx context.Context, req proto.ListResourcesReque
 			resources[i] = respResource.GetKubernetesServer()
 		case types.KindUserGroup:
 			resources[i] = respResource.GetUserGroup()
+		case types.KindAppAndIdPServiceProvider:
+			resources[i] = respResource.GetAppServerOrSAMLIdPServiceProvider()
 		default:
 			return nil, trace.NotImplemented("resource type %s does not support pagination", req.ResourceType)
 		}
@@ -3131,6 +3133,8 @@ func GetResourcePage[T types.ResourceWithLabels](ctx context.Context, clt GetRes
 				resource = respResource.GetKubernetesServer()
 			case types.KindUserGroup:
 				resource = respResource.GetUserGroup()
+			case types.KindAppAndIdPServiceProvider:
+				resource = respResource.GetAppServerOrSAMLIdPServiceProvider()
 			default:
 				out.Resources = nil
 				return out, trace.NotImplemented("resource type %s does not support pagination", req.ResourceType)

lib/services/matchers.go
@@ -162,6 +162,22 @@ func MatchResourceByFilters(resource types.ResourceWithLabels, filter MatchResou
 		specResource = server.GetDatabase()
 		resourceKey.name = specResource.GetName()
 
+	case types.KindAppAndIdPServiceProvider:
+		appServer, okApp := resource.(types.AppServer)
+		sp, okSP := resource.(types.SAMLIdPServiceProvider)
+		if !okApp && !okSP {
+			return false, trace.BadParameter("expected types.SAMLIdPServiceProvider or types.AppServer, got %T", resource)
+		}
+
+		if okApp {
+			app := appServer.GetApp()
+			specResource = app
+			resourceKey.name = app.GetPublicAddr()
+			resourceKey.addr = app.GetPublicAddr()
+		} else if okSP {
+			specResource = sp
+			resourceKey.name = sp.GetName()
+		}
 	default:
 		return false, trace.NotImplemented("filtering for resource kind %q not supported", filter.ResourceKind)
 	}

lib/web/ui/app.go
@@ -47,6 +47,8 @@ type App struct {
 	AWSRoles []aws.Role `json:"awsRoles,omitempty"`
 	// FriendlyName is a friendly name for the app.
 	FriendlyName string `json:"friendlyName,omitempty"`
+	// SAMLApp if true, indicates that the app is a SAML Application (SAML IdP Service Provider)
+	SAMLApp bool `json:"samlApp,omitempty"`
 }
 
 // MakeAppsConfig contains parameters for converting apps to UI representation.
@@ -57,37 +59,57 @@ type MakeAppsConfig struct {
 	LocalProxyDNSName string
 	// AppClusterName is the name of the cluster apps reside in.
 	AppClusterName string
-	// Apps is a list of registered apps.
-	Apps types.Apps
+	// AppServersOrSAMLIdPServiceProviders is a list of AppServers or SAMLIdPServiceProviders.
+	AppServerOrSAMLIdPServiceProviders types.AppServersOrSAMLIdPServiceProviders
 	// Identity is identity of the logged in user.
 	Identity *tlsca.Identity
 }
 
-// MakeApps creates server application objects
+// MakeApps creates application objects (either Application Servers or SAML IdP Service Provider) for the WebUI.
 func MakeApps(c MakeAppsConfig) []App {
 	result := []App{}
-	for _, teleApp := range c.Apps {
-		fqdn := AssembleAppFQDN(c.LocalClusterName, c.LocalProxyDNSName, c.AppClusterName, teleApp)
-		labels := makeLabels(teleApp.GetAllLabels())
+	for _, appOrSP := range c.AppServerOrSAMLIdPServiceProviders {
+		if appOrSP.IsAppServer() {
+			app := appOrSP.GetAppServer().GetApp()
+			fqdn := AssembleAppFQDN(c.LocalClusterName, c.LocalProxyDNSName, c.AppClusterName, app)
+			labels := makeLabels(app.GetAllLabels())
 
-		app := App{
-			Name:         teleApp.GetName(),
-			Description:  teleApp.GetDescription(),
-			URI:          teleApp.GetURI(),
-			PublicAddr:   teleApp.GetPublicAddr(),
-			Labels:       labels,
-			ClusterID:    c.AppClusterName,
-			FQDN:         fqdn,
-			AWSConsole:   teleApp.IsAWSConsole(),
-			FriendlyName: services.FriendlyName(teleApp),
+			resultApp := App{
+				Name:         appOrSP.GetAppOrServiceProviderName(),
+				Description:  appOrSP.GetAppOrServiceProviderDescription(),
+				URI:          app.GetURI(),
+				PublicAddr:   appOrSP.GetAppOrServiceProviderPublicAddr(),
+				Labels:       labels,
+				ClusterID:    c.AppClusterName,
+				FQDN:         fqdn,
+				AWSConsole:   app.IsAWSConsole(),
+				FriendlyName: services.FriendlyName(app),
+				SAMLApp:      false,
+			}
+
+			if app.IsAWSConsole() {
+				resultApp.AWSRoles = aws.FilterAWSRoles(c.Identity.AWSRoleARNs,
+					app.GetAWSAccountID())
+			}
+
+			result = append(result, resultApp)
+		} else {
+			labels := makeLabels(appOrSP.GetSAMLIdPServiceProvider().GetAllLabels())
+			resultApp := App{
+				Name:         appOrSP.GetAppOrServiceProviderName(),
+				Description:  appOrSP.GetAppOrServiceProviderDescription(),
+				URI:          "",
+				PublicAddr:   appOrSP.GetAppOrServiceProviderPublicAddr(),
+				Labels:       labels,
+				ClusterID:    c.AppClusterName,
+				FQDN:         "",
+				AWSConsole:   false,
+				FriendlyName: services.FriendlyName(appOrSP),
+				SAMLApp:      true,
+			}
+
+			result = append(result, resultApp)
 		}
-
-		if teleApp.IsAWSConsole() {
-			app.AWSRoles = aws.FilterAWSRoles(c.Identity.AWSRoleARNs,
-				teleApp.GetAWSAccountID())
-		}
-
-		result = append(result, app)
 	}
 
 	return result

gravitational/teleport

api/client/client.go
@@ -3090,6 +3090,8 @@ func (c *Client) ListResources(ctx context.Context, req proto.ListResourcesReque
 			resources[i] = respResource.GetKubernetesServer()
 		case types.KindUserGroup:
 			resources[i] = respResource.GetUserGroup()
+		case types.KindAppOrSAMLIdPServiceProvider:
+			resources[i] = respResource.GetAppServerOrSAMLIdPServiceProvider()
 		default:
 			return nil, trace.NotImplemented("resource type %s does not support pagination", req.ResourceType)
 		}
@@ -3182,6 +3184,8 @@ func GetResourcePage[T types.ResourceWithLabels](ctx context.Context, clt GetRes
 				resource = respResource.GetKubernetesServer()
 			case types.KindUserGroup:
 				resource = respResource.GetUserGroup()
+			case types.KindAppOrSAMLIdPServiceProvider:
+				resource = respResource.GetAppServerOrSAMLIdPServiceProvider()
 			default:
 				out.Resources = nil
 				return out, trace.NotImplemented("resource type %s does not support pagination", req.ResourceType)

lib/services/matchers.go
@@ -177,6 +177,19 @@ func MatchResourceByFilters(resource types.ResourceWithLabels, filter MatchResou
 		specResource = server.GetDatabase()
 		resourceKey.name = specResource.GetName()
 
+	case types.KindAppOrSAMLIdPServiceProvider:
+		switch appOrSP := resource.(type) {
+		case types.AppServer:
+			app := appOrSP.GetApp()
+			specResource = app
+			resourceKey.name = app.GetName()
+			resourceKey.addr = app.GetPublicAddr()
+		case types.SAMLIdPServiceProvider:
+			specResource = appOrSP
+			resourceKey.name = appOrSP.GetName()
+		default:
+			return false, trace.BadParameter("expected types.SAMLIdPServiceProvider or types.AppServer, got %T", resource)
+		}
 	default:
 		// We check if the resource kind is a Kubernetes resource kind to reduce the amount of
 		// of cases we need to handle. If the resource type didn't match any arm before

lib/web/ui/app.go
@@ -49,6 +49,8 @@ type App struct {
 	FriendlyName string `json:"friendlyName,omitempty"`
 	// UserGroups is a list of associated user groups.
 	UserGroups []UserGroupAndDescription `json:"userGroups,omitempty"`
+	// SAMLApp if true, indicates that the app is a SAML Application (SAML IdP Service Provider)
+	SAMLApp bool `json:"samlApp,omitempty"`
 }
 
 // UserGroupAndDescription is a user group name and its description.
@@ -67,50 +69,67 @@ type MakeAppsConfig struct {
 	LocalProxyDNSName string
 	// AppClusterName is the name of the cluster apps reside in.
 	AppClusterName string
-	// Apps is a list of registered apps.
-	Apps types.Apps
 	// AppsToUserGroups is a mapping of application names to user groups.
 	AppsToUserGroups map[string]types.UserGroups
+	// AppServersAndSAMLIdPServiceProviders is a list of AppServers and SAMLIdPServiceProviders.
+	AppServersAndSAMLIdPServiceProviders types.AppServersOrSAMLIdPServiceProviders
 	// Identity is identity of the logged in user.
 	Identity *tlsca.Identity
 }
 
-// MakeApps creates server application objects
+// MakeApps creates application objects (either Application Servers or SAML IdP Service Provider) for the WebUI.
 func MakeApps(c MakeAppsConfig) []App {
 	result := []App{}
-	for _, teleApp := range c.Apps {
-		fqdn := AssembleAppFQDN(c.LocalClusterName, c.LocalProxyDNSName, c.AppClusterName, teleApp)
-		labels := makeLabels(teleApp.GetAllLabels())
+	for _, appOrSP := range c.AppServersAndSAMLIdPServiceProviders {
+		if appOrSP.IsAppServer() {
+			app := appOrSP.GetAppServer().GetApp()
+			fqdn := AssembleAppFQDN(c.LocalClusterName, c.LocalProxyDNSName, c.AppClusterName, app)
+			labels := makeLabels(app.GetAllLabels())
 
-		userGroups := c.AppsToUserGroups[teleApp.GetName()]
+			userGroups := c.AppsToUserGroups[app.GetName()]
 
-		userGroupAndDescriptions := make([]UserGroupAndDescription, len(userGroups))
-		for i, userGroup := range userGroups {
-			userGroupAndDescriptions[i] = UserGroupAndDescription{
-				Name:        userGroup.GetName(),
-				Description: userGroup.GetMetadata().Description,
+			userGroupAndDescriptions := make([]UserGroupAndDescription, len(userGroups))
+			for i, userGroup := range userGroups {
+				userGroupAndDescriptions[i] = UserGroupAndDescription{
+					Name:        userGroup.GetName(),
+					Description: userGroup.GetMetadata().Description,
+				}
 			}
-		}
 
-		app := App{
-			Name:         teleApp.GetName(),
-			Description:  teleApp.GetDescription(),
-			URI:          teleApp.GetURI(),
-			PublicAddr:   teleApp.GetPublicAddr(),
-			Labels:       labels,
-			ClusterID:    c.AppClusterName,
-			FQDN:         fqdn,
-			AWSConsole:   teleApp.IsAWSConsole(),
-			FriendlyName: services.FriendlyName(teleApp),
-			UserGroups:   userGroupAndDescriptions,
-		}
+			resultApp := App{
+				Name:         appOrSP.GetName(),
+				Description:  appOrSP.GetDescription(),
+				URI:          app.GetURI(),
+				PublicAddr:   appOrSP.GetPublicAddr(),
+				Labels:       labels,
+				ClusterID:    c.AppClusterName,
+				FQDN:         fqdn,
+				AWSConsole:   app.IsAWSConsole(),
+				FriendlyName: services.FriendlyName(app),
+				UserGroups:   userGroupAndDescriptions,
+				SAMLApp:      false,
+			}
 
-		if teleApp.IsAWSConsole() {
-			app.AWSRoles = aws.FilterAWSRoles(c.Identity.AWSRoleARNs,
-				teleApp.GetAWSAccountID())
-		}
+			if app.IsAWSConsole() {
+				resultApp.AWSRoles = aws.FilterAWSRoles(c.Identity.AWSRoleARNs,
+					app.GetAWSAccountID())
+			}
 
-		result = append(result, app)
+			result = append(result, resultApp)
+		} else {
+			labels := makeLabels(appOrSP.GetSAMLIdPServiceProvider().GetAllLabels())
+			resultApp := App{
+				Name:         appOrSP.GetName(),
+				Description:  appOrSP.GetDescription(),
+				PublicAddr:   appOrSP.GetPublicAddr(),
+				Labels:       labels,
+				ClusterID:    c.AppClusterName,
+				FriendlyName: services.FriendlyName(appOrSP),
+				SAMLApp:      true,
+			}
+
+			result = append(result, resultApp)
+		}
 	}
 
 	return result