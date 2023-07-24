255e092e87843d45e4ed56bd4015e450f61aca4c
Steven Martin <steven@goteleport.com>
2023-07-24 14:23:22 -0400

    Displays warning when SSO is used and username specified (#29240)
    * Displays warning when SSO is used and username specified
    
    * improve logic
    
    * adds logic for passwordless auth types

--- tool/tsh/common/tsh.go
+++ tool/tsh/common/tsh.go
@@ -1833,8 +1833,23 @@ func onLogin(cf *CLIConf) error {
 
 	// If the cluster is using single-sign on, providing the user name
 	// with --user is likely a mistake, so display a warning.
-	if cf.AuthConnector != "" && cf.AuthConnector != constants.LocalConnector && cf.Username != "" {
-		fmt.Fprintf(os.Stderr, "WARNING: Ignoring Teleport user (%v) for Single Sign-On (SSO) login.\nProvide the user name during the SSO flow instead. Use --auth=local if you did not intend to login with SSO.\n", cf.Username)
+	if cf.Username != "" {
+		var displayIgnoreUserWarning = false
+		if cf.AuthConnector != "" && cf.AuthConnector != constants.LocalConnector && cf.AuthConnector != constants.PasswordlessConnector {
+			displayIgnoreUserWarning = true
+		} else if cf.AuthConnector == "" {
+			// Get the Ping so we check if the default Auth type is SSO
+			pr, err := tc.Ping(cf.Context)
+			if err != nil {
+				return trace.Wrap(err, "Teleport proxy not available at %s.", tc.WebProxyAddr)
+			}
+			if pr.Auth.Type != constants.LocalConnector && pr.Auth.Type != constants.PasswordlessConnector {
+				displayIgnoreUserWarning = true
+			}
+		}
+		if displayIgnoreUserWarning {
+			fmt.Fprintf(os.Stderr, "WARNING: Ignoring Teleport user (%v) for Single Sign-On (SSO) login.\nProvide the user name during the SSO flow instead. Use --auth=local if you did not intend to login with SSO.\n", cf.Username)
+		}
 	}
 
 	if cf.Username == "" {
