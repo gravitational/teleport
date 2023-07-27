2153b956f3a981d126229d908e87db95649ecad8
STeve (Xin) Huang <xin.huang@goteleport.com>
2023-07-27 14:00:20 -0400

    Fix MachineID not working behind L7 LB (#29692) (#29700)

--- lib/teleterm/clusters/storage.go
+++ lib/teleterm/clusters/storage.go
@@ -22,7 +22,6 @@ import (
 
 	"github.com/gravitational/trace"
 
-	apiclient "github.com/gravitational/teleport/api/client"
 	"github.com/gravitational/teleport/api/profile"
 	"github.com/gravitational/teleport/lib/client"
 	"github.com/gravitational/teleport/lib/teleterm/api/uri"
@@ -159,11 +158,6 @@ func (s *Storage) addCluster(ctx context.Context, dir, webProxyAddress string) (
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
-
-	// Do the ALPN handshake test to decide if connection upgrades are required
-	// for TLS Routing. Only do the test once Ping verifies the cluster is
-	// reachable.
-	clusterClient.TLSRoutingConnUpgradeRequired = apiclient.IsALPNConnUpgradeRequired(ctx, webProxyAddress, s.InsecureSkipVerify)
 
 	if err := clusterClient.SaveProfile(false); err != nil {
 		return nil, trace.Wrap(err)
