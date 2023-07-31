d548720f9ffc1bb40ce38a36ad4a2a57ab435f90
Michael Myers <michael.myers@goteleport.com>
2023-07-31 18:35:02 -0500

    Fix imports

--- lib/services/unified_resource.go
+++ lib/services/unified_resource.go
@@ -20,14 +20,14 @@ import (
 	"sync"
 
 	"github.com/google/btree"
-	"github.com/gravitational/teleport/api/types"
-	"github.com/gravitational/teleport/lib/backend"
 	"github.com/gravitational/trace"
 	"github.com/jonboulle/clockwork"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/gravitational/teleport"
 	apidefaults "github.com/gravitational/teleport/api/defaults"
+	"github.com/gravitational/teleport/api/types"
+	"github.com/gravitational/teleport/lib/backend"
 )
 
 type UnifiedResourceCacheConfig struct {
