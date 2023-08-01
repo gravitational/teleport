ef6d203fea689f958c015472e00b9f8ea64906b0
Michael Myers <michael.myers@goteleport.com>
2023-08-01 15:53:05 -0500

    Use cloneany

--- lib/services/unified_resource.go
+++ lib/services/unified_resource.go
@@ -229,22 +229,13 @@ func (u *UnifiedResourceCache) GetUnifiedResources(ctx context.Context) ([]types
 		return nil, trace.Wrap(err, "getting unified resource range")
 	}
 	for _, item := range result.Items {
-		switch r := item.Value.(type) {
-		case types.Server:
-			resources = append(resources, r.DeepCopy())
-		case types.AppServer:
-			resources = append(resources, r.Copy())
-		case types.SAMLIdPServiceProvider:
-			resources = append(resources, r.Copy())
-		case types.DatabaseServer:
-			resources = append(resources, r.Copy())
-		case types.KubeCluster:
-			resources = append(resources, r.Copy())
-		case types.WindowsDesktop:
-			resources = append(resources, r.Copy())
-		default:
-			return nil, trace.NotImplemented("unsupported type received from unified resources cache")
+		cloned := item.Value.CloneAny()
+		clonedResource, ok := cloned.(types.ResourceWithLabels)
+		if !ok {
+			return nil, trace.BadParameter("clone returned unexpected type %T", clonedResource)
 		}
+
+		resources = append(resources, clonedResource)
 	}
 
 	return resources, nil
@@ -452,7 +443,7 @@ func (u *UnifiedResourceCache) processEventAndUpdateCurrent(ctx context.Context,
 	case types.OpPut:
 		u.put(ctx, Item{
 			Key:   keyOf(event.Resource),
-			Value: event.Resource.(types.ResourceWithLabels),
+			Value: event.Resource.(types.CloneAny),
 		})
 	default:
 		u.log.Warnf("unsupported event type %s.", event.Type)
@@ -508,7 +499,7 @@ type Item struct {
 	// Key is a key of the key value item
 	Key []byte
 	// Value represents a resource such as types.Server or types.DatabaseServer
-	Value types.ResourceWithLabels
+	Value types.CloneAny
 }
 
 // Event represents an event that happened in the backend
