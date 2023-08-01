e2e58c5065332cfcdc34c4226929621af6828c6c
Bartosz Leper <bartosz.leper@goteleport.com>
2023-08-01 21:06:55 +0200

    Add a search panel to the unified resources list

---
+++ web/packages/teleport/src/UnifiedResources/SearchInput.tsx
@@ -0,0 +1,89 @@
+import React, { SetStateAction } from 'react';
+import styled from 'styled-components';
+
+import { height, space, color } from 'design/system';
+
+// Taken from design.dataTable.InputSearch; will be modified later.
+export default function SearchInput({
+  searchValue,
+  setSearchValue,
+  children,
+}: Props) {
+  return (
+    <WrapperBackground>
+      <Wrapper>
+        <StyledInput
+          placeholder="Search for resources..."
+          px={3}
+          value={searchValue}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setSearchValue(e.target.value)
+          }
+        />
+        <ChildWrapperBackground>
+          <ChildWrapper>{children}</ChildWrapper>
+        </ChildWrapperBackground>
+      </Wrapper>
+    </WrapperBackground>
+  );
+}
+
+type Props = {
+  searchValue: string;
+  setSearchValue: React.Dispatch<SetStateAction<string>>;
+  children?: JSX.Element;
+};
+
+const ChildWrapper = styled.div`
+  position: relative;
+  height: 100%;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 0 200px 200px 0;
+`;
+
+const ChildWrapperBackground = styled.div`
+  position: absolute;
+  height: 100%;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-left: ${props => props.theme.borders[1]}
+    ${props => props.theme.colors.spotBackground[0]};
+  border-radius: 0 200px 200px 0;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  overflow: hidden;
+  width: 100%;
+  border-radius: 200px;
+  height: 32px;
+  background: transparent;
+`;
+
+const WrapperBackground = styled.div`
+  background: ${props => props.theme.colors.levels.sunken};
+  border-radius: 200px;
+  width: 100%;
+  height: 32px;
+`;
+
+const StyledInput = styled.input`
+  border: none;
+  outline: none;
+  box-sizing: border-box;
+  height: 100%;
+  font-size: 12px;
+  width: 100%;
+  transition: all 0.2s;
+  ${color}
+  ${space}
+  ${height}
+  background: ${props => props.theme.colors.spotBackground[0]};
+  padding-right: 184px;
+`;
