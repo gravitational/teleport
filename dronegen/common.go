07d4f88ad366852c346d352e02af7bf119b7cd44
Cam Hutchison <camh@goteleport.com>
2023-08-02 15:10:29 +1000

    dronegen: Convert some linux tag pipelines to GitHub Actions
    Convert some of the linux-based tag build pipelines to run on GitHub
    Actions. The following pipelines have been converted:
    
        build-linux-amd64
        build-linux-amd64-centos7
        build-linux-amd64-centos7-fips
        build-linux-386
        build-linux-arm
        build-linux-amd64-deb
        build-linux-amd64-deb-fips
        build-linux-amd64-centos7-rpm
        build-linux-amd64-centos7-fips-rpm
        build-linux-386-deb
        build-linux-386-rpm
        build-linux-arm-deb
        build-linux-arm-rpm
    
    The GHA workflows builds tarballs as well as deb/rpm packages in the one
    workflow, so the `-deb` and `-rpm` pipelines will need to be manually
    removed from `.drone.yml`. The amd64 centos7 and non-centos7 pipelines
    have been combined as they were calling the same `make` target
    duplicating work. The amd64 build is always done on centos7. As a
    result, we do not name the pipeline with -centos7 any more, but we do
    still specify it as the build.assets `Makefile` still has a centos7
    target which is called, and we do still release an asset named with
    "centos7".
    
    Still remaining of the linux-based tag build pipelines are the arm64
    pipelines which are already converted using a different workflow and the
    non-native windows build.

--- dronegen/common.go
+++ dronegen/common.go
@@ -153,6 +153,7 @@ type buildType struct {
 	fips         bool
 	centos7      bool
 	buildConnect bool
+	buildOSPkg   bool
 }
 
 // Description provides a human-facing description of the artifact, e.g.:
