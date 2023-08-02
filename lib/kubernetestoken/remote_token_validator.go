e9d3d6ca7186dfe4e13478369d135667ae01c4ae
Noah <noah.stride@goteleport.com>
2023-08-02 22:05:08 +0100

    Proof of concept token validation

--- lib/kubernetestoken/remote_token_validator.go
+++ lib/kubernetestoken/remote_token_validator.go
@@ -15,3 +15,87 @@ limitations under the License.
 */
 
 package kubernetestoken
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/gravitational/trace"
+	"github.com/mitchellh/mapstructure"
+	"gopkg.in/square/go-jose.v2"
+	josejwt "gopkg.in/square/go-jose.v2/jwt"
+	"time"
+)
+
+type PodSubClaim struct {
+	Name string `json:"name"`
+	UID  string `json:"uid"`
+}
+
+type ServiceAccountSubClaim struct {
+	Name string `json:"name"`
+	UID  string `json:"uid"`
+}
+
+type KubernetesSubClaim struct {
+	Namespace      string                  `json:"namespace"`
+	ServiceAccount *ServiceAccountSubClaim `json:"serviceaccount"`
+	Pod            *PodSubClaim            `json:"pod"`
+}
+
+type ServiceAccountTokenClaims struct {
+	Sub        string              `json:"sub"`
+	Issuer     string              `json:"issuer"`
+	Kubernetes *KubernetesSubClaim `json:"kubernetes.io"`
+}
+
+// JoinAuditAttributes returns a series of attributes that can be inserted into
+// audit events related to a specific join.
+func (c *ServiceAccountTokenClaims) JoinAuditAttributes() (map[string]interface{}, error) {
+	res := map[string]interface{}{}
+	d, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
+		TagName: "json",
+		Result:  &res,
+	})
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	if err := d.Decode(c); err != nil {
+		return nil, trace.Wrap(err)
+	}
+	return res, nil
+}
+
+func ValidateRemoteToken(_ context.Context, jwksData []byte, expectAudience string, token string) (*ServiceAccountTokenClaims, error) {
+	jwt, err := josejwt.ParseSigned(token)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	jwks := jose.JSONWebKeySet{}
+	if err := json.Unmarshal(jwksData, &jwks); err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	var errs []error
+	for _, jwk := range jwks.Keys {
+		claims := josejwt.Claims{}
+		err := jwt.Claims(jwk.Public(), &claims)
+		if err != nil {
+			// If the jwt fails to verify, we can try the next one.
+			errs = append(errs, err)
+			continue
+		}
+
+		// TODO: Double check leeway
+		err = claims.ValidateWithLeeway(josejwt.Expected{
+			Audience: []string{
+				expectAudience,
+			},
+			Time: time.Now(),
+		}, time.Minute)
+		if err != nil {
+			return nil, trace.Wrap(err)
+		}
+	}
+	return nil, trace.Wrap(trace.NewAggregate(errs...))
+}
