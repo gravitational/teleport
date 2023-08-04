b6da889e4f3f4328f340805d42a50f9bff7c95dd
STeve Huang <xin.huang@goteleport.com>
2023-08-04 09:43:27 -0400

    AWS configurator support for OpenSearch

--- lib/configurators/aws/aws.go
+++ lib/configurators/aws/aws.go
@@ -254,6 +254,19 @@ var (
 	dynamodbActions = databaseActions{
 		authBoundary: stsActions,
 	}
+	// opensearchActions contains IAM actions for services.AWSMatcherOpenSearch
+	opensearchActions = databaseActions{
+		discovery: []string{
+			"es:ListDomainNames",
+			"es:DescribeDomains",
+			"es:ListTags",
+		},
+		metadata: []string{
+			// Used for url validation.
+			"es:DescribeDomains",
+		},
+		authBoundary: stsActions,
+	}
 )
 
 // awsConfigurator defines the AWS database configurator.
@@ -641,6 +654,7 @@ func buildPolicyDocument(flags configurators.BootstrapFlags, targetCfg targetCon
 	}
 
 	// Build statements for databases.
+	// TODO(greedy52) remove discovery permissions for static databases.
 	var requireSecretsManager, requireIAMEdit bool
 	var allActions []databaseActions
 	if hasRDSDatabases(flags, targetCfg) {
@@ -666,6 +680,9 @@ func buildPolicyDocument(flags configurators.BootstrapFlags, targetCfg targetCon
 	}
 	if hasDynamoDBDatabases(flags, targetCfg) {
 		allActions = append(allActions, dynamodbActions)
+	}
+	if hasOpenSearchDatabases(flags, targetCfg) {
+		allActions = append(allActions, opensearchActions)
 	}
 
 	dbOption := makeDatabaseActionsBuildOption(flags, targetCfg, boundary)
@@ -806,6 +823,18 @@ func hasMemoryDBDatabases(flags configurators.BootstrapFlags, targetCfg targetCo
 	}
 	return isAutoDiscoveryEnabledForMatcher(services.AWSMatcherMemoryDB, targetCfg.awsMatchers) ||
 		findEndpointIs(targetCfg.databases, apiawsutils.IsMemoryDBEndpoint)
+}
+
+// hasOpenSearchDatabases checks if the agent needs permission for OpenSearch
+// databases.
+func hasOpenSearchDatabases(flags configurators.BootstrapFlags, targetCfg targetConfig) bool {
+	if flags.ForceOpenSearchPermissions {
+		return true
+	}
+	return isAutoDiscoveryEnabledForMatcher(services.AWSMatcherOpenSearch, targetCfg.awsMatchers) ||
+		findDatabaseIs(targetCfg.databases, func(db *servicecfg.Database) bool {
+			return db.Protocol == defaults.ProtocolOpenSearch
+		})
 }
 
 // hasAWSKeyspacesDatabases checks if the agent needs permission for AWS Keyspaces.
