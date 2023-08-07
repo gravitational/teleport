70ecfa9fafc69128bf434b190b7a7b6a64f11730
Justinas Stankevicius <justinas@goteleport.com>
2023-08-07 20:02:52 +0300

    Remove Limit
    SearchEvents will default to defaults.EventsIterationLimit

--- lib/auth/usage.go
+++ lib/auth/usage.go
@@ -20,7 +20,6 @@ import (
 
 	"github.com/gravitational/trace"
 
-	apidefaults "github.com/gravitational/teleport/api/defaults"
 	"github.com/gravitational/teleport/api/types"
 	apievents "github.com/gravitational/teleport/api/types/events"
 	"github.com/gravitational/teleport/lib/events"
@@ -39,7 +38,6 @@ func GetAccessRequestMonthlyUsage(ctx context.Context, alog events.AuditLogger, 
 		results, startKey, err = alog.SearchEvents(ctx, events.SearchEventsRequest{
 			From:       monthStart,
 			To:         now,
-			Limit:      apidefaults.DefaultChunkSize,
 			Order:      types.EventOrderAscending,
 			EventTypes: []string{events.AccessRequestCreateEvent},
 			StartKey:   startKey,
