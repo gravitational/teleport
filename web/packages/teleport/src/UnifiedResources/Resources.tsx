c4e62f10e07d14117dbc383f2b669cf6d81f1dc4
Bartosz Leper <bartosz.leper@goteleport.com>
2023-08-09 15:47:18 +0200

    Fix the useEffect dependency list

--- web/packages/teleport/src/UnifiedResources/Resources.tsx
+++ web/packages/teleport/src/UnifiedResources/Resources.tsx
@@ -45,7 +45,7 @@ export function Resources() {
       observer.observe(observed.current);
       return () => observer.disconnect();
     }
-  });
+  }, [observed.current, fetchMore]);
 
   return (
     <FeatureBox>
