gravitational/teleport

commit 848e3e7b25e42a19db392a810c42a2e8f1032291

Wait for FileUploader to finish all writes before leaving `Close()` (#30211)
`FileUploader` doesn't wait for all go-routines handling writes to
finish before releasing the close method.
This casuses writes after close which makes tests to fail while running
the rmdir function on non-empty dirs.

Signed-off-by: Tiago Silva <tiago.silva@goteleport.com>

lib/events/filesessions/fileasync.go
@@ -23,6 +23,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 
 	"github.com/gravitational/trace"
@@ -130,12 +131,15 @@ type Uploader struct {
 
 	eventsCh chan events.UploadEvent
 	closeC   chan struct{}
+	wg       sync.WaitGroup
 
 	eventPreparer *events.NoOpPreparer
 }
 
 func (u *Uploader) Close() {
 	close(u.closeC)
+	// wait for all uploads to finish
+	u.wg.Wait()
 }
 
 func (u *Uploader) writeSessionError(sessionID session.ID, err error) error {
@@ -163,6 +167,8 @@ func (u *Uploader) checkSessionError(sessionID session.ID) (bool, error) {
 
 // Serve runs the uploader until stopped
 func (u *Uploader) Serve(ctx context.Context) error {
+	u.wg.Add(1)
+	defer u.wg.Done()
 	u.log.Infof("uploader will scan %v every %v", u.cfg.ScanDir, u.cfg.ScanPeriod.String())
 	backoff, err := retryutils.NewLinear(retryutils.LinearConfig{
 		Step:  u.cfg.ScanPeriod,
@@ -427,7 +433,9 @@ func (u *Uploader) startUpload(ctx context.Context, fileName string) error {
 	if time.Since(start) > 500*time.Millisecond {
 		u.log.Debugf("Semaphore acquired in %v for upload %v.", time.Since(start), fileName)
 	}
+	u.wg.Add(1)
 	go func() {
+		defer u.wg.Done()
 		if err := u.upload(ctx, upload); err != nil {
 			u.log.WithError(err).Warningf("Upload failed.")
 			u.emitEvent(events.UploadEvent{
@@ -505,7 +513,11 @@ func (u *Uploader) upload(ctx context.Context, up *upload) error {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	go u.monitorStreamStatus(ctx, up, stream, cancel)
+	u.wg.Add(1)
+	go func() {
+		defer u.wg.Done()
+		u.monitorStreamStatus(ctx, up, stream, cancel)
+	}()
 
 	for {
 		event, err := up.reader.Read(ctx)

integration/ec2_test.go
@@ -481,6 +481,9 @@ func TestEC2Hostname(t *testing.T) {
 	proc, err := service.NewTeleport(tconf)
 	require.NoError(t, err)
 	require.NoError(t, proc.Start())
-	t.Cleanup(func() { require.NoError(t, proc.Close()) })
+	t.Cleanup(func() {
+		require.NoError(t, proc.Close())
+		require.NoError(t, proc.Wait())
+	})
 	require.Equal(t, teleportHostname, proc.Config.Hostname)
 }

commit f094903f75a0dcbec2a611c1a3cf2bbb23ea70cf

Fix data race when calling Uploader's `Close` and `Serve` simultaneously (#30360)
* Fix data race when calling Uploader's `Close` and `Serve` at the same time

This PR fixes a data race that was introduced by #30211 while trying to
fix the uploader write after being closed.

The data race happens when `Serve` and `Close` methods are called at the
same time - i.e. `Serve` is spawned by `RegisterFunc` and `Close` is
called via `OnExit` function.
There is no guarentee that when `OnExit` is called `Serve` is already
spawn and running. This creates a data race when `Close` blocks on
`wg.Wait()` but `Serve` still tries to call `wg.Add(1)` into the
waitgroup.

To prevent the data race, we added a `isClosing` field that controls
when the server is marked to be closing and we call `wg.Add(1)` if
`isClosing=false` and under locking to prevent the race between `Close`
and `Serve`.

Signed-off-by: Tiago Silva <tiago.silva@goteleport.com>

* add todo message

---------

Signed-off-by: Tiago Silva <tiago.silva@goteleport.com>

lib/events/filesessions/fileasync.go
@@ -110,7 +110,6 @@ func NewUploader(cfg UploaderConfig) (*Uploader, error) {
 		eventsCh:      make(chan events.UploadEvent, cfg.ConcurrentUploads),
 		eventPreparer: &events.NoOpPreparer{},
 	}
-
 	return uploader, nil
 }
 
@@ -129,14 +128,21 @@ type Uploader struct {
 	cfg UploaderConfig
 	log *log.Entry
 
-	eventsCh chan events.UploadEvent
-	closeC   chan struct{}
-	wg       sync.WaitGroup
+	eventsCh  chan events.UploadEvent
+	closeC    chan struct{}
+	wg        sync.WaitGroup
+	mu        sync.Mutex
+	isClosing bool
 
 	eventPreparer *events.NoOpPreparer
 }
 
 func (u *Uploader) Close() {
+	// TODO(tigrato): prevent close to be called before Serve starts.
+	u.mu.Lock()
+	u.isClosing = true
+	u.mu.Unlock()
+
 	close(u.closeC)
 	// wait for all uploads to finish
 	u.wg.Wait()
@@ -167,8 +173,22 @@ func (u *Uploader) checkSessionError(sessionID session.ID) (bool, error) {
 
 // Serve runs the uploader until stopped
 func (u *Uploader) Serve(ctx context.Context) error {
+	// Check if close operation is already in progress.
+	// We need to do this because Serve is spawned in a goroutine
+	// and Close can be called before Serve starts which ends up in a data
+	// race because Close is waiting for wg to be 0 and Serve is adding to wg.
+	// To avoid this, we check if Close is already in progress and return
+	// immediately. If Close is not in progress, we add to wg under the mutex
+	// lock to ensure that Close can't reach wg.Wait() before Serve adds to wg.
+	u.mu.Lock()
+	if u.isClosing {
+		u.mu.Unlock()
+		return nil
+	}
 	u.wg.Add(1)
+	u.mu.Unlock()
 	defer u.wg.Done()
+
 	u.log.Infof("uploader will scan %v every %v", u.cfg.ScanDir, u.cfg.ScanPeriod.String())
 	backoff, err := retryutils.NewLinear(retryutils.LinearConfig{
 		Step:  u.cfg.ScanPeriod,