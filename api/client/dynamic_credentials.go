8844c1f64d79ea141b9a30f6b85ec857140606d9
Noah <noah.stride@goteleport.com>
2023-08-15 17:55:54 +0100

    Add note to what needs doing

--- api/client/dynamic_credentials.go
+++ api/client/dynamic_credentials.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2023 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package client
 
 import (
@@ -67,7 +83,7 @@ func (d *WatchedIdentityFileCreds) Reload() error {
 	if err != nil {
 		return trace.Wrap(err)
 	}
-	_, err := sshutils.ProxyClientSSHConfig(
+	cc, err := sshutils.ProxyClientSSHConfig(
 		sshCert, sshPrivateKey, id.CACerts.SSH...,
 	)
 	if err != nil {
@@ -157,6 +173,7 @@ func (d *WatchedIdentityFileCreds) SSHClientConfig() (*ssh.ClientConfig, error) 
 	// Build a "dynamic" ssh config. Based roughly on
 	// `sshutils.ProxyClientSSHConfig` with modifications to make it work with
 	// dynamically changing credentials.
+	// TODO: Handle changing host certs
 	cfg := &ssh.ClientConfig{
 		Auth: []ssh.AuthMethod{
 			ssh.PublicKeysCallback(func() (signers []ssh.Signer, err error) {
