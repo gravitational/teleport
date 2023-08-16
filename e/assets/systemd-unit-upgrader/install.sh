ae537ab91cd61bcec46f6e8cdb2b1c19f89e8933
Gus Luxton <gus@goteleport.com>
2023-08-16 13:07:52 -0300

    systemd-unit-upgrader: Only reload if the unit is active (#2001)
    Source-commit: gravitational/teleport.e@7c033be70265cb0425478ff9aeaaa429b9a70438
    Source-pull-requests: gravitational/teleport.e#2001

--- e/assets/systemd-unit-upgrader/install.sh
+++ e/assets/systemd-unit-upgrader/install.sh
@@ -6,6 +6,10 @@ set -eu
 
 # reload systemd configuration and start/restart units
 systemctl daemon-reload
-systemctl reload teleport.service
+# only reload teleport if the unit is already active, mimicking what systemctl try-restart does
+# reloading when the unit is not already active (for example on a fresh install) generates an error
+if systemctl is-active --quiet teleport.service; then
+    systemctl reload teleport.service
+fi
 systemctl enable teleport-upgrade.timer
 systemctl start teleport-upgrade.timer
