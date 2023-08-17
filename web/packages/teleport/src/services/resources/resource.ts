c2a4dd89e008e0fdd04ef9b178ee51aad084f661
Michael Myers <michael.myers@goteleport.com>
2023-08-17 15:08:04 -0500

    Add legacy leaf cluster support

--- web/packages/teleport/src/services/resources/resource.ts
+++ web/packages/teleport/src/services/resources/resource.ts
@@ -16,8 +16,10 @@
 
 import api from 'teleport/services/api';
 import cfg, { UrlResourcesParams } from 'teleport/config';
+import history from 'teleport/services/history';
 
 import { UnifiedResource, AgentResponse } from '../agents';
+import { KeysEnum } from '../localStorage';
 
 import { makeUnifiedResource } from './makeUnifiedResource';
 
@@ -40,11 +42,27 @@ class ResourceService {
       .then(json => {
         const items = json?.items || [];
 
+        // TODO (avatus) REMOVE IN 15.0
+        // if this request succeeds, we don't need a legacy view
+        localStorage.removeItem(KeysEnum.UNIFIED_RESOURCES_NOT_SUPPORTED);
         return {
           agents: items.map(makeUnifiedResource),
           startKey: json?.startKey,
           totalCount: json?.totalCount,
         };
+      })
+      .catch(res => {
+        // TODO (avatus) : a temporary check to catch unimplemented errors for unified resources
+        // This is a quick hacky way to catch the error until we migrate completely to unified resources
+        // REMOVE IN 15.0
+        if (res.response?.status === 404 || res.response?.status === 501) {
+          localStorage.setItem(
+            KeysEnum.UNIFIED_RESOURCES_NOT_SUPPORTED,
+            'true'
+          );
+          history.replace(cfg.getNodesRoute(clusterId));
+        }
+        throw res;
       });
   }
 
