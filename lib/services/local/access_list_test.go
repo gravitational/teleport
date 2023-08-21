b8da75ee68b59e3c562cdec56a477fc2a1846c5f
Lisa Kim <lisa@goteleport.com>
2023-08-21 13:58:30 -0700

    Add required title to access list resource (#30782)

--- lib/services/local/access_list_test.go
+++ lib/services/local/access_list_test.go
@@ -133,6 +133,7 @@ func newAccessList(t *testing.T, name string) *accesslist.AccessList {
 			Name: name,
 		},
 		accesslist.Spec{
+			Title:       "title",
 			Description: "test access list",
 			Owners: []accesslist.Owner{
 				{
