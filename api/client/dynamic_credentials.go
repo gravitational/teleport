772250f1b161746a5b30915939abc743dc9f405d
Noah <noah.stride@goteleport.com>
2023-08-22 13:52:43 +0100

    Further tidy dynamic credential implementation

--- api/client/dynamic_credentials.go
+++ api/client/dynamic_credentials.go
@@ -23,7 +23,6 @@ import (
 	"github.com/gravitational/teleport/api/constants"
 	"github.com/gravitational/teleport/api/defaults"
 	"github.com/gravitational/teleport/api/identityfile"
-	"github.com/gravitational/teleport/api/utils"
 	"github.com/gravitational/teleport/api/utils/keys"
 	"github.com/gravitational/teleport/api/utils/sshutils"
 	"github.com/gravitational/trace"
@@ -33,36 +32,39 @@ import (
 	"sync"
 )
 
-type WatchedIdentityFileCreds struct {
+// DynamicIdentityFileCreds allows a changing identity file to be used as the
+// source of authentication for Client. It does not automatically watch the
+// identity file or reload on an interval, this is left as an exercise for the
+// consumer.
+type DynamicIdentityFileCreds struct {
 	// mu protects the fields that may change if the underlying identity file
 	// is reloaded.
 	mu            sync.RWMutex
 	tlsCert       *tls.Certificate
-	pool          *x509.CertPool
+	tlsRootCAs    *x509.CertPool
 	sshCert       *ssh.Certificate
 	sshKey        crypto.Signer
 	sshKnownHosts []ssh.PublicKey
+	sshUser       string
 
-	path        string
-	clusterName string
+	// Path is the path to the identity file to load and reload.
+	Path string
 }
 
-func LoadAndWatchIdentityFile(path string, clusterName string) (*WatchedIdentityFileCreds, error) {
-	d := &WatchedIdentityFileCreds{
-		path:        path,
-		clusterName: clusterName,
+func NewDynamicIdentityFileCreds(path string) (*DynamicIdentityFileCreds, error) {
+	d := &DynamicIdentityFileCreds{
+		Path: path,
 	}
-
-	err := d.Reload()
-	if err != nil {
+	if err := d.Reload(); err != nil {
 		return nil, trace.Wrap(err)
 	}
-
 	return d, nil
 }
 
-func (d *WatchedIdentityFileCreds) Reload() error {
-	id, err := identityfile.ReadFile(d.path)
+// Reload causes the identity file to be re-read from the disk. It will return
+// an error if loading the credentials fails.
+func (d *DynamicIdentityFileCreds) Reload() error {
+	id, err := identityfile.ReadFile(d.Path)
 	if err != nil {
 		return trace.Wrap(err)
 	}
@@ -92,37 +94,48 @@ func (d *WatchedIdentityFileCreds) Reload() error {
 	if err != nil {
 		return trace.Wrap(err)
 	}
+	// The KeyId is not always a valid principal, so we use the first valid
+	// principal instead.
+	sshUser := d.sshCert.KeyId
+	if len(d.sshCert.ValidPrincipals) > 0 {
+		sshUser = d.sshCert.ValidPrincipals[0]
+	}
 
 	d.mu.Lock()
-	d.pool = pool
+	defer d.mu.Unlock()
+	d.tlsRootCAs = pool
 	d.tlsCert = &cert
 	d.sshCert = sshCert
 	d.sshKey = sshPrivateKey
 	d.sshKnownHosts = knownHosts
-	d.mu.Unlock()
+	d.sshUser = sshUser
 	return nil
 }
 
-// Dialer is used to dial a connection to an Auth server.
-func (d *WatchedIdentityFileCreds) Dialer(
+// Dialer returns a dialer for the client to use. This is not used, but is
+// needed to implement the Credentials interface.
+func (d *DynamicIdentityFileCreds) Dialer(
 	_ Config,
 ) (ContextDialer, error) {
 	// Returning a dialer isn't necessary for this credential.
 	return nil, trace.NotImplemented("no dialer")
 }
 
-// TLSConfig returns TLS configuration.
-func (d *WatchedIdentityFileCreds) TLSConfig() (*tls.Config, error) {
+// TLSConfig returns TLS configuration. Implementing the Credentials interface.
+func (d *DynamicIdentityFileCreds) TLSConfig() (*tls.Config, error) {
 	d.mu.RLock()
 	defer d.mu.RUnlock()
 
+	// Build a "dynamic" tls.Config which can support a changing cert and root
+	// CA pool.
 	cfg := &tls.Config{
+		NextProtos: []string{http2.NextProtoTLS},
+
 		// GetClientCertificate is used instead of the static Certificates
 		// field.
 		Certificates: nil,
-
 		GetClientCertificate: func(
-			info *tls.CertificateRequestInfo,
+			_ *tls.CertificateRequestInfo,
 		) (*tls.Certificate, error) {
 			// GetClientCertificate callback is used to allow us to dynamically
 			// change the certificate when reloaded.
@@ -130,6 +143,9 @@ func (d *WatchedIdentityFileCreds) TLSConfig() (*tls.Config, error) {
 			defer d.mu.RUnlock()
 			return d.tlsCert, nil
 		},
+
+		// VerifyConnection is used instead of the static RootCAs field.
+		RootCAs: nil,
 		// InsecureSkipVerify is forced true to ensure that only our
 		// VerifyConnection callback is used to verify the server's presented
 		// certificate.
@@ -144,7 +160,7 @@ func (d *WatchedIdentityFileCreds) TLSConfig() (*tls.Config, error) {
 			opts := x509.VerifyOptions{
 				DNSName:       state.ServerName,
 				Intermediates: x509.NewCertPool(),
-				Roots:         d.pool,
+				Roots:         d.tlsRootCAs,
 			}
 			for _, cert := range state.PeerCertificates[1:] {
 				// Whilst we don't currently use intermediate certs at
@@ -155,32 +171,22 @@ func (d *WatchedIdentityFileCreds) TLSConfig() (*tls.Config, error) {
 			_, err := state.PeerCertificates[0].Verify(opts)
 			return err
 		},
-		NextProtos: []string{http2.NextProtoTLS},
-	}
-
-	// If the cluster name has been provided, we should use that for SNI.
-	// This enables alpn routed connections on Teleport Cloud which relies on
-	// a distinct SNI to route traffic to the correct Teleport Tenant.
-	// TODO: Could we determine this from the cert 🤔 and not need to ask
-	// the user explicitly for this.
-	if d.clusterName != "" {
-		cfg.ServerName = utils.EncodeClusterName(d.clusterName)
-	} else {
-		// Otherwise fall back to `teleport.cluster.local` which should work
-		// in most general teleport installs.
-		cfg.ServerName = constants.APIDomain
+		// Set the default name that's included in all Teleport issued
+		// certificates.
+		ServerName: constants.APIDomain,
 	}
 
 	return cfg, nil
 }
 
-// SSHClientConfig returns SSH configuration.
-func (d *WatchedIdentityFileCreds) SSHClientConfig() (*ssh.ClientConfig, error) {
+// SSHClientConfig returns SSH configuration, implementing the Credentials
+// interface.
+func (d *DynamicIdentityFileCreds) SSHClientConfig() (*ssh.ClientConfig, error) {
 	d.mu.RLock()
 	defer d.mu.RUnlock()
 	// Build a "dynamic" ssh config. Based roughly on
 	// `sshutils.ProxyClientSSHConfig` with modifications to make it work with
-	// dynamically changing credentials.
+	// dynamically changing credentials and CAs.
 	cfg := &ssh.ClientConfig{
 		Auth: []ssh.AuthMethod{
 			ssh.PublicKeysCallback(func() (signers []ssh.Signer, err error) {
@@ -206,13 +212,7 @@ func (d *WatchedIdentityFileCreds) SSHClientConfig() (*ssh.ClientConfig, error) 
 			return hostKeyCallback(hostname, remote, key)
 		},
 		Timeout: defaults.DefaultIOTimeout,
+		User:    d.sshUser,
 	}
-
-	// TODO: Evaluate if this needs to be made "dynamic"
-	cfg.User = d.sshCert.KeyId
-	if len(d.sshCert.ValidPrincipals) > 0 {
-		cfg.User = d.sshCert.ValidPrincipals[0]
-	}
-
 	return cfg, nil
 }
