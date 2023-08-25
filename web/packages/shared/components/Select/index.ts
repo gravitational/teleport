46dce399cbcc9ea0f8a297ac8c0ac2e3b0653aa3
Lisa Kim <lisa@goteleport.com>
2023-08-25 11:49:33 -0700

    Web: Add calendar icon, export select style, and add type to validation rule (#30817)
    * Add calendar icon
    
    * Add another type to valdiation rule
    
    * Export select style

--- web/packages/shared/components/Select/index.ts
+++ web/packages/shared/components/Select/index.ts
@@ -14,9 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import Select, { SelectAsync } from './Select';
+import Select, { SelectAsync, StyledSelect } from './Select';
 import DarkStyledSelect from './DarkStyledSelect';
 
 export * from './types';
 export default Select;
-export { SelectAsync, DarkStyledSelect };
+export { SelectAsync, DarkStyledSelect, StyledSelect };
