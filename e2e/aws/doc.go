28bcb883c24476a940a976eb4f2954ea5044dd90
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2023-08-28 21:56:12 +0200

    Update e2e/aws/doc.go
    Co-authored-by: Zac Bergquist <zac.bergquist@goteleport.com>

--- e2e/aws/doc.go
+++ e2e/aws/doc.go
@@ -12,4 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package e2e contains end-to-end tests for Teleport's AWS integrations.
 package e2e
