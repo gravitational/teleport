398dc9ad923e88da2cdaac1b5992e88fc9e1dd88
FireDrunk <thijs.cramer@gmail.com>
2023-08-28 17:56:34 +0200

    Updated tbot usage example

--- examples/local-cluster/bot/start.sh
+++ examples/local-cluster/bot/start.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
-set -e
 
+# Determine the directory the script was run from.
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-echo "SCRIPT_DIR: ${SCRIPT_DIR}"
 
 echo "Generating bot ID"
 BOT_ID=$(tr -dc a-z0-9 </dev/urandom | head -c 6)
 
 echo "Generating bot token"
 cd ${SCRIPT_DIR}/../auth
-TOKEN=$(tctl -c teleport.yaml bots add static-${BOT_ID} --roles=access --logins=root --format=json | jq .token_id)
+TCTL_OUTPUT=$(tctl -c teleport.yaml bots add static-${BOT_ID} --roles=access --logins=root --format=json)
+if [ $? -ne 0 ]; then
+  echo "Failed to retrieve a new bot token, is the Auth server running?"
+  exit 1
+fi
+
+TOKEN=$(echo "${TCTL_OUTPUT}" | jq -r .token_id)
+if [ $? -ne 0 ]; then
+  echo "Failed to parse tctl output, do you have jq installed?"
+  exit 1
+fi
+
+# Return to tbot directory
 cd -
 
-echo "Resetting config file"
-cp tbot.yaml.orig tbot.yaml
+echo "Generating Config file using \`tbot configure\`"
+tbot configure \
+  --auth-server localhost:5000 \
+  --oneshot \
+  --join-method "token" \
+  --token ${TOKEN} \
+  --data-dir ".data" \
+  --insecure \
+  --debug \
+  -o "${SCRIPT_DIR}/tbot.yaml"
 
-echo "Replacing bot token in config"
-sed -i "s/__TOKEN__/${TOKEN}/g" tbot.yaml
+if [ $? -ne 0 ]; then
+  echo "Failed to generate a valid tbot configuration file using configure."
+  exit 1
+fi
 
 tbot start -c tbot.yaml -d
