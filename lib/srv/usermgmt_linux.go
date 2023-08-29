c39bcc090181913467eec5bc9534cbcd98bb58ce
Alex McGrath <alex.mcgrath@goteleport.com>
2023-08-29 16:45:11 +0100

    Manually create the users HOME rather than letting useradd do it
    Test that CreateHomeDirectory does not follow symlinks
    
    resolve comments
    
    add our own recursive directory copy
    
    Resolve comments

--- lib/srv/usermgmt_linux.go
+++ lib/srv/usermgmt_linux.go
@@ -17,14 +17,18 @@ limitations under the License.
 package srv
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"os/user"
 	"path/filepath"
+	"strconv"
+	"strings"
 
 	"github.com/gravitational/trace"
 	log "github.com/sirupsen/logrus"
 
+	"github.com/gravitational/teleport/lib/utils"
 	"github.com/gravitational/teleport/lib/utils/host"
 )
 
@@ -73,7 +77,11 @@ func (*HostUsersProvisioningBackend) CreateGroup(name string) error {
 
 // CreateUser creates a user on a host
 func (*HostUsersProvisioningBackend) CreateUser(name string, groups []string) error {
-	_, err := host.UserAdd(name, groups)
+	home, err := readDefaultHome(name)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	_, err = host.UserAdd(name, groups, home)
 	return trace.Wrap(err)
 }
 
@@ -144,4 +152,104 @@ func (u *HostUsersProvisioningBackend) RemoveSudoersFile(username string) error 
 		return nil
 	}
 	return trace.Wrap(os.Remove(sudoersFilePath))
+}
+
+// readDefaultKey reads /etc/default/useradd and returns the key if
+// its found, if its not found it'll return the provided defaultValue
+func readDefaultKey(key string, defaultValue string) (string, error) {
+	b, err := os.Open("/etc/default/useradd")
+	if err != nil {
+		if os.IsNotExist(err) {
+			return defaultValue, nil
+		}
+		return "", err
+	}
+
+	scanner := bufio.NewScanner(b)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if !strings.HasPrefix(line, key) {
+			continue
+		}
+		kv := strings.SplitN(line, "=", 2)
+		if len(kv) != 2 {
+			return defaultValue, nil
+		}
+		return strings.TrimSpace(kv[1]), nil
+	}
+	return defaultValue, nil
+}
+
+// readDefaultHome reads /etc/default/useradd for the HOME key,
+// defaulting to "/home" and join it with the user for the user
+// home directory
+func readDefaultHome(user string) (string, error) {
+	const defaultHome = "/home"
+	home, err := readDefaultKey("HOME", defaultHome)
+	return filepath.Join(home, user), trace.Wrap(err)
+}
+
+// readDefaultHome reads /etc/default/useradd for the SKEL key, defaulting to "/etc/skel"
+func readDefaultSkel() (string, error) {
+	const defaultSkel = "/etc/skel"
+	skel, err := readDefaultKey("SKEL", defaultSkel)
+	return skel, trace.Wrap(err)
+}
+
+func (u *HostUsersProvisioningBackend) CreateHomeDirectory(user string, uidS, gidS string) error {
+	uid, err := strconv.Atoi(uidS)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	gid, err := strconv.Atoi(gidS)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	userHome, err := readDefaultHome(user)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	err = os.Mkdir(userHome, 0o700)
+	if err != nil {
+		if os.IsExist(err) {
+			return nil
+		}
+		return trace.Wrap(err)
+	}
+
+	skelDir, err := readDefaultSkel()
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	_, err = os.Stat(skelDir)
+	if err != nil && !os.IsNotExist(err) {
+		return trace.Wrap(err)
+	}
+
+	if !os.IsNotExist(err) {
+		if err := utils.RecursiveCopy(skelDir, userHome, func(src, dest string) (bool, error) {
+			destInfo, err := os.Lstat(dest)
+			if err != nil {
+				if os.IsNotExist(err) {
+					// dont filter destination files that dont exist as they
+					// still need to get copied
+					return false, nil
+				}
+				return true, trace.ConvertSystemError(err)
+			}
+			return destInfo.Mode().Type()&os.ModeSymlink != 0, nil
+		}); err != nil {
+			return trace.Wrap(err)
+		}
+	}
+
+	if err := utils.RecursiveChown(userHome, uid, gid); err != nil {
+		return trace.Wrap(err)
+	}
+
+	return nil
 }
