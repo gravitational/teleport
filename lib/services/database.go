3dbd0b84bdcb5397b516b4c605c43dd7301501fb
Marco André Dinis <marco.dinis@goteleport.com>
2023-08-31 08:53:46 +0100

    AWS OIDC - List RDS: add VPC ID (#30971)
    * AWS OIDC - List RDS: add VPC ID
    
    When deploying a Teleport service using the AWS OIDC DeployService, we
    can provide the SecurityGroups to be applied to the network
    configuration of the service.
    
    Using the default SG for the VPC (vpc is inferred from the used subnets)
    might not always be the best one (it is usually locked down).
    
    The VPC is required to list the possible SecurityGroups.
    
    Currently, there's no VPC information in the database.
    
    This PR adds a new field to the AWS.RDS fields to indicate which VPC is
    this Database in.
    
    When running the DeployService, the user should be able to list the
    Security Groups and then pick one or many to apply to the service.
    
    * vpcId to vpcID

--- lib/services/database.go
+++ lib/services/database.go
@@ -620,8 +620,10 @@ func MetadataFromRDSV2Instance(rdsInstance *rdsTypesV2.DBInstance) (*types.AWS, 
 		return nil, trace.Wrap(err)
 	}
 
+	var vpcID string
 	var subnets []string
 	if rdsInstance.DBSubnetGroup != nil {
+		vpcID = aws.StringValue(rdsInstance.DBSubnetGroup.VpcId)
 		subnets = make([]string, 0, len(rdsInstance.DBSubnetGroup.Subnets))
 		for _, s := range rdsInstance.DBSubnetGroup.Subnets {
 			if s.SubnetIdentifier == nil || *s.SubnetIdentifier == "" {
@@ -640,6 +642,7 @@ func MetadataFromRDSV2Instance(rdsInstance *rdsTypesV2.DBInstance) (*types.AWS, 
 			ResourceID: aws.StringValue(rdsInstance.DbiResourceId),
 			IAMAuth:    rdsInstance.IAMDatabaseAuthenticationEnabled,
 			Subnets:    subnets,
+			VPCID:      vpcID,
 		},
 	}, nil
 }
