2af89d91b2e1da94a7300eccf5bf620eea0b71b9
Marco Dinis <marco.dinis@goteleport.com>
2023-09-06 19:01:43 +0100

    AWS OIDC: Set up integration with a single command
    Creating an AWS OIDC Integration requires a lot of clicks, copy/paste,
    navigation between tabs.
    
    This PR adds a single teleport command that creates all the required
    resources in AWS:
    - AWS OIDC Identity Provider that uses Teleport as source
    - AWS IAM Role that can be used by this Identity Provider
    
    This role, will then have inline policies allowing multiple features in
    Teleport.
    Example: a policy must be added to this role that has
    `rds:DescribeInstances` and `rds:DescribeClusters` so that the user can
    use the integration to enroll RDS Databases.

--- lib/cloud/aws/policy_statements.go
+++ lib/cloud/aws/policy_statements.go
@@ -16,6 +16,8 @@ limitations under the License.
 
 package aws
 
+import "fmt"
+
 var (
 	allResources = []string{"*"}
 )
@@ -123,5 +125,25 @@ func StatementForEC2InstanceConnectEndpoint() *Statement {
 			"ec2-instance-connect:OpenTunnel",
 		},
 		Resources: allResources,
+	}
+}
+
+// StatementForAWSOIDCRoleTrustRelationship returns the Trust Relationship to allow the OpenID Connect Provider
+// set up during the AWS OIDC Onboarding to assume this Role.
+func StatementForAWSOIDCRoleTrustRelationship(accountID, providerURL string, audiences []string) *Statement {
+	federatedARN := fmt.Sprintf("arn:aws:iam::%s:oidc-provider/%s", accountID, providerURL)
+	federatedAudience := fmt.Sprintf("%s:aud", providerURL)
+
+	return &Statement{
+		Effect:  EffectAllow,
+		Actions: SliceOrString{"sts:AssumeRoleWithWebIdentity"},
+		Principals: map[string]SliceOrString{
+			"Federated": []string{federatedARN},
+		},
+		Conditions: map[string]map[string]SliceOrString{
+			"StringEquals": {
+				federatedAudience: audiences,
+			},
+		},
 	}
 }
