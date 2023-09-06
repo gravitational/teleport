148162e5d5100dbe613a14159ae6927ce2724140
Alan Parra <alan.parra@goteleport.com>
2023-09-06 14:28:20 -0300

    chore: Bump Go to 1.20.8

--- build.assets/versions.mk
+++ build.assets/versions.mk
@@ -1,7 +1,7 @@
 # Keep all tool versions in one place.
 # This file can be included in other Makefiles to avoid duplication.
 
-GOLANG_VERSION ?= go1.20.7
+GOLANG_VERSION ?= go1.20.8
 
 NODE_VERSION ?= 16.18.1
 
