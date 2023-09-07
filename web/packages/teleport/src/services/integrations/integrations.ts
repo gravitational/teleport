1c9e7e333b40c247fbca3d66092e66942ee52cac
Marco Dinis <marco.dinis@goteleport.com>
2023-09-07 18:38:46 +0100

    WebAPI: Include new DB RDS fields (vpc and subnet)
    Those two fields were not being saved to the backend.

--- web/packages/teleport/src/services/integrations/integrations.ts
+++ web/packages/teleport/src/services/integrations/integrations.ts
@@ -160,6 +160,7 @@ export function makeAwsDatabase(json: any): AwsRdsDatabase {
     labels: labels ?? [],
     subnets: aws?.rds?.subnets,
     resourceId: aws?.rds?.resource_id,
+    vpcId: aws?.rds?.vpc_id,
     accountId: aws?.account_id,
     region: aws?.region,
   };
