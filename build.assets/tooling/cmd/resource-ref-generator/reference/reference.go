060f5d8bcb8be424c790846ce50f26309b1b5776
Paul Gottschling <paul.gottschling@goteleport.com>
2023-09-08 12:02:49 -0400

    Add a resource reference generator
    Closes #16948
    Closes #9949
    
    Implements RFD 130.
    
    See the included README for information about how the generator works.
    
    This implementation breaks from, and updates, RFD 130:
    
    - The reference includes separate pages for dynamic resources in order
      to make it easier to read. Edit the RFD to be less specific about the
      output template so this isn't wedded to implementation details.
    - The generator uses a YAML configuration file. This is because the
      config ended up including more fields than specified in the RFD, so
      separating the config from the source made sense to give users less
      noise to deal with.
    - The generator requires that a user specify the resources to include in
      the reference docs. This makes it easier to start with a smaller, more
      manageable set of resource reference docs.
    - Support RFD 153 in the reference generator: RFD 153 resource types
      include unexported fields and include a `Metadata` field from a
      different package than legacy resources types. They also include the
      `Metadata` field with a pointer. Adjust the generator to ignore
      unexported fields in struct declarations, and to treat pointer field
      types the same way as value field types.
    - Remove the description of example YAML delimiters. These are no longer
      viable, as we maintain several documentation generators that extract
      comments from the source. Instead, if we can't generate example YAML,
      show a comment in the YAML example advising the reader to read the
      description of a field.
    
    Other changes:
    
     - Add a Make target for generating dynamic resource reference
       documentation, plus a target for checking that the resource reference
       docs are up to date. Run the checker in the `Lint (Proto)` job.
     - Generate reference docs for Bot, RoleV6, OIDCConnectorV3, and
       SAMLConnectorV2.

---
+++ build.assets/tooling/cmd/resource-ref-generator/reference/reference.go
@@ -0,0 +1,154 @@
+// Teleport
+// Copyright (C) 2025  Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package reference
+
+import (
+	_ "embed"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+
+	"github.com/gravitational/teleport/build.assets/tooling/cmd/resource-ref-generator/resource"
+)
+
+// pageContent represents a reference page for a single resource and its related
+// fields. Fields must be exported so we can use them in templates.
+type pageContent struct {
+	Resource resourceSection
+	Fields   map[resource.PackageInfo]resource.ReferenceEntry
+}
+
+// resourceSection represents a top-level section of the resource reference
+// dedicated to a dynamic resource.
+type resourceSection struct {
+	Version string
+	Kind    string
+	resource.ReferenceEntry
+}
+
+// TypeInfo represents the name and package name of an exported Go type. It
+// makes no guarantees about whether the type was actually declared within the
+// package.
+type TypeInfo struct {
+	// Go package path (not a file path)
+	Package string `yaml:"package"`
+	// Name of the type, e.g., Metadata
+	Name string `yaml:"name"`
+}
+
+// ResourceConfig describes a resource type to include in the reference.
+type ResourceConfig struct {
+	// The name of the struct type as declared in the Go source, e.g.,
+	// RoleV6.
+	TypeName string `yaml:"type"`
+	// The final path segment in the name of the Go package containing this
+	// type declaration, e.g., "api".
+	PackageName string `yaml:"package"`
+	// The value of the "kind" field within a YAML manifest for this
+	// resource, e.g., "role".
+	KindValue string `yaml:"yaml_kind"`
+	// The value of the "version" field within a YAML manifest for this
+	// resource, e.g., "v6".
+	VersionValue string `yaml:"yaml_version"`
+}
+
+// GeneratorConfig is the user-facing configuration for the resource reference
+// generator.
+type GeneratorConfig struct {
+	Resources []ResourceConfig `yaml:"resources"`
+	// Path to the root of the Go project directory.
+	SourcePath string `yaml:"source"`
+	// Directory where the generator writes reference pages.
+	DestinationDirectory string `yaml:"destination"`
+}
+
+type GenerationError struct {
+	messages []error
+}
+
+func (g GenerationError) Error() string {
+	// Begin with a newline to format the first list item below the outer
+	// error.
+	final := "\n"
+	for _, e := range g.messages {
+		final += fmt.Sprintf("- %v\n", e)
+	}
+	return final
+}
+
+// Generate uses the provided user-facing configuration to write the resource
+// reference to fs.
+func Generate(conf GeneratorConfig, tmpl *template.Template) error {
+	sourceData, err := resource.NewSourceData(conf.SourcePath)
+	if err != nil {
+		return fmt.Errorf("loading Go source files: %w", err)
+	}
+
+	var errs GenerationError
+	for _, r := range conf.Resources {
+		k := resource.PackageInfo{
+			DeclName:    r.TypeName,
+			PackageName: r.PackageName,
+		}
+
+		decl, ok := sourceData.TypeDecls[k]
+		if !ok {
+			errs.messages = append(errs.messages, fmt.Errorf("creating a reference entry for declaration %v.%v: cannot find a declaration of this resource type", k.PackageName, k.DeclName))
+			continue
+		}
+
+		pc := pageContent{}
+
+		// decl is a dynamic resource type, so get data for the type and
+		// its dependencies.
+		entries, err := resource.ReferenceDataFromDeclaration(decl, sourceData.TypeDecls)
+		if errors.Is(err, resource.NotAGenDeclError{}) {
+			continue
+		}
+		if err != nil {
+			errs.messages = append(errs.messages, fmt.Errorf("creating a reference entry for declaration %v.%v in file %v: %w", k.PackageName, k.DeclName, decl.FilePath, err))
+		}
+
+		pc.Resource.ReferenceEntry = entries[k]
+		delete(entries, k)
+		pc.Fields = entries
+
+		pc.Resource.Kind = r.KindValue
+		pc.Resource.Version = r.VersionValue
+
+		filename := strings.ReplaceAll(strings.ToLower(pc.Resource.SectionName), " ", "-")
+		docpath := filepath.Join(conf.DestinationDirectory, filename+".mdx")
+		doc, err := os.Create(docpath)
+		if err != nil {
+			errs.messages = append(errs.messages, fmt.Errorf("cannot create page at %v: %w", docpath, err))
+			continue
+		}
+		defer doc.Close()
+
+		if err := tmpl.Execute(doc, pc); err != nil {
+			errs.messages = append(errs.messages, fmt.Errorf("cannot populate the resource reference template: %w", err))
+		}
+	}
+	if len(errs.messages) > 0 {
+		return errs
+	}
+
+	return nil
+}
