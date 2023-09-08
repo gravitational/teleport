92fe1d716dad535c30d49900c488a0353b54d2c8
Paul Gottschling <paul.gottschling@goteleport.com>
2023-09-08 12:02:49 -0400

    Add go mod and resource type

---
+++ docs-generators/resource-reference/resource/resource.go
@@ -0,0 +1,15 @@
+package resource
+
+type Resource struct {
+	SectionName string
+	Description string
+	SourcePath  string
+	Fields      []Field
+	YAMLExample string
+}
+
+type Field struct {
+	Name        string
+	Description string
+	Type        string
+}
---
+++ docs-generators/resource-reference/resource/resource_test.go
@@ -0,0 +1,12 @@
+package resource
+
+import "testing"
+
+func TestGenerate(t *testing.T) {
+	cases := []struct {
+		description string
+		decl        string
+		expected    Resource
+	}
+	// t.Fatal("not implemented")
+}
