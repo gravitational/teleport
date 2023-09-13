d8ba7a1a67b05bee21b2c54d18be265ac1541dc4
Michael <michael.myers@goteleport.com>
2023-09-13 17:31:16 -0500

    Update okta app resource card (#31833)

--- web/packages/teleport/src/UnifiedResources/ResourceCard.tsx
+++ web/packages/teleport/src/UnifiedResources/ResourceCard.tsx
@@ -319,6 +319,9 @@ function CopyButton({ name }: { name: string }) {
 }
 
 function resourceName(resource: UnifiedResource) {
+  if (resource.kind === 'app' && resource.friendlyName) {
+    return resource.friendlyName;
+  }
   return resource.kind === 'node' ? resource.hostname : resource.name;
 }
 
@@ -326,8 +329,8 @@ function resourceDescription(resource: UnifiedResource) {
   switch (resource.kind) {
     case 'app':
       return {
-        primary: resource.addrWithProtocol,
-        secondary: resource.description,
+        primary: resource.description,
+        secondary: resource.addrWithProtocol,
       };
     case 'db':
       return { primary: resource.type, secondary: resource.description };
