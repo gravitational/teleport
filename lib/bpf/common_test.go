6be712c279a1659888475e4ea6d3d4740cdb52cb
Alan Parra <alan.parra@goteleport.com>
2023-09-15 14:47:58 -0300

    Refactor TestBPFConfig_CheckAndSetDefaults

--- lib/bpf/common_test.go
+++ lib/bpf/common_test.go
@@ -19,6 +19,7 @@ package bpf
 import (
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/require"
 
 	"github.com/gravitational/teleport/lib/defaults"
@@ -28,48 +29,50 @@ import (
 // TestCheckAndSetDefaults makes sure defaults are set when the user does not
 // provide values for the page sizes and hard coded values (like zero or a
 // specific page size) are respected when given.
-func TestCheckAndSetDefaults(t *testing.T) {
-	var perfBufferPageCount = defaults.PerfBufferPageCount
-	var openPerfBufferPageCount = defaults.OpenPerfBufferPageCount
-	var zeroPageCount = 0
+func TestBPFConfig_CheckAndSetDefaults(t *testing.T) {
+	perfBufferPageCount := defaults.PerfBufferPageCount
+	openPerfBufferPageCount := defaults.OpenPerfBufferPageCount
+	zeroPageCount := 0
 
 	var tests = []struct {
-		inConfig  *servicecfg.BPFConfig
-		outConfig *servicecfg.BPFConfig
+		name string
+		got  *servicecfg.BPFConfig
+		want *servicecfg.BPFConfig
 	}{
-		// Empty values get defaults.
 		{
-			inConfig: &servicecfg.BPFConfig{
-				CommandBufferSize: nil,
-				DiskBufferSize:    nil,
-				NetworkBufferSize: nil,
-			},
-			outConfig: &servicecfg.BPFConfig{
+			name: "all defaults",
+			got:  &servicecfg.BPFConfig{},
+			want: &servicecfg.BPFConfig{
 				CommandBufferSize: &perfBufferPageCount,
 				DiskBufferSize:    &openPerfBufferPageCount,
 				NetworkBufferSize: &perfBufferPageCount,
+				CgroupPath:        defaults.CgroupPath,
 			},
 		},
-		// Values are not wiped out with defaults.
 		{
-			inConfig: &servicecfg.BPFConfig{
+			name: "values set",
+			got: &servicecfg.BPFConfig{
 				CommandBufferSize: &zeroPageCount,
 				DiskBufferSize:    &zeroPageCount,
 				NetworkBufferSize: &perfBufferPageCount,
+				CgroupPath:        "/my/cgroup/",
 			},
-			outConfig: &servicecfg.BPFConfig{
+			want: &servicecfg.BPFConfig{
 				CommandBufferSize: &zeroPageCount,
 				DiskBufferSize:    &zeroPageCount,
 				NetworkBufferSize: &perfBufferPageCount,
+				CgroupPath:        "/my/cgroup/",
 			},
 		},
 	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := test.got.CheckAndSetDefaults()
+			require.NoError(t, err, "CheckAndSetDefaults errored")
 
-	for _, tt := range tests {
-		err := tt.inConfig.CheckAndSetDefaults()
-		require.NoError(t, err)
-		require.Equal(t, *tt.outConfig.CommandBufferSize, *tt.inConfig.CommandBufferSize)
-		require.Equal(t, *tt.outConfig.DiskBufferSize, *tt.inConfig.DiskBufferSize)
-		require.Equal(t, *tt.outConfig.NetworkBufferSize, *tt.inConfig.NetworkBufferSize)
+			if diff := cmp.Diff(test.want, test.got); diff != "" {
+				t.Errorf("CheckAndSetDefaults mismatch (-want +got)\n%s", diff)
+			}
+		})
 	}
 }
