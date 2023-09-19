5645724c71d2a91b4555e99226665a56038eaf53
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2023-09-19 01:19:59 -0400

    Fix test

--- lib/services/role_test.go
+++ lib/services/role_test.go
@@ -7432,6 +7432,10 @@ func (u mockCurrentUser) GetTraits() map[string][]string {
 	return u.traits
 }
 
+func (u mockCurrentUser) GetName() string {
+	return "mockCurrentUser"
+}
+
 func TestNewAccessCheckerForRemoteCluster(t *testing.T) {
 	user := mockCurrentUser{
 		roles: []string{"dev", "admin"},
