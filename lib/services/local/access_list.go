ae7e286a284c991e33f9325ab618af13ddaa65ab
Mike Wilson <michael.wilson@goteleport.com>
2023-09-25 09:06:56 -0400

    Use map struct, run GCI.

--- lib/services/local/access_list.go
+++ lib/services/local/access_list.go
@@ -112,12 +112,12 @@ func (a *AccessListService) GetAccessList(ctx context.Context, name string) (*ac
 // UpsertAccessList creates or updates an access list resource.
 func (a *AccessListService) UpsertAccessList(ctx context.Context, accessList *accesslist.AccessList) (*accesslist.AccessList, error) {
 	err := a.service.RunWhileLocked(ctx, lockName(accessList.GetName()), accessListLockTTL, func(ctx context.Context, _ backend.Backend) error {
-		ownerMap := map[string]bool{}
+		ownerMap := make(map[string]struct{}, len(accessList.Spec.Owners))
 		for _, owner := range accessList.Spec.Owners {
-			if ownerMap[owner.Name] {
+			if _, ok := ownerMap[owner.Name]; ok {
 				return trace.AlreadyExists("owner %s already exists in the owner list", owner.Name)
 			}
-			ownerMap[owner.Name] = true
+			ownerMap[owner.Name] = struct{}{}
 		}
 		return trace.Wrap(a.service.UpsertResource(ctx, accessList))
 	})
