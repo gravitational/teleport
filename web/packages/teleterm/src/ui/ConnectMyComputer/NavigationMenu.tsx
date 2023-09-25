78c25fef7616f8df69b62f2cea54291ff90d257b
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2023-09-25 15:20:18 +0200

    Show warning icon instead of read dot as the status indicator

--- web/packages/teleterm/src/ui/ConnectMyComputer/NavigationMenu.tsx
+++ web/packages/teleterm/src/ui/ConnectMyComputer/NavigationMenu.tsx
@@ -186,14 +186,18 @@ export const MenuIcon = forwardRef<HTMLDivElement, MenuIconProps>(
         title="Open Connect My Computer"
       >
         <Laptop size="medium" />
-        {indicatorStatusToStyledStatus(props.indicatorStatus)}
+        {props.indicatorStatus === 'error' ? (
+          <StyledWarning />
+        ) : (
+          indicatorStatusToStyledStatus(props.indicatorStatus)
+        )}
       </StyledButton>
     );
   }
 );
 
 const indicatorStatusToStyledStatus = (
-  indicatorStatus: IndicatorStatus
+  indicatorStatus: '' | 'processing' | 'success'
 ): JSX.Element => {
   return (
     <StyledStatus
@@ -224,13 +228,14 @@ const indicatorStatusToStyledStatus = (
   );
 };
 
-function getIndicatorColor(status: IndicatorStatus, theme: any): string {
+function getIndicatorColor(
+  status: 'processing' | 'success',
+  theme: any
+): string {
   switch (status) {
     case 'processing':
     case 'success':
       return theme.colors.success;
-    case 'error':
-      return theme.colors.error.main;
   }
 }
 
@@ -251,4 +256,19 @@ const StyledStatus = styled(Box)`
   height: 8px;
   border-radius: 50%;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+const StyledWarning = styled(Warning).attrs({
+  size: 'small',
+  color: 'error.main',
+})`
+  position: absolute;
+  top: -6px;
+  right: -6px;
+  z-index: 1;
+
+  > svg {
+    width: 14px;
+    height: 14px;
+  }
 `;
