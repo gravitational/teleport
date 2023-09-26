9e9d929f0126ace6864d0deec760cbe1fcb2068f
Mike Wilson <michael.wilson@goteleport.com>
2023-09-26 10:34:32 -0400

    Add the internal access list review resource.
    The internal access list review resource has been added.

---
+++ api/types/accesslist/convert/v1/review.go
@@ -0,0 +1,113 @@
+/*
+Copyright 2023 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	"time"
+
+	"github.com/gravitational/trace"
+	"google.golang.org/protobuf/types/known/durationpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	accesslistv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/accesslist/v1"
+	"github.com/gravitational/teleport/api/types/accesslist"
+	headerv1 "github.com/gravitational/teleport/api/types/header/convert/v1"
+	traitv1 "github.com/gravitational/teleport/api/types/trait/convert/v1"
+)
+
+// FromReviewProto converts a v1 access list review into an internal access list review object.
+func FromReviewProto(msg *accesslistv1.Review) (*accesslist.Review, error) {
+	if msg == nil {
+		return nil, trace.BadParameter("access list review message is nil")
+	}
+
+	if msg.Spec == nil {
+		return nil, trace.BadParameter("spec is missing")
+	}
+
+	// Manually check for the presence of the time so that we can be sure that the review date is
+	// zero if the proto message's review date is nil.
+	var reviewDate time.Time
+	if msg.Spec.ReviewDate != nil {
+		reviewDate = msg.Spec.ReviewDate.AsTime()
+	}
+
+	var reviewChanges accesslist.ReviewChanges
+	if msg.Spec.Changes != nil {
+		if msg.Spec.Changes.FrequencyChanged != nil {
+			reviewChanges.FrequencyChanged = msg.Spec.Changes.FrequencyChanged.AsDuration()
+		}
+		if msg.Spec.Changes.MembershipRequirementsChanged != nil {
+			reviewChanges.MembershipRequirementsChanged = &accesslist.Requires{
+				Roles:  msg.Spec.Changes.MembershipRequirementsChanged.Roles,
+				Traits: traitv1.FromProto(msg.Spec.Changes.MembershipRequirementsChanged.Traits),
+			}
+		}
+		reviewChanges.RemovedMembers = msg.Spec.Changes.RemovedMembers
+	}
+
+	member, err := accesslist.NewReview(headerv1.FromMetadataProto(msg.Header.Metadata), accesslist.ReviewSpec{
+		AccessList: msg.Spec.AccessList,
+		Reviewers:  msg.Spec.Reviewers,
+		ReviewDate: reviewDate,
+		Notes:      msg.Spec.Notes,
+		Changes:    reviewChanges,
+	})
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	return member, nil
+}
+
+// ToReviewProto converts an internal access list review into a v1 access list review object.
+func ToReviewProto(review *accesslist.Review) *accesslistv1.Review {
+	var reviewChanges *accesslistv1.ReviewChanges
+	if review.Spec.Changes.FrequencyChanged > 0 {
+		reviewChanges = &accesslistv1.ReviewChanges{
+			FrequencyChanged: durationpb.New(review.Spec.Changes.FrequencyChanged),
+		}
+	}
+	if review.Spec.Changes.MembershipRequirementsChanged != nil {
+		if reviewChanges == nil {
+			reviewChanges = &accesslistv1.ReviewChanges{}
+		}
+
+		reviewChanges.MembershipRequirementsChanged = &accesslistv1.AccessListRequires{
+			Roles:  review.Spec.Changes.MembershipRequirementsChanged.Roles,
+			Traits: traitv1.ToProto(review.Spec.Changes.MembershipRequirementsChanged.Traits),
+		}
+	}
+	if len(review.Spec.Changes.RemovedMembers) > 0 {
+		if reviewChanges == nil {
+			reviewChanges = &accesslistv1.ReviewChanges{}
+		}
+
+		reviewChanges.RemovedMembers = review.Spec.Changes.RemovedMembers
+	}
+
+	return &accesslistv1.Review{
+		Header: headerv1.ToResourceHeaderProto(review.ResourceHeader),
+		Spec: &accesslistv1.ReviewSpec{
+			AccessList: review.Spec.AccessList,
+			Reviewers:  review.Spec.Reviewers,
+			ReviewDate: timestamppb.New(review.Spec.ReviewDate),
+			Notes:      review.Spec.Notes,
+			Changes:    reviewChanges,
+		},
+	}
+}
---
+++ api/types/accesslist/review.go
@@ -0,0 +1,111 @@
+/*
+Copyright 2023 Gravitational, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package accesslist
+
+import (
+	"time"
+
+	"github.com/gravitational/trace"
+
+	"github.com/gravitational/teleport/api/types"
+	"github.com/gravitational/teleport/api/types/header"
+	"github.com/gravitational/teleport/api/types/header/convert/legacy"
+)
+
+// Review is an access list review resource.
+type Review struct {
+	// ResourceHeader is the common resource header for all resources.
+	header.ResourceHeader
+
+	// Spec is the specification for the access list review.
+	Spec ReviewSpec `json:"spec" yaml:"spec"`
+}
+
+// ReviewSpec describes the specification of a review of an access list.
+type ReviewSpec struct {
+	// AccessList is the name of the associated access list.
+	AccessList string `json:"access_list" yaml:"access_list"`
+
+	// Reviewers are the users who performed the review.
+	Reviewers []string `json:"reviewers" yaml:"reviewers"`
+
+	// ReviewDate is the date that this review was created.
+	ReviewDate time.Time `json:"joined" yaml:"joined"`
+
+	// Notes is an optional plaintext attached to the review that can be used by the review for arbitrary
+	// note taking on the review.
+	Notes string `json:"notes" yaml:"notes"`
+
+	// Changes are the changes made as part of the review.
+	Changes ReviewChanges `json:"changes" yaml:"changes"`
+}
+
+// ReviewChanges are the changes that were made as part of the review.
+type ReviewChanges struct {
+	// FrequencyChanged  is populated if the audit frequency was changed.
+	FrequencyChanged time.Duration `json:"frequency_changed" yaml:"frequency_changed"`
+
+	// MembershipRequirementsChanged is populated if the requirements were changed as part of this review.
+	MembershipRequirementsChanged *Requires `json:"membership_requirements_changed" yaml:"membership_requirements_changed"`
+
+	// RemovedMembers contains the members that were removed as part of this review.
+	RemovedMembers []string `json:"removed_members" yaml:"removed_members"`
+}
+
+// NewReview will create a new access list review.
+func NewReview(metadata header.Metadata, spec ReviewSpec) (*Review, error) {
+	member := &Review{
+		ResourceHeader: header.ResourceHeaderFromMetadata(metadata),
+		Spec:           spec,
+	}
+
+	if err := member.CheckAndSetDefaults(); err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	return member, nil
+}
+
+// CheckAndSetDefaults validates fields and populates empty fields with default values.
+func (r *Review) CheckAndSetDefaults() error {
+	r.SetKind(types.KindAccessListReview)
+	r.SetVersion(types.V1)
+
+	if err := r.ResourceHeader.CheckAndSetDefaults(); err != nil {
+		return trace.Wrap(err)
+	}
+
+	if r.Spec.AccessList == "" {
+		return trace.BadParameter("access list is missing")
+	}
+
+	if len(r.Spec.Reviewers) == 0 {
+		return trace.BadParameter("reviewers are missing")
+	}
+
+	if r.Spec.ReviewDate.IsZero() {
+		return trace.BadParameter("review date is missing")
+	}
+
+	return nil
+}
+
+// GetMetadata returns metadata. This is specifically for conforming to the Resource interface,
+// and should be removed when possible.
+func (r *Review) GetMetadata() types.Metadata {
+	return legacy.FromHeaderMetadata(r.Metadata)
+}
