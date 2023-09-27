4f4e13d622483c05f5b373c2b8de0b236a28fcb2
Mike Wilson <michael.wilson@goteleport.com>
2023-09-27 08:29:12 -0400

    Use ExactKey, fix accessListMemberParser as well.

--- lib/services/local/events.go
+++ lib/services/local/events.go
@@ -1637,7 +1637,7 @@ func (p *headlessAuthenticationParser) parse(event backend.Event) (types.Resourc
 
 func newAccessListParser() *accessListParser {
 	return &accessListParser{
-		baseParser: newBaseParser(backend.Key(accessListPrefix, "")),
+		baseParser: newBaseParser(backend.ExactKey(accessListPrefix)),
 	}
 }
 
@@ -1687,7 +1687,7 @@ func (p *userLoginStateParser) parse(event backend.Event) (types.Resource, error
 
 func newAccessListMemberParser() *accessListMemberParser {
 	return &accessListMemberParser{
-		baseParser: newBaseParser(backend.Key(accessListMemberPrefix)),
+		baseParser: newBaseParser(backend.ExactKey(accessListMemberPrefix)),
 	}
 }
 
