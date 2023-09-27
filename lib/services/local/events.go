59f49191e7081a0755519dca50c0c1bcba3ce2a8
Michael Wilson <mike@mdwn.dev>
2023-09-27 09:04:52 -0400

    Fix Access List Members cache and eventing.
    * Fix Access List Members cache and eventing.
    
    Two things were happening that were shadowing the Access List members cache
    and eventing.
    
    1. In the cache collections, the wrong reader was being assigned to the
       lookup map. The correct reader was being used elsewhere, however, so the
       caching tests appear to have still been working.
    2. The watcher in lib/services/local/events.go apparently collapses prefixes
       if they overlap. Prefix `access_list_members` is encompassed by
       `access_list`, so the access list members prefix was eliminated from the
       watcher. As a result, access list member events were being processed by
       the access list parser, which resulted in non-critical warnings.
    
    Local testing and dogfooding has yielded that this has had no apparent impact,
    at least in situations without cache propagation. However, I've got a feeling
    that this could affect situations with multiple auth servers.
    
    While I'm here, I've eliminated the pointer-to-pointer logic in the access
    list unmarshaling, which was excised elsewhere and should be excised here as
    well.

--- lib/services/local/events.go
+++ lib/services/local/events.go
@@ -1568,7 +1568,7 @@ func (p *integrationParser) parse(event backend.Event) (types.Resource, error) {
 
 func newAccessListParser() *accessListParser {
 	return &accessListParser{
-		baseParser: newBaseParser(backend.Key(accessListPrefix)),
+		baseParser: newBaseParser(backend.ExactKey(accessListPrefix)),
 	}
 }
 
@@ -1651,7 +1651,7 @@ func (p *userLoginStateParser) parse(event backend.Event) (types.Resource, error
 
 func newAccessListMemberParser() *accessListMemberParser {
 	return &accessListMemberParser{
-		baseParser: newBaseParser(backend.Key(accessListMemberPrefix)),
+		baseParser: newBaseParser(backend.ExactKey(accessListMemberPrefix)),
 	}
 }
 
