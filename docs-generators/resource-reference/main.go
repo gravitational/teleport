3763b1d85d2706700eeec4550d5d2d7f024aa170
Paul Gottschling <paul.gottschling@goteleport.com>
2023-09-27 09:37:52 -0400

    Use a config file

--- docs-generators/resource-reference/main.go
+++ docs-generators/resource-reference/main.go
@@ -5,20 +5,58 @@ import (
 	"fmt"
 	"gen-resource-ref/reference"
 	"os"
+
+	"gopkg.in/yaml.v3"
 )
 
+const help string = `the path to a YAML configuration file with the following fields:
+
+## Main config
+
+required_types (string): a list of type info mappings (see "Type info")
+source (string): the path to the root of a Go project directory
+destination (string): the file path of the resource reference
+
+## Type info
+
+package: The path of a Go package
+name: The name of a type within the package
+
+## Example
+
+required_types:
+  - name: Metadata
+    package: api/types
+  - name: ResourceHeader
+    package: api/types
+source: "api"
+destination: "docs/pages/includes/resource-reference.mdx"
+`
+
 func main() {
-	src := flag.String("source", ".", "the project directory in which to parse Go packages")
-	out := flag.String("out", ".", "the path where the generator will write the resource reference")
+	conf := flag.String("config", "./conf.yaml", help)
 	flag.Parse()
 
-	outfile, err := os.OpenFile(*out, os.O_WRONLY|os.O_CREATE, 0644)
+	conffile, err := os.Open(*conf)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not open the configuration file %v: %v", *conf, err)
+		os.Exit(1)
+	}
+
+	genconf := reference.GeneratorConfig{}
+
+	if err = yaml.NewDecoder(conffile).Decode(&genconf); err != nil {
+		fmt.Fprintf(os.Stderr, "could not parse the configuration file: %v", err)
+		os.Exit(1)
+	}
+
+	outfile, err := os.OpenFile(genconf.DestinationPath, os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "could not prepare the output file for writing: %v", err)
 		os.Exit(1)
 	}
 
-	err = reference.Generate(outfile, *src)
+	err = reference.Generate(outfile, genconf)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "could not generate the resource reference: %v", err)
 		os.Exit(1)
