dfe75efa6b7c8f11d981e91a079d10a78b7cb69c
Tobiasz Heller <tobiasz.heller@goteleport.com>
2023-10-02 10:48:54 +0200

    Add ClusterExternalAudit to event.proto

--- api/gen/proto/go/teleport/externalaudit/v1/externalaudit.pb.go
+++ api/gen/proto/go/teleport/externalaudit/v1/externalaudit.pb.go
@@ -202,6 +202,114 @@ func (x *ExternalAuditSpec) GetAthenaResultsUri() string {
 	return ""
 }
 
+// ClusterExternalAudit contains cluster external audit configuration.
+type ClusterExternalAudit struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// Header is the header for the resource.
+	Header *v1.ResourceHeader `protobuf:"bytes,1,opt,name=header,proto3" json:"header,omitempty"`
+	// Spec is the specification for the cluster external audit.
+	Spec *ClusterExternalAuditSpec `protobuf:"bytes,2,opt,name=spec,proto3" json:"spec,omitempty"`
+}
+
+func (x *ClusterExternalAudit) Reset() {
+	*x = ClusterExternalAudit{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_teleport_externalaudit_v1_externalaudit_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *ClusterExternalAudit) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ClusterExternalAudit) ProtoMessage() {}
+
+func (x *ClusterExternalAudit) ProtoReflect() protoreflect.Message {
+	mi := &file_teleport_externalaudit_v1_externalaudit_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ClusterExternalAudit.ProtoReflect.Descriptor instead.
+func (*ClusterExternalAudit) Descriptor() ([]byte, []int) {
+	return file_teleport_externalaudit_v1_externalaudit_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *ClusterExternalAudit) GetHeader() *v1.ResourceHeader {
+	if x != nil {
+		return x.Header
+	}
+	return nil
+}
+
+func (x *ClusterExternalAudit) GetSpec() *ClusterExternalAuditSpec {
+	if x != nil {
+		return x.Spec
+	}
+	return nil
+}
+
+// ClusterExternalAuditSpec is the specification of cluster external audit.
+type ClusterExternalAuditSpec struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// ExternalAuditName is name of existing external audit resource,
+	// that will be used in cluster.
+	ExternalAuditName string `protobuf:"bytes,1,opt,name=external_audit_name,json=externalAuditName,proto3" json:"external_audit_name,omitempty"`
+}
+
+func (x *ClusterExternalAuditSpec) Reset() {
+	*x = ClusterExternalAuditSpec{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_teleport_externalaudit_v1_externalaudit_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *ClusterExternalAuditSpec) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ClusterExternalAuditSpec) ProtoMessage() {}
+
+func (x *ClusterExternalAuditSpec) ProtoReflect() protoreflect.Message {
+	mi := &file_teleport_externalaudit_v1_externalaudit_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ClusterExternalAuditSpec.ProtoReflect.Descriptor instead.
+func (*ClusterExternalAuditSpec) Descriptor() ([]byte, []int) {
+	return file_teleport_externalaudit_v1_externalaudit_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *ClusterExternalAuditSpec) GetExternalAuditName() string {
+	if x != nil {
+		return x.ExternalAuditName
+	}
+	return ""
+}
+
 var File_teleport_externalaudit_v1_externalaudit_proto protoreflect.FileDescriptor
 
 var file_teleport_externalaudit_v1_externalaudit_proto_rawDesc = []byte{
@@ -242,13 +350,28 @@ var file_teleport_externalaudit_v1_externalaudit_proto_rawDesc = []byte{
 	0x54, 0x65, 0x72, 0x6d, 0x55, 0x72, 0x69, 0x12, 0x2c, 0x0a, 0x12, 0x61, 0x74, 0x68, 0x65, 0x6e,
 	0x61, 0x5f, 0x72, 0x65, 0x73, 0x75, 0x6c, 0x74, 0x73, 0x5f, 0x75, 0x72, 0x69, 0x18, 0x07, 0x20,
 	0x01, 0x28, 0x09, 0x52, 0x10, 0x61, 0x74, 0x68, 0x65, 0x6e, 0x61, 0x52, 0x65, 0x73, 0x75, 0x6c,
-	0x74, 0x73, 0x55, 0x72, 0x69, 0x42, 0x5e, 0x5a, 0x5c, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e,
-	0x63, 0x6f, 0x6d, 0x2f, 0x67, 0x72, 0x61, 0x76, 0x69, 0x74, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x61,
-	0x6c, 0x2f, 0x74, 0x65, 0x6c, 0x65, 0x70, 0x6f, 0x72, 0x74, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x67,
-	0x65, 0x6e, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x67, 0x6f, 0x2f, 0x74, 0x65, 0x6c, 0x65,
-	0x70, 0x6f, 0x72, 0x74, 0x2f, 0x65, 0x78, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x61, 0x75, 0x64,
-	0x69, 0x74, 0x2f, 0x76, 0x31, 0x3b, 0x65, 0x78, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x61, 0x75,
-	0x64, 0x69, 0x74, 0x76, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x74, 0x73, 0x55, 0x72, 0x69, 0x22, 0x9b, 0x01, 0x0a, 0x14, 0x43, 0x6c, 0x75, 0x73, 0x74, 0x65,
+	0x72, 0x45, 0x78, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x41, 0x75, 0x64, 0x69, 0x74, 0x12, 0x3a,
+	0x0a, 0x06, 0x68, 0x65, 0x61, 0x64, 0x65, 0x72, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x22,
+	0x2e, 0x74, 0x65, 0x6c, 0x65, 0x70, 0x6f, 0x72, 0x74, 0x2e, 0x68, 0x65, 0x61, 0x64, 0x65, 0x72,
+	0x2e, 0x76, 0x31, 0x2e, 0x52, 0x65, 0x73, 0x6f, 0x75, 0x72, 0x63, 0x65, 0x48, 0x65, 0x61, 0x64,
+	0x65, 0x72, 0x52, 0x06, 0x68, 0x65, 0x61, 0x64, 0x65, 0x72, 0x12, 0x47, 0x0a, 0x04, 0x73, 0x70,
+	0x65, 0x63, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x33, 0x2e, 0x74, 0x65, 0x6c, 0x65, 0x70,
+	0x6f, 0x72, 0x74, 0x2e, 0x65, 0x78, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x61, 0x75, 0x64, 0x69,
+	0x74, 0x2e, 0x76, 0x31, 0x2e, 0x43, 0x6c, 0x75, 0x73, 0x74, 0x65, 0x72, 0x45, 0x78, 0x74, 0x65,
+	0x72, 0x6e, 0x61, 0x6c, 0x41, 0x75, 0x64, 0x69, 0x74, 0x53, 0x70, 0x65, 0x63, 0x52, 0x04, 0x73,
+	0x70, 0x65, 0x63, 0x22, 0x4a, 0x0a, 0x18, 0x43, 0x6c, 0x75, 0x73, 0x74, 0x65, 0x72, 0x45, 0x78,
+	0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x41, 0x75, 0x64, 0x69, 0x74, 0x53, 0x70, 0x65, 0x63, 0x12,
+	0x2e, 0x0a, 0x13, 0x65, 0x78, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x5f, 0x61, 0x75, 0x64, 0x69,
+	0x74, 0x5f, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x11, 0x65, 0x78,
+	0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x41, 0x75, 0x64, 0x69, 0x74, 0x4e, 0x61, 0x6d, 0x65, 0x42,
+	0x5e, 0x5a, 0x5c, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x67, 0x72,
+	0x61, 0x76, 0x69, 0x74, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x61, 0x6c, 0x2f, 0x74, 0x65, 0x6c, 0x65,
+	0x70, 0x6f, 0x72, 0x74, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x67, 0x65, 0x6e, 0x2f, 0x70, 0x72, 0x6f,
+	0x74, 0x6f, 0x2f, 0x67, 0x6f, 0x2f, 0x74, 0x65, 0x6c, 0x65, 0x70, 0x6f, 0x72, 0x74, 0x2f, 0x65,
+	0x78, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x61, 0x75, 0x64, 0x69, 0x74, 0x2f, 0x76, 0x31, 0x3b,
+	0x65, 0x78, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x61, 0x75, 0x64, 0x69, 0x74, 0x76, 0x31, 0x62,
+	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -263,20 +386,24 @@ func file_teleport_externalaudit_v1_externalaudit_proto_rawDescGZIP() []byte {
 	return file_teleport_externalaudit_v1_externalaudit_proto_rawDescData
 }
 
-var file_teleport_externalaudit_v1_externalaudit_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_teleport_externalaudit_v1_externalaudit_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_teleport_externalaudit_v1_externalaudit_proto_goTypes = []interface{}{
-	(*ExternalAudit)(nil),     // 0: teleport.externalaudit.v1.ExternalAudit
-	(*ExternalAuditSpec)(nil), // 1: teleport.externalaudit.v1.ExternalAuditSpec
-	(*v1.ResourceHeader)(nil), // 2: teleport.header.v1.ResourceHeader
+	(*ExternalAudit)(nil),            // 0: teleport.externalaudit.v1.ExternalAudit
+	(*ExternalAuditSpec)(nil),        // 1: teleport.externalaudit.v1.ExternalAuditSpec
+	(*ClusterExternalAudit)(nil),     // 2: teleport.externalaudit.v1.ClusterExternalAudit
+	(*ClusterExternalAuditSpec)(nil), // 3: teleport.externalaudit.v1.ClusterExternalAuditSpec
+	(*v1.ResourceHeader)(nil),        // 4: teleport.header.v1.ResourceHeader
 }
 var file_teleport_externalaudit_v1_externalaudit_proto_depIdxs = []int32{
-	2, // 0: teleport.externalaudit.v1.ExternalAudit.header:type_name -> teleport.header.v1.ResourceHeader
+	4, // 0: teleport.externalaudit.v1.ExternalAudit.header:type_name -> teleport.header.v1.ResourceHeader
 	1, // 1: teleport.externalaudit.v1.ExternalAudit.spec:type_name -> teleport.externalaudit.v1.ExternalAuditSpec
-	2, // [2:2] is the sub-list for method output_type
-	2, // [2:2] is the sub-list for method input_type
-	2, // [2:2] is the sub-list for extension type_name
-	2, // [2:2] is the sub-list for extension extendee
-	0, // [0:2] is the sub-list for field type_name
+	4, // 2: teleport.externalaudit.v1.ClusterExternalAudit.header:type_name -> teleport.header.v1.ResourceHeader
+	3, // 3: teleport.externalaudit.v1.ClusterExternalAudit.spec:type_name -> teleport.externalaudit.v1.ClusterExternalAuditSpec
+	4, // [4:4] is the sub-list for method output_type
+	4, // [4:4] is the sub-list for method input_type
+	4, // [4:4] is the sub-list for extension type_name
+	4, // [4:4] is the sub-list for extension extendee
+	0, // [0:4] is the sub-list for field type_name
 }
 
 func init() { file_teleport_externalaudit_v1_externalaudit_proto_init() }
@@ -309,6 +436,30 @@ func file_teleport_externalaudit_v1_externalaudit_proto_init() {
 				return nil
 			}
 		}
+		file_teleport_externalaudit_v1_externalaudit_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*ClusterExternalAudit); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_teleport_externalaudit_v1_externalaudit_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*ClusterExternalAuditSpec); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -316,7 +467,7 @@ func file_teleport_externalaudit_v1_externalaudit_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_teleport_externalaudit_v1_externalaudit_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
