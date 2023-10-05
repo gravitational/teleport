f21336e04fb2d39ca69b5163e29f20099ebd84d5
greedy52 <xin.huang@goteleport.com>
2023-10-05 20:01:06 +0000

    revoke all-in-one role on deactivation

--- lib/srv/db/mysql/mariadb_revoke_roles.sql
+++ lib/srv/db/mysql/mariadb_revoke_roles.sql
@@ -1,19 +1,25 @@
 CREATE PROCEDURE teleport_revoke_roles(IN username VARCHAR(80))
 BEGIN
+    DECLARE cur_user CHAR(128);
     DECLARE cur_role CHAR(128);
     DECLARE done INT DEFAULT FALSE;
-    DECLARE role_cursor CURSOR FOR SELECT Role FROM mysql.roles_mapping WHERE User = CONCAT("tp-role-", username) AND Admin_option = 'N';
+    -- Revoke all roles assigned to the all-in-one role, and all roles assigned
+    -- to the username (expect 'teleport-auto-user')
+    DECLARE role_cursor CURSOR FOR
+        (SELECT User,Role FROM mysql.roles_mapping WHERE User = CONCAT("tp-role-", username) AND Admin_option = 'N')
+        UNION
+        (SELECT User,Role FROM mysql.roles_mapping WHERE Role != 'teleport-auto-user' AND User = username AND Admin_option = 'N')
+    ;
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-    SET @all_in_one_role := CONCAT("tp-role-", username);
 
     OPEN role_cursor;
     revoke_roles: LOOP
-        FETCH role_cursor INTO cur_role;
+        FETCH role_cursor INTO cur_user, cur_role;
         IF done THEN
             LEAVE revoke_roles;
         END IF;
 
-        SET @sql := CONCAT_WS(' ', 'REVOKE', QUOTE(cur_role), 'FROM', QUOTE(@all_in_one_role));
+        SET @sql := CONCAT_WS(' ', 'REVOKE', QUOTE(cur_role), 'FROM', QUOTE(cur_user));
         PREPARE stmt FROM @sql;
         EXECUTE stmt;
         DEALLOCATE PREPARE stmt;
