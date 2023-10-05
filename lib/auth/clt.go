e77b45ced1aa5d1978cedc9c155cf4f0b1ec34aa
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2023-10-05 12:43:55 -0400

    Allow breaker tripped error to be configurable (#32869)

--- lib/auth/clt.go
+++ lib/auth/clt.go
@@ -85,6 +85,9 @@ var _ ClientI = &Client{}
 // functionality that hasn't been ported to the new client yet.
 func NewClient(cfg client.Config, params ...roundtrip.ClientParam) (*Client, error) {
 	cfg.DialInBackground = true
+
+	cfg.CircuitBreakerConfig.TrippedErrorMessage = "Unable to communicate with the Teleport Auth Service"
+
 	if err := cfg.CheckAndSetDefaults(); err != nil {
 		return nil, trace.Wrap(err)
 	}
