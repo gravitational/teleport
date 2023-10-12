8afb9d362e55b09159d0e66ecb7457cd02eef5a8
Michael Wilson <mike@mdwn.dev>
2023-10-11 21:07:41 -0400

    Remove access lists and members from the cache. (#33324)
    The access lists and members have been removed from the cache. There's not
    a lot of value having them in there and they create some uncomfortable
    questions since members are dependent on access lists and the cache isn't
    designed for dependent operations like this. By removing them we avoid a
    lot of pain here.

--- lib/service/service.go
+++ lib/service/service.go
@@ -2160,7 +2160,6 @@ func (process *TeleportProcess) newAccessCache(cfg accessCacheConfig) (*cache.Ca
 		SAMLIdPServiceProviders: cfg.services,
 		UserGroups:              cfg.services,
 		Okta:                    cfg.services.OktaClient(),
-		AccessLists:             cfg.services.AccessListClient(),
 		UserLoginStates:         cfg.services.UserLoginStateClient(),
 		Integrations:            cfg.services,
 		WebSession:              cfg.services.WebSessions(),
