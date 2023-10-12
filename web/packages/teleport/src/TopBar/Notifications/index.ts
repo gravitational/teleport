c53dc1d081a273a863e775c890b27da0bb4853e4
Lisa Kim <lisa@goteleport.com>
2023-10-12 10:41:21 -0700

    Web: Add notification store (#32970)
    * Add notification bell to top bar
    
    Also fixes topbar.test act warnings (even though tests passed)
    
    * Add attention dot to navigation switcher
    
    * Add attention dot to navigation item
    
    * Create store notification and enable it
    
    * Address review
    
    - Made store notification type more generic
    - Drop opactiy styling for top bar drop downs
    - Use assertUnreachable in switch cases
    
    * Address CR
    
    * Fix import path and rename file
    
    * Leave a TODO comment

--- web/packages/teleport/src/TopBar/Notifications/index.ts
+++ web/packages/teleport/src/TopBar/Notifications/index.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2022 Gravitational, Inc.
+ * Copyright 2023 Gravitational, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,9 +14,4 @@
  * limitations under the License.
  */
 
-import { assertUnreachable } from 'shared/utils/assertUnreachable';
-
-/**
- * @deprecated Import assertUnreachable from `shared/utils/assertUnreachable` instead.
- */
-export { assertUnreachable };
+export { Notifications } from './Notifications';
