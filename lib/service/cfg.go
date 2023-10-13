f3dc548c3f34ce7a4d1b32e2c95cd11de5d73c4b
Zac Bergquist <zac.bergquist@goteleport.com>
2023-10-13 08:21:51 -0600

    Allow for Windows PKI operations to target a different domain (#33395)
    Today, our AD support largely assumes there is a single active directory
    domain. The certificates that we generate are for users in this domain,
    the computers we discover via LDAP come from this domain, and the PKI
    set up we perform targets this domain.
    
    In more complicated AD configurations, PKI is often configured in a root
    domain, while users, servers, and discovery should be done against a
    child domain.
    
    The new pki_domain configuration field will allow you to override the
    default domain specified in the ldap section with a root domain that is
    used for configuring the NTAuth store and publishing the CRL. Teleport
    continues to do discovery and issue certificates for the domain
    specified in the ldap section of the config.

--- lib/service/cfg.go
+++ lib/service/cfg.go
@@ -1389,6 +1389,13 @@ type WindowsDesktopConfig struct {
 	// LDAP is the LDAP connection parameters.
 	LDAP LDAPConfig
 
+	// PKIDomain optionally configures a separate Active Directory domain
+	// for PKI operations. If empty, the domain from the LDAP config is used.
+	// This can be useful for cases where PKI is configured in a root domain
+	// but Teleport is used to provide access to users and computers in a child
+	// domain.
+	PKIDomain string
+
 	// Discovery configures automatic desktop discovery via LDAP.
 	Discovery LDAPDiscoveryConfig
 
