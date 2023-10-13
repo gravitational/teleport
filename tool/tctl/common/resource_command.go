1b30e31cbdf09dd626abc66f52da645623a3ba0e
Tim Ross <tim.ross@goteleport.com>
2023-10-13 11:48:01 -0400

    Enable optimistic locking support for SAML connectors
    Updates tctl edit and the web ui to start using optimistic locking.
    The functionality to support optimistic locking already existed,
    the APIs used by both clients were updated to use create/update
    instead of upsert so that optimistic locking could be enforced.
    Most of the changes introduced are tests to ensure that tctl edit,
    tctl create behave as expected.
    
    Note: the web ui changes are include in the e ref update.
    
    Contributes to #30416.

--- tool/tctl/common/resource_command.go
+++ tool/tctl/common/resource_command.go
@@ -143,6 +143,7 @@ func (rc *ResourceCommand) Initialize(app *kingpin.Application, config *servicec
 		types.KindUser:            rc.updateUser,
 		types.KindGithubConnector: rc.updateGithubConnector,
 		types.KindOIDCConnector:   rc.updateOIDCConnector,
+		types.KindSAMLConnector:   rc.updateSAMLConnector,
 	}
 	rc.config = config
 
@@ -870,6 +871,20 @@ func (rc *ResourceCommand) createSAMLConnector(ctx context.Context, client auth.
 		return trace.Wrap(err)
 	}
 	fmt.Printf("authentication connector '%s' has been %s\n", connectorName, UpsertVerb(exists, rc.IsForced()))
+	return nil
+}
+
+func (rc *ResourceCommand) updateSAMLConnector(ctx context.Context, client auth.ClientI, raw services.UnknownResource) error {
+	// Create services.SAMLConnector from raw YAML to extract the connector name.
+	conn, err := services.UnmarshalSAMLConnector(raw.Raw)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	if _, err = client.UpdateSAMLConnector(ctx, conn); err != nil {
+		return trace.Wrap(err)
+	}
+	fmt.Printf("authentication connector '%s' has been updated\n", conn.GetName())
 	return nil
 }
 
