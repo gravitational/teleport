63fd5c8b8af0220043eee7e19e905ee317040afd
Brian Joerger <bjoerger@goteleport.com>
2023-10-13 17:25:41 -0700

    Enable hardware key support in the WebUI (#33483)

--- lib/auth/auth_with_roles.go
+++ lib/auth/auth_with_roles.go
@@ -2412,10 +2412,8 @@ func (a *ServerWithRoles) WebSessions() types.WebSessionInterface {
 
 // Get returns the web session specified with req.
 func (r *webSessionsWithRoles) Get(ctx context.Context, req types.GetWebSessionRequest) (types.WebSession, error) {
-	if err := r.c.currentUserAction(req.User); err != nil {
-		if err := r.c.action(apidefaults.Namespace, types.KindWebSession, types.VerbRead); err != nil {
-			return nil, trace.Wrap(err)
-		}
+	if err := r.c.action(apidefaults.Namespace, types.KindWebSession, types.VerbRead); err != nil {
+		return nil, trace.Wrap(err)
 	}
 	return r.ws.Get(ctx, req)
 }
