df2b5031bc2e84b52fff241a408eb8628bc005cd
Brian Joerger <bjoerger@goteleport.com>
2023-10-13 17:31:55 -0700

    Configure custom PIV slot for hardware key support (#31732) (#33352)
    * Update RFD.
    
    * Add custom PIV slot logic.
    
    * Add custom piv slot to cluster auth preference.
    
    * Fix error handling of parsing private key policy errors.
    
    * Add new PIVSlot string type.

--- tool/tsh/common/tsh.go
+++ tool/tsh/common/tsh.go
@@ -484,6 +484,9 @@ type CLIConf struct {
 
 	// LeafClusterName is the optional name of a leaf cluster to connect to instead
 	LeafClusterName string
+
+	// PIVSlot specifies a specific PIV slot to use with hardware key support.
+	PIVSlot string
 }
 
 // Stdout returns the stdout writer.
@@ -692,6 +695,7 @@ func Run(ctx context.Context, args []string, opts ...CliOption) error {
 		Envar(mlockModeEnvVar).
 		StringVar(&cf.MlockMode)
 	app.HelpFlag.Short('h')
+	app.Flag("piv-slot", "Specify a PIV slot key to use for Hardware Key support instead of the default. Ex: \"9d\"").Envar("TELEPORT_PIV_SLOT").StringVar(&cf.PIVSlot)
 
 	ver := app.Command("version", "Print the tsh client and Proxy server versions for the current context.")
 	ver.Flag("format", defaults.FormatFlagDescription(defaults.DefaultFormats...)).Short('f').Default(teleport.Text).EnumVar(&cf.Format, defaults.DefaultFormats...)
@@ -3570,6 +3574,13 @@ func loadClientConfigFromCLIConf(cf *CLIConf, proxy string) (*client.Config, err
 
 	if err := tryLockMemory(cf); err != nil {
 		return nil, trace.Wrap(err)
+	}
+
+	if cf.PIVSlot != "" {
+		c.PIVSlot = keys.PIVSlot(cf.PIVSlot)
+		if err = c.PIVSlot.Validate(); err != nil {
+			return nil, trace.Wrap(err)
+		}
 	}
 
 	c.ClientStore, err = initClientStore(cf, proxy)
