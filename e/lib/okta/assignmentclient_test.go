118423487f012b7acf455e0378603c7fc6ffec29
Trent Clarke <trent@goteleport.com>
2023-10-17 17:10:08 +1100

    Refactored Okta assignment tests (#2338)
    Refactors the Okta assignment client tests. Mainly done as an exercise
    to learn how the existing test client works.
    
    Source-commit: gravitational/teleport.e@863a5f9df2261f5d0d644e10a40e22d60227ed94
    Source-pull-requests: gravitational/teleport.e#2338

--- e/lib/okta/assignmentclient_test.go
+++ e/lib/okta/assignmentclient_test.go
@@ -20,7 +20,6 @@ import (
 	"context"
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
 	"github.com/gravitational/trace"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
@@ -28,120 +27,216 @@ import (
 	"github.com/gravitational/teleport/e/lib/teleport"
 )
 
+// purgeCache clears the assignmentClient caches, forcing the client to reload
+// everything from the upstream Okta service. Used only in tests.
+func (a *assignmentClient) purgeCache() {
+	a.usersMu.Lock()
+	a.users = nil
+	a.usersMu.Unlock()
+
+	a.appsMu.Lock()
+	clear(a.apps)
+	a.appsMu.Unlock()
+
+	a.groupsMu.Lock()
+	clear(a.groups)
+	a.groupsMu.Unlock()
+}
+
 func TestAssignmentClient(t *testing.T) {
 	ctx := context.Background()
-	oktaClient := newTestClient()
 	log := logrus.WithField(trace.Component, teleport.ComponentOkta)
-	assignmentClient := newAssignmentClient(log, oktaClient)
 	testGroup := "test-group"
 	testApp := "test-app"
 	testUser := "test-user@test.user"
 	testOktaUserID := "okta-user-id"
 
-	// No user present.
-	_, err := assignmentClient.userAssignedToApp(ctx, testUser, testApp)
-	require.ErrorIs(t, trace.NotFound("unable to find ID for user %s", testUser), err)
+	// Factory for creating an assignment client backed by a test Okta client
+	// pre-configured with a user and some group and app memberships.
+	testClientWithAssignments := func() (*testOktaClient, *assignmentClient) {
+		oktaClient := newTestClient()
+		oktaClient.usernamesToUserIDs = map[string]string{
+			testUser: testOktaUserID,
+		}
+		oktaClient.appsToUsers = map[string]map[string]bool{
+			testApp: {testOktaUserID: true},
+		}
+		oktaClient.groupsToUsers = map[string]map[string]bool{
+			testGroup: {testOktaUserID: true},
+		}
+		oktaClient.appsToGroups = map[string][]string{
+			testApp: {testGroup},
+		}
+		assignmentClient := newAssignmentClient(log, oktaClient)
 
-	_, err = assignmentClient.userAssignedToGroup(ctx, testUser, testGroup)
-	require.ErrorIs(t, trace.NotFound("unable to find ID for user %s", testUser), err)
+		return oktaClient, assignmentClient
+	}
 
-	oktaClient.addUserID(testUser, testOktaUserID)
+	t.Run("no such user is an error", func(t *testing.T) {
+		// Given an Okta system with no users or groups...
+		assignmentClient := newAssignmentClient(log, newTestClient())
 
-	// Refresh client so that the cache will retry the user.
-	assignmentClient = newAssignmentClient(log, oktaClient)
+		// When I attempt to perform operations that require a given
+		// user to exist, those operations will fail
 
-	// User present, but no apps/groups present.
-	_, err = assignmentClient.userAssignedToApp(ctx, testUser, testApp)
-	require.ErrorIs(t, err, trace.NotFound("assignments for app %s not found", testApp))
+		_, err := assignmentClient.userAssignedToApp(ctx, testUser, testApp)
+		require.ErrorIs(t, trace.NotFound("unable to find ID for user %s", testUser), err)
 
-	_, err = assignmentClient.userAssignedToGroup(ctx, testUser, testGroup)
-	require.ErrorIs(t, err, trace.NotFound("assignments for group %s not found", testGroup))
+		_, err = assignmentClient.userAssignedToGroup(ctx, testUser, testGroup)
+		require.ErrorIs(t, trace.NotFound("unable to find ID for user %s", testUser), err)
+	})
 
-	// Cache didn't populate for the apps/groups due to the failures, so we'll add in the apps/groups now.
-	oktaClient.addApplicationToMapping(testApp)
-	oktaClient.addGroupToMapping(testGroup)
+	t.Run("user with no apps no groups", func(t *testing.T) {
+		// Given an assignmentClient backed by an Okta system with one user and
+		// no apps or groups configured...
+		oktaClient := newTestClient()
+		oktaClient.usernamesToUserIDs = map[string]string{
+			testUser: testOktaUserID,
+		}
+		assignmentClient := newAssignmentClient(log, oktaClient)
 
-	// Refresh client.
-	assignmentClient = newAssignmentClient(log, oktaClient)
+		// When I test that user's group and app memberships, the tests all
+		// return NotFound because the Apps and Groups do not exist .
 
-	// User present, apps/groups present. This shouldn't fail, but the result of this should be cached.
-	ok, err := assignmentClient.userAssignedToApp(ctx, testUser, testApp)
-	require.NoError(t, err)
-	require.False(t, ok)
+		_, err := assignmentClient.userAssignedToApp(ctx, testUser, testApp)
+		require.ErrorIs(t, err, trace.NotFound("assignments for app %s not found", testApp))
 
-	ok, err = assignmentClient.userAssignedToGroup(ctx, testUser, testGroup)
-	require.NoError(t, err)
-	require.False(t, ok)
+		_, err = assignmentClient.userAssignedToGroup(ctx, testUser, testGroup)
+		require.ErrorIs(t, err, trace.NotFound("assignments for group %s not found", testGroup))
+	})
 
-	// Assign users to client, which will be ignored.
-	oktaClient.assignUserToApplication(ctx, testOktaUserID, testApp)
-	oktaClient.assignUserToGroup(ctx, testOktaUserID, testGroup)
+	t.Run("assignments and caching", func(t *testing.T) {
+		// Given an assignmentClient backed by an Okta system with one user, one
+		// group and one app configured, but the user is not assigned to either...
+		oktaClient := newTestClient()
+		oktaClient.usernamesToUserIDs = map[string]string{
+			testUser: testOktaUserID,
+		}
+		oktaClient.appsToUsers = map[string]map[string]bool{
+			testApp: {},
+		}
+		oktaClient.groupsToUsers = map[string]map[string]bool{
+			testGroup: {},
+		}
+		assignmentClient := newAssignmentClient(log, oktaClient)
 
-	// User present, apps/groups present, using cached result.
-	ok, err = assignmentClient.userAssignedToApp(ctx, testUser, testApp)
-	require.NoError(t, err)
-	require.False(t, ok)
+		// When we test for membership, the operations succeed and return `false`.
+		isAssigned, err := assignmentClient.userAssignedToApp(ctx, testUser, testApp)
+		require.NoError(t, err)
+		require.False(t, isAssigned)
 
-	ok, err = assignmentClient.userAssignedToGroup(ctx, testUser, testGroup)
-	require.NoError(t, err)
-	require.False(t, ok)
+		isAssigned, err = assignmentClient.userAssignedToGroup(ctx, testUser, testGroup)
+		require.NoError(t, err)
+		require.False(t, isAssigned)
 
-	// Refresh caching client.
-	assignmentClient = newAssignmentClient(log, oktaClient)
+		// When we manually make the user and group assignments in the back-end
+		// test client and re-test the membership, expect that the
+		// assignmentClient uses cached data rather than re-querying the back
+		// end, and so still reports `false`.
+		oktaClient.appsToUsers[testApp][testOktaUserID] = true
+		oktaClient.groupsToUsers[testGroup][testOktaUserID] = true
 
-	// Updated assignments should be used.
-	ok, err = assignmentClient.userAssignedToApp(ctx, testUser, testApp)
-	require.NoError(t, err)
-	require.True(t, ok)
+		isAssigned, err = assignmentClient.userAssignedToApp(ctx, testUser, testApp)
+		require.NoError(t, err)
+		require.False(t, isAssigned)
 
-	ok, err = assignmentClient.userAssignedToGroup(ctx, testUser, testGroup)
-	require.NoError(t, err)
-	require.True(t, ok)
+		isAssigned, err = assignmentClient.userAssignedToGroup(ctx, testUser, testGroup)
+		require.NoError(t, err)
+		require.False(t, isAssigned)
 
-	// Unregister app should be reflected in the cache and in the backend client.
-	require.NoError(t, assignmentClient.unregisterUserFromApp(ctx, testUser, testApp))
+		// When we purge the assignmentClient cache and retry the membership
+		// tests, expect that the assignmentClient fetches new values from Okta
+		// and so now reports `true`
+		assignmentClient.purgeCache()
 
-	ok, err = assignmentClient.userAssignedToApp(ctx, testUser, testApp)
-	require.NoError(t, err)
-	require.False(t, ok)
+		isAssigned, err = assignmentClient.userAssignedToApp(ctx, testUser, testApp)
+		require.NoError(t, err)
+		require.True(t, isAssigned)
 
-	require.Empty(t, cmp.Diff(map[string]map[string]bool{
-		testApp: {},
-	}, oktaClient.appsToUsers))
+		isAssigned, err = assignmentClient.userAssignedToGroup(ctx, testUser, testGroup)
+		require.NoError(t, err)
+		require.True(t, isAssigned)
+	})
 
-	// Unregister group should be reflected in the cache and in the backend client.
-	require.NoError(t, assignmentClient.unregisterUserFromGroup(ctx, testUser, testGroup))
+	t.Run("reassignment", func(t *testing.T) {
+		// Given an assignmentClient backed by an Okta system with one user, one
+		// group and one app configured, and the user is assigned to both...
+		oktaClient, assignmentClient := testClientWithAssignments()
 
-	ok, err = assignmentClient.userAssignedToGroup(ctx, testUser, testGroup)
-	require.NoError(t, err)
-	require.False(t, ok)
+		// When I attempt to disassociate a user from an app or group,
+		// expect the operations to succeed and subsequent membership
+		// tests to return `false`
+		require.NoError(t, assignmentClient.unregisterUserFromApp(ctx, testUser, testApp))
+		ok, err := assignmentClient.userAssignedToApp(ctx, testUser, testApp)
+		require.NoError(t, err)
+		require.False(t, ok)
 
-	require.Empty(t, cmp.Diff(map[string]map[string]bool{
-		testGroup: {},
-	}, oktaClient.groupsToUsers))
+		require.NoError(t, assignmentClient.unregisterUserFromGroup(ctx, testUser, testGroup))
+		ok, err = assignmentClient.userAssignedToGroup(ctx, testUser, testGroup)
+		require.NoError(t, err)
+		require.False(t, ok)
 
-	// Reassign user to app and group
-	require.NoError(t, assignmentClient.registerUserToApp(ctx, testUser, testApp))
-	ok, err = assignmentClient.userAssignedToApp(ctx, testUser, testApp)
-	require.NoError(t, err)
-	require.True(t, ok)
+		// Also expect that the change has been passed through to the okta
+		// client
+		require.Empty(t, oktaClient.groupsToUsers[testGroup])
+		require.Empty(t, oktaClient.appsToUsers[testApp])
 
-	require.NoError(t, assignmentClient.registerUserToGroup(ctx, testUser, testGroup))
-	ok, err = assignmentClient.userAssignedToGroup(ctx, testUser, testGroup)
-	require.NoError(t, err)
-	require.True(t, ok)
+		// When I attempt to re-associate a user to an app or group,
+		// expect the operations to succeed and subsequent membership
+		// tests to return `true`
 
-	// Error unassigning app and group
-	oktaClient.unassignAppErr[testApp] = trace.BadParameter("bad parameter")
-	oktaClient.unassignGroupErr[testGroup] = trace.BadParameter("bad parameter")
+		require.NoError(t, assignmentClient.registerUserToApp(ctx, testUser, testApp))
+		ok, err = assignmentClient.userAssignedToApp(ctx, testUser, testApp)
+		require.NoError(t, err)
+		require.True(t, ok)
 
-	require.Error(t, assignmentClient.unregisterUserFromApp(ctx, testUser, testApp))
-	require.Error(t, assignmentClient.unregisterUserFromGroup(ctx, testUser, testGroup))
+		require.NoError(t, assignmentClient.registerUserToGroup(ctx, testUser, testGroup))
+		ok, err = assignmentClient.userAssignedToGroup(ctx, testUser, testGroup)
+		require.NoError(t, err)
+		require.True(t, ok)
+	})
 
-	// API validation yields no error
-	oktaClient.unassignAppErr[testApp] = oktaAPIValidationError{}
-	oktaClient.unassignGroupErr[testGroup] = oktaAPIValidationError{}
+	t.Run("validation errors are not propagated", func(t *testing.T) {
+		// Given an assignment client with pre-existing memberships...
+		oktaClient, assignmentClient := testClientWithAssignments()
 
-	require.NoError(t, assignmentClient.unregisterUserFromApp(ctx, testUser, testApp))
-	require.NoError(t, assignmentClient.unregisterUserFromGroup(ctx, testUser, testGroup))
+		// When calls to the underlying client fail with a oktaAPIValidationError
+		oktaClient.unassignAppErr[testApp] = oktaAPIValidationError{}
+		oktaClient.unassignGroupErr[testGroup] = oktaAPIValidationError{}
+
+		// Expect that the oktaAPIValidationError is treated as a success, the
+		// error is *NOT* propagated from the underlying Okta client, and the
+		// user assignments are updated as requested
+		require.NoError(t, assignmentClient.unregisterUserFromApp(ctx, testUser, testApp))
+		isAssigned, err := assignmentClient.userAssignedToApp(ctx, testUser, testApp)
+		require.NoError(t, err)
+		require.False(t, isAssigned)
+
+		require.NoError(t, assignmentClient.unregisterUserFromGroup(ctx, testUser, testGroup))
+		isAssigned, err = assignmentClient.userAssignedToGroup(ctx, testUser, testGroup)
+		require.NoError(t, err)
+		require.False(t, isAssigned)
+	})
+
+	t.Run("unassignment errors are propagated", func(t *testing.T) {
+		// Given an assignment client with pre-existing memberships...
+		oktaClient, assignmentClient := testClientWithAssignments()
+
+		// When calls to the underlying client fail...
+		oktaClient.unassignAppErr[testApp] = trace.BadParameter("bad parameter")
+		oktaClient.unassignGroupErr[testGroup] = trace.BadParameter("bad parameter")
+
+		// Expect that general errors are propagated from the underlying Okta
+		// client, and that the assignments have not been affected.
+
+		require.Error(t, assignmentClient.unregisterUserFromApp(ctx, testUser, testApp))
+		isAssigned, err := assignmentClient.userAssignedToApp(ctx, testUser, testApp)
+		require.NoError(t, err)
+		require.True(t, isAssigned)
+
+		require.Error(t, assignmentClient.unregisterUserFromGroup(ctx, testUser, testGroup))
+		isAssigned, err = assignmentClient.userAssignedToGroup(ctx, testUser, testGroup)
+		require.NoError(t, err)
+		require.True(t, isAssigned)
+	})
 }
