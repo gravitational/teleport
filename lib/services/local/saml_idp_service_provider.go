99907528b54c70d3701ed3b2f40171de895176f2
Tim Ross <tim.ross@goteleport.com>
2023-10-17 10:46:38 -0400

    Make all parse errors bad paramter errors

--- lib/services/local/saml_idp_service_provider.go
+++ lib/services/local/saml_idp_service_provider.go
@@ -18,7 +18,6 @@ package local
 
 import (
 	"context"
-	"strings"
 	"time"
 
 	"github.com/crewjam/saml/samlsp"
@@ -165,10 +164,7 @@ func (s *SAMLIdPServiceProviderService) ensureEntityIDIsUnique(ctx context.Conte
 func validateSAMLIdPServiceProvider(sp types.SAMLIdPServiceProvider) error {
 	ed, err := samlsp.ParseMetadata([]byte(sp.GetEntityDescriptor()))
 	if err != nil {
-		if strings.Contains(err.Error(), "invalid url") {
-			return trace.BadParameter(err.Error())
-		}
-		return trace.Wrap(err)
+		return trace.BadParameter(err.Error())
 	}
 
 	if ed.EntityID != sp.GetEntityID() {
