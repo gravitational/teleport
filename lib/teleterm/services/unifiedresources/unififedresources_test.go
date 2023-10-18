163e04e16712a3cfa55a360ee75f87b385d5c28d
Grzegorz Zdunek <gzdunek@users.noreply.github.com>
2023-10-18 13:45:29 +0200

    Add unified resource view gRPC handler to tshd (#33150)
    * Add unified resource view gRPC handler to tshd
    
    * Improve comments
    
    * Move `kinds` validation to `unifiedresources.List`
    
    * Make `listUnifiedResources` return a union
    
    * Rename `CombinedResource` to `UnifiedResource`
    
    * Support `pinnedOnly`
    
    * Ignore unsupported resources
    
    * Fix lint issues
    
    * Run prettier
    
    * Take kubes from `PaginatedResource_KubernetesServer`, not `PaginatedResource_KubeCluster`

---
+++ lib/teleterm/services/unifiedresources/unififedresources_test.go
@@ -0,0 +1,103 @@
+// Copyright 2023 Gravitational, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package unifiedresources
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+
+	"github.com/gravitational/teleport/api/client/proto"
+	"github.com/gravitational/teleport/api/types"
+	"github.com/gravitational/teleport/lib/defaults"
+	"github.com/gravitational/teleport/lib/teleterm/api/uri"
+	"github.com/gravitational/teleport/lib/teleterm/clusters"
+)
+
+func TestUnifiedResourcesList(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+
+	cluster := &clusters.Cluster{URI: uri.NewClusterURI("foo"), ProfileName: "foo"}
+
+	node, err := types.NewServer("testNode", types.KindNode, types.ServerSpecV2{})
+	require.NoError(t, err)
+
+	database, err := types.NewDatabaseServerV3(types.Metadata{
+		Name: "testDb",
+	}, types.DatabaseServerSpecV3{
+		Hostname: "localhost",
+		HostID:   uuid.New().String(), Database: &types.DatabaseV3{
+			Spec: types.DatabaseSpecV3{
+				Protocol: defaults.ProtocolPostgres, URI: "localhost:5432",
+			},
+			Metadata: types.Metadata{Name: "testDb"}}})
+	require.NoError(t, err)
+
+	kube, err := types.NewKubernetesServerV3(types.Metadata{
+		Name: "testKube",
+	}, types.KubernetesServerSpecV3{
+		HostID: uuid.New().String(),
+		Cluster: &types.KubernetesClusterV3{
+			Metadata: types.Metadata{
+				Name: "testKube",
+			},
+			Spec: types.KubernetesClusterSpecV3{},
+		},
+	})
+	require.NoError(t, err)
+
+	mockedResources := []*proto.PaginatedResource{
+		{Resource: &proto.PaginatedResource_Node{Node: node.(*types.ServerV2)}},
+		{Resource: &proto.PaginatedResource_DatabaseServer{DatabaseServer: database}},
+		{Resource: &proto.PaginatedResource_KubernetesServer{KubernetesServer: kube}},
+	}
+	mockedNextKey := "nextKey"
+
+	mockedClient := &mockClient{
+		paginatedResources: mockedResources,
+		nextKey:            mockedNextKey,
+	}
+
+	response, err := List(ctx, cluster, mockedClient, &proto.ListUnifiedResourcesRequest{})
+	require.NoError(t, err)
+	require.Equal(t, UnifiedResource{Server: &clusters.Server{
+		URI:    uri.NewClusterURI(cluster.ProfileName).AppendServer(node.GetName()),
+		Server: node,
+	}}, response.Resources[0])
+	require.Equal(t, UnifiedResource{Database: &clusters.Database{
+		URI:      uri.NewClusterURI(cluster.ProfileName).AppendDB(database.GetName()),
+		Database: database.GetDatabase(),
+	}}, response.Resources[1])
+	require.Equal(t, UnifiedResource{Kube: &clusters.Kube{
+		URI:               uri.NewClusterURI(cluster.ProfileName).AppendKube(kube.GetCluster().GetName()),
+		KubernetesCluster: kube.GetCluster(),
+	}}, response.Resources[2])
+	require.Equal(t, mockedNextKey, response.NextKey)
+}
+
+type mockClient struct {
+	paginatedResources []*proto.PaginatedResource
+	nextKey            string
+}
+
+func (m *mockClient) ListUnifiedResources(ctx context.Context, req *proto.ListUnifiedResourcesRequest) (*proto.ListUnifiedResourcesResponse, error) {
+	return &proto.ListUnifiedResourcesResponse{
+		Resources: m.paginatedResources,
+		NextKey:   m.nextKey,
+	}, nil
+}
