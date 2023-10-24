4398d815b27ab1e68c1cada8fd59c1868515f145
Zac Bergquist <zac.bergquist@goteleport.com>
2023-10-24 13:24:51 -0600

    Disambiguate directory sharing's disabled and inactive states (#33815)
    Prior to this change, the UI would show "disabled" when directory
    sharing is disabled due to RBAC and when it is enabled but inactive.
    
    Closes #33748

--- web/packages/teleport/src/DesktopSession/TopBar.tsx
+++ web/packages/teleport/src/DesktopSession/TopBar.tsx
@@ -59,11 +59,7 @@ export default function TopBar(props: Props) {
           <StyledFolderShared
             style={primaryOnTrue(isSharingDirectory)}
             pr={3}
-            title={
-              isSharingDirectory
-                ? 'Directory Sharing Enabled'
-                : 'Directory Sharing Disabled'
-            }
+            title={directorySharingTitle(canShareDirectory, isSharingDirectory)}
           />
           <StyledClipboard
             style={primaryOnTrue(clipboardSharingEnabled)}
@@ -87,6 +83,16 @@ export default function TopBar(props: Props) {
       </Flex>
     </TopNav>
   );
+}
+
+function directorySharingTitle(canShare: boolean, isSharing: boolean): string {
+  if (!canShare) {
+    return 'Directory Sharing Disabled';
+  }
+  if (!isSharing) {
+    return 'Directory Sharing Inactive';
+  }
+  return 'Directory Sharing Enabled';
 }
 
 export const TopBarHeight = 40;
