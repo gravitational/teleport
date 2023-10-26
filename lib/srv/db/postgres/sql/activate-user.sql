104af830b79b539fde4f28794abe481dbbe6f037
greedy52 <xin.huang@goteleport.com>
2023-10-26 20:16:11 +0000

    Fix an issue auto-provisioned PostgreSQL user may keep old roles indefinitely

--- lib/srv/db/postgres/sql/activate-user.sql
+++ lib/srv/db/postgres/sql/activate-user.sql
@@ -3,14 +3,19 @@ language plpgsql
 as $$
 declare
     role_ varchar;
+    cur_roles varchar[];
 begin
     -- If the user already exists and was provisioned by Teleport, reactivate
     -- it, otherwise provision a new one.
     if exists (select * from pg_auth_members where roleid = (select oid from pg_roles where rolname = 'teleport-auto-user') and member = (select oid from pg_roles where rolname = username)) then
-        -- If the user has active connections, just use it to avoid messing up
-        -- its existing roles.
+        -- If the user has active connections, make sure the provided roles
+        -- match what the user currently has.
         if exists (select usename from pg_stat_activity where usename = username) then
-          return;
+            select cast(array_agg(rolname) as varchar[]) into cur_roles from pg_auth_members join pg_roles on roleid = oid where member=(select oid from pg_roles where rolname = username) and rolname != 'teleport-auto-user';
+            if roles <@ cur_roles AND cur_roles <@roles then
+                return;
+            end if;
+            RAISE EXCEPTION SQLSTATE 'TP002' USING MESSAGE = 'TP002: User has active connections and roles have changed';
         end if;
         -- Otherwise reactivate the user, but first strip if of all roles to
         -- account for scenarios with left-over roles if database agent crashed
