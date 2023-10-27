dd8173a22fe3b46c925a8debf284786ab0d824bb
STeve Huang <xin.huang@goteleport.com>
2023-10-27 11:08:19 -0400

    upper case the sql scripts

--- lib/srv/db/postgres/sql/delete-user.sql
+++ lib/srv/db/postgres/sql/delete-user.sql
@@ -1,21 +1,21 @@
-create or replace procedure teleport_delete_user(username varchar, inout state varchar default 'TP003')
-language plpgsql
-as $$
-declare
+CREATE OR REPLACE PROCEDURE teleport_delete_user(username varchar, inout state varchar default 'TP003')
+LANGUAGE plpgsql
+AS $$
+DECLARE
     role_ varchar;
-begin
+BEGIN
     -- Only drop if the user doesn't have other active sessions.
-    if exists (select usename from pg_stat_activity where usename = username) then
-        raise notice 'User has active connections';
-    else
-        begin
-            execute format('drop user %I', username);
-        exception
-            when SQLSTATE '2BP01' then
+    IF EXISTS (SELECT usename FROM pg_stat_activity WHERE usename = username) THEN
+        RAISE NOTICE 'User has active connections';
+    ELSE
+        BEGIN
+            EXECUTE FORMAT('DROP USER %I', username);
+        EXCEPTION
+            WHEN SQLSTATE '2BP01' THEN
                 state := 'TP004';
                 -- Drop user/role will fail if user has dependent objects.
                 -- In this scenario, fallback into disabling the user.
-                call teleport_deactivate_user(username);
-        end;
-    end if;
-end;$$;
+                CALL teleport_deactivate_user(username);
+        END;
+    END IF;
+END;$$;
