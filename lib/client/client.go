dfecea465f1e35d70da550f1cae2e302d7a56bc6
Steven Martin <steven@goteleport.com>
2023-10-30 07:35:00 -0400

    provide more user friendly error message on ssh access denied

--- lib/client/client.go
+++ lib/client/client.go
@@ -1483,7 +1483,7 @@ func NewNodeClient(ctx context.Context, sshConfig *ssh.ClientConfig, conn net.Co
 			//  An alternative we have here is querying the cluster to check if device
 			//  trust is required, a check similar to `IsMFARequired`.
 			log.Infof("Access denied to %v connecting to %v: %v", sshConfig.User, nodeName, err)
-			return nil, trace.AccessDenied(`access denied to %v connecting to %v`, sshConfig.User, nodeName)
+			return nil, trace.AccessDenied(`access denied to %v connecting to %v. User does not have permissions. Confirm SSH user`, sshConfig.User, nodeName)
 		}
 		return nil, trace.Wrap(err)
 	}
