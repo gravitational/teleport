ee5f5b38b20fc62abe40c9c728bda6e68fbd9fb6
Tim Ross <tim.ross@goteleport.com>
2023-11-03 17:01:02 -0400

    Implement ListUsers in the usersv1.Service
    The api client was also updated to prefer using ListUsers over
    GetUsers.

--- lib/auth/users/usersv1/service.go
+++ lib/auth/users/usersv1/service.go
@@ -37,6 +37,8 @@ import (
 type Cache interface {
 	// GetUser returns a user by name.
 	GetUser(ctx context.Context, user string, withSecrets bool) (types.User, error)
+	// ListUsers returns a page of users.
+	ListUsers(ctx context.Context, pageSize int, nextToken string, withSecrets bool) ([]types.User, string, error)
 	// GetRole returns a role by name.
 	GetRole(ctx context.Context, name string) (types.Role, error)
 }
@@ -198,7 +200,7 @@ func (s *Service) GetUser(ctx context.Context, req *userspb.GetUserRequest) (*us
 
 	v2, ok := user.(*types.UserV2)
 	if !ok {
-		s.logger.Warnf("expected type services.UserV2, got %T for user %q", user, user.GetName())
+		s.logger.Warnf("expected type UserV2, got %T for user %q", user, user.GetName())
 		return nil, trace.BadParameter("encountered unexpected user type")
 	}
 
@@ -255,7 +257,7 @@ func (s *Service) CreateUser(ctx context.Context, req *userspb.CreateUserRequest
 
 	v2, ok := created.(*types.UserV2)
 	if !ok {
-		s.logger.Warnf("expected type services.UserV2, got %T for user %q", created, created.GetName())
+		s.logger.Warnf("expected type UserV2, got %T for user %q", created, created.GetName())
 		return nil, trace.BadParameter("encountered unexpected user type")
 	}
 
@@ -307,7 +309,7 @@ func (s *Service) UpdateUser(ctx context.Context, req *userspb.UpdateUserRequest
 
 	v2, ok := updated.(*types.UserV2)
 	if !ok {
-		s.logger.Warnf("expected type services.UserV2, got %T for user %q", updated, updated.GetName())
+		s.logger.Warnf("expected type UserV2, got %T for user %q", updated, updated.GetName())
 		return nil, trace.BadParameter("encountered unexpected user type")
 	}
 
@@ -366,7 +368,7 @@ func (s *Service) UpsertUser(ctx context.Context, req *userspb.UpsertUserRequest
 
 	v2, ok := upserted.(*types.UserV2)
 	if !ok {
-		s.logger.Warnf("expected type services.UserV2, got %T for user %q", upserted, upserted.GetName())
+		s.logger.Warnf("expected type UserV2, got %T for user %q", upserted, upserted.GetName())
 		return nil, trace.BadParameter("encountered unexpected user type")
 	}
 
@@ -423,4 +425,61 @@ func (s *Service) DeleteUser(ctx context.Context, req *userspb.DeleteUserRequest
 	}
 
 	return &emptypb.Empty{}, nil
+}
+
+func (s *Service) ListUsers(ctx context.Context, req *userspb.ListUsersRequest) (*userspb.ListUsersResponse, error) {
+	authCtx, err := s.authorizer.Authorize(ctx)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	if req.WithSecrets {
+		// TODO(fspmarshall): replace admin requirement with VerbReadWithSecrets once we've
+		// migrated to that model.
+		if !authz.HasBuiltinRole(*authCtx, string(types.RoleAdmin)) {
+			err := trace.AccessDenied("user %q requested access to all users with secrets", authCtx.User.GetName())
+			s.logger.Warn(err)
+			if err := s.emitter.EmitAuditEvent(ctx, &apievents.UserLogin{
+				Metadata: apievents.Metadata{
+					Type: events.UserLoginEvent,
+					Code: events.UserLocalLoginFailureCode,
+				},
+				Method: events.LoginMethodClientCert,
+				Status: apievents.Status{
+					Success:     false,
+					Error:       trace.Unwrap(err).Error(),
+					UserMessage: err.Error(),
+				},
+			}); err != nil {
+				s.logger.WithError(err).Warn("Failed to emit local login failure event.")
+			}
+			return nil, trace.AccessDenied("this request can be only executed by an admin")
+		}
+	} else {
+		if _, err := authz.AuthorizeWithVerbs(ctx, s.logger, s.authorizer, true, types.KindUser, types.VerbList, types.VerbRead); err != nil {
+			return nil, trace.Wrap(err)
+		}
+	}
+
+	users, next, err := s.cache.ListUsers(ctx, int(req.PageSize), req.PageToken, req.WithSecrets)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	resp := &userspb.ListUsersResponse{
+		Users:         make([]*types.UserV2, 0, len(users)),
+		NextPageToken: next,
+	}
+
+	for _, user := range users {
+		v2, ok := user.(*types.UserV2)
+		if !ok {
+			s.logger.Warnf("expected type UserV2, got %T", user)
+		}
+
+		resp.Users = append(resp.Users, v2)
+
+	}
+
+	return resp, nil
 }
