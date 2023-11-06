4e46bf4b2a1943e64dfefadad946e8676905547b
Michael Myers <michael.myers@goteleport.com>
2023-11-05 20:37:44 -0600

    Prevent updated hostnames from creating duplicate entires in unified cache

--- lib/services/unified_resource.go
+++ lib/services/unified_resource.go
@@ -128,8 +128,26 @@ func (c *UnifiedResourceCache) put(ctx context.Context, resource resource) error
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	key := resourceKey(resource)
-	c.resources[key] = resource
 	sortKey := makeResourceSortKey(resource)
+	oldResource, exists := c.resources[key]
+	if exists {
+		// If the resource has changed in such a way that the sort keys
+		// for the nameTree or typeTree change, remove the old entries
+		// from those trees before adding a new one. This can happen
+		// when a node's hostname changes
+		oldSortKey := makeResourceSortKey(oldResource)
+		if string(oldSortKey.byName) != string(sortKey.byName) {
+			if _, ok := c.nameTree.Delete(&item{Key: oldSortKey.byName}); !ok {
+				return trace.NotFound("key %q is not found in unified cache name sort tree", string(sortKey.byName))
+			}
+		}
+		if string(oldSortKey.byType) != string(sortKey.byType) {
+			if _, ok := c.typeTree.Delete(&item{Key: oldSortKey.byType}); !ok {
+				return trace.NotFound("key %q is not found in unified cache type sort tree", string(sortKey.byType))
+			}
+		}
+	}
+	c.resources[key] = resource
 	c.nameTree.ReplaceOrInsert(&item{Key: sortKey.byName, Value: key})
 	c.typeTree.ReplaceOrInsert(&item{Key: sortKey.byType, Value: key})
 	return nil
