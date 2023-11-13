6047c4778392651b815dffd3f92d65fea867ca0e
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2023-11-13 18:58:17 +0100

    Remove an import from `UnifiedResources`

--- web/packages/teleterm/src/ui/Search/searchResult.ts
+++ web/packages/teleterm/src/ui/Search/searchResult.ts
@@ -18,7 +18,6 @@ import type { ClusterUri } from 'teleterm/ui/uri';
 import type { Cluster } from 'teleterm/services/tshd/types';
 
 import type * as resourcesServiceTypes from 'teleterm/ui/services/resources';
-import type { SharedUnifiedResource } from 'shared/components/UnifiedResources';
 
 type ResourceSearchResultBase<
   Result extends resourcesServiceTypes.SearchResult
@@ -28,10 +27,7 @@ type ResourceSearchResultBase<
   score: number;
 };
 
-export type ResourceTypeFilter = Extract<
-  SharedUnifiedResource['resource']['kind'],
-  'node' | 'kube_cluster' | 'db'
->;
+export type ResourceTypeFilter = 'node' | 'kube_cluster' | 'db';
 
 export type SearchResultServer =
   ResourceSearchResultBase<resourcesServiceTypes.SearchResultServer>;
