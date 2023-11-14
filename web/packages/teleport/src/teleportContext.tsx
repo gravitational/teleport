12bb71213ce76d2ebe10974006ad9689ba2b07cd
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2023-11-14 11:49:42 -0500

    Add TAG UI (#34570)
    * Add TAG UI
    
    * Add missing license
    
    * Remove elkjs

--- web/packages/teleport/src/teleportContext.tsx
+++ web/packages/teleport/src/teleportContext.tsx
@@ -182,6 +182,7 @@ class TeleportContext implements types.Context {
       assist: userContext.getAssistantAccess().list && this.assistEnabled,
       accessMonitoring: hasAccessMonitoringAccess(),
       managementSection: hasManagementSectionAccess(),
+      accessGraph: userContext.getAccessGraphAccess().list,
     };
   }
 }
@@ -214,6 +215,7 @@ export const disabledFeatureFlags: types.FeatureFlags = {
   assist: false,
   managementSection: false,
   accessMonitoring: false,
+  accessGraph: false,
 };
 
 export default TeleportContext;
