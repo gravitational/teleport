41b9f56a0b418a4446c48d6c92ec0b0516be95d0
Mike Jensen <jentfoo@users.noreply.github.com>
2023-11-16 01:56:07 -0700

    [v12] Prevent `.tsh/environment` values from overloading prior set values (#34624)
    * Prevent `.tsh/environment` values from overloading prior set values
    
    It's not possible to have duplicate environment values within an environment.  And in fact the last value in the string slice will be preserved.  Prior to this change that allows users to possibly change any environment values through the use of the `.tsh/environment` file.  This is within user level control, where other environment value sources originate from a more protected location (for example the PAM configuration).
    
    Prior to this change that allows users to possibly change any environment passed configuration through the use of the `.tsh/environment` file.
    
    This change makes it so that the administrative set values will be preferred, and any duplicate records will be ignored.
    
    * Apply PR feedback
    
    * Only exclude duplicate environment values sourced from .tsh/environment
    
    This change updates `SafeEnv` to be allow the caller to select if the value should be checked for duplicates.
    We then leverage this to avoid this check when sourced from a trusted source.  But then exclude potential duplicates when sourced from .tsh/environment file or the local environment.

--- lib/srv/reexec.go
+++ lib/srv/reexec.go
@@ -798,21 +798,22 @@ func buildCommand(c *ExecCommand, localUser *user.User, tty *os.File, pty *os.Fi
 	}
 
 	// Add in Teleport specific environment variables.
-	env.AddFull(c.Environment...)
+	env.AddFullTrusted(c.Environment...)
+
+	// If any additional environment variables come from PAM, apply them as well.
+	env.AddFullTrusted(pamEnvironment...)
 
 	// If the server allows reading in of ~/.tsh/environment read it in
 	// and pass environment variables along to new session.
+	// User controlled values are added last to ensure administrator controlled sources take priority (duplicates ignored)
 	if c.PermitUserEnvironment {
 		filename := filepath.Join(localUser.HomeDir, ".tsh", "environment")
 		userEnvs, err := envutils.ReadEnvironmentFile(filename)
 		if err != nil {
 			return nil, trace.Wrap(err)
 		}
-		env.AddFull(userEnvs...)
+		env.AddFullUnique(userEnvs...)
 	}
-
-	// If any additional environment variables come from PAM, apply them as well.
-	env.AddFull(pamEnvironment...)
 
 	// after environment is fully built, set it to cmd
 	cmd.Env = *env
@@ -955,7 +956,7 @@ func ConfigureCommand(ctx *ServerContext, extraFiles ...*os.File) (*exec.Cmd, er
 
 	// build env for `teleport exec`
 	env := &envutils.SafeEnv{}
-	env.AddFull(cmdmsg.Environment...)
+	env.AddFullTrusted(cmdmsg.Environment...)
 	env.AddExecEnvironment()
 
 	// Build the "teleport exec" command.
