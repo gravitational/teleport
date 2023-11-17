8bbf5f8fba6433750eeb5ee820ef08dee55b997a
Alan Parra <alan.parra@goteleport.com>
2023-11-17 20:29:26 -0300

    Move WebAuthNGetApiVersionNumber to mkwinsyscall

---
+++ lib/auth/webauthnwin/syscall_gen.go
@@ -0,0 +1,20 @@
+// Copyright 2023 Gravitational, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:generate go run golang.org/x/sys/windows/mkwinsyscall -output syscall_windows.go syscall_gen.go
+
+//sys webAuthNGetApiVersionNumber() (ret int, err error) [failretval==0] = WebAuthn.WebAuthNGetApiVersionNumber
+// https://github.com/microsoft/webauthn/blob/7ab979cc833bfab9a682ed51761309db57f56c8c/webauthn.h#L895-L897
+
+package webauthnwin
