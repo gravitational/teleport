444086e6be5bf6c9e1d36d64a619aeb75cf6a4a5
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2023-11-20 11:33:59 +0100

    Add a `makeDocumentCluster` test helper

--- web/packages/teleterm/src/ui/DocumentCluster/DocumentCluster.story.tsx
+++ web/packages/teleterm/src/ui/DocumentCluster/DocumentCluster.story.tsx
@@ -37,6 +37,7 @@ import { MockWorkspaceContextProvider } from 'teleterm/ui/fixtures/MockWorkspace
 import { ConnectMyComputerContextProvider } from 'teleterm/ui/ConnectMyComputer';
 import * as docTypes from 'teleterm/ui/services/workspacesService/documentsService/types';
 import * as tsh from 'teleterm/services/tshd/types';
+import { makeDocumentCluster } from 'teleterm/ui/services/workspacesService/documentsService/testHelpers';
 
 import DocumentCluster from './DocumentCluster';
 import { ResourcesContextProvider } from './resourcesContext';
@@ -45,19 +46,15 @@ export default {
   title: 'Teleterm/DocumentCluster',
 };
 
-const rootClusterDoc = {
-  kind: 'doc.cluster' as const,
-  clusterUri: '/clusters/localhost' as const,
-  uri: '/docs/123' as const,
-  title: 'sample',
-};
+const rootClusterDoc = makeDocumentCluster({
+  clusterUri: '/clusters/localhost',
+  uri: '/docs/123',
+});
 
-const leafClusterDoc = {
-  kind: 'doc.cluster' as const,
-  clusterUri: '/clusters/localhost/leaves/foo' as const,
-  uri: '/docs/456' as const,
-  title: 'sample',
-};
+const leafClusterDoc = makeDocumentCluster({
+  clusterUri: '/clusters/localhost/leaves/foo',
+  uri: '/docs/456',
+});
 
 export const OnlineEmptyResourcesAndCanAddResourcesAndConnectComputer = () => {
   const state = createClusterServiceState();
