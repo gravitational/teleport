68e65b3cf74c1f7d8f5e02ab6b6d350bca62104c
Nic Klaassen <nic@goteleport.com>
2023-11-21 17:22:05 -0800

    External Audit Storage: bootstrap script improvements
    Previously the bootstrap script had no output if it was successful and
    didn't make it clear that it actually did anything.
    This change adds some output to inform the user what is being done, and
    renames to command to `externalauditstorage` to align with the new
    external name for the feature.
    
    Example command invocation:
    
    ```
    > teleport integration configure externalauditstorage --bootstrap \
      --aws-region=us-west-2 --role=nic-byob-test \
      --policy=ExternalCloudAuditPolicy-8b0df11a-74cd-4e4c-bc71-dbe96b8e4e04 \
      --session-recordings=s3://teleport-longterm-8b0df11a-74cd-4e4c-bc71-dbe96b8e4e04/sessions \
      --audit-events=s3://teleport-longterm-8b0df11a-74cd-4e4c-bc71-dbe96b8e4e04/events \
      --athena-results=s3://teleport-transient-8b0df11a-74cd-4e4c-bc71-dbe96b8e4e04/query_results \
      --athena-workgroup=teleport_events_8b0df11a_74cd_4e4c_bc71_dbe96b8e4e04 \
      --glue-database=teleport_events_8b0df11a_74cd_4e4c_bc71_dbe96b8e4e04 \
      --glue-table=teleport_events
    
    Bootstrapping External Audit Storage infrastructure
    Creating long term storage S3 bucket teleport-longterm-8b0df11a-74cd-4e4c-bc71-dbe96b8e4e04
    Applying object lock configuration to long term storage S3 bucket with default retention period of 4 years
    Creating transient storage S3 bucket teleport-transient-8b0df11a-74cd-4e4c-bc71-dbe96b8e4e04
    Applying bucket lifecycle configuration to transient storage S3 bucket
    Creating Athena workgroup teleport_events_8b0df11a_74cd_4e4c_bc71_dbe96b8e4e04
    Creating Glue database teleport_events_8b0df11a_74cd_4e4c_bc71_dbe96b8e4e04
    Creating Glue table teleport_events
    
    Configuring necessary IAM permissions for External Audit Storage
    Attaching inline policy ExternalCloudAuditPolicy-8b0df11a-74cd-4e4c-bc71-dbe96b8e4e04 to role nic-byob-test
    ```

--- tool/tctl/common/cmds.go
+++ tool/tctl/common/cmds.go
@@ -43,7 +43,7 @@ func Commands() []CLICommand {
 		&ProxyCommand{},
 		&ResourceCommand{},
 		&EditCommand{},
-		&ExternalCloudAuditCommand{},
+		&ExternalAuditStorageCommand{},
 		&LoadtestCommand{},
 		&DevicesCommand{},
 		&SAMLCommand{},
