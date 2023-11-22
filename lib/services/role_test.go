a77adcd931e21a46d310840325fa6cf7d167cb9e
STeve Huang <xin.huang@goteleport.com>
2023-11-22 08:40:07 -0500

    add mock username

--- lib/services/role_test.go
+++ lib/services/role_test.go
@@ -7423,6 +7423,10 @@ func (u mockCurrentUser) GetTraits() map[string][]string {
 	return u.traits
 }
 
+func (u mockCurrentUser) GetName() string {
+	return "mockCurrentUser"
+}
+
 func TestNewAccessCheckerForRemoteCluster(t *testing.T) {
 	user := mockCurrentUser{
 		roles: []string{"dev", "admin"},
