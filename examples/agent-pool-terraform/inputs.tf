bc6ed6ebeb1528b488034160f2191d0210606283
Paul Gottschling <paul.gottschling@goteleport.com>
2023-11-27 14:06:36 -0500

    Remove unneeded TF files

--- examples/agent-pool-terraform/inputs.tf
+++ examples/agent-pool-terraform/inputs.tf
@@ -1,33 +1,6 @@
-variable "azure_resource_group" {
-  type        = string
-  default     = ""
-  description = "Azure location in which to deploy agents"
-}
-
 variable "agent_count" {
   type        = number
   description = "Number of agents to deploy"
-}
-
-variable "cloud" {
-  type        = string
-  description = "Cloud provider: aws|gcp|azure"
-  validation {
-    condition     = var.cloud == "aws" || var.cloud == "gcp" || var.cloud == "azure"
-    error_message = "The value of \"cloud\" must be \"aws\", \"gcp\", or \"azure\"."
-  }
-}
-
-variable "google_project" {
-  type        = string
-  default     = ""
-  description = "GCP project to associate agents with"
-}
-
-variable "gcp_zone" {
-  type        = string
-  default     = ""
-  description = "GCP zone to associate agents with"
 }
 
 variable "proxy_service_address" {
@@ -35,20 +8,9 @@ variable "proxy_service_address" {
   description = "Host and HTTPS port of the Teleport Proxy Service"
 }
 
-variable "public_key_path" {
+variable "aws_region" {
   type        = string
-  description = "Path to a valid RSA public key with at least 2048 bits. The key is only used to pass validation in Azure, and is deleted from VMs created by this module."
-  default     = ""
-}
-
-variable "region" {
-  type        = string
-  description = "Location in which to deploy agents (Azure location, AWS or GCP region)"
-}
-
-variable "subnet_id" {
-  type        = string
-  description = "Cloud provider subnet for deploying Teleport agents (subnet ID if using AWS or Azure, name or self link if using GCP)"
+  description = "Region in which to deploy AWS resources"
 }
 
 variable "teleport_edition" {
@@ -64,4 +26,9 @@ variable "teleport_edition" {
 variable "teleport_version" {
   type        = string
   description = "Version of Teleport to install on each agent"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "ID of the AWS subnet for deploying Teleport agents"
 }
