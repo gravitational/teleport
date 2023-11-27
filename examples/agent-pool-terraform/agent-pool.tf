d384c1a07ba47141a7f1d27337c20cb176e040bc
Paul Gottschling <paul.gottschling@goteleport.com>
2023-11-27 13:30:14 -0500

    Revert "Support paid editions in agent-pool-terraform"
    This reverts commit 64c9546c7240d79c2d3f5cc1cdb4c5a6856185b0.

--- examples/agent-pool-terraform/agent-pool.tf
+++ examples/agent-pool-terraform/agent-pool.tf
@@ -1,7 +1,3 @@
-locals {
-  userdata_path = var.is_community ? "./userdata" : "./userdata-ent"
-}
-
 resource "random_string" "token" {
   count  = var.agent_count
   length = 32
@@ -29,7 +25,7 @@ resource "aws_instance" "teleport_agent" {
   ami           = "ami-04a0ae173da5807d3"
   instance_type = "t3.small"
   subnet_id     = var.subnet_id
-  user_data = templatefile(local.userdata_path, {
+  user_data = templatefile("./userdata", {
     token                 = teleport_provision_token.agent[count.index].metadata.name
     proxy_service_address = var.proxy_service_address
     teleport_version      = var.teleport_version
