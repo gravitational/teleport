18c1eaece60a13e88a5adddd9db021b1a9874b04
Paul Gottschling <paul.gottschling@goteleport.com>
2023-11-27 14:12:06 -0500

    Restore agent-pool.tf with modifications
    Add an AMI data source

---
+++ examples/agent-pool-terraform/agent-pool.tf
@@ -0,0 +1,70 @@
+resource "random_string" "token" {
+  count  = var.agent_count
+  length = 32
+}
+
+resource "teleport_provision_token" "agent" {
+  count = var.agent_count
+  spec = {
+    roles = [
+      "App",
+      "Db",
+      "Discovery",
+      "Kube",
+      "Node",
+    ]
+    name = random_string.token[count.index].result
+  }
+  metadata = {
+    expires = timeadd(timestamp(), "1h")
+  }
+}
+
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+
+  filter {
+    name   = "description"
+    values = ["Amazon Linux 2023 AMI*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+}
+
+resource "aws_instance" "teleport_agent" {
+  count = var.cloud == "aws" ? var.agent_count : 0
+  # Amazon Linux 2023 64-bit x86
+  ami           = data.aws_ami.amazon_linux_2023.id
+  instance_type = "t3.small"
+  subnet_id     = var.subnet_id
+  user_data = templatefile("./userdata", {
+    token                 = teleport_provision_token.agent[count.index].metadata.name
+    proxy_service_address = var.proxy_service_address
+    teleport_edition      = var.teleport_edition
+    teleport_version      = var.teleport_version
+  })
+
+  // The following two blocks adhere to security best practices.
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
+  root_block_device {
+    encrypted = true
+  }
+}
