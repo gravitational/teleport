4d83250d43817d2299e5983b0fe2703e9471b561
Grzegorz Zdunek <gzdunek@users.noreply.github.com>
2023-11-29 14:35:47 +0100

    Update TypeScript to 5.3 (#35046) (#35114)
    * Update TypeScript to 5.3
    
    * Fix "An object literal cannot have multiple properties with the same name" error
    
    (cherry picked from commit 6a2622759e254a1c1512cb9be139ff3293023b4f)

--- web/packages/teleport/src/Audit/EventList/EventTypeCell.tsx
+++ web/packages/teleport/src/Audit/EventList/EventTypeCell.tsx
@@ -35,7 +35,6 @@ const EventIconMap: Record<EventCode, React.FC> = {
   [eventCodes.OIDC_CONNECTOR_CREATED]: Icons.Info,
   [eventCodes.OIDC_CONNECTOR_DELETED]: Icons.Info,
   [eventCodes.SAML_CONNECTOR_CREATED]: Icons.Info,
-  [eventCodes.SAML_CONNECTOR_CREATED]: Icons.Info,
   [eventCodes.SAML_CONNECTOR_DELETED]: Icons.Info,
   [eventCodes.ROLE_CREATED]: Icons.Info,
   [eventCodes.ROLE_DELETED]: Icons.Info,
