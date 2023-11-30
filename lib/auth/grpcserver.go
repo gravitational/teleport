56939dda8ff51f1da103ca69f7d12cc7cf95e921
Tiago Silva <tiago.silva@goteleport.com>
2023-11-30 23:07:20 +0000

    Correctly clone RoleV6 when downgrading (#35230)
    * Correctly clone RoleV6 when downgrading
    
    Fix panic caused by improper clone of `RoleV6` when downgrading.
    This PR forces a clone before downgrading it.
    
    Fixes #35229
    
    Signed-off-by: Tiago Silva <tiago.silva@goteleport.com>
    
    * add test to ensure it was properly cloned
    
    * clear comment
    
    * make test fail without the fix
    
    ---------
    
    Signed-off-by: Tiago Silva <tiago.silva@goteleport.com>
    Co-authored-by: Nic Klaassen <nic@goteleport.com>

--- lib/auth/grpcserver.go
+++ lib/auth/grpcserver.go
@@ -61,6 +61,7 @@ import (
 	apievents "github.com/gravitational/teleport/api/types/events"
 	"github.com/gravitational/teleport/api/types/installers"
 	"github.com/gravitational/teleport/api/types/wrappers"
+	apiutils "github.com/gravitational/teleport/api/utils"
 	"github.com/gravitational/teleport/lib/auth/assist/assistv1"
 	"github.com/gravitational/teleport/lib/auth/discoveryconfig/discoveryconfigv1"
 	integrationService "github.com/gravitational/teleport/lib/auth/integration/integrationv1"
@@ -2090,6 +2091,12 @@ func maybeDowngradeRoleLabelExpressions(ctx context.Context, role *types.RoleV6,
 	if !clientVersion.LessThan(minSupportedLabelExpressionVersion) {
 		return role, nil
 	}
+	// Make a shallow copy of the role so that we don't mutate the original.
+	// This is necessary because the role is shared
+	// between multiple clients sessions when notifying about changes in watchers.
+	// If we mutate the original role, it will be mutated for all clients
+	// which can cause panics since it causes a race condition.
+	role = apiutils.CloneProtoMsg(role)
 	hasLabelExpression := false
 	for _, kind := range types.LabelMatcherKinds {
 		allowLabelMatchers, err := role.GetLabelMatchers(types.Allow, kind)
@@ -2163,11 +2170,13 @@ func downgradeRoleToV6(r *types.RoleV6) (*types.RoleV6, bool, error) {
 	case types.V3, types.V4, types.V5, types.V6:
 		return r, false, nil
 	case types.V7:
-		var (
-			downgraded types.RoleV6
-			restricted bool
-		)
-		downgraded = *r
+		var restricted bool
+		// Make a shallow copy of the role so that we don't mutate the original.
+		// This is necessary because the role is shared
+		// between multiple clients sessions when notifying about changes in watchers.
+		// If we mutate the original role, it will be mutated for all clients
+		// which can cause panics since it causes a race condition.
+		downgraded := apiutils.CloneProtoMsg(r)
 		downgraded.Version = types.V6
 
 		if len(downgraded.GetKubeResources(types.Deny)) > 0 {
@@ -2225,7 +2234,7 @@ func downgradeRoleToV6(r *types.RoleV6) (*types.RoleV6, bool, error) {
 			restricted = true
 		}
 
-		return &downgraded, restricted, nil
+		return downgraded, restricted, nil
 	default:
 		return nil, false, trace.BadParameter("unrecognized role version %T", r.Version)
 	}
