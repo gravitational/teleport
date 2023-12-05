5ab7eb6564e6a685b87300a5487f8faaa3f53ef4
Lisa Kim <lisa@goteleport.com>
2023-12-05 12:07:12 -0800

    Address cr

--- web/packages/teleterm/src/ui/ConnectMyComputer/NavigationMenu.story.tsx
+++ web/packages/teleterm/src/ui/ConnectMyComputer/NavigationMenu.story.tsx
@@ -159,7 +159,6 @@ function ShowState({
   autoStart?: boolean;
 }) {
   const cluster = makeRootCluster({
-    features: { isUsageBasedBilling: false, advancedAccessWorkflows: false },
     proxyVersion: '17.0.0',
   });
   appContext.clustersService.state.clusters.set(cluster.uri, cluster);
