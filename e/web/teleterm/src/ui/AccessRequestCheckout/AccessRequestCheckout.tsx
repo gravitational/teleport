c6831676fcc6c3293ba03a750478fc1edb329ca7
Maja Cieślak <maja.cieslak@goteleport.com>
2023-12-07 13:04:38 +0100

    Run Storybook smoke tests in CI (#2756)
    * Run Storybook smoke tests in CI
    
    * Add insignificant change in e/web to trigger workflow
    
    Source-commit: gravitational/teleport.e@5e8f02653dfe9806184aa9cdb0939e081bbbc0a5
    Source-pull-requests: gravitational/teleport.e#2756

--- e/web/teleterm/src/ui/AccessRequestCheckout/AccessRequestCheckout.tsx
+++ e/web/teleterm/src/ui/AccessRequestCheckout/AccessRequestCheckout.tsx
@@ -3,7 +3,6 @@ import { Transition } from 'react-transition-group';
 
 import { Box, Flex, ButtonPrimary, ButtonText, Text, ButtonIcon } from 'design';
 import { ChevronDown } from 'design/Icon';
-
 import { pluralize } from 'shared/utils/text';
 
 import { RequestCheckout } from 'e-teleport/Workflow/NewRequest/RequestCheckout/RequestCheckout';
