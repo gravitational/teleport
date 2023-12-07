c79a1ddef8b389d4c27b47568b5cf425391c49ea
Paul Gottschling <paul.gottschling@goteleport.com>
2023-12-07 11:33:03 -0500

    Respond to hugoShaka feedback
    - Disable non-SSH services by default on the agent token and in the
      configuration file written by the init script. Use comments to
      instruct the reader to enable other services.
    - Add instructions in the how-to guide to enable services you want to
      run on agent nodes.
    
    Also removes a statement about the init script that is no longer
    accurate.

--- examples/agent-pool-terraform/token.tf
+++ examples/agent-pool-terraform/token.tf
@@ -7,10 +7,13 @@ resource "teleport_provision_token" "agent" {
   count = var.agent_count
   spec = {
     roles = [
-      "App",
-      "Db",
-      "Discovery",
-      "Kube",
+      // Uncomment the roles that correspond to the Teleport services you plan
+      // to run on your agent nodes. We recommend running the SSH Service at a
+      // minimum to enable secure access to the nodes.
+      // "App",
+      // "Db",
+      // "Discovery",
+      // "Kube",
       "Node",
     ]
     name = random_string.token[count.index].result
