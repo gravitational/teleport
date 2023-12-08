fbccd964ce021f1d34445281308569bd1a2dc13a
Mike Wilson <michael.wilson@goteleport.com>
2023-12-08 10:50:08 -0500

    Add comment about user e-mail expectation.

--- integrations/access/slack/bot.go
+++ integrations/access/slack/bot.go
@@ -124,7 +124,8 @@ func (b Bot) BroadcastAccessRequestMessage(ctx context.Context, recipients []com
 	var data accessrequest.SentMessages
 	var errors []error
 
-	// Fetch the user as a recipient.
+	// Fetch the user as a recipient. The user is expected to be an e-mail here, as should be
+	// the case with most SSO setups.
 	userRecipient, err := b.FetchRecipient(ctx, reqData.User)
 	if err != nil {
 		log.Warningf("Unable to find user %s in Slack, will not be able to notify.", reqData.User)
