3444a3436ec23e98467d26454e20ded862e4683c
Brian Joerger <bjoerger@goteleport.com>
2023-12-15 10:45:05 -0800

    Ensure expiration of Webauthn sessions (#35279)
    * Ensure expiration of webauthn sessions.
    
    * Address comments.
    
    * Add test.
    
    * Resolve comments.
    
    * Refactor test-only methods; Change expired get error to be a not found error.
    
    * Change not found error message.

--- lib/services/local/users_test.go
+++ lib/services/local/users_test.go
@@ -765,6 +765,34 @@ func TestIdentityService_GlobalWebauthnSessionDataCRUD(t *testing.T) {
 	}
 }
 
+func TestIdentityService_WebauthnSessionDataExpiry(t *testing.T) {
+	t.Parallel()
+	clock := clockwork.NewFakeClock()
+	identity := newIdentityService(t, clock)
+	identity.SetDesyncedClockForTesting(clock)
+
+	ctx := context.Background()
+	sessionData := &wanpb.SessionData{Challenge: []byte("challenge"), UserId: []byte("userid")}
+	err := identity.UpsertWebauthnSessionData(ctx, "user", "login", sessionData)
+	require.NoError(t, err)
+
+	got, err := identity.GetWebauthnSessionData(ctx, "user", "login")
+	require.NoError(t, err)
+	require.Equal(t, sessionData, got)
+
+	// If the session data is expired but not automatically removed from
+	// the backend, it should be manually deleted on retrieval attempt.
+	// Advance the identity service clock and not the backend clock to
+	// emulate this occurrence.
+	clock.Advance(10 * time.Minute)
+	_, err = identity.GetWebauthnSessionData(ctx, "user", "login")
+	require.True(t, trace.IsNotFound(err), "expected not found error but got %v", err)
+
+	// Another retrieval should result in a not found error since it was deleted during the last get.
+	_, err = identity.GetWebauthnSessionData(ctx, "user", "login")
+	require.True(t, trace.IsNotFound(err), "expected not found error but got %v", err)
+}
+
 func TestIdentityService_UpsertGlobalWebauthnSessionData_maxLimit(t *testing.T) {
 	// Don't t.Parallel()!
 
