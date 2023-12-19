fc880f1bb2f2c75b92766db76508a3782ea6dfe6
Reed Loden <reed@goteleport.com>
2023-12-19 03:32:11 -0800

    Simplify `Dockerfile-centos7-fips` and prepare for FIPS arm64 builds (#34950)
    Replace `Dockerfile-centos7-fips` with a simple version that just uses
    the existing `Dockerfile-centos7` image with some extra modifications
    (specifically, enabling BoringCrypto for Go and configuring OpenSSL to
    use FIPS mode).
    
    Several changes were made to `Dockerfile-centos7` to support this.
    
    There is a mix of `public.ecr.aws` and `ghcr.io` being used for the
    buildboxes. The default now uses `ghcr.io`.
    
    Buildbox images did not handle varying architectures well, so use
    docker buildx multi-architecture support to make it easy to target
    specific architectures. `--platform` usage has been removed for
    the time being, as it wasn't correctly implemented.
    
    Additionally, start building arm and CentOS 7 FIPS buildbox images
    in GHA to bring it in sync with `dronegen/buildbox.go`.
    
    This does not include any drone/dronegen changes yet. Changes will
    come in a separate PR once this groundwork has been done.
    
    Ref #5068.
    Ref #10581.

--- build.assets/grpcbox.mk
+++ build.assets/grpcbox.mk
@@ -25,7 +25,7 @@ GRPCBOX_RUN := $(DOCKER) run -it --rm -v "$$(pwd)/../:/workdir" -w /workdir $(GR
 # It's leaner, meaner, faster and not supposed to compile code.
 .PHONY: grpcbox
 grpcbox:
-	DOCKER_BUILDKIT=1 $(DOCKER) build \
+	$(DOCKER) buildx build \
 		--build-arg BUF_VERSION=$(BUF_VERSION) \
 		--build-arg GOGO_PROTO_TAG=$(GOGO_PROTO_TAG) \
 		--build-arg NODE_GRPC_TOOLS_VERSION=$(NODE_GRPC_TOOLS_VERSION) \
