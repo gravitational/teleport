74302daba234351ece75d2856d6511371ae4dc29
Logan Davis <logan.davis@goteleport.com>
2023-12-19 17:26:59 -0600

    defer file close

--- tool/teleport/common/teleport.go
+++ tool/teleport/common/teleport.go
@@ -1062,6 +1062,7 @@ func onCloudAWSCredCmd(cloudAWSCredArg string) error {
 	if err != nil {
 		return trace.Wrap(err)
 	}
+	defer credFile.Close()
 
 	if _, err := io.Copy(os.Stdout, credFile); err != nil {
 		return trace.Wrap(err)
