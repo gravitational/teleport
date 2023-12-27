8e444c16674b2bca4bb91fa3ee758e4ed0901a4c
Logan Davis <38335829+logand22@users.noreply.github.com>
2023-12-27 11:27:27 -0600

    Add cloud-aws-cred command (#3014)
    * Add cloud-aws-cred command
    
    * Update to hardcoded path
    
    * Remove unused arg
    
    * Fix comment
    
    * Update tool/teleport/main.go
    
    Co-authored-by: rosstimothy <39066650+rosstimothy@users.noreply.github.com>
    
    * Remove unecessary nil checks
    
    * gci-ed
    
    ---------
    
    Co-authored-by: rosstimothy <39066650+rosstimothy@users.noreply.github.com>
    
    Source-commit: gravitational/teleport.e@866237e71a9f5b291ff84428e9ef400cb905dd7b
    Source-pull-requests: gravitational/teleport.e#3014

--- e/tool/teleport/main.go
+++ e/tool/teleport/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"context"
+	"io"
 	"os"
 
+	"github.com/gravitational/trace"
+
+	"github.com/gravitational/teleport/e/lib/licensefile"
 	"github.com/gravitational/teleport/e/tool/teleport/process"
 	"github.com/gravitational/teleport/lib/observability/metrics"
 	"github.com/gravitational/teleport/lib/service"
+	"github.com/gravitational/teleport/lib/service/servicecfg"
 	"github.com/gravitational/teleport/lib/utils"
 	"github.com/gravitational/teleport/tool/teleport/common"
 )
@@ -17,16 +22,52 @@ func init() {
 
 func main() {
 	app, executedCommand, config := common.Run(common.Options{
-		Args:     os.Args[1:],
-		InitOnly: true,
+		Args:                  os.Args[1:],
+		InitOnly:              true,
+		EnableCloudAWSCredCmd: true,
 	})
 	startCmd := app.GetCommand("start")
 	appStartCmd := app.GetCommand("app").GetCommand("start")
 	dbStartCmd := app.GetCommand("db").GetCommand("start")
+	cloudAWSCredCmd := app.GetCommand("cloud-aws-cred")
 	switch executedCommand {
 	case startCmd.FullCommand(), appStartCmd.FullCommand(), dbStartCmd.FullCommand():
 		if err := service.Run(context.Background(), *config, process.NewTeleport); err != nil {
 			utils.FatalError(err)
 		}
+	case cloudAWSCredCmd.FullCommand():
+		if err := onCloudAWSCredCmd(config); err != nil {
+			utils.FatalError(err)
+		}
 	}
+}
+
+// onCloudAWSCredCmd used with the credential process provider in AWS Shared Credential File
+// to enable the cloud team to dynamically refresh AWS temporary credentials without restarting teleport.
+func onCloudAWSCredCmd(cfg *servicecfg.Config) error {
+	// cloudCredFile is defined in the cloud repository by the following code:
+	// * https://github.com/gravitational/cloud/blob/2eae7dd12f81e2fd1b3e666ed1ae7b476345511c/pkg/teleportcontroller/deployments.go#L893
+	// * https://github.com/gravitational/cloud/blob/2eae7dd12f81e2fd1b3e666ed1ae7b476345511c/pkg/teleportcontroller/iam_secret.go#L118
+	const cloudCredFile = "/etc/aws-iam/credentials.json"
+
+	licenseFile, err := licensefile.NewLicenseFile(cfg.Auth.LicenseFile)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	if !licenseFile.License.GetCloud().Value() {
+		return trace.Errorf("This command can only be run from the cloud environment")
+	}
+
+	credFile, err := os.Open(cloudCredFile)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	defer credFile.Close()
+
+	if _, err := io.Copy(os.Stdout, credFile); err != nil {
+		return trace.Wrap(err)
+	}
+
+	return nil
 }
