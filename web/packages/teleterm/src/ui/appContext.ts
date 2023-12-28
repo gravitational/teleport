5f59c5ef4057f1bd86e3c8d9c75c2cd0df63ee70
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2023-12-28 13:44:38 +0100

    Expose `tshd` in `IAppContext`

--- web/packages/teleterm/src/ui/appContext.ts
+++ web/packages/teleterm/src/ui/appContext.ts
@@ -41,6 +41,7 @@ import { UsageService } from 'teleterm/ui/services/usage';
 import { ResourcesService } from 'teleterm/ui/services/resources';
 import { ConnectMyComputerService } from 'teleterm/ui/services/connectMyComputer';
 import { ConfigService } from 'teleterm/services/config';
+import { TshClient } from 'teleterm/services/tshd/types';
 import { IAppContext } from 'teleterm/ui/types';
 import { DeepLinksService } from 'teleterm/ui/services/deepLinks';
 import { parseDeepLink } from 'teleterm/deepLinks';
@@ -61,6 +62,7 @@ export default class AppContext implements IAppContext {
   connectionTracker: ConnectionTrackerService;
   fileTransferService: FileTransferService;
   resourcesService: ResourcesService;
+  tshd: TshClient;
   /**
    * subscribeToTshdEvent lets you add a listener that's going to be called every time a client
    * makes a particular RPC to the tshd events service. The listener receives the request converted
@@ -86,6 +88,7 @@ export default class AppContext implements IAppContext {
   constructor(config: ElectronGlobals) {
     const { tshClient, ptyServiceClient, mainProcessClient } = config;
     this.logger = new Logger('AppContext');
+    this.tshd = tshClient;
     this.subscribeToTshdEvent = config.subscribeToTshdEvent;
     this.mainProcessClient = mainProcessClient;
     this.notificationsService = new NotificationsService();
