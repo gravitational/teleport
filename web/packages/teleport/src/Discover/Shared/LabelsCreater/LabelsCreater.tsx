afc2f96d25910bae3538d5102ec04725baab8bcf
sshahcodes <sshah@goteleport.com>
2024-01-03 17:59:22 -0500

    remove  from labelsCreater.tsx

--- web/packages/teleport/src/Discover/Shared/LabelsCreater/LabelsCreater.tsx
+++ web/packages/teleport/src/Discover/Shared/LabelsCreater/LabelsCreater.tsx
@@ -176,9 +176,6 @@ export function LabelsCreater({
         })}
       </Box>
       <ButtonTextWithAddIcon
-        css={`
-          margin-top: -2px;
-        `}
         label="Add New Label"
         onClick={addLabel}
         disabled={disableBtns}
