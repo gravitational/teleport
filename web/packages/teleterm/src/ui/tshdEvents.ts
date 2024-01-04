5a52666547f286183387f27a80128639fbfed4da
Rafał Cieślak <rafal.cieslak@goteleport.com>
2024-01-04 17:29:00 +0100

    Show MFA modal on promptMFA

--- web/packages/teleterm/src/ui/tshdEvents.ts
+++ web/packages/teleterm/src/ui/tshdEvents.ts
@@ -51,8 +51,37 @@ export function createTshdEventsContextBridgeService(
       return {};
     },
 
-    promptMFA: async () => {
-      throw new Error('Not implemented yet');
+    promptMFA: async ({ request, onRequestCancelled }) => {
+      const { totpCode, hasCanceledModal } = await new Promise<{
+        totpCode: string;
+        hasCanceledModal: boolean;
+      }>(resolve => {
+        const { closeDialog } = ctx.modalsService.openImportantDialog({
+          kind: 'reauthenticate',
+          promptMfaRequest: request as tshdEvents.PromptMfaRequest,
+          onSuccess: totpCode => resolve({ hasCanceledModal: false, totpCode }),
+          onCancel: () =>
+            resolve({ hasCanceledModal: true, totpCode: undefined }),
+        });
+
+        // If Webauthn is available, tshd starts two goroutines – one that sends this request and
+        // one that starts listening for a hardware key tap. When a tap is detected, tshd cancels
+        // this request.
+        onRequestCancelled(closeDialog);
+      });
+
+      if (hasCanceledModal) {
+        // Throwing an object here instead of an error to future-proof this code in case we need to
+        // return more than just the error name and message.
+        // Refer to processEvent in the preload part of tshd events service for more details.
+        throw {
+          isCrossContextError: true,
+          name: 'AbortError',
+          message: 'MFA modal closed by user',
+        };
+      }
+
+      return { totpCode };
     },
   };
 }
