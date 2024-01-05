089dc3d7cc0b6bb08b6ab6b355d3bf6c8e84068b
Reed Loden <reed@goteleport.com>
2024-01-05 07:55:04 -0800

    [v12] Update Rust version to 1.71.1
    Backport of #30281.
    
    The project's Rust version was updated from 1.68.0 to 1.71.1 in the versions.mk file.

--- build.assets/versions.mk
+++ build.assets/versions.mk
@@ -6,7 +6,7 @@ GOLANG_VERSION ?= go1.20.12
 NODE_VERSION ?= 18.18.2
 
 # Run lint-rust check locally before merging code after you bump this.
-RUST_VERSION ?= 1.68.0
+RUST_VERSION ?= 1.71.1
 LIBBPF_VERSION ?= 1.2.2
 LIBPCSCLITE_VERSION ?= 1.9.9-teleport
 
