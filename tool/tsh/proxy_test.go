ce35bda10de6e1967544ea0eabda2bcf172ea1f5
Nic Klaassen <nic@goteleport.com>
2024-01-08 07:40:03 -0800

    revert whitespace changes

--- tool/tsh/proxy_test.go
+++ tool/tsh/proxy_test.go
@@ -949,11 +949,11 @@ Learn more at https://goteleport.com/docs/connect-your-client/teleport-connect/#
 
 Use one of the following commands to connect to the database or to the address above using other database GUI/CLI clients:
 
-  * default:
+  * default: 
 
   $ echo "hello world"
 
-  * alternative:
+  * alternative: 
 
   $ echo "goodbye world"
 
@@ -1008,11 +1008,11 @@ Learn more at https://goteleport.com/docs/connect-your-client/teleport-connect/#
 
 Use one of the following commands to connect to the database or to the address above using other database GUI/CLI clients:
 
-  * default:
+  * default: 
 
   $ echo "hello world"
 
-  * alternative:
+  * alternative: 
 
   $ echo "goodbye world"
 
