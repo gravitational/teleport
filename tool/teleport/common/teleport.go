564c70050ac93f5728e0a81e914af6ff8ce0dfeb
Logan Davis <logan.davis@goteleport.com>
2024-01-10 18:08:43 -0600

    Configure config to respect the license file locations

--- tool/teleport/common/teleport.go
+++ tool/teleport/common/teleport.go
@@ -504,7 +504,10 @@ func Run(options Options) (app *kingpin.Application, executedCommand string, con
 	integrationConfExternalAuditCmd.Flag("aws-partition", "AWS partition (default: aws).").Default("aws").StringVar(&ccf.IntegrationConfExternalAuditStorageArguments.Partition)
 
 	if options.EnableCloudAWSCredCmd {
-		app.Command("cloud-aws-cred", "Helper command used by Teleport Cloud to produce credentials.").Hidden()
+		cloudAWSCred := app.Command("cloud-aws-cred", "Helper command used by Teleport Cloud to produce credentials.").Hidden()
+		cloudAWSCred.Flag("config",
+			fmt.Sprintf("Path to a configuration file [%v]", defaults.ConfigFilePath)).
+			Short('c').ExistingFileVar(&ccf.ConfigFile)
 	}
 
 	// parse CLI commands+flags:
@@ -608,6 +611,13 @@ func Run(options Options) (app *kingpin.Application, executedCommand string, con
 	if err != nil {
 		utils.FatalError(err)
 	}
+
+	if options.EnableCloudAWSCredCmd && command == app.GetCommand("cloud-aws-cred").FullCommand() {
+		if err = config.Configure(&ccf, conf, false); err != nil {
+			utils.FatalError(err)
+		}
+	}
+
 	return app, command, conf
 }
 
