03d14b0f1a600bf6a66459a86731d212224226c1
Lisa Kim <lisa@goteleport.com>
2024-01-10 20:36:16 -0800

    Add alert outline-info

--- web/packages/design/src/Alert/Alert.story.js
+++ web/packages/design/src/Alert/Alert.story.js
@@ -32,5 +32,6 @@ export const Alerts = () => (
     <Alert kind="warning">Some warning message</Alert>
     <Alert kind="info">Some informational message</Alert>
     <Alert kind="success">This is success</Alert>
+    <Alert kind="outline-info">Text align it yourself</Alert>
   </Box>
 );
