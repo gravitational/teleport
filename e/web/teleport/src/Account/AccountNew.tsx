c6bb279f33c8fb731f27b77038a361bcaada20b1
Bartosz Leper <bartosz.leper@goteleport.com>
2024-01-12 17:47:29 +0100

    Don't show recovery codes if this feature is not enabled (#3150)
    * Don't show recovery codes if this feature is not enabled
    
    (Also don't fetch the metadata in this case, because it may return 404.)
    
    * Review
    
    * Fix lint
    
    Source-commit: gravitational/teleport.e@5d288e6eba742fde9bfa760ebf38fd53591b7fb6
    Source-pull-requests: gravitational/teleport.e#3150

--- e/web/teleport/src/Account/AccountNew.tsx
+++ e/web/teleport/src/Account/AccountNew.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import AccountPage from 'teleport/Account/AccountNew';
 
+import cfg from 'e-teleport/config';
+
 import Recovery from './Recovery/RecoveryNew';
 
 export default function Account() {
-  return <AccountPage enterpriseComponent={Recovery} />;
+  return (
+    <AccountPage
+      enterpriseComponent={cfg.oss.recoveryCodesEnabled ? Recovery : undefined}
+    />
+  );
 }
