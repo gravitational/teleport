e4fac74fbc97371209647eeaba75f0eb2243d338
Michael Myers <michael.myers@goteleport.com>
2024-01-12 12:36:14 -0600

    Update Toggle component styles

--- web/packages/design/src/Label/Label.jsx
+++ web/packages/design/src/Label/Label.jsx
@@ -46,7 +46,7 @@ const kind = ({ kind, theme }) => {
 
   if (kind === 'success') {
     return {
-      backgroundColor: theme.colors.success,
+      backgroundColor: theme.colors.success.main,
       color: theme.colors.text.primaryInverse,
     };
   }
