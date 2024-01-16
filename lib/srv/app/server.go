c15b444c3146ebfea7544ddd6d4888985fb367c0
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2024-01-16 09:11:06 -0500

    Prevent ConnectionMonitor leaks (#36669)
    The context used to create the connection monitor for app sessions
    was never canceled which left the connection monitor running until
    the certificate expired, the user was locked, or the idle timeout
    was enforced. By using the correct context and canceling appropriately
    the monitor is terminated as soon as the app session has concluded.

--- lib/srv/app/server.go
+++ lib/srv/app/server.go
@@ -720,7 +720,7 @@ func (s *Server) handleConnection(conn net.Conn) (func(), error) {
 		return nil, trace.Wrap(err)
 	}
 
-	ctx = authz.ContextWithUser(s.closeContext, user)
+	ctx = authz.ContextWithUser(ctx, user)
 	ctx = authz.ContextWithClientSrcAddr(ctx, conn.RemoteAddr())
 	authCtx, _, err := s.authorizeContext(ctx)
 
@@ -748,12 +748,15 @@ func (s *Server) handleConnection(conn net.Conn) (func(), error) {
 	// differently than HTTP requests from web apps.
 	if app.IsTCP() {
 		identity := authCtx.Identity.GetIdentity()
-		return nil, s.handleTCPApp(ctx, tlsConn, &identity, app)
+		defer cancel()
+		return nil, trace.Wrap(s.handleTCPApp(ctx, tlsConn, &identity, app))
 	}
 
-	return func() {
+	cleanup := func() {
+		cancel()
 		s.deleteConnAuth(tlsConn)
-	}, s.handleHTTPApp(ctx, tlsConn)
+	}
+	return cleanup, trace.Wrap(s.handleHTTPApp(ctx, tlsConn))
 }
 
 // handleTCPApp handles connection for a TCP application.
