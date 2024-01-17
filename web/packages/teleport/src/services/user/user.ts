409e9a564c43be5668f8b3babea0a0eea7d252c7
joerger <bjoerger@goteleport.com>
2024-01-16 20:50:35 -0800

    Add admin action option web.isMFARequired; Add IsMFARequired check to web.createAuthenticateChallenge.

--- web/packages/teleport/src/services/user/user.ts
+++ web/packages/teleport/src/services/user/user.ts
@@ -61,12 +61,12 @@ const service = {
   },
 
   createUser(user: User) {
-    return api.post(cfg.getUsersUrl(), user).then(makeUser);
+    return api.post(cfg.getUsersUrl(), user, null).then(makeUser);
   },
 
   createResetPasswordToken(name: string, type: ResetPasswordType) {
     return api
-      .post(cfg.api.resetPasswordTokenPath, { name, type })
+      .post(cfg.api.resetPasswordTokenPath, { name, type }, null)
       .then(makeResetToken);
   },
 
