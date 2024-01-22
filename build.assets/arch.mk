fb5949add35ac7c6b27c0964d9c7be0b3a26ce67
Walt Della <walt@goteleport.com>
2024-01-22 11:13:12 -0800

    Force go to be installed

--- build.assets/arch.mk
+++ build.assets/arch.mk
@@ -1,8 +1,13 @@
 # These variables are extracted from build.assets/Makefile so they can be imported
 # by other Makefiles
 ifeq ($(origin ARCH), undefined) # avoid use of "ARCH ?= $(...)" as the lazy loading will repeatedly re-run the same shell command
-	ARCH := $(shell go env GOARCH)
+GO_BINARY := $(shell command -v go 2>/dev/null)
+ifndef GO_BINARY
+$(error go not installed to determine ARCH))
 endif
+ARCH := $(shell go env GOARCH)
+endif
+
 HOST_ARCH := $(shell uname -m)
 RUNTIME_ARCH_x86_64 := amd64
 # uname returns different value on Linux (aarch64) and macOS (arm64).
