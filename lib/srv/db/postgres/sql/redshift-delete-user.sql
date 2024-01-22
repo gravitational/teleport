e51c5afc2618a79befce118b2a008927cf289b15
STeve Huang <xin.huang@goteleport.com>
2024-01-22 11:14:58 -0500

    update comment with link

--- lib/srv/db/postgres/sql/redshift-delete-user.sql
+++ lib/srv/db/postgres/sql/redshift-delete-user.sql
@@ -3,6 +3,9 @@ LANGUAGE plpgsql
 AS $$
 BEGIN
     -- Only drop if the user doesn't have other active sessions.
+    -- Update to pg_stat_activity is delayed for a few hundred ms. Use
+    -- stv_sessions instead:
+    -- https://docs.aws.amazon.com/redshift/latest/dg/r_STV_SESSIONS.html
     IF EXISTS (SELECT user_name FROM stv_sessions WHERE user_name = CONCAT('IAM:', username)) THEN
         RAISE EXCEPTION 'TP000: User has active connections';
     ELSE
