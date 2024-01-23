f40495151bf4ba4b5b0de5139c2c614193d48bce
Alan Parra <alan.parra@goteleport.com>
2024-01-23 16:25:25 -0300

    test: Fix minor lint issues

--- lib/auth/webauthncli/fido2_test.go
+++ lib/auth/webauthncli/fido2_test.go
@@ -144,7 +144,7 @@ func (p *pinCancelPrompt) PromptPIN() (string, error) {
 	return p.pin, nil
 }
 
-func (p pinCancelPrompt) PromptTouch() (wancli.TouchAcknowledger, error) {
+func (p *pinCancelPrompt) PromptTouch() (wancli.TouchAcknowledger, error) {
 	// 2nd touch never happens
 	return func() error { return nil }, nil
 }
@@ -159,7 +159,7 @@ func TestIsFIDO2Available(t *testing.T) {
 		{
 			name: "env var unset",
 			setenv: func() {
-				os.Unsetenv(fido2Key)
+				_ = os.Unsetenv(fido2Key)
 			},
 			want: true,
 		},
