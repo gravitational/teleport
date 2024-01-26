787b19f2d4e7b362883905610a652f77479df35d
Grzegorz Zdunek <gzdunek@users.noreply.github.com>
2024-01-26 11:46:03 +0100

    Allow launching web apps in Connect (#37246)
    * Add `getWebAppLaunchUrl` function
    
    * Add launch button to resources view
    
    * Launch web apps from the search bar in a browser
    
    * Rename `actionButtons.tsx` to `ActionButtons.tsx`
    
    * Move `getWebAppLaunchUrl` and `isWebApp` to `app.ts`, improve API
    
    * Simplify check in `getWebAppLaunchUrl`
    
    * Improve copies
    
    * `launchInBrowser` -> `launchInBrowserIfWebApp`
    
    * Fix comment
    
    * Add `noopener` to `window.open`

--- web/packages/teleterm/src/ui/services/workspacesService/documentsService/connectToApp.ts
+++ web/packages/teleterm/src/ui/services/workspacesService/documentsService/connectToApp.ts
@@ -20,13 +20,17 @@ import { routing } from 'teleterm/ui/uri';
 import { IAppContext } from 'teleterm/ui/types';
 
 import { App } from 'teleterm/services/tshd/types';
+import { getWebAppLaunchUrl, isWebApp } from 'teleterm/services/tshd/app';
 
 import { DocumentOrigin } from './types';
 
 export async function connectToApp(
   ctx: IAppContext,
   target: App,
-  telemetry: { origin: DocumentOrigin }
+  telemetry: { origin: DocumentOrigin },
+  options?: {
+    launchInBrowserIfWebApp: boolean;
+  }
 ): Promise<void> {
   //TODO(gzdunek): Add regular dialogs for connecting to unsupported apps (non HTTP/TCP)
   // that will explain that the user can connect via tsh/Web UI to them.
@@ -43,6 +47,23 @@ export async function connectToApp(
 
   if (target.endpointUri.startsWith('cloud://')) {
     alert('Cloud apps are supported only in tsh.');
+    return;
+  }
+
+  if (isWebApp(target) && options?.launchInBrowserIfWebApp) {
+    captureAppLaunchInBrowser(ctx, target, telemetry);
+    const rootCluster = ctx.clustersService.findCluster(
+      routing.ensureRootClusterUri(target.uri)
+    );
+    const cluster = ctx.clustersService.findClusterByResource(target.uri);
+    // Generally, links should be opened with <a> elements.
+    // Unfortunately, in some cases it is not possible,
+    // for example, in the search bar.
+    window.open(
+      getWebAppLaunchUrl({ app: target, rootCluster, cluster }),
+      '_blank',
+      'noreferrer,noopener'
+    );
     return;
   }
 
@@ -67,4 +88,16 @@ export async function connectToApp(
     documentsService.add(doc);
     documentsService.open(doc.uri);
   }
+}
+
+/**
+ * When the app is opened outside Connect,
+ * the usage event has to be captured manually.
+ */
+export function captureAppLaunchInBrowser(
+  ctx: IAppContext,
+  target: Pick<App, 'uri'>,
+  telemetry: { origin: DocumentOrigin }
+) {
+  ctx.usageService.captureProtocolUse(target.uri, 'app', telemetry.origin);
 }
