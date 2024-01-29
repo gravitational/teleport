b96902d0590c3063ba7b2cfb3a8a379000cf90ab
Hugo Shaka <hugo.hervieux@goteleport.com>
2024-01-29 09:18:34 -0500

    [v13] Fix a bug causing the webUI to be unusable if automatic upgrades are misconfigured (#37131)
    * Fix a bug causing the webUI to be unusable if automatic upgrades are misconfigured (#37091)
    
    * Fix a bug causing the webUI to be unusable if automatic upgrades are misconfigured
    
    * document which env var
    
    * add sync
    
    * remove sync not avilable in this go version
    
    * lint

--- lib/web/apiserver.go
+++ lib/web/apiserver.go
@@ -1492,9 +1492,7 @@ func (h *Handler) getWebConfig(w http.ResponseWriter, r *http.Request, p httprou
 	var automaticUpgradesTargetVersion string
 	if automaticUpgradesEnabled {
 		automaticUpgradesTargetVersion, err = h.cfg.AutomaticUpgradesChannels.DefaultVersion(r.Context())
-		if err != nil {
-			return nil, trace.Wrap(err)
-		}
+		h.log.WithError(err).Error("Cannot read target version")
 	}
 
 	webCfg := webclient.WebConfig{
