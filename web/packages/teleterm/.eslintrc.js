8a948946e3232dadfd08ffb1c26ffe5903dba596
Rafał Cieślak <rafal.cieslak@goteleport.com>
2024-01-29 15:06:46 +0100

    Clean up ESLint's ignorePatterns
    *_pb.js has been long ignored through .eslintignore. dist is no longer
    used as the build output.

--- web/packages/teleterm/.eslintrc.js
+++ web/packages/teleterm/.eslintrc.js
@@ -1,6 +1,5 @@
 const eslintConfig = require('@gravitational/build/.eslintrc');
 
-eslintConfig.ignorePatterns = ['**/tshd/**/*_pb.js'];
 eslintConfig.rules['no-console'] = 'off';
 
 module.exports = eslintConfig;
