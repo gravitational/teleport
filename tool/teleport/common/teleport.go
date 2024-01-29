259946cb736b603ac556c38fd5a866c7aacb93ea
Marco André Dinis <marco.dinis@goteleport.com>
2024-01-29 20:53:06 +0000

    AWS OIDC: remove config dependency in ExternalAuditStorage (#37315)
    * AWS OIDC: remove config dependency in ExternalAuditStorage
    
    Currently the ExternalAuditStorage methods require the `config` package
    to re-use the configuration struct used in `teleport integration configure`
    
    We are refactoring the AWS OIDC actions to use a gRPC Service.
    Importing the `config` package makes this refactoring a bit difficult
    because of a cycling dependency: `awsoidc` requires `config` which
    requires `auth` which will require the `awsoidc service` which requires
    the `awsoidc` package.
    
    This PR removes the usage of the `config` package in `awsoidc` package.
    
    * rename type and godoc
    
    * move type to servicecfg
    
    * fix gci

--- tool/teleport/common/teleport.go
+++ tool/teleport/common/teleport.go
@@ -1028,7 +1028,7 @@ func onIntegrationConfListDatabasesIAM(params config.IntegrationConfListDatabase
 	return nil
 }
 
-func onIntegrationConfExternalAuditCmd(params config.IntegrationConfExternalAuditStorage) error {
+func onIntegrationConfExternalAuditCmd(params servicecfg.ExternalAuditStorageConfiguration) error {
 	ctx := context.Background()
 	cfg, err := awsConfig.LoadDefaultConfig(ctx, awsConfig.WithRegion(params.Region))
 	if err != nil {
