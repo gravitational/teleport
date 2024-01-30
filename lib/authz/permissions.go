3bede180f257f8223e1d75130687f55fe149c8c2
joerger <bjoerger@goteleport.com>
2024-01-30 13:45:26 -0800

    Add authz context helper methods for authorizing admin actions.

--- lib/authz/permissions.go
+++ lib/authz/permissions.go
@@ -1450,6 +1450,7 @@ func (c *Context) CheckAccessToRule(quiet bool, ruleCtx *services.Context, kind 
 }
 
 // AuthorizeAdminAction will ensure that the user is authorized to perform admin actions.
+// TODO(Joerger): replace with authCtx.AuthorizeAdminAction
 func AuthorizeAdminAction(ctx context.Context, authCtx *Context) error {
 	switch authCtx.AdminActionAuthState {
 	case AdminActionAuthMFAVerified, AdminActionAuthNotRequired:
@@ -1460,11 +1461,30 @@ func AuthorizeAdminAction(ctx context.Context, authCtx *Context) error {
 
 // AuthorizeAdminActionAllowReusedMFA will ensure that the user is authorized to perform
 // admin actions. Additionally, MFA challenges that allow reuse will be accepted.
+// TODO(Joerger): replace with authCtx.AuthorizeAdminActionAllowReusedMFA
 func AuthorizeAdminActionAllowReusedMFA(ctx context.Context, authCtx *Context) error {
 	if authCtx.AdminActionAuthState == AdminActionAuthMFAVerifiedWithReuse {
 		return nil
 	}
 	return AuthorizeAdminAction(ctx, authCtx)
+}
+
+// AuthorizeAdminAction will ensure that the user is authorized to perform admin actions.
+func (c *Context) AuthorizeAdminAction() error {
+	switch c.AdminActionAuthState {
+	case AdminActionAuthMFAVerified, AdminActionAuthNotRequired:
+		return nil
+	}
+	return trace.Wrap(&mfa.ErrAdminActionMFARequired)
+}
+
+// AuthorizeAdminActionAllowReusedMFA will ensure that the user is authorized to perform
+// admin actions. Additionally, MFA challenges that allow reuse will be accepted.
+func (c *Context) AuthorizeAdminActionAllowReusedMFA() error {
+	if c.AdminActionAuthState == AdminActionAuthMFAVerifiedWithReuse {
+		return nil
+	}
+	return c.AuthorizeAdminAction()
 }
 
 // LocalUser is a local user
