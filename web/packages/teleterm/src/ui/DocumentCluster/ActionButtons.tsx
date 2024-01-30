0d2443a228acced249b50bed57d987bdc1c47b43
Grzegorz Zdunek <gzdunek@users.noreply.github.com>
2024-01-30 18:23:26 +0100

    [v15] Support SAML apps in Connect (#37531)
    * Support SAML apps
    
    * Add stories
    
    * Add articles
    
    * Remove 'app' from copy in the search bar
    
    * Replace ternary condition with simple ifs
    
    * Make test name more grammatically correct
    
    * Report opening AWS and SAML apps
    
    * Display button as link
    
    * `it` -> `test`

--- web/packages/teleterm/src/ui/DocumentCluster/ActionButtons.tsx
+++ web/packages/teleterm/src/ui/DocumentCluster/ActionButtons.tsx
@@ -47,6 +47,7 @@ import {
   getWebAppLaunchUrl,
   isWebApp,
   getAwsAppLaunchUrl,
+  getSamlAppSsoUrl,
 } from 'teleterm/services/tshd/app';
 
 export function ConnectServerActionButton(props: {
@@ -229,7 +230,27 @@ function AppButton(props: {
             arn,
           })
         }
+        onLaunchUrl={props.onLaunchUrl}
       />
+    );
+  }
+
+  if (props.app.samlApp) {
+    return (
+      <ButtonBorder
+        size="small"
+        onClick={props.onLaunchUrl}
+        as="a"
+        textTransform="none"
+        title="Log in to the app in the browser"
+        href={getSamlAppSsoUrl({
+          app: props.app,
+          rootCluster: props.rootCluster,
+        })}
+        target="_blank"
+      >
+        Login
+      </ButtonBorder>
     );
   }
 
@@ -247,7 +268,7 @@ function AppButton(props: {
           })}
           onClick={props.onLaunchUrl}
           target="_blank"
-          title="Launch app in the browser"
+          title="Launch the app in the browser"
           css={`
             border-top-right-radius: 0;
             border-bottom-right-radius: 0;
