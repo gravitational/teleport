583ba09f1695cd6bdf06f00719ec7e999d5c08c7
Tiago Silva <tiago.silva@goteleport.com>
2024-01-31 09:55:42 +0000

    Correct gRPC handler name `StreamUnstructuredSessionEvents`
    This PR fixes the name of the gRPC method
    `StreamUnstructuredSessionEvents` from
    `StreamUnstructuredSessionEventsServer` to the correct version. This typo was introduced during the original review and was never detected because clients implemented a fallback mechanism to old `StreamSessionEvents` api.
    
    This PR also partially reverts commit
    386e63c5849e19bd5b55fd2ac19ed33d3dca3e32 so v15 clients can still connect to v14 clusters.
    
    Signed-off-by: Tiago Silva <tiago.silva@goteleport.com>

--- lib/auth/grpcserver.go
+++ lib/auth/grpcserver.go
@@ -4445,7 +4445,6 @@ func (g *GRPCServer) ListUnifiedResources(ctx context.Context, req *authpb.ListU
 	}
 
 	return auth.ListUnifiedResources(ctx, req)
-
 }
 
 // ListResources retrieves a paginated list of resources.
@@ -5575,8 +5574,8 @@ func (g *GRPCServer) GetUnstructuredEvents(ctx context.Context, req *auditlogpb.
 	}, nil
 }
 
-// StreamUnstructuredSessionEventsServer streams all events from a given session recording as an unstructured format.
-func (g *GRPCServer) StreamUnstructuredSessionEventsServer(req *auditlogpb.StreamUnstructuredSessionEventsRequest, stream auditlogpb.AuditLogService_StreamUnstructuredSessionEventsServer) error {
+// StreamUnstructuredSessionEvents streams all events from a given session recording as an unstructured format.
+func (g *GRPCServer) StreamUnstructuredSessionEvents(req *auditlogpb.StreamUnstructuredSessionEventsRequest, stream auditlogpb.AuditLogService_StreamUnstructuredSessionEventsServer) error {
 	auth, err := g.authenticate(stream.Context())
 	if err != nil {
 		return trace.Wrap(err)
