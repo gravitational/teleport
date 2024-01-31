fd65f29d636baf800918d7e94b57556ccdb32dad
Sakshyam Shah <sshah@goteleport.com>
2024-01-30 21:12:29 -0500

    updates TextSelectCopyMulti to save content as file (#37232)
    * feat: save content as file
    
    * add min height to have consistent height on empty text
    
    * run linter and update snapshot
    
    * move download icon to the right of copy icon
    
    * increase left padding when showing download icon
    
    * accomodate vertical and horizontal scroller and icon button view
    
    * move download function to shared utils
    
    * rename downloadContentAsFile to saveOnDisk, unit test
    
    * original style with increased padding for download icon

---
+++ web/packages/shared/utils/saveOnDisk.ts
@@ -0,0 +1,37 @@
+/**
+ * Teleport
+ * Copyright (C) 2023  Gravitational, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * saveOnDisk saves content to local disk.
+ * @param content content to download.
+ * @param filename preset file name.
+ * @param fileType file type.
+ */
+export function saveOnDisk(
+  content: string,
+  filename: string,
+  fileType: string
+): void {
+  const a = document.createElement('a');
+  const blob = new Blob([content], { type: fileType });
+  a.href = window.URL.createObjectURL(blob);
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
