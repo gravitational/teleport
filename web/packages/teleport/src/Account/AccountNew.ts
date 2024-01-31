ef8e0c68bb404fb1d720f29d28a5cd5eddb88c89
Bartosz Leper <bartosz.leper@goteleport.com>
2024-01-31 10:28:50 +0100

    Add a compatibility alias

---
+++ web/packages/teleport/src/Account/AccountNew.ts
@@ -0,0 +1,6 @@
+// Compatibility aliases to prevent e/ from breaking.
+// TODO(bl-nero): Remove these once e/ stops referring to them.
+import Account from './Account';
+export type { EnterpriseComponentProps } from './Account';
+
+export default Account;
