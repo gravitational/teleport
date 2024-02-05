f23ad1620bd2668ba943e516e03e30bb8eb1773c
Forrest Marshall <forrest@goteleport.com>
2024-02-05 21:57:29 +0000

    precalculate batch items & fix nits

--- lib/backend/pgbk/atomicwrite.go
+++ lib/backend/pgbk/atomicwrite.go
@@ -36,124 +36,115 @@ func (b *Backend) AtomicWrite(ctx context.Context, condacts []backend.Conditiona
 
 	newRevision := newRevision()
 
-	var includesPut bool
+	type batchItem struct {
+		query     string
+		arguments []any
+	}
+	var condBatchItems, actBatchItems []batchItem
+	var actBatchIncludesPut bool
 
-	var n int
+	for _, ca := range condacts {
+		switch ca.Condition.Kind {
+		case backend.KindWhatever:
+			// no comparison to assert
+		case backend.KindExists:
+			condBatchItems = append(condBatchItems, batchItem{
+				"SELECT EXISTS (SELECT FROM kv WHERE key = $1 AND (expires IS NULL OR expires >= now()))",
+				[]any{nonNil(ca.Key)},
+			})
+		case backend.KindNotExists:
+			condBatchItems = append(condBatchItems, batchItem{
+				"SELECT NOT EXISTS (SELECT FROM kv WHERE key = $1 AND (expires IS NULL OR expires >= now()))",
+				[]any{nonNil(ca.Key)},
+			})
+		case backend.KindRevision:
+			expectedRevision, ok := revisionFromString(ca.Condition.Revision)
+			if !ok {
+				return "", trace.Wrap(backend.ErrConditionFailed)
+			}
+			condBatchItems = append(condBatchItems, batchItem{
+				"SELECT EXISTS (SELECT FROM kv WHERE key = $1 AND revision = $2 AND (expires IS NULL OR expires >= now()))",
+				[]any{nonNil(ca.Key), expectedRevision},
+			})
+		default:
+			// condacts was already checked for validity
+			return "", trace.BadParameter("unexpected condition kind %v in conditional action against key %q (this is a bug)", ca.Condition.Kind, ca.Key)
+		}
 
-	_, err = pgcommon.Retry(ctx, b.log, func() (struct{}, error) {
-		n++
-		var conditionFailed bool
+		switch ca.Action.Kind {
+		case backend.KindNop:
+			// no action to be taken
+		case backend.KindPut:
+			actBatchIncludesPut = true
+			actBatchItems = append(actBatchItems, batchItem{
+				"INSERT INTO kv (key, value, expires, revision) VALUES ($1, $2, $3, $4)" +
+					" ON CONFLICT (key) DO UPDATE SET" +
+					" value = excluded.value, expires = excluded.expires, revision = excluded.revision",
+				[]any{nonNil(ca.Key), nonNil(ca.Action.Item.Value), zeronull.Timestamptz(ca.Action.Item.Expires.UTC()), newRevision},
+			})
+		case backend.KindDelete:
+			actBatchItems = append(actBatchItems, batchItem{
+				"DELETE FROM kv WHERE kv.key = $1 AND (kv.expires IS NULL OR kv.expires > now())",
+				[]any{nonNil(ca.Key)},
+			})
+		default:
+			// condacts was already checked for validity
+			return "", trace.BadParameter("unexpected action kind %v in conditional action against key %q (this is a bug)", ca.Action.Kind, ca.Key)
+		}
+	}
+
+	var success bool
+	querySuccess := func(row pgx.Row) error {
+		if !success {
+			return nil
+		}
+		return trace.Wrap(row.Scan(&success))
+	}
+
+	var tries int
+	err = pgcommon.RetryTx(ctx, b.log, b.pool, pgx.TxOptions{}, false, func(tx pgx.Tx) error {
+		tries++
+
 		var condBatch, actBatch pgx.Batch
-
-		for _, ca := range condacts {
-			switch ca.Condition.Kind {
-			case backend.KindWhatever:
-				// no comparison to assert
-			case backend.KindExists:
-				condBatch.Queue(
-					"SELECT EXISTS (SELECT FROM kv WHERE key = $1 AND (expires IS NULL OR expires >= now()))",
-					nonNil(ca.Key),
-				).QueryRow(func(row pgx.Row) error {
-					var cond bool
-					if err := row.Scan(&cond); err != nil {
-						return trace.Wrap(err)
-					}
-					conditionFailed = conditionFailed || !cond
-					return nil
-				})
-			case backend.KindNotExists:
-				condBatch.Queue(
-					"SELECT NOT EXISTS (SELECT FROM kv WHERE key = $1 AND (expires IS NULL OR expires >= now()))",
-					nonNil(ca.Key),
-				).QueryRow(func(row pgx.Row) error {
-					var cond bool
-					if err := row.Scan(&cond); err != nil {
-						return trace.Wrap(err)
-					}
-					conditionFailed = conditionFailed || !cond
-					return nil
-				})
-			case backend.KindRevision:
-				expectedRevision, ok := revisionFromString(ca.Condition.Revision)
-				if !ok {
-					return struct{}{}, trace.Wrap(backend.ErrConditionFailed)
-				}
-				condBatch.Queue(
-					"SELECT EXISTS (SELECT FROM kv WHERE key = $1 AND revision = $2 AND (expires IS NULL OR expires >= now()))",
-					nonNil(ca.Key), expectedRevision,
-				).QueryRow(func(row pgx.Row) error {
-					var cond bool
-					if err := row.Scan(&cond); err != nil {
-						return trace.Wrap(err)
-					}
-					conditionFailed = conditionFailed || !cond
-					return nil
-				})
-			default:
-				return struct{}{}, trace.BadParameter("unexpected condition kind %v in conditional action against key %q", ca.Condition.Kind, ca.Key)
-			}
-
-			switch ca.Action.Kind {
-			case backend.KindNop:
-				// no action to be taken
-			case backend.KindPut:
-				includesPut = true
-				actBatch.Queue(
-					"INSERT INTO kv (key, value, expires, revision) VALUES ($1, $2, $3, $4)"+
-						" ON CONFLICT (key) DO UPDATE SET"+
-						" value = excluded.value, expires = excluded.expires, revision = excluded.revision",
-					nonNil(ca.Key), nonNil(ca.Action.Item.Value), zeronull.Timestamptz(ca.Action.Item.Expires.UTC()), newRevision,
-				)
-			case backend.KindDelete:
-				actBatch.Queue(
-					"DELETE FROM kv WHERE kv.key = $1 AND (kv.expires IS NULL OR kv.expires > now())", nonNil(ca.Key),
-				)
-			default:
-				return struct{}{}, trace.BadParameter("unexpected action kind %v in conditional action against key %q", ca.Action.Kind, ca.Key)
-			}
+		for _, bi := range condBatchItems {
+			condBatch.Queue(bi.query, bi.arguments...).QueryRow(querySuccess)
+		}
+		for _, bi := range actBatchItems {
+			actBatch.Queue(bi.query, bi.arguments...)
 		}
 
-		tx, err := b.pool.Begin(ctx)
-		if err != nil {
-			return struct{}{}, trace.Wrap(err)
-		}
-
-		// Rollback is designed to be safe to defer and will have no effect if we end up
-		// committing the transaction before returning.
-		defer tx.Rollback(ctx)
-
-		if condBatch.Len() != 0 {
+		success = true
+		if condBatch.Len() > 0 {
 			if err := tx.SendBatch(ctx, &condBatch).Close(); err != nil {
-				return struct{}{}, trace.Wrap(err)
+				return trace.Wrap(err)
 			}
-		}
-
-		if conditionFailed {
-			return struct{}{}, trace.Wrap(backend.ErrConditionFailed)
+			if !success {
+				return nil
+			}
 		}
 
 		if err := tx.SendBatch(ctx, &actBatch).Close(); err != nil {
-			return struct{}{}, trace.Wrap(err)
+			return trace.Wrap(err)
 		}
 
-		if err := tx.Commit(ctx); err != nil {
-			return struct{}{}, trace.Wrap(err)
-		}
-
-		return struct{}{}, nil
+		return nil
 	})
 
-	if n > 2 {
+	if tries > 2 {
 		// if we retried more than once, txn experienced non-trivial conflict and we should warn about it. Infrequent warnings of this kind
 		// are nothing to be concerned about, but high volumes may indicate that an automatic process is creating excessive conflicts.
-		b.log.Warnf("AtomicWrite retried %d times due to postgres transaction contention. Some conflict is expected, but persistent conflict warnings may indicate an unhealthy state.", n)
+		b.log.Warnf("AtomicWrite retried %d times due to postgres transaction contention. Some conflict is expected, but persistent conflict warnings may indicate an unhealthy state.", tries)
 	}
 
 	if err != nil {
 		return "", trace.Wrap(err)
 	}
 
-	if !includesPut {
+	if !success {
+		return "", trace.Wrap(backend.ErrConditionFailed)
+	}
+
+	if !actBatchIncludesPut {
 		return "", nil
 	}
 
