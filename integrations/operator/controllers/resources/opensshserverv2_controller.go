5bbaa2d63708498312bf419bafb332900d0d605f
Hugo Shaka <hugo.hervieux@goteleport.com>
2024-02-05 11:22:21 -0500

    lint

--- integrations/operator/controllers/resources/opensshserverv2_controller.go
+++ integrations/operator/controllers/resources/opensshserverv2_controller.go
@@ -20,14 +20,14 @@ package resources
 
 import (
 	"context"
-	"github.com/gravitational/teleport/api/defaults"
-	resourcesv1 "github.com/gravitational/teleport/integrations/operator/apis/resources/v1"
 
 	"github.com/gravitational/trace"
 	kclient "sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/gravitational/teleport/api/client"
+	"github.com/gravitational/teleport/api/defaults"
 	"github.com/gravitational/teleport/api/types"
+	resourcesv1 "github.com/gravitational/teleport/integrations/operator/apis/resources/v1"
 )
 
 // opensshServerClient implements TeleportResourceClient and offers CRUD methods needed to reconcile provision tokens
