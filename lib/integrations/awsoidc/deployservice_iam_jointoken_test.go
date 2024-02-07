f3cb18291aab2c75e2f8db19e0ad76185f4bff3c
Alan Parra <alan.parra@goteleport.com>
2024-02-07 16:54:00 -0300

    chore: Bump golangci-lint to v1.56.0 (#37854)
    * chore: Bump golangci-lint to v1.56.0
    
    * Use only enable linters for testifylint
    
    * Fix various testifylint offenders
    
    * Fix testifylint offenders in api/

--- lib/integrations/awsoidc/deployservice_iam_jointoken_test.go
+++ lib/integrations/awsoidc/deployservice_iam_jointoken_test.go
@@ -67,10 +67,10 @@ func TestUpsertIAMJoinToken(t *testing.T) {
 		require.Equal(t, "t", iamToken.GetName())
 		require.Contains(t, iamToken.GetRoles(), types.RoleDatabase)
 		require.Len(t, iamToken.GetAllowRules(), 1)
-		require.Equal(t, iamToken.GetAllowRules()[0], &types.TokenRule{
+		require.Equal(t, &types.TokenRule{
 			AWSAccount: "123456789012",
 			AWSARN:     "arn:aws:sts::123456789012:assumed-role/myrole/*",
-		})
+		}, iamToken.GetAllowRules()[0])
 	})
 
 	t.Run("when token exist but is missing the required allow rule and system role, it is updated", func(t *testing.T) {
@@ -102,10 +102,10 @@ func TestUpsertIAMJoinToken(t *testing.T) {
 		require.Equal(t, "t", iamToken.GetName())
 		require.Len(t, iamToken.GetAllowRules(), 1)
 		require.Contains(t, iamToken.GetRoles(), types.RoleDatabase)
-		require.Equal(t, iamToken.GetAllowRules()[0], &types.TokenRule{
+		require.Equal(t, &types.TokenRule{
 			AWSAccount: "123456789012",
 			AWSARN:     "arn:aws:sts::123456789012:assumed-role/myrole/*",
-		})
+		}, iamToken.GetAllowRules()[0])
 	})
 
 	t.Run("when token exist but has an invalid join method, it returns an error", func(t *testing.T) {
