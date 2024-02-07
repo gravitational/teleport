b74cbe98d2ad08aab6a19100c8e8048a03416059
Alan Parra <alan.parra@goteleport.com>
2024-02-07 16:50:26 -0300

    chore: Bump Go toolchain to 1.22.0 (#37845)
    * chore: Bump Go to 1.22.0
    
    * Bump Go version on docs
    
    * Fix testing bug in ./lib/auth/webauthn

--- build.assets/versions.mk
+++ build.assets/versions.mk
@@ -3,7 +3,7 @@
 # Keep versions in sync with devbox.json, when applicable.
 
 # Sync with devbox.json.
-GOLANG_VERSION ?= go1.21.11
+GOLANG_VERSION ?= go1.22.0
 GOLANGCI_LINT_VERSION ?= v1.58.1
 
 NODE_VERSION ?= 20.14.0
