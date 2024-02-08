a54a53627b74c19dec06f04a4dc8039e0a3a3a29
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2024-02-08 11:54:29 +0100

    Use the correct timestamp for the last resource presence report

--- lib/usagereporter/teleport/aggregating/reporter.go
+++ lib/usagereporter/teleport/aggregating/reporter.go
@@ -326,7 +326,7 @@ Ingest:
 	}
 
 	if len(resourcePresences) > 0 {
-		r.persistResourcePresence(ctx, userActivityStartTime, resourcePresences)
+		r.persistResourcePresence(ctx, resourceUsageStartTime, resourcePresences)
 	}
 
 	wg.Wait()
