65fbe047a2e026359107c763903dad31fc0f6572
Cam Hutchison <camh@goteleport.com>
2024-02-09 16:58:45 +1100

    build: Update buildbox definitions for spacelift runner
    Add a var to `build.assets/images.mk` that defines the centos7 buildbox
    without the host architecture suffix, and print that in the
    `show-buildbox-centos7-target` used by the spacelift runner CI job.
    
    The spacelift runner needs to know the buildbox to use to build `tbot`,
    but since it does a `docker buildx` multiarch build, it needs to be able
    to select different architecture buildboxes itself using `$TARGETARCH`
    and not just use the one for the host architecture.

--- build.assets/images.mk
+++ build.assets/images.mk
@@ -11,7 +11,8 @@ BUILDBOX_VERSION ?= teleport15
 BUILDBOX_BASE_NAME ?= ghcr.io/gravitational/teleport-buildbox
 
 BUILDBOX = $(BUILDBOX_BASE_NAME):$(BUILDBOX_VERSION)
-BUILDBOX_CENTOS7 = $(BUILDBOX_BASE_NAME)-centos7:$(BUILDBOX_VERSION)-$(RUNTIME_ARCH)
+BUILDBOX_CENTOS7_NOARCH = $(BUILDBOX_BASE_NAME)-centos7:$(BUILDBOX_VERSION)
+BUILDBOX_CENTOS7 = $(BUILDBOX_CENTOS7_NOARCH)-$(RUNTIME_ARCH)
 BUILDBOX_CENTOS7_ASSETS = $(BUILDBOX_BASE_NAME)-centos7-assets:$(BUILDBOX_VERSION)-$(RUNTIME_ARCH)
 BUILDBOX_CENTOS7_FIPS = $(BUILDBOX_BASE_NAME)-centos7-fips:$(BUILDBOX_VERSION)-$(RUNTIME_ARCH)
 BUILDBOX_ARM = $(BUILDBOX_BASE_NAME)-arm:$(BUILDBOX_VERSION)
@@ -22,6 +23,11 @@ BUILDBOX_NODE = $(BUILDBOX_BASE_NAME)-node:$(BUILDBOX_VERSION)
 show-buildbox-base-image:
 	@echo "$(BUILDBOX)"
 
+# show-buildbox-centos7-image is used by the spacelift-runner workflow to know
+# the buildbox needed to build tbot. It needs the image without the
+# architecture as it is a `docker buildx` multi-arch build so it needs to
+# construct the image name itself based on $TARGETARCH not $(RUNTIME_ARCH) from
+# the Makefile.
 .PHONY:show-buildbox-centos7-image
 show-buildbox-centos7-image:
-	@echo "$(BUILDBOX_CENTOS7)"
+	@echo "$(BUILDBOX_CENTOS7_NOARCH)"
