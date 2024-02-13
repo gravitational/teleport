de147a1c4b3a72a35498349a281e61f9edbb0d32
Edward Dowling <EdwardDowling@users.noreply.github.com>
2024-02-13 16:15:06 +0000

    [v14] Fix opsgenie plugin to use correct annotation to determine whether or not to create alerts (#38104)
    * Fix bug in opsgenie plugin mixing up notify-services and schedules
    
    * Update docs for opsgenie plugin to use correct schedules
    
    * Remove accidental change to opsgenie client
    
    * Update opsgenie client tests

--- integrations/access/opsgenie/client_test.go
+++ integrations/access/opsgenie/client_test.go
@@ -54,7 +54,7 @@ func TestCreateAlert(t *testing.T) {
 		Roles:         []string{"role1", "role2"},
 		RequestReason: "someReason",
 		SystemAnnotations: types.Labels{
-			types.TeleportNamespace + types.ReqAnnotationSchedulesLabel: {"responder@teleport.com"},
+			types.TeleportNamespace + types.ReqAnnotationNotifyServicesLabel: {"responder@teleport.com"},
 		},
 	})
 	assert.NoError(t, err)
