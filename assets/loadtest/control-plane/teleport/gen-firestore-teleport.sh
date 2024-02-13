2132184f44c2022e6ef0f1f6b2ed29026a1b4417
Tim Ross <tim.ross@goteleport.com>
2024-02-13 15:28:50 -0500

    Update loadtest infra with changes from v15 testing
    - Uses the distroless-debug images
    - Adds firestore support
    - Updates images to v15.0.0

--- assets/loadtest/control-plane/teleport/gen-firestore-teleport.sh
+++ assets/loadtest/control-plane/teleport/gen-firestore-teleport.sh
@@ -9,28 +9,32 @@ values_yaml="$STATE_DIR/teleport-values.yaml"
 mkdir -p "$STATE_DIR"
 
 cat > "$values_yaml" <<EOF
-chartMode: aws
+chartMode: gcp
 clusterName: ${CLUSTER_NAME}.${ROUTE53_ZONE}      # Name of your cluster. Use the FQDN you intend to configure in DNS below.
 teleportVersionOverride: ${TELEPORT_VERSION}
 proxyListenerMode: "multiplex"
-aws:
+gcp:
+  projectId: ${GCP_PROJECT}
   region: ${REGION}                           # AWS region
-  backendTable: ${CLUSTER_NAME}-backend           # DynamoDB table to use for the Teleport backend
-  auditLogTable: ${CLUSTER_NAME}-events           # DynamoDB table to use for the Teleport audit log (must be different to the backend table)
+  backendTable: ${CLUSTER_NAME}-backend           # Firestore table to use for the Teleport backend
+  auditLogTable: ${CLUSTER_NAME}-events           # Firestore table to use for the Teleport audit log (must be different to the backend table)
   auditLogMirrorOnStdout: false                   # Whether to mirror audit log entries to stdout in JSON format (useful for external log collectors)
-  sessionRecordingBucket: ${SESSION_BUCKET}       # S3 bucket to use for Teleport session recordings
+  sessionRecordingBucket: ${FIRESTORE_SESSION_BUCKET}       # Storage bucket to use for Teleport session recordings
   backups: true                                   # Whether or not to turn on DynamoDB backups
   dynamoAutoScaling: false                        # Whether Teleport should configure DynamoDB's autoscaling.
 highAvailability:
   replicaCount: 2                                 # Number of replicas to configure
   certManager:
-    enabled: true                                 # Enable cert-manager support to get TLS certificates
-    issuerName: letsencrypt-production            # Name of the cert-manager Issuer to use (as configured above)
+    enabled: false                                # No certManager because we only run simulated load tests and don't need to connect any agents
 # If you are running Kubernetes 1.23 or above, disable PodSecurityPolicies
 podSecurityPolicy:
   enabled: false
 podMonitor:
   enabled: true
+extraArgs:
+  - --debug
+image: "public.ecr.aws/gravitational/teleport-distroless-debug"
+enterpriseImage: "public.ecr.aws/gravitational/teleport-ent-distroless-debug"
 auth:
   teleportConfig:
     kubernetes_service:
