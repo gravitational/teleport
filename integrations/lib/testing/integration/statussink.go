e614d9eb33dbb0f36f969e65034bddd7c25e136c
Hugo Shaka <hugo.hervieux@goteleport.com>
2024-02-13 16:16:36 -0500

    godocs

--- integrations/lib/testing/integration/statussink.go
+++ integrations/lib/testing/integration/statussink.go
@@ -7,15 +7,18 @@ import (
 	"github.com/gravitational/teleport/api/types"
 )
 
+// FakeStatusSink is a fake status sink that can be used when testing plugins.
 type FakeStatusSink struct {
 	status atomic.Pointer[types.PluginStatus]
 }
 
+// Emit implements the common.StatusSink interface.
 func (s *FakeStatusSink) Emit(_ context.Context, status types.PluginStatus) error {
 	s.status.Store(&status)
 	return nil
 }
 
+// Get returns the last status stored by the plugin.
 func (s *FakeStatusSink) Get() types.PluginStatus {
 	status := s.status.Load()
 	if status == nil {
