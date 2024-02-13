30780a7c5855f13dc42c191c2626a380706ae638
Hugo Shaka <hugo.hervieux@goteleport.com>
2024-02-13 16:16:36 -0500

    godocs

--- integrations/lib/testing/integration/interfaces.go
+++ integrations/lib/testing/integration/interfaces.go
@@ -8,6 +8,9 @@ import (
 	"github.com/gravitational/teleport/api/types"
 )
 
+// AuthHelper is the interface one must implement to run the AccessRequestSuite.
+// It can be implemented by an OSS Auth server, or an Enterprise auth server
+// (in teleport.e).
 type AuthHelper interface {
 	StartServer(t *testing.T) *client.Client
 	ServerAddr() string
@@ -15,6 +18,7 @@ type AuthHelper interface {
 	SignIdentityForUser(t *testing.T, ctx context.Context, user types.User) string
 }
 
+// NewAccessRequestClient returns a new integration.Client.
 func NewAccessRequestClient(client *client.Client) *Client {
 	return &Client{client}
 }
