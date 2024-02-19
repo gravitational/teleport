fccc15ae3741bfd6c975bfa18b3ef74cd3d0b7ad
Gabriel Corado <gabriel.oliveira@goteleport.com>
2024-02-19 13:00:29 -0300

    chore(config): add comment on default value

--- lib/config/fileconf.go
+++ lib/config/fileconf.go
@@ -1573,6 +1573,7 @@ type BPF struct {
 	CgroupPath string `yaml:"cgroup_path"`
 
 	// RootPath root directory for the Teleport cgroups.
+	// Optional, defaults to /teleport
 	RootPath string `yaml:"root_path"`
 }
 
