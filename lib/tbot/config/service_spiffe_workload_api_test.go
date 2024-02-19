eb3a3913bd60b5a1b6807bca279e0a48894ad367
Noah Stride <noah.stride@goteleport.com>
2024-02-19 12:40:28 +0000

    Add config tests for SPIFFEWorkloadAPIService

--- lib/tbot/config/service_spiffe_workload_api_test.go
+++ lib/tbot/config/service_spiffe_workload_api_test.go
@@ -17,3 +17,147 @@
  */
 
 package config
+
+import (
+	"testing"
+
+	"github.com/gravitational/teleport/lib/auth/machineid/machineidv1/experiment"
+)
+
+func TestSPIFFEWorkloadAPIService_YAML(t *testing.T) {
+	t.Parallel()
+
+	tests := []testYAMLCase[SPIFFEWorkloadAPIService]{
+		{
+			name: "full",
+			in: SPIFFEWorkloadAPIService{
+				Listen: "unix:///var/run/spiffe.sock",
+				SVIDs: []SVIDRequest{
+					{
+						Path: "/foo",
+						Hint: "hint",
+						SANS: SVIDRequestSANs{
+							DNS: []string{"example.com"},
+							IP:  []string{"10.0.0.1", "10.42.0.1"},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "minimal",
+			in: SPIFFEWorkloadAPIService{
+				Listen: "unix:///var/run/spiffe.sock",
+				SVIDs: []SVIDRequest{
+					{
+						Path: "/foo",
+					},
+				},
+			},
+		},
+	}
+	testYAML(t, tests)
+}
+
+func TestSPIFFEWorkloadAPIService_CheckAndSetDefaults(t *testing.T) {
+	experiment.SetEnabled(true)
+	defer experiment.SetEnabled(false)
+
+	tests := []testCheckAndSetDefaultsCase[*SPIFFEWorkloadAPIService]{
+		{
+			name: "valid",
+			in: func() *SPIFFEWorkloadAPIService {
+				return &SPIFFEWorkloadAPIService{
+					Listen: "unix:///var/run/spiffe.sock",
+					SVIDs: []SVIDRequest{
+						{
+							Path: "/foo",
+							Hint: "hint",
+							SANS: SVIDRequestSANs{
+								DNS: []string{"example.com"},
+								IP:  []string{"10.0.0.1", "10.42.0.1"},
+							},
+						},
+					},
+				}
+			},
+		},
+		{
+			name: "missing path",
+			in: func() *SPIFFEWorkloadAPIService {
+				return &SPIFFEWorkloadAPIService{
+					Listen: "unix:///var/run/spiffe.sock",
+					SVIDs: []SVIDRequest{
+						{
+							Path: "",
+							Hint: "hint",
+							SANS: SVIDRequestSANs{
+								DNS: []string{"example.com"},
+								IP:  []string{"10.0.0.1", "10.42.0.1"},
+							},
+						},
+					},
+				}
+			},
+			wantErr: "svid.path: should not be empty",
+		},
+		{
+			name: "path missing leading slash",
+			in: func() *SPIFFEWorkloadAPIService {
+				return &SPIFFEWorkloadAPIService{
+					Listen: "unix:///var/run/spiffe.sock",
+					SVIDs: []SVIDRequest{
+						{
+							Path: "foo",
+							Hint: "hint",
+							SANS: SVIDRequestSANs{
+								DNS: []string{"example.com"},
+								IP:  []string{"10.0.0.1", "10.42.0.1"},
+							},
+						},
+					},
+				}
+			},
+			wantErr: "svid.path: should be prefixed with /",
+		},
+		{
+			name: "missing listen addr",
+			in: func() *SPIFFEWorkloadAPIService {
+				return &SPIFFEWorkloadAPIService{
+					Listen: "",
+					SVIDs: []SVIDRequest{
+						{
+							Path: "foo",
+							Hint: "hint",
+							SANS: SVIDRequestSANs{
+								DNS: []string{"example.com"},
+								IP:  []string{"10.0.0.1", "10.42.0.1"},
+							},
+						},
+					},
+				}
+			},
+			wantErr: "listen: should not be empty",
+		},
+		{
+			name: "invalid ip",
+			in: func() *SPIFFEWorkloadAPIService {
+				return &SPIFFEWorkloadAPIService{
+					Listen: "unix:///var/run/spiffe.sock",
+					SVIDs: []SVIDRequest{
+						{
+							Path: "/foo",
+							Hint: "hint",
+							SANS: SVIDRequestSANs{
+								DNS: []string{"example.com"},
+								IP:  []string{"invalid ip"},
+							},
+						},
+					},
+				}
+			},
+			wantErr: "ip_sans[0]: invalid IP address",
+		},
+	}
+	testCheckAndSetDefaults(t, tests)
+}
