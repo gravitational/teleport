9ca17db342fcb729f2ea30d95f6bdd790b252bec
Mike Wilson <michael.wilson@goteleport.com>
2024-02-26 15:25:30 -0500

    Introduce a member count method to access lists.
    The access list interface now supports counting members without unmarshaling.
    This method has been added to the generic backend service as well. This will
    allow for quicker counts when necessary. This method has been added to the
    cache.
    
    Additionally, a status field as per RFD-153 has been added to access lists
    that will house the new optional member count field.

--- lib/cache/cache.go
+++ lib/cache/cache.go
@@ -2926,6 +2926,19 @@ func (c *Cache) GetAccessList(ctx context.Context, name string) (*accesslist.Acc
 	return rg.reader.GetAccessList(ctx, name)
 }
 
+// CountAccessListMembers will count all access list members.
+func (c *Cache) CountAccessListMembers(ctx context.Context, accessListName string) (uint32, error) {
+	ctx, span := c.Tracer.Start(ctx, "cache/CountAccessListMembers")
+	defer span.End()
+
+	rg, err := readCollectionCache(c, c.collections.accessListMembers)
+	if err != nil {
+		return 0, trace.Wrap(err)
+	}
+	defer rg.Release()
+	return rg.reader.CountAccessListMembers(ctx, accessListName)
+}
+
 // ListAccessListMembers returns a paginated list of all access list members.
 // May return a DynamicAccessListError if the requested access list has an
 // implicit member list and the underlying implementation does not have
