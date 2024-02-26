e84564046ed65226bf9bae739315b5003bb83b4e
Bernard Kim <bernard@goteleport.com>
2024-02-26 15:03:29 -0800

    Remove periodic instance metrics

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -1226,7 +1226,6 @@ func (a *Server) doInstancePeriodics(ctx context.Context) {
 	skipControlPlane := modules.GetModules().Features().Cloud
 
 	// set up aggregators for our periodics
-	imp := newInstanceMetricsPeriodic()
 	uep := newUpgradeEnrollPeriodic()
 
 	// stream all instances to all aggregators
@@ -1239,7 +1238,6 @@ func (a *Server) doInstancePeriodics(ctx context.Context) {
 			}
 		}
 
-		imp.VisitInstance(instances.Item())
 		uep.VisitInstance(instances.Item())
 	}
 
