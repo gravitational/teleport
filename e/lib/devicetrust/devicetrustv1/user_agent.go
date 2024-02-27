fb2d7c926d0b0e872193c347f691f4ff0b28008d
Alan Parra <alan.parra@goteleport.com>
2024-02-27 14:24:47 -0300

    Detect OS from user agent strings (#3542)
    * Detect OS from user agent strings
    
    * Add a macOS/Firefox sample
    
    * Appease linter
    
    Source-commit: gravitational/teleport.e@5208aa5ac0c4399238c0dfad72b5fb42cdb44e76
    Source-pull-requests: gravitational/teleport.e#3542

---
+++ e/lib/devicetrust/devicetrustv1/user_agent.go
@@ -0,0 +1,58 @@
+package devicetrustv1
+
+import (
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+
+	devicepb "github.com/gravitational/teleport/api/gen/proto/go/teleport/devicetrust/v1"
+)
+
+// TODO(codingllama): Remove once loggedGetOSFromUserAgent is called.
+var _ = loggedGetOSFromUserAgent
+
+// loggedGetOSFromUserAgent debug-logs the result of [getOSFromUserAgent] before
+// returning it.
+func loggedGetOSFromUserAgent(logger log.FieldLogger, ua string) devicepb.OSType {
+	os := getOSFromUserAgent(ua)
+	logger.
+		WithFields(log.Fields{
+			"OS":        os,
+			"UserAgent": ua,
+		}).
+		Debug("Mapped user agent to OS")
+	return os
+}
+
+// getOSFromUserAgent returns the [devicepb.OSType] inferred from the user agent
+// string.
+//
+// This function makes no attempt to parse the user agent, instead it assumes a
+// valid string and looks for certain keywords within it.
+//
+// Returns [devicepb.OSType_OS_TYPE_UNSPECIFIED] for both unmapped systems and
+// unknown strings.
+func getOSFromUserAgent(ua string) devicepb.OSType {
+	// Partially based on
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent#os.
+
+	// Be case-insensitive.
+	ua = strings.ToLower(ua)
+
+	switch {
+	case strings.Contains(ua, "mobile"), strings.Contains(ua, "android"):
+		// Ignore mobile user agents.
+		return devicepb.OSType_OS_TYPE_UNSPECIFIED
+	case strings.Contains(ua, "xbox"):
+		// Looks remarkably like Windows otherwise.
+		return devicepb.OSType_OS_TYPE_UNSPECIFIED
+	case strings.Contains(ua, "mac os x"):
+		return devicepb.OSType_OS_TYPE_MACOS
+	case strings.Contains(ua, "windows"):
+		return devicepb.OSType_OS_TYPE_WINDOWS
+	case strings.Contains(ua, "linux"):
+		return devicepb.OSType_OS_TYPE_LINUX
+	}
+
+	return devicepb.OSType_OS_TYPE_UNSPECIFIED
+}
