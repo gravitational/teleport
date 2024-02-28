75c7d01741d701192a59639c977b1affcc9c0db3
Anton Miniailo <anton@goteleport.com>
2024-02-28 14:28:21 -0500

    AWS OIDC: Move enrollment of EKS clusters to the gRPC service. (#38683)
    * AWSOIDC: Move enrollment of EKS clusters to the gRPC service.
    
    * Add missing godoc.
    
    * Remove unneeded code.
    
    Co-authored-by: Marco André Dinis <marco.dinis@goteleport.com>
    
    * Fix after rebase.
    
    * Remove proxyPublicAddr field from grpc request.
    
    * Change test address.
    
    Co-authored-by: rosstimothy <39066650+rosstimothy@users.noreply.github.com>
    
    * Remove isCloud and enableAutoUpgrades fields from grpc request.
    
    * Update EKS cluster name field in AWSOIDC integration
    
    * Fix tests.
    
    * Refactor variable initiation.
    
    Co-authored-by: rosstimothy <39066650+rosstimothy@users.noreply.github.com>
    
    * Fix after rebase
    
    * Remove unused code.
    
    ---------
    
    Co-authored-by: Marco André Dinis <marco.dinis@goteleport.com>
    Co-authored-by: rosstimothy <39066650+rosstimothy@users.noreply.github.com>

--- lib/auth/integration/integrationv1/awsoidc_test.go
+++ lib/auth/integration/integrationv1/awsoidc_test.go
@@ -136,9 +136,10 @@ func TestListEICE(t *testing.T) {
 	require.NoError(t, err)
 
 	awsoidService, err := NewAWSOIDCService(&AWSOIDCServiceConfig{
-		IntegrationService: resourceSvc,
-		Authorizer:         resourceSvc.authorizer,
-		Cache:              &mockCache{},
+		IntegrationService:    resourceSvc,
+		Authorizer:            resourceSvc.authorizer,
+		ProxyPublicAddrGetter: func() string { return "128.0.0.1" },
+		Cache:                 &mockCache{},
 	})
 	require.NoError(t, err)
 
@@ -180,6 +181,71 @@ func TestListEICE(t *testing.T) {
 	})
 }
 
+func TestEnrollEKSClusters(t *testing.T) {
+	t.Parallel()
+
+	clusterName := "test-cluster"
+	proxyPublicAddr := "127.0.0.1"
+	integrationName := "my-awsoidc-integration"
+	ig, err := types.NewIntegrationAWSOIDC(
+		types.Metadata{Name: integrationName},
+		&types.AWSOIDCIntegrationSpecV1{
+			RoleARN: "arn:aws:iam::123456789012:role/OpsTeam",
+		},
+	)
+	require.NoError(t, err)
+
+	ca := newCertAuthority(t, types.HostCA, clusterName)
+	ctx, localClient, resourceSvc := initSvc(t, ca, clusterName, proxyPublicAddr)
+
+	_, err = localClient.CreateIntegration(ctx, ig)
+	require.NoError(t, err)
+
+	awsoidService, err := NewAWSOIDCService(&AWSOIDCServiceConfig{
+		IntegrationService:    resourceSvc,
+		Authorizer:            resourceSvc.authorizer,
+		ProxyPublicAddrGetter: func() string { return "128.0.0.1" },
+		Cache:                 &mockCache{},
+	})
+	require.NoError(t, err)
+
+	t.Run("fails when user doesn't have access to integration.use", func(t *testing.T) {
+		role := types.RoleSpecV6{
+			Allow: types.RoleConditions{Rules: []types.Rule{{
+				Resources: []string{types.KindIntegration},
+				Verbs:     []string{types.VerbRead},
+			}}},
+		}
+
+		userCtx := authorizerForDummyUser(t, ctx, role, localClient)
+
+		_, err = awsoidService.EnrollEKSClusters(userCtx, &integrationv1.EnrollEKSClustersRequest{
+			Integration:     integrationName,
+			Region:          "my-region",
+			EksClusterNames: []string{"EKS1"},
+			AgentVersion:    "10.0.0",
+		})
+		require.True(t, trace.IsAccessDenied(err), "expected AccessDenied error, but got %T", err)
+	})
+	t.Run("calls awsoidc package when user has access to integration.use/read", func(t *testing.T) {
+		role := types.RoleSpecV6{
+			Allow: types.RoleConditions{Rules: []types.Rule{{
+				Resources: []string{types.KindIntegration},
+				Verbs:     []string{types.VerbRead, types.VerbUse},
+			}}},
+		}
+
+		userCtx := authorizerForDummyUser(t, ctx, role, localClient)
+
+		_, err := awsoidService.EnrollEKSClusters(userCtx, &integrationv1.EnrollEKSClustersRequest{
+			Integration:     integrationName,
+			Region:          "my-region",
+			EksClusterNames: []string{"EKS1"},
+		})
+		require.True(t, trace.IsBadParameter(err), "expected BadParameter error, but got %T", err)
+	})
+}
+
 func TestDeployService(t *testing.T) {
 	t.Parallel()
 
@@ -201,9 +267,10 @@ func TestDeployService(t *testing.T) {
 	require.NoError(t, err)
 
 	awsoidService, err := NewAWSOIDCService(&AWSOIDCServiceConfig{
-		IntegrationService: resourceSvc,
-		Authorizer:         resourceSvc.authorizer,
-		Cache:              &mockCache{},
+		IntegrationService:    resourceSvc,
+		Authorizer:            resourceSvc.authorizer,
+		ProxyPublicAddrGetter: func() string { return "128.0.0.1" },
+		Cache:                 &mockCache{},
 	})
 	require.NoError(t, err)
 
