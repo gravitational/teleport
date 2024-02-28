78b47dcea3ba5ab7be88a3a5ffbe9f255953b07c
Michael Wilson <mike@mdwn.dev>
2024-02-27 20:19:58 -0800

    Remove grpc-tools-node-protoc-gen. (#38696)
    We're no longer using grpc-tools-node-protoc-gen in master/v15, so it's been
    removed.

--- build.assets/genproto.sh
+++ build.assets/genproto.sh
@@ -72,17 +72,6 @@ main() {
   echoed buf generate --template=buf-connect-go.gen.yaml \
     --path=proto/prehog/
 
-  # Generate JS protos.
-  # TODO(ryan): remove once Connect has migrated to the TS protobufs.
-  [[ $skip_js -eq 0 ]] && echoed buf generate --template=buf-js.gen.yaml \
-    --path=proto/prehog/ \
-    --path=proto/teleport/lib/teleterm/ \
-    --path=api/proto/teleport/accesslist/ \
-    --path=api/proto/teleport/devicetrust/ \
-    --path=api/proto/teleport/header/ \
-    --path=api/proto/teleport/trait/ \
-    --path=api/proto/teleport/userpreferences/
-
   # Generate TS protos.
   [[ $skip_js -eq 0 ]] && echoed buf generate --template=buf-ts.gen.yaml \
     --path=proto/prehog/ \
