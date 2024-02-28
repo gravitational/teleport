1cbc0907c064ad406a3f01cc53d877fb442a9882
Anton Miniailo <anton@goteleport.com>
2024-02-28 10:13:33 -0500

    Improve a comment.
    Co-authored-by: Lisa Kim <lisa@goteleport.com>

--- web/packages/teleport/src/Discover/Shared/AutoDiscovery/AutoEnrollDialog.tsx
+++ web/packages/teleport/src/Discover/Shared/AutoDiscovery/AutoEnrollDialog.tsx
@@ -37,7 +37,11 @@ export type AutoEnrollDialog = {
   close(): void;
   next(): void;
   region: string;
-  notifyAboutDelay: boolean; // show notification that resources might take some time to appear after setup has finished.
+/**
+   * show notification that resources might take some
+   * time to appear after setup has finished.
+   */
+  notifyAboutDelay: boolean;
 };
 
 export function AutoEnrollDialog({
