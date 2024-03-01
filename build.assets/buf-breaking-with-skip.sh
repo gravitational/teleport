3be21c070191529d9df28cf8fc4a751790c08b21
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2024-03-01 16:32:24 +0100

    Tweak output a bit

--- build.assets/buf-breaking-with-skip.sh
+++ build.assets/buf-breaking-with-skip.sh
@@ -10,11 +10,6 @@ BUF="${BUF:-buf}"
 base_input="$( "${BUF}" build "$1" --output - | sha256sum | cut -w -f1 )"
 against_input="$( "${BUF}" build "$2" --output - | sha256sum | cut -w -f1 )"
 
-if grep -q "${base_input} ${against_input}" $3 ; then
-    echo "Inputs found in skip list, skipping check."
-    exit 0
-fi
-
 "${BUF}" breaking "$1" --against "$2"
 buf_exit=$?
 
@@ -22,13 +17,13 @@ if [ ${buf_exit} -eq 0 ] ; then
     exit 0
 fi
 
-if grep -q "${base_input} ${against_input}" $3 ; then
-    echo "Inputs found in skip list, returning success even after a failed check."
-    exit 0
-fi
-
 echo "Failed inputs:"
 echo "  \"base\" input hash: ${base_input}"
 echo "  \"against\" input hash: ${against_input}"
+
+if grep -q "${base_input} ${against_input}" "$3" ; then
+    echo "Inputs found in skip list, returning success after a failed check."
+    exit 0
+fi
 
 exit ${buf_exit}
