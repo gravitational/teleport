1a624e4640da596c9741e2ba42ac9388e5dd3018
Alan Parra <alan.parra@goteleport.com>
2024-03-01 16:48:21 -0300

    Fix TestPasswordChange

--- lib/web/apiserver_test.go
+++ lib/web/apiserver_test.go
@@ -665,6 +665,7 @@ func (s *WebSuite) authPack(t *testing.T, user string, roles ...string) *authPac
 		otpSecret: otpSecret,
 		user:      user,
 		login:     login,
+		password:  pass,
 		session:   sess,
 		clt:       clt,
 		cookies:   httpResp.Cookies(),
@@ -952,8 +953,8 @@ func TestPasswordChange(t *testing.T) {
 	require.NoError(t, err)
 
 	req := changePasswordReq{
-		OldPassword:       []byte("abc123"),
-		NewPassword:       []byte("abc1234"),
+		OldPassword:       []byte(pack.password),
+		NewPassword:       []byte("defabc12345678"),
 		SecondFactorToken: validToken,
 	}
 
