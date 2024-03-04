fe0ed209c64cd779aaf1869badd1f16285987db2
Michael Wilson <michael.wilson@goteleport.com>
2024-03-04 08:02:39 -0800

    Eliminate spurious Okta app assignment matches. (#3559)
    Okta app assignments were being counted for users who were assigned to an
    application via a group. This change ensures that only direct application
    assignments are taken into account.
    
    Source-commit: gravitational/teleport.e@f9768a936641b6d6fe6a420a1a22fc71d51cddde
    Source-pull-requests: gravitational/teleport.e#3559

--- e/lib/okta/client.go
+++ e/lib/okta/client.go
@@ -20,6 +20,7 @@ import (
 const (
 	oktaErrorID          = "errorId"
 	oktaUserProfileLogin = "login"
+	oktaUserScope        = "USER"
 )
 
 // Static assertion that the wrappedClient type implements OktaClient
@@ -216,7 +217,12 @@ func (w *wrappedClient) getGroupAssignments(ctx context.Context, groupID string)
 func (w *wrappedClient) getAppAssignments(ctx context.Context, appID string) ([]string, error) {
 	var userIDs []string
 	err := w.iterateAppUsers(ctx, appID, func(appUser *okta.AppUser) error {
-		userIDs = append(userIDs, appUser.Id)
+		// We only want direct assignments to applications here. iterateAppUsers will return users who are
+		// assigned to an application through group membership or direct assignment. By filtering on the
+		// USER scope, we'll only get direct assignments.
+		if appUser.Scope == oktaUserScope {
+			userIDs = append(userIDs, appUser.Id)
+		}
 		return nil
 	})
 	if err != nil {
