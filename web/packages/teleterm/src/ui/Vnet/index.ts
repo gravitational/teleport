db4dca60a080259c95ddd3064bb9232a90d1bdd1
Rafał Cieślak <rafal.cieslak@goteleport.com>
2024-03-05 12:42:56 +0100

    Remove DocumentVnet

--- web/packages/teleterm/src/ui/Vnet/index.ts
+++ web/packages/teleterm/src/ui/Vnet/index.ts
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-export * from './DocumentVnet';
 export * from './VnetSliderStep';
 export * from './vnetContext';
 export { VnetConnectionItem } from './VnetConnectionItem';
