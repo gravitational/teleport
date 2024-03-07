8ae590881ac103548c14101db9987d9499dbc242
Noah Stride <noah.stride@goteleport.com>
2024-03-07 10:04:31 +0000

    Add godoc for ProxyServer

--- lib/tbot/config/config.go
+++ lib/tbot/config/config.go
@@ -160,6 +160,7 @@ type CLIConf struct {
 
 	// ProxyServer is the teleport proxy address. Unlike `AuthServer` this must
 	// explicitly point to a Teleport proxy.
+	// Example: "example.teleport.sh:443"
 	ProxyServer string
 
 	// Cluster is the name of the Teleport cluster on which resources should
@@ -271,8 +272,11 @@ type BotConfig struct {
 	Outputs    Outputs          `yaml:"outputs,omitempty"`
 	Services   Services         `yaml:"services,omitempty"`
 
-	Debug           bool          `yaml:"debug"`
-	AuthServer      string        `yaml:"auth_server,omitempty"`
+	Debug      bool   `yaml:"debug"`
+	AuthServer string `yaml:"auth_server,omitempty"`
+	// ProxyServer is the teleport proxy address. Unlike `AuthServer` this must
+	// explicitly point to a Teleport proxy.
+	// Example: "example.teleport.sh:443"
 	ProxyServer     string        `yaml:"proxy_server,omitempty"`
 	CertificateTTL  time.Duration `yaml:"certificate_ttl"`
 	RenewalInterval time.Duration `yaml:"renewal_interval"`
