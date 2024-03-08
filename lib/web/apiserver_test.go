76f4a074e3161682845986cc25975330aeee927a
Hugo Shaka <hugo.hervieux@goteleport.com>
2024-03-08 09:28:10 -0500

    Enable automatic upgrade server by default (#38937) (#39096)
    * Enable automatic upgrade server by default
    
    This commit means the proxy now serves automatic upgrade channels by
    default. The default channel is "default" and its version is the
    teleport proxy binary version.
    
    This commit also fixes a bug with the automatic update version server
    returning `teleport.Version` instead of `"v"+teleport.Version` when the
    version was too high. This was not a critical issue because agents are
    supposed to tolerate versions not prefixed with 'v'.
    
    * lint
    
    * thread safety
    
    * Update lib/automaticupgrades/channel.go
    
    * Update lib/config/configuration.go
    
    
    
    * fix tests and lint
    
    ---------
    
    Co-authored-by: Noah Stride <noah.stride@goteleport.com>

--- lib/web/apiserver_test.go
+++ lib/web/apiserver_test.go
@@ -4630,13 +4630,13 @@ func TestGetWebConfig(t *testing.T) {
 			StaticVersion: testVersion,
 		},
 	}
-	require.NoError(t, channels.CheckAndSetDefaults(authproto.Features{AutomaticUpgrades: true, Cloud: true}))
+	require.NoError(t, channels.CheckAndSetDefaults())
 	env.proxies[0].handler.handler.cfg.AutomaticUpgradesChannels = channels
 
 	expectedCfg.IsCloud = true
 	expectedCfg.IsUsageBasedBilling = true
 	expectedCfg.AutomaticUpgrades = true
-	expectedCfg.AutomaticUpgradesTargetVersion = teleport.Version
+	expectedCfg.AutomaticUpgradesTargetVersion = "v" + teleport.Version
 	expectedCfg.AssistEnabled = false
 
 	// request and verify enabled features are enabled.
