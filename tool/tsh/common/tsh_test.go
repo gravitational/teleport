6d266f4199cf89aad0a541beca53e48db7bd9994
Hugo Shaka <hugo.hervieux@goteleport.com>
2024-03-11 14:55:42 -0400

    fix tests + address roman's feedback

--- tool/tsh/common/tsh_test.go
+++ tool/tsh/common/tsh_test.go
@@ -5389,7 +5389,7 @@ func TestFlatten(t *testing.T) {
 		IdentityFormat:     identityfile.FormatFile,
 		HomePath:           home,
 		AuthConnector:      connector.GetName(),
-		MockSSOLogin:       mockSSOLogin(t, authServer, alice),
+		MockSSOLogin:       mockSSOLogin(authServer, alice),
 		Context:            context.Background(),
 	}
 	require.NoError(t, onLogin(&conf))
