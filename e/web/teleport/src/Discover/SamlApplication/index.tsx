90363599e869e6f2d6b8ed21b17ce5c63bebc3f8
Sakshyam Shah <sshah@goteleport.com>
2024-03-12 11:16:30 -0400

    refactor: use `SAMLServiceProviderPresets` to determine SAML app type (#3657)
    * refactor: use SAMLServiceProviderPresets to determine SAML app type
    
    * SAMLServiceProviderPresets to SamlServiceProviderPreset
    
    Source-commit: gravitational/teleport.e@806a90fb0d9f8b7523a50a96bc1734602c7e0b7d
    Source-pull-requests: gravitational/teleport.e#3657

--- e/web/teleport/src/Discover/SamlApplication/index.tsx
+++ e/web/teleport/src/Discover/SamlApplication/index.tsx
@@ -1,6 +1,7 @@
 import { ResourceViewConfig } from 'teleport/Discover/flow';
 import { DiscoverEvent } from 'teleport/services/userEvent';
 import { ResourceKind } from 'teleport/Discover/Shared';
+import { SamlServiceProviderPreset } from 'teleport/Discover/SelectResource/types';
 
 import { DownloadMetadata, AddServiceProvider } from './SamlApp';
 import { Finished } from './Finished';
@@ -12,34 +13,36 @@ export const SamlApplicationResource: ResourceViewConfig = {
   views(resource) {
     let configureResourceViews;
     let title;
-    if (resource.name.toLowerCase().includes('grafana')) {
-      title = 'Add SAML Application (Grafana)';
-      configureResourceViews = [
-        {
-          title: "Configure Grafana with Teleport's IdP Metadata",
-          component: DownloadMetadataGrafana,
-          eventName: DiscoverEvent.Started,
-        },
-        {
-          title: "Add Grafana's Service Provider to Teleport",
-          component: AddGrafanaSaml,
-          eventName: DiscoverEvent.DeployService,
-        },
-      ];
-    } else {
-      title = 'Add SAML Application';
-      configureResourceViews = [
-        {
-          title: "Configure Service Provider with Teleport's IdP Metadata",
-          component: DownloadMetadata,
-          eventName: DiscoverEvent.Started,
-        },
-        {
-          title: 'Add Service Provider to Teleport',
-          component: AddServiceProvider,
-          eventName: DiscoverEvent.DeployService,
-        },
-      ];
+    switch (resource.samlMeta?.preset) {
+      case SamlServiceProviderPreset.Grafana:
+        title = 'Add SAML Application (Grafana)';
+        configureResourceViews = [
+          {
+            title: "Configure Grafana with Teleport's IdP Metadata",
+            component: DownloadMetadataGrafana,
+            eventName: DiscoverEvent.Started,
+          },
+          {
+            title: "Add Grafana's Service Provider to Teleport",
+            component: AddGrafanaSaml,
+            eventName: DiscoverEvent.DeployService,
+          },
+        ];
+        break;
+      default:
+        title = 'Add SAML Application';
+        configureResourceViews = [
+          {
+            title: "Configure Service Provider with Teleport's IdP Metadata",
+            component: DownloadMetadata,
+            eventName: DiscoverEvent.Started,
+          },
+          {
+            title: 'Add Service Provider to Teleport',
+            component: AddServiceProvider,
+            eventName: DiscoverEvent.DeployService,
+          },
+        ];
     }
     return [
       { title, views: configureResourceViews },
