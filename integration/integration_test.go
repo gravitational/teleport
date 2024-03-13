12cbfc64420670f6dc25e66e376f89cc50899b98
Brian Joerger <bjoerger@goteleport.com>
2024-03-13 10:58:59 -0700

    Update integration/integration_test.go
    Co-authored-by: rosstimothy <39066650+rosstimothy@users.noreply.github.com>

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -4834,12 +4834,12 @@ func testX11Forwarding(t *testing.T, suite *integrationTestSuite) {
 						// Reading the display may fail if the session is not fully initialized
 						// and the write to stdin is swallowed.
 						var display string
-						require.EventuallyWithT(t, func(collect *assert.CollectT) {
+						require.EventuallyWithT(t, func(t *assert.CollectT) {
 							// enter 'printenv DISPLAY > /path/to/tmp/file' into the session (dumping the value of DISPLAY into the temp file)
 							_, err = keyboard.Write([]byte(fmt.Sprintf("printenv %v > %s\n\r", x11.DisplayEnv, tmpFile.Name())))
-							assert.NoError(collect, err)
+							assert.NoError(t, err)
 
-							assert.Eventually(collect, func() bool {
+							assert.Eventually(t, func() bool {
 								output, err := os.ReadFile(tmpFile.Name())
 								if err == nil && len(output) != 0 {
 									display = strings.TrimSpace(string(output))
