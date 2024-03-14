dab6e4ef3a4603f3120ac47ea74179132188e2a8
Andrew LeFevre <andrew.lefevre@goteleport.com>
2024-03-14 17:35:13 -0600

    [v15] ensure moderated file transfers only perform allowed operations (#39351)
    * ensure moderated file transfers only perform allowed operations
    
    * ensure moderated file transfers only preform allowed operations
    
    * fix sess test
    
    * wip integration test
    
    * integration test working
    
    * tweak some error messages and test cases
    
    * allow setstat for uploads
    
    * address nits from code review
    
    * expand paths when necessary when creating file transfer request
    
    * improve UX of errors in web UI
    
    * deny request when an invalid user tries to transfer files
    
    * don't allow open requests, they aren't done by the webui
    
    * address feedback
    
    * use a buffered reader when reading the file request, address a few nits
    
    * Remove unnecessary buffered reader wrapper
    
    ---------
    
    Co-authored-by: Zac Bergquist <zac.bergquist@goteleport.com>

--- lib/sshutils/sftp/http.go
+++ lib/sshutils/sftp/http.go
@@ -38,14 +38,6 @@ import (
 type contextKey string
 
 const (
-	// FileTransferDstPath is the dstPath (location) for the requested file transfer. This would be equal
-	// to the file to be downloaded, or location for a file to be uploaded.
-	FileTransferDstPath string = "TELEPORT_FILE_TRANSFER_DST_PATH"
-	// FileTransferRequestID is an optional parameter id of an file transfer request that has gone through
-	// an approval process during a moderated session to allow a file transfer scp command to be executed
-	// used as a value in the file transfer context and env var for exec session
-	FileTransferRequestID contextKey = "TELEPORT_FILE_TRANSFER_REQUEST_ID"
-
 	// ModeratedSessionID is an optional parameter sent during SCP requests to specify which moderated session
 	// to check for valid FileTransferRequests
 	// used as a value in the file transfer context and env var for exec session
