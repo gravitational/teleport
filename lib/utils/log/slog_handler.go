73910bc01b138ba1618d28070432df910be98767
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2024-03-18 14:32:52 -0400

    Make slog attributes output the same content regardless of format (#39428)
    * Make slog attributes output the same content regardless of format
    
    There were a few instances of fmt.Stringers being passed to slog
    as attributes to delay their evaluation as per the performance
    considerations of slog: https://pkg.go.dev/log/slog#hdr-Performance_considerations.
    However, this resulted in the json handler emitting different
    content since they check to see if attributes are json.Marshalers
    and not fmt.Stringers. To rectify the situation, a new slog.LogValuer
    implementation was added that wraps a fmt.Stringer so that attributes
    can be lazily constructed while producing the same output no matter
    the log output format.
    
    * fix: wrap identity and correct typo

--- lib/utils/log/slog_handler.go
+++ lib/utils/log/slog_handler.go
@@ -555,3 +555,26 @@ func getCaller(a slog.Attr) (file string, line int) {
 
 	return file, line
 }
+
+type stringerAttr struct {
+	fmt.Stringer
+}
+
+// StringerAttr creates a [slog.LogValuer] that will defer to
+// the provided [fmt.Stringer]. All slog attributes are always evaluated,
+// even if the log event is discarded due to the configured log level.
+// A text [slog.Handler] will try to defer evaluation if the attribute is a
+// [fmt.Stringer], however, the JSON [slog.Handler] only defers to [json.Marshaler].
+// This means that to defer evaluation and creation of the string representation,
+// the object must implement [fmt.Stringer] and [json.Marshaler], otherwise additional
+// and unwanted values may be emitted if the logger is configured to use JSON
+// instead of text. This wrapping mechanism allows a value that implements [fmt.Stringer],
+// to be guaranteed to be lazily constructed and always output the same
+// content regardless of the output format.
+func StringerAttr(s fmt.Stringer) slog.LogValuer {
+	return stringerAttr{Stringer: s}
+}
+
+func (s stringerAttr) LogValue() slog.Value {
+	return slog.StringValue(s.Stringer.String())
+}
