6931bc58826da52c513a2f62f0730e4a4513b0ac
Trent Clarke <trent@goteleport.com>
2024-03-18 12:46:12 +0000

    Implements ResourceWithLabels on AccessListMember

--- api/types/accesslist/member.go
+++ api/types/accesslist/member.go
@@ -24,6 +24,7 @@ import (
 	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/api/types/header"
 	"github.com/gravitational/teleport/api/types/header/convert/legacy"
+	"github.com/gravitational/teleport/api/utils"
 )
 
 // AccessListMember is an access list member resource.
@@ -115,4 +116,14 @@ func (a *AccessListMember) GetMetadata() types.Metadata {
 func (a *AccessListMember) IsEqual(other *AccessListMember) bool {
 	return a.Spec.Name == other.Spec.Name &&
 		a.Spec.AccessList == other.Spec.AccessList
+}
+
+var _ types.ResourceWithOrigin = (*AccessListMember)(nil)
+var _ types.ResourceWithLabels = (*AccessListMember)(nil)
+
+// MatchSearch goes through select field values of a resource
+// and tries to match against the list of search values.
+func (a *AccessListMember) MatchSearch(values []string) bool {
+	fieldVals := append(utils.MapToStrings(a.GetAllLabels()), a.GetName())
+	return types.MatchSearch(fieldVals, values, nil)
 }
