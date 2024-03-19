8aa5af4fdd7afd1eaa97ddf5faeb7cf8db7e43f2
Hugo Shaka <hugo.hervieux@goteleport.com>
2024-03-19 12:27:32 -0400

    fix panic

--- integrations/access/servicenow/app.go
+++ integrations/access/servicenow/app.go
@@ -138,7 +138,7 @@ func (a *App) init(ctx context.Context) error {
 	log := logger.Get(ctx)
 
 	var err error
-	a.teleport, err = a.Conf.GetTeleportClient(ctx)
+	a.teleport, err = a.conf.GetTeleportClient(ctx)
 	if err != nil {
 		return trace.Wrap(err, "getting teleport client")
 	}
