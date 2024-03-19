90bab495f3f560b089ce6a9989118441fe8726eb
Edward Dowling <EdwardDowling@users.noreply.github.com>
2024-03-19 16:58:52 +0000

    Update lib/services/access_monitoring_rules.go
    Co-authored-by: rosstimothy <39066650+rosstimothy@users.noreply.github.com>

--- lib/services/access_monitoring_rules.go
+++ lib/services/access_monitoring_rules.go
@@ -28,7 +28,6 @@ import (
 	"github.com/gravitational/teleport/lib/utils"
 )
 
-var _ AccessMonitoringRules = (*accessmonitoringrules.Client)(nil)
 
 // AccessMonitoringRules is the AccessMonitoringRule service
 type AccessMonitoringRules interface {
