54a36289f132e08fd08e1f9ee4d5b7879e935d21
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2024-03-19 16:11:47 +0100

    Revert changes to `Clusters.story.test` and `Recordings.story.test`

--- web/packages/teleport/src/Clusters/Clusters.story.test.tsx
+++ web/packages/teleport/src/Clusters/Clusters.story.test.tsx
@@ -18,14 +18,13 @@
 
 import React from 'react';
 
-import { render, screen } from 'design/utils/testing';
+import { render, waitFor } from 'design/utils/testing';
 
 import { Story, createContext } from './Clusters.story';
 
 test('render clusters', async () => {
   const ctx = createContext();
   const { container } = render(<Story value={ctx} />);
-  // wait for Showing 1 - x of y
-  expect(await screen.findByText(/Showing/)).toBeInTheDocument();
+  await waitFor(() => document.querySelector('table'));
   expect(container.firstChild).toMatchSnapshot();
 });
