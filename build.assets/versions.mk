8575ac7e7e13dc109b8c927b2d724cc3d9de502a
Grzegorz Zdunek <gzdunek@users.noreply.github.com>
2024-03-21 14:39:55 +0100

    Update Node.js, Electron and node-pty (#39296) (#39658)
    * Update Node.js to 20.11.1
    
    * Update Electron to 29.1.1
    
    * Update node-pty to 1.1.0-beta12
    
    * Remove commands for building native deps manually
    
    * Remove file skipping in electron-builder that seems to do nothing
    
    * Regenerate `devbox.lock` using the same Devbox version as the CI (0.9.0)
    
    * Update `devbox` to `0.9.1`
    
    * Update `README.md`
    
    * Update electron to 29.1.4
    
    * Update devbox checksum
    
    * Remove mention of Node.js version in teleterm/README.md
    
    (cherry picked from commit 3140ad80148a102012b05b1a9313db283b6fd63b)

--- build.assets/versions.mk
+++ build.assets/versions.mk
@@ -6,7 +6,7 @@
 GOLANG_VERSION ?= go1.21.8
 GOLANGCI_LINT_VERSION ?= v1.56.2
 
-NODE_VERSION ?= 18.19.1
+NODE_VERSION ?= 20.11.1
 
 # Run lint-rust check locally before merging code after you bump this.
 RUST_VERSION ?= 1.71.1
