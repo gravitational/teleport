0de23a71a34f03f418b4e2426a81c28a614ace61
sshahcodes <sshah@goteleport.com>
2024-03-22 00:46:06 -0400

    input param and resource name validator, unit tests

--- lib/web/scripts/samlidp/gcpwif.go
+++ lib/web/scripts/samlidp/gcpwif.go
@@ -21,6 +21,8 @@ package samlidp
 import (
 	"bytes"
 	_ "embed"
+	"regexp"
+	"strconv"
 	"text/template"
 
 	"github.com/gravitational/trace"
@@ -47,10 +49,61 @@ type GCPWIFConfigParams struct {
 // SAML IdP metadata and runs gcloud command to create workforce
 // identity pool and pool provider
 func BuildScript(p GCPWIFConfigParams) (string, error) {
+	if err := p.checkAndSetDefaults(); err != nil {
+		return "", trace.Wrap(err)
+	}
+
 	out := &bytes.Buffer{}
 	if err := gcpWIFConfigTemplate.Execute(out, p); err != nil {
 		return "", trace.Wrap(err)
 	}
 
 	return out.String(), nil
+}
+
+// CheckAndSetDefaults checks if the required params are valid.
+// GCP naming convention docs:
+// https://cloud.google.com/compute/docs/naming-resources#resource-name-format
+func (p *GCPWIFConfigParams) checkAndSetDefaults() error {
+	if err := validateOrgID(p.OrgID); err != nil {
+		return trace.Wrap(err)
+	}
+
+	if err := validateGCPResourceName(p.PoolName); err != nil {
+		return trace.BadParameter("invalid pool name: %v", err)
+	}
+
+	if err := validateGCPResourceName(p.PoolProviderName); err != nil {
+		return trace.BadParameter("invalid pool provider name: %v", err)
+	}
+
+	return nil
+}
+
+func validateOrgID(orgID string) error {
+	if orgID == "" {
+		return trace.BadParameter("organization ID is required.")
+	}
+	if _, err := strconv.Atoi(orgID); err != nil {
+		return trace.BadParameter("organization ID must be of numeric value.")
+	}
+
+	return nil
+}
+
+var isValidGCPResourceName = regexp.MustCompile(`^[a-z]([-a-z0-9]*[a-z0-9])?$`)
+
+func validateGCPResourceName(name string) error {
+	if name == "" {
+		return trace.BadParameter("name is empty.")
+	}
+	if len(name) > 63 {
+		return trace.BadParameter("resource name cannot exceed 63 character length.")
+	}
+
+	if ok := isValidGCPResourceName.MatchString(name); !ok {
+		return trace.BadParameter("resource name does not follow GCP resource naming convention.")
+	}
+
+	return nil
 }
