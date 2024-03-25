4d729f9e761ef623e412948234692687f8529d1d
Rafał Cieślak <rafal.cieslak@goteleport.com>
2024-03-25 18:04:58 +0100

    Add stub of VNet panel in connection list

--- web/packages/teleterm/src/ui/TopBar/Connections/ConnectionsFilterableList/ConnectionsFilterableList.tsx
+++ web/packages/teleterm/src/ui/TopBar/Connections/ConnectionsFilterableList/ConnectionsFilterableList.tsx
@@ -16,55 +16,62 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from 'react';
-
-import { Box, Text } from 'design';
+import { Text } from 'design';
 
 import { FilterableList } from 'teleterm/ui/components/FilterableList';
 import { ExtendedTrackedConnection } from 'teleterm/ui/services/connectionTracker';
-
 import { useKeyboardArrowsNavigationStateUpdate } from 'teleterm/ui/components/KeyboardArrowsNavigation';
+import { VnetConnectionItem, useVnetContext } from 'teleterm/ui/Vnet';
 
 import { ConnectionItem } from './ConnectionItem';
 
-interface ConnectionsFilterableListProps {
+export function ConnectionsFilterableList(props: {
   items: ExtendedTrackedConnection[];
-
   onActivateItem(id: string): void;
-
   onRemoveItem(id: string): void;
-
   onDisconnectItem(id: string): void;
-}
-
-export function ConnectionsFilterableList(
-  props: ConnectionsFilterableListProps
-) {
+  slideToVnet(): void;
+}) {
   const { setActiveIndex } = useKeyboardArrowsNavigationStateUpdate();
+  const { isSupported: isVnetSupported } = useVnetContext();
+
+  if (!isVnetSupported && props.items.length === 0) {
+    return <Text color="text.muted">No Connections</Text>;
+  } // With VNet being supported, there's always at least one item to show – the VNet item.
 
   return (
-    <Box width="300px">
-      {props.items.length ? (
-        <FilterableList<ExtendedTrackedConnection>
-          items={props.items}
-          filterBy="title"
-          placeholder="Search Connections"
-          onFilterChange={value =>
-            value.length ? setActiveIndex(0) : setActiveIndex(-1)
-          }
-          Node={({ item, index }) => (
-            <ConnectionItem
-              item={item}
-              index={index}
-              onActivate={() => props.onActivateItem(item.id)}
-              onRemove={() => props.onRemoveItem(item.id)}
-              onDisconnect={() => props.onDisconnectItem(item.id)}
-            />
-          )}
+    <FilterableList<ExtendedTrackedConnection>
+      items={props.items}
+      filterBy="title"
+      placeholder="Search Connections"
+      onFilterChange={value =>
+        value.length ? setActiveIndex(0) : setActiveIndex(-1)
+      }
+      Node={({ item, index }) => (
+        <ConnectionItem
+          item={item}
+          index={index}
+          onActivate={() => props.onActivateItem(item.id)}
+          onRemove={() => props.onRemoveItem(item.id)}
+          onDisconnect={() => props.onDisconnectItem(item.id)}
         />
-      ) : (
-        <Text color="text.muted">No Connections</Text>
       )}
-    </Box>
+    >
+      {/*
+        TODO(ravicious): Change the type of FilterableList above to something like
+        FilterableList<ExtendedTrackedConnection | Vnet> and render a different component in Node
+        depending on the item type. This way VNet will have tighter integration with the connection
+        list, i.e. be searchable and selectable through keyboard.
+
+        We don't want to put VNet into ExtendedTrackedConnection because these are two fundamentally
+        different things.
+      */}
+      {isVnetSupported && (
+        <VnetConnectionItem
+          onClick={props.slideToVnet}
+          title="Open VNet panel"
+        />
+      )}
+    </FilterableList>
   );
 }
