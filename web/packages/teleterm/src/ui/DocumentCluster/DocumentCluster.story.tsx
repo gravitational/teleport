5dde943765e80534cef02f87b757bf3935bd8434
Rafał Cieślak <rafal.cieslak@goteleport.com>
2024-03-25 17:48:02 +0100

    Account for VNet in resource buttons

--- web/packages/teleterm/src/ui/DocumentCluster/DocumentCluster.story.tsx
+++ web/packages/teleterm/src/ui/DocumentCluster/DocumentCluster.story.tsx
@@ -43,6 +43,7 @@ import { ConnectMyComputerContextProvider } from 'teleterm/ui/ConnectMyComputer'
 import * as docTypes from 'teleterm/ui/services/workspacesService/documentsService/types';
 import * as tsh from 'teleterm/services/tshd/types';
 import { makeDocumentCluster } from 'teleterm/ui/services/workspacesService/documentsService/testHelpers';
+import { VnetContextProvider } from 'teleterm/ui/Vnet';
 
 import DocumentCluster from './DocumentCluster';
 import { ResourcesContextProvider } from './resourcesContext';
@@ -335,15 +336,17 @@ function renderState({
 
   return (
     <AppContextProvider value={appContext}>
-      <MockWorkspaceContextProvider>
-        <ResourcesContextProvider>
-          <ConnectMyComputerContextProvider rootClusterUri={rootClusterUri}>
-            <Wrapper>
-              <DocumentCluster visible={true} doc={doc} />
-            </Wrapper>
-          </ConnectMyComputerContextProvider>
-        </ResourcesContextProvider>
-      </MockWorkspaceContextProvider>
+      <VnetContextProvider>
+        <MockWorkspaceContextProvider>
+          <ResourcesContextProvider>
+            <ConnectMyComputerContextProvider rootClusterUri={rootClusterUri}>
+              <Wrapper>
+                <DocumentCluster visible={true} doc={doc} />
+              </Wrapper>
+            </ConnectMyComputerContextProvider>
+          </ResourcesContextProvider>
+        </MockWorkspaceContextProvider>
+      </VnetContextProvider>
     </AppContextProvider>
   );
 }
