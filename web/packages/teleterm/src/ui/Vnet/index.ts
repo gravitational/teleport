9c7b7276ddf6679e085de4d5f8b71b314622b96a
Rafał Cieślak <rafal.cieslak@goteleport.com>
2024-03-25 18:04:58 +0100

    Add stub of VNet panel in connection list

--- web/packages/teleterm/src/ui/Vnet/index.ts
+++ web/packages/teleterm/src/ui/Vnet/index.ts
@@ -16,4 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+export * from './VnetSliderStep';
 export * from './vnetContext';
+export { VnetConnectionItem } from './VnetConnectionItem';
