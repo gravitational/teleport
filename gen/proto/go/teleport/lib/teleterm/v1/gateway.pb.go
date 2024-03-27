17944a237a8d746cb793990bd863a9f7ded40a19
Rafał Cieślak <rafal.cieslak@goteleport.com>
2024-03-27 16:50:14 +0100

    Update comments for GatewayCLICommand message

--- gen/proto/go/teleport/lib/teleterm/v1/gateway.pb.go
+++ gen/proto/go/teleport/lib/teleterm/v1/gateway.pb.go
@@ -171,8 +171,9 @@ func (x *Gateway) GetGatewayCliCommand() *GatewayCLICommand {
 	return nil
 }
 
-// GatewayCLICommand represents a command that the user can execute to connect to the gateway
-// resource. It is a direct translation of os.exec.Cmd.
+// GatewayCLICommand represents a command that the user can execute to connect to a gateway
+// resource. It is a combination of two different os/exec.Cmd structs, where path, args and env are
+// directly taken from one Cmd and the preview field is constructed from another Cmd.
 type GatewayCLICommand struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -182,10 +183,14 @@ type GatewayCLICommand struct {
 	Args []string `protobuf:"bytes,2,rep,name=args,proto3" json:"args,omitempty"`
 	Env  []string `protobuf:"bytes,3,rep,name=env,proto3" json:"env,omitempty"`
 	// preview is used to show the user what command will be executed before they decide to run it.
-	// It's like os.exec.Cmd.String with two exceptions:
+	// It can also be copied and then pasted into a terminal.
+	// It's like os/exec.Cmd.String with two exceptions:
 	//
 	// 1) It is prepended with Cmd.Env.
 	// 2) The command name is relative and not absolute.
+	// 3) It is taken from a different Cmd than the other fields in this message. This Cmd uses a
+	// special print format which makes the args suitable to be entered into a terminal, but not to
+	// directly spawn a process.
 	Preview string `protobuf:"bytes,4,opt,name=preview,proto3" json:"preview,omitempty"`
 }
 
