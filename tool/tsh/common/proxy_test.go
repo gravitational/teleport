a0c5b4395e0b5c6f751ecfbc166d17d72993aa44
joerger <bjoerger@goteleport.com>
2024-04-04 17:07:08 -0700

    Fix mockSSOLogin.

--- tool/tsh/common/proxy_test.go
+++ tool/tsh/common/proxy_test.go
@@ -629,7 +629,11 @@ func TestProxySSHJumpHost(t *testing.T) {
 					"--insecure",
 					"login",
 					"--proxy", rootProxyAddr.String(),
-				}, setHomePath(tshHome), setMockSSOLogin(rootServer.GetAuthServer(), accessUser, connector.GetName()))
+				}, setHomePath(tshHome), func(cf *CLIConf) error {
+					cf.MockSSOLogin = mockSSOLogin(t, rootServer.GetAuthServer(), accessUser)
+					cf.AuthConnector = connector.GetName()
+					return nil
+				})
 				require.NoError(t, err)
 
 				// Connect through the leaf proxy jumphost.
