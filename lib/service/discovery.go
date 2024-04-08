12ea7d61c5487149c7e02f4ca5416300b289032d
Tiago Silva <tiago.silva@goteleport.com>
2024-04-08 10:02:05 +0100

    Empty `tlsConfig.ServerName` field
    Drop `tlsConfig.ServerName` default value to avoid SNI dial problems
    to access graph service.
    
    Signed-off-by: Tiago Silva <tiago.silva@goteleport.com>

--- lib/service/discovery.go
+++ lib/service/discovery.go
@@ -71,6 +71,10 @@ func (process *TeleportProcess) initDiscoveryService() error {
 		return trace.Wrap(err)
 	}
 
+	if tlsConfig != nil {
+		tlsConfig.ServerName = "" /* empty the server name to avoid SNI collisions with access graph addr */
+	}
+
 	accessGraphCfg, err := buildAccessGraphFromTAGOrFallbackToAuth(
 		process.ExitContext(),
 		process.Config,
