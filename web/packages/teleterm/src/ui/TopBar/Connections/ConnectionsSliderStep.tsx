c849ff66b84840c37197e458f86cde725dcf81f4
Rafał Cieślak <rafal.cieslak@goteleport.com>
2024-04-09 12:00:10 +0200

    Get rid of useConnections hook

--- web/packages/teleterm/src/ui/TopBar/Connections/ConnectionsSliderStep.tsx
+++ web/packages/teleterm/src/ui/TopBar/Connections/ConnectionsSliderStep.tsx
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { useEffect } from 'react';
+import { useState } from 'react';
 import { Box } from 'design';
 import { StepComponentProps } from 'design/StepSlider';
 
@@ -24,39 +24,62 @@ import { useAppContext } from 'teleterm/ui/appContextProvider';
 import { KeyboardArrowsNavigation } from 'teleterm/ui/components/KeyboardArrowsNavigation';
 
 import { ConnectionsFilterableList } from './ConnectionsFilterableList/ConnectionsFilterableList';
-import { useConnections } from './useConnections';
 
 export const ConnectionsSliderStep = (
   props: StepComponentProps & { closeConnectionList: () => void }
 ) => {
   const { connectionTracker } = useAppContext();
-  const connections = useConnections();
-  const { updateSorting } = connections;
+  connectionTracker.useState();
 
+  const items = connectionTracker.getConnections();
+  const [sortedIds, setSortedIds] = useState<string[]>(null);
+
+  // Sorting needs to be updated only once when the component gets first rendered. This is so that
+  // when new connections are added while the list is open, the sorting is _not_ updated and new
+  // items end up at the top of the list.
+  //
+  // This also keeps the list stable when you e.g. disconnect the first connection in the list.
+  // Instead of the item jumping around, it stays as the top until the user closes and then opens
+  // the list again.
+  if (sortedIds === null) {
+    const sorted = items
+      .slice()
+      // New connections are pushed to the list in `connectionTracker`, so we have to reverse them
+      // to get the newest items on the top
+      .reverse()
+      // Connected items first.
+      .sort((a, b) => (a.connected === b.connected ? 0 : a.connected ? -1 : 1))
+      .map(a => a.id);
+    setSortedIds(sorted);
+    return null;
+  }
+
+  const sortedItems =
+    // It is possible that new connections are added when the menu is open.
+    // They will have -1 index and appear on the top.
+    // Items are sorted by insertion order, meaning that if I add A then B,
+    // then close both, open A and close it, it's going to appear after B
+    // even though it was used more recently than B.
+    items
+      .slice()
+      .sort((a, b) => sortedIds.indexOf(a.id) - sortedIds.indexOf(b.id));
+
+  const removeItem = connectionTracker.removeItem.bind(connectionTracker);
+  const disconnectItem =
+    connectionTracker.disconnectItem.bind(connectionTracker);
   const activateItem = (id: string) => {
     props.closeConnectionList();
     connectionTracker.activateItem(id, { origin: 'connection_list' });
   };
 
-  useEffect(() => {
-    updateSorting();
-    // Sorting needs to be updated only once when the component gets rendered. This is so that when
-    // new connections are added while the list is open, the sorting is _not_ updated and new items
-    // end up at the top of the list.
-    //
-    // This also keeps the list stable when you e.g. disconnect the first connection in the list.
-    // Instead of the item jumping around, it stays as the first until the user closes and then
-    // opens the list again.
-  }, []);
-
   return (
     <Box p={2} ref={props.refCallback}>
       <KeyboardArrowsNavigation>
         <ConnectionsFilterableList
-          items={connections.items}
+          items={sortedItems}
           onActivateItem={activateItem}
-          onRemoveItem={connections.removeItem}
-          onDisconnectItem={connections.disconnectItem}
+          onRemoveItem={removeItem}
+          onDisconnectItem={disconnectItem}
           slideToVnet={props.next}
         />
       </KeyboardArrowsNavigation>
