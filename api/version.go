b37c4fd9fdb58bc7ea22a4f571417933e34b0618
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2024-04-12 16:11:33 +0200

    14.3.14-dev.kubelsdebug.2

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "14.3.14-dev.kubelsdebug.1"
+const Version = "14.3.14-dev.kubelsdebug.2"
 
 var SemVersion = semver.New(Version)
