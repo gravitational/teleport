b7210e5116aeb32d9068f4c1444bb2b8e6e0965f
Noah Stride <noah.stride@goteleport.com>
2024-04-15 17:59:57 +0100

    Add `teleport tpm identify` and `tbot tpm identify` commands (#40418) (#40557)
    * Add func for tbot tpm identify command
    
    * Add `tbot tpm identify` command
    
    * Move helper to `tpm` package and add tests
    
    * Add tpm identify command to `teleport`

--- tool/tbot/main.go
+++ tool/tbot/main.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"log/slog"
 	"os"
 	"os/signal"
 	"strings"
@@ -36,6 +37,7 @@ import (
 	"github.com/gravitational/teleport/lib/observability/tracing"
 	"github.com/gravitational/teleport/lib/tbot"
 	"github.com/gravitational/teleport/lib/tbot/config"
+	"github.com/gravitational/teleport/lib/tpm"
 	"github.com/gravitational/teleport/lib/utils"
 )
 
@@ -159,6 +161,9 @@ func Run(args []string, stdout io.Writer) error {
 	kubeCredentialsCmd := kubeCmd.Command("credentials", "Get credentials for kubectl access").Hidden()
 	kubeCredentialsCmd.Flag("destination-dir", "The destination directory with which to generate Kubernetes credentials").Required().StringVar(&cf.DestinationDir)
 
+	tpmCommand := app.Command("tpm", "Commands related to managing TPM joining functionality.")
+	tpmIdentifyCommand := tpmCommand.Command("identify", "Output identifying information related to the TPM detected on the system.")
+
 	utils.UpdateAppUsageTemplate(app, args)
 	command, err := app.Parse(args)
 	if err != nil {
@@ -230,6 +235,12 @@ func Run(args []string, stdout io.Writer) error {
 		err = onProxyCommand(botConfig, &cf)
 	case kubeCredentialsCmd.FullCommand():
 		err = onKubeCredentialsCommand(botConfig)
+	case tpmIdentifyCommand.FullCommand():
+		query, err := tpm.Query(context.Background(), slog.Default())
+		if err != nil {
+			return trace.Wrap(err, "querying TPM")
+		}
+		tpm.PrintQuery(query, cf.Debug, os.Stdout)
 	default:
 		// This should only happen when there's a missing switch case above.
 		err = trace.BadParameter("command %q not configured", command)
