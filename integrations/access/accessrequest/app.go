e038bf106ac606a8ba8056f7f14c610198ac6daa
Edward Dowling <EdwardDowling@users.noreply.github.com>
2024-04-15 18:15:59 +0100

    [v15] Fix opsgenie plugin to use correct annotation to determine whether or not to create alerts  (#40121)
    * Add Opsgenie annotation fix
    
    * Fix behaviour when only notify label is set
    
    And add tests for accessrequest app
    
    * Fix opsgenie tests
    
    * Update servicenow tests to use new name for annotations
    
    * Appease linter
    
    * Add missing license

--- integrations/access/accessrequest/app.go
+++ integrations/access/accessrequest/app.go
@@ -352,16 +352,23 @@ func (a *App) getMessageRecipients(ctx context.Context, req types.AccessRequest)
 		recipientSet.Add(common.Recipient{})
 		return recipientSet.ToSlice()
 	case types.PluginTypeOpsgenie:
-		if recipients, ok := req.GetSystemAnnotations()[types.TeleportNamespace+types.ReqAnnotationSchedulesLabel]; ok {
-			for _, recipient := range recipients {
-				rec, err := a.bot.FetchRecipient(ctx, recipient)
-				if err != nil {
-					log.Warning(err)
-				}
-				recipientSet.Add(*rec)
-			}
-			return recipientSet.ToSlice()
+		// When both notify-services and approve-schedules are present, each is used for their own intended purpose.
+		recipients := make([]string, 0)
+		if approveSchedules, ok := req.GetSystemAnnotations()[types.TeleportNamespace+types.ReqAnnotationApproveSchedulesLabel]; ok {
+			recipients = approveSchedules
 		}
+		if notifySchedules, ok := req.GetSystemAnnotations()[types.TeleportNamespace+types.ReqAnnotationNotifySchedulesLabel]; ok {
+			recipients = notifySchedules
+		}
+		for _, recipient := range recipients {
+			rec, err := a.bot.FetchRecipient(ctx, recipient)
+			if err != nil {
+				log.Warningf("Failed to fetch Opsgenie recipient: %v", err)
+				continue
+			}
+			recipientSet.Add(*rec)
+		}
+		return recipientSet.ToSlice()
 	}
 
 	validEmailSuggReviewers := []string{}
