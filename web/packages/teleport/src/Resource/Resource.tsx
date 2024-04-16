93350f6a304e6d263fb43c0b669a0c3e08a3d6e0
Michael Myers <michael.myers@goteleport.com>
2024-04-16 15:45:21 +0100

    Add CreateLockButton components

--- web/packages/teleport/src/Resource/Resource.tsx
+++ web/packages/teleport/src/Resource/Resource.tsx
@@ -16,11 +16,44 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from 'react';
+import { useCallback } from 'react';
+import { Box } from 'design';
 import { useParams } from 'react-router';
+import { useAsync } from 'shared/hooks/useAsync';
+
+import { lockService } from 'teleport/services/locks';
+
+import { CreateLockButton } from './CreateLockButton';
 
 export function Resource() {
   const { resourceId } = useParams<{ clusterId: string; resourceId: string }>();
 
-  return <>resource id: {resourceId}</>;
+  const [createLockAttempt, runCreateLockAttempt] = useAsync(
+    useCallback(
+      async (
+        lockType: string,
+        targetId: string,
+        message: string,
+        ttl: string
+      ) => {
+        lockService.createLock({
+          targets: { [lockType]: targetId },
+          message,
+          ttl,
+        });
+      },
+      []
+    )
+  );
+
+  return (
+    <Box>
+      <CreateLockButton
+        createLock={runCreateLockAttempt}
+        createLockAttempt={createLockAttempt}
+        targetId={resourceId}
+        lockType={'node'}
+      />
+    </Box>
+  );
 }
