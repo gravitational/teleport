fa941598550f93b603a5e47a7e442eb9a4accd80
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2024-04-16 11:38:51 +0200

    Fix missing shell error checking logic

--- lib/shell/shell_test.go
+++ lib/shell/shell_test.go
@@ -27,9 +27,8 @@ func TestGetShell(t *testing.T) {
 	require.NoError(t, err)
 	require.True(t, shell == "/bin/bash" || shell == "/bin/sh")
 
-	shell, err = GetLoginShell("non-existent-user")
-	require.NoError(t, err)
-	require.Equal(t, shell, DefaultShell)
+	_, err = GetLoginShell("non-existent-user")
+	require.ErrorContains(t, err, "unknown user")
 
 	shell, err = GetLoginShell("nobody")
 	require.NoError(t, err)
