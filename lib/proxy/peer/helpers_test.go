0067fe3aec204b07756066b21e59a0ebb356901e
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2024-04-19 16:25:20 +0200

    [v14] Fix some gRPC-tunneled connections (#40675)
    * Fix some gRPC-tunneled connections (#40632)
    
    * Fix some gRPC-tunneled connections
    
    * Fix tests checking the return value of dial()
    
    * go 1.21 compatibility

--- lib/proxy/peer/helpers_test.go
+++ lib/proxy/peer/helpers_test.go
@@ -202,9 +202,7 @@ func setupClient(t *testing.T, clientCA, serverCA *tlsca.CertAuthority, role typ
 	})
 	require.NoError(t, err)
 
-	t.Cleanup(func() {
-		client.Shutdown()
-	})
+	t.Cleanup(func() { client.Stop() })
 
 	return client
 }
@@ -260,11 +258,7 @@ func setupServer(t *testing.T, name string, serverCA, clientCA *tlsca.CertAuthor
 	return server, ts
 }
 
-func sendMsg(t *testing.T, stream clientapi.ProxyService_DialNodeClient) {
-	err := stream.Send(&clientapi.Frame{
-		Message: &clientapi.Frame_Data{
-			Data: &clientapi.Data{Bytes: []byte("ping")},
-		},
-	})
+func sendMsg(t *testing.T, stream frameStream) {
+	err := stream.Send([]byte("ping"))
 	require.NoError(t, err)
 }
