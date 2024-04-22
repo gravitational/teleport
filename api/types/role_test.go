f213b2ccd9f7f180af9c07ec41e0661d833bb3d2
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2024-04-22 12:04:26 +0200

    Correctly filter out system roles (#40545)
    * Correctly filter out system roles
    
    * Move `SkipSystemRoles` check above `SearchKeywords` check
    
    * Add tests
    
    (cherry picked from commit 22a33891526a43884a7ae7faf3ad0f89c63622fe)

--- api/types/role_test.go
+++ api/types/role_test.go
@@ -437,3 +437,65 @@ func TestUnmarshallCreateHostUserModeYAML(t *testing.T) {
 		require.Equal(t, tc.expected, got)
 	}
 }
+
+func TestRoleFilterMatch(t *testing.T) {
+	regularRole := RoleV6{
+		Metadata: Metadata{
+			Name: "request-approver",
+		},
+	}
+	systemRole := RoleV6{
+		Metadata: Metadata{
+			Name: "bot",
+			Labels: map[string]string{
+				TeleportInternalResourceType: SystemResource,
+			},
+		},
+	}
+
+	tests := []struct {
+		name        string
+		role        *RoleV6
+		filter      *RoleFilter
+		shouldMatch bool
+	}{
+		{
+			name:        "empty filter should match everything",
+			role:        &regularRole,
+			filter:      &RoleFilter{},
+			shouldMatch: true,
+		},
+		{
+			name:        "correct search keyword should match the role",
+			role:        &regularRole,
+			filter:      &RoleFilter{SearchKeywords: []string{"appr"}},
+			shouldMatch: true,
+		},
+		{
+			name:        "incorrect search keyword shouldn't match the role",
+			role:        &regularRole,
+			filter:      &RoleFilter{SearchKeywords: []string{"xyz"}},
+			shouldMatch: false,
+		},
+		{
+			name:        "skip system roles filter shouldn't match the system role",
+			role:        &systemRole,
+			filter:      &RoleFilter{SkipSystemRoles: true},
+			shouldMatch: false,
+		},
+		{
+			name:        "skip system roles filter should match the regular role",
+			role:        &regularRole,
+			filter:      &RoleFilter{SkipSystemRoles: true},
+			shouldMatch: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.role.CheckAndSetDefaults()
+			require.NoError(t, err)
+			require.Equal(t, tt.shouldMatch, tt.filter.Match(tt.role))
+		})
+	}
+}
