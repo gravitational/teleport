c68a2372abbfe36236e65c62a05b97ba992b6b29
Alan Parra <alan.parra@goteleport.com>
2024-04-24 13:25:59 -0300

    Fix compilation issues

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -2220,7 +2220,7 @@ func (a *Server) AugmentContextUserCertificates(
 	}
 
 	// Issue audit event on success, same as [Server.generateCert].
-	a.emitCertCreateEvent(ctx, newIdentity, notAfter)
+	a.emitCertCreateEvent(ctx, &newIdentity, notAfter)
 
 	return &proto.Certs{
 		SSH: newAuthorizedKey,
