53afcb01fded4d6fdf0aeb0fc936d5fc74e7aa95
mcbattirola <matheus.battirola@goteleport.com>
2024-04-26 08:34:58 -0300

    Update godocs comments

--- api/client/webclient/webconfig.go
+++ api/client/webclient/webconfig.go
@@ -77,7 +77,7 @@ type WebConfig struct {
 	HideInaccessibleFeatures bool `json:"hideInaccessibleFeatures"`
 	// CustomTheme is a string that represents the name of the custom theme that the WebUI should use.
 	CustomTheme string `json:"customTheme"`
-	// IsTeam is true if [Features.ProductType] = Team
+	// Deprecated: IsTeam is true if [Features.ProductType] = Team
 	// Prefer checking the cluster features over this flag, as this will be removed.
 	IsTeam bool `json:"isTeam"`
 	// IsIGSEnabled is true if [Features.IdentityGovernance] = true
@@ -92,19 +92,19 @@ type WebConfig struct {
 	IsStripeManaged bool `json:"isStripeManaged"`
 	// ExternalAuditStorage indicates whether the EAS feature is enabled in the cluster.
 	ExternalAuditStorage bool `json:"externalAuditStorage"`
-	// PremiumSupport indicates if the customer has premium support
+	// PremiumSupport indicates whether the customer has premium support
 	PremiumSupport bool `json:"premiumSupport"`
 	// JoinActiveSessions indicates whether joining active sessions via web UI is enabled
 	JoinActiveSessions bool `json:"joinActiveSessions"`
-	// AccessRequests enables access requests
+	// AccessRequests indicates whether access requests are enabled
 	AccessRequests bool `json:"accessRequests"`
-	// TrustedDevices enables trusted devices pages
+	// TrustedDevices indicates whether trusted devices page is enabled
 	TrustedDevices bool `json:"trustedDevices"`
-	// OIDC enables the OIDC integration flow
+	// OIDC indicates whether the OIDC integration flow is enabled
 	OIDC bool `json:"oidc"`
-	// SAML enables the SAML integration flow
+	// SAML indicates whether the SAML integration flow is enabled
 	SAML bool `json:"saml"`
-	// MobileDeviceManagement enables adding Jamf plugin
+	// MobileDeviceManagement indicates whether adding Jamf plugin is enabled
 	MobileDeviceManagement bool `json:"mobileDeviceManagement"`
 }
 
