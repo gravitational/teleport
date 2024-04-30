a584e9631678491ce28becc2fc9364187694371f
Trent Clarke <trent@goteleport.com>
2024-04-30 05:35:42 +0000

    Let tctl make plugins

--- tool/tctl/common/plugins_command.go
+++ tool/tctl/common/plugins_command.go
@@ -21,22 +21,34 @@ package common
 import (
 	"context"
 	"fmt"
-
 	"github.com/alecthomas/kingpin/v2"
-	"github.com/gravitational/trace"
-
+	"github.com/google/uuid"
 	pluginsv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/plugins/v1"
 	"github.com/gravitational/teleport/api/types"
+	"github.com/gravitational/teleport/e/lib/teleport"
 	"github.com/gravitational/teleport/lib/auth"
 	"github.com/gravitational/teleport/lib/service/servicecfg"
+	"github.com/gravitational/teleport/lib/services"
+	"github.com/gravitational/teleport/lib/utils"
+	"github.com/gravitational/trace"
+	"github.com/sirupsen/logrus"
+	"io"
+	"os"
 )
 
 // PluginsCommand allows for management of plugins.
 type PluginsCommand struct {
 	config     *servicecfg.Config
 	cleanupCmd *kingpin.CmdClause
+	installCmd *kingpin.CmdClause
 	pluginType string
 	dryRun     bool
+
+	install struct {
+		definitionFile string
+		token          string
+		bcryptToken    bool
+	}
 }
 
 // Initialize creates the plugins command and subcommands
@@ -45,10 +57,20 @@ func (p *PluginsCommand) Initialize(app *kingpin.Application, config *servicecfg
 	p.dryRun = true
 
 	pluginsCommand := app.Command("plugins", "Manage Teleport plugins.").Hidden()
+
 	p.cleanupCmd = pluginsCommand.Command("cleanup", "Cleans up the given plugin type.")
 	p.cleanupCmd.Arg("type", "The type of plugin to cleanup. Only supports okta at present.").Required().EnumVar(&p.pluginType, string(types.PluginTypeOkta))
 	p.cleanupCmd.Flag("dry-run", "Dry run the cleanup command. Dry run defaults to on.").Default("true").BoolVar(&p.dryRun)
 
+	p.installCmd = pluginsCommand.Command("install", "Install new plugin instance")
+	p.installCmd.Arg("filename", "File containing the plugin definition").
+		Required().
+		ExistingFileVar(&p.install.definitionFile)
+	p.installCmd.Flag("api-token", "API token to install with plugin").
+		StringVar(&p.install.token)
+	p.installCmd.Flag("hash-token", "Hash the token before storage.").
+		Default("false").
+		BoolVar(&p.install.bcryptToken)
 }
 
 // Cleanup cleans up the given plugin.
@@ -96,11 +118,105 @@ func (p *PluginsCommand) Cleanup(ctx context.Context, clusterAPI *auth.Client) e
 	return nil
 }
 
+func (p *PluginsCommand) Install(ctx context.Context, client *auth.Client) error {
+	pi, err := loadPlugin(p.install.definitionFile)
+	if err != nil {
+		return trace.Wrap(err, "loading plugin")
+	}
+
+	pluginName := pi.GetName()
+	plugin, ok := pi.(*types.PluginV1)
+	if !ok {
+		return trace.BadParameter("Plugin %q has invalid type %T", pluginName, p)
+	}
+
+	// The loaded plugin may have a credential ref merely to satisfy the plugin resource
+	// Validation. Delete it if its one we know will be overwritten anyway.
+	if plugin.Credentials != nil && plugin.Credentials.GetStaticCredentialsRef() != nil {
+		plugin.Credentials = nil
+	}
+
+	var creds *types.PluginStaticCredentialsV1
+	var credLabels map[string]string
+	if p.install.token != "" {
+		// The Plugins service currently panics Teleport on an empty `CredentialsLabels`
+		// map in a CreatePluginRequest, which is not good. To get around this for
+		// unpatched Teleport servers we create a dummy PluginID label that we know the
+		// backend will overwrite.
+		pluginID := uuid.NewString()
+
+		if plugin.Metadata.Labels == nil {
+			plugin.Metadata.Labels = map[string]string{}
+		}
+		plugin.Metadata.Labels[teleport.PluginLabel] = pluginID
+		credLabels = map[string]string{teleport.PluginLabel: pluginID}
+
+		creds = &types.PluginStaticCredentialsV1{
+			ResourceHeader: types.ResourceHeader{
+				Metadata: types.Metadata{
+					Name: pluginName,
+				},
+			},
+			Spec: &types.PluginStaticCredentialsSpecV1{
+				Credentials: &types.PluginStaticCredentialsSpecV1_APIToken{
+					APIToken: p.install.token,
+				},
+			},
+		}
+	}
+
+	plugins := client.PluginsClient()
+	_, err = plugins.CreatePlugin(ctx, &pluginsv1.CreatePluginRequest{
+		Plugin:            plugin,
+		StaticCredentials: creds,
+		CredentialLabels:  credLabels,
+	})
+
+	if err != nil {
+		fmt.Printf("Plugin creation failed: %v", err)
+		return trace.Wrap(err)
+	}
+
+	return nil
+}
+
+func loadPlugin(filename string) (types.Plugin, error) {
+	var src io.Reader
+	if filename == "" {
+		src = os.Stdin
+	} else {
+		f, err := utils.OpenFileAllowingUnsafeLinks(filename)
+		if err != nil {
+			return nil, trace.Wrap(err)
+		}
+		defer func() {
+			if err := f.Close(); err != nil {
+				logrus.Debugf("Failed closing plugin definition file: %v", err)
+			}
+		}()
+		src = f
+	}
+
+	text, err := utils.ReadAtMost(src, 1024)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	plugin, err := services.UnmarshalPlugin(text)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	return plugin, nil
+}
+
 // TryRun runs the plugins command
 func (p *PluginsCommand) TryRun(ctx context.Context, cmd string, client *auth.Client) (match bool, err error) {
 	switch cmd {
 	case p.cleanupCmd.FullCommand():
 		err = p.Cleanup(ctx, client)
+	case p.installCmd.FullCommand():
+		err = p.Install(ctx, client)
 	default:
 		return false, nil
 	}
