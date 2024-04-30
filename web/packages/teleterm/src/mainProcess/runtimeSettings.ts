c202e717bb18a23cb885899d88600e9e0afe9ab4
Rafał Cieślak <rafal.cieslak@goteleport.com>
2024-04-30 17:15:55 +0200

    Move tshd flags out of runtime settings

--- web/packages/teleterm/src/mainProcess/runtimeSettings.ts
+++ web/packages/teleterm/src/mainProcess/runtimeSettings.ts
@@ -23,11 +23,9 @@ import path from 'path';
 import { app } from 'electron';
 
 import Logger from 'teleterm/logger';
-import { staticConfig } from 'teleterm/staticConfig';
 
 import { GrpcServerAddresses, RuntimeSettings } from './types';
 import { loadInstallationId } from './loadInstallationId';
-import { getAgentsDir } from './createAgentConfigFile';
 
 const { argv, env } = process;
 
@@ -76,8 +74,6 @@ export function getRuntimeSettings(): RuntimeSettings {
   // Before switching to the recommended path, we need to investigate the impact of this change.
   // https://www.electronjs.org/docs/latest/api/app#appgetpathname
   const logsDir = path.join(userDataDir, 'logs');
-  // DO NOT expose agentsDir through RuntimeSettings. See the comment in getAgentsDir.
-  const agentsDir = getAgentsDir(userDataDir);
   const installationId = loadInstallationId(
     path.resolve(app.getPath('userData'), 'installation_id')
   );
@@ -86,18 +82,6 @@ export function getRuntimeSettings(): RuntimeSettings {
     binaryPath: tshBinPath,
     homeDir: getTshHomeDir(),
     requestedNetworkAddress: tshAddress,
-    flags: [
-      'daemon',
-      'start',
-      // grpc-js requires us to pass localhost:port for TCP connections,
-      // for tshd we have to specify the protocol as well.
-      `--addr=${tshAddress}`,
-      `--certs-dir=${getCertsDir()}`,
-      `--prehog-addr=${staticConfig.prehogAddress}`,
-      `--kubeconfigs-dir=${kubeConfigsDir}`,
-      `--agents-dir=${agentsDir}`,
-      `--installation-id=${installationId}`,
-    ],
   };
   const sharedProcess = {
     requestedNetworkAddress: sharedAddress,
@@ -114,13 +98,6 @@ export function getRuntimeSettings(): RuntimeSettings {
   //
   // A workaround is to read the version from `process.env.npm_package_version`.
   const appVersion = dev ? process.env.npm_package_version : app.getVersion();
-
-  if (insecure) {
-    tshd.flags.unshift('--insecure');
-  }
-  if (debug) {
-    tshd.flags.unshift('--debug');
-  }
 
   return {
     dev,
