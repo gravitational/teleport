631a641a0fe94408ea11d40cac4d1e5676d19f0e
Rafał Cieślak <rafal.cieslak@goteleport.com>
2024-04-30 17:15:55 +0200

    Move tshd flags out of runtime settings

--- web/packages/teleterm/src/mainProcess/fixtures/mocks.ts
+++ web/packages/teleterm/src/mainProcess/fixtures/mocks.ts
@@ -156,7 +156,6 @@ export const makeRuntimeSettings = (
     requestedNetworkAddress: '',
     binaryPath: '',
     homeDir: '',
-    flags: [],
   },
   sharedProcess: {
     requestedNetworkAddress: '',
