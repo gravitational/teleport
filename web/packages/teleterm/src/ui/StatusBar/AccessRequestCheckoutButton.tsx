1abd8e9d50478f9ffecb6164e6505288598ebaf8
Grzegorz Zdunek <gzdunek@users.noreply.github.com>
2024-05-01 21:47:12 +0200

    [v15] Move the shared part of access requests UI to OSS (#41065)
    * Move `teleport.e/AccessRequests` to `shared/components/AccessRequests`
    
    The last commit from teleport.e that includes the original files: gravitational/teleport.e@5ca6881e29d5342eea70201c3f8b365320348c46
    
    * Move `teleport.e/services/accessRequests` to `shared/services/AccessRequests`
    
    * Move `teleterm.e/ui` to `teleterm/ui`
    
    * `make fix-license`
    
    * Fix imports
    
    * Run prettier and eslint
    
    * Rename stories
    
    * Remove ts-ignore comments
    
    `accessRequestsService.test.ts` was refactored slightly,
    we don't need assumed requests there at all.

---
+++ web/packages/teleterm/src/ui/StatusBar/AccessRequestCheckoutButton.tsx
@@ -0,0 +1,45 @@
+/**
+ * Teleport
+ * Copyright (C) 2024 Gravitational, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+
+import { ButtonPrimary, Text } from 'design';
+import { EnvelopeOpen } from 'design/Icon';
+
+import { useAccessRequestsButton } from 'teleterm/ui/StatusBar/useAccessRequestCheckoutButton';
+
+export function AccessRequestCheckoutButton() {
+  const { toggleAccessRequestBar, getPendingResourceCount, isCollapsed } =
+    useAccessRequestsButton();
+  const count = getPendingResourceCount();
+
+  if (count > 0 && isCollapsed()) {
+    return (
+      <ButtonPrimary
+        onClick={toggleAccessRequestBar}
+        px={2}
+        size="small"
+        title="Toggle Access Request Checkout"
+      >
+        <EnvelopeOpen mr={2} size="small" color="buttons.primary.text" />
+        <Text fontSize="12px">{count}</Text>
+      </ButtonPrimary>
+    );
+  }
+  return null;
+}
