80f314a55011492ab0d4630e3755fd02c80b639a
Michael <michael.myers@goteleport.com>
2024-05-02 11:53:13 -0500

    Add DisableIncludeRequestableRequests to UIConfig (#41064)

--- api/constants/constants.go
+++ api/constants/constants.go
@@ -346,6 +346,19 @@ const (
 	SessionRecordingModeBestEffort = SessionRecordingMode("best_effort")
 )
 
+// ShowResources determines which resources are shown in the web UI. Default if unset is "requestable"
+// which means resources the user has access to and resources they can request will be shown in the
+// resources UI. If set to `accessible_only`, only resources the user already has access to will be shown.
+type ShowResources string
+
+const (
+	// ShowResourcesaccessibleOnly will only show resources the user currently has access to.
+	ShowResourcesaccessibleOnly = ShowResources("accessible_only")
+
+	// ShowResourcesRequestable will allow resources that the user can request into resources page.
+	ShowResourcesRequestable = ShowResources("requestable")
+)
+
 // Constants for Traits
 const (
 	// TraitLogins is the name of the role variable used to store
