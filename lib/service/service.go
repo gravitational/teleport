f57d4b60124d37f7c01908b9e723ed21e28425ba
Tim Ross <tim.ross@goteleport.com>
2024-05-02 15:18:42 -0400

    Convert App heartbeats to use the inventory control stream
    Updates app server heartbeats to use srv.Heartbeatv2 in order to
    leverage the inventory control stream instead of the legacy mechanism.
    The new srv.heartbeatV2Driver implementation for app servers detects
    the version of Auth and falls back appropriately if the Auth server
    is too old to know about the new field added to the InventoryHeartbeat
    message. The connected_resources metric should be accurate if the
    auth and app service are on the same version or if the auth service
    is on a more recent version. If the app service is newer than auth
    and the new app server driver kicks into fall back mode then the
    connected resource metric will not be updated.
    
    Partially addresses #40917

--- lib/service/service.go
+++ lib/service/service.go
@@ -5680,6 +5680,7 @@ func (process *TeleportProcess) initApps() {
 			OnHeartbeat:          process.OnHeartbeat(teleport.ComponentApp),
 			ConnectedProxyGetter: proxyGetter,
 			ConnectionsHandler:   connectionsHandler,
+			InventoryHandle:      process.inventoryHandle,
 		})
 		if err != nil {
 			return trace.Wrap(err)
@@ -5746,7 +5747,7 @@ func (process *TeleportProcess) initApps() {
 		})
 
 		// Block and wait while the server and agent pool are running.
-		if err := appServer.Wait(); err != nil {
+		if err := appServer.Wait(); err != nil && !errors.Is(err, context.Canceled) {
 			return trace.Wrap(err)
 		}
 		agentPool.Wait()
