eb499915ebc8c2c8e84f4997899fa8bd37f2ca4d
Trent Clarke <trent@goteleport.com>
2024-05-03 09:44:42 +0000

    Review feedback

--- tool/tctl/common/plugins_command.go
+++ tool/tctl/common/plugins_command.go
@@ -21,9 +21,8 @@ package common
 import (
 	"context"
 	"fmt"
-	"github.com/sirupsen/logrus"
-
 	"github.com/alecthomas/kingpin/v2"
+	"github.com/gravitational/teleport"
 	pluginsv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/plugins/v1"
 	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/lib/auth"
@@ -32,7 +31,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-type installArgs struct {
+type pluginInstallArgs struct {
 	cmd  *kingpin.CmdClause
 	name string
 	scim scimArgs
@@ -42,6 +41,13 @@ type scimArgs struct {
 	cmd           *kingpin.CmdClause
 	samlConnector string
 	token         string
+	role          string
+	force         bool
+}
+
+type pluginDeleteArgs struct {
+	cmd  *kingpin.CmdClause
+	name string
 }
 
 // PluginsCommand allows for management of plugins.
@@ -50,7 +56,8 @@ type PluginsCommand struct {
 	cleanupCmd *kingpin.CmdClause
 	pluginType string
 	dryRun     bool
-	install    installArgs
+	install    pluginInstallArgs
+	delete     pluginDeleteArgs
 }
 
 // Initialize creates the plugins command and subcommands
@@ -64,7 +71,12 @@ func (p *PluginsCommand) Initialize(app *kingpin.Application, config *servicecfg
 	p.cleanupCmd.Arg("type", "The type of plugin to cleanup. Only supports okta at present.").Required().EnumVar(&p.pluginType, string(types.PluginTypeOkta))
 	p.cleanupCmd.Flag("dry-run", "Dry run the cleanup command. Dry run defaults to on.").Default("true").BoolVar(&p.dryRun)
 
-	p.install.cmd = pluginsCommand.Command("install", "Install new plugin instance")
+	p.initInstall(pluginsCommand)
+	p.initDelete(pluginsCommand)
+}
+
+func (p *PluginsCommand) initInstall(parent *kingpin.CmdClause) {
+	p.install.cmd = parent.Command("install", "Install new plugin instance")
 
 	p.install.scim.cmd = p.install.cmd.Command("scim", "Install a new SCIM integration")
 	p.install.scim.cmd.
@@ -72,14 +84,49 @@ func (p *PluginsCommand) Initialize(app *kingpin.Application, config *servicecfg
 		Default("scim").
 		StringVar(&p.install.name)
 	p.install.scim.cmd.
-		Flag("saml-connector", "The name of the Teleport SAML connector users wil log in with.").
+		Flag("saml-connector", "The name of the Teleport SAML connector users will log in with.").
 		Required().
 		StringVar(&p.install.scim.samlConnector)
 	p.install.scim.cmd.
-		Flag("token", "The bearer token used by the SCIM client to authentcate").
+		Flag("role", "The Teleport role to assign users created by the plugin").
+		Short('r').
+		Default(teleport.PresetRequesterRoleName).
+		StringVar(&p.install.scim.role)
+	p.install.scim.cmd.
+		Flag("token", "The bearer token used by the SCIM client to authenticate").
+		Short('t').
 		Required().
 		Envar("SCIM_BEARER_TOKEN").
 		StringVar(&p.install.scim.token)
+	p.install.scim.cmd.
+		Flag("force", "Proceed with installation even if validation fails").
+		Short('f').
+		Default("false").
+		BoolVar(&p.install.scim.force)
+
+}
+
+func (p *PluginsCommand) initDelete(parent *kingpin.CmdClause) {
+	p.delete.cmd = parent.Command("delete", "Remove a plugin instance")
+	p.delete.cmd.
+		Arg("name", "The name of the SCIM plugin resource to delete").
+		StringVar(&p.delete.name)
+}
+
+func (p *PluginsCommand) Delete(ctx context.Context, client *auth.Client) error {
+	log := p.config.Logger
+	plugins := client.PluginsClient()
+
+	req := &pluginsv1.DeletePluginRequest{Name: p.delete.name}
+	if _, err := plugins.DeletePlugin(ctx, req); err != nil {
+		if trace.IsNotFound(err) {
+			log.Info(fmt.Sprintf("Plugin [%s] not found", p.delete.name))
+			return nil
+		}
+		log.Error(fmt.Sprintf("Failed deleting plugin [%s]: %v"), p.delete.name, err)
+		return trace.Wrap(err)
+	}
+	return nil
 }
 
 // Cleanup cleans up the given plugin.
@@ -128,10 +175,12 @@ func (p *PluginsCommand) Cleanup(ctx context.Context, clusterAPI *auth.Client) e
 }
 
 func (p *PluginsCommand) InstallSCIM(ctx context.Context, client *auth.Client) error {
-	logrus.Debug("Fetching cluster info...")
+	log := p.config.Logger
+
+	log.Debug("Fetching cluster info...")
 	info, err := client.Ping(ctx)
 	if err != nil {
-		logrus.Errorf("Failed fetching cluster info %v", err)
+		log.Error(fmt.Sprintf("Failed fetching cluster info %v", err))
 		return trace.Wrap(err)
 	}
 
@@ -140,10 +189,23 @@ func (p *PluginsCommand) InstallSCIM(ctx context.Context, client *auth.Client) e
 		return trace.Wrap(err)
 	}
 
-	logrus.Debug("Validating SAML Connector ID...")
+	connectorID := p.install.scim.samlConnector
+	log.Debug(fmt.Sprintf("Validating SAML Connector ID [%s]...", connectorID))
 	connector, err := client.GetSAMLConnector(ctx, p.install.scim.samlConnector, false)
 	if err != nil {
-		return trace.Wrap(err)
+		log.Error(fmt.Sprintf("Failed validating SAML connector [%s]: %v", connectorID, err))
+		if !p.install.scim.force {
+			return trace.Wrap(err)
+		}
+	}
+
+	role := p.install.scim.role
+	log.Debug(fmt.Sprintf("Validating Default Role [%s]...", role))
+	if _, err := client.GetRole(ctx, role); err != nil {
+		log.Error(fmt.Sprintf("Failed validating role[%s]: %v", role, err))
+		if !p.install.scim.force {
+			return trace.Wrap(err)
+		}
 	}
 
 	request := &pluginsv1.CreatePluginRequest{
@@ -158,7 +220,8 @@ func (p *PluginsCommand) InstallSCIM(ctx context.Context, client *auth.Client) e
 			Spec: types.PluginSpecV1{
 				Settings: &types.PluginSpecV1_Scim{
 					Scim: &types.PluginSCIMSettings{
-						SsoConnectorId: p.install.scim.samlConnector,
+						SamlConnectorId: p.install.scim.samlConnector,
+						DefaultRole:     p.install.scim.role,
 					},
 				},
 			},
@@ -177,9 +240,9 @@ func (p *PluginsCommand) InstallSCIM(ctx context.Context, client *auth.Client) e
 		},
 	}
 
-	logrus.Debugf("Creating SCIM Plugin %q...", p.install.name)
+	log.Debug(fmt.Sprintf("Creating SCIM Plugin %q...", p.install.name))
 	if _, err := client.PluginsClient().CreatePlugin(ctx, request); err != nil {
-		logrus.Errorf("Failed to create SCIM integration %q: %v", p.install.name, err)
+		log.Error(fmt.Sprintf("Failed to create SCIM integration %q: %v", p.install.name, err))
 		return trace.Wrap(err)
 	}
 
@@ -201,6 +264,8 @@ func (p *PluginsCommand) TryRun(ctx context.Context, cmd string, client *auth.Cl
 		err = p.Cleanup(ctx, client)
 	case p.install.scim.cmd.FullCommand():
 		err = p.InstallSCIM(ctx, client)
+	case p.delete.cmd.FullCommand():
+		err = p.Delete(ctx, client)
 	default:
 		return false, nil
 	}
