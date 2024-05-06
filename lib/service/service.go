310032fd969bea16397ca0ebcb6cbd014f41edbe
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2024-05-06 15:07:51 +0200

    Lift host signers callback into reversetunnel.Server

--- lib/service/service.go
+++ lib/service/service.go
@@ -58,7 +58,6 @@ import (
 	"go.opentelemetry.io/otel/attribute"
 	"golang.org/x/crypto/acme"
 	"golang.org/x/crypto/acme/autocert"
-	"golang.org/x/crypto/ssh"
 	"golang.org/x/sys/unix"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -4026,7 +4025,7 @@ func (process *TeleportProcess) initProxyEndpoint(conn *Connector) error {
 				ClusterName:                   clusterName,
 				ClientTLS:                     clientTLSConfig,
 				Listener:                      reverseTunnelLimiter.WrapListener(listeners.reverseTunnel),
-				HostSigners:                   []ssh.Signer{conn.ServerIdentity.KeySigner},
+				GetHostSigners:                sshutils.StaticHostSigners(conn.ServerIdentity.KeySigner),
 				LocalAuthClient:               conn.Client,
 				LocalAccessPoint:              accessPoint,
 				NewCachingAccessPoint:         process.newLocalCacheForRemoteProxy,
