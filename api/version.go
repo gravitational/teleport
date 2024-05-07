8c1f439343ac1b0fc7e8cb87f4a2f6b80e4b70ff
Fred Heinecke <fred.heinecke@goteleport.com>
2024-05-07 13:24:37 -0500

    14.0.0-dev.fred.updater-nonmajor-7

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "14.0.0-dev.fred.updater-nonmajor-6"
+const Version = "14.0.0-dev.fred.updater-nonmajor-7"
 
 var SemVersion = semver.New(Version)
