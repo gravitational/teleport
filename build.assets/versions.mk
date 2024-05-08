b61ac82077462dbb4269595726333b3aacd5562c
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2024-05-08 16:11:34 +0200

    node 20.13

--- build.assets/versions.mk
+++ build.assets/versions.mk
@@ -6,7 +6,7 @@
 GOLANG_VERSION ?= go1.21.10
 GOLANGCI_LINT_VERSION ?= v1.57.2
 
-NODE_VERSION ?= 20.11.1
+NODE_VERSION ?= 20.13.0
 
 # Run lint-rust check locally before merging code after you bump this.
 RUST_VERSION ?= 1.77.0
