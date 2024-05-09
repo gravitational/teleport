a09bc6550a01d01701e5e96a598146130cc84de7
Alan Parra <alan.parra@goteleport.com>
2024-05-09 18:26:07 -0300

    Emit login failure event in the /mfa/login/begin step

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -3192,6 +3192,15 @@ func (a *Server) CreateAuthenticateChallenge(ctx context.Context, req *proto.Cre
 		if err := a.WithUserLock(ctx, username, func() error {
 			return a.checkPasswordWOToken(ctx, username, req.GetUserCredentials().GetPassword())
 		}); err != nil {
+			// This is only ever used as a means to acquire a login challenge, so
+			// let's issue an authentication failure event.
+			if err := a.emitAuthAuditEvent(ctx, authAuditProps{
+				username: username,
+				authErr:  err,
+			}); err != nil {
+				log.WithError(err).Warn("Failed to emit login event")
+				// err swallowed on purpose.
+			}
 			return nil, trace.Wrap(err)
 		}
 
