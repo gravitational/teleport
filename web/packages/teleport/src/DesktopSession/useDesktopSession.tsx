f3ffbb74d6eb9c2524a3179280f152104b2b972a
Zac Bergquist <zac.bergquist@goteleport.com>
2024-05-12 10:47:58 -0600

    Desktop access: add option to send ctrl+alt+del
    Closes #41448

--- web/packages/teleport/src/DesktopSession/useDesktopSession.tsx
+++ web/packages/teleport/src/DesktopSession/useDesktopSession.tsx
@@ -21,6 +21,7 @@ import { useParams } from 'react-router';
 
 import useAttempt from 'shared/hooks/useAttemptNext';
 
+import { ButtonState } from 'teleport/lib/tdp';
 import useWebAuthn from 'teleport/lib/useWebAuthn';
 import desktopService from 'teleport/services/desktops';
 import userService from 'teleport/services/user';
@@ -185,6 +186,15 @@ export default function useDesktopSession() {
     }
   };
 
+  const onCtrlAltDel = () => {
+    if (!tdpClient) {
+      return;
+    }
+    tdpClient.sendKeyboardInput('ControlLeft', ButtonState.DOWN);
+    tdpClient.sendKeyboardInput('AltLeft', ButtonState.DOWN);
+    tdpClient.sendKeyboardInput('Delete', ButtonState.DOWN);
+  };
+
   return {
     hostname,
     username,
@@ -200,6 +210,7 @@ export default function useDesktopSession() {
     showAnotherSessionActiveDialog,
     setShowAnotherSessionActiveDialog,
     onShareDirectory,
+    onCtrlAltDel,
     warnings,
     onRemoveWarning,
     ...clientCanvasProps,
