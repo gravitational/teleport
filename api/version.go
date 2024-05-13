6fe65961f1325ef2a2fa9015ca8adb1ac028dc10
Fred Heinecke <fred.heinecke@goteleport.com>
2024-05-13 15:25:58 -0500

    16.0.0-dev.fred-helm-test.2

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "16.0.0-dev.fred-helm-test-1"
+const Version = "16.0.0-dev.fred-helm-test.2"
 
 var SemVersion = semver.New(Version)
