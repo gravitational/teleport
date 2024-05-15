343a6a9b5a73a6b5b51940727aff4c14ebaa9d76
Fred Heinecke <fred.heinecke@goteleport.com>
2024-05-15 01:59:03 -0500

    13.4.24-dev.fred-stage.2

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "13.4.24-dev.fred-stage.1"
+const Version = "13.4.24-dev.fred-stage.2"
 
 var SemVersion = semver.New(Version)
