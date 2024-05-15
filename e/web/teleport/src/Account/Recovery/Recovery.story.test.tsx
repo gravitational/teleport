5183e26a196fbb0f254f33f2f05251a2a1a468b5
Bartosz Leper <bartosz.leper@goteleport.com>
2024-05-15 13:23:49 +0200

    Temporarily disable the recovery story tests (#4128)
    Source-commit: gravitational/teleport.e@a48e75225237a75a236bcef0d3cef817e47cdd61
    Source-pull-requests: gravitational/teleport.e#4128

--- e/web/teleport/src/Account/Recovery/Recovery.story.test.tsx
+++ e/web/teleport/src/Account/Recovery/Recovery.story.test.tsx
@@ -7,19 +7,22 @@ import {
   LoadedNoEmailAddress,
 } from './Recovery.story';
 
-test('render screen for recovery tab in account settings', () => {
+// TODO(bl-nero): Reactivate these tests after snapshots are updated.
+test.skip('render screen for recovery tab in account settings', () => {
   const { container } = render(<Loaded />);
 
   expect(container.firstChild).toMatchSnapshot();
 });
 
-test('render screen for first timer recovery tab in account settings', () => {
+// TODO(bl-nero): Reactivate these tests after snapshots are updated.
+test.skip('render screen for first timer recovery tab in account settings', () => {
   const { container } = render(<LoadedFirstTime />);
 
   expect(container.firstChild).toMatchSnapshot();
 });
 
-test('render screen for recovery tab for users without a valid email as username', () => {
+// TODO(bl-nero): Reactivate these tests after snapshots are updated.
+test.skip('render screen for recovery tab for users without a valid email as username', () => {
   const { container } = render(<LoadedNoEmailAddress />);
 
   expect(container.firstChild).toMatchSnapshot();
