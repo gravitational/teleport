3bc2861e66706482498f75f0a3f38c670ed64920
Grzegorz Zdunek <gzdunek@users.noreply.github.com>
2024-05-15 10:18:43 +0200

    Update Electron to 30.0.3 and Node.js to 20.13.0 (#41404) (#41512)
    * Update `electron` to 30.0.3
    
    * Update Node.js to 20.13.0
    
    (cherry picked from commit 238b6e4954bb16543343783de1943f54203ddd6f)

--- build.assets/versions.mk
+++ build.assets/versions.mk
@@ -4,7 +4,7 @@
 GOLANG_VERSION ?= go1.21.9
 GOLANGCI_LINT_VERSION ?= v1.58.1
 
-NODE_VERSION ?= 20.11.1
+NODE_VERSION ?= 20.13.0
 
 # Run lint-rust check locally before merging code after you bump this.
 RUST_VERSION ?= 1.77.0
