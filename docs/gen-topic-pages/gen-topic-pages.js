129f51787db23183338f74fc742ab0218e03313f
Paul Gottschling <paul.gottschling@goteleport.com>
2024-05-16 15:19:12 -0400

    Rework menu pages
    Make menu pages less overwhelming for users by showing a single level of
    the directory tree per menu page, including a list of both
    subdirectories and docs pages in each directory. To make it easier to
    produce menu pages while reorganizing the docs, use a script to generate
    menu pages from the directory structure, using a YAML configuration file
    to provide the title and description of a given menu page.
    
    This change also reorganizes the "Admin Guides" section of the
    documentation to provide fewer first-level links, making the initial
    menu page shorter. It also uses bulleted lists for menu page links,
    since these appear lighter than our table component and thus less
    overwhelming.
    
    Since the directory structure is now quite deep in places, we'll want to
    add breadcrumbs to the docs navigation to help users see where a page
    sits in the structure of the docs and visit other pages in a section.

--- docs/gen-topic-pages/gen-topic-pages.js
+++ docs/gen-topic-pages/gen-topic-pages.js
@@ -1,46 +1,54 @@
 const yaml = require('yaml');
 const path = require('path');
 
+const generationLine = `{/*GENERATED MENU PAGE. DO NOT EDIT. RECREATE WITH THIS COMMAND:`;
+
 // TopicContentsFragment contains data necessary to generate a table of contents
 // page for the subdirectories of a user-specified root directory.
 // @param fs - The filesystem to use. Either memfs or the NodeJS fs package.
-// @param {string} prefix - prefix to append before any links in the topic tree.
-// This is a sequence of relative path characters, e.g., "../../", and enables
-// users to place topic trees within directories for partials.
 // @param {string} root - file path in fs in which to place the table of
 // contents partial. While TopicContentsFragment is not responsible for writing
 // the output file, it builds the table of contents based on the assumption that
 // it will be a partial in the specified directory.
 class TopicContentsFragment {
-  constructor(command, fs, root, prefix) {
+  constructor(command, fs, root) {
     this.command = command;
     this.fs = fs;
-    this.prefix = prefix;
     this.root = root;
   }
-  // makeTopicTree constructs an index of the files in the directory, returning
+  // makeTopicPage constructs an index of the files in the directory, returning
   // it as a string.
   //
-  // makeTopicTree expects files in the directory to be MDX files with two
+  // makeTopicPage expects files in the directory to be MDX files with two
   // frontmatter keys, title and description. For each subdirectory, there must
   // be a menu page in the same directory as the subdirectory with the same name
   // as the subdirectory. For example, if there is a subdirectory called
   // "guides", we expect a menu page called "guides.mdx". We use this to
   // provide context for the subdirectory in the table of contents.
-  makeTopicTree() {
-    const initial = `{/*GENERATED PARTIAL. DO NOT EDIT. RECREATE WITH THIS COMMAND:
+  makeTopicPage() {
+    const parts = path.parse(this.root);
+    const rootConfig = path.join(parts.dir, parts.name + '.yaml');
+    const fm = this.getFrontmatter(this.fs.readFileSync(rootConfig, 'utf-8'));
+    const initial = `---
+title: ${fm.title}
+description: ${fm.description}
+---
+
+${generationLine}
 ${this.command}*/}
 
+${fm.description}
+
 `;
-    return this.addTopicsFromDir(this.root, initial, 2);
+    return this.addTopicsFromDir(this.root, initial, 0);
   }
 
   // getFrontmatter returns the frontmatter YAML of an MDX page as an object
   // containing keys "title" and "description".
-  getFrontmatter(fullPath) {
-    let text = this.fs.readFileSync(fullPath, 'utf8');
+  getFrontmatter(pageContent) {
+    let text = pageContent;
     // Remove any initial YAML document separator
-    if (text.startsWith('---\n')) {
+    if (pageContent.startsWith('---\n')) {
       text = text.slice('---\n'.length);
     }
     const docs = text.split('---');
@@ -61,11 +69,8 @@ ${this.command}*/}
   // to the directory where we are placing the table of contents page.
   // @param filepath {string} - the path from which to generate a link path.
   relativePathToFile(filepath) {
-    // Remove the root and replace it with the prefix. Add any
-    // path segments to get us from the prefix to any inner
-    // child directories.
-    const additionalpathsegments = filepath.slice(this.root.length);
-    return path.join(this.prefix, additionalpathsegments);
+    const rootName = path.parse(this.root).name;
+    return path.join(rootName, filepath.slice(this.root.length));
   }
 
   // addTopicsFromDir takes the path at dirPath and recursively adds any topic
@@ -80,87 +85,68 @@ ${this.command}*/}
     let newText = sofar;
     const files = this.fs.readdirSync(dirPath, 'utf8');
 
-    // List all MDX files in the current directory
-    const mdxFiles = files.reduce((accum, current) => {
-      const filename = path.parse(current).base;
-      if (
-        !current.endsWith('.mdx') ||
-        // We assume that each All Topics partial is included in a page called
-        // "all-topics.mdx".
-        filename == 'all-topics.mdx'
-      ) {
+    const dirs = files.reduce((accum, current) => {
+      const stats = this.fs.statSync(path.join(dirPath, current));
+      if (!stats.isDirectory()) {
         return accum;
       }
       accum[path.join(dirPath, current)] = true;
       return accum;
     }, {});
 
-    // List all subdirectories of the current directory
-    const dirs = files.reduce((accum, current) => {
-      const stats = this.fs.statSync(path.join(dirPath, current));
-      if (stats.isDirectory()) {
-        accum[path.join(dirPath, current)] = true;
-      }
-
-      return accum;
-    }, {});
-
-    // List all MDX files in the subdirectory with the same name as a
-    // subdirectory. By convention, these are menu pages for the subdirectory.
-    const menuPages = Object.keys(mdxFiles).reduce((accum, current) => {
-      const parts = path.parse(current);
-      const asDir = path.join(parts.dir, parts.name);
-      if (dirs[asDir]) {
-        accum[current] = true;
-        // Exclude the menu page from the map of regular MDX pages. We
-        // treat these separately.
-        delete mdxFiles[current];
-      }
-      return accum;
-    }, {});
-
-    // Add a table of MDX pages within the directory.
-    if (Object.keys(mdxFiles).length > 0) {
-      newText =
-        newText +
-        `|Topic|Description|
-|---|---|
-`;
-    }
-
-    // Add rows to the table.
-    Object.keys(mdxFiles).forEach(f => {
-      const relPath = this.relativePathToFile(f);
-      const fm = this.getFrontmatter(f);
-      newText = newText + `|[${fm.title}](${relPath})|${fm.description}|\n`;
-    });
-
-    // Add another section of the topic for each subdirectory.
-    Object.keys(dirs).forEach(p => {
-      if (!menuPages[p + '.mdx']) {
+    // List all MDX files in the current directory and add them to a map of
+    // files we'll use to build the menu page.
+    const frontmatterFiles = files.reduce((accum, current) => {
+      const pathParts = path.parse(path.join(dirPath, current));
+      const asDir = path.join(pathParts.dir, pathParts.name);
+      if (!dirs[asDir] && current.endsWith('.yaml')) {
         throw new Error(
-          `expecting a menu page for ${p} called ${p + '.mdx'}, but there is none`
+          'Found a menu page config that does not correspond to a subdirectory: ' +
+            path.join(dirPath, current)
         );
       }
-      const fm = this.getFrontmatter(p + '.mdx');
-      let heading = '';
-      for (let i = 0; i < level; i++) {
-        heading += '#';
+
+      if (!current.endsWith('.mdx') && !current.endsWith('yaml')) {
+        return accum;
+      }
+      accum[path.join(dirPath, current)] = true;
+      return accum;
+    }, {});
+
+    // Add rows to the menu page.
+    Object.keys(frontmatterFiles).forEach(f => {
+      const text = this.fs.readFileSync(f, 'utf8');
+      const lines = text.split('\n');
+      const hasCorrespondingDir = !!dirs[f.replace(/\.[a-z]+$/, '')];
+
+      if (!hasCorrespondingDir && lines.includes(generationLine)) {
+        throw new Error(
+          'Found a menu page that no longer corresponds to a subdirectory: ' + f
+        );
       }
 
-      // Make sure there is a blank line before the heading
-      if (!newText.endsWith('\n\n')) {
-        newText = newText + '\n';
+      if (hasCorrespondingDir && f.endsWith('.mdx')) {
+        if (!lines.includes(generationLine)) {
+          throw new Error(
+            `Found a menu page that was not automatically generated (${f}). Move any content you want to preserve and delete this page.`
+          );
+        }
+        // Don't create a row for this pre-existing menu page. We'll create
+        // one from the YAML config instead.
+        return;
       }
-      newText =
-        newText +
-        `${heading} ${fm.title}
 
-${fm.description} ([more info](${this.relativePathToFile(p) + '.mdx'}))
+      let relPath = this.relativePathToFile(f);
+      const fm = this.getFrontmatter(text);
 
-`;
+      // We're using a YAML file for directory information, so change the link
+      // to an MDX file. We expect there to be a menu page corresponding to the
+      // YAML config anyway.
+      if (relPath.endsWith('.yaml')) {
+        relPath = relPath.replace(/\.yaml$/, '.mdx');
+      }
 
-      newText = this.addTopicsFromDir(p, newText, level + 1);
+      newText = newText + `- [${fm.title}](${relPath}): ${fm.description}\n`;
     });
 
     return newText;
--- docs/gen-topic-pages/gen-topic-pages.test.js
+++ docs/gen-topic-pages/gen-topic-pages.test.js
@@ -1,36 +1,51 @@
 import { Volume, createFsFromVolume } from 'memfs';
 import { TopicContentsFragment } from './gen-topic-pages.js';
 
-describe('generate All Topics page', () => {
+describe('generate a menu page', () => {
   const testFilesTwoSections = {
-    '/docs/database-access.mdx': `title: "Database Access"
-description: "Guides related to Database Access"
+    '/docs.yaml': `---
+title: "Documentation Home"
+description: "Guides to setting up the product."
+`,
+    '/docs/database-access.yaml': `---
+title: "Database Access"
+description: "Guides related to Database Access."
 ---`,
-    '/docs/database-access/page1.mdx': `title: "Database Access Page 1"
+    '/docs/database-access/page1.mdx': `---
+title: "Database Access Page 1"
 description: "Protecting DB 1 with Teleport"
 ---`,
-    '/docs/database-access/page2.mdx': `title: "Database Access Page 2"
+    '/docs/database-access/page2.mdx': `---
+title: "Database Access Page 2"
 description: "Protecting DB 2 with Teleport"
 ---`,
-    '/docs/application-access.mdx': `title: "Application Access"
+    '/docs/application-access.yaml': `---
+title: "Application Access"
 description: "Guides related to Application Access"
 ---`,
-    '/docs/application-access/page1.mdx': `title: "Application Access Page 1"
+    '/docs/application-access/page1.mdx': `---
+title: "Application Access Page 1"
 description: "Protecting App 1 with Teleport"
 ---`,
-    '/docs/application-access/page2.mdx': `title: "Application Access Page 2"
+    '/docs/application-access/page2.mdx': `---
+title: "Application Access Page 2"
 description: "Protecting App 2 with Teleport"
 ---`,
   };
 
   test('lists the contents of a directory', () => {
-    const expected = `{/*GENERATED PARTIAL. DO NOT EDIT. RECREATE WITH THIS COMMAND:
+    const expected = `---
+title: Database Access
+description: Guides related to Database Access.
+---
+
+{/*GENERATED MENU PAGE. DO NOT EDIT. RECREATE WITH THIS COMMAND:
 sample-command*/}
 
-|Topic|Description|
-|---|---|
-|[Database Access Page 1](../page1.mdx)|Protecting DB 1 with Teleport|
-|[Database Access Page 2](../page2.mdx)|Protecting DB 2 with Teleport|
+Guides related to Database Access.
+
+- [Database Access Page 1](database-access/page1.mdx): Protecting DB 1 with Teleport
+- [Database Access Page 2](database-access/page2.mdx): Protecting DB 2 with Teleport
 `;
 
     const vol = Volume.fromJSON(testFilesTwoSections);
@@ -38,32 +53,38 @@ sample-command*/}
     const frag = new TopicContentsFragment(
       'sample-command',
       fs,
-      '/docs/database-access',
-      '..'
+      '/docs/database-access'
     );
-    const actual = frag.makeTopicTree();
+    const actual = frag.makeTopicPage();
     expect(actual).toBe(expected);
   });
 
   test('handles frontmatter document separators', () => {
-    const expected = `{/*GENERATED PARTIAL. DO NOT EDIT. RECREATE WITH THIS COMMAND:
+    const expected = `---
+title: Database Access
+description: Guides related to Database Access.
+---
+
+{/*GENERATED MENU PAGE. DO NOT EDIT. RECREATE WITH THIS COMMAND:
 sample-command*/}
 
-|Topic|Description|
-|---|---|
-|[Database Access Page 1](../page1.mdx)|Protecting DB 1 with Teleport|
-|[Database Access Page 2](../page2.mdx)|Protecting DB 2 with Teleport|
+Guides related to Database Access.
+
+- [Database Access Page 1](database-access/page1.mdx): Protecting DB 1 with Teleport
+- [Database Access Page 2](database-access/page2.mdx): Protecting DB 2 with Teleport
 `;
 
     const vol = Volume.fromJSON({
-      '/docs/database-access.mdx': `title: "Database Access"
-description: "Guides related to Database Access"
+      '/docs/database-access.yaml': `---
+title: Database Access
+description: Guides related to Database Access.
 ---`,
       '/docs/database-access/page1.mdx': `---
 title: "Database Access Page 1"
 description: "Protecting DB 1 with Teleport"
 ---`,
-      '/docs/database-access/page2.mdx': `title: "Database Access Page 2"
+      '/docs/database-access/page2.mdx': `---
+title: "Database Access Page 2"
 description: "Protecting DB 2 with Teleport"
 ---`,
     });
@@ -71,159 +92,205 @@ description: "Protecting DB 2 with Teleport"
     const frag = new TopicContentsFragment(
       'sample-command',
       fs,
-      '/docs/database-access',
-      '..'
+      '/docs/database-access'
     );
-    const actual = frag.makeTopicTree();
+    const actual = frag.makeTopicPage();
     expect(actual).toBe(expected);
   });
 
-  test('ignores files called "all-topics.mdx"', () => {
-    const expected = `{/*GENERATED PARTIAL. DO NOT EDIT. RECREATE WITH THIS COMMAND:
+  test('treats links to directories as regular links (single)', () => {
+    const expected = `---
+title: Documentation Home
+description: Guides for setting up the product.
+---
+
+{/*GENERATED MENU PAGE. DO NOT EDIT. RECREATE WITH THIS COMMAND:
 sample-command*/}
 
-|Topic|Description|
-|---|---|
-|[Introduction](../introduction.mdx)|Protecting databases with Teleport|
-|[Database Access Page 1](../page1.mdx)|Protecting DB 1 with Teleport|
-|[Database Access Page 2](../page2.mdx)|Protecting DB 2 with Teleport|
+Guides for setting up the product.
+
+- [Application Access](docs/application-access.mdx): Guides related to Application Access
 `;
 
     const vol = Volume.fromJSON({
-      '/docs/database-access.mdx': `title: "Database Access"
-description: "Guides related to Database Access"
+      '/docs.yaml': `---
+title: Documentation Home
+description: Guides for setting up the product.
 ---`,
-      '/docs/database-access/introduction.mdx': `title: "Introduction"
-description: Protecting databases with Teleport
----`,
-      '/docs/database-access/all-topics.mdx': `title: All Topics
-description: "The All Topics page"
----`,
-      '/docs/database-access/page1.mdx': `title: "Database Access Page 1"
-description: "Protecting DB 1 with Teleport"
----`,
-      '/docs/database-access/page2.mdx': `title: "Database Access Page 2"
-description: "Protecting DB 2 with Teleport"
----`,
-    });
-    const fs = createFsFromVolume(vol);
-    const frag = new TopicContentsFragment(
-      'sample-command',
-      fs,
-      '/docs/database-access',
-      '..'
-    );
-    const actual = frag.makeTopicTree();
-    expect(actual).toBe(expected);
-  });
-
-  test('adds section headings if the root includes directories (single)', () => {
-    const expected = `{/*GENERATED PARTIAL. DO NOT EDIT. RECREATE WITH THIS COMMAND:
-sample-command*/}
-
-## Application Access
-
-Guides related to Application Access ([more info](../application-access.mdx))
-
-|Topic|Description|
-|---|---|
-|[Application Access Page 1](../application-access/page1.mdx)|Protecting App 1 with Teleport|
-|[Application Access Page 2](../application-access/page2.mdx)|Protecting App 2 with Teleport|
-`;
-
-    const vol = Volume.fromJSON({
-      '/docs/application-access.mdx': `title: "Application Access"
+      '/docs/application-access.yaml': `---
+title: "Application Access"
 description: "Guides related to Application Access"
 ---`,
-      '/docs/application-access/page1.mdx': `title: "Application Access Page 1"
+      '/docs/application-access/page1.mdx': `---
+title: "Application Access Page 1"
 description: "Protecting App 1 with Teleport"
 ---`,
-      '/docs/application-access/page2.mdx': `title: "Application Access Page 2"
+      '/docs/application-access/page2.mdx': `---
+title: "Application Access Page 2"
 description: "Protecting App 2 with Teleport"
 ---`,
     });
     const fs = createFsFromVolume(vol);
-    const frag = new TopicContentsFragment('sample-command', fs, '/docs', '..');
-    const actual = frag.makeTopicTree();
+    const frag = new TopicContentsFragment('sample-command', fs, '/docs');
+    const actual = frag.makeTopicPage();
     expect(actual).toBe(expected);
   });
 
-  test('adds section headings if the root includes directories (multiple)', () => {
-    const expected = `{/*GENERATED PARTIAL. DO NOT EDIT. RECREATE WITH THIS COMMAND:
+  test('treats links to directories as regular links (multiple)', () => {
+    const expected = `---
+title: Documentation Home
+description: Guides to setting up the product.
+---
+
+{/*GENERATED MENU PAGE. DO NOT EDIT. RECREATE WITH THIS COMMAND:
 sample-command*/}
 
-## Application Access
+Guides to setting up the product.
 
-Guides related to Application Access ([more info](../application-access.mdx))
-
-|Topic|Description|
-|---|---|
-|[Application Access Page 1](../application-access/page1.mdx)|Protecting App 1 with Teleport|
-|[Application Access Page 2](../application-access/page2.mdx)|Protecting App 2 with Teleport|
-
-## Database Access
-
-Guides related to Database Access ([more info](../database-access.mdx))
-
-|Topic|Description|
-|---|---|
-|[Database Access Page 1](../database-access/page1.mdx)|Protecting DB 1 with Teleport|
-|[Database Access Page 2](../database-access/page2.mdx)|Protecting DB 2 with Teleport|
+- [Application Access](docs/application-access.mdx): Guides related to Application Access
+- [Database Access](docs/database-access.mdx): Guides related to Database Access.
 `;
 
     const vol = Volume.fromJSON(testFilesTwoSections);
     const fs = createFsFromVolume(vol);
-    const frag = new TopicContentsFragment('sample-command', fs, '/docs', '..');
-    const actual = frag.makeTopicTree();
+    const frag = new TopicContentsFragment('sample-command', fs, '/docs');
+    const actual = frag.makeTopicPage();
     expect(actual).toBe(expected);
   });
 
-  test('nested directories', () => {
-    const expected = `{/*GENERATED PARTIAL. DO NOT EDIT. RECREATE WITH THIS COMMAND:
+  test('limits menus to one directory level', () => {
+    const expected = `---
+title: Documentation Home
+description: Guides to setting up the product.
+---
+
+{/*GENERATED MENU PAGE. DO NOT EDIT. RECREATE WITH THIS COMMAND:
 sample-command*/}
 
-## Application Access
+Guides to setting up the product.
 
-Guides related to Application Access ([more info](../application-access.mdx))
-
-|Topic|Description|
-|---|---|
-|[Application Access Page 1](../application-access/page1.mdx)|Protecting App 1 with Teleport|
-|[Application Access Page 2](../application-access/page2.mdx)|Protecting App 2 with Teleport|
-
-### JWT guides
-
-Guides related to JWTs ([more info](../application-access/jwt.mdx))
-
-|Topic|Description|
-|---|---|
-|[JWT Page 1](../application-access/jwt/page1.mdx)|Protecting JWT App 1 with Teleport|
-|[JWT Page 2](../application-access/jwt/page2.mdx)|Protecting JWT App 2 with Teleport|
+- [Application Access](docs/application-access.mdx): Guides related to Application Access
 `;
 
     const vol = Volume.fromJSON({
-      '/docs/application-access.mdx': `title: "Application Access"
+      '/docs.yaml': `---
+title: "Documentation Home"
+description: "Guides to setting up the product."
+`,
+      '/docs/application-access.yaml': `---
+title: "Application Access"
 description: "Guides related to Application Access"
 ---`,
-      '/docs/application-access/page1.mdx': `title: "Application Access Page 1"
+      '/docs/application-access/page1.mdx': `---
+title: "Application Access Page 1"
 description: "Protecting App 1 with Teleport"
 ---`,
-      '/docs/application-access/page2.mdx': `title: "Application Access Page 2"
+      '/docs/application-access/page2.mdx': `---
+title: "Application Access Page 2"
 description: "Protecting App 2 with Teleport"
 ---`,
-      '/docs/application-access/jwt.mdx': `title: "JWT guides"
+      '/docs/application-access/jwt.yaml': `---
+title: "JWT guides"
 description: "Guides related to JWTs"
 ---`,
-      '/docs/application-access/jwt/page1.mdx': `title: "JWT Page 1"
+      '/docs/application-access/jwt/page1.mdx': `---
+title: "JWT Page 1"
 description: "Protecting JWT App 1 with Teleport"
 ---`,
-      '/docs/application-access/jwt/page2.mdx': `title: "JWT Page 2"
+      '/docs/application-access/jwt/page2.mdx': `---
+title: "JWT Page 2"
 description: "Protecting JWT App 2 with Teleport"
 ---`,
     });
     const fs = createFsFromVolume(vol);
-    const frag = new TopicContentsFragment('sample-command', fs, '/docs', '..');
-    const actual = frag.makeTopicTree();
+    const frag = new TopicContentsFragment('sample-command', fs, '/docs');
+    const actual = frag.makeTopicPage();
     expect(actual).toBe(expected);
+  });
+
+  test(`throws an error for YAML configs that don't correspond to subdirectory names`, () => {
+    const vol = Volume.fromJSON({
+      '/docs.yaml': `---
+title: "Documentation Home"
+description: "Guides to setting up the product."
+`,
+      '/docs/application-access.yaml': `---
+title: "Application Access"
+description: "Guides related to Application Access"
+---`,
+      '/docs/application-access/page1.mdx': `---
+title: "Application Access Page 1"
+description: "Protecting App 1 with Teleport"
+---`,
+      '/docs/jwt.yaml': `---
+title: "JWT guides"
+description: "Guides related to JWTs"
+---`,
+    });
+
+    const fs = createFsFromVolume(vol);
+    expect(() => {
+      const frag = new TopicContentsFragment('sample-command', fs, '/docs');
+      frag.makeTopicPage();
+    }).toThrow('jwt.yaml');
+  });
+
+  test(`throws an error on a generated menu page that does not correspond to a subdirectory`, () => {
+    const vol = Volume.fromJSON({
+      '/docs.yaml': `---
+title: "Documentation Home"
+description: "Guides to setting up the product."
+`,
+      '/docs/application-access.yaml': `---
+title: "Application Access"
+description: "Guides related to Application Access"
+---`,
+      '/docs/application-access/page1.mdx': `---
+title: "Application Access Page 1"
+description: "Protecting App 1 with Teleport"
+---`,
+      '/docs/jwt.mdx': `---
+title: "JWT guides"
+description: "Guides related to JWTs"
+---
+{/*GENERATED MENU PAGE. DO NOT EDIT. RECREATE WITH THIS COMMAND:
+sample-command*/}
+`,
+    });
+
+    const fs = createFsFromVolume(vol);
+    expect(() => {
+      const frag = new TopicContentsFragment('sample-command', fs, '/docs');
+      frag.makeTopicPage();
+    }).toThrow('jwt.mdx');
+  });
+
+  test(`throws an error on a menu page that was not auto-generated`, () => {
+    const vol = Volume.fromJSON({
+      '/docs.yaml': `---
+title: "Documentation Home"
+description: "Guides to setting up the product."
+`,
+      '/docs/application-access.yaml': `---
+title: "Application Access"
+description: "Guides related to Application Access"
+---`,
+      '/docs/application-access/page1.mdx': `---
+title: "Application Access Page 1"
+description: "Protecting App 1 with Teleport"
+---`,
+      '/docs/application-access.mdx': `---
+title: "JWT guides"
+description: "Guides related to JWTs"
+---
+This menu page was written manually.
+`,
+    });
+
+    const fs = createFsFromVolume(vol);
+    expect(() => {
+      const frag = new TopicContentsFragment('sample-command', fs, '/docs');
+      frag.makeTopicPage();
+    }).toThrow('application-access.mdx');
   });
 });
