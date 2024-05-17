578920359d0e3a077fb6ae676c3c2192acc2070e
Noah Stride <noah.stride@goteleport.com>
2024-05-17 10:45:39 +0100

    Add remote address to failed/succeeded Bot/Instance joins (#41452)
    * Add connection metadata to instance join/bot join events
    
    * Add RemoteAddr to join failure/succsss logging

--- lib/auth/join.go
+++ lib/auth/join.go
@@ -125,6 +125,7 @@ func (a *Server) handleJoinFailure(
 		fields["role"] = req.Role
 		fields["host_id"] = req.HostID
 		fields["node_name"] = req.NodeName
+		fields["remote_addr"] = req.RemoteAddr
 	}
 
 	// Fetch and encode attributes if they are available.
@@ -162,10 +163,14 @@ func (a *Server) handleJoinFailure(
 			},
 			Status:     status,
 			Attributes: attributesProto,
+			ConnectionMetadata: apievents.ConnectionMetadata{
+				RemoteAddr: req.RemoteAddr,
+			},
 		}
 		if pt != nil {
 			botJoinEvent.Method = string(pt.GetJoinMethod())
 			botJoinEvent.TokenName = pt.GetSafeName()
+			botJoinEvent.BotName = pt.GetBotName()
 		}
 		evt = botJoinEvent
 	} else {
@@ -185,6 +190,7 @@ func (a *Server) handleJoinFailure(
 			instanceJoinEvent.Role = string(req.Role)
 			instanceJoinEvent.NodeName = req.NodeName
 			instanceJoinEvent.HostID = req.HostID
+			instanceJoinEvent.RemoteAddr = req.RemoteAddr
 		}
 		evt = instanceJoinEvent
 	}
@@ -366,6 +372,9 @@ func (a *Server) generateCertsBot(
 		BotName:   botName,
 		Method:    string(joinMethod),
 		TokenName: provisionToken.GetSafeName(),
+		ConnectionMetadata: apievents.ConnectionMetadata{
+			RemoteAddr: req.RemoteAddr,
+		},
 	}
 	if joinAttributeSrc != nil {
 		attributes, err := joinAttributeSrc.JoinAuditAttributes()
@@ -443,6 +452,9 @@ func (a *Server) generateCerts(
 		TokenName:    provisionToken.GetSafeName(),
 		TokenExpires: provisionToken.Expiry(),
 		HostID:       req.HostID,
+		ConnectionMetadata: apievents.ConnectionMetadata{
+			RemoteAddr: req.RemoteAddr,
+		},
 	}
 	if joinAttributeSrc != nil {
 		attributes, err := joinAttributeSrc.JoinAuditAttributes()
