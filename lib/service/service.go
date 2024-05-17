d9cb9f9c42cf37706a67ae6e1c046c07d3b86117
Tim Ross <tim.ross@goteleport.com>
2024-05-16 20:22:12 -0400

    Decouple servicecfg and lib/auth/keystore
    This was the last remaining way for lib/cloud/gcp to slip
    into client tools that aren't importing lib/auth directly.
    
    ```
    goda tree "reach(github.com/gravitational/teleport/tool/tctl...:all, cloud.google.com/go/compute/apiv1)"
    ├ github.com/gravitational/teleport/tool/tctl
        └ github.com/gravitational/teleport/tool/tctl/common
          ├ github.com/gravitational/teleport/lib/config
            ├ github.com/gravitational/teleport/lib/pam
              └ github.com/gravitational/teleport/lib/service/servicecfg
                └ github.com/gravitational/teleport/lib/auth/keystore
                  └ github.com/gravitational/teleport/lib/cloud
                    └ github.com/gravitational/teleport/lib/cloud/gcp
                      └ cloud.google.com/go/compute/apiv1
            └ github.com/gravitational/teleport/lib/service/servicecfg ~
          ├ github.com/gravitational/teleport/lib/service/servicecfg ~
          ├ github.com/gravitational/teleport/tool/tctl/common/accessmonitoring
            └ github.com/gravitational/teleport/lib/service/servicecfg ~
          ├ github.com/gravitational/teleport/tool/tctl/common/loginrule
            └ github.com/gravitational/teleport/lib/service/servicecfg ~
          ├ github.com/gravitational/teleport/tool/tctl/sso/configure
            ├ github.com/gravitational/teleport/lib/service/servicecfg ~
            └ github.com/gravitational/teleport/tool/tctl/sso/tester
              └ github.com/gravitational/teleport/lib/service/servicecfg ~
          └ github.com/gravitational/teleport/tool/tctl/sso/tester ~
    ```

--- lib/service/service.go
+++ lib/service/service.go
@@ -89,6 +89,7 @@ import (
 	"github.com/gravitational/teleport/lib/auth/accesspoint"
 	"github.com/gravitational/teleport/lib/auth/authclient"
 	"github.com/gravitational/teleport/lib/auth/keygen"
+	"github.com/gravitational/teleport/lib/auth/keystore"
 	"github.com/gravitational/teleport/lib/auth/native"
 	"github.com/gravitational/teleport/lib/auth/state"
 	"github.com/gravitational/teleport/lib/authz"
@@ -1890,7 +1891,27 @@ func (process *TeleportProcess) initAuthService() error {
 			OIDCConnectors:          cfg.OIDCConnectors,
 			AuditLog:                process.auditLog,
 			CipherSuites:            cfg.CipherSuites,
-			KeyStoreConfig:          cfg.Auth.KeyStore,
+			KeyStoreConfig: keystore.Config{
+				PKCS11: keystore.PKCS11Config{
+					Path:       cfg.Auth.KeyStore.PKCS11.Path,
+					SlotNumber: cfg.Auth.KeyStore.PKCS11.SlotNumber,
+					TokenLabel: cfg.Auth.KeyStore.PKCS11.TokenLabel,
+					Pin:        cfg.Auth.KeyStore.PKCS11.Pin,
+					HostUUID:   cfg.Auth.KeyStore.PKCS11.HostUUID,
+				},
+				GCPKMS: keystore.GCPKMSConfig{
+					KeyRing:         cfg.Auth.KeyStore.GCPKMS.KeyRing,
+					ProtectionLevel: cfg.Auth.KeyStore.GCPKMS.ProtectionLevel,
+					HostUUID:        cfg.Auth.KeyStore.GCPKMS.HostUUID,
+				},
+				AWSKMS: keystore.AWSKMSConfig{
+					Cluster:      cfg.Auth.KeyStore.AWSKMS.Cluster,
+					AWSAccount:   cfg.Auth.KeyStore.AWSKMS.AWSAccount,
+					AWSRegion:    cfg.Auth.KeyStore.AWSKMS.AWSRegion,
+					CloudClients: cloudClients,
+				},
+				Logger: process.log,
+			},
 			Emitter:                 checkingEmitter,
 			Streamer:                events.NewReportingStreamer(streamer, process.Config.Testing.UploadEventsC),
 			TraceClient:             traceClt,
