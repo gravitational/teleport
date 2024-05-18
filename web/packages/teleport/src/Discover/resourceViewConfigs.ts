d1b493134b7f190cee266083527fc6d11990ab2b
Isaiah Becker-Mayer <isaiah@goteleport.com>
2024-05-17 18:01:45 -0700

    Removes Desktop discovery flow from the frontend, marks the relevant backend endpoints deprecated

--- web/packages/teleport/src/Discover/resourceViewConfigs.ts
+++ web/packages/teleport/src/Discover/resourceViewConfigs.ts
@@ -19,7 +19,6 @@
 import { ServerResource } from 'teleport/Discover/Server';
 import { DatabaseResource } from 'teleport/Discover/Database';
 import { KubernetesResource } from 'teleport/Discover/Kubernetes';
-import { DesktopResource } from 'teleport/Discover/Desktop';
 import { ConnectMyComputerResource } from 'teleport/Discover/ConnectMyComputer';
 
 import { ResourceViewConfig } from './flow';
@@ -28,6 +27,5 @@ export const viewConfigs: ResourceViewConfig[] = [
   ServerResource,
   DatabaseResource,
   KubernetesResource,
-  DesktopResource,
   ConnectMyComputerResource,
 ];
