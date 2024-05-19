567aebb19cb8355f825834fcaf441c7b018dee31
Fred Heinecke <fred.heinecke@goteleport.com>
2024-05-19 16:58:06 -0500

    16.0.0-dev.fred-manifests.9

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "16.0.0-dev.fred-manifests.8"
+const Version = "16.0.0-dev.fred-manifests.9"
 
 var SemVersion = semver.New(Version)
