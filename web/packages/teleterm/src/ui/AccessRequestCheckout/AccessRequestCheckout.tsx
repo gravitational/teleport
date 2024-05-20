78a3f8640db9cc8cfad71e7e440391dbdcc2cbbb
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2024-05-20 13:03:35 +0200

    Display more information about the request in the access request checkout bar

--- web/packages/teleterm/src/ui/AccessRequestCheckout/AccessRequestCheckout.tsx
+++ web/packages/teleterm/src/ui/AccessRequestCheckout/AccessRequestCheckout.tsx
@@ -19,8 +19,16 @@
 import React from 'react';
 import { Transition } from 'react-transition-group';
 
-import { Box, Flex, ButtonPrimary, ButtonText, Text, ButtonIcon } from 'design';
-import { ChevronDown } from 'design/Icon';
+import {
+  Box,
+  Flex,
+  ButtonPrimary,
+  ButtonText,
+  Text,
+  ButtonIcon,
+  Label,
+} from 'design';
+import * as Icon from 'design/Icon';
 import { pluralize } from 'shared/utils/text';
 
 import { RequestCheckoutWithSlider } from 'shared/components/AccessRequests/NewRequest';
@@ -91,26 +99,89 @@ export function AccessRequestCheckout() {
     setRequestTTL,
   } = useAccessRequestCheckout();
 
+  const isRoleRequest = data[0]?.kind === 'role';
+
   return (
     <>
       {data.length > 0 && !isCollapsed() && (
         <Box
-          p={3}
+          px={3}
+          py={2}
           css={`
             border-top: 1px solid
               ${props => props.theme.colors.spotBackground[1]};
           `}
         >
-          <Flex justifyContent="space-between" alignItems="center">
-            <Text typography="h4" bold>
-              {data.length} {pluralize(data.length, 'Resource')} Selected
-            </Text>
+          <Flex
+            justifyContent="space-between"
+            alignItems="center"
+            css={`
+              gap: ${props => props.theme.space[1]}px;
+            `}
+          >
+            <Flex flexDirection="column" minWidth={0}>
+              <Text mb={1}>
+                {data.length}{' '}
+                {pluralize(data.length, isRoleRequest ? 'role' : 'resource')}{' '}
+                added to access request:
+              </Text>
+              <Flex direction="row" gap={1} flexWrap="wrap">
+                {data
+                  .map(c => {
+                    let resource = { name: c.name, Icon: undefined };
+                    switch (c.kind) {
+                      case 'app':
+                        resource.Icon = Icon.Application;
+                        break;
+                      case 'node':
+                        resource.Icon = Icon.Server;
+                        break;
+                      case 'db':
+                        resource.Icon = Icon.Database;
+                        break;
+                      case 'kube_cluster':
+                        resource.Icon = Icon.Kubernetes;
+                        break;
+                    }
+                    return resource;
+                  })
+                  .map(c => (
+                    <Label
+                      kind="secondary"
+                      key={c.name}
+                      css={`
+                        display: flex;
+                        align-items: center;
+                        min-width: 0;
+                        gap: ${props => props.theme.space[1]}px;
+                      `}
+                    >
+                      {c.Icon && <c.Icon size={15} />}
+                      <span
+                        css={`
+                          text-overflow: ellipsis;
+                          white-space: nowrap;
+                          overflow: hidden;
+                        `}
+                      >
+                        {c.name}
+                      </span>
+                    </Label>
+                  ))}
+              </Flex>
+            </Flex>
             <Flex gap={3}>
-              <ButtonPrimary onClick={() => setShowCheckout(!showCheckout)}>
-                Proceed to Request
+              <ButtonPrimary
+                onClick={() => setShowCheckout(!showCheckout)}
+                textTransform="none"
+                css={`
+                  white-space: nowrap;
+                `}
+              >
+                Proceed to request
               </ButtonPrimary>
               <ButtonIcon onClick={collapseBar}>
-                <ChevronDown size="medium" />
+                <Icon.ChevronDown size="medium" />
               </ButtonIcon>
             </Flex>
           </Flex>
@@ -147,7 +218,7 @@ export function AccessRequestCheckout() {
             setSelectedReviewers={setSelectedReviewers}
             requireReason={false}
             numRequestedResources={requestedCount}
-            isResourceRequest={data[0]?.kind !== 'role'}
+            isResourceRequest={isRoleRequest}
             fetchStatus={'loaded'}
             dryRunResponse={dryRunResponse}
             maxDuration={maxDuration}
