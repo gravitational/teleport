387d14b8afab15a5df3dcbe2fbb14282448999bb
Justinas Stankevicius <justinas@goteleport.com>
2024-05-21 17:35:42 +0300

    Validate names in azure script handler, add test

--- lib/web/integrations_azureoidc.go
+++ lib/web/integrations_azureoidc.go
@@ -18,6 +18,7 @@ import (
 // by creating an Enterprise Application in the Azure account
 // with Teleport OIDC as a trusted credential issuer.
 func (h *Handler) azureOIDCConfigure(w http.ResponseWriter, r *http.Request, p httprouter.Params) (any, error) {
+	ctx := r.Context()
 	queryParams := r.URL.Query()
 
 	oidcIssuer, err := oidc.IssuerFromPublicAddress(h.cfg.PublicProxyAddr)
@@ -28,6 +29,24 @@ func (h *Handler) azureOIDCConfigure(w http.ResponseWriter, r *http.Request, p h
 	authConnectorName := queryParams.Get("authConnectorName")
 	if authConnectorName == "" {
 		return nil, trace.BadParameter("authConnectorName must be specified")
+	}
+	// Ensure the auth connector name is valid
+	const withSecrets = false
+	_, err = h.GetProxyClient().GetSAMLConnector(ctx, authConnectorName, false)
+	// NotFound error is ignored to prevent disclosure of whether the integration exists in a public/no-auth endpoint.
+	if err != nil && !trace.IsNotFound(err) {
+		return nil, trace.Wrap(err)
+	}
+
+	integrationName := queryParams.Get("integrationName")
+	if len(integrationName) == 0 {
+		return nil, trace.BadParameter("integrationName must be specified")
+	}
+	// Ensure the integration name is valid.
+	_, err = h.GetProxyClient().GetIntegration(ctx, integrationName)
+	// NotFound error is ignored to prevent disclosure of whether the integration exists in a public/no-auth endpoint.
+	if err != nil && !trace.IsNotFound(err) {
+		return nil, trace.Wrap(err)
 	}
 
 	// The script must execute the following command:
