74c094dec15d44b25362ce17d4e4f4b7fd5d594a
Noah Stride <noah.stride@goteleport.com>
2024-05-22 12:29:56 +0100

    [v15] Include attempt count in session upload retry logs (#41882)
    * Include attempt count in session upload retry logs
    
    * Shadow log

--- lib/events/stream.go
+++ lib/events/stream.go
@@ -738,6 +738,8 @@ func (w *sliceWriter) startUpload(partNumber int64, slice *slice) (*activeUpload
 
 		var retry retryutils.Retry
 		for i := 0; i < defaults.MaxIterationLimit; i++ {
+			log := log.WithField("attempt", i)
+
 			reader, err := slice.reader()
 			if err != nil {
 				activeUpload.setError(err)
@@ -778,7 +780,7 @@ func (w *sliceWriter) startUpload(partNumber int64, slice *slice) (*activeUpload
 			}
 			select {
 			case <-retry.After():
-				log.WithError(err).Debugf("Part upload failed, retrying after backoff.")
+				log.WithError(err).Debugf("Back off period for retry has passed. Retrying")
 			case <-w.proto.cancelCtx.Done():
 				return
 			}
