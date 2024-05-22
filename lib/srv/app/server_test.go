2a72fc6c3c17764c08baccf5e71922756876e1d2
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2024-05-22 15:48:31 -0400

    Convert App heartbeats to use the inventory control stream (#41628)
    Updates app server heartbeats to use srv.Heartbeatv2 in order to
    leverage the inventory control stream instead of the legacy mechanism.
    The new srv.heartbeatV2Driver implementation for app servers detects
    the version of Auth and falls back appropriately if the Auth server
    is too old to know about the new field added to the InventoryHeartbeat
    message. The connected_resources metric should be accurate if the
    auth and app service are on the same version or if the auth service
    is on a more recent version. If the app service is newer than auth
    and the new app server driver kicks into fall back mode then the
    connected resource metric will not be updated.
    
    Partially addresses #40917

--- lib/srv/app/server_test.go
+++ lib/srv/app/server_test.go
@@ -44,6 +44,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/gravitational/teleport"
+	"github.com/gravitational/teleport/api/client/proto"
 	"github.com/gravitational/teleport/api/constants"
 	"github.com/gravitational/teleport/api/defaults"
 	"github.com/gravitational/teleport/api/types"
@@ -54,6 +55,7 @@ import (
 	"github.com/gravitational/teleport/lib/auth/testauthority"
 	"github.com/gravitational/teleport/lib/authz"
 	"github.com/gravitational/teleport/lib/events"
+	"github.com/gravitational/teleport/lib/inventory"
 	libjwt "github.com/gravitational/teleport/lib/jwt"
 	"github.com/gravitational/teleport/lib/labels"
 	"github.com/gravitational/teleport/lib/services"
@@ -329,6 +331,13 @@ func SetUpSuiteWithConfig(t *testing.T, config suiteConfig) *Suite {
 
 	discard := events.NewDiscardEmitter()
 
+	inventoryHandle := inventory.NewDownstreamHandle(s.authClient.InventoryControlStream, proto.UpstreamInventoryHello{
+		ServerID: s.hostUUID,
+		Version:  teleport.Version,
+		Services: []types.SystemRole{types.RoleApp},
+		Hostname: "test",
+	})
+
 	s.appServer, err = New(s.closeContext, &Config{
 		Clock:             s.clock,
 		DataDir:           s.dataDir,
@@ -348,13 +357,18 @@ func SetUpSuiteWithConfig(t *testing.T, config suiteConfig) *Suite {
 		Emitter:           discard,
 		CloudLabels:       config.CloudImporter,
 		ConnectionMonitor: fakeConnMonitor{},
+		InventoryHandle:   inventoryHandle,
 	})
 	require.NoError(t, err)
 
 	err = s.appServer.Start(s.closeContext)
 	require.NoError(t, err)
-	err = s.appServer.ForceHeartbeat()
-	require.NoError(t, err)
+
+	for _, app := range apps {
+		_, err := s.authServer.AuthServer.UpsertApplicationServer(s.closeContext, s.appServer.getServerInfo(app))
+		require.NoError(t, err)
+	}
+
 	t.Cleanup(func() {
 		s.appServer.Close()
 
@@ -482,11 +496,13 @@ func TestShutdown(t *testing.T) {
 			})
 
 			// Validate heartbeat is present after start.
-			s.appServer.ForceHeartbeat()
+			_, err = s.authServer.AuthServer.UpsertApplicationServer(ctx, s.appServer.getServerInfo(app0))
+			require.NoError(t, err)
+
 			appServers, err := s.authClient.GetApplicationServers(ctx, defaults.Namespace)
 			require.NoError(t, err)
 			require.Len(t, appServers, 1)
-			require.Equal(t, appServers[0].GetApp(), app0)
+			require.Empty(t, cmp.Diff(appServers[0].GetApp(), app0, cmpopts.IgnoreFields(types.Metadata{}, "Expires")))
 
 			// Shutdown should not return error.
 			shutdownCtx, cancel := context.WithTimeout(ctx, time.Second*5)
@@ -501,7 +517,7 @@ func TestShutdown(t *testing.T) {
 			appServersAfterShutdown, err := s.authClient.GetApplicationServers(ctx, defaults.Namespace)
 			require.NoError(t, err)
 			if test.wantAppServersAfterShutdown {
-				require.Equal(t, appServers, appServersAfterShutdown)
+				require.Empty(t, cmp.Diff(appServersAfterShutdown[0].GetApp(), app0))
 			} else {
 				require.Empty(t, appServersAfterShutdown)
 			}
