ae4c4beb3bb83c3ff58a0dcbfd70b538a4a04a53
Fred Heinecke <fred.heinecke@goteleport.com>
2024-05-23 17:19:19 -0500

    16.0.0-dev.fred-plugins-3.5

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "16.0.0-dev.fred-plugins-3.4"
+const Version = "16.0.0-dev.fred-plugins-3.5"
 
 var SemVersion = semver.New(Version)
