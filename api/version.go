2715ad2697be548ff42fb81c6d8e7148472963e1
Fred Heinecke <fred.heinecke@goteleport.com>
2024-05-23 13:18:37 -0500

    16.0.0-dev.fred-plugins-fix-1.1

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "16.0.0-dev.fred-plugins.1"
+const Version = "16.0.0-dev.fred-plugins-fix-1.1"
 
 var SemVersion = semver.New(Version)
