ce5df8007e5d03c1d06d3b64b51fb567aa4ddea1
Lisa Kim <lisa@goteleport.com>
2024-05-24 17:30:13 -0700

    Define ssm endpoint and related types

--- web/packages/teleport/src/services/discovery/types.ts
+++ web/packages/teleport/src/services/discovery/types.ts
@@ -29,6 +29,12 @@ export type DiscoveryConfig = {
 
 type AwsMatcherTypes = 'rds' | 'eks' | 'ec2';
 
+export enum InstallParamEnrollMode {
+  'Unspecified' = 0,
+  'Script' = 1,
+  'Eice' = 2,
+}
+
 // AWSMatcher matches AWS EC2 instances, AWS EKS clusters and AWS Databases
 export type AwsMatcher = {
   // types are AWS types to match, "ec2", "eks", "rds", "redshift", "elasticache",
@@ -43,6 +49,30 @@ export type AwsMatcher = {
   integration: string;
   // kubeAppDiscovery specifies if Kubernetes App Discovery should be enabled for a discovered cluster.
   kubeAppDiscovery?: boolean;
+  /**
+   * InstallParams sets the join method when installing on
+   * discovered EC2 nodes
+   */
+  install?: {
+    /**
+     *  EnrollMode indicates the mode used to enroll the node into Teleport.
+     * Valid values: script, eice.
+     */
+    enrollMode: InstallParamEnrollMode;
+    /**
+     * InstallTeleport disables agentless discovery
+     */
+    installTeleport: boolean;
+    /**
+     * JoinToken is the token to use when joining the cluster
+     */
+    joinToken: string;
+  };
+  /**
+   * SSM provides options to use when sending a document command to
+   * an EC2 node
+   */
+  ssm?: { documentName: string };
 };
 
 type Labels = Record<string, string[]>;
