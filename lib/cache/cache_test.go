12cf39393dc98a6fde0195af02fdd674098dee04
EdwardDowling <edward.dowling@goteleport.com>
2024-05-28 17:32:26 +0100

    Remove unused import

--- lib/cache/cache_test.go
+++ lib/cache/cache_test.go
@@ -43,7 +43,6 @@ import (
 	"github.com/gravitational/teleport/api/client/proto"
 	apidefaults "github.com/gravitational/teleport/api/defaults"
 	accessmonitoringrulesv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/accessmonitoringrules/v1"
-	headerv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/header/v1"
 	kubewaitingcontainerpb "github.com/gravitational/teleport/api/gen/proto/go/teleport/kubewaitingcontainer/v1"
 	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/api/types/accesslist"
