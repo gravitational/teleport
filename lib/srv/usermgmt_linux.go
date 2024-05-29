80009f50c77bd9332153c393393b915d8a338e74
Andrew Burke <andrew.burke@goteleport.com>
2024-05-28 17:00:32 -0700

    Fix home dir for insecure_drop mode
    This change fixes a bug where file uploads/downloads in the web UI
    would fail if the local user (provisioned by Teleport in
    insecure-drop mode) didn't have a home directory.

--- lib/srv/usermgmt_linux.go
+++ lib/srv/usermgmt_linux.go
@@ -92,12 +92,8 @@ func (*HostUsersProvisioningBackend) CreateGroup(name string, gid string) error 
 }
 
 // CreateUser creates a user on a host
-func (*HostUsersProvisioningBackend) CreateUser(name string, groups []string, uid, gid string) error {
-	home, err := readDefaultHome(name)
-	if err != nil {
-		return trace.Wrap(err)
-	}
-	_, err = host.UserAdd(name, groups, home, uid, gid)
+func (*HostUsersProvisioningBackend) CreateUser(name string, groups []string, home, uid, gid string) error {
+	_, err := host.UserAdd(name, groups, home, uid, gid)
 	return trace.Wrap(err)
 }
 
@@ -214,17 +210,12 @@ func readDefaultSkel() (string, error) {
 	return skel, trace.Wrap(err)
 }
 
-func (u *HostUsersProvisioningBackend) CreateHomeDirectory(user string, uidS, gidS string) error {
+func (u *HostUsersProvisioningBackend) CreateHomeDirectory(userHome, uidS, gidS string) error {
 	uid, err := strconv.Atoi(uidS)
 	if err != nil {
 		return trace.Wrap(err)
 	}
 	gid, err := strconv.Atoi(gidS)
-	if err != nil {
-		return trace.Wrap(err)
-	}
-
-	userHome, err := readDefaultHome(user)
 	if err != nil {
 		return trace.Wrap(err)
 	}
