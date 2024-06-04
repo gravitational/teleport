88901f73fd8bad65fec72dfa34f183d4aeef195e
Bernard Kim <bernard@goteleport.com>
2024-06-04 11:55:21 -0700

    Refactor updater tests (#4248)
    * Refactor updater tests
    
    * Fix working directory
    
    * Add basic teleport installation test
    
    * Remove extra quotes
    
    * Create separate integrations workflow
    
    * Add deploy input
    
    * Run tests if deploy is skipped
    
    * Fix env var
    
    * Check setup result
    
    * fix input
    
    * extra quotes
    
    * set domain name
    
    * log pre install
    
    * Add separate teleport and updater test jobs
    
    * Missing test-case input
    
    * Swap test cases
    
    * shellcheck
    
    * Address feedback
    
    - Check variables idiomatically
    - Use REPO_DOMAIN_NAME for all package types
    
    * Address feedback
    
    - Use array for flags
    - Use conf_dir variable
    
    * Use matrix object
    
    * newline
    
    Source-commit: gravitational/teleport.e@37c9c0949484e220e546ebeec8e07b5f0e79fa81
    Source-pull-requests: gravitational/teleport.e#4248

---
+++ e/assets/systemd-unit-upgrader-tests/scripts/apt.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Import utils.sh functions
+source utils.sh
+
+# DEBIAN_FRONTEND when set to noninteractive, instructs debconf to run non-interactively
+# and accepts default answers.
+export DEBIAN_FRONTEND=noninteractive
+
+# APT_FLAGS defines a few apt-get command flags
+#   -y assumes "yes"
+#   -qq runs in quiet mode to reduce noise
+#   -o=Dpkg::Use-Pty=0 disables use of pty to further reduce noise
+APT_FLAGS=("-yqq" "-o=Dpkg::Use-Pty=0")
+
+# log_pre_install_info logs relevant package information before installation.
+function log_pre_install_info() {
+    : "${1:?package_name was not provided}"
+    local package_name="$1"
+
+    apt-file update > /dev/null
+    echo "Available versions of package:"
+    apt-cache policy "${package_name}"
+    echo "Package files:"
+    apt-file list "${package_name}"
+}
+
+# install_dependencies installs test dependencies.
+function install_dependencies() {
+    apt-get "${APT_FLAGS[@]}" update
+    apt-get "${APT_FLAGS[@]}" install curl apt-file apt-utils
+}
+
+# initialize_repo initializes the teleport package repository with the provided
+# name.
+function initialize_repo() {
+    : "${1:?repository_name was not provided}"
+    local repository_name="$1"
+
+    source /etc/os-release
+    curl "https://${REPO_DOMAIN_NAME}/gpg" -o /usr/share/keyrings/teleport-archive-keyring.asc
+    echo "deb [signed-by=/usr/share/keyrings/teleport-archive-keyring.asc] \
+        https://${REPO_DOMAIN_NAME}/${ID?} ${VERSION_CODENAME?} ${RELEASE_CHANNEL}/${VERSION_CHANNEL}" \
+        | tee "/etc/apt/sources.list.d/${repository_name}.list" > /dev/null
+    apt-get "${APT_FLAGS[@]}" update
+
+    echo "successfully initialized teleport repository"
+    echo "# /etc/apt/sources.list.d/${repository_name}.list"
+    cat "/etc/apt/sources.list.d/${repository_name}.list"
+}
+
+# install_teleport installs teleport at the specified version.
+function install_teleport() {
+    : "${1:?teleport_version was not provided}"
+    local teleport_version="$1"
+
+    apt-get "${APT_FLAGS[@]}" update
+    apt-get "${APT_FLAGS[@]}" install "teleport-ent=${teleport_version}"
+    verify_teleport "${teleport_version}"
+    echo "successfully installed teleport-ent ${teleport_version}"
+}
+
+# install_updater installs the teleport updater at the specified version.
+function install_updater() {
+    : "${1:?updater_version was not provided}"
+    local updater_version="$1"
+
+    apt-get "${APT_FLAGS[@]}" update
+    apt-get "${APT_FLAGS[@]}" install "teleport-ent-updater=${updater_version}"
+    verify_updater "${updater_version}"
+    echo "successfully installed teleport-ent-updater ${updater_version}"
+}
+
+# update_system performs a system update.
+function update_system() {
+    local expected_teleport_version
+    expected_teleport_version="$(teleport version --raw)"
+
+    # system update may return a non 0 exit code due to teleport-ent-updater post install
+    apt-get "${APT_FLAGS[@]}" upgrade || true
+    verify_teleport "${expected_teleport_version}"
+}
