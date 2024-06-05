7acdfcfc026246f684bfcc03ba9e4ef0375db5e3
Fred Heinecke <fred.heinecke@goteleport.com>
2024-06-05 01:56:35 -0500

    17.0.0-dev.fred-macos-manifests-1.7

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "17.0.0-dev.fred-macos-manifests-1.6"
+const Version = "17.0.0-dev.fred-macos-manifests-1.7"
 
 var SemVersion = semver.New(Version)
