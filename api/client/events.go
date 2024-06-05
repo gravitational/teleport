57470d928cd848879a3bee57900ee78ec78c91a0
Tiago Silva <tiago.silva@goteleport.com>
2024-06-05 12:11:18 +0100

    accesslist: return "ineligible_status" for all write operations
    This PR propagates access list members and owner's ineglibile_status
    field with the data from the backend. Prior to this change, the field
    was ignored.
    
    Signed-off-by: Tiago Silva <tiago.silva@goteleport.com>

--- api/client/events.go
+++ api/client/events.go
@@ -414,7 +414,10 @@ func EventFromGRPC(in *proto.Event) (*types.Event, error) {
 		out.Resource = r
 		return &out, nil
 	} else if r := in.GetAccessList(); r != nil {
-		out.Resource, err = accesslistv1conv.FromProto(r)
+		out.Resource, err = accesslistv1conv.FromProto(
+			r,
+			accesslistv1conv.WithOwnersIneligibleStatusField(r.GetSpec().GetOwners()),
+		)
 		if err != nil {
 			return nil, trace.Wrap(err)
 		}
@@ -426,7 +429,10 @@ func EventFromGRPC(in *proto.Event) (*types.Event, error) {
 		}
 		return &out, nil
 	} else if r := in.GetAccessListMember(); r != nil {
-		out.Resource, err = accesslistv1conv.FromMemberProto(r)
+		out.Resource, err = accesslistv1conv.FromMemberProto(
+			r,
+			accesslistv1conv.WithMemberIneligibleStatusField(r),
+		)
 		if err != nil {
 			return nil, trace.Wrap(err)
 		}
