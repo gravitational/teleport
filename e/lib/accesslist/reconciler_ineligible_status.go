gravitational/teleport

commit 0209c7d3eeed4309f4c1172786a21c14669360ba

accesslist: correctly propagate membership removal to access graph (#4323)
This PR correctly maps the deletion process of access list member when a
user is removed from an access list.

It also extends the inelibile status reconciler logging.

Signed-off-by: Tiago Silva <tiago.silva@goteleport.com>

Source-commit: gravitational/teleport.e@04a45b5781cd1b59371aff622feb4487bbf842d4
Source-pull-requests: gravitational/teleport.e#4323

e/lib/accesslist/reconciler_ineligible_status.go
@@ -82,7 +82,6 @@ func waitForInitOp(watcher types.Watcher) error {
 		case <-watcher.Done():
 			return trace.Wrap(watcher.Error())
 		}
-
 	}
 }
 
@@ -98,11 +97,22 @@ type IneligibleStatusReconciler struct {
 const (
 	// 100 years is a good enough approximation for "never"
 	neverDuration = time.Hour * 24 * 365 * 100
+	// forceReconcileDuration is the duration after which we will force a reconciliation to happen.
+	forceReconcileDuration = 30 * time.Minute
 )
 
 // Run runs the reconciliation loop.
 func (r *IneligibleStatusReconciler) Run(ctx context.Context) error {
 	t := r.clock.NewTimer(neverDuration)
+	// forceReconcile is a timer that will force a reconciliation to happen
+	// after a certain amount of time has passed.
+	forceReconcile := r.clock.NewTimer(forceReconcileDuration)
+	drainAndResetTimer := func(t clockwork.Timer, timeout time.Duration) {
+		if !t.Stop() {
+			<-t.Chan()
+		}
+		t.Reset(timeout)
+	}
 	// reconcile is a blocking channel that will be used to signal that
 	// a reconciliation must happen.
 	// It must be unbuffered to ensure that we don't re-reconcile if we
@@ -134,10 +144,8 @@ func (r *IneligibleStatusReconciler) Run(ctx context.Context) error {
 		if nextExpirationTime <= 0 {
 			nextExpirationTime = 1 * time.Second
 		}
-		if !t.Stop() {
-			<-t.Chan()
-		}
-		t.Reset(nextExpirationTime)
+		drainAndResetTimer(t, nextExpirationTime)
+		drainAndResetTimer(forceReconcile, forceReconcileDuration)
 		select {
 		case <-ctx.Done():
 			return nil
@@ -148,7 +156,6 @@ func (r *IneligibleStatusReconciler) Run(ctx context.Context) error {
 		}
 
 	}
-
 }
 
 // Close closes the reconciler.
@@ -156,7 +163,17 @@ func (r *IneligibleStatusReconciler) Close() error {
 	return r.watcher.Close()
 }
 
-func (r *IneligibleStatusReconciler) reconciliationLoop(ctx context.Context, now time.Time) (time.Duration, error) {
+func (r *IneligibleStatusReconciler) reconciliationLoop(ctx context.Context, now time.Time) (nextExpirationTime time.Duration, err error) {
+	r.log.Debug("Reconciling memberships")
+	defer func() {
+		fields := logrus.Fields{
+			"next_expiration_time": nextExpirationTime,
+		}
+		if err != nil && !trace.IsCompareFailed(err) {
+			fields["error"] = err
+		}
+		r.log.WithFields(fields).Debug("AccessList reconciliation complete")
+	}()
 	// get all users
 	users, err := getAllUsers(ctx, r.cache, 0 /* use the default page size */)
 	if err != nil {
@@ -173,8 +190,7 @@ func (r *IneligibleStatusReconciler) reconciliationLoop(ctx context.Context, now
 		return 0, trace.Wrap(err, "unable to reconcile access list ownership")
 	}
 
-	nextExpirationTime, err := r.reconcileMemberships(ctx, now, accessLists, usersMap)
-
+	nextExpirationTime, err = r.reconcileMemberships(ctx, now, accessLists, usersMap)
 	return nextExpirationTime, trace.Wrap(err, "unable to reconcile memberships")
 }
 
@@ -210,6 +226,12 @@ func (r *IneligibleStatusReconciler) reconcileAccessListOwnership(ctx context.Co
 			oldIneligibleStatus := owner.IneligibleStatus
 			owner.IneligibleStatus = accesslistv1.IneligibleStatus_name[int32(ineligibleStatus)]
 			if oldIneligibleStatus != owner.IneligibleStatus {
+				r.log.WithFields(logrus.Fields{
+					"access_list": accessList.GetName(),
+					"username":    owner.Name,
+					"old_status":  oldIneligibleStatus,
+					"new_status":  owner.IneligibleStatus,
+				}).Debug("Updating access list owner ineligibility status")
 				toUpdate = true
 			}
 			accessList.Spec.Owners[i] = owner
@@ -228,10 +250,7 @@ func (r *IneligibleStatusReconciler) reconcileAccessListOwnership(ctx context.Co
 }
 
 func (r *IneligibleStatusReconciler) reconcileMemberships(ctx context.Context, now time.Time, accessLists []*accesslist.AccessList, usersMap map[string]types.User) (time.Duration, error) {
-	accessListsMap := make(map[string]*accesslist.AccessList, len(accessLists))
-	for _, accessList := range accessLists {
-		accessListsMap[accessList.GetName()] = accessList
-	}
+	accessListsMap := sliceToMap(accessLists)
 
 	startKey := ""
 	nextExpiration := neverDuration
@@ -258,6 +277,12 @@ func (r *IneligibleStatusReconciler) reconcileMemberships(ctx context.Context, n
 			oldIneligibleStatus := member.Spec.IneligibleStatus
 			member.Spec.IneligibleStatus = ineligibleStatus.String()
 			if oldIneligibleStatus != member.Spec.IneligibleStatus {
+				r.log.WithFields(logrus.Fields{
+					"access_list": member.Spec.AccessList,
+					"username":    member.Spec.Name,
+					"old_status":  oldIneligibleStatus,
+					"new_status":  member.Spec.IneligibleStatus,
+				}).Debug("Updating access list member ineligibility status")
 				toUpdate = append(toUpdate, member)
 			}
 		}
@@ -268,9 +293,7 @@ func (r *IneligibleStatusReconciler) reconcileMemberships(ctx context.Context, n
 		}
 
 		batchNextExpirationTime := nextExpirationTime(now, accessListsMembers)
-		if batchNextExpirationTime < nextExpiration {
-			nextExpiration = batchNextExpirationTime
-		}
+		nextExpiration = min(nextExpiration, batchNextExpirationTime)
 		if nextKey == "" {
 			break
 		}
@@ -344,7 +367,6 @@ func (r *IneligibleStatusReconciler) informReconciliationMustHappen(reconcile ch
 				case <-r.watcher.Done():
 					return
 				}
-
 			}
 		case <-r.watcher.Done():
 			return

e/lib/accessgraph/accessgraph.go
@@ -158,7 +158,7 @@ func initializeAndWatchAccessGraph(ctx context.Context, log *slog.Logger, config
 				return trace.Wrap(err)
 			}
 			missingWatchKinds(ctx, log, supportedKinds, cacheSupportedResources)
-			errc := make(chan error)
+			errc := make(chan error, 1)
 			go func() {
 				// Start watching the auth server for events.
 				// Subscribe for new events before sending all resources.
@@ -271,7 +271,6 @@ func waitForInit(watcher types.Watcher) ([]string, error) {
 			}
 			evt, ok := event.Resource.(types.WatchStatus)
 			if !ok {
-
 				return nil, nil
 			}
 			var kinds []string
@@ -388,7 +387,8 @@ func forwardEventsWatch(watcher types.Watcher, eventWatcher *tagEventWatcher) er
 // sendUsers sends all users to the access graph service.
 func sendUsers(ctx context.Context, authServer interface {
 	ListUsers(ctx context.Context, req *userspb.ListUsersRequest) (*userspb.ListUsersResponse, error)
-}, stream accessgraphv1.AccessGraphService_EventsStreamV2Client) error {
+}, stream accessgraphv1.AccessGraphService_EventsStreamV2Client,
+) error {
 	req := userspb.ListUsersRequest{
 		PageSize: apidefaults.DefaultChunkSize,
 	}
@@ -414,7 +414,8 @@ func sendUsers(ctx context.Context, authServer interface {
 
 func sendCrownJewels(ctx context.Context, authServer interface {
 	ListCrownJewels(ctx context.Context, pageSize int64, nextToken string) ([]*crownjewelv1.CrownJewel, string, error)
-}, stream accessgraphv1.AccessGraphService_EventsStreamV2Client) error {
+}, stream accessgraphv1.AccessGraphService_EventsStreamV2Client,
+) error {
 	nextToken := ""
 
 	for {
@@ -478,7 +479,8 @@ func pushCrownJewelsToTAG(ctx context.Context, stream accessgraphv1.AccessGraphS
 // sendRoles sends all roles to the access graph service.
 func sendRoles(ctx context.Context, authServer interface {
 	GetRoles(context.Context) ([]types.Role, error)
-}, stream accessgraphv1.AccessGraphService_EventsStreamV2Client) error {
+}, stream accessgraphv1.AccessGraphService_EventsStreamV2Client,
+) error {
 	// Get all roles.
 	// Auth server does not support pagination for roles, so we have to get all roles at once
 	// and we chunk them after.
@@ -528,7 +530,8 @@ func pushRolesToTAG(ctx context.Context, stream accessgraphv1.AccessGraphService
 func sendAccessLists(ctx context.Context, authServer interface {
 	ListAccessLists(context.Context, int, string) ([]*accesslist.AccessList, string, error)
 	ListAccessListMembers(ctx context.Context, accessListName string, pageSize int, pageToken string) (members []*accesslist.AccessListMember, nextToken string, err error)
-}, stream accessgraphv1.AccessGraphService_EventsStreamV2Client) error {
+}, stream accessgraphv1.AccessGraphService_EventsStreamV2Client,
+) error {
 	startToken := ""
 	limit := 0 // use default limit
 
@@ -578,7 +581,8 @@ func pushAccessListsToTAG(ctx context.Context, stream accessgraphv1.AccessGraphS
 
 func sendAccessListMembers(ctx context.Context, authServer interface {
 	ListAccessListMembers(ctx context.Context, accessListName string, pageSize int, pageToken string) (members []*accesslist.AccessListMember, nextToken string, err error)
-}, stream accessgraphv1.AccessGraphService_EventsStreamV2Client, accessList *accesslist.AccessList) error {
+}, stream accessgraphv1.AccessGraphService_EventsStreamV2Client, accessList *accesslist.AccessList,
+) error {
 	startToken := ""
 	limit := 0 // use default limit
 
@@ -875,8 +879,25 @@ func (t *tagEventWatcher) sendDelete(event *proto.Event) error {
 			),
 		)
 	case *proto.Event_ResourceHeader:
+		if resource.ResourceHeader.Kind == types.KindAccessListMember {
+			// Access list member uses a different object format
+			// when it is sent to the access graph service.
+			req = &accessgraphv1.EventsStreamV2Request{
+				Operation: &accessgraphv1.EventsStreamV2Request_ExcludeAccessListMembers{
+					ExcludeAccessListMembers: &accessgraphv1.ExcludeAccessListsMembers{
+						Members: []*accessgraphv1.ExcludeAccessListMember{
+							{
+								// access list name comes from the resource header description
+								AccessList: resource.ResourceHeader.GetMetadata().Description,
+								Username:   resource.ResourceHeader.GetName(),
+							},
+						},
+					},
+				},
+			}
+			break
+		}
 		req = deleteEventStreamRequest(resource.ResourceHeader)
-
 	case *proto.Event_AccessList:
 		header := headerv1.FromResourceHeaderProto(resource.AccessList.GetHeader())
 		req = deleteEventStreamRequest(

e/lib/accesslist/reconciler_ineligible_status_test.go
@@ -34,7 +34,6 @@ func TestNewIneligibleStatusReconciler(t *testing.T) {
 		for _, member := range members {
 			assert.Equal(t, "INELIGIBLE_STATUS_ELIGIBLE", member.Spec.IneligibleStatus)
 		}
-
 	}, 5*time.Second, 100*time.Millisecond)
 
 	// Update the access list member to be ineligible.
@@ -71,7 +70,6 @@ func TestNewIneligibleStatusReconciler(t *testing.T) {
 		for _, member := range members {
 			assert.Equal(t, "INELIGIBLE_STATUS_ELIGIBLE", member.Spec.IneligibleStatus)
 		}
-
 	}, 5*time.Second, 100*time.Millisecond)
 
 	// Update access list to change the requirements.
@@ -108,7 +106,6 @@ func TestNewIneligibleStatusReconciler(t *testing.T) {
 		for _, owner := range al.Spec.Owners {
 			assert.Equal(t, "INELIGIBLE_STATUS_MISSING_REQUIREMENTS", owner.IneligibleStatus)
 		}
-
 	}, 5*time.Second, 100*time.Millisecond)
 
 	// Expire all access list members.
@@ -121,5 +118,4 @@ func TestNewIneligibleStatusReconciler(t *testing.T) {
 			assert.Equal(t, "INELIGIBLE_STATUS_EXPIRED", member.Spec.IneligibleStatus)
 		}
 	}, 5*time.Second, 100*time.Millisecond)
-
 }

commit 1e6ebb1f1ad436639141fe48a6e035196459bf04

fix: add `forceReconcile` to select statement (#4328)
This PR fixes a typo introduced by #4323 where the `forceReconciler`
chan wasn't watched.

Signed-off-by: Tiago Silva <tiago.silva@goteleport.com>

Source-commit: gravitational/teleport.e@73388fd8223c64df392117fd4c2af41eb92f455e
Source-pull-requests: gravitational/teleport.e#4328

e/lib/accesslist/reconciler_ineligible_status.go
@@ -153,6 +153,7 @@ func (r *IneligibleStatusReconciler) Run(ctx context.Context) error {
 			return trace.Wrap(r.watcher.Error())
 		case <-reconcile:
 		case <-t.Chan():
+		case <-forceReconcile.Chan():
 		}
 
 	}