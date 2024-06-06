cd4acf29ef83ceb9f7b473be1a786c4ca2ef17a4
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2024-06-06 11:51:15 +0200

    Remove methods from `ClustersService` than only forward the call `TshdClient`

--- web/packages/teleterm/src/ui/AccessRequestCheckout/useAccessRequestCheckout.ts
+++ web/packages/teleterm/src/ui/AccessRequestCheckout/useAccessRequestCheckout.ts
@@ -114,8 +114,8 @@ export default function useAccessRequestCheckout() {
 
     const data = getPendingAccessRequestsPerResource(pendingAccessRequest);
     runFetchResourceRoles(() =>
-      retryWithRelogin(ctx, clusterUri, () =>
-        ctx.clustersService.getRequestableRoles({
+      retryWithRelogin(ctx, clusterUri, async () => {
+        const { response } = await ctx.tshd.getRequestableRoles({
           clusterUri: rootClusterUri,
           resourceIds: data
             .filter(d => d.kind !== 'role')
@@ -128,8 +128,9 @@ export default function useAccessRequestCheckout() {
               clusterName: d.clusterName,
               subResourceName: '',
             })),
-        })
-      ).then(response => {
+        });
+        return response;
+      }).then(response => {
         setResourceRequestRoles(response.applicableRoles);
         setSelectedResourceRequestRoles(response.applicableRoles);
       })
