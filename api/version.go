424f11ce809deca8886a939419bf5543783e7073
Fred Heinecke <fred.heinecke@goteleport.com>
2024-06-10 03:40:18 -0500

    15.4.1-dev.tplaceholder-4

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "15.4.1-dev.tplaceholder-3"
+const Version = "15.4.1-dev.tplaceholder-4"
 
 var SemVersion = semver.New(Version)
