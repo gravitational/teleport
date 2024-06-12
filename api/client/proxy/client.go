f4940f6e069615f2cd58ce693e0e0a1dd995cda8
Noah Stride <noah.stride@goteleport.com>
2024-06-12 09:08:14 +0100

    Introduce long-lived SSH proxy for Machine ID (#42592) (#42760)
    * Skeleton out SSHProxyService
    
    * Skeleton out actual service implementation
    
    * Simple unit tests for yaml/validation
    
    * Add main config file unit test
    
    * Crudely copy in the connection handle code
    
    * Copy in basic setup code
    
    * Add rudimentary "dial cycler"
    
    * Tidy up handling of listener closure
    
    * Add basic prometheus metrics
    
    * Add ssh proxy command connect
    
    * Fix crashes
    
    * Make session resumption enabled by default
    
    * Use `utils.ProxyConn`
    
    * last few changes before i convert to socks5
    
    * Rename socket
    
    * Add support for loading proxy templates
    
    * Tidy up logging
    
    * rename to multiplexer
    
    * Fix up a few bits
    
    * Generate artifacts on initial start
    
    * Support specifying command and subcommand
    
    * Add identity generation and renewal to ssh multiplexer
    
    * Tidy up code post-identity generation
    
    * Tidy up logging for connection multiplexing/proxying
    
    * Americanize spellings
    
    * Info( -> InfoContext(
    
    * stash
    
    * Replace low-level cycler with higher-level but broken cycler
    
    * Remove reference to grpcClientConnInterfaceCloser
    
    * minor tweaks and fixes to the cycler
    
    * Fix lone tab in the ssh_config template
    
    * Better metrics
    
    * Fix ineff assign
    
    * Simplify config for custom muxcommand
    
    * Start wrtiign test
    
    * Fix broken fdpass
    
    * Potentially flaky but working test
    
    * Switch to NULL delimited
    
    * Tidy up tests
    
    * Remove commented out code
    
    * Drain buffer after the conn resumption has been enabled
    
    * Change socket name to `v1.sock`
    
    * Switch away from JSON
    
    * Switch to ProxyCommand style config for overrides
    
    * Switch to `filepath`
    
    * Update lib/tbot/service_ssh_multiplexer.go
    
    
    
    * Update lib/tbot/ssh/ssh.go
    
    
    
    * Update lib/tbot/service_ssh_multiplexer.go
    
    
    
    * Absolute paths
    
    * Remove unused dep
    
    * Update lib/tbot/service_ssh_multiplexer.go
    
    
    
    * Update lib/tbot/ssh/ssh.go
    
    
    
    * Drain buffer as part of proxying goroutine
    
    * Fix missing godoc
    
    * Missing godoc
    
    * Add test for connection cycler
    
    * Update lib/tbot/service_ssh_multiplexer.go
    
    
    
    * Simpler buffer draining
    
    * Fix race in test
    
    * More generous time outs
    
    ---------
    
    Co-authored-by: Edoardo Spadolini <edoardo.spadolini@goteleport.com>
    Co-authored-by: rosstimothy <39066650+rosstimothy@users.noreply.github.com>
    Co-authored-by: Tim Buckley <tim@goteleport.com>

--- api/client/proxy/client.go
+++ api/client/proxy/client.go
@@ -33,7 +33,6 @@ import (
 
 	"github.com/gravitational/teleport/api/breaker"
 	"github.com/gravitational/teleport/api/client"
-	authpb "github.com/gravitational/teleport/api/client/proto"
 	"github.com/gravitational/teleport/api/client/proxy/transport/transportv1"
 	"github.com/gravitational/teleport/api/defaults"
 	transportv1pb "github.com/gravitational/teleport/api/gen/proto/go/teleport/transport/v1"
@@ -431,10 +430,7 @@ func (c *Client) ClusterDetails(ctx context.Context) (ClusterDetails, error) {
 func (c *Client) Ping(ctx context.Context) error {
 	// TODO(tross): Update to call Ping when it is added to the transport service.
 	// For now we don't really care what method is used we just want to measure
-	// how long it takes to get a reply. This will always fail with a not implemented
-	// error since the Proxy gRPC server doesn't serve the auth service proto. However,
-	// we use it because it's already imported in the api package.
-	clt := authpb.NewAuthServiceClient(c.grpcConn)
-	_, _ = clt.Ping(ctx, &authpb.PingRequest{})
+	// how long it takes to get a reply.
+	_, _ = c.transport.ClusterDetails(ctx)
 	return nil
 }
