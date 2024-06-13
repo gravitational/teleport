7075a4b8c4a763ebb3f49fec72ac0eff0e4aadbe
Marco Dinis <marco.dinis@goteleport.com>
2024-06-13 10:34:46 +0100

    Allow arguments for the oneoff script (#42854)
    The oneoff script downloads teleport and runs a teleport command.
    In order to use the oneoff script for Server Auto Discover we must allow
    it to receive a token as an argument.
    You can see how the script is meant to be executed:
    lib/cloud/aws/ssm_document.EC2DiscoverySSMDocument

--- api/types/constants.go
+++ api/types/constants.go
@@ -38,7 +38,7 @@ const (
 
 	// PackageNameOSS is the teleport package name for the OSS version.
 	PackageNameOSS = "teleport"
-	// PackageNameOSS is the teleport package name for the Enterprise version.
+	// PackageNameEnt is the teleport package name for the Enterprise version.
 	PackageNameEnt = "teleport-ent"
 
 	// ActionRead grants read access (get, list)
@@ -549,6 +549,9 @@ const (
 	V1 = "v1"
 )
 
+// PackageNameKinds is the list of valid teleport package names.
+var PackageNameKinds = []string{PackageNameOSS, PackageNameEnt}
+
 // WebSessionSubKinds lists subkinds of web session resources
 var WebSessionSubKinds = []string{KindAppSession, KindWebSession, KindSnowflakeSession, KindSAMLIdPSession}
 
@@ -671,7 +674,7 @@ const (
 	// id found via automatic discovery, to avoid re-running
 	// installation commands on the node.
 	ProjectIDLabel = TeleportInternalLabelPrefix + "project-id"
-	// ZoneLabek is used to identify virtual machines by GCP zone
+	// ZoneLabel is used to identify virtual machines by GCP zone
 	// found via automatic discovery, to avoid re-running installation
 	// commands on the node.
 	ZoneLabel = TeleportInternalLabelPrefix + "zone"
