f5e62d84660982dbb415c73f8a668d9fa1364bd3
STeve (Xin) Huang <xin.huang@goteleport.com>
2024-06-13 13:58:27 -0400

    [v14] Fix an issue Oracle access failed through trusted cluster (#42929)
    * Fix an issue Oracle access failed through trusted cluster
    
    * add note on profile.Cluster

--- lib/client/db/dbcmd/dbcmd_test.go
+++ lib/client/db/dbcmd/dbcmd_test.go
@@ -69,6 +69,7 @@ func TestCLICommandBuilderGetConnectCommand(t *testing.T) {
 		Name:     "example.com",
 		Username: "bob",
 		Dir:      "/tmp",
+		Cluster:  "example.com",
 	}
 
 	tests := []struct {
@@ -614,7 +615,7 @@ func TestCLICommandBuilderGetConnectCommand(t *testing.T) {
 			opts:         []ConnectCommandFunc{WithLocalProxy("localhost", 12345, "")},
 			execer:       &fakeExec{},
 			databaseName: "oracle01",
-			cmd:          []string{"sql", "-L", "jdbc:oracle:thin:@tcps://localhost:12345/oracle01?TNS_ADMIN=/tmp/keys/example.com/bob-db/mysql-wallet"},
+			cmd:          []string{"sql", "-L", "jdbc:oracle:thin:@tcps://localhost:12345/oracle01?TNS_ADMIN=/tmp/keys/example.com/bob-db/db.example.com/mysql-wallet"},
 			wantErr:      false,
 		},
 		{
@@ -623,7 +624,7 @@ func TestCLICommandBuilderGetConnectCommand(t *testing.T) {
 			opts:         []ConnectCommandFunc{WithLocalProxy("localhost", 12345, ""), WithPrintFormat()},
 			execer:       &fakeExec{},
 			databaseName: "oracle01",
-			cmd:          []string{"sql", "-L", "'jdbc:oracle:thin:@tcps://localhost:12345/oracle01?TNS_ADMIN=/tmp/keys/example.com/bob-db/mysql-wallet'"},
+			cmd:          []string{"sql", "-L", "'jdbc:oracle:thin:@tcps://localhost:12345/oracle01?TNS_ADMIN=/tmp/keys/example.com/bob-db/db.example.com/mysql-wallet'"},
 			wantErr:      false,
 		},
 	}
@@ -846,6 +847,7 @@ func TestConvertCommandError(t *testing.T) {
 		Name:     "example.com",
 		Username: "bob",
 		Dir:      homePath,
+		Cluster:  "example.com",
 	}
 
 	tests := []struct {
