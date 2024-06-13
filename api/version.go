95eb497083f87b22e8c49a2c059a8a31b8bac3bb
Fred Heinecke <fred.heinecke@goteleport.com>
2024-06-13 14:13:14 -0500

    17.0.0-dev.fred-tsh-fix-4

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "17.0.0-dev.fred-tsh-fix-3"
+const Version = "17.0.0-dev.fred-tsh-fix-4"
 
 var SemVersion = semver.New(Version)
