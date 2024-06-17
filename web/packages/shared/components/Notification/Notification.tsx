39adc9883733a7a954f69392ebd11b8d07649b7a
Rafał Cieślak <rafal.cieslak@goteleport.com>
2024-06-17 10:23:33 +0200

    Show public_addr of VNet app in relogin modals and notifications (#43027)
    * Always copy publicAddr to clipboard
    
    * Show public addr in relogin modal and notification
    
    * Break up long addresses in notifications

--- web/packages/shared/components/Notification/Notification.tsx
+++ web/packages/shared/components/Notification/Notification.tsx
@@ -241,4 +241,6 @@ const Container = styled(Flex)`
   color: ${props => props.theme.colors.text.main};
   border-radius: 4px;
   cursor: pointer;
+  // Break up long addresses.
+  word-break: break-word;
 `;
