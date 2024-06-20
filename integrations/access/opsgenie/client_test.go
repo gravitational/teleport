4eaf61594d6f85efcdbc61de652f1f28731da731
Carlos Castro <carlos.castro@jumo.world>
2024-06-20 14:52:12 +0100

    Opsgenie team responders support

--- integrations/access/opsgenie/client_test.go
+++ integrations/access/opsgenie/client_test.go
@@ -60,6 +60,7 @@ func TestCreateAlert(t *testing.T) {
 		RequestReason: "someReason",
 		SystemAnnotations: types.Labels{
 			types.TeleportNamespace + types.ReqAnnotationNotifySchedulesLabel: {"responder@teleport.com"},
+			types.TeleportNamespace + types.ReqAnnotationTeamsLabel:           {"MyOpsGenieTeam"},
 		},
 	})
 	assert.NoError(t, err)
@@ -68,8 +69,11 @@ func TestCreateAlert(t *testing.T) {
 		Message:     "Access request from someUser",
 		Alias:       "teleport-access-request/someRequestID",
 		Description: "someUser requested permissions for roles role1, role2 on Teleport at 01 Jan 01 00:00 UTC.\nReason: someReason\n\n",
-		Responders:  []Responder{{Type: "schedule", ID: "responder@teleport.com"}},
-		Priority:    "somePriority",
+		Responders: []Responder{
+			{Type: "schedule", ID: "responder@teleport.com"},
+			{Type: "team", ID: "MyOpsGenieTeam"},
+		},
+		Priority: "somePriority",
 	}
 	var got AlertBody
 	err = json.Unmarshal([]byte(recievedReq), &got)
