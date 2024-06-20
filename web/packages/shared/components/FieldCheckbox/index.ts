894dcccbb0a2046293de7d85084fc18ea402a940
Bartosz Leper <bartosz.leper@goteleport.com>
2024-06-20 19:18:32 +0200

    Add a FieldCheckbox component (#43171)
    * Add a FieldCheckbox component
    
    Also use it wherever applicable
    
    * Add licenses

--- web/packages/shared/components/FieldCheckbox/index.ts
+++ web/packages/shared/components/FieldCheckbox/index.ts
@@ -1,6 +1,6 @@
 /**
  * Teleport
- * Copyright (C) 2023  Gravitational, Inc.
+ * Copyright (C) 2024 Gravitational, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -16,9 +16,4 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-export {
-  CheckboxInput,
-  CheckboxWrapper,
-  StyledCheckbox,
-  type CheckboxSize,
-} from './Checkbox';
+export { FieldCheckbox } from './FieldCheckbox';
