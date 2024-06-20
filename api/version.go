5ecd29252a278084abdc42b5ce52e2b7c79f1ca4
Tim Ross <tim.ross@goteleport.com>
2024-06-20 16:53:47 -0400

    17.0.0-dev-tross-tbot.5

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "17.0.0-dev-tross-tbot.4"
+const Version = "17.0.0-dev-tross-tbot.5"
 
 var SemVersion = semver.New(Version)
