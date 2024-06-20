4163989f4b6b29dcf287bde43edc1c1b4c82a211
Noah Stride <noah.stride@goteleport.com>
2024-06-20 12:35:54 +0100

    Fix gRPC connections being disconnected regardless of DisconnectCertExpiry

--- lib/service/service.go
+++ lib/service/service.go
@@ -6306,6 +6306,7 @@ func (process *TeleportProcess) initSecureGRPCServer(cfg initSecureGRPCServerCfg
 
 	tlsConf := copyAndConfigureTLS(serverTLSConfig, process.log, cfg.accessPoint, clusterName)
 	creds, err := auth.NewTransportCredentials(auth.TransportCredentialsConfig{
+		Authorizer:           authorizer,
 		TransportCredentials: credentials.NewTLS(tlsConf),
 		UserGetter:           authMiddleware,
 		GetAuthPreference:    cfg.accessPoint.GetAuthPreference,
