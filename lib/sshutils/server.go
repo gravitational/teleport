c8b322a4c25785dd81601e05b52545494903eba7
Brian Joerger <bjoerger@goteleport.com>
2024-06-20 19:51:56 -0700

    Check if listener is nil before wrapping. (#43201) (#43332)

--- lib/sshutils/server.go
+++ lib/sshutils/server.go
@@ -331,7 +331,12 @@ func (s *Server) Start() error {
 			return trace.ConvertSystemError(err)
 		}
 
-		if err := s.SetListener(s.limiter.WrapListener(listener)); err != nil {
+		listener, err = s.limiter.WrapListener(listener)
+		if err != nil {
+			return trace.Wrap(err)
+		}
+
+		if err := s.SetListener(listener); err != nil {
 			return trace.Wrap(err)
 		}
 	}
