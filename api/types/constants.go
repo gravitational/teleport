91bfbdff3e4123340703561b137a95fd9600f394
Marco Dinis <marco.dinis@goteleport.com>
2024-06-24 18:24:20 +0100

    Teleport install command for discovery agents (go)
    This PR adds a new command that will install teleport on the current
    system.
    Its goal is to replace the default installer script used in Auto
    Discovery scripts.
    
    Its correctness should be compared to the current script. Improvements
    will only be considered on a 2nd phase.

--- api/types/constants.go
+++ api/types/constants.go
@@ -669,6 +669,14 @@ const (
 	// id found via automatic discovery, to avoid re-running
 	// installation commands on the node.
 	ProjectIDLabel = TeleportInternalLabelPrefix + "project-id"
+	// RegionLabel is used to identify virtual machines by region found
+	// via automatic discovery, to avoid re-running installation commands
+	// on the node.
+	RegionLabel = TeleportInternalLabelPrefix + "region"
+	// ResourceGroupLabel is used to identify virtual machines by resource-group found
+	// via automatic discovery, to avoid re-running installation commands
+	// on the node.
+	ResourceGroupLabel = TeleportInternalLabelPrefix + "resource-group"
 	// ZoneLabel is used to identify virtual machines by GCP zone
 	// found via automatic discovery, to avoid re-running installation
 	// commands on the node.
