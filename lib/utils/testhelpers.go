71f1c920391cb93959494f683da5acc37ec51f14
Andrew Burke <andrew.burke@goteleport.com>
2024-06-25 15:00:25 -0700

    Remove unused func

--- lib/utils/testhelpers.go
+++ lib/utils/testhelpers.go
@@ -17,81 +17,15 @@
 package utils
 
 import (
-	"context"
 	"crypto/rand"
 	"errors"
 	"fmt"
 	"os"
 	"os/user"
 	"testing"
-	"time"
 
-	"github.com/gravitational/trace"
 	"github.com/stretchr/testify/require"
 )
-
-// TestBackgroundTask is a task that should be run in the background for the remaining duration of a test and
-// reliably exit before the test completes.
-type TestBackgroundTask struct {
-	// Name is an identifier for the task that will be included in logs and error messages.
-	Name string
-
-	// Task is the function that will be called in a background goroutine to run the task.
-	//
-	// It must not terminate before the context is canceled, and it must reliably terminate after the context
-	// is canceled and Terminate is called.
-	//
-	// Any error other than [context.Canceled] will fail the test.
-	Task func(ctx context.Context) error
-
-	// Terminate is an optional function that will be called to terminate the task during test cleanup.
-	// It does not need to be set if the task will reliably terminate after context cancellation.
-	Terminate func() error
-}
-
-// RunTestBackgroundTask runs task.Task in the background for the remaining duration of the test.
-// During test cleanup it will cancel the context passed to the task, call task.Terminate if set, and wait for
-// the task to terminate before allowing the test to complete.
-func RunTestBackgroundTask(ctx context.Context, t *testing.T, task *TestBackgroundTask) {
-	ctx, cancel := context.WithCancel(ctx)
-	done := make(chan struct{})
-
-	go func() {
-		defer close(done)
-		err := task.Task(ctx)
-		if ctx.Err() == nil {
-			// The context hasn't been canceled yet, meaning the task has exited prematurely. This should
-			// fail the test even if the error is nil.
-			t.Errorf("Test background task %q exited prematurely with error: %s", task.Name, trace.DebugReport(err))
-			return
-		}
-		// The context has been canceled and the task has successfully exited, but any error other than
-		// context.Canceled should still fail the test.
-		if err != nil && !errors.Is(err, context.Canceled) {
-			t.Errorf("Test background task %q exited with error: %+v", task.Name, trace.DebugReport(err))
-		}
-	}()
-
-	t.Cleanup(func() {
-		t.Logf("Cleanup: terminating test background task %q.", task.Name)
-		cancel()
-		if task.Terminate != nil {
-			if err := task.Terminate(); err != nil {
-				t.Errorf("Terminating test background task %q failed with error: %s", task.Name, trace.DebugReport(err))
-			}
-		}
-		ticker := time.NewTicker(2 * time.Second)
-		defer ticker.Stop()
-		for {
-			select {
-			case <-ticker.C:
-				t.Logf("Waiting for test background task %q to terminate.", task.Name)
-			case <-done:
-				return
-			}
-		}
-	})
-}
 
 // RequireRoot skips the current test if it is not running as root.
 func RequireRoot(t *testing.T) {
