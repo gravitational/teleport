859277251f6246a5492e22e13770d381042e95e7
Paul Gottschling <paul.gottschling@goteleport.com>
2024-06-26 14:44:00 -0400

    Revert "Start a Go file"
    This reverts commit 0cc825e4197fe2f0310766087c9f5e129f88b7e2.

--- build.assets/tooling/cmd/gen-crd-docs/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func main() {
-	// TODO: Take a flag for an input directory of CRDs
-	// TODO: Take a flag for an output directory
-	// TODO: Unmarshal CRDs from the input directory
-	// TODO: Load unmarshalled data into a template
-	// TODO: Write pages to the output directory
-}
