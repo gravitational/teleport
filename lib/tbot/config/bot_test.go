5bee377b36943671a85640c7d8ee9ba590289ddd
Noah Stride <noah.stride@goteleport.com>
2024-06-28 19:22:31 +0100

    Remove unused files

--- lib/tbot/config/bot_test.go
+++ lib/tbot/config/bot_test.go
@@ -21,24 +21,12 @@ package config
 import (
 	"context"
 	"slices"
-	"testing"
-	"time"
 
 	"github.com/gravitational/trace"
-	"github.com/jonboulle/clockwork"
-	"github.com/stretchr/testify/require"
-	"golang.org/x/crypto/ssh"
 
-	"github.com/gravitational/teleport/api/client/proto"
 	"github.com/gravitational/teleport/api/client/webclient"
-	"github.com/gravitational/teleport/api/constants"
 	"github.com/gravitational/teleport/api/types"
-	"github.com/gravitational/teleport/api/utils/keys"
-	"github.com/gravitational/teleport/lib/auth/testauthority"
 	"github.com/gravitational/teleport/lib/fixtures"
-	"github.com/gravitational/teleport/lib/services"
-	"github.com/gravitational/teleport/lib/tbot/identity"
-	"github.com/gravitational/teleport/lib/tlsca"
 )
 
 const (
@@ -159,80 +147,4 @@ func (p *mockProvider) ProxyPing(ctx context.Context) (*webclient.PingResponse, 
 
 func (p *mockProvider) Config() *BotConfig {
 	return p.cfg
-}
-
-// identRequest is a function used to add additional requests to an identity in
-// getTestIdent.
-type identRequest func(id *tlsca.Identity)
-
-// getTestIdent returns a mostly-valid bot Identity without starting up an
-// entire Teleport server instance.
-func getTestIdent(t *testing.T, username string, reqs ...identRequest) *identity.Identity {
-	ca, err := tlsca.FromKeys([]byte(fixtures.TLSCACertPEM), []byte(fixtures.TLSCAKeyPEM))
-	require.NoError(t, err)
-
-	privateKey, sshPublicKey, err := testauthority.New().GenerateKeyPair()
-	require.NoError(t, err)
-
-	sshPrivateKey, err := ssh.ParseRawPrivateKey(privateKey)
-	require.NoError(t, err)
-
-	tlsPublicKeyPEM, err := tlsca.MarshalPublicKeyFromPrivateKeyPEM(sshPrivateKey)
-	require.NoError(t, err)
-
-	tlsPublicKey, err := keys.ParsePublicKey(tlsPublicKeyPEM)
-	require.NoError(t, err)
-
-	// Note: it'd be nice to make this more universally useful in our tests at
-	// some point.
-	clock := clockwork.NewFakeClock()
-	notAfter := clock.Now().Add(time.Hour)
-	id := tlsca.Identity{
-		Username:         username,
-		KubernetesUsers:  []string{"foo"},
-		KubernetesGroups: []string{"bar"},
-		RouteToCluster:   mockClusterName,
-	}
-	for _, req := range reqs {
-		req(&id)
-	}
-	subject, err := id.Subject()
-	require.NoError(t, err)
-	certBytes, err := ca.GenerateCertificate(tlsca.CertificateRequest{
-		Clock:     clock,
-		PublicKey: tlsPublicKey,
-		Subject:   subject,
-		NotAfter:  notAfter,
-	})
-	require.NoError(t, err)
-
-	caSigner, err := ssh.ParsePrivateKey([]byte(fixtures.SSHCAPrivateKey))
-	require.NoError(t, err)
-	ta := testauthority.New()
-	sshCertBytes, err := ta.GenerateUserCert(services.UserCertParams{
-		CASigner:          caSigner,
-		PublicUserKey:     sshPublicKey,
-		Username:          username,
-		CertificateFormat: constants.CertificateFormatStandard,
-		TTL:               time.Minute,
-		AllowedLogins:     []string{"foo"},
-		RouteToCluster:    mockClusterName,
-	})
-
-	require.NoError(t, err)
-
-	certs := &proto.Certs{
-		SSH:        sshCertBytes,
-		TLS:        certBytes,
-		TLSCACerts: [][]byte{[]byte(fixtures.TLSCACertPEM)},
-		SSHCACerts: [][]byte{[]byte(fixtures.SSHCAPublicKey)},
-	}
-
-	ident, err := identity.ReadIdentityFromStore(&identity.LoadIdentityParams{
-		PrivateKeyBytes: privateKey,
-		PublicKeyBytes:  tlsPublicKeyPEM,
-	}, certs)
-	require.NoError(t, err)
-
-	return ident
 }
