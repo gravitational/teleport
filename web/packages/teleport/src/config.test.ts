dbc73c271a96ac4aad47d731579a1669901d8729
Gavin Frazar <gavin.frazar@goteleport.com>
2024-07-03 16:22:21 -0700

    Add "v1" prefix to deploydatabaseservices endpoint

--- web/packages/teleport/src/config.test.ts
+++ web/packages/teleport/src/config.test.ts
@@ -27,7 +27,7 @@ test('getDeployServiceIamConfigureScriptPath formatting', async () => {
     taskRoleArn: 'task-arn',
   };
   const base =
-    'http://localhost/webapi/scripts/integrations/configure/deployservice-iam.sh?';
+    'http://localhost/v1/webapi/scripts/integrations/configure/deployservice-iam.sh?';
   const expected = `integrationName=${'int-name'}&awsRegion=${'us-east-1'}&role=${'oidc-arn'}&taskRole=${'task-arn'}`;
   expect(cfg.getDeployServiceIamConfigureScriptUrl(params)).toBe(
     `${base}${expected}`
@@ -42,7 +42,7 @@ test('getAwsOidcConfigureIdpScriptUrl formatting with s3 fields', async () => {
     s3Prefix: 's3-prefix',
   };
   const base =
-    'http://localhost/webapi/scripts/integrations/configure/awsoidc-idp.sh?';
+    'http://localhost/v1/webapi/scripts/integrations/configure/awsoidc-idp.sh?';
   const expected = `integrationName=int-name&role=role-arn&s3Bucket=s3-bucket&s3Prefix=s3-prefix`;
   expect(cfg.getAwsOidcConfigureIdpScriptUrl(params)).toBe(
     `${base}${expected}`
@@ -55,7 +55,7 @@ test('getAwsOidcConfigureIdpScriptUrl formatting, without s3 fields', async () =
     roleName: 'role-arn',
   };
   const base =
-    'http://localhost/webapi/scripts/integrations/configure/awsoidc-idp.sh?';
+    'http://localhost/v1/webapi/scripts/integrations/configure/awsoidc-idp.sh?';
   const expected = `integrationName=int-name&role=role-arn`;
   expect(cfg.getAwsOidcConfigureIdpScriptUrl(params)).toBe(
     `${base}${expected}`
