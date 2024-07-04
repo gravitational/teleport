f12ef6a7a29f8baa7fda59d7ddc22c3b4cb0d027
flyinghermit <sshah@goteleport.com>
2024-07-04 13:02:23 -0400

    send 'unspecified' preset to web ui if the value is an empty string

--- lib/web/ui/app.go
+++ lib/web/ui/app.go
@@ -155,8 +155,14 @@ func MakeApp(app types.Application, c MakeAppsConfig) App {
 
 // MakeAppTypeFromSAMLApp creates App type from SAMLIdPServiceProvider type for the WebUI.
 // Keep in sync with lib/teleterm/apiserver/handler/handler_apps.go.
+// Note: The SAMLAppPreset field is used in SAML service provider update flow in the
+// Web UI. Thus, this field is currently not available in the Connect App type.
 func MakeAppTypeFromSAMLApp(app types.SAMLIdPServiceProvider, c MakeAppsConfig) App {
 	labels := makeLabels(app.GetAllLabels())
+	samlAppPreset := "unspecified"
+	if app.GetPreset() != "" {
+		samlAppPreset = app.GetPreset()
+	}
 	resultApp := App{
 		Kind:            types.KindApp,
 		Name:            app.GetName(),
@@ -166,7 +172,7 @@ func MakeAppTypeFromSAMLApp(app types.SAMLIdPServiceProvider, c MakeAppsConfig) 
 		ClusterID:       c.AppClusterName,
 		FriendlyName:    types.FriendlyName(app),
 		SAMLApp:         true,
-		SAMLAppPreset:   app.GetPreset(),
+		SAMLAppPreset:   samlAppPreset,
 		RequiresRequest: c.RequiresRequest,
 	}
 
