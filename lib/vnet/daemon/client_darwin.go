19871a00a6fb2028915b0a0a8db3bdb8da7b1cc6
Rafał Cieślak <rafal.cieslak@goteleport.com>
2024-07-05 11:08:56 +0200

    Remove unnecessary darwin build tags

--- lib/vnet/daemon/client_darwin.go
+++ lib/vnet/daemon/client_darwin.go
@@ -14,9 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-//go:build darwin
-// +build darwin
-
 package daemon
 
 // #cgo CFLAGS: -Wall -xobjective-c -fblocks -fobjc-arc -mmacosx-version-min=10.15
