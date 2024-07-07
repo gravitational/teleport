bd8439736012bee6d96710b54ba73f4e862cf9ae
Samir Aguiar <sjorgedeaguiar@netskope.com>
2024-07-06 22:23:58 -0300

    Support double dash delimiter in tsh ssh
    This PR extends the tsh ssh command by adding support for the
    double dash (--) delimiter before remote commands (e.g.
    `tsh ssh -- echo test`), aligning its behavior with the standard
    ssh binary. This improves compatibility with tools that rely on the
    standard ssh binary behavior, such as sshuttle.
    
    Fixes #18453, #16589.
    
    Signed-off-by: Tim Ross <tim.ross@goteleport.com>

--- tool/tsh/common/tsh_test.go
+++ tool/tsh/common/tsh_test.go
@@ -2345,6 +2345,17 @@ func TestSSHCommands(t *testing.T) {
 		}
 	}, 10*time.Second, 100*time.Millisecond)
 
+	tmpHomePath := t.TempDir()
+	rootAuth := rootServer.GetAuthServer()
+
+	err = Run(ctx, []string{
+		"login",
+		"--insecure",
+		"--proxy", rootProxyAddr.String(),
+		"--user", user.Username,
+	}, setHomePath(tmpHomePath), setMockSSOLogin(rootAuth, accessUser, connector.GetName()))
+	require.NoError(t, err)
+
 	tests := []struct {
 		name      string
 		cmd       []string
@@ -2400,16 +2411,6 @@ func TestSSHCommands(t *testing.T) {
 		ctx := context.Background()
 		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
-			tmpHomePath := t.TempDir()
-			rootAuth := rootServer.GetAuthServer()
-
-			err = Run(ctx, []string{
-				"login",
-				"--insecure",
-				"--proxy", rootProxyAddr.String(),
-				"--user", user.Username,
-			}, setHomePath(tmpHomePath), setMockSSOLogin(rootAuth, accessUser, connector.GetName()))
-			require.NoError(t, err)
 
 			stdout := &output{buf: bytes.Buffer{}}
 			stderr := &output{buf: bytes.Buffer{}}
