c1f97ba7cf4c4f6843c818ad577063e8f630f3b2
Gavin Frazar <gavin.frazar@goteleport.com>
2024-07-09 16:48:39 -0700

    reenable redshift serverless e2e test (#42602) (#43973)

--- e2e/aws/redshift_test.go
+++ e2e/aws/redshift_test.go
@@ -27,7 +27,6 @@ import (
 )
 
 func testRedshiftServerless(t *testing.T) {
-	t.Skip("skipped until we fix the spacelift stack")
 	t.Parallel()
 	accessRole := mustGetEnv(t, rssAccessRoleEnv)
 	discoveryRole := mustGetEnv(t, rssDiscoveryRoleEnv)
