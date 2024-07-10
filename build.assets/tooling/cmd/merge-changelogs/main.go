7b0e04ddace02b6a6cb0cd8f31335300d30fd892
Paul Gottschling <paul.gottschling@goteleport.com>
2024-07-10 15:42:09 -0400

    First test

--- build.assets/tooling/cmd/merge-changelogs/main.go
+++ build.assets/tooling/cmd/merge-changelogs/main.go
@@ -6,8 +6,9 @@ import (
 )
 
 // merge merges changelog1 and changelog2 by combining H2-level sections, which
-// are specific to each released version.
-func merge(changelog1, changelog2 io.Reader) ([]byte, error) {
+// are specific to each released version. Returns the resulting merged document
+// along with a list of messages indicating potential issues.
+func merge(changelog1, changelog2 io.Reader) ([]byte, []string) {
 	return nil, nil
 }
 
--- build.assets/tooling/cmd/merge-changelogs/main_test.go
+++ build.assets/tooling/cmd/merge-changelogs/main_test.go
@@ -9,17 +9,49 @@ import (
 
 func Test_merge(t *testing.T) {
 	cases := []struct {
-		description string
-		changelog1  string
-		changelog2  string
-		expected    string
-	}{}
+		description      string
+		changelog1       string
+		changelog2       string
+		expected         string
+		expectedMessages []string
+	}{
+		{
+			description: "Two complete version headings",
+			changelog1: `## 1.2.3
 
+### Database access
+
+We added the ability to connect to a new database.
+
+`,
+
+			changelog2: `## 4.5.6
+
+### Application access
+
+We added the ability to connect to more web applications.
+`,
+			expected: `## 1.2.3
+
+### Database access
+
+We added the ability to connect to a new database.
+
+## 4.5.6
+
+### Application access
+
+We added the ability to connect to more web applications.
+`,
+		},
+	}
+
+	// TODO: clashing versions (combine H3s but add a warning message)
 	for _, c := range cases {
 		t.Run(c.description, func(t *testing.T) {
-			actual, err := merge(strings.NewReader(c.changelog1), strings.NewReader(c.changelog2))
-			assert.NoError(t, err)
+			actual, msg := merge(strings.NewReader(c.changelog1), strings.NewReader(c.changelog2))
 			assert.Equal(t, c.expected, actual)
+			assert.Equal(t, c.expectedMessages, msg)
 		})
 	}
 }
