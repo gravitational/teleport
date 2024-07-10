d68d028227c760ea2f6961cecfec41e9244c662d
Grzegorz Zdunek <gzdunek@users.noreply.github.com>
2024-07-10 10:37:56 +0200

    [v15] Export configured Vite plugins from `packages/build` (#43954)
    * Move `cspPlugin` to teleterm
    
    * Keep `reactPlugin` config in `build/vite`
    
    * Keep `tsconfigPathsPlugin` config in `build/vite`
    
    * Add a comment about .mjs
    
    * Lint
    
    * `resolve` -> `path.resolve`
    
    * Convert arrow function to regular function

--- web/packages/build/vite/react.mjs
+++ web/packages/build/vite/react.mjs
@@ -1,6 +1,6 @@
 /**
  * Teleport
- * Copyright (C) 2023  Gravitational, Inc.
+ * Copyright (C) 2024 Gravitational, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -16,7 +16,23 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-export function getStyledComponentsConfig(mode: string) {
+/**
+ * This needs to be an .mjs file,
+ * because Vite can't import .ts files from a workspace out of the box.
+ * https://github.com/vitejs/vite/issues/5370
+ */
+
+import react from '@vitejs/plugin-react-swc';
+
+/** @param {string} mode */
+export function reactPlugin(mode) {
+  return react({
+    plugins: [['@swc/plugin-styled-components', getStyledComponentsConfig(mode)]],
+  });
+}
+
+/** @param {string} mode */
+function getStyledComponentsConfig(mode) {
   // https://nextjs.org/docs/advanced-features/compiler#styled-components
   if (mode === 'production') {
     return {
