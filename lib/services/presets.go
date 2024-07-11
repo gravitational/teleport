674e7e3b91cd370dfae3be72c8f984d77c61943b
Tiago Silva <tiago.silva@goteleport.com>
2024-07-11 10:57:19 +0100

    [sec_scan][15] add support for edits to `AccessGraphSettings` via `tctl`
    This PR allows any cluster admin to edit `access_graph_settings` objects via `tctl`.
    
    This PR is part of https://github.com/gravitational/access-graph/issues/637.
    
    Signed-off-by: Tiago Silva <tiago.silva@goteleport.com>

--- lib/services/presets.go
+++ lib/services/presets.go
@@ -177,6 +177,7 @@ func NewPresetEditorRole() types.Role {
 					types.NewRule(types.KindAppServer, RW()),
 					types.NewRule(types.KindVnetConfig, RW()),
 					types.NewRule(types.KindBotInstance, RW()),
+					types.NewRule(types.KindAccessGraphSettings, RW()),
 				},
 			},
 		},
