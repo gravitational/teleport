eb88db12bfc5d91ba552a37dd8575b50183b0ba6
Michael <michael.myers@goteleport.com>
2024-07-15 08:28:26 -0500

    [v14] Backport logos (#43748)

--- web/packages/teleport/src/components/LogoHero/LogoHero.jsx
+++ web/packages/teleport/src/components/LogoHero/LogoHero.jsx
@@ -17,9 +17,8 @@ limitations under the License.
 import React from 'react';
 import { useTheme } from 'styled-components';
 import Image from 'design/Image';
-
-import LogoHeroLight from './LogoHeroLight.svg';
-import LogoHeroDark from './LogoHeroDark.svg';
+import LogoHeroLight from 'design/assets/images/enterprise-light.svg';
+import LogoHeroDark from 'design/assets/images/enterprise-dark.svg';
 
 const LogoHero = ({ ...rest }) => {
   const theme = useTheme();
