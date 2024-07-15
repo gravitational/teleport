8a07472dafbd253c6d93e5a933e2b9e2fd07f123
Noah Stride <noah.stride@goteleport.com>
2024-07-15 18:17:29 +0100

    Use gopsutil to determine gid/uid on unix systems

--- lib/tbot/workloadattest/unix.go
+++ lib/tbot/workloadattest/unix.go
@@ -2,6 +2,9 @@ package workloadattest
 
 import (
 	"context"
+
+	"github.com/gravitational/trace"
+	"github.com/shirou/gopsutil/v4/process"
 )
 
 type UnixAttestation struct {
@@ -15,13 +18,30 @@ type UnixAttestor struct {
 }
 
 func (a *UnixAttestor) Attest(ctx context.Context, pid int) (UnixAttestation, error) {
+	p, err := process.NewProcessWithContext(ctx, int32(pid))
+	if err != nil {
+		return UnixAttestation{}, trace.Wrap(err, "getting process")
+	}
+
+	gids, err := p.Gids()
+	if err != nil {
+		return UnixAttestation{}, trace.Wrap(err, "getting gids")
+	}
+
+	uids, err := p.Uids()
+	if err != nil {
+		return UnixAttestation{}, trace.Wrap(err, "getting uids")
+	}
+
+	// TODO: Check lengths ???
+
 	// This attestor is a little special in that we pull
 	return UnixAttestation{
 		Attested: true,
 		PID:      pid,
 		// TODO
-		UID: 0,
+		UID: int(uids[0]),
 		// TODO
-		GID: 0,
+		GID: int(gids[0]),
 	}, nil
 }
