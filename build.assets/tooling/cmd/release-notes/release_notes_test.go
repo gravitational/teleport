36c0edd2354a3b992ec737aadc0787620aaf50e1
Gus Rivera <gus.rivera@goteleport.com>
2024-07-15 15:15:18 -0500

    Adding fail testcase

--- build.assets/tooling/cmd/release-notes/release_notes_test.go
+++ build.assets/tooling/cmd/release-notes/release_notes_test.go
@@ -6,19 +6,58 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestGenerateReleaseNotes(t *testing.T) {
-	testChangelog, err := os.Open(filepath.Join("testdata", "test-changelog.md"))
-	require.NoError(t, err)
-	expectedReleaseNotes, err := os.ReadFile(filepath.Join("testdata", "expected-release-notes.md"))
-	require.NoError(t, err)
-
-	gen := releaseNotesGenerator{
-		releaseVersion: "16.0.1",
+func Test_generateReleaseNotes(t *testing.T) {
+	tests := []struct {
+		name           string
+		releaseVersion string
+		clFile         *os.File
+		want           string
+		wantErr        bool
+	}{
+		{
+			name:           "happy path",
+			releaseVersion: "16.0.1",
+			clFile:         mustOpen(t, "test-changelog.md"),
+			want:           mustRead(t, "expected-release-notes.md"),
+			wantErr:        false,
+		},
+		{
+			name:           "version mismatch",
+			releaseVersion: "15.0.1", // test-changelog has 16.0.1
+			clFile:         mustOpen(t, "test-changelog.md"),
+			want:           "",
+			wantErr:        true,
+		},
 	}
-	out, err := gen.generateReleaseNotes(testChangelog)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &releaseNotesGenerator{
+				releaseVersion: tt.releaseVersion,
+			}
+
+			got, err := r.generateReleaseNotes(tt.clFile)
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+			assert.NoError(t, err)
+			assert.Equal(t, got, tt.want)
+		})
+	}
+}
+
+func mustOpen(t *testing.T, filename string) *os.File {
+	testfile, err := os.Open(filepath.Join("testdata", filename))
 	require.NoError(t, err)
-	require.Equal(t, string(expectedReleaseNotes), out)
+	return testfile
+}
+
+func mustRead(t *testing.T, filename string) string {
+	expectedReleaseNotes, err := os.ReadFile(filepath.Join("testdata", filename))
+	require.NoError(t, err)
+	return string(expectedReleaseNotes)
 }
