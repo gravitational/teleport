8b29f96a7721d41eebd4d0d3d830b76f85df40b3
Noah Stride <noah.stride@goteleport.com>
2024-07-17 15:09:55 +0100

    Update YAML tests

--- lib/tbot/spiffe/workloadattest/kubernetes.go
+++ lib/tbot/spiffe/workloadattest/kubernetes.go
@@ -88,7 +88,7 @@ type KubernetesAttestorConfig struct {
 	// Enabled is true if the KubernetesAttestor is enabled. If false,
 	// Kubernetes attestation will not be attempted.
 	Enabled bool                `yaml:"enabled"`
-	Kubelet KubeletClientConfig `yaml:"kubelet"`
+	Kubelet KubeletClientConfig `yaml:"kubelet,omitempty"`
 }
 
 func (c *KubernetesAttestorConfig) CheckAndSetDefaults() error {
@@ -280,31 +280,31 @@ type KubeletClientConfig struct {
 	// read-only operations. This is mutually exclusive with SecurePort.
 	// This is primarily left for legacy support - since Kubernetes 1.16, the
 	// read-only port is disabled by default.
-	ReadOnlyPort int `yaml:"read_only_port"`
+	ReadOnlyPort int `yaml:"read_only_port,omitempty"`
 	// SecurePort specifies the secure port on which the Kubelet API is exposed.
 	// If unspecified, this defaults to `10250`. This is mutually exclusive
 	// with ReadOnlyPort.
-	SecurePort int `yaml:"secure_port"`
+	SecurePort int `yaml:"secure_port,omitempty"`
 
 	// TokenPath is the path to the token file used to authenticate with the
 	// Kubelet API when using the secure port.
 	// Defaults to `/var/run/secrets/kubernetes.io/serviceaccount/token`.
-	TokenPath string `yaml:"token_path"`
+	TokenPath string `yaml:"token_path,omitempty"`
 	// CAPath is the path to the CA file used to verify the certificate
 	// presented by Kubelet when using the secure port.
 	// Defaults to `/var/run/secrets/kubernetes.io/serviceaccount/ca.crt`.
-	CAPath string `yaml:"ca_path"`
+	CAPath string `yaml:"ca_path,omitempty"`
 	// SkipVerify is used to skip verification of the Kubelet's certificate when
 	// using the secure port. If set, CAPath will be ignored.
 	//
 	// This is useful in scenarios where Kubelet has not been configured with a
 	// valid certificate signed by the cluster CA. This is more common than
 	// you'd think.
-	SkipVerify bool `yaml:"skip_verify"`
+	SkipVerify bool `yaml:"skip_verify,omitempty"`
 	// Anonymous is used to indicate that no authentication should be used
 	// when connecting to the secure Kubelet API. If set, TokenPath will be
 	// ignored.
-	Anonymous bool `yaml:"anonymous"`
+	Anonymous bool `yaml:"anonymous,omitempty"`
 }
 
 func (c KubeletClientConfig) CheckAndSetDefaults() error {
