59cff0f9bfd5f88f43a9a0dd9dded05b1982cd21
Noah Stride <noah.stride@goteleport.com>
2024-07-19 11:00:51 +0100

    Machine ID: `application-tunnel` service  (#44087)
    * Implement `application-tunnel` service to replace `tbot proxy app`/`tsh` wrapping
    
    * Add basic unit testing of config
    
    * Add E2E test
    
    * Only apply fix to testenv for tbot tests
    
    * Remove redundant return

--- lib/tbot/service_application_output.go
+++ lib/tbot/service_application_output.go
@@ -119,7 +119,12 @@ func (s *ApplicationOutputService) generate(ctx context.Context) error {
 	}
 	defer impersonatedClient.Close()
 
-	routeToApp, err := getRouteToApp(ctx, s.getBotIdentity(), impersonatedClient, s.cfg)
+	routeToApp, _, err := getRouteToApp(
+		ctx,
+		s.getBotIdentity(),
+		impersonatedClient,
+		s.cfg.AppName,
+	)
 	if err != nil {
 		return trace.Wrap(err)
 	}
@@ -197,13 +202,18 @@ func (s *ApplicationOutputService) render(
 	return trace.Wrap(writeTLSCAs(ctx, s.cfg.Destination, hostCAs, userCAs, databaseCAs))
 }
 
-func getRouteToApp(ctx context.Context, botIdentity *identity.Identity, client *authclient.Client, output *config.ApplicationOutput) (proto.RouteToApp, error) {
+func getRouteToApp(
+	ctx context.Context,
+	botIdentity *identity.Identity,
+	client *authclient.Client,
+	appName string,
+) (proto.RouteToApp, types.Application, error) {
 	ctx, span := tracer.Start(ctx, "getRouteToApp")
 	defer span.End()
 
-	app, err := getApp(ctx, client, output.AppName)
+	app, err := getApp(ctx, client, appName)
 	if err != nil {
-		return proto.RouteToApp{}, trace.Wrap(err)
+		return proto.RouteToApp{}, nil, trace.Wrap(err)
 	}
 
 	// TODO: AWS?
@@ -213,20 +223,21 @@ func getRouteToApp(ctx context.Context, botIdentity *identity.Identity, client *
 		PublicAddr:  app.GetPublicAddr(),
 	})
 	if err != nil {
-		return proto.RouteToApp{}, trace.Wrap(err)
+		return proto.RouteToApp{}, nil, trace.Wrap(err)
 	}
 
 	err = authclient.WaitForAppSession(ctx, ws.GetName(), ws.GetUser(), client)
 	if err != nil {
-		return proto.RouteToApp{}, trace.Wrap(err)
+		return proto.RouteToApp{}, nil, trace.Wrap(err)
 	}
 
-	return proto.RouteToApp{
+	routeToApp := proto.RouteToApp{
 		Name:        app.GetName(),
 		SessionID:   ws.GetName(),
 		PublicAddr:  app.GetPublicAddr(),
 		ClusterName: botIdentity.ClusterName,
-	}, nil
+	}
+	return routeToApp, app, nil
 }
 
 func getApp(ctx context.Context, clt *authclient.Client, appName string) (types.Application, error) {
