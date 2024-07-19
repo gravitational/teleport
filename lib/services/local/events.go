279c7dd8e61c24f12b6906ee8cc09f86a16d49e0
Forrest <30576607+fspmarshall@users.noreply.github.com>
2024-07-19 13:34:57 -0700

    instance event plumbing (#43776) (#44289)

--- lib/services/local/events.go
+++ lib/services/local/events.go
@@ -38,6 +38,7 @@ import (
 	"github.com/gravitational/teleport/lib/backend"
 	"github.com/gravitational/teleport/lib/defaults"
 	"github.com/gravitational/teleport/lib/services"
+	"github.com/gravitational/teleport/lib/services/local/generic"
 )
 
 // EventsService implements service to watch for events
@@ -208,6 +209,8 @@ func (e *EventsService) NewWatcher(ctx context.Context, watch types.Watch) (type
 			parser = newGlobalNotificationParser()
 		case types.KindAccessMonitoringRule:
 			parser = newAccessMonitoringRuleParser()
+		case types.KindInstance:
+			parser = newInstanceParser()
 		default:
 			if watch.AllowPartialSuccess {
 				continue
@@ -2102,6 +2105,31 @@ func (p *globalNotificationParser) parse(event backend.Event) (types.Resource, e
 			return nil, trace.Wrap(err)
 		}
 		return types.Resource153ToLegacy(globalNotification), nil
+	default:
+		return nil, trace.BadParameter("event %v is not supported", event.Type)
+	}
+}
+
+func newInstanceParser() *instanceParser {
+	return &instanceParser{
+		baseParser: newBaseParser(backend.Key(instancePrefix)),
+	}
+}
+
+type instanceParser struct {
+	baseParser
+}
+
+func (p *instanceParser) parse(event backend.Event) (types.Resource, error) {
+	switch event.Type {
+	case types.OpDelete:
+		return resourceHeader(event, types.KindInstance, types.V1, 0)
+	case types.OpPut:
+		instance, err := generic.FastUnmarshal[*types.InstanceV1](event.Item)
+		if err != nil {
+			return nil, trace.Wrap(err)
+		}
+		return instance, nil
 	default:
 		return nil, trace.BadParameter("event %v is not supported", event.Type)
 	}
