f6599c9dc1216025160599d57b75590000056984
Fred Heinecke <fred.heinecke@goteleport.com>
2024-07-22 10:10:59 -0500

    17.0.0-dev.fred-stage-sign.2

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "17.0.0-dev.fred-stage-sign.1"
+const Version = "17.0.0-dev.fred-stage-sign.2"
 
 var SemVersion = semver.New(Version)
