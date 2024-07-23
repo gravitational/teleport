2d5820c69dc0ca355762720d1d692313a62a8f92
Edward Dowling <edward.dowling@goteleport.com>
2024-07-23 16:59:44 +0100

    Use seperate context for jira webhook retry

--- integrations/access/jira/app.go
+++ integrations/access/jira/app.go
@@ -290,6 +290,9 @@ func (a *App) onWatcherEvent(ctx context.Context, event types.Event) error {
 
 // onJiraWebhook processes Jira webhook and updates the status of an issue
 func (a *App) onJiraWebhook(ctx context.Context, webhook Webhook) error {
+	ctx, cancel := context.WithTimeout(ctx, webhookIssueAPIRetryTimeout)
+	defer cancel()
+
 	webhookEvent := webhook.WebhookEvent
 	issueEventTypeName := webhook.IssueEventTypeName
 	if webhookEvent != "jira:issue_updated" || issueEventTypeName != "issue_generic" {
