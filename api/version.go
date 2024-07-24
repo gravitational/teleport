3a5d8cc20e3839696c8b42af82c888ca12812b37
Edward Dowling <edward.dowling@goteleport.com>
2024-07-24 17:03:30 +0100

    Release v15.4.10-dev.edwarddowling.3

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "15.4.10-dev.edwarddowling.2"
+const Version = "15.4.10-dev.edwarddowling.3"
 
 var SemVersion = semver.New(Version)
