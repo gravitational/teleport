863bb8700abd197e8d46908aeff0569a8152834d
Rafał Cieślak <rafal.cieslak@goteleport.com>
2024-07-26 16:09:34 +0200

    Update version to 16.1.1-dev.ravicious.3
    The one with launch daemon for VNet with code sign requirement and
    feature flag turned on by default.

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "16.1.1-dev.ravicious.2"
+const Version = "16.1.1-dev.ravicious.3"
 
 var SemVersion = semver.New(Version)
