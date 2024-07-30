5472633f0e28951b492d3ed61e30977cd7bb3feb
Forrest <30576607+fspmarshall@users.noreply.github.com>
2024-07-29 17:24:21 -0700

    Revert "fix access request cache test race (#44653)" (#44787)
    This reverts commit a422be4a91d6727c1ddc942fd40d6bbeaea72097.

--- lib/services/access_request_cache.go
+++ lib/services/access_request_cache.go
@@ -352,7 +352,6 @@ func (c *AccessRequestCache) getResourcesAndUpdateCurrent(ctx context.Context) e
 	c.rw.Lock()
 	defer c.rw.Unlock()
 	c.primaryCache = cache
-	close(c.initC)
 	return nil
 }
 
@@ -403,12 +402,6 @@ func (c *AccessRequestCache) initializationChan() <-chan struct{} {
 	c.rw.RLock()
 	defer c.rw.RUnlock()
 	return c.initC
-}
-
-// InitializationChan is part of the resourceCollector interface and gets the channel
-// used to signal that the accessRequestCache has been initialized.
-func (c *AccessRequestCache) InitializationChan() <-chan struct{} {
-	return c.initializationChan()
 }
 
 // Close terminates the background process that keeps the access request cache up to
