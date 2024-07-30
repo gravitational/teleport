769c43543277753d56ec95ecc277ab57171cb0b9
Tim Buckley <tim@goteleport.com>
2024-07-29 18:20:41 -0600

    Machine ID: Validate generation counter using bot instances (#44583)
    * Show bot instance ID in tbot log output
    
    This tweaks the "fetched new bot identity" message to show the bot
    name and instance ID as embedded in the bot's certificate.
    
    Example:
    
    ```
    2024-07-23T15:51:20-06:00 INFO [TBOT:IDEN] Fetched new bot identity identity:tpm-test, id=5a2865d3-d3dc-4eaa-853c-5377a1fe83f6 | valid: after=2024-07-23T21:50:20Z, before=2024-07-23T21:56:19Z, duration=5m59s | kind=tls, renewable=false, disallow-reissue=false, roles=[bot-tpm-test], principals=[-teleport-internal-join], generation=0 tbot/service_bot_identity.go:223
    ```
    
    * Machine ID: Validate generation counter using bot instances
    
    This change moves the generation counter from bot user labels to a
    field in `BotInstanceStatusAuthentication`. This allows for tracking
    of individual generations regardless of join method, allows for
    multiple `token`-type joins per bot, and opens the door for multi-use
    `token`-type tokens in the future.
    
    For now, the new behavior remains behind the `BOT_INSTANCE_EXPERIMENT`
    feature flag, and generation counter handling is largely unchanged
    when it is unset.
    
    * Update legacy generation counter for downgrade compatibility
    
    This updates the legacy user label generation counter to support some
    downgrade compatibility if a user either disables the bot instance
    experiment or downgrades to a Teleport version predating it.
    
    Also includes some logger cleanup, and adds a warning when a
    generation counter mismatch is detected but not enforced.
    
    * Set initial generation value for legacy mode
    
    `validateGenerationLabel` expects a nonzero generation counter value
    for renewable certs to populate the counter, so this provides it
    again.
    
    * Add new generation counter tests
    
    This adds various new tests to validate new per-instance generation
    counter behavior.
    
    Also includes backcompat fixes to import old generation counters, and
    a small fix to ensure downgrades don't leave bots in a bad state.
    
    * Fix imports
    
    * Tweak TODO message

--- lib/auth/join.go
+++ lib/auth/join.go
@@ -377,9 +377,6 @@ func (a *Server) generateCertsBot(
 		// TODO(nklaassen): consider logging the SSH public key as well, for now
 		// the SSH and TLS public keys are still identical for tbot.
 		PublicKey: req.PublicTLSKey,
-
-		// Note: Generation will be set during `generateInitialBotCerts()` as
-		// needed.
 	}
 
 	if joinAttributeSrc != nil {
@@ -409,6 +406,7 @@ func (a *Server) generateCertsBot(
 		renewable,
 		auth,
 		req.BotInstanceID,
+		req.BotGeneration,
 	)
 	if err != nil {
 		return nil, trace.Wrap(err)
