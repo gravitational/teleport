3921bb155fa96443181cd8ed370f7cd525b3f2cd
Zac Bergquist <zac.bergquist@goteleport.com>
2024-07-30 20:57:45 -0600

    Remove open source licenses (#4793)
    Teleport Enterprise is only available under commercial license
    agreements. Its source code is not governed by open source licenses
    like Apache 2 or AGPLv3.
    
    Source-commit: gravitational/teleport.e@d8e17bae926cbe5ebe374f69371b7811f1f9e987
    Source-pull-requests: gravitational/teleport.e#4793

--- e/tooling/plugins/cmd/with-secrets/main.go
+++ e/tooling/plugins/cmd/with-secrets/main.go
@@ -1,22 +1,6 @@
-/*
-Copyright 2022 Gravitational, Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-// Emulates the behaviour of sops --exec-env, in that it takes the decrypted
-// yaml output from `sops -d $drone-secrets-file` and runs the supplied program
-// with the secrets injected its environment.
+// Command with-secrets emulates the behaviour of sops --exec-env, in that it
+// takes the decrypted YAML output from `sops -d $drone-secrets-file` and runs
+// the supplied program with the secrets injected its environment.
 //
 // For some reason (either the way we've structured the yaml in the drone
 // secrets files, the use use of multi-line secrets, or both), `sops --exec-env`
@@ -24,9 +8,10 @@ limitations under the License.
 // behaviour
 //
 // For example:
-//  $ sops -d encrypted-secrets.yaml | with-secrets ./do-the-thing arg1 arg2
+//
+//	$ sops -d encrypted-secrets.yaml | with-secrets ./do-the-thing arg1 arg2
+//
 // ... so that the decrypted secrets are never written to disk.
-
 package main
 
 import (
