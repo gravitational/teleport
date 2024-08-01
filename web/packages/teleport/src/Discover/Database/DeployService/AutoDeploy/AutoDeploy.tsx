acaed9998cda1258d11c6bd95e5c54fd8bae24cc
Lisa Kim <lisa@goteleport.com>
2024-08-01 14:53:36 -0700

    Add discovery label account ID when deploying services (#44856) (#44911)
    * Add discovery label account ID when deploying services
    
    * Fix acronyms

--- web/packages/teleport/src/Discover/Database/DeployService/AutoDeploy/AutoDeploy.tsx
+++ web/packages/teleport/src/Discover/Database/DeployService/AutoDeploy/AutoDeploy.tsx
@@ -38,6 +38,7 @@ import {
   DiscoverServiceDeployType,
 } from 'teleport/services/userEvent';
 import cfg from 'teleport/config';
+import { splitAwsIamArn } from 'teleport/services/integrations/aws';
 
 import {
   ActionButtons,
@@ -99,9 +100,13 @@ export function AutoDeploy({ toggleDeployMethod }: DeployServiceProp) {
         dbMeta.autoDiscovery.requiredVpcsAndSubnets;
       const vpcIds = Object.keys(requiredVpcsAndSubnets);
 
+      const { awsAccountId } = splitAwsIamArn(
+        agentMeta.awsIntegration.spec.roleArn
+      );
       integrationService
         .deployDatabaseServices(integrationName, {
           region: dbMeta.awsRegion,
+          accountId: awsAccountId,
           taskRoleArn,
           deployments: vpcIds.map(vpcId => ({
             vpcId,
