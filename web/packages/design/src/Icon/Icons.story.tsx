b3433e33c12deda4b2fc5d9832c5491b706a781b
Michael Myers <michael.myers@goteleport.com>
2024-08-01 16:25:26 -0500

    Add Device Trust Requirement icon to web UI
    This PR expands upon the previous Device Trust topbar icon by adding a
    new icon to signal to users that device trust is required if they did
    not authorize the session

--- web/packages/design/src/Icon/Icons.story.tsx
+++ web/packages/design/src/Icon/Icons.story.tsx
@@ -183,6 +183,7 @@ export const Icons = () => (
     <IconBox IconCmpt={Icon.Server} text="Server" />
     <IconBox IconCmpt={Icon.Share} text="Share" />
     <IconBox IconCmpt={Icon.ShieldCheck} text="ShieldCheck" />
+    <IconBox IconCmpt={Icon.ShieldWarning} text="ShieldWarning" />
     <IconBox IconCmpt={Icon.Sliders} text="Sliders" />
     <IconBox IconCmpt={Icon.SlidersVertical} text="SlidersVertical" />
     <IconBox IconCmpt={Icon.Speed} text="Speed" />
