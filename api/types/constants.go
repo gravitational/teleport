83690dc57eb935ed7f3a9a29cac48c24dc2be885
Noah Stride <noah.stride@goteleport.com>
2024-08-01 15:09:50 +0100

    Start wiring SPIFFEFederations into cache

--- api/types/constants.go
+++ api/types/constants.go
@@ -66,7 +66,8 @@ const (
 	// KindBot is a Machine ID bot resource
 	KindBot = "bot"
 	// KindBotInstance is an instance of a Machine ID bot
-	KindBotInstance      = "bot_instance"
+	KindBotInstance = "bot_instance"
+	// KindSPIFFEFederation is a SPIFFE federation resource
 	KindSPIFFEFederation = "spiffe_federation"
 
 	// KindHostCert is a host certificate
--- lib/cache/cache.go
+++ lib/cache/cache.go
@@ -182,6 +182,7 @@ func ForAuth(cfg Config) Config {
 		{Kind: types.KindAccessMonitoringRule},
 		{Kind: types.KindDatabaseObject},
 		{Kind: types.KindAccessGraphSettings},
+		{Kind: types.KindSPIFFEFederation},
 	}
 	cfg.QueueSize = defaults.AuthQueueSize
 	// We don't want to enable partial health for auth cache because auth uses an event stream
