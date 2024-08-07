gravitational/teleport

commit f81d6018ddf04a00757e7e74267a1dee83573542

Update host user groups for existing users (#41919) (#42884)
This change fixes a bug in host user creation where Teleport would not
update the groups of a returning user if groups were changed in
the user's role.

lib/srv/sess.go
@@ -283,7 +283,7 @@ func (s *SessionRegistry) TryCreateHostUser(ctx *ServerContext) error {
 	if trace.IsAccessDenied(err) && existsErr != nil {
 		return trace.WrapWithMessage(err, "Insufficient permission for host user creation")
 	}
-	userCloser, err := s.users.CreateUser(ctx.Identity.Login, ui)
+	userCloser, err := s.users.UpsertUser(ctx.Identity.Login, ui)
 	if userCloser != nil {
 		ctx.AddCloser(userCloser)
 	}

lib/srv/usermgmt.go
@@ -96,6 +96,8 @@ type HostUsersBackend interface {
 	LookupGroup(group string) (*user.Group, error)
 	// LookupGroupByID retrieves a group by its ID.
 	LookupGroupByID(gid string) (*user.Group, error)
+	// SetUserGroups sets a user's groups, replacing their existing groups.
+	SetUserGroups(name string, groups []string) error
 	// CreateGroup creates a group on a host.
 	CreateGroup(group string, gid string) error
 	// CreateUser creates a user on a host.
@@ -145,8 +147,8 @@ func (*HostSudoersNotImplemented) RemoveSudoers(name string) error {
 }
 
 type HostUsers interface {
-	// CreateUser creates a temporary Teleport user in the TeleportServiceGroup
-	CreateUser(name string, hostRoleInfo *services.HostUsersInfo) (io.Closer, error)
+	// UpsertUser creates a temporary Teleport user in the TeleportServiceGroup
+	UpsertUser(name string, hostRoleInfo *services.HostUsersInfo) (io.Closer, error)
 	// DeleteUser deletes a temporary Teleport user only if they are
 	// in a specified group
 	DeleteUser(name string, gid string) error
@@ -221,58 +223,10 @@ func (u *HostSudoersManagement) RemoveSudoers(name string) error {
 	return nil
 }
 
-// CreateUser creates a temporary Teleport user in the TeleportServiceGroup
-func (u *HostUserManagement) CreateUser(name string, ui *services.HostUsersInfo) (io.Closer, error) {
+// UpsertUser creates a temporary Teleport user in the TeleportServiceGroup
+func (u *HostUserManagement) UpsertUser(name string, ui *services.HostUsersInfo) (io.Closer, error) {
 	if ui.Mode == types.CreateHostUserMode_HOST_USER_MODE_UNSPECIFIED {
 		return nil, trace.BadParameter("Mode is a required argument to CreateUser")
-	}
-
-	tempUser, err := u.backend.Lookup(name)
-	if err != nil && err != user.UnknownUserError(name) {
-		return nil, trace.Wrap(err)
-	}
-
-	if tempUser != nil {
-		gids, err := u.backend.UserGIDs(tempUser)
-		if err != nil {
-			return nil, trace.Wrap(err)
-		}
-		systemGroup, err := u.backend.LookupGroup(types.TeleportServiceGroup)
-		if err != nil {
-			if isUnknownGroupError(err, types.TeleportServiceGroup) {
-				return nil, trace.AlreadyExists("User %q already exists, however no users are currently managed by teleport", name)
-			}
-			return nil, trace.Wrap(err)
-		}
-		var found bool
-		for _, gid := range gids {
-			if gid == systemGroup.Gid {
-				found = true
-				break
-			}
-		}
-		if !found {
-			return nil, trace.AlreadyExists("User %q already exists and is not managed by teleport", name)
-		}
-
-		err = u.doWithUserLock(func(_ types.SemaphoreLease) error {
-			if err := u.storage.UpsertHostUserInteractionTime(u.ctx, name, time.Now()); err != nil {
-				return trace.Wrap(err)
-			}
-			return nil
-		})
-		if err != nil {
-			return nil, trace.Wrap(err)
-		}
-		// try to delete even if the user already exists as only users
-		// in the teleport-system group will be deleted and this way
-		// if a user creates multiple sessions the account will
-		// succeed in deletion
-		return &userCloser{
-			username: name,
-			users:    u,
-			backend:  u.backend,
-		}, trace.AlreadyExists("User %q already exists", name)
 	}
 
 	groups := make([]string, 0, len(ui.Groups))
@@ -298,6 +252,71 @@ func (u *HostUserManagement) CreateUser(name string, ui *services.HostUsersInfo)
 		return nil, trace.WrapWithMessage(err, "error while creating groups")
 	}
 
+	tempUser, err := u.backend.Lookup(name)
+	if err != nil && !errors.Is(err, user.UnknownUserError(name)) {
+		return nil, trace.Wrap(err)
+	}
+
+	if tempUser != nil {
+		// Collect actions that need to be done together under a lock on the user.
+		actionsUnderLock := []func() error{
+			func() error {
+				// If the user exists, set user groups again as they might have changed.
+				return trace.Wrap(u.backend.SetUserGroups(name, groups))
+			},
+		}
+		doWithUserLock := func() error {
+			return trace.Wrap(u.doWithUserLock(func(_ types.SemaphoreLease) error {
+				for _, action := range actionsUnderLock {
+					if err := action(); err != nil {
+						return trace.Wrap(err)
+					}
+				}
+				return nil
+			}))
+		}
+
+		systemGroup, err := u.backend.LookupGroup(types.TeleportServiceGroup)
+		if err != nil {
+			if isUnknownGroupError(err, types.TeleportServiceGroup) {
+				// Teleport service group doesn't exist, so we don't need to update interaction time.
+				return nil, trace.Wrap(doWithUserLock())
+			}
+			return nil, trace.Wrap(err)
+		}
+		gids, err := u.backend.UserGIDs(tempUser)
+		if err != nil {
+			return nil, trace.Wrap(err)
+		}
+		var found bool
+		for _, gid := range gids {
+			if gid == systemGroup.Gid {
+				found = true
+				break
+			}
+		}
+		if !found {
+			// User isn't managed by Teleport, so we don't need to update interaction time.
+			return nil, trace.Wrap(doWithUserLock())
+		}
+
+		actionsUnderLock = append(actionsUnderLock, func() error {
+			return trace.Wrap(u.storage.UpsertHostUserInteractionTime(u.ctx, name, time.Now()))
+		})
+		if err := doWithUserLock(); err != nil {
+			return nil, trace.Wrap(err)
+		}
+		// try to delete even if the user already exists as only users
+		// in the teleport-system group will be deleted and this way
+		// if a user creates multiple sessions the account will
+		// succeed in deletion
+		return &userCloser{
+			username: name,
+			users:    u,
+			backend:  u.backend,
+		}, nil
+	}
+
 	var home string
 	if ui.Mode != types.CreateHostUserMode_HOST_USER_MODE_INSECURE_DROP {
 		home, err = readDefaultHome(name)
@@ -307,8 +326,10 @@ func (u *HostUserManagement) CreateUser(name string, ui *services.HostUsersInfo)
 	}
 
 	err = u.doWithUserLock(func(_ types.SemaphoreLease) error {
-		if err := u.storage.UpsertHostUserInteractionTime(u.ctx, name, time.Now()); err != nil {
-			return trace.Wrap(err)
+		if ui.Mode != types.CreateHostUserMode_HOST_USER_MODE_KEEP {
+			if err := u.storage.UpsertHostUserInteractionTime(u.ctx, name, time.Now()); err != nil {
+				return trace.Wrap(err)
+			}
 		}
 		if ui.GID != "" {
 			// if gid is specified a group must already exist
@@ -341,7 +362,7 @@ func (u *HostUserManagement) CreateUser(name string, ui *services.HostUsersInfo)
 	}
 
 	if ui.Mode == types.CreateHostUserMode_HOST_USER_MODE_KEEP {
-		return nil, trace.Wrap(err)
+		return nil, nil
 	}
 
 	closer := &userCloser{

integration/hostuser_test.go
@@ -28,6 +28,7 @@ import (
 	"os/exec"
 	"os/user"
 	"path/filepath"
+	"slices"
 	"testing"
 
 	"github.com/gravitational/trace"
@@ -180,7 +181,7 @@ func TestRootHostUsersBackend(t *testing.T) {
 	})
 }
 
-func requireUserInGroups(t *testing.T, u *user.User, requiredGroups []string) {
+func getUserGroups(t *testing.T, u *user.User) []string {
 	var userGroups []string
 	userGids, err := u.GroupIds()
 	require.NoError(t, err)
@@ -189,7 +190,11 @@ func requireUserInGroups(t *testing.T, u *user.User, requiredGroups []string) {
 		require.NoError(t, err)
 		userGroups = append(userGroups, group.Name)
 	}
-	require.Subset(t, userGroups, requiredGroups)
+	return userGroups
+}
+
+func requireUserInGroups(t *testing.T, u *user.User, requiredGroups []string) {
+	require.Subset(t, getUserGroups(t, u), requiredGroups)
 }
 
 func cleanupUsersAndGroups(users []string, groups []string) func() {
@@ -219,7 +224,7 @@ func TestRootHostUsers(t *testing.T) {
 		users := srv.NewHostUsers(context.Background(), presence, "host_uuid")
 
 		testGroups := []string{"group1", "group2"}
-		closer, err := users.CreateUser(testuser, &services.HostUsersInfo{Groups: testGroups, Mode: types.CreateHostUserMode_HOST_USER_MODE_INSECURE_DROP})
+		closer, err := users.UpsertUser(testuser, &services.HostUsersInfo{Groups: testGroups, Mode: types.CreateHostUserMode_HOST_USER_MODE_INSECURE_DROP})
 		require.NoError(t, err)
 
 		testGroups = append(testGroups, types.TeleportServiceGroup)
@@ -244,7 +249,7 @@ func TestRootHostUsers(t *testing.T) {
 		_, err := user.LookupGroupId(testGID)
 		require.ErrorIs(t, err, user.UnknownGroupIdError(testGID))
 
-		closer, err := users.CreateUser(testuser, &services.HostUsersInfo{
+		closer, err := users.UpsertUser(testuser, &services.HostUsersInfo{
 			Mode: types.CreateHostUserMode_HOST_USER_MODE_INSECURE_DROP,
 			UID:  testUID,
 			GID:  testGID,
@@ -273,7 +278,7 @@ func TestRootHostUsers(t *testing.T) {
 		expectedHome := filepath.Join("/home", testuser)
 		require.NoDirExists(t, expectedHome)
 
-		closer, err := users.CreateUser(testuser, &services.HostUsersInfo{Mode: types.CreateHostUserMode_HOST_USER_MODE_KEEP})
+		closer, err := users.UpsertUser(testuser, &services.HostUsersInfo{Mode: types.CreateHostUserMode_HOST_USER_MODE_KEEP})
 		require.NoError(t, err)
 		require.Nil(t, closer)
 		t.Cleanup(cleanupUsersAndGroups([]string{testuser}, nil))
@@ -303,7 +308,7 @@ func TestRootHostUsers(t *testing.T) {
 			os.Remove(sudoersPath(testuser, uuid))
 			host.UserDel(testuser)
 		})
-		closer, err := users.CreateUser(testuser,
+		closer, err := users.UpsertUser(testuser,
 			&services.HostUsersInfo{
 				Mode: types.CreateHostUserMode_HOST_USER_MODE_INSECURE_DROP,
 			})
@@ -334,12 +339,12 @@ func TestRootHostUsers(t *testing.T) {
 
 		deleteableUsers := []string{"teleport-user1", "teleport-user2", "teleport-user3"}
 		for _, user := range deleteableUsers {
-			_, err := users.CreateUser(user, &services.HostUsersInfo{Mode: types.CreateHostUserMode_HOST_USER_MODE_INSECURE_DROP})
+			_, err := users.UpsertUser(user, &services.HostUsersInfo{Mode: types.CreateHostUserMode_HOST_USER_MODE_INSECURE_DROP})
 			require.NoError(t, err)
 		}
 
 		// this user should not be in the service group as it was created with mode keep.
-		closer, err := users.CreateUser("teleport-user4", &services.HostUsersInfo{
+		closer, err := users.UpsertUser("teleport-user4", &services.HostUsersInfo{
 			Mode: types.CreateHostUserMode_HOST_USER_MODE_KEEP,
 		})
 		require.NoError(t, err)
@@ -358,6 +363,71 @@ func TestRootHostUsers(t *testing.T) {
 		for _, us := range deleteableUsers {
 			_, err := user.Lookup(us)
 			require.Equal(t, err, user.UnknownUserError(us))
+		}
+	})
+
+	t.Run("test update changed groups", func(t *testing.T) {
+		tests := []struct {
+			name         string
+			firstGroups  []string
+			secondGroups []string
+		}{
+			{
+				name:         "add groups",
+				secondGroups: []string{"group1", "group2"},
+			},
+			{
+				name:        "delete groups",
+				firstGroups: []string{"group1", "group2"},
+			},
+			{
+				name:         "change groups",
+				firstGroups:  []string{"group1", "group2"},
+				secondGroups: []string{"group2", "group3"},
+			},
+			{
+				name:         "no change",
+				firstGroups:  []string{"group1", "group2"},
+				secondGroups: []string{"group2", "group1"},
+			},
+		}
+
+		for _, tc := range tests {
+			t.Run(tc.name, func(t *testing.T) {
+				allGroups := make([]string, 0, len(tc.firstGroups)+len(tc.secondGroups))
+				allGroups = append(allGroups, tc.firstGroups...)
+				allGroups = append(allGroups, tc.secondGroups...)
+				t.Cleanup(cleanupUsersAndGroups([]string{testuser}, allGroups))
+
+				// Verify that the user is created with the first set of groups.
+				users := srv.NewHostUsers(context.Background(), presence, "host_uuid")
+				_, err := users.UpsertUser(testuser, &services.HostUsersInfo{
+					Groups: tc.firstGroups,
+					Mode:   types.CreateHostUserMode_HOST_USER_MODE_KEEP,
+				})
+				require.NoError(t, err)
+				u, err := user.Lookup(testuser)
+				require.NoError(t, err)
+				requireUserInGroups(t, u, tc.firstGroups)
+
+				// Verify that the user is updated with the second set of groups.
+				_, err = users.UpsertUser(testuser, &services.HostUsersInfo{
+					Groups: tc.secondGroups,
+					Mode:   types.CreateHostUserMode_HOST_USER_MODE_KEEP,
+				})
+				require.NoError(t, err)
+				u, err = user.Lookup(testuser)
+				require.NoError(t, err)
+				requireUserInGroups(t, u, tc.secondGroups)
+
+				// Verify that the appropriate groups form the first set were deleted.
+				userGroups := getUserGroups(t, u)
+				for _, group := range tc.firstGroups {
+					if !slices.Contains(tc.secondGroups, group) {
+						require.NotContains(t, userGroups, group)
+					}
+				}
+			})
 		}
 	})
 }

lib/srv/usermgmt_linux.go
@@ -79,6 +79,12 @@ func (*HostUsersProvisioningBackend) LookupGroupByID(gid string) (*user.Group, e
 	return user.LookupGroupId(gid)
 }
 
+// SetUserGroups sets a user's groups, replacing their existing groups.
+func (*HostUsersProvisioningBackend) SetUserGroups(name string, groups []string) error {
+	_, err := host.SetUserGroups(name, groups)
+	return trace.Wrap(err)
+}
+
 // GetAllUsers returns a full list of users present on a system
 func (*HostUsersProvisioningBackend) GetAllUsers() ([]string, error) {
 	users, _, err := host.GetAllUsers()

lib/srv/usermgmt_test.go
@@ -89,6 +89,14 @@ func (tm *testHostUserBackend) LookupGroupByID(gid string) (*user.Group, error) 
 	}, nil
 }
 
+func (tm *testHostUserBackend) SetUserGroups(name string, groups []string) error {
+	if _, ok := tm.users[name]; !ok {
+		return trace.NotFound("User %q doesn't exist", name)
+	}
+	tm.users[name] = groups
+	return nil
+}
+
 func (tm *testHostUserBackend) UserGIDs(u *user.User) ([]string, error) {
 	ids := make([]string, 0, len(tm.users[u.Username]))
 	for _, id := range tm.users[u.Username] {
@@ -175,7 +183,7 @@ func TestUserMgmt_CreateTemporaryUser(t *testing.T) {
 		Mode:   types.CreateHostUserMode_HOST_USER_MODE_INSECURE_DROP,
 	}
 	// create a user with some groups
-	closer, err := users.CreateUser("bob", userinfo)
+	closer, err := users.UpsertUser("bob", userinfo)
 	require.NoError(t, err)
 	require.NotNil(t, closer, "user closer was nil")
 
@@ -185,8 +193,8 @@ func TestUserMgmt_CreateTemporaryUser(t *testing.T) {
 	}, backend.users["bob"])
 
 	// try creat the same user again
-	secondCloser, err := users.CreateUser("bob", userinfo)
-	require.True(t, trace.IsAlreadyExists(err))
+	secondCloser, err := users.UpsertUser("bob", userinfo)
+	require.NoError(t, err)
 	require.NotNil(t, secondCloser)
 
 	// Close will remove the user if the user is in the teleport-system group
@@ -197,8 +205,8 @@ func TestUserMgmt_CreateTemporaryUser(t *testing.T) {
 	backend.CreateUser("simon", []string{}, "", "", "")
 
 	// try to create a temporary user for simon
-	closer, err = users.CreateUser("simon", userinfo)
-	require.True(t, trace.IsAlreadyExists(err))
+	closer, err = users.UpsertUser("simon", userinfo)
+	require.NoError(t, err)
 	require.Nil(t, closer)
 }
 
@@ -217,7 +225,7 @@ func TestUserMgmtSudoers_CreateTemporaryUser(t *testing.T) {
 		backend: backend,
 	}
 
-	closer, err := users.CreateUser("bob", &services.HostUsersInfo{
+	closer, err := users.UpsertUser("bob", &services.HostUsersInfo{
 		Groups: []string{"hello", "sudo"},
 		Mode:   types.CreateHostUserMode_HOST_USER_MODE_INSECURE_DROP,
 	})
@@ -241,16 +249,15 @@ func TestUserMgmtSudoers_CreateTemporaryUser(t *testing.T) {
 		// test user already exists but teleport-service group has not yet
 		// been created
 		backend.CreateUser("testuser", nil, "", "", "")
-		_, err := users.CreateUser("testuser", &services.HostUsersInfo{
+		_, err := users.UpsertUser("testuser", &services.HostUsersInfo{
 			Mode: types.CreateHostUserMode_HOST_USER_MODE_INSECURE_DROP,
 		})
-		require.True(t, trace.IsAlreadyExists(err))
+		require.NoError(t, err)
 		backend.CreateGroup(types.TeleportServiceGroup, "")
-		// IsAlreadyExists error when teleport-service group now exists
-		_, err = users.CreateUser("testuser", &services.HostUsersInfo{
+		_, err = users.UpsertUser("testuser", &services.HostUsersInfo{
 			Mode: types.CreateHostUserMode_HOST_USER_MODE_INSECURE_DROP,
 		})
-		require.True(t, trace.IsAlreadyExists(err))
+		require.NoError(t, err)
 	})
 }
 
@@ -286,7 +293,7 @@ func TestUserMgmt_DeleteAllTeleportSystemUsers(t *testing.T) {
 			mgmt.CreateGroup(group, "")
 		}
 		if slices.Contains(user.groups, types.TeleportServiceGroup) {
-			users.CreateUser(user.user, &services.HostUsersInfo{Groups: user.groups})
+			users.UpsertUser(user.user, &services.HostUsersInfo{Groups: user.groups})
 		} else {
 			mgmt.CreateUser(user.user, user.groups, "", "", "")
 		}

lib/utils/host/hostusers.go
@@ -92,17 +92,15 @@ func UserAdd(username string, groups []string, home, uid, gid string) (exitCode 
 	return cmd.ProcessState.ExitCode(), trace.Wrap(err)
 }
 
-// AddUserToGroups adds a user to a list of specified groups on a host using `usermod`
-func AddUserToGroups(username string, groups []string) (exitCode int, err error) {
+// SetUserGroups adds a user to a list of specified groups on a host using `usermod`,
+// overriding any existing supplementary groups.
+func SetUserGroups(username string, groups []string) (exitCode int, err error) {
 	usermodBin, err := exec.LookPath("usermod")
 	if err != nil {
 		return -1, trace.Wrap(err, "cant find usermod binary")
 	}
-	args := []string{"-aG"}
-	args = append(args, groups...)
-	args = append(args, username)
-	// usermod -aG (append groups) (username)
-	cmd := exec.Command(usermodBin, args...)
+	// usermod -G (replace groups) (username)
+	cmd := exec.Command(usermodBin, "-G", strings.Join(groups, ","), username)
 	output, err := cmd.CombinedOutput()
 	log.Debugf("%s output: %s", cmd.Path, string(output))
 	return cmd.ProcessState.ExitCode(), trace.Wrap(err)

commit ccb8a1057398c79bdc4b6ce3d6a7bea09c48d7ad

Add host user creation benchmark test
Includes a new benchmark test meant to cover the performance
regression introduced in https://github.com/gravitational/teleport/pull/42884.
A few additional tweaks to the ssh test suite were required to use
the same fixtures in benchmark and unit tests.

lib/services/presence.go
@@ -20,6 +20,7 @@ package services
 
 import (
 	"context"
+	"time"
 
 	"github.com/gravitational/teleport/api/client/proto"
 	"github.com/gravitational/teleport/api/internalutils/stream"
@@ -203,4 +204,7 @@ type Presence interface {
 type PresenceInternal interface {
 	Presence
 	InventoryInternal
+
+	UpsertHostUserInteractionTime(ctx context.Context, name string, loginTime time.Time) error
+	GetHostUserInteractionTime(ctx context.Context, name string) (time.Time, error)
 }

lib/srv/regular/sshserver.go
@@ -66,7 +66,6 @@ import (
 	authorizedkeysreporter "github.com/gravitational/teleport/lib/secretsscanner/authorizedkeys"
 	"github.com/gravitational/teleport/lib/service/servicecfg"
 	"github.com/gravitational/teleport/lib/services"
-	"github.com/gravitational/teleport/lib/services/local"
 	"github.com/gravitational/teleport/lib/srv"
 	"github.com/gravitational/teleport/lib/srv/ingress"
 	"github.com/gravitational/teleport/lib/sshutils"
@@ -211,7 +210,7 @@ type Server struct {
 	// creation
 	createHostUser bool
 
-	storage *local.PresenceService
+	storage services.PresenceInternal
 
 	// users is used to start the automatic user deletion loop
 	users srv.HostUsers
@@ -616,7 +615,7 @@ func SetCreateHostUser(createUser bool) ServerOption {
 }
 
 // SetStoragePresenceService configures host user creation on a server
-func SetStoragePresenceService(service *local.PresenceService) ServerOption {
+func SetStoragePresenceService(service services.PresenceInternal) ServerOption {
 	return func(s *Server) error {
 		s.storage = service
 		return nil

lib/srv/sess.go
@@ -271,6 +271,7 @@ func (s *SessionRegistry) TryCreateHostUser(ctx *ServerContext) error {
 	ui, err := ctx.Identity.AccessChecker.HostUsers(ctx.srv.GetInfo())
 	if err != nil {
 		if trace.IsAccessDenied(err) {
+			log.Warnf("Unable to create host users: %v", err)
 			return nil
 		}
 		log.Debug("Error while checking host users creation permission: ", err)

lib/srv/usermgmt.go
@@ -36,11 +36,10 @@ import (
 	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/api/utils/retryutils"
 	"github.com/gravitational/teleport/lib/services"
-	"github.com/gravitational/teleport/lib/services/local"
 )
 
 // NewHostUsers initialize a new HostUsers object
-func NewHostUsers(ctx context.Context, storage *local.PresenceService, uuid string) HostUsers {
+func NewHostUsers(ctx context.Context, storage services.PresenceInternal, uuid string) HostUsers {
 	//nolint:staticcheck // SA4023. False positive on macOS.
 	backend, err := newHostUsersBackend()
 	switch {
@@ -177,7 +176,7 @@ type HostUserManagement struct {
 	backend HostUsersBackend
 	ctx     context.Context
 	cancel  context.CancelFunc
-	storage *local.PresenceService
+	storage services.PresenceInternal
 
 	userGrace time.Duration
 }

lib/utils/testhelpers.go
@@ -94,32 +94,32 @@ func RunTestBackgroundTask(ctx context.Context, t *testing.T, task *TestBackgrou
 }
 
 // RequireRoot skips the current test if it is not running as root.
-func RequireRoot(t *testing.T) {
-	t.Helper()
+func RequireRoot(tb testing.TB) {
+	tb.Helper()
 	if os.Geteuid() != 0 {
-		t.Skip("This test will be skipped because tests are not being run as root.")
+		tb.Skip("This test will be skipped because tests are not being run as root.")
 	}
 }
 
-func generateUsername(t *testing.T) string {
+func generateUsername(tb testing.TB) string {
 	suffix := make([]byte, 8)
 	_, err := rand.Read(suffix)
-	require.NoError(t, err)
+	require.NoError(tb, err)
 	return fmt.Sprintf("teleport-%x", suffix)
 }
 
 // GenerateLocalUsername generates the username for a local user that does not
 // already exists (but it does not create the user).
-func GenerateLocalUsername(t *testing.T) string {
+func GenerateLocalUsername(tb testing.TB) string {
 	const maxAttempts = 10
 	for i := 0; i < maxAttempts; i++ {
-		login := generateUsername(t)
+		login := generateUsername(tb)
 		_, err := user.Lookup(login)
 		if errors.Is(err, user.UnknownUserError(login)) {
 			return login
 		}
-		require.NoError(t, err)
+		require.NoError(tb, err)
 	}
-	t.Fatalf("Unable to generate unused username after %v attempts", maxAttempts)
+	tb.Fatalf("Unable to generate unused username after %v attempts", maxAttempts)
 	return ""
 }

lib/srv/regular/sshserver_test.go
@@ -127,7 +127,7 @@ func newFixture(t *testing.T) *sshTestFixture {
 	return newCustomFixture(t, func(*auth.TestServerConfig) {})
 }
 
-func newFixtureWithoutDiskBasedLogging(t *testing.T, sshOpts ...ServerOption) *sshTestFixture {
+func newFixtureWithoutDiskBasedLogging(t testing.TB, sshOpts ...ServerOption) *sshTestFixture {
 	t.Helper()
 
 	f := newCustomFixture(t, func(cfg *auth.TestServerConfig) {
@@ -144,7 +144,7 @@ func newFixtureWithoutDiskBasedLogging(t *testing.T, sshOpts ...ServerOption) *s
 	return f
 }
 
-func (f *sshTestFixture) newSSHClient(ctx context.Context, t *testing.T, user *user.User) *tracessh.Client {
+func (f *sshTestFixture) newSSHClient(ctx context.Context, t testing.TB, user *user.User) *tracessh.Client {
 	// set up SSH client using the user private key for signing
 	up, err := newUpack(f.testSrv, user.Username, []string{user.Username}, wildcardAllow)
 	require.NoError(t, err)
@@ -173,7 +173,7 @@ func (f *sshTestFixture) newSSHClient(ctx context.Context, t *testing.T, user *u
 	return client
 }
 
-func newCustomFixture(t *testing.T, mutateCfg func(*auth.TestServerConfig), sshOpts ...ServerOption) *sshTestFixture {
+func newCustomFixture(t testing.TB, mutateCfg func(*auth.TestServerConfig), sshOpts ...ServerOption) *sshTestFixture {
 	ctx := context.Background()
 
 	u, err := user.Current()
@@ -237,6 +237,7 @@ func newCustomFixture(t *testing.T, mutateCfg func(*auth.TestServerConfig), sshO
 		SetLockWatcher(lockWatcher),
 		SetX11ForwardingConfig(&x11.ServerConfig{}),
 		SetSessionController(sessionController),
+		SetStoragePresenceService(testServer.AuthServer.AuthServer.PresenceInternal),
 	}
 
 	serverOptions = append(serverOptions, sshOpts...)
@@ -2887,6 +2888,7 @@ func newUpack(testSvr *auth.TestServer, username string, allowedLogins []string,
 	role.SetRules(types.Allow, rules)
 	opts := role.GetOptions()
 	opts.PermitX11Forwarding = types.NewBool(true)
+	opts.CreateHostUser = types.NewBoolOption(true)
 	role.SetOptions(opts)
 	role.SetLogins(types.Allow, allowedLogins)
 	role.SetNodeLabels(types.Allow, allowedLabels)
@@ -2935,7 +2937,7 @@ func newUpack(testSvr *auth.TestServer, username string, allowedLogins []string,
 	}, nil
 }
 
-func newLockWatcher(ctx context.Context, t *testing.T, client types.Events) *services.LockWatcher {
+func newLockWatcher(ctx context.Context, t testing.TB, client types.Events) *services.LockWatcher {
 	lockWatcher, err := services.NewLockWatcher(ctx, services.LockWatcherConfig{
 		ResourceWatcherConfig: services.ResourceWatcherConfig{
 			Component: "test",
@@ -2973,7 +2975,7 @@ func newCertAuthorityWatcher(ctx context.Context, t *testing.T, client types.Eve
 }
 
 // newSigner creates a new SSH signer that can be used by the Server.
-func newSigner(t *testing.T, ctx context.Context, testServer *auth.TestServer) ssh.Signer {
+func newSigner(t testing.TB, ctx context.Context, testServer *auth.TestServer) ssh.Signer {
 	t.Helper()
 
 	priv, pub, err := testauthority.New().GenerateKeyPair()

lib/srv/regular/sshserver_unix_test.go
@@ -21,15 +21,22 @@
 package regular
 
 import (
+	"context"
 	"net"
 	"os"
+	"os/user"
+	"sync"
 	"syscall"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/sys/unix"
 
+	tracessh "github.com/gravitational/teleport/api/observability/tracing/ssh"
 	"github.com/gravitational/teleport/lib/srv"
+	"github.com/gravitational/teleport/lib/utils"
+	"github.com/gravitational/teleport/lib/utils/host"
 	"github.com/gravitational/teleport/lib/utils/uds"
 )
 
@@ -127,4 +134,75 @@ func TestValidateListenerSocket(t *testing.T) {
 			tc.assert(t, err)
 		})
 	}
+}
+
+// BenchmarkRootExecCommand measures performance of running multiple exec requests
+// over a single ssh connection. The same test is run with and without host user
+// creation support to catch any performance degradation caused by user provisioning.
+func BenchmarkRootExecCommand(b *testing.B) {
+	utils.RequireRoot(b)
+
+	b.ReportAllocs()
+
+	cases := []struct {
+		name       string
+		createUser bool
+	}{
+		{
+			name: "no user creation",
+		},
+		{
+			name:       "with user creation",
+			createUser: true,
+		},
+	}
+
+	for _, test := range cases {
+		b.Run(test.name, func(b *testing.B) {
+			var opts []ServerOption
+			if test.createUser {
+				opts = []ServerOption{SetCreateHostUser(true)}
+			}
+
+			f := newFixtureWithoutDiskBasedLogging(b, opts...)
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				username := f.user
+				if test.createUser {
+					username = utils.GenerateLocalUsername(b)
+					b.Cleanup(func() { _, _ = host.UserDel(username) })
+				}
+
+				_, err := newUpack(f.testSrv, username, []string{username, f.user}, wildcardAllow)
+				require.NoError(b, err)
+
+				clt := f.newSSHClient(context.Background(), b, &user.User{Username: username})
+
+				executeCommand(b, clt, "uptime", 10)
+			}
+		})
+	}
+}
+
+func executeCommand(tb testing.TB, clt *tracessh.Client, command string, executions int) {
+	tb.Helper()
+
+	var wg sync.WaitGroup
+	for i := 0; i < executions; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			ctx := context.Background()
+
+			se, err := clt.NewSession(ctx)
+			assert.NoError(tb, err)
+			defer se.Close()
+
+			assert.NoError(tb, se.Run(ctx, command))
+		}()
+	}
+
+	wg.Wait()
 }