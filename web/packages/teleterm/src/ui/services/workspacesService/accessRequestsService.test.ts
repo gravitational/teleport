b86dbab683c7111596f8d4287f8a8011fee52a5a
Sakshyam Shah <sshah@goteleport.com>
2024-08-09 11:48:52 -0400

    connect: resource access request for `saml_idp_service_provider`  (#45186)
    * feat: support saml_idp_service_provider resource access request in Connect
    
    * use extractResourceRequestProperties to send saml_idp_service_provider kind
    
    * tuck saml kind with app for icon, bring back default resource return

--- web/packages/teleterm/src/ui/services/workspacesService/accessRequestsService.test.ts
+++ web/packages/teleterm/src/ui/services/workspacesService/accessRequestsService.test.ts
@@ -37,6 +37,11 @@ function getMockPendingResourceAccessRequest(): PendingAccessRequest {
   const server = makeServer();
   const app1 = makeApp();
   const app2 = makeApp({ uri: `${rootClusterUri}/apps/foo2`, name: 'foo2' });
+  const samlApp = makeApp({
+    uri: `${rootClusterUri}/samlapp`,
+    name: 'samlapp',
+    samlApp: true,
+  });
   const kube = makeKube();
   const database = makeDatabase();
 
@@ -48,6 +53,7 @@ function getMockPendingResourceAccessRequest(): PendingAccessRequest {
       [app2.uri, { kind: 'app', resource: app2 }],
       [kube.uri, { kind: 'kube', resource: kube }],
       [database.uri, { kind: 'database', resource: database }],
+      [samlApp.uri, { kind: 'app', resource: samlApp }],
     ]),
   };
 }
@@ -113,7 +119,7 @@ test('getAddedItemsCount() returns added resource count for pending request', ()
   const { accessRequestsService: service } = getTestSetup(
     getMockPendingResourceAccessRequest()
   );
-  expect(service.getAddedItemsCount()).toBe(5);
+  expect(service.getAddedItemsCount()).toBe(6);
   service.clearPendingAccessRequest();
   expect(service.getAddedItemsCount()).toBe(0);
 });
