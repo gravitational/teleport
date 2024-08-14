7b0bca5906bbefce4930b7e8c5c4a1cf5b07a209
Alan Parra <alan.parra@goteleport.com>
2024-08-14 12:20:44 -0300

    Add nolints to lib/srv/usermgmt.go

--- lib/srv/usermgmt_other.go
+++ lib/srv/usermgmt_other.go
@@ -35,6 +35,7 @@ func newHostSudoersBackend(_ string) (HostSudoersBackend, error) {
 	return nil, trace.NotImplemented("Host user creation management is only supported on linux")
 }
 
+//nolint:staticcheck // intended to always return an error for non-linux builds
 func readDefaultHome(user string) (string, error) {
 	return "", trace.NotImplemented("readDefaultHome is only supported on linux")
 }
