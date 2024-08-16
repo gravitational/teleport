44d5589d1a84ddd8e662f59fbeabc9fd866cb0e2
Edward Dowling <edward.dowling@goteleport.com>
2024-08-16 16:13:44 +0100

    Remove unused handlerTimeout

--- integrations/access/servicenow/app.go
+++ integrations/access/servicenow/app.go
@@ -50,8 +50,6 @@ const (
 	minServerVersion = "13.0.0"
 	// initTimeout is used to bound execution time of health check and teleport version check.
 	initTimeout = time.Second * 10
-	// handlerTimeout is used to bound the execution time of watcher event handler.
-	handlerTimeout = time.Second * 30
 	// modifyPluginDataBackoffBase is an initial (minimum) backoff value.
 	modifyPluginDataBackoffBase = time.Millisecond
 	// modifyPluginDataBackoffMax is a backoff threshold
