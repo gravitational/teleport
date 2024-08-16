0c311874b2f00705bc7a93f69545f124a7359d67
matheus <matheus.battirola@goteleport.com>
2024-08-16 12:21:25 -0300

    Improve `getPresetUsers` godoc
    Co-authored-by: Sakshyam Shah <sshah@goteleport.com>

--- lib/auth/init.go
+++ lib/auth/init.go
@@ -1048,7 +1048,7 @@ type PresetUsers interface {
 	UpsertUser(ctx context.Context, user types.User) (types.User, error)
 }
 
-// getPresetUsers returns a list of all users roles expected to be available on
+// getPresetUsers returns a list of all preset users expected to be available on
 // this cluster.
 func getPresetUsers() []types.User {
 	presets := []types.User{
