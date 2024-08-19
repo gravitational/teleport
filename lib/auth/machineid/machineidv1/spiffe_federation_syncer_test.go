a8d5457bd6518683a41b31596de9b73d1d84fecb
Noah Stride <noah.stride@goteleport.com>
2024-08-19 12:40:47 +0100

    Start putting together test for SPIFFEFederationSyncer

--- lib/auth/machineid/machineidv1/spiffe_federation_syncer_test.go
+++ lib/auth/machineid/machineidv1/spiffe_federation_syncer_test.go
@@ -20,15 +20,20 @@ package machineidv1
 
 import (
 	"context"
+	headerv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/header/v1"
+	machineidv1pb "github.com/gravitational/teleport/api/gen/proto/go/teleport/machineid/v1"
+	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/lib/backend/memory"
 	"github.com/gravitational/teleport/lib/services/local"
 	"github.com/gravitational/teleport/lib/utils"
 	"github.com/jonboulle/clockwork"
 	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func TestSPIFFEFederationSyncer_Run(t *testing.T) {
+func TestSPIFFEFederationSyncer_syncFederation(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	t.Cleanup(cancel)
 
@@ -37,15 +42,43 @@ func TestSPIFFEFederationSyncer_Run(t *testing.T) {
 	backend, err := memory.New(memory.Config{})
 	require.NoError(t, err)
 
-	spiffeFederationStore, err := local.NewSPIFFEFederationService(backend)
+	store, err := local.NewSPIFFEFederationService(backend)
 	require.NoError(t, err)
 	eventsSvc := local.NewEventsService(backend)
 
 	syncer, err := NewSPIFFEFederationSyncer(SPIFFEFederationSyncerConfig{
 		Backend:       backend,
-		Store:         spiffeFederationStore,
+		Store:         store,
 		EventsWatcher: eventsSvc,
 		Clock:         clock,
 		Logger:        logger,
 	})
+	require.NoError(t, err)
+
+	testSrv := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	}))
+
+	in := &machineidv1pb.SPIFFEFederation{
+		Kind:    types.KindSPIFFEFederation,
+		Version: types.V1,
+		Metadata: &headerv1.Metadata{
+			Name: "foo",
+		},
+		Spec: &machineidv1pb.SPIFFEFederationSpec{
+			BundleSource: &machineidv1pb.SPIFFEFederationBundleSource{
+				HttpsWeb: &machineidv1pb.SPIFFEFederationBundleSourceHTTPSWeb{
+					BundleEndpointUrl: testSrv.URL,
+				},
+			},
+		},
+	}
+	created, err := store.CreateSPIFFEFederation(ctx, in)
+	require.NoError(t, err)
+
+	// TODO: Add test for static bundle, change and no change
+	// TODO: Add test for already synced recently (e.g sync not necessary)
+
+	out, err := syncer.syncFederation(ctx, "foo")
+	require.NoError(t, err)
 }
