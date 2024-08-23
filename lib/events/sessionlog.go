febf98a6b0ff922e7b6ff16e59cd649c1511ce84
Forrest Marshall <forrest@goteleport.com>
2024-08-23 12:22:32 -0700

    fix session corruption

--- lib/events/sessionlog.go
+++ lib/events/sessionlog.go
@@ -106,7 +106,7 @@ func newGzipWriter(writer io.WriteCloser) *gzipWriter {
 // gzipReader wraps file, on close close both gzip writer and file
 type gzipReader struct {
 	io.ReadCloser
-	inner io.Closer
+	inner io.ReadCloser
 }
 
 // Close closes file and gzip writer
@@ -128,6 +128,11 @@ func newGzipReader(reader io.ReadCloser) (*gzipReader, error) {
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
+	// older bugged versions of teleport would sometimes incorrectly inject padding bytes into
+	// the gzip section of the archive. this causes gzip readers with multistream enabled (the
+	// default behavior) to fail. we  disable multistream here in order to ensure that the gzip
+	// reader halts when it reaches the end of the current (only) valid gzip entry.
+	gzReader.Multistream(false)
 	return &gzipReader{
 		ReadCloser: gzReader,
 		inner:      reader,
