ef7683a78421e4b1cc9ee1f14e7311a69aea9935
Sakshyam Shah <sshah@goteleport.com>
2024-08-27 13:42:37 -0400

    [web] file upload button shared component (#45717)
    * file upload button
    
    * update buttonTextWithAddIcon to use IconSize type
    
    * display error message on parent validation result, license file
    
    * disabled and error story

--- web/packages/shared/components/ButtonFileUpload/index.ts
+++ web/packages/shared/components/ButtonFileUpload/index.ts
@@ -1,6 +1,6 @@
 /**
  * Teleport
- * Copyright (C) 2023  Gravitational, Inc.
+ * Copyright (C) 2024  Gravitational, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -16,33 +16,4 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from 'react';
-import { ButtonText } from 'design';
-import { Add as AddIcon } from 'design/Icon';
-
-import type { IconSize } from 'design/Icon/Icon';
-
-export const ButtonTextWithAddIcon = ({
-  label,
-  onClick,
-  disabled,
-  iconSize = 12,
-}: {
-  label: string;
-  onClick: () => void;
-  disabled?: boolean;
-  iconSize?: IconSize;
-}) => {
-  return (
-    <ButtonText onClick={onClick} disabled={disabled} compact>
-      <AddIcon
-        className="icon-add"
-        size={iconSize}
-        css={`
-          margin-right: 3px;
-        `}
-      />
-      {label}
-    </ButtonText>
-  );
-};
+export { ButtonFileUpload } from './ButtonFileUpload';
