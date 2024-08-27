9c9b0ba5d1561afa4a6a72ac025916bbd8d361b9
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2024-08-27 12:42:28 -0400

    Prevent interactive sessions from hanging on exit (#45879)
    The TTY allocated for interactive sessions was not being handled
    properly. The parent process was never closing its half of the
    TTY, which prevented the PTY read from unblocking when the child
    process terminated. This became more noticeable after #45223
    landed because interactive sessions were now properly waiting for
    the PTY copying to be completed before exiting.
    
    Additionally, the PTY is no longer being provided to the child reexec
    process since it should be only be used by the parent process.
    
    Closes #45878.

--- lib/srv/reexec.go
+++ lib/srv/reexec.go
@@ -79,8 +79,10 @@ const (
 	// ErrorFile is used to communicate any errors terminating the child process
 	// to the parent process
 	ErrorFile
-	// PTYFile is a PTY the parent process passes to the child process.
-	PTYFile
+	// PTYFileDeprecated is a placeholder for the unused PTY file that
+	// was passed to the child process. The PTY should only be used in the
+	// the parent process but was left here for compatibility purposes.
+	PTYFileDeprecated
 	// TTYFile is a TTY the parent process passes to the child process.
 	TTYFile
 
@@ -278,17 +280,15 @@ func RunCommand() (errw io.Writer, code int, err error) {
 	}()
 
 	var tty *os.File
-	var pty *os.File
 	uaccEnabled := false
 
-	// If a terminal was requested, file descriptors 6 and 7 always point to the
-	// PTY and TTY. Extract them and set the controlling TTY. Otherwise, connect
+	// If a terminal was requested, file descriptor 7 always points to the
+	// TTY. Extract it and set the controlling TTY. Otherwise, connect
 	// std{in,out,err} directly.
 	if c.Terminal {
-		pty = os.NewFile(PTYFile, fdName(PTYFile))
 		tty = os.NewFile(TTYFile, fdName(TTYFile))
-		if pty == nil || tty == nil {
-			return errorWriter, teleport.RemoteCommandFailure, trace.BadParameter("pty and tty not found")
+		if tty == nil {
+			return errorWriter, teleport.RemoteCommandFailure, trace.BadParameter("tty not found")
 		}
 		errorWriter = tty
 	}
@@ -365,7 +365,7 @@ func RunCommand() (errw io.Writer, code int, err error) {
 	}
 
 	// Build the actual command that will launch the shell.
-	cmd, err := buildCommand(&c, localUser, tty, pty, pamEnvironment)
+	cmd, err := buildCommand(&c, localUser, tty, pamEnvironment)
 	if err != nil {
 		return errorWriter, teleport.RemoteCommandFailure, trace.Wrap(err)
 	}
@@ -777,7 +777,7 @@ func IsReexec() bool {
 
 // buildCommand constructs a command that will execute the users shell. This
 // function is run by Teleport while it's re-executing.
-func buildCommand(c *ExecCommand, localUser *user.User, tty *os.File, pty *os.File, pamEnvironment []string) (*exec.Cmd, error) {
+func buildCommand(c *ExecCommand, localUser *user.User, tty *os.File, pamEnvironment []string) (*exec.Cmd, error) {
 	var cmd exec.Cmd
 	isReexec := false
 
