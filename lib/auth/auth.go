d23ba8408a9ba48dccb3cdc5c660c965c5a616e3
Bartosz Leper <bartosz.leper@goteleport.com>
2024-09-02 13:43:21 +0200

    Rename CTA to "link text"

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -144,9 +144,9 @@ const (
 	OSSDesktopsAlertMessage = "Your cluster is beyond its allocation of 5 non-Active Directory Windows desktops. " +
 		"Reach out for unlimited desktops with Teleport Enterprise."
 
-	OSSDesktopsAlertLink    = "https://goteleport.com/r/upgrade-community?utm_campaign=CTA_windows_local"
-	OSSDesktopsAlertLinkCTA = "Contact Sales"
-	OSSDesktopsLimit        = 5
+	OSSDesktopsAlertLink     = "https://goteleport.com/r/upgrade-community?utm_campaign=CTA_windows_local"
+	OSSDesktopsAlertLinkText = "Contact Sales"
+	OSSDesktopsLimit         = 5
 )
 
 const (
@@ -5557,7 +5557,7 @@ func (a *Server) syncDesktopsLimitAlert(ctx context.Context) {
 		types.WithAlertLabel(types.AlertOnLogin, "yes"),
 		types.WithAlertLabel(types.AlertPermitAll, "yes"),
 		types.WithAlertLabel(types.AlertLink, OSSDesktopsAlertLink),
-		types.WithAlertLabel(types.AlertLinkCTA, OSSDesktopsAlertLinkCTA),
+		types.WithAlertLabel(types.AlertLinkText, OSSDesktopsAlertLinkText),
 		types.WithAlertExpires(time.Now().Add(OSSDesktopsCheckPeriod)))
 	if err != nil {
 		log.Warnf("Can't create OSS non-AD desktops limit alert: %v", err)
