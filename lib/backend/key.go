0fb193a2fa2621810af4fadc1cfade1b1b7079fe
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2024-09-05 15:50:12 +0000

    Move external backend key logic to receiver methods of backend.Key (#45985) (#46195)
    There are a number of places that the backend.Key is assumed to
    be a []byte and is passed to various functions from the `bytes`
    package. This creates equivalent functionality on the backend.Key
    type so that all assumptions about the backend key type can be
    removed.

--- lib/backend/key.go
+++ lib/backend/key.go
@@ -16,5 +16,93 @@
 
 package backend
 
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
 // Key is the unique identifier for an [Item].
-type Key = []byte
+type Key []byte
+
+// Separator is used as a separator between key parts
+const Separator = '/'
+
+// NewKey joins parts into path separated by Separator,
+// makes sure path always starts with Separator ("/")
+func NewKey(parts ...string) Key {
+	return internalKey("", parts...)
+}
+
+// ExactKey is like Key, except a Separator is appended to the result
+// path of Key. This is to ensure range matching of a path will only
+// math child paths and not other paths that have the resulting path
+// as a prefix.
+func ExactKey(parts ...string) Key {
+	return append(NewKey(parts...), Separator)
+}
+
+func internalKey(internalPrefix string, parts ...string) Key {
+	return Key(strings.Join(append([]string{internalPrefix}, parts...), string(Separator)))
+}
+
+// String returns the textual representation of the key with
+// each component concatenated together via the [Separator].
+func (k Key) String() string {
+	return string(k)
+}
+
+// HasPrefix reports whether the key begins with prefix.
+func (k Key) HasPrefix(prefix Key) bool {
+	return bytes.HasPrefix(k, prefix)
+}
+
+// TrimPrefix returns the key without the provided leading prefix string.
+// If the key doesn't start with prefix, it is returned unchanged.
+func (k Key) TrimPrefix(prefix Key) Key {
+	return bytes.TrimPrefix(k, prefix)
+}
+
+func (k Key) PrependPrefix(p Key) Key {
+	return append(p, k...)
+}
+
+// HasSuffix reports whether the key ends with suffix.
+func (k Key) HasSuffix(suffix Key) bool {
+	return bytes.HasSuffix(k, suffix)
+}
+
+// TrimSuffix returns the key without the provided trailing suffix string.
+// If the key doesn't end with suffix, it is returned unchanged.
+func (k Key) TrimSuffix(suffix Key) Key {
+	return bytes.TrimSuffix(k, suffix)
+}
+
+func (k Key) Components() [][]byte {
+	if len(k) == 0 {
+		return nil
+	}
+
+	sep := []byte{Separator}
+	return bytes.Split(bytes.TrimPrefix(k, sep), sep)
+}
+
+func (k Key) Compare(o Key) int {
+	return bytes.Compare(k, o)
+}
+
+// Scan implement sql.Scanner, allowing a [Key] to
+// be directly retrieved from sql backends without
+// an intermediary object.
+func (k *Key) Scan(scan any) error {
+	switch key := scan.(type) {
+	case []byte:
+		*k = bytes.Clone(key)
+	case string:
+		*k = []byte(strings.Clone(key))
+	default:
+		return fmt.Errorf("invalid Key type %T", scan)
+	}
+
+	return nil
+}
--- lib/services/local/events.go
+++ lib/services/local/events.go
@@ -19,9 +19,7 @@
 package local
 
 import (
-	"bytes"
 	"context"
-	"strings"
 
 	"github.com/gravitational/trace"
 	"github.com/jonboulle/clockwork"
@@ -372,7 +370,7 @@ func (p baseParser) prefixes() []backend.Key {
 
 func (p baseParser) match(key backend.Key) bool {
 	for _, prefix := range p.matchPrefixes {
-		if bytes.HasPrefix(key, prefix) {
+		if key.HasPrefix(prefix) {
 			return true
 		}
 	}
@@ -719,8 +717,8 @@ func (p *namespaceParser) match(key backend.Key) bool {
 	// namespaces are stored under key '/namespaces/<namespace-name>/params'
 	// and this code matches similar pattern
 	return p.baseParser.match(key) &&
-		bytes.HasSuffix(key, []byte(paramsPrefix)) &&
-		bytes.Count(key, []byte{backend.Separator}) == 3
+		key.HasSuffix(backend.Key(paramsPrefix)) &&
+		len(key.Components()) == 3
 }
 
 func (p *namespaceParser) parse(event backend.Event) (types.Resource, error) {
@@ -794,10 +792,10 @@ func (p *accessRequestParser) prefixes() []backend.Key {
 }
 
 func (p *accessRequestParser) match(key backend.Key) bool {
-	if !bytes.HasPrefix(key, p.matchPrefix) {
+	if !key.HasPrefix(p.matchPrefix) {
 		return false
 	}
-	if !bytes.HasSuffix(key, p.matchSuffix) {
+	if !key.HasSuffix(p.matchSuffix) {
 		return false
 	}
 	return true
@@ -835,8 +833,8 @@ func (p *userParser) match(key backend.Key) bool {
 	// users are stored under key '/web/users/<username>/params'
 	// and this code matches similar pattern
 	return p.baseParser.match(key) &&
-		bytes.HasSuffix(key, []byte(paramsPrefix)) &&
-		bytes.Count(key, []byte{backend.Separator}) == 4
+		key.HasSuffix(backend.Key(paramsPrefix)) &&
+		len(key.Components()) == 4
 }
 
 func (p *userParser) parse(event backend.Event) (types.Resource, error) {
@@ -1353,7 +1351,7 @@ func (p *remoteClusterParser) prefixes() []backend.Key {
 }
 
 func (p *remoteClusterParser) match(key backend.Key) bool {
-	return bytes.HasPrefix(key, p.matchPrefix)
+	return key.HasPrefix(p.matchPrefix)
 }
 
 func (p *remoteClusterParser) parse(event backend.Event) (types.Resource, error) {
@@ -1416,7 +1414,7 @@ func (p *networkRestrictionsParser) prefixes() []backend.Key {
 }
 
 func (p *networkRestrictionsParser) match(key backend.Key) bool {
-	return bytes.HasPrefix(key, p.matchPrefix)
+	return key.HasPrefix(p.matchPrefix)
 }
 
 func (p *networkRestrictionsParser) parse(event backend.Event) (types.Resource, error) {
@@ -1964,25 +1962,19 @@ type kubeWaitingContainerParser struct {
 func (p *kubeWaitingContainerParser) parse(event backend.Event) (types.Resource, error) {
 	switch event.Type {
 	case types.OpDelete:
-		// remove the first separator so no separated parts should be
-		// empty strings
-		key := string(event.Item.Key)
-		if len(key) > 0 && key[0] == backend.Separator {
-			key = key[1:]
-		}
-		parts := strings.Split(key, string(backend.Separator))
+		parts := event.Item.Key.Components()
 		if len(parts) != 6 {
 			return nil, trace.BadParameter("malformed key for %s event: %s", types.KindKubeWaitingContainer, event.Item.Key)
 		}
 
 		resource, err := kubewaitingcontainer.NewKubeWaitingContainer(
-			parts[5],
+			string(parts[5]),
 			&kubewaitingcontainerpb.KubernetesWaitingContainerSpec{
-				Username:      parts[1],
-				Cluster:       parts[2],
-				Namespace:     parts[3],
-				PodName:       parts[4],
-				ContainerName: parts[5],
+				Username:      string(parts[1]),
+				Cluster:       string(parts[2]),
+				Namespace:     string(parts[3]),
+				PodName:       string(parts[4]),
+				ContainerName: string(parts[5]),
 				Patch:         []byte("{}"),                       // default to empty patch. It doesn't matter for delete ops.
 				PatchType:     kubewaitingcontainer.JSONPatchType, // default to JSON patch. It doesn't matter for delete ops.
 			},
@@ -2128,7 +2120,7 @@ func resourceHeader(event backend.Event, kind, version string, offset int) (type
 		Kind:    kind,
 		Version: version,
 		Metadata: types.Metadata{
-			Name:      string(name),
+			Name:      name,
 			Namespace: apidefaults.Namespace,
 		},
 	}, nil
@@ -2144,7 +2136,7 @@ func resourceHeaderWithTemplate(event backend.Event, hdr types.ResourceHeader, o
 		SubKind: hdr.SubKind,
 		Version: hdr.Version,
 		Metadata: types.Metadata{
-			Name:      string(name),
+			Name:      name,
 			Namespace: apidefaults.Namespace,
 		},
 	}, nil
@@ -2208,7 +2200,7 @@ func (p *deviceParser) parse(event backend.Event) (types.Resource, error) {
 				Kind:    types.KindDevice,
 				Version: types.V1,
 				Metadata: types.Metadata{
-					Name: string(name),
+					Name: name,
 				},
 			},
 		}
@@ -2335,19 +2327,19 @@ type EventMatcher interface {
 	Match(types.Event) (types.Resource, error)
 }
 
-// base returns last element delimited by separator, index is
-// is an index of the key part to get counting from the end
-func base(key backend.Key, offset int) ([]byte, error) {
-	parts := bytes.Split(key, []byte{backend.Separator})
+// base returns the key component that is offset
+// components before the last component.
+func base(key backend.Key, offset int) (string, error) {
+	parts := key.Components()
 	if len(parts) < offset+1 {
-		return nil, trace.NotFound("failed parsing %v", string(key))
+		return "", trace.NotFound("failed parsing %v", string(key))
 	}
-	return parts[len(parts)-offset-1], nil
+	return string(parts[len(parts)-offset-1]), nil
 }
 
 // baseTwoKeys returns two last keys
 func baseTwoKeys(key backend.Key) (string, string, error) {
-	parts := bytes.Split(key, []byte{backend.Separator})
+	parts := key.Components()
 	if len(parts) < 2 {
 		return "", "", trace.NotFound("failed parsing %v", string(key))
 	}
