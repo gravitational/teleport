9ffaf667ee2301f162308e4f0aa57dad0ab9d085
Tim Ross <tim.ross@goteleport.com>
2024-09-05 17:51:42 -0400

    v16.1.0-dev-tross-putty-templates.3

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "17.0.0-dev-tross-putty-templates.2"
+const Version = "17.0.0-dev-tross-putty-templates.3"
 
 var SemVersion = semver.New(Version)
