9aa2f7800c458b3e3bb45c352ddad25d24dc762e
Nic Klaassen <nic@goteleport.com>
2024-09-05 09:25:33 -0700

    add nolint tags for req.PublicKey (#4988)
    Source-commit: gravitational/teleport.e@514e8f4913535ae4de3d3200528b01c56ae0024e
    Source-pull-requests: gravitational/teleport.e#4988

--- e/lib/auth/oidc_test.go
+++ e/lib/auth/oidc_test.go
@@ -1449,7 +1449,7 @@ func TestOIDCAuthCompat(t *testing.T) {
 			// The proxy should get back the keys exactly as it sent them. Older
 			// proxies won't look for the new split keys, and they do check for
 			// the old single key to tell if this was a console or web request.
-			require.Equal(t, tc.pubKey, resp.Req.PublicKey)
+			require.Equal(t, tc.pubKey, resp.Req.PublicKey) //nolint:staticcheck // SA1019. Checking deprecated field expected by older clients.
 			require.Equal(t, tc.sshPubKey, resp.Req.SSHPubKey)
 			require.Equal(t, tc.tlsPubKey, resp.Req.TLSPubKey)
 
