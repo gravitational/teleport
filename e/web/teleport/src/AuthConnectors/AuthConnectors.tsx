f8ddcfb5977a9e0900ebdc832b64447389963fb3
Steven Martin <steven@goteleport.com>
2024-09-10 13:21:28 -0400

    update AD to Msft Entra ID in sso web ui (#4999)
    Co-authored-by: Steven Martin <stevenmartin@Stevens-MBP.fios-router.home>
    
    Source-commit: gravitational/teleport.e@60eeab00bbdfe66158695c01949a49d442e7c3ef
    Source-pull-requests: gravitational/teleport.e#4999

--- e/web/teleport/src/AuthConnectors/AuthConnectors.tsx
+++ e/web/teleport/src/AuthConnectors/AuthConnectors.tsx
@@ -39,7 +39,7 @@ export function AuthConnectors(props: State) {
       ? 'Creating a new auth connector'
       : 'Editing auth connector';
   const description =
-    'Auth connectors allow Teleport to authenticate users via an external identity source such as Okta, Active Directory, GitHub, etc. This authentication method is commonly known as single sign-on (SSO).';
+    'Auth connectors allow Teleport to authenticate users via an external identity source such as Okta, Microsoft Entra ID, GitHub, etc. This authentication method is commonly known as single sign-on (SSO).';
 
   function handleOnRemove() {
     return remove(resources.item);
