3ad6604e00410904958f5c66a959141d1da4b374
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2024-09-10 16:57:51 +0000

    Update generic storage service to support validation and custom keys (#46319)
    Adds two new optional configuration options to the generic.Service:
    ValidateFunc and KeyFunc.
    
    ValidateFunc is a custom function that is called prior to persisting
    a resource in the backend. If the function returns an error, the
    resource is not stored and the error is returned to users.
    
    KeyFunc is a custom function used to derive the key for a particular
    resource. By default the generic service uses the metadata name as
    the key, however, in some scenarios more control over the key might
    be desired. For instance, a singleton resource might want to enforce
    that the key is also a static value instead of something that a
    user may supply.

--- lib/services/local/generic/generic.go
+++ lib/services/local/generic/generic.go
@@ -45,16 +45,28 @@ type UnmarshalFunc[T any] func([]byte, ...services.MarshalOption) (T, error)
 
 // ServiceConfig is the configuration for the service configuration.
 type ServiceConfig[T Resource] struct {
-	Backend       backend.Backend
-	ResourceKind  string
-	PageLimit     uint
+	// Backend used to persist the resource.
+	Backend backend.Backend
+	// ResourceKind is the friendly name of the resource.
+	ResourceKind string
+	// PageLimit
+	PageLimit uint
+	// BackendPrefix used when constructing the [backend.Item.Key].
 	BackendPrefix string
-	MarshalFunc   MarshalFunc[T]
+	// MarshlFunc converts the resource to bytes for persistence.
+	MarshalFunc MarshalFunc[T]
+	// UnmarshalFunc converts the bytes read from the backend to the resource.
 	UnmarshalFunc UnmarshalFunc[T]
+	// ValidateFunc optionally validates the resource prior to persisting it. Any errors
+	// returned from the validation function will prevent writes to the backend.
+	ValidateFunc func(T) error
 	// RunWhileLockedRetryInterval is the interval to retry the RunWhileLocked function.
 	// If set to 0, the default interval of 250ms will be used.
 	// WARNING: If set to a negative value, the RunWhileLocked function will retry immediately.
 	RunWhileLockedRetryInterval time.Duration
+	// KeyFunc optionally allows resource to have a custom key. If not provided the
+	// name of the resource will be used.
+	KeyFunc func(T) string
 }
 
 func (c *ServiceConfig[T]) CheckAndSetDefaults() error {
@@ -78,6 +90,15 @@ func (c *ServiceConfig[T]) CheckAndSetDefaults() error {
 	if c.UnmarshalFunc == nil {
 		return trace.BadParameter("unmarshal func is missing")
 	}
+
+	if c.ValidateFunc == nil {
+		c.ValidateFunc = func(t T) error { return nil }
+	}
+
+	if c.KeyFunc == nil {
+		c.KeyFunc = func(t T) string { return t.GetName() }
+	}
+
 	return nil
 }
 
@@ -89,7 +110,9 @@ type Service[T Resource] struct {
 	backendPrefix               string
 	marshalFunc                 MarshalFunc[T]
 	unmarshalFunc               UnmarshalFunc[T]
+	validateFunc                func(T) error
 	runWhileLockedRetryInterval time.Duration
+	keyFunc                     func(T) string
 }
 
 // NewService will return a new generic service with the given config. This will
@@ -106,7 +129,9 @@ func NewService[T Resource](cfg *ServiceConfig[T]) (*Service[T], error) {
 		backendPrefix:               cfg.BackendPrefix,
 		marshalFunc:                 cfg.MarshalFunc,
 		unmarshalFunc:               cfg.UnmarshalFunc,
+		validateFunc:                cfg.ValidateFunc,
 		runWhileLockedRetryInterval: cfg.RunWhileLockedRetryInterval,
+		keyFunc:                     cfg.KeyFunc,
 	}, nil
 }
 
@@ -117,12 +142,15 @@ func (s *Service[T]) WithPrefix(parts ...string) *Service[T] {
 	}
 
 	return &Service[T]{
-		backend:       s.backend,
-		resourceKind:  s.resourceKind,
-		pageLimit:     s.pageLimit,
-		backendPrefix: strings.Join(append([]string{s.backendPrefix}, parts...), string(backend.Separator)),
-		marshalFunc:   s.marshalFunc,
-		unmarshalFunc: s.unmarshalFunc,
+		backend:                     s.backend,
+		resourceKind:                s.resourceKind,
+		pageLimit:                   s.pageLimit,
+		backendPrefix:               strings.Join(append([]string{s.backendPrefix}, parts...), string(backend.Separator)),
+		marshalFunc:                 s.marshalFunc,
+		unmarshalFunc:               s.unmarshalFunc,
+		validateFunc:                s.validateFunc,
+		runWhileLockedRetryInterval: s.runWhileLockedRetryInterval,
+		keyFunc:                     s.keyFunc,
 	}
 }
 
@@ -285,7 +313,11 @@ func (s *Service[T]) GetResource(ctx context.Context, name string) (resource T, 
 // CreateResource creates a new resource.
 func (s *Service[T]) CreateResource(ctx context.Context, resource T) (T, error) {
 	var t T
-	item, err := s.MakeBackendItem(resource, resource.GetName())
+	if err := s.validateFunc(resource); err != nil {
+		return t, trace.Wrap(err)
+	}
+
+	item, err := s.MakeBackendItem(resource, s.keyFunc(resource))
 	if err != nil {
 		return t, trace.Wrap(err)
 	}
@@ -305,7 +337,12 @@ func (s *Service[T]) CreateResource(ctx context.Context, resource T) (T, error) 
 // UpdateResource updates an existing resource.
 func (s *Service[T]) UpdateResource(ctx context.Context, resource T) (T, error) {
 	var t T
-	item, err := s.MakeBackendItem(resource, resource.GetName())
+
+	if err := s.validateFunc(resource); err != nil {
+		return t, trace.Wrap(err)
+	}
+
+	item, err := s.MakeBackendItem(resource, s.keyFunc(resource))
 	if err != nil {
 		return t, trace.Wrap(err)
 	}
@@ -325,7 +362,12 @@ func (s *Service[T]) UpdateResource(ctx context.Context, resource T) (T, error) 
 // ConditionalUpdateResource updates an existing resource if revision matches.
 func (s *Service[T]) ConditionalUpdateResource(ctx context.Context, resource T) (T, error) {
 	var t T
-	item, err := s.MakeBackendItem(resource, resource.GetName())
+
+	if err := s.validateFunc(resource); err != nil {
+		return t, trace.Wrap(err)
+	}
+
+	item, err := s.MakeBackendItem(resource, s.keyFunc(resource))
 	if err != nil {
 		return t, trace.Wrap(err)
 	}
@@ -345,7 +387,12 @@ func (s *Service[T]) ConditionalUpdateResource(ctx context.Context, resource T) 
 // UpsertResource upserts a resource.
 func (s *Service[T]) UpsertResource(ctx context.Context, resource T) (T, error) {
 	var t T
-	item, err := s.MakeBackendItem(resource, resource.GetName())
+
+	if err := s.validateFunc(resource); err != nil {
+		return t, trace.Wrap(err)
+	}
+
+	item, err := s.MakeBackendItem(resource, s.keyFunc(resource))
 	if err != nil {
 		return t, trace.Wrap(err)
 	}
@@ -394,8 +441,11 @@ func (s *Service[T]) UpdateAndSwapResource(ctx context.Context, name string, mod
 		return t, trace.Wrap(err)
 	}
 
-	err = modify(resource)
-	if err != nil {
+	if err := modify(resource); err != nil {
+		return t, trace.Wrap(err)
+	}
+
+	if err := s.validateFunc(resource); err != nil {
 		return t, trace.Wrap(err)
 	}
 
