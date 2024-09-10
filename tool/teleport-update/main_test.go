beaadda8b7a0f2e7ad2d92cb6a3a30d575eed5c8
Stephen Levine <stephen.levine@goteleport.com>
2024-09-09 20:55:32 -0400

    test

---
+++ tool/teleport-update/main_test.go
@@ -0,0 +1,121 @@
+/*
+ * Teleport
+ * Copyright (C) 2024  Gravitational, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
+
+	"github.com/gravitational/teleport/lib/utils/golden"
+)
+
+func TestRun_Disable(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		args     []string
+		cfg      *UpdatesConfig // nil -> file not present
+		errMatch string
+	}{
+		{
+			name: "enabled",
+			args: []string{"disable"},
+			cfg: &UpdatesConfig{
+				Version: updatesVersion,
+				Kind:    updatesKind,
+				Spec: UpdatesSpec{
+					Enabled: true,
+				},
+			},
+		},
+		{
+			name: "already disabled",
+			args: []string{"disable"},
+			cfg: &UpdatesConfig{
+				Version: updatesVersion,
+				Kind:    updatesKind,
+				Spec: UpdatesSpec{
+					Enabled: false,
+				},
+			},
+		},
+		{
+			name: "config does not exist",
+			args: []string{"disable"},
+		},
+		{
+			name: "invalid metadata",
+			args: []string{"disable"},
+			cfg: &UpdatesConfig{
+				Spec: UpdatesSpec{
+					Enabled: true,
+				},
+			},
+			errMatch: "invalid",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			dir := t.TempDir()
+			path := filepath.Join(dir, versionsDirName, updatesFileName)
+
+			// Create config file only if provided in test case
+			if tt.cfg != nil {
+				err := os.MkdirAll(filepath.Dir(path), 0777)
+				require.NoError(t, err)
+				b, err := yaml.Marshal(tt.cfg)
+				require.NoError(t, err)
+				err = os.WriteFile(path, b, 0600)
+				require.NoError(t, err)
+			}
+
+			args := append(tt.args, []string{"--data-dir", dir}...)
+			err := Run(args)
+
+			if tt.errMatch != "" {
+				require.Error(t, err)
+				assert.Contains(t, err.Error(), tt.errMatch)
+				return
+			}
+			require.NoError(t, err)
+
+			data, err := os.ReadFile(path)
+
+			// If no config is present, disable should not create it
+			if tt.cfg == nil {
+				require.ErrorIs(t, err, os.ErrNotExist)
+				return
+			}
+			require.NoError(t, err)
+
+			if golden.ShouldSet() {
+				golden.Set(t, data)
+			}
+			require.Equal(t, string(golden.Get(t)), string(data))
+		})
+	}
+}
