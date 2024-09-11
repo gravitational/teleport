34437712fdbcd01cfb32af4f27c17396e1ded527
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2024-09-11 21:32:52 +0000

    Allow cluster wide ssh dial timeout to be set in config file (#46458) (#46508)
    Adds a new `auth_service.ssh_dial_timeout` option to the config
    file. The cluster networking configuration is overridden during
    Auth initialization if the config file has any networking options
    present. Only allowing the SSH dial timeout to be set dynamically
    caused the value to be reset on each Auth restart.

--- lib/config/configuration_test.go
+++ lib/config/configuration_test.go
@@ -341,6 +341,7 @@ func TestConfigReading(t *testing.T) {
 			WebIdleTimeout:        types.Duration(19 * time.Second),
 			RoutingStrategy:       types.RoutingStrategy_MOST_RECENT,
 			ProxyPingInterval:     types.Duration(10 * time.Second),
+			SSHDialTimeout:        types.Duration(45 * time.Second),
 		},
 		SSH: SSH{
 			Service: Service{
@@ -1495,6 +1496,7 @@ func makeConfigFixture() string {
 	conf.Auth.DisconnectExpiredCert = types.NewBoolOption(true)
 	conf.Auth.RoutingStrategy = types.RoutingStrategy_MOST_RECENT
 	conf.Auth.ProxyPingInterval = types.NewDuration(10 * time.Second)
+	conf.Auth.SSHDialTimeout = types.NewDuration(45 * time.Second)
 
 	// ssh service:
 	conf.SSH.EnabledFlag = "true"
