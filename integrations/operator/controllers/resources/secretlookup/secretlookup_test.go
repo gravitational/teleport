964ec784d4562967ab35f23aa67bc898909c312b
hugoShaka <hugo.hervieux@goteleport.com>
2024-09-12 16:56:59 -0400

    operator: Add secretlookup lib

---
+++ integrations/operator/controllers/resources/secretlookup/secretlookup_test.go
@@ -0,0 +1,237 @@
+package secretlookup
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+func TestLookupSecret(t *testing.T) {
+	// Test setup: crafting fixtures
+	ctx := context.Background()
+	namespace := "foo"
+	crName := "test-cr-name"
+	secretName := "test-secret"
+	keyName := "test-key-name"
+	keyValue := "test-key-value"
+	b64KeyValue := make([]byte, base64.StdEncoding.EncodedLen(len(keyValue)))
+	base64.StdEncoding.Encode(b64KeyValue, []byte(keyValue))
+
+	secretWithAnnotations := func(annotations map[string]string) v1.Secret {
+		return v1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:        secretName,
+				Namespace:   namespace,
+				Annotations: annotations,
+			},
+			Data: map[string][]byte{
+				keyName: b64KeyValue,
+			},
+			StringData: nil,
+			Type:       v1.SecretTypeOpaque,
+		}
+	}
+	okAnnotations := map[string]string{AllowInclusionAnnotation: strings.Join([]string{"other-cr-name", crName}, ", ")}
+	okSecret := secretWithAnnotations(okAnnotations)
+
+	// Test setup: defining test cases
+	tests := []struct {
+		name      string
+		input     string
+		secrets   v1.SecretList
+		expect    string
+		assertErr require.ErrorAssertionFunc
+	}{
+		{
+			name:    "not an uri",
+			input:   "secret://How do you do, fellow kids?",
+			secrets: v1.SecretList{Items: []v1.Secret{okSecret}},
+			assertErr: func(t require.TestingT, err error, i ...interface{}) {
+				require.ErrorContains(t, err, "parse")
+			},
+		},
+		{
+			name:    "uri does not contain secret name",
+			input:   fmt.Sprintf("secret:///%s", keyName),
+			secrets: v1.SecretList{Items: []v1.Secret{okSecret}},
+			assertErr: func(t require.TestingT, err error, i ...interface{}) {
+				require.ErrorContains(t, err, "missing secret name")
+			},
+		},
+		{
+			name:    "uri does not contain secret key",
+			input:   fmt.Sprintf("secret://%s", secretName),
+			secrets: v1.SecretList{Items: []v1.Secret{okSecret}},
+			assertErr: func(t require.TestingT, err error, i ...interface{}) {
+				require.ErrorContains(t, err, "missing secret key")
+			},
+		},
+		{
+			name:    "secret does not exist",
+			input:   fmt.Sprintf("secret://%s/%s", secretName, keyName),
+			secrets: v1.SecretList{Items: []v1.Secret{}},
+			assertErr: func(t require.TestingT, err error, i ...interface{}) {
+				require.ErrorContains(t, err, "not found")
+			},
+		},
+		{
+			name:    "secret has no annotation",
+			input:   fmt.Sprintf("secret://%s/%s", secretName, keyName),
+			secrets: v1.SecretList{Items: []v1.Secret{secretWithAnnotations(nil)}},
+			assertErr: func(t require.TestingT, err error, i ...interface{}) {
+				require.ErrorContains(t, err, "no annotation")
+			},
+		},
+		{
+			name:    "secret annotations don't allow inclusion",
+			input:   fmt.Sprintf("secret://%s/%s", secretName, keyName),
+			secrets: v1.SecretList{Items: []v1.Secret{secretWithAnnotations(map[string]string{AllowInclusionAnnotation: "not-the-right-cr"})}},
+			assertErr: func(t require.TestingT, err error, i ...interface{}) {
+				require.ErrorContains(t, err, "does not contain")
+			},
+		},
+		{
+			name:  "secret is missing the key",
+			input: fmt.Sprintf("secret://%s/%s", secretName, keyName),
+			secrets: v1.SecretList{
+				Items: []v1.Secret{
+					{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:        secretName,
+							Namespace:   namespace,
+							Annotations: okAnnotations,
+						},
+						Data: map[string][]byte{
+							"wrong-key-name": b64KeyValue,
+						},
+						StringData: nil,
+						Type:       v1.SecretTypeOpaque,
+					},
+				},
+			},
+			assertErr: func(t require.TestingT, err error, i ...interface{}) {
+				require.ErrorContains(t, err, "is missing key")
+			},
+		},
+		{
+			name:      "successful lookup",
+			input:     fmt.Sprintf("secret://%s/%s", secretName, keyName),
+			secrets:   v1.SecretList{Items: []v1.Secret{okSecret}},
+			expect:    keyValue,
+			assertErr: require.NoError,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Test setup: creating a mock with the fixtures
+			clientBuilder := fake.NewClientBuilder()
+			clientBuilder.WithLists(&tt.secrets)
+			fakeClient := clientBuilder.Build()
+
+			// Test execution
+			result, err := Try(ctx, fakeClient, crName, namespace, tt.input)
+			t.Log(err)
+			tt.assertErr(t, err)
+			require.Equal(t, tt.expect, result)
+		})
+	}
+}
+
+func Test_isInclusionAllowed(t *testing.T) {
+	crName := "test-cr-name"
+	tests := []struct {
+		name      string
+		secret    v1.Secret
+		assertErr require.ErrorAssertionFunc
+	}{
+		{
+			name: "No annotation",
+			secret: v1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: nil,
+				},
+			},
+			assertErr: require.Error,
+		},
+		{
+			name: "Missing inclusion annotation",
+			secret: v1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						"foo": "bar",
+					},
+				},
+			},
+			assertErr: require.Error,
+		},
+		{
+			name: "Empty inclusion annotation",
+			secret: v1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						AllowInclusionAnnotation: "",
+					},
+				},
+			},
+			assertErr: require.Error,
+		},
+		{
+			name: "Not matching inclusion annotation",
+			secret: v1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						AllowInclusionAnnotation: "foo, bar",
+					},
+				},
+			},
+			assertErr: require.Error,
+		},
+		{
+			name: "Single-item matching annotation",
+			secret: v1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						AllowInclusionAnnotation: crName,
+					},
+				},
+			},
+			assertErr: require.NoError,
+		},
+		{
+			name: "Multi-item matching annotation",
+			secret: v1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						AllowInclusionAnnotation: strings.Join([]string{"foo", "bar", crName}, ", "),
+					},
+				},
+			},
+			assertErr: require.NoError,
+		},
+		{
+			name: "Wildcard annotation",
+			secret: v1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						AllowInclusionAnnotation: "*",
+					},
+				},
+			},
+			assertErr: require.NoError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := isInclusionAllowed(&tt.secret, crName)
+			tt.assertErr(t, result)
+		})
+	}
+}
