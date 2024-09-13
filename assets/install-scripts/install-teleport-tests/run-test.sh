62a53ff2e6051623708c83e89688ec94140248fe
Paul Gottschling <paul.gottschling@goteleport.com>
2024-09-13 16:20:53 -0400

    Get version assertion to work

--- assets/install-scripts/install-teleport-tests/run-test.sh
+++ assets/install-scripts/install-teleport-tests/run-test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# Script parameters:
 # $1: Teleport version (e.g., 10.11.12)
 # $2: edition: cloud, team, oss, or enterprise
+
 function test_teleport() {
     if ! type teleport; then
       echo "INSTALL_SCRIPT_TEST_FAILURE: teleport not found"
@@ -31,10 +33,11 @@ function test_upgrader() {
     fi
 }
 
+# $1: Expected version
 function test_teleport_version() {
-    VER=$(teleport version | sed -E 's|.*Teleport (Enterprise)? v([0-9]+\.[0-9]+\.[0-9]+).*|\2|');
+    VER=$(teleport version | sed -E 's|.*Teleport (Enterprise )?v([0-9]+\.[0-9]+\.[0-9]+).*|\2|');
     if [ "$VER" != "$1" ]; then
-      echo "INSTALL_SCRIPT_TEST_FAILURE: expected teleport to have version $VER but got $1"
+      echo "INSTALL_SCRIPT_TEST_FAILURE: expected teleport to have version $1 but got $VER"
     fi
 }
 
@@ -57,21 +60,21 @@ case $2 in
     	test_tsh
     	test_tbot
     	test_upgrader
-    	test_teleport_version
+    	test_teleport_version $1
     ;;
     oss)
     	test_teleport
     	test_tctl
     	test_tsh
     	test_tbot
-    	test_teleport_version
+    	test_teleport_version $1
     ;;
     enterprise)
     	test_teleport
     	test_tctl
     	test_tsh
     	test_tbot
-    	test_teleport_version
+    	test_teleport_version $1
     ;;
     *)
     	echo "INSTALL_SCRIPT_TEST_FAILURE: unsupported edition $2"
