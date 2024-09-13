809827d0268280b8f505564a67fd0399b6a481bd
Rafał Cieślak <rafal.cieslak@goteleport.com>
2024-09-13 13:56:38 +0200

    Rewrite err when downloading dir through Web UI and Connect

--- lib/sshutils/sftp/sftp.go
+++ lib/sshutils/sftp/sftp.go
@@ -412,7 +412,7 @@ func (c *Config) transfer(ctx context.Context) error {
 			if fi.IsDir() && !c.opts.Recursive {
 				// Note: Using an error constructor included in lib/client.IsErrorResolvableWithRelogin,
 				// e.g. BadParameter, will lead to relogin attempt and a completely obscure error message.
-				return trace.Errorf("%q is a directory, but the recursive option was not passed", match)
+				return trace.Wrap(&NonRecursiveDirectoryTransferError{Path: match})
 			}
 			fileInfos = append(fileInfos, fi)
 		}
@@ -665,4 +665,16 @@ func NewProgressBar(size int64, desc string, writer io.Writer) *progressbar.Prog
 		progressbar.OptionFullWidth(),
 		progressbar.OptionSetRenderBlankState(true),
 	)
+}
+
+// NonRecursiveDirectoryTransferError is returned when an attempt is made
+// to download a directory without providing the recursive option.
+// It's used to distinguish this specific situation in clients which
+// do not support the recursive option.
+type NonRecursiveDirectoryTransferError struct {
+	Path string
+}
+
+func (n *NonRecursiveDirectoryTransferError) Error() string {
+	return fmt.Sprintf("%q is a directory, but the recursive option was not passed", n.Path)
 }
