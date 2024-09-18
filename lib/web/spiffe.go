88ec46b739a14930201bbf95f60f7e89a948e4fe
Noah Stride <noah.stride@goteleport.com>
2024-09-18 11:26:38 +0100

    Expose local SPIFFE CA's JWT keypairs in SPIFFE trust bundles

--- lib/web/spiffe.go
+++ lib/web/spiffe.go
@@ -26,6 +26,8 @@ import (
 	"github.com/spiffe/go-spiffe/v2/spiffeid"
 
 	"github.com/gravitational/teleport/api/types"
+	"github.com/gravitational/teleport/api/utils/keys"
+	"github.com/gravitational/teleport/lib/jwt"
 	"github.com/gravitational/teleport/lib/services"
 	"github.com/gravitational/teleport/lib/tlsca"
 )
@@ -71,12 +73,28 @@ func (h *Handler) getSPIFFEBundle(w http.ResponseWriter, r *http.Request, _ http
 		return nil, trace.Wrap(err, "fetching SPIFFE CA")
 	}
 
+	// Add X509 authorities to the trust bundle.
 	for _, certPEM := range services.GetTLSCerts(spiffeCA) {
 		cert, err := tlsca.ParseCertificatePEM(certPEM)
 		if err != nil {
 			return nil, trace.Wrap(err, "parsing certificate")
 		}
 		bundle.AddX509Authority(cert)
+	}
+
+	// Add JWT authorities to the trust bundle.
+	for _, keyPair := range spiffeCA.GetTrustedJWTKeyPairs() {
+		pubKey, err := keys.ParsePublicKey(keyPair.PublicKey)
+		if err != nil {
+			return nil, trace.Wrap(err, "parsing public key")
+		}
+		kid, err := jwt.KeyID(pubKey)
+		if err != nil {
+			return nil, trace.Wrap(err, "generating key ID")
+		}
+		if err := bundle.AddJWTAuthority(kid, pubKey); err != nil {
+			return nil, trace.Wrap(err, "adding JWT authority to bundle")
+		}
 	}
 
 	bundleBytes, err := bundle.Marshal()
