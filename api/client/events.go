98177f3c3fd8ce5b0cc506d556600a79773e594c
Marco Dinis <marco.dinis@goteleport.com>
2024-09-19 10:56:41 +0100

    User Tasks: services and clients implementation (#46131)
    This PR adds the implementation for the User Tasks:
    - services (backend+cache)
    - clients (API + tctl)
    - light validation to set up the path for later PRs

--- api/client/events.go
+++ api/client/events.go
@@ -27,6 +27,7 @@ import (
 	machineidv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/machineid/v1"
 	notificationsv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/notifications/v1"
 	userprovisioningpb "github.com/gravitational/teleport/api/gen/proto/go/teleport/userprovisioning/v2"
+	usertasksv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/usertasks/v1"
 	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/api/types/accesslist"
 	accesslistv1conv "github.com/gravitational/teleport/api/types/accesslist/convert/v1"
@@ -107,6 +108,10 @@ func EventToGRPC(in types.Event) (*proto.Event, error) {
 		case *autoupdate.AutoUpdateVersion:
 			out.Resource = &proto.Event_AutoUpdateVersion{
 				AutoUpdateVersion: r,
+			}
+		case *usertasksv1.UserTask:
+			out.Resource = &proto.Event_UserTask{
+				UserTask: r,
 			}
 		default:
 			return nil, trace.BadParameter("resource type %T is not supported", r)
@@ -555,6 +560,9 @@ func EventFromGRPC(in *proto.Event) (*types.Event, error) {
 		out.Resource = types.Resource153ToLegacy(r)
 		return &out, nil
 	} else if r := in.GetAutoUpdateVersion(); r != nil {
+		out.Resource = types.Resource153ToLegacy(r)
+		return &out, nil
+	} else if r := in.GetUserTask(); r != nil {
 		out.Resource = types.Resource153ToLegacy(r)
 		return &out, nil
 	} else {
