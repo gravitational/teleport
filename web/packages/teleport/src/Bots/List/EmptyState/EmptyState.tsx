26f5d05e862f9d8ae975232d2b26681cde66878e
Michael Myers <michael.myers@goteleport.com>
2024-09-19 10:41:43 -0500

    Add Bots EmptyState component

---
+++ web/packages/teleport/src/Bots/List/EmptyState/EmptyState.tsx
@@ -0,0 +1,206 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { GitHubIcon, SpaceliftIcon, AnsibleIcon } from 'design/SVGIcon';
+import { ResourceIcon } from 'design/ResourceIcon';
+import { Theme } from 'gen-proto-ts/teleport/userpreferences/v1/theme_pb';
+import {
+  Description,
+  DetailsTab,
+  Feature,
+  FeatureContainer,
+  FeatureProps,
+  FeatureSlider,
+  Title,
+} from 'shared/components/EmptyState/EmptyState';
+import { Box, ButtonPrimary, Flex, H1, Image, Text } from 'design';
+
+import cfg from 'teleport/config';
+import { DisplayTile } from 'teleport/Bots/Add/AddBotsPicker';
+
+import elimiateSecretsImage from './eliminate-secrets.svg';
+import elimiateSecretsLightImage from './eliminate-secrets-light.svg';
+
+import controlWorkflowsImage from './control-workflows.svg';
+import controlWorkflowsLightImage from './control-workflows-light.svg';
+
+import argoCD from './argocd.png';
+
+const maxWidth = '1204px';
+
+export function EmptyState({ theme }: { theme: Theme }) {
+  const [currIndex, setCurrIndex] = useState(0);
+  const [intervalId, setIntervalId] = useState<any>();
+
+  function handleOnClick(clickedIndex: number) {
+    clearInterval(intervalId);
+    setCurrIndex(clickedIndex);
+    setIntervalId(null);
+  }
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCurrIndex(latestIndex => (latestIndex + 1) % 3);
+    }, 3000);
+    setIntervalId(id);
+    return () => clearInterval(id);
+  }, []);
+
+  return (
+    <Box mt={4}>
+      <Box mb={3}>
+        <H1 mb={3}>What are Bots?</H1>
+        <Text css={{ maxWidth }}>
+          Static keys and API keys in your automated workflows are the target of
+          hackers and are one of the key sources of security breaches.
+          <br />
+          Teleport Machine ID replaces shared credentials and secrets with
+          short-lived x.509 or SSH certificates and gives you a unified plan to
+          register, define access policies, and audit all your workflows.
+        </Text>
+      </Box>
+      <FeatureContainer py={2} pr={2}>
+        <Box css={{ position: 'relative' }}>
+          <FeatureSlider $currIndex={currIndex} />
+          <DetailsTab
+            active={currIndex === 0}
+            isSliding={!!intervalId}
+            onClick={() => handleOnClick(0)}
+            title="Eliminate secrets and shared credentials from CI/CD workflows"
+            description="Teleport Machine ID replaces passwords, API, and static keys with short-lived SSH and x.509 certificates."
+          />
+          <DetailsTab
+            active={currIndex === 1}
+            isSliding={!!intervalId}
+            onClick={() => handleOnClick(1)}
+            title="Control all your workflows from a unified plane."
+            description="Unify access policies and get structured audit events for all automatic workflows on your infrastructure. Lock out and terminate connections to potentially compromised workflows."
+          />
+          <DetailsTab
+            active={currIndex === 2}
+            isSliding={!!intervalId}
+            onClick={() => handleOnClick(2)}
+            title="Works with everything you have"
+            description="Connect your ArgoCD, Jenkins, Spacelift, GitHub Actions, Ansible, Terraform, and more."
+          />
+        </Box>
+        <Box mt={-2} height={330}>
+          {currIndex === 0 && <EliminateSecretsPreview theme={theme} />}
+          {currIndex === 1 && <ControlWorkflowsPreview theme={theme} />}
+          {currIndex === 2 && <BotTiles />}
+        </Box>
+      </FeatureContainer>
+      {/* setting a max width here to keep it "in the center" with the content above instead of with the screen */}
+      <Box width="100%" maxWidth={maxWidth} textAlign="center" mt={6}>
+        <ButtonPrimary
+          width="280px"
+          as={Link}
+          to={cfg.getBotsNewRoute()}
+          size="large"
+        >
+          Create Your First Bot
+        </ButtonPrimary>
+      </Box>
+    </Box>
+  );
+}
+
+export const EliminateSecrets = ({
+  active,
+  onClick,
+  isSliding,
+}: FeatureProps) => {
+  return (
+    <Feature $active={active} onClick={onClick} $isSliding={isSliding}>
+      <Title>
+        Eliminate secrets and shared credentials from CI/CD workflows.
+      </Title>
+      <Description>
+        Teleport Machine ID replaces passwords, API, and static keys with
+        short-lived SSH and x.509 certificates.
+      </Description>
+    </Feature>
+  );
+};
+
+const eliminateSecretsImages = {
+  [Theme.LIGHT]: elimiateSecretsLightImage,
+  [Theme.DARK]: elimiateSecretsImage,
+};
+
+export const EliminateSecretsPreview = ({ theme }: { theme: Theme }) => {
+  return (
+    <PreviewBox>
+      <Image maxHeight="100%" src={eliminateSecretsImages[theme]} />
+    </PreviewBox>
+  );
+};
+
+const controlWorkflowsImages = {
+  [Theme.LIGHT]: controlWorkflowsLightImage,
+  [Theme.DARK]: controlWorkflowsImage,
+};
+
+export const ControlWorkflowsPreview = ({ theme }: { theme: Theme }) => {
+  return (
+    <PreviewBox>
+      <Image maxHeight="100%" src={controlWorkflowsImages[theme]} />
+    </PreviewBox>
+  );
+};
+
+const TILE_ICON_HEIGHT = 80;
+
+// These logos don't have dark/light mode variants, or are
+// not in our system and are exported as PNG by design team
+const integrationsTop = [
+  {
+    title: 'Jenkins',
+    icon: <ResourceIcon height={TILE_ICON_HEIGHT} name="jenkins" />,
+  },
+  {
+    title: 'Terraform',
+    icon: <ResourceIcon height={TILE_ICON_HEIGHT} name="terraform" />,
+  },
+  {
+    title: 'Argo CD',
+    icon: <Image height={TILE_ICON_HEIGHT} src={argoCD} />,
+  },
+];
+
+const integrationsBottom = [
+  {
+    title: 'GitHub',
+    icon: <GitHubIcon size={TILE_ICON_HEIGHT} />,
+  },
+  {
+    title: 'Ansible',
+    icon: <AnsibleIcon size={TILE_ICON_HEIGHT} />,
+  },
+  {
+    title: 'Spacelift',
+    icon: <SpaceliftIcon size={TILE_ICON_HEIGHT} />,
+  },
+];
+
+export const BotTiles = () => {
+  return (
+    <PreviewBox>
+      <Flex>
+        {integrationsTop.map(integration => (
+          <DisplayTile icon={integration.icon} title={integration.title} />
+        ))}
+      </Flex>
+      <Flex>
+        {integrationsBottom.map(integration => (
+          <DisplayTile icon={integration.icon} title={integration.title} />
+        ))}
+      </Flex>
+    </PreviewBox>
+  );
+};
+
+const PreviewBox = styled(Box)`
+  margin-left: ${p => p.theme.space[5]}px;
+  max-height: 330px;
+`;
