aa6e3df44bc1062dcea79b103c7cdc338ccc44d0
Rafał Cieślak <rafal.cieslak@goteleport.com>
2024-09-20 12:03:59 +0200

    Disable agent forwarding by default in Connect

--- web/packages/teleterm/src/services/config/appConfigSchema.ts
+++ web/packages/teleterm/src/services/config/appConfigSchema.ts
@@ -190,7 +190,7 @@ export const createAppConfigSchema = (settings: RuntimeSettings) => {
       .describe('Disables SSH connection resumption.'),
     'ssh.forwardAgent': z
       .boolean()
-      .default(true)
+      .default(false)
       .describe(
         "Enables agent forwarding when connecting to SSH nodes. It's the equivalent of the forward-agent flag in tsh ssh."
       ),
