53c2f7955f48308dff078956b0399e2ee5f96dc9
mcbattirola <matheus.battirola@goteleport.com>
2024-09-25 10:48:31 -0300

    Improve startFeatureWatcher godocs

--- lib/web/features.go
+++ lib/web/features.go
@@ -43,6 +43,8 @@ func (h *Handler) GetClusterFeatures() proto.Features {
 // startFeatureWatcher periodically pings the auth server and updates `clusterFeatures`.
 // Must be called only once per `handler`, otherwise it may close an already closed channel
 // which will cause a panic.
+// The watcher doesn't ping the auth server immediately upon start because features are
+// already set by the config object in `NewHandler`.
 func (h *Handler) startFeatureWatcher() {
 	ticker := h.clock.NewTicker(h.cfg.FeatureWatchInterval)
 	h.log.WithField("interval", h.cfg.FeatureWatchInterval).Info("Proxy handler features watcher has started")
