40393b83fd25982687de8829b3c3d0a71274088f
Bernard Kim <bernard@goteleport.com>
2024-10-03 03:48:01 -0700

    Set UsePreviewAsRoles field (#47112)

--- api/client/client.go
+++ api/client/client.go
@@ -4185,6 +4185,7 @@ func GetResourcesWithFilters(ctx context.Context, clt ListResourcesClient, req p
 			SearchKeywords:      req.SearchKeywords,
 			PredicateExpression: req.PredicateExpression,
 			UseSearchAsRoles:    req.UseSearchAsRoles,
+			UsePreviewAsRoles:   req.UsePreviewAsRoles,
 		})
 		if err != nil {
 			if trace.IsLimitExceeded(err) {
