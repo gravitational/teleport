991db458095c983bc98cc5d72ffae53807b2f344
joerger <bjoerger@goteleport.com>
2024-10-03 12:51:04 -0700

    Unify and simplify MFA Ceremony helpers (#46986)
    * Refactor MFA ceremony helpers.
    
    * Refactor session MFA ceremony to use new MFA ceremony helpers.
    
    * Simplify calls to NewMFACeremony.
    
    * Remove remaining usage of tc.PromptMFA in favor of Ceremony.
    
    * Rename prompt constructor.
    
    * Add godoc to ceremony; update tests.
    
    * Cleanup.
    
    * Resolve comments; fix tests.
    
    * Update comments.
    
    * Fix test.
    
    * Fix lint.

--- lib/client/presence.go
+++ lib/client/presence.go
@@ -76,46 +76,58 @@ func RunPresenceTask(ctx context.Context, term io.Writer, maintainer PresenceMai
 		return trace.Wrap(err)
 	}
 
-	for {
-		select {
-		case <-ticker.Chan():
+	mfaCeremony := &mfa.Ceremony{
+		CreateAuthenticateChallenge: func(ctx context.Context, _ *proto.CreateAuthenticateChallengeRequest) (*proto.MFAAuthenticateChallenge, error) {
 			req := &proto.PresenceMFAChallengeSend{
 				Request: &proto.PresenceMFAChallengeSend_ChallengeRequest{
 					ChallengeRequest: &proto.PresenceMFAChallengeRequest{SessionID: sessionID},
 				},
 			}
 
-			err = stream.Send(req)
-			if err != nil {
-				return trace.Wrap(err)
+			if err := stream.Send(req); err != nil {
+				return nil, trace.Wrap(err)
 			}
 
 			challenge, err := stream.Recv()
 			if err != nil {
-				return trace.Wrap(err)
+				return nil, trace.Wrap(err)
 			}
-
-			fmt.Fprint(term, "\r\nTeleport > Please tap your MFA key\r\n")
 
 			// This is here to enforce the usage of a MFA device.
 			// We don't support TOTP for live presence.
 			challenge.TOTP = nil
 
-			solution, err := mfaPrompt.Run(ctx, challenge)
+			return challenge, nil
+		},
+		SolveAuthenticateChallenge: func(ctx context.Context, chal *proto.MFAAuthenticateChallenge) (*proto.MFAAuthenticateResponse, error) {
+			fmt.Fprint(term, "\r\nTeleport > Please tap your MFA key\r\n")
+
+			mfaResp, err := mfaPrompt.Run(ctx, chal)
 			if err != nil {
 				fmt.Fprintf(term, "\r\nTeleport > Failed to confirm presence: %v\r\n", err)
-				return trace.Wrap(err)
+				return nil, trace.Wrap(err)
 			}
 
 			fmt.Fprint(term, "\r\nTeleport > Received MFA presence confirmation\r\n")
+			return mfaResp, nil
+		},
+	}
 
-			req = &proto.PresenceMFAChallengeSend{
+	for {
+		select {
+		case <-ticker.Chan():
+			mfaResp, err := mfaCeremony.Run(ctx, nil /* req is not needed for MaintainSessionPresence */)
+			if err != nil {
+				return trace.Wrap(err)
+			}
+
+			resp := &proto.PresenceMFAChallengeSend{
 				Request: &proto.PresenceMFAChallengeSend_ChallengeResponse{
-					ChallengeResponse: solution,
+					ChallengeResponse: mfaResp,
 				},
 			}
 
-			err = stream.Send(req)
+			err = stream.Send(resp)
 			if err != nil {
 				return trace.Wrap(err)
 			}
