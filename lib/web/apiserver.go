e0326f375b225d9d26a709de02e3a78df7a06bf4
Lisa Kim <lisa@goteleport.com>
2024-10-03 15:29:19 -0700

    Return request mode with get web user context

--- lib/web/apiserver.go
+++ lib/web/apiserver.go
@@ -1195,6 +1195,7 @@ func (h *Handler) getUserContext(w http.ResponseWriter, r *http.Request, p httpr
 	userContext.AccessCapabilities = ui.AccessCapabilities{
 		RequestableRoles:   res.RequestableRoles,
 		SuggestedReviewers: res.SuggestedReviewers,
+		RequestMode:        ui.RequestMode{KubernetesResources: res.RequestMode.KubernetesResources},
 	}
 
 	userContext.AllowedSearchAsRoles = accessChecker.GetAllowedSearchAsRoles()
