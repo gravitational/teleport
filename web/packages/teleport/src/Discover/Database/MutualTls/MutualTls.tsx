f2c674695d4613ebe4f1d256c6c728a375d4559c
Lisa Kim <lisa@goteleport.com>
2024-10-02 17:15:36 -0700

    Refactor restart database messaging

--- web/packages/teleport/src/Discover/Database/MutualTls/MutualTls.tsx
+++ web/packages/teleport/src/Discover/Database/MutualTls/MutualTls.tsx
@@ -160,17 +160,7 @@ function DbEngineInstructions({ dbEngine }: { dbEngine: DatabaseEngine }) {
             },
           ]}
         />
-        <Text mt={1}>
-          Restart the database server to apply the configuration. The
-          certificate is valid for 90 days so this will require installing an{' '}
-          <Link
-            href="https://goteleport.com/docs/enroll-resources/database-access/enroll-self-hosted-databases/postgres-self-hosted/#step-25-create-a-certificatekey-pair"
-            target="_blank"
-          >
-            updated certificate
-          </Link>{' '}
-          and restarting the database server before that to continue access.
-        </Text>
+        <RestartDatabaseText link="https://goteleport.com/docs/enroll-resources/database-access/enroll-self-hosted-databases/postgres-self-hosted/#step-25-create-a-certificatekey-pair" />
       </Box>
     );
   }
@@ -259,19 +249,7 @@ function DbEngineInstructions({ dbEngine }: { dbEngine: DatabaseEngine }) {
                       },
                     ]}
                   />
-                  <Text mt={1}>
-                    Restart the database server to apply the configuration. The
-                    certificate is valid for 90 days so this will require
-                    installing an{' '}
-                    <Link
-                      href="https://goteleport.com/docs/enroll-resources/database-access/enroll-self-hosted-databases/mysql-self-hosted/#step-24-create-a-certificatekey-pair"
-                      target="_blank"
-                    >
-                      updated certificate
-                    </Link>{' '}
-                    and restarting the database server before that to continue
-                    access.
-                  </Text>
+                  <RestartDatabaseText link="https://goteleport.com/docs/enroll-resources/database-access/enroll-self-hosted-databases/mysql-self-hosted/#step-24-create-a-certificatekey-pair" />
                   <Text mt={2}>
                     See{' '}
                     <Link
@@ -302,19 +280,7 @@ function DbEngineInstructions({ dbEngine }: { dbEngine: DatabaseEngine }) {
                       },
                     ]}
                   />
-                  <Text mt={1}>
-                    Restart the database server to apply the configuration. The
-                    certificate is valid for 90 days so this will require
-                    installing an{' '}
-                    <Link
-                      href="https://goteleport.com/docs/enroll-resources/database-access/enroll-self-hosted-databases/mysql-self-hosted/#step-24-create-a-certificatekey-pair"
-                      target="_blank"
-                    >
-                      updated certificate
-                    </Link>{' '}
-                    and restarting the database server before that to continue
-                    access.
-                  </Text>
+                  <RestartDatabaseText link="https://goteleport.com/docs/enroll-resources/database-access/enroll-self-hosted-databases/mysql-self-hosted/#step-24-create-a-certificatekey-pair" />
                   <Text mt={2}>
                     See{' '}
                     <Link
@@ -334,3 +300,14 @@ function DbEngineInstructions({ dbEngine }: { dbEngine: DatabaseEngine }) {
     );
   }
 }
+
+const RestartDatabaseText = ({ link }: { link: string }) => (
+  <Text mt={1}>
+    Restart the database server to apply the configuration. The certificate is
+    valid for 90 days so this will require installing an{' '}
+    <Link href={link} target="_blank">
+      updated certificate
+    </Link>{' '}
+    and restarting the database server before that to continue access.
+  </Text>
+);
