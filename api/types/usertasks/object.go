76d630a64a72ba97df5ccf58ce98afe84e479c52
Marco Dinis <marco.dinis@goteleport.com>
2024-10-04 12:05:27 +0100

    User Tasks: add `discover-ec2` task type (#47062)
    * User Tasks: add `discover-ec2` task type
    
    This PR adds more business logic into the User Tasks, in particular into
    the `discover-ec2` task type.
    
    One of the key features of the DiscoverEC2 User Tasks is that we must
    have a single task per:
    - integration
    - region
    - account id
    - issue type
    
    This allows user to have a detailed view of the issues their are facing
    but still grouping EC2 instances.
    
    To do this, we had to move the region and account id up one level.
    Previously they were at the instance level, and it would require
    iterating over them to actually create the group (uniq key) we want.
    
    This also adds well known errors as issue types to ensure we validate
    them.
    
    A later PR will come where we actually start creating/updating
    DiscoverEC2 User Tasks from the DiscoveryService.
    
    * use strings.Compare
    
    * improve docs and fix typos

--- api/types/usertasks/object.go
+++ api/types/usertasks/object.go
@@ -19,31 +19,65 @@
 package usertasks
 
 import (
+	"encoding/binary"
+	"slices"
+	"time"
+
+	"github.com/google/uuid"
 	"github.com/gravitational/trace"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	headerv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/header/v1"
 	usertasksv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/usertasks/v1"
 	"github.com/gravitational/teleport/api/types"
 )
 
-// NewUserTask creates a new UserTask object.
-// It validates the object before returning it.
-func NewUserTask(name string, spec *usertasksv1.UserTaskSpec) (*usertasksv1.UserTask, error) {
-	cj := &usertasksv1.UserTask{
+// UserTaskOption defines a function that mutates a User Task.
+type UserTaskOption func(ut *usertasksv1.UserTask)
+
+// WithExpiration sets the expiration of the UserTask resource.
+func WithExpiration(t time.Time) func(ut *usertasksv1.UserTask) {
+	return func(ut *usertasksv1.UserTask) {
+		ut.Metadata.Expires = timestamppb.New(t)
+	}
+}
+
+// NewDiscoverEC2UserTask creates a new DiscoverEC2 User Task Type.
+func NewDiscoverEC2UserTask(spec *usertasksv1.UserTaskSpec, opts ...UserTaskOption) (*usertasksv1.UserTask, error) {
+	taskName := TaskNameForDiscoverEC2(TaskNameForDiscoverEC2Parts{
+		Integration: spec.GetIntegration(),
+		IssueType:   spec.GetIssueType(),
+		AccountID:   spec.GetDiscoverEc2().GetAccountId(),
+		Region:      spec.GetDiscoverEc2().GetRegion(),
+	})
+
+	ut := &usertasksv1.UserTask{
 		Kind:    types.KindUserTask,
 		Version: types.V1,
 		Metadata: &headerv1.Metadata{
-			Name: name,
+			Name: taskName,
 		},
 		Spec: spec,
 	}
+	for _, o := range opts {
+		o(ut)
+	}
 
-	if err := ValidateUserTask(cj); err != nil {
+	if err := ValidateUserTask(ut); err != nil {
 		return nil, trace.Wrap(err)
 	}
 
-	return cj, nil
+	return ut, nil
 }
+
+const (
+	// TaskStateOpen identifies an issue with an instance that is not yet resolved.
+	TaskStateOpen = "OPEN"
+	// TaskStateResolved identifies an issue with an instance that is resolved.
+	TaskStateResolved = "RESOLVED"
+)
+
+var validTaskStates = []string{TaskStateOpen, TaskStateResolved}
 
 const (
 	// TaskTypeDiscoverEC2 identifies a User Tasks that is created
@@ -52,44 +86,170 @@ const (
 	TaskTypeDiscoverEC2 = "discover-ec2"
 )
 
+// List of Auto Discover EC2 issues identifiers.
+// This value is used to populate the UserTasks.Spec.IssueType for Discover EC2 tasks.
+// The Web UI will then use those identifiers to show detailed instructions on how to fix the issue.
+const (
+	// AutoDiscoverEC2IssueEICEFailedToCreateNode is used when the EICE flow fails to create a node.
+	// This can happen when the Node does not have a valid PrivateIPAddress.
+	// This is very unlikely and should only happen if the AWS API returns an unexpected response.
+	AutoDiscoverEC2IssueEICEFailedToCreateNode = "ec2-eice-create-node"
+
+	// AutoDiscoverEC2IssueEICEFailedToUpsertNode is used when the EICE flow fails to upsert a node into the cluster.
+	// This is very unlikely and should only happen
+	// - if the Discovery system role was changed
+	// - if the Node resource validation was changed on the Auth and not on the DiscoveryService
+	// - if Teleport backend is offline or in failing mode
+	// - or because of a network error
+	AutoDiscoverEC2IssueEICEFailedToUpsertNode = "ec2-eice-upsert-node"
+
+	// AutoDiscoverEC2IssueScriptInstanceNotRegistered is used to identify instances that failed to auto-enroll
+	// because they are not present in Amazon Systems Manager.
+	// This usually means that the Instance does not have the SSM Agent running,
+	// or that the instance's IAM Profile does not allow have the managed IAM Policy AmazonSSMManagedInstanceCore assigned to it.
+	AutoDiscoverEC2IssueScriptInstanceNotRegistered = "ec2-ssm-agent-not-registered"
+
+	// AutoDiscoverEC2IssueScriptInstanceConnectionLost is used to identify instances that failed to auto-enroll
+	// because the agent lost connection to Amazon Systems Manager.
+	// This can happen if the user changed some setting in the instance's network or IAM profile.
+	AutoDiscoverEC2IssueScriptInstanceConnectionLost = "ec2-ssm-agent-connection-lost"
+
+	// AutoDiscoverEC2IssueScriptInstanceUnsupportedOS is used to identify instances that failed to auto-enroll
+	// because its OS is not supported by teleport.
+	// This can happen if the instance is running Windows.
+	AutoDiscoverEC2IssueScriptInstanceUnsupportedOS = "ec2-ssm-unsupported-os"
+
+	// AutoDiscoverEC2IssueScriptFailure is used to identify instances that failed to auto-enroll
+	// because the installation script failed.
+	// The invocation url must be included in the report, so that users can see what was wrong.
+	AutoDiscoverEC2IssueScriptFailure = "ec2-ssm-script-failure"
+
+	// AutoDiscoverEC2IssueInvocationFailure is used to identify instances that failed to auto-enroll
+	// because the SSM Script Run (also known as Invocation) failed.
+	// This happens when there's a failure with permissions or an invalid configuration (eg, invalid document name).
+	AutoDiscoverEC2IssueInvocationFailure = "ec2-ssm-invocation-failure"
+)
+
+// discoverEC2IssueTypes is a list of issue types that can occur when trying to auto enroll EC2 instances.
+var discoverEC2IssueTypes = []string{
+	AutoDiscoverEC2IssueEICEFailedToCreateNode,
+	AutoDiscoverEC2IssueEICEFailedToUpsertNode,
+	AutoDiscoverEC2IssueScriptInstanceNotRegistered,
+	AutoDiscoverEC2IssueScriptInstanceConnectionLost,
+	AutoDiscoverEC2IssueScriptInstanceUnsupportedOS,
+	AutoDiscoverEC2IssueScriptFailure,
+	AutoDiscoverEC2IssueInvocationFailure,
+}
+
 // ValidateUserTask validates the UserTask object without modifying it.
-func ValidateUserTask(uit *usertasksv1.UserTask) error {
+func ValidateUserTask(ut *usertasksv1.UserTask) error {
 	switch {
-	case uit.GetKind() != types.KindUserTask:
+	case ut.GetKind() != types.KindUserTask:
 		return trace.BadParameter("invalid kind")
-	case uit.GetVersion() != types.V1:
+	case ut.GetVersion() != types.V1:
 		return trace.BadParameter("invalid version")
-	case uit.GetSubKind() != "":
+	case ut.GetSubKind() != "":
 		return trace.BadParameter("invalid sub kind, must be empty")
-	case uit.GetMetadata() == nil:
+	case ut.GetMetadata() == nil:
 		return trace.BadParameter("user task metadata is nil")
-	case uit.Metadata.GetName() == "":
+	case ut.Metadata.GetName() == "":
 		return trace.BadParameter("user task name is empty")
-	case uit.GetSpec() == nil:
+	case ut.GetSpec() == nil:
 		return trace.BadParameter("user task spec is nil")
-	case uit.GetSpec().Integration == "":
-		return trace.BadParameter("integration is required")
+	case !slices.Contains(validTaskStates, ut.GetSpec().State):
+		return trace.BadParameter("invalid task state, allowed values: %v", validTaskStates)
 	}
 
-	switch uit.Spec.TaskType {
+	switch ut.Spec.TaskType {
 	case TaskTypeDiscoverEC2:
-		if err := validateDiscoverEC2TaskType(uit); err != nil {
+		if err := validateDiscoverEC2TaskType(ut); err != nil {
 			return trace.Wrap(err)
 		}
 	default:
-		return trace.BadParameter("task type %q is not valid", uit.Spec.TaskType)
+		return trace.BadParameter("task type %q is not valid", ut.Spec.TaskType)
 	}
 
 	return nil
 }
 
-func validateDiscoverEC2TaskType(uit *usertasksv1.UserTask) error {
-	if uit.Spec.DiscoverEc2 == nil {
+func validateDiscoverEC2TaskType(ut *usertasksv1.UserTask) error {
+	if ut.GetSpec().Integration == "" {
+		return trace.BadParameter("integration is required")
+	}
+	if ut.GetSpec().DiscoverEc2 == nil {
 		return trace.BadParameter("%s requires the discover_ec2 field", TaskTypeDiscoverEC2)
 	}
-	if uit.Spec.IssueType == "" {
-		return trace.BadParameter("issue type is required")
+	if ut.GetSpec().DiscoverEc2.AccountId == "" {
+		return trace.BadParameter("%s requires the discover_ec2.account_id field", TaskTypeDiscoverEC2)
+	}
+	if ut.GetSpec().DiscoverEc2.Region == "" {
+		return trace.BadParameter("%s requires the discover_ec2.region field", TaskTypeDiscoverEC2)
+	}
+
+	expectedTaskName := TaskNameForDiscoverEC2(TaskNameForDiscoverEC2Parts{
+		Integration: ut.Spec.Integration,
+		IssueType:   ut.Spec.IssueType,
+		AccountID:   ut.Spec.DiscoverEc2.AccountId,
+		Region:      ut.Spec.DiscoverEc2.Region,
+	})
+	if ut.Metadata.GetName() != expectedTaskName {
+		return trace.BadParameter("task name is pre-defined for discover-ec2 types, expected %q, got %q",
+			expectedTaskName,
+			ut.Metadata.GetName(),
+		)
+	}
+
+	if !slices.Contains(discoverEC2IssueTypes, ut.GetSpec().IssueType) {
+		return trace.BadParameter("invalid issue type state, allowed values: %v", discoverEC2IssueTypes)
+	}
+
+	if len(ut.Spec.DiscoverEc2.Instances) == 0 {
+		return trace.BadParameter("at least one instance is required")
+	}
+	for instanceID, instanceIssue := range ut.Spec.DiscoverEc2.Instances {
+		if instanceID == "" {
+			return trace.BadParameter("instance id in discover_ec2.instances map is required")
+		}
+		if instanceIssue.InstanceId == "" {
+			return trace.BadParameter("instance id in discover_ec2.instances field is required")
+		}
+		if instanceID != instanceIssue.InstanceId {
+			return trace.BadParameter("instance id in discover_ec2.instances map and field are different")
+		}
+		if instanceIssue.DiscoveryConfig == "" {
+			return trace.BadParameter("discovery config in discover_ec2.instances field is required")
+		}
+		if instanceIssue.DiscoveryGroup == "" {
+			return trace.BadParameter("discovery group in discover_ec2.instances field is required")
+		}
 	}
 
 	return nil
 }
+
+// TaskNameForDiscoverEC2Parts are the fields that deterministically compute a Discover EC2 task name.
+// To be used with TaskNameForDiscoverEC2 function.
+type TaskNameForDiscoverEC2Parts struct {
+	Integration string
+	IssueType   string
+	AccountID   string
+	Region      string
+}
+
+// TaskNameForDiscoverEC2 returns a deterministic name for the DiscoverEC2 task type.
+// This method is used to ensure a single UserTask is created to report issues in enrolling EC2 instances for a given integration, issue type, account id and region.
+func TaskNameForDiscoverEC2(parts TaskNameForDiscoverEC2Parts) string {
+	var bs []byte
+	bs = append(bs, binary.LittleEndian.AppendUint64(nil, uint64(len(parts.Integration)))...)
+	bs = append(bs, []byte(parts.Integration)...)
+	bs = append(bs, binary.LittleEndian.AppendUint64(nil, uint64(len(parts.IssueType)))...)
+	bs = append(bs, []byte(parts.IssueType)...)
+	bs = append(bs, binary.LittleEndian.AppendUint64(nil, uint64(len(parts.AccountID)))...)
+	bs = append(bs, []byte(parts.AccountID)...)
+	bs = append(bs, binary.LittleEndian.AppendUint64(nil, uint64(len(parts.Region)))...)
+	bs = append(bs, []byte(parts.Region)...)
+	return uuid.NewSHA1(discoverEC2Namespace, bs).String()
+}
+
+// discoverEC2Namespace is an UUID that represents the name space to be used for generating UUIDs for DiscoverEC2 User Task names.
+var discoverEC2Namespace = uuid.Must(uuid.Parse("6ba7b815-9dad-11d1-80b4-00c04fd430c8"))
