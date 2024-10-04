392e3a13406fc903fcb80ef8af5592830b677312
Michael Myers <michael.myers@goteleport.com>
2024-10-04 13:59:48 -0500

    [v15] Add new empty state for Bots page in the web UI (#47118)

--- web/packages/teleport/src/Bots/Add/AddBotsPicker.tsx
+++ web/packages/teleport/src/Bots/Add/AddBotsPicker.tsx
@@ -213,6 +213,20 @@ function GuidedTile({ integration }: { integration: BotIntegration }) {
   );
 }
 
+export function DisplayTile({
+  icon,
+  title,
+}: {
+  title: string;
+  icon: JSX.Element;
+}) {
+  return (
+    <HoverIntegrationTile>
+      <TileContent icon={icon} title={title} />
+    </HoverIntegrationTile>
+  );
+}
+
 function TileContent({ icon, title }) {
   return (
     <>
@@ -234,4 +248,9 @@ const BadgeGuided = styled.div`
   top: 0px;
   right: 0px;
   font-size: 10px;
+`;
+
+const HoverIntegrationTile = styled(IntegrationTile)`
+  background: none;
+  transition: all 0.1s ease-in;
 `;
