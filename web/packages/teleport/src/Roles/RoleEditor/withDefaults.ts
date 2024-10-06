3be310f95a61e2cdef14de06655e65edb02d601c
Bartosz Leper <bartosz.leper@goteleport.com>
2024-10-06 20:14:02 +0200

    Fix broken import

--- web/packages/teleport/src/Roles/RoleEditor/withDefaults.ts
+++ web/packages/teleport/src/Roles/RoleEditor/withDefaults.ts
@@ -16,8 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { freezeDeep } from 'shared/utils/highbar';
-
 import { Role, RoleOptions } from 'teleport/services/resources';
 
 export type DeepPartial<T> = {
