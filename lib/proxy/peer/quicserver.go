dd0c9995699aa9b44e6bfbfce111dfc608d5dfb1
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2024-10-07 12:18:55 +0200

    QUICServer docs

--- lib/proxy/peer/quicserver.go
+++ lib/proxy/peer/quicserver.go
@@ -30,13 +30,28 @@ import (
 	"github.com/gravitational/teleport"
 )
 
+// QUICServerConfig holds the parameters for [NewQUICServer].
 type QUICServerConfig struct {
-	Log           *slog.Logger
+	Log *slog.Logger
+	// ClusterDialer is the dialer used to open connections to agents on behalf
+	// of the peer proxies. Required.
 	ClusterDialer ClusterDialer
 
-	CipherSuites   []uint16
+	// CipherSuites is the set of TLS ciphersuites to be used by the server.
+	//
+	// Note: it won't actually have an effect, since QUIC always uses (the DTLS
+	// equivalent of) TLS 1.3, and TLS 1.3 ciphersuites can't be configured in
+	// crypto/tls, but for consistency's sake this should be passed along from
+	// the agent configuration.
+	CipherSuites []uint16
+	// GetCertificate should return the server certificate at time of use. It
+	// should be a certificate with the Proxy host role. Required.
 	GetCertificate func(*tls.ClientHelloInfo) (*tls.Certificate, error)
-	GetClientCAs   func(*tls.ClientHelloInfo) (*x509.CertPool, error)
+	// GetClientCAs should return the certificate pool that should be used to
+	// validate the client certificates of peer proxies; i.e., a pool containing
+	// the trusted signers for the certificate authority of the local cluster.
+	// Required.
+	GetClientCAs func(*tls.ClientHelloInfo) (*x509.CertPool, error)
 }
 
 func (c *QUICServerConfig) checkAndSetDefaults() error {
@@ -65,6 +80,7 @@ func (c *QUICServerConfig) checkAndSetDefaults() error {
 // QUICServer is a proxy peering server that uses the QUIC protocol.
 type QUICServer struct{}
 
+// NewQUICServer returns a [QUICServer] with the given config.
 func NewQUICServer(cfg QUICServerConfig) (*QUICServer, error) {
 	if err := cfg.checkAndSetDefaults(); err != nil {
 		return nil, trace.Wrap(err)
@@ -72,14 +88,21 @@ func NewQUICServer(cfg QUICServerConfig) (*QUICServer, error) {
 	panic("QUIC proxy peering is not implemented")
 }
 
+// Serve opens a listener and serves incoming connection. Returns after calling
+// Close or Shutdown.
 func (s *QUICServer) Serve(t *quic.Transport) error {
 	panic("QUIC proxy peering is not implemented")
 }
 
+// Close stops listening for incoming connections and ungracefully terminates
+// all the existing ones.
 func (s *QUICServer) Close() error {
 	panic("QUIC proxy peering is not implemented")
 }
 
+// Shutdown stops listening for incoming connections and waits until the
+// existing ones are closed or until the context expires. If the context
+// expires, running connections are ungracefully terminated.
 func (s *QUICServer) Shutdown(ctx context.Context) error {
 	panic("QUIC proxy peering is not implemented")
 }
