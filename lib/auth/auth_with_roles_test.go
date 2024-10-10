a99cca9ebc606735909f51a84621497251a5090c
Zac Bergquist <zac.bergquist@goteleport.com>
2024-10-10 10:22:59 -0600

    Fix active session filtering for legacy sessions
    This code never worked correctly, but mostly went unnoticed because
    it is only triggered when using legacy roles prior to RoleV5.
    
    Prior to moderated sessions, RBAC for viewing active sessions was
    based on whether or not you could join a session as the OS login
    that is being used, along with a pseudo-resource of kind "ssh_session".
    
    With moderated sessions we introduced more flexible RBAC semantics
    that allow you to join sessions in different modes (peer, observer,
    moderator), even if you don't actually have permission to start
    sessions.
    
    In #11223 we decided that we need to support both types of RBAC checks
    (legacy checks against the "ssh_session" resource, and newer checks
    against the session_tracker and join_sessions policies). The code that
    was doing the legacy checks was flawed for two reasons:
    
    1. It used (types.SessionTracker).GetKind() (which will always be
       "session_tracker") instead of
       (types.SessionTracker).GetSessionKind().
    2. When checking whether the session was SSH, it was checking for
       the legacy "ssh_session" value, instead of the "ssh" value that
       session trackers actually use.

--- lib/auth/auth_with_roles_test.go
+++ lib/auth/auth_with_roles_test.go
@@ -6291,7 +6291,41 @@ func TestGetActiveSessionTrackers(t *testing.T) {
 		require.NoError(t, err)
 
 		return getActiveSessionsTestCase{"no access with match expression", tracker, role, false}
-	}()}
+	}(), func() getActiveSessionsTestCase {
+		tracker, err := types.NewSessionTracker(types.SessionTrackerSpecV1{
+			SessionID: "1",
+			Kind:      string(types.SSHSessionKind),
+		})
+		require.NoError(t, err)
+
+		role, err := types.NewRoleWithVersion("dev", types.V3, types.RoleSpecV6{
+			Allow: types.RoleConditions{
+				AppLabels:        types.Labels{"*": []string{"*"}},
+				DatabaseLabels:   types.Labels{"*": []string{"*"}},
+				KubernetesLabels: types.Labels{"*": []string{"*"}},
+				KubernetesResources: []types.KubernetesResource{
+					{Kind: types.KindKubePod, Name: "*", Namespace: "*", Verbs: []string{"*"}},
+				},
+				NodeLabels:           types.Labels{"*": []string{"*"}},
+				NodeLabelsExpression: `contains(user.spec.traits["cluster_ids"], labels["cluster_id"]) || contains(user.spec.traits["sub"], labels["owner"])`,
+				Logins:               []string{"{{external.sub}}"},
+				WindowsDesktopLabels: types.Labels{"cluster_id": []string{"{{external.cluster_ids}}"}},
+				WindowsDesktopLogins: []string{"{{external.sub}}", "{{external.windows_logins}}"},
+			},
+			Deny: types.RoleConditions{
+				Rules: []types.Rule{
+					{
+						Resources: []string{types.KindDatabaseServer, types.KindAppServer, types.KindSession, types.KindSSHSession, types.KindKubeService, types.KindSessionTracker},
+						Verbs:     []string{"list", "read"},
+					},
+				},
+			},
+		})
+		require.NoError(t, err)
+
+		return getActiveSessionsTestCase{"filter bug v3 role", tracker, role, false}
+	}(),
+	}
 
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
