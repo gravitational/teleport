9992568d3080fe24a94583b7ee2bd0d77b40aa9a
Michael <michael.myers@goteleport.com>
2024-10-10 03:52:43 -0500

    Connect: delete v16 TODOs (#47368)
    - Remove the no longer needed UnimplementedError for approving access
    lists in Connect
    - Remove the need to merge unified resource preferences with defaults.
    Now we either use what is returned or a default set

--- web/packages/teleport/src/services/resources/resource.ts
+++ web/packages/teleport/src/services/resources/resource.ts
@@ -60,20 +60,7 @@ class ResourceService {
     items: RoleResource[];
     startKey: string;
   }> {
-    const response = await api.get(cfg.getListRolesUrl(params));
-
-    // This will handle backward compatibility with roles.
-    // The old roles API returns only an array of resources while
-    // the new one sends the paginated object with startKey/requests
-    // If this webclient requests an older proxy
-    // (this may happen in multi proxy deployments),
-    //  this should allow the old request to not break the Web UI.
-    // TODO (gzdunek): DELETE in 17.0.0
-    if (Array.isArray(response)) {
-      return makeRolesPageLocally(params, response);
-    }
-
-    return response;
+    return await api.get(cfg.getListRolesUrl(params));
   }
 
   fetchPresetRoles() {
@@ -132,31 +119,3 @@ class ResourceService {
 }
 
 export default ResourceService;
-
-// TODO (gzdunek): DELETE in 17.0.0.
-// See the comment where this function is used.
-function makeRolesPageLocally(
-  params: UrlListRolesParams,
-  response: RoleResource[]
-): {
-  items: RoleResource[];
-  startKey: string;
-} {
-  if (params.search) {
-    // A serverside search would also match labels, here we only check the name.
-    response = response.filter(p =>
-      p.name.toLowerCase().includes(params.search.toLowerCase())
-    );
-  }
-
-  if (params.startKey) {
-    const startIndex = response.findIndex(p => p.name === params.startKey);
-    response = response.slice(startIndex);
-  }
-
-  const limit = params.limit || 200;
-  const nextKey = response.at(limit)?.name;
-  response = response.slice(0, limit);
-
-  return { items: response, startKey: nextKey };
-}
