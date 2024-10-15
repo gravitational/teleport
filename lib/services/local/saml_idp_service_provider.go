bb88f557afa48479aaabb1ab927a1a53024eb2ff
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2024-10-15 20:56:16 +0000

    Prep for upcoming backend.Key type change (#46675) (#47481)
    Adds a (Key) IsZero method to determine if a key is populated. Some
    Backend implementations today validate keys before operations via
    a `len(key) == 0` check, however, that will no longer work once the
    key is migrated.
    
    Starts migrating the backend.LockConfiguration away from prepopulating
    the lock name to passing in a list of components. There were a few
    locks constructing a portion of the name manually, which will not
    work when the key type is changed.

--- lib/services/local/saml_idp_service_provider.go
+++ lib/services/local/saml_idp_service_provider.go
@@ -143,7 +143,7 @@ func (s *SAMLIdPServiceProviderService) CreateSAMLIdPServiceProvider(ctx context
 		return trace.Wrap(err)
 	}
 
-	return trace.Wrap(s.svc.RunWhileLocked(ctx, samlIDPServiceProviderModifyLock, samlIDPServiceProviderModifyLockTTL,
+	return trace.Wrap(s.svc.RunWhileLocked(ctx, []string{samlIDPServiceProviderModifyLock}, samlIDPServiceProviderModifyLockTTL,
 		func(ctx context.Context, backend backend.Backend) error {
 			if err := s.ensureEntityIDIsUnique(ctx, sp); err != nil {
 				return trace.Wrap(err)
@@ -181,7 +181,7 @@ func (s *SAMLIdPServiceProviderService) UpdateSAMLIdPServiceProvider(ctx context
 		return trace.Wrap(err)
 	}
 
-	return trace.Wrap(s.svc.RunWhileLocked(ctx, samlIDPServiceProviderModifyLock, samlIDPServiceProviderModifyLockTTL,
+	return trace.Wrap(s.svc.RunWhileLocked(ctx, []string{samlIDPServiceProviderModifyLock}, samlIDPServiceProviderModifyLockTTL,
 		func(ctx context.Context, backend backend.Backend) error {
 			if err := s.ensureEntityIDIsUnique(ctx, sp); err != nil {
 				return trace.Wrap(err)
