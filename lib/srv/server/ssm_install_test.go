98c6cf2b3fc0e51f53bd7897374238b11a9de767
Marco Dinis <marco.dinis@goteleport.com>
2024-10-16 15:15:15 +0100

    UserTasks: Add SSM Document and Installer for DiscoverEC2 Issues
    This PR adds two new fields to the DiscoverEC2 User Task.
    SSM Document used to install teleport
    Teleport Installer script name used to install teleport
    
    This can be used to let the user know which scripts were used.
    For the SSM Document, users can then open it in webui.
    For the Installer Script, users can manage it using `tctl` client.

--- lib/srv/server/ssm_install_test.go
+++ lib/srv/server/ssm_install_test.go
@@ -144,7 +144,8 @@ func TestSSMInstaller(t *testing.T) {
 					Status:        ssm.CommandStatusSuccess,
 					InvocationURL: "https://eu-central-1.console.aws.amazon.com/systems-manager/run-command/command-id-1/instance-id-1",
 				},
-				IssueType: "ec2-ssm-script-failure",
+				IssueType:       "ec2-ssm-script-failure",
+				SSMDocumentName: "ssmdocument",
 			}},
 		},
 		{
@@ -189,7 +190,8 @@ func TestSSMInstaller(t *testing.T) {
 					Status:        ssm.CommandStatusSuccess,
 					InvocationURL: "https://eu-central-1.console.aws.amazon.com/systems-manager/run-command/command-id-1/instance-id-1",
 				},
-				IssueType: "ec2-ssm-script-failure",
+				IssueType:       "ec2-ssm-script-failure",
+				SSMDocumentName: "ssmdocument-without-sshdConfigPath-param",
 			}},
 		},
 		{
@@ -236,7 +238,8 @@ func TestSSMInstaller(t *testing.T) {
 					StandardError:  "timeout error",
 					InvocationURL:  "https://eu-central-1.console.aws.amazon.com/systems-manager/run-command/command-id-1/instance-id-1",
 				},
-				IssueType: "ec2-ssm-script-failure",
+				IssueType:       "ec2-ssm-script-failure",
+				SSMDocumentName: "ssmdocument",
 			}},
 		},
 		{
@@ -287,7 +290,8 @@ func TestSSMInstaller(t *testing.T) {
 					StandardError:  "timeout error",
 					InvocationURL:  "https://eu-central-1.console.aws.amazon.com/systems-manager/run-command/command-id-1/instance-id-1",
 				},
-				IssueType: "ec2-ssm-script-failure",
+				IssueType:       "ec2-ssm-script-failure",
+				SSMDocumentName: "ssmdocument",
 			}},
 		},
 		{
@@ -355,7 +359,8 @@ func TestSSMInstaller(t *testing.T) {
 						Status:        ssm.CommandStatusSuccess,
 						InvocationURL: "https://eu-central-1.console.aws.amazon.com/systems-manager/run-command/command-id-1/instance-id-1",
 					},
-					IssueType: "ec2-ssm-script-failure",
+					IssueType:       "ec2-ssm-script-failure",
+					SSMDocumentName: "ssmdocument",
 				},
 				{
 					SSMRunEvent: &events.SSMRun{
@@ -370,7 +375,8 @@ func TestSSMInstaller(t *testing.T) {
 						ExitCode:   -1,
 						Status:     "SSM Agent in EC2 Instance is not connecting to SSM Service. Restart or reinstall the SSM service. See https://docs.aws.amazon.com/systems-manager/latest/userguide/ami-preinstalled-agent.html#verify-ssm-agent-status for more details.",
 					},
-					IssueType: "ec2-ssm-agent-connection-lost",
+					IssueType:       "ec2-ssm-agent-connection-lost",
+					SSMDocumentName: "ssmdocument",
 				},
 				{
 					SSMRunEvent: &events.SSMRun{
@@ -385,7 +391,8 @@ func TestSSMInstaller(t *testing.T) {
 						ExitCode:   -1,
 						Status:     "EC2 instance is running an unsupported Operating System. Only Linux is supported.",
 					},
-					IssueType: "ec2-ssm-unsupported-os",
+					IssueType:       "ec2-ssm-unsupported-os",
+					SSMDocumentName: "ssmdocument",
 				},
 				{
 					SSMRunEvent: &events.SSMRun{
@@ -400,7 +407,8 @@ func TestSSMInstaller(t *testing.T) {
 						ExitCode:   -1,
 						Status:     "EC2 Instance is not registered in SSM. Make sure that the instance has AmazonSSMManagedInstanceCore policy assigned.",
 					},
-					IssueType: "ec2-ssm-agent-not-registered",
+					IssueType:       "ec2-ssm-agent-not-registered",
+					SSMDocumentName: "ssmdocument",
 				},
 			},
 		},
@@ -456,7 +464,8 @@ func TestSSMInstaller(t *testing.T) {
 					StandardOutput: "custom output",
 					InvocationURL:  "https://eu-central-1.console.aws.amazon.com/systems-manager/run-command/command-id-1/instance-id-1",
 				},
-				IssueType: "ec2-ssm-script-failure",
+				IssueType:       "ec2-ssm-script-failure",
+				SSMDocumentName: "ssmdocument",
 			}},
 		},
 		{
@@ -497,7 +506,8 @@ func TestSSMInstaller(t *testing.T) {
 					Status:        ssm.CommandStatusSuccess,
 					InvocationURL: "https://eu-central-1.console.aws.amazon.com/systems-manager/run-command/command-id-1/instance-id-1",
 				},
-				IssueType: "ec2-ssm-script-failure",
+				IssueType:       "ec2-ssm-script-failure",
+				SSMDocumentName: "ssmdocument",
 			}},
 		},
 		// todo(amk): test that incomplete commands eventually return
