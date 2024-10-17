fccabf88438f23f4f434f39589c7696e6a83a876
Alan Parra <alan.parra@goteleport.com>
2024-10-17 16:16:56 -0300

    feat: Disable auto-enroll via environment variable

--- lib/devicetrust/enroll/auto_enroll_test.go
+++ lib/devicetrust/enroll/auto_enroll_test.go
@@ -16,6 +16,7 @@ package enroll_test
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -62,4 +63,11 @@ func TestAutoEnrollCeremony_Run(t *testing.T) {
 			assert.NotNil(t, dev, "AutoEnroll returned nil device")
 		})
 	}
+}
+
+func TestAutoEnroll_disabledByEnv(t *testing.T) {
+	os.Setenv("TELEPORT_DEVICE_AUTO_ENROLL_DISABLED", "1")
+
+	_, err := enroll.AutoEnroll(context.Background(), nil /* devicesClient */)
+	assert.ErrorIs(t, err, enroll.ErrAutoEnrollDisabled, "AutoEnroll() error mismatch")
 }
