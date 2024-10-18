3bd5f75dcc17af17a7cda250b562a973aba111d3
Lisa Kim <lisa@goteleport.com>
2024-10-18 11:20:12 -0700

    Web: rename generic variable name to a helpful name

--- web/packages/teleterm/src/ui/AccessRequestCheckout/AccessRequestCheckout.tsx
+++ web/packages/teleterm/src/ui/AccessRequestCheckout/AccessRequestCheckout.tsx
@@ -85,7 +85,7 @@ export function AccessRequestCheckout() {
     fetchResourceRolesAttempt,
     setSelectedResourceRequestRoles,
     clearCreateAttempt,
-    data,
+    pendingAccessRequests,
     shouldShowClusterNameColumn,
     selectedReviewers,
     setSelectedReviewers,
@@ -104,7 +104,7 @@ export function AccessRequestCheckout() {
     onStartTimeChange,
   } = useAccessRequestCheckout();
 
-  const isRoleRequest = data[0]?.kind === 'role';
+  const isRoleRequest = pendingAccessRequests[0]?.kind === 'role';
   const transitionRef = useRef<HTMLDivElement>();
 
   function closeCheckout() {
@@ -113,10 +113,15 @@ export function AccessRequestCheckout() {
 
   // We should rather detect how much space we have,
   // but for simplicity we only count items.
-  const moreToShow = Math.max(data.length - MAX_RESOURCES_IN_BAR_TO_SHOW, 0);
+  const moreToShow = Math.max(
+    pendingAccessRequests.length - MAX_RESOURCES_IN_BAR_TO_SHOW,
+    0
+  );
+  const numPendingAccessRequests = pendingAccessRequests.length;
+
   return (
     <>
-      {data.length > 0 && !isCollapsed() && (
+      {pendingAccessRequests.length > 0 && !isCollapsed() && (
         <Box
           px={3}
           py={2}
@@ -134,12 +139,15 @@ export function AccessRequestCheckout() {
           >
             <Flex flexDirection="column" minWidth={0}>
               <Text mb={1}>
-                {data.length}{' '}
-                {pluralize(data.length, isRoleRequest ? 'role' : 'resource')}{' '}
+                {numPendingAccessRequests}{' '}
+                {pluralize(
+                  numPendingAccessRequests,
+                  isRoleRequest ? 'role' : 'resource'
+                )}{' '}
                 added to access request:
               </Text>
               <Flex gap={1} flexWrap="wrap">
-                {data
+                {pendingAccessRequests
                   .slice(0, MAX_RESOURCES_IN_BAR_TO_SHOW)
                   .map(c => {
                     let resource = {
@@ -238,7 +246,7 @@ export function AccessRequestCheckout() {
               })
             }
             reset={reset}
-            data={data}
+            pendingAccessRequests={pendingAccessRequests}
             showClusterNameColumn={shouldShowClusterNameColumn}
             createAttempt={createRequestAttempt}
             resourceRequestRoles={resourceRequestRoles}
