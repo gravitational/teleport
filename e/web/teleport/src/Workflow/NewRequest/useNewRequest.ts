86b2e0dd9bbc451368b79462969387a57537e2a9
Bartosz Leper <bartosz.leper@goteleport.com>
2024-10-23 15:19:06 +0200

    Revert accidental debugging code that broke access requests (#5226)
    Source-commit: gravitational/teleport.e@d30b6e5b71a00ff25915eb7e7a3888d8d4a3fada
    Source-pull-requests: gravitational/teleport.e#5226

--- e/web/teleport/src/Workflow/NewRequest/useNewRequest.ts
+++ e/web/teleport/src/Workflow/NewRequest/useNewRequest.ts
@@ -181,18 +181,7 @@ export function useNewRequest(ctx: Ctx) {
 
           return {
             startKey: response.startKey,
-            agents: response.agents.map(a => {
-              if (a.kind === 'app') {
-                return {
-                  ...a,
-                  userGroups: [
-                    { name: 'group1', description: 'This is group 1' },
-                    { name: 'group2', description: 'This is group 2' },
-                  ],
-                };
-              }
-              return a;
-            }),
+            agents: response.agents,
             totalCount: response.agents.length,
           };
         } catch (err) {
