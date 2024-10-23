cf538a85a172a3e586023c6256144a58a02f76c4
Rafał Cieślak <rafal.cieslak@goteleport.com>
2024-10-23 13:00:05 +0200

    Pass TargetPort during cert generation

--- lib/auth/auth_with_roles.go
+++ lib/auth/auth_with_roles.go
@@ -3156,6 +3156,7 @@ func (a *ServerWithRoles) generateUserCerts(ctx context.Context, req proto.UserC
 			DeviceExtensions:  DeviceExtensions(a.context.Identity.GetIdentity().DeviceExtensions),
 			AppName:           req.RouteToApp.Name,
 			AppURI:            req.RouteToApp.URI,
+			AppTargetPort:     uint16(req.RouteToApp.TargetPort),
 		})
 		if err != nil {
 			return nil, trace.Wrap(err)
@@ -3200,6 +3201,7 @@ func (a *ServerWithRoles) generateUserCerts(ctx context.Context, req proto.UserC
 		appName:                          req.RouteToApp.Name,
 		appPublicAddr:                    req.RouteToApp.PublicAddr,
 		appURI:                           req.RouteToApp.URI,
+		appTargetPort:                    uint16(req.RouteToApp.TargetPort),
 		appClusterName:                   req.RouteToApp.ClusterName,
 		awsRoleARN:                       req.RouteToApp.AWSRoleARN,
 		azureIdentity:                    req.RouteToApp.AzureIdentity,
