013d2eed29bcbc682f7a4f54bdaa9e6a3aa9f1cd
Stephen Levine <stephen.levine@goteleport.com>
2024-10-28 12:05:29 -0400

    [teleport-update] Add linking into /usr/local (#47879)
    * clean up download logic
    
    * Finish installer tests
    
    * fips and ent support
    
    * feedback
    
    * move enterprise/fips to webapi
    
    * wip
    
    * wip2
    
    * add cleanup
    
    * fix extract
    
    * wip
    
    * fix tests
    
    * remove safety
    
    * cleanup
    
    * cleanup extract
    
    * cleanup
    
    * cleanup
    
    * fix bugs
    
    * cleanup

--- lib/autoupdate/agent/updater_test.go
+++ lib/autoupdate/agent/updater_test.go
@@ -130,6 +130,7 @@ func TestUpdater_Enable(t *testing.T) {
 		userCfg    OverrideConfig
 		installErr error
 
+		removedVersion    string
 		installedVersion  string
 		installedTemplate string
 		errMatch          string
@@ -222,6 +223,34 @@ func TestUpdater_Enable(t *testing.T) {
 			installedTemplate: cdnURITemplate,
 		},
 		{
+			name: "backup version removed on install",
+			cfg: &UpdateConfig{
+				Version: updateConfigVersion,
+				Kind:    updateConfigKind,
+				Status: UpdateStatus{
+					ActiveVersion: "old-version",
+					BackupVersion: "backup-version",
+				},
+			},
+			installedVersion:  "16.3.0",
+			installedTemplate: cdnURITemplate,
+			removedVersion:    "backup-version",
+		},
+		{
+			name: "backup version kept for validation",
+			cfg: &UpdateConfig{
+				Version: updateConfigVersion,
+				Kind:    updateConfigKind,
+				Status: UpdateStatus{
+					ActiveVersion: "16.3.0",
+					BackupVersion: "backup-version",
+				},
+			},
+			installedVersion:  "16.3.0",
+			installedTemplate: cdnURITemplate,
+			removedVersion:    "",
+		},
+		{
 			name:              "config does not exist",
 			installedVersion:  "16.3.0",
 			installedTemplate: cdnURITemplate,
@@ -263,12 +292,28 @@ func TestUpdater_Enable(t *testing.T) {
 			})
 			require.NoError(t, err)
 
-			var installedVersion, installedTemplate string
+			var (
+				installedVersion  string
+				installedTemplate string
+				linkedVersion     string
+				removedVersion    string
+			)
 			updater.Installer = &testInstaller{
 				FuncInstall: func(_ context.Context, version, template string, _ InstallFlags) error {
 					installedVersion = version
 					installedTemplate = template
 					return tt.installErr
+				},
+				FuncLink: func(_ context.Context, version string) error {
+					linkedVersion = version
+					return nil
+				},
+				FuncList: func(_ context.Context) (versions []string, err error) {
+					return []string{"old"}, nil
+				},
+				FuncRemove: func(_ context.Context, version string) error {
+					removedVersion = version
+					return nil
 				},
 			}
 
@@ -282,6 +327,8 @@ func TestUpdater_Enable(t *testing.T) {
 			require.NoError(t, err)
 			require.Equal(t, tt.installedVersion, installedVersion)
 			require.Equal(t, tt.installedTemplate, installedTemplate)
+			require.Equal(t, tt.installedVersion, linkedVersion)
+			require.Equal(t, tt.removedVersion, removedVersion)
 
 			data, err := os.ReadFile(cfgPath)
 			require.NoError(t, err)
@@ -304,6 +351,8 @@ func blankTestAddr(s []byte) []byte {
 type testInstaller struct {
 	FuncInstall func(ctx context.Context, version, template string, flags InstallFlags) error
 	FuncRemove  func(ctx context.Context, version string) error
+	FuncLink    func(ctx context.Context, version string) error
+	FuncList    func(ctx context.Context) (versions []string, err error)
 }
 
 func (ti *testInstaller) Install(ctx context.Context, version, template string, flags InstallFlags) error {
@@ -312,4 +361,12 @@ func (ti *testInstaller) Install(ctx context.Context, version, template string, 
 
 func (ti *testInstaller) Remove(ctx context.Context, version string) error {
 	return ti.FuncRemove(ctx, version)
+}
+
+func (ti *testInstaller) Link(ctx context.Context, version string) error {
+	return ti.FuncLink(ctx, version)
+}
+
+func (ti *testInstaller) List(ctx context.Context) (versions []string, err error) {
+	return ti.FuncList(ctx)
 }
