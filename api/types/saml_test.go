3d3ee992b28c3d90f2e015baa7a96d9dafbd2216
Brian Joerger <bjoerger@goteleport.com>
2024-10-30 11:29:35 -0700

    Fix `SAMLForceAuthn` marshalling (#48098)
    * Marshal SAMLForceAuthn to/from string/bool.
    
    * Use any; Move to api/types/saml.go; add test.

--- api/types/saml_test.go
+++ api/types/saml_test.go
@@ -17,9 +17,13 @@ limitations under the License.
 package types_test
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v2"
 
 	"github.com/gravitational/teleport/api/types"
 )
@@ -104,6 +108,65 @@ func TestSAMLForceAuthn(t *testing.T) {
 
 			require.NoError(t, samlConnector.WithMFASettings())
 			require.Equal(t, tt.expectMFA, samlConnector.GetForceAuthn(), "expected force_authn to be %v for mfa but got %v", tt.expectMFA, samlConnector.GetForceAuthn())
+		})
+	}
+}
+
+func TestSAMLForceAuthn_Encoding(t *testing.T) {
+	for _, tt := range []struct {
+		forceAuthn    types.SAMLForceAuthn
+		expectEncoded string
+	}{
+		{
+			forceAuthn:    types.SAMLForceAuthn_FORCE_AUTHN_UNSPECIFIED,
+			expectEncoded: "",
+		}, {
+			forceAuthn:    types.SAMLForceAuthn_FORCE_AUTHN_YES,
+			expectEncoded: "yes",
+		}, {
+			forceAuthn:    types.SAMLForceAuthn_FORCE_AUTHN_NO,
+			expectEncoded: "no",
+		},
+	} {
+		t.Run(tt.forceAuthn.String(), func(t *testing.T) {
+			type object struct {
+				ForceAuthn types.SAMLForceAuthn `json:"force_authn" yaml:"force_authn"`
+			}
+			o := object{
+				ForceAuthn: tt.forceAuthn,
+			}
+			objectJSON := fmt.Sprintf(`{"force_authn":%q}`, tt.expectEncoded)
+			objectYAML := fmt.Sprintf("force_authn: %q\n", tt.expectEncoded)
+
+			t.Run("JSON", func(t *testing.T) {
+				t.Run("Marshal", func(t *testing.T) {
+					gotJSON, err := json.Marshal(o)
+					assert.NoError(t, err, "unexpected error from json.Marshal")
+					assert.Equal(t, objectJSON, string(gotJSON), "unexpected json.Marshal value")
+				})
+
+				t.Run("Unmarshal", func(t *testing.T) {
+					var gotObject object
+					err := json.Unmarshal([]byte(objectJSON), &gotObject)
+					assert.NoError(t, err, "unexpected error from json.Unmarshal")
+					assert.Equal(t, tt.forceAuthn, gotObject.ForceAuthn, "unexpected json.Unmarshal value")
+				})
+			})
+
+			t.Run("YAML", func(t *testing.T) {
+				t.Run("Marshal", func(t *testing.T) {
+					gotYAML, err := yaml.Marshal(o)
+					assert.NoError(t, err, "unexpected error from yaml.Marshal")
+					assert.Equal(t, objectYAML, string(gotYAML), "unexpected yaml.Marshal value")
+				})
+
+				t.Run("Unmarshal", func(t *testing.T) {
+					var gotObject object
+					err := yaml.Unmarshal([]byte(objectYAML), &gotObject)
+					assert.NoError(t, err, "unexpected error from yaml.Unmarshal")
+					assert.Equal(t, tt.forceAuthn, gotObject.ForceAuthn, "unexpected yaml.Unmarshal value")
+				})
+			})
 		})
 	}
 }
