aaad2262159495cb2a794d0b2d72942e4092b155
Zac Bergquist <zac.bergquist@goteleport.com>
2024-10-31 14:59:56 -0600

    Properly default auth's storage config
    Most of our tests override cfg.DataDir, but auth's storage config
    still uses a hard-coded /var/lib/teleport for backend state.
    
    Instead of fixing this in a bunch of tests, we stop defaulting to
    /var/lib/teleport and set the storage dir only after we know what
    the configured data dir is.

--- lib/service/servicecfg/config_test.go
+++ lib/service/servicecfg/config_test.go
@@ -22,7 +22,6 @@ import (
 	"fmt"
 	"io"
 	"log/slog"
-	"path/filepath"
 	"regexp"
 	"strings"
 	"testing"
@@ -88,7 +87,7 @@ func TestDefaultConfig(t *testing.T) {
 	require.Equal(t, localAuthAddr, auth.ListenAddr)
 	require.Equal(t, int64(defaults.LimiterMaxConnections), auth.Limiter.MaxConnections)
 	require.Equal(t, lite.GetName(), config.Auth.StorageConfig.Type)
-	require.Equal(t, filepath.Join(config.DataDir, defaults.BackendDir), auth.StorageConfig.Params[defaults.BackendPath])
+	require.Empty(t, auth.StorageConfig.Params[defaults.BackendPath])
 
 	// SSH section
 	ssh := config.SSH
