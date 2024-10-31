98751031e5da8fecbde78a5948935b1899e5ee4c
joerger <bjoerger@goteleport.com>
2024-10-31 10:59:52 -0700

    Document use of SSOHostname in the profile.

--- api/profile/profile.go
+++ api/profile/profile.go
@@ -120,7 +120,9 @@ type Profile struct {
 	// SSHDialTimeout is the timeout value that should be used for SSH connections.
 	SSHDialTimeout time.Duration `yaml:"ssh_dial_timeout,omitempty"`
 
-	// SSOHostname is the hostname of the SSO provider used to log in.
+	// SSOHostname is the hostname of the SSO provider used to log in. Clients can check this value,
+	// along with WebProxyAddr, to determine if a webpage is safe to open. Currently used by Teleport
+	// Connect in the proxy host allow list.
 	SSOHostname string `yaml:"sso_hostname,omitempty"`
 }
 
