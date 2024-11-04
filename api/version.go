b1281575694e64e6133fe181592e7320a699b1e9
Gus Rivera <gus.rivera@goteleport.com>
2024-11-04 16:41:23 -0600

    Sync fix test

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "17.0.0-alpha.3"
+const Version = "17.0.0-alpha.3.gus-s3-sync-fix.1"
 
 var SemVersion = semver.New(Version)
