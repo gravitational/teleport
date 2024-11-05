7fa148b4fc826188cc129b81a19404957addb1eb
Michael Myers <michael.myers@goteleport.com>
2024-11-05 14:12:11 -0600

    Narrow EnrollStatus type in web UI
    This makes the enroll status explicitly Enrolled or Not Enrolled instead
    of just a string. Also, I've made 'owner' no longer optional now that
    the code in `e` is merged

--- web/packages/teleport/src/DeviceTrust/types.ts
+++ web/packages/teleport/src/DeviceTrust/types.ts
@@ -22,8 +22,8 @@ export type TrustedDevice = {
   id: string;
   assetTag: string;
   osType: TrustedDeviceOSType;
-  enrollStatus: string;
-  owner?: string;
+  enrollStatus: 'Enrolled' | 'Not Enrolled';
+  owner: string;
 };
 
 export type TrustedDeviceOSType = 'Windows' | 'Linux' | 'macOS';
