88c6198f338dffa0f6e641aba23a8424f3aa922a
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2024-11-05 09:00:13 -0500

    Prevent overwriting existing host_uuid file (#48012)
    In some circumstances, multiple Teleport processes may be trying
    to write the host_uuid file in the same data directory simultaneously.
    The last of the writers would win, and any process using a host
    UUID that did not match what ended up on disk could get into a perpertual
    state of being unable to connect to the cluster.
    
    To avoid the raciness, the host_uuid file writing process is no
    longer a blind upsert. Instead, special care is taken to ensure
    that there can only be a single writer, and that any subsequent
    updates to the file are aborted and the first value written is
    used instead.

--- lib/service/service_test.go
+++ lib/service/service_test.go
@@ -69,6 +69,7 @@ import (
 	"github.com/gravitational/teleport/lib/services"
 	"github.com/gravitational/teleport/lib/services/local"
 	"github.com/gravitational/teleport/lib/utils"
+	"github.com/gravitational/teleport/lib/utils/hostid"
 )
 
 func TestMain(m *testing.M) {
@@ -1167,7 +1168,7 @@ func Test_readOrGenerateHostID(t *testing.T) {
 			dataDir := t.TempDir()
 			// write host_uuid file to temp dir.
 			if len(tt.args.hostIDContent) > 0 {
-				err := utils.WriteHostUUID(dataDir, tt.args.hostIDContent)
+				err := hostid.WriteFile(dataDir, tt.args.hostIDContent)
 				require.NoError(t, err)
 			}
 
