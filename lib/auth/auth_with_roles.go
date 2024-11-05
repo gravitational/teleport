1d9be3794d1ecd637f4a62689bf763174426185c
Tiago Silva <tiago.silva@goteleport.com>
2024-11-05 23:45:41 +0000

    [entraid] ask for mfa reuse during entraid plugin setup
    This PR fixes a bug where MFA for admin actions prevents creating
    multiple objects during the entra id setup guide.
    
    Signed-off-by: Tiago Silva <tiago.silva@goteleport.com>

--- lib/auth/auth_with_roles.go
+++ lib/auth/auth_with_roles.go
@@ -3645,7 +3645,7 @@ func (a *ServerWithRoles) CreateSAMLConnector(ctx context.Context, connector typ
 		return nil, trace.Wrap(err)
 	}
 
-	if err := a.context.AuthorizeAdminAction(); err != nil {
+	if err := a.context.AuthorizeAdminActionAllowReusedMFA(); err != nil {
 		return nil, trace.Wrap(err)
 	}
 
