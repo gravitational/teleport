9210e35eeeaa095b35bba5372b94a64882e51d63
Michelle Bergquist <11967646+michellescripts@users.noreply.github.com>
2024-11-05 19:03:53 -0700

    Use cloud anonymization key (#46819) (#48169)
    - When present, is used to anonymize events

--- lib/web/features.go
+++ lib/web/features.go
@@ -19,6 +19,8 @@
 package web
 
 import (
+	"bytes"
+
 	"github.com/gravitational/teleport/api/client/proto"
 )
 
@@ -28,6 +30,10 @@ import (
 func (h *Handler) SetClusterFeatures(features proto.Features) {
 	h.Mutex.Lock()
 	defer h.Mutex.Unlock()
+
+	if !bytes.Equal(h.ClusterFeatures.CloudAnonymizationKey, features.CloudAnonymizationKey) {
+		h.log.Info("Received new cloud anonymization key from server")
+	}
 
 	h.ClusterFeatures = features
 }
