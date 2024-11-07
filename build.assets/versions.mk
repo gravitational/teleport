ac583990d2e00e2c6cc8cf47490db4ea7f3470f1
Alan Parra <alan.parra@goteleport.com>
2024-11-07 10:47:05 -0300

    chore: Bump Go to 1.22.9

--- build.assets/versions.mk
+++ build.assets/versions.mk
@@ -3,7 +3,7 @@
 # Keep versions in sync with devbox.json, when applicable.
 
 # Sync with devbox.json.
-GOLANG_VERSION ?= go1.22.8
+GOLANG_VERSION ?= go1.22.9
 GOLANGCI_LINT_VERSION ?= v1.61.0
 
 NODE_VERSION ?= 20.17.0
