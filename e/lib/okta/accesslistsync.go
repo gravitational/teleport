511ee21e1ead53274e87cdb9c0d690efdf30056b
Marek Smoliński <marek@goteleport.com>
2024-11-07 18:14:23 +0100

    Fix Okta Access list sync interval (#5370)
    Source-commit: gravitational/teleport.e@d2ffa7f25aa10d5fe2e0bd9f0721260ab10b4b36
    Source-pull-requests: gravitational/teleport.e#5370

--- e/lib/okta/accesslistsync.go
+++ e/lib/okta/accesslistsync.go
@@ -44,10 +44,10 @@ const (
 
 var (
 	// AccessListSyncFirstDuration Wait 5 minutes for the first loop in hopes that the synchronizer has run.
-	AccessListSyncFirstDuration = 1 * time.Minute
+	AccessListSyncFirstDuration = 5 * time.Minute
 	// DefaultAccessListSyncInterval defines a 30 minute default time between running
 	// the access list synchronizer.
-	DefaultAccessListSyncInterval = 1 * time.Minute
+	DefaultAccessListSyncInterval = 30 * time.Minute
 )
 
 // accessListSyncConfig is the configuration for the access list synchronizer.
