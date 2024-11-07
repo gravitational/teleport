afec326bb3d1534fdc12115d7a4c79cc9c501165
Alan Parra <alan.parra@goteleport.com>
2024-11-07 11:12:55 -0300

    chore: Bump Go to 1.23.3 (#48575)

--- build.assets/versions.mk
+++ build.assets/versions.mk
@@ -3,7 +3,7 @@
 # Keep versions in sync with devbox.json, when applicable.
 
 # Sync with devbox.json.
-GOLANG_VERSION ?= go1.23.2
+GOLANG_VERSION ?= go1.23.3
 GOLANGCI_LINT_VERSION ?= v1.61.0
 
 NODE_VERSION ?= 20.18.0
--- integrations/event-handler/go.mod
+++ integrations/event-handler/go.mod
@@ -1,6 +1,6 @@
 module github.com/gravitational/teleport/integrations/event-handler
 
-go 1.23.2
+go 1.23.3
 
 require (
 	github.com/alecthomas/kong v1.2.1
