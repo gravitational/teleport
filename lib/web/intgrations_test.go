4efaa6c31700c52fca665a126d6e2f518793de6d
STeve Huang <xin.huang@goteleport.com>
2024-11-14 14:50:46 -0500

    fix lib/web

--- lib/web/intgrations_test.go
+++ lib/web/intgrations_test.go
@@ -68,7 +68,7 @@ func TestIntegrationsCreateWithAudience(t *testing.T) {
 			require.Equal(t, 200, createResp.Code())
 
 			// check origin label stored in backend
-			intgrationResource, err := wPack.server.Auth().GetIntegration(ctx, integrationName)
+			intgrationResource, err := wPack.server.Auth().GetIntegration(ctx, integrationName, false)
 			require.NoError(t, err)
 			require.Equal(t, test.audience, intgrationResource.GetAWSOIDCIntegrationSpec().Audience)
 
