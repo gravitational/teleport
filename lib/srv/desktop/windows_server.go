13d6fe1b7200ecdaa4100153ec5f94b633b8ca32
Zac Bergquist <zac.bergquist@goteleport.com>
2024-11-14 15:04:55 -0700

    Improve troubleshooting for LDAP authentication errors (#48946)
    This introduces two small changes:
    
    1. Use an aggregate error to make sure the original error is included
       along with our attempt at providing a better error message. This
       change should help distinguish between bad authn/invalid cert
       and valid authentication but insufficient user permissions.
    2. Make the CRL Distribution Point in Windows certs optional. This
       metadata is required in the certs we issue for users to RDP,
       but it doesn't need to be present in the certs we use to
       authenticate our service account. The problem with including it
       when it is not needed is it causes windows to perform a revocation
       check and log a failure, which can lead to wasted time when
       troubleshooting.

--- lib/srv/desktop/windows_server.go
+++ lib/srv/desktop/windows_server.go
@@ -454,6 +454,7 @@ func (s *WindowsService) tlsConfigForLDAP() (*tls.Config, error) {
 		domain:             s.cfg.Domain,
 		ttl:                windowsDesktopServiceCertTTL,
 		activeDirectorySID: s.cfg.SID,
+		omitCDP:            true,
 	})
 	if err != nil {
 		return nil, trace.Wrap(err)
@@ -1279,7 +1280,8 @@ type generateCredentialsRequest struct {
 	// createUser specifies if Windows user should be created if missing
 	createUser bool
 	// groups are groups that user should be member of
-	groups []string
+	groups  []string
+	omitCDP bool
 }
 
 // generateCredentials generates a private key / certificate pair for the given
@@ -1307,6 +1309,7 @@ func (s *WindowsService) generateCredentials(ctx context.Context, request genera
 		AuthClient:         s.cfg.AuthClient,
 		CreateUser:         request.createUser,
 		Groups:             request.groups,
+		OmitCDP:            request.omitCDP,
 	})
 }
 
