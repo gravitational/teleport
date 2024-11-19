01a381394a90971bfe8976c75b6186eb0e58c23d
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2024-11-19 12:18:54 -0500

    Extend hostid test backoff (#49193)
    Attempts to reduce any flakiness caused by contention producing a
    host id by allowing tests to use a longer exponential backoff and
    retrying more times.

--- lib/utils/hostid/hostid_test.go
+++ lib/utils/hostid/hostid_test.go
@@ -24,12 +24,14 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/sync/errgroup"
 
+	"github.com/gravitational/teleport/api/utils/retryutils"
 	"github.com/gravitational/teleport/lib/utils"
 	"github.com/gravitational/teleport/lib/utils/hostid"
 )
@@ -53,7 +55,16 @@ func TestReadOrCreate(t *testing.T) {
 		i := i
 		wg.Go(func() error {
 			<-barrier
-			id, err := hostid.ReadOrCreateFile(dir)
+			id, err := hostid.ReadOrCreateFile(
+				dir,
+				hostid.WithBackoff(retryutils.RetryV2Config{
+					First:  50 * time.Millisecond,
+					Driver: retryutils.NewExponentialDriver(100 * time.Millisecond),
+					Max:    15 * time.Second,
+					Jitter: retryutils.NewFullJitter(),
+				}),
+				hostid.WithIterationLimit(10),
+			)
 			ids[i] = id
 			return err
 		})
