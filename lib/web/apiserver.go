00775b151a91c0008953c1976fc8cb25215cdae8
joerger <bjoerger@goteleport.com>
2024-11-21 11:45:44 -0800

    Include sso channel ID in web mfa challenges.

--- lib/web/apiserver.go
+++ lib/web/apiserver.go
@@ -2772,7 +2772,7 @@ func (h *Handler) mfaLoginBegin(w http.ResponseWriter, r *http.Request, p httpro
 		return nil, trace.AccessDenied("invalid credentials")
 	}
 
-	return makeAuthenticateChallenge(mfaChallenge), nil
+	return makeAuthenticateChallenge(mfaChallenge, "" /*channelID*/), nil
 }
 
 // mfaLoginFinish completes the MFA login ceremony, returning a new SSH
