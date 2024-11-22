8a4d02db5402a78f6260e950b602cbbd7cd528fc
Gavin Frazar <gavin.frazar@goteleport.com>
2024-11-22 13:23:38 -0800

    update required db name helper

--- web/packages/teleport/src/Discover/Database/TestConnection/TestConnection.tsx
+++ web/packages/teleport/src/Discover/Database/TestConnection/TestConnection.tsx
@@ -185,7 +185,6 @@ export function TestConnection() {
                 isDisabled={
                   attempt.status === 'processing' || nameOpts.length === 0
                 }
-                // Database name is required for Postgres.
                 isClearable={!isDbNameRequired(dbEngine)}
               />
               <CustomInputFieldForAsterisks
@@ -242,7 +241,11 @@ function getInputValue(input: string, inputKind: 'name' | 'user') {
 function isDbNameRequired(engine: DatabaseEngine) {
   const protocol = getDatabaseProtocol(engine);
   switch (protocol) {
+    case 'mongodb':
+    case 'oracle':
     case 'postgres':
+    case 'spanner':
+    case 'sqlserver':
       return true;
     default:
       return false;
