24b55b17b715cea23d6f20863980c54d8e66baa3
STeve Huang <xin.huang@goteleport.com>
2024-11-26 14:37:25 -0500

    fix lint

--- lib/cache/plugin_static_credentials_test.go
+++ lib/cache/plugin_static_credentials_test.go
@@ -21,9 +21,10 @@ import (
 	"testing"
 	"time"
 
-	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/trace"
 	"github.com/stretchr/testify/require"
+
+	"github.com/gravitational/teleport/api/types"
 )
 
 func TestPluginStaticCredentials(t *testing.T) {
