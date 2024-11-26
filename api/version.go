6767019a1f4a6b34593b511d8afd6ad39ec2a45a
Fred Heinecke <fred.heinecke@goteleport.com>
2024-11-26 15:21:51 -0600

    17.0.0-dev.fred-env-var-migration-1-8

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "17.0.0-dev.fred-env-var-migration-1-7"
+const Version = "17.0.0-dev.fred-env-var-migration-1-8"
 
 var SemVersion = semver.New(Version)
