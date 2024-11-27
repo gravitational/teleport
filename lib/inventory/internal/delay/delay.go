e9e6f31b3e4557a565789a8ba8544fa3b6a0a66c
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2024-11-27 11:03:36 +0100

    Rename jitteredticker to delay

--- lib/inventory/internal/delay/delay.go
+++ lib/inventory/internal/delay/delay.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-package jitteredticker
+package delay
 
 import (
 	"time"
@@ -27,11 +27,10 @@ import (
 
 // Params contains the parameters for [New].
 type Params struct {
-	// FirstInterval is the expected time between the creation of the
-	// [JitteredTicker] and the first tick. It's not modified by the configured
-	// jitter.
+	// FirstInterval is the expected time between the creation of the [Delay]
+	// and the first tick. It's not modified by the configured jitter.
 	FirstInterval time.Duration
-	// FixedInterval is the interval of the ticker, unless VariableInterval is
+	// FixedInterval is the interval of the delay, unless VariableInterval is
 	// set. If a jitter is configured, the interval will be jittered every tick.
 	FixedInterval time.Duration
 	// VariableInterval, if set, overrides FixedInterval at every tick.
@@ -43,12 +42,12 @@ type Params struct {
 	clock clockwork.Clock
 }
 
-// New returns a new, running [*JitteredTicker].
-func New(p Params) *JitteredTicker {
+// New returns a new, running [*Delay].
+func New(p Params) *Delay {
 	if p.clock == nil {
 		p.clock = clockwork.NewRealClock()
 	}
-	return &JitteredTicker{
+	return &Delay{
 		clock: p.clock,
 		timer: p.clock.NewTimer(p.FirstInterval),
 
@@ -59,11 +58,12 @@ func New(p Params) *JitteredTicker {
 	}
 }
 
-// JitteredTicker is a ticker that ticks with some jitter. Its use requires some
-// care as the logic driving the ticks and the jittering must be explicitly
-// invoked by the code making use of the ticker, but uses no background
-// resources (as it's essentially a small helper around a [*time.Timer]).
-type JitteredTicker struct {
+// Delay is a ticker-like abstraction around a [*time.Timer] that's made to tick
+// periodically with a potentially varying interval and optionally some jitter.
+// Its use requires some care as the logic driving the ticks and the jittering
+// must be explicitly invoked by the code making use of it, but uses no
+// background resources.
+type Delay struct {
 	clock clockwork.Clock
 	timer clockwork.Timer
 
@@ -73,25 +73,24 @@ type JitteredTicker struct {
 	jitter retryutils.Jitter
 }
 
-// Next returns the channel on which the ticks are delivered. This method can be
-// called on a nil ticker, resulting in a nil channel. The
-// [*JitteredTicker.Advance] method must be called after receiving each tick
-// from the channel.
+// Elapsed returns the channel on which the ticks are delivered. This method can
+// be called on a nil delay, resulting in a nil channel. The [*Delay.Advance]
+// method must be called after receiving a tick from the channel.
 //
 //	select {
 //		// other cases
-//		case now := <-t.Next():
+//		case now := <-t.Elapsed():
 //			t.Advance(now)
 //			// business logic here
 //	}
-func (i *JitteredTicker) Next() <-chan time.Time {
+func (i *Delay) Elapsed() <-chan time.Time {
 	if i == nil {
 		return nil
 	}
 	return i.timer.Chan()
 }
 
-func (i *JitteredTicker) interval() time.Duration {
+func (i *Delay) interval() time.Duration {
 	ivl := i.fixedInterval
 	if i.variableInterval != nil {
 		ivl = i.variableInterval.Duration()
@@ -102,20 +101,19 @@ func (i *JitteredTicker) interval() time.Duration {
 	return ivl
 }
 
-// Advance sets up the next tick of the ticker. Must be called after receiving
-// from the [*JitteredTicker.Next] channel; specifically, to maintain
-// compatibility with [clockwork.Clock], it must only be called with a drained
-// timer channel. For consistency, the value passed to Advance should be the
-// value received from the Next channel (passing the current time will also
-// work, but will not compensate for the time that passed since the last tick).
-func (i *JitteredTicker) Advance(now time.Time) {
+// Advance sets up the next tick of the delay. Must be called after receiving
+// from the [*Delay.Elapsed] channel; specifically, to maintain compatibility
+// with [clockwork.Clock], it must only be called with a drained timer channel.
+// For consistency, the value passed to Advance should be the value received
+// from the Elapsed channel (passing the current time will also work, but will
+// not compensate for the time that passed since the last tick).
+func (i *Delay) Advance(now time.Time) {
 	i.timer.Reset(i.interval() - i.clock.Since(now))
 }
 
-// Stop stops the ticker. Only needed for [clockwork.Clock] compatibility. Can
-// be called on a nil ticker, as a no-op. The ticker should not be used
-// afterwards.
-func (i *JitteredTicker) Stop() {
+// Stop stops the delay. Only needed for [clockwork.Clock] compatibility. Can be
+// called on a nil delay, as a no-op. The delay should not be used afterwards.
+func (i *Delay) Stop() {
 	if i == nil {
 		return
 	}
