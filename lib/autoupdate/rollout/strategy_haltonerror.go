963c6de90cebb099627d434b1010648aa49c9141
hugoShaka <hugo.hervieux@goteleport.com>
2024-12-03 18:13:27 -0500

    fix missing constants + add license

--- lib/autoupdate/rollout/strategy_haltonerror.go
+++ lib/autoupdate/rollout/strategy_haltonerror.go
@@ -1,3 +1,21 @@
+/*
+ * Teleport
+ * Copyright (C) 2024  Gravitational, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package rollout
 
 import (
@@ -10,6 +28,14 @@ import (
 
 	"github.com/gravitational/teleport/api/gen/proto/go/teleport/autoupdate/v1"
 	update "github.com/gravitational/teleport/api/types/autoupdate"
+)
+
+const (
+	updateReasonCanStart              = "can_start"
+	updateReasonCannotStart           = "cannot_start"
+	updateReasonPreviousGroupsNotDone = "previous_groups_not_done"
+	updateReasonUpdateComplete        = "update_complete"
+	updateReasonUpdateInProgress      = "update_in_progress"
 )
 
 type haltOnErrorStrategy struct {
