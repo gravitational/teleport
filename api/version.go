84114f82f52d6edfbddfc8cee2443e5128b1536e
Fred Heinecke <fred.heinecke@goteleport.com>
2024-12-03 14:30:19 -0600

    17.0.0-dev.fred-remove-e-ref-arg-3

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "17.0.0-dev.fred-remove-e-ref-arg-2"
+const Version = "17.0.0-dev.fred-remove-e-ref-arg-3"
 
 var SemVersion = semver.New(Version)
