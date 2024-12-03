b9591832ed016bb3332a95c9c0dc84ac498f57a4
Stephen Levine <stephen.levine@goteleport.com>
2024-12-03 16:58:23 -0500

    [teleport-update] Isolated installation suffix (#49364)
    * namespacing
    
    * words
    
    * cli
    
    * fix
    
    * err
    
    * use structured logs consistently
    
    * comments
    
    * bugs
    
    * test
    
    * switch to new paths
    
    * test
    
    * adjust
    
    * reserved
    
    * cleanup
    
    * cleanup
    
    * docs
    
    * fix uninstall
    
    * test
    
    * simplify init
    
    * cleanup
    
    * namespace -> install-suffix
    
    * log

--- lib/autoupdate/agent/setup_test.go
+++ lib/autoupdate/agent/setup_test.go
@@ -20,46 +20,172 @@ package agent
 
 import (
 	"bytes"
+	"log/slog"
 	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
-	libdefaults "github.com/gravitational/teleport/lib/defaults"
 	"github.com/gravitational/teleport/lib/utils/golden"
 )
 
-func TestWriteConfigFiles(t *testing.T) {
-	t.Parallel()
-	linkDir := t.TempDir()
-	dataDir := t.TempDir()
-	err := writeConfigFiles(linkDir, dataDir)
-	require.NoError(t, err)
-
-	for _, p := range []string{
-		filepath.Join(linkDir, serviceDir, updateServiceName),
-		filepath.Join(linkDir, serviceDir, updateTimerName),
+func TestNewNamespace(t *testing.T) {
+	for _, p := range []struct {
+		name      string
+		namespace string
+		linkDir   string
+		dataDir   string
+		errMatch  string
+		ns        *Namespace
+	}{
+		{
+			name: "no namespace",
+			ns: &Namespace{
+				dataDir:            "/var/lib/teleport",
+				linkDir:            "/usr/local/bin",
+				versionsDir:        "/opt/teleport/default/versions",
+				serviceFile:        "/lib/systemd/system/teleport.service",
+				configFile:         "/etc/teleport.yaml",
+				pidFile:            "/run/teleport.pid",
+				updaterLockFile:    "/opt/teleport/default/update.lock",
+				updaterConfigFile:  "/opt/teleport/default/update.yaml",
+				updaterBinFile:     "/usr/local/bin/teleport-update",
+				updaterServiceFile: "/etc/systemd/system/teleport-update.service",
+				updaterTimerFile:   "/etc/systemd/system/teleport-update.timer",
+			},
+		},
+		{
+			name:    "no namespace with dirs",
+			linkDir: "/link",
+			dataDir: "/data",
+			ns: &Namespace{
+				dataDir:            "/data",
+				linkDir:            "/link",
+				versionsDir:        "/opt/teleport/default/versions",
+				serviceFile:        "/lib/systemd/system/teleport.service",
+				configFile:         "/etc/teleport.yaml",
+				pidFile:            "/run/teleport.pid",
+				updaterLockFile:    "/opt/teleport/default/update.lock",
+				updaterConfigFile:  "/opt/teleport/default/update.yaml",
+				updaterBinFile:     "/link/teleport-update",
+				updaterServiceFile: "/etc/systemd/system/teleport-update.service",
+				updaterTimerFile:   "/etc/systemd/system/teleport-update.timer",
+			},
+		},
+		{
+			name:      "test namespace",
+			namespace: "test",
+			ns: &Namespace{
+				name:               "test",
+				dataDir:            "/var/lib/teleport_test",
+				linkDir:            "/opt/teleport/test/bin",
+				versionsDir:        "/opt/teleport/test/versions",
+				serviceFile:        "/etc/systemd/system/teleport_test.service",
+				configFile:         "/etc/teleport_test.yaml",
+				pidFile:            "/run/teleport_test.pid",
+				updaterLockFile:    "/opt/teleport/test/update.lock",
+				updaterConfigFile:  "/opt/teleport/test/update.yaml",
+				updaterBinFile:     "/opt/teleport/test/bin/teleport-update",
+				updaterServiceFile: "/etc/systemd/system/teleport-update_test.service",
+				updaterTimerFile:   "/etc/systemd/system/teleport-update_test.timer",
+			},
+		},
+		{
+			name:      "test namespace with dirs",
+			namespace: "test",
+			linkDir:   "/link",
+			dataDir:   "/data",
+			ns: &Namespace{
+				name:               "test",
+				dataDir:            "/data",
+				linkDir:            "/link",
+				versionsDir:        "/opt/teleport/test/versions",
+				serviceFile:        "/etc/systemd/system/teleport_test.service",
+				configFile:         "/etc/teleport_test.yaml",
+				pidFile:            "/run/teleport_test.pid",
+				updaterLockFile:    "/opt/teleport/test/update.lock",
+				updaterConfigFile:  "/opt/teleport/test/update.yaml",
+				updaterBinFile:     "/link/teleport-update",
+				updaterServiceFile: "/etc/systemd/system/teleport-update_test.service",
+				updaterTimerFile:   "/etc/systemd/system/teleport-update_test.timer",
+			},
+		},
+		{
+			name:      "reserved default",
+			namespace: defaultNamespace,
+			errMatch:  "reserved",
+		},
+		{
+			name:      "reserved system",
+			namespace: systemNamespace,
+			errMatch:  "reserved",
+		},
 	} {
-		t.Run(filepath.Base(p), func(t *testing.T) {
-			data, err := os.ReadFile(p)
+		t.Run(p.name, func(t *testing.T) {
+			log := slog.Default()
+			ns, err := NewNamespace(log, p.namespace, p.dataDir, p.linkDir)
+			if p.errMatch != "" {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), p.errMatch)
+				return
+			}
+			require.NoError(t, err)
+			ns.log = nil
+			require.Equal(t, p.ns, ns)
+		})
+	}
+}
+
+func TestWriteConfigFiles(t *testing.T) {
+	for _, p := range []struct {
+		name      string
+		namespace string
+	}{
+		{
+			name: "no namespace",
+		},
+		{
+			name:      "test namespace",
+			namespace: "test",
+		},
+	} {
+		t.Run(p.name, func(t *testing.T) {
+			log := slog.Default()
+			linkDir := t.TempDir()
+			ns, err := NewNamespace(log, p.namespace, "", linkDir)
+			require.NoError(t, err)
+			ns.updaterServiceFile = filepath.Join(linkDir, serviceDir, filepath.Base(ns.updaterServiceFile))
+			ns.updaterTimerFile = filepath.Join(linkDir, serviceDir, filepath.Base(ns.updaterTimerFile))
+			err = ns.writeConfigFiles()
+			require.NoError(t, err)
+
+			data, err := os.ReadFile(ns.updaterServiceFile)
 			require.NoError(t, err)
 			data = replaceValues(data, map[string]string{
-				DefaultLinkDir:      linkDir,
-				libdefaults.DataDir: dataDir,
+				DefaultLinkDir: linkDir,
 			})
 			if golden.ShouldSet() {
-				golden.Set(t, data)
+				golden.SetNamed(t, "service", data)
 			}
-			require.Equal(t, string(golden.Get(t)), string(data))
+			require.Equal(t, string(golden.GetNamed(t, "service")), string(data))
+
+			data, err = os.ReadFile(ns.updaterTimerFile)
+			require.NoError(t, err)
+			data = replaceValues(data, map[string]string{
+				DefaultLinkDir: linkDir,
+			})
+			if golden.ShouldSet() {
+				golden.SetNamed(t, "timer", data)
+			}
+			require.Equal(t, string(golden.GetNamed(t, "timer")), string(data))
 		})
 	}
 }
 
 func replaceValues(data []byte, m map[string]string) []byte {
 	for k, v := range m {
-		data = bytes.ReplaceAll(data, []byte(v),
-			[]byte(k))
+		data = bytes.ReplaceAll(data, []byte(v), []byte(k))
 	}
 	return data
 }
