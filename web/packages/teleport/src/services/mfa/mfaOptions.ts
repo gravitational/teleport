b1ae9e09c581071b48ccc8127fd59e6c4d18c9eb
joerger <bjoerger@goteleport.com>
2024-12-03 10:51:10 -0800

    Add MFA Option constants.

--- web/packages/teleport/src/services/mfa/mfaOptions.ts
+++ web/packages/teleport/src/services/mfa/mfaOptions.ts
@@ -1,30 +1,20 @@
 import { Auth2faType } from 'shared/services';
 
-import { DeviceType, MfaAuthenticateChallenge } from './types';
-
-export type MfaOption = {
-  value: DeviceType;
-  label: string;
-};
+import { DeviceType, MfaAuthenticateChallenge, SSOChallenge } from './types';
 
 export function getMfaChallengeOptions(mfaChallenge: MfaAuthenticateChallenge) {
   const mfaOptions: MfaOption[] = [];
 
   if (mfaChallenge?.webauthnPublicKey) {
-    mfaOptions.push({ value: 'webauthn', label: 'Passkey or Security Key' });
+    mfaOptions.push(webauthnOption);
   }
 
   if (mfaChallenge?.totpChallenge) {
-    mfaOptions.push({ value: 'totp', label: 'Authenticator App' });
+    mfaOptions.push(totpOption);
   }
 
   if (mfaChallenge?.ssoChallenge) {
-    mfaOptions.push({
-      value: 'sso',
-      label:
-        mfaChallenge.ssoChallenge.device.displayName ||
-        mfaChallenge.ssoChallenge.device.connectorId,
-    });
+    mfaOptions.push(getSsoOption(mfaChallenge.ssoChallenge));
   }
 
   return mfaOptions;
@@ -34,12 +24,31 @@ export function getMfaRegisterOptions(auth2faType: Auth2faType) {
   const mfaOptions: MfaOption[] = [];
 
   if (auth2faType === 'webauthn' || auth2faType === 'on') {
-    mfaOptions.push({ value: 'webauthn', label: 'Passkey or Security Key' });
+    mfaOptions.push(webauthnOption);
   }
 
   if (auth2faType === 'otp' || auth2faType === 'on') {
-    mfaOptions.push({ value: 'totp', label: 'Authenticator App' });
+    mfaOptions.push(totpOption);
   }
 
   return mfaOptions;
 }
+
+export type MfaOption = {
+  value: DeviceType;
+  label: string;
+};
+
+const webauthnOption: MfaOption = {
+  value: 'webauthn',
+  label: 'Passkey or Security Key',
+};
+
+const totpOption: MfaOption = { value: 'totp', label: 'Authenticator App' };
+
+const getSsoOption = (ssoChallenge: SSOChallenge): MfaOption => {
+  return {
+    value: 'sso',
+    label: ssoChallenge.device.displayName || ssoChallenge.device.connectorId,
+  };
+};
