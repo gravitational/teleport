1b078c2e3f2f54756ee853519efb1d32fc73133d
Tim Ross <tim.ross@goteleport.com>
2024-12-03 15:13:51 -0500

    v16.4.2-dev-tross-kbd-interactive.2

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "16.4.2-dev-tross-kbd-interactive.1"
+const Version = "16.4.2-dev-tross-kbd-interactive.2"
 
 var SemVersion = semver.New(Version)
