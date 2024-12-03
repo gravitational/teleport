426a00b191d13229ce542da198125573d2137113
Tim Ross <tim.ross@goteleport.com>
2024-12-03 09:05:38 -0500

    v16.4.2-dev-tross-pam-auth.2

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "16.4.2-dev-tross-pam-auth.1"
+const Version = "16.4.2-dev-tross-pam-auth.2"
 
 var SemVersion = semver.New(Version)
