5911b8508ed9f87f62d7c2281cd741be382d5188
joerger <bjoerger@goteleport.com>
2024-12-04 13:02:25 -0800

    Reuse useReAuthenticate in useMfa.

--- web/packages/teleport/src/components/AuthnDialog/AuthnDialog.story.tsx
+++ web/packages/teleport/src/components/AuthnDialog/AuthnDialog.story.tsx
@@ -31,18 +31,20 @@ export const LoadedWithMultipleOptions = () => {
     ...defaultProps,
     mfa: {
       ...defaultProps.mfa,
-      ssoChallenge: {
-        redirectUrl: 'hi',
-        requestId: '123',
-        channelId: '123',
-        device: {
-          connectorId: '123',
-          connectorType: 'saml',
-          displayName: 'Okta',
+      challenge: {
+        ssoChallenge: {
+          redirectUrl: 'hi',
+          requestId: '123',
+          channelId: '123',
+          device: {
+            connectorId: '123',
+            connectorType: 'saml',
+            displayName: 'Okta',
+          },
         },
-      },
-      webauthnPublicKey: {
-        challenge: new ArrayBuffer(1),
+        webauthnPublicKey: {
+          challenge: new ArrayBuffer(1),
+        },
       },
     },
   };
@@ -54,8 +56,10 @@ export const LoadedWithSingleOption = () => {
     ...defaultProps,
     mfa: {
       ...defaultProps.mfa,
-      webauthnPublicKey: {
-        challenge: new ArrayBuffer(1),
+      challenge: {
+        webauthnPublicKey: {
+          challenge: new ArrayBuffer(1),
+        },
       },
     },
   };
@@ -67,7 +71,10 @@ export const Error = () => {
     ...defaultProps,
     mfa: {
       ...defaultProps.mfa,
-      errorText: 'Something went wrong',
+      submitAttempt: {
+        status: 'failed',
+        statusText: 'Something went wrong',
+      },
     },
   };
   return <AuthnDialog {...props} />;
