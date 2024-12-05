b1253e1f522640b29cbb820085fbc4b108d1196d
Tim Ross <tim.ross@goteleport.com>
2024-12-05 09:55:44 -0500

    Use builtin tracing machinery of aws-sdk-go-v2
    Replaces the use of go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-sdk-go-v2/otelaws
    with the tracing support now provided directly by the aws sdk.
    See https://github.com/aws/aws-sdk-go-v2/discussions/2810 for more
    details.
    
    Some additional aws clients were also updated to include tracing
    support.

--- lib/auth/keystore/aws_kms.go
+++ lib/auth/keystore/aws_kms.go
@@ -37,8 +37,10 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/kms"
 	kmstypes "github.com/aws/aws-sdk-go-v2/service/kms/types"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
+	"github.com/aws/smithy-go/tracing/smithyoteltracing"
 	"github.com/gravitational/trace"
 	"github.com/jonboulle/clockwork"
+	"go.opentelemetry.io/otel"
 	"golang.org/x/sync/errgroup"
 
 	"github.com/gravitational/teleport/api/types"
@@ -81,10 +83,14 @@ func newAWSKMSKeystore(ctx context.Context, cfg *servicecfg.AWSKMSConfig, opts *
 			return nil, trace.Wrap(err, "loading default AWS config")
 		}
 		if stsClient == nil {
-			stsClient = sts.NewFromConfig(awsCfg)
+			stsClient = sts.NewFromConfig(awsCfg, func(o *sts.Options) {
+				o.TracerProvider = smithyoteltracing.Adapt(otel.GetTracerProvider())
+			})
 		}
 		if kmsClient == nil {
-			kmsClient = kms.NewFromConfig(awsCfg)
+			kmsClient = kms.NewFromConfig(awsCfg, func(o *kms.Options) {
+				o.TracerProvider = smithyoteltracing.Adapt(otel.GetTracerProvider())
+			})
 		}
 	}
 	id, err := stsClient.GetCallerIdentity(ctx, &sts.GetCallerIdentityInput{})
