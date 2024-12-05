38d25b479e95eede78223cc8119aa9dd17ecea9f
hugoShaka <hugo.hervieux@goteleport.com>
2024-12-05 11:57:57 -0500

    Address edoardo and tiago's feedback + dedup audiences

--- lib/kubernetestoken/token_validator_test.go
+++ lib/kubernetestoken/token_validator_test.go
@@ -156,7 +156,7 @@ func TestGetTokenAudiences(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			result, err := getTokenAudiences(tt.token)
+			result, err := unsafeGetTokenAudiences(tt.token)
 			tt.expectErr(t, err)
 			assert.Equal(t, tt.expectedAudiences, result)
 		})
