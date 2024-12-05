5715ecacf0be98afdc7392a36defa08ff18e33f1
Rafał Cieślak <rafal.cieslak@goteleport.com>
2024-12-05 13:50:27 +0100

    Pass entry to callback, add info about conditionally rendering ref

--- web/packages/design/src/utils/useResizeObserver.ts
+++ web/packages/design/src/utils/useResizeObserver.ts
@@ -24,11 +24,12 @@ import { RefObject, useCallback, useLayoutEffect } from 'react';
  * It does not fire if ref.current.contentRect.height is zero, to account for a special case in
  * Connect where tabs are hidden using `display: none;`.
  *
- * Uses a layout effect underneath.
+ * Uses a layout effect underneath. If ref is conditionally rendered, set enabled to false when ref
+ * is null.
  */
 export function useResizeObserver(
   ref: RefObject<HTMLElement>,
-  callback: () => void,
+  callback: (entry: ResizeObserverEntry) => void,
   { enabled = true }
 ): void {
   const effect = useCallback(() => {
@@ -37,16 +38,16 @@ export function useResizeObserver(
     }
 
     const observer = new ResizeObserver(entries => {
-      const element = entries[0];
+      const entry = entries[0];
 
       // In Connect, when a tab becomes active, its outermost DOM element switches from `display:
       // none` to `display: flex`. This callback is then fired with the height reported as zero.
       // To avoid unnecessary calls to callback, return early here.
-      if (element.contentRect.height === 0) {
+      if (entry.contentRect.height === 0) {
         return;
       }
 
-      callback();
+      callback(entry);
     });
 
     observer.observe(ref.current);
