a5ea81d7bbec885b960d57481845aeb6098e57bc
hugoShaka <hugo.hervieux@goteleport.com>
2024-12-09 12:56:14 -0500

    lint

--- lib/autoupdate/rollout/controller.go
+++ lib/autoupdate/rollout/controller.go
@@ -69,8 +69,8 @@ func NewController(client Client, log *slog.Logger, clock clockwork.Clock, perio
 		clock: clock,
 		log:   log,
 		reconciler: reconciler{
-			clt: client,
-			log: log,
+			clt:               client,
+			log:               log,
 			rolloutStrategies: []rolloutStrategy{
 				// TODO(hugoShaka): add the strategies here as we implement them
 			},
