182a8355678fd3ab36ca767c308f992396e69a72
Tim Ross <tim.ross@goteleport.com>
2024-12-09 16:09:49 -0500

    Filter while paging in GetByVMID
    This updates GetByVMID, used by azure joining, to filter by id for
    each page of vms instead of filtering after accumulating _all_ vms
    in memory first. While this does reduce memory consumption, the listing
    process is still suboptimal in that _all_ instances in a subscription
    are retrieved in response to _every_ join request.

--- lib/srv/discovery/discovery_test.go
+++ lib/srv/discovery/discovery_test.go
@@ -2302,7 +2302,7 @@ func (m *mockAzureClient) Get(_ context.Context, _ string) (*azure.VirtualMachin
 	return nil, nil
 }
 
-func (m *mockAzureClient) GetByVMID(_ context.Context, _, _ string) (*azure.VirtualMachine, error) {
+func (m *mockAzureClient) GetByVMID(_ context.Context, _ string) (*azure.VirtualMachine, error) {
 	return nil, nil
 }
 
