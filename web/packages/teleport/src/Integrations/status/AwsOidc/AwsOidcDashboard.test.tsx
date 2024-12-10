e47ec40a002c36922dfb1246448a7a9f202bf7b8
Michelle Bergquist <11967646+michellescripts@users.noreply.github.com>
2024-12-10 09:11:39 -0700

    Show aws summary stats on dashboard (#49516)

--- web/packages/teleport/src/Integrations/status/AwsOidc/AwsOidcDashboard.test.tsx
+++ web/packages/teleport/src/Integrations/status/AwsOidc/AwsOidcDashboard.test.tsx
@@ -19,16 +19,20 @@
 import React from 'react';
 import { render, screen } from 'design/utils/testing';
 
+import { within } from '@testing-library/react';
+
 import { AwsOidcDashboard } from 'teleport/Integrations/status/AwsOidc/AwsOidcDashboard';
 import { MockAwsOidcStatusProvider } from 'teleport/Integrations/status/AwsOidc/testHelpers/mockAwsOidcStatusProvider';
 import { IntegrationKind } from 'teleport/services/integrations';
+import { addHours } from 'teleport/components/BannerList/useAlerts';
 
-test('renders header', () => {
+test('renders header and stats cards', () => {
   render(
     <MockAwsOidcStatusProvider
       value={{
-        attempt: {
+        integrationAttempt: {
           status: 'success',
+          statusText: '',
           data: {
             resourceType: 'integration',
             name: 'integration-one',
@@ -38,7 +42,41 @@ test('renders header', () => {
             },
             statusCode: 1,
           },
+        },
+        statsAttempt: {
+          status: 'success',
           statusText: '',
+          data: {
+            name: 'integration-one',
+            subKind: IntegrationKind.AwsOidc,
+            awsoidc: {
+              roleArn: 'arn:aws:iam::111456789011:role/bar',
+            },
+            awsec2: {
+              rulesCount: 24,
+              resourcesFound: 12,
+              resourcesEnrollmentFailed: 3,
+              resourcesEnrollmentSuccess: 9,
+              discoverLastSync: new Date().getTime(),
+              ecsDatabaseServiceCount: 0, // irrelevant
+            },
+            awsrds: {
+              rulesCount: 14,
+              resourcesFound: 5,
+              resourcesEnrollmentFailed: 5,
+              resourcesEnrollmentSuccess: 0,
+              discoverLastSync: addHours(new Date().getTime(), -4),
+              ecsDatabaseServiceCount: 8, // relevant
+            },
+            awseks: {
+              rulesCount: 33,
+              resourcesFound: 3,
+              resourcesEnrollmentFailed: 0,
+              resourcesEnrollmentSuccess: 3,
+              discoverLastSync: addHours(new Date().getTime(), -48),
+              ecsDatabaseServiceCount: 0, // irrelevant
+            },
+          },
         },
       }}
     >
@@ -53,4 +91,49 @@ test('renders header', () => {
   expect(screen.getByText('integration-one')).toBeInTheDocument();
   expect(screen.getByLabelText('status')).toHaveAttribute('kind', 'success');
   expect(screen.getByLabelText('status')).toHaveTextContent('Running');
+
+  const ec2 = screen.getByTestId('ec2-stats');
+  expect(within(ec2).getByTestId('sync')).toHaveTextContent(
+    'Last Sync: 0 seconds ago'
+  );
+  expect(within(ec2).getByTestId('rules')).toHaveTextContent(
+    'Enrollment Rules 24'
+  );
+  expect(within(ec2).queryByTestId('rds-agents')).not.toBeInTheDocument();
+  expect(within(ec2).getByTestId('enrolled')).toHaveTextContent(
+    'Enrolled Instances 9'
+  );
+  expect(within(ec2).getByTestId('failed')).toHaveTextContent(
+    'Failed Instances 3'
+  );
+
+  const rds = screen.getByTestId('rds-stats');
+  expect(within(rds).getByTestId('sync')).toHaveTextContent(
+    'Last Sync: 4 hours ago'
+  );
+  expect(within(rds).getByTestId('rules')).toHaveTextContent(
+    'Enrollment Rules 14'
+  );
+  expect(within(rds).getByTestId('rds-agents')).toHaveTextContent('Agents 8');
+  expect(within(rds).getByTestId('enrolled')).toHaveTextContent(
+    'Enrolled Databases 0'
+  );
+  expect(within(rds).getByTestId('failed')).toHaveTextContent(
+    'Failed Databases 5'
+  );
+
+  const eks = screen.getByTestId('eks-stats');
+  expect(within(eks).getByTestId('sync')).toHaveTextContent(
+    'Last Sync: 2 days ago'
+  );
+  expect(within(eks).getByTestId('rules')).toHaveTextContent(
+    'Enrollment Rules 33'
+  );
+  expect(within(eks).queryByTestId('rds-agents')).not.toBeInTheDocument();
+  expect(within(eks).getByTestId('enrolled')).toHaveTextContent(
+    'Enrolled Clusters 3'
+  );
+  expect(within(eks).getByTestId('failed')).toHaveTextContent(
+    'Failed Clusters 0'
+  );
 });
