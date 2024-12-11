5b15728326bb1db8525fcc43ff864aa62970e63e
Tim Ross <tim.ross@goteleport.com>
2024-12-11 15:20:06 -0500

    Wait for process to exit before terminating tests
    This attempts to reduce the flakiness of tests caused by failing
    to clean up the data dir:
    
    ```
     testing.go:1232: TempDir RemoveAll cleanup: unlinkat /tmp/TestMonitor3923075621/001: directory not empty
    ```
    
    The root cause of the flakiness is that the tests are exiting
    prior to the Teleport process terminating. All places that Close
    was called now include a Wait call to ensure things have all exited
    before returning from the test.

--- lib/service/service_test.go
+++ lib/service/service_test.go
@@ -293,7 +293,10 @@ func TestMonitor(t *testing.T) {
 	process.BroadcastEvent(Event{Name: TeleportOKEvent, Payload: teleport.ComponentAuth})
 
 	require.NoError(t, process.Start())
-	t.Cleanup(func() { require.NoError(t, process.Close()) })
+	t.Cleanup(func() {
+		require.NoError(t, process.Close())
+		require.NoError(t, process.Wait())
+	})
 
 	diagAddr, err := process.DiagnosticAddr()
 	require.NoError(t, err)
@@ -1630,7 +1633,10 @@ func TestDebugServiceStartSocket(t *testing.T) {
 	require.NoError(t, err)
 
 	require.NoError(t, process.Start())
-	t.Cleanup(func() { require.NoError(t, process.Close()) })
+	t.Cleanup(func() {
+		require.NoError(t, process.Close())
+		require.NoError(t, process.Wait())
+	})
 
 	httpClient := &http.Client{
 		Timeout: 10 * time.Second,
@@ -1744,6 +1750,7 @@ func TestInstanceMetadata(t *testing.T) {
 			require.NoError(t, err)
 			t.Cleanup(func() {
 				require.NoError(t, process.Close())
+				require.NoError(t, process.Wait())
 			})
 
 			if tc.expectCloudLabels {
