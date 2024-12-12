b93e6ee674e6d4a3d2cffcb8beb3d475b3efee5d
Trent Clarke <trent@goteleport.com>
2024-12-13 10:25:48 +1100

    Code review sugestions

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -5130,7 +5130,7 @@ func (a *Server) CreateAccessRequestV2(ctx context.Context, req types.AccessRequ
 	}
 
 	// Look for user groups and associated applications to the request.
-	requestedResourceIDs, err := a.appendAdditionalResources(ctx, req.GetRequestedResourceIDs())
+	requestedResourceIDs, err := a.appendImplicitlyRequiredResources(ctx, req.GetRequestedResourceIDs())
 	if err != nil {
 		return nil, trace.Wrap(err, "adding additional implicitly required resources")
 	}
@@ -5285,6 +5285,9 @@ func (a *Server) appendImplicitlyRequiredResources(ctx context.Context, resource
 
 	icAccounts := utils.NewSet[string]()
 	for _, resource := range accountAssignments {
+		// The UI needs access to the account associated with an Account Assignment
+		// in order to display the enclosing Account, otherwise the user will not
+		// be able to see their assigned permission sets.
 		assignmentID := services.IdentityCenterAccountAssignmentID(resource.Name)
 		asmt, err := a.Services.IdentityCenter.GetAccountAssignment(ctx, assignmentID)
 		if err != nil {
@@ -5299,7 +5302,6 @@ func (a *Server) appendImplicitlyRequiredResources(ctx context.Context, resource
 			ClusterName: resource.ClusterName,
 			Kind:        types.KindIdentityCenterAccount,
 			Name:        asmt.GetSpec().GetAccountId(),
-		}),
 		})
 		icAccounts.Add(asmt.GetSpec().GetAccountId())
 	}
