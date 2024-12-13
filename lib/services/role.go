aa520bcfa86361621fc08657798e7c4b050a6650
Erik Tate <erik.tate@goteleport.com>
2024-12-13 16:05:49 -0500

    adding SSHPortForwarding config to RoleOptions (#50215)

--- lib/services/role.go
+++ lib/services/role.go
@@ -2851,6 +2851,7 @@ func (set RoleSet) CanForwardAgents() bool {
 // CanPortForward returns true if a role in the RoleSet allows port forwarding.
 func (set RoleSet) CanPortForward() bool {
 	for _, role := range set {
+		//nolint:staticcheck // this field is preserved for existing deployments, but shouldn't be used going forward
 		if types.BoolDefaultTrue(role.GetOptions().PortForwarding) {
 			return true
 		}
