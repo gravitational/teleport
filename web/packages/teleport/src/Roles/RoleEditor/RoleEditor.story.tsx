55b7ee521c1d4140f61997639e954622c42c1cef
Bartosz Leper <bartosz.leper@goteleport.com>
2024-12-13 22:03:07 +0100

    [v17] More tweaks to the role editor UI (#50222)
    * More tweaks to the role editor UI
    
    - Add default trait references to resource access definitions
    - Remove the role deletion button
    
    * Type check fix

--- web/packages/teleport/src/Roles/RoleEditor/RoleEditor.story.tsx
+++ web/packages/teleport/src/Roles/RoleEditor/RoleEditor.story.tsx
@@ -286,7 +286,6 @@ export const Dialog: StoryObj = {
           open={open}
           onClose={() => setOpen(false)}
           onSave={async () => setOpen(false)}
-          onDelete={async () => setOpen(false)}
         />
       </>
     );
@@ -311,7 +310,6 @@ export const DialogWithPolicyEnabled: StoryObj = {
           open={open}
           onClose={() => setOpen(false)}
           onSave={async () => setOpen(false)}
-          onDelete={async () => setOpen(false)}
         />
       </>
     );
