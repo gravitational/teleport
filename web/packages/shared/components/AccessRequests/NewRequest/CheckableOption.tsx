941a99301ef1b3871c0ff3bafefeaddfc5f83904
Grzegorz Zdunek <gzdunek@users.noreply.github.com>
2024-12-13 16:12:14 +0100

    Remove unused `React` imports from `packages/shared` (#50207)

--- web/packages/shared/components/AccessRequests/NewRequest/CheckableOption.tsx
+++ web/packages/shared/components/AccessRequests/NewRequest/CheckableOption.tsx
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from 'react';
 import { Flex, Text } from 'design';
 import { components, OptionProps } from 'react-select';
 
--- web/packages/shared/components/AccessRequests/ReviewRequests/RequestView/RequestDelete/RequestDelete.tsx
+++ web/packages/shared/components/AccessRequests/ReviewRequests/RequestView/RequestDelete/RequestDelete.tsx
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from 'react';
 import { ButtonWarning, ButtonSecondary, Flex, Alert } from 'design';
 import TextSelectCopy from 'teleport/components/TextSelectCopy';
 
