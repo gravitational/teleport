317744cce9a1c894cdd94785ee7011fe9f50c940
Zac Bergquist <zac.bergquist@goteleport.com>
2024-12-18 11:47:29 -0700

    Remove unused CSRF token (#5721)
    This probably should never have been added to the gRPC API and looks
    like a leftover of the old web API. In either case, this field is
    never consulted. Removing it here so that a future OSS commit can
    remove the field completely.
    
    Source-commit: gravitational/teleport.e@b6e7fac8adebd22d3f77b214455e204161cc0302
    Source-pull-requests: gravitational/teleport.e#5721

--- e/lib/web/sso.go
+++ e/lib/web/sso.go
@@ -38,7 +38,6 @@ func (p *Plugin) oidcLoginWeb(w http.ResponseWriter, r *http.Request, params htt
 
 	proxyClient := p.h.GetProxyClient()
 	response, err := proxyClient.CreateOIDCAuthRequest(r.Context(), types.OIDCAuthRequest{
-		CSRFToken:         req.CSRFToken,
 		ConnectorID:       req.ConnectorID,
 		CreateWebSession:  true,
 		ClientRedirectURL: req.ClientRedirectURL,
@@ -213,7 +212,6 @@ func (p *Plugin) samlSSO(w http.ResponseWriter, r *http.Request, params httprout
 	proxyClient := p.h.GetProxyClient()
 	response, err := proxyClient.CreateSAMLAuthRequest(r.Context(), types.SAMLAuthRequest{
 		ConnectorID:       req.ConnectorID,
-		CSRFToken:         req.CSRFToken,
 		CreateWebSession:  true,
 		ClientRedirectURL: req.ClientRedirectURL,
 		ClientLoginIP:     remoteAddr,
