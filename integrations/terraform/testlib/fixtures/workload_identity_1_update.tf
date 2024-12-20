3796023d0132c67023b11e2c1bd38721603c1641
Noah Stride <noah.stride@goteleport.com>
2024-12-20 18:37:20 +0000

    Add support for WorkloadIdentity resource to the Teleport Terraform Provider (#50382)
    * First pass at trying to generate terraform provider
    
    * Wire up types
    
    * Fix generation of docs
    
    * Add godoc comments
    
    * Add tests
    
    * Tflint and add example

---
+++ integrations/terraform/testlib/fixtures/workload_identity_1_update.tf
@@ -0,0 +1,21 @@
+resource "teleport_workload_identity" "test" {
+  version = "v1"
+  metadata = {
+    name = "test"
+  }
+  spec = {
+    rules = {
+      allow = [
+        {
+          conditions = [{
+            attribute = "user.name"
+            equals    = "foo"
+          }]
+        }
+      ]
+    }
+    spiffe = {
+      id = "/test/updated"
+    }
+  }
+}
