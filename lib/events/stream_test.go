ea8e0b9f829c7cd58df292a416770e05aa866312
Dustin Specker <dustin.specker@goteleport.com>
2024-12-20 11:42:35 -0600

    fix(lib/events): remove already moved unit test
    TestReadCorruptedReading was moved to api/sessionrecording.

--- lib/events/stream_test.go
+++ lib/events/stream_test.go
@@ -21,7 +21,6 @@ package events_test
 import (
 	"context"
 	"errors"
-	"os"
 	"strings"
 	"testing"
 	"time"
@@ -29,7 +28,6 @@ import (
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 
-	"github.com/gravitational/teleport/api/sessionrecording"
 	apievents "github.com/gravitational/teleport/api/types/events"
 	"github.com/gravitational/teleport/lib/events"
 	"github.com/gravitational/teleport/lib/events/eventstest"
@@ -202,26 +200,6 @@ func TestProtoStreamLargeEvent(t *testing.T) {
 		})
 	}
 	require.NoError(t, stream.Complete(ctx))
-}
-
-// TestReadCorruptedRecording tests that the streamer can successfully decode the kind of corrupted
-// recordings that some older bugged versions of teleport might end up producing when under heavy load/throttling.
-func TestReadCorruptedRecording(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	f, err := os.Open("testdata/corrupted-session")
-	require.NoError(t, err)
-	defer f.Close()
-
-	reader := sessionrecording.NewReader(f)
-	defer reader.Close()
-
-	events, err := reader.ReadAll(ctx)
-	require.NoError(t, err)
-
-	// verify that the expected number of events are extracted
-	require.Len(t, events, 12)
 }
 
 func makeQueryEvent(id string, query string) *apievents.DatabaseSessionQuery {
