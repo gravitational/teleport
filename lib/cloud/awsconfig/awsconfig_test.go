d280bc5fc308a48f2f83cae6f4d6830c3cc7032f
Gavin Frazar <gavin.frazar@goteleport.com>
2024-12-23 12:42:34 -0800

    Update awsconfig
    * Add a Cache for caching credentials, similar to SDK v1 session cache.
    * Add a Provider interface that provides aws.Config
    * Simplified role chaining options
    
    Unlike our SDK v1 session cache, the SDK v2 implementation in this PR
    does not include region as a cache key.
    There are regional AWS STS endpoints for lower latency calls, but the
    lowest latency path is to just grab credentials from the cache if we
    already have them - the region they were originally taken from doesn't
    matter.

--- lib/cloud/awsconfig/awsconfig_test.go
+++ lib/cloud/awsconfig/awsconfig_test.go
@@ -18,9 +18,14 @@ package awsconfig
 
 import (
 	"context"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+	ststypes "github.com/aws/aws-sdk-go-v2/service/sts/types"
 	"github.com/gravitational/trace"
 	"github.com/stretchr/testify/require"
 )
@@ -29,18 +34,60 @@ type mockCredentialProvider struct {
 	cred aws.Credentials
 }
 
-func (m *mockCredentialProvider) Retrieve(ctx context.Context) (aws.Credentials, error) {
+func (m *mockCredentialProvider) Retrieve(_ context.Context) (aws.Credentials, error) {
 	return m.cred, nil
+}
+
+type mockAssumeRoleAPIClient struct{}
+
+func (m *mockAssumeRoleAPIClient) AssumeRole(_ context.Context, params *sts.AssumeRoleInput, optFns ...func(*sts.Options)) (*sts.AssumeRoleOutput, error) {
+	fakeKeyID := fmt.Sprintf("role: %s, externalID: %s", aws.ToString(params.RoleArn), aws.ToString(params.ExternalId))
+	return &sts.AssumeRoleOutput{
+		AssumedRoleUser: &ststypes.AssumedRoleUser{
+			Arn:           params.RoleArn,
+			AssumedRoleId: aws.String("role-id"),
+		},
+		Credentials: &ststypes.Credentials{
+			AccessKeyId:     aws.String(fakeKeyID),
+			Expiration:      aws.Time(time.Time{}),
+			SecretAccessKey: aws.String("fake-secret-access-key"),
+			SessionToken:    aws.String("fake-session-token"),
+		},
+	}, nil
 }
 
 func TestGetConfigIntegration(t *testing.T) {
 	t.Parallel()
+
+	cache, err := NewCache()
+	require.NoError(t, err)
+	tests := []struct {
+		desc string
+		Provider
+	}{
+		{
+			desc:     "uncached",
+			Provider: ProviderFunc(GetConfig),
+		},
+		{
+			desc:     "cached",
+			Provider: cache,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			testGetConfigIntegration(t, test.Provider)
+		})
+	}
+}
+
+func testGetConfigIntegration(t *testing.T, provider Provider) {
 	dummyIntegration := "integration-test"
 	dummyRegion := "test-region-123"
 
 	t.Run("without an integration credential provider, must return missing credential provider error", func(t *testing.T) {
 		ctx := context.Background()
-		_, err := GetConfig(ctx, dummyRegion, WithCredentialsMaybeIntegration(dummyIntegration))
+		_, err := provider.GetConfig(ctx, dummyRegion, WithCredentialsMaybeIntegration(dummyIntegration))
 		require.True(t, trace.IsBadParameter(err), "unexpected error: %v", err)
 		require.ErrorContains(t, err, "missing aws integration credential provider")
 	})
@@ -48,7 +95,7 @@ func TestGetConfigIntegration(t *testing.T) {
 	t.Run("with an integration credential provider, must return the credentials", func(t *testing.T) {
 		ctx := context.Background()
 
-		cfg, err := GetConfig(ctx, dummyRegion,
+		cfg, err := provider.GetConfig(ctx, dummyRegion,
 			WithCredentialsMaybeIntegration(dummyIntegration),
 			WithIntegrationCredentialProvider(func(ctx context.Context, region, integration string) (aws.CredentialsProvider, error) {
 				if region == dummyRegion && integration == dummyIntegration {
@@ -66,10 +113,40 @@ func TestGetConfigIntegration(t *testing.T) {
 		require.Equal(t, "foo-bar", creds.SessionToken)
 	})
 
+	t.Run("with an integration credential provider assuming a role, must return assumed role credentials", func(t *testing.T) {
+		ctx := context.Background()
+
+		cfg, err := provider.GetConfig(ctx, dummyRegion,
+			WithCredentialsMaybeIntegration(dummyIntegration),
+			WithIntegrationCredentialProvider(func(ctx context.Context, region, integration string) (aws.CredentialsProvider, error) {
+				if region == dummyRegion && integration == dummyIntegration {
+					return &mockCredentialProvider{
+						cred: aws.Credentials{
+							SessionToken: "foo-bar",
+						},
+					}, nil
+				}
+				return nil, trace.NotFound("no creds in region %q with integration %q", region, integration)
+			}),
+			WithAssumeRole("roleA", "abc123"),
+			WithAssumeRoleClientProviderFunc(func(cfg aws.Config) stscreds.AssumeRoleAPIClient {
+				creds, err := cfg.Credentials.Retrieve(context.Background())
+				require.NoError(t, err)
+				require.Equal(t, "foo-bar", creds.SessionToken)
+				return &mockAssumeRoleAPIClient{}
+			}),
+		)
+		require.NoError(t, err)
+		creds, err := cfg.Credentials.Retrieve(ctx)
+		require.NoError(t, err)
+		require.Equal(t, "role: roleA, externalID: abc123", creds.AccessKeyID)
+		require.Equal(t, "fake-session-token", creds.SessionToken)
+	})
+
 	t.Run("with an integration credential provider, but using an empty integration falls back to ambient credentials", func(t *testing.T) {
 		ctx := context.Background()
 
-		_, err := GetConfig(ctx, dummyRegion,
+		_, err := provider.GetConfig(ctx, dummyRegion,
 			WithCredentialsMaybeIntegration(""),
 			WithIntegrationCredentialProvider(func(ctx context.Context, region, integration string) (aws.CredentialsProvider, error) {
 				require.Fail(t, "this function should not be called")
@@ -81,7 +158,7 @@ func TestGetConfigIntegration(t *testing.T) {
 	t.Run("with an integration credential provider, but using ambient credentials", func(t *testing.T) {
 		ctx := context.Background()
 
-		_, err := GetConfig(ctx, dummyRegion,
+		_, err := provider.GetConfig(ctx, dummyRegion,
 			WithAmbientCredentials(),
 			WithIntegrationCredentialProvider(func(ctx context.Context, region, integration string) (aws.CredentialsProvider, error) {
 				require.Fail(t, "this function should not be called")
@@ -93,7 +170,7 @@ func TestGetConfigIntegration(t *testing.T) {
 	t.Run("with an integration credential provider, but no credential source", func(t *testing.T) {
 		ctx := context.Background()
 
-		_, err := GetConfig(ctx, dummyRegion,
+		_, err := provider.GetConfig(ctx, dummyRegion,
 			WithIntegrationCredentialProvider(func(ctx context.Context, region, integration string) (aws.CredentialsProvider, error) {
 				require.Fail(t, "this function should not be called")
 				return nil, nil
@@ -101,4 +178,51 @@ func TestGetConfigIntegration(t *testing.T) {
 		require.Error(t, err)
 		require.ErrorContains(t, err, "missing credentials source")
 	})
+}
+
+func TestGetCacheKeyForRoles(t *testing.T) {
+	tests := []struct {
+		desc    string
+		roles   []assumeRole
+		want    string
+		wantErr string
+	}{
+		{
+			desc: "valid without external ID",
+			roles: []assumeRole{
+				{roleARN: "roleA"},
+				{roleARN: "roleB"},
+			},
+			want: "roleA||roleB||",
+		},
+		{
+			desc: "valid with external ID",
+			roles: []assumeRole{
+				{roleARN: "roleA", externalID: "extA"},
+				{roleARN: "roleB", externalID: "extB"},
+			},
+			want: "roleA|extA|roleB|extB|",
+		},
+		{
+			desc:    "invalid role ARN",
+			roles:   []assumeRole{{roleARN: "roleA|extA|roleB"}},
+			wantErr: "invalid role ARN",
+		},
+		{
+			desc:    "invalid external ID",
+			roles:   []assumeRole{{roleARN: "roleA", externalID: "extA|roleB|extB"}},
+			wantErr: "invalid external ID",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			got, err := getCacheKeyForRoles(test.roles)
+			if test.wantErr != "" {
+				require.Error(t, err)
+				require.ErrorContains(t, err, test.wantErr)
+				return
+			}
+			require.Equal(t, test.want, got)
+		})
+	}
 }
