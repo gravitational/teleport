d71268f45e6da54f61bb6232b4ca7e5eb7c3bac8
matheus <matheus.battirola@goteleport.com>
2024-12-24 12:09:18 -0300

    Show cluster management page on dashboards (#50489)

--- web/packages/teleport/src/features.tsx
+++ web/packages/teleport/src/features.tsx
@@ -569,7 +569,7 @@ export class FeatureClusters implements TeleportFeature {
   };
 
   hasAccess(flags: FeatureFlags) {
-    return flags.trustedClusters;
+    return cfg.isDashboard || flags.trustedClusters;
   }
 
   navigationItem = {
