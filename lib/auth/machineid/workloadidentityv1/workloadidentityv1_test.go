56fde49445b04f6e5b39a71c10ca2ced07a88388
Noah Stride <noah.stride@goteleport.com>
2025-01-01 16:49:22 +0000

    Add test

--- lib/auth/machineid/workloadidentityv1/workloadidentityv1_test.go
+++ lib/auth/machineid/workloadidentityv1/workloadidentityv1_test.go
@@ -218,6 +218,12 @@ func TestIssueWorkloadIdentity(t *testing.T) {
 			Spiffe: &workloadidentityv1pb.WorkloadIdentitySPIFFE{
 				Id:   "/example/{{user.name}}/{{ workload.kubernetes.namespace }}/{{ workload.kubernetes.service_account }}",
 				Hint: "Wow - what a lovely hint, {{user.name}}!",
+				X509Svid: &workloadidentityv1pb.WorkloadIdentitySPIFFEX509SVID{
+					DnsSans: []string{
+						"example.com",
+						"{{user.name}}.example.com",
+					},
+				},
 			},
 		},
 	})
@@ -386,6 +392,7 @@ func TestIssueWorkloadIdentity(t *testing.T) {
 				require.WithinDuration(t, tp.clock.Now().Add(-1*time.Minute), cert.NotBefore, time.Second)
 				// Check cert TTL
 				require.Equal(t, cert.NotAfter.Sub(cert.NotBefore), wantTTL+time.Minute)
+				require.Equal(t, []string{"example.com", "dog.example.com"}, cert.DNSNames)
 
 				// Check against SPIFFE SPEC
 				// References are to https://github.com/spiffe/spiffe/blob/main/standards/X509-SVID.md
@@ -433,6 +440,10 @@ func TestIssueWorkloadIdentity(t *testing.T) {
 						Hint:                     "Wow - what a lovely hint, dog!",
 						WorkloadIdentity:         full.GetMetadata().GetName(),
 						WorkloadIdentityRevision: full.GetMetadata().GetRevision(),
+						DNSSANs: []string{
+							"example.com",
+							"dog.example.com",
+						},
 					},
 					cmpopts.IgnoreFields(
 						events.SPIFFESVIDIssued{},
