4ec2e05f13d4409375e73b3f4b97cdaa9d1cd0dc
Zac Bergquist <zac.bergquist@goteleport.com>
2025-01-02 15:52:08 -0700

    Reduce flakiness in TestInitDatabaseService
    This test was setting the auth's storage config to use a different
    temp dir than the backend's storage dir, which could result in the
    non-empty directory that fails during cleanup.
    
    As of #48247 tests don't need to set this at all since the default
    behavior now does the right thing.

--- lib/service/service_test.go
+++ lib/service/service_test.go
@@ -1801,12 +1801,10 @@ func TestInitDatabaseService(t *testing.T) {
 			cfg.DebugService = servicecfg.DebugConfig{
 				Enabled: false,
 			}
-			cfg.Auth.StorageConfig.Params["path"] = t.TempDir()
 			cfg.Hostname = "default.example.com"
 			cfg.Auth.Enabled = true
 			cfg.SetAuthServerAddress(utils.NetAddr{AddrNetwork: "tcp", Addr: "127.0.0.1:0"})
 			cfg.Auth.ListenAddr = utils.NetAddr{AddrNetwork: "tcp", Addr: "127.0.0.1:0"}
-			cfg.Auth.StorageConfig.Params["path"] = t.TempDir()
 			cfg.Auth.SessionRecordingConfig.SetMode(types.RecordOff)
 			cfg.Proxy.Enabled = true
 			cfg.Proxy.DisableWebInterface = true
