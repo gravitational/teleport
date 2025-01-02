4af7dd44972065d433566b6d8810a1d1ff538efd
Michael Myers <michael.myers@goteleport.com>
2025-01-02 16:58:05 -0600

    Move DeviceTrust EmptyList to OSS
    This takes the empty list components from e and moves them to OSS. The
    main difference is the different CTAs and buttons now displayed in oss v
    e.
    
    The empty list itself is unchanged, only moved

--- web/packages/teleport/src/DeviceTrust/DeviceList/index.ts
+++ web/packages/teleport/src/DeviceTrust/DeviceList/index.ts
@@ -1,6 +1,6 @@
 /**
  * Teleport
- * Copyright (C) 2023  Gravitational, Inc.
+ * Copyright (C) 2024 Gravitational, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -16,18 +16,4 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { Box } from 'design';
-
-import { FeatureBox } from 'teleport/components/Layout';
-
-import { EmptyList } from './EmptyList';
-
-export function DeviceTrustLocked() {
-  return (
-    <FeatureBox>
-      <Box>
-        <EmptyList />
-      </Box>
-    </FeatureBox>
-  );
-}
+export { DeviceList } from './DeviceList';
