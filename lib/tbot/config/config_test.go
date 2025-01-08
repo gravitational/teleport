8d90596457d407955ef67b2240bd2e2f7a0fee15
Noah Stride <noah.stride@goteleport.com>
2025-01-08 11:58:49 +0000

    Add new config struct for `workload-identity-api` service

--- lib/tbot/config/config_test.go
+++ lib/tbot/config/config_test.go
@@ -272,6 +272,12 @@ func TestBotConfig_YAML(t *testing.T) {
 							Name: "my-workload-identity",
 						},
 					},
+					&WorkloadIdentityAPIService{
+						Listen: "tcp://127.0.0.1:123",
+						WorkloadIdentity: WorkloadIdentitySelector{
+							Name: "my-workload-identity",
+						},
+					},
 				},
 			},
 		},
