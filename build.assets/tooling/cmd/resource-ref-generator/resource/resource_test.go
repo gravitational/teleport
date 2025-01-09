0efefd315cf0b851788dd304acce36e8ab2bb213
Paul Gottschling <paul.gottschling@goteleport.com>
2025-01-09 10:22:42 -0500

    Incorporate strideynet feedback
    - Update copyright year.
    - Wrap errors properly in `fmt.Errorf`.

--- build.assets/tooling/cmd/resource-ref-generator/resource/resource_test.go
+++ build.assets/tooling/cmd/resource-ref-generator/resource/resource_test.go
@@ -1,5 +1,5 @@
 // Teleport
-// Copyright (C) 2023  Gravitational, Inc.
+// Copyright (C) 2025  Gravitational, Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
