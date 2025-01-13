2166de0f1927e5b85c24c30d4cfc71d4f739a3d8
Tim Ross <tim.ross@goteleport.com>
2025-01-13 12:49:37 -0500

    Allow enabling SSH listener for tunnel agents

--- lib/service/service.go
+++ lib/service/service.go
@@ -2975,13 +2975,16 @@ func (process *TeleportProcess) initSSH() error {
 			logger.WarnContext(process.ExitContext(), warn)
 		}
 
+		explicitListen := os.Getenv("TELEPORT_UNSTABLE_EXPLICIT_SSH_LISTEN") == "yes"
+		useLocalListener := explicitListen || !conn.UseTunnel()
+
 		// Provide helpful log message if listen_addr or public_addr are not being
 		// used (tunnel is used to connect to cluster).
 		//
 		// If a tunnel is not being used, set the default here (could not be done in
 		// file configuration because at that time it's not known if server is
 		// joining cluster directly or through a tunnel).
-		if conn.UseTunnel() {
+		if !useLocalListener {
 			if !cfg.SSH.Addr.IsEmpty() {
 				logger.InfoContext(process.ExitContext(), "Connected to cluster over tunnel connection, ignoring listen_addr setting.")
 			}
@@ -2989,7 +2992,7 @@ func (process *TeleportProcess) initSSH() error {
 				logger.InfoContext(process.ExitContext(), "Connected to cluster over tunnel connection, ignoring public_addr setting.")
 			}
 		}
-		if !conn.UseTunnel() && cfg.SSH.Addr.IsEmpty() {
+		if useLocalListener && cfg.SSH.Addr.IsEmpty() {
 			cfg.SSH.Addr = *defaults.SSHServerListenAddr()
 		}
 
@@ -3107,7 +3110,7 @@ func (process *TeleportProcess) initSSH() error {
 		}
 
 		var agentPool *reversetunnel.AgentPool
-		if !conn.UseTunnel() {
+		if useLocalListener {
 			listener, err := process.importOrCreateListener(ListenerNodeSSH, cfg.SSH.Addr.Addr)
 			if err != nil {
 				return trace.Wrap(err)
@@ -3149,11 +3152,15 @@ func (process *TeleportProcess) initSSH() error {
 			}
 
 			go s.Serve(listener)
-		} else {
-			// Start the SSH server. This kicks off updating labels and starting the
-			// heartbeat.
-			if err := s.Start(); err != nil {
-				return trace.Wrap(err)
+		}
+
+		if conn.UseTunnel() {
+			if !useLocalListener {
+				// Start the SSH server. This kicks off updating labels and starting the
+				// heartbeat.
+				if err := s.Start(); err != nil {
+					return trace.Wrap(err)
+				}
 			}
 
 			var serverHandler reversetunnel.ServerHandler = s
