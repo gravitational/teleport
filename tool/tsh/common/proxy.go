77b7489d998f537f05f34232b895f32f9a86685a
Jinwook Jeong <jinwookjeongg@gmail.com>
2025-01-13 22:06:15 +0900

    Add `--listen` and `--insecure-listen-anywhere` flags to `tsh proxy db`

--- tool/tsh/common/proxy.go
+++ tool/tsh/common/proxy.go
@@ -175,7 +175,17 @@ func onProxyCommandDB(cf *CLIConf) error {
 
 	addr := "localhost:0"
 	randomPort := true
-	if cf.LocalProxyPort != "" {
+	if cf.LocalProxyAddr != "" {
+		netAddr, err := utils.ParseAddr(cf.LocalProxyAddr)
+		if err != nil {
+			return trace.Wrap(err)
+		}
+		if !cf.InsecureListenAnywhere && !netAddr.IsLoopback() {
+			return trace.BadParameter("only loopback addresses are allowed for listener without the insecure switch, got %q", cf.LocalProxyAddr)
+		}
+		addr = cf.LocalProxyAddr
+		randomPort = strings.HasSuffix(addr, ":0")
+	} else if cf.LocalProxyPort != "" {
 		randomPort = false
 		addr = fmt.Sprintf("127.0.0.1:%s", cf.LocalProxyPort)
 	}
