e4689ab463bbd6fe018c5d77690cfced915101a3
Alan Parra <alan.parra@goteleport.com>
2025-01-16 18:24:28 -0300

    chore: Bump Go to 1.23.5

--- build.assets/versions.mk
+++ build.assets/versions.mk
@@ -3,7 +3,7 @@
 # Keep versions in sync with devbox.json, when applicable.
 
 # Sync with devbox.json.
-GOLANG_VERSION ?= go1.23.4
+GOLANG_VERSION ?= go1.23.5
 GOLANGCI_LINT_VERSION ?= v1.63.4
 
 NODE_VERSION ?= 20.18.0
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/gravitational/teleport
 
-go 1.23.4
+go 1.23.5
 
 require (
 	cloud.google.com/go/cloudsqlconn v1.12.1
