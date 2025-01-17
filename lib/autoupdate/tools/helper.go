b01e6d007dd63b362dc21057fc9fc84fe698b3b9
Vadym Popov <vadym.popov@goteleport.com>
2025-01-17 08:08:51 -0800

    [v17] Fix auto-update re-exec arguments modified by aliases (#51154)
    * Fix auto-update re-exec arguments modified by aliases
    
    * Make arguments to be required to set
    
    * Restore progress bar show before request
    
    * Improve integration tests to execute with `tsh` and `tctl`
    
    Added a full-cycle integration test to verify client tools
    auto-updates within a test cluster by modifying AutoUpdateConfig
    and AutoUpdateVersion resources. The test executes the login
    command using alias configurations to ensure no recursive
    re-execution occurs.
    
    The updater binary used in integration tests has been replaced
    with the `Run` logic of tctl and tsh.
    
    * Set generated test password by env variable instead of constant value
    
    * Restore priority of env check over remote check
    
    In case of double re-execution case we should stop second one to prevent loop re-execution
    Drop localDir set during compilation

--- lib/autoupdate/tools/helper.go
+++ lib/autoupdate/tools/helper.go
@@ -26,7 +26,16 @@ import (
 
 	"github.com/gravitational/trace"
 
+	"github.com/gravitational/teleport"
 	stacksignal "github.com/gravitational/teleport/lib/utils/signal"
+)
+
+// Variables might to be overridden during compilation time for integration tests.
+var (
+	// version is the current version of the Teleport.
+	version = teleport.Version
+	// baseURL is CDN URL for downloading official Teleport packages.
+	baseURL = defaultBaseURL
 )
 
 // CheckAndUpdateLocal verifies if the TELEPORT_TOOLS_VERSION environment variable
@@ -36,13 +45,14 @@ import (
 // with the updated version.
 // If $TELEPORT_HOME/bin contains downloaded client tools, it always re-executes
 // using the version from the home directory.
-func CheckAndUpdateLocal(ctx context.Context, currentVersion string) error {
+func CheckAndUpdateLocal(ctx context.Context, reExecArgs []string) error {
 	toolsDir, err := Dir()
 	if err != nil {
 		slog.WarnContext(ctx, "Client tools update is disabled", "error", err)
 		return nil
 	}
-	updater := NewUpdater(toolsDir, currentVersion)
+
+	updater := NewUpdater(toolsDir, version, WithBaseURL(baseURL))
 	// At process startup, check if a version has already been downloaded to
 	// $TELEPORT_HOME/bin or if the user has set the TELEPORT_TOOLS_VERSION
 	// environment variable. If so, re-exec that version of client tools.
@@ -51,7 +61,7 @@ func CheckAndUpdateLocal(ctx context.Context, currentVersion string) error {
 		return trace.Wrap(err)
 	}
 	if reExec {
-		return trace.Wrap(updateAndReExec(ctx, updater, toolsVersion))
+		return trace.Wrap(updateAndReExec(ctx, updater, toolsVersion, reExecArgs))
 	}
 
 	return nil
@@ -64,13 +74,13 @@ func CheckAndUpdateLocal(ctx context.Context, currentVersion string) error {
 // with the updated version.
 // If $TELEPORT_HOME/bin contains downloaded client tools, it always re-executes
 // using the version from the home directory.
-func CheckAndUpdateRemote(ctx context.Context, currentVersion string, proxy string, insecure bool) error {
+func CheckAndUpdateRemote(ctx context.Context, proxy string, insecure bool, reExecArgs []string) error {
 	toolsDir, err := Dir()
 	if err != nil {
 		slog.WarnContext(ctx, "Client tools update is disabled", "error", err)
 		return nil
 	}
-	updater := NewUpdater(toolsDir, currentVersion)
+	updater := NewUpdater(toolsDir, version, WithBaseURL(baseURL))
 	// The user has typed a command like `tsh ssh ...` without being logged in,
 	// if the running binary needs to be updated, update and re-exec.
 	//
@@ -81,13 +91,13 @@ func CheckAndUpdateRemote(ctx context.Context, currentVersion string, proxy stri
 		return trace.Wrap(err)
 	}
 	if reExec {
-		return trace.Wrap(updateAndReExec(ctx, updater, toolsVersion))
+		return trace.Wrap(updateAndReExec(ctx, updater, toolsVersion, reExecArgs))
 	}
 
 	return nil
 }
 
-func updateAndReExec(ctx context.Context, updater *Updater, toolsVersion string) error {
+func updateAndReExec(ctx context.Context, updater *Updater, toolsVersion string, args []string) error {
 	ctxUpdate, cancel := stacksignal.GetSignalHandler().NotifyContext(ctx)
 	defer cancel()
 	// Download the version of client tools required by the cluster. This
@@ -99,7 +109,7 @@ func updateAndReExec(ctx context.Context, updater *Updater, toolsVersion string)
 	}
 
 	// Re-execute client tools with the correct version of client tools.
-	code, err := updater.Exec()
+	code, err := updater.Exec(args)
 	if err != nil && !errors.Is(err, os.ErrNotExist) {
 		slog.DebugContext(ctx, "Failed to re-exec client tool", "error", err)
 		os.Exit(code)
