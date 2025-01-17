906c4e40f2e6426693b33bc5a9a7ebb85e7f1cd9
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2025-01-17 16:45:15 +0100

    Use auth.InitTestAuthCache in tests (#5870)
    Source-commit: gravitational/teleport.e@15340b715be1b087972ac00ad652215b6fc61910
    Source-pull-requests: gravitational/teleport.e#5870

--- e/lib/aws/identitycenter/test/fixture.go
+++ e/lib/aws/identitycenter/test/fixture.go
@@ -11,7 +11,6 @@ import (
 	"github.com/jonboulle/clockwork"
 	"github.com/stretchr/testify/require"
 
-	"github.com/gravitational/teleport"
 	apidefaults "github.com/gravitational/teleport/api/defaults"
 	headerv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/header/v1"
 	identitycenterv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/identitycenter/v1"
@@ -28,12 +27,10 @@ import (
 	"github.com/gravitational/teleport/integrations/access/common"
 	"github.com/gravitational/teleport/integrations/lib/testing/integration"
 	"github.com/gravitational/teleport/lib/auth"
-	"github.com/gravitational/teleport/lib/auth/accesspoint"
 	"github.com/gravitational/teleport/lib/auth/authclient"
 	authority "github.com/gravitational/teleport/lib/auth/testauthority"
 	"github.com/gravitational/teleport/lib/backend"
 	"github.com/gravitational/teleport/lib/backend/memory"
-	"github.com/gravitational/teleport/lib/cache"
 	"github.com/gravitational/teleport/lib/services"
 	"github.com/gravitational/teleport/lib/services/local"
 	"github.com/gravitational/teleport/lib/utils/pagination"
@@ -56,69 +53,13 @@ type CacheArgs struct {
 	Started bool
 }
 
-func initCache(srv *auth.Server, args CacheArgs) error {
-	// TODO: See if we can trim the requirements for the cache down a bit.
-
-	svces := srv.Services
-	var err error
-	srv.Cache, err = accesspoint.NewCache(accesspoint.Config{
-		Context:      srv.CloseContext(),
-		Setup:        cache.ForAuth,
-		CacheName:    []string{teleport.ComponentAuth},
-		EventsSystem: true,
-		Unstarted:    !args.Started,
-
-		Access:                  svces.Access,
-		AccessLists:             svces.AccessLists,
-		AccessMonitoringRules:   svces.AccessMonitoringRules,
-		AppSession:              svces.Identity,
-		Apps:                    svces.Apps,
-		ClusterConfig:           svces.ClusterConfiguration,
-		AutoUpdateService:       svces.AutoUpdateService,
-		CrownJewels:             svces.CrownJewels,
-		DatabaseObjects:         svces.DatabaseObjects,
-		DatabaseServices:        svces.DatabaseServices,
-		Databases:               svces.Databases,
-		DiscoveryConfigs:        svces.DiscoveryConfigs,
-		DynamicAccess:           svces.DynamicAccessExt,
-		Events:                  svces.Events,
-		IdentityCenter:          svces.IdentityCenter,
-		Integrations:            svces.Integrations,
-		KubeWaitingContainers:   svces.KubeWaitingContainer,
-		Kubernetes:              svces.Kubernetes,
-		Notifications:           svces.Notifications,
-		Okta:                    svces.Okta,
-		Presence:                svces.PresenceInternal,
-		Provisioner:             svces.Provisioner,
-		ProvisioningStates:      svces.ProvisioningStates,
-		Restrictions:            svces.Restrictions,
-		SAMLIdPServiceProviders: svces.SAMLIdPServiceProviders,
-		SAMLIdPSession:          svces.Identity,
-		SecReports:              svces.SecReports,
-		SnowflakeSession:        svces.Identity,
-		SPIFFEFederations:       svces.SPIFFEFederations,
-		StaticHostUsers:         svces.StaticHostUser,
-		Trust:                   svces.TrustInternal,
-		UserGroups:              svces.UserGroups,
-		UserTasks:               svces.UserTasks,
-		UserLoginStates:         svces.UserLoginStates,
-		Users:                   svces.Identity,
-		WebSession:              svces.Identity.WebSessions(),
-		WebToken:                svces.WebTokens(),
-		WindowsDesktops:         svces.WindowsDesktops,
-		DynamicWindowsDesktops:  svces.DynamicWindowsDesktops,
-		PluginStaticCredentials: svces.PluginStaticCredentials,
-		GitServers:              svces.GitServers,
-		WorkloadIdentity:        svces.WorkloadIdentities,
-	})
-	if err != nil {
-		return trace.Wrap(err)
-	}
-	return nil
-}
-
 func WithCache(args CacheArgs) func(*auth.Server) error {
-	return func(srv *auth.Server) error { return initCache(srv, args) }
+	return func(srv *auth.Server) error {
+		return auth.InitTestAuthCache(auth.TestAuthCacheParams{
+			AuthServer: srv,
+			Unstarted:  !args.Started,
+		})
+	}
 }
 
 func NewFixture(t *testing.T, opts ...auth.ServerOption) *Fixture {
