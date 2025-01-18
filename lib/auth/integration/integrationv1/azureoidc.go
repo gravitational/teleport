e2f6e249b63e97ada0a9531d74bd169dd7cd3355
Matt Brock <mvbrock@gmail.com>
2025-01-18 12:02:04 -0600

    Invoking token generation and returning the response

---
+++ lib/auth/integration/integrationv1/azureoidc.go
@@ -0,0 +1,22 @@
+package integrationv1
+
+import (
+	"context"
+	integrationpb "github.com/gravitational/teleport/api/gen/proto/go/teleport/integration/v1"
+	"github.com/gravitational/teleport/lib/integrations/azureoidc"
+	"github.com/gravitational/trace"
+)
+
+func (s *Service) GenerateAzureOIDCToken(ctx context.Context, req *integrationpb.GenerateAzureOIDCTokenRequest) (*integrationpb.GenerateAzureOIDCTokenResponse, error) {
+	_, err := s.cache.GetIntegration(ctx, req.GetIntegration())
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	token, err := azureoidc.GenerateEntraOIDCToken(ctx, s.cache, s.keyStoreManager, s.clock)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	return &integrationpb.GenerateAzureOIDCTokenResponse{
+		Token: token,
+	}, nil
+}
