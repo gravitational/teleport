71ce3224dd308bcf4e08f18c627538a0b3fd7190
Matt Brock <mvbrock@gmail.com>
2025-01-18 21:55:39 -0600

    Fetching the Azure OIDC token during fetcher creation and establishing a credential assertion approach

--- lib/auth/integration/integrationv1/azureoidc.go
+++ lib/auth/integration/integrationv1/azureoidc.go
@@ -3,8 +3,18 @@ package integrationv1
 import (
 	"context"
 	integrationpb "github.com/gravitational/teleport/api/gen/proto/go/teleport/integration/v1"
+	"github.com/gravitational/teleport/lib/integrations/azureoidc"
+	"github.com/gravitational/trace"
 )
 
 func (s *Service) GenerateAzureOIDCToken(ctx context.Context, req *integrationpb.GenerateAzureOIDCTokenRequest) (*integrationpb.GenerateAzureOIDCTokenResponse, error) {
-	return nil, nil
+	_, err := s.cache.GetIntegration(ctx, req.Integration)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	token, err := azureoidc.GenerateEntraOIDCToken(ctx, s.cache, s.keyStoreManager, s.clock)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	return &integrationpb.GenerateAzureOIDCTokenResponse{Token: token}, nil
 }
