20cded469636454bd457d13c493521a325422c10
Evan Freed <2314084+evanfreed@users.noreply.github.com>
2025-01-22 12:47:29 -0600

    Update lib/service/service.go
    Co-authored-by: rosstimothy <39066650+rosstimothy@users.noreply.github.com>

--- lib/service/service.go
+++ lib/service/service.go
@@ -1351,8 +1351,7 @@ func NewTeleport(cfg *servicecfg.Config) (*TeleportProcess, error) {
 	}
 
 	if address := os.Getenv("TELEPORT_PYROSCOPE_SERVER_ADDRESS"); address != "" {
-		process.initPyroscope()
-		serviceStarted = true
+		process.initPyroscope(address)
 	}
 
 	if cfg.DebugService.Enabled {
