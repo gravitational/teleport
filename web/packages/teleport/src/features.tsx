c2ab0f25aa37405b3ee9ede1654f75150a96fd6c
Michael Myers <michael.myers@goteleport.com>
2025-01-21 18:05:53 -0600

    Add optional component props to Roles component
    This PR enables us to add an optional RoleDiff component to the props
    for our Roles feature so that we can create an "e wrapper" around the
    Roles feature for enterprise specifically. Then we can pass the Role
    Diff visualizer into the E route.

--- web/packages/teleport/src/features.tsx
+++ web/packages/teleport/src/features.tsx
@@ -320,6 +320,11 @@ export class FeatureRoles implements TeleportFeature {
   };
 
   showInDashboard = true;
+  // getRoute allows child class extending this
+  // parent class to refer to this parent's route.
+  getRoute() {
+    return this.route;
+  }
 }
 
 export class FeatureAuthConnectors implements TeleportFeature {
