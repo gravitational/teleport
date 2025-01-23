9a965d005e31d5ccd05279a9f06c800da416161f
Vadym Popov <vadym.popov@goteleport.com>
2025-01-23 11:02:40 -0800

    Reorganize tctl commands to not require an auth client by default (#48894) (#50968)
    * Reorganize tctl commands to have commands not required auth client
    
    * Replace auth client with lazy loading approach
    
    * Fix linter warning
    
    * Replace camel case in import alias
    Replace logrus to use slog
    
    * Rename close function
    
    * Refactor plugin commands to use interface of auth client and plugin client
    Code review changes
    
    * Refactor workload identity commands
    
    * Add access to global config for the commands
    
    * Add test checking all tctl commands match process
    
    * Fix golangci-lint warnings

--- integration/tctl_terraform_env_test.go
+++ integration/tctl_terraform_env_test.go
@@ -105,7 +105,7 @@ func TestTCTLTerraformCommand_ProxyJoin(t *testing.T) {
 	tctlCommand := common.TerraformCommand{}
 
 	app := kingpin.New("test", "test")
-	tctlCommand.Initialize(app, tctlCfg)
+	tctlCommand.Initialize(app, nil, tctlCfg)
 	_, err = app.Parse([]string{"terraform", "env"})
 	require.NoError(t, err)
 	// Create io buffer writer
@@ -179,7 +179,7 @@ func TestTCTLTerraformCommand_AuthJoin(t *testing.T) {
 	tctlCommand := common.TerraformCommand{}
 
 	app := kingpin.New("test", "test")
-	tctlCommand.Initialize(app, tctlCfg)
+	tctlCommand.Initialize(app, nil, tctlCfg)
 	_, err = app.Parse([]string{"terraform", "env"})
 	require.NoError(t, err)
 	// Create io buffer writer
