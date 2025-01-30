729bc133705517a8d9999744ba264ba93b68cbcb
Grzegorz Zdunek <gzdunek@users.noreply.github.com>
2025-01-30 16:48:05 +0100

    Redesign profile switcher  (#51419)
    * Add mapping of profile color to theme color
    
    * Prepare UserIcon to display colors, make it slightly larger
    
    * Rewrite single identity item to show icon and user/cluster in separate lines
    
    * Render single cluster items in a list
    
    * Add color picker
    
    * Render active cluster separately, above inactive clusters
    
    * Put the new identity components together
    
    * Refactor `Identity` story, render the container component instead of "presentational" one
    
    * Refresh startup page
    
    * Minor style fixes
    
    * Add comment about `dataVisualisationColors`
    
    * Bring back the original styling of `StaticListItem`
    
    * Make the pencil icon hoverable and clickable
    
    * Increase min-width from 200 px to 300 px, simplify active cluster layout
    
    * Show logout button when any part of `ListItem` is hovered
    
    * Remove text from logout button, improve titles
    
    * Pass correct index number to `AddClusterItem`
    
    * Update comment for `focusGrabber`
    
    * Use `useStoreSelector` correctly
    
    * Improve accessibility of color picker
    
    * Replace Pam icon with plus symbol
    
    * Remove unnecessary optional chaining
    
    * Align profile status error with user icon
    
    * Add story for cluster connect panel
    
    * Make user icon letter follow active theme
    
    This was suggested by Kenny, and I think it looks good.
    
    * Change gap from 11 px to 10 px
    
    * Rename profile color -> workspace color
    
    (cherry picked from commit 937e4a9ac9f85b2c3501168fd509693fea2e218e)

--- web/packages/teleterm/src/ui/services/workspacesService/color.ts
+++ web/packages/teleterm/src/ui/services/workspacesService/color.ts
@@ -18,6 +18,8 @@
 
 import { z } from 'zod';
 
+import { dataVisualisationColors } from 'design/theme/themes/darkTheme';
+
 import Logger from 'teleterm/logger';
 
 export type WorkspaceColor = z.infer<typeof workspaceColors>;
@@ -57,7 +59,7 @@ export function parseWorkspaceColor(
 
 /**
  * Determines the next available unused color across all workspaces.
- * If all colors are already in use, it defaults to returning purple.
+ * If all colors are already in use, it returns purple.
  */
 function getNextWorkspaceColor(
   workspaces: Record<string, { color: WorkspaceColor }>
@@ -67,3 +69,19 @@ function getNextWorkspaceColor(
   const unusedColors = allColors.difference(takenColors);
   return unusedColors.size > 0 ? [...unusedColors][0] : 'purple';
 }
+
+/**
+ * Maps workspace colors to the theme colors.
+ * We always use dark theme colors.
+ * They look good in both light and dark modes,
+ * and we avoid confusing users with different shades of the same color.
+ */
+export const workspaceColorMapping: Record<WorkspaceColor, string> = {
+  purple: dataVisualisationColors.primary.purple,
+  red: dataVisualisationColors.primary.abbey,
+  green: dataVisualisationColors.primary.caribbean,
+  yellow: dataVisualisationColors.primary.sunflower,
+  blue: dataVisualisationColors.primary.picton,
+  cyan: dataVisualisationColors.primary.cyan,
+  pink: dataVisualisationColors.primary.wednesdays,
+};
