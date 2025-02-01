2004893be0a8314a4a2b9307a4490dfd7baa48ba
Lisa Kim <lisa@goteleport.com>
2025-01-31 16:18:19 -0800

    Web: make SelectResource.tsx component leaner (#51698)
    * Web: Move resource specs into resource directory
    
    * Web: move code out into separate files

--- web/packages/teleport/src/Discover/SelectResource/resources/index.ts
+++ web/packages/teleport/src/Discover/SelectResource/resources/index.ts
@@ -1,6 +1,6 @@
 /**
  * Teleport
- * Copyright (C) 2023  Gravitational, Inc.
+ * Copyright (C) 2025  Gravitational, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -16,10 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-export { SelectResource } from './SelectResource';
-export {
-  getResourcePretitle,
-  getDatabaseProtocol,
-  getDefaultDatabasePort,
-} from './resources';
-export * from './types';
+export * from './databases';
+export * from './resources';
+export * from './resourcesE';
