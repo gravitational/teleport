8ca3858bfbb50fe54d3ee2f0a7c615b82e55268e
Rafał Cieślak <rafal.cieslak@goteleport.com>
2025-02-04 11:38:33 +0100

    Install corepack@0.31.0 instead of hardcoding integrity keys

--- build.assets/versions.mk
+++ build.assets/versions.mk
@@ -6,8 +6,8 @@
 GOLANG_VERSION ?= go1.23.5
 GOLANGCI_LINT_VERSION ?= v1.63.4
 
-# TODO(ravicious): When attempting to update Node.js version, see if corepack distributed with this
-# version is > 0.31.0. If so, remove definitions of COREPACK_INTEGRITY_KEYS from CI.
+# TODO(ravicious): When updating Node.js, see if corepack distributed with the new Node.JS version
+# is >= 0.31.0. If so, remove manual calls to install corepack@0.31.0 from CI scripts.
 NODE_VERSION ?= 20.18.0
 
 # Run lint-rust check locally before merging code after you bump this.
