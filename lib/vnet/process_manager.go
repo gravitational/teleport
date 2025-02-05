409956b8662ccc88361faa3566c2771a69221329
Rafał Cieślak <rafal.cieslak@goteleport.com>
2025-02-05 17:25:23 +0100

    Make NetworkStackInfo platform-independent

--- lib/vnet/process_manager.go
+++ lib/vnet/process_manager.go
@@ -48,7 +48,6 @@ type ProcessManager struct {
 	cancel    context.CancelFunc
 	closed    chan struct{}
 	closeOnce func()
-	networkStackInfo NetworkStackInfo
 }
 
 // AddCriticalBackgroundTask adds a function to the error group. [task] is expected to block until
