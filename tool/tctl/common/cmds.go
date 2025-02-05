387d247acb8c28e63de34281d3abb33b12a59eec
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2025-02-05 12:08:03 -0500

    Add tctl decision command (#51094) (#51875)
    Implements the new subcommand as described in RFD024e. The only
    deviation is in naming, i.e. `tctl decision` instead of the
    proposed `decision-service`. All commands are hidden now and
    subject to change as development of the PDP is underway.

--- tool/tctl/common/cmds.go
+++ tool/tctl/common/cmds.go
@@ -20,6 +20,7 @@ package common
 
 import (
 	"github.com/gravitational/teleport/tool/tctl/common/accessmonitoring"
+	"github.com/gravitational/teleport/tool/tctl/common/decision"
 	"github.com/gravitational/teleport/tool/tctl/common/loginrule"
 	"github.com/gravitational/teleport/tool/tctl/common/plugin"
 	"github.com/gravitational/teleport/tool/tctl/common/top"
@@ -68,5 +69,6 @@ func Commands() []CLICommand {
 		&touchIDCommand{},
 		&TerraformCommand{},
 		&AutoUpdateCommand{},
+		&decision.Command{},
 	}
 }
