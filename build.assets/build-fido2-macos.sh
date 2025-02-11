f2fc089feaf71d7da392afba1dff5aaf7e49eb39
Alan Parra <alan.parra@goteleport.com>
2025-02-11 15:31:35 -0300

    chore: Bump OpenSSL to 3.0.16 (#52035) (#52038)

--- build.assets/build-fido2-macos.sh
+++ build.assets/build-fido2-macos.sh
@@ -23,8 +23,8 @@ fi
 # Note: versions are the same as the corresponding git tags for each repo.
 readonly CBOR_VERSION=v0.10.2
 readonly CBOR_COMMIT=efa6c0886bae46bdaef9b679f61f4b9d8bc296ae
-readonly CRYPTO_VERSION=openssl-3.0.15
-readonly CRYPTO_COMMIT=c523121f902fde2929909dc7f76b13ceb4961efe
+readonly CRYPTO_VERSION=openssl-3.0.16
+readonly CRYPTO_COMMIT=fa1e5dfb142bb1c26c3c38a10aafa7a095df52e5
 readonly FIDO2_VERSION=1.14.0
 readonly FIDO2_COMMIT=1a9d335c8f0e821f9eff27482fdda96e59a4f577
 
