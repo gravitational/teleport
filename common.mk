8373c55b0c09817cb4e01f62353f3a24ebe949ae
Cam Hutchison <camh@goteleport.com>
2025-02-14 15:26:19 +1100

    [v15] build: Fix buildbox for fips builds of ironrdp
    Apply some changes that have been made to the later branches to v15 to
    try to get the FIPS release builds working. It seems to have broken with
    an ironrdp update recently.
    
    This rolls back clang in the buildbox from v14 to v12 as required by
    FIPS compliance, and also disables rdpclient for arm64 FIPS builds
    (although that is actually controlled by the e/Makefile, so this change
    here is just to keep things in sync).

--- common.mk
+++ common.mk
@@ -23,8 +23,8 @@ ifneq ("$(wildcard /usr/libbpf-${LIBBPF_VER}/include/bpf/bpf.h)","")
 with_bpf := yes
 BPF_TAG := bpf
 BPF_MESSAGE := with-BPF-support
-CLANG ?= $(shell which clang || which clang-14)
-LLVM_STRIP ?= $(shell which llvm-strip || which llvm-strip-14)
+CLANG ?= $(shell which clang || which clang-12)
+LLVM_STRIP ?= $(shell which llvm-strip || which llvm-strip-12)
 KERNEL_ARCH := $(shell uname -m | sed 's/x86_64/x86/g; s/aarch64/arm64/g')
 INCLUDES :=
 ER_BPF_BUILDDIR := lib/bpf/bytecode
