d875439a48ee9f1c43bce27499e33fe34f0dc43d
Forrest Marshall <forrest@goteleport.com>
2025-02-18 10:03:13 -0800

    Release v18.0.0-dev.forrest-pdp.2

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "18.0.0-dev.forrest-pdp.1"
+const Version = "18.0.0-dev.forrest-pdp.2"
 
 var SemVersion = semver.New(Version)
