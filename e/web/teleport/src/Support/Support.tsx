30787ed5402d20e56c90afb433c675c089f8269e
Michael <michaelzmyers@gmail.com>
2025-02-18 13:04:55 -0600

    Add Scheduled Upgrade window to Help & Support page (#6062)
    This is a partial revert of
    https://github.com/gravitational/teleport.e/pull/5567
    
    The UX team still desires the upgrade window form to live on the Manage
    Clusters page so this just adds to back to where it was without removing
    it from the manage clusters page.
    
    A future PR will update the RBAC resource that allows access in a more
    clear way to the manage clusters page.
    
    Source-commit: gravitational/teleport.e@85829f614fc789da87b453a093b244fa064f245c
    Source-pull-requests: gravitational/teleport.e#6062

--- e/web/teleport/src/Support/Support.tsx
+++ e/web/teleport/src/Support/Support.tsx
@@ -1,5 +1,25 @@
+import styled from 'styled-components';
+
+import { Flex, Text } from 'design';
+import { Clock, Edit } from 'design/Icon';
+import { H2 } from 'design/Text';
+
+import {
+  makeLabel,
+  ScheduleUpgrades,
+} from 'e-teleport/Clusters/ManageCluster/ScheduleUpgrades/ScheduleUpgrades';
+import { useUpgradeWindowStart } from 'e-teleport/Clusters/ManageCluster/useUpgradeWindowStart';
+import useTeleportE from 'e-teleport/useTeleportE';
 import { ExternalAuditStorageCta } from 'teleport/components/ExternalAuditStorageCta';
+import cfg from 'teleport/config';
 import { Support } from 'teleport/Support';
+import {
+  DataItem,
+  IconBox,
+  MobileSeparator,
+  StyledMultiRowBox,
+  StyledRow,
+} from 'teleport/Support/Support';
 
 export default function Container() {
   return (
@@ -10,5 +30,88 @@ export default function Container() {
 }
 
 export const SupportE = () => {
-  return <ExternalAuditStorageCta />;
+  const ctx = useTeleportE();
+  const {
+    scheduleUpgradesVisible,
+    onUpdate,
+    closeScheduleUpgrade,
+    selectedUpgradeWindowStart,
+    showScheduleUpgrade,
+    setSelectedUpgradeWindowStart,
+    updateWindowAttempt,
+  } = useUpgradeWindowStart(ctx, ctx.storeUser.getClusterId(), cfg.isCloud);
+  return (
+    <>
+      {cfg.isCloud && (
+        <>
+          <MobileSeparator />
+          <StyledMultiRowBox
+            mb={3}
+            css={`
+              @media screen and (max-width: ${props =>
+                  props.theme.breakpoints.mobile}px) {
+                margin-top: 0px;
+              }
+            `}
+          >
+            <StyledRow>
+              <Flex alignItems="center" justifyContent="start">
+                <IconBox>
+                  <Clock />
+                </IconBox>
+                <H2>Scheduled Upgrades</H2>
+              </Flex>
+            </StyledRow>
+            <StyledRow css="padding-left: 40px !important;">
+              <DataItem
+                title="Window Start Time"
+                data={
+                  <Flex alignItems="center">
+                    {makeLabel(selectedUpgradeWindowStart)}
+                    <EditLink
+                      onClick={showScheduleUpgrade}
+                      ml="2"
+                      size="medium"
+                    />
+                  </Flex>
+                }
+              />
+              <Text
+                typography="body2"
+                css={`
+                  @media screen and (max-width: ${props =>
+                      props.theme.breakpoints.mobile}px) {
+                    margin-left: ${props => props.theme.space[2]}px;
+                  }
+                `}
+              >
+                Window Start Time is the hour in which an upgrade may begin.
+                Changing this value changes it for everyone in your
+                organization.
+              </Text>
+            </StyledRow>
+          </StyledMultiRowBox>
+        </>
+      )}
+      {scheduleUpgradesVisible && (
+        <ScheduleUpgrades
+          onSave={onUpdate}
+          onCancel={closeScheduleUpgrade}
+          selectedWindow={selectedUpgradeWindowStart}
+          onSelectedWindowChange={setSelectedUpgradeWindowStart}
+          attempt={updateWindowAttempt}
+        />
+      )}
+      <ExternalAuditStorageCta />
+    </>
+  );
 };
+
+const EditLink = styled(Edit)`
+  color: ${props => props.theme.colors.text.slightlyMuted};
+  &:hover,
+  &:focus {
+    color: ${props => props.theme.colors.text.main};
+    cursor: pointer;
+  }
+`;
