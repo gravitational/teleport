4d7f13f157140ad4b2b84a163d61ab3a1208c327
Tim Ross <tim.ross@goteleport.com>
2025-02-19 17:19:12 -0500

    migrate cas to in-memory cache

--- lib/cache/store.go
+++ lib/cache/store.go
@@ -17,9 +17,13 @@
 package cache
 
 import (
+	"iter"
+	"reflect"
 	"sync/atomic"
 
 	"github.com/gravitational/trace"
+
+	"github.com/gravitational/teleport/lib/utils/sortcache"
 )
 
 type singletonStore[T any] struct {
@@ -48,8 +52,63 @@ func (s *singletonStore[T]) get() (T, error) {
 	item := s.t.Load()
 	if item == nil {
 		var t T
-		return t, trace.NotFound("no value for singleton of type %T", new(T))
+		return t, trace.NotFound("no value for singleton of type %v", reflect.TypeOf((*T)(nil)).Elem())
 	}
 
 	return *item, nil
+}
+
+type resourceStore[T any] struct {
+	filter  func(T) bool
+	cache   *sortcache.SortCache[T]
+	indexes map[string]func(T) string
+}
+
+func newResourceStore[T any](indexes map[string]func(T) string) *resourceStore[T] {
+	return newResourceStoreWithFilter(nil, indexes)
+}
+
+func newResourceStoreWithFilter[T any](filter func(T) bool, indexes map[string]func(T) string) *resourceStore[T] {
+	return &resourceStore[T]{
+		filter:  filter,
+		indexes: indexes,
+		cache: sortcache.New(sortcache.Config[T]{
+			Indexes: indexes,
+		}),
+	}
+}
+
+func (s *resourceStore[T]) clear() error {
+	s.cache.Clear()
+	return nil
+}
+
+func (s *resourceStore[T]) put(t T) error {
+	if s.filter != nil && !s.filter(t) {
+		return nil
+	}
+
+	s.cache.Put(t)
+	return nil
+}
+
+func (s *resourceStore[T]) delete(t T) error {
+	for idx, transform := range s.indexes {
+		s.cache.Delete(idx, transform(t))
+	}
+
+	return nil
+}
+
+func (s *resourceStore[T]) get(index, key string) (T, error) {
+	t, ok := s.cache.Get(index, key)
+	if !ok {
+		return t, trace.NotFound("no value for resource of type %v", reflect.TypeOf((*T)(nil)).Elem())
+	}
+
+	return t, nil
+}
+
+func (s *resourceStore[T]) iterate(index string, start string, stop string) iter.Seq[T] {
+	return s.cache.Ascend(index, start, stop)
 }
