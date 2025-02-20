dc3c324fc5f550488d19c7d354b94620c9aa09f3
Lisa Kim <lisa@goteleport.com>
2025-02-19 18:58:06 -0800

    Wrap Main.tsx with info guide context

---
+++ web/packages/teleport/src/Main/InfoGuideContext.tsx
@@ -0,0 +1,51 @@
+/**
+ * Teleport
+ * Copyright (C) 2025  Gravitational, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { createContext, useContext, useEffect, useState } from 'react';
+
+type InfoGuidePanelContextState = {
+  setInfoGuideElement: (element: JSX.Element | null) => void;
+  infoGuideElement: JSX.Element | null;
+};
+
+export const InfoGuidePanelContext =
+  createContext<InfoGuidePanelContextState>(null);
+
+/**
+ * hook that allows you to set the info guide element that
+ * will render in the InfoGuideSidePanel component.
+ *
+ * To close the InfoGuideSidePanel component, set infoGuideElement
+ * state back to null.
+ */
+export const useInfoGuide = () => {
+  const { infoGuideElement, setInfoGuideElement } = useContext(
+    InfoGuidePanelContext
+  );
+
+  useEffect(() => {
+    return () => {
+      setInfoGuideElement(null);
+    };
+  }, []);
+
+  return {
+    setInfoGuideElement,
+    infoGuideElement,
+  };
+};
