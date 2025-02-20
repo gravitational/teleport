6a4837ed30140c02cb813dd75adb8b64eb6e69b6
Nic Klaassen <nic@goteleport.com>
2025-02-19 16:32:10 -0800

    Release v18.0.0-dev.vnet-windows.4

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "18.0.0-dev.vnet-windows.3"
+const Version = "18.0.0-dev.vnet-windows.4"
 
 var SemVersion = semver.New(Version)
