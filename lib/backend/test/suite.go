7897555d0123496ca7cfc3f75f560a54cf4c3517
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2025-02-24 18:20:30 +0100

    [v16] Define `Backend.GetRange` to be inclusive (#52211)
    * Test for Backend.GetRange being inclusive
    
    * Get rid of the unused prefixItem
    
    * Fix memorybk GetRange not being inclusive
    
    * Fix etcdbk GetRange not being inclusive
    
    * Document the desired behavior of GetRange
    
    * Fix tests for the backport

--- lib/backend/test/suite.go
+++ lib/backend/test/suite.go
@@ -330,6 +330,11 @@ func testQueryRange(t *testing.T, newBackend Constructor) {
 	require.NoError(t, err)
 	RequireItems(t, []backend.Item{c1, c2}, result.Items)
 
+	// item at the end of the range
+	result, err = uut.GetRange(ctx, prefix("/prefix/c/c1"), prefix("/prefix/c/c2"), backend.NoLimit)
+	require.NoError(t, err)
+	RequireItems(t, []backend.Item{c1, c2}, result.Items)
+
 	// pagination
 	result, err = uut.GetRange(ctx, prefix("/prefix"), backend.RangeEnd(prefix("/prefix")), 2)
 	require.NoError(t, err)
