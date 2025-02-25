25f9c72eed7f29f49c38767570bd8957ca221dce
Lisa Kim <lisa@goteleport.com>
2025-02-25 10:30:07 -0800

    Create v2 web api endpoint for deleting integration with cleanup (#52286)
    * Create v2 endpoint that deletes integration with cleanup support
    
    * Use v2 endpoint with unsupported check
    
    * Option to pass around cleanup resource flag
    
    * Add proxy version custom error if exist
    
    * Address CRs

--- web/packages/teleport/src/services/version/unsupported.ts
+++ web/packages/teleport/src/services/version/unsupported.ts
@@ -32,6 +32,44 @@ import { JoinToken, JoinTokenRequest } from '../joinToken';
 
 export const ProxyRequiresUpgrade = 'Ensure all proxies are upgraded';
 
+/**
+ * Throws a custom error that are a result of `path not found` as a generic
+ * error message about this request not being supported and suggests
+ * to user to upgrade all proxies to the specified version.
+ *
+ * Else, rethrows the same error.
+ *
+ * @param supportedVersion the minimum version required for this
+ * request to succeed eg: v17.3.0
+ */
+export function withGenericUnsupportedError(
+  err: unknown,
+  supportedVersion: string
+) {
+  if (err instanceof ApiError && err.response.status === 404) {
+    if (err.proxyVersion) {
+      throw new Error(
+        'We could not complete your request. ' +
+          `Your proxy (${err.proxyVersion.string}) may be behind the ` +
+          `minimum required version (${supportedVersion}) to support ` +
+          `this request. ${ProxyRequiresUpgrade} and try again.`
+      );
+    }
+    // DELETE IN 19.0
+    // pre v17 this is the legacy error message crafted as a result
+    // of no path found.
+    if (err.message == `${err.response.status} - ${err.response.url}`) {
+      throw new Error(
+        'We could not complete your request. ' +
+          'Your proxy may be behind the minimum required version ' +
+          `(${supportedVersion}) to support this request. ` +
+          `${ProxyRequiresUpgrade} and try again.`
+      );
+    }
+  }
+  throw err;
+}
+
 export function withUnsupportedLabelFeatureErrorConversion(
   err: unknown
 ): never {
