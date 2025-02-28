b3bfeab89415fec2090d7c37f9bd8792764031f0
Gus Rivera <gus.rivera@goteleport.com>
2025-02-28 12:14:05 -0600

    Bump

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "18.0.0-dev.gus-mac-tooling.4"
+const Version = "18.0.0-dev.gus-mac-tooling.5"
 
 var SemVersion = semver.New(Version)
