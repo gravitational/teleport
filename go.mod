2e98c1e28472f5e5d1372cd823cd160b2d6e04d8
Dan Upton <daniel.upton@goteleport.com>
2025-03-03 14:01:12 +0000

    Add Systemd workload attestor

--- go.mod
+++ go.mod
@@ -250,6 +250,8 @@ require (
 
 require github.com/docker/docker v27.5.1+incompatible
 
+require github.com/coreos/go-systemd/v22 v22.5.0
+
 require (
 	cel.dev/expr v0.19.1 // indirect
 	cloud.google.com/go v0.118.2 // indirect
@@ -314,7 +316,6 @@ require (
 	github.com/containerd/log v0.1.0 // indirect
 	github.com/containerd/platforms v0.2.1 // indirect
 	github.com/containerd/stargz-snapshotter/estargz v0.16.3 // indirect
-	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
 	github.com/coreos/pkg v0.0.0-20220810130054-c7d1c02cb6cf // indirect
 	github.com/crewjam/httperr v0.2.0 // indirect
 	github.com/cyberphone/json-canonicalization v0.0.0-20231011164504-785e29786b46 // indirect
@@ -373,6 +374,7 @@ require (
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/godbus/dbus v0.0.0-20190726142602-4481cbc300e2 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
---
+++ lib/tbot/workloadidentity/workloadattest/systemd_linux.go
@@ -0,0 +1,81 @@
+//go:build linux
+
+/*
+ * Teleport
+ * Copyright (C) 2025  Gravitational, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package workloadattest
+
+import (
+	"context"
+	"log/slog"
+	"strings"
+	"time"
+
+	"github.com/coreos/go-systemd/v22/dbus"
+	"github.com/gravitational/trace"
+
+	workloadidentityv1pb "github.com/gravitational/teleport/api/gen/proto/go/teleport/workloadidentity/v1"
+)
+
+// SystemdAttestor attests the identity of a Systemd service.
+type SystemdAttestor struct {
+	cfg    SystemdAttestorConfig
+	log    *slog.Logger
+	dialer func(context.Context) (dbusConn, error)
+}
+
+func NewSystemdAttestor(cfg SystemdAttestorConfig, log *slog.Logger) *SystemdAttestor {
+	return &SystemdAttestor{
+		dialer: func(ctx context.Context) (dbusConn, error) {
+			return dbus.NewWithContext(ctx)
+		},
+	}
+}
+
+func (a *SystemdAttestor) Attest(ctx context.Context, pid int) (*workloadidentityv1pb.WorkloadAttrsSystemd, error) {
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+
+	conn, err := a.dialer(ctx)
+	if err != nil {
+		return nil, trace.Wrap(err, "creating dbus connection")
+	}
+	defer conn.Close()
+
+	unit, err := conn.GetUnitNameByPID(ctx, uint32(pid))
+	if err != nil {
+		return nil, trace.Wrap(err, "getting unit name")
+	}
+
+	service, isService := strings.CutSuffix(unit, ".service")
+	if !isService {
+		return nil, trace.Errorf("unit %q is not a service", unit)
+	}
+
+	return &workloadidentityv1pb.WorkloadAttrsSystemd{
+		Attested: true,
+		Service:  service,
+	}, nil
+}
+
+type dbusConn interface {
+	GetUnitNameByPID(context.Context, uint32) (string, error)
+	Close()
+}
+
+var _ dbusConn = (*dbus.Conn)(nil)
