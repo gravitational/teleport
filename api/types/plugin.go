41324dfde25b14e0270bdd2da4e30b239852192c
Trent Clarke <trent@goteleport.com>
2025-03-05 11:55:07 +1100

    Remove import status enum validation

--- api/types/plugin.go
+++ api/types/plugin.go
@@ -820,25 +820,6 @@ func (s AWSICResourceFilter) MarshalJSON() ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-// Check validates the [code] value, returning an error if the value is not a
-// defined member of the [AWSICGroupImportStatusCode] enum.
-func (code AWSICGroupImportStatusCode) Check() error {
-	if _, ok := AWSICGroupImportStatusCode_name[int32(code)]; ok {
-		return nil
-	}
-	return trace.BadParameter("invalid group import state %d", int(code))
-}
-
-// CheckAndSetDefaults validates the values in the plugin status struct.
-func (s *PluginAWSICStatusV1) CheckAndSetDefaults() error {
-	if gis := s.GroupImportStatus; gis != nil {
-		if err := gis.StatusCode.Check(); err != nil {
-			return trace.Wrap(err)
-		}
-	}
-	return nil
-}
-
 func (c *PluginEmailSettings) CheckAndSetDefaults() error {
 	if c.Sender == "" {
 		return trace.BadParameter("sender must be set")
