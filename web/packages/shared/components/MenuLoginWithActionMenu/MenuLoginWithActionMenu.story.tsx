4d40aaa50f8607acc587079e233c6976e1ffb0b0
flyinghermit <sshah@goteleport.com>
2025-03-06 22:48:51 -0500

    feat: MenuLoginWithActionMenu component

---
+++ web/packages/shared/components/MenuLoginWithActionMenu/MenuLoginWithActionMenu.story.tsx
@@ -0,0 +1,115 @@
+/**
+ * Teleport
+ * Copyright (C) 2025  Gravitational, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import styled from 'styled-components';
+
+import { Flex, H3, MenuItem } from 'design';
+
+import { MenuLoginWithActionMenu as MenuLoginWithActionMenuComponent } from './MenuLoginWithActionMenu';
+
+export default {
+  title: 'Shared/MenuLoginWithActionMenu',
+};
+
+export const MenuLoginWithActionMenu = () => {
+  return (
+    <Flex
+      inline
+      p={4}
+      gap="128px"
+      justifyContent="flex-start"
+      bg="levels.surface"
+    >
+      <Example>
+        <H3>No logins</H3>
+        <MenuLoginWithActionMenuComponent
+          getLoginItems={() => []}
+          width="100px"
+          onSelect={() => null}
+          buttonText="click me"
+        >
+          {MenuItems}
+        </MenuLoginWithActionMenuComponent>
+      </Example>
+      <Example>
+        <H3>Processing state</H3>
+        <MenuLoginWithActionMenuComponent
+          width="100px"
+          getLoginItems={() => new Promise(() => {})}
+          onSelect={() => null}
+          buttonText="click me"
+        >
+          {MenuItems}
+        </MenuLoginWithActionMenuComponent>
+      </Example>
+      <Example>
+        <H3>With logins</H3>
+        <MenuLoginWithActionMenuComponent
+          width="100px"
+          buttonText="click me"
+          getLoginItems={() => urlItems}
+          onSelect={() => null}
+          placeholder="Select item to log in..."
+        >
+          {MenuItems}
+        </MenuLoginWithActionMenuComponent>
+      </Example>
+      <Example>
+        <H3>With logins and search input</H3>
+        <MenuLoginWithActionMenuComponent
+          width="100px"
+          buttonText="click me"
+          getLoginItems={() => loginItems}
+          onSelect={() => null}
+          placeholder="search login"
+          disableSearchAndFilter={true}
+        >
+          {MenuItems}
+        </MenuLoginWithActionMenuComponent>
+      </Example>
+    </Flex>
+  );
+};
+
+const Example = styled(Flex).attrs({
+  gap: 2,
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+})``;
+
+const makeLoginItem = (login: string) => ({ url: '', login });
+
+const urlItems = [
+  'https://portal.azure.com/xxxxx2432-xxx-xx-xx-cxxxxx37ded',
+  'https://portal.azure.com/xxxxx2432-xxx-xx-xx-cxxxxx37ded&login_hint=user@example.com',
+  'https://myapplications.microsoft.com/?tenantid=xxxxx2432-xxx-xx-xx-cxxxxx37ded',
+].map(makeLoginItem);
+
+const loginItems = ['alice', 'bob', 'hector'].map(makeLoginItem);
+
+const MenuItems = (
+  <>
+    <MenuItem onClick={() => alert('Foo')}>Foo</MenuItem>
+    <MenuItem as="a" href="https://example.com" target="_blank">
+      Link to example.com
+    </MenuItem>
+    <MenuItem onClick={() => alert('Lorem ipsum dolor sit amet')}>
+      Lorem ipsum dolor sit amet
+    </MenuItem>
+  </>
+);
---
+++ web/packages/shared/components/MenuLoginWithActionMenu/MenuLoginWithActionMenu.test.tsx
@@ -0,0 +1,92 @@
+/**
+ * Teleport
+ * Copyright (C) 2023  Gravitational, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { MenuItem } from 'design';
+import { fireEvent, render, screen, waitFor } from 'design/utils/testing';
+
+import { MenuLoginWithActionMenu } from './MenuLoginWithActionMenu';
+
+test('clicking on primary menu and secondary menu opens respective menu items', async () => {
+  render(
+    <MenuLoginWithActionMenu
+      buttonText="Click Me"
+      getLoginItems={() => [{ url: '', login: 'alice' }]}
+      onSelect={() => null}
+    >
+      <MenuItem>Menu item</MenuItem>
+    </MenuLoginWithActionMenu>
+  );
+
+  fireEvent.click(screen.getByText('Click Me'));
+  await waitFor(() => {
+    expect(screen.getByText('alice')).toBeInTheDocument();
+  });
+
+  fireEvent.click(screen.getByTitle('Open menu'));
+  await waitFor(() => {
+    expect(screen.getByText('Menu item')).toBeInTheDocument();
+  });
+});
+
+test('search and filter input field should be visible by default', async () => {
+  render(
+    <MenuLoginWithActionMenu
+      buttonText="Click Me"
+      getLoginItems={() => [
+        { url: '', login: 'alice' },
+        { url: '', login: 'bob' },
+      ]}
+      onSelect={() => null}
+      placeholder="search me"
+    >
+      <MenuItem>Menu item</MenuItem>
+    </MenuLoginWithActionMenu>
+  );
+
+  fireEvent.click(screen.getByText('Click Me'));
+  await waitFor(() => {
+    expect(screen.getByText('alice')).toBeInTheDocument();
+  });
+
+  expect(screen.getByPlaceholderText('search me')).toBeInTheDocument();
+});
+
+test('disableSearchAndFilter should show static menu item laabel', async () => {
+  render(
+    <MenuLoginWithActionMenu
+      buttonText="Click Me"
+      getLoginItems={() => [
+        { url: '', login: 'alice' },
+        { url: '', login: 'bob' },
+      ]}
+      onSelect={() => null}
+      placeholder="search me"
+      disableSearchAndFilter={true}
+    >
+      <MenuItem>Menu item</MenuItem>
+    </MenuLoginWithActionMenu>
+  );
+
+  fireEvent.click(screen.getByText('Click Me'));
+  await waitFor(() => {
+    expect(screen.getByText('alice')).toBeInTheDocument();
+  });
+
+  expect(screen.getByText('search me')).toBeInTheDocument();
+  expect(screen.queryByPlaceholderText('search me')).not.toBeInTheDocument();
+});
