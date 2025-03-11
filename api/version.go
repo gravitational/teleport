fa1c433afc1c7afdd390dd9909440135551fdccb
Przemko Robakowski <przemko.robakowski@goteleport.com>
2025-03-11 19:22:03 +0100

    Update version to 18.0.0-dev.multiad.1

--- api/version.go
+++ api/version.go
@@ -3,6 +3,6 @@ package api
 
 import "github.com/coreos/go-semver/semver"
 
-const Version = "18.0.0-dev.braintrust.2"
+const Version = "18.0.0-dev.multiad.1"
 
 var SemVersion = semver.New(Version)
