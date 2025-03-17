0ea858602487e33b649e23ba3c5c572e6d956136
Bartosz Leper <bartosz.leper@goteleport.com>
2025-03-17 13:33:21 +0100

    Support wrapping in StepSlider (#52742)
    * Support wrapping in StepSlider
    
    This is required by the upcoming change to the Teleport Identity
    Security promo in the role editor.
    
    * Add third step to singleFlow
    
    Also, make Body2 and Body3 buttons consistent with Body1.
    
    ---------
    
    Co-authored-by: Rafał Cieślak <rafal.cieslak@goteleport.com>

--- web/packages/design/src/StepSlider/StepSlider.test.tsx
+++ web/packages/design/src/StepSlider/StepSlider.test.tsx
@@ -18,15 +18,21 @@
 
 import React from 'react';
 
-import { render, fireEvent, screen } from 'design/utils/testing';
+import {
+  fireEvent,
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from 'design/utils/testing';
 
 import {
-  SingleFlowInPlaceSlider,
   MultiFlowWheelSlider,
+  SingleFlowInPlaceSlider,
 } from './StepSlider.story';
 
-test('single flow', () => {
-  render(<SingleFlowInPlaceSlider />);
+test('single flow', async () => {
+  // Use custom animation duration to make tests faster.
+  render(<SingleFlowInPlaceSlider tDuration={0} />);
 
   // Test initial render.
   expect(screen.getByTestId('single-body1')).toBeVisible();
@@ -35,23 +41,55 @@ test('single flow', () => {
   // Should do nothing as expected.
   fireEvent.click(screen.getByText(/back1/i));
   expect(screen.getByTestId('single-body1')).toBeVisible();
+  // The above is not enough; make sure we didn't start transitioning.
+  expect(screen.queryByTestId('single-body2')).not.toBeInTheDocument();
 
   // Test next.
   fireEvent.click(screen.getByText(/next1/i));
   expect(screen.getByTestId('single-body2')).toBeVisible();
+  await waitForElementToBeRemoved(() => screen.queryByTestId('single-body1'));
 
   // Test going next when at the end of array.
   // Should do nothing.
   fireEvent.click(screen.getByText(/next2/i));
   expect(screen.getByTestId('single-body2')).toBeVisible();
+  // The above is not enough; make sure we didn't start transitioning.
+  expect(screen.queryByTestId('single-body1')).not.toBeInTheDocument();
 
   // Test going back.
   fireEvent.click(screen.getByText(/back2/i));
   expect(screen.getByTestId('single-body1')).toBeVisible();
 });
 
-test('switching between multi flow', () => {
-  render(<MultiFlowWheelSlider />);
+test('single flow with wrapping', async () => {
+  // Use custom animation duration to make tests faster.
+  render(<SingleFlowInPlaceSlider wrapping tDuration={0} />);
+  expect(screen.getByTestId('single-body1')).toBeVisible();
+
+  // Test going backwards on step 1
+  fireEvent.click(screen.getByText(/back1/i));
+  expect(screen.getByTestId('single-body3')).toBeVisible();
+  await waitForElementToBeRemoved(() => screen.queryByTestId('single-body1'));
+
+  // Test going forwards on step 3
+  fireEvent.click(screen.getByText(/next3/i));
+  expect(screen.getByTestId('single-body1')).toBeVisible();
+  await waitForElementToBeRemoved(() => screen.queryByTestId('single-body3'));
+
+  // Test the "normal" flow: forwards on step 1...
+  fireEvent.click(screen.getByText(/next1/i));
+  expect(screen.getByTestId('single-body2')).toBeVisible();
+  await waitForElementToBeRemoved(() => screen.queryByTestId('single-body1'));
+
+  // ...and backwards on step 2.
+  fireEvent.click(screen.getByText(/back2/i));
+  expect(screen.getByTestId('single-body1')).toBeVisible();
+  await waitForElementToBeRemoved(() => screen.queryByTestId('single-body2'));
+});
+
+test('switching between multi flow', async () => {
+  // Use custom animation duration to make tests faster.
+  render(<MultiFlowWheelSlider tDuration={0} />);
 
   // Test initial primary flow.
   expect(screen.getByTestId('multi-primary1')).toBeVisible();
@@ -59,6 +97,7 @@ test('switching between multi flow', () => {
   // Test switching to secondary flow.
   fireEvent.click(screen.getByText(/secondary flow/i));
   expect(screen.getByTestId('multi-secondary1')).toBeVisible();
+  await waitForElementToBeRemoved(() => screen.queryByTestId('multi-primary1'));
 
   // Test switching back to primary flow.
   fireEvent.click(screen.getByText(/primary flow/i));
