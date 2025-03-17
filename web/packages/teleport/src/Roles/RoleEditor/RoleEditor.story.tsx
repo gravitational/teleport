191f89f03fc7611a56a219b95cb2e6ce7fad3682
Bartosz Leper <bartosz.leper@goteleport.com>
2025-03-17 18:42:47 +0100

    [v17] Improve collapsible sections in the role editor (#53073)
    * Improve collapsible sections in the role editor
    
    - Changes the collapse icons from plus/minus symbols to chevrons.
    - Adds animations.
    - Cleans up the role editor story a bit.
    
    * Review. Fix the animation on Safari.

--- web/packages/teleport/src/Roles/RoleEditor/RoleEditor.story.tsx
+++ web/packages/teleport/src/Roles/RoleEditor/RoleEditor.story.tsx
@@ -23,20 +23,28 @@ import { useEffect, useState } from 'react';
 import { Info } from 'design/Alert';
 import { ButtonPrimary } from 'design/Button';
 import Flex from 'design/Flex';
+import { Indicator } from 'design/Indicator';
+import Text from 'design/Text';
+import { Attempt, useAsync } from 'shared/hooks/useAsync';
+import { wait } from 'shared/utils/wait';
 
 import useResources from 'teleport/components/useResources';
 import cfg from 'teleport/config';
 import { createTeleportContext } from 'teleport/mocks/contexts';
 import { RoleVersion } from 'teleport/services/resources';
+import { storageService } from 'teleport/services/storageService';
 import { Access } from 'teleport/services/user';
 import { YamlSupportedResourceKind } from 'teleport/services/yaml/types';
 import TeleportContextProvider from 'teleport/TeleportContextProvider';
 
 import { RoleEditor } from './RoleEditor';
 import { RoleEditorDialog } from './RoleEditorDialog';
+import { unableToUpdatePreviewMessage } from './Shared';
 import { withDefaults } from './StandardEditor/withDefaults';
 
 const defaultIsPolicyEnabled = cfg.isPolicyEnabled;
+const defaultGetAccessGraphRoleTesterEnabled =
+  storageService.getAccessGraphRoleTesterEnabled;
 
 export default {
   title: 'Teleport/Roles/Role Editor',
@@ -46,15 +54,21 @@ export default {
       if (parameters.acl) {
         ctx.storeUser.getRoleAccess = () => parameters.acl;
       }
+      if (parameters.roleTesterEnabled) {
+        cfg.isPolicyEnabled = true;
+        storageService.getAccessGraphRoleTesterEnabled = () => true;
+      }
       useEffect(() => {
         // Clean up
         return () => {
           cfg.isPolicyEnabled = defaultIsPolicyEnabled;
+          storageService.getAccessGraphRoleTesterEnabled =
+            defaultGetAccessGraphRoleTesterEnabled;
         };
       }, []);
       return (
         <TeleportContextProvider ctx={ctx}>
-          <Flex flexDirection="column" width="700px" height="800px">
+          <Flex flexDirection="column" width="550px" height="800px">
             <Story />
           </Flex>
         </TeleportContextProvider>
@@ -219,8 +233,14 @@ export const saving: StoryObj = {
   render() {
     return (
       <>
-        <Info>Save the role to see the saving state</Info>
-        <RoleEditor onSave={() => delay('infinite')} />
+        <Info>Edit and save the role to see the saving state</Info>
+        <RoleEditor
+          originalRole={{
+            object: withDefaults({ metadata: { name: 'dummy-role' } }),
+            yaml: dummyRoleYaml,
+          }}
+          onSave={() => delay('infinite')}
+        />
       </>
     );
   },
@@ -235,10 +255,16 @@ export const savingError: StoryObj = {
   render() {
     return (
       <>
-        <Info>Save the role to see the error state</Info>
+        <Info>Edit and save the role to see the error state</Info>
         <RoleEditor
+          originalRole={{
+            object: withDefaults({ metadata: { name: 'dummy-role' } }),
+            yaml: dummyRoleYaml,
+          }}
           onSave={async () => {
-            throw new Error('server error');
+            throw new Error('Server error', {
+              cause: new Error('Unexpected rack explosion'),
+            });
           }}
         />
       </>
@@ -301,14 +327,15 @@ export const Dialog: StoryObj = {
 
 export const DialogWithPolicyEnabled: StoryObj = {
   render() {
-    cfg.isPolicyEnabled = true;
     const [open, setOpen] = useState(false);
     const resources = useResources([], {});
+    const [roleDiffAttempt, mockGetDiff] = useAsync(() => wait(1000));
     return (
       <>
         <ButtonPrimary onClick={() => setOpen(true)}>Open</ButtonPrimary>
         <RoleEditorDialog
           resources={resources}
+          roleDiffProps={getRoleDiffProps(roleDiffAttempt, mockGetDiff)}
           open={open}
           onClose={() => setOpen(false)}
           onSave={async () => setOpen(false)}
@@ -320,8 +347,60 @@ export const DialogWithPolicyEnabled: StoryObj = {
     msw: {
       handlers: [yamlifyHandler, parseHandler],
     },
+    roleTesterEnabled: true,
   },
 };
+
+export const AccessGraphError: StoryObj = {
+  render() {
+    const [open, setOpen] = useState(false);
+    const resources = useResources([], {});
+    const [roleDiffAttempt, mockGetDiff] = useAsync(async () => {
+      await wait(1000);
+      throw new Error(unableToUpdatePreviewMessage, {
+        cause: new Error("There's a raccoon in the router"),
+      });
+    });
+    return (
+      <>
+        <ButtonPrimary onClick={() => setOpen(true)}>Open</ButtonPrimary>
+        <RoleEditorDialog
+          resources={resources}
+          roleDiffProps={getRoleDiffProps(roleDiffAttempt, mockGetDiff)}
+          open={open}
+          onClose={() => setOpen(false)}
+          onSave={async () => setOpen(false)}
+        />
+      </>
+    );
+  },
+  parameters: {
+    msw: {
+      handlers: [yamlifyHandler, parseHandler],
+    },
+    roleTesterEnabled: true,
+  },
+};
+
+const getRoleDiffProps = (
+  roleDiffAttempt: Attempt<unknown>,
+  getRoleDiff: () => void
+) => ({
+  roleDiffElement: (
+    <Flex
+      flex="1"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+      gap="2"
+    >
+      <Text typography="h1">Access Graph Placeholder</Text>
+      {roleDiffAttempt.status === 'processing' && <Indicator />}
+    </Flex>
+  ),
+  updateRoleDiff: getRoleDiff,
+  roleDiffAttempt,
+});
 
 const dummyRoleYaml = `kind: role
 metadata:
