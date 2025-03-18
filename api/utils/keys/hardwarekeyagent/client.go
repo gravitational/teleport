ea488b5c765299f1bf3a7532de938a2929236803
joerger <bjoerger@goteleport.com>
2025-03-17 18:03:48 -0700

    Add hardware key agent client and server implementation.

---
+++ api/utils/keys/hardwarekeyagent/client.go
@@ -0,0 +1,172 @@
+// Teleport
+// Copyright (C) 2025 Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package hardwarekeyagent
+
+import (
+	"context"
+	"crypto"
+	"crypto/rsa"
+	"crypto/x509"
+	"io"
+	"log/slog"
+	"math"
+	"os"
+	"path/filepath"
+
+	"github.com/gravitational/trace"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"github.com/gravitational/teleport/api/utils/keys/hardwarekey"
+	hardwarekeyagentv1 "github.com/gravitational/teleport/gen/proto/go/teleport/lib/teleterm/hardwarekeyagent/v1"
+)
+
+// AgentClient is a hardware key agent client implementation of [hardwarekey.Service].
+type AgentClient struct {
+	client        hardwarekeyagentv1.HardwareKeyAgentServiceClient
+	directService hardwarekey.Service
+}
+
+// NewClient creates a new hardware key agent client. If the hardware key agent connection
+// fails, this client will fallback to the given direct hardware key service.
+func NewClient(ctx context.Context, directService hardwarekey.Service) (*AgentClient, error) {
+	client, err := newClient()
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	return &AgentClient{
+		client:        client,
+		directService: directService,
+	}, nil
+}
+
+func newClient() (hardwarekeyagentv1.HardwareKeyAgentServiceClient, error) {
+	agentPath := filepath.Join(os.TempDir(), dirName, sockName)
+	if _, err := os.Stat(agentPath); err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	certPath := filepath.Join(os.TempDir(), dirName, certFileName)
+	creds, err := credentials.NewClientTLSFromFile(certPath, "" /*serverNameOverride*/)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	cc, err := grpc.NewClient("unix://"+agentPath, grpc.WithTransportCredentials(creds))
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	return hardwarekeyagentv1.NewHardwareKeyAgentServiceClient(cc), nil
+}
+
+// NewPrivateKey creates or retrieves a hardware private key for the given config.
+func (c *AgentClient) NewPrivateKey(ctx context.Context, config hardwarekey.PrivateKeyConfig) (*hardwarekey.Signer, error) {
+	return c.directService.NewPrivateKey(ctx, config)
+}
+
+// Sign performs a cryptographic signature using the specified hardware
+// private key and provided signature parameters.
+func (c *AgentClient) Sign(ctx context.Context, ref *hardwarekey.PrivateKeyRef, keyInfo hardwarekey.ContextualKeyInfo, rand io.Reader, digest []byte, opts crypto.SignerOpts) ([]byte, error) {
+	signature, err := c.agentSign(ctx, ref, keyInfo, rand, digest, opts)
+	if err != nil {
+		slog.ErrorContext(ctx, "Failed to perform signature over hardware key agent, falling back to direct hardware key service", "agent_err", err)
+		signature, err = c.directService.Sign(ctx, ref, keyInfo, rand, digest, opts)
+	}
+
+	return signature, trace.Wrap(err)
+}
+
+func (c *AgentClient) agentSign(ctx context.Context, ref *hardwarekey.PrivateKeyRef, keyInfo hardwarekey.ContextualKeyInfo, _ io.Reader, digest []byte, opts crypto.SignerOpts) ([]byte, error) {
+	slotKey, err := pivSlotKeyToProto(ref.SlotKey)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	publicKeyDER, err := x509.MarshalPKIXPublicKey(ref.PublicKey)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	var hash hardwarekeyagentv1.Hash
+	switch opts.HashFunc() {
+	case 0:
+		hash = hardwarekeyagentv1.Hash_HASH_NONE
+	case crypto.SHA256:
+		hash = hardwarekeyagentv1.Hash_HASH_SHA256
+	case crypto.SHA512:
+		hash = hardwarekeyagentv1.Hash_HASH_SHA512
+	default:
+		return nil, trace.BadParameter("unsupported hash func %q", opts.HashFunc().String())
+	}
+
+	var saltLength int
+	if pssOpts, ok := opts.(*rsa.PSSOptions); ok {
+		saltLength = pssOpts.SaltLength
+
+		// If the salt length is [rsa.PSSSaltLengthEqualsHash] or [rsa.PSSSaltLengthAuto],
+		// pre-calculate the salt length so we can send it over the gRPC message.
+		switch saltLength {
+		case rsa.PSSSaltLengthEqualsHash:
+			saltLength = opts.HashFunc().Size()
+		case rsa.PSSSaltLengthAuto:
+			rsaPub, ok := ref.PublicKey.(rsa.PublicKey)
+			if !ok {
+				return nil, trace.BadParameter("cannot perform PSS signature for non-rsa key")
+			}
+
+			// We use the same salt length calculation as the crypto/rsa package.
+			// https://github.com/golang/go/blob/21483099632c11743d01ec6f38577f31de26b0d0/src/crypto/internal/fips140/rsa/pkcs1v22.go#L253
+			saltLength = (rsaPub.N.BitLen()-1+7)/8 - 2 - opts.HashFunc().Size()
+		}
+
+		if saltLength < 0 {
+			return nil, rsa.ErrMessageTooLong
+		}
+
+		if saltLength > math.MaxUint32 {
+			return nil, trace.BadParameter("invalid salt length %d", saltLength)
+		}
+	}
+
+	req := &hardwarekeyagentv1.SignRequest{
+		Digest:     digest,
+		Hash:       hash,
+		SaltLength: uint32(saltLength),
+		KeyRef: &hardwarekeyagentv1.KeyRef{
+			SerialNumber: ref.SerialNumber,
+			SlotKey:      slotKey,
+			PublicKeyDer: publicKeyDER,
+		},
+		KeyInfo: &hardwarekeyagentv1.KeyInfo{
+			TouchRequired: ref.Policy.TouchRequired,
+			PinRequired:   ref.Policy.PINRequired,
+			ProxyHost:     keyInfo.ProxyHost,
+			Username:      keyInfo.Username,
+			ClusterName:   keyInfo.ClusterName,
+		},
+		// TODO: Add command to sign request for prompt context.
+	}
+
+	resp, err := c.client.Sign(ctx, req)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	return resp.Signature, nil
+}
---
+++ api/utils/keys/hardwarekeyagent/server.go
@@ -0,0 +1,230 @@
+// Copyright 2025 Gravitational, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hardwarekeyagent
+
+import (
+	"context"
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"path/filepath"
+	"syscall"
+
+	"github.com/gravitational/trace"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"github.com/gravitational/teleport/api/utils/grpc/interceptors"
+	"github.com/gravitational/teleport/api/utils/keys"
+	"github.com/gravitational/teleport/api/utils/keys/hardwarekey"
+	hardwarekeyagentv1 "github.com/gravitational/teleport/gen/proto/go/teleport/lib/teleterm/hardwarekeyagent/v1"
+	"github.com/gravitational/teleport/lib/utils/cert"
+)
+
+const (
+	dirName      = ".Teleport-PIV"
+	sockName     = "agent.sock"
+	certFileName = "cert.pem"
+)
+
+// Server implementation [hardwarekeyagentv1.HardwareKeyAgentServiceServer].
+type Server struct {
+	hardwarekeyagentv1.UnimplementedHardwareKeyAgentServiceServer
+	s hardwarekey.Service
+}
+
+// NewServer returns a new hardware key agent server.
+func NewServer(s hardwarekey.Service) *Server {
+	return &Server{s: s}
+}
+
+// RunServer runs a new [hardwarekeyagentv1.HardwareKeyAgentServiceServer] using the service.
+func (s *Server) RunServer(ctx context.Context) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	l, err := newAgentListener(ctx)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	context.AfterFunc(ctx, func() { l.Close() })
+
+	cert, err := generateServerCert()
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	grpcServer := grpc.NewServer(
+		grpc.Creds(credentials.NewServerTLSFromCert(&cert)),
+		grpc.UnaryInterceptor(interceptors.GRPCServerUnaryErrorInterceptor),
+	)
+	hardwarekeyagentv1.RegisterHardwareKeyAgentServiceServer(grpcServer, s)
+
+	fmt.Fprintln(os.Stderr, "Listening for hardware key agent requests")
+	return trace.Wrap(grpcServer.Serve(l))
+}
+
+func newAgentListener(ctx context.Context) (net.Listener, error) {
+	keyAgentDir := filepath.Join(os.TempDir(), dirName)
+	if err := os.MkdirAll(keyAgentDir, 0o700); err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	keyAgentPath := filepath.Join(keyAgentDir, sockName)
+	l, err := net.Listen("unix", keyAgentPath)
+	if err == nil {
+		return l, nil
+	} else if !errors.Is(err, syscall.EADDRINUSE) {
+		return nil, trace.Wrap(err)
+	}
+
+	// A hardware key agent already exists in the given path. Before replacing it,
+	// try to connect to it and see if it is active.
+	client, err := newClient()
+	if err == nil {
+		pong, err := client.Ping(ctx, &hardwarekeyagentv1.PingRequest{})
+		if err == nil {
+			return nil, trace.AlreadyExists("another agent instance is already running; PID: %d", pong.Pid)
+		}
+	}
+
+	// If it isn't running, remove the agent dir and try again.
+	if err := os.RemoveAll(keyAgentDir); err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	return newAgentListener(ctx)
+}
+
+func generateServerCert() (tls.Certificate, error) {
+	creds, err := cert.GenerateSelfSignedCert([]string{"localhost"}, nil /*ipAddresses*/)
+	if err != nil {
+		return tls.Certificate{}, trace.Wrap(err, "failed to generate the certificate")
+	}
+
+	certPath := filepath.Join(os.TempDir(), dirName, certFileName)
+	f, err := os.OpenFile(certPath, os.O_RDWR|os.O_CREATE, 0o600)
+	if err != nil {
+		return tls.Certificate{}, trace.Wrap(err)
+	}
+	if _, err = f.Write(creds.Cert); err != nil {
+		return tls.Certificate{}, trace.Wrap(err)
+	}
+	if err = f.Close(); err != nil {
+		return tls.Certificate{}, trace.Wrap(err)
+	}
+
+	return keys.X509KeyPair(creds.Cert, creds.PrivateKey)
+}
+
+// Sign the given digest with the specified hardware private key.
+func (s *Server) Sign(ctx context.Context, req *hardwarekeyagentv1.SignRequest) (*hardwarekeyagentv1.Signature, error) {
+	slotKey, err := pivSlotKeyFromProto(req.KeyRef.SlotKey)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	pub, err := x509.ParsePKIXPublicKey(req.KeyRef.PublicKeyDer)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	keyRef := &hardwarekey.PrivateKeyRef{
+		SerialNumber: req.KeyRef.SerialNumber,
+		SlotKey:      slotKey,
+		PublicKey:    pub,
+		Policy: hardwarekey.PromptPolicy{
+			TouchRequired: req.KeyInfo.TouchRequired,
+			PINRequired:   req.KeyInfo.PinRequired,
+		},
+	}
+
+	keyInfo := hardwarekey.ContextualKeyInfo{
+		ProxyHost:   req.KeyInfo.ProxyHost,
+		Username:    req.KeyInfo.Username,
+		ClusterName: req.KeyInfo.ClusterName,
+	}
+
+	var signerOpts crypto.SignerOpts
+	switch req.Hash {
+	case hardwarekeyagentv1.Hash_HASH_NONE:
+		signerOpts = crypto.Hash(0)
+	case hardwarekeyagentv1.Hash_HASH_SHA256:
+		signerOpts = crypto.SHA256
+	case hardwarekeyagentv1.Hash_HASH_SHA512:
+		signerOpts = crypto.SHA512
+	default:
+		return nil, trace.BadParameter("unsupported hash %q", req.Hash.String())
+	}
+
+	if req.SaltLength > 0 {
+		signerOpts = &rsa.PSSOptions{
+			Hash:       signerOpts.HashFunc(),
+			SaltLength: int(req.SaltLength),
+		}
+	}
+
+	signature, err := s.s.Sign(ctx, keyRef, keyInfo, rand.Reader, req.Digest, signerOpts)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	return &hardwarekeyagentv1.Signature{
+		Signature: signature,
+	}, nil
+}
+
+// Ping the server and get its PID.
+func (s *Server) Ping(ctx context.Context, req *hardwarekeyagentv1.PingRequest) (*hardwarekeyagentv1.PingResponse, error) {
+	return &hardwarekeyagentv1.PingResponse{
+		Pid: uint32(os.Getpid()),
+	}, nil
+}
+
+func pivSlotKeyFromProto(pivSlot hardwarekeyagentv1.PIVSlotKey) (hardwarekey.PIVSlotKey, error) {
+	switch pivSlot {
+	case hardwarekeyagentv1.PIVSlotKey_PIV_SLOT_KEY_9A:
+		return 0x9a, nil
+	case hardwarekeyagentv1.PIVSlotKey_PIV_SLOT_KEY_9C:
+		return 0x9c, nil
+	case hardwarekeyagentv1.PIVSlotKey_PIV_SLOT_KEY_9D:
+		return 0x9d, nil
+	case hardwarekeyagentv1.PIVSlotKey_PIV_SLOT_KEY_9E:
+		return 0x9e, nil
+	default:
+		return 0, trace.BadParameter("unknown piv slot key for proto enum %d", pivSlot)
+	}
+}
+
+func pivSlotKeyToProto(slotKey hardwarekey.PIVSlotKey) (hardwarekeyagentv1.PIVSlotKey, error) {
+	switch slotKey {
+	case 0x9a:
+		return hardwarekeyagentv1.PIVSlotKey_PIV_SLOT_KEY_9A, nil
+	case 0x9c:
+		return hardwarekeyagentv1.PIVSlotKey_PIV_SLOT_KEY_9C, nil
+	case 0x9d:
+		return hardwarekeyagentv1.PIVSlotKey_PIV_SLOT_KEY_9D, nil
+	case 0x9e:
+		return hardwarekeyagentv1.PIVSlotKey_PIV_SLOT_KEY_9E, nil
+	default:
+		return 0, trace.BadParameter("unknown proto enum for piv slot key %d", slotKey)
+	}
+}
