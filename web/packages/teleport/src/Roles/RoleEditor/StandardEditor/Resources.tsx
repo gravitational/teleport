e5c073698ca2237082dcb7a79da9614159a03add
Bartosz Leper <bartosz.leper@goteleport.com>
2025-03-20 22:29:01 +0100

    Clarify the wording in role editor
    Use "Teleport resources" and "Kubernetes resources" instead of
    "resources" to make them unambiguous.

--- web/packages/teleport/src/Roles/RoleEditor/StandardEditor/Resources.tsx
+++ web/packages/teleport/src/Roles/RoleEditor/StandardEditor/Resources.tsx
@@ -121,7 +121,7 @@ export const ResourcesTab = memo(function ResourcesTab({
           buttonText={
             <>
               <Plus size="small" mr={2} />
-              Add Resource Access
+              Add Teleport Resource Access
             </>
           }
           buttonProps={{
@@ -351,8 +351,8 @@ export function KubernetesAccessSection({
           >
             <Add disabled={isProcessing} size="small" />
             {value.resources.length > 0
-              ? 'Add Another Resource'
-              : 'Add a Resource'}
+              ? 'Add Another Kubernetes Resource'
+              : 'Add a Kubernetes Resource'}
           </ButtonSecondary>
         </Box>
       </Flex>
@@ -384,10 +384,10 @@ function KubernetesResourceView({
     >
       <Flex>
         <Box flex="1">
-          <H4 mb={3}>Resource</H4>
+          <H4 mb={3}>Kubernetes Resource</H4>
         </Box>
         <ButtonIcon
-          aria-label="Remove resource"
+          aria-label="Remove Kubernetes resource"
           disabled={isProcessing}
           onClick={onRemove}
         >
