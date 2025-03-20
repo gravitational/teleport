879366060a754626e462678e86323057c0e5e6c9
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2025-03-20 11:57:45 -0400

    [v16] Reduce resource consumption when generating Kubernetes certificates (#52147)
    * Reduce resource consumption when generating Kubernetes certificates (#52109)
    
    Closes https://github.com/gravitational/teleport/issues/52073.
    
    The requested Kubernetes cluster is now cross referenced with the
    KubeServers in the unified resource cache. This results in a
    reduction in CPU, memory, and cert generation latency. This also
    cleans up some of the helper functions in lib/kube/utils that
    were no longer needed, and suboptimal.
    
    The client side changes here shouldn't have any impact, as the
    server is performing the same check, and returning the equivalent
    error the client side code used to. This will also cut the time
    of `tctl auth sign` in half as both the client and server were
    performing the same expensive CheckKubeCluster operation.
    
    * Fix test flakes related to Kubernetes user cert generation (#52442)
    
    #52109 added a dependency on the unified resource cache to user
    cert generation to reduce resource consumption. A number of tests
    that exercise generating Kubernetes user certs were either not
    waiting for the Kubernetes resources to exist prior to authentication
    and getting lucky, or checking that the resources were in the auth
    cache, but not the unified resource cache.
    
    This attempts to cover any tests which generate Kubernetes user
    certificates to verify that the unified resource cache contains
    the expected cluster before proceeding.
    
    Fixes #52157.
    Fixes #52441.

--- lib/kube/utils/utils_test.go
+++ lib/kube/utils/utils_test.go
@@ -26,73 +26,8 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/gravitational/teleport/api/client/proto"
-	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/lib/automaticupgrades"
 )
-
-func TestCheckKubeCluster(t *testing.T) {
-	t.Parallel()
-	ctx := context.Background()
-
-	kubeServers := []types.KubeServer{
-		kubeServer(t, "k8s-1", "server1", "uuuid"),
-		kubeServer(t, "k8s-2", "server1", "uuuid"),
-		kubeServer(t, "k8s-3", "server1", "uuuid"),
-		kubeServer(t, "k8s-4", "server1", "uuuid"),
-	}
-
-	tests := []struct {
-		desc        string
-		services    []types.KubeServer
-		kubeCluster string
-		assertErr   require.ErrorAssertionFunc
-	}{
-		{
-			desc:        "valid cluster name",
-			services:    kubeServers,
-			kubeCluster: "k8s-4",
-			assertErr:   require.NoError,
-		},
-		{
-			desc:        "invalid cluster name",
-			services:    kubeServers,
-			kubeCluster: "k8s-5",
-			assertErr:   require.Error,
-		},
-		{
-			desc:        "no registered clusters",
-			services:    []types.KubeServer{},
-			kubeCluster: "k8s-1",
-			assertErr:   require.Error,
-		},
-		{
-			desc:        "empty cluster provided",
-			services:    kubeServers,
-			kubeCluster: "",
-			assertErr:   require.Error,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.desc, func(t *testing.T) {
-			err := CheckKubeCluster(ctx, mockKubeServicesPresence(tt.services), tt.kubeCluster)
-			tt.assertErr(t, err)
-		})
-	}
-}
-
-type mockKubeServicesPresence []types.KubeServer
-
-func (p mockKubeServicesPresence) GetKubernetesServers(context.Context) ([]types.KubeServer, error) {
-	return p, nil
-}
-
-func kubeServer(t *testing.T, kubeCluster, hostname, hostID string) types.KubeServer {
-	cluster, err := types.NewKubernetesClusterV3(types.Metadata{Name: kubeCluster}, types.KubernetesClusterSpecV3{})
-	require.NoError(t, err)
-	server, err := types.NewKubernetesServerV3FromCluster(cluster, hostname, hostID)
-	require.NoError(t, err)
-	return server
-}
 
 func TestGetAgentVersion(t *testing.T) {
 	t.Parallel()
@@ -160,21 +95,4 @@ type pinger struct {
 
 func (p *pinger) Ping(ctx context.Context) (proto.PingResponse, error) {
 	return p.pingFn(ctx)
-}
-
-func TestExtractAndSortKubeClusterNames(t *testing.T) {
-	t.Parallel()
-
-	server1 := kubeServer(t, "watermelon", "server1", "uuuid")
-
-	server2 := kubeServer(t, "watermelon", "server1", "uuuid")
-
-	server3 := kubeServer(t, "banana", "server2", "uuuid2")
-
-	server4 := kubeServer(t, "apple", "server2", "uuuid2")
-
-	server5 := kubeServer(t, "pear", "server2", "uuuid2")
-
-	names := extractAndSortKubeClusterNames(types.KubeServers{server1, server2, server3, server4, server5})
-	require.Equal(t, []string{"apple", "banana", "pear", "watermelon"}, names)
 }
