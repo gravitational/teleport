8fcf535599168aa86a75db6001c32fbe79e3216e
Andrew Burke <andrew.burke@goteleport.com>
2025-03-20 17:33:59 -0700

    Make remoteFS ctx more useful

--- lib/sshutils/sftp/remote.go
+++ lib/sshutils/sftp/remote.go
@@ -19,13 +19,29 @@
 package sftp
 
 import (
+	"cmp"
 	"context"
 	"os"
 	portablepath "path"
 	"time"
 
+	"github.com/gravitational/trace"
 	"github.com/pkg/sftp"
 )
+
+func runCancelableFunc(ctx context.Context, f func()) error {
+	done := make(chan struct{})
+	go func() {
+		f()
+		close(done)
+	}()
+	select {
+	case <-done:
+		return nil
+	case <-ctx.Done():
+		return trace.Wrap(ctx.Err())
+	}
+}
 
 // remoteFS provides API for accessing the files on
 // the local file system
@@ -41,45 +57,29 @@ func (r *remoteFS) Type() string {
 	return "remote"
 }
 
-func (r *remoteFS) Glob(ctx context.Context, pattern string) ([]string, error) {
-	if err := ctx.Err(); err != nil {
-		return nil, err
-	}
-
-	matches, err := r.c.Glob(pattern)
-	if err != nil {
-		return nil, err
-	}
-
-	return matches, nil
+func (r *remoteFS) Glob(ctx context.Context, pattern string) (matches []string, err error) {
+	ctxErr := runCancelableFunc(ctx, func() {
+		matches, err = r.c.Glob(pattern)
+	})
+	return matches, cmp.Or(ctxErr, err)
 }
 
-func (r *remoteFS) Stat(ctx context.Context, path string) (os.FileInfo, error) {
-	if err := ctx.Err(); err != nil {
-		return nil, err
-	}
-
-	fi, err := r.c.Stat(path)
-	if err != nil {
-		return nil, err
-	}
-
-	return fi, nil
+func (r *remoteFS) Stat(ctx context.Context, path string) (fi os.FileInfo, err error) {
+	ctxErr := runCancelableFunc(ctx, func() {
+		fi, err = r.c.Stat(path)
+	})
+	return fi, cmp.Or(ctxErr, err)
 }
 
 func (r *remoteFS) ReadDir(ctx context.Context, path string) ([]os.FileInfo, error) {
-	if err := ctx.Err(); err != nil {
-		return nil, err
-	}
-
-	fileInfos, err := r.c.ReadDir(path)
+	fileInfos, err := r.c.ReadDirContext(ctx, path)
 	if err != nil {
 		return nil, err
 	}
 	for i := range fileInfos {
 		// if the file is a symlink, return the info of the linked file
 		if fileInfos[i].Mode().Type()&os.ModeSymlink != 0 {
-			fileInfos[i], err = r.c.Stat(portablepath.Join(path, fileInfos[i].Name()))
+			fileInfos[i], err = r.Stat(ctx, portablepath.Join(path, fileInfos[i].Name()))
 			if err != nil {
 				return nil, err
 			}
@@ -90,130 +90,100 @@ func (r *remoteFS) ReadDir(ctx context.Context, path string) ([]os.FileInfo, err
 }
 
 func (r *remoteFS) Open(ctx context.Context, path string) (File, error) {
-	if err := ctx.Err(); err != nil {
-		return nil, err
-	}
-
-	f, err := r.c.Open(path)
-	if err != nil {
-		return nil, err
-	}
-
-	return f, nil
+	return r.OpenFile(ctx, path, os.O_RDONLY)
 }
 
 func (r *remoteFS) Create(ctx context.Context, path string, _ int64) (File, error) {
 	return r.OpenFile(ctx, path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC)
 }
 
-func (r *remoteFS) OpenFile(ctx context.Context, path string, flags int) (File, error) {
-	if err := ctx.Err(); err != nil {
-		return nil, err
-	}
-	f, err := r.c.OpenFile(path, flags)
-	if err != nil {
-		return nil, err
-	}
-
-	return f, nil
+func (r *remoteFS) OpenFile(ctx context.Context, path string, flags int) (f File, err error) {
+	ctxErr := runCancelableFunc(ctx, func() {
+		f, err = r.c.OpenFile(path, flags)
+	})
+	return f, cmp.Or(ctxErr, err)
 }
 
-func (r *remoteFS) Mkdir(ctx context.Context, path string) error {
-	if err := ctx.Err(); err != nil {
-		return err
-	}
-
-	err := r.c.MkdirAll(path)
-	if err != nil {
-		return err
-	}
-
-	return nil
+func (r *remoteFS) Mkdir(ctx context.Context, path string) (err error) {
+	ctxErr := runCancelableFunc(ctx, func() {
+		err = r.c.MkdirAll(path)
+	})
+	return cmp.Or(ctxErr, err)
 }
 
-func (r *remoteFS) Chmod(ctx context.Context, path string, mode os.FileMode) error {
-	if err := ctx.Err(); err != nil {
-		return err
-	}
-
-	return r.c.Chmod(path, mode)
+func (r *remoteFS) Chmod(ctx context.Context, path string, mode os.FileMode) (err error) {
+	ctxErr := runCancelableFunc(ctx, func() {
+		err = r.c.Chmod(path, mode)
+	})
+	return cmp.Or(ctxErr, err)
 }
 
-func (r *remoteFS) Chtimes(ctx context.Context, path string, atime, mtime time.Time) error {
-	if err := ctx.Err(); err != nil {
-		return err
-	}
-
-	return r.c.Chtimes(path, atime, mtime)
+func (r *remoteFS) Chtimes(ctx context.Context, path string, atime, mtime time.Time) (err error) {
+	ctxErr := runCancelableFunc(ctx, func() {
+		err = r.c.Chtimes(path, atime, mtime)
+	})
+	return cmp.Or(ctxErr, err)
 }
 
-func (r *remoteFS) Rename(ctx context.Context, oldpath, newpath string) error {
-	if err := ctx.Err(); err != nil {
-		return err
-	}
-	return r.c.Rename(oldpath, newpath)
+func (r *remoteFS) Rename(ctx context.Context, oldpath, newpath string) (err error) {
+	ctxErr := runCancelableFunc(ctx, func() {
+		err = r.c.Rename(oldpath, newpath)
+	})
+	return cmp.Or(ctxErr, err)
 }
 
-func (r *remoteFS) Lstat(ctx context.Context, name string) (os.FileInfo, error) {
-	if err := ctx.Err(); err != nil {
-		return nil, err
-	}
-	fi, err := r.c.Lstat(name)
-	if err != nil {
-		return nil, err
-	}
-	return fi, nil
+func (r *remoteFS) Lstat(ctx context.Context, name string) (fi os.FileInfo, err error) {
+	ctxErr := runCancelableFunc(ctx, func() {
+		fi, err = r.c.Lstat(name)
+	})
+	return fi, cmp.Or(ctxErr, err)
 }
 
-func (r *remoteFS) RemoveAll(ctx context.Context, path string) error {
-	if err := ctx.Err(); err != nil {
-		return err
-	}
-	return r.c.RemoveAll(path)
+func (r *remoteFS) RemoveAll(ctx context.Context, path string) (err error) {
+	ctxErr := runCancelableFunc(ctx, func() {
+		err = r.c.RemoveAll(path)
+	})
+	return cmp.Or(ctxErr, err)
 }
 
-func (r *remoteFS) Link(ctx context.Context, oldname, newname string) error {
-	if err := ctx.Err(); err != nil {
-		return err
-	}
-	return r.c.Link(oldname, newname)
+func (r *remoteFS) Link(ctx context.Context, oldname, newname string) (err error) {
+	ctxErr := runCancelableFunc(ctx, func() {
+		err = r.c.Link(oldname, newname)
+	})
+	return cmp.Or(ctxErr, err)
 }
 
-func (r *remoteFS) Symlink(ctx context.Context, oldname, newname string) error {
-	if err := ctx.Err(); err != nil {
-		return err
-	}
-	return r.c.Symlink(oldname, newname)
+func (r *remoteFS) Symlink(ctx context.Context, oldname, newname string) (err error) {
+	ctxErr := runCancelableFunc(ctx, func() {
+		err = r.c.Symlink(oldname, newname)
+	})
+	return cmp.Or(ctxErr, err)
 }
 
-func (r *remoteFS) Remove(ctx context.Context, name string) error {
-	if err := ctx.Err(); err != nil {
-		return err
-	}
-	return r.c.Remove(name)
+func (r *remoteFS) Remove(ctx context.Context, name string) (err error) {
+	ctxErr := runCancelableFunc(ctx, func() {
+		err = r.c.Remove(name)
+	})
+	return cmp.Or(ctxErr, err)
 }
 
-func (r *remoteFS) Chown(ctx context.Context, name string, uid, gid int) error {
-	if err := ctx.Err(); err != nil {
-		return err
-	}
-	return r.c.Chown(name, uid, gid)
+func (r *remoteFS) Chown(ctx context.Context, name string, uid, gid int) (err error) {
+	ctxErr := runCancelableFunc(ctx, func() {
+		err = r.c.Chown(name, uid, gid)
+	})
+	return cmp.Or(ctxErr, err)
 }
 
-func (r *remoteFS) Truncate(ctx context.Context, name string, size int64) error {
-	if err := ctx.Err(); err != nil {
-		return err
-	}
-	return r.c.Truncate(name, size)
+func (r *remoteFS) Truncate(ctx context.Context, name string, size int64) (err error) {
+	ctxErr := runCancelableFunc(ctx, func() {
+		err = r.c.Truncate(name, size)
+	})
+	return cmp.Or(ctxErr, err)
 }
 
-func (r *remoteFS) Readlink(ctx context.Context, name string) (string, error) {
-	if err := ctx.Err(); err != nil {
-		return "", err
-	}
-	dest, err := r.c.ReadLink(name)
-	if err != nil {
-		return "", err
-	}
-	return dest, nil
+func (r *remoteFS) Readlink(ctx context.Context, name string) (dest string, err error) {
+	ctxErr := runCancelableFunc(ctx, func() {
+		dest, err = r.c.ReadLink(name)
+	})
+	return dest, cmp.Or(ctxErr, err)
 }
