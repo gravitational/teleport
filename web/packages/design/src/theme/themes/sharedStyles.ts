e2ef749ebb76896ed3561b23660d70e6e3891ada
Rafał Cieślak <rafal.cieslak@goteleport.com>
2025-03-27 16:30:48 +0100

    Add rowGap and columnGap to Flex (#53459)

--- web/packages/design/src/theme/themes/sharedStyles.ts
+++ web/packages/design/src/theme/themes/sharedStyles.ts
@@ -46,6 +46,11 @@ export const sharedStyles: SharedStyles = {
     large: 1280,
   },
   topBarHeight: [44, 56, 72],
+  /**
+   *
+   * idx:    0  1  2   3   4   5   6   7   8   9  10  11
+   * space: [0, 4, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80]
+   */
   space: [0, 4, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80],
   borders: [
     0,
