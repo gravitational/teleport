9bde9615c3fd2b935cf562b10a5442e0e0ca6649
Stephen Levine <stephen.levine@goteleport.com>
2025-03-31 16:05:34 -0400

    order

--- lib/autoupdate/agent/config.go
+++ lib/autoupdate/agent/config.go
@@ -75,6 +75,8 @@ type UpdateSpec struct {
 type UpdateStatus struct {
 	// IDFile is the path to a temporary file containing the updater ID.
 	IDFile string `yaml:"id_file,omitempty"`
+	// LastUpdate status, if attempted
+	LastUpdate *LastUpdate `yaml:"last_update,omitempty"`
 	// Active is the currently active revision of Teleport.
 	Active Revision `yaml:"active"`
 	// Backup is the last working revision of Teleport.
@@ -83,8 +85,6 @@ type UpdateStatus struct {
 	// Skipped revisions are not applied because they
 	// are known to crash.
 	Skip *Revision `yaml:"skip,omitempty"`
-	// LastUpdate status, if attempted
-	LastUpdate *LastUpdate `yaml:"last_update,omitempty"`
 }
 
 // LastUpdate describes the last attempted updated.
