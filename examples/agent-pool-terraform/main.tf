df42d6cb136e53a0367835fd92ac9747ffad4099
Matt Smith <matt.smith@goteleport.com>
2025-03-31 21:12:50 -0400

    Fix loading issues and ignore modules

--- examples/agent-pool-terraform/main.tf
+++ examples/agent-pool-terraform/main.tf
@@ -1,7 +1,7 @@
 module "teleport" {
-  source                = "./teleport"
+  source                = "./agent-installation"
   agent_count           = var.agent_count
-  agent_roles           = var.agent_roles
+  agent_labels          = var.agent_labels
   proxy_service_address = var.proxy_service_address
   teleport_edition      = var.teleport_edition
   teleport_version      = var.teleport_version
