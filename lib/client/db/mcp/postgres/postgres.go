96a99f3848721d79f2613d9973ef115f1231e920
Gabriel Corado <gabriel.oliveira@goteleport.com>
2025-04-02 12:20:10 -0700

    feat: multiple pg databases initial support

--- lib/client/db/mcp/postgres/postgres.go
+++ lib/client/db/mcp/postgres/postgres.go
@@ -17,28 +17,46 @@ import (
 )
 
 type Session struct {
+	dbs map[string]*servedDB
+}
+
+type servedDB struct {
 	conn *pgx.Conn
+	info DBInfo
 }
 
 type NewSessionConfig struct {
-	MCPServer *mcpserver.MCPServer
-	RawDBConn net.Conn
-	Route     clientproto.RouteToDatabase
+	MCPServer   *mcpserver.MCPServer
+	Datatabases []DBInfo
+}
+
+type DBInfo struct {
+	RawConn     net.Conn
+	Route       clientproto.RouteToDatabase
+	Description string
 }
 
 func NewSession(ctx context.Context, cfg NewSessionConfig) (*Session, error) {
 	slog.DebugContext(ctx, "New database MCP session")
-	pgConfig, err := buildConfig(cfg)
-	if err != nil {
-		return nil, trace.Wrap(err)
+
+	sess := &Session{}
+	dbs := make(map[string]*servedDB, len(cfg.Datatabases))
+	for _, dbInfo := range cfg.Datatabases {
+		pgConfig, err := buildConfig(dbInfo)
+		if err != nil {
+			return nil, trace.Wrap(err)
+		}
+		pgConn, err := pgx.ConnectConfig(ctx, pgConfig)
+		if err != nil {
+			return nil, trace.ConnectionProblem(err, "Unable to connect to database %q: %v", dbInfo.Route.ServiceName, err)
+		}
+
+		dbURI := buildDBURI(dbInfo.Route.ServiceName)
+		dbs[dbURI] = &servedDB{conn: pgConn, info: dbInfo}
+		cfg.MCPServer.AddResource(mcp.NewResource(dbURI, fmt.Sprintf("%s PostgreSQL Datatabase", dbInfo.Route.ServiceName)), sess.DatabaseResourceHandler)
 	}
 
-	pgConn, err := pgx.ConnectConfig(ctx, pgConfig)
-	if err != nil {
-		return nil, trace.ConnectionProblem(err, "Unable to connect to database: %v", err)
-	}
-
-	sess := &Session{conn: pgConn}
+	sess.dbs = dbs
 	cfg.MCPServer.AddTool(queryTool, sess.QueryToolHandler)
 
 	// TODO add resources support. Note: the current MCP SDK doesn't support
@@ -48,14 +66,39 @@ func NewSession(ctx context.Context, cfg NewSessionConfig) (*Session, error) {
 }
 
 func (sess *Session) Close(ctx context.Context) error {
-	return sess.conn.Close(ctx)
+	var errors []error
+	for _, db := range sess.dbs {
+		errors = append(errors, db.conn.Close(ctx))
+	}
+	return trace.NewAggregate(errors...)
+}
+
+func (sess *Session) DatabaseResourceHandler(ctx context.Context, request mcp.ReadResourceRequest) ([]mcp.ResourceContents, error) {
+	if db, ok := sess.dbs[request.Params.URI]; ok {
+		return []mcp.ResourceContents{
+			mcp.TextResourceContents{
+				URI:      request.Params.URI,
+				MIMEType: "text/plain",
+				Text:     db.info.Description,
+			},
+		}, nil
+	}
+
+	return nil, trace.NotFound("database %q not found", request.Params.URI)
 }
 
 func (sess *Session) QueryToolHandler(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
 	slog.DebugContext(ctx, "Handle query", "request", request)
 	sql := request.Params.Arguments[queryToolSQLParamName].(string)
+	dbURI := request.Params.Arguments[queryToolDBNameParamName].(string)
 
-	tx, err := sess.conn.BeginTx(ctx, pgx.TxOptions{AccessMode: pgx.ReadOnly})
+	// TODO have some better handling for db name, db URI, and or description (or query).
+	db, ok := sess.dbs[buildDBURI(dbURI)]
+	if !ok {
+		return nil, trace.NotFound("Database %q not found. Only databases resources can be used for queries.")
+	}
+
+	tx, err := db.conn.BeginTx(ctx, pgx.TxOptions{AccessMode: pgx.ReadOnly})
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
@@ -102,13 +145,15 @@ func buildQueryResult(rows pgx.Rows) (string, error) {
 	return string(out), trace.Wrap(err)
 }
 
-// func (sess *Session) ListResources() ([]ListResourceItem, error) { return nil, nil }
-// func (sess *Session) ReadResource(uri string) (Resource, error)  { return Resource{}, nil }
+func buildDBURI(name string) string {
+	return fmt.Sprintf("teleport://db/postgres/%s", name)
+}
 
 var (
 	queryTool = mcp.NewTool(
 		queryToolName,
 		mcp.WithDescription("Run a read-only SQL query"),
+		mcp.WithString(queryToolDBNameParamName, mcp.Description("Database resource URI where the query will be executed"), mcp.Required()),
 		mcp.WithString(queryToolSQLParamName, mcp.Required()),
 	)
 )
@@ -116,8 +161,9 @@ var (
 const (
 	hostnamePlaceholder = "repl"
 
-	queryToolName         = "query"
-	queryToolSQLParamName = "sql"
+	queryToolName            = "query"
+	queryToolSQLParamName    = "sql"
+	queryToolDBNameParamName = "db_name"
 
 	// TODO use to list tables and their schema.
 	// listTablesQuery       = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
@@ -136,13 +182,13 @@ const (
 	applicationNameParamValue = "teleport-repl"
 )
 
-func buildConfig(cfg NewSessionConfig) (*pgx.ConnConfig, error) {
+func buildConfig(info DBInfo) (*pgx.ConnConfig, error) {
 	config, err := pgx.ParseConfig(fmt.Sprintf("postgres://%s", hostnamePlaceholder))
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
-	config.User = cfg.Route.Username
-	config.Database = cfg.Route.Database
+	config.User = info.Route.Username
+	config.Database = info.Route.Database
 	config.ConnectTimeout = defaults.DatabaseConnectTimeout
 	config.RuntimeParams = map[string]string{
 		applicationNameParamName: applicationNameParamValue,
@@ -155,7 +201,7 @@ func buildConfig(cfg NewSessionConfig) (*pgx.ConnConfig, error) {
 		return []string{hostnamePlaceholder}, nil
 	}
 	config.DialFunc = func(_ context.Context, _, _ string) (net.Conn, error) {
-		return cfg.RawDBConn, nil
+		return info.RawConn, nil
 	}
 
 	return config, nil
--- tool/tsh/common/mcp_db.go
+++ tool/tsh/common/mcp_db.go
@@ -20,25 +20,29 @@ package common
 
 import (
 	"net"
+	"sort"
+	"strings"
+	"time"
 
 	"github.com/gravitational/trace"
 	"github.com/mark3labs/mcp-go/server"
 
 	"github.com/gravitational/teleport"
+	apiclient "github.com/gravitational/teleport/api/client"
+	clientproto "github.com/gravitational/teleport/api/client/proto"
+	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/lib/client"
 	pgmcp "github.com/gravitational/teleport/lib/client/db/mcp/postgres"
+	"github.com/gravitational/teleport/lib/defaults"
+	"github.com/gravitational/teleport/lib/services"
 	"github.com/gravitational/teleport/lib/srv/alpnproxy"
 	alpncommon "github.com/gravitational/teleport/lib/srv/alpnproxy/common"
+	"github.com/gravitational/teleport/lib/tlsca"
 	"github.com/gravitational/teleport/lib/utils"
 	listenerutils "github.com/gravitational/teleport/lib/utils/listener"
 )
 
 func onMCPStartDB(cf *CLIConf) error {
-	if cf.DatabaseService == "" {
-		// lib/mcp/proxy.go
-		return trace.Wrap(onGenericMCPServerOnProxy(cf))
-	}
-
 	tc, err := makeClient(cf)
 	if err != nil {
 		return trace.Wrap(err)
@@ -51,49 +55,53 @@ func onMCPStartDB(cf *CLIConf) error {
 	if err != nil {
 		return trace.Wrap(err)
 	}
-	dbInfo, err := getDatabaseInfo(cf, tc, routes)
-	if err != nil {
-		return trace.Wrap(err)
-	}
-	requires := &dbLocalProxyRequirement{
-		localProxy: true,
-		tunnel:     true,
-	}
-	if err := maybeDatabaseLogin(cf, tc, profile, dbInfo, requires); err != nil {
-		return trace.Wrap(err)
-	}
-	cert, err := loadDBCertificate(tc, dbInfo.ServiceName)
+
+	dbs, err := listAvailableMCPDatatabases(cf, tc, profile, routes)
 	if err != nil {
 		return trace.Wrap(err)
 	}
 
-	in, out := net.Pipe()
-
-	listener := listenerutils.NewSingleUseListener(out)
-	defer listener.Close()
-
-	lp, err := alpnproxy.NewLocalProxy(
-		makeBasicLocalProxyConfig(cf.Context, tc, listener, tc.InsecureSkipVerify),
-		alpnproxy.WithDatabaseProtocol(dbInfo.Protocol),
-		alpnproxy.WithClientCert(cert),
-		alpnproxy.WithClusterCAsIfConnUpgrade(cf.Context, tc.RootClusterCACertPool),
-	)
-	if err != nil {
-		return trace.Wrap(err)
-	}
-	go func() {
-		defer lp.Close()
-		if err = lp.Start(cf.Context); err != nil {
-			logger.ErrorContext(cf.Context, "Failed to start local ALPN proxy", "error", err)
+	var mcpInfo []pgmcp.DBInfo
+	for _, db := range dbs {
+		// skip non-postgres databases
+		if db.info.database.GetProtocol() != defaults.ProtocolPostgres {
+			continue
 		}
-	}()
 
-	serverName := "teleport_database_" + dbInfo.ServiceName
-	mcpServer := server.NewMCPServer(serverName, teleport.Version)
+		in, out := net.Pipe()
+		listener := listenerutils.NewSingleUseListener(out)
+		defer listener.Close()
+
+		cc := client.NewDBCertChecker(tc, db.info.RouteToDatabase, nil, client.WithTTL(time.Duration(cf.MinsToLive)*time.Minute))
+
+		lp, err := alpnproxy.NewLocalProxy(
+			makeBasicLocalProxyConfig(cf.Context, tc, listener, tc.InsecureSkipVerify),
+			alpnproxy.WithDatabaseProtocol(db.info.Protocol),
+			// alpnproxy.WithClientCert(db.cert),
+			alpnproxy.WithMiddleware(cc),
+			alpnproxy.WithClusterCAsIfConnUpgrade(cf.Context, tc.RootClusterCACertPool),
+		)
+		if err != nil {
+			return trace.Wrap(err)
+		}
+		go func() {
+			defer lp.Close()
+			if err = lp.Start(cf.Context); err != nil {
+				logger.ErrorContext(cf.Context, "Failed to start local ALPN proxy", "error", err)
+			}
+		}()
+
+		mcpInfo = append(mcpInfo, pgmcp.DBInfo{
+			RawConn:     in,
+			Route:       client.RouteToDatabaseToProto(db.info.RouteToDatabase),
+			Description: db.info.database.GetDescription(),
+		})
+	}
+
+	mcpServer := server.NewMCPServer("teleport_databases", teleport.Version)
 	sess, err := pgmcp.NewSession(cf.Context, pgmcp.NewSessionConfig{
-		MCPServer: mcpServer,
-		RawDBConn: in,
-		Route:     client.RouteToDatabaseToProto(dbInfo.RouteToDatabase),
+		MCPServer:   mcpServer,
+		Datatabases: mcpInfo,
 	})
 	if err != nil {
 		return trace.Wrap(err)
@@ -103,6 +111,77 @@ func onMCPStartDB(cf *CLIConf) error {
 	return trace.Wrap(
 		server.NewStdioServer(mcpServer).Listen(cf.Context, cf.Stdin(), cf.Stdout()),
 	)
+}
+
+type mcpDb struct {
+	info  *databaseInfo
+	users *dbUsers
+	in    net.Conn
+	route clientproto.RouteToDatabase
+}
+
+func listAvailableMCPDatatabases(cf *CLIConf, tc *client.TeleportClient, profile *client.ProfileStatus, routes []tlsca.RouteToDatabase) ([]mcpDb, error) {
+	var clusterClient *client.ClusterClient
+	err := client.RetryWithRelogin(cf.Context, tc, func() error {
+		var err error
+		clusterClient, err = tc.ConnectToCluster(cf.Context)
+		return trace.Wrap(err)
+	})
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	defer clusterClient.Close()
+
+	// list available databases
+	servers, err := apiclient.GetAllResources[types.DatabaseServer](cf.Context, clusterClient.AuthClient, tc.ResourceFilter(types.KindDatabaseServer))
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	accessChecker, err := services.NewAccessCheckerForRemoteCluster(cf.Context, profile.AccessInfo(), tc.SiteName, clusterClient.AuthClient)
+	if err != nil {
+		logger.DebugContext(cf.Context, "Failed to fetch user roles", "error", err)
+	}
+
+	databases := types.DatabaseServers(servers).ToDatabases()
+	sort.Sort(types.Databases(databases))
+
+	var dbs []mcpDb
+	for _, db := range databases {
+		dbInfo := &databaseInfo{
+			database: db,
+			RouteToDatabase: tlsca.RouteToDatabase{
+				ServiceName: db.GetName(),
+				Protocol:    db.GetProtocol(),
+			},
+		}
+		// check for an active route now that we have the full db name.
+		if route, ok := findActiveDatabase(db.GetName(), routes); ok {
+			dbInfo.RouteToDatabase = route
+			dbInfo.isActive = true
+		}
+
+		users := getDBUsers(db, accessChecker)
+		// TODO better handle this
+		if len(users.Allowed) < 1 || users.Allowed[0] == "*" {
+			return nil, trace.BadParameter("could not take a database username from %s", strings.Join(users.Allowed, ","))
+		}
+		// manually fill the user info
+		dbInfo.RouteToDatabase.Username = users.Allowed[0]
+		// TODO make it dynamic
+		// TODO grab this from the list of available (allowed) databases
+		dbInfo.RouteToDatabase.Database = "postgres"
+
+		// TODO update check and set to deal with this
+		// if err := dbInfo.checkAndSetDefaults(cf, tc); err != nil {
+		// 	return nil, trace.Wrap(err)
+		// }
+
+		dbs = append(dbs, mcpDb{info: dbInfo})
+	}
+
+	return dbs, nil
+
 }
 
 func onGenericMCPServerOnProxy(cf *CLIConf) error {
