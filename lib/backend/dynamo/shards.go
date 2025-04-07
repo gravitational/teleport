ec76a1ade9b2ba6c3ef2e0a4b8c59632ac3ab4b3
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2025-04-07 22:43:04 +0200

    Wait a full second between refreshShards calls

--- lib/backend/dynamo/shards.go
+++ lib/backend/dynamo/shards.go
@@ -150,8 +150,8 @@ func (b *Backend) pollStreams(externalCtx context.Context) error {
 	b.buf.SetInit()
 	defer b.buf.Reset()
 
-	ticker := time.NewTicker(b.PollStreamPeriod)
-	defer ticker.Stop()
+	timer := time.NewTimer(b.PollStreamPeriod)
+	defer timer.Stop()
 
 	for {
 		select {
@@ -172,10 +172,11 @@ func (b *Backend) pollStreams(externalCtx context.Context) error {
 			} else {
 				b.buf.Emit(event.events...)
 			}
-		case <-ticker.C:
+		case <-timer.C:
 			if err := refreshShards(false); err != nil {
 				return trace.Wrap(err)
 			}
+			timer.Reset(b.PollStreamPeriod)
 		case <-ctx.Done():
 			b.Tracef("Context is closing, returning.")
 			return nil
