ea827896eea87c423d25719f99af43a90bfb212c
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2025-04-10 15:47:45 -0400

    chore: Bump Go to 1.23.8 (#53920)

--- build.assets/versions.mk
+++ build.assets/versions.mk
@@ -3,7 +3,7 @@
 # Keep versions in sync with devbox.json, when applicable.
 
 # Sync with devbox.json.
-GOLANG_VERSION ?= go1.23.7
+GOLANG_VERSION ?= go1.23.8
 GOLANGCI_LINT_VERSION ?= v1.64.7
 
 NODE_VERSION ?= 20.18.0
