2dd67134ce692b5217881798815db989847ddff1
Sakshyam Shah <sshah@goteleport.com>
2025-04-11 13:41:44 -0400

    [v17] Web UI: support SAML app launch URLs (#53256)
    * feat: MenuLoginWithActionMenu component
    
    * update MenuLogin to accomodate changes for MenuLoginWithActionMenu
    
    * handle samlAppLaunchUrls in makeapp.ts
    
    * update saml app login button
    
    * update app.go to return SAMLAppLaunchURLs
    
    * resource action button:
    - added test and story
    - handles both action and non-action buttons
    
    * launchExternalUrl props for MenuLogin to open url with <a> tag
    
    * handle memu login onClick to apply onClose callback
    
    * use same comment over be and fe, app_test.go
    
    * add isExternalUrl to LoginItem type instead of MenuLoginProps
    
    * use text-align:left, update story
    
    * proper js comment tag
    
    * extend MenuInputType with NONE value
    
    * update resourceActionButton story
    
    * address review comments
    
    * remove unused waitfor

--- web/packages/teleport/src/services/apps/types.ts
+++ web/packages/teleport/src/services/apps/types.ts
@@ -59,6 +59,11 @@ export interface App {
    * aws_ic_account.
    */
   permissionSets?: PermissionSet[];
+  /**
+   * SamlAppLaunchUrl contains service provider specific authentication
+   * endpoints where user should be launched to start SAML authentication.
+   */
+  samlAppLaunchUrls?: SamlAppLaunchUrl[];
 }
 
 export type UserGroupAndDescription = {
@@ -82,4 +87,15 @@ export type PermissionSet = {
   arn: string;
   /** assignmentId is an account assignment ID. */
   assignmentId: string;
+};
+
+/**
+ * SamlAppLaunchUrl contains service provider specific authentication
+ * endpoint where user should be launched to start SAML authentication.
+ */
+export type SamlAppLaunchUrl = {
+  /* launch URL. */
+  url: string;
+  /* friendly name of the URL. */
+  friendlyName?: string;
 };
