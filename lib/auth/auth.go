8f3b3767429b4a63080953db4db1b4c493d5edec
Stephen Levine <stephen.levine@goteleport.com>
2025-04-15 12:49:08 -0400

    set cmc to mon-thu (#53766)

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -1276,6 +1276,7 @@ func (a *Server) syncUpgradeWindowStartHour(ctx context.Context) error {
 	agentWindow, _ := cmc.GetAgentUpgradeWindow()
 
 	agentWindow.UTCStartHour = uint32(startHour)
+	agentWindow.Weekdays = []string{"Mon", "Tue", "Wed", "Thu"}
 
 	cmc.SetAgentUpgradeWindow(agentWindow)
 
