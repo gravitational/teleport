2dd19107a347391765a19eaf3c1a41cadff6c740
Maxim Dietz <maxim.dietz@goteleport.com>
2025-04-15 17:56:34 -0400

    fix: More specific Okta Integration status errors
    - Add more specific statusCode and errors for Okta Integration error states

--- web/packages/teleport/src/services/integrations/types.ts
+++ web/packages/teleport/src/services/integrations/types.ts
@@ -148,6 +148,7 @@ export enum IntegrationStatusCode {
   OtherError = 2,
   Unauthorized = 3,
   SlackNotInChannel = 10,
+  OktaConfigError = 20,
   Draft = 100,
 }
 
@@ -159,6 +160,8 @@ export function getStatusCodeTitle(code: IntegrationStatusCode): string {
       return 'Running';
     case IntegrationStatusCode.Unauthorized:
       return 'Unauthorized';
+    case IntegrationStatusCode.OktaConfigError:
+      return 'Configuration Error';
     case IntegrationStatusCode.SlackNotInChannel:
       return 'Bot not invited to channel';
     case IntegrationStatusCode.Draft:
@@ -169,12 +172,14 @@ export function getStatusCodeTitle(code: IntegrationStatusCode): string {
 }
 
 export function getStatusCodeDescription(
-  code: IntegrationStatusCode
+  code: IntegrationStatusCode,
+  msg?: string
 ): string | null {
   switch (code) {
     case IntegrationStatusCode.Unauthorized:
       return 'The integration was denied access. This could be a result of revoked authorization on the 3rd party provider. Try removing and re-connecting the integration.';
-
+    case IntegrationStatusCode.OktaConfigError:
+      return `There was an error with the integration's configuration.${msg ? ` ${msg}` : ''}`;
     case IntegrationStatusCode.SlackNotInChannel:
       return 'The Slack integration must be invited to the default channel in order to receive access request notifications.';
     default:
