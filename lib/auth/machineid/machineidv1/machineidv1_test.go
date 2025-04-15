70c4e3ab16e1952e68b9e022f60644dd091ca2d5
Tim Buckley <tim@goteleport.com>
2025-04-15 15:28:59 -0600

    MWI: Extend max bot TTL to 7 days (#53893)
    * MWI: Extend max bot TTL to 7 days
    
    This change extends the maximum allowed lifetime for bot certificates
    to 7 days, up from the default maximum value of 24 hours. The default
    values (1 hour TTL, 20 minute renewal) remain unchanged.
    
    This somewhat circumvents the cluster's session TTL limit of 30
    hours, however this limit is soft and can be overridden via roles
    that specify an alternative MaxSessionTTL. This change takes
    advantage of this behavior by setting this longer MaxSessionTTL on
    the auto-generated bot roles, raising it from the previous default
    of 12 hours.
    
    As a result of this implementation detail, existing bots will not be
    affected by this change, and will keep whichever MaxSessionTTL they
    were created with, unless changed manually. Additionally, this can be
    overridden with assigned bot roles as desired.
    
    * Make max_session_ttl configurable via Bot resource
    
    * Fix tests; fix inclusion of field in generated bot specs; fix update
    
    * Update operator CRDs
    
    * Attempt to fix integration test
    
    * Update docstring with sane units
    
    * Update crdgen test data
    
    * Fix failing tests by enabling UseProtoNames
    
    Also fixes this in the TeleportWorkloadIdentityV1 resource, which was
    affected by the same problem.

--- lib/auth/machineid/machineidv1/machineidv1_test.go
+++ lib/auth/machineid/machineidv1/machineidv1_test.go
@@ -32,6 +32,7 @@ import (
 	"github.com/jonboulle/clockwork"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/testing/protocmp"
+	"google.golang.org/protobuf/types/known/durationpb"
 	"google.golang.org/protobuf/types/known/fieldmaskpb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
@@ -42,6 +43,7 @@ import (
 	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/lib/auth"
 	"github.com/gravitational/teleport/lib/auth/machineid/machineidv1"
+	libdefaults "github.com/gravitational/teleport/lib/defaults"
 	"github.com/gravitational/teleport/lib/events/eventstest"
 	"github.com/gravitational/teleport/lib/modules"
 )
@@ -146,6 +148,8 @@ func TestCreateBot(t *testing.T) {
 								Values: []string{},
 							},
 						},
+						// Note: Deliberately omitting MaxSessionTtl here to verify
+						// the default value.
 					},
 				},
 			},
@@ -169,6 +173,7 @@ func TestCreateBot(t *testing.T) {
 							Values: []string{"root"},
 						},
 					},
+					MaxSessionTtl: durationpb.New(libdefaults.DefaultBotMaxSessionTTL),
 				},
 				Status: &machineidv1pb.BotStatus{
 					UserName: "bot-success",
@@ -214,7 +219,7 @@ func TestCreateBot(t *testing.T) {
 				},
 				Spec: types.RoleSpecV6{
 					Options: types.RoleOptions{
-						MaxSessionTTL: types.Duration(12 * time.Hour),
+						MaxSessionTTL: types.Duration(libdefaults.DefaultBotMaxSessionTTL),
 					},
 					Allow: types.RoleConditions{
 						Impersonate: &types.ImpersonateConditions{
@@ -257,6 +262,8 @@ func TestCreateBot(t *testing.T) {
 								Values: []string{},
 							},
 						},
+						// Note: Deliberately omitting MaxSessionTtl here to
+						// validate the default value.
 					},
 				},
 			},
@@ -281,6 +288,7 @@ func TestCreateBot(t *testing.T) {
 							Values: []string{"root"},
 						},
 					},
+					MaxSessionTtl: durationpb.New(libdefaults.DefaultBotMaxSessionTTL),
 				},
 				Status: &machineidv1pb.BotStatus{
 					UserName: "bot-success-with-expiry",
@@ -328,7 +336,93 @@ func TestCreateBot(t *testing.T) {
 				},
 				Spec: types.RoleSpecV6{
 					Options: types.RoleOptions{
-						MaxSessionTTL: types.Duration(12 * time.Hour),
+						MaxSessionTTL: types.Duration(libdefaults.DefaultBotMaxSessionTTL),
+					},
+					Allow: types.RoleConditions{
+						Impersonate: &types.ImpersonateConditions{
+							Roles: []string{testRole.GetName()},
+						},
+						Rules: []types.Rule{
+							types.NewRule(
+								types.KindCertAuthority,
+								[]string{types.VerbReadNoSecrets},
+							),
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "success with max ttl",
+			user: botCreator.GetName(),
+			req: &machineidv1pb.CreateBotRequest{
+				Bot: &machineidv1pb.Bot{
+					Metadata: &headerv1.Metadata{
+						Name: "success-with-max-ttl",
+						Labels: map[string]string{
+							"my-label":       "my-value",
+							"my-other-label": "my-other-value",
+						},
+						Expires: timestamppb.New(expiry),
+					},
+					Spec: &machineidv1pb.BotSpec{
+						Roles: []string{testRole.GetName()},
+						Traits: []*machineidv1pb.Trait{
+							{
+								Name:   constants.TraitLogins,
+								Values: []string{"root"},
+							},
+							{
+								Name:   constants.TraitKubeUsers,
+								Values: []string{},
+							},
+						},
+						MaxSessionTtl: durationpb.New(libdefaults.MaxRenewableCertTTL),
+					},
+				},
+			},
+			assertError: require.NoError,
+			want: &machineidv1pb.Bot{
+				Kind:    types.KindBot,
+				Version: types.V1,
+				Metadata: &headerv1.Metadata{
+					Name: "success-with-max-ttl",
+					Labels: map[string]string{
+						"my-label":       "my-value",
+						"my-other-label": "my-other-value",
+					},
+					Expires: timestamppb.New(expiry),
+				},
+				Spec: &machineidv1pb.BotSpec{
+					Roles: []string{testRole.GetName()},
+					Traits: []*machineidv1pb.Trait{
+						{
+							Name:   constants.TraitLogins,
+							Values: []string{"root"},
+						},
+					},
+					MaxSessionTtl: durationpb.New(libdefaults.MaxRenewableCertTTL),
+				},
+				Status: &machineidv1pb.BotStatus{
+					UserName: "bot-success-with-max-ttl",
+					RoleName: "bot-success-with-max-ttl",
+				},
+			},
+			wantRole: &types.RoleV6{
+				Kind:    types.KindRole,
+				Version: types.V7,
+				Metadata: types.Metadata{
+					Name:      "bot-success-with-max-ttl",
+					Namespace: defaults.Namespace,
+					Labels: map[string]string{
+						types.BotLabel: "success-with-max-ttl",
+					},
+					Description: "Automatically generated role for bot success-with-max-ttl",
+					Expires:     &expiry,
+				},
+				Spec: types.RoleSpecV6{
+					Options: types.RoleOptions{
+						MaxSessionTTL: types.Duration(libdefaults.MaxRenewableCertTTL),
 					},
 					Allow: types.RoleConditions{
 						Impersonate: &types.ImpersonateConditions{
@@ -579,10 +673,11 @@ func TestUpdateBot(t *testing.T) {
 								},
 							},
 						},
+						MaxSessionTtl: durationpb.New(libdefaults.MaxRenewableCertTTL),
 					},
 				},
 				UpdateMask: &fieldmaskpb.FieldMask{
-					Paths: []string{"spec.roles", "spec.traits"},
+					Paths: []string{"spec.roles", "spec.traits", "spec.max_session_ttl"},
 				},
 			},
 
@@ -607,6 +702,7 @@ func TestUpdateBot(t *testing.T) {
 							},
 						},
 					},
+					MaxSessionTtl: durationpb.New(libdefaults.MaxRenewableCertTTL),
 				},
 				Status: &machineidv1pb.BotStatus{
 					UserName: preExistingBot.Status.UserName,
@@ -653,7 +749,7 @@ func TestUpdateBot(t *testing.T) {
 				},
 				Spec: types.RoleSpecV6{
 					Options: types.RoleOptions{
-						MaxSessionTTL: types.Duration(12 * time.Hour),
+						MaxSessionTTL: types.Duration(libdefaults.MaxRenewableCertTTL),
 					},
 					Allow: types.RoleConditions{
 						Impersonate: &types.ImpersonateConditions{
@@ -977,6 +1073,7 @@ func TestUpsertBot(t *testing.T) {
 							Values: []string{"root"},
 						},
 					},
+					MaxSessionTtl: durationpb.New(libdefaults.DefaultBotMaxSessionTTL),
 				},
 				Status: &machineidv1pb.BotStatus{
 					UserName: "bot-new",
@@ -1019,7 +1116,7 @@ func TestUpsertBot(t *testing.T) {
 				},
 				Spec: types.RoleSpecV6{
 					Options: types.RoleOptions{
-						MaxSessionTTL: types.Duration(12 * time.Hour),
+						MaxSessionTTL: types.Duration(libdefaults.DefaultBotMaxSessionTTL),
 					},
 					Allow: types.RoleConditions{
 						Impersonate: &types.ImpersonateConditions{
@@ -1079,6 +1176,7 @@ func TestUpsertBot(t *testing.T) {
 							Values: []string{"root"},
 						},
 					},
+					MaxSessionTtl: durationpb.New(libdefaults.DefaultBotMaxSessionTTL),
 				},
 				Status: &machineidv1pb.BotStatus{
 					UserName: "bot-new-with-expiry",
@@ -1123,7 +1221,7 @@ func TestUpsertBot(t *testing.T) {
 				},
 				Spec: types.RoleSpecV6{
 					Options: types.RoleOptions{
-						MaxSessionTTL: types.Duration(12 * time.Hour),
+						MaxSessionTTL: types.Duration(libdefaults.DefaultBotMaxSessionTTL),
 					},
 					Allow: types.RoleConditions{
 						Impersonate: &types.ImpersonateConditions{
@@ -1179,7 +1277,96 @@ func TestUpsertBot(t *testing.T) {
 				},
 				Spec: types.RoleSpecV6{
 					Options: types.RoleOptions{
-						MaxSessionTTL: types.Duration(12 * time.Hour),
+						MaxSessionTTL: types.Duration(libdefaults.DefaultBotMaxSessionTTL),
+					},
+					Allow: types.RoleConditions{
+						Impersonate: &types.ImpersonateConditions{
+							Roles: []string{testRole.GetName()},
+						},
+						Rules: []types.Rule{
+							types.NewRule(
+								types.KindCertAuthority,
+								[]string{types.VerbReadNoSecrets},
+							),
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "already exists with max session ttl",
+			user: botCreator.GetName(),
+			req: &machineidv1pb.UpsertBotRequest{
+				Bot: &machineidv1pb.Bot{
+					Metadata: &headerv1.Metadata{
+						Name: "pre-existing",
+						Labels: map[string]string{
+							"my-label":       "my-value",
+							"my-other-label": "my-other-value",
+						},
+					},
+					Spec: &machineidv1pb.BotSpec{
+						Roles:         []string{testRole.GetName()},
+						MaxSessionTtl: durationpb.New(libdefaults.MaxRenewableCertTTL),
+					},
+				},
+			},
+
+			assertError: require.NoError,
+			want: &machineidv1pb.Bot{
+				Kind:    types.KindBot,
+				Version: types.V1,
+				Metadata: &headerv1.Metadata{
+					Name: "pre-existing",
+					Labels: map[string]string{
+						"my-label":       "my-value",
+						"my-other-label": "my-other-value",
+					},
+				},
+				Spec: &machineidv1pb.BotSpec{
+					Roles:         []string{testRole.GetName()},
+					MaxSessionTtl: durationpb.New(libdefaults.MaxRenewableCertTTL),
+				},
+				Status: &machineidv1pb.BotStatus{
+					UserName: "bot-pre-existing",
+					RoleName: "bot-pre-existing",
+				},
+			},
+			wantUser: &types.UserV2{
+				Kind:    types.KindUser,
+				Version: types.V2,
+				Metadata: types.Metadata{
+					Name:      "bot-pre-existing",
+					Namespace: defaults.Namespace,
+					Labels: map[string]string{
+						types.BotLabel:           "pre-existing",
+						types.BotGenerationLabel: "1337",
+						"my-label":               "my-value",
+						"my-other-label":         "my-other-value",
+					},
+				},
+				Spec: types.UserSpecV2{
+					CreatedBy: types.CreatedBy{
+						User: types.UserRef{Name: botCreator.GetName()},
+					},
+					Roles:  []string{"bot-pre-existing"},
+					Traits: nil,
+				},
+			},
+			wantRole: &types.RoleV6{
+				Kind:    types.KindRole,
+				Version: types.V7,
+				Metadata: types.Metadata{
+					Name:      "bot-pre-existing",
+					Namespace: defaults.Namespace,
+					Labels: map[string]string{
+						types.BotLabel: "pre-existing",
+					},
+					Description: "Automatically generated role for bot pre-existing",
+				},
+				Spec: types.RoleSpecV6{
+					Options: types.RoleOptions{
+						MaxSessionTTL: types.Duration(libdefaults.MaxRenewableCertTTL),
 					},
 					Allow: types.RoleConditions{
 						Impersonate: &types.ImpersonateConditions{
