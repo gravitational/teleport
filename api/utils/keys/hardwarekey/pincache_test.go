9a72fb1ba848f8afc01b605c976ffd64dbac8c85
joerger <bjoerger@goteleport.com>
2025-04-18 12:25:26 -0700

    Add hardware key PINCache.

---
+++ api/utils/keys/hardwarekey/pincache_test.go
@@ -0,0 +1,73 @@
+// Copyright 2025 Gravitational, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hardwarekey_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/jonboulle/clockwork"
+	"github.com/stretchr/testify/require"
+
+	"github.com/gravitational/teleport/api/utils/keys/hardwarekey"
+)
+
+func TestPINCache(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	clock := clockwork.NewFakeClock()
+	pinCache := hardwarekey.PINCache{Clock: clock}
+
+	testPIN := "123467"
+
+	smallTTL := time.Second
+	mediumTTL := time.Minute
+	largeTTL := time.Hour
+
+	// Set the PIN with the medium TTL.
+	pinCache.SetPIN(testPIN, mediumTTL)
+	require.Equal(t, testPIN, pinCache.GetPIN(smallTTL))
+	require.Equal(t, testPIN, pinCache.GetPIN(mediumTTL))
+	require.Equal(t, testPIN, pinCache.GetPIN(largeTTL))
+
+	// Wait for the cleanup goroutine to block on clock.Sleep.
+	clock.BlockUntilContext(ctx, 1)
+
+	// Advancing by the small TTL should only expire the pin for the small TTL.
+	clock.Advance(smallTTL)
+	require.Zero(t, pinCache.GetPIN(smallTTL))
+	require.Equal(t, testPIN, pinCache.GetPIN(mediumTTL))
+	require.Equal(t, testPIN, pinCache.GetPIN(largeTTL))
+
+	// Setting the PIN with the small TTL should reset the PIN's set-at time.
+	pinCache.SetPIN(testPIN, smallTTL)
+	require.Equal(t, testPIN, pinCache.GetPIN(smallTTL))
+
+	// The expiration time should remain tied to the medium TTL.
+	clock.Advance(2 * smallTTL)
+	require.Equal(t, testPIN, pinCache.GetPIN(mediumTTL))
+
+	// Advancing by the medium TTL, used to set the initial cache, should wipe the PIN cache.
+	clock.Advance(mediumTTL)
+
+	// Wait for the cleanup goroutine to unblock on clock.Sleep.
+	clock.BlockUntilContext(ctx, 0)
+
+	require.Zero(t, pinCache.GetPIN(smallTTL))
+	require.Zero(t, pinCache.GetPIN(mediumTTL))
+	require.Zero(t, pinCache.GetPIN(largeTTL))
+}
