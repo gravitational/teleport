ee0708a8c2d32f3848e7f415e568da8402747abc
joerger <bjoerger@goteleport.com>
2025-04-18 16:07:44 -0700

    Remove unused field.

--- lib/teleterm/daemon/hardwarekeyprompt.go
+++ lib/teleterm/daemon/hardwarekeyprompt.go
@@ -50,8 +50,7 @@ func (s *Service) NewHardwareKeyPrompt() hardwarekey.Prompt {
 }
 
 type hardwareKeyPrompter struct {
-	s        *Service
-	pinCache hardwarekey.PINCache
+	s *Service
 }
 
 // Touch prompts the user to touch the hardware key.
