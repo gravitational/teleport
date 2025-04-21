ad37191c87190853fbd595d51c7f1231d697370d
Andrew Burke <andrew.burke@goteleport.com>
2025-04-21 13:41:03 -0700

    Add cli plumbing

--- tool/tsh/common/tsh.go
+++ tool/tsh/common/tsh.go
@@ -627,6 +627,8 @@ type CLIConf struct {
 	ForkAfterAuthentication bool
 
 	forkSignalFd uint64
+
+	getSSHForkArgs func(cluster, host string, signalFd uintptr) []string
 }
 
 // Stdout returns the stdout writer.
@@ -687,6 +689,42 @@ func (c *CLIConf) PromptConfirmation(question string) error {
 		return trace.Wrap(err)
 	} else if !ok {
 		return trace.Errorf("Operation canceled by user request.")
+	}
+	return nil
+
+}
+func (c *CLIConf) formatSSHForkCommand(parseContext *kingpin.ParseContext) error {
+	c.getSSHForkArgs = func(cluster, host string, signalFd uintptr) []string {
+		args := make([]string, 0, len(parseContext.Elements))
+		for _, elem := range parseContext.Elements {
+			switch clause := elem.Clause.(type) {
+			case *kingpin.CmdClause:
+				args = append(args, "ssh", "--fork-signal-fd", strconv.FormatUint(uint64(signalFd), 10), "--cluster", cluster)
+			case *kingpin.FlagClause:
+				switch clause.Model().Name {
+				case "cluster", "fork-after-authentication":
+					continue
+				default:
+					args = append(args, "--"+clause.Model().Name)
+					if elem.Value != nil {
+						args = append(args, *elem.Value)
+					}
+				}
+			case *kingpin.ArgClause:
+				if clause.Model().Name != "[user@]host" && elem.Value != nil {
+					args = append(args, *elem.Value)
+					continue
+				}
+				userHost := strings.Split(*elem.Value, "@")
+				if len(userHost) > 1 {
+					userHost[1] = host
+				} else {
+					userHost[0] = host
+				}
+				args = append(args, strings.Join(userHost, "@"))
+			}
+		}
+		return args
 	}
 	return nil
 }
@@ -874,7 +912,7 @@ func Run(ctx context.Context, args []string, opts ...CliOption) error {
 		BoolVar(&cf.clientOnlyVersionCheck)
 	// ssh
 	// Use Interspersed(false) to forward all flags to ssh.
-	ssh := app.Command("ssh", "Run shell or execute a command on a remote SSH node.").Interspersed(false)
+	ssh := app.Command("ssh", "Run shell or execute a command on a remote SSH node.").Interspersed(false).Action(cf.formatSSHForkCommand)
 	ssh.Arg("[user@]host", "Remote hostname and the login to use, this argument is required").StringVar(&cf.UserHost)
 	ssh.Arg("command", "Command to execute on a remote host").StringsVar(&cf.RemoteCommand)
 	app.Flag("jumphost", "SSH jumphost").Short('J').StringVar(&cf.ProxyJump)
@@ -4055,9 +4093,9 @@ func onSSH(cf *CLIConf) error {
 			if cf.LocalExec {
 				opts = append(opts, client.WithLocalCommandExecutor(runLocalCommand))
 			}
-			// if cf.ForkAfterAuthentication {
-			// 	opts = append(opts, client.ForkAfterAuthentication())
-			// }
+			if cf.ForkAfterAuthentication {
+				opts = append(opts, client.ForkAfterAuthentication(cf.getSSHForkArgs))
+			}
 
 			return tc.SSH(cf.Context, cf.RemoteCommand, opts...)
 		}
