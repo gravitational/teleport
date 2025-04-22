da9b53507ca9515d2ffbda95ce9b7f23e4467480
Stephen Levine <stephen.levine@goteleport.com>
2025-04-22 11:11:54 -0400

    [teleport-update] Stabilize binary paths in generated tbot config (#54145) (#54179)
    * Fix tbot configuration paths by finding stable binary paths
    
    * cleanup
    
    * require path in update.yaml
    
    * add warnings
    
    * more cleanup
    
    * import cleanup
    
    * clarify logic
    
    * remove duplicate test case
    
    * feedback
    
    * Fix additional tbot path references
    
    * fix windows
    
    * correct log

--- lib/tbot/service_kubernetes_output.go
+++ lib/tbot/service_kubernetes_output.go
@@ -21,6 +21,7 @@ package tbot
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net"
@@ -35,6 +36,7 @@ import (
 	"github.com/gravitational/teleport/api/defaults"
 	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/lib/auth/authclient"
+	autoupdate "github.com/gravitational/teleport/lib/autoupdate/agent"
 	"github.com/gravitational/teleport/lib/client"
 	"github.com/gravitational/teleport/lib/kube/kubeconfig"
 	"github.com/gravitational/teleport/lib/reversetunnelclient"
@@ -262,7 +264,9 @@ func (s *KubernetesOutputService) render(
 		}
 	} else {
 		executablePath, err := s.executablePath()
-		if err != nil {
+		if errors.Is(err, autoupdate.ErrUnstableExecutable) {
+			s.log.WarnContext(ctx, "Kubernetes template will be rendered with an unstable path to the tbot executable. Please reinstall tbot with Managed Updates to prevent instability.")
+		} else if err != nil {
 			return trace.Wrap(err)
 		}
 
