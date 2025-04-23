835eed92d249d04cf41e3044da2f2f20cc07673e
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2025-04-23 16:46:41 +0200

    Remove string parsing for min openat2 kernel version

--- lib/tbot/botfs/fs_linux.go
+++ lib/tbot/botfs/fs_linux.go
@@ -42,10 +42,6 @@ import (
 	"github.com/gravitational/teleport/lib/utils"
 )
 
-// Openat2MinKernel is the kernel release that adds support for the openat2()
-// syscall.
-const Openat2MinKernel = "5.6.0"
-
 // mostACLRead is a permission mode granting readonly access to a file.
 const modeACLRead fs.FileMode = 04
 
@@ -673,7 +669,6 @@ func HasACLSupport() bool {
 // We've encountered this being incorrect in environments where access to the
 // kernel is hampered e.g. seccomp/apparmor/container runtimes.
 func HasSecureWriteSupport() bool {
-	minKernel := semver.New(Openat2MinKernel)
 	version, err := utils.KernelVersion()
 	if err != nil {
 		log.InfoContext(
@@ -683,7 +678,8 @@ func HasSecureWriteSupport() bool {
 		)
 		return false
 	}
-	if version.LessThan(*minKernel) {
+	// kernel release that added support for the openat2() syscall
+	if version.LessThan(semver.Version{Major: 5, Minor: 6, Patch: 0}) {
 		return false
 	}
 
