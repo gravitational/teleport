9da6eb6c93b01d52fa2dda6b2047f518599beaa6
Tim Ross <tim.ross@goteleport.com>
2025-04-29 13:42:39 -0400

    Convert access lists to new in memory cache collection
    Moves access lists, access list members and access list reviews
    to the new cache collection scheme that was introduced in #52210.
    No additional functionality changes have been made here. This should
    be a purely mechanical translation to the new internal caching machinery.

--- api/types/accesslist/member.go
+++ api/types/accesslist/member.go
@@ -135,3 +135,10 @@ func (a *AccessListMember) MatchSearch(values []string) bool {
 	fieldVals := append(utils.MapToStrings(a.GetAllLabels()), a.GetName())
 	return types.MatchSearch(fieldVals, values, nil)
 }
+
+// Clone returns a copy of the member.
+func (a *AccessListMember) Clone() *AccessListMember {
+	var copy *AccessListMember
+	utils.StrictObjectToStruct(a, &copy)
+	return copy
+}
