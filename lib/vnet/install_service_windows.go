5c63c87e81f82eebd9bf5fbda86ab6bea177c5f6
Rafał Cieślak <rafal.cieslak@goteleport.com>
2025-04-29 16:06:37 +0200

    Log service version during (un)installation

--- lib/vnet/install_service_windows.go
+++ lib/vnet/install_service_windows.go
@@ -19,14 +19,17 @@ package vnet
 import (
 	"context"
 	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/gravitational/trace"
 	"golang.org/x/sys/windows"
+	"golang.org/x/sys/windows/svc/eventlog"
 	"golang.org/x/sys/windows/svc/mgr"
 
+	"github.com/gravitational/teleport"
 	eventlogutils "github.com/gravitational/teleport/lib/utils/log/eventlog"
 )
 
@@ -79,6 +82,9 @@ func InstallService(ctx context.Context) (err error) {
 	if err := installEventSource(); err != nil {
 		trace.Wrap(err, "creating event source for logging")
 	}
+	if err := logInstallationEvent("VNet service installed"); err != nil {
+		trace.Wrap(err, "logging installation event")
+	}
 	return nil
 }
 
@@ -99,6 +105,9 @@ func UninstallService(ctx context.Context) (err error) {
 		return trace.Wrap(err, "closing VNet Windows service")
 	}
 
+	if err := logInstallationEvent("VNet service uninstalled"); err != nil {
+		trace.Wrap(err, "logging installation event")
+	}
 	if err := eventlogutils.Remove(eventlogutils.LogName, eventSource); err != nil {
 		return trace.Wrap(err, "removing event source for logging")
 	}
@@ -211,4 +220,17 @@ func installEventSource() error {
 	// they might display a message about missing event ID until the user reinstalls the app.
 	err = eventlogutils.Install(eventlogutils.LogName, eventSource, msgFilePath, false /* useExpandKey */)
 	return trace.Wrap(err)
+}
+
+func logInstallationEvent(eventMessage string) error {
+	log, err := eventlog.Open(eventSource)
+	if err != nil {
+		return trace.Wrap(err, "opening logger")
+	}
+
+	if err := log.Info(eventlogutils.EventID, fmt.Sprintf("%s version:%s", eventMessage, teleport.Version)); err != nil {
+		return trace.Wrap(err, "writing log message")
+	}
+
+	return trace.Wrap(log.Close(), "closing logger")
 }
