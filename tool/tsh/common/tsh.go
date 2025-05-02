d12007752795f6d778c09388e813d69daa3db144
Paul Gottschling <paul.gottschling@goteleport.com>
2025-05-02 14:37:19 -0400

    Respond to zmb3 and atburke feedback
    **Separate docs generation code from production code.**
    
    Add a separate implementation of `UpdateAppUsageTemplate` that requires
    the `docs` build tag. Since all Go-based Teleport CLI tools call
    `UpdateAppUsageTemplate`, the `docs` build of the function ensure that
    the `help` command for Teleport CLI tools prints a docs page.
    
    This change also adds a `cli-docs-tsh` make target to generate a docs
    page for `tsh`.
    
    **Escape pipes in `formatHelp`** to avoid breaking table cells.

--- tool/tsh/common/tsh.go
+++ tool/tsh/common/tsh.go
@@ -1319,8 +1319,6 @@ func Run(ctx context.Context, args []string, opts ...CliOption) error {
 		puttyConfig.Hidden()
 	}
 
-	printDocs := app.Command("docs", "Print CLI documentation").Hidden()
-
 	// FIDO2, TouchID and WebAuthnWin commands.
 	f2 := fido2.NewCommand(app)
 	tid := touchid.NewCommand(app)
@@ -1747,8 +1745,6 @@ func Run(ctx context.Context, args []string, opts ...CliOption) error {
 		err = gitCmd.clone.run(&cf)
 	case pivCmd.agent.FullCommand():
 		err = pivCmd.agent.run(&cf)
-	case printDocs.FullCommand():
-		utils.PrintCLIDocs(os.Stdout, app)
 	default:
 		// Handle commands that might not be available.
 		switch {
