360d4456d0b4be16b9690262f56a0e4290a726d3
Grzegorz Zdunek <gzdunek@users.noreply.github.com>
2025-05-07 15:40:34 +0200

    [v17] Convert desktop session dialogs to inline messages (#54587)
    * Convert desktop session dialogs to inline messages (#54127)
    
    * Allow clearing the MFA attempt state
    
    * Shutdown the websocket connection if the user cancels an MFA prompt
    
    * Properly handle situations when abort signal is aborted before we open the websocket
    
    * Reconnect the client instead of refreshing the page
    
    * Replace "error" status with "disconnected", make it more capable
    
    * Propagate errors from the `TdpClient`
    
    * Convert dialogs to inline messages
    
    * Increase the gap slightly
    
    * Revert `.off()` to `.removeListener()` change
    
    (cherry picked from commit 921eecaddf2dc555a1c80118b2a0cd084d46b39a)
    
    * Make `jest-canvas-mock` available in all packages

--- web/packages/shared/components/DesktopSession/index.ts
+++ web/packages/shared/components/DesktopSession/index.ts
@@ -16,4 +16,4 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-export { DesktopSession, AlertDialog } from './DesktopSession';
+export { DesktopSession, DisconnectedState } from './DesktopSession';
