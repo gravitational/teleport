0e77641d8b811b194dae5cbddb9a61ecf0376d61
Marco Dinis <marco.dinis@goteleport.com>
2025-05-08 14:36:19 +0100

    AWS Web/Console Access using IAM Roles Anywhere Integration
    This PR adds support for AWS Web/Console Access using the IAM Roles
    Anywhere Integration.
    
    When trying to access an AWS App which has an associated:
    - integration of AWS RA kind
    - Roles Anywhere metadata: profile ARN
    - IAM Role
    
    There is a new flow which generates credentials using the:
    - trust anchor present in the integration metadata
    - profile arn present in the AppServer metadata
    - target IAM Role
    - and a X.509 certificate generated from the AWS Roles Anywhere CA
    
    Those credentials are then used to generate the signing URL using the
    federation service.

--- lib/service/service.go
+++ lib/service/service.go
@@ -4893,6 +4893,7 @@ func (process *TeleportProcess) initProxyEndpoint(conn *Connector) error {
 			ServiceComponent:  teleport.ComponentWebProxy,
 			AWSConfigOptions: []awsconfig.OptionsFn{
 				awsconfig.WithOIDCIntegrationClient(conn.Client),
+				awsconfig.WithRolesAnywhereIntegrationClient(conn.Client),
 			},
 		})
 		if err != nil {
