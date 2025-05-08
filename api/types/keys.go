cefe0d583ea8ed5dd141db0af26e835076ad7b22
Erik Tate <erik.tate@goteleport.com>
2025-05-08 11:44:59 -0400

    adding missing license and reworking EncryptOAEP helper to work on EncryptionKeys instead of EncryptionKeyPairs

--- api/types/keys.go
+++ api/types/keys.go
@@ -1,3 +1,17 @@
+// Copyright 2025 Gravitational, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package types
 
 import (
@@ -10,15 +24,26 @@ import (
 	"github.com/gravitational/teleport/api/utils/keys"
 )
 
-func (p *EncryptionKeyPair) EncryptOAEP(plaintext []byte) ([]byte, error) {
-	pub, err := keys.ParsePublicKey(p.PublicKey)
+// EncryptionKey returns the public portion of an EncryptionKeyPair to be
+// used in asymmetric encryption.
+func (p EncryptionKeyPair) EncryptionKey() EncryptionKey {
+	return EncryptionKey{
+		PublicKey: p.PublicKey,
+		Hash:      p.Hash,
+	}
+}
+
+// EncryptOAEP encrypts data using OAEP with the public key and hash present
+// in the EncryptionKey receiver.
+func (k EncryptionKey) EncryptOAEP(plaintext []byte) ([]byte, error) {
+	pub, err := keys.ParsePublicKey(k.PublicKey)
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
 
 	hash := crypto.SHA256
-	if p.Hash > 0 {
-		hash = crypto.Hash(p.Hash)
+	if k.Hash > 0 {
+		hash = crypto.Hash(k.Hash)
 	}
 	switch pubKey := pub.(type) {
 	case *rsa.PublicKey:
