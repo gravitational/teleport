9eef795d87053e00322a8f02e758311b8b1e1841
Gus Rivera <gus.rivera@goteleport.com>
2025-05-09 17:57:18 -0500

    Testing with hardcoded

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.0.0-dev"
+const Version = "18.0.0-dev.gus-ew2"
 
 const VersionMajor = 18
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev"
+const VersionPreRelease = "dev.gus-ew2"
 const VersionMetadata = ""
