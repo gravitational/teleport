f88ff0a9ca36c689b8d6d35e71754b0a5e6bdcb8
STeve (Xin) Huang <huangin52@gmail.com>
2025-05-09 16:44:13 -0400

    Release v18.0.0-dev.steve.mcp.6

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.0.0-dev.steve.mcp.5"
+const Version = "18.0.0-dev.steve.mcp.6"
 
 const VersionMajor = 18
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev.steve.mcp.5"
+const VersionPreRelease = "dev.steve.mcp.6"
 const VersionMetadata = ""
