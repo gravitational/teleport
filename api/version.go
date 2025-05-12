78fb28c06367fdb946651c966a6ea9e0c6a18dc8
Gus Rivera <gus.rivera@goteleport.com>
2025-05-12 11:15:33 -0500

    Trying out v18 build

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.0.0-dev"
+const Version = "18.0.0-dev.gus-prealpha"
 
 const VersionMajor = 18
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev"
+const VersionPreRelease = "dev.gus-prealpha"
 const VersionMetadata = ""
