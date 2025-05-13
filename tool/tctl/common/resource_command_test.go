7076a01ffa049d05a095ff3c9d126e74431859f7
flyinghermit <sshah@goteleport.com>
2025-05-12 22:54:22 -0400

    keep v7 semantics in integration tests

--- tool/tctl/common/resource_command_test.go
+++ tool/tctl/common/resource_command_test.go
@@ -1584,6 +1584,9 @@ spec:
     - command
     - network
     forward_agent: false
+    idp:
+      saml:
+        enabled: true
     max_session_ttl: 30h0m0s
     pin_source_ip: false
     port_forwarding: true
