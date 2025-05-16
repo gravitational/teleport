96eb2e68f6244ca7dfa999448b9c1ba702871cdb
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2025-05-16 13:38:09 +0200

    Support directory sharing in Connect  (#54663)
    * Register directory access when starting a desktop session
    
    * Add RPC to attach a directory to desktop session
    
    * Do not allow `attachDirectoryToDesktopSession` to be called from the renderer process
    
    * Open the directory picker and send the selected path to tshd
    
    * Intercept file system events coming from the server and handle them
    
    * Disallow file system messages to be sent from the renderer
    
    * Refactor dir sharing
    
    * `AttachDirectoryToDesktopSession` -> `SetSharedDirectoryForDesktopSession`
    
    * Improve comments
    
    * Small fixes
    
    * Add missing defer for `s.dirAccessMu.RUnlock()`
    
    * `TestOpenSharedDirectory` -> `TestNewDirectoryAccess`
    
    * Add a comment for JS file system handlers
    
    * `make grpc`
    
    ---------
    
    Co-authored-by: Rafał Cieślak <rafal.cieslak@goteleport.com>
    
    (cherry picked from commit 45428c01e22f8cc6ab2174d3404d2c00c67ebc51)

--- gen/proto/ts/teleport/lib/teleterm/v1/service_pb.grpc-server.ts
+++ gen/proto/ts/teleport/lib/teleterm/v1/service_pb.grpc-server.ts
@@ -21,6 +21,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
+import { SetSharedDirectoryForDesktopSessionResponse } from "./service_pb";
+import { SetSharedDirectoryForDesktopSessionRequest } from "./service_pb";
 import { ConnectToDesktopResponse } from "./service_pb";
 import { ConnectToDesktopRequest } from "./service_pb";
 import { GetAppResponse } from "./service_pb";
@@ -404,6 +406,17 @@ export interface ITerminalService extends grpc.UntypedServiceImplementation {
      * @generated from protobuf rpc: ConnectToDesktop(stream teleport.lib.teleterm.v1.ConnectToDesktopRequest) returns (stream teleport.lib.teleterm.v1.ConnectToDesktopResponse);
      */
     connectToDesktop: grpc.handleBidiStreamingCall<ConnectToDesktopRequest, ConnectToDesktopResponse>;
+    /**
+     * SetSharedDirectoryForDesktopSession opens a directory for a desktop session and enables file system operations for it.
+     * If there is no active desktop session associated with the specified desktop_uri and login,
+     * the RPC returns an error.
+     *
+     * This RPC does not automatically share the directory with the server (it does not send a SharedDirectoryAnnounce message).
+     * It only registers file system handlers for processing file system-related TDP events.
+     *
+     * @generated from protobuf rpc: SetSharedDirectoryForDesktopSession(teleport.lib.teleterm.v1.SetSharedDirectoryForDesktopSessionRequest) returns (teleport.lib.teleterm.v1.SetSharedDirectoryForDesktopSessionResponse);
+     */
+    setSharedDirectoryForDesktopSession: grpc.handleUnaryCall<SetSharedDirectoryForDesktopSessionRequest, SetSharedDirectoryForDesktopSessionResponse>;
 }
 /**
  * @grpc/grpc-js definition for the protobuf service teleport.lib.teleterm.v1.TerminalService.
@@ -836,5 +849,15 @@ export const terminalServiceDefinition: grpc.ServiceDefinition<ITerminalService>
         requestDeserialize: bytes => ConnectToDesktopRequest.fromBinary(bytes),
         responseSerialize: value => Buffer.from(ConnectToDesktopResponse.toBinary(value)),
         requestSerialize: value => Buffer.from(ConnectToDesktopRequest.toBinary(value))
+    },
+    setSharedDirectoryForDesktopSession: {
+        path: "/teleport.lib.teleterm.v1.TerminalService/SetSharedDirectoryForDesktopSession",
+        originalName: "SetSharedDirectoryForDesktopSession",
+        requestStream: false,
+        responseStream: false,
+        responseDeserialize: bytes => SetSharedDirectoryForDesktopSessionResponse.fromBinary(bytes),
+        requestDeserialize: bytes => SetSharedDirectoryForDesktopSessionRequest.fromBinary(bytes),
+        responseSerialize: value => Buffer.from(SetSharedDirectoryForDesktopSessionResponse.toBinary(value)),
+        requestSerialize: value => Buffer.from(SetSharedDirectoryForDesktopSessionRequest.toBinary(value))
     }
 };
