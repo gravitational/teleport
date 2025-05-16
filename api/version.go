f2888bb80925737490c237937494ecbc7fadf5e9
joerger <bjoerger@goteleport.com>
2025-05-15 19:04:24 -0700

    Release v16.5.9-dev.joerger.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "16.5.9"
+const Version = "16.5.9-dev.joerger.1"
 
 const VersionMajor = 16
 const VersionMinor = 5
 const VersionPatch = 9
-const VersionPreRelease = ""
+const VersionPreRelease = "dev.joerger.1"
 const VersionMetadata = ""
