83fc0e1ac933ffb2c67151851b54a84c21f2f6ac
Nick Marais <nicholas.marais@goteleport.com>
2025-05-19 14:37:02 +0100

    Populate page info

--- web/packages/teleport/src/BotInstances/BotInstances.tsx
+++ web/packages/teleport/src/BotInstances/BotInstances.tsx
@@ -19,7 +19,9 @@
 import { Alert } from 'design/Alert/Alert';
 import Box from 'design/Box/Box';
 import { Indicator } from 'design/Indicator/Indicator';
+import { Mark } from 'design/Mark/Mark';
 import {
+  InfoExternalTextLink,
   InfoGuideButton,
   InfoParagraph,
   ReferenceLinks,
@@ -64,21 +66,53 @@ export function BotInstances() {
 const InfoGuide = () => (
   <Box>
     <InfoParagraph>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
+      A{' '}
+      <InfoExternalTextLink
+        target="_blank"
+        href={InfoGuideReferenceLinks.BotInstances.href}
+      >
+        Bot Instance
+      </InfoExternalTextLink>{' '}
+      identifies a single lineage of{' '}
+      <InfoExternalTextLink
+        target="_blank"
+        href={InfoGuideReferenceLinks.Bots.href}
+      >
+        bot
+      </InfoExternalTextLink>{' '}
+      identities, even through certificate renewals and rejoins. When the{' '}
+      <Mark>tbot</Mark> client first authenticates to a cluster, a Bot Instance
+      is generated and its UUID is embedded in the returned client identity.
+    </InfoParagraph>
+    <InfoParagraph>
+      Bot Instances track a variety of information about <Mark>tbot</Mark>{' '}
+      instances, including regular heartbeats which include basic information
+      about the <Mark>tbot</Mark> host, like its architecture and OS version.
+    </InfoParagraph>
+    <InfoParagraph>
+      {' '}
+      Bot Instances have a relatively short lifespan and are set to expire after
+      the most recent identity issued for that instance will expire. If the{' '}
+      <Mark>tbot</Mark> client associated with a particular Bot Instance renews
+      or rejoins, the expiration of the bot instance is reset. This is designed
+      to allow users to list Bot Instances for an accurate view of the number of
+      active <Mark>tbot</Mark> clients interacting with their Teleport cluster.
     </InfoParagraph>
     <ReferenceLinks links={Object.values(InfoGuideReferenceLinks)} />
   </Box>
 );
 
 const InfoGuideReferenceLinks = {
-  Users: {
-    title: 'Teleport Users',
-    href: 'https://goteleport.com/docs/core-concepts/#teleport-users',
+  BotInstances: {
+    title: 'What are Bot instances',
+    href: 'https://goteleport.com/docs/enroll-resources/machine-id/introduction/#bot-instances',
+  },
+  Bots: {
+    title: 'What are Bots',
+    href: 'https://goteleport.com/docs/enroll-resources/machine-id/introduction/#bots',
+  },
+  Tctl: {
+    title: 'Use tctl to manage bot instances',
+    href: 'https://goteleport.com/docs/reference/cli/tctl/#tctl-bots-instances-add',
   },
 };
