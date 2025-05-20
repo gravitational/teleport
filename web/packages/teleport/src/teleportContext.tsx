96cdd9332af774a663f9d230fe03415d07048df6
Bartosz Leper <bartosz.leper@goteleport.com>
2025-05-20 14:13:22 +0200

    Turn on unused-imports/no-unused-imports
    This allows removing unused imports automatically when executing `pnpm lint-fix`.

--- web/packages/teleport/src/teleportContext.tsx
+++ web/packages/teleport/src/teleportContext.tsx
@@ -94,7 +94,7 @@ class TeleportContext implements types.Context {
   // The caller of this function provides the try/catch
   // block.
   // preferences are needed in TeleportContextE, but not in TeleportContext.
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // eslint-disable-next-line unused-imports/no-unused-vars
   async init(preferences: UserPreferences) {
     const user = await userService.fetchUserContext();
     this.storeUser.setState(user);
