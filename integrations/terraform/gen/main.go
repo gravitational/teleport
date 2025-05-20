gravitational/teleport

commit 280e1f5f6f9e16ae954153663455a3ae9101e877

Remove code marked for deletion (#54957)

integrations/terraform/provider/resource_teleport_auth_preference.go
@@ -21,13 +21,15 @@ import (
 	"context"
 	"fmt"
 
-	apitypes "github.com/gravitational/teleport/api/types"
-	"github.com/gravitational/teleport/integrations/lib/backoff"
 	"github.com/gravitational/trace"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/jonboulle/clockwork"
+
+	clusterconfigv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/clusterconfig/v1"
+	apitypes "github.com/gravitational/teleport/api/types"
+	"github.com/gravitational/teleport/integrations/lib/backoff"
 
 	"github.com/gravitational/teleport/integrations/terraform/tfschema"
 )
@@ -78,15 +80,13 @@ func (r resourceTeleportAuthPreference) Create(ctx context.Context, req tfsdk.Cr
 		return
 	}
 
-	
-
 	authPreferenceBefore, err := r.p.Client.GetAuthPreference(ctx)
 	if err != nil && !trace.IsNotFound(err) {
 		resp.Diagnostics.Append(diagFromWrappedErr("Error reading AuthPreference", trace.Wrap(err), "cluster_auth_preference"))
 		return
 	}
 
-	err = r.p.Client.SetAuthPreference(ctx, authPreference)
+	_, err = r.p.Client.ClusterConfigClient().UpsertAuthPreference(ctx, &clusterconfigv1.UpsertAuthPreferenceRequest{AuthPreference: authPreference})
 	if err != nil {
 		resp.Diagnostics.Append(diagFromWrappedErr("Error creating AuthPreference", trace.Wrap(err), "cluster_auth_preference"))
 		return
@@ -162,7 +162,6 @@ func (r resourceTeleportAuthPreference) Read(ctx context.Context, req tfsdk.Read
 		return
 	}
 
-	
 	authPreference := authPreferenceI.(*apitypes.AuthPreferenceV2)
 	diags = tfschema.CopyAuthPreferenceV2ToTerraform(ctx, authPreference, &state)
 	resp.Diagnostics.Append(diags...)
@@ -210,7 +209,7 @@ func (r resourceTeleportAuthPreference) Update(ctx context.Context, req tfsdk.Up
 		return
 	}
 
-	err = r.p.Client.SetAuthPreference(ctx, authPreference)
+	_, err = r.p.Client.ClusterConfigClient().UpsertAuthPreference(ctx, &clusterconfigv1.UpsertAuthPreferenceRequest{AuthPreference: authPreference})
 	if err != nil {
 		resp.Diagnostics.Append(diagFromWrappedErr("Error updating AuthPreference", trace.Wrap(err), "cluster_auth_preference"))
 		return
@@ -244,7 +243,6 @@ func (r resourceTeleportAuthPreference) Update(ctx context.Context, req tfsdk.Up
 		return
 	}
 
-	
 	authPreference = authPreferenceI.(*apitypes.AuthPreferenceV2)
 	diags = tfschema.CopyAuthPreferenceV2ToTerraform(ctx, authPreference, &plan)
 	resp.Diagnostics.Append(diags...)

integrations/terraform/provider/resource_teleport_session_recording_config.go
@@ -21,13 +21,15 @@ import (
 	"context"
 	"fmt"
 
-	apitypes "github.com/gravitational/teleport/api/types"
-	"github.com/gravitational/teleport/integrations/lib/backoff"
 	"github.com/gravitational/trace"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/jonboulle/clockwork"
+
+	clusterconfigv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/clusterconfig/v1"
+	apitypes "github.com/gravitational/teleport/api/types"
+	"github.com/gravitational/teleport/integrations/lib/backoff"
 
 	"github.com/gravitational/teleport/integrations/terraform/tfschema"
 )
@@ -78,15 +80,13 @@ func (r resourceTeleportSessionRecordingConfig) Create(ctx context.Context, req 
 		return
 	}
 
-	
-
 	sessionRecordingConfigBefore, err := r.p.Client.GetSessionRecordingConfig(ctx)
 	if err != nil && !trace.IsNotFound(err) {
 		resp.Diagnostics.Append(diagFromWrappedErr("Error reading SessionRecordingConfig", trace.Wrap(err), "session_recording_config"))
 		return
 	}
 
-	err = r.p.Client.SetSessionRecordingConfig(ctx, sessionRecordingConfig)
+	_, err = r.p.Client.ClusterConfigClient().UpsertSessionRecordingConfig(ctx, &clusterconfigv1.UpsertSessionRecordingConfigRequest{SessionRecordingConfig: sessionRecordingConfig})
 	if err != nil {
 		resp.Diagnostics.Append(diagFromWrappedErr("Error creating SessionRecordingConfig", trace.Wrap(err), "session_recording_config"))
 		return
@@ -162,7 +162,6 @@ func (r resourceTeleportSessionRecordingConfig) Read(ctx context.Context, req tf
 		return
 	}
 
-	
 	sessionRecordingConfig := sessionRecordingConfigI.(*apitypes.SessionRecordingConfigV2)
 	diags = tfschema.CopySessionRecordingConfigV2ToTerraform(ctx, sessionRecordingConfig, &state)
 	resp.Diagnostics.Append(diags...)
@@ -210,7 +209,7 @@ func (r resourceTeleportSessionRecordingConfig) Update(ctx context.Context, req 
 		return
 	}
 
-	err = r.p.Client.SetSessionRecordingConfig(ctx, sessionRecordingConfig)
+	_, err = r.p.Client.ClusterConfigClient().UpsertSessionRecordingConfig(ctx, &clusterconfigv1.UpsertSessionRecordingConfigRequest{SessionRecordingConfig: sessionRecordingConfig})
 	if err != nil {
 		resp.Diagnostics.Append(diagFromWrappedErr("Error updating SessionRecordingConfig", trace.Wrap(err), "session_recording_config"))
 		return
@@ -244,7 +243,6 @@ func (r resourceTeleportSessionRecordingConfig) Update(ctx context.Context, req 
 		return
 	}
 
-	
 	sessionRecordingConfig = sessionRecordingConfigI.(*apitypes.SessionRecordingConfigV2)
 	diags = tfschema.CopySessionRecordingConfigV2ToTerraform(ctx, sessionRecordingConfig, &plan)
 	resp.Diagnostics.Append(diags...)

api/client/client.go
@@ -2782,17 +2782,6 @@ func (c *Client) SetClusterNetworkingConfig(ctx context.Context, netConfig *type
 	return trace.Wrap(err)
 }
 
-// setClusterNetworkingConfig sets cluster networking configuration.
-func (c *Client) setClusterNetworkingConfig(ctx context.Context, netConfig *types.ClusterNetworkingConfigV2) (types.ClusterNetworkingConfig, error) {
-	_, err := c.grpc.SetClusterNetworkingConfig(ctx, netConfig)
-	if err != nil {
-		return nil, trace.Wrap(err)
-	}
-
-	cfg, err := c.grpc.GetClusterNetworkingConfig(ctx, &emptypb.Empty{})
-	return cfg, trace.Wrap(err)
-}
-
 // UpdateClusterNetworkingConfig updates an existing cluster networking configuration.
 func (c *Client) UpdateClusterNetworkingConfig(ctx context.Context, cfg types.ClusterNetworkingConfig) (types.ClusterNetworkingConfig, error) {
 	v2, ok := cfg.(*types.ClusterNetworkingConfigV2)
@@ -2801,12 +2790,6 @@ func (c *Client) UpdateClusterNetworkingConfig(ctx context.Context, cfg types.Cl
 	}
 
 	updated, err := c.ClusterConfigClient().UpdateClusterNetworkingConfig(ctx, &clusterconfigpb.UpdateClusterNetworkingConfigRequest{ClusterNetworkConfig: v2})
-	// TODO(tross) DELETE IN v18.0.0
-	if trace.IsNotImplemented(err) {
-		cnc, err := c.setClusterNetworkingConfig(ctx, v2)
-		return cnc, trace.Wrap(err)
-	}
-
 	return updated, trace.Wrap(err)
 }
 
@@ -2818,71 +2801,24 @@ func (c *Client) UpsertClusterNetworkingConfig(ctx context.Context, cfg types.Cl
 	}
 
 	updated, err := c.ClusterConfigClient().UpsertClusterNetworkingConfig(ctx, &clusterconfigpb.UpsertClusterNetworkingConfigRequest{ClusterNetworkConfig: v2})
-	// TODO(tross) DELETE IN v18.0.0
-	if trace.IsNotImplemented(err) {
-		cnc, err := c.setClusterNetworkingConfig(ctx, v2)
-		return cnc, trace.Wrap(err)
-	}
-
 	return updated, trace.Wrap(err)
 }
 
 // ResetClusterNetworkingConfig resets cluster networking configuration to defaults.
 func (c *Client) ResetClusterNetworkingConfig(ctx context.Context) error {
 	_, err := c.ClusterConfigClient().ResetClusterNetworkingConfig(ctx, &clusterconfigpb.ResetClusterNetworkingConfigRequest{})
-	if err != nil && trace.IsNotImplemented(err) {
-		_, err := c.grpc.ResetClusterNetworkingConfig(ctx, &emptypb.Empty{})
-		return trace.Wrap(err)
-	}
-
 	return trace.Wrap(err)
 }
 
 // GetSessionRecordingConfig gets session recording configuration.
 func (c *Client) GetSessionRecordingConfig(ctx context.Context) (types.SessionRecordingConfig, error) {
 	resp, err := c.ClusterConfigClient().GetSessionRecordingConfig(ctx, &clusterconfigpb.GetSessionRecordingConfigRequest{})
-	if err != nil && trace.IsNotImplemented(err) {
-		resp, err = c.grpc.GetSessionRecordingConfig(ctx, &emptypb.Empty{})
-	}
-
 	return resp, trace.Wrap(err)
-}
-
-// SetSessionRecordingConfig sets session recording configuration.
-// Deprecated: Use UpdateSessionRecordingConfig or UpsertSessionRecordingConfig instead.
-func (c *Client) SetSessionRecordingConfig(ctx context.Context, recConfig types.SessionRecordingConfig) error {
-	recConfigV2, ok := recConfig.(*types.SessionRecordingConfigV2)
-	if !ok {
-		return trace.BadParameter("invalid type %T", recConfig)
-	}
-
-	_, err := c.grpc.SetSessionRecordingConfig(ctx, recConfigV2)
-	return trace.Wrap(err)
-}
-
-// setSessionRecordingConfig sets session recording configuration.
-func (c *Client) setSessionRecordingConfig(ctx context.Context, recConfig types.SessionRecordingConfig) (types.SessionRecordingConfig, error) {
-	recConfigV2, ok := recConfig.(*types.SessionRecordingConfigV2)
-	if !ok {
-		return nil, trace.BadParameter("invalid type %T", recConfig)
-	}
-
-	if _, err := c.grpc.SetSessionRecordingConfig(ctx, recConfigV2); err != nil {
-		return nil, trace.Wrap(err)
-	}
-
-	cfg, err := c.grpc.GetSessionRecordingConfig(ctx, &emptypb.Empty{})
-	return cfg, trace.Wrap(err)
 }
 
 // ResetSessionRecordingConfig resets session recording configuration to defaults.
 func (c *Client) ResetSessionRecordingConfig(ctx context.Context) error {
 	_, err := c.ClusterConfigClient().ResetSessionRecordingConfig(ctx, &clusterconfigpb.ResetSessionRecordingConfigRequest{})
-	if err != nil && trace.IsNotImplemented(err) {
-		_, err := c.grpc.ResetSessionRecordingConfig(ctx, &emptypb.Empty{})
-		return trace.Wrap(err)
-	}
-
 	return trace.Wrap(err)
 }
 
@@ -2894,11 +2830,6 @@ func (c *Client) UpdateSessionRecordingConfig(ctx context.Context, cfg types.Ses
 	}
 
 	updated, err := c.ClusterConfigClient().UpdateSessionRecordingConfig(ctx, &clusterconfigpb.UpdateSessionRecordingConfigRequest{SessionRecordingConfig: v2})
-	// TODO(tross) DELETE IN v18.0.0
-	if trace.IsNotImplemented(err) {
-		cfg, err = c.setSessionRecordingConfig(ctx, v2)
-		return cfg, trace.Wrap(err)
-	}
 	return updated, trace.Wrap(err)
 }
 
@@ -2910,61 +2841,18 @@ func (c *Client) UpsertSessionRecordingConfig(ctx context.Context, cfg types.Ses
 	}
 
 	updated, err := c.ClusterConfigClient().UpsertSessionRecordingConfig(ctx, &clusterconfigpb.UpsertSessionRecordingConfigRequest{SessionRecordingConfig: v2})
-	// TODO(tross) DELETE IN v18.0.0
-	if trace.IsNotImplemented(err) {
-		cfg, err = c.setSessionRecordingConfig(ctx, v2)
-		return cfg, trace.Wrap(err)
-	}
 	return updated, trace.Wrap(err)
 }
 
 // GetAuthPreference gets the active cluster auth preference.
 func (c *Client) GetAuthPreference(ctx context.Context) (types.AuthPreference, error) {
 	pref, err := c.ClusterConfigClient().GetAuthPreference(ctx, &clusterconfigpb.GetAuthPreferenceRequest{})
-	// TODO(tross) DELETE IN v18.0.0
-	if err != nil && trace.IsNotImplemented(err) {
-		pref, err = c.grpc.GetAuthPreference(ctx, &emptypb.Empty{})
-		if err != nil {
-			return nil, trace.Wrap(err)
-		}
-	}
-
-	return pref, trace.Wrap(err)
-}
-
-// SetAuthPreference sets cluster auth preference via the legacy mechanism.
-// Deprecated: Use UpdateAuthPreference or UpsertAuthPreference instead.
-// TODO(tross) DELETE IN v18.0.0
-func (c *Client) SetAuthPreference(ctx context.Context, authPref types.AuthPreference) error {
-	authPrefV2, ok := authPref.(*types.AuthPreferenceV2)
-	if !ok {
-		return trace.BadParameter("invalid type %T", authPref)
-	}
-
-	_, err := c.grpc.SetAuthPreference(ctx, authPrefV2)
-	return trace.Wrap(err)
-}
-
-// setAuthPreference sets cluster auth preference via the legacy mechanism.
-// TODO(tross) DELETE IN v18.0.0
-func (c *Client) setAuthPreference(ctx context.Context, authPref *types.AuthPreferenceV2) (types.AuthPreference, error) {
-	_, err := c.grpc.SetAuthPreference(ctx, authPref)
-	if err != nil {
-		return nil, trace.Wrap(err)
-	}
-
-	pref, err := c.grpc.GetAuthPreference(ctx, &emptypb.Empty{})
 	return pref, trace.Wrap(err)
 }
 
 // ResetAuthPreference resets cluster auth preference to defaults.
 func (c *Client) ResetAuthPreference(ctx context.Context) error {
 	_, err := c.ClusterConfigClient().ResetAuthPreference(ctx, &clusterconfigpb.ResetAuthPreferenceRequest{})
-	// TODO(tross) DELETE IN v18.0.0
-	if err != nil && trace.IsNotImplemented(err) {
-		_, err := c.grpc.ResetAuthPreference(ctx, &emptypb.Empty{})
-		return trace.Wrap(err)
-	}
 	return trace.Wrap(err)
 }
 
@@ -2976,11 +2864,6 @@ func (c *Client) UpdateAuthPreference(ctx context.Context, p types.AuthPreferenc
 	}
 
 	updated, err := c.ClusterConfigClient().UpdateAuthPreference(ctx, &clusterconfigpb.UpdateAuthPreferenceRequest{AuthPreference: v2})
-	// TODO(tross) DELETE IN v18.0.0
-	if trace.IsNotImplemented(err) {
-		pref, err := c.setAuthPreference(ctx, v2)
-		return pref, trace.Wrap(err)
-	}
 	return updated, trace.Wrap(err)
 }
 
@@ -2992,11 +2875,6 @@ func (c *Client) UpsertAuthPreference(ctx context.Context, p types.AuthPreferenc
 	}
 
 	updated, err := c.ClusterConfigClient().UpsertAuthPreference(ctx, &clusterconfigpb.UpsertAuthPreferenceRequest{AuthPreference: v2})
-	// TODO(tross) DELETE IN v18.0.0
-	if trace.IsNotImplemented(err) {
-		pref, err := c.setAuthPreference(ctx, v2)
-		return pref, trace.Wrap(err)
-	}
 	return updated, trace.Wrap(err)
 }
 

api/client/sessions.go
@@ -51,7 +51,7 @@ func (r *webSessions) Get(ctx context.Context, req types.GetWebSessionRequest) (
 func (r *webSessions) List(ctx context.Context) ([]types.WebSession, error) {
 	sessions, err := r.listStream(ctx)
 	if err != nil {
-		// TODO(espadolini): DELETE IN 18.0
+		// TODO(espadolini): DELETE IN 19.0.0
 		if trace.IsNotImplemented(err) {
 			return r.listUnary(ctx)
 		}

integration/ec2_test.go
@@ -52,7 +52,6 @@ import (
 	"github.com/gravitational/teleport/lib/services"
 	"github.com/gravitational/teleport/lib/utils"
 	"github.com/gravitational/teleport/lib/utils/aws/stsutils"
-	logutils "github.com/gravitational/teleport/lib/utils/log"
 )
 
 func newNodeConfig(t *testing.T, tokenName string, joinMethod types.JoinMethod) *servicecfg.Config {
@@ -65,7 +64,7 @@ func newNodeConfig(t *testing.T, tokenName string, joinMethod types.JoinMethod) 
 	config.Auth.Enabled = false
 	config.Proxy.Enabled = false
 	config.DataDir = t.TempDir()
-	config.Logger = slog.New(logutils.DiscardHandler{})
+	config.Logger = slog.New(slog.DiscardHandler)
 	config.CircuitBreakerConfig = breaker.NoopBreakerConfig()
 	config.InstanceMetadataClient = cloudimds.NewDisabledIMDSClient()
 	return config
@@ -86,7 +85,7 @@ func newProxyConfig(t *testing.T, authAddr utils.NetAddr, tokenName string, join
 
 	config.DataDir = t.TempDir()
 	config.SetAuthServerAddress(authAddr)
-	config.Logger = slog.New(logutils.DiscardHandler{})
+	config.Logger = slog.New(slog.DiscardHandler)
 	config.CircuitBreakerConfig = breaker.NoopBreakerConfig()
 	config.InstanceMetadataClient = cloudimds.NewDisabledIMDSClient()
 	return config
@@ -120,7 +119,7 @@ func newAuthConfig(t *testing.T, clock clockwork.Clock) *servicecfg.Config {
 	config.Proxy.Enabled = false
 	config.SSH.Enabled = false
 	config.Clock = clock
-	config.Logger = slog.New(logutils.DiscardHandler{})
+	config.Logger = slog.New(slog.DiscardHandler)
 	config.CircuitBreakerConfig = breaker.NoopBreakerConfig()
 	config.InstanceMetadataClient = cloudimds.NewDisabledIMDSClient()
 	return config
@@ -342,7 +341,7 @@ func TestEC2Labels(t *testing.T) {
 		},
 	}
 	tconf := servicecfg.MakeDefaultConfig()
-	tconf.Logger = slog.New(logutils.DiscardHandler{})
+	tconf.Logger = slog.New(slog.DiscardHandler)
 	tconf.DataDir = t.TempDir()
 	tconf.Auth.Enabled = true
 	tconf.Proxy.Enabled = true
@@ -467,7 +466,7 @@ func TestEC2Hostname(t *testing.T) {
 		},
 	}
 	tconf := servicecfg.MakeDefaultConfig()
-	tconf.Logger = slog.New(logutils.DiscardHandler{})
+	tconf.Logger = slog.New(slog.DiscardHandler)
 	tconf.DataDir = t.TempDir()
 	tconf.Auth.Enabled = true
 	tconf.Proxy.Enabled = true

integration/helpers/instance.go
@@ -71,7 +71,6 @@ import (
 	"github.com/gravitational/teleport/lib/sshutils"
 	"github.com/gravitational/teleport/lib/tlsca"
 	"github.com/gravitational/teleport/lib/utils"
-	logutils "github.com/gravitational/teleport/lib/utils/log"
 	"github.com/gravitational/teleport/lib/web"
 	websession "github.com/gravitational/teleport/lib/web/session"
 	"github.com/gravitational/teleport/lib/web/terminal"
@@ -354,7 +353,7 @@ func NewInstance(t *testing.T, cfg InstanceConfig) *TeleInstance {
 	}
 
 	if cfg.Logger == nil {
-		cfg.Logger = slog.New(logutils.DiscardHandler{})
+		cfg.Logger = slog.New(slog.DiscardHandler)
 	}
 
 	// generate instance secrets (keys):

integrations/terraform/testlib/auth_preference_test.go
@@ -24,6 +24,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/stretchr/testify/require"
 
+	clusterconfigv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/clusterconfig/v1"
 	"github.com/gravitational/teleport/api/types"
 )
 
@@ -82,7 +83,7 @@ func (s *TerraformSuiteOSS) TestImportAuthPreference() {
 	authPreferencesBefore, err := s.client.GetAuthPreference(ctx)
 	require.NoError(s.T(), err)
 
-	err = s.client.SetAuthPreference(ctx, authPreference)
+	_, err = s.client.ClusterConfigClient().UpsertAuthPreference(ctx, &clusterconfigv1.UpsertAuthPreferenceRequest{AuthPreference: authPreference})
 	require.NoError(s.T(), err)
 
 	require.Eventually(s.T(), func() bool {

integrations/terraform/testlib/session_recording_config_test.go
@@ -24,6 +24,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/stretchr/testify/require"
 
+	clusterconfigv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/clusterconfig/v1"
 	"github.com/gravitational/teleport/api/types"
 )
 
@@ -80,7 +81,9 @@ func (s *TerraformSuiteOSS) TestImportSessionRecordingConfig() {
 	recordingConfigBefore, err := s.client.GetSessionRecordingConfig(ctx)
 	require.NoError(s.T(), err)
 
-	err = s.client.SetSessionRecordingConfig(ctx, sessionrRecordingConfig)
+	_, err = s.client.ClusterConfigClient().UpsertSessionRecordingConfig(ctx, &clusterconfigv1.UpsertSessionRecordingConfigRequest{
+		SessionRecordingConfig: sessionrRecordingConfig,
+	})
 	require.NoError(s.T(), err)
 
 	require.Eventually(s.T(), func() bool {

commit 2035ef5787155f9d0fcde64c47d51f445fd05c4a

[terraform] fix terraform generation code
This PR fixes terraform generation bug introduced in #54957

Signed-off-by: Tiago Silva <tiago.silva@goteleport.com>

integrations/terraform/gen/main.go
@@ -154,8 +154,9 @@ var (
 		TypeName:               "AuthPreferenceV2",
 		VarName:                "authPreference",
 		GetMethod:              "GetAuthPreference",
-		CreateMethod:           "SetAuthPreference",
-		UpdateMethod:           "SetAuthPreference",
+		CreateMethod:           "UpsertAuthPreference",
+		UpdateMethod:           "UpsertAuthPreference",
+		UpsertMethodArity:      2,
 		DeleteMethod:           "ResetAuthPreference",
 		ID:                     `"auth_preference"`,
 		Kind:                   "cluster_auth_preference",
@@ -317,8 +318,9 @@ var (
 		TypeName:               "SessionRecordingConfigV2",
 		VarName:                "sessionRecordingConfig",
 		GetMethod:              "GetSessionRecordingConfig",
-		CreateMethod:           "SetSessionRecordingConfig",
-		UpdateMethod:           "SetSessionRecordingConfig",
+		CreateMethod:           "UpsertSessionRecordingConfig",
+		UpdateMethod:           "UpsertSessionRecordingConfig",
+		UpsertMethodArity:      2,
 		DeleteMethod:           "ResetSessionRecordingConfig",
 		ID:                     `"session_recording_config"`,
 		Kind:                   "session_recording_config",

integrations/terraform/provider/resource_teleport_auth_preference.go
@@ -21,15 +21,13 @@ import (
 	"context"
 	"fmt"
 
+	apitypes "github.com/gravitational/teleport/api/types"
+	"github.com/gravitational/teleport/integrations/lib/backoff"
 	"github.com/gravitational/trace"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/jonboulle/clockwork"
-
-	clusterconfigv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/clusterconfig/v1"
-	apitypes "github.com/gravitational/teleport/api/types"
-	"github.com/gravitational/teleport/integrations/lib/backoff"
 
 	"github.com/gravitational/teleport/integrations/terraform/tfschema"
 )
@@ -80,13 +78,15 @@ func (r resourceTeleportAuthPreference) Create(ctx context.Context, req tfsdk.Cr
 		return
 	}
 
+	
+
 	authPreferenceBefore, err := r.p.Client.GetAuthPreference(ctx)
 	if err != nil && !trace.IsNotFound(err) {
 		resp.Diagnostics.Append(diagFromWrappedErr("Error reading AuthPreference", trace.Wrap(err), "cluster_auth_preference"))
 		return
 	}
 
-	_, err = r.p.Client.ClusterConfigClient().UpsertAuthPreference(ctx, &clusterconfigv1.UpsertAuthPreferenceRequest{AuthPreference: authPreference})
+	_, err = r.p.Client.UpsertAuthPreference(ctx, authPreference)
 	if err != nil {
 		resp.Diagnostics.Append(diagFromWrappedErr("Error creating AuthPreference", trace.Wrap(err), "cluster_auth_preference"))
 		return
@@ -162,6 +162,7 @@ func (r resourceTeleportAuthPreference) Read(ctx context.Context, req tfsdk.Read
 		return
 	}
 
+	
 	authPreference := authPreferenceI.(*apitypes.AuthPreferenceV2)
 	diags = tfschema.CopyAuthPreferenceV2ToTerraform(ctx, authPreference, &state)
 	resp.Diagnostics.Append(diags...)
@@ -209,7 +210,7 @@ func (r resourceTeleportAuthPreference) Update(ctx context.Context, req tfsdk.Up
 		return
 	}
 
-	_, err = r.p.Client.ClusterConfigClient().UpsertAuthPreference(ctx, &clusterconfigv1.UpsertAuthPreferenceRequest{AuthPreference: authPreference})
+	_, err = r.p.Client.UpsertAuthPreference(ctx, authPreference)
 	if err != nil {
 		resp.Diagnostics.Append(diagFromWrappedErr("Error updating AuthPreference", trace.Wrap(err), "cluster_auth_preference"))
 		return
@@ -243,6 +244,7 @@ func (r resourceTeleportAuthPreference) Update(ctx context.Context, req tfsdk.Up
 		return
 	}
 
+	
 	authPreference = authPreferenceI.(*apitypes.AuthPreferenceV2)
 	diags = tfschema.CopyAuthPreferenceV2ToTerraform(ctx, authPreference, &plan)
 	resp.Diagnostics.Append(diags...)

integrations/terraform/provider/resource_teleport_session_recording_config.go
@@ -21,15 +21,13 @@ import (
 	"context"
 	"fmt"
 
+	apitypes "github.com/gravitational/teleport/api/types"
+	"github.com/gravitational/teleport/integrations/lib/backoff"
 	"github.com/gravitational/trace"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/jonboulle/clockwork"
-
-	clusterconfigv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/clusterconfig/v1"
-	apitypes "github.com/gravitational/teleport/api/types"
-	"github.com/gravitational/teleport/integrations/lib/backoff"
 
 	"github.com/gravitational/teleport/integrations/terraform/tfschema"
 )
@@ -80,13 +78,15 @@ func (r resourceTeleportSessionRecordingConfig) Create(ctx context.Context, req 
 		return
 	}
 
+	
+
 	sessionRecordingConfigBefore, err := r.p.Client.GetSessionRecordingConfig(ctx)
 	if err != nil && !trace.IsNotFound(err) {
 		resp.Diagnostics.Append(diagFromWrappedErr("Error reading SessionRecordingConfig", trace.Wrap(err), "session_recording_config"))
 		return
 	}
 
-	_, err = r.p.Client.ClusterConfigClient().UpsertSessionRecordingConfig(ctx, &clusterconfigv1.UpsertSessionRecordingConfigRequest{SessionRecordingConfig: sessionRecordingConfig})
+	_, err = r.p.Client.UpsertSessionRecordingConfig(ctx, sessionRecordingConfig)
 	if err != nil {
 		resp.Diagnostics.Append(diagFromWrappedErr("Error creating SessionRecordingConfig", trace.Wrap(err), "session_recording_config"))
 		return
@@ -162,6 +162,7 @@ func (r resourceTeleportSessionRecordingConfig) Read(ctx context.Context, req tf
 		return
 	}
 
+	
 	sessionRecordingConfig := sessionRecordingConfigI.(*apitypes.SessionRecordingConfigV2)
 	diags = tfschema.CopySessionRecordingConfigV2ToTerraform(ctx, sessionRecordingConfig, &state)
 	resp.Diagnostics.Append(diags...)
@@ -209,7 +210,7 @@ func (r resourceTeleportSessionRecordingConfig) Update(ctx context.Context, req 
 		return
 	}
 
-	_, err = r.p.Client.ClusterConfigClient().UpsertSessionRecordingConfig(ctx, &clusterconfigv1.UpsertSessionRecordingConfigRequest{SessionRecordingConfig: sessionRecordingConfig})
+	_, err = r.p.Client.UpsertSessionRecordingConfig(ctx, sessionRecordingConfig)
 	if err != nil {
 		resp.Diagnostics.Append(diagFromWrappedErr("Error updating SessionRecordingConfig", trace.Wrap(err), "session_recording_config"))
 		return
@@ -243,6 +244,7 @@ func (r resourceTeleportSessionRecordingConfig) Update(ctx context.Context, req 
 		return
 	}
 
+	
 	sessionRecordingConfig = sessionRecordingConfigI.(*apitypes.SessionRecordingConfigV2)
 	diags = tfschema.CopySessionRecordingConfigV2ToTerraform(ctx, sessionRecordingConfig, &plan)
 	resp.Diagnostics.Append(diags...)