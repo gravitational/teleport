3d40b11c2c1a14a8399b6672483be47c1b42995f
MichaelKo <viacheslav.sychov@gmail.com>
2025-05-21 23:28:04 +0200

    [kube] add support for teleport.dev/description in app discovery

--- api/types/constants.go
+++ api/types/constants.go
@@ -873,6 +873,8 @@ const (
 	DiscoveryAppRewriteLabel = TeleportNamespace + "/app-rewrite"
 	// DiscoveryAppNameLabel specifies explicitly name of an app created from Kubernetes service.
 	DiscoveryAppNameLabel = TeleportNamespace + "/name"
+	// DiscoveryAppDescriptionLabelKey Key used to pass a human-friendly description through labels.
+	DiscoveryAppDescriptionLabelKey = TeleportNamespace + "/description"
 	// DiscoveryPathLabel optionally specifies a context path for apps created from Kubernetes services.
 	DiscoveryPathLabel = TeleportNamespace + "/path"
 	// DiscoveryAppInsecureSkipVerify specifies the TLS verification enforcement for a discovered app created from Kubernetes service.
