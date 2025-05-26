86c7e4619bc4e42302f7bd542b72a91b0438b9f8
Dan Share <10600907+danielashare@users.noreply.github.com>
2025-05-26 23:45:31 +0100

    Selectable keyboard layout for desktop access (#55119)
    * Selectable keyboard layout for desktop access
    
    * Fix tests
    
    * Minor fixups
    
    ---------
    
    Co-authored-by: Zac Bergquist <zac.bergquist@goteleport.com>

--- web/packages/shared/components/DesktopSession/DesktopSession.story.tsx
+++ web/packages/shared/components/DesktopSession/DesktopSession.story.tsx
@@ -57,7 +57,7 @@ export default meta;
 const fakeClient = () => {
   const client = new TdpClient(() => null, new BrowserFileSystem());
   // Don't try to connect to a websocket.
-  client.connect = async spec => {
+  client.connect = async (keyboardLayout: number, spec: ClientScreenSpec) => {
     emitFrame(client, spec);
   };
   return client;
@@ -171,7 +171,7 @@ export const SharingDisabledRbac = () => (
 
 export const Alerts = () => {
   const client = fakeClient();
-  client.connect = async spec => {
+  client.connect = async (keyboardLayout: number, spec: ClientScreenSpec) => {
     emitFrame(client, spec);
     client.emit(
       TdpClientEvent.TDP_WARNING,
