bb9bcca12c791066c3e7a8b1b701dec908e155e8
Andrew LeFevre <andrew.lefevre@goteleport.com>
2025-05-29 13:36:05 -0400

    make session ID env vars agree (#53652)

--- lib/web/apiserver.go
+++ lib/web/apiserver.go
@@ -3917,7 +3917,6 @@ func (h *Handler) generateSession(ctx context.Context, req *TerminalRequest, clu
 		ServerHostname: host,
 		ServerHostPort: port,
 		Moderated:      accessEvaluator.IsModerated(),
-		ID:             session.NewID(),
 		Created:        time.Now().UTC(),
 		LastActive:     time.Now().UTC(),
 		Namespace:      apidefaults.Namespace,
