bcc4ff02a175aa816134fe0bd1e3c44ac207bf14
Guillaume J. Charmes <git+guillaume@charmes.net>
2025-06-03 22:29:29 +0000

    add tests

--- lib/kube/proxy/foo_test.go
+++ lib/kube/proxy/foo_test.go
@@ -1,6 +1,14 @@
 package proxy
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestFoo(t *testing.T) {
+	f := ForwarderConfig{}
+	if strings.Contains(f.ClusterName, "ffoo") {
+		t.Fatal("ok")
+	}
+	f.CheckAndSetDefaults()
 }
