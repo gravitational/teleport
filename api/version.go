2402f6e12d8ae94cac6ce4afd7a70c7c3fc4ad99
Rafał Cieślak <rafal.cieslak@goteleport.com>
2025-06-03 09:11:09 +0200

    Update version to 18.0.0-dev.ravicious.27
    Updated Electron version

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.0.0-dev"
+const Version = "18.0.0-dev.ravicious.27"
 
 const VersionMajor = 18
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev"
+const VersionPreRelease = "dev.ravicious.27"
 const VersionMetadata = ""
