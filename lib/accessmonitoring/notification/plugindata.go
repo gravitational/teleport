3dbd69c19df77eb81caa021bcc9353fcc230c7ca
Bernard Kim <bernard@goteleport.com>
2025-06-04 17:06:57 -0700

    Add static recipients and separately handle requester/reviewer

--- lib/accessmonitoring/notification/plugindata.go
+++ lib/accessmonitoring/notification/plugindata.go
@@ -24,12 +24,18 @@ type Message struct {
 type Notification struct {
 	// ID specifies the notification ID.
 	ID string
-	// Recipients contains the list of recipients to notify.
-	Recipients []common.Recipient
 	// AccessRequestData contains Access Request data associated with this notification.
 	AccessRequestData plugindata.AccessRequestData
+
+	// RequesterRecipient specifies the requester recipient.
+	RequesterRecipient common.Recipient
+	// ReviewerRecipients contains the list of recipients to notify.
+	ReviewerRecipients []common.Recipient
+
+	// RequesterMessage specifies the requester message and reviews.
+	RequesterMessage Message
 	// SentMessages contains a map of messages and reviews.
-	SentMessages map[MessageID]Message
+	ReviewerMessages map[MessageID]Message
 }
 
 type MessageID any
@@ -44,8 +50,16 @@ func DecodeNotification(data map[string]string) (notification Notification, err 
 
 	notification.ID = data["id"]
 
-	messages := data["messages"]
-	if err := json.Unmarshal([]byte(messages), &notification.SentMessages); err != nil {
+	if err := json.Unmarshal([]byte(data["requester_recipient"]), &notification.RequesterRecipient); err != nil {
+		return Notification{}, trace.Wrap(err)
+	}
+	if err := json.Unmarshal([]byte(data["reviewer_recipients"]), &notification.ReviewerRecipients); err != nil {
+		return Notification{}, trace.Wrap(err)
+	}
+	if err := json.Unmarshal([]byte(data["requester_message"]), &notification.RequesterMessage); err != nil {
+		return Notification{}, trace.Wrap(err)
+	}
+	if err := json.Unmarshal([]byte(data["reviewer_messages"]), &notification.ReviewerMessages); err != nil {
 		return Notification{}, trace.Wrap(err)
 	}
 
@@ -59,11 +73,29 @@ func EncodeNotification(notification Notification) (map[string]string, error) {
 	}
 	result["id"] = notification.ID
 
-	bytes, err := json.Marshal(notification.SentMessages)
+	bytes, err := json.Marshal(notification.RequesterRecipient)
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
-	result["messages"] = string(bytes)
+	result["requester_recipient"] = string(bytes)
+
+	bytes, err = json.Marshal(notification.ReviewerRecipients)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	result["reviewer_recipients"] = string(bytes)
+
+	bytes, err = json.Marshal(notification.RequesterMessage)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	result["requester_message"] = string(bytes)
+
+	bytes, err = json.Marshal(notification.ReviewerMessages)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	result["reviewer_messages"] = string(bytes)
 
 	return result, nil
 }
