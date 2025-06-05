f0b596a4e4eddbcaea80383605f5c4c9bbc4c063
STeve (Xin) Huang <xin.huang@goteleport.com>
2025-06-05 11:17:34 -0400

    MCP access part 6: "tsh mcp ls" (#55292)
    * MCP access part 6: "tsh mcp ls"
    
    * address feedback

--- tool/tsh/common/tsh.go
+++ tool/tsh/common/tsh.go
@@ -1344,8 +1344,7 @@ func Run(ctx context.Context, args []string, opts ...CliOption) error {
 
 	gitCmd := newGitCommands(app)
 	pivCmd := newPIVCommands(app)
-
-	mcpCmd := newMCPCommands(app)
+	mcpCmd := newMCPCommands(app, &cf)
 
 	if runtime.GOOS == constants.WindowsOS {
 		bench.Hidden()
@@ -1754,6 +1753,8 @@ func Run(ctx context.Context, args []string, opts ...CliOption) error {
 		err = pivCmd.agent.run(&cf)
 	case mcpCmd.dbStart.FullCommand():
 		err = mcpCmd.dbStart.run(&cf)
+	case mcpCmd.list.FullCommand():
+		err = mcpCmd.list.run()
 	default:
 		// Handle commands that might not be available.
 		switch {
@@ -3211,14 +3212,7 @@ func getDBUsers(db types.Database, accessChecker services.AccessChecker) *dbUser
 		)
 		return &dbUsers{}
 	}
-	var denied []string
-	allowed := users.Allowed()
-	if users.WildcardAllowed() {
-		// start the list with *.
-		allowed = append([]string{types.Wildcard}, allowed...)
-		// only include denied users if the wildcard is allowed.
-		denied = append(denied, users.Denied()...)
-	}
+	allowed, denied := users.ToEntities()
 	return &dbUsers{
 		Allowed: allowed,
 		Denied:  denied,
@@ -3283,9 +3277,6 @@ func formatUsersForDB(database types.Database, accessChecker services.AccessChec
 	}
 
 	dbUsers := getDBUsers(database, accessChecker)
-	if len(dbUsers.Allowed) == 0 {
-		return "(none)"
-	}
 
 	// Add a note for auto-provisioned user.
 	if database.IsAutoUsersEnabled() {
@@ -3302,10 +3293,7 @@ func formatUsersForDB(database types.Database, accessChecker services.AccessChec
 		}
 	}
 
-	if len(dbUsers.Denied) == 0 {
-		return fmt.Sprintf("%v", dbUsers.Allowed)
-	}
-	return fmt.Sprintf("%v, except: %v", dbUsers.Allowed, dbUsers.Denied)
+	return common.FormatAllowedEntities(dbUsers.Allowed, dbUsers.Denied)
 }
 
 // TODO(greedy52) more refactoring on db printing and move them to db_print.go.
