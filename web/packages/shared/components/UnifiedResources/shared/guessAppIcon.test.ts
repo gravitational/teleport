15d9dc1ebb610a650d89b66acce800db83abbf47
Rafał Cieślak <rafal.cieslak@goteleport.com>
2025-06-06 16:35:55 +0200

    Run eslint-disable-inserter
        pnpm exec eslint --quiet '+(e|web)/**/*.{ts,tsx,js,jsx,mts}' --format json | eslint-disable-inserter

--- web/packages/shared/components/UnifiedResources/shared/guessAppIcon.test.ts
+++ web/packages/shared/components/UnifiedResources/shared/guessAppIcon.test.ts
@@ -16,7 +16,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+// eslint-disable-next-line no-restricted-imports -- FIXME
 import { App } from 'teleport/services/apps';
+// eslint-disable-next-line no-restricted-imports -- FIXME
 import makeApp from 'teleport/services/apps/makeApps';
 
 import { guessAppIcon } from './guessAppIcon';
