601c7ba3bee21f78bb830684c8c60574b793a90b
Rafał Cieślak <rafal.cieslak@goteleport.com>
2025-06-10 18:14:12 +0200

    Add more general types

--- web/packages/teleport/src/services/audit/gen-event-reference/gen-event-reference.ts
+++ web/packages/teleport/src/services/audit/gen-event-reference/gen-event-reference.ts
@@ -1,4 +1,22 @@
-import { Event, Formatters } from '../types';
+/**
+ * Teleport
+ * Copyright (C) 2025 Gravitational, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { Event, Formatters } from './types';
 
 // eventsWithoutExamples returns an array of event objects based on the
 // elements in formatters that do not have corresponding examples in fixtures.
