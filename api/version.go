28dd75f98a2d357a160e2f76d0b9645005078728
Edward Dowling <edward.dowling@goteleport.com>
2025-06-11 15:35:57 +0100

    Release v18.0.0-dev.edward.2

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.0.0-dev.edward.1"
+const Version = "18.0.0-dev.edward.2"
 
 const VersionMajor = 18
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev.edward.1"
+const VersionPreRelease = "dev.edward.2"
 const VersionMetadata = ""
