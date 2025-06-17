c5f0f712ad5a9690af931f9ed9094b5e8df69f3f
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2025-06-17 11:54:52 -0400

    Remove unused UsageReporter field from servicecfg.Config (#55817)

--- lib/service/service.go
+++ lib/service/service.go
@@ -2100,7 +2100,6 @@ func (process *TeleportProcess) initAuthService() error {
 			Provisioner:             cfg.Provisioner,
 			Identity:                cfg.Identity,
 			Access:                  cfg.Access,
-			UsageReporter:           cfg.UsageReporter,
 			StaticTokens:            cfg.Auth.StaticTokens,
 			Roles:                   cfg.Auth.Roles,
 			AuthPreference:          cfg.Auth.Preference,
