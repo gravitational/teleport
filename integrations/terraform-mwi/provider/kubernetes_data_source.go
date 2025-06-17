05d5ede5432c918d0a634a8e259cf7b1af1bc472
Noah Stride <noah.stride@goteleport.com>
2025-06-17 09:45:59 +0100

    MWI: Terraform Provider - Basic validation/attributes (#55416)
    * Add initially required provider attributes and validators
    
    * Update tests for provider required attributes
    
    * Add attributes/validation to ephemeral resource
    
    * Add attributes for Ephemeral Resource
    
    * Add attributes/arguments to datasource
    
    * Remove test fields
    
    * Nest output attributes

--- integrations/terraform-mwi/provider/kubernetes_data_source.go
+++ integrations/terraform-mwi/provider/kubernetes_data_source.go
@@ -19,7 +19,9 @@ package provider
 import (
 	"context"
 	"fmt"
+	"time"
 
+	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -29,12 +31,25 @@ func NewKubernetesDataSource() datasource.DataSource {
 	return &KubernetesDataSource{}
 }
 
+type KubernetesDataSourceModelSelector struct {
+	Name types.String `tfsdk:"name"`
+}
+
 type KubernetesDataSourceModel struct {
 	// Arguments
-	ExampleInput types.String `tfsdk:"example_input"`
+	Selector      KubernetesDataSourceModelSelector `tfsdk:"selector"`
+	CredentialTTL timetypes.GoDuration              `tfsdk:"credential_ttl"`
 
 	// Attributes
-	ExampleOutput types.String `tfsdk:"example_output"`
+	Output *KubernetesDataSourceModelOutput `tfsdk:"output"`
+}
+
+type KubernetesDataSourceModelOutput struct {
+	ClientKey            types.String `tfsdk:"client_key"`
+	Host                 types.String `tfsdk:"host"`
+	TLSServerName        types.String `tfsdk:"tls_server_name"`
+	ClientCertificate    types.String `tfsdk:"client_certificate"`
+	ClusterCACertificate types.String `tfsdk:"cluster_ca_certificate"`
 }
 
 type KubernetesDataSource struct{}
@@ -53,18 +68,52 @@ func (d *KubernetesDataSource) Schema(
 	resp *datasource.SchemaResponse,
 ) {
 	resp.Schema = schema.Schema{
-		MarkdownDescription: "TODO",
+		MarkdownDescription: "The Kubernetes data source provides credentials to allow other providers to access Kubernetes cluster through Teleport Machine & Workload Identity.",
 
 		Attributes: map[string]schema.Attribute{
-			// Input
-			"example_input": schema.StringAttribute{
-				MarkdownDescription: "TODO",
-				Required:            true,
+			// Arguments
+			"selector": schema.SingleNestedAttribute{
+				MarkdownDescription: "Selects the Kubernetes cluster to connect to.",
+				Attributes: map[string]schema.Attribute{
+					"name": schema.StringAttribute{
+						MarkdownDescription: "The name of the Kubernetes cluster to connect to.",
+						Required:            true,
+					},
+				},
+				Required: true,
 			},
-			// Output
-			"example_output": schema.StringAttribute{
-				MarkdownDescription: "TODO",
+			"credential_ttl": schema.StringAttribute{
+				CustomType:          timetypes.GoDurationType{},
+				MarkdownDescription: "How long the issued credentials should be valid for. Defaults to 30 minutes.",
+				Optional:            true,
 				Computed:            true,
+			},
+			// Attributes
+			"output": schema.SingleNestedAttribute{
+				Computed: true,
+				Attributes: map[string]schema.Attribute{
+					"client_key": schema.StringAttribute{
+						Computed:            true,
+						Sensitive:           true,
+						MarkdownDescription: "Compatible with the `client_key` argument of the `kubernetes` provider.",
+					},
+					"host": schema.StringAttribute{
+						Computed:            true,
+						MarkdownDescription: "Compatible with the `host` argument of the `kubernetes` provider.",
+					},
+					"tls_server_name": schema.StringAttribute{
+						Computed:            true,
+						MarkdownDescription: "Compatible with the `tls_server_name` argument of the `kubernetes` provider.",
+					},
+					"client_certificate": schema.StringAttribute{
+						Computed:            true,
+						MarkdownDescription: "Compatible with the `client_certificate` argument of the `kubernetes` provider.",
+					},
+					"cluster_ca_certificate": schema.StringAttribute{
+						Computed:            true,
+						MarkdownDescription: "Compatible with the `cluster_ca_certificate` argument of the `kubernetes` provider.",
+					},
+				},
 			},
 		},
 	}
@@ -78,18 +127,43 @@ func (d *KubernetesDataSource) Configure(
 	// TODO: Fetch bot config data from the provider.
 }
 
+func (r *KubernetesDataSource) loadModelAndSetDefaults(
+	ctx context.Context,
+	req datasource.ReadRequest,
+	resp *datasource.ReadResponse,
+) KubernetesDataSourceModel {
+	var data KubernetesDataSourceModel
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return data
+	}
+
+	// Set default for credential TTL if not provided.
+	if data.CredentialTTL.IsNull() || data.CredentialTTL.IsUnknown() {
+		data.CredentialTTL = timetypes.NewGoDurationValue(
+			time.Minute * 30,
+		)
+	}
+
+	return data
+}
+
 func (d *KubernetesDataSource) Read(
 	ctx context.Context,
 	req datasource.ReadRequest,
 	resp *datasource.ReadResponse,
 ) {
-	var data KubernetesDataSourceModel
-	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	data := d.loadModelAndSetDefaults(ctx, req, resp)
 	if resp.Diagnostics.HasError() {
 		return
 	}
-	data.ExampleOutput = types.StringValue(
-		fmt.Sprintf("Hello, %s!", data.ExampleInput.ValueString()),
-	)
+
+	out := KubernetesDataSourceModelOutput{
+		Host: types.StringValue(
+			fmt.Sprintf("Hello, %s!", data.Selector.Name.ValueString()),
+		),
+	}
+
+	data.Output = &out
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
