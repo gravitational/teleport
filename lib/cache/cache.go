9df51cde4c767a80a93fa491213656f6e4c6683d
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2025-06-17 14:04:54 +0200

    `relay_server` definition, CRUD api and caching (#55441)
    * relay_server proto definition
    
    * make grpc
    
    * Support for relay_server in local presence storage
    
    * relay_server CRUD rpc definitions
    
    * make grpc
    
    * grpc implementation for relay_server CRUD
    
    * relay_server in WatchEvents proto definition
    
    * make grpc
    
    * Cache support for relay_server
    
    * fix TestCacheWatchKindExistsInEvents

--- lib/cache/cache.go
+++ lib/cache/cache.go
@@ -43,6 +43,7 @@ import (
 	apitracing "github.com/gravitational/teleport/api/observability/tracing"
 	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/api/utils/retryutils"
+	"github.com/gravitational/teleport/lib/auth/authclient"
 	"github.com/gravitational/teleport/lib/backend"
 	"github.com/gravitational/teleport/lib/backend/backendmetrics"
 	"github.com/gravitational/teleport/lib/defaults"
@@ -206,6 +207,7 @@ func ForAuth(cfg Config) Config {
 		{Kind: types.KindGitServer},
 		{Kind: types.KindWorkloadIdentity},
 		{Kind: types.KindHealthCheckConfig},
+		{Kind: types.KindRelayServer},
 	}
 	cfg.QueueSize = defaults.AuthQueueSize
 	// We don't want to enable partial health for auth cache because auth uses an event stream
@@ -262,6 +264,7 @@ func ForProxy(cfg Config) Config {
 		{Kind: types.KindAutoUpdateAgentRollout},
 		{Kind: types.KindUserTask},
 		{Kind: types.KindGitServer},
+		{Kind: types.KindRelayServer},
 	}
 	cfg.QueueSize = defaults.ProxyQueueSize
 	return cfg
@@ -512,6 +515,8 @@ type Cache struct {
 	// closed indicates that the cache has been closed
 	closed atomic.Bool
 }
+
+var _ authclient.Cache = (*Cache)(nil)
 
 func (c *Cache) setInitError(err error) {
 	c.initOnce.Do(func() {
