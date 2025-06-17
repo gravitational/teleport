49b60895944cdb461df496fe86fd9dcd6e6ccb27
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2025-06-17 14:04:54 +0200

    `relay_server` definition, CRUD api and caching (#55441)
    * relay_server proto definition
    
    * make grpc
    
    * Support for relay_server in local presence storage
    
    * relay_server CRUD rpc definitions
    
    * make grpc
    
    * grpc implementation for relay_server CRUD
    
    * relay_server in WatchEvents proto definition
    
    * make grpc
    
    * Cache support for relay_server
    
    * fix TestCacheWatchKindExistsInEvents

--- lib/authz/permissions.go
+++ lib/authz/permissions.go
@@ -924,6 +924,7 @@ func roleSpecForProxy(clusterName string) types.RoleSpecV6 {
 				types.NewRule(types.KindUserTask, services.RO()),
 				types.NewRule(types.KindGitServer, services.RO()),
 				types.NewRule(types.KindAccessGraphSettings, services.RO()),
+				types.NewRule(types.KindRelayServer, services.RO()),
 				types.NewRule(types.KindAccessList, services.RO()),
 				// this rule allows cloud proxies to read
 				// plugins of `openai` type, since Assist uses the OpenAI API and runs in Proxy.
