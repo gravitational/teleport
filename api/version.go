2ccfcb4cba4a844144afbcb754f24a176f70ae9f
Vadym Popov <vadym.popov@goteleport.com>
2025-06-17 01:14:16 -0700

    Release v17.5.1-dev.ctmu.5

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "17.5.1-dev.ctmu.4"
+const Version = "17.5.1-dev.ctmu.5"
 
 const VersionMajor = 17
 const VersionMinor = 5
 const VersionPatch = 1
-const VersionPreRelease = "dev.ctmu.4"
+const VersionPreRelease = "dev.ctmu.5"
 const VersionMetadata = ""
