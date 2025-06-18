4dfdfc2add3b13c97566088a4258d88f21102f6b
Noah Stride <noah.stride@goteleport.com>
2025-06-18 18:24:37 +0100

    experiment with testing

--- integrations/terraform-mwi/provider/kubernetes_ephemeral_resource_test.go
+++ integrations/terraform-mwi/provider/kubernetes_ephemeral_resource_test.go
@@ -17,16 +17,123 @@ limitations under the License.
 package provider
 
 import (
+	"context"
+	"net"
+	"path/filepath"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
 	"github.com/hashicorp/terraform-plugin-testing/statecheck"
 	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 	"github.com/hashicorp/terraform-plugin-testing/tfversion"
+	"github.com/stretchr/testify/require"
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+
+	"github.com/gravitational/teleport/api/constants"
+	"github.com/gravitational/teleport/integration/helpers"
+	testingkubemock "github.com/gravitational/teleport/lib/kube/proxy/testing/kube_server"
+	"github.com/gravitational/teleport/lib/service"
+	"github.com/gravitational/teleport/lib/service/servicecfg"
+	"github.com/gravitational/teleport/lib/utils"
+	"github.com/gravitational/teleport/tool/teleport/testenv"
 )
 
+const kubeClusterName = "test-cluster"
+
+func k8ClientConfig(serverAddr, sni string) clientcmdapi.Config {
+	return clientcmdapi.Config{
+		Clusters: map[string]*clientcmdapi.Cluster{
+			kubeClusterName: {
+				Server:                serverAddr,
+				InsecureSkipTLSVerify: true,
+				TLSServerName:         sni,
+			},
+		},
+		Contexts: map[string]*clientcmdapi.Context{
+			kubeClusterName: {
+				Cluster:  kubeClusterName,
+				AuthInfo: kubeClusterName,
+			},
+		},
+		CurrentContext: kubeClusterName,
+	}
+}
+
+func startKubeAPIMock(t *testing.T) *testingkubemock.KubeMockServer {
+	kubeMock, err := testingkubemock.NewKubeAPIMock()
+	require.NoError(t, err)
+	t.Cleanup(func() { kubeMock.Close() })
+	return kubeMock
+}
+
+func mustCreateKubeConfigFile(t *testing.T, config clientcmdapi.Config) string {
+	t.Helper()
+
+	configPath := filepath.Join(t.TempDir(), "config.yaml")
+	err := clientcmd.WriteToFile(config, configPath)
+	require.NoError(t, err)
+	return configPath
+}
+
+const localK8SSNI = constants.KubeTeleportProxyALPNPrefix + "teleport.cluster.local"
+
 func TestAccKubernetesEphemeralResource(t *testing.T) {
+	log := utils.NewSlogLoggerForTests()
+	ctx := context.Background()
+
+	kubeMock := startKubeAPIMock(t)
+	kubeConfigPath := mustCreateKubeConfigFile(t, k8ClientConfig(
+		kubeMock.URL,
+		localK8SSNI,
+	))
+
+	process := testenv.MakeTestServer(
+		t,
+		func(o *testenv.TestServersOpts) {
+			testenv.WithClusterName(t, "root")(o)
+			testenv.WithConfig(func(cfg *servicecfg.Config) {
+				cfg.Logger = log
+				cfg.Proxy.PublicAddrs = []utils.NetAddr{
+					{
+						AddrNetwork: "tcp",
+						Addr: net.JoinHostPort(
+							"localhost",
+							strconv.Itoa(cfg.Proxy.WebAddr.Port(0)),
+						),
+					},
+				}
+				cfg.Proxy.TunnelPublicAddrs = []utils.NetAddr{
+					cfg.Proxy.ReverseTunnelListenAddr,
+				}
+
+				// Seemingly not needed.
+				//cfg.Proxy.Kube.Enabled = true
+				//cfg.Proxy.Kube.ListenAddr = utils.NetAddr{
+				//	AddrNetwork: "tcp",
+				//	Addr: testenv.NewTCPListener(
+				//		t, service.ListenerProxyKube, &cfg.FileDescriptors,
+				//	),
+				//}
+
+				cfg.Kube.Enabled = true
+				cfg.Kube.KubeconfigPath = kubeConfigPath
+				cfg.Kube.ListenAddr = utils.MustParseAddr(
+					helpers.NewListener(t, service.ListenerKube, &cfg.FileDescriptors))
+			})(o)
+		},
+		testenv.WithProxyKube(t),
+	)
+	rootClient := testenv.MakeDefaultAuthClient(t, process)
+
+	time.Sleep(10 * time.Second)
+	kubeServers, err := rootClient.GetKubernetesServers(ctx)
+	require.NoError(t, err)
+	require.Len(t, kubeServers, 1)
+
 	const config = `
 provider "teleportmwi" {
   proxy_server = "example.com:3080"
