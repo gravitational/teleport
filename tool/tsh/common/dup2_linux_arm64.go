gravitational/teleport

commit 57c909dab1b8f7f0a8ba7e3199a902775f82f42a

Add fork after authentication for tsh ssh (#54696)
This change adds fork after authentication support
to tsh ssh.

tool/tsh/common/reexec_unix.go
@@ -1,0 +1,68 @@
+// Teleport
+// Copyright (C) 2025 Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//go:build unix
+
+package common
+
+import (
+	"os"
+	"syscall"
+
+	"github.com/gravitational/trace"
+)
+
+func isValidForkSignalFd(fd uint64) bool {
+	// Don't allow stdin, stdout, or stderr.
+	return fd > 2
+}
+
+// newSignalFile creates a signaling file for --fork-after-authentication from
+// a file descriptor.
+func newSignalFile(fd uint64) *os.File {
+	syscall.CloseOnExec(int(fd))
+	return os.NewFile(uintptr(fd), "disown")
+}
+
+// replaceStdin returns a file for /dev/null that should be used from now
+// on instead of stdin.
+func replaceStdin() (*os.File, error) {
+	devNull, err := os.Open(os.DevNull)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	rc, err := devNull.SyscallConn()
+	if err != nil {
+		_ = devNull.Close()
+		return nil, trace.Wrap(err)
+	}
+	var dupErr error
+	if ctrlErr := rc.Control(func(fd uintptr) {
+		dupErr = syscall.Dup2(int(fd), syscall.Stdin)
+		// stdin is not O_CLOEXEC after dup2 but thankfully the three stdio
+		// file descriptors must be not O_CLOEXEC anyway, so we can avoid
+		// a linux-specific implementation or syscall.ForkLock shenanigans
+	}); ctrlErr != nil {
+		_ = devNull.Close()
+		return nil, trace.Wrap(ctrlErr)
+	}
+	if dupErr != nil {
+		// this is the error from Dup2
+		_ = devNull.Close()
+		return nil, trace.Wrap(err)
+	}
+	return devNull, err
+}

lib/client/api.go
@@ -1901,6 +1901,14 @@ type SSHOptions struct {
 	// machine. If provided, it will be used instead of establishing a connection
 	// to the target host and executing the command remotely.
 	LocalCommandExecutor func(string, []string) error
+	// OnChildAuthenticate is a function to run in the child process during
+	// --fork-after authentications. It runs after authentication completes
+	// but before the session begins.
+	OnChildAuthenticate func() error
+}
+
+func (opts SSHOptions) forkAfterAuthentication() bool {
+	return opts.OnChildAuthenticate != nil
 }
 
 // WithHostAddress returns a SSHOptions which overrides the
@@ -1916,6 +1924,15 @@ func WithHostAddress(addr string) func(*SSHOptions) {
 func WithLocalCommandExecutor(executor func(string, []string) error) func(*SSHOptions) {
 	return func(opt *SSHOptions) {
 		opt.LocalCommandExecutor = executor
+	}
+}
+
+// WithForkAfterAuthentication indicates that tsh is currently reexec-ing
+// for --fork-after-authentication. The given function is called after
+// authentication is complete but before the session starts.
+func WithForkAfterAuthentication(onAuthenticate func() error) func(*SSHOptions) {
+	return func(opt *SSHOptions) {
+		opt.OnChildAuthenticate = onAuthenticate
 	}
 }
 
@@ -1961,9 +1978,14 @@ func (tc *TeleportClient) SSH(ctx context.Context, command []string, opts ...fun
 	}
 
 	if len(nodeAddrs) > 1 {
+		if options.forkAfterAuthentication() {
+			return &NonRetryableError{
+				Err: trace.BadParameter("fork after authentication not supported for commands on multiple nodes"),
+			}
+		}
 		return tc.runShellOrCommandOnMultipleNodes(ctx, clt, nodeAddrs, command)
 	}
-	return tc.runShellOrCommandOnSingleNode(ctx, clt, nodeAddrs[0].Addr, command, options.LocalCommandExecutor)
+	return tc.runShellOrCommandOnSingleNode(ctx, clt, nodeAddrs[0].Addr, command, options)
 }
 
 // ConnectToNode attempts to establish a connection to the node resolved to by the provided
@@ -2166,7 +2188,7 @@ func (tc *TeleportClient) connectToNodeWithMFA(ctx context.Context, clt *Cluster
 	return nodeClient, trace.Wrap(err)
 }
 
-func (tc *TeleportClient) runShellOrCommandOnSingleNode(ctx context.Context, clt *ClusterClient, nodeAddr string, command []string, commandExecutor func(string, []string) error) error {
+func (tc *TeleportClient) runShellOrCommandOnSingleNode(ctx context.Context, clt *ClusterClient, nodeAddr string, command []string, options SSHOptions) error {
 	cluster := clt.ClusterName()
 	ctx, span := tc.Tracer.Start(
 		ctx,
@@ -2190,6 +2212,12 @@ func (tc *TeleportClient) runShellOrCommandOnSingleNode(ctx context.Context, clt
 		return trace.Wrap(err)
 	}
 	defer nodeClient.Close()
+
+	if options.OnChildAuthenticate != nil {
+		if err := options.OnChildAuthenticate(); err != nil {
+			return trace.Wrap(err)
+		}
+	}
 	// If forwarding ports were specified, start port forwarding.
 	if err := tc.startPortForwarding(ctx, nodeClient); err != nil {
 		return trace.Wrap(err)
@@ -2221,11 +2249,11 @@ func (tc *TeleportClient) runShellOrCommandOnSingleNode(ctx context.Context, clt
 
 	// After port forwarding, run a local command that uses the connection, and
 	// then disconnect.
-	if commandExecutor != nil {
+	if options.LocalCommandExecutor != nil {
 		if len(tc.Config.LocalForwardPorts) == 0 {
 			fmt.Println("Executing command locally without connecting to any servers. This makes no sense.")
 		}
-		return commandExecutor(tc.Config.HostLogin, command)
+		return options.LocalCommandExecutor(tc.Config.HostLogin, command)
 	}
 
 	if len(command) > 0 {
@@ -2260,7 +2288,7 @@ func (tc *TeleportClient) runShellOrCommandOnMultipleNodes(ctx context.Context, 
 
 	// Issue "shell" request to the first matching node.
 	fmt.Printf("\x1b[1mWARNING\x1b[0m: Multiple nodes match the label selector, picking first: %q\n", nodeAddrs[0])
-	return tc.runShellOrCommandOnSingleNode(ctx, clt, nodeAddrs[0], nil, nil)
+	return tc.runShellOrCommandOnSingleNode(ctx, clt, nodeAddrs[0], nil, SSHOptions{})
 }
 
 func (tc *TeleportClient) startPortForwarding(ctx context.Context, nodeClient *NodeClient) error {

lib/client/reexec/reexec.go
@@ -1,0 +1,148 @@
+// Teleport
+// Copyright (C) 2025 Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package reexec
+
+import (
+	"context"
+	"errors"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/gravitational/trace"
+)
+
+// NotifyFileSignal signals on the returned channel when the provided file
+// receives a signal (a one-byte read).
+func NotifyFileSignal(f *os.File) <-chan error {
+	errorCh := make(chan error, 1)
+	go func() {
+		n, err := f.Read(make([]byte, 1))
+		if n > 0 {
+			errorCh <- nil
+		} else if err == nil {
+			// this should be impossible according to the io.Reader contract
+			errorCh <- io.ErrUnexpectedEOF
+		} else {
+			errorCh <- err
+		}
+	}()
+	return errorCh
+}
+
+// SignalAndClose writes a byte to the provided file (to signal a caller of
+// NotifyFileSignal) and closes it.
+func SignalAndClose(f *os.File) error {
+	_, err := f.Write([]byte{0x00})
+	return trace.NewAggregate(err, f.Close())
+}
+
+// ForkAuthenticateParams are the parameters to RunForkAuthenticate.
+type ForkAuthenticateParams struct {
+	// GetArgs gets the arguments to re-exec with, excluding the executable
+	// (equivalent to os.Args[1:]).
+	GetArgs func(signalFd, killFd uint64) []string
+	// executable is the executable to run while re-execing. Overridden in tests.
+	executable string
+	// Stdin is the child process' stdin.
+	Stdin io.Reader
+	// Stdout is the child process' stdout.
+	Stdout io.Writer
+	// Stderr is the child process' stderr.
+	Stderr io.Writer
+}
+
+// RunForkAuthenticate re-execs the current executable and waits for any of
+// the following:
+//   - The child process exits (usually in error).
+//   - The child process signals the parent that it is ready to be disowned.
+//   - The context is canceled.
+func RunForkAuthenticate(ctx context.Context, params ForkAuthenticateParams) error {
+	if params.executable == "" {
+		executable, err := getExecutable()
+		if err != nil {
+			return trace.Wrap(err)
+		}
+		params.executable = executable
+	}
+	cmd := exec.Command(params.executable)
+	// Set up signal pipes.
+	disownR, disownW, err := os.Pipe()
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	killR, killW, err := os.Pipe()
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	defer func() {
+		// If the child is still listening, kill it. If the child successfully
+		// disowned, this will do nothing.
+		SignalAndClose(killW)
+		killR.Close()
+		disownW.Close()
+		disownR.Close()
+	}()
+
+	signalFd, killFd := configureReexecForOS(cmd, disownW, killR)
+	cmd.Args = append(cmd.Args, params.GetArgs(signalFd, killFd)...)
+	cmd.Args[0] = os.Args[0]
+	cmd.Stdin = params.Stdin
+	cmd.Stdout = params.Stdout
+	cmd.Stderr = params.Stderr
+
+	if err := cmd.Start(); err != nil {
+		return trace.Wrap(err)
+	}
+
+	// Clean up parent end of pipes.
+	if err := disownW.Close(); err != nil {
+		return trace.NewAggregate(err, killAndWaitProcess(cmd))
+	}
+	if err := killR.Close(); err != nil {
+		return trace.NewAggregate(err, killAndWaitProcess(cmd))
+	}
+
+	select {
+	case err := <-NotifyFileSignal(disownR):
+		if err == nil {
+			return trace.Wrap(cmd.Process.Release())
+		} else if errors.Is(err, io.EOF) {
+			// EOF means the child process exited, no need to report it on top of kill/wait.
+			return trace.Wrap(killAndWaitProcess(cmd))
+		}
+		return trace.NewAggregate(err, killAndWaitProcess(cmd))
+	case <-ctx.Done():
+		return trace.NewAggregate(ctx.Err(), killAndWaitProcess(cmd))
+	}
+}
+
+func killAndWaitProcess(cmd *exec.Cmd) error {
+	if err := cmd.Process.Kill(); err != nil {
+		return trace.Wrap(err)
+	}
+	err := cmd.Wait()
+	var execErr *exec.ExitError
+	if errors.As(err, &execErr) && execErr.ExitCode() != 0 {
+		return trace.Wrap(err)
+	} else if err != nil && strings.Contains(err.Error(), "signal: killed") {
+		// If the process was successfully killed, there is no issue.
+		return nil
+	}
+	return trace.Wrap(err)
+}

lib/client/reexec/reexec_darwin.go
@@ -1,0 +1,43 @@
+// Teleport
+// Copyright (C) 2025 Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//go:build darwin
+
+package reexec
+
+import (
+	"os"
+	"os/exec"
+	"syscall"
+
+	"github.com/gravitational/trace"
+)
+
+// getExecutable gets the path to the executable that should be used for re-exec.
+func getExecutable() (string, error) {
+	executable, err := os.Executable()
+	return executable, trace.Wrap(err)
+}
+
+// configureReexecForOS configures the command with files to inherit and
+// os-specific tweaks.
+func configureReexecForOS(cmd *exec.Cmd, signal, kill *os.File) (signalFd, killFd uint64) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setsid: true,
+	}
+	cmd.ExtraFiles = []*os.File{signal, kill}
+	return 3, 4
+}

lib/client/reexec/reexec_linux.go
@@ -1,0 +1,40 @@
+// Teleport
+// Copyright (C) 2025 Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//go:build linux
+
+package reexec
+
+import (
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+// getExecutable gets the path to the executable that should be used for re-exec.
+func getExecutable() (string, error) {
+	return "/proc/self/exe", nil
+}
+
+// configureReexecForOS configures the command with files to inherit and
+// os-specific tweaks.
+func configureReexecForOS(cmd *exec.Cmd, signal, kill *os.File) (signalFd, killFd uint64) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setsid: true,
+	}
+	cmd.ExtraFiles = []*os.File{signal, kill}
+	return 3, 4
+}

lib/client/reexec/reexec_test.go
@@ -1,0 +1,185 @@
+// Teleport
+// Copyright (C) 2025 Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package reexec
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os/exec"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/gravitational/teleport/lib/utils/testutils"
+)
+
+type syncBuffer struct {
+	buf *bytes.Buffer
+	mu  sync.Mutex
+}
+
+func newSyncBuffer() *syncBuffer {
+	return &syncBuffer{
+		buf: &bytes.Buffer{},
+	}
+}
+
+func (rw *syncBuffer) Read(b []byte) (int, error) {
+	rw.mu.Lock()
+	defer rw.mu.Unlock()
+	return rw.buf.Read(b)
+}
+
+func (rw *syncBuffer) Write(b []byte) (int, error) {
+	rw.mu.Lock()
+	defer rw.mu.Unlock()
+	return rw.buf.Write(b)
+}
+
+func (rw *syncBuffer) String() string {
+	rw.mu.Lock()
+	defer rw.mu.Unlock()
+	return rw.buf.String()
+}
+
+func TestRunForkAuthenticate(t *testing.T) {
+	t.Parallel()
+
+	t.Run("child disowns successfully", func(t *testing.T) {
+		t.Parallel()
+		const script = `
+		read
+		# Close signal fd.
+		echo x >&%d
+		exec %d>&-
+		# stdout/err should still work.
+		echo "stdout: $REPLY"
+		echo "stderr: $REPLY" >&2
+		# Wait to ensure the fd closure is caught before the process ends.
+		sleep 1
+		`
+		getArgs := func(signalFd, killFd uint64) []string {
+			return []string{"-c", fmt.Sprintf(script, signalFd, signalFd)}
+		}
+		stdout := newSyncBuffer()
+		stderr := newSyncBuffer()
+		params := ForkAuthenticateParams{
+			GetArgs:    getArgs,
+			executable: "bash",
+			Stdin:      bytes.NewBufferString("hello\n"),
+			Stdout:     stdout,
+			Stderr:     stderr,
+		}
+
+		err := RunForkAuthenticate(t.Context(), params)
+		assert.NoError(t, err)
+		assert.EventuallyWithT(t, func(t *assert.CollectT) {
+			assert.Equal(t, "stdout: hello\n", stdout.String())
+			assert.Equal(t, "stderr: hello\n", stderr.String())
+		}, 10*time.Second, 100*time.Millisecond)
+	})
+
+	t.Run("child exits with error", func(t *testing.T) {
+		t.Parallel()
+		const script = `
+		# Make sure stdin/out/err work.
+		read
+		echo "stdout: $REPLY"
+		echo "stderr: $REPLY" >&2
+		# Exit with error.
+		exit 42
+		`
+		getArgs := func(signalFd, killFd uint64) []string {
+			return []string{"-c", script}
+		}
+		stdout := newSyncBuffer()
+		stderr := newSyncBuffer()
+		params := ForkAuthenticateParams{
+			GetArgs:    getArgs,
+			executable: "bash",
+			Stdin:      bytes.NewBufferString("hello\n"),
+			Stdout:     stdout,
+			Stderr:     stderr,
+		}
+
+		err := RunForkAuthenticate(t.Context(), params)
+		var execErr *exec.ExitError
+		if assert.ErrorAs(t, err, &execErr) {
+			assert.Equal(t, 42, execErr.ExitCode())
+		}
+		assert.Equal(t, "stdout: hello\n", stdout.String())
+		assert.Equal(t, "stderr: hello\n", stderr.String())
+	})
+
+	t.Run("context canceled", func(t *testing.T) {
+		t.Parallel()
+		getArgs := func(_, _ uint64) []string {
+			return []string{"-c", `
+			# Make sure stdin/out/err work.
+			read
+			echo "stdout: $REPLY"
+			echo "stderr: $REPLY" >&2
+			# wait for cancellation
+			sleep 2
+			# should not be executed
+			echo "extra output"
+			`}
+		}
+		stdout := newSyncBuffer()
+		stderr := newSyncBuffer()
+		params := ForkAuthenticateParams{
+			GetArgs:    getArgs,
+			executable: "bash",
+			Stdin:      bytes.NewBufferString("hello\n"),
+			Stdout:     stdout,
+			Stderr:     stderr,
+		}
+		ctx, cancel := context.WithCancel(t.Context())
+		t.Cleanup(cancel)
+
+		errorCh := make(chan error, 1)
+		testutils.RunTestBackgroundTask(ctx, t, &testutils.TestBackgroundTask{
+			Name: "RunForkAuthenticate",
+			Task: func(ctx context.Context) error {
+				errorCh <- RunForkAuthenticate(ctx, params)
+				return nil
+			},
+		})
+
+		require.EventuallyWithT(t, func(t *assert.CollectT) {
+			assert.Equal(t, "stdout: hello\n", stdout.String())
+			assert.Equal(t, "stderr: hello\n", stderr.String())
+		}, 10*time.Second, 100*time.Millisecond)
+
+		cancel()
+		select {
+		case err := <-errorCh:
+			require.ErrorIs(t, err, context.Canceled)
+		case <-time.After(5 * time.Second):
+			require.Fail(t, "timed out waiting for child to finish")
+		}
+
+		require.Never(t, func() bool {
+			return strings.Contains(stdout.String(), "extra output")
+		}, 3*time.Second, time.Second)
+	})
+}

lib/client/reexec/reexec_windows.go
@@ -1,0 +1,48 @@
+// Teleport
+// Copyright (C) 2025 Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//go:build windows
+
+package reexec
+
+import (
+	"os"
+	"os/exec"
+	"runtime"
+	"syscall"
+
+	"github.com/gravitational/trace"
+)
+
+// getExecutable gets the path to the executable that should be used for re-exec.
+func getExecutable() (string, error) {
+	executable, err := os.Executable()
+	return executable, trace.Wrap(err)
+}
+
+// configureReexecForOS configures the command with files to inherit and
+// os-specific tweaks.
+func configureReexecForOS(cmd *exec.Cmd, signal, kill *os.File) (signalFd, killFd uint64) {
+	// Prevent handle from being closed when signal is garbage collected.
+	runtime.SetFinalizer(signal, nil)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		AdditionalInheritedHandles: []syscall.Handle{
+			syscall.Handle(signal.Fd()),
+			syscall.Handle(kill.Fd()),
+		},
+	}
+	return uint64(signal.Fd()), uint64(kill.Fd())
+}

tool/tsh/common/reexec_windows.go
@@ -1,0 +1,45 @@
+// Teleport
+// Copyright (C) 2025 Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//go:build windows
+
+package common
+
+import (
+	"os"
+	"syscall"
+
+	"github.com/gravitational/trace"
+)
+
+func isValidForkSignalFd(fd uint64) bool {
+	// Don't allow NULL.
+	return fd != 0
+}
+
+// newSignalFile creates a signaling file for --fork-after-authentication from
+// a file descriptor.
+func newSignalFile(fd uint64) *os.File {
+	syscall.CloseOnExec(syscall.Handle(fd))
+	return os.NewFile(uintptr(fd), "disown")
+}
+
+// replaceStdin returns a file for /dev/null that should be used from now
+// on instead of stdin.
+func replaceStdin() (*os.File, error) {
+	devNull, err := os.Open(os.DevNull)
+	return devNull, trace.Wrap(err)
+}

commit 8ac1321075ea4f215da4c80e0ffdbb9af0f73d8c

tsh: Add wrapper for syscall.Dup2 for linux/arm64
Add a wrapper for `syscall.Dup2()` as linux ARM64 does not have that
syscall. On that platform, `syscall.Dup3()` needs to be used instead.

Fixes: 57c909dab1b8f7f0a8ba7e3199a902775f82f42a

tool/tsh/common/dup2_linux_arm64.go
@@ -1,0 +1,34 @@
+// Teleport
+// Copyright (C) 2025 Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//go:build linux && arm64
+
+package common
+
+import "syscall"
+
+// dup2 implements syscall.Dup2(oldfd, newfd) on Linux ARM64, as that platform
+// does not have syscall.Dup2(). Instead syscall.Dup3() must be used.
+// syscall.Dup3() is not available on all unix platforms so it cannot be used
+// unconditionally.
+func dup2(oldfd, newfd int) error {
+	if oldfd == newfd {
+		// dup2 would do nothing in this case, but dup3 returns an error.
+		// Emulate dup2 behavior.
+		return nil
+	}
+	return syscall.Dup3(oldfd, newfd, 0)
+}

tool/tsh/common/dup2_unix.go
@@ -1,0 +1,27 @@
+// Teleport
+// Copyright (C) 2025 Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//go:build !(linux && arm64)
+
+package common
+
+import "syscall"
+
+// dup2 wraps syscall.Dup2(oldfd, newfd) on platforms that have it, allowing
+// platforms that do not to implement dup2() with syscall.Dup3() instead.
+func dup2(oldfd, newfd int) error {
+	return syscall.Dup2(oldfd, newfd)
+}

tool/tsh/common/reexec_unix.go
@@ -51,7 +51,7 @@ func replaceStdin() (*os.File, error) {
 	}
 	var dupErr error
 	if ctrlErr := rc.Control(func(fd uintptr) {
-		dupErr = syscall.Dup2(int(fd), syscall.Stdin)
+		dupErr = dup2(int(fd), syscall.Stdin)
 		// stdin is not O_CLOEXEC after dup2 but thankfully the three stdio
 		// file descriptors must be not O_CLOEXEC anyway, so we can avoid
 		// a linux-specific implementation or syscall.ForkLock shenanigans
@@ -60,7 +60,7 @@ func replaceStdin() (*os.File, error) {
 		return nil, trace.Wrap(ctrlErr)
 	}
 	if dupErr != nil {
-		// this is the error from Dup2
+		// this is the error from dup2
 		_ = devNull.Close()
 		return nil, trace.Wrap(err)
 	}