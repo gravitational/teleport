66b82a133685de70a46266f51d76d6857cc97863
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2025-06-23 15:43:07 +0200

    Skeleton for `relay_service` and heartbeat (#55782)
    * Add Relay system role
    
    * Add relay_service skeleton
    
    * Proto definitions for relay heartbeats in the inventory stream
    
    * make grpc
    
    * Relay heartbeats through the inventory
    
    * Allow RoleRelay to not have audit log write permissions for now
    
    * godoc for top level config structs

--- lib/service/service.go
+++ lib/service/service.go
@@ -190,6 +190,11 @@ const (
 	// identity has been registered with the Auth Server.
 	ProxyIdentityEvent = "ProxyIdentity"
 
+	// RelayIdentityEvent is generated after the Teleport agent has successfully
+	// joined a cluster or reused credentials to connect to the control plane as
+	// a relay service.
+	RelayIdentityEvent = "RelayIdentityEvent"
+
 	// SSHIdentityEvent is generated when node's identity has been registered
 	// with the Auth Server.
 	SSHIdentityEvent = "SSHIdentity"
@@ -238,6 +243,10 @@ const (
 	// ProxySSHReady is generated when the proxy has initialized a SSH server
 	// and is ready to start accepting connections.
 	ProxySSHReady = "ProxySSHReady"
+
+	// RelayReady is generated when a relay service is ready to accept
+	// connections.
+	RelayReady = "RelayReady"
 
 	// NodeSSHReady is generated when the Teleport node has initialized a SSH server
 	// and is ready to start accepting SSH connections.
@@ -1450,6 +1459,9 @@ func NewTeleport(cfg *servicecfg.Config) (_ *TeleportProcess, err error) {
 	if cfg.Proxy.Enabled {
 		eventMapping.In = append(eventMapping.In, ProxySSHReady)
 	}
+	if cfg.Relay.Enabled {
+		eventMapping.In = append(eventMapping.In, RelayReady)
+	}
 	if cfg.Kube.Enabled {
 		eventMapping.In = append(eventMapping.In, KubernetesReady)
 	}
@@ -1506,6 +1518,13 @@ func NewTeleport(cfg *servicecfg.Config) (_ *TeleportProcess, err error) {
 		serviceStarted = true
 	} else {
 		warnOnErr(process.ExitContext(), process.closeImportedDescriptors(teleport.ComponentProxy), process.logger)
+	}
+
+	if cfg.Relay.Enabled {
+		process.initRelay()
+		serviceStarted = true
+	} else {
+		warnOnErr(process.ExitContext(), process.closeImportedDescriptors(teleport.ComponentRelay), process.logger)
 	}
 
 	if cfg.Kube.Enabled {
@@ -3932,6 +3951,8 @@ func (process *TeleportProcess) getAdditionalPrincipals(role types.SystemRole) (
 				}
 			}
 		}
+	case types.RoleRelay:
+		dnsNames = append(dnsNames, process.Config.Relay.APIPublicHostnames...)
 	case types.RoleAuth, types.RoleAdmin:
 		addrs = process.Config.Auth.PublicAddrs
 	case types.RoleNode:
@@ -6044,6 +6065,10 @@ func (process *TeleportProcess) registerExpectedServices(cfg *servicecfg.Config)
 
 	if cfg.Proxy.Enabled {
 		process.SetExpectedInstanceRole(types.RoleProxy, ProxyIdentityEvent)
+	}
+
+	if cfg.Relay.Enabled {
+		process.SetExpectedInstanceRole(types.RoleRelay, RelayIdentityEvent)
 	}
 
 	if cfg.Kube.Enabled {
