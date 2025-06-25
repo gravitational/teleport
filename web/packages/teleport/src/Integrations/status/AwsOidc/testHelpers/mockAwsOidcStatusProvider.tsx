56c1093886cd794378bcd05b165a4e87d5ba4c9a
Michelle Bergquist <michelle.bergquist@goteleport.com>
2025-06-25 13:09:37 -0600

    Update impact reponses for eks/rds
    - Fix storybook for aws tasks

--- web/packages/teleport/src/Integrations/status/AwsOidc/testHelpers/mockAwsOidcStatusProvider.tsx
+++ web/packages/teleport/src/Integrations/status/AwsOidc/testHelpers/mockAwsOidcStatusProvider.tsx
@@ -16,8 +16,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import { createMemoryHistory } from 'history';
 import React from 'react';
-import { MemoryRouter } from 'react-router';
+import { Router } from 'react-router';
 
 import { InfoGuidePanelProvider } from 'shared/components/SlidingSidePanel/InfoGuide';
 
@@ -41,16 +42,25 @@ export const MockAwsOidcStatusProvider = ({
   initialEntries?: string[];
 }) => {
   const ctx = createTeleportContext();
+  const history = createMemoryHistory({
+    initialEntries: initialEntries,
+    initialIndex: 0,
+  });
 
   return (
-    <MemoryRouter initialEntries={initialEntries}>
+    <Router history={history}>
       <ContextProvider ctx={ctx}>
         <InfoGuidePanelProvider>
           <awsOidcStatusContext.Provider value={value}>
-            <Route path={path} render={() => <>{children}</>} />
+            <Route
+              path={path}
+              render={() => (
+                <span key={history.location.search}>{children}</span>
+              )}
+            />
           </awsOidcStatusContext.Provider>
         </InfoGuidePanelProvider>
       </ContextProvider>
-    </MemoryRouter>
+    </Router>
   );
 };
