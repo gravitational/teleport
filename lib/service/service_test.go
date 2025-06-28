69c910f8637ed9e47ed5f7754811a775a726fa90
Tiago Silva <tiago.silva@goteleport.com>
2025-06-28 19:09:58 +0100

    chore: remove `host_uuid` local storage compatibility layer
    This PR removes the compatibility layer introduced in Teleport 11 to support converting existing deployments that used storage to a the Kubernetes Secret storage used after Teleport 11.
    
    This logic reads the data from the local file and re-stores it in Kubernetes secret if the secret doesn't have the `host_uuid` entry set. This behaviour should be sane because we re-get the secret to ensure it's not stored that but in reality this is no longer needed since Teleport 11 and should. be removed to ensure this isn't the cause of racy behaviour.
    
    Signed-off-by: Tiago Silva <tiago.silva@goteleport.com>

--- lib/service/service_test.go
+++ lib/service/service_test.go
@@ -1107,25 +1107,6 @@ func Test_readOrGenerateHostID(t *testing.T) {
 			},
 		},
 		{
-			name: "Kube Backend is available but key is missing. Load from local storage and store in lube",
-			args: args{
-				kubeBackend: &fakeKubeBackend{
-					getData: nil,
-					getErr:  fmt.Errorf("key not found"),
-				},
-				hostIDContent: id,
-			},
-			wantFunc: func(receivedID string) bool {
-				return receivedID == id
-			},
-			wantKubeItemFunc: func(i *backend.Item) bool {
-				return cmp.Diff(&backend.Item{
-					Key:   []byte(hostUUIDKey),
-					Value: []byte(id),
-				}, i) == ""
-			},
-		},
-		{
 			name: "Kube Backend is available with key. Load from kube storage",
 			args: args{
 				kubeBackend: &fakeKubeBackend{
