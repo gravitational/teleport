463b3d94afe182a9f787be5645a08bb824a819a7
Tim Ross <tim.ross@goteleport.com>
2025-06-30 21:56:14 -0400

    Prevent leaking the tls config generator

--- lib/service/service.go
+++ lib/service/service.go
@@ -5378,6 +5378,10 @@ func (process *TeleportProcess) initProxyEndpoint(conn *Connector) error {
 	if err != nil {
 		return trace.Wrap(err)
 	}
+	process.OnExit("tls.config.generator", func(a any) {
+		clientTLSConfigGenerator.Close()
+	})
+
 	sshGRPCTLSConfig.GetConfigForClient = clientTLSConfigGenerator.GetConfigForClient
 
 	sshGRPCCreds, err := auth.NewTransportCredentials(auth.TransportCredentialsConfig{
@@ -5998,7 +6002,7 @@ func (process *TeleportProcess) setupTLSConfigClientCAGeneratorForCluster(tlsCon
 		return trace.Wrap(err)
 	}
 
-	process.OnExit("closer", func(payload any) {
+	process.OnExit("tls.config.generator", func(payload any) {
 		generator.Close()
 	})
 
