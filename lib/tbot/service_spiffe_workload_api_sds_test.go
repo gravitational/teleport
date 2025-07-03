66aaeb6bc949842b4b5f912703a443eb59dfe175
Dan Upton <daniel.upton@goteleport.com>
2025-07-03 22:47:11 +0100

    fixup sds and spiffe

--- lib/tbot/service_spiffe_workload_api_sds_test.go
+++ lib/tbot/service_spiffe_workload_api_sds_test.go
@@ -24,7 +24,6 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
-	"log/slog"
 	"os"
 	"path"
 	"path/filepath"
@@ -34,28 +33,18 @@ import (
 	tlsv3pb "github.com/envoyproxy/go-control-plane/envoy/extensions/transport_sockets/tls/v3"
 	discoveryv3pb "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
 	secretv3pb "github.com/envoyproxy/go-control-plane/envoy/service/secret/v3"
-	"github.com/google/go-cmp/cmp"
-	"github.com/spiffe/go-spiffe/v2/bundle/spiffebundle"
-	workloadpb "github.com/spiffe/go-spiffe/v2/proto/spiffe/workload"
-	"github.com/spiffe/go-spiffe/v2/spiffeid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/protobuf/encoding/protojson"
-	"google.golang.org/protobuf/testing/protocmp"
 	"google.golang.org/protobuf/types/known/anypb"
 
 	"github.com/gravitational/teleport/api/types"
-	"github.com/gravitational/teleport/lib/fixtures"
-	"github.com/gravitational/teleport/lib/tbot/bot"
 	"github.com/gravitational/teleport/lib/tbot/config"
-	"github.com/gravitational/teleport/lib/tbot/internal/sds"
 	"github.com/gravitational/teleport/lib/tbot/services/legacyspiffe"
 	"github.com/gravitational/teleport/lib/tbot/workloadidentity"
 	"github.com/gravitational/teleport/lib/utils"
 	"github.com/gravitational/teleport/lib/utils/testutils"
-	"github.com/gravitational/teleport/lib/utils/testutils/golden"
 	"github.com/gravitational/teleport/tool/teleport/testenv"
 )
 
@@ -65,150 +54,6 @@ type mockTrustBundleCache struct {
 
 func (m *mockTrustBundleCache) GetBundleSet(ctx context.Context) (*workloadidentity.BundleSet, error) {
 	return m.currentBundle, nil
-}
-
-// TestSDS_FetchSecrets performs a unit-test over the FetchSecrets method.
-// It tests the generation of the DiscoveryResponses and that authentication
-// is enforced.
-func TestSDS_FetchSecrets(t *testing.T) {
-	log := utils.NewSlogLoggerForTests()
-	ctx := context.Background()
-
-	td, err := spiffeid.TrustDomainFromString("example.com")
-	require.NoError(t, err)
-
-	b, _ := pem.Decode([]byte(fixtures.TLSCACertPEM))
-	require.NotNil(t, b, "Decode failed")
-	ca, err := x509.ParseCertificate(b.Bytes)
-	require.NoError(t, err)
-
-	clientAuthenticator := func(ctx context.Context) (*slog.Logger, sds.SVIDFetcher, error) {
-		return log, func(ctx context.Context, localBundle *spiffebundle.Bundle) ([]*workloadpb.X509SVID, error) {
-			return []*workloadpb.X509SVID{
-				{
-					SpiffeId:    "spiffe://example.com/default",
-					X509Svid:    []byte("CERT-spiffe://example.com/default"),
-					X509SvidKey: []byte("KEY-spiffe://example.com/default"),
-					Bundle:      workloadidentity.MarshalX509Bundle(localBundle.X509Bundle()),
-				},
-				{
-					SpiffeId:    "spiffe://example.com/second",
-					X509Svid:    []byte("CERT-spiffe://example.com/second"),
-					X509SvidKey: []byte("KEY-spiffe://example.com/second"),
-					Bundle:      workloadidentity.MarshalX509Bundle(localBundle.X509Bundle()),
-				},
-			}, nil
-		}, nil
-	}
-
-	bundle := spiffebundle.New(td)
-	bundle.AddX509Authority(ca)
-
-	federatedBundle := spiffebundle.New(spiffeid.RequireTrustDomainFromString("federated.example.com"))
-	federatedBundle.AddX509Authority(ca)
-
-	mockBundleCache := &mockTrustBundleCache{
-		currentBundle: &workloadidentity.BundleSet{
-			Local: bundle,
-			Federated: map[string]*spiffebundle.Bundle{
-				"federated.example.com": federatedBundle,
-			},
-		},
-	}
-	botConfig := &config.BotConfig{
-		CredentialLifetime: bot.CredentialLifetime{
-			RenewalInterval: time.Minute,
-		},
-	}
-
-	tests := []struct {
-		name string
-
-		svids         []legacyspiffe.SVIDRequestWithRules
-		resourceNames []string
-
-		wantErr string
-	}{
-		{
-			name:          "all",
-			resourceNames: []string{},
-		},
-		{
-			name: "specific svid",
-			resourceNames: []string{
-				"spiffe://example.com/second",
-			},
-		},
-		{
-			name: "specific ca",
-			resourceNames: []string{
-				"spiffe://example.com",
-			},
-		},
-		{
-			name: "specific ca federated",
-			resourceNames: []string{
-				"spiffe://federated.example.com",
-			},
-		},
-		{
-			name: "special default",
-			resourceNames: []string{
-				sds.EnvoyDefaultSVIDName,
-			},
-		},
-		{
-			name: "special ROOTCA",
-			resourceNames: []string{
-				sds.EnvoyDefaultBundleName,
-			},
-		},
-		{
-			name: "special ALL",
-			resourceNames: []string{
-				sds.EnvoyAllBundlesName,
-			},
-		},
-		{
-			name:          "no results",
-			resourceNames: []string{"spiffe://example.com/not-matching"},
-			wantErr:       "unknown resource names: [spiffe://example.com/not-matching]",
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			sdsHandler := sds.NewHandler(sds.HandlerConfig{
-				Logger:              log,
-				BotConfig:           botConfig,
-				TrustBundleCache:    mockBundleCache,
-				ClientAuthenticator: clientAuthenticator,
-			})
-
-			req := &discoveryv3pb.DiscoveryRequest{
-				TypeUrl:       "type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.Secret",
-				ResourceNames: tt.resourceNames,
-			}
-
-			res, err := sdsHandler.FetchSecrets(ctx, req)
-			if tt.wantErr != "" {
-				require.ErrorContains(t, err, tt.wantErr)
-				return
-			}
-			require.NoError(t, err)
-			if golden.ShouldSet() {
-				resBytes, err := protojson.MarshalOptions{
-					Multiline: true,
-				}.Marshal(res)
-				require.NoError(t, err)
-				golden.Set(t, resBytes)
-			}
-
-			want := &discoveryv3pb.DiscoveryResponse{}
-			require.NoError(t, protojson.UnmarshalOptions{}.Unmarshal(golden.Get(t), want))
-			require.Empty(t, cmp.Diff(res, want, protocmp.Transform()))
-		})
-	}
 }
 
 // Test_E2E_SPIFFE_SDS is an end-to-end test of Workload ID's ability
