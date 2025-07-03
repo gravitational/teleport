721b7d72f944be1607d359d6d4351e29f653a471
Bernard Kim <bernard@goteleport.com>
2025-07-03 12:32:07 -0700

    Update domain sending key link (#6724)
    Source-commit: gravitational/teleport.e@fde980cc1e83b1f56f0e54efad2241643846413b
    Source-pull-requests: gravitational/teleport.e#6724

--- e/web/teleport/src/Integrations/IntegrationEnroll/PluginEnroll/MultiStep/Email/EmailService.tsx
+++ e/web/teleport/src/Integrations/IntegrationEnroll/PluginEnroll/MultiStep/Email/EmailService.tsx
@@ -86,7 +86,7 @@ export function EmailService() {
           Create a Mailgun private key by following the{' '}
           <Link
             target="_blank"
-            href="https://documentation.mailgun.com/docs/mailgun/user-manual/get-started/#domain-sending-keys"
+            href="https://documentation.mailgun.com/docs/mailgun/api-reference/authentication/#domain-sending-keys"
           >
             Domain Sending Keys
           </Link>{' '}
