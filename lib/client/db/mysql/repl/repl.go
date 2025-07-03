b52afd51724b7b1fe66f0a2d341e864b2f8a0a19
Gavin Frazar <gavin.frazar@goteleport.com>
2025-07-03 12:41:07 -0700

    Add MySQL REPL to the web UI

---
+++ lib/client/db/mysql/repl/repl.go
@@ -0,0 +1,347 @@
+// Teleport
+// Copyright (C) 2025 Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package repl
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"iter"
+	"net"
+	"strings"
+
+	"github.com/go-mysql-org/go-mysql/client"
+	"github.com/go-mysql-org/go-mysql/mysql"
+	"github.com/gravitational/trace"
+	"golang.org/x/term"
+
+	"github.com/gravitational/teleport"
+	clientproto "github.com/gravitational/teleport/api/client/proto"
+	"github.com/gravitational/teleport/lib/asciitable"
+	dbrepl "github.com/gravitational/teleport/lib/client/db/repl"
+)
+
+type REPL struct {
+	client     io.ReadWriteCloser
+	serverConn net.Conn
+	route      clientproto.RouteToDatabase
+	term       *term.Terminal
+	commands   *commandManager
+	myConn     mysqlConn
+	lex        lexer
+
+	// testPassword is normally blank, only used in tests where the REPL connects
+	// directly to a MySQL instance without Teleport proxying.
+	testPassword string
+}
+
+type mysqlConn interface {
+	Execute(command string, args ...any) (*mysql.Result, error)
+	UseDB(dbName string) error
+}
+
+func New(_ context.Context, cfg *dbrepl.NewREPLConfig) (dbrepl.REPLInstance, error) {
+	cmds, err := newCommands()
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	return &REPL{
+		client:     cfg.Client,
+		serverConn: cfg.ServerConn,
+		route:      cfg.Route,
+		term:       term.NewTerminal(cfg.Client, ""),
+		commands:   cmds,
+	}, nil
+}
+
+// Run starts and run the PostgreSQL REPL session. The provided context is used
+// to interrupt the execution and clean up resources.
+func (r *REPL) Run(ctx context.Context) error {
+	dialer := func(context.Context, string, string) (net.Conn, error) {
+		return r.serverConn, nil
+	}
+	const hostnamePlaceholder = "repl"
+	myConn, err := client.ConnectWithDialer(ctx, "tcp", hostnamePlaceholder,
+		r.route.GetUsername(),
+		r.testPassword,
+		r.route.GetDatabase(),
+		dialer,
+		withClientCapabilities(
+			mysql.CLIENT_MULTI_RESULTS,
+			mysql.CLIENT_MULTI_STATEMENTS,
+		),
+		func(c *client.Conn) error {
+			c.SetAttributes(map[string]string{clientNameParamName: clientNameParamValue})
+			return nil
+		},
+	)
+	if err != nil {
+		return trace.ConnectionProblem(err, "Unable to connect to database: %v", err)
+	}
+	r.myConn = myConn
+	defer func() {
+		_ = myConn.Close()
+	}()
+
+	// term.Terminal blocks reads/writes without respecting the context. The
+	// only thing that unblocks it is closing the underlaying connection (in
+	// our case r.client). On this goroutine we only watch for context
+	// cancelation and close the connection. This will unblocks all terminal
+	// reads/writes.
+	stop := context.AfterFunc(ctx, func() {
+		_ = r.client.Close()
+	})
+	defer stop()
+
+	if err := r.presentBanner(); err != nil {
+		return trace.Wrap(err)
+	}
+
+	for {
+		line, err := r.readLine()
+		if err != nil {
+			return trace.Wrap(rewriteTermError(ctx, err))
+		}
+		for reply, exit := range r.eval(line) {
+			if exit {
+				return nil
+			}
+			if err := r.print(reply); err != nil {
+				return trace.Wrap(err)
+			}
+		}
+	}
+}
+
+func withClientCapabilities(caps ...uint32) client.Option {
+	return func(conn *client.Conn) error {
+		for _, cap := range caps {
+			conn.SetCapability(cap)
+		}
+		return nil
+	}
+}
+
+func (r *REPL) presentBanner() error {
+	_, err := fmt.Fprintf(
+		r.term,
+		`Teleport MySQL interactive shell (v%s)
+Connected to instance %q as user %q.
+Type 'help' or '\h' for help.
+
+`,
+		teleport.Version,
+		r.route.GetServiceName(),
+		r.route.GetUsername())
+	return trace.Wrap(err)
+}
+
+func (r *REPL) readLine() (string, error) {
+	r.term.SetPrompt(r.getPrompt())
+	return r.term.ReadLine()
+}
+
+func (r *REPL) getPrompt() string {
+	dbName := formatDatabaseName(r.route.Database)
+	var suffix string
+	switch {
+	case r.lex.isInComment():
+		suffix = "/*"
+	case r.lex.isInString():
+		suffix = r.lex.inStringKind()
+	case r.lex.isInQuery():
+		suffix = "-"
+	default:
+		suffix = dbName
+	}
+	padding := len(dbName)
+	return fmt.Sprintf("%*s> ", padding, suffix)
+}
+
+func (r *REPL) eval(line string) iter.Seq2[string, bool] {
+	r.lex.setLine(line)
+	return func(yield func(string, bool) bool) {
+		for !r.lex.isEmpty() {
+			switch {
+			case r.lex.isInComment():
+				r.lex.discardMultiLineComment()
+			case r.lex.isInString():
+				r.lex.acceptString()
+			case !r.lex.isInQuery():
+				if !yield(r.parseCommand()) {
+					return
+				}
+			default:
+				if !yield(r.parseQuery()) {
+					return
+				}
+			}
+		}
+		if r.lex.isInQuery() {
+			r.lex.writeString(lineBreak)
+		}
+	}
+}
+
+// print writes a reply to the REPL client.
+func (r *REPL) print(reply string) error {
+	reply = strings.TrimSpace(reply)
+	if reply == "" {
+		return nil
+	}
+	_, err := r.term.Write([]byte(lineBreak + reply + lineBreak + lineBreak))
+	return trace.Wrap(err)
+}
+
+// parseCommand looks for a command and its args on the current line, executing
+// the command and returning a client reply and whether the REPL should exit.
+// If the line contains the current delimiter and the command is not the special
+// DELIMITER command, then it must be parsed as a potential query instead.
+func (r *REPL) parseCommand() (string, bool) {
+	r.lex.discardWhitespace()
+	line := r.lex.peekString()
+	cmd, args, err := r.commands.findCommand(line)
+	switch {
+	case err != nil:
+		r.lex.discardRemaining()
+		return err.Error(), false
+	case cmd != nil:
+		if cmd.name != "delimiter" && strings.Contains(line, r.lex.delimiter()) {
+			return r.parseQuery()
+		}
+		r.lex.discardRemaining()
+		return cmd.execFunc(r, args)
+	default:
+		return r.parseQuery()
+	}
+}
+
+// parseQuery looks for a query in the input, executing the query and returning
+// a client reply and whether the REPL should exit.
+func (r *REPL) parseQuery() (string, bool) {
+	if r.lex.advanceByDelimiter() {
+		query := r.lex.getQuery()
+		if !r.lex.isMultilineQuery() || strings.HasPrefix(query, r.commands.shortcutPrefix) {
+			cmd, args, err := r.commands.findCommand(query)
+			switch {
+			case err != nil:
+				return err.Error(), false
+			case cmd != nil:
+				return cmd.execFunc(r, args)
+			}
+		}
+		return r.runStatement(query), false
+	}
+
+	tok := r.lex.scan()
+	switch tok.kind {
+	case tokenSingleComment:
+		// intentionally skip writing out comments to our query buffer
+		r.lex.discardSingleLineComment()
+	case tokenOpenComment:
+		r.lex.setOpenComment()
+		r.lex.discardMultiLineComment()
+	case tokenBackslash:
+		r.lex.writeString(tok.text)
+		r.lex.acceptEscapedRune()
+	case tokenSingleQuote, tokenDoubleQuote, tokenBacktick:
+		r.lex.writeString(tok.text)
+		r.lex.setOpenString(tok)
+		r.lex.acceptString()
+	default:
+		r.lex.writeString(tok.text)
+	}
+	return "", false
+}
+
+// runStatement executes a single statement and formats the response as a reply
+// to the REPL client.
+func (r *REPL) runStatement(statement string) string {
+	result, err := r.myConn.Execute(statement)
+	if err != nil {
+		return errorReplyPrefix + err.Error()
+	}
+	defer result.Close()
+	reply := formatResult(result)
+	return reply
+}
+
+// rewriteTermError changes the term.Terminal error to match caller expectations.
+func rewriteTermError(ctx context.Context, err error) error {
+	// When context is canceled it will immediately lead read/write errors due
+	// to the closed connection. For this cases we return the context error.
+	if ctx.Err() != nil && (errors.Is(err, io.ErrClosedPipe) || errors.Is(err, net.ErrClosed)) {
+		return ctx.Err()
+	}
+	if errors.Is(err, io.EOF) {
+		return nil
+	}
+
+	return err
+}
+
+// formatResult formats a query result.
+func formatResult(result *mysql.Result) string {
+	if result.Resultset != nil {
+		return lineBreak + formatResultset(result.Resultset)
+	}
+	return lineBreak + fmt.Sprintf("%v warnings, %v rows affected", result.Warnings, result.AffectedRows)
+}
+
+// formatResultset formats a query result that contains rows.
+func formatResultset(set *mysql.Resultset) string {
+	if set == nil || len(set.Values) == 0 {
+		return "Empty set"
+	}
+	var sb strings.Builder
+	columns := make([]string, 0, len(set.Fields))
+	for _, f := range set.Fields {
+		columns = append(columns, string(f.Name))
+	}
+	table := asciitable.MakeTable(columns)
+	for _, rowValues := range set.Values {
+		row := make([]string, 0, len(rowValues))
+		for _, val := range rowValues {
+			row = append(row, val.String())
+		}
+		table.AddRow(row)
+	}
+	table.WriteTo(&sb)
+	sb.WriteString(lineBreak)
+
+	if numRows := len(set.Values); numRows == 1 {
+		sb.WriteString("(1 row in set)")
+	} else {
+		// pluralize 0 or multiple rows
+		fmt.Fprintf(&sb, "(%d rows in set)", numRows)
+	}
+	return sb.String()
+}
+
+const (
+	// lineBreak represents a line break on the REPL.
+	lineBreak = "\n"
+	// errorReplyPrefix is the prefix presented when there is a execution error.
+	errorReplyPrefix = "ERR "
+	// clientNameParamName defines the application name parameter name.
+	//
+	// https://dev.mysql.com/doc/refman/8.0/en/performance-schema-connection-attribute-tables.html
+	clientNameParamName = "_client_name"
+	// clientNameParamValue defines the application name parameter value.
+	clientNameParamValue = "teleport-repl"
+)
---
+++ lib/client/db/mysql/repl/repl_test.go
@@ -0,0 +1,361 @@
+// Teleport
+// Copyright (C) 2025 Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package repl
+
+import (
+	"bytes"
+	"cmp"
+	_ "embed"
+	"io"
+	"net"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/go-mysql-org/go-mysql/mysql"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	mysqlcontainer "github.com/testcontainers/testcontainers-go/modules/mysql"
+	"golang.org/x/term"
+
+	clientproto "github.com/gravitational/teleport/api/client/proto"
+	dbrepl "github.com/gravitational/teleport/lib/client/db/repl"
+	"github.com/gravitational/teleport/lib/defaults"
+	"github.com/gravitational/teleport/lib/utils"
+	"github.com/gravitational/teleport/lib/utils/testutils/golden"
+)
+
+func TestREPL(t *testing.T) {
+	testSrv := newTestServer(t)
+	route := clientproto.RouteToDatabase{
+		ServiceName: "mysql-test-container",
+		Protocol:    defaults.ProtocolMySQL,
+		Username:    testSrv.username,
+		// Pass a blank database name, as if a user who didn't specify one.
+		// The REPL should handle this case gracefully.
+		Database: "",
+	}
+	for _, test := range []struct {
+		name  string
+		input string
+	}{
+		{
+			name:  "ddl",
+			input: testDDL,
+		},
+		{
+			name:  "all data types",
+			input: testDataTypes,
+		},
+		{
+			name:  "literals",
+			input: testLiterals,
+		},
+		{
+			name:  "commands",
+			input: testCommands,
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			// the term only recognizes line breaks with a carriage-return
+			const crlf = "\r\n"
+			input := strings.ReplaceAll(test.input, "\n", crlf) + crlf
+			recorder := newRecordingClient(t, io.NopCloser(strings.NewReader(input)))
+			testREPL, err := New(t.Context(), &dbrepl.NewREPLConfig{
+				Client:     recorder,
+				ServerConn: testSrv.connectTCP(t, 10*time.Second),
+				Route:      route,
+			})
+			require.NoError(t, err)
+			testREPL.(*REPL).testPassword = testSrv.password
+			err = testREPL.Run(t.Context())
+			require.NoError(t, err)
+			goldenName := strings.ReplaceAll(test.name, " ", "-")
+			if golden.ShouldSet() {
+				golden.SetNamed(t, goldenName, recorder.buf.Bytes())
+			}
+			require.Equal(t, string(golden.GetNamed(t, goldenName)), recorder.buf.String())
+		})
+	}
+}
+
+var (
+	//go:embed fixtures/ddl.sql
+	testDDL string
+	//go:embed fixtures/all-data-types.sql
+	testDataTypes string
+	//go:embed fixtures/literals.sql
+	testLiterals string
+	//go:embed fixtures/commands.sql
+	testCommands string
+)
+
+func TestGetPrompt(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		desc         string
+		inQuery      bool
+		inComment    bool
+		inStringKind string
+		database     string
+		want         string
+	}{
+		{
+			desc: "no default database",
+			want: "(none)> ",
+		},
+		{
+			desc:     "default",
+			database: "llama",
+			want:     "llama> ",
+		},
+		{
+			desc:     "in a query",
+			inQuery:  true,
+			database: "llama",
+			want:     "    -> ",
+		},
+		{
+			desc:      "in a comment",
+			inComment: true,
+			database:  "llama",
+			want:      "   /*> ",
+		},
+		{
+			desc:         "in a quote string",
+			inStringKind: "'",
+			database:     "llama",
+			want:         "    '> ",
+		},
+		{
+			desc:         "in a double quote string",
+			inStringKind: `"`,
+			database:     "llama",
+			want:         `    "> `,
+		},
+		{
+			desc:         "in a backtick string",
+			inStringKind: "`",
+			database:     "llama",
+			want:         "    `> ",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			r := &REPL{
+				route: clientproto.RouteToDatabase{
+					Database: test.database,
+				},
+				lex: lexer{
+					inComment:     test.inComment,
+					inStringToken: token{text: test.inStringKind},
+				},
+			}
+			if test.inQuery {
+				r.lex.queryBuf.WriteString("select")
+			}
+			require.Equal(t, test.want, r.getPrompt())
+		})
+	}
+}
+
+func FuzzEval(f *testing.F) {
+	cmds, err := newCommands()
+	require.NoError(f, err)
+
+	for _, cmd := range cmds.byName {
+		f.Add(cmd.name)
+		f.Add(cmds.shortcutPrefix + string(cmd.shortcut))
+	}
+
+	f.Add("")  // eof
+	f.Add(" ") // space
+
+	// MySQL string literal quotes
+	f.Add("'")
+	f.Add(`"`)
+	f.Add("`")
+
+	// single line comments
+	f.Add("--")
+	f.Add("#")
+
+	// multiline comments
+	f.Add("/*")
+	f.Add("*/")
+	f.Add("*")
+
+	// valid delimiters
+	f.Add(";")
+	f.Add("$")
+	f.Add("/")
+
+	// some statement syntax
+	f.Add("select")
+	f.Add("create")
+	f.Add("drop")
+	f.Add("replace")
+	f.Add("alter")
+	f.Add("(")
+	f.Add(")")
+
+	// multiline
+	f.Add("\n")
+
+	f.Fuzz(func(t *testing.T, line string) {
+		repl := &REPL{
+			myConn: &fakeMySQLConn{
+				exec: func(command string, args ...any) (*mysql.Result, error) {
+					return nil, io.EOF
+				},
+			},
+			commands: cmds,
+		}
+		require.NotPanics(t, func() {
+			for line := range strings.SplitSeq(line, "\n") {
+				for _, exit := range repl.eval(line) {
+					if exit {
+						return
+					}
+				}
+			}
+		})
+	})
+}
+
+func TestInteractively(t *testing.T) {
+	// You can get an interactive REPL to experiment with like this:
+	// $ export MYSQL_TEST_REPL_INTERACTIVELY=1
+	// $ go test -c -o ./test ./lib/client/db/mysql/repl
+	// $ ./test -test.v -test.count=1 -test.run TestInteractively
+	if os.Getenv("MYSQL_TEST_REPL_INTERACTIVELY") == "" {
+		t.Skip()
+	}
+	testSrv := newTestServer(t)
+	route := clientproto.RouteToDatabase{
+		ServiceName: "mysql-test-container",
+		Protocol:    defaults.ProtocolMySQL,
+		Username:    testSrv.username,
+		Database:    testSrv.database,
+		Roles:       []string{},
+	}
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	require.NoError(t, err)
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+	recorder := newRecordingClient(t, os.Stdin, os.Stdout)
+	repl, err := New(t.Context(), &dbrepl.NewREPLConfig{
+		Client:     recorder,
+		ServerConn: testSrv.connectTCP(t, 10*time.Second),
+		Route:      route,
+	})
+	require.NoError(t, err)
+	repl.(*REPL).testPassword = testSrv.password
+	repl.Run(t.Context())
+}
+
+func mustCryptoRandomHex(t *testing.T, length int) string {
+	t.Helper()
+	out, err := utils.CryptoRandomHex(length)
+	require.NoError(t, err)
+	return out
+}
+
+func newTestServer(t *testing.T) *testServer {
+	t.Helper()
+	ctx := t.Context()
+
+	user := cmp.Or(os.Getenv("MYSQL_TEST_SERVER_USER"), "root")
+	db := cmp.Or(os.Getenv("MYSQL_TEST_SERVER_DB"), "mysql")
+	pass := cmp.Or(os.Getenv("MYSQL_TEST_SERVER_PASS"), mustCryptoRandomHex(t, 32))
+	reuseName := os.Getenv("MYSQL_TEST_SERVER_REUSE_CONTAINER_BY_NAME")
+
+	opts := []testcontainers.ContainerCustomizer{
+		mysqlcontainer.WithDatabase(db),
+		mysqlcontainer.WithUsername(user),
+		mysqlcontainer.WithPassword(pass),
+	}
+	if reuseName != "" {
+		opts = append(opts, testcontainers.WithReuseByName(reuseName))
+		t.Setenv("TESTCONTAINERS_RYUK_DISABLED", "true")
+	}
+
+	// MySQL 8.4
+	const img = "mysql@sha256:85144db2a70009020bc161bb11c34064e80e8e951b983c5fe3905ef688b54de8"
+	container, err := mysqlcontainer.Run(ctx, img, opts...)
+	if reuseName == "" {
+		defer testcontainers.CleanupContainer(t, container)
+	}
+	require.NoError(t, err)
+
+	host, err := container.Host(ctx)
+	require.NoError(t, err)
+
+	mappedPort, err := container.MappedPort(ctx, "3306/tcp")
+	require.NoError(t, err)
+
+	return &testServer{
+		host:     host,
+		port:     mappedPort.Port(),
+		username: user,
+		password: pass,
+		database: db,
+	}
+}
+
+// testServer is a MySQL server for tests.
+type testServer struct {
+	// Port is the MySQL connection endpoint host.
+	host string
+	// port is the MySQL connection endpoint port.
+	port string
+	// username is the database superuser.
+	username string
+	// password is the database superuser password.
+	password string
+	// database is a database schema provisioned in the database.
+	database string
+}
+
+// hostPort returns the server host:port.
+func (s *testServer) hostPort() string {
+	return s.host + ":" + s.port
+}
+
+func (s *testServer) connectTCP(t *testing.T, timeout time.Duration) net.Conn {
+	t.Helper()
+	conn, err := net.DialTimeout("tcp", s.hostPort(), timeout)
+	require.NoError(t, err)
+	return conn
+}
+
+func newRecordingClient(t *testing.T, reader io.ReadCloser, writers ...io.Writer) *recordingClient {
+	t.Helper()
+	var buf bytes.Buffer
+	writers = append(writers, &buf)
+	return &recordingClient{
+		ReadCloser: reader,
+		Writer:     io.MultiWriter(writers...),
+		buf:        &buf,
+	}
+}
+
+type recordingClient struct {
+	io.ReadCloser
+	io.Writer
+	buf *bytes.Buffer
+}
