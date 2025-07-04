101df37ad978ab1ae6363af87e912f90e129c62d
hugoShaka <hugo.hervieux@goteleport.com>
2025-07-04 16:08:36 -0400

    Release v19.0.0-dev.hugocanaries.10

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-dev"
+const Version = "19.0.0-dev.hugocanaries.10"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev"
+const VersionPreRelease = "dev.hugocanaries.10"
 const VersionMetadata = ""
