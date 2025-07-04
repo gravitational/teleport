bd9e096cfc7d7165858e451844b303217e3c0e9c
Tiago Silva <tiago.silva@goteleport.com>
2025-07-04 20:13:43 +0100

    Release v18.0.0-dev.tiago.16

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.0.0-dev.tiago.15"
+const Version = "18.0.0-dev.tiago.16"
 
 const VersionMajor = 18
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev.tiago.15"
+const VersionPreRelease = "dev.tiago.16"
 const VersionMetadata = ""
