95986e3c4c0b5af5e448bda8640ad6e01983f2a0
Marek Smoliński <marek@goteleport.com>
2025-07-09 20:30:08 +0200

    Add Cache resources pagination tests (#56547)

--- lib/cache/users_test.go
+++ lib/cache/users_test.go
@@ -39,7 +39,7 @@ func TestUsers(t *testing.T) {
 	t.Run("GetUsers", func(t *testing.T) {
 		testResources(t, p, testFuncs[types.User]{
 			newResource: func(name string) (types.User, error) {
-				return types.NewUser("bob")
+				return types.NewUser(name)
 			},
 			create: func(ctx context.Context, user types.User) error {
 				_, err := p.usersS.UpsertUser(ctx, user)
@@ -64,7 +64,7 @@ func TestUsers(t *testing.T) {
 	t.Run("ListUsers", func(t *testing.T) {
 		testResources(t, p, testFuncs[types.User]{
 			newResource: func(name string) (types.User, error) {
-				return types.NewUser("bob")
+				return types.NewUser(name)
 			},
 			create: func(ctx context.Context, user types.User) error {
 				_, err := p.usersS.UpsertUser(ctx, user)
