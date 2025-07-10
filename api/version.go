c62f656a2d0060cff08c67b87c8ed245fbd582d2
Noah Stride <noah.stride@goteleport.com>
2025-07-10 13:42:38 +0100

    Release v19.0.0-dev.noahmwitf.5

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-dev.noahmwitf.4"
+const Version = "19.0.0-dev.noahmwitf.5"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev.noahmwitf.4"
+const VersionPreRelease = "dev.noahmwitf.5"
 const VersionMetadata = ""
