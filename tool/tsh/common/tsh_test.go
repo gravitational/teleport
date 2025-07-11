91bb60cfdee5d40258b39fcc66524e5525c4f28a
Maxim Dietz <maxim.dietz@goteleport.com>
2025-07-10 21:24:31 -0400

    fix: Move long-term resource grouping validation to auth

--- tool/tsh/common/tsh_test.go
+++ tool/tsh/common/tsh_test.go
@@ -241,6 +241,10 @@ func handleReexec() {
 
 type cliModules struct{}
 
+func (p *cliModules) GenerateLongTermResourceGrouping(_ context.Context, _ modules.AccessResourcesGetter, _ types.AccessRequest) (*types.LongTermResourceGrouping, error) {
+	return nil, nil
+}
+
 func (p *cliModules) GenerateAccessRequestPromotions(_ context.Context, _ modules.AccessResourcesGetter, _ types.AccessRequest) (*types.AccessRequestAllowedPromotions, error) {
 	return &types.AccessRequestAllowedPromotions{}, nil
 }
