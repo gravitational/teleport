80aedf783b0790745061a2f3b37d6982968d6acb
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2025-07-14 16:19:46 +0200

    Implement version resolution algorithm

---
+++ web/packages/teleterm/src/services/appUpdater/autoUpdatesState.test.ts
@@ -0,0 +1,334 @@
+/**
+ * Teleport
+ * Copyright (C) 2025 Gravitational, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Logger, { NullService } from 'teleterm/logger';
+
+import { resolveAutoUpdatesStatus } from './autoUpdatesStatus';
+
+beforeAll(() => {
+  Logger.init(new NullService());
+});
+
+test.each([
+  {
+    title: 'disabled when env var is "off"',
+    input: {
+      versionEnvVar: 'off',
+      managingClusterUri: '',
+      getClusterVersions: async () => ({
+        reachableClusters: [],
+        unreachableClusters: [],
+      }),
+      getDownloadBaseUrl: async () => 'cdn.teleport.dev',
+    },
+    expected: {
+      enabled: false,
+      reason: 'disabled-by-env-var',
+    },
+  },
+  {
+    title: 'resolving with env var when set to version',
+    input: {
+      versionEnvVar: '14.0.0',
+      managingClusterUri: '',
+      getClusterVersions: async () => ({
+        reachableClusters: [],
+        unreachableClusters: [],
+      }),
+      getDownloadBaseUrl: async () => 'cdn.teleport.dev',
+    },
+    expected: {
+      enabled: true,
+      baseUrl: 'cdn.teleport.dev',
+      version: '14.0.0',
+      source: { kind: 'env-var' },
+    },
+  },
+  {
+    title: 'disabled when no versions with auto-update enabled',
+    input: {
+      versionEnvVar: '',
+      managingClusterUri: '',
+      getClusterVersions: async () => ({
+        reachableClusters: [
+          {
+            clusterUri: '/clusters/cluster-a',
+            toolsVersion: '14.0.0',
+            minToolsVersion: '13.0.0',
+            toolsAutoUpdate: false,
+          },
+        ],
+        unreachableClusters: [],
+      }),
+      getDownloadBaseUrl: async () => 'cdn.teleport.dev',
+    },
+    expected: {
+      enabled: false,
+      reason: 'no-cluster-with-auto-update',
+      unreachableClusters: [],
+    },
+  },
+  {
+    title: 'resolving with managing cluster if specified',
+    input: {
+      versionEnvVar: '',
+      getClusterVersions: async () => ({
+        reachableClusters: [
+          {
+            clusterUri: '/clusters/cluster-a',
+            toolsVersion: '14.0.0',
+            minToolsVersion: '13.0.0',
+            toolsAutoUpdate: true,
+          },
+        ],
+        unreachableClusters: [],
+      }),
+      getDownloadBaseUrl: async () => 'cdn.teleport.dev',
+      managingClusterUri: '/clusters/cluster-a',
+    },
+    expected: {
+      enabled: true,
+      version: '14.0.0',
+      baseUrl: 'cdn.teleport.dev',
+      source: {
+        kind: 'managing-cluster',
+        clusterUri: '/clusters/cluster-a',
+        candidateClusters: [
+          expect.objectContaining({
+            clusterUri: '/clusters/cluster-a',
+            toolsVersion: '14.0.0',
+            minToolsVersion: '13.0.0',
+            otherCompatibleClusters: [],
+          }),
+        ],
+        unreachableClusters: [],
+      },
+    },
+  },
+  {
+    title:
+      'resolving using most compatible version when clusters are on the same version',
+    input: {
+      versionEnvVar: '',
+      getClusterVersions: async () => ({
+        reachableClusters: [
+          {
+            clusterUri: '/clusters/cluster-a',
+            toolsVersion: '14.0.0',
+            minToolsVersion: '13.0.0',
+            toolsAutoUpdate: true,
+          },
+          {
+            clusterUri: '/clusters/cluster-b',
+            toolsVersion: '14.0.0',
+            minToolsVersion: '13.0.0',
+            toolsAutoUpdate: true,
+          },
+        ],
+        unreachableClusters: [],
+      }),
+      getDownloadBaseUrl: async () => 'cdn.teleport.dev',
+      managingClusterUri: '/clusters/missing-cluster',
+    },
+    expected: {
+      enabled: true,
+      version: '14.0.0',
+      baseUrl: 'cdn.teleport.dev',
+      source: {
+        kind: 'most-compatible',
+        candidateClusters: [
+          expect.objectContaining({
+            clusterUri: '/clusters/cluster-a',
+            toolsVersion: '14.0.0',
+            minToolsVersion: '13.0.0',
+            otherCompatibleClusters: ['/clusters/cluster-b'],
+          }),
+          expect.objectContaining({
+            clusterUri: '/clusters/cluster-b',
+            toolsVersion: '14.0.0',
+            minToolsVersion: '13.0.0',
+            otherCompatibleClusters: ['/clusters/cluster-a'],
+          }),
+        ],
+        unreachableClusters: [],
+        clustersUri: ['/clusters/cluster-a', '/clusters/cluster-b'],
+      },
+    },
+  },
+  {
+    title:
+      'resolving using most compatible version when clusters are on the same major version',
+    input: {
+      versionEnvVar: '',
+      getClusterVersions: async () => ({
+        reachableClusters: [
+          {
+            clusterUri: '/clusters/cluster-a',
+            toolsVersion: '14.1.0',
+            minToolsVersion: '13.0.0',
+            toolsAutoUpdate: true,
+          },
+          {
+            clusterUri: '/clusters/cluster-b',
+            toolsVersion: '14.2.0',
+            minToolsVersion: '13.0.0',
+            toolsAutoUpdate: true,
+          },
+        ],
+        unreachableClusters: [],
+      }),
+      getDownloadBaseUrl: async () => 'cdn.teleport.dev',
+      managingClusterUri: '/clusters/missing-cluster',
+    },
+    expected: {
+      enabled: true,
+      version: '14.2.0', // the newer version should be taken
+      baseUrl: 'cdn.teleport.dev',
+      source: {
+        kind: 'most-compatible',
+        candidateClusters: [
+          expect.objectContaining({
+            clusterUri: '/clusters/cluster-a',
+            toolsVersion: '14.1.0',
+            minToolsVersion: '13.0.0',
+            otherCompatibleClusters: ['/clusters/cluster-b'],
+          }),
+          expect.objectContaining({
+            clusterUri: '/clusters/cluster-b',
+            toolsVersion: '14.2.0',
+            minToolsVersion: '13.0.0',
+            otherCompatibleClusters: ['/clusters/cluster-a'],
+          }),
+        ],
+        unreachableClusters: [],
+        clustersUri: ['/clusters/cluster-b'],
+      },
+    },
+  },
+  {
+    title:
+      'resolving using most compatible version where clusters are on different major version',
+    input: {
+      versionEnvVar: '',
+      getClusterVersions: async () => ({
+        reachableClusters: [
+          {
+            clusterUri: '/clusters/cluster-a',
+            toolsVersion: '16.0.0',
+            minToolsVersion: '15.0.0',
+            toolsAutoUpdate: true,
+          },
+          {
+            clusterUri: '/clusters/cluster-b',
+            toolsVersion: '15.0.0',
+            minToolsVersion: '14.0.0',
+            toolsAutoUpdate: true,
+          },
+        ],
+        unreachableClusters: [],
+      }),
+
+      getDownloadBaseUrl: async () => 'cdn.teleport.dev',
+      managingClusterUri: '/clusters/missing-cluster',
+    },
+    expected: {
+      enabled: true,
+      version: '15.0.0',
+      baseUrl: 'cdn.teleport.dev',
+      source: {
+        kind: 'most-compatible',
+        candidateClusters: [
+          expect.objectContaining({
+            clusterUri: '/clusters/cluster-a',
+            toolsVersion: '16.0.0',
+            minToolsVersion: '15.0.0',
+            otherCompatibleClusters: [],
+          }),
+          expect.objectContaining({
+            clusterUri: '/clusters/cluster-b',
+            toolsVersion: '15.0.0',
+            minToolsVersion: '14.0.0',
+            otherCompatibleClusters: ['/clusters/cluster-a'],
+          }),
+        ],
+        unreachableClusters: [],
+        clustersUri: ['/clusters/cluster-b'],
+      },
+    },
+  },
+  {
+    title:
+      'resolving with no compatible version when clusters are on incompatibles versions',
+    input: {
+      versionEnvVar: '',
+      getClusterVersions: async () => ({
+        reachableClusters: [
+          {
+            clusterUri: '/clusters/cluster-a',
+            toolsVersion: '18.0.0',
+            minToolsVersion: '17.0.0',
+            toolsAutoUpdate: true,
+          },
+          {
+            clusterUri: '/clusters/cluster-b',
+            toolsVersion: '16.0.0',
+            minToolsVersion: '15.0.0',
+            toolsAutoUpdate: true,
+          },
+          {
+            clusterUri: '/clusters/cluster-c',
+            toolsVersion: '16.1.0',
+            minToolsVersion: '15.0.0',
+            toolsAutoUpdate: true,
+          },
+        ],
+        unreachableClusters: [],
+      }),
+      getDownloadBaseUrl: async () => 'cdn.teleport.dev',
+      managingClusterUri: '',
+    },
+    expected: {
+      enabled: false,
+      reason: 'no-compatible-version',
+      candidateClusters: [
+        expect.objectContaining({
+          clusterUri: '/clusters/cluster-a',
+          toolsVersion: '18.0.0',
+          minToolsVersion: '17.0.0',
+          otherCompatibleClusters: [],
+        }),
+        expect.objectContaining({
+          clusterUri: '/clusters/cluster-b',
+          toolsVersion: '16.0.0',
+          minToolsVersion: '15.0.0',
+          otherCompatibleClusters: ['/clusters/cluster-c'],
+        }),
+        expect.objectContaining({
+          clusterUri: '/clusters/cluster-c',
+          toolsVersion: '16.1.0',
+          minToolsVersion: '15.0.0',
+          otherCompatibleClusters: ['/clusters/cluster-b'],
+        }),
+      ],
+      unreachableClusters: [],
+    },
+  },
+])('$title', async ({ input, expected }) => {
+  const result = await resolveAutoUpdatesStatus(input);
+  expect(result).toEqual(expected);
+});
---
+++ web/packages/teleterm/src/services/appUpdater/autoUpdatesStatus.ts
@@ -0,0 +1,246 @@
+/**
+ * Teleport
+ * Copyright (C) 2025 Gravitational, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import type {
+  ClusterVersionInfo,
+  GetClusterVersionsResponse,
+  UnreachableCluster,
+} from 'gen-proto-ts/teleport/lib/teleterm/auto_update/v1/auto_update_service_pb';
+import { compareSemVers, getMajor } from 'shared/utils/semVer';
+
+import Logger from 'teleterm/logger';
+import { RootClusterUri } from 'teleterm/ui/uri';
+
+const logger = new Logger('resolveAutoUpdatesStatus');
+
+/**
+ * Determines the auto-update status based on all connected clusters.
+ * Updates can be either enabled or disabled, depending on the passed sources.
+ * The version is selected using the following precedence:
+ * 1. `TELEPORT_TOOLS_VERSION` env var, if defined.
+ * 2. `tools_version` from the cluster manually selected to manage updates, if selected.
+ * 3. Most compatible version, if found.
+ * 4. If there's no version at this point, stop auto updates.
+ */
+export async function resolveAutoUpdatesStatus(sources: {
+  versionEnvVar: string;
+  managingClusterUri: string | undefined;
+  getClusterVersions(): Promise<GetClusterVersionsResponse>;
+  getDownloadBaseUrl(): Promise<string>;
+}): Promise<AutoUpdatesStatus> {
+  if (sources.versionEnvVar === 'off') {
+    return { enabled: false, reason: 'disabled-by-env-var' };
+  }
+  if (sources.versionEnvVar) {
+    return {
+      enabled: true,
+      baseUrl: await sources.getDownloadBaseUrl(),
+      version: sources.versionEnvVar,
+      source: { kind: 'env-var' },
+    };
+  }
+
+  const { reachableClusters, unreachableClusters } =
+    await sources.getClusterVersions();
+  const candidateClusters = makeCandidateClusters(reachableClusters);
+  if (!candidateClusters.length) {
+    return {
+      enabled: false,
+      reason: 'no-cluster-with-auto-update',
+      unreachableClusters,
+    };
+  }
+
+  const clusterUri = sources.managingClusterUri;
+  if (clusterUri) {
+    const cluster = candidateClusters.find(c => c.clusterUri === clusterUri);
+    if (cluster) {
+      return {
+        enabled: true,
+        baseUrl: await sources.getDownloadBaseUrl(),
+        version: cluster.toolsVersion,
+        source: {
+          kind: 'managing-cluster',
+          candidateClusters,
+          clusterUri,
+          unreachableClusters,
+        },
+      };
+    }
+    logger.warn(
+      `Cluster ${clusterUri} managing updates was not found, continuing resolution.`
+    );
+  }
+
+  const mostCompatibleVersion =
+    findMostCompatibleToolsVersion(candidateClusters);
+  if (mostCompatibleVersion) {
+    return {
+      enabled: true,
+      baseUrl: await sources.getDownloadBaseUrl(),
+      version: mostCompatibleVersion,
+      source: {
+        kind: 'most-compatible',
+        candidateClusters,
+        unreachableClusters,
+        clustersUri: candidateClusters
+          .filter(c => c.toolsVersion === mostCompatibleVersion)
+          .map(c => c.clusterUri),
+      },
+    };
+  }
+
+  return {
+    enabled: false,
+    reason: 'no-compatible-version',
+    unreachableClusters,
+    candidateClusters,
+  };
+}
+
+/** Assigns each candidate a compatibility with client tools from other clusters. */
+function makeCandidateClusters(
+  versions: ClusterVersionInfo[]
+): CandidateCluster[] {
+  const candidates = versions.filter(v => v.toolsAutoUpdate);
+
+  return candidates.map(candidate => {
+    const candidateMajorToolsVersion = getMajor(candidate.toolsVersion);
+
+    const otherCompatibleClusters = candidates
+      .filter(v => {
+        // The list should only contain other clusters.
+        if (v.clusterUri === candidate.clusterUri) {
+          return false;
+        }
+
+        const minMajor = getMajor(v.minToolsVersion);
+        const maxMajor = getMajor(v.toolsVersion);
+
+        // Check compatibility: candidate's major version must be within [minMajor, maxMajor].
+        return (
+          candidateMajorToolsVersion >= minMajor &&
+          candidateMajorToolsVersion <= maxMajor
+        );
+      })
+      .map(v => v.clusterUri);
+
+    return {
+      ...candidate,
+      otherCompatibleClusters,
+    };
+  });
+}
+
+/** Finds version of client tools that is compatible with all other clusters. */
+function findMostCompatibleToolsVersion(
+  candidates: CandidateCluster[]
+): string | undefined {
+  // Get the highest version first.
+  const sorted = candidates.toSorted((a, b) =>
+    compareSemVers(b.toolsVersion, a.toolsVersion)
+  );
+
+  const requiredClusters = new Set(candidates.map(c => c.clusterUri));
+
+  for (const candidate of sorted) {
+    // The candidate is compatible with itself and `otherCompatibleClusters`.
+    const compatibleClusters = new Set([
+      candidate.clusterUri,
+      ...candidate.otherCompatibleClusters,
+    ]);
+    // Check if candidate is compatible with all required clusters.
+    if (compatibleClusters.isSupersetOf(requiredClusters)) {
+      return candidate.toolsVersion;
+    }
+  }
+}
+
+export type AutoUpdatesEnabled = {
+  enabled: true;
+  version: string;
+  baseUrl: string;
+  source:
+    | {
+        /** TELEPORT_TOOLS_VERSION configures app version. */
+        kind: 'env-var';
+      }
+    | {
+        /** Updates are auto by a specific cluster. */
+        kind: 'managing-cluster';
+        /** URI of the managing cluster. */
+        clusterUri: RootClusterUri;
+        /** Candidate clusters that are able to manage updates. */
+        candidateClusters: CandidateCluster[];
+        /** Clusters from which version information could not be retrieved. */
+        unreachableClusters: UnreachableCluster[];
+      }
+    | {
+        /** Updates determined by the most compatible clusters available. */
+        kind: 'most-compatible';
+        /** URIs of all clusters that specify the same version. */
+        clustersUri: RootClusterUri[];
+        /** Candidate clusters that are able to manage updates. */
+        candidateClusters: CandidateCluster[];
+        /**
+         * Clusters from which version information could not be retrieved.
+         * If non-empty, the update is not automatically downloaded.
+         * */
+        unreachableClusters: UnreachableCluster[];
+      };
+};
+
+export type AutoUpdatesDisabled =
+  | {
+      enabled: false;
+      /** `TELEPORT_TOOLS_VERSION` is 'off'. */
+      reason: 'disabled-by-env-var';
+    }
+  | {
+      enabled: false;
+      /** There is no cluster that could manage updates. */
+      reason: 'no-cluster-with-auto-update';
+      /** Clusters from which version information could not be retrieved. */
+      unreachableClusters: UnreachableCluster[];
+    }
+  | {
+      enabled: false;
+      /**
+       * There are clusters that could manage updates, but they specify
+       * incompatible client tools versions.
+       */
+      reason: 'no-compatible-version';
+      /** Candidate clusters that are able to manage updates. */
+      candidateClusters: CandidateCluster[];
+      /** Clusters from which version information could not be retrieved. */
+      unreachableClusters: UnreachableCluster[];
+    };
+
+export type AutoUpdatesStatus = AutoUpdatesEnabled | AutoUpdatesDisabled;
+
+/** Represents a cluster that could manage updates. */
+export interface CandidateCluster {
+  /** URI of the cluster. */
+  clusterUri: RootClusterUri;
+  /** The tools version required by this cluster. */
+  toolsVersion: string;
+  /** The minimum tools version allowed by this cluster. */
+  minToolsVersion: string;
+  /** URIs of clusters whose client tools are compatible with this cluster's client tools. */
+  otherCompatibleClusters: RootClusterUri[];
+}
