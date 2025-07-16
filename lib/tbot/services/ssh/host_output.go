c6743f72815325bd2d24b3c911d00f1385c53b56
Dan Upton <daniel.upton@goteleport.com>
2025-07-16 11:42:47 +0100

    Use `LoggerForService` in SSH services

--- lib/tbot/services/ssh/host_output.go
+++ lib/tbot/services/ssh/host_output.go
@@ -58,10 +58,7 @@ func HostOutputServiceBuilder(cfg *HostOutputConfig, defaultCredentialLifetime b
 			identityGenerator:         deps.IdentityGenerator,
 			clientBuilder:             deps.ClientBuilder,
 		}
-		svc.log = deps.Logger.With(
-			teleport.ComponentKey,
-			teleport.Component(teleport.ComponentTBot, "svc", svc.String()),
-		)
+		svc.log = deps.LoggerForService(svc)
 		svc.statusReporter = deps.StatusRegistry.AddService(svc.String())
 		return svc, nil
 	}
--- lib/tbot/services/ssh/multiplexer.go
+++ lib/tbot/services/ssh/multiplexer.go
@@ -117,10 +117,7 @@ func MultiplexerServiceBuilder(
 			identityGenerator:         deps.IdentityGenerator,
 			clientBuilder:             deps.ClientBuilder,
 		}
-		svc.log = deps.Logger.With(
-			teleport.ComponentKey,
-			teleport.Component(teleport.ComponentTBot, "svc", svc.String()),
-		)
+		svc.log = deps.LoggerForService(svc)
 		svc.statusReporter = deps.StatusRegistry.AddService(svc.String())
 		return svc, nil
 	}
