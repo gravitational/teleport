11702fa6e5801028dbdcce90b3718cec5e0f5b46
Dan Upton <daniel.upton@goteleport.com>
2025-07-16 11:40:14 +0100

    Use `LoggerForService` in database services

--- lib/tbot/services/database/output_service.go
+++ lib/tbot/services/database/output_service.go
@@ -52,10 +52,7 @@ func OutputServiceBuilder(cfg *OutputConfig, defaultCredentialLifetime bot.Crede
 			identityGenerator:         deps.IdentityGenerator,
 			clientBuilder:             deps.ClientBuilder,
 		}
-		svc.log = deps.Logger.With(
-			teleport.ComponentKey,
-			teleport.Component(teleport.ComponentTBot, "svc", svc.String()),
-		)
+		svc.log = deps.LoggerForService(svc)
 		svc.statusReporter = deps.StatusRegistry.AddService(svc.String())
 		return svc, nil
 	}
