6d6d1cb692305a6bef65ddade91983ee4c6bb002
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2025-07-16 21:27:55 +0200

    Release v18.0.0-dev.joerger.2-relay.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.0.0-dev.joerger.2"
+const Version = "18.0.0-dev.joerger.2-relay.1"
 
 const VersionMajor = 18
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev.joerger.2"
+const VersionPreRelease = "dev.joerger.2-relay.1"
 const VersionMetadata = ""
