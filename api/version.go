833c1e7b4dcfb8758d389f823f5a5234c2ec932c
joerger <bjoerger@goteleport.com>
2025-07-17 12:58:40 -0700

    Release v18.0.2-dev.jsonpath.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.0.2"
+const Version = "18.0.2-dev.jsonpath.1"
 
 const VersionMajor = 18
 const VersionMinor = 0
 const VersionPatch = 2
-const VersionPreRelease = ""
+const VersionPreRelease = "dev.jsonpath.1"
 const VersionMetadata = ""
