824f75bba40258c1942be50eea2b51acc2b88dd3
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2025-07-17 23:52:05 +0200

    Release v18.0.2-dev.jsonpath.1-relay.2

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.0.2-dev.jsonpath.1"
+const Version = "18.0.2-dev.jsonpath.1-relay.2"
 
 const VersionMajor = 18
 const VersionMinor = 0
 const VersionPatch = 2
-const VersionPreRelease = "dev.jsonpath.1"
+const VersionPreRelease = "dev.jsonpath.1-relay.2"
 const VersionMetadata = ""
