a720790bdf32e959a6936ef9be4387874cdb6fdf
Andrew Burke <andrew.burke@goteleport.com>
2025-07-21 11:26:03 -0700

    Move stuff around

--- lib/srv/uacc/wtmpdb.go
+++ lib/srv/uacc/wtmpdb.go
@@ -1,49 +1,93 @@
+//go:build linux
+
 package uacc
 
 import (
 	"database/sql"
-	"os"
+	"errors"
+	"net"
+	"strconv"
 	"time"
 
 	"github.com/gravitational/trace"
 )
 
-const dbLocation = "/var/lib/wtmpdb/wtmp.db"
+const wtmpdbLocation = "/var/lib/wtmpdb/wtmp.db"
 
-type wtmpdb struct {
+type wtmpdbBackend struct {
 	db *sql.DB
 }
 
-func newWtmpdb(dbFile string) (*wtmpdb, error) {
-	db, err := sql.Open("sqlite3", dbFile)
+func newWtmpdb(dbPath string) (*wtmpdbBackend, error) {
+	db, err := sql.Open("sqlite3", dbPath)
 	if err != nil {
 		return nil, trace.Wrap(err)
 	}
-	return &wtmpdb{db: db}, nil
+	return &wtmpdbBackend{db: db}, nil
 }
 
-func (w *wtmpdb) Login(tty *os.File, username, hostname string, ts time.Time) error {
-	ttyName, err := getTTYName(tty)
+func init() {
+	wtmpdb, err := newWtmpdb(wtmpdbLocation)
+	if err == nil {
+		registerBackend(wtmpdb)
+	}
+}
+
+func (w *wtmpdbBackend) Name() string {
+	return "wtmpdb"
+}
+
+func (w *wtmpdbBackend) Login(ttyName, username, hostname string, remote net.Addr, ts time.Time) (string, error) {
+	remoteHost, _, err := net.SplitHostPort(remote.String())
 	if err != nil {
-		return trace.Wrap(err)
+		return "", trace.Wrap(err)
 	}
 	stmt, err := w.db.Prepare("INSET INTO wtmp(Type, User, Login, TTY, RemoteHost) VALUES(?,?,?,?,?)")
 	if err != nil {
+		return "", trace.Wrap(err)
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec("USER_PROCESS", username, ts.UnixMicro(), ttyName, remoteHost)
+	if err != nil {
+		return "", trace.Wrap(err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return "", trace.Wrap(err)
+	}
+	return strconv.Itoa(int(id)), nil
+}
+
+func (w *wtmpdbBackend) Logout(id string, ts time.Time) error {
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
 		return trace.Wrap(err)
 	}
-	_, err = stmt.Exec("USER_PROCESS", username, ts.UnixMicro(), ttyName, hostname)
+	stmt, err := w.db.Prepare("UPDATE wtmp SET Logout = ? WHERE ID = ?")
+	if err != nil {
+		return trace.Wrap(err, int64(idInt))
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(ts.UnixMicro(), id)
 	return trace.Wrap(err)
 }
 
-func (w *wtmpdb) Logout(tty *os.File, ts time.Time) error {
-	ttyName, err := getTTYName(tty)
+func (w *wtmpdbBackend) FailedLogin(username, hostname string, remote net.Addr, ts time.Time) error {
+	return trace.NotImplemented("wtmpdb backend does not support logging failed logins")
+}
+
+func (w *wtmpdbBackend) IsUserLoggedIn(username string) (bool, error) {
+	stmt, err := w.db.Prepare("SELECT TTY FROM wtmp WHERE User = ? AND Logout = 0")
 	if err != nil {
-		return trace.Wrap(err)
+		return false, trace.Wrap(err)
 	}
-	stmt, err := w.db.Prepare("UPDATE wtmp SET Logout = ? WHERE TTY = ? AND LOGOUT = 0")
-	if err != nil {
-		return trace.Wrap(err)
+	defer stmt.Close()
+	var tty string
+	if err := stmt.QueryRow(username).Scan(&tty); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return false, nil
+		}
+		return false, trace.Wrap(err)
 	}
-	_, err = stmt.Exec(ts.UnixMicro(), ttyName)
-	return trace.Wrap(err)
+	return tty != "", nil
 }
