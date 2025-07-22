831984c4f232ed3d14bc7d04ca62084aa1eb77e8
Dan Upton <daniel.upton@goteleport.com>
2025-07-22 10:24:58 +0100

    MWI: Move internal services into separate packages (#56558)
    * Move `withTypeHeader` to internal package
    
    * Move `CredentialLifetime` struct into `bot` package
    
    * Move memory and directory destinations to `destination` package
    
    * Rename marshaling package to encoding
    
    * Fix missing import in tctl
    
    * Remove stray bot.Destination interface
    
    * make go-mod-tidy-all
    
    * Fix remaining reference to config.DestinationMemory
    
    * Fix imports
    
    * Move diagnostics service to separate package
    
    * Move `channelBroadcaster` into internal package
    
    * Move CA rotation service to separate package
    
    * Move `runOnInterval` helper to `internal` package
    
    * Move heartbeat service to separate package
    
    * Fix imports

--- lib/tbot/service_kubernetes_v2_output.go
+++ lib/tbot/service_kubernetes_v2_output.go
@@ -44,6 +44,7 @@ import (
 	"github.com/gravitational/teleport/lib/tbot/client"
 	"github.com/gravitational/teleport/lib/tbot/config"
 	"github.com/gravitational/teleport/lib/tbot/identity"
+	"github.com/gravitational/teleport/lib/tbot/internal"
 	"github.com/gravitational/teleport/lib/tbot/readyz"
 	logutils "github.com/gravitational/teleport/lib/utils/log"
 )
@@ -61,7 +62,7 @@ type KubernetesV2OutputService struct {
 	getBotIdentity     getBotIdentityFn
 	log                *slog.Logger
 	proxyPingCache     *proxyPingCache
-	reloadBroadcaster  *channelBroadcaster
+	reloadBroadcaster  *internal.ChannelBroadcaster
 	statusReporter     readyz.Reporter
 	// executablePath is called to get the path to the tbot executable.
 	// Usually this is os.Executable
@@ -82,19 +83,19 @@ func (s *KubernetesV2OutputService) OneShot(ctx context.Context) error {
 }
 
 func (s *KubernetesV2OutputService) Run(ctx context.Context) error {
-	reloadCh, unsubscribe := s.reloadBroadcaster.subscribe()
+	reloadCh, unsubscribe := s.reloadBroadcaster.Subscribe()
 	defer unsubscribe()
 
-	return trace.Wrap(runOnInterval(ctx, runOnIntervalConfig{
-		service:         s.String(),
-		name:            "output-renewal",
-		f:               s.generate,
-		interval:        cmp.Or(s.cfg.CredentialLifetime, s.botCfg.CredentialLifetime).RenewalInterval,
-		retryLimit:      renewalRetryLimit,
-		log:             s.log,
-		reloadCh:        reloadCh,
-		identityReadyCh: s.botIdentityReadyCh,
-		statusReporter:  s.statusReporter,
+	return trace.Wrap(internal.RunOnInterval(ctx, internal.RunOnIntervalConfig{
+		Service:         s.String(),
+		Name:            "output-renewal",
+		F:               s.generate,
+		Interval:        cmp.Or(s.cfg.CredentialLifetime, s.botCfg.CredentialLifetime).RenewalInterval,
+		RetryLimit:      renewalRetryLimit,
+		Log:             s.log,
+		ReloadCh:        reloadCh,
+		IdentityReadyCh: s.botIdentityReadyCh,
+		StatusReporter:  s.statusReporter,
 	}))
 }
 
