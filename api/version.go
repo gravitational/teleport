e41f2d6062f90f668f8c52610903db21d1a06ff4
Marek Smoliński <marek@goteleport.com>
2025-07-24 10:56:14 +0200

    Release 18.0.2-dev.marek.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.0.2"
+const Version = "18.0.2-dev.marek.1"
 
 const VersionMajor = 18
 const VersionMinor = 0
 const VersionPatch = 2
-const VersionPreRelease = ""
+const VersionPreRelease = "dev.marek.1"
 const VersionMetadata = ""
