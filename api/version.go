5fb93979d804ec0f769ab2a4de86cb2d9cb2dad2
hugoShaka <hugo.hervieux@goteleport.com>
2025-07-24 00:53:49 -0400

    Release 18.1.0-dev.hugocanary.3

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.1.0-dev.hugocanary.2"
+const Version = "18.1.0-dev.hugocanary.3"
 
 const VersionMajor = 18
 const VersionMinor = 1
 const VersionPatch = 0
-const VersionPreRelease = "dev.hugocanary.2"
+const VersionPreRelease = "dev.hugocanary.3"
 const VersionMetadata = ""
