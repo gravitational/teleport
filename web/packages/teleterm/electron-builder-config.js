fc27ad053a5fb757438e8974a5e6cb47e532d1f2
Grzegorz Zdunek <gzdunek@users.noreply.github.com>
2025-07-28 13:18:24 +0200

    Add ZIP target for macOS to support Connect in-app updates (#55676) (#57184)
    (cherry picked from commit 1be524f5b3e4ee06f90268d32790c3f6ff4433cb)

--- web/packages/teleterm/electron-builder-config.js
+++ web/packages/teleterm/electron-builder-config.js
@@ -103,7 +103,8 @@ module.exports = {
     },
   ],
   mac: {
-    target: 'dmg',
+    // ZIP target is used only for app updates.
+    target: ['zip', 'dmg'],
     category: 'public.app-category.developer-tools',
     type: 'distribution',
     notarize: true,
