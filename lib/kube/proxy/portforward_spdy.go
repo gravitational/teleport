86dac0d19c8f33f4415d94f0992f3ac29bf25110
Rana Ian <rana.ian@goteleport.com>
2025-07-29 15:04:29 -0700

    Restore `case <-h.targetConn.CloseChan()`
    - Consensus is that the logic is correct

--- lib/kube/proxy/portforward_spdy.go
+++ lib/kube/proxy/portforward_spdy.go
@@ -308,6 +308,14 @@ func (h *portForwardProxy) run() {
 		case <-h.sourceConn.CloseChan():
 			h.logger.DebugContext(h.context, "Upgraded connection closed")
 			return
+		case <-h.targetConn.CloseChan():
+			// Backend pod lifecycle completed
+			h.logger.DebugContext(h.context, "Target connection closed")
+			// Close source connection
+			if err := h.sourceConn.Close(); err != nil {
+				h.logger.ErrorContext(h.context, "Unable to close source connection", "error", err)
+			}
+			return
 		case stream := <-h.streamChan:
 			requestID, err := h.requestID(stream)
 			if err != nil {
