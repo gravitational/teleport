843f05ea333538fe2996be354bce4217dfb78fd2
Vadym Popov <vadym.popov@goteleport.com>
2025-07-29 00:04:44 -0700

    Release v18.1.0-dev.vapopov.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-dev"
+const Version = "18.1.0-dev.vapopov.1"
 
-const VersionMajor = 19
-const VersionMinor = 0
+const VersionMajor = 18
+const VersionMinor = 1
 const VersionPatch = 0
-const VersionPreRelease = "dev"
+const VersionPreRelease = "dev.vapopov.1"
 const VersionMetadata = ""
