00f8547328e64f1a7fb5ed35ad1ffa9b2f35ed38
Przemko Robakowski <przemko.robakowski@goteleport.com>
2025-07-30 17:52:55 +0200

    [v18] Disable NLA in FIPS mode (#57307)
    * Disable NLA in FIPS mode (#54048)
    
    * Disable NLA in FIPS mode
    
    * Disable NLA in FIPS mode
    
    * lint
    
    * Update docs for NLA in FIPS mode (#57310)

--- lib/srv/desktop/windows_server.go
+++ lib/srv/desktop/windows_server.go
@@ -27,7 +27,6 @@ import (
 	"fmt"
 	"log/slog"
 	"net"
-	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -187,6 +186,9 @@ type WindowsServiceConfig struct {
 	Labels               map[string]string
 	// ResourceMatchers match dynamic Windows desktop resources.
 	ResourceMatchers []services.ResourceMatcher
+	// NLA indicates whether the client should perform Network Level Authentication
+	// (NLA) when initiating the RDP session.
+	NLA bool
 }
 
 // HeartbeatConfig contains the configuration for service heartbeats.
@@ -354,10 +356,7 @@ func NewWindowsService(cfg WindowsServiceConfig) (*WindowsService, error) {
 		closeCtx:    ctx,
 		close:       close,
 		auditCache:  newSharedDirectoryAuditCache(),
-
-		// For now, NLA is opt-in via an environment variable.
-		// We'll make it the default behavior in a future release.
-		enableNLA: os.Getenv("TELEPORT_ENABLE_RDP_NLA") == "yes",
+		enableNLA:   cfg.NLA,
 	}
 
 	s.ca = winpki.NewCertificateStoreClient(winpki.CertificateStoreConfig{
