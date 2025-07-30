a7060713683e95584bcac4e0f2316f28812dcfa2
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2025-07-30 07:34:48 -0400

    Remove testing symbols from lib/service (#57262) (#57273)
    `func (*TeleportProcess) GetIdentityForTesting` was the only place
    consuming test symbols outside of tests. It existed to allow
    `integration.TestInstanceCertReissue` to assert the identity after
    certificate reissuance. To avoid this, TestInstanceCertReissue
    was moved from integration into lib/service so that it could make
    use of the private `(*TeleportProcess) getIdentity` and allow the
    `GetIdentityForTesting` to be removed.
    
    Updates #51023.

--- lib/service/service.go
+++ lib/service/service.go
@@ -44,7 +44,6 @@ import (
 	"strings"
 	"sync"
 	"sync/atomic"
-	"testing"
 	"time"
 
 	awssession "github.com/aws/aws-sdk-go/aws/session"
@@ -868,13 +867,6 @@ func (process *TeleportProcess) getAuthSubjectiveAddr() string {
 	process.Lock()
 	defer process.Unlock()
 	return process.authSubjectiveAddr
-}
-
-func (process *TeleportProcess) GetIdentityForTesting(t *testing.T, role types.SystemRole) (i *state.Identity, err error) {
-	if !testing.Testing() {
-		panic("GetIdentityForTesting can only be called in tests")
-	}
-	return process.getIdentity(role)
 }
 
 // getIdentity returns the current identity (credentials to the auth server) for
