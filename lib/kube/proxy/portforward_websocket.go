91295ee92eed99b798aafe6c9ed5cfaba1dd0adf
Rana Ian <rana.ian@goteleport.com>
2025-08-01 10:51:51 -0700

    Close `targetErrorStream` for unidriectional copy
    - Matches change in `lib/kube/proxy/portforward_spdy.go` `func (h *portForwardProxy) forwardStreamPair()`

--- lib/kube/proxy/portforward_websocket.go
+++ lib/kube/proxy/portforward_websocket.go
@@ -271,6 +271,7 @@ func (h *websocketPortforwardHandler) forwardStreamPair(p *websocketChannelPair)
 	go func() {
 		defer wg.Done()
 		// Use a unidirectional copy from Kube API to client.
+		_ = targetErrorStream.Close()
 		// Enables error propagation from Kube API server to kubectl client.
 		if _, err := io.Copy(p.errorStream, targetErrorStream); err != nil {
 			h.logger.DebugContext(h.context, "Unable to proxy portforward error-stream", "error", err)
