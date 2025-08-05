e4cd746b404fbcc1c5156df6d75fea49ff3f59fa
Raphael Ribeiro <raphaelpr01@gmail.com>
2025-08-05 11:38:24 -0300

    ci: fix branch filters (#4)
    * fix branch filters
    
    * add comment to ecr module
    
    * add allowed_pull_account ids comment

--- terraform/general/ecr.tf
+++ terraform/general/ecr.tf
@@ -3,4 +3,6 @@ module "ecr_teleport" {
 
   project = "platform/teleport"
   squad   = local.squad
+
+  #allowed_pull_account_ids = ["OD_DEV_ACCOUNT_ID", "OD_PROD_ACCOUNT_ID", "DEV_ACCOUNT_ID",""]
 }
