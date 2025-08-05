212ebe64a31ffef0b00462f7a37025b1f6ead652
Brian Joerger <bjoerger@goteleport.com>
2025-08-05 13:57:00 -0700

    refactor: Move ssh agent server helpers to `sshagent` package (#57244)
    * Move lib/teleagent/server.go to lib/sshagent/server.go.
    
    * Use NewStaticClientGetter instead of NoopCloser.
    
    * Remove lib/sshagent dependency from sshutils package.
    
    * Fix typo.

--- lib/srv/regular/proxy.go
+++ lib/srv/regular/proxy.go
@@ -34,6 +34,7 @@ import (
 	"github.com/gravitational/teleport/lib/agentless"
 	"github.com/gravitational/teleport/lib/proxy"
 	"github.com/gravitational/teleport/lib/srv"
+	"github.com/gravitational/teleport/lib/sshagent"
 	"github.com/gravitational/teleport/lib/utils"
 )
 
@@ -256,7 +257,9 @@ func (t *proxySubsys) proxyToHost(ctx context.Context, ch ssh.Channel, clientSrc
 
 	signer := agentless.SignerFromSSHIdentity(identity.UnmappedIdentity, authClient, t.clusterName, identity.TeleportUser)
 
-	aGetter := t.ctx.StartAgentChannel
+	aGetter := func() (sshagent.Client, error) {
+		return t.ctx.StartAgentChannel()
+	}
 	conn, err := t.router.DialHost(ctx, clientSrcAddr, clientDstAddr, t.host, t.port, t.clusterName, t.ctx.Identity.AccessChecker, aGetter, signer)
 	if err != nil {
 		return trace.Wrap(err)
