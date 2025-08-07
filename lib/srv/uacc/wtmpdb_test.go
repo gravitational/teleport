473d649f2d2193ff3bec4f095f3efb88397f9619
Andrew Burke <andrew.burke@goteleport.com>
2025-08-07 13:14:47 -0700

    Lint

--- lib/srv/uacc/wtmpdb_test.go
+++ lib/srv/uacc/wtmpdb_test.go
@@ -1,3 +1,19 @@
+// Teleport
+// Copyright (C) 2025 Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package uacc
 
 import (
@@ -6,8 +22,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/gravitational/teleport/lib/utils"
 	"github.com/stretchr/testify/require"
+
+	"github.com/gravitational/teleport/lib/utils"
 )
 
 func requireDBEntry(t *testing.T, db *sql.DB, key string, expectUsername, expectTTY, expectAddr string, expectLoginTime, expectLogoutTime time.Time) {
