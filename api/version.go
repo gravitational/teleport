d9f571b83027f28ab0a85756d7bc572ba3c59c99
Ryan Hammonds <ryan.hammonds@goteleport.com>
2025-08-07 13:25:56 -0400

    Release 17.7.1-dev.signedjar.4

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "17.7.1-dev.signedjar.3"
+const Version = "17.7.1-dev.signedjar.4"
 
 const VersionMajor = 17
 const VersionMinor = 7
 const VersionPatch = 1
-const VersionPreRelease = "dev.signedjar.3"
+const VersionPreRelease = "dev.signedjar.4"
 const VersionMetadata = ""
