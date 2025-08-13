15dff1a8498e8b649a1ac84b527d95146f71a295
Lukasz Okraszewski <lukasz.okraszewski@goteleport.com>
2025-08-13 12:59:34 +0100

    remove humanize dep

--- api/utils/grpc/size.go
+++ api/utils/grpc/size.go
@@ -15,14 +15,78 @@
 package grpc
 
 import (
+	"fmt"
 	"math"
 	"os"
-
-	"github.com/dustin/go-humanize"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 // defaultClientRecvSize is the grpc client default for received message sizes
 const defaultClientRecvSize = 4 * 1024 * 1024 // 4MB
+
+// parseBytes takes human represtation of bytes such as '24mb' and returns number of bytes as an integer
+//
+// Only support a subset of SI prefixes up to gi/gb.
+func parseBytes(s string) (int, error) {
+	const (
+		Byte = 1 << (iota * 10)
+		KiByte
+		MiByte
+		GiByte
+		IByte = 1
+		KByte = IByte * 1000
+		MByte = KByte * 1000
+		GByte = MByte * 1000
+	)
+
+	var bytesSizeTable = map[string]int{
+		"b":   Byte,
+		"kib": KiByte,
+		"kb":  KByte,
+		"mib": MiByte,
+		"mb":  MByte,
+		"gib": GiByte,
+		"gb":  GByte,
+		"":    Byte,
+		"ki":  KiByte,
+		"k":   KByte,
+		"mi":  MiByte,
+		"m":   MByte,
+		"gi":  GiByte,
+		"g":   GByte,
+	}
+
+	lastDigit := 0
+	for _, r := range s {
+		if !(unicode.IsDigit(r)) {
+			break
+		}
+		lastDigit++
+	}
+
+	num := s[:lastDigit]
+
+	var value float32
+	if f, err := strconv.ParseFloat(num, 32); err == nil {
+		value = float32(f)
+	} else {
+		return 0, err
+
+	}
+
+	extra := strings.ToLower(strings.TrimSpace(s[lastDigit:]))
+	if m, ok := bytesSizeTable[extra]; ok {
+		value *= float32(m)
+		if value >= math.MaxInt32 {
+			return 0, fmt.Errorf("too large: %v", s)
+		}
+		return int(value), nil
+	}
+
+	return 0, fmt.Errorf("unhandled size name: %v", extra)
+}
 
 // MaxRecvSize returns maximum message size in bytes the client can receive.
 //
@@ -35,14 +99,10 @@ func MaxRecvSize() int {
 		return defaultClientRecvSize
 	}
 
-	size, err := humanize.ParseBytes(val)
+	size, err := parseBytes(val)
 	if err != nil {
 		return defaultClientRecvSize
 	}
 
-	if size > uint64(math.MaxInt32) {
-		return defaultClientRecvSize
-	}
-
-	return int(size)
+	return size
 }
