1bff5192048f00d4f554a5c4664d147d016b4690
Bartosz Leper <bartosz.leper@goteleport.com>
2025-08-17 05:40:03 +0200

    Summarizer resource validation

--- api/types/summarizer/summarizer.go
+++ api/types/summarizer/summarizer.go
@@ -15,9 +15,12 @@
 package summarizer
 
 import (
+	"github.com/gravitational/trace"
+
 	headerv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/header/v1"
 	summarizerv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/summarizer/v1"
 	"github.com/gravitational/teleport/api/types"
+	"github.com/gravitational/teleport/lib/services"
 )
 
 // NewInferenceModel creates a new InferenceModel resource with the given name
@@ -33,6 +36,42 @@ func NewInferenceModel(name string, spec *summarizerv1.InferenceModelSpec) *summ
 	}
 }
 
+func ValidateInferenceModel(m *summarizerv1.InferenceModel) error {
+	switch {
+	case m == nil:
+		return trace.BadParameter("inference model is nil")
+	case m.GetKind() != types.KindInferenceModel:
+		return trace.BadParameter("kind must be %s, got %s", types.KindInferenceModel, m.GetKind())
+	case m.GetSubKind() != "":
+		return trace.BadParameter("subkind must be empty")
+	case m.GetVersion() != types.V1:
+		return trace.BadParameter("unsupported version %s", m.GetVersion())
+
+	case m.GetMetadata() == nil:
+		return trace.BadParameter("metadata is required")
+	case m.GetMetadata().GetName() == "":
+		return trace.BadParameter("metadata.name is required")
+
+	case m.GetSpec() == nil:
+		return trace.BadParameter("spec is required")
+	}
+
+	provider := m.GetSpec().GetProvider()
+	// if provider != nil {
+	// 	return trace.BadParameter("missing or unsupported provider")
+	// }
+	switch p := provider.(type) {
+	case *summarizerv1.InferenceModelSpec_Openai:
+		if p.Openai.GetOpenaiModelId() == "" {
+			return trace.BadParameter("spec.openai.openai_model_id is required")
+		}
+	default:
+		return trace.BadParameter("unsupported inference provider %T", p)
+	}
+
+	return nil
+}
+
 // NewInferenceSecret creates a new InferenceSecret resource with the given name
 // and spec.
 func NewInferenceSecret(name string, spec *summarizerv1.InferenceSecretSpec) *summarizerv1.InferenceSecret {
@@ -46,6 +85,31 @@ func NewInferenceSecret(name string, spec *summarizerv1.InferenceSecretSpec) *su
 	}
 }
 
+func ValidateInferenceSecret(s *summarizerv1.InferenceSecret) error {
+	switch {
+	case s == nil:
+		return trace.BadParameter("inference secret is nil")
+	case s.GetKind() != types.KindInferenceSecret:
+		return trace.BadParameter("kind must be %s, got %s", types.KindInferenceSecret, s.GetKind())
+	case s.GetSubKind() != "":
+		return trace.BadParameter("subkind must be empty")
+	case s.GetVersion() != types.V1:
+		return trace.BadParameter("unsupported version: %s", s.GetVersion())
+
+	case s.GetMetadata() == nil:
+		return trace.BadParameter("metadata is required")
+	case s.GetMetadata().GetName() == "":
+		return trace.BadParameter("metadata.name is required")
+
+	case s.GetSpec() == nil:
+		return trace.BadParameter("spec is required")
+	case s.GetSpec().GetValue() == "":
+		return trace.BadParameter("spec.value is required")
+	}
+
+	return nil
+}
+
 // NewInferencePolicy creates a new InferencePolicy resource with the given name
 // and spec.
 func NewInferencePolicy(name string, spec *summarizerv1.InferencePolicySpec) *summarizerv1.InferencePolicy {
@@ -57,4 +121,41 @@ func NewInferencePolicy(name string, spec *summarizerv1.InferencePolicySpec) *su
 		},
 		Spec: spec,
 	}
+}
+
+func ValidateInferencePolicy(p *summarizerv1.InferencePolicy) error {
+	switch {
+	case p == nil:
+		return trace.BadParameter("inference policy is nil")
+	case p.GetKind() != types.KindInferencePolicy:
+		return trace.BadParameter("kind must be %s, got %s", types.KindInferencePolicy, p.GetKind())
+	case p.GetSubKind() != "":
+		return trace.BadParameter("subkind must be empty")
+	case p.GetVersion() != types.V1:
+		return trace.BadParameter("unsupported version: %s", p.GetVersion())
+
+	case p.GetMetadata() == nil:
+		return trace.BadParameter("metadata is required")
+	case p.GetMetadata().GetName() == "":
+		return trace.BadParameter("metadata.name is required")
+
+	case p.GetSpec() == nil:
+		return trace.BadParameter("spec is required")
+	}
+
+	kinds := p.GetSpec().GetKinds()
+	if len(kinds) == 0 {
+		return trace.BadParameter("spec.kinds are required")
+	}
+
+	parser, err := services.NewWhereParser(&services.Context{})
+	if err != nil {
+		return trace.Wrap(err, "spec.filter has to be a valid predicate")
+	}
+	_, err = parser.Parse(p.GetSpec().Filter)
+	if err != nil {
+		return trace.Wrap(err, "spec.filter has to be a valid predicate")
+	}
+
+	return nil
 }
