7f2544655457aaf7abbcc8ba9319aafe87bef1d8
Zac Bergquist <zac.bergquist@goteleport.com>
2025-08-18 16:18:00 -0600

    Improve the dynamic Windows desktop reconciler
    The reconciler runs in the Windows desktop service, watching for
    dynamic_windows_desktop resources that match its configured labels,
    and creating corresponding windows_desktop resources for any matches.
    
    This change makes several improvements to the reconciler:
    
    - emit a debug-level log entry showing how long the reconciliation took
    - move more state into local variables in the reconciler goroutine
    - ensure that we hit the local process cache instead of the backend
    - remove a suspicious delete operation that seems to be unnecessary
      at best and actively harmful at worst
    
    Co-authored-by: Tim Ross <tim.ross@goteleport.com>

--- lib/auth/authclient/clt.go
+++ lib/auth/authclient/clt.go
@@ -1482,6 +1482,8 @@ type ClientI interface {
 	types.WebTokensGetter
 
 	DynamicDesktopClient() *dynamicwindows.Client
+	GetDynamicWindowsDesktop(ctx context.Context, name string) (types.DynamicWindowsDesktop, error)
+	ListDynamicWindowsDesktops(ctx context.Context, pageSize int, pageToken string) ([]types.DynamicWindowsDesktop, string, error)
 
 	// TrustClient returns a client to the Trust service.
 	TrustClient() trustpb.TrustServiceClient
