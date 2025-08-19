e2e5946bfa5825f54cb5ab5cbef018d0a6dbddf5
Ryan Clark <ryan.clark@goteleport.com>
2025-08-19 19:24:04 +0200

    Update lib/web/recordingmetadata.go
    Co-authored-by: Tiago Silva <tiago.silva@goteleport.com>

--- lib/web/recordingmetadata.go
+++ lib/web/recordingmetadata.go
@@ -1,5 +1,6 @@
-/**
- * Copyright (C) 2025 Gravitational, Inc.
+/*
+ * Teleport
+ * Copyright (C) 2023  Gravitational, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
