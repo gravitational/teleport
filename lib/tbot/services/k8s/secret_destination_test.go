6224028797eca2deda2eaed597902e0493f8c32b
Noah Stride <noah.stride@goteleport.com>
2025-08-20 12:02:19 +0100

    Fix tests

--- lib/tbot/services/k8s/secret_destination_test.go
+++ lib/tbot/services/k8s/secret_destination_test.go
@@ -88,7 +88,7 @@ func TestDestinationKubernetesSecret(t *testing.T) {
 			dest: &SecretDestination{
 				Name:      "my-secret",
 				Namespace: "my-other-namespace",
-				k8s:       fakeClientSet(),
+				k8s:       fake.NewClientset(),
 			},
 			wantNamespace: "my-other-namespace",
 		},
@@ -195,5 +195,9 @@ func TestDestinationKubernetesSecret_YAML(t *testing.T) {
 }
 
 func TestDestinationKubernetesSecret_String(t *testing.T) {
-	require.Equal(t, "kubernetes_secret: my-secret", (&SecretDestination{Name: "my-secret"}).String())
+	require.Equal(
+		t,
+		"kubernetes_secret: foo/my-secret",
+		(&SecretDestination{Namespace: "foo", Name: "my-secret"}).String(),
+	)
 }
