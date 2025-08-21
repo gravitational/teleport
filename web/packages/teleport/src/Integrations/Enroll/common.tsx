1d8b0b02f0af2443da7ca7c4fe534dc2f2af3272
alex hemard <alexhemard@gmail.com>
2025-08-21 12:43:35 -0500

    bug(Integrations): fix icon size (#57910)
    * bug(Integrations): fix icon size
    
    * don't forward size prop
    
    * cleanup

--- web/packages/teleport/src/Integrations/Enroll/common.tsx
+++ web/packages/teleport/src/Integrations/Enroll/common.tsx
@@ -71,8 +71,9 @@ export const NoCodeIntegrationDescription = () => (
  * IntegrationIcon wraps ResourceIcon with css required for integration
  * and plugin tiles.
  */
-export const IntegrationIcon = styled(ResourceIcon)<{ size?: number }>`
-  display: inline-block;
+export const IntegrationIcon = styled(ResourceIcon).withConfig({
+  shouldForwardProp: prop => prop !== 'size',
+})<{ size?: number }>`
   margin: 0 auto;
   height: 100%;
   min-width: 0;
