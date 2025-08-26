987cb192671e2262d8a7bca699194b423dd3654f
Pawel Kopiczko <pawel.kopiczko@goteleport.com>
2025-08-26 19:35:28 +0100

    Release v18.2.0-dev.kopiczko.2

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.1.6"
+const Version = "18.2.0-dev.kopiczko.2"
 
 const VersionMajor = 18
-const VersionMinor = 1
-const VersionPatch = 6
-const VersionPreRelease = ""
+const VersionMinor = 2
+const VersionPatch = 0
+const VersionPreRelease = "dev.kopiczko.2"
 const VersionMetadata = ""
