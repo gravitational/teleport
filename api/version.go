e86c41609da164437fac4a99ac3824196e2439cf
Angel Dionisio <angel.dionisio@goteleport.com>
2025-08-26 15:31:40 -0400

    Release v19.0.1-dev.angel.5

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.1-dev.angel.4"
+const Version = "19.0.1-dev.angel.5"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 1
-const VersionPreRelease = "dev.angel.4"
+const VersionPreRelease = "dev.angel.5"
 const VersionMetadata = ""
