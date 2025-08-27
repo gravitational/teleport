ddaf6504b4e6d3649a24f9b588a70086e43a9e1d
Gus Rivera <gus.rivera@goteleport.com>
2025-08-27 17:00:37 -0500

    Release 18.1.7 (#58414)

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.1.6"
+const Version = "18.1.7"
 
 const VersionMajor = 18
 const VersionMinor = 1
-const VersionPatch = 6
+const VersionPatch = 7
 const VersionPreRelease = ""
 const VersionMetadata = ""
