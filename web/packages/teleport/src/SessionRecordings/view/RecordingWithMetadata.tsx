dd89a1fc31c52553c380b69f4973e31e1ee051fe
Ryan Clark <ryan.clark@goteleport.com>
2025-08-27 11:15:47 +0200

    Move styles to the bottom

--- web/packages/teleport/src/SessionRecordings/view/RecordingWithMetadata.tsx
+++ web/packages/teleport/src/SessionRecordings/view/RecordingWithMetadata.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 import Flex from 'design/Flex';
-import { ArrowLineLeft, ChevronLeft, MoreVert, Terminal } from 'design/Icon';
+import { ArrowLineLeft, ChevronLeft, Terminal } from 'design/Icon';
 import { H3 } from 'design/Text';
 import { HoverTooltip } from 'design/Tooltip';
 
@@ -20,93 +20,6 @@ interface RecordingWithMetadataProps {
   sessionId: string;
   summarySlot?: SummarySlot;
 }
-
-const Grid = styled.div<{ sidebarVisible: boolean }>`
-  display: grid;
-  grid-template-areas: ${p =>
-    p.sidebarVisible
-      ? `'sidebar recording' 'timeline timeline'`
-      : `'recording recording' 'timeline timeline'`};
-  grid-template-columns: 1fr 4fr;
-  grid-template-rows: 1fr auto;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-`;
-
-const InfoGrid = styled.div`
-  display: grid;
-  column-gap: ${p => p.theme.space[3]}px;
-  row-gap: ${p => p.theme.space[2]}px;
-  grid-template-columns: 80px 1fr;
-  padding: 0 ${p => p.theme.space[3]}px;
-`;
-
-const Player = styled.div`
-  grid-area: recording;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-`;
-
-const Sidebar = styled.div`
-  grid-area: sidebar;
-  border-right: 1px solid ${p => p.theme.colors.spotBackground[1]};
-`;
-
-const Summary = styled.div`
-  border-top: 1px solid ${p => p.theme.colors.spotBackground[1]};
-`;
-
-const InfoGridLabel = styled.div`
-  font-weight: bold;
-  color: ${p => p.theme.colors.text.slightlyMuted};
-`;
-
-const BackLink = styled(Link)`
-  color: ${p => p.theme.colors.text.slightlyMuted};
-  text-decoration: none;
-  font-weight: 500;
-  display: flex;
-  align-items: center;
-  gap: ${p => p.theme.space[2]}px;
-`;
-
-const HideSidebarButton = styled.button`
-  background: none;
-  border: none;
-  color: ${p => p.theme.colors.text.slightlyMuted};
-  cursor: pointer;
-  padding: ${p => p.theme.space[2]}px;
-  border-radius: ${p => p.theme.radii[2]}px;
-  display: flex;
-  align-items: center;
-
-  &:hover {
-    background: ${p => p.theme.colors.spotBackground[1]};
-  }
-`;
-
-const ShowSidebarButton = styled(HideSidebarButton)`
-  background: ${p => p.theme.colors.levels.surface};
-  border: 1px solid ${p => p.theme.colors.spotBackground[2]};
-  border-left: none;
-  opacity: 0.6;
-  position: absolute;
-  top: 50%;
-  left: 0;
-  transform: translateY(-50%);
-  z-index: 10;
-  border-radius: 0 ${p => p.theme.radii[3]}px ${p => p.theme.radii[3]}px 0;
-  padding: ${p => p.theme.space[2]}px 0;
-
-  &:hover {
-    opacity: 1;
-  }
-`;
 
 export function RecordingWithMetadata({
   clusterId,
@@ -199,13 +112,76 @@ export function RecordingWithMetadata({
             {summary && <Summary>{summary}</Summary>}
           </Flex>
         </Sidebar>
-      ) : (
-        <HoverTooltip tipContent="Show Sidebar" placement="right">
-          <ShowSidebarButton onClick={toggleSidebar}>
-            <MoreVert size="large" />
-          </ShowSidebarButton>
-        </HoverTooltip>
       )}
     </Grid>
   );
 }
+
+const Grid = styled.div<{ sidebarVisible: boolean }>`
+  display: grid;
+  grid-template-areas: ${p =>
+    p.sidebarVisible
+      ? `'sidebar recording' 'timeline timeline'`
+      : `'recording recording' 'timeline timeline'`};
+  grid-template-columns: 1fr 4fr;
+  grid-template-rows: 1fr auto;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+const InfoGrid = styled.div`
+  display: grid;
+  column-gap: ${p => p.theme.space[3]}px;
+  row-gap: ${p => p.theme.space[2]}px;
+  grid-template-columns: 80px 1fr;
+  padding: 0 ${p => p.theme.space[3]}px;
+`;
+
+const Player = styled.div`
+  grid-area: recording;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const Sidebar = styled.div`
+  grid-area: sidebar;
+  border-right: 1px solid ${p => p.theme.colors.spotBackground[1]};
+`;
+
+const Summary = styled.div`
+  border-top: 1px solid ${p => p.theme.colors.spotBackground[1]};
+`;
+
+const InfoGridLabel = styled.div`
+  font-weight: bold;
+  color: ${p => p.theme.colors.text.slightlyMuted};
+`;
+
+const BackLink = styled(Link)`
+  color: ${p => p.theme.colors.text.slightlyMuted};
+  text-decoration: none;
+  font-weight: 500;
+  display: flex;
+  align-items: center;
+  gap: ${p => p.theme.space[2]}px;
+`;
+
+const HideSidebarButton = styled.button`
+  background: none;
+  border: none;
+  color: ${p => p.theme.colors.text.slightlyMuted};
+  cursor: pointer;
+  padding: ${p => p.theme.space[2]}px;
+  border-radius: ${p => p.theme.radii[2]}px;
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    background: ${p => p.theme.colors.spotBackground[1]};
+  }
+`;
