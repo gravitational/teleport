866e957f6bf297f66331fc647d9d7e6d4da4a36b
Trent Clarke <trent@goteleport.com>
2025-08-28 17:37:32 +1000

    [AWSIC] Adds RoleSyncMode selection to `tctl plugins install awsic`
    Allows the user to specify a RolesSyncMode on the command line when installing or
    editing the Identity Center integration. Also prevents the user from specifying
    group import filters when rolesSyncMode == NONE, because allowing them will make
    an invalid plugin configuration.

--- tool/tctl/common/plugin/plugins_command.go
+++ tool/tctl/common/plugin/plugins_command.go
@@ -53,6 +53,11 @@ type pluginInstallArgs struct {
 	github  githubArgs
 }
 
+type pluginEditArgs struct {
+	cmd   *kingpin.CmdClause
+	awsIC awsICEditArgs
+}
+
 type scimArgs struct {
 	cmd           *kingpin.CmdClause
 	samlConnector string
@@ -72,6 +77,7 @@ type PluginsCommand struct {
 	dryRun     bool
 	install    pluginInstallArgs
 	delete     pluginDeleteArgs
+	edit       pluginEditArgs
 }
 
 // Initialize creates the plugins command and subcommands
@@ -87,6 +93,7 @@ func (p *PluginsCommand) Initialize(app *kingpin.Application, _ *tctlcfg.GlobalC
 
 	p.initInstall(pluginsCommand, config)
 	p.initDelete(pluginsCommand)
+	p.initEdit(pluginsCommand)
 }
 
 func (p *PluginsCommand) initInstall(parent *kingpin.CmdClause, config *servicecfg.Config) {
@@ -105,6 +112,11 @@ func (p *PluginsCommand) initDelete(parent *kingpin.CmdClause) {
 	p.delete.cmd.
 		Arg("name", "The name of the SCIM plugin resource to delete").
 		StringVar(&p.delete.name)
+}
+
+func (p *PluginsCommand) initEdit(parent *kingpin.CmdClause) {
+	p.edit.cmd = parent.Command("edit", "Edits a plugin's or an integration's settings")
+	p.initEditAWSIC(p.edit.cmd)
 }
 
 // Delete implements `tctl plugins delete`, deleting a plugin from the Teleport cluster
@@ -214,6 +226,8 @@ func (p *PluginsCommand) TryRun(ctx context.Context, cmd string, clientFunc comm
 		commandFunc = p.InstallGithub
 	case p.delete.cmd.FullCommand():
 		commandFunc = p.Delete
+	case p.edit.awsIC.cmd.FullCommand():
+		commandFunc = p.EditAWSIC
 	default:
 		return false, nil
 	}
