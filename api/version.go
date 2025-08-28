8b459afb96802344055030ed9a5d138478630d3e
Angel Dionisio <angel.dionisio@goteleport.com>
2025-08-28 17:01:34 -0400

    Release v19.0.1-dev.aadcdev.2

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.1-dev.aadcdev.1"
+const Version = "19.0.1-dev.aadcdev.2"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 1
-const VersionPreRelease = "dev.aadcdev.1"
+const VersionPreRelease = "dev.aadcdev.2"
 const VersionMetadata = ""
