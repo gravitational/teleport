809a401d7bc7d95ea339e4c851d74c072ffbe867
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2025-08-28 09:25:39 +0200

    19.0.0-dev.gzdunek.24

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-dev.gzdunek.23"
+const Version = "19.0.0-dev.gzdunek.24"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev.gzdunek.23"
+const VersionPreRelease = "dev.gzdunek.24"
 const VersionMetadata = ""
