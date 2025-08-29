e14646c573e91ac1d29999983be3dad9b31d5250
Krzysztof Skrzętnicki <krzysztof.skrzetnicki@goteleport.com>
2025-08-29 16:00:14 +0200

    Release v18.1.7-dev.oratls.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.1.7"
+const Version = "18.1.7-dev.oratls.1"
 
 const VersionMajor = 18
 const VersionMinor = 1
 const VersionPatch = 7
-const VersionPreRelease = ""
+const VersionPreRelease = "dev.oratls.1"
 const VersionMetadata = ""
