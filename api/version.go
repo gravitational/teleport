93e8d5d7180166a67b6b8bf5d11ea88c8264be14
Ryan Hammonds <ryan.hammonds@goteleport.com>
2025-08-29 14:36:46 -0400

    Release v19.0.0-dev.removewebpack.3

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-dev.removewebpack.2"
+const Version = "19.0.0-dev.removewebpack.3"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev.removewebpack.2"
+const VersionPreRelease = "dev.removewebpack.3"
 const VersionMetadata = ""
