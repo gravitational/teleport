b17147c3b1dcc698794eed9e34e6d8c93eafd93b
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2025-09-01 14:14:32 +0200

    Open dialogs first before showing window

--- web/packages/teleterm/src/ui/services/headlessAuthn/headlessAuthnService.ts
+++ web/packages/teleterm/src/ui/services/headlessAuthn/headlessAuthnService.ts
@@ -35,15 +35,10 @@ export class HeadlessAuthenticationService {
     request: SendPendingHeadlessAuthenticationRequest,
     onRequestCancelled: (callback: () => void) => void
   ): Promise<void> {
-    const skipConfirm = this.configService.get('headless.skipConfirm').value;
-
-    // If the user wants to skip the confirmation step, then don't force the window.
-    // Instead, they can just tap their blinking yubikey with the window in the background.
-    if (!skipConfirm) {
-      await this.mainProcessClient.forceFocusWindow();
-    }
-
-    return new Promise(resolve => {
+    // Open the dialog before showing the window to defer connecting to
+    // resources that could show per-session MFA dialogs
+    // (see the comment for ForegroundSession in DocumentsRenderer.tsx).
+    const dialog = new Promise<void>(resolve => {
       const { closeDialog } = this.modalsService.openImportantDialog({
         kind: 'headless-authn',
         rootClusterUri: request.rootClusterUri,
@@ -56,6 +51,15 @@ export class HeadlessAuthenticationService {
 
       onRequestCancelled(closeDialog);
     });
+
+    const skipConfirm = this.configService.get('headless.skipConfirm').value;
+    // If the user wants to skip the confirmation step, then don't force the window.
+    // Instead, they can just tap their blinking yubikey with the window in the background.
+    if (!skipConfirm) {
+      await this.mainProcessClient.forceFocusWindow();
+    }
+
+    await dialog;
   }
 
   async updateHeadlessAuthenticationState(
