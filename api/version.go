89bd3cc28d225dbe8ee6d27f8a01592f924f2970
Fred Heinecke <fred.heinecke@goteleport.com>
2025-09-02 17:32:40 -0500

    Release 17.7.3

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "17.7.2"
+const Version = "17.7.3"
 
 const VersionMajor = 17
 const VersionMinor = 7
-const VersionPatch = 2
+const VersionPatch = 3
 const VersionPreRelease = ""
 const VersionMetadata = ""
