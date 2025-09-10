8e348f2a4ae3ea965c8d78a507b81a8eff646029
Ryan Hammonds <ryan.hammonds@goteleport.com>
2025-09-10 12:45:57 -0400

    Add tests for connection proxy.

--- lib/srv/desktop/tdp/conn_test.go
+++ lib/srv/desktop/tdp/conn_test.go
@@ -20,10 +20,14 @@ package tdp
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
 	"io"
 	"net"
+	"sync"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -83,4 +87,167 @@ func (f fakeTrackingConn) LocalAddr() net.Addr {
 
 func (f fakeTrackingConn) RemoteAddr() net.Addr {
 	return f.remote
+}
+
+func newMockRWC() rwc {
+	return rwc{
+		readChan:  make(chan Message, 10),
+		writeChan: make(chan Message, 10),
+	}
+}
+
+type rwc struct {
+	closeErr   error
+	readError  error
+	writeError error
+	readChan   chan Message
+	writeChan  chan Message
+	once       sync.Once
+}
+
+func (r *rwc) ReadMessage() (Message, error) {
+	if msg, ok := <-r.readChan; ok {
+		return msg, nil
+	}
+	return nil, fmt.Errorf("read failed: %w", r.readError)
+}
+
+func (r *rwc) WriteMessage(m Message) error {
+	if r.writeError != nil {
+		return fmt.Errorf("write failed: %w", r.writeError)
+	}
+	if r.writeChan != nil {
+		r.writeChan <- m
+		return nil
+	}
+	panic("invariant violation")
+}
+
+func (r *rwc) Close() error {
+	r.once.Do(func() {
+		close(r.readChan)
+		close(r.writeChan)
+	})
+	return r.closeErr
+}
+
+type mockMessage string
+
+func (m mockMessage) Encode() ([]byte, error) {
+	return []byte(string(m)), nil
+}
+
+func TestConnProxy(t *testing.T) {
+	// distinguished errors to use for validation
+	clientReadErr := errors.New("failed client")
+	serverReadErr := errors.New("failed server")
+	clientCloseError := errors.New("client close error")
+	serverCloseError := errors.New("server close error")
+	writeError := errors.New("something went wrong")
+
+	t.Run("error-handling", func(t *testing.T) {
+		tests := []struct {
+			name     string
+			setupFn  func(t *testing.T, client, server *rwc)
+			expectFn func(t *testing.T, proxyError error)
+		}{
+			{
+				name: "bidirectional-copy-no-errors",
+				setupFn: func(t *testing.T, clientConn, serverConn *rwc) {
+					// Message copied from client to server
+					clientConn.readChan <- mockMessage("hello server!")
+					msg := <-serverConn.writeChan
+					assert.Equal(t, "hello server!", string(msg.(mockMessage)))
+
+					// Message copied from server to client
+					serverConn.readChan <- mockMessage("hello client!")
+					msg = <-clientConn.writeChan
+					assert.Equal(t, "hello client!", string(msg.(mockMessage)))
+
+					// Both sides return EOF, no errors will be reported
+					serverConn.readError = io.EOF
+					clientConn.readError = io.EOF
+					_ = serverConn.Close()
+				},
+				expectFn: func(t *testing.T, proxyError error) {
+					require.NoError(t, proxyError)
+				},
+			},
+			{
+				name: "server-write-error",
+				setupFn: func(t *testing.T, clientConn, serverConn *rwc) {
+					serverConn.writeError = writeError
+					serverConn.readError = io.EOF
+					// Copy from client to server will fail
+					clientConn.readChan <- mockMessage("hello server!")
+					// Write error should be returned
+					serverConn.Close()
+				},
+				expectFn: func(t *testing.T, proxyError error) {
+					assert.ErrorIs(t, proxyError, writeError)
+				},
+			},
+			{
+				// Same as server-write-error, but swapped
+				name: "client-write-error",
+				setupFn: func(t *testing.T, clientConn, serverConn *rwc) {
+					clientConn.writeError = writeError
+					clientConn.readError = io.EOF
+					// Copy from server to client will fail
+					serverConn.readChan <- mockMessage("hello client!")
+					// Write error should be returned
+					clientConn.Close()
+				},
+				expectFn: func(t *testing.T, proxyError error) {
+					assert.ErrorIs(t, proxyError, writeError)
+				},
+			},
+			{
+				// Same as server and client read both fail
+				name: "server-and-client-read-error",
+				setupFn: func(t *testing.T, clientConn, serverConn *rwc) {
+					clientConn.readError = clientReadErr
+					serverConn.readError = serverReadErr
+					clientConn.Close()
+				},
+				expectFn: func(t *testing.T, proxyError error) {
+					// Both errors should be found in the error chain
+					assert.ErrorIs(t, proxyError, clientReadErr)
+					assert.ErrorIs(t, proxyError, serverReadErr)
+				},
+			},
+			{
+				// Same as server and client read both fail
+				name: "close-errors-returned",
+				setupFn: func(t *testing.T, clientConn, serverConn *rwc) {
+					// Both sides return EOF from read, but return errors on clos
+					serverConn.closeErr = serverCloseError
+					clientConn.closeErr = clientCloseError
+					serverConn.readError = io.EOF
+					clientConn.readError = io.EOF
+					_ = serverConn.Close()
+				},
+				expectFn: func(t *testing.T, proxyError error) {
+					// Both errors should be found in the error chain
+					assert.ErrorIs(t, proxyError, clientCloseError)
+					assert.ErrorIs(t, proxyError, serverCloseError)
+				},
+			},
+		}
+
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				clientConn := newMockRWC()
+				serverConn := newMockRWC()
+				proxy := NewConnProxy(&clientConn, &serverConn)
+				proxyError := make(chan error)
+				go func() {
+					proxyError <- proxy.Run()
+				}()
+
+				test.setupFn(t, &clientConn, &serverConn)
+				test.expectFn(t, <-proxyError)
+			})
+		}
+	})
 }
