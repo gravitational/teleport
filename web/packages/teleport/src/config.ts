db0f543656c94cd5b724f7a8244db9c86a8ec19a
Yassine Bounekhla <yassine@goteleport.com>
2025-09-10 11:27:22 -0500

    Update help and support page styles and remove manage cluster page

--- web/packages/teleport/src/config.ts
+++ web/packages/teleport/src/config.ts
@@ -174,7 +174,6 @@ const cfg = {
     sso: '/web/sso',
     cluster: '/web/cluster/:clusterId/',
     clusters: '/web/clusters',
-    manageCluster: '/web/clusters/:clusterId/manage',
 
     trustedClusters: '/web/trust',
     audit: '/web/cluster/:clusterId/audit',
@@ -729,10 +728,6 @@ const cfg = {
 
   getNodesRoute(clusterId: string) {
     return generatePath(cfg.routes.nodes, { clusterId });
-  },
-
-  getManageClusterRoute(clusterId: string) {
-    return generatePath(cfg.routes.manageCluster, { clusterId });
   },
 
   getUnifiedResourcesRoute(clusterId: string) {
