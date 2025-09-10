b9abf13a87be60e9a404c6f0ce2c9906a00399cb
Tim Buckley <tim@goteleport.com>
2025-09-10 13:06:37 -0600

    Add missing oracle reference in bot_service as well

--- lib/auth/machineid/machineidv1/bot_service.go
+++ lib/auth/machineid/machineidv1/bot_service.go
@@ -59,6 +59,7 @@ var SupportedJoinMethods = []types.JoinMethod{
 	types.JoinMethodTPM,
 	types.JoinMethodTerraformCloud,
 	types.JoinMethodBitbucket,
+	types.JoinMethodOracle,
 	types.JoinMethodBoundKeypair,
 }
 
