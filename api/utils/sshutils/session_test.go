b1c6aaf1a2d93ca004993da28113c0750c29cbb7
joerger <bjoerger@goteleport.com>
2025-09-10 17:20:49 -0700

    Update copyright year.

--- api/utils/sshutils/session_test.go
+++ api/utils/sshutils/session_test.go
@@ -1,4 +1,4 @@
-// Copyright 2022 Gravitational, Inc
+// Copyright 2025 Gravitational, Inc
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
