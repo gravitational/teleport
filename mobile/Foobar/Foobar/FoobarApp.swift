b7d79393e75de1f466d06bc92d7419758622b982
Rafał Cieślak <rafal.cieslak@goteleport.com>
2025-09-11 16:05:06 +0200

    Add mobile app skeleton

---
+++ mobile/Foobar/Foobar/FoobarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoobarApp.swift
+//  Foobar
+//
+//  Created by Rafał Cieślak on 2025-09-11.
+//
+
+import SwiftUI
+
+@main
+struct FoobarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
