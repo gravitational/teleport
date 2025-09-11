cb60c33d099486c3c550a25a78d70e5c84ee69b5
Angel Dionisio <angel.dionisio@goteleport.com>
2025-09-11 12:27:52 -0400

    Release v19.0.1-dev.winterrprovider.13

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.1-dev.winterrprovider.12"
+const Version = "19.0.1-dev.winterrprovider.13"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 1
-const VersionPreRelease = "dev.winterrprovider.12"
+const VersionPreRelease = "dev.winterrprovider.13"
 const VersionMetadata = ""
