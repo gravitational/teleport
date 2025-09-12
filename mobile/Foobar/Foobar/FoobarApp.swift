ab5f25781cce9379141cec66f544e5c83c799894
Rafał Cieślak <rafal.cieslak@goteleport.com>
2025-09-12 15:32:54 +0200

    Adjust lint and formatter configs

--- mobile/Foobar/Foobar/FoobarApp.swift
+++ mobile/Foobar/Foobar/FoobarApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct FoobarApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
