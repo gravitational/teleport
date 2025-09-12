62f8998ee2dba4343bfb061d9eb8c549ae47e4c0
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2025-09-12 11:15:47 +0200

    Do not use `set` utilities which are not available on v18

--- lib/client/api.go
+++ lib/client/api.go
@@ -98,7 +98,6 @@ import (
 	"github.com/gravitational/teleport/lib/tlsca"
 	"github.com/gravitational/teleport/lib/utils"
 	logutils "github.com/gravitational/teleport/lib/utils/log"
-	"github.com/gravitational/teleport/lib/utils/set"
 )
 
 const (
@@ -5617,11 +5616,14 @@ func (tc *TeleportClient) DialDatabase(ctx context.Context, route proto.RouteToD
 // so we need to ensure that we only present the allowed logins that would
 // result in a successful connection, if any exists.
 func CalculateSSHLogins(identityPrincipals []string, allowedLogins []string) ([]string, error) {
-	allowed := set.New(allowedLogins...)
+	allowed := make(map[string]struct{})
+	for _, login := range allowedLogins {
+		allowed[login] = struct{}{}
+	}
 
 	var logins []string
 	for _, local := range identityPrincipals {
-		if allowed.Contains(local) {
+		if _, ok := allowed[local]; ok {
 			logins = append(logins, local)
 		}
 	}
