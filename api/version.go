ca7a5c82a06a905ead05bab99a1c417a5573bde9
Erik Tate <erik.tate@goteleport.com>
2025-09-17 19:09:27 -0400

    Release v19.0.0-dev.erikt.3

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-dev.erikt.2"
+const Version = "19.0.0-dev.erikt.3"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev.erikt.2"
+const VersionPreRelease = "dev.erikt.3"
 const VersionMetadata = ""
