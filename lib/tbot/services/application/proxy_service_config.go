12f819861363c3437874d22b30b778314080dead
Noah Stride <noah.stride@goteleport.com>
2025-09-18 13:31:08 +0100

    Fix lint issues

--- lib/tbot/services/application/proxy_service_config.go
+++ lib/tbot/services/application/proxy_service_config.go
@@ -4,9 +4,8 @@ import (
 	"net"
 	"net/url"
 
-	"gopkg.in/yaml.v3"
-
 	"github.com/gravitational/trace"
+	"gopkg.in/yaml.v3"
 
 	"github.com/gravitational/teleport/lib/tbot/bot"
 	"github.com/gravitational/teleport/lib/tbot/internal/encoding"
