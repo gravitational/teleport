25727a30aa252ddfea4925b044685e3096dceef3
Zac Bergquist <zac.bergquist@goteleport.com>
2025-09-17 18:41:35 -0600

    Release 19.0.0-dev.backoff.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-dev"
+const Version = "19.0.0-dev.backoff.1"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev"
+const VersionPreRelease = "dev.backoff.1"
 const VersionMetadata = ""
