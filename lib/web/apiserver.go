94e98aefafae466cda3b6aa5810a519158bbc3f3
joerger <bjoerger@goteleport.com>
2025-09-18 12:56:28 -0700

    Fix tests and lint.

--- lib/web/apiserver.go
+++ lib/web/apiserver.go
@@ -4186,7 +4186,6 @@ func (h *Handler) generateSession(ctx context.Context, req *TerminalRequest, clu
 		ServerHostname: host,
 		ServerHostPort: port,
 		Moderated:      accessEvaluator.IsModerated(),
-		ID:             session.NewID(),
 		Created:        time.Now().UTC(),
 		LastActive:     time.Now().UTC(),
 		Namespace:      apidefaults.Namespace,
