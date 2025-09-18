e46fc635b8b1eba717317d5c1e6faabcc1b5c9a2
Rafał Cieślak <rafal.cieslak@goteleport.com>
2025-09-18 12:35:21 +0200

    Handle URLs of teleport scheme

--- mobile/Foobar/Foobar/ContentView.swift
+++ mobile/Foobar/Foobar/ContentView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var proxyAddr: String = "cluster.mirrors.link"
-  @FocusState private var focused: Bool
+  @State private var openedURL: URL?
+  @State private var isConfirmingEnrollment: Bool = false
+
+  var body: some View {
+    ScannedURLView(openedURL: $openedURL, isConfirmingEnrollment: $isConfirmingEnrollment)
+      .onOpenURL { url in
+        openedURL = url
+        isConfirmingEnrollment = true
+      }
+  }
+}
+
+struct ScannedURLView: View {
+  @Binding var openedURL: URL?
+  @Binding var isConfirmingEnrollment: Bool
 
   var body: some View {
     VStack(spacing: 16) {
@@ -20,86 +33,59 @@ struct ContentView: View {
           .padding(.horizontal, 16)
           .frame(height: 60)
       }.padding(.top, 16)
-      Form {
-        TextField("Cluster Address", text: $proxyAddr)
-          .focused($focused)
-          .autocorrectionDisabled()
-          .textInputAutocapitalization(.never)
-          .keyboardType(.URL)
-          .textContentType(.URL)
-          .onAppear {
-            UITextField.appearance().clearButtonMode = .whileEditing
-          }
-      }.onAppear {
-        focused = true
+      Spacer()
+      VStack {
+        VStack(alignment: .leading, spacing: 4) {
+          Label(
+            "Open Account Settings in the\u{00a0}Teleport Web\u{00a0}UI.",
+            systemImage: "1.circle"
+          )
+          Label("Select Enroll Mobile Device.", systemImage: "2.circle")
+          Label("Scan the QR code with the camera app.", systemImage: "3.circle")
+        }
+      }.padding(8)
+      Spacer()
+      Spacer()
+    }.confirmationDialog(
+      "Do you want to enroll this device?",
+      isPresented: $isConfirmingEnrollment,
+      titleVisibility: .visible,
+      presenting: openedURL
+    ) { _ in
+      Button("Enroll") {}
+      Button("Cancel", role: .cancel) {
+        openedURL = nil
       }
-      .onSubmit {
-        var addr = proxyAddr
-        if !addr.starts(with: "https://") {
-          addr = "https://\(addr)"
-        }
-        // TODO(ravicious): Ping the cluster first to see if it's a Teleport cluster.
-        var url = URL(string: addr)!
-        url.append(components: "webapi", "mfa", "login", "begin")
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = Data("{\"passwordless\":true}".utf8)
-
-        Task {
-          var data: Data
-          var response: URLResponse
-          do {
-            (data, response) = try await URLSession.shared.data(for: request)
-          } catch {
-            print("Failed to send POST request: \(error)")
-            return
-          }
-          guard let httpResponse = response as? HTTPURLResponse,
-                (200 ... 299).contains(httpResponse.statusCode)
-          else {
-            print("Got non-200 response: \(response)")
-            return
-          }
-          let decoder = JSONDecoder()
-          var mfaResponse: WebapiMFALoginBeginResponse
-          do {
-            mfaResponse = try decoder.decode(WebapiMFALoginBeginResponse.self, from: data)
-          } catch {
-            print("Failed to decode response: \(error)")
-            return
-          }
-          print("Got response \(mfaResponse)")
-        }
-      }.submitLabel(.send)
+    } message: { url in
+      Text(
+        """
+        This will enable \(url
+          .user(percentEncoded: false) ?? "") to authorize Device Trust web sessions \
+        in \(url.host(percentEncoded: false) ?? "") with this device.
+        """
+      )
     }
   }
 }
 
-#Preview {
-  ContentView()
+#Preview("No URL") {
+  ScannedURLView(openedURL: .constant(nil), isConfirmingEnrollment: .constant(false))
 }
 
-struct WebapiMFALoginBeginResponse: Codable {
-  let webauthnChallenge: WebauthnChallenge
-
-  enum CodingKeys: String, CodingKey {
-    case webauthnChallenge = "webauthn_challenge"
-  }
+#Preview("URL") {
+  ScannedURLView(
+    openedURL: .constant(
+      URL(string: "teleport://alice@example.com/enroll_mobile_device?user_token=1234")
+    ),
+    isConfirmingEnrollment: .constant(true)
+  )
 }
 
-struct WebauthnChallenge: Codable {
-  let publicKey: PublicKey
-}
-
-struct PublicKey: Codable {
-  let challenge: String
-  let timeout: Int
-  let rpID, userVerification: String
-
-  enum CodingKeys: String, CodingKey {
-    case challenge, timeout
-    case rpID = "rpId"
-    case userVerification
-  }
+#Preview("URL with email username") {
+  ScannedURLView(
+    openedURL: .constant(
+      URL(string: "teleport://alice%40example.com@example.com/enroll_mobile_device?user_token=1234")
+    ),
+    isConfirmingEnrollment: .constant(true)
+  )
 }
