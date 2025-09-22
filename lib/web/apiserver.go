1c7ab368f4b68b690943ce27513d3a6da11241c1
alex hemard <alex.hemard@goteleport.com>
2025-09-22 16:55:07 -0500

    [v18] fix(Web API): include SAML apps when filtering resources by 'app' (#59327)
    * fix(Web API): include SAML 'apps' when filtering resources by 'app'
    
    * feedback

--- lib/web/apiserver.go
+++ lib/web/apiserver.go
@@ -3227,6 +3227,12 @@ func makeUnifiedResourceRequest(r *http.Request) (*proto.ListUnifiedResourcesReq
 		}
 	}
 
+	// include KindSAMLIdPServiceProvider when requesting KindApp
+	if slices.Contains(kinds, types.KindApp) &&
+		!slices.Contains(kinds, types.KindSAMLIdPServiceProvider) {
+		kinds = append(kinds, types.KindSAMLIdPServiceProvider)
+	}
+
 	// set default kinds to be requested if none exist in the request
 	if len(kinds) == 0 {
 		kinds = []string{
