480c3b8e6852cd8aa97b5f16aecb1f2b453c0531
Krzysztof Skrzętnicki <krzysztof.skrzetnicki@goteleport.com>
2025-09-23 15:12:00 +0200

    sort imports

--- lib/gcp/dbusers_test.go
+++ lib/gcp/dbusers_test.go
@@ -19,9 +19,10 @@ package gcp
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/lib/defaults"
-	"github.com/stretchr/testify/require"
 )
 
 func TestAdjustDatabaseUsername(t *testing.T) {
