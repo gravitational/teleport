1b9e86ec9f37be302df216b69dee537f5a5feb30
Vadym Popov <vadym.popov@goteleport.com>
2025-09-23 23:09:28 -0700

    Add OS label for registered servers metric

--- lib/auth/auth.go
+++ lib/auth/auth.go
@@ -961,6 +961,7 @@ var (
 			Help:      "The number of Teleport services that are connected to an auth server.",
 		},
 		[]string{
+			teleport.TagOS,
 			teleport.TagVersion,
 			teleport.TagAutomaticUpdates,
 		},
@@ -2079,6 +2080,7 @@ func (a *Server) updateAgentMetrics() {
 	registeredAgents.Reset()
 	for agent, count := range imp.RegisteredAgentsCount() {
 		registeredAgents.With(prometheus.Labels{
+			teleport.TagOS:               agent.os,
 			teleport.TagVersion:          agent.version,
 			teleport.TagAutomaticUpdates: agent.automaticUpdates,
 		}).Set(float64(count))
