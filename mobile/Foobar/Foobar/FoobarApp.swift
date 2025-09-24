2c132f9c84ebf16ed5914e55e0fba8b0ec537015
Rafał Cieślak <rafal.cieslak@goteleport.com>
2025-09-24 14:57:46 +0200

    Connect to device trust service from Swift

--- mobile/Foobar/Foobar/FoobarApp.swift
+++ mobile/Foobar/Foobar/FoobarApp.swift
@@ -5,13 +5,26 @@
 //  Created by Rafał Cieślak on 2025-09-11.
 //
 
+import Connect
 import SwiftUI
 
 @main
 struct FoobarApp: App {
+  @State private var client = ProtocolClient(
+    httpClient: URLSessionHTTPClient(),
+    config: ProtocolClientConfig(
+      host: "https://teleport-mbp.ocelot-paradise.ts.net:3030/webapi/devicetrust/",
+      networkProtocol: .connect,
+      codec: ProtoCodec(),
+    )
+  )
   var body: some Scene {
     WindowGroup {
-      ContentView()
+      ContentView(
+        viewModel: DeviceTrustViewModel(
+          client: Teleport_Devicetrust_V1_DeviceTrustServiceClient(client: client)
+        )
+      )
     }
   }
 }
