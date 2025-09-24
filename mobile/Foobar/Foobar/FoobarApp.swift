db2a12ceb228fd04d2714fa9ee069a33f8329e0a
Rafał Cieślak <rafal.cieslak@goteleport.com>
2025-09-24 15:40:45 +0200

    Use NIOHTTPClient

--- mobile/Foobar/Foobar/FoobarApp.swift
+++ mobile/Foobar/Foobar/FoobarApp.swift
@@ -6,18 +6,24 @@
 //
 
 import Connect
+import ConnectNIO
 import SwiftUI
 
 @main
 struct FoobarApp: App {
   @State private var client = ProtocolClient(
-    httpClient: URLSessionHTTPClient(),
+    httpClient: NIOHTTPClient(
+      host: "https://teleport-mbp.ocelot-paradise.ts.net",
+      port: 3030,
+      timeout: nil
+    ),
     config: ProtocolClientConfig(
       host: "https://teleport-mbp.ocelot-paradise.ts.net:3030/webapi/devicetrust/",
       networkProtocol: .connect,
       codec: ProtoCodec(),
     )
   )
+
   var body: some Scene {
     WindowGroup {
       ContentView(
