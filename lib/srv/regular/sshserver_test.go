234d6574dd7de5cca3087936ce01764282b7d752
Brian Joerger <bjoerger@goteleport.com>
2025-09-26 08:13:26 -0700

    Replace flaky test with more straightforward event metadata test. (#59610)

--- lib/srv/regular/sshserver_test.go
+++ lib/srv/regular/sshserver_test.go
@@ -3041,7 +3041,7 @@ func TestHandlePuTTYWinadj(t *testing.T) {
 	require.Equal(t, "hello once more\n", string(out))
 }
 
-func TestTargetMetadata(t *testing.T) {
+func TestEventMetadata(t *testing.T) {
 	ctx := context.Background()
 	testServer, err := authtest.NewTestServer(authtest.ServerConfig{
 		Auth: authtest.AuthServerConfig{
