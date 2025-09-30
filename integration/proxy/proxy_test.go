7342e8e6676d1c9acdf573cfd41a9110a6d1cb03
Rafał Cieślak <rafal.cieslak@goteleport.com>
2025-09-30 18:05:18 +0200

    Attempt to trigger flaky tests detector

--- integration/proxy/proxy_test.go
+++ integration/proxy/proxy_test.go
@@ -1258,6 +1258,7 @@ func TestALPNSNIProxyDatabaseAccess(t *testing.T) {
 	})
 
 	t.Run("teleterm db gateways cert renewal", func(t *testing.T) {
+		t.Log("Flaky tests detector please trigger :~)")
 		testTeletermDbGatewaysCertRenewal(t, pack)
 	})
 }
@@ -1316,6 +1317,7 @@ func TestALPNSNIProxyAppAccess(t *testing.T) {
 	})
 
 	t.Run("teleterm app gateways cert renewal", func(t *testing.T) {
+		t.Log("Flaky tests detector please trigger :~)")
 		t.Run("without per-session MFA", func(t *testing.T) {
 			makeTC := func(t *testing.T) (*libclient.TeleportClient, mfa.WebauthnLoginFunc) {
 				user, _ := pack.CreateUser(t)
