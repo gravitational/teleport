2afbe8df835494502da9341331faa8da02f07f9c
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2025-09-30 02:54:25 +0200

    Release v18.2.4-dev.relay.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.2.3"
+const Version = "18.2.4-dev.relay.1"
 
 const VersionMajor = 18
 const VersionMinor = 2
-const VersionPatch = 3
-const VersionPreRelease = ""
+const VersionPatch = 4
+const VersionPreRelease = "dev.relay.1"
 const VersionMetadata = ""
