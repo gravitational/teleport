01d0c6ec9bb7910b157e35743460bcc17a8321af
Rafał Cieślak <rafal.cieslak@goteleport.com>
2025-10-02 15:25:43 +0200

    Collect device data and ping the server

--- mobile/Foobar/Foobar/FoobarApp.swift
+++ mobile/Foobar/Foobar/FoobarApp.swift
@@ -5,25 +5,10 @@
 //  Created by Rafał Cieślak on 2025-09-11.
 //
 
-import Connect
-import ConnectNIO
 import SwiftUI
 
 @main
 struct FoobarApp: App {
-  @State private var client = ProtocolClient(
-    httpClient: NIOHTTPClient(
-      host: "https://teleport-mbp.ocelot-paradise.ts.net",
-      port: 3030,
-      timeout: nil
-    ),
-    config: ProtocolClientConfig(
-      host: "https://teleport-mbp.ocelot-paradise.ts.net:3030/webapi/devicetrust/",
-      networkProtocol: .connect,
-      codec: ProtoCodec(),
-    )
-  )
-
   var body: some Scene {
     WindowGroup {
       ContentView(
