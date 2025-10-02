b7ab8694b2256c53b226875b58ad18ced86ae225
Cam Hutchison <camh@goteleport.com>
2025-10-02 10:28:02 +1000

    Release 18.2.4 (#59849)

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.2.3"
+const Version = "18.2.4"
 
 const VersionMajor = 18
 const VersionMinor = 2
-const VersionPatch = 3
+const VersionPatch = 4
 const VersionPreRelease = ""
 const VersionMetadata = ""
