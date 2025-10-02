eaee2b0018867f4e96b4e8d27c99c4eb9e8e0202
joerger <bjoerger@goteleport.com>
2025-10-02 14:50:58 -0700

    Remove extra major version grace period.

--- constants.go
+++ constants.go
@@ -834,7 +834,7 @@ const (
 	// will generate and share their own session ID when a new session
 	// is started (session and exec/shell channels accepted).
 	//
-	// TODO(Joerger): DELETE IN v21.0.0 (1 extra major version grace period)
+	// TODO(Joerger): DELETE IN v20.0.0
 	// All v17+ servers set the session ID. v19+ clients stop checking.
 	SessionIDQueryRequest = "session-id-query@goteleport.com"
 
@@ -842,7 +842,7 @@ const (
 	// will generate and share their own session ID when a new session
 	// channel is accepted, rather than when the shell/exec channel is.
 	//
-	// TODO(Joerger): DELETE IN v22.0.0 (1 extra major version grace period)
+	// TODO(Joerger): DELETE IN v21.0.0
 	// all v19+ servers set the session ID directly after accepting the session channel.
 	// clients should stop checking in v21, and servers should stop responding to the query in v22.
 	SessionIDQueryRequestV2 = "session-id-query-v2@goteleport.com"
