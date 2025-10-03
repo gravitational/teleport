65572b043c3ab954ec0ca04affd2970462d6d280
STeve (Xin) Huang <huangin52@gmail.com>
2025-10-03 14:35:02 -0400

    move reporting test to individual tests

--- lib/srv/mcp/stdio_test.go
+++ lib/srv/mcp/stdio_test.go
@@ -180,6 +180,14 @@ func TestHandleSession_execMCPServer(t *testing.T) {
 
 		// Check container is running.
 		require.NotEmpty(t, findDockerContainerID(t.Context(), dockerClient, containerName))
+
+		// Note that some metrics may be incremented by other tests too so here
+		// just checking they are non-zero.
+		require.Positive(t, testutil.ToFloat64(accumulatedSessions.WithLabelValues(types.MCPTransportStdio)))
+		require.Positive(t, testutil.ToFloat64(activeSessions.WithLabelValues(types.MCPTransportStdio)))
+		require.Positive(t, testutil.ToFloat64(messagesFromClient.WithLabelValues(types.MCPTransportStdio, "request", "initialize")))
+		require.Positive(t, testutil.ToFloat64(messagesFromClient.WithLabelValues(types.MCPTransportStdio, "notification", "notifications/initialized")))
+		require.Positive(t, testutil.ToFloat64(messagesFromServer.WithLabelValues(types.MCPTransportStdio, "response", "initialize")))
 	}
 
 	tests := []struct {
@@ -220,6 +228,9 @@ func TestHandleSession_execMCPServer(t *testing.T) {
 			cmd:                "fail-to-start",
 			checkHandlerError:  require.Error,
 			waitForHandlerExit: time.Second * 5,
+			afterHandlerStop: func(t *testing.T, _ *testContext, _ string) {
+				require.Positive(t, testutil.ToFloat64(setupErrors.WithLabelValues(types.MCPTransportStdio)))
+			},
 		},
 		{
 			// Make sure handler is not blocked when command starts then fails
@@ -308,14 +319,4 @@ func TestHandleSession_execMCPServer(t *testing.T) {
 			}
 		})
 	}
-
-	t.Run("reporting", func(t *testing.T) {
-		// Note that some metrics may be incremented by other tests too so here
-		// just checking they are non-zero.
-		require.Positive(t, testutil.ToFloat64(setupErrors.WithLabelValues(types.MCPTransportStdio)))
-		require.Positive(t, testutil.ToFloat64(accumulatedSessions.WithLabelValues(types.MCPTransportStdio)))
-		require.Positive(t, testutil.ToFloat64(messagesFromClient.WithLabelValues(types.MCPTransportStdio, "request", "initialize")))
-		require.Positive(t, testutil.ToFloat64(messagesFromClient.WithLabelValues(types.MCPTransportStdio, "notification", "notifications/initialized")))
-		require.Positive(t, testutil.ToFloat64(messagesFromServer.WithLabelValues(types.MCPTransportStdio, "response", "initialize")))
-	})
 }
