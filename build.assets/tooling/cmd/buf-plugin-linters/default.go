18350cda38da7b12b52f593d9cdb3e408173e8c4
Luke Okraszewski <lukasz.okraszewski@goteleport.com>
2025-10-08 16:01:19 +0100

    [api] Add ListKubernetesClusters (#59820)
    This commits deperecates GetKubernetesClusters, which
    should be replaced by a paginated counterpart:
    ListKubernetesClusters
    
    Towards: gravitational/teleport.e/issues/6759
    Changelog: Add paginated API ListKubernetesClusters, deprecate GetKubernetesClusters

--- build.assets/tooling/cmd/buf-plugin-linters/default.go
+++ build.assets/tooling/cmd/buf-plugin-linters/default.go
@@ -132,7 +132,6 @@ func newDefaultConfig() *Config {
 			"proto.AuthService.GetEvents":                                               {},
 			"proto.AuthService.GetGithubConnectors":                                     {},
 			"proto.AuthService.GetInstallers":                                           {},
-			"proto.AuthService.GetKubernetesClusters":                                   {},
 			"proto.AuthService.GetLocks":                                                {},
 			"proto.AuthService.GetMFADevices":                                           {},
 			"proto.AuthService.GetOIDCConnectors":                                       {},
