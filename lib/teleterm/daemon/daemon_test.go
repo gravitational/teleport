1ba0fc43ba425976c2d49497d7875341d635fc67
Chris Thach <chris.thach@goteleport.com>
2025-10-09 15:18:58 -0400

    chore: Bump Go to 1.24.8 ⬆️
    Signed-off-by: Chris Thach <chris.thach@goteleport.com>

--- lib/teleterm/daemon/daemon_test.go
+++ lib/teleterm/daemon/daemon_test.go
@@ -88,6 +88,7 @@ func (m *mockGatewayCreator) CreateGateway(ctx context.Context, params clusters.
 		WebProxyAddr:          hs.Listener.Addr().String(),
 		TCPPortAllocator:      m.tcpPortAllocator,
 		KubeconfigsDir:        m.t.TempDir(),
+		ClusterName:           "test-cluster", // Required to generate valid DNS SANs in cert.
 		Cert:                  gatewaytest.MustGenCertSignedWithCA(m.t, ca, identity),
 	}
 
