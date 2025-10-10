596569a4e356a259b3908232ba8733b6d974c661
Hugo Shaka <hugo.hervieux@goteleport.com>
2025-10-10 10:06:30 -0400

    add new tctl resource handler types and logic (#59518)
    * add new tctl resource handler types and logic
    
    * convert role
    
    * Update tool/tctl/common/resources/resource.go
    
    Co-authored-by: rosstimothy <39066650+rosstimothy@users.noreply.github.com>
    
    * Update tool/tctl/common/resources/role.go
    
    Co-authored-by: rosstimothy <39066650+rosstimothy@users.noreply.github.com>
    
    * address tim's feedback
    
    * fix after rebase
    
    * lint
    
    ---------
    
    Co-authored-by: rosstimothy <39066650+rosstimothy@users.noreply.github.com>

--- tool/tctl/common/db_command.go
+++ tool/tctl/common/db_command.go
@@ -112,7 +112,7 @@ func (c *DBCommand) ListDatabases(ctx context.Context, clt *authclient.Client) e
 	coll := &databaseServerCollection{servers: servers}
 	switch c.format {
 	case teleport.Text:
-		return trace.Wrap(coll.writeText(os.Stdout, c.verbose))
+		return trace.Wrap(coll.WriteText(os.Stdout, c.verbose))
 	case teleport.JSON:
 		return trace.Wrap(coll.writeJSON(os.Stdout))
 	case teleport.YAML:
