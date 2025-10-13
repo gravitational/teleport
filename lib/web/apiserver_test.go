940e9ea579fe689c4698c20f54101e7a25ada971
Tiago Silva <tiago.silva@goteleport.com>
2025-10-13 09:38:03 +0100

    remove duplicate watcher wait

--- lib/web/apiserver_test.go
+++ lib/web/apiserver_test.go
@@ -9490,8 +9490,6 @@ func startKubeWithoutCleanup(ctx context.Context, t *testing.T, cfg startKubeOpt
 		}
 		errChan <- err
 	}()
-	// wait for the watcher to init or it may race with test cleanup.
-	require.NoError(t, watcher.WaitInitialization())
 
 	// Waits for len(clusters) heartbeats to start
 	heartbeatsToExpect := len(cfg.clusters)
