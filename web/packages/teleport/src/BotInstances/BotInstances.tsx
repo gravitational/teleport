1a785e3a19af635dab521888cdb6f3c51ead6a59
Nick Marais <nicholas.marais@goteleport.com>
2025-10-13 12:47:07 +0100

    Make instance list messaging filter aware

--- web/packages/teleport/src/BotInstances/BotInstances.tsx
+++ web/packages/teleport/src/BotInstances/BotInstances.tsx
@@ -212,6 +212,7 @@ export function BotInstances() {
               onLoadNextPage={fetchNextPage}
               selectedItem={selectedItemId}
               onItemSelected={handleItemSelected}
+              isFiltering={!!query}
             />
             {selectedItemId ? (
               <BotInstanceDetails
