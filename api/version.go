4f1e39635da6a587a664e50faf88c353716fce8c
hugoShaka <hugo.hervieux@goteleport.com>
2025-10-14 09:26:23 -0400

    Release 19.0.0-dev.hugooai.0

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-dev"
+const Version = "19.0.0-dev.hugooai.0"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev"
+const VersionPreRelease = "dev.hugooai.0"
 const VersionMetadata = ""
