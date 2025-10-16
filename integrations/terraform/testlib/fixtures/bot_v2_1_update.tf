d1d7d366563fbf3d5931339c84213da636adafc0
Dan Upton <daniel.upton@goteleport.com>
2025-10-16 14:21:37 +0100

    Add expiry to test fixtures

--- integrations/terraform/testlib/fixtures/bot_v2_1_update.tf
+++ integrations/terraform/testlib/fixtures/bot_v2_1_update.tf
@@ -20,7 +20,8 @@ resource "teleport_bot_v2" "test" {
   version = "v1"
 
   metadata = {
-    name = local.bot_name
+    expires = "2038-01-01T00:00:00Z"
+    name    = local.bot_name
   }
 
   spec = {
