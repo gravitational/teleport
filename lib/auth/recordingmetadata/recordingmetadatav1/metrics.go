65dc8320c97e5e2128d2eb2383be58674131ea0f
Ryan Clark <ryan.clark@goteleport.com>
2025-10-16 16:41:11 +0200

    Define subsystem once

--- lib/auth/recordingmetadata/recordingmetadatav1/metrics.go
+++ lib/auth/recordingmetadata/recordingmetadatav1/metrics.go
@@ -7,23 +7,25 @@ import (
 	"github.com/gravitational/teleport/lib/observability/metrics"
 )
 
+const subsystem = "recording_metadata"
+
 var sessionsProcessedMetric = prometheus.NewCounterVec(prometheus.CounterOpts{
 	Namespace: teleport.MetricNamespace,
-	Subsystem: "recording_metadata",
+	Subsystem: subsystem,
 	Name:      "sessions_processed_total",
 	Help:      "Total number of session recordings processed, with success or failure",
 }, []string{"success"})
 
 var sessionsProcessingMetric = prometheus.NewGauge(prometheus.GaugeOpts{
 	Namespace: teleport.MetricNamespace,
-	Subsystem: "recording_metadata",
+	Subsystem: subsystem,
 	Name:      "sessions_processing_total",
 	Help:      "Number of session recordings being processed",
 })
 
 var sessionsPendingMetric = prometheus.NewGauge(prometheus.GaugeOpts{
 	Namespace: teleport.MetricNamespace,
-	Subsystem: "recording_metadata",
+	Subsystem: subsystem,
 	Name:      "sessions_pending_total",
 	Help:      "Number of sessions waiting to be processed",
 })
