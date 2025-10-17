3a34da3b69b9c5792ce3d60ffdbd0c88207d5121
Rafał Cieślak <rafal.cieslak@goteleport.com>
2025-10-17 11:56:22 +0200

    Update version to 18.2.4-dev.ravicious.1
    Refresh VNet policies

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.2.4"
+const Version = "18.2.4-dev.ravicious.1"
 
 const VersionMajor = 18
 const VersionMinor = 2
 const VersionPatch = 4
-const VersionPreRelease = ""
+const VersionPreRelease = "dev.ravicious.1"
 const VersionMetadata = ""
