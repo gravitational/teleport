2ea4f51aec3804a3ae6a3748aae637909879aca9
Paul Gottschling <paul.gottschling@goteleport.com>
2025-10-17 09:37:24 -0400

    Shorten help text

--- build.assets/tooling/cmd/resource-ref-generator/main.go
+++ build.assets/tooling/cmd/resource-ref-generator/main.go
@@ -26,46 +26,7 @@ import (
 	"github.com/gravitational/teleport/build.assets/tooling/cmd/resource-ref-generator/reference"
 )
 
-const configHelp string = `The path to a YAML configuration file with the following fields:
-
-## Main config
-
-required_field_types: a list of type info mappings (see "Type info") indicating
-type names of fields that must be present in a dynamic resource before we
-include it in the reference. For example, if this is "Metadata" from package
-"types", a struct type must include a field with the a field of "types.Metadata"
-before we add it to the reference.
-
-source (string): the path to the root of a Go project directory.
-
-destination (string): the path to a directory where the generator writes
-reference pages.
-
-excluded_resource_types: a list of type info mappings (see "Type info")
-indicating names of resources to exclude from the reference. 
-
-field_assignment_method: the name of a method of a resource type that assigns
-fields to the resource. Used to identify the kind and version of a resource.
-
-## Type info
-
-package: The path of a Go package
-name: The name of a type within the package
-
-## Example
-
-required_types:
-  - name: Metadata
-    package: api/types
-  - name: ResourceHeader
-    package: api/types
-source: "api"
-destination: "docs/pages/includes/resource-reference.mdx"
-excluded_resource_types:
-  - package: "types"
-    name: "ResourceHeader"
-field_assignment_method: "setStaticFields"
-`
+const configHelp string = `The path to a YAML configuration file (see the README)`
 
 func main() {
 	conf := flag.String("config", "conf.yaml", configHelp)
