432fd5bc3f328305b3bef380ab84caad01d5063a
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2025-10-20 10:33:59 -0400

    tctl resources: convert Auth, Proxy, Node (#60383)
    Updates https://github.com/gravitational/teleport/issues/60370.

--- tool/tctl/common/collection.go
+++ tool/tctl/common/collection.go
@@ -113,45 +113,6 @@ func printMetadataLabels(labels map[string]string) string {
 	return strings.Join(pairs, ",")
 }
 
-type serverCollection struct {
-	servers []types.Server
-}
-
-func (s *serverCollection) Resources() (r []types.Resource) {
-	for _, resource := range s.servers {
-		r = append(r, resource)
-	}
-	return r
-}
-
-func (s *serverCollection) WriteText(w io.Writer, verbose bool) error {
-	var rows [][]string
-	for _, se := range s.servers {
-		labels := common.FormatLabels(se.GetAllLabels(), verbose)
-		rows = append(rows, []string{
-			se.GetHostname(), se.GetName(), se.GetAddr(), labels, se.GetTeleportVersion(),
-		})
-	}
-	headers := []string{"Host", "UUID", "Public Address", "Labels", "Version"}
-	var t asciitable.Table
-	if verbose {
-		t = asciitable.MakeTable(headers, rows...)
-	} else {
-		t = asciitable.MakeTableWithTruncatedColumn(headers, rows, "Labels")
-	}
-
-	_, err := t.AsBuffer().WriteTo(w)
-	return trace.Wrap(err)
-}
-
-func (s *serverCollection) writeYAML(w io.Writer) error {
-	return utils.WriteYAML(w, s.servers)
-}
-
-func (s *serverCollection) writeJSON(w io.Writer) error {
-	return utils.WriteJSONArray(w, s.servers)
-}
-
 type authorityCollection struct {
 	cas []types.CertAuthority
 }
