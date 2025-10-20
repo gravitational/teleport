de3052f151223d321a8e54cfbcd1f7d362048422
Przemko Robakowski <przemko.robakowski@goteleport.com>
2025-10-20 20:51:32 +0200

    Update version to 18.1.8-dev.qiwa.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.1.8"
+const Version = "18.1.8-dev.qiwa.1"
 
 const VersionMajor = 18
 const VersionMinor = 1
 const VersionPatch = 8
-const VersionPreRelease = ""
+const VersionPreRelease = "dev.qiwa.1"
 const VersionMetadata = ""
