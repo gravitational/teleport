0e034d94efdcad71585c5a4d5a1357ae59253108
Przemko Robakowski <przemko.robakowski@goteleport.com>
2025-10-22 21:20:53 +0200

    Update version to 16.5.18-dev.nrel.6

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "16.5.18-dev.nrel.5"
+const Version = "16.5.18-dev.nrel.6"
 
 const VersionMajor = 16
 const VersionMinor = 5
 const VersionPatch = 18
-const VersionPreRelease = "dev.nrel.5"
+const VersionPreRelease = "dev.nrel.6"
 const VersionMetadata = ""
