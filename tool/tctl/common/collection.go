bfa4ce16a4cca2c92bd4d07fae2582c1cb7389ce
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2025-10-23 10:24:02 -0400

    Remove tctl get namespaces (#60500)
    Updates https://github.com/gravitational/teleport/issues/60370
    Updates https://github.com/gravitational/teleport/issues/49509
    
    Namesapces are a vestigial concept that do not apply to modern
    Teleport. Instead of migrating namespaces to the new tctl resources
    format they have been removed entirely.

--- tool/tctl/common/collection.go
+++ tool/tctl/common/collection.go
@@ -82,26 +82,6 @@ func (c namedResourceCollection) WriteText(w io.Writer, verbose bool) error {
 	return trace.Wrap(t.WriteTo(w))
 }
 
-type namespaceCollection struct {
-	namespaces []types.Namespace
-}
-
-func (n *namespaceCollection) Resources() (r []types.Resource) {
-	for i := range n.namespaces {
-		r = append(r, &n.namespaces[i])
-	}
-	return r
-}
-
-func (n *namespaceCollection) WriteText(w io.Writer, verbose bool) error {
-	t := asciitable.MakeTable([]string{"Name"})
-	for _, n := range n.namespaces {
-		t.AddRow([]string{n.Metadata.Name})
-	}
-	_, err := t.AsBuffer().WriteTo(w)
-	return trace.Wrap(err)
-}
-
 func printMetadataLabels(labels map[string]string) string {
 	pairs := []string{}
 	for key, value := range labels {
