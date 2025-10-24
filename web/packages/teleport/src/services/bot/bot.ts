fc64472c9804884af0ec87ee4718535c6d8266b1
Nick Marais <nicholas.marais@goteleport.com>
2025-10-24 17:02:18 +0100

    Update min supported version for `/metrics` endpoint

--- web/packages/teleport/src/services/bot/bot.ts
+++ web/packages/teleport/src/services/bot/bot.ts
@@ -281,7 +281,6 @@ export async function getBotInstanceMetrics(
 
     return data;
   } catch (err: unknown) {
-    // TODO(nicholasmarais1158) DELETE IN v20.0.0
-    withGenericUnsupportedError(err, '19.0.0');
+    withGenericUnsupportedError(err, '18.4.0');
   }
 }
