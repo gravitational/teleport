e92a0ebe74b01becbaab1e0d219e9126100e519d
Tim Buckley <tim@goteleport.com>
2025-10-24 20:24:41 -0600

    Fix imports

--- lib/join/server_oidc.go
+++ lib/join/server_oidc.go
@@ -21,12 +21,13 @@ package join
 import (
 	"context"
 
+	"github.com/gravitational/trace"
+
 	workloadidentityv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/workloadidentity/v1"
 	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/lib/join/internal/authz"
 	"github.com/gravitational/teleport/lib/join/internal/diagnostic"
 	"github.com/gravitational/teleport/lib/join/internal/messages"
-	"github.com/gravitational/trace"
 )
 
 // oidcTokenValidator is a function type that validates an OIDC token and checks that
