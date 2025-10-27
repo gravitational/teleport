8b6a05e7322b69a3321207121d201b69b3dcc526
Tim Buckley <tim@goteleport.com>
2025-10-27 17:02:20 -0600

    Add Env0 join method (#60386)
    * Add Env0 method
    
    This adds a new `env0` join method to support joining from env0
    workflows, and especially the embedded tbot in the Terraform provider
    when running on env0.
    
    This is the first OIDC join method on the new join service, and will
    not support legacy joining. As such, it won't be backported beyond
    v18.
    
    Closes #53798
    
    changelog: Add new `env0` join method to support joining within Env0 workflows
    
    * Fix imports
    
    * Fix failing test
    
    * Add env0 token source
    
    * Fix missing env0 join method in tbot
    
    * Fix env0 validation
    
    This fixes some issues in env0 token validation:
    - azp check is disabled since it's set to a random (ish) value
    - Use correct audience URL (their docs specified the wrong value)
    
    * Fix incorrect date in new file copyright header
    
    * Fix generated tfschema
    
    * Fix failing test after adding the token source
    
    * First round of code review
    
    * Add example token claims in comment
    
    * Move common OIDC handling logic into a separate handler
    
    Specific validation logic is now separate and the validator is passed
    as an argument to a generic `handleOIDCJoin()` handler.
    
    * Fix imports
    
    * Rename joinclient/join_env0.go to join_oidc.go

--- lib/auth/machineid/machineidv1/bot_service.go
+++ lib/auth/machineid/machineidv1/bot_service.go
@@ -49,6 +49,7 @@ var SupportedJoinMethods = []types.JoinMethod{
 	types.JoinMethodAzure,
 	types.JoinMethodAzureDevops,
 	types.JoinMethodCircleCI,
+	types.JoinMethodEnv0,
 	types.JoinMethodGCP,
 	types.JoinMethodGitHub,
 	types.JoinMethodGitLab,
