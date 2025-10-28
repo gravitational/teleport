7047cc52e35a2a6f72ae8c9adc6bd5e6a6e0e981
Chris Thach <chris.thach@protonmail.com>
2025-10-28 11:35:42 -0400

    tctl resource: Convert types.KindInstaller to Handler format (#60639)
    * refactor: Convert types.KindInstaller to Handler format
    
    Signed-off-by: Chris Thach <chris.thach@goteleport.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Hugo Shaka <hugo.hervieux@goteleport.com>
    
    ---------
    
    Signed-off-by: Chris Thach <chris.thach@goteleport.com>
    Co-authored-by: Hugo Shaka <hugo.hervieux@goteleport.com>

--- tool/tctl/common/collection.go
+++ tool/tctl/common/collection.go
@@ -770,33 +770,6 @@ func (c *kubeClusterCollection) WriteText(w io.Writer, verbose bool) error {
 	return trace.Wrap(err)
 }
 
-type installerCollection struct {
-	installers []types.Installer
-}
-
-func (c *installerCollection) Resources() []types.Resource {
-	var r []types.Resource
-	for _, inst := range c.installers {
-		r = append(r, inst)
-	}
-	return r
-}
-
-func (c *installerCollection) WriteText(w io.Writer, verbose bool) error {
-	for _, inst := range c.installers {
-		if _, err := fmt.Fprintf(w, "Script: %s\n----------\n", inst.GetName()); err != nil {
-			return trace.Wrap(err)
-		}
-		if _, err := fmt.Fprintln(w, inst.GetScript()); err != nil {
-			return trace.Wrap(err)
-		}
-		if _, err := fmt.Fprintln(w, "----------"); err != nil {
-			return trace.Wrap(err)
-		}
-	}
-	return nil
-}
-
 type integrationCollection struct {
 	integrations []types.Integration
 }
