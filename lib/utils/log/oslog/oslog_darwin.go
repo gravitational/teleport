fed4dda4174c102c0bbb4bc65e914e249cc16b54
Rafał Cieślak <rafal.cieslak@goteleport.com>
2025-10-28 16:43:37 +0100

    Avoid building macOS-specific stuff on iOS

--- lib/utils/log/oslog/oslog_darwin.go
+++ lib/utils/log/oslog/oslog_darwin.go
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+//go:build darwin && !ios
 package oslog
 
 // #cgo CFLAGS: -Wall -xobjective-c -fblocks -fobjc-arc -mmacosx-version-min=11.0
