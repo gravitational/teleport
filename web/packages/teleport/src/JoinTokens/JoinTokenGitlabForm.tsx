5da8830e3fe01f022e5bd7bfb84d577afba4940b
Nick Marais <nicholas.marais@goteleport.com>
2025-10-28 09:25:32 +0000

    feat: Create/Edit GitLab tokens from the Web UI (#60469)
    * Fix a bunch of `strictNullChecks` warnings
    
    * Improve GitHub form tests
    
    * Return gitlab config and rules from `/webapi/tokens`
    
    * Add `JoinTokenGitlabForm` component
    
    * Parse gitlab config from api response
    
    * Wire-in `JoinTokenGitlabForm`
    
    * Sort join method select options
    
    * Remove expiry for gitlab tokes
    
    * Remove unnecessary `toBeInTheDocument` assertions

--- web/packages/teleport/src/JoinTokens/JoinTokenGitlabForm.tsx
+++ web/packages/teleport/src/JoinTokens/JoinTokenGitlabForm.tsx
@@ -18,42 +18,36 @@
 
 import styled from 'styled-components';
 
-import { Info } from 'design/Alert/Alert';
 import Box from 'design/Box/Box';
 import { ButtonText } from 'design/Button/Button';
 import ButtonIcon from 'design/ButtonIcon/ButtonIcon';
 import Flex from 'design/Flex/Flex';
 import { Plus } from 'design/Icon/Icons/Plus';
 import { Trash } from 'design/Icon/Icons/Trash';
-import Link from 'design/Link/Link';
-import Text from 'design/Text/Text';
+import Text from 'design/Text';
 import FieldInput from 'shared/components/FieldInput/FieldInput';
-import { FieldSelect } from 'shared/components/FieldSelect';
+import { FieldSelect } from 'shared/components/FieldSelect/FieldSelect';
 import { requiredField } from 'shared/components/Validation/rules';
 
-import cfg from 'teleport/config';
 import { SectionBox } from 'teleport/Roles/RoleEditor/StandardEditor/sections';
 
 import { NewJoinTokenState } from './UpsertJoinTokenDialog';
 
-export const JoinTokenGithubForm = ({
-  tokenState,
-  onUpdateState,
-  readonly,
-}: {
+export function JoinTokenGitlabForm(props: {
   tokenState: NewJoinTokenState;
   onUpdateState: (newToken: NewJoinTokenState) => void;
   readonly: boolean;
-}) => {
-  const { github } = tokenState;
-  const { rules } = github ?? {};
+}) {
+  const { tokenState, onUpdateState, readonly } = props;
+  const { gitlab } = tokenState;
+  const { domain, static_jwks, rules } = gitlab ?? {};
 
   function removeRule(index: number) {
     const newRules = rules?.filter((_, i) => index !== i);
     const newState = {
       ...tokenState,
-      github: {
-        ...tokenState.github,
+      gitlab: {
+        ...tokenState.gitlab,
         rules: newRules,
       },
     };
@@ -63,8 +57,8 @@ export const JoinTokenGithubForm = ({
   function addNewRule() {
     const newState = {
       ...tokenState,
-      github: {
-        ...tokenState.github,
+      gitlab: {
+        ...tokenState.gitlab,
         rules: [
           ...(rules ?? []),
           {
@@ -79,8 +73,8 @@ export const JoinTokenGithubForm = ({
   function updateRuleField(index: number, fieldName: string, opts: unknown) {
     const newState = {
       ...tokenState,
-      github: {
-        ...tokenState.github,
+      gitlab: {
+        ...tokenState.gitlab,
         rules: rules?.map((rule, i) => {
           if (i === index) {
             return { ...rule, [fieldName]: opts };
@@ -101,7 +95,7 @@ export const JoinTokenGithubForm = ({
         >
           <Flex alignItems="center" justifyContent="space-between">
             <Text fontWeight={700} mb={2}>
-              GitHub Rule
+              GitLab Rule
             </Text>
 
             {rules.length > 1 && ( // at least one rule is required, so lets not allow the user to remove it
@@ -115,40 +109,36 @@ export const JoinTokenGithubForm = ({
           </Flex>
 
           <FieldInput
-            label="Repository"
-            placeholder="gravitational/teleport"
-            toolTipContent="Fully qualified name of a GitHub repository (i.e. including the owner)"
-            value={rule.repository ?? ''}
-            onChange={e => updateRuleField(index, 'repository', e.target.value)}
-            rule={
-              rule.repository_owner
-                ? undefined
-                : requiredField('Either repository name or owner is required')
-            }
-            readonly={readonly}
-          />
-
-          <FieldInput
-            label="Repository owner"
-            placeholder="gravitational"
-            toolTipContent="Name of an organization or user that a repository belongs to"
-            value={rule.repository_owner ?? ''}
+            label="Project path"
+            placeholder="my-user/my-project"
+            value={rule.project_path ?? ''}
             onChange={e =>
-              updateRuleField(index, 'repository_owner', e.target.value)
+              updateRuleField(index, 'project_path', e.target.value)
             }
             rule={
-              rule.repository
+              rule.namespace_path
                 ? undefined
-                : requiredField('Either repository owner or name is required')
+                : requiredField(
+                    'Either project path or namespace path is required'
+                  )
             }
             readonly={readonly}
           />
 
           <FieldInput
-            label="Workflow name"
-            placeholder="my-workflow"
-            value={rule.workflow ?? ''}
-            onChange={e => updateRuleField(index, 'workflow', e.target.value)}
+            label="Namespace path"
+            placeholder="my-user"
+            value={rule.namespace_path ?? ''}
+            onChange={e =>
+              updateRuleField(index, 'namespace_path', e.target.value)
+            }
+            rule={
+              rule.project_path
+                ? undefined
+                : requiredField(
+                    'Either namespace path or project path is required'
+                  )
+            }
             readonly={readonly}
           />
 
@@ -187,91 +177,54 @@ export const JoinTokenGithubForm = ({
 
       <ButtonText onClick={addNewRule} compact mb={4}>
         <Plus size={16} mr={2} />
-        Add another GitHub rule
+        Add another GitLab rule
       </ButtonText>
 
       {/* TODO(nicholasmarais1158): Make SectionBox a component instead of reusing it from Roles */}
       <SectionBox
-        titleSegments={['GitHub Enterprise Server']}
-        initiallyCollapsed={[
-          'server_host',
-          'enterprise_slug',
-          'static_jwks',
-        ].every(k => !github?.[k])}
+        titleSegments={['Advanced Configuration']}
+        initiallyCollapsed={['domain', 'static_jwks'].every(k => !gitlab?.[k])}
         validation={{
           valid: true,
         }}
       >
-        <Text fontWeight="regular" mb={3}>
-          Additional settings for configuring GHES.
-        </Text>
-
-        {cfg.edition !== 'ent' ? (
-          <Info alignItems="flex-start">
-            GitHub Enterprise Server configuration requires Teleport Enterprise.
-            Please use a repository hosted at github.com or{' '}
-            <Link
-              target="_blank"
-              href="https://goteleport.com/signup/enterprise/"
-            >
-              contact us
-            </Link>
-            .
-          </Info>
-        ) : undefined}
-
         <FieldInput
-          label="Server host"
-          placeholder="github.example.com"
-          value={github?.server_host ?? ''}
+          label="Domain"
+          placeholder="gitlab.example.com"
+          toolTipContent="If you are using GitLab's cloud hosted offering, leave this field empty"
+          value={domain ?? ''}
           onChange={e =>
             onUpdateState({
               ...tokenState,
-              github: {
-                ...tokenState.github,
-                server_host: e.target.value,
+              gitlab: {
+                ...tokenState.gitlab,
+                domain: e.target.value,
               },
             })
           }
-          readonly={readonly || cfg.edition !== 'ent'}
-        />
-
-        <FieldInput
-          label="Slug"
-          placeholder="octo-enterprise"
-          value={github?.enterprise_slug ?? ''}
-          onChange={e =>
-            onUpdateState({
-              ...tokenState,
-              github: {
-                ...tokenState.github,
-                enterprise_slug: e.target.value,
-              },
-            })
-          }
-          readonly={readonly || cfg.edition !== 'ent'}
+          readonly={readonly}
         />
 
         <FieldInput
           label="Static JWKS"
           placeholder='{"keys":[--snip--]}'
-          toolTipContent="JSON Web Key Set used to verify the token issued by GitHub Actions"
-          value={github?.static_jwks ?? ''}
+          toolTipContent="JSON Web Key Set used to verify the token issued by GitLab"
+          value={static_jwks ?? ''}
           onChange={e =>
             onUpdateState({
               ...tokenState,
-              github: {
-                ...tokenState.github,
+              gitlab: {
+                ...tokenState.gitlab,
                 static_jwks: e.target.value,
               },
             })
           }
-          readonly={readonly || cfg.edition !== 'ent'}
+          readonly={readonly}
         />
       </SectionBox>
     </>
   );
-};
+}
 
 const refTypeOptions = [
   { value: 'any' as const, label: 'Any' },
