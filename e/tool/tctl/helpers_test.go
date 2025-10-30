f94129e46319f3f5fc6173817b7758279c9bd295
Kevin <76608931+kshi36@users.noreply.github.com>
2025-10-30 10:54:17 -0700

    test: Add `tctl devices` --format test using an Enterprise Auth Server (#7443)
    * Added devices formatting test using Enterprise Auth Server, depends on OSS changes
    
    * Fix lint
    
    * Refactor
    
    Source-commit: gravitational/teleport.e@c3c8a1d3e388180c0522df82756f4395cfea62f2
    Source-pull-requests: gravitational/teleport.e#7443

---
+++ e/tool/tctl/helpers_test.go
@@ -0,0 +1,84 @@
+package main_test
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	kyaml "k8s.io/apimachinery/pkg/util/yaml"
+
+	"github.com/gravitational/teleport/api/breaker"
+	"github.com/gravitational/teleport/lib/auth/authclient"
+	"github.com/gravitational/teleport/lib/service/servicecfg"
+	"github.com/gravitational/teleport/lib/services"
+	"github.com/gravitational/teleport/lib/utils"
+	tctl "github.com/gravitational/teleport/tool/tctl/common"
+	tctlcfg "github.com/gravitational/teleport/tool/tctl/common/config"
+)
+
+func runCommand(t require.TestingT, client *authclient.Client, cmd tctl.CLICommand, args []string) error {
+	cfg := servicecfg.MakeDefaultConfig()
+	cfg.CircuitBreakerConfig = breaker.NoopBreakerConfig()
+
+	app := utils.InitCLIParser("tctl", tctl.GlobalHelpString)
+	cmd.Initialize(app, &tctlcfg.GlobalCLIFlags{}, cfg)
+
+	selectedCmd, err := app.Parse(args)
+	require.NoError(t, err)
+
+	_, err = cmd.TryRun(context.Background(), selectedCmd, func(ctx context.Context) (*authclient.Client, func(context.Context), error) {
+		return client, func(context.Context) {}, nil
+	})
+	return err
+}
+
+func runDevicesCommand(t *testing.T, client *authclient.Client, args []string) (*bytes.Buffer, error) {
+	var stdoutBuff bytes.Buffer
+	command := &tctl.DevicesCommand{
+		Stdout: &stdoutBuff,
+	}
+
+	args = append([]string{"devices"}, args...)
+	return &stdoutBuff, runCommand(t, client, command, args)
+}
+
+func mustDecodeJSON[T any](t *testing.T, r io.Reader) T {
+	var out T
+	err := json.NewDecoder(r).Decode(&out)
+	require.NoError(t, err)
+	return out
+}
+
+func mustTranscodeYAMLToJSON(t *testing.T, r io.Reader) []byte {
+	decoder := kyaml.NewYAMLToJSONDecoder(r)
+	var resource services.UnknownResource
+	require.NoError(t, decoder.Decode(&resource))
+	return resource.Raw
+}
+
+// mustTranscodeYAMLDocsToJSON safely transcodes YAML docs for unknown resources.
+func mustTranscodeYAMLDocsToJSON(t *testing.T, r io.Reader) []byte {
+	decoder := kyaml.NewYAMLToJSONDecoder(r)
+	var jsonRaw []json.RawMessage
+
+	for {
+		var resource services.UnknownResource
+		if err := decoder.Decode(&resource); err != nil {
+			// Break when there are no more documents to decode
+			if !errors.Is(err, io.EOF) {
+				require.FailNow(t, "error transcoding YAML docs to JSON: %v", err)
+			}
+			break
+		}
+		jsonRaw = append(jsonRaw, resource.Raw)
+	}
+
+	jsonDocs, err := json.Marshal(jsonRaw)
+	require.NoError(t, err)
+
+	return jsonDocs
+}
