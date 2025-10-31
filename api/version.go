85dad13e95edd0412e70c857969dda275f1e386c
Forrest Marshall <forrest@goteleport.com>
2025-10-30 19:28:49 -0700

    Release v18.3.1-dev.scopes.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.3.0"
+const Version = "18.3.1-dev.scopes.1"
 
 const VersionMajor = 18
 const VersionMinor = 3
-const VersionPatch = 0
-const VersionPreRelease = ""
+const VersionPatch = 1
+const VersionPreRelease = "dev.scopes.1"
 const VersionMetadata = ""
