59ec566c70bc45248aaa2e23a67a66d52e362c15
Chris Thach <chris.thach@goteleport.com>
2025-11-06 12:32:52 -0500

    Print NotBefore
    Signed-off-by: Chris Thach <chris.thach@goteleport.com>

--- lib/utils/cli.go
+++ lib/utils/cli.go
@@ -265,7 +265,8 @@ DEBUG INFO:
     Subject: %s
     Issuer: %s
     Serial Number: %s
-    Expiration: %s
+    Not Before: %s
+    Not After: %s
     DNS Names: %v
     IP Addresses: %v`,
 			hostnameErr.Host,
@@ -273,6 +274,7 @@ DEBUG INFO:
 			hostnameErr.Certificate.Subject,
 			hostnameErr.Certificate.Issuer,
 			hostnameErr.Certificate.SerialNumber,
+			hostnameErr.Certificate.NotBefore,
 			hostnameErr.Certificate.NotAfter,
 			hostnameErr.Certificate.DNSNames,
 			hostnameErr.Certificate.IPAddresses,
