85de6ca24152f9c97ec87f40d031edb94702adbb
Stephen Levine <stephen.levine@goteleport.com>
2025-11-07 13:05:45 -0500

    Release 18.3.2-dev.sclevine.8

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.3.1"
+const Version = "18.3.2-dev.sclevine.8"
 
 const VersionMajor = 18
 const VersionMinor = 3
-const VersionPatch = 1
-const VersionPreRelease = ""
+const VersionPatch = 2
+const VersionPreRelease = "dev.sclevine.8"
 const VersionMetadata = ""
