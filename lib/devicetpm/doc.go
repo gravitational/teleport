2aae7788415df412952480c1250944f459822d1b
Alan Parra <alan.parra@goteleport.com>
2025-11-12 12:39:05 -0300

    Add the lib/devicetpm package

---
+++ lib/devicetpm/doc.go
@@ -0,0 +1,5 @@
+// Package devicetpm wraps TPM-related methods for Device Trust.
+//
+// It is useful to keep consistent TPM interactions and to allow for clean
+// go-attestation upgrades on e/.
+package devicetpm
