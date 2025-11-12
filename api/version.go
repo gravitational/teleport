f5bb94614545251ec47fd781c9e7d6f59ba6760c
Maxim Dietz <maxim.dietz@goteleport.com>
2025-11-12 15:43:35 -0500

    Release 18.3.3-dev.arrc.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.3.2"
+const Version = "18.3.3-dev.arrc.1"
 
 const VersionMajor = 18
 const VersionMinor = 3
-const VersionPatch = 2
-const VersionPreRelease = ""
+const VersionPatch = 3
+const VersionPreRelease = "dev.arrc.1"
 const VersionMetadata = ""
