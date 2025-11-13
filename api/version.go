cbe60444b7feecefef72ccef27ceada6ff914177
Maxim Dietz <maxim.dietz@goteleport.com>
2025-11-13 17:29:48 -0500

    Release 18.3.3-dev.kiosion.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.3.2"
+const Version = "18.3.3-dev.kiosion.1"
 
 const VersionMajor = 18
 const VersionMinor = 3
-const VersionPatch = 2
-const VersionPreRelease = ""
+const VersionPatch = 3
+const VersionPreRelease = "dev.kiosion.1"
 const VersionMetadata = ""
