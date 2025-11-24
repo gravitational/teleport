1e4e2fbb14e383db86aede70268b55540b55515c
Zac Bergquist <zac.bergquist@goteleport.com>
2025-11-24 15:59:55 -0700

    Add Rust RDP decoder library
    This library makes the IronRDP fast path decoder available to Go code
    via CGo. There are three build options:
    
    1. A standalone library (for use by tsh), enabled with the rust_rdp_decoder
       build tag.
    2. All functionality included in the existing RDP client library,
       enabled with the desktop_access_rdp build tag.
    3. A no-op implementation which avoids any Rust dependencies.
    
    The decoder also respects the RDPCLIENT_SKIP_BUILD=1 flag from the makefile,
    under the assumption that if you don't have Rust tooling and don't want
    to build the RDP client that you also don't want the decoder.

---
+++ lib/srv/desktop/rdp/decoder/decoder_standalone.go
@@ -0,0 +1,35 @@
+//go:build !desktop_access_rdp && rust_rdp_decoder
+
+// Teleport
+// Copyright (C) 2025 Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package decoder
+
+/*
+#cgo linux,386 LDFLAGS: -L${SRCDIR}/../../../../../target/i686-unknown-linux-gnu/release
+#cgo linux,amd64 LDFLAGS: -L${SRCDIR}/../../../../../target/x86_64-unknown-linux-gnu/release
+#cgo linux,arm LDFLAGS: -L${SRCDIR}/../../../../../target/arm-unknown-linux-gnueabihf/release
+#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/../../../../../target/aarch64-unknown-linux-gnu/release
+#cgo linux LDFLAGS: -lrdp_decoder
+
+#cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/../../../../../target/x86_64-apple-darwin/release
+#cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/../../../../../target/aarch64-apple-darwin/release
+#cgo darwin LDFLAGS: -lrdp_decoder
+
+#cgo windows,amd64 LDFLAGS: -L${SRCDIR}/../../../../../target/x86_64-pc-windows-gnu/release
+#cgo windows LDFLAGS: -lrdp_decoder
+*/
+import "C"
