fc2f599ee023b850fc13f8ce5ab6a958d95770bc
Kevin Shi <kevin.shi@goteleport.com>
2025-11-25 15:20:10 -0800

    Fix test

--- lib/srv/discovery/discovery_eks_test.go
+++ lib/srv/discovery/discovery_eks_test.go
@@ -25,7 +25,6 @@ import (
 	"iter"
 	"maps"
 	"slices"
-	"sync"
 	"testing"
 	"testing/synctest"
 	"time"
@@ -319,42 +318,8 @@ type mockAuthServer struct {
 	enrollEKSClusters func(context.Context, *integrationpb.EnrollEKSClustersRequest, ...grpc.CallOption) (*integrationpb.EnrollEKSClustersResponse, error)
 }
 
-type mockWatcher struct {
-	done     chan struct{}
-	events   chan types.Event
-	doneOnce sync.Once
-}
-
-func (w *mockWatcher) Events() <-chan types.Event {
-	return w.events
-}
-
-func (w *mockWatcher) Close() error {
-	w.doneOnce.Do(func() {
-		close(w.done)
-		close(w.events)
-	})
-	return nil
-}
-
-func (w *mockWatcher) Error() error {
-	return nil
-}
-
-func (w *mockWatcher) Done() <-chan struct{} {
-	return w.done
-}
-
 func (m *mockAuthServer) NewWatcher(ctx context.Context, watch types.Watch) (types.Watcher, error) {
-	w := &mockWatcher{
-		done:   make(chan struct{}),
-		events: make(chan types.Event, 1),
-	}
-
-	w.events <- types.Event{
-		Type: types.OpInit,
-	}
-	return w, nil
+	return m.events.NewWatcher(ctx, watch)
 }
 
 func (m *mockAuthServer) Ping(context.Context) (proto.PingResponse, error) {
