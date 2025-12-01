31f4d08ad6d8f504cbe87d285f1b90d441431330
Zac Bergquist <zac.bergquist@goteleport.com>
2025-12-01 09:55:44 -0700

    authtest: properly close auth server

--- lib/auth/authtest/authtest.go
+++ lib/auth/authtest/authtest.go
@@ -1180,11 +1180,10 @@ func (t *TLSServer) Close() error {
 		errs = append(errs, err)
 	}
 
-	if t.AuthServer.Backend != nil {
-		if err := t.AuthServer.Backend.Close(); err != nil {
-			errs = append(errs, err)
-		}
+	if err := t.AuthServer.Close(); err != nil {
+		errs = append(errs, err)
 	}
+
 	return trace.NewAggregate(errs...)
 }
 
@@ -1199,13 +1198,12 @@ func (t *TLSServer) Shutdown(ctx context.Context) error {
 		if err := t.Listener.Close(); err != nil && !utils.IsUseOfClosedNetworkError(err) {
 			errs = append(errs, err)
 		}
+	}
 
+	if err := t.AuthServer.Close(); err != nil {
+		errs = append(errs, err)
 	}
-	if t.AuthServer.Backend != nil {
-		if err := t.AuthServer.Backend.Close(); err != nil {
-			errs = append(errs, err)
-		}
-	}
+
 	return trace.NewAggregate(errs...)
 }
 
