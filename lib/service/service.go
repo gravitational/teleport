385aba68a9611350f0689ebffd288d3e5cae7b86
Stephen Levine <stephen.levine@goteleport.com>
2025-12-03 13:23:14 -0500

    remove warning for kube

--- lib/service/service.go
+++ lib/service/service.go
@@ -1377,6 +1377,12 @@ func NewTeleport(cfg *servicecfg.Config) (_ *TeleportProcess, err error) {
 		if upgraderKind == types.UpgraderKindTeleportUpdate ||
 			upgraderKind == types.UpgraderKindKubeController {
 			info, err := autoupdate.ReadHelloUpdaterInfo(supervisor.ExitContext(), cfg.Logger, hostID)
+			// If the config file is not present for Kubernetes upgraders, do not set the updater info.
+			// This ensures that older chart versions maintain the same behavior when used with newer Teleport.
+			if errors.Is(err, autoupdate.ErrConfigNotFound) &&
+				upgraderKind == types.UpgraderKindKubeController {
+				return hello, nil
+			}
 			if err != nil {
 				// Failing to detect updater info is not fatal, we continue.
 				cfg.Logger.WarnContext(supervisor.ExitContext(), "Error recovering updater status, this might affect automatic update tracking and progress.", "error", err)
