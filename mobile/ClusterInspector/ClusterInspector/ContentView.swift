a7a4a51ad54f6cf18a4f5234ca69b44742db4457
Rafał Cieślak <rafal.cieslak@goteleport.com>
2025-12-03 12:54:25 +0100

    Fall back to webapi/find if gRPC service is not implemented

--- mobile/ClusterInspector/ClusterInspector/ContentView.swift
+++ mobile/ClusterInspector/ClusterInspector/ContentView.swift
@@ -46,8 +46,10 @@ struct ContentView: View {
           options: .repeat(.continuous),
           isActive: true
         )
-      case let .success(response):
+      case let .success(.grpc(response)):
         FindResponseView(response: response)
+      case let .success(.webapi(response)):
+        WebapiFindResponseView(response: response)
       case let .failure(failure):
         Spacer()
         VStack {
@@ -72,10 +74,23 @@ struct ContentView: View {
   }
 }
 
+// It looks like @preconcurrency on the Ping framework poisons everything else.
+// As long as we compare FindResponse values within the main actor (e.g. within SwiftUI views),
+// everything should work fine. But the moment we compare it within a background task or a different
+// actor,
+// we'll get a runtime crash.
+// https://forums.swift.org/t/default-main-actor-isolation-and-protocol-inheritance/80419
+enum FindResponse: @preconcurrency Equatable, Sendable {
+  case grpc(PingFindResponse)
+  case webapi(WebapiFindResponse)
+}
+
+let GoErrorDomain = "go"
+
 @Observable
 class FindViewModel {
   var clusterAddress = ""
-  var findAttempt: Attempt<PingFindResponse, FindError> = .idle
+  var findAttempt: Attempt<FindResponse, FindError> = .idle
   private var findTask: Task<Void, Never>?
 
   func startFind() {
@@ -97,7 +112,26 @@ class FindViewModel {
         do {
           let response = try await ping.find(ctx, proxyServer: clusterAddress)
           if Task.isCancelled { return }
-          findAttempt = .success(response)
+
+          findAttempt = .success(.grpc(response))
+          return
+        } catch {
+          if Task.isCancelled { return }
+          let nsError = error as NSError
+          let isPingServiceUnimplemented = nsError.domain == GoErrorDomain && nsError.description
+            .contains("unknown service")
+          if !isPingServiceUnimplemented {
+            findAttempt = .failure(.unknownError(error))
+            return
+          }
+        }
+
+        // If find through gRPC failed because of PingService being unimplemented, fall back to
+        // webapi.
+        do {
+          let response = try await webapiFind(proxyServer: clusterAddress)
+          if Task.isCancelled { return }
+          findAttempt = .success(.webapi(response))
         } catch {
           if Task.isCancelled { return }
           findAttempt = .failure(.unknownError(error))
@@ -180,6 +214,58 @@ struct FindResponseView: View {
   }
 }
 
+private let previewResponse: PingFindResponse = {
+  let r = PingFindResponse()
+  r.clusterName = "example.com"
+  r.serverVersion = "18.2.4"
+  return r
+}()
+
+// TODO(ravicious): This preview just doesn't work. I assume it's due to the @preconcurrency stuff.
+#Preview("FindResponseView") {
+  FindResponseView(response: previewResponse)
+}
+
+struct WebapiFindResponseView: View {
+  var response: WebapiFindResponse
+
+  var body: some View {
+    ScrollView {
+      VStack(alignment: .leading, spacing: 10) {
+        Text("Cluster name: \(response.clusterName)")
+        Text("Server version: \(response.serverVersion)")
+        Text("Min client version: \(response.minClientVersion)")
+        Text(
+          "TLS routing: \(response.proxy.tlsRoutingEnabled ? "enabled" : "not enabled")"
+        )
+        Text("Public address: \(response.proxy.ssh.publicAddr)")
+        Text("Web listen address: \(response.proxy.ssh.webListenAddr)")
+        Text("Listen address: \(response.proxy.ssh.listenAddr)")
+        Text("Tunnel listen address: \(response.proxy.ssh.tunnelListenAddr)")
+        Text("Edition: \(response.edition)")
+        Text("FIPS mode: \(response.fips ? "enabled" : "not enabled")")
+      }
+    }.padding(hPadding)
+  }
+}
+
+#Preview("WebapiFindResponseView") {
+  @Previewable let response = WebapiFindResponse(
+    proxy: ProxySettings(ssh: SSHProxySettings(
+      listenAddr: "0.0.0.0:3080",
+      tunnelListenAddr: "0.0.0.0:3080",
+      webListenAddr: "0.0.0.0:3080",
+      publicAddr: "teleport.example.com:443"
+    ), tlsRoutingEnabled: true),
+    serverVersion: "18.2.4",
+    minClientVersion: "17.0.0-aa",
+    clusterName: "teleport.example.com",
+    edition: "community",
+    fips: false
+  )
+  WebapiFindResponseView(response: response)
+}
+
 struct OptionalField: View {
   var label: String
   var value: String?
@@ -189,13 +275,6 @@ struct OptionalField: View {
       Text("\(label): \(value!)")
     }
   }
-}
-
-#Preview("FindResponseView") {
-  let response = PingFindResponse()
-  response.clusterName = "example.com"
-  response.serverVersion = "18.2.4"
-  return FindResponseView(response: response)
 }
 
 enum FindError: Error & Equatable, LocalizedError {
@@ -236,6 +315,117 @@ struct ClearButton: ViewModifier {
             .contentShape(Rectangle())
         }
       }
+    }
+  }
+}
+
+// MARK: - WebapiFindResponse structs
+
+struct WebapiFindResponse: Codable, Equatable, Sendable {
+  let proxy: ProxySettings
+  let serverVersion, minClientVersion, clusterName, edition: String
+  let fips: Bool
+
+  enum CodingKeys: String, CodingKey {
+    case proxy
+    case serverVersion = "server_version"
+    case minClientVersion = "min_client_version"
+    case clusterName = "cluster_name"
+    case edition, fips
+  }
+}
+
+struct ProxySettings: Codable, Equatable, Sendable {
+  let ssh: SSHProxySettings
+  let tlsRoutingEnabled: Bool
+
+  enum CodingKeys: String, CodingKey {
+    case ssh
+    case tlsRoutingEnabled = "tls_routing_enabled"
+  }
+}
+
+struct SSHProxySettings: Codable, Equatable, Sendable {
+  let listenAddr, tunnelListenAddr, webListenAddr, publicAddr: String
+
+  enum CodingKeys: String, CodingKey {
+    case listenAddr = "listen_addr"
+    case tunnelListenAddr = "tunnel_listen_addr"
+    case webListenAddr = "web_listen_addr"
+    case publicAddr = "public_addr"
+  }
+}
+
+func webapiFind(proxyServer: String) async throws(WebapiFindError) -> WebapiFindResponse {
+  guard let url = URL(string: "https://\(proxyServer)/webapi/find") else {
+    throw WebapiFindError.incorrectURL
+  }
+  let request = URLRequest(url: url)
+  let data: Data, response: URLResponse
+
+  do {
+    (data, response) = try await URLSession.shared.data(for: request)
+  } catch {
+    throw .clientError(error)
+  }
+
+  guard let httpResponse = response as? HTTPURLResponse,
+        (200 ... 299).contains(httpResponse.statusCode)
+  else {
+    throw .serverError(response)
+  }
+
+  do {
+    return try JSONDecoder().decode(WebapiFindResponse.self, from: data)
+  } catch {
+    throw .decodingError(error)
+  }
+}
+
+enum WebapiFindError: Error & Equatable, LocalizedError {
+  case incorrectURL
+  case clientError(Error)
+  case serverError(URLResponse)
+  case decodingError(Error)
+
+  static func == (lhs: WebapiFindError, rhs: WebapiFindError) -> Bool {
+    if case .incorrectURL = lhs,
+       case .incorrectURL = rhs
+    {
+      return true
+    }
+
+    if case let .clientError(lhsError) = lhs,
+       case let .clientError(rhsError) = rhs
+    {
+      return (lhsError as NSError) === (rhsError as NSError)
+    }
+
+    if case let .serverError(lhsResp) = lhs,
+       case let .serverError(rhsResp) = rhs
+    {
+      return lhsResp == rhsResp
+    }
+
+    if case let .decodingError(lhsError) = lhs,
+       case let .decodingError(rhsError) = rhs
+    {
+      return (lhsError as NSError) === (rhsError as NSError)
+    }
+
+    return false
+  }
+
+  var errorDescription: String? {
+    switch self {
+    case .incorrectURL:
+      "The provided proxy URL is incorrect."
+    case let .clientError(error):
+      (error as NSError).localizedDescription
+    case let .serverError(response):
+      "The proxy returned an error response: \(response)"
+    case let .decodingError(error):
+      (error as NSError).localizedDescription
     }
   }
 }
