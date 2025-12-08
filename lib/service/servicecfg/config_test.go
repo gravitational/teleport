6b60b3dba9f1ec932eb95814fdad6fa435013afd
Lukasz Okraszewski <lukasz.okraszewski@goteleport.com>
2025-12-08 17:37:45 +0000

    fix default config test

--- lib/service/servicecfg/config_test.go
+++ lib/service/servicecfg/config_test.go
@@ -109,8 +109,8 @@ func TestDefaultConfig(t *testing.T) {
 
 	// Reconnect section
 	require.Equal(t, defaults.MaxWatcherBackoff, config.AuthConnectionConfig.UpperLimitBetweenRetries)
-	require.Equal(t, 9*time.Second, config.AuthConnectionConfig.BackoffStepDuration)
-	require.Equal(t, 18*time.Second, config.AuthConnectionConfig.InitialConnectionDelay)
+	require.Equal(t, 18*time.Second, config.AuthConnectionConfig.BackoffStepDuration)
+	require.Equal(t, 9*time.Second, config.AuthConnectionConfig.InitialConnectionDelay)
 
 }
 
