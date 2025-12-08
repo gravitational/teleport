19c643ef0e8eb705d2b1d02363919aa9f31f0b51
Nick Marais <nicholas.marais@goteleport.com>
2025-12-08 14:59:47 +0000

    Finish step

---
+++ web/packages/teleport/src/Bots/Add/GitHubActionsK8s/Finish.tsx
@@ -0,0 +1,102 @@
+/**
+ * Teleport
+ * Copyright (C) 2025 Gravitational, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { useHistory } from 'react-router';
+import { styled } from 'styled-components';
+
+import Box from 'design/Box/Box';
+import { ButtonPrimary, ButtonSecondary } from 'design/Button/Button';
+import Flex from 'design/Flex/Flex';
+import Link from 'design/Link/Link';
+import Text, { H2 } from 'design/Text/Text';
+
+import cfg from 'teleport/config';
+
+import { FlowStepProps } from '../Shared/GuidedFlow';
+import { CodePanelPlaceholder } from './CodePanel';
+
+export function Finish(props: FlowStepProps) {
+  const { prevStep } = props;
+
+  const history = useHistory();
+
+  const handleFinish = () => {
+    history.replace(cfg.getBotsRoute());
+  };
+
+  return (
+    <Container>
+      <Box flex={1}>
+        <H2 mb={3} mt={3}>
+          Setup Workflow
+        </H2>
+
+        <Text as="p" mt={3}>
+          <strong>To complete the setup</strong>;
+        </Text>
+        <ul>
+          <li>Use the Infrastructure as Code templates to create resources</li>
+          <li>Copy the workflow template and add it to your repository</li>
+        </ul>
+
+        <Text as="p">
+          See the{' '}
+          <Link
+            target="_blank"
+            href="https://goteleport.com/docs/zero-trust-access/infrastructure-as-code/"
+          >
+            Infrastructure as Code
+          </Link>{' '}
+          docs for information about setting up and using IaC with Teleport.
+        </Text>
+
+        <Text as="p" mt={3}>
+          See the{' '}
+          <Link
+            target="_blank"
+            href="https://goteleport.com/docs/machine-workload-identity/deployment/github-actions/"
+          >
+            Deploying tbot on GitHub Actions
+          </Link>{' '}
+          docs for information about running tbot in a GitHub Actions workflow.
+        </Text>
+
+        <Flex gap={2} pt={5}>
+          <ButtonPrimary onClick={handleFinish}>Finish</ButtonPrimary>
+          <ButtonSecondary onClick={prevStep}>Back</ButtonSecondary>
+        </Flex>
+      </Box>
+
+      <CodeContainer>
+        <CodePanelPlaceholder>Code panel coming soon...</CodePanelPlaceholder>
+      </CodeContainer>
+    </Container>
+  );
+}
+
+const Container = styled(Flex)`
+  flex: 1;
+  overflow: auto;
+  gap: ${({ theme }) => theme.space[3]}px;
+`;
+
+const CodeContainer = styled(Flex)`
+  flex: 1;
+  flex-direction: column;
+  overflow: auto;
+`;
