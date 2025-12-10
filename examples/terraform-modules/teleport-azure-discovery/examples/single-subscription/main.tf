082b786fae6dad92b416615370b5b7b93483b1a6
Michael Myers <michael.myers@goteleport.com>
2025-12-10 17:27:57 -0600

    Add terraform module for azure auto discovery
    This will add a terraform module that will support the automatic
    discovery of VMs in azure, using an OIDC integration.
    
    Closes #60818
    
    Changelog: Added a terraform module to support automatic discovery of
    azure resources.

---
+++ examples/terraform-modules/teleport-azure-discovery/examples/single-subscription/main.tf
@@ -0,0 +1,16 @@
+module "teleport_azure_discovery" {
+  source = "../.."
+
+  subscription_id                = var.subscription_id
+  tenant_id                      = var.tenant_id
+  region                         = var.region
+  discovery_resource_group_names = var.discovery_resource_group_names
+  proxy_addr                     = var.teleport_proxy_public_addr
+
+  # optional
+  discovery_tags               = var.discovery_tags
+  identity_resource_group_name = var.identity_resource_group_name
+  tags                         = var.tags
+  discovery_group_name         = var.discovery_group_name
+  installer_script_name        = var.installer_script_name
+}
