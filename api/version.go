1da1fa48c38b31a21b6bb8caf9b009c85bb2fdaa
Ryan Hammonds <ryan.hammonds@goteleport.com>
2025-12-11 14:51:28 -0500

    Release v17.7.8-dev.rhammonds.removewasmpackv17

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "17.7.8"
+const Version = "17.7.8-dev.rhammonds.removewasmpackv17"
 
 const VersionMajor = 17
 const VersionMinor = 7
 const VersionPatch = 8
-const VersionPreRelease = ""
+const VersionPreRelease = "dev.rhammonds.removewasmpackv17"
 const VersionMetadata = ""
