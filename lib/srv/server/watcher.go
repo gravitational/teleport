1e1715a6bb02313e6e3c13f691ece8b90cfd1b6a
Krzysztof Skrzętnicki <krzysztof.skrzetnicki@goteleport.com>
2025-12-12 20:28:08 +0100

    fix license corruption

--- lib/srv/server/watcher.go
+++ lib/srv/server/watcher.go
@@ -5,7 +5,7 @@
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * (at your Option[Instances]) any later version.
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
