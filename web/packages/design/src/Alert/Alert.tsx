b6a9a1088ba5cdf115956bd71bd9e43a8aca75c7
Charles Bryan <charles.bryan@goteleport.com>
2025-12-17 16:33:07 -0800

    62151 CTA to use new IAC discover flow on empty resources list page Discovery call-to-action alert on the new resource page

--- web/packages/design/src/Alert/Alert.tsx
+++ web/packages/design/src/Alert/Alert.tsx
@@ -47,7 +47,8 @@ export type AlertKind =
   | 'success'
   | 'outline-danger'
   | 'outline-info'
-  | 'outline-warn';
+  | 'outline-warn'
+  | 'cta';
 
 const alertBorder = (
   props: ThemedAlertProps
@@ -77,6 +78,11 @@ const alertBorder = (
       return {
         border: theme.borders[1],
         borderColor: theme.colors.text.disabled,
+      };
+    case 'cta':
+      return {
+        border: theme.borders[2],
+        borderColor: theme.colors.interactive.solid.primary.default,
       };
   }
 };
@@ -108,6 +114,10 @@ const backgroundColor = (
     case 'neutral':
       return {
         background: theme.colors.interactive.tonal.neutral[0],
+      };
+    case 'cta':
+      return {
+        background: 'inherit',
       };
   }
 };
@@ -327,6 +337,12 @@ const iconContainerStyles = ({
         background: theme.colors.interactive.tonal.neutral[0],
         padding: `${theme.space[2]}px`,
       };
+    case 'cta':
+      return {
+        color: theme.colors.text.primaryInverse,
+        background: theme.colors.interactive.solid.primary.default,
+        padding: `${theme.space[2]}px`,
+      };
   }
 };
 
@@ -353,7 +369,7 @@ const IconContainer = styled.div<{ kind: AlertKind; wrapContents?: boolean }>`
 const primaryButtonProps = (
   kind: AlertKind | BannerKind
 ): { fill: ButtonFill; intent: ButtonIntent } => {
-  return kind === 'neutral'
+  return kind === 'neutral' || kind === 'cta'
     ? { fill: 'filled', intent: 'primary' }
     : { fill: 'border', intent: 'neutral' };
 };
@@ -585,6 +601,7 @@ const iconKind = (kind: AlertKind | BannerKind): StatusKind => {
     case 'outline-info':
       return 'info';
     case 'primary':
+    case 'cta':
       return 'neutral';
     default:
       return kind;
