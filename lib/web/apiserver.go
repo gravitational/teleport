1af393cde4a498246cbc267f330826ef39c36eda
Dan Share <daniel.share@goteleport.com>
2025-12-22 14:39:45 +0000

    Added AllowBrowserMFA flag to backend

--- lib/web/apiserver.go
+++ lib/web/apiserver.go
@@ -1403,8 +1403,7 @@ func localSettings(ctx context.Context, cap types.AuthPreference, logger *slog.L
 		PIVPINCacheTTL:          cap.GetPIVPINCacheTTL(),
 		DeviceTrust:             deviceTrustSettings(cap),
 		SignatureAlgorithmSuite: cap.GetSignatureAlgorithmSuite(),
-		// TODO: Have this settable
-		AllowBrowserMFA: true,
+		AllowBrowserMFA:         cap.GetAllowBrowserMFA(),
 	}
 
 	// Only copy the connector name if it's truly local and not a local fallback.
