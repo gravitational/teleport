ff63614b5b8b30fa55d00c8a6e2101917838c2cb
Zac Bergquist <zac.bergquist@goteleport.com>
2025-12-26 16:24:21 -0700

    Release 19.0.0-dev.danielashare.3

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-dev.danielashare.2"
+const Version = "19.0.0-dev.danielashare.3"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev.danielashare.2"
+const VersionPreRelease = "dev.danielashare.3"
 const VersionMetadata = ""
