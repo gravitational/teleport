be055c9d25e719cf37b2b8b71e19863c912470df
hugoShaka <hugo.hervieux@goteleport.com>
2026-01-05 17:00:25 -0500

    fix imports

--- integrations/operator/controllers/resources/testlib/env.go
+++ integrations/operator/controllers/resources/testlib/env.go
@@ -34,7 +34,6 @@ import (
 	"github.com/stretchr/testify/require"
 	core "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/rest"
 	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
