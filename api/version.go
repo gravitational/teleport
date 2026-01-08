dfe1ece45fb0b4ffebcaaad778d8cb3b7ca8c436
Rafał Cieślak <rafal.cieslak@goteleport.com>
2026-01-08 17:58:07 +0100

    Update version to 18.6.1-dev.pnpm-10-27-0.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.6.1"
+const Version = "18.6.1-dev.pnpm-10-27-0.1"
 
 const VersionMajor = 18
 const VersionMinor = 6
 const VersionPatch = 1
-const VersionPreRelease = ""
+const VersionPreRelease = "dev.pnpm-10-27-0.1"
 const VersionMetadata = ""
