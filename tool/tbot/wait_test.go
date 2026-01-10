df1d527bb81735ba359fe75f71c724829746cfe8
Tim Buckley <tim@goteleport.com>
2026-01-09 19:43:35 -0700

    [v18] MWI: Add `/wait` endpoint and `tbot wait` CLI helper (#62719)
    * MWI: Add `/wait` endpoint and `tbot wait` CLI helper
    
    This adds two new features meant to improve use of `tbot` as a
    background service:
    - A new `/wait(/service)` endpoint that functions similarly to
      `/readyz/`, but waits to respond until specified service (or all
      services) are ready
    - A new CLI helper, `tbot wait`, that requests this endpoint with a
      few protections to make it safe and useful in bash scripts.
    
    Note that as with `/readyz` the diagnostics service needs to be
    enabled via `--diag-addr` or similar for this functionality to be
    useful.
    
    The HTTP API and the CLI helper have slightly different semantics for
    what "ready" means, as we want to minimize parsing and error handling
    for shell users:
    - The HTTP API waits for the first readiness report from the service,
      after which it returns the latest status without delay. This means
      it may return when the service is unhealthy.
    - The CLI helper waits for the service to actually report healthy.
      This means it may continue checking even once the wait endpoint
      returns a value.
    
    ---
    
    ```code
    $ tbot start identity ... --diag-addr 127.0.0.1:4444
    
    $ tbot wait --diag-addr 127.0.0.1:4444
    
    $ tbot wait --diag-addr 127.0.0.1:4444 --service identity-1
    
    $ tbot wait --diag-addr 127.0.0.1:4444 --service identity-1 --timeout 30s
    ```
    
    * Parse and log detailed status info when available
    
    * Fix imports, remove unused error
    
    * Add tests and improve user-facing output from `tbot wait` helper
    
    * Fix imports
    
    * Remove unused test helpers
    
    * Switch to testing/synctest
    
    This switches the tests depending on `time.Sleep()` to use
    synctest along with an in-memory roundtripper.
    
    * Fix error wording
    
    * Fix lints
    
    * Use synctest wrapper

---
+++ tool/tbot/wait_test.go
@@ -0,0 +1,179 @@
+/*
+ * Teleport
+ * Copyright (C) 2025  Gravitational, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/gravitational/teleport/lib/tbot/cli"
+	"github.com/gravitational/teleport/lib/tbot/readyz"
+	"github.com/gravitational/teleport/lib/utils/testutils/synctest"
+)
+
+type inMemoryTransport struct {
+	handler http.Handler
+}
+
+func (c *inMemoryTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	done := make(chan struct{})
+	rr := httptest.NewRecorder()
+
+	// We can't just naively return the result of ServeHTTP since it doesn't
+	// return errors when the context is canceled. To fix that, wrap it in
+	// another goroutine and manually return an error if the context is
+	// canceled.
+	go func() {
+		c.handler.ServeHTTP(rr, r)
+		close(done)
+	}()
+
+	select {
+	case <-r.Context().Done():
+		<-done
+		return nil, r.Context().Err()
+	case <-done:
+		return rr.Result(), nil
+	}
+}
+
+func createInMemoryWaitClient(handler http.Handler) *http.Client {
+	return &http.Client{
+		Transport: &inMemoryTransport{
+			handler: handler,
+		},
+	}
+}
+
+func TestWaitTimeoutExceeded(t *testing.T) {
+	t.Parallel()
+
+	synctest.Test(t, func(t *testing.T) {
+		reg := readyz.NewRegistry()
+		_ = reg.AddService("svc", "a")
+
+		handler := readyz.HTTPWaitHandler(reg)
+
+		ch := make(chan error)
+		go func() {
+			ch <- onWaitCommand(t.Context(), &cli.WaitCommand{
+				DiagAddr: "http://fake",
+				Service:  "a",
+				Timeout:  time.Millisecond * 250,
+				Client:   createInMemoryWaitClient(handler),
+			})
+		}()
+
+		synctest.Wait()
+
+		time.Sleep(time.Millisecond * 500)
+
+		synctest.Wait()
+
+		select {
+		case res := <-ch:
+			require.ErrorContains(t, res, "context deadline exceeded")
+		case <-time.After(250 * time.Millisecond):
+			require.Fail(t, "wait failed to honor timeout")
+		}
+	})
+}
+
+func TestWaitSuccess(t *testing.T) {
+	t.Parallel()
+
+	synctest.Test(t, func(t *testing.T) {
+		reg := readyz.NewRegistry()
+		a := reg.AddService("svc", "a")
+
+		handler := readyz.HTTPWaitHandler(reg)
+
+		ch := make(chan error)
+		go func() {
+			ch <- onWaitCommand(t.Context(), &cli.WaitCommand{
+				DiagAddr: "http://fake",
+				Service:  "a",
+				Timeout:  time.Second * 2,
+				Client:   createInMemoryWaitClient(handler),
+			})
+		}()
+
+		synctest.Wait()
+
+		a.Report(readyz.Healthy)
+
+		synctest.Wait()
+
+		select {
+		case res := <-ch:
+			require.NoError(t, res, "must report ready")
+		case <-time.After(3 * time.Second):
+			require.Fail(t, "test timed out and failed to honor configured timeout")
+		}
+	})
+}
+
+func TestWaitEventualSuccess(t *testing.T) {
+	t.Parallel()
+
+	synctest.Test(t, func(t *testing.T) {
+		reg := readyz.NewRegistry()
+		a := reg.AddService("svc", "a")
+
+		handler := readyz.HTTPWaitHandler(reg)
+
+		ch := make(chan error)
+		go func() {
+			ch <- onWaitCommand(t.Context(), &cli.WaitCommand{
+				DiagAddr: "http://fake",
+				Service:  "a",
+				Client:   createInMemoryWaitClient(handler),
+
+				// More generous timeout since this will depend on exponential
+				// backoff (with configured worst case of 2 seconds)
+				Timeout: time.Second * 5,
+			})
+		}()
+
+		synctest.Wait()
+
+		// Initially report unhealthy. This internally triggers a response and the
+		// HTTP endpoint will return the unhealthy status instead of waiting. The
+		// CLI should retry until the endpoint reports healthy.
+		a.ReportReason(readyz.Unhealthy, "oops")
+		time.Sleep(time.Millisecond * 200)
+
+		synctest.Wait()
+
+		a.Report(readyz.Healthy)
+
+		synctest.Wait()
+
+		select {
+		case res := <-ch:
+			require.NoError(t, res, "must report ready")
+		case <-time.After(6 * time.Second):
+			require.Fail(t, "test timed out and failed to honor configured timeout")
+		}
+	})
+}
