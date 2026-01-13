cd4299ae8777aa92d9a9477b7ab64e97ee654a37
Gabriel Corado <gabriel.oliveira@goteleport.com>
2026-01-12 21:52:07 -0300

    test(appauthconfigv1): update test

--- lib/auth/appauthconfig/appauthconfigv1/sessions_service_test.go
+++ lib/auth/appauthconfig/appauthconfigv1/sessions_service_test.go
@@ -426,7 +426,7 @@ func TestCreateAppSessionWithJWT(t *testing.T) {
 				}, nil
 			},
 			assertError:      require.Error,
-			assertAuditEvent: assertAuditEventFailure,
+			assertAuditEvent: require.Nil,
 		},
 		"invalid token": {
 			config: config,
