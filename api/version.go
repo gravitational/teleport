3993c4eee4e906888d374dd377d3f0323b56ccc4
Zac Bergquist <zac.bergquist@goteleport.com>
2026-01-17 13:25:29 -0700

    Release 19.0.0-dev.zac.2

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-dev.zac.1"
+const Version = "19.0.0-dev.zac.2"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev.zac.1"
+const VersionPreRelease = "dev.zac.2"
 const VersionMetadata = ""
