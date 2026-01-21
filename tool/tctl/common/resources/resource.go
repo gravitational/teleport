ea938622428db7253ad164b2eaf12e785280e590
Hugo Shaka <hugo.hervieux@goteleport.com>
2026-01-20 19:09:06 -0500

    convert audit-query (#62972)

--- tool/tctl/common/resources/resource.go
+++ tool/tctl/common/resources/resource.go
@@ -40,6 +40,7 @@ func Handlers() map[string]Handler {
 		types.KindAccessRequest:                      accessRequestHandler(),
 		types.KindApp:                                appHandler(),
 		types.KindAppServer:                          appServerHandler(),
+		types.KindAuditQuery:                         auditQueryHandler(),
 		types.KindAuthServer:                         authHandler(),
 		types.KindAutoUpdateAgentReport:              autoUpdateAgentReportHandler(),
 		types.KindAutoUpdateAgentRollout:             autoUpdateAgentRolloutHandler(),
