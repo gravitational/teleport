9c74daa387b2b08381ff160e790b2e7cdec1a8bf
Dustin Specker <dustin.specker@goteleport.com>
2026-01-22 11:47:23 -0600

    Release 18.6.4-dev.dustinspecker.workloadclusters.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.6.4"
+const Version = "18.6.4-dev.dustinspecker.workloadclusters.1"
 
 const VersionMajor = 18
 const VersionMinor = 6
 const VersionPatch = 4
-const VersionPreRelease = ""
+const VersionPreRelease = "dev.dustinspecker.workloadclusters.1"
 const VersionMetadata = ""
