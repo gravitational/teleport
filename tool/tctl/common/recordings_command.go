5424161879da0dba98c2caf5a4d629dbcf837d11
Paul Gottschling <paul.gottschling@goteleport.com>
2026-01-26 11:57:30 -0500

    Add flag default overrides for tctl reference
    Configure flags with dynamic values to have hardcoded values in the
    reference docs page so there is no need to edit the dynamic argument
    logic in the tctl source.

--- tool/tctl/common/recordings_command.go
+++ tool/tctl/common/recordings_command.go
@@ -92,7 +92,10 @@ func (c *RecordingsCommand) Initialize(app *kingpin.Application, t *tctlcfg.Glob
 
 	download := recordings.Command("download", "Download session recordings.")
 	download.Arg("session-id", "ID of the session to download recordings for.").Required().StringVar(&c.recordingsDownloadSessionID)
-	pwd := "."
+	pwd, err := os.Getwd()
+	if err != nil {
+		pwd = "."
+	}
 	download.Flag("output-dir", "Directory to download session recordings to.").Short('o').Default(pwd).StringVar(&c.recordingsDownloadOutputDir)
 	c.recordingsDownload = download
 
