26e984a460a128697ee00589f81c2a15bd7de98e
Michael Myers <michael.myers@goteleport.com>
2026-01-28 18:53:34 -0600

    Release v19.0.0-dev.avatus.8

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-dev.avatus.7"
+const Version = "19.0.0-dev.avatus.8"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev.avatus.7"
+const VersionPreRelease = "dev.avatus.8"
 const VersionMetadata = ""
