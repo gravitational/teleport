8a04d993f1dea9d0a182ed80db9d248fdfbe6fc7
Rafał Cieślak <rafal.cieslak@goteleport.com>
2026-02-02 12:13:39 +0100

    Move nil check to `MFAChallengeResponse`

--- lib/web/headless.go
+++ lib/web/headless.go
@@ -24,7 +24,6 @@ import (
 	"github.com/gravitational/trace"
 	"github.com/julienschmidt/httprouter"
 
-	"github.com/gravitational/teleport/api/client/proto"
 	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/lib/client"
 	"github.com/gravitational/teleport/lib/httplib"
@@ -73,13 +72,9 @@ func (h *Handler) putHeadlessState(_ http.ResponseWriter, r *http.Request, param
 	}
 
 	// MFAResponse is required only when accepting a request.
-	var mfaResp *proto.MFAAuthenticateResponse
-	if req.MFAResponse != nil {
-		var err error
-		mfaResp, err = req.MFAResponse.GetOptionalMFAResponseProtoReq()
-		if err != nil {
-			return nil, trace.Wrap(err)
-		}
+	mfaResp, err := req.MFAResponse.GetOptionalMFAResponseProtoReq()
+	if err != nil {
+		return nil, trace.Wrap(err)
 	}
 
 	var action types.HeadlessAuthenticationState
