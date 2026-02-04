dd6e8df03792bb50e6ee141c1d1de8eb4765aa9b
Nick Marais <nicholas.marais@goteleport.com>
2026-02-04 12:14:53 +0000

    [v18] feat: GHA + K8s guide improvements (#63185)
    * Add IaC info alert to welcome step
    
    * Add conditional info alert to "allow any branch" input
    
    * Rename "done" button (to "finish")
    
    * Add repo restriction help text
    
    * Ensure "refs/heads/" prefix for branch input
    
    * Add code panel to Welcome step
    
    * Add `inProgress` mode to CodePanel
    
    * Shift CodePanel left on finish step
    
    * De-emphasise final button and make prior steps clearer
    
    * Explain how to apply terraform template
    
    * Improve manual label entry
    
    * Update labels field label
    
    * Enforce wildcard label
    
    * Update label values format (OR)
    
    * Explain workflows on "main branch"
    
    * Clarify prerequisites
    
    * Improve groups and users entry
    
    * Copy tweak on Configure Access step
    
    * Add groups and users validation
    
    * Add kubernetes cluster selector
    
    * Fix Terraform template tests
    
    * Move cluster select to Setup Workflow step
    
    * Tweak copy warning wording
    
    * With emphasis not clarity
    
    * Replace "setup" with "set up"
    
    * Make cluster selector async searchable
    
    * Fix lint issue
    
    * Fix lint issue (again)

--- web/packages/teleport/src/Bots/Add/GitHubActionsK8s/GitHubActionsK8s.story.tsx
+++ web/packages/teleport/src/Bots/Add/GitHubActionsK8s/GitHubActionsK8s.story.tsx
@@ -1,6 +1,6 @@
 /**
  * Teleport
- * Copyright (C) 2025 Gravitational, Inc.
+ * Copyright (C) 2026  Gravitational, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -18,26 +18,25 @@
 
 import { Meta, StoryObj } from '@storybook/react-vite';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MemoryRouter } from 'react-router';
 import styled from 'styled-components';
 
 import Flex from 'design/Flex/Flex';
+import { InfoGuidePanelProvider } from 'shared/components/SlidingSidePanel/InfoGuide';
 
+import { BotFlowType } from 'teleport/Bots/types';
+import cfg from 'teleport/config';
 import { ContextProvider } from 'teleport/index';
+import { ContentMinWidth } from 'teleport/Main/Main';
 import { createTeleportContext } from 'teleport/mocks/contexts';
-import {
-  genWizardCiCdError,
-  genWizardCiCdForever,
-  genWizardCiCdSuccess,
-} from 'teleport/test/helpers/bots';
+import { genWizardCiCdSuccess } from 'teleport/test/helpers/bots';
 import { fetchUnifiedResourcesSuccess } from 'teleport/test/helpers/resources';
 import { userEventCaptureSuccess } from 'teleport/test/helpers/userEvents';
 
-import { TrackingProvider } from '../Shared/useTracking';
-import { ConnectGitHub } from './ConnectGitHub';
-import { GitHubK8sFlowProvider } from './useGitHubK8sFlow';
+import { GitHubActionsK8s } from './GitHubActionsK8s';
 
 const meta = {
-  title: 'Teleport/Bots/Add/GitHubActions+K8s/ConnectGitHub',
+  title: 'Teleport/Bots/Add/GitHubActions+K8s/GitHubActionsK8s',
   component: Wrapper,
   beforeEach: () => {
     queryClient.clear(); // Prevent cached data sharing between stories
@@ -53,31 +52,10 @@ export const Happy: Story = {
     msw: {
       handlers: [
         genWizardCiCdSuccess({ prettyFormat: true }),
-        fetchUnifiedResourcesSuccess(),
-        userEventCaptureSuccess(),
-      ],
-    },
-  },
-};
-
-export const TemplateFetchFailed: Story = {
-  parameters: {
-    msw: {
-      handlers: [
-        genWizardCiCdError(500, 'something went wrong'),
-        fetchUnifiedResourcesSuccess(),
-        userEventCaptureSuccess(),
-      ],
-    },
-  },
-};
-
-export const TemplateFetching: Story = {
-  parameters: {
-    msw: {
-      handlers: [
-        genWizardCiCdForever(),
-        fetchUnifiedResourcesSuccess(),
+        fetchUnifiedResourcesSuccess({
+          delayMs: 1000,
+          mockSearch: true,
+        }),
         userEventCaptureSuccess(),
       ],
     },
@@ -95,22 +73,30 @@ const queryClient = new QueryClient({
 
 function Wrapper() {
   const ctx = createTeleportContext();
+
   return (
     <QueryClientProvider client={queryClient}>
       <ContextProvider ctx={ctx}>
-        <TrackingProvider>
-          <GitHubK8sFlowProvider>
-            <Container>
-              <ConnectGitHub />
-            </Container>
-          </GitHubK8sFlowProvider>
-        </TrackingProvider>
+        <InfoGuidePanelProvider>
+          <ContentMinWidth>
+            <MemoryRouter
+              initialEntries={[
+                cfg.getBotsNewRoute(BotFlowType.GitHubActionsK8s),
+              ]}
+            >
+              <Container>
+                <GitHubActionsK8s />
+              </Container>
+            </MemoryRouter>
+          </ContentMinWidth>
+        </InfoGuidePanelProvider>
       </ContextProvider>
     </QueryClientProvider>
   );
 }
 
 const Container = styled(Flex)`
+  flex-direction: column;
   height: 820px;
   overflow: auto;
 `;
