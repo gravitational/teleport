c5547ee2a057f4bf424a0e2ec4404dd4bc737ae9
Carlisia Campos <carlisia@goteleport.com>
2026-02-05 09:52:05 -0800

    Add back instructions, expand naming

--- lib/usertasks/descriptions/ec2-ssm-script-failure.md
+++ lib/usertasks/descriptions/ec2-ssm-script-failure.md
@@ -8,3 +8,5 @@ Common causes:
 - The Teleport binary could not be downloaded
 - The SSM Agent version is below 3.1
 
+Below you'll find two links: the Resource Name links to the EC2 instance details, and the Invocation URL provides details on the cause of the failure.
+
--- web/packages/teleport/src/Discover/Overview/UserTaskDrawer.tsx
+++ web/packages/teleport/src/Discover/Overview/UserTaskDrawer.tsx
@@ -338,7 +338,7 @@ function makeImpactsTable(impacted: { rows: ImpactRow[] }): {
   const columns: TableColumn<ImpactRow>[] = [
     {
       key: 'id',
-      headerText: 'Resource',
+      headerText: 'Resource Name',
       render: row => {
         if (row.resourceUrl) {
           return (
@@ -376,7 +376,7 @@ function makeImpactsTable(impacted: { rows: ImpactRow[] }): {
   if (hasInvocation) {
     columns.push({
       altKey: 'invocation',
-      headerText: 'Invocation',
+      headerText: 'Invocation URL',
       render: row =>
         row.invocationUrl ? (
           <Cell align="center">
