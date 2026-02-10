518d3f0a31bebab68eca4b9295838d0e747101ef
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2026-02-10 08:58:06 +0100

    19.0.0-dev.updater.6

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-dev.updater.5"
+const Version = "19.0.0-dev.updater.6"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev.updater.5"
+const VersionPreRelease = "dev.updater.6"
 const VersionMetadata = ""
