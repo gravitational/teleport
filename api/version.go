5e28a2507ab78e6a5ee17e2e1b99e3c9225d6b02
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2026-02-10 19:41:52 +0100

    19.0.0-dev.updater.8

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-dev.updater.7"
+const Version = "19.0.0-dev.updater.8"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev.updater.7"
+const VersionPreRelease = "dev.updater.8"
 const VersionMetadata = ""
