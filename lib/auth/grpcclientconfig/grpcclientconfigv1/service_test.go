09b4064b7f7f30c0021bac46cdcb8d2caf95dd0d
David Boslee <david@goteleport.com>
2026-02-13 11:08:51 -0500

    Update lib/auth/grpcclientconfig/grpcclientconfigv1/service_test.go
    Co-authored-by: rosstimothy <39066650+rosstimothy@users.noreply.github.com>

--- lib/auth/grpcclientconfig/grpcclientconfigv1/service_test.go
+++ lib/auth/grpcclientconfig/grpcclientconfigv1/service_test.go
@@ -66,7 +66,7 @@ func TestGetServiceConfigEnvVar(t *testing.T) {
 			service, err := NewService()
 			require.NoError(t, err)
 
-			resp, err := service.GetServiceConfig(context.Background(), &grpcv1.GetServiceConfigRequest{})
+			resp, err := service.GetServiceConfig(t.Context(), &grpcv1.GetServiceConfigRequest{})
 			require.NoError(t, err)
 			require.EqualExportedValues(t, tt.expected, resp.GetConfig())
 		})
