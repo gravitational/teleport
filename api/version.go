1866f2a5b0d804e4047f277e01fda9ff569c5f3b
Matt Smith <matt.smith@goteleport.com>
2026-02-18 14:10:06 -0500

    version

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.7.0"
+const Version = "18.7.0-dev.matt.1"
 
 const VersionMajor = 18
 const VersionMinor = 7
 const VersionPatch = 0
-const VersionPreRelease = ""
+const VersionPreRelease = "dev.matt.1"
 const VersionMetadata = ""
