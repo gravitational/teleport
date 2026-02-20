d7c45546dabe11b50794e83bd62d072100d1552e
Ryan Clark <ryan.clark@goteleport.com>
2026-02-20 14:22:41 +0100

    Add item to KeysEnum for identity security unified resources CTA

--- web/packages/teleport/src/services/storageService/types.ts
+++ web/packages/teleport/src/services/storageService/types.ts
@@ -65,6 +65,9 @@ export const KeysEnum = {
   USE_NEW_ROLE_EDITOR: 'grv_teleport_use_new_role_editor',
   //TODO(rudream): Remove once sidenav implementation is complete.
   USE_TOP_BAR: 'grv_teleport_use_topbar',
+
+  IDENTITY_SECURITY_RECOMMENDATIONS_UNIFIED_RESOURCES_CTA:
+    'grv_teleport_identity_security_recommendations_unified_resources_cta',
 };
 
 // SurveyRequest is the request for sending data to the back end
