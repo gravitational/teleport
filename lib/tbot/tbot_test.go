d36f35fee6c519fa334b8bb4322bfca2714cb0d6
Chris Thach <chris.thach@goteleport.com>
2026-02-23 16:44:45 -0500

    [v18] Lint to enforce using t.Context() instead of context.TODO() (#64052)
    Signed-off-by: Chris Thach <chris.thach@goteleport.com>

--- lib/tbot/tbot_test.go
+++ lib/tbot/tbot_test.go
@@ -117,7 +117,7 @@ func defaultTestServerOpts(log *slog.Logger) testenv.TestServerOptFunc {
 
 // makeBot creates a server-side bot and returns joining parameters.
 func makeBot(t *testing.T, client *authclient.Client, name string, roles ...string) (*onboarding.Config, *machineidv1pb.Bot) {
-	ctx := context.TODO()
+	ctx := t.Context()
 	t.Helper()
 
 	b, err := client.BotServiceClient().CreateBot(ctx, &machineidv1pb.CreateBotRequest{
