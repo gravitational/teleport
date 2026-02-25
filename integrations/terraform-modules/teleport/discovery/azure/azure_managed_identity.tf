1a67eb72040ec746b6d40c21b2e541a9b91dc3ba
Gavin Frazar <gavin.frazar@goteleport.com>
2026-02-25 11:10:23 -0800

    Fix azurerm_federated_identity_credential
    Remove unsupported argument "resource_group_name".

--- integrations/terraform-modules/teleport/discovery/azure/azure_managed_identity.tf
+++ integrations/terraform-modules/teleport/discovery/azure/azure_managed_identity.tf
@@ -18,9 +18,8 @@ resource "azurerm_federated_identity_credential" "teleport_discovery_service" {
 
   audience = ["api://AzureADTokenExchange"]
   # Extract the host from proxy_addr (format: host:port) to construct the OIDC issuer URL
-  issuer              = replace(local.teleport_proxy_public_url, "/:[0-9]+.*/", "")
-  name                = var.azure_federated_identity_credential_name
-  parent_id           = one(azurerm_user_assigned_identity.teleport_discovery_service[*].id)
-  resource_group_name = var.azure_resource_group_name
-  subject             = "teleport-azure"
+  issuer    = replace(local.teleport_proxy_public_url, "/:[0-9]+.*/", "")
+  name      = var.azure_federated_identity_credential_name
+  parent_id = one(azurerm_user_assigned_identity.teleport_discovery_service[*].id)
+  subject   = "teleport-azure"
 }
