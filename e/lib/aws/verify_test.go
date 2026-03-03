ad5636f6569082f3f89eb9117dd98fb07dc7d31a
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2026-03-03 11:14:23 -0500

    Replace use of gopkg.in/check.v1 with testing (#8146)
    These were the last remaining tests leveraging check. Once they are
    removed and OSS is updated it will allow us to drop the dependency.
    
    Source-commit: gravitational/teleport.e@510117786d10afab0ee2f4caab3dc4c2fac6a711
    Source-pull-requests: gravitational/teleport.e#8146

--- e/lib/aws/verify_test.go
+++ e/lib/aws/verify_test.go
@@ -1,41 +1,33 @@
 package aws
 
 import (
-	"slices"
 	"strings"
 	"testing"
 
-	check "gopkg.in/check.v1"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/gravitational/teleport/e/lib/fixtures"
 )
 
-func TestAPI(t *testing.T) { check.TestingT(t) }
-
-type AWSSuite struct {
-}
-
-var _ = check.Suite(&AWSSuite{})
-
-func (s *AWSSuite) TestVerifyOK(c *check.C) {
+func TestVerifyOK(t *testing.T) {
 	meta, err := GetInstanceMetadata(func(uri string) ([]byte, error) {
 		if strings.HasSuffix(uri, "document") {
 			return []byte(fixtures.AWSDoc), nil
 		}
 		return []byte(fixtures.AWSSig), nil
 	})
-	c.Assert(err, check.IsNil)
-	c.Assert(meta.AccountID, check.Equals, "126027368216")
-	isProductLicensed := slices.Contains(meta.MarketplaceProductCodes, "9x4pv56fe6h1gj8hejc5r6a3z")
-	c.Assert(isProductLicensed, check.Equals, true)
+	require.NoError(t, err)
+	assert.Equal(t, "126027368216", meta.AccountID)
+	assert.Contains(t, meta.MarketplaceProductCodes, "9x4pv56fe6h1gj8hejc5r6a3z")
 }
 
-func (s *AWSSuite) TestVerifyTampered(c *check.C) {
+func TestVerifyTampered(t *testing.T) {
 	_, err := GetInstanceMetadata(func(uri string) ([]byte, error) {
 		if strings.HasSuffix(uri, "document") {
 			return []byte(fixtures.AWSDoc + "tampered"), nil
 		}
 		return []byte(fixtures.AWSSig), nil
 	})
-	c.Assert(err, check.NotNil)
+	require.Error(t, err)
 }
