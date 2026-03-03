c75d5f5e9c9fd5b9a262f4fff61ddb514a1c0ff3
Ryan Clark <ryan.clark@goteleport.com>
2026-03-03 17:16:08 +0100

    Add a runner utility for E2E tests

---
+++ e2e/runner/teleport.go
@@ -0,0 +1,220 @@
+/**
+ * Teleport
+ * Copyright (C) 2026  Gravitational, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"syscall"
+	"text/template"
+	"time"
+)
+
+type teleportInstance struct {
+	config     *e2eConfig
+	configPath string
+	stateFile  string
+	logFile    string // empty means stdout/stderr
+
+	cmd      *exec.Cmd
+	logF     *os.File
+	waitErr  error
+	waitDone chan struct{}
+}
+
+func (t *teleportInstance) start(ctx context.Context) error {
+	t.cmd = exec.CommandContext(ctx, t.config.teleportBin, "start", "-c", t.configPath, "--bootstrap", t.stateFile)
+
+	if t.logFile != "" {
+		f, err := os.Create(t.logFile)
+		if err != nil {
+			return fmt.Errorf("creating log file: %w", err)
+		}
+		t.logF = f
+		t.cmd.Stdout = f
+		t.cmd.Stderr = f
+	} else {
+		t.cmd.Stdout = os.Stdout
+		t.cmd.Stderr = os.Stderr
+	}
+
+	t.cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+
+	slog.Info("starting teleport with bootstrap state")
+	if err := t.cmd.Start(); err != nil {
+		return fmt.Errorf("starting teleport: %w", err)
+	}
+
+	t.waitDone = make(chan struct{})
+	go func() {
+		t.waitErr = t.cmd.Wait()
+		close(t.waitDone)
+	}()
+
+	return nil
+}
+
+// waitReady polls the proxy's /webapi/ping endpoint until it responds with 200.
+func (t *teleportInstance) waitReady(ctx context.Context, timeout time.Duration) error {
+	slog.Debug("waiting for teleport to be ready")
+
+	client := &http.Client{
+		Timeout: 2 * time.Second,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+	defer client.CloseIdleConnections()
+
+	probe := func(_ context.Context) (bool, error) {
+		select {
+		case <-t.waitDone:
+			return false, fmt.Errorf("teleport exited unexpectedly: %w", t.waitErr)
+		default:
+		}
+
+		resp, err := client.Get(fmt.Sprintf("https://localhost:%d/webapi/ping", t.config.proxyPort))
+		if err != nil {
+			return false, nil
+		}
+
+		resp.Body.Close()
+
+		return resp.StatusCode == http.StatusOK, nil
+	}
+
+	if err := pollUntil(ctx, timeout, 1*time.Second, probe); err != nil {
+		return fmt.Errorf("teleport failed to become ready: %w", err)
+	}
+
+	slog.Info("teleport is ready")
+
+	return nil
+}
+
+func (t *teleportInstance) stop() {
+	if t.cmd == nil || t.cmd.Process == nil {
+		return
+	}
+
+	slog.Info("stopping teleport")
+
+	select {
+	case <-t.waitDone:
+		// Already exited.
+	default:
+		_ = syscall.Kill(-t.cmd.Process.Pid, syscall.SIGTERM)
+
+		select {
+		case <-t.waitDone:
+		case <-time.After(5 * time.Second):
+			slog.Warn("teleport did not exit gracefully, sending SIGKILL")
+			_ = syscall.Kill(-t.cmd.Process.Pid, syscall.SIGKILL)
+			<-t.waitDone
+		}
+	}
+
+	if t.logF != nil {
+		t.logF.Close()
+	}
+}
+
+type TeleportConfig struct {
+	DataDir        string
+	AuthServerPort int
+	ProxyPort      int
+	KeyFilePath    string
+	CertFilePath   string
+	LicenseFile    string
+}
+
+func generateTeleportConfig(templatePath string, config *e2eConfig) (string, error) {
+	teleportConfig := &TeleportConfig{
+		DataDir:        config.dataDir,
+		AuthServerPort: config.authPort,
+		ProxyPort:      config.proxyPort,
+		KeyFilePath:    filepath.Join(config.certsDir, keyFileName),
+		CertFilePath:   filepath.Join(config.certsDir, certFileName),
+		LicenseFile:    config.licenseFile,
+	}
+
+	return renderTemplate(templatePath, teleportConfig)
+}
+
+type TeleportNodeConfig struct {
+	AuthServerPort int
+	SSHServerPort  int
+}
+
+func generateTeleportNodeConfig(templatePath string, config *e2eConfig) (string, error) {
+	nodeConfig := &TeleportNodeConfig{
+		AuthServerPort: config.authPort,
+		SSHServerPort:  config.sshPort,
+	}
+
+	return renderTemplate(templatePath, nodeConfig)
+}
+
+type StateConfig struct {
+	PasswordHashBase64  string
+	CredentialIDBase64  string
+	PublicKeyCBORBase64 string
+}
+
+func generateStateFile(templatePath string, creds *credentials) (string, error) {
+	stateConfig := &StateConfig{
+		PasswordHashBase64:  creds.passwordHashBase64,
+		CredentialIDBase64:  creds.credentialIDBase64,
+		PublicKeyCBORBase64: creds.publicKeyCBORBase64,
+	}
+
+	return renderTemplate(templatePath, stateConfig)
+}
+
+func renderTemplate(templatePath string, data any) (string, error) {
+	outPath := strings.TrimSuffix(templatePath, ".tmpl")
+
+	tmpl, err := template.ParseFiles(templatePath)
+	if err != nil {
+		return "", err
+	}
+
+	f, err := os.Create(outPath)
+	if err != nil {
+		return "", err
+	}
+
+	if err := tmpl.Execute(f, data); err != nil {
+		f.Close()
+		return "", err
+	}
+
+	if err := f.Close(); err != nil {
+		return "", err
+	}
+
+	return outPath, nil
+}
