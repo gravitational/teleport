d8a84ad9402ab23c1bd9619e0988a5be4ae8e273
Przemko Robakowski <przemko.robakowski@goteleport.com>
2026-03-04 21:39:09 +0100

    Add Xvfb controller

--- lib/srv/desktop/x11/xvfb.go
+++ lib/srv/desktop/x11/xvfb.go
@@ -3,12 +3,15 @@ package x11
 import (
 	"bufio"
 	"context"
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"os"
 	"os/exec"
+	"regexp"
 	"strings"
 	"time"
 
@@ -22,26 +25,39 @@ import (
 	"go.uber.org/atomic"
 )
 
+var asciiRE = regexp.MustCompile("[[:^ascii:]]")
+
 var modeCount atomic.Uint64
 
 func init() {
 	xgb.Logger = log.New(io.Discard, "", 0)
 }
 
+type Config struct {
+	ClipboardDataReceiver func([]byte)
+}
+
 type Xvfb struct {
 	cmd     *exec.Cmd
-	dial    *xgb.Conn
+	conn    *xgb.Conn
 	setup   *xproto.SetupInfo
 	cancel  context.CancelFunc
 	Display string
+	damage  damage.Damage
+
+	clipboardAtom   xproto.Atom
+	clipboardWindow xproto.Window
+	clipboardData   atomic.Pointer[[]byte]
 }
 
+// IsXvfbPresent reports whether the Xvfb binary is available in PATH.
 func IsXvfbPresent() bool {
 	_, err := exec.LookPath("Xvfb")
 	return err == nil
 }
 
-func NewXvfb(ctx context.Context) (*Xvfb, error) {
+// NewXvfb starts a Xvfb server and returns a connected client wrapper for interacting with the display.
+func NewXvfb(ctx context.Context, config Config) (*Xvfb, error) {
 	if !IsXvfbPresent() {
 		return nil, trace.NotFound("Xvfb is not installed")
 	}
@@ -91,25 +107,121 @@ func NewXvfb(ctx context.Context) (*Xvfb, error) {
 	}
 	display = ":" + strings.TrimSuffix(strings.TrimPrefix(display, ":"), "\n")
 
-	dial, setup, err := connectToDisplay(display)
+	conn, setup, err := connectToDisplay(display)
 	if err != nil {
 		return nil, trace.Wrap(err, "connecting to display")
 	}
 
+	id, err := conn.NewId()
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	damageID := damage.Damage(id)
+
+	err = damage.CreateChecked(conn, damageID, xproto.Drawable(setup.Roots[0].Root), damage.ReportLevelNonEmpty).Check()
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	atomReply, err := xproto.InternAtom(conn, false, uint16(len("CLIPBOARD")), "CLIPBOARD").Reply()
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	clipboard := atomReply.Atom
+
+	window := setup.Roots[0].Root
+	if err := xfixes.SelectSelectionInputChecked(conn, window, clipboard, xfixes.SelectionEventMaskSetSelectionOwner).Check(); err != nil {
+		return nil, trace.Wrap(err)
+	}
+	clipWindow, err := xproto.NewWindowId(conn)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	if err := xproto.CreateWindowChecked(conn, 0, clipWindow, window, -10, -10, 1, 1, 0, xproto.WindowClassInputOnly, xproto.WindowClassCopyFromParent, 0, nil).Check(); err != nil {
+		return nil, trace.Wrap(err)
+	}
+	property, err := xproto.InternAtom(conn, false, uint16(len("TELEPORT_SELECTION")), "TELEPORT_SELECTION").Reply()
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	targets, err := xproto.InternAtom(conn, false, uint16(len("TARGETS")), "TARGETS").Reply()
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	utf8, err := xproto.InternAtom(conn, false, uint16(len("UTF8_STRING")), "UTF8_STRING").Reply()
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	x := &Xvfb{
+		cmd:             cmd,
+		conn:            conn,
+		setup:           setup,
+		cancel:          cancel,
+		Display:         display,
+		damage:          damageID,
+		clipboardAtom:   clipboard,
+		clipboardWindow: clipWindow,
+	}
+
+	go func() {
+		for {
+			event, err := conn.WaitForEvent()
+			if event == nil && err == nil {
+				return
+			}
+			switch event := event.(type) {
+			case xproto.SelectionRequestEvent:
+				if event.Property == xproto.AtomNone {
+					event.Property = property.Atom
+				}
+				data := *x.clipboardData.Load()
+				switch event.Target {
+				case utf8.Atom:
+					xproto.ChangeProperty(conn, xproto.PropModeReplace, event.Requestor, event.Property, xproto.AtomString, 8, uint32(len(data)), data)
+				case xproto.AtomString:
+					data := asciiRE.ReplaceAllLiteralString(string(data), "")
+					xproto.ChangeProperty(conn, xproto.PropModeReplace, event.Requestor, event.Property, xproto.AtomString, 8, uint32(len(data)), []byte(data))
+				case targets.Atom:
+					atoms := make([]byte, 8)
+					binary.LittleEndian.PutUint32(atoms, uint32(utf8.Atom))
+					binary.LittleEndian.PutUint32(atoms[4:], xproto.AtomString)
+					xproto.ChangeProperty(conn, xproto.PropModeReplace, event.Requestor, event.Property, xproto.AtomString, 8, uint32(len(atoms)), atoms)
+				default:
+					event.Property = xproto.AtomNone
+				}
+				xproto.SendEvent(conn, true, event.Requestor, 0, string(xproto.SelectionNotifyEvent{
+					Time:      event.Time,
+					Requestor: event.Requestor,
+					Selection: event.Selection,
+					Target:    event.Target,
+					Property:  event.Property,
+				}.Bytes()))
+			case xproto.SelectionNotifyEvent:
+				if config.ClipboardDataReceiver == nil {
+					continue
+				}
+				prop, err := xproto.GetProperty(conn, true, clipWindow, event.Property, event.Target, 0, 1024).Reply()
+				if err != nil {
+					continue
+				}
+				config.ClipboardDataReceiver(prop.Value)
+			case xfixes.SelectionNotifyEvent:
+				xproto.ConvertSelection(conn, clipWindow, clipboard, utf8.Atom, property.Atom, 0)
+			}
+		}
+	}()
+
 	success = true
-	return &Xvfb{
-		cmd:     cmd,
-		dial:    dial,
-		setup:   setup,
-		cancel:  cancel,
-		Display: display,
-	}, nil
+	return x, nil
 }
 
 func (x *Xvfb) root() xproto.Window {
 	return x.setup.Roots[0].Root
 }
 
+// Close stops the Xvfb process and waits for it to exit.
 func (x *Xvfb) Close() error {
 	x.cancel()
 	var e *exec.ExitError
@@ -122,94 +234,135 @@ func (x *Xvfb) Close() error {
 }
 
 func connectToDisplay(display string) (*xgb.Conn, *xproto.SetupInfo, error) {
-	dial, err := xgb.NewConnDisplay(display)
+	conn, err := xgb.NewConnDisplay(display)
 	if err != nil {
 		return nil, nil, trace.Wrap(err)
 	}
-	setup := xproto.Setup(dial)
-	if err := randr.Init(dial); err != nil {
+	setup := xproto.Setup(conn)
+	if err := randr.Init(conn); err != nil {
 		return nil, nil, trace.Wrap(err)
 	}
-	if err := xtest.Init(dial); err != nil {
+	if err := xtest.Init(conn); err != nil {
 		return nil, nil, trace.Wrap(err)
 	}
-	if err := damage.Init(dial); err != nil {
+	if err := damage.Init(conn); err != nil {
 		return nil, nil, trace.Wrap(err)
 	}
-	if err := xfixes.Init(dial); err != nil {
+	if err := xfixes.Init(conn); err != nil {
 		return nil, nil, trace.Wrap(err)
 	}
-	if err := randr.Init(dial); err != nil {
-		return nil, nil, trace.Wrap(err)
-	}
-
-	_, err = xfixes.QueryVersion(dial, 5, 0).Reply()
-	if err != nil {
+	if err := randr.Init(conn); err != nil {
 		return nil, nil, trace.Wrap(err)
 	}
 
-	_, err = damage.QueryVersion(dial, 1, 1).Reply()
+	_, err = xfixes.QueryVersion(conn, 5, 0).Reply()
 	if err != nil {
 		return nil, nil, trace.Wrap(err)
 	}
 
-	_, err = xfixes.QueryVersion(dial, 5, 0).Reply()
+	_, err = damage.QueryVersion(conn, 1, 1).Reply()
 	if err != nil {
 		return nil, nil, trace.Wrap(err)
 	}
 
-	_, err = randr.QueryVersion(dial, 5, 0).Reply()
+	_, err = xfixes.QueryVersion(conn, 5, 0).Reply()
 	if err != nil {
 		return nil, nil, trace.Wrap(err)
 	}
 
-	return dial, setup, nil
+	_, err = randr.QueryVersion(conn, 5, 0).Reply()
+	if err != nil {
+		return nil, nil, trace.Wrap(err)
+	}
+
+	return conn, setup, nil
 }
 
+// GetChanges returns regions changed since the last call of this method
+func (x *Xvfb) GetChanges() ([]xproto.Rectangle, error) {
+	id, err := x.conn.NewId()
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	region := xfixes.Region(id)
+	if err := xfixes.CreateRegionChecked(x.conn, region, nil).Check(); err != nil {
+		return nil, trace.Wrap(err)
+	}
+	defer xfixes.DestroyRegion(x.conn, region)
+	if err := damage.SubtractChecked(x.conn, x.damage, xfixes.RegionNone, region).Check(); err != nil {
+		return nil, trace.Wrap(err)
+	}
+	fetched, err := xfixes.FetchRegion(x.conn, region).Reply()
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	return fetched.Rectangles, nil
+}
+
+// GetImage captures image data for the requested rectangle in RGBA format.
+func (x *Xvfb) GetImage(rect xproto.Rectangle) ([]byte, error) {
+	root := xproto.Drawable(x.root())
+	reply, err := xproto.GetImage(x.conn, xproto.ImageFormatZPixmap, root, rect.X, rect.Y, rect.Width, rect.Height, math.MaxUint32).Reply()
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	data := reply.Data
+	// Data returned from xproto.GetImage is BGRA and alpha is always 0, we change it to RGBA and alpha set to 0xFF
+	for i := 0; i < len(data); i += 4 {
+		data[i+0], data[i+2] = data[i+2], data[i+0]
+		data[i+3] = 0xff
+	}
+	return data, nil
+}
+
+// SendKeyboardButton sends a key press or release event.
 func (x *Xvfb) SendKeyboardButton(keycode byte, pressed bool) error {
-	eventType := byte(3)
+	eventType := xproto.KeyRelease
 	if pressed {
-		eventType = 2
+		eventType = xproto.KeyPress
 	}
-	err := xtest.FakeInputChecked(x.dial, eventType, keycode+8, xproto.TimeCurrentTime, x.root(), 0, 0, 0).Check()
+	err := xtest.FakeInputChecked(x.conn, byte(eventType), keycode+8, xproto.TimeCurrentTime, x.root(), 0, 0, 0).Check()
 	return trace.Wrap(err)
 }
 
+// SendMouseButton sends a mouse button press or release event.
 func (x *Xvfb) SendMouseButton(button byte, pressed bool) error {
-	eventType := byte(5)
+	eventType := xproto.ButtonRelease
 	if pressed {
-		eventType = 4
+		eventType = xproto.ButtonPress
 	}
-	err := xtest.FakeInputChecked(x.dial, eventType, button+1, xproto.TimeCurrentTime, x.root(), 0, 0, 0).Check()
+	err := xtest.FakeInputChecked(x.conn, byte(eventType), button+1, xproto.TimeCurrentTime, x.root(), 0, 0, 0).Check()
 	return trace.Wrap(err)
 }
 
+// SendMouseWheel sends one wheel step event in the requested direction.
 func (x *Xvfb) SendMouseWheel(delta int) error {
 	detail := byte(5)
 	if delta > 0 {
 		detail = 4
 	}
-	if err := xtest.FakeInputChecked(x.dial, 4, detail, xproto.TimeCurrentTime, x.root(), 0, 0, 0).Check(); err != nil {
+	if err := xtest.FakeInputChecked(x.conn, 4, detail, xproto.TimeCurrentTime, x.root(), 0, 0, 0).Check(); err != nil {
 		return trace.Wrap(err)
 	}
-	err := xtest.FakeInputChecked(x.dial, 5, detail, xproto.TimeCurrentTime, x.root(), 0, 0, 0).Check()
+	err := xtest.FakeInputChecked(x.conn, 5, detail, xproto.TimeCurrentTime, x.root(), 0, 0, 0).Check()
 	return trace.Wrap(err)
 }
 
+// Resize changes the virtual screen size.
 func (x *Xvfb) Resize(width, height uint16) error {
 	if width > 8192 || height > 8192 {
 		return trace.BadParameter("maximum size is 8192x8192")
 	}
-	dial := x.dial
+	conn := x.conn
 	root := x.root()
 
 	modeName := fmt.Sprintf("mode%d", modeCount.Inc())
 
-	id, err := dial.NewId()
+	id, err := conn.NewId()
 	if err != nil {
 		return trace.Wrap(err)
 	}
-	mode, err := randr.CreateMode(dial, root, randr.ModeInfo{
+	mode, err := randr.CreateMode(conn, root, randr.ModeInfo{
 		Id:      id,
 		Width:   width,
 		Height:  height,
@@ -218,20 +371,20 @@ func (x *Xvfb) Resize(width, height uint16) error {
 	if err != nil {
 		return trace.Wrap(err)
 	}
-	screenResources, err := randr.GetScreenResources(dial, root).Reply()
+	screenResources, err := randr.GetScreenResources(conn, root).Reply()
 	if err != nil {
 		return trace.Wrap(err)
 	}
-	if err := randr.AddOutputModeChecked(dial, screenResources.Outputs[0], mode.Mode).Check(); err != nil {
+	if err := randr.AddOutputModeChecked(conn, screenResources.Outputs[0], mode.Mode).Check(); err != nil {
 		return trace.Wrap(err)
 	}
-	screen, err := randr.GetScreenInfo(dial, root).Reply()
+	screen, err := randr.GetScreenInfo(conn, root).Reply()
 	if err != nil {
 		return trace.Wrap(err)
 	}
 	for i := 0; i < len(screen.Sizes); i++ {
 		if screen.Sizes[i].Width == width && screen.Sizes[i].Height == height {
-			_, err := randr.SetScreenConfig(dial, root, 0, screen.ConfigTimestamp, uint16(i), screen.Rotation, screen.Rate).Reply()
+			_, err := randr.SetScreenConfig(conn, root, 0, screen.ConfigTimestamp, uint16(i), screen.Rotation, screen.Rate).Reply()
 			if err != nil {
 				return trace.Wrap(err)
 			}
@@ -239,4 +392,11 @@ func (x *Xvfb) Resize(width, height uint16) error {
 		}
 	}
 	return trace.Errorf("could not find a screen with width %d and height %d", width, height)
+}
+
+// SetClipboardData stores clipboard data and claims clipboard ownership.
+func (x *Xvfb) SetClipboardData(data []byte) error {
+	x.clipboardData.Store(&data)
+	err := xproto.SetSelectionOwnerChecked(x.conn, x.clipboardWindow, x.clipboardAtom, 0).Check()
+	return trace.Wrap(err)
 }
--- lib/srv/desktop/x11/xvfb_test.go
+++ lib/srv/desktop/x11/xvfb_test.go
@@ -1,12 +1,19 @@
 package x11
 
 import (
+	"fmt"
+	"io"
+	"os/exec"
 	"regexp"
+	"sync/atomic"
 	"syscall"
 	"testing"
 	"time"
 
 	"github.com/gravitational/trace"
+	"github.com/jezek/xgb"
+	"github.com/jezek/xgb/randr"
+	"github.com/jezek/xgb/xproto"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -15,7 +22,7 @@ func TestNewXvfb(t *testing.T) {
 	if !IsXvfbPresent() {
 		t.Skip("this test requires Xvfb to be installed")
 	}
-	xvfb, err := NewXvfb(t.Context())
+	xvfb, err := NewXvfb(t.Context(), Config{})
 	require.NoError(t, err)
 	require.NotNil(t, xvfb)
 
@@ -33,13 +40,256 @@ func TestNewXvfb(t *testing.T) {
 }
 
 func TestResize(t *testing.T) {
-	xvfb, err := NewXvfb(t.Context())
-	defer xvfb.Close()
+	if !IsXvfbPresent() {
+		t.Skip("this test requires Xvfb to be installed")
+	}
+
+	xvfb, err := NewXvfb(t.Context(), Config{})
 	require.NoError(t, err)
+	defer xvfb.Close()
+
+	conn, _, err := connectToDisplay(xvfb.Display)
+	require.NoError(t, err)
+	defer conn.Close()
+
+	screenInfo, err := randr.GetScreenInfo(conn, xvfb.root()).Reply()
+	require.NoError(t, err)
+	require.Equal(t, uint16(0), screenInfo.SizeID)
+	require.Equal(t, uint16(8192), screenInfo.Sizes[0].Width)
+	require.Equal(t, uint16(8192), screenInfo.Sizes[0].Height)
+
 	err = xvfb.Resize(9000, 200)
 	require.True(t, trace.IsBadParameter(err))
 	err = xvfb.Resize(2000, 9200)
 	require.True(t, trace.IsBadParameter(err))
+	err = xvfb.Resize(2000, 1000)
+	require.NoError(t, err)
+
+	screenInfo, err = randr.GetScreenInfo(conn, xvfb.root()).Reply()
+	require.NoError(t, err)
+	require.Equal(t, uint16(1), screenInfo.SizeID)
+	require.Equal(t, uint16(2000), screenInfo.Sizes[1].Width)
+	require.Equal(t, uint16(1000), screenInfo.Sizes[1].Height)
+}
+
+func TestGetChanges(t *testing.T) {
+	if !IsXvfbPresent() {
+		t.Skip("this test requires Xvfb to be installed")
+	}
+
+	xvfb, err := NewXvfb(t.Context(), Config{})
+	require.NoError(t, err)
+	defer xvfb.Close()
+
+	changes, err := xvfb.GetChanges()
+	require.NoError(t, err)
+	require.Len(t, changes, 1)
+	require.Equal(t, uint16(8192), changes[0].Width)
+	require.Equal(t, uint16(8192), changes[0].Height)
+
+	changes, err = xvfb.GetChanges()
+	require.NoError(t, err)
+	require.Len(t, changes, 0)
+
 	err = xvfb.Resize(1000, 1000)
+	require.NoError(t, err)
+
+	changes, err = xvfb.GetChanges()
+	require.NoError(t, err)
+	require.Len(t, changes, 1)
+	require.Equal(t, uint16(1000), changes[0].Width)
+	require.Equal(t, uint16(1000), changes[0].Height)
+
+	conn, setup, err := connectToDisplay(xvfb.Display)
+	require.NoError(t, err)
+	defer conn.Close()
+
+	drawRectangle(t, conn, setup, xproto.Rectangle{
+		X:      5,
+		Y:      10,
+		Width:  10,
+		Height: 20,
+	})
+
+	changes, err = xvfb.GetChanges()
+	require.NoError(t, err)
+	require.Len(t, changes, 1)
+	require.Equal(t, int16(5), changes[0].X)
+	require.Equal(t, int16(10), changes[0].Y)
+	require.Equal(t, uint16(10), changes[0].Width)
+	require.Equal(t, uint16(20), changes[0].Height)
+}
+
+func TestGetImage(t *testing.T) {
+	if !IsXvfbPresent() {
+		t.Skip("this test requires Xvfb to be installed")
+	}
+
+	xvfb, err := NewXvfb(t.Context(), Config{})
+	require.NoError(t, err)
+	defer xvfb.Close()
+
+	conn, setup, err := connectToDisplay(xvfb.Display)
+	require.NoError(t, err)
+	defer conn.Close()
+
+	drawRectangle(t, conn, setup, xproto.Rectangle{
+		X:      1,
+		Y:      1,
+		Width:  1,
+		Height: 1,
+	})
+
+	image, err := xvfb.GetImage(xproto.Rectangle{
+		Width:  2,
+		Height: 2,
+	})
+	require.NoError(t, err)
+	require.Equal(t, []byte{0, 0, 0, 0xFF, 0, 0, 0, 0xFF, 0, 0, 0, 0xFF, 0x12, 0x34, 0x56, 0xFF}, image)
+}
+
+func TestInputs(t *testing.T) {
+	if !IsXvfbPresent() {
+		t.Skip("this test requires Xvfb to be installed")
+	}
+
+	xvfb, err := NewXvfb(t.Context(), Config{})
+	require.NoError(t, err)
+	defer xvfb.Close()
+
+	conn, _, err := connectToDisplay(xvfb.Display)
+	require.NoError(t, err)
+	defer conn.Close()
+
+	eventMask := uint16(xproto.EventMaskPointerMotion | xproto.EventMaskButtonPress | xproto.EventMaskButtonRelease)
+	_, err = xproto.GrabPointer(conn, true, xvfb.root(), eventMask, xproto.GrabModeAsync, xproto.GrabModeAsync, xproto.AtomNone, xproto.AtomNone, xproto.TimeCurrentTime).Reply()
+	require.NoError(t, err)
+	_, err = xproto.GrabKeyboard(conn, true, xvfb.root(), xproto.TimeCurrentTime, xproto.GrabModeAsync, xproto.GrabModeAsync).Reply()
+	require.NoError(t, err)
+
+	err = xvfb.SendMouseButton(0, true)
+	require.NoError(t, err)
+
+	bp := pollEvent[xproto.ButtonPressEvent](t, conn)
+	require.Equal(t, xproto.Button(1), bp.Detail)
+
+	err = xvfb.SendMouseButton(0, false)
+	require.NoError(t, err)
+
+	br := pollEvent[xproto.ButtonReleaseEvent](t, conn)
+	require.Equal(t, xproto.Button(1), br.Detail)
+
+	err = xvfb.SendMouseWheel(1)
+	require.NoError(t, err)
+
+	bp = pollEvent[xproto.ButtonPressEvent](t, conn)
+	require.Equal(t, xproto.Button(4), bp.Detail)
+
+	br = pollEvent[xproto.ButtonReleaseEvent](t, conn)
+	require.Equal(t, xproto.Button(4), br.Detail)
+
+	err = xvfb.SendMouseWheel(-1)
+	require.NoError(t, err)
+
+	bp = pollEvent[xproto.ButtonPressEvent](t, conn)
+	require.Equal(t, xproto.Button(5), bp.Detail)
+
+	br = pollEvent[xproto.ButtonReleaseEvent](t, conn)
+	require.Equal(t, xproto.Button(5), br.Detail)
+
+	err = xvfb.SendKeyboardButton(13, true)
+	require.NoError(t, err)
+
+	pollEvent[xproto.MappingNotifyEvent](t, conn)
+	pollEvent[xproto.MappingNotifyEvent](t, conn)
+	kp := pollEvent[xproto.KeyPressEvent](t, conn)
+	require.Equal(t, xproto.Keycode(21), kp.Detail)
+
+	err = xvfb.SendKeyboardButton(13, false)
+	require.NoError(t, err)
+	kr := pollEvent[xproto.KeyReleaseEvent](t, conn)
+	require.Equal(t, xproto.Keycode(21), kr.Detail)
+}
+
+func TestClipboard(t *testing.T) {
+	if !IsXvfbPresent() {
+		t.Skip("this test requires Xvfb to be installed")
+	}
+
+	// clipboard interactions are really complex in X11
+	_, err := exec.LookPath("xclip")
+	if err != nil {
+		t.Skip("this test requires xclip to be installed")
+	}
+
+	var data atomic.Pointer[[]byte]
+
+	xvfb, err := NewXvfb(t.Context(), Config{
+		ClipboardDataReceiver: func(bytes []byte) {
+			data.Store(&bytes)
+		},
+	})
+	require.NoError(t, err)
+	defer xvfb.Close()
+
+	err = xvfb.SetClipboardData([]byte("sent_to_clipboard"))
+	require.NoError(t, err)
+
+	cmd := exec.CommandContext(t.Context(), "xclip", "-selection", "clipboard", "-o")
+	cmd.Env = []string{fmt.Sprintf("DISPLAY=%s", xvfb.Display)}
+	out, err := cmd.CombinedOutput()
+	require.NoError(t, err, string(out))
+	require.Equal(t, "sent_to_clipboard", string(out))
+
+	cmd = exec.CommandContext(t.Context(), "xclip", "-selection", "clipboard")
+	cmd.Env = []string{fmt.Sprintf("DISPLAY=%s", xvfb.Display)}
+	w, err := cmd.StdinPipe()
+	require.NoError(t, err)
+
+	n, err := io.WriteString(w, "abcd")
+	require.NoError(t, err)
+	require.Equal(t, len("abcd"), n)
+	require.NoError(t, w.Close())
+
+	err = cmd.Run()
+	require.NoError(t, err)
+
+	assert.Eventually(t, func() bool {
+		return data.Load() != nil && len(*data.Load()) > 0
+	}, 1*time.Second, 50*time.Millisecond, "waiting on clipboard data")
+
+	require.Equal(t, "abcd", string(*data.Load()))
+}
+
+func pollEvent[T xgb.Event](t *testing.T, conn *xgb.Conn) T {
+	var event xgb.Event
+	var err error
+	assert.Eventually(t, func() bool {
+		event, err = conn.PollForEvent()
+		return event != nil || err != nil
+	}, 1*time.Second, 10*time.Millisecond, "polling for event")
+	require.NoError(t, err)
+	require.NotNil(t, event)
+	tt, ok := event.(T)
+	require.True(t, ok)
+	return tt
+}
+
+func drawRectangle(t *testing.T, conn *xgb.Conn, setup *xproto.SetupInfo, rect xproto.Rectangle) {
+	screen := setup.Roots[0]
+	root := screen.Root
+	// Create a GC (Graphics Context) on the root window
+	gc, err := xproto.NewGcontextId(conn)
+	require.NoError(t, err)
+
+	// GC flags and values: set foreground color
+	mask := uint32(xproto.GcForeground)
+	values := []uint32{0x123456}
+
+	err = xproto.CreateGCChecked(conn, gc, xproto.Drawable(root), mask, values).Check()
+	require.NoError(t, err)
+
+	// Draw a filled rectangle on the root window
+	err = xproto.PolyFillRectangleChecked(conn, xproto.Drawable(root), gc, []xproto.Rectangle{rect}).Check()
 	require.NoError(t, err)
 }
