4592aee458e13297ec692bfed7f1e1e14fdb4c33
Adam Pickering <adam.pickering@goteleport.com>
2026-03-06 15:56:26 -0700

    Return early from postgres procedures where possible
    This improves readability by getting rid of a level of indentation.

--- lib/srv/db/postgres/sql/deactivate-user.sql
+++ lib/srv/db/postgres/sql/deactivate-user.sql
@@ -7,17 +7,18 @@ BEGIN
     -- Only deactivate if the user doesn't have other active sessions.
     IF EXISTS (SELECT usename FROM pg_stat_activity WHERE usename = username) THEN
         RAISE NOTICE 'User has active connections';
-    ELSE
-        -- Revoke all role memberships except teleport-auto-user group.
-        FOR role_ IN
-        SELECT r.rolname
-        FROM pg_roles r
-        WHERE r.rolname NOT IN (username, 'teleport-auto-user') AND
-              r.oid IN (select m.roleid from pg_auth_members m where m.member = to_regrole(QUOTE_IDENT(username))::oid)
-        LOOP
-            EXECUTE FORMAT('REVOKE %I FROM %I', role_, username);
-        END LOOP;
-        -- Disable ability to login for the user.
-        EXECUTE FORMAT('ALTER USER %I WITH NOLOGIN', username);
+        RETURN;
     END IF;
+
+    -- Revoke all role memberships except teleport-auto-user group.
+    FOR role_ IN
+    SELECT r.rolname
+    FROM pg_roles r
+    WHERE r.rolname NOT IN (username, 'teleport-auto-user') AND
+          r.oid IN (select m.roleid from pg_auth_members m where m.member = to_regrole(QUOTE_IDENT(username))::oid)
+    LOOP
+        EXECUTE FORMAT('REVOKE %I FROM %I', role_, username);
+    END LOOP;
+    -- Disable ability to login for the user.
+    EXECUTE FORMAT('ALTER USER %I WITH NOLOGIN', username);
 END;$$;
--- lib/srv/db/postgres/sql/delete-user.sql
+++ lib/srv/db/postgres/sql/delete-user.sql
@@ -7,15 +7,16 @@ BEGIN
     -- Only drop if the user doesn't have other active sessions.
     IF EXISTS (SELECT usename FROM pg_stat_activity WHERE usename = username) THEN
         RAISE NOTICE 'User has active connections';
-    ELSE
-        BEGIN
-            EXECUTE FORMAT('DROP USER IF EXISTS %I', username);
-        EXCEPTION
-            WHEN SQLSTATE '2BP01' THEN
-                state := 'TP004';
-                -- Drop user/role will fail if user has dependent objects.
-                -- In this scenario, fallback into disabling the user.
-                CALL pg_temp.teleport_deactivate_user(username);
-        END;
+        RETURN;
     END IF;
+
+    BEGIN
+        EXECUTE FORMAT('DROP USER IF EXISTS %I', username);
+    EXCEPTION
+        WHEN SQLSTATE '2BP01' THEN
+            state := 'TP004';
+            -- Drop user/role will fail if user has dependent objects.
+            -- In this scenario, fallback into disabling the user.
+            CALL pg_temp.teleport_deactivate_user(username);
+    END;
 END;$$;
