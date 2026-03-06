ead2e9b41c018ff1189139ec000499bf85d4d6c5
Erik Tate <erik.tate@goteleport.com>
2026-03-05 17:10:59 -0800

    building in some headroom for proto wire format overhead during encrypted recording uploads and (#64227)
    making encrypted recording max upload size configurable with TELEPORT_UNSTABLE_GRPC_RECV_SIZE

--- lib/service/service.go
+++ lib/service/service.go
@@ -81,6 +81,7 @@ import (
 	"github.com/gravitational/teleport/api/types/discoveryconfig"
 	apievents "github.com/gravitational/teleport/api/types/events"
 	apiutils "github.com/gravitational/teleport/api/utils"
+	grpcutils "github.com/gravitational/teleport/api/utils/grpc"
 	"github.com/gravitational/teleport/api/utils/grpc/interceptors"
 	"github.com/gravitational/teleport/api/utils/keys"
 	"github.com/gravitational/teleport/api/utils/retryutils"
@@ -3892,7 +3893,7 @@ func (process *TeleportProcess) initUploaderService() error {
 
 		// encrypted uploads are aggregated and uploaded directly rather than with an event stream.
 		// Since we are using the gRPC client, we must set this maximum for the aggregation step.
-		encryptedRecordingMaxUploadSize = 4 * 1024 * 1024 // 4MiB, default gRPC max msg recv size.
+		encryptedRecordingMaxUploadSize = grpcutils.MaxClientRecvMsgSize()
 	}
 
 	logger.InfoContext(process.ExitContext(), "starting upload completer service")
