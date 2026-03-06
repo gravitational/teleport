517ebdf1564ae7b5ad7bd588db15ffaae9a01618
williamong-tel <william.ong@goteleport.com>
2026-03-06 12:11:08 -0800

    ran make docs
    Update resource_teleport_scoped_token.go

--- integrations/terraform/provider/resource_teleport_scoped_token.go
+++ integrations/terraform/provider/resource_teleport_scoped_token.go
@@ -24,12 +24,13 @@ import (
 
 	joiningv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/scopes/joining/v1"
 	
-	"github.com/gravitational/teleport/api/utils/retryutils"
+	"github.com/gravitational/teleport/integrations/lib/backoff"
 	"github.com/gravitational/trace"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/jonboulle/clockwork"
 
 	schemav1 "github.com/gravitational/teleport/integrations/terraform/tfschema/scopes/joining/v1"
 )
@@ -104,24 +105,14 @@ func (r resourceTeleportScopedToken) Create(ctx context.Context, req tfsdk.Creat
 		var scopedTokenI *joiningv1.ScopedToken
 	// Try getting the resource until it exists.
 	tries := 0
-	retry, err := retryutils.NewRetryV2(retryutils.RetryV2Config{
-		Driver: retryutils.NewExponentialDriver(r.p.RetryConfig.Base),
-		First:  r.p.RetryConfig.Base,
-		Max:    r.p.RetryConfig.Cap,
-		Jitter: retryutils.HalfJitter,
-	})
-	if err != nil {
-		return
-	}
+	backoff := backoff.NewDecorr(r.p.RetryConfig.Base, r.p.RetryConfig.Cap, clockwork.NewRealClock())
 	for {
 		tries = tries + 1
 		scopedTokenI, err = r.p.Client.GetScopedToken(ctx, id, true)
 		if trace.IsNotFound(err) {
-		    select {
-			case <-ctx.Done():
-			    resp.Diagnostics.Append(diagFromWrappedErr("Error reading ScopedToken", trace.Wrap(ctx.Err()), "scoped_token"))
+			if bErr := backoff.Do(ctx); bErr != nil {
+				resp.Diagnostics.Append(diagFromWrappedErr("Error reading ScopedToken", trace.Wrap(err), "scoped_token"))
 				return
-			case <-retry.After():
 			}
 			if tries >= r.p.RetryConfig.MaxTries {
 				diagMessage := fmt.Sprintf("Error reading ScopedToken (tried %d times) - state outdated, please import resource", tries)
@@ -219,7 +210,6 @@ func (r resourceTeleportScopedToken) Update(ctx context.Context, req tfsdk.Updat
 	}
 	scopedTokenResource := scopedToken
 
-	scopedTokenResource.Kind = apitypes.KindScopedToken
 
 	
 	name := scopedTokenResource.Metadata.Name
@@ -238,15 +228,7 @@ func (r resourceTeleportScopedToken) Update(ctx context.Context, req tfsdk.Updat
 		var scopedTokenI *joiningv1.ScopedToken
 
 	tries := 0
-	retry, err := retryutils.NewRetryV2(retryutils.RetryV2Config{
-		Driver: retryutils.NewExponentialDriver(r.p.RetryConfig.Base),
-		First:  r.p.RetryConfig.Base,
-		Max:    r.p.RetryConfig.Cap,
-		Jitter: retryutils.HalfJitter,
-	})
-	if err != nil {
-		return
-	}
+	backoff := backoff.NewDecorr(r.p.RetryConfig.Base, r.p.RetryConfig.Cap, clockwork.NewRealClock())
 	for {
 		tries = tries + 1
 		scopedTokenI, err = r.p.Client.GetScopedToken(ctx, name, true)
@@ -258,11 +240,9 @@ func (r resourceTeleportScopedToken) Update(ctx context.Context, req tfsdk.Updat
 			break
 		}
 
-		select {
-		case <-ctx.Done():
-		    resp.Diagnostics.Append(diagFromWrappedErr("Error reading ScopedToken", trace.Wrap(ctx.Err()), "scoped_token"))
+		if err := backoff.Do(ctx); err != nil {
+			resp.Diagnostics.Append(diagFromWrappedErr("Error reading ScopedToken", trace.Wrap(err), "scoped_token"))
 			return
-		case <-retry.After():
 		}
 		if tries >= r.p.RetryConfig.MaxTries {
 			diagMessage := fmt.Sprintf("Error reading ScopedToken (tried %d times) - state outdated, please import resource", tries)
