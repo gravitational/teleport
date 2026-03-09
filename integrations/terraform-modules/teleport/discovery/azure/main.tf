07c4b3f2eef91fbf0dc0660818c503bcec31dadc
Gavin Frazar <gavin.frazar@goteleport.com>
2026-03-09 13:07:47 -0700

    Simplify client config data source
    It's so cheap to read the data source that we don't need to make it
    conditional and it improves the terraform plan.

--- integrations/terraform-modules/teleport/discovery/azure/main.tf
+++ integrations/terraform-modules/teleport/discovery/azure/main.tf
@@ -1,7 +1,7 @@
 locals {
   create                = var.create
-  azure_subscription_id = try(data.azurerm_client_config.this[0].subscription_id, "")
-  azure_tenant_id       = try(data.azurerm_client_config.this[0].tenant_id, "")
+  azure_subscription_id = data.azurerm_client_config.this.subscription_id
+  azure_tenant_id = data.azurerm_client_config.this.tenant_id
   apply_azure_tags = merge(var.apply_azure_tags, {
     "TeleportCluster"     = local.teleport_cluster_name
     "TeleportIntegration" = local.teleport_integration_name
@@ -17,9 +17,7 @@ locals {
   teleport_resource_name_suffix = "azure-${local.azure_subscription_id}"
 }
 
-data "azurerm_client_config" "this" {
-  count = local.create ? 1 : 0
-}
+data "azurerm_client_config" "this" {}
 
 data "http" "teleport_ping" {
   count = local.create ? 1 : 0
