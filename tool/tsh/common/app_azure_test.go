ed76e109f3c1ff5a38f090ebee52bc4c890c990a
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2026-03-11 09:39:00 +0100

    Forbid `semver.New` in the forbidigo linter (#64307)
    * Forbid semver.New in forbidigo
    
    * Fix type in require.Greater
    
    * Fix missed semver.New in build.assets
    
    * Restore whitespace in Test_formatAzureIdentities
    
    * Re-fix TestAzure

--- tool/tsh/common/app_azure_test.go
+++ tool/tsh/common/app_azure_test.go
@@ -92,7 +92,9 @@ func TestAzure(t *testing.T) {
 		return ""
 	}
 
-	versionWithoutMSAL := semver.New(azureCLIVersionMSALRequirement.String())
+	versionWithoutMSAL := new(semver.Version)
+	*versionWithoutMSAL = *azureCLIVersionMSALRequirement
+	require.Greater(t, versionWithoutMSAL.Minor, int64(0))
 	versionWithoutMSAL.Minor -= 1
 
 	for name, tc := range map[string]struct {
