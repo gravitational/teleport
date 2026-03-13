87c7debaa98104b50cba268d23309777b9b7f571
Alex Hemard <alex.hemard@goteleport.com>
2026-03-13 17:32:19 -0500

    Release v19.0.0-dev.alexh.2

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-dev.alexh.1"
+const Version = "19.0.0-dev.alexh.2"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev.alexh.1"
+const VersionPreRelease = "dev.alexh.2"
 const VersionMetadata = ""
