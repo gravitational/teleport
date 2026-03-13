27e899cbd8c196d46fe865db4b458f3d03b30e1f
Zac Bergquist <zac.bergquist@goteleport.com>
2026-03-13 11:13:54 -0600

    Release 19.0.0-dev.osslfips.2

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-prealpha.1"
+const Version = "19.0.0-dev.osslfips.2"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "prealpha.1"
+const VersionPreRelease = "dev.osslfips.2"
 const VersionMetadata = ""
