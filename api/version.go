3acd0e018da6226fa349d48a9d862f120ae0f692
Ryan Hammonds <ryan.hammonds@goteleport.com>
2026-03-13 14:06:06 -0400

    Release v17.7.20-dev.rhammonds.toolchainbackport.v17.2

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "17.7.20"
+const Version = "17.7.20-dev.rhammonds.toolchainbackport.v17.2"
 
 const VersionMajor = 17
 const VersionMinor = 7
 const VersionPatch = 20
-const VersionPreRelease = ""
+const VersionPreRelease = "dev.rhammonds.toolchainbackport.v17.2"
 const VersionMetadata = ""
