a83bcd54c7e4d2a934440117c98285aee6a74406
Lukasz Okraszewski <lukasz.okraszewski@goteleport.com>
2026-03-17 13:14:48 +0000

    [loadtest] update gcp docs and examples
    Add examples of the required env vars when testing against gcp.
    Updates docs to match the makefile
    Switch to devel chart repo by default.

--- assets/loadtest/control-plane/init.sh
+++ assets/loadtest/control-plane/init.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 
 # initialize dependencies
 
-helm repo add teleport https://charts.releases.teleport.dev
+# helm repo add teleport https://charts.releases.teleport.dev
+helm repo add teleport https://charts.releases.development.teleport.dev
 helm repo add jetstack https://charts.jetstack.io
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
