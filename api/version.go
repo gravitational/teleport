8c995de5e2e5c2eaaac44a41c288263f2fc16865
Marek Smoliński <marek@goteleport.com>
2026-03-18 09:55:17 +0100

    Relase 18.6.4-dev.scim-list-cache

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.6.4"
+const Version = "18.6.4-dev.scim-list-cache"
 
 const VersionMajor = 18
 const VersionMinor = 6
 const VersionPatch = 4
-const VersionPreRelease = ""
+const VersionPreRelease = "dev.scim-list-cache"
 const VersionMetadata = ""
