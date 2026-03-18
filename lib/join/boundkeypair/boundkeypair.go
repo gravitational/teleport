1214049f923e4954b9da8e4978b703c028c12318
Tim Buckley <tim@goteleport.com>
2026-03-18 12:59:42 +0000

    Zero check on MustRegisterBefore due to proto impl differences

--- lib/join/boundkeypair/boundkeypair.go
+++ lib/join/boundkeypair/boundkeypair.go
@@ -935,7 +935,7 @@ func HandleBoundKeypairJoin(
 				return nil, trace.AccessDenied(errMsg)
 			}
 
-			if spec.Onboarding.MustRegisterBefore != nil {
+			if spec.Onboarding.MustRegisterBefore != nil && !spec.Onboarding.MustRegisterBefore.IsZero() {
 				if params.Clock.Now().After(*spec.Onboarding.MustRegisterBefore) {
 					log.WarnContext(
 						ctx,
