e83e58801d96f820807a4bdb79671e4c4b2e5dcb
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2026-03-19 12:14:34 +0100

    Add missing accessibility specifiers

--- web/packages/teleterm/src/ui/HeadlessAuthn/HeadlessPrompt/HeadlessPrompt.tsx
+++ web/packages/teleterm/src/ui/HeadlessAuthn/HeadlessPrompt/HeadlessPrompt.tsx
@@ -92,6 +92,7 @@ export function HeadlessPrompt({
         </H2>
         <ButtonIcon
           type="button"
+          aria-label="Close"
           color="text.slightlyMuted"
           onClick={() => {
             abortApproval();
