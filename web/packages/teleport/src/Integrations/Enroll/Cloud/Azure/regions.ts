187a36068eb2f2ce226566b3ff4ae0dfda58d79e
Alex Hemard <alex.hemard@goteleport.com>
2026-03-20 18:05:00 -0500

    [web] discover: add azure regions

---
+++ web/packages/teleport/src/Integrations/Enroll/Cloud/Azure/regions.ts
@@ -0,0 +1,112 @@
+/**
+ * Teleport
+ * Copyright (C) 2026  Gravitational, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { azureRegionMap, AzureRegion } from 'teleport/services/integrations';
+
+import { RegionGroup } from '../Shared/RegionSelect';
+
+const regions: { name: string; regions: AzureRegion[] }[] = [
+  {
+    name: 'Americas',
+    regions: [
+      'brazilsouth',
+      'brazilsoutheast',
+      'canadacentral',
+      'canadaeast',
+      'centralus',
+      'chilecentral',
+      'eastus',
+      'eastus2',
+      'mexicocentral',
+      'northcentralus',
+      'southcentralus',
+      'westcentralus',
+      'westus',
+      'westus2',
+      'westus3',
+    ],
+  },
+
+  {
+    name: 'Europe',
+    regions: [
+      'austriaeast',
+      'belgiumcentral',
+      'denmarkeast',
+      'francecentral',
+      'francesouth',
+      'germanynorth',
+      'germanywestcentral',
+      'italynorth',
+      'northeurope',
+      'norwayeast',
+      'norwaywest',
+      'polandcentral',
+      'spaincentral',
+      'swedencentral',
+      'switzerlandnorth',
+      'switzerlandwest',
+      'uksouth',
+      'ukwest',
+      'westeurope',
+    ],
+  },
+  {
+    name: 'Asia Pacific',
+    regions: [
+      'australiacentral',
+      'australiacentral2',
+      'australiaeast',
+      'australiasoutheast',
+      'centralindia',
+      'eastasia',
+      'indonesiacentral',
+      'japaneast',
+      'japanwest',
+      'koreacentral',
+      'koreasouth',
+      'malaysiawest',
+      'newzealandnorth',
+      'southeastasia',
+      'southindia',
+      'westindia',
+    ],
+  },
+  {
+    name: 'Middle East',
+    regions: ['uaenorth', 'uaecentral', 'qatarcentral', 'israelcentral'],
+  },
+  {
+    name: 'Africa',
+    regions: ['southafricanorth', 'southafricawest'],
+  },
+];
+
+function createAzureRegionGroups(): readonly RegionGroup[] {
+  return regions.map(({ name, regions }) => ({
+    name,
+    regions: regions
+      .filter(id => id in azureRegionMap)
+      .map(id => ({
+        id,
+        name: azureRegionMap[id],
+      })),
+  }));
+}
+
+export const azureRegionGroups = createAzureRegionGroups();
