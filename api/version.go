496f76bb48c585a38a37f4d9e707e2eae090848d
Zac Bergquist <zac.bergquist@goteleport.com>
2026-03-22 14:46:01 -0600

    Release 18.7.4-dev.fips.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.7.2"
+const Version = "18.7.4-dev.fips.1"
 
 const VersionMajor = 18
 const VersionMinor = 7
-const VersionPatch = 2
-const VersionPreRelease = ""
+const VersionPatch = 4
+const VersionPreRelease = "dev.fips.1"
 const VersionMetadata = ""
