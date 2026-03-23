633047e87a382b452912160cf00b62b7d1a22b16
Chris Thach <chris.thach@goteleport.com>
2026-03-23 17:54:55 -0400

    Set SSHClientVersion to SSH-2.0-Teleport_v1.
    Signed-off-by: Chris Thach <chris.thach@goteleport.com>

--- api/defaults/defaults.go
+++ api/defaults/defaults.go
@@ -29,9 +29,8 @@ const (
 	// Namespace is default namespace
 	Namespace = "default"
 
-	// SSHClientVersion is the SSH client identification string used by
-	// Teleport SSH clients.
-	SSHClientVersion = "SSH-2.0-Teleport mfav1"
+	// SSHClientVersion is the SSH client identification string used by Teleport SSH clients.
+	SSHClientVersion = "SSH-2.0-Teleport_v1"
 
 	// DefaultIOTimeout is a default network IO timeout.
 	DefaultIOTimeout = 30 * time.Second
