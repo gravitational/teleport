cc18063539ef13ca8760bf8d2df08c41e45c2892
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2026-03-23 12:46:10 +0100

    Remove inline forwarders unused by e

--- lib/utils/errors_inline.go
+++ lib/utils/errors_inline.go
@@ -5,24 +5,4 @@ import (
 )
 
 //go:fix inline
-func IsUseOfClosedNetworkError(err error) bool { return netutils.IsUseOfClosedNetworkError(err) }
-
-//go:fix inline
-func IsFailedToSendCloseNotifyError(err error) bool {
-	return netutils.IsFailedToSendCloseNotifyError(err)
-}
-
-//go:fix inline
 func IsOKNetworkError(err error) bool { return netutils.IsOKNetworkError(err) }
-
-//go:fix inline
-func IsConnectionRefused(err error) bool { return netutils.IsConnectionRefused(err) }
-
-//go:fix inline
-func IsUntrustedCertErr(err error) bool { return netutils.IsUntrustedCertErr(err) }
-
-//go:fix inline
-func CanExplainNetworkError(err error) (string, bool) { return netutils.CanExplainNetworkError(err) }
-
-//go:fix inline
-const SelfSignedCertsMsg = netutils.SelfSignedCertsMsg
