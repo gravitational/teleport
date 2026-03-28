6387627929404590607e9491f84a2ccf805e93fe
stevenGravy <steven@goteleport.com>
2026-03-27 21:26:00 -0400

    ui: fix tests

--- web/packages/teleport/src/Integrations/Enroll/AwsOidc/AwsOidc.test.tsx
+++ web/packages/teleport/src/Integrations/Enroll/AwsOidc/AwsOidc.test.tsx
@@ -95,7 +95,7 @@ test('generate command', async () => {
 
   fireEvent.click(screen.getByRole('button', { name: /Generate Command/i }));
 
-  const commandBoxEl = screen.getByText(/AWS CloudShell/i, { exact: false });
+  const commandBoxEl = screen.getAllByText(/AWS CloudShell/i, { exact: false })[0];
   await waitFor(() => {
     expect(commandBoxEl).toBeInTheDocument();
   });
