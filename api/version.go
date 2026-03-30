649839c230e6b82292fbffc585c50bef1b9ceea1
hugoShaka <hugo.hervieux@goteleport.com>
2026-03-30 09:00:39 -0400

    Release 19.0.0-hugoslack.13

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-prealpha.2"
+const Version = "19.0.0-hugoslack.13"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "prealpha.2"
+const VersionPreRelease = "hugoslack.13"
 const VersionMetadata = ""
