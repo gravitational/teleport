9fc924d159133fad902f34e07556d04e3014df3f
hugoShaka <hugo.hervieux@goteleport.com>
2026-03-30 08:34:29 -0400

    re-render after rebase

--- integrations/terraform/provider/resource_teleport_lock.go
+++ integrations/terraform/provider/resource_teleport_lock.go
@@ -23,12 +23,13 @@ import (
 
 	apitypes "github.com/gravitational/teleport/api/types"
 	
-	"github.com/gravitational/teleport/api/utils/retryutils"
+	"github.com/gravitational/teleport/integrations/lib/backoff"
 	"github.com/gravitational/trace"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/jonboulle/clockwork"
 
 	"github.com/gravitational/teleport/integrations/terraform/tfschema"
 )
@@ -109,24 +110,14 @@ func (r resourceTeleportLock) Create(ctx context.Context, req tfsdk.CreateResour
 	var lockI apitypes.Lock
 	// Try getting the resource until it exists.
 	tries := 0
-	retry, err := retryutils.NewRetryV2(retryutils.RetryV2Config{
-		Driver: retryutils.NewExponentialDriver(r.p.RetryConfig.Base),
-		First:  r.p.RetryConfig.Base,
-		Max:    r.p.RetryConfig.Cap,
-		Jitter: retryutils.HalfJitter,
-	})
-	if err != nil {
-		return
-	}
+	backoff := backoff.NewDecorr(r.p.RetryConfig.Base, r.p.RetryConfig.Cap, clockwork.NewRealClock())
 	for {
 		tries = tries + 1
 		lockI, err = r.p.Client.GetLock(ctx, id)
 		if trace.IsNotFound(err) {
-		    select {
-			case <-ctx.Done():
-			    resp.Diagnostics.Append(diagFromWrappedErr("Error reading Lock", trace.Wrap(ctx.Err()), "lock"))
+			if bErr := backoff.Do(ctx); bErr != nil {
+				resp.Diagnostics.Append(diagFromWrappedErr("Error reading Lock", trace.Wrap(err), "lock"))
 				return
-			case <-retry.After():
 			}
 			if tries >= r.p.RetryConfig.MaxTries {
 				diagMessage := fmt.Sprintf("Error reading Lock (tried %d times) - state outdated, please import resource", tries)
@@ -251,15 +242,7 @@ func (r resourceTeleportLock) Update(ctx context.Context, req tfsdk.UpdateResour
 	var lockI apitypes.Lock
 
 	tries := 0
-	retry, err := retryutils.NewRetryV2(retryutils.RetryV2Config{
-		Driver: retryutils.NewExponentialDriver(r.p.RetryConfig.Base),
-		First:  r.p.RetryConfig.Base,
-		Max:    r.p.RetryConfig.Cap,
-		Jitter: retryutils.HalfJitter,
-	})
-	if err != nil {
-		return
-	}
+	backoff := backoff.NewDecorr(r.p.RetryConfig.Base, r.p.RetryConfig.Cap, clockwork.NewRealClock())
 	for {
 		tries = tries + 1
 		lockI, err = r.p.Client.GetLock(ctx, name)
@@ -271,11 +254,9 @@ func (r resourceTeleportLock) Update(ctx context.Context, req tfsdk.UpdateResour
 			break
 		}
 
-		select {
-		case <-ctx.Done():
-		    resp.Diagnostics.Append(diagFromWrappedErr("Error reading Lock", trace.Wrap(ctx.Err()), "lock"))
+		if err := backoff.Do(ctx); err != nil {
+			resp.Diagnostics.Append(diagFromWrappedErr("Error reading Lock", trace.Wrap(err), "lock"))
 			return
-		case <-retry.After():
 		}
 		if tries >= r.p.RetryConfig.MaxTries {
 			diagMessage := fmt.Sprintf("Error reading Lock (tried %d times) - state outdated, please import resource", tries)
