7bcec510d42793c9ac4399b0583405b404cf3b4d
Julia Ogris <julia.ogris@gmail.com>
2026-04-02 09:23:55 +1100

    [v18] limiter: Fix custom rate bucket clobbering (#65208)

--- lib/auth/grpcserver_test.go
+++ lib/auth/grpcserver_test.go
@@ -4760,6 +4760,12 @@ func TestCustomRateLimiting(t *testing.T) {
 			},
 		},
 		{
+			name: "RPC ChangePassword",
+			fn: func(ctx context.Context, clt *authclient.Client) error {
+				return clt.ChangePassword(ctx, &proto.ChangePasswordRequest{})
+			},
+		},
+		{
 			name: "RPC GetAccountRecoveryToken",
 			fn: func(ctx context.Context, clt *authclient.Client) error {
 				_, err := clt.GetAccountRecoveryToken(ctx, &proto.GetAccountRecoveryTokenRequest{})
