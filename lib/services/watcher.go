d57b4d10d12faaf9b3aec9816f73d1958a3eb9df
Pawel Kopiczko <pawel.kopiczko@goteleport.com>
2026-04-01 10:59:49 +0100

    Fix other watcher races

--- lib/services/watcher.go
+++ lib/services/watcher.go
@@ -1163,7 +1163,7 @@ func (p *lockCollector) getResourcesAndUpdateCurrent(ctx context.Context) error 
 	p.isStale = false
 	p.defineCollectorAsInitialized()
 	for _, lock := range p.current {
-		p.fanout.Emit(types.Event{Type: types.OpPut, Resource: lock})
+		p.fanout.Emit(types.Event{Type: types.OpPut, Resource: lock.Clone()})
 	}
 	return nil
 }
@@ -1624,7 +1624,7 @@ func (p *accessRequestCollector) getResourcesAndUpdateCurrent(ctx context.Contex
 	}
 	newCurrent := make(map[string]types.AccessRequest, len(accessRequests))
 	for _, accessRequest := range accessRequests {
-		newCurrent[accessRequest.GetName()] = accessRequest
+		newCurrent[accessRequest.GetName()] = accessRequest.Copy()
 	}
 	p.lock.Lock()
 	defer p.lock.Unlock()
