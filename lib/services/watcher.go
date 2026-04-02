a68fab0f260b96dd52ba0570f1b9628a109a7f10
Pawel Kopiczko <pawel.kopiczko@goteleport.com>
2026-04-02 21:52:35 +0100

    [v18] Fix OktaAssignment and AccessRequest watcher write race (#65259)
    * Fix OktaAssignment watcher write race
    
    * Fix other watcher races
    
    * Add test for AccessRequestWatcher race
    
    * Add missing copy to caCollector.processEventsAndUpdateCurrent
    
    * Revert lock and ca clones - I can't prove a race
    
    * Remove ineffectual assignment

--- lib/services/watcher.go
+++ lib/services/watcher.go
@@ -1624,7 +1624,7 @@ func (p *accessRequestCollector) getResourcesAndUpdateCurrent(ctx context.Contex
 	}
 	newCurrent := make(map[string]types.AccessRequest, len(accessRequests))
 	for _, accessRequest := range accessRequests {
-		newCurrent[accessRequest.GetName()] = accessRequest
+		newCurrent[accessRequest.GetName()] = accessRequest.Copy()
 	}
 	p.lock.Lock()
 	defer p.lock.Unlock()
@@ -1800,7 +1800,7 @@ func (c *oktaAssignmentCollector) getResourcesAndUpdateCurrent(ctx context.Conte
 
 	newCurrent := make(map[string]types.OktaAssignment, len(oktaAssignments))
 	for _, oktaAssignment := range oktaAssignments {
-		newCurrent[oktaAssignment.GetName()] = oktaAssignment
+		newCurrent[oktaAssignment.GetName()] = oktaAssignment.Copy()
 	}
 	c.mu.Lock()
 	c.current = newCurrent
