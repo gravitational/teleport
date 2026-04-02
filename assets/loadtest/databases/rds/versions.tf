ccb59f352f336be0d42f5a011cbf23be424c3dc3
Adam Pickering <adam.pickering@goteleport.com>
2026-04-02 12:23:34 -0600

    Make fixes to database load testing scripts

--- assets/loadtest/databases/rds/versions.tf
+++ assets/loadtest/databases/rds/versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 1.5.5"
+  required_version = ">= 1.5.5"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.12"
+      version = ">= 5.12"
     }
   }
 }
