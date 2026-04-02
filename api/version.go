73ddfc0f2a7b705eb7413bc810672b05e1803a89
Charles Bryan <charles.bryan@goteleport.com>
2026-04-01 20:09:28 -0700

    Release v18.7.4-dev.disceks.2

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.7.4-dev.disceks.1"
+const Version = "18.7.4-dev.disceks.2"
 
 const VersionMajor = 18
 const VersionMinor = 7
 const VersionPatch = 4
-const VersionPreRelease = "dev.disceks.1"
+const VersionPreRelease = "dev.disceks.2"
 const VersionMetadata = ""
