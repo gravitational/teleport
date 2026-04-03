fbe76f65b6a249500e3a05f1da9f6290769fc2ba
hugoShaka <hugo.hervieux@goteleport.com>
2026-04-03 09:10:01 -0400

    Extract Helm logic into lib

--- build.assets/tooling/lib/helm/template.go
+++ build.assets/tooling/lib/helm/template.go
@@ -1,3 +1,5 @@
+package helm
+
 /*
  * Teleport
  * Copyright (C) 2023  Gravitational, Inc.
@@ -15,8 +17,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-package main
 
 import (
 	"bytes"
