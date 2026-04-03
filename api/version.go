007de01512665ba753fcbd855c3ae20013e02cb9
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2026-04-03 17:04:08 +0200

    18.7.2-dev.electron-41-1-1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.7.2"
+const Version = "18.7.2-dev.electron-41-1-1"
 
 const VersionMajor = 18
 const VersionMinor = 7
 const VersionPatch = 2
-const VersionPreRelease = ""
+const VersionPreRelease = "dev.electron-41-1-1"
 const VersionMetadata = ""
