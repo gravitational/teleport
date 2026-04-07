13b30ebecd002ea5ace5bd497d6ee61fb2878b6f
Noah Stride <noah.stride@goteleport.com>
2026-04-07 09:02:52 +0100

    [v18] Fix double start check in bot.Bot (#65403)
    * Fix double start check in bot.Bot
    
    * simplify

---
+++ lib/tbot/bot/bot_test.go
@@ -0,0 +1,48 @@
+/*
+ * Teleport
+ * Copyright (C) 2026  Gravitational, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bot
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/gravitational/teleport/lib/tbot/bot/connection"
+)
+
+func TestBot_RejectsDoubleStart(t *testing.T) {
+	b, err := New(Config{
+		Connection: connection.Config{
+			Address:            "localhost:3025",
+			AddressKind:        connection.AddressKindProxy,
+			StaticProxyAddress: true,
+		},
+	})
+	require.NoError(t, err)
+
+	ctx := t.Context()
+
+	_ = b.OneShot(ctx)
+
+	err = b.OneShot(ctx)
+	require.ErrorContains(t, err, "already been started")
+
+	err = b.Run(ctx)
+	require.ErrorContains(t, err, "already been started")
+}
