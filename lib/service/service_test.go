7a26cbc3cd8d67b3b0dbc497b0831ba12975add0
Julia Ogris <julia.ogris@goteleport.com>
2026-04-07 13:03:26 +1000

    app: Cancel supervisor closeContext in test cleanup
    Call `supervisor.Wait()` in test cleanup to cancel `closeContext`
    and stop the `fanOut()` goroutine started by `NewSupervisor`.
    `signalExit()` alone only cancels `exitContext`, which is a sibling
    context, leaving `fanOut()` blocked on `closeContext.Done()`.

--- lib/service/service_test.go
+++ lib/service/service_test.go
@@ -2461,6 +2461,7 @@ func TestInitAppsEmptyConfig(t *testing.T) {
 	log := logtest.NewLogger()
 	supervisor, err := NewSupervisor("test-initApps", log, clock)
 	require.NoError(t, err)
+	t.Cleanup(func() { supervisor.Wait() })
 	process := &TeleportProcess{
 		Supervisor: supervisor,
 		Clock:      clock,
@@ -2485,7 +2486,7 @@ func TestInitKubernetesUnlicensed(t *testing.T) {
 	stor, err := storage.NewProcessStorage(context.Background(), dataDir)
 	require.NoError(t, err)
 	t.Cleanup(func() { stor.Close() })
-	t.Cleanup(func() { supervisor.signalExit() })
+	t.Cleanup(func() { supervisor.signalExit(); supervisor.Wait() })
 
 	process := &TeleportProcess{
 		Supervisor:    supervisor,
