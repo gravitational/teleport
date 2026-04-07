a8152192d82c8e8f82f427b9d9af143eba2b86b8
Dan Upton <daniel.upton@goteleport.com>
2026-04-07 15:52:04 +0100

    Wire up the delegation sessions service

--- lib/service/service.go
+++ lib/service/service.go
@@ -3094,6 +3094,7 @@ func (process *TeleportProcess) newAccessCacheForServices(cfg accesspoint.Config
 	cfg.AppSession = services.IdentityInternal
 	cfg.Applications = services.ApplicationsInternal
 	cfg.Beams = services.Beams
+	cfg.DelegationSessions = services.DelegationSessions
 	cfg.ClusterConfig = services.ClusterConfigurationInternal
 	cfg.StaticScopedToken = services.ClusterConfigurationInternal
 	cfg.CrownJewels = services.CrownJewels
