37c848f0c2587f7f3f042f8a7ca2b04dfb8823b4
williamong-tel <william.ong@goteleport.com>
2026-04-06 22:51:25 -0700

    add scoped role assignments to terraform
    fix duplicate
    
    Fix tests and permissions
    
    fix test
    
    Fix template

---
+++ integrations/terraform/testlib/fixtures/scoped_role_assignment_0_create.tf
@@ -0,0 +1,15 @@
+resource "teleport_scoped_role_assignment" "test" {
+  version = "v1"
+  metadata = {
+    name = "dc6961fa-cbfb-4024-83f1-f6168310638b"
+  }
+  scope    = "/staging"
+  sub_kind = "dynamic"
+  spec = {
+    user = "testuser"
+    assignments = [{
+      role  = "test-scoped-role"
+      scope = "/staging/aa"
+    }]
+  }
+}
