474bb451b3621c03deb48b13abfa6286b9a8bd46
Alex Hemard <alex.hemard@goteleport.com>
2026-04-09 11:06:54 -0500

    [terraform] update azure discovery module matcher to match aws

--- integrations/terraform-modules/teleport/discovery/azure/azure_role_definition.tf
+++ integrations/terraform-modules/teleport/discovery/azure/azure_role_definition.tf
@@ -3,43 +3,49 @@
 ################################################################################
 
 locals {
-  azure_role_assignable_scopes = coalescelist(
-    var.azure_role_assignable_scopes,
-    [local.scope.subscription],
-  )
   azure_role_assignment_scopes = coalescelist(
     var.azure_role_assignment_scopes,
-    [local.scope.subscription],
+    [for sub in local.azure_matcher_subscriptions : "/subscriptions/${sub}"],
   )
+
+  uses_vm = contains(local.azure_matcher_types, "vm")
+
+  vm_actions = [
+    "Microsoft.Compute/virtualMachines/read",
+    "Microsoft.Compute/virtualMachines/runCommand/action",
+    "Microsoft.Compute/virtualMachines/runCommands/delete",
+    "Microsoft.Compute/virtualMachines/runCommands/read",
+    "Microsoft.Compute/virtualMachines/runCommands/write",
+  ]
+
+  role_actions = distinct(concat(
+    ["Microsoft.Resources/subscriptions/read"],
+    local.uses_vm ? local.vm_actions : [],
+  ))
 }
 
-# Custom role for discovery permissions
+# Custom role for discovery permissions for each scope.
 resource "azurerm_role_definition" "teleport_discovery" {
-  count = local.create ? 1 : 0
+  for_each = local.create ? toset(local.azure_role_assignment_scopes) : toset([])
 
-  assignable_scopes = local.azure_role_assignable_scopes
+  assignable_scopes = [each.value]
   description       = "Azure role that allows a Teleport Discovery Service to discover VMs."
-  name              = var.azure_role_definition_name
-  scope             = local.scope.subscription
+  # Split each scope by '/' and hyphenate the last two segments
+  # e.g. "subscriptions-{uuid}", "managementGroups-{name}".
+  name  = "${var.azure_role_definition_name}-${join("-", slice(split("/", each.value), length(split("/", each.value)) - 2, length(split("/", each.value))))}"
+  scope = each.value
 
   permissions {
-    actions = [
-      "Microsoft.Compute/virtualMachines/read",
-      "Microsoft.Compute/virtualMachines/runCommand/action",
-      "Microsoft.Compute/virtualMachines/runCommands/delete",
-      "Microsoft.Compute/virtualMachines/runCommands/read",
-      "Microsoft.Compute/virtualMachines/runCommands/write",
-      "Microsoft.Resources/subscriptions/read",
-    ]
+    actions     = local.role_actions
     not_actions = []
   }
 }
 
-# Assign the custom role to the managed identity principal
+# Assign the custom roles to the managed identity principal for each scope.
 resource "azurerm_role_assignment" "teleport_discovery" {
-  for_each = local.create ? toset(local.azure_role_assignment_scopes) : []
+  for_each = local.create ? toset(local.azure_role_assignment_scopes) : toset([])
 
   principal_id       = one(azurerm_user_assigned_identity.teleport_discovery_service[*].principal_id)
-  role_definition_id = one(azurerm_role_definition.teleport_discovery[*].role_definition_resource_id)
+  role_definition_id = azurerm_role_definition.teleport_discovery[each.key].role_definition_resource_id
   scope              = each.value
 }
