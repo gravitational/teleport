e06a512790aa590a87c97908842d61d4d7c9a2f1
STeve (Xin) Huang <xin.huang@goteleport.com>
2026-04-09 11:12:27 -0400

    [v17] pgx migration: use pgx v5 for postgres.MakeTestClient (#65533)
    * pgx migration: use pgx v5 for postgres.MakeTestClient (#65476)
    
    * pgx migration: use pgx v5 for postgres.MakeTestClient
    
    * update go.mod
    
    * add a ping to pgx pool
    
    * format files
    
    * fix leak on ping error

--- integration/db/db_integration_test.go
+++ integration/db/db_integration_test.go
@@ -26,7 +26,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jonboulle/clockwork"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
