929df4b50280e656a38fdd10f11b5f5d30d64d2c
Tim Buckley <tim@goteleport.com>
2026-04-08 18:54:45 -0600

    Use ScopedAuthorizer in join/Server.authenticate()
    `authenticate()` currently rejects scoped identities which prevented
    clients from reauthenticating with an existing identity, which is
    important for bot renewal and bound keypair joining.

--- lib/auth/grpcserver.go
+++ lib/auth/grpcserver.go
@@ -6400,7 +6400,7 @@ func NewGRPCServer(cfg GRPCServerConfig) (*GRPCServer, error) {
 
 	if !cfg.DisableJoinV1 {
 		joinv1.RegisterJoinServiceServer(server, join.NewServer(&join.ServerConfig{
-			Authorizer:         cfg.Authorizer,
+			ScopedAuthorizer:   cfg.ScopedAuthorizer,
 			AuthService:        cfg.AuthServer,
 			FIPS:               cfg.AuthServer.fips,
 			ScopedTokenService: cfg.AuthServer.Services,
