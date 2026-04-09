5391ae901460c919d1607876e9ba09355f1e830b
williamong-tel <william.ong@goteleport.com>
2026-04-09 14:17:08 -0700

    Update ssh_access_checker_test.go

--- lib/services/ssh_access_checker_test.go
+++ lib/services/ssh_access_checker_test.go
@@ -22,6 +22,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/gravitational/trace"
 	"github.com/stretchr/testify/require"
 
 	"github.com/gravitational/teleport/api/constants"
@@ -30,7 +31,6 @@ import (
 	scopedaccessv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/scopes/access/v1"
 	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/api/types/wrappers"
-	"github.com/gravitational/trace"
 )
 
 // newScopedCheckerWithRole is a test helper that builds a minimal ScopedAccessChecker
