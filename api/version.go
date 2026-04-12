388de0544cca526b33d7c6630dd55434c3201d3d
Gus Rivera <gus.rivera@goteleport.com>
2026-04-12 16:14:35 -0500

    Bump

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-dev.gus-idempotent-oci-test.4"
+const Version = "19.0.0-dev.gus-idempotent-oci-test.5"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev.gus-idempotent-oci-test.4"
+const VersionPreRelease = "dev.gus-idempotent-oci-test.5"
 const VersionMetadata = ""
