673498bee9037007e614f6fe30966a090766e7d9
Lisa Kim <lisa@goteleport.com>
2026-04-13 17:27:55 -0700

    tfgen: add support for depends_on meta argument (#65617)
    * tfgen: add support for depends_on meta argument
    
    * Support multiple WithDependsOn
    
    * Apply code review

--- lib/tfgen/tfgen_test.go
+++ lib/tfgen/tfgen_test.go
@@ -256,6 +256,38 @@ func TestGenerate_AccessListMember(t *testing.T) {
 	)
 }
 
+func TestGenerate_DependsOn_Single(t *testing.T) {
+	t.Parallel()
+
+	goldenTest(t, &types.RoleV6{
+		Kind:    types.KindRole,
+		Version: types.V7,
+		Metadata: types.Metadata{
+			Name: "example-role",
+		},
+		Spec: types.RoleSpecV6{Allow: types.RoleConditions{KubeGroups: []string{"system:masters"}}},
+	},
+		tfgen.WithDependsOn("teleport_role.some-role-id"),
+	)
+}
+
+func TestGenerate_DependsOn_Multiple(t *testing.T) {
+	t.Parallel()
+
+	goldenTest(t, &types.RoleV6{
+		Kind:    types.KindRole,
+		Version: types.V7,
+		Metadata: types.Metadata{
+			Name: "example-role",
+		},
+		Spec: types.RoleSpecV6{Allow: types.RoleConditions{KubeGroups: []string{"system:masters"}}},
+	},
+		tfgen.WithDependsOn("teleport_role.some-role-id-1"),
+		tfgen.WithDependsOn("teleport_role.some-role-id-2"),
+		tfgen.WithDependsOn("teleport_role.some-role-id-3"),
+	)
+}
+
 func TestGenerate_ResourceComment(t *testing.T) {
 	t.Parallel()
 
