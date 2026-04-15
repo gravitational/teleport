cee311da52ae9f0607be9334082b02e715e98c80
nibrasohin <nibrasul.ohin@goteleport.com>
2026-04-15 15:57:52 -0400

    fix: update resource name handling to include VNet DNS label for databases

--- lib/srv/discovery/common/renaming.go
+++ lib/srv/discovery/common/renaming.go
@@ -197,9 +197,14 @@ func applyDiscoveryNameSuffix(resource types.ResourceWithLabels, suffix string) 
 	}
 	// set the originally discovered name as a label.
 	labels[types.DiscoveredNameLabel] = discoveredName
-	resource.SetStaticLabels(labels)
 	// update the resource name with a suffix.
-	resource.SetName(fmt.Sprintf("%s-%s", discoveredName, suffix))
+	resourceNameWithSuffix := fmt.Sprintf("%s-%s", discoveredName, suffix)
+	resource.SetName(resourceNameWithSuffix)
+	// Recompute the VNet DNS label from the final name after the suffix is applied.
+	if _, ok := resource.(types.Database); ok {
+		labels[types.VNetDNSNameLabel] = types.VNetDNSName(resourceNameWithSuffix)
+	}
+	resource.SetStaticLabels(labels)
 }
 
 // suffixValidatorFn is a func that validates a suffix.
