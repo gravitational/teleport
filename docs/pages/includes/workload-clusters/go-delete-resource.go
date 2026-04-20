b50e6cd24c9155f879b7c09c39cf6864e98faff4
Dustin Specker <dustinspecker@dustinspecker.com>
2026-04-20 15:39:53 -0500

    fix typo
    Co-authored-by: Paul Gottschling <paul.gottschling@goteleport.com>

--- docs/pages/includes/workload-clusters/go-delete-resource.go
+++ docs/pages/includes/workload-clusters/go-delete-resource.go
@@ -2,7 +2,7 @@
 	* Delete a workload cluster *
 	****************************/
 
-	// Cleanup the previously created workload_cluster resource in the parent
+	// Clean up the previously created workload_cluster resource in the parent
 	// Teleport Cloud cluster.
 	if err := parentClient.DeleteWorkloadCluster(ctx, wc.Metadata.Name); err != nil {
 		log.Panicf("error deleting cloud cluster: %v", err)
