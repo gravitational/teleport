5bfaf9c737cddfe921d8e2f6598be986b2760d6a
Alex Hemard <alex.hemard@goteleport.com>
2026-04-20 22:24:37 -0500

    add provision token allow rules, provision token create var

--- integrations/terraform-modules/teleport/discovery/azure/teleport_provision_token.tf
+++ integrations/terraform-modules/teleport/discovery/azure/teleport_provision_token.tf
@@ -3,13 +3,20 @@
 ################################################################################
 
 locals {
+  create_teleport_provision_token = local.create && var.create_teleport_provision_token
+
   teleport_provision_token_name = (
-    var.teleport_provision_token_use_name_prefix
+    var.create_teleport_provision_token && var.teleport_provision_token_use_name_prefix
     ? "${var.teleport_provision_token_name}-${local.teleport_resource_name_suffix}"
     : var.teleport_provision_token_name
   )
 
-  token_allow_rules = flatten([
+  has_wildcard_subscription_matcher = anytrue([
+    for matcher in var.azure_matchers :
+    contains(matcher.subscriptions, "*")
+  ])
+
+  token_allow_rules_from_matchers = flatten([
     for matcher in var.azure_matchers : [
       for sub in matcher.subscriptions : merge(
         { subscription = sub },
@@ -17,11 +24,17 @@ locals {
       )
     ]
   ])
+
+  token_allow_rules = (
+    var.teleport_provision_token_allow_rules != null
+    ? var.teleport_provision_token_allow_rules
+    : local.token_allow_rules_from_matchers
+  )
 }
 
 # Teleport provision token for Azure join
 resource "teleport_provision_token" "azure" {
-  count = local.create ? 1 : 0
+  count = local.create_teleport_provision_token ? 1 : 0
 
   metadata = {
     description = "Allow Teleport nodes to join the cluster using Azure credentials."
@@ -36,4 +49,11 @@ resource "teleport_provision_token" "azure" {
     roles       = ["Node"]
   }
   version = "v2"
+
+  lifecycle {
+    precondition {
+      condition     = !(local.has_wildcard_subscription_matcher && var.teleport_provision_token_allow_rules == null)
+      error_message = "Wildcard (`*`) subscription discovery requires `teleport_provision_token_allow_rules` to be set."
+    }
+  }
 }
