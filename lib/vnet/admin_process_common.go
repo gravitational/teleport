8ab90bedea855474c9d6e3d5bfa06dc2ff06cd88
Dan Upton <daniel.upton@goteleport.com>
2026-04-21 14:34:10 +0100

    lib/vnet: add `EmbeddedVNet` type

--- lib/vnet/admin_process_common.go
+++ lib/vnet/admin_process_common.go
@@ -23,7 +23,7 @@ import (
 	"github.com/jonboulle/clockwork"
 )
 
-func newNetworkStackConfig(ctx context.Context, tun tunDevice, clt *clientApplicationServiceClient) (*networkStackConfig, error) {
+func newNetworkStackConfig(ctx context.Context, tun TUNDevice, clt *clientApplicationServiceClient) (*networkStackConfig, error) {
 	clock := clockwork.NewRealClock()
 	sshProvider, err := newSSHProvider(ctx, sshProviderConfig{
 		clt:   clt,
