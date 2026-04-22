617b9532bf8cf9e2ef4e120661c27b0846bdcbd5
williamong-tel <william.ong@goteleport.com>
2026-04-22 10:21:55 -0700

    Remove unnecessary string matching

--- integrations/operator/controllers/resources/scopedrole_controller.go
+++ integrations/operator/controllers/resources/scopedrole_controller.go
@@ -18,7 +18,6 @@ package resources
 
 import (
 	"context"
-	"strings"
 
 	"github.com/gravitational/trace"
 	kclient "sigs.k8s.io/controller-runtime/pkg/client"
@@ -47,9 +46,7 @@ func (s *scopedRoleClient) Delete(ctx context.Context, name string) error {
 	})
 	// The deletetion API for scoped roles returns CompareFailed if not found, which was an intentional decision
 	// We handle it here and return a not found error for consistency with the deletion reconcile.
-	if err != nil && trace.IsCompareFailed(err) &&
-		(strings.Contains(err.Error(), "not found") ||
-			strings.Contains(err.Error(), "concurrently modified and/or deleted")) {
+	if err != nil && trace.IsCompareFailed(err) {
 		return trace.NotFound("scoped role %q not found: %v", name, err)
 	}
 	return trace.Wrap(err)
