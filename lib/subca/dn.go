0b5a7704d71492185a9a4143c6dc5383a8138faa
Alan Parra <alan.parra@goteleport.com>
2026-04-22 11:09:58 -0300

    Mention asn1.parseBase128Int validation of OIDs

--- lib/subca/dn.go
+++ lib/subca/dn.go
@@ -43,6 +43,9 @@ func RDNSequenceToDistinguishedNameProto(rdns pkix.RDNSequence) (*subcav1.Distin
 		for j, atv := range nameSet {
 			oid := make([]int32, len(atv.Type))
 			for i, x := range atv.Type {
+				// OID components are guaranteed to fit in a 32 bit integer as checked
+				// by encoding/asn1.parseBase128Int.
+				// https://cs.opensource.google/go/go/+/refs/tags/go1.26.2:src/encoding/asn1/asn1.go;l=323
 				oid[i] = int32(x)
 			}
 			val, ok := atv.Value.(string)
