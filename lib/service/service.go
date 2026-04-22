e970f2ceaf5d5b53fedb765a8152d5216e91da88
Gabriel Corado <gabriel.oliveira@goteleport.com>
2026-04-22 11:33:21 -0300

    fix(service): missing app tls

--- lib/service/service.go
+++ lib/service/service.go
@@ -6827,6 +6827,7 @@ func (process *TeleportProcess) initApps() {
 				TCPPorts:              makeApplicationTCPPorts(app.TCPPorts),
 				MCP:                   app.MCP,
 				LLM:                   app.LLM,
+				TLS:                   app.TLS,
 			})
 			if err != nil {
 				return trace.Wrap(err)
