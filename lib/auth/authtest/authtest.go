139d7a96854b0246319e3eef68a3623154e7caa9
Erik Tate <erik.tate@goteleport.com>
2026-04-23 22:03:50 -0400

    remove kube resource kinds from default scoped kube system role (#65814)

--- lib/auth/authtest/authtest.go
+++ lib/auth/authtest/authtest.go
@@ -766,6 +766,7 @@ func generateCertificate(authServer *auth.Server, identity TestIdentity) ([]byte
 				PublicSSHKey: sshPublicKeyPEM,
 				SystemRoles:  id.AdditionalSystemRoles,
 			},
+			AgentScope: id.Identity.AgentScope,
 		})
 		if err != nil {
 			return nil, nil, trace.Wrap(err)
