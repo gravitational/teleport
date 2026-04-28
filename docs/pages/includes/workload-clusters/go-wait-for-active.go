5e48147e7c8392e4d778fa42743fba0a498bbb04
Dustin Specker <dustin.specker@goteleport.com>
2026-04-28 11:41:51 -0500

    docs(zta/wc): move wc Go to examples dir

--- docs/pages/includes/workload-clusters/go-wait-for-active.go
+++
@@ -1,7 +0,0 @@
-	// Wait for the created workload cluster to reach an active state.
-	timeoutctx, cancel := context.WithTimeout(ctx, 10*time.Minute)
-	defer cancel()
-	wc, err = waitForActiveWorkloadCluster(timeoutctx, parentClient, wc.Metadata.Name, 30*time.Second)
-	if err != nil {
-		log.Panicf("failed waiting for workload cluster to be active: %v", err)
-	}
