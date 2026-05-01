4c102fe8a92df0b7d3d6a69ee9a126388f64d7bd
Charles Bryan <charles.bryan@goteleport.com>
2026-05-01 14:38:43 -0700

    Release v18.8.0-dev.charles-eks.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.7.6"
+const Version = "18.8.0-dev.charles-eks.1"
 
 const VersionMajor = 18
-const VersionMinor = 7
-const VersionPatch = 6
-const VersionPreRelease = ""
+const VersionMinor = 8
+const VersionPatch = 0
+const VersionPreRelease = "dev.charles-eks.1"
 const VersionMetadata = ""
