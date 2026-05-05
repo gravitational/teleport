5b73e1c58a46bb28a08525aef57477c1ec36713b
Gabriel Corado <gabriel.oliveira@goteleport.com>
2026-05-05 12:50:04 -0300

    Add TLS options to app spec (#65995)
    * feat: add TLS options for app spec
    
    * fix(service): missing app tls
    
    * chore: missing generations
    
    * refactor(api): update
    
    * feat(api): add `tls://` scheme for TCP apps
    
    * refactor: move app tls validation to services
    
    * refactor(services): improve ca certificates validation
    
    * refactor(services): move app tls validation to happen before public_addr early return
    
    * refactor(services): remove time-based validations and allow single cert
    
    * test: improve testing around tls fields
    
    * refactor: code review suggestions
    
    * chore(api): regenerate proto
    
    * chore(docs): regenerate docs
    
    * refactor(api): always return a copy of the allowed ca aliases
    
    * chore(api): typo

--- lib/service/service.go
+++ lib/service/service.go
@@ -6757,6 +6757,7 @@ func (process *TeleportProcess) initApps() {
 				TCPPorts:              makeApplicationTCPPorts(app.TCPPorts),
 				MCP:                   app.MCP,
 				LLM:                   app.LLM,
+				TLS:                   app.TLS,
 			})
 			if err != nil {
 				return trace.Wrap(err)
