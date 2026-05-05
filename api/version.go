d80c80aa2d80fa7c02611a74fcfc5ef5493be956
Edoardo Spadolini <edoardo.spadolini@goteleport.com>
2026-05-05 17:11:58 +0200

    Release v18.8.0-dev.espadolini.20260505

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.7.6"
+const Version = "18.8.0-dev.espadolini.20260505"
 
 const VersionMajor = 18
-const VersionMinor = 7
-const VersionPatch = 6
-const VersionPreRelease = ""
+const VersionMinor = 8
+const VersionPatch = 0
+const VersionPreRelease = "dev.espadolini.20260505"
 const VersionMetadata = ""
