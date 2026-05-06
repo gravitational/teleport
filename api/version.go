8e812e02e5405a73a63ac394fa4740d28aac62d6
Gavin Frazar <gavin.frazar@goteleport.com>
2026-05-05 18:52:06 -0700

    Release v18.7.6-dev.gavin.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.7.6"
+const Version = "18.7.6-dev.gavin.1"
 
 const VersionMajor = 18
 const VersionMinor = 7
 const VersionPatch = 6
-const VersionPreRelease = ""
+const VersionPreRelease = "dev.gavin.1"
 const VersionMetadata = ""
