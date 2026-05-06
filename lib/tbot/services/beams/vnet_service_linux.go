f4e44457cf857a0a9c122c811aa9b4a1b4a4390a
Dan Upton <daniel.upton@goteleport.com>
2026-05-06 16:09:00 +0100

    Drop `github.com/vishvananda/netlink` dependency

--- lib/tbot/services/beams/vnet_service_linux.go
+++ lib/tbot/services/beams/vnet_service_linux.go
@@ -25,7 +25,8 @@ import (
 	"time"
 
 	"github.com/gravitational/trace"
-	"github.com/vishvananda/netlink"
+	"github.com/jsimonetti/rtnetlink/v2"
+	"golang.org/x/sys/unix"
 	"golang.zx2c4.com/wireguard/tun"
 
 	"github.com/gravitational/teleport/lib/vnet"
@@ -52,51 +53,118 @@ func platformUpstreamNameserverSource(logger *slog.Logger) (dns.UpstreamNameserv
 	return src, nil
 }
 
-func platformConfigureHost(_ context.Context, device vnet.TUNDevice, cfg *vnet.EmbeddedVNetHostConfig) error {
+func platformConfigureHost(ctx context.Context, device vnet.TUNDevice, cfg *vnet.EmbeddedVNetHostConfig) error {
 	deviceName, err := device.Name()
 	if err != nil {
 		return trace.Wrap(err, "getting device name")
 	}
-	link, err := netlink.LinkByName(deviceName)
+	iface, err := net.InterfaceByName(deviceName)
 	if err != nil {
-		return trace.Wrap(err, "resolving device link")
+		return trace.Wrap(err, "resolving TUN device interface")
 	}
 
-	// Add IPv4 address for the TUN device.
-	linkAddrV4 := &netlink.Addr{
-		IPNet: netlink.NewIPNet(net.ParseIP(cfg.DeviceIPv4)),
+	conn, err := rtnetlink.Dial(nil)
+	if err != nil {
+		return trace.Wrap(err, "dialing route netlink")
 	}
-	if err := netlink.AddrAdd(link, linkAddrV4); err != nil {
+	defer conn.Close()
+
+	if err := addInterfaceAddress(ctx, conn, iface.Index, cfg.DeviceIPv4); err != nil {
 		return trace.Wrap(err, "adding IPv4 address %s to TUN device", cfg.DeviceIPv4)
 	}
 
-	// Add IPv6 address for the TUN device.
-	linkAddrV6 := &netlink.Addr{
-		IPNet: netlink.NewIPNet(net.ParseIP(cfg.DeviceIPv6)),
-	}
-	if err := netlink.AddrAdd(link, linkAddrV6); err != nil {
+	if err := addInterfaceAddress(ctx, conn, iface.Index, cfg.DeviceIPv6); err != nil {
 		return trace.Wrap(err, "adding IPv6 address %s to TUN device", cfg.DeviceIPv6)
 	}
 
-	// Bring the TUN device up.
-	if err := netlink.LinkSetUp(link); err != nil {
+	if err := setInterfaceUp(ctx, conn, iface.Index); err != nil {
 		return trace.Wrap(err, "bringing TUN device up")
 	}
 
-	// Update the routing table.
-	linkIdx := link.Attrs().Index
 	for _, cidr := range cfg.CIDRRanges {
-		dst, err := netlink.ParseIPNet(cidr)
-		if err != nil {
-			return trace.Wrap(err, "parsing CIDR range %s", cidr)
-		}
-		route := &netlink.Route{
-			LinkIndex: linkIdx,
-			Dst:       dst,
-		}
-		if err := netlink.RouteReplace(route); err != nil {
+		if err := replaceRoute(ctx, conn, iface.Index, cidr); err != nil {
 			return trace.Wrap(err, "adding route from %s to TUN device", cidr)
 		}
 	}
 	return nil
+}
+
+func addInterfaceAddress(ctx context.Context, conn *rtnetlink.Conn, linkIndex int, address string) error {
+	family, addr, prefixLen, err := parseInterfaceAddress(address)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	return trace.Wrap(conn.Address.New(&rtnetlink.AddressMessage{
+		Family:       family,
+		PrefixLength: prefixLen,
+		Index:        uint32(linkIndex),
+		Attributes: &rtnetlink.AddressAttributes{
+			Address: addr,
+			Local:   addr,
+		},
+	}))
+}
+
+func setInterfaceUp(ctx context.Context, conn *rtnetlink.Conn, linkIndex int) error {
+	return trace.Wrap(conn.Link.Set(&rtnetlink.LinkMessage{
+		Family: unix.AF_UNSPEC,
+		Index:  uint32(linkIndex),
+		Flags:  unix.IFF_UP,
+		Change: unix.IFF_UP,
+	}))
+}
+
+func replaceRoute(ctx context.Context, conn *rtnetlink.Conn, linkIndex int, cidr string) error {
+	family, dst, prefixLen, err := parseRouteDestination(cidr)
+	if err != nil {
+		return trace.Wrap(err, "parsing CIDR range")
+	}
+	return trace.Wrap(conn.Route.Replace(&rtnetlink.RouteMessage{
+		Family:    family,
+		DstLength: prefixLen,
+		Table:     unix.RT_TABLE_MAIN,
+		Protocol:  unix.RTPROT_BOOT,
+		Scope:     unix.RT_SCOPE_UNIVERSE,
+		Type:      unix.RTN_UNICAST,
+		Attributes: rtnetlink.RouteAttributes{
+			Dst:      dst,
+			OutIface: uint32(linkIndex),
+		},
+	}))
+}
+
+func parseInterfaceAddress(address string) (family uint8, addr net.IP, prefixLen uint8, err error) {
+	ip := net.ParseIP(address)
+	if ip == nil {
+		return 0, nil, 0, trace.BadParameter("invalid IP address %q", address)
+	}
+	if ipv4 := ip.To4(); ipv4 != nil {
+		return unix.AF_INET, ipv4, 32, nil
+	}
+	return unix.AF_INET6, ip.To16(), 128, nil
+}
+
+func parseRouteDestination(cidr string) (family uint8, dst net.IP, prefixLen uint8, err error) {
+	_, ipNet, err := net.ParseCIDR(cidr)
+	if err != nil {
+		return 0, nil, 0, trace.Wrap(err)
+	}
+
+	ones, bits := ipNet.Mask.Size()
+	if ones < 0 {
+		return 0, nil, 0, trace.BadParameter("invalid CIDR mask %q", cidr)
+	}
+
+	if ipv4 := ipNet.IP.To4(); ipv4 != nil {
+		if bits != net.IPv4len*8 {
+			return 0, nil, 0, trace.BadParameter("invalid IPv4 CIDR mask size %d for %q", bits, cidr)
+		}
+		return unix.AF_INET, ipv4, uint8(ones), nil
+	}
+
+	ipv6 := ipNet.IP.To16()
+	if ipv6 == nil || bits != net.IPv6len*8 {
+		return 0, nil, 0, trace.BadParameter("invalid IPv6 CIDR %q", cidr)
+	}
+	return unix.AF_INET6, ipv6, uint8(ones), nil
 }
