2aac9ec8ff77dff106b00f82e8f7918991d00843
Yassine Bounekhla <56373201+rudream@users.noreply.github.com>
2026-05-07 16:45:13 -0400

    use exported upgrade window update logic when updating cluster maintenance config (#8715)
    Source-commit: gravitational/teleport.e@81158df2bcafd4120a0e32494312f495c78605da
    Source-pull-requests: gravitational/teleport.e#8715

--- e/lib/auth/auth_with_roles.go
+++ e/lib/auth/auth_with_roles.go
@@ -117,22 +117,7 @@ func (ac *cloudWithRoles) UpdateAccountUpgradeWindowStartHour(ctx context.Contex
 
 	// Update the cluster maintenance config resource in the backend so it reflects the new value immediately.
 	// This is an optimistic update and the value we set here shouldn't be fully trusted to be in sync with the current value in Cloud.
-	authServer := ac.plugin.authServer.AuthServer
-	cmc, err := authServer.GetClusterMaintenanceConfig(ctx)
-	if err != nil && !trace.IsNotFound(err) {
-		ac.plugin.logger.WarnContext(ctx, "failed to get cluster maintenance config", "error", err)
-		return resp, nil
-	}
-	if trace.IsNotFound(err) {
-		cmc = types.NewClusterMaintenanceConfig()
-	}
-
-	agentWindow, _ := cmc.GetAgentUpgradeWindow()
-	agentWindow.UTCStartHour = uint32(req.UpgradeWindowStartHour)
-	agentWindow.Weekdays = []string{"Mon", "Tue", "Wed", "Thu"}
-	cmc.SetAgentUpgradeWindow(agentWindow)
-
-	if err := authServer.UpdateClusterMaintenanceConfig(ctx, cmc); err != nil {
+	if err := ac.plugin.authServer.AuthServer.SetAgentUpgradeWindowStartHour(ctx, req.UpgradeWindowStartHour); err != nil {
 		ac.plugin.logger.WarnContext(ctx, "failed to update cluster maintenance config", "error", err)
 	}
 
