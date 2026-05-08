169594a3838783e73e085e5266f894655f94645e
Zac Bergquist <zac.bergquist@goteleport.com>
2026-05-08 09:26:49 -0600

    [v18] app access: reject dynamic labels for beams (#66560)
    * app access: reject dynamic labels for beams
    
    When app_service runs in Teleport-hosted infrastructure (like it
    does for beams), reject any dynamically registered applications
    that use dynamic 'command' labels.
    
    * Move env var check to higher level

--- lib/srv/app/watcher.go
+++ lib/srv/app/watcher.go
@@ -187,5 +187,21 @@ func (s *Server) onDelete(ctx context.Context, app types.Application) error {
 }
 
 func (s *Server) matcher(app types.Application) bool {
-	return services.MatchResourceLabels(s.c.ResourceMatchers, app.GetAllLabels())
+	matchesLabels := services.MatchResourceLabels(s.c.ResourceMatchers, app.GetAllLabels())
+	if !matchesLabels {
+		return false
+	}
+
+	if s.c.IgnoreAppsWithCommandLabels {
+		if len(app.GetDynamicLabels()) > 0 {
+			s.log.WarnContext(
+				context.Background(),
+				"refusing to register app with dynamic labels",
+				"app_name", app.GetName(),
+			)
+			return false
+		}
+	}
+
+	return true
 }
