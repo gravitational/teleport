5311cc5dd872fd0eb07e04f3909f9132dd04d068
Zac Bergquist <zac.bergquist@goteleport.com>
2026-05-08 08:57:36 -0600

    Move env var check to higher level

--- lib/srv/app/server.go
+++ lib/srv/app/server.go
@@ -85,7 +85,7 @@ type Config struct {
 	// ResourceMatchers is a list of app resource matchers.
 	ResourceMatchers []services.ResourceMatcher
 
-	// OnReconcile is called after each database resource reconciliation.
+	// OnReconcile is called after each app resource reconciliation.
 	OnReconcile func(types.Apps)
 
 	// ConnectedProxyGetter gets the proxies teleport is connected to.
@@ -96,6 +96,10 @@ type Config struct {
 
 	// InventoryHandle is used to send app server heartbeats via the inventory control stream.
 	InventoryHandle inventory.DownstreamHandle
+
+	// IgnoreAppsWithCommandLabels configures the app server to reject app resources
+	// with dynamic/command labels even if the app's labels otherwise match.
+	IgnoreAppsWithCommandLabels bool
 }
 
 // CheckAndSetDefaults makes sure the configuration has the minimum required
--- lib/srv/app/watcher.go
+++ lib/srv/app/watcher.go
@@ -21,13 +21,11 @@ package app
 import (
 	"context"
 	"fmt"
-	"os"
 
 	"github.com/gravitational/trace"
 
 	"github.com/gravitational/teleport"
 	"github.com/gravitational/teleport/api/types"
-	apiutils "github.com/gravitational/teleport/api/utils"
 	"github.com/gravitational/teleport/api/utils/clientutils"
 	"github.com/gravitational/teleport/lib/services"
 	"github.com/gravitational/teleport/lib/services/readonly"
@@ -188,20 +186,13 @@ func (s *Server) onDelete(ctx context.Context, app types.Application) error {
 	return s.unregisterAndRemoveApp(ctx, app.GetName())
 }
 
-const teleportBeamsEnvVar = "TELEPORT_BEAMS_RUNTIME"
-
 func (s *Server) matcher(app types.Application) bool {
 	matchesLabels := services.MatchResourceLabels(s.c.ResourceMatchers, app.GetAllLabels())
 	if !matchesLabels {
 		return false
 	}
 
-	// When the app_service itself is running on Teleport Cloud, then we reject any app
-	// with dynamic labels, which allow for code execution.
-	// This is primarily for the beams use case, where the only apps we expect
-	// to see are those created by `tsh beams publish`.
-	runningOnBeams, _ := apiutils.ParseBool(os.Getenv(teleportBeamsEnvVar))
-	if runningOnBeams {
+	if s.c.IgnoreAppsWithCommandLabels {
 		if len(app.GetDynamicLabels()) > 0 {
 			s.log.WarnContext(
 				context.Background(),
