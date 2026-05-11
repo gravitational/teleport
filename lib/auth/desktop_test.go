14b6e55d0e3b50b5f1719685b97c8fabbd0d632e
Alan Parra <alan.parra@goteleport.com>
2026-05-11 16:58:07 -0300

    Don't apply overrides on OSS builds

--- lib/auth/desktop_test.go
+++ lib/auth/desktop_test.go
@@ -31,6 +31,7 @@ import (
 	"github.com/gravitational/teleport/api/client/proto"
 	"github.com/gravitational/teleport/api/types"
 	"github.com/gravitational/teleport/api/utils/tlsutils"
+	"github.com/gravitational/teleport/entitlements"
 	"github.com/gravitational/teleport/lib/modules"
 	"github.com/gravitational/teleport/lib/modules/modulestest"
 	"github.com/gravitational/teleport/lib/subca/testenv"
@@ -62,7 +63,14 @@ func TestDesktopAccessDisabled(t *testing.T) {
 func TestDesktopAccessCAOverrides(t *testing.T) {
 	t.Parallel()
 
-	tlsServer := newTestTLSServer(t)
+	tlsServer := newTestTLSServer(t, withModules(&modulestest.Modules{
+		TestBuildType: modules.BuildEnterprise,
+		TestFeatures: modules.Features{
+			Entitlements: map[entitlements.EntitlementKind]modules.EntitlementInfo{
+				entitlements.Desktop: {Enabled: true, Limit: 0},
+			},
+		},
+	}))
 	authServer := tlsServer.Auth()
 	authServer.SetSubCAEnabled(true)
 
