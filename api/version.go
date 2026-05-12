bfe11151a755955040f4a1485be36653b129c768
Marco Dinis <marco.dinis@goteleport.com>
2026-05-12 13:22:07 +0100

    Release v18.8.0-dev.marco.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.8.0"
+const Version = "18.8.0-dev.marco.1"
 
 const VersionMajor = 18
 const VersionMinor = 8
 const VersionPatch = 0
-const VersionPreRelease = ""
+const VersionPreRelease = "dev.marco.1"
 const VersionMetadata = ""
