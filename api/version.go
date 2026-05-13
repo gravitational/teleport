4841aaa148f2e6a9748191f03187b85d22534f36
Angel Dionisio <angel.dionisio@goteleport.com>
2026-05-13 14:32:34 -0400

    Release v18.8.0-dev.angelgnu.4

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.8.0-dev.angelgnu.3"
+const Version = "18.8.0-dev.angelgnu.4"
 
 const VersionMajor = 18
 const VersionMinor = 8
 const VersionPatch = 0
-const VersionPreRelease = "dev.angelgnu.3"
+const VersionPreRelease = "dev.angelgnu.4"
 const VersionMetadata = ""
