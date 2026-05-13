32ffc14088a801e66ad1bf2e67e1f09c81aeee89
Noah Stride <noah.stride@goteleport.com>
2026-05-13 09:47:24 +0100

    Wildcard support for `kubernetes` join method (#66520)
    * Add protos
    
    * implement validation and matching support
    
    * generated integrations stuff
    
    * De Morgan, and his faithful followers, have once again defeated me in battle
    
    * Udpate tests in lib/kube/token
    
    * Extend join_kubernetes_test.go
    
    * Fix missing param
    
    * Extend validation tests
    
    * Fix minor nits
    
    * qF1001: CoUld aPpLY de MORgan'S LaW (stAticCHEck)

--- lib/services/provisioning.go
+++ lib/services/provisioning.go
@@ -136,15 +136,19 @@ func strongValidateProvisionTokenWithDefaults(token *types.ProvisionTokenV2) err
 	}
 
 	for i, rule := range kube.Allow {
+		serviceAccountSet := rule.ServiceAccount != ""
+
 		// validation for empty namespace and account was added much later than the rest of the validations
 		// in CheckAndSetDefaults(), so we only enforce them when marshaling a token rather than when unmarshaling
-		namespace, account, _ := strings.Cut(rule.ServiceAccount, ":")
-		if namespace == "" || account == "" {
-			return trace.BadParameter(
-				`allow[%d].service_account: name of service account should be in format "namespace:service_account", got %q instead`,
-				i,
-				rule.ServiceAccount,
-			)
+		if serviceAccountSet {
+			namespace, account, _ := strings.Cut(rule.ServiceAccount, ":")
+			if namespace == "" || account == "" {
+				return trace.BadParameter(
+					`allow[%d].service_account: name of service account should be in format "namespace:service_account", got %q instead`,
+					i,
+					rule.ServiceAccount,
+				)
+			}
 		}
 	}
 
