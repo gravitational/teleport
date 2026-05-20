bfa5c648699c5b41e09c236fd93bba3751c5ae11
Bartosz Leper <bartosz.leper@goteleport.com>
2026-05-20 10:07:35 +0200

    Fix lint

--- lib/auth/grpcserver.go
+++ lib/auth/grpcserver.go
@@ -6121,7 +6121,7 @@ func NewGRPCServer(cfg GRPCServerConfig) (*GRPCServer, error) {
 		Authorizer: cfg.Authorizer,
 		Cache:      cfg.AuthServer.Cache,
 		Backend:    cfg.AuthServer.Services,
-		Auth: cfg.AuthServer,
+		Auth:       cfg.AuthServer,
 		Emitter:    cfg.Emitter,
 		Reporter:   cfg.AuthServer.Services.UsageReporter,
 		Clock:      cfg.AuthServer.GetClock(),
