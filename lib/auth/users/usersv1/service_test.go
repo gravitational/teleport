3488c28cbba34481e724f2cdf3225f736a69ff2a
Bartosz Leper <bartosz.leper@goteleport.com>
2026-05-20 10:03:15 +0200

    Reset all passwords, just to be safe

--- lib/auth/users/usersv1/service_test.go
+++ lib/auth/users/usersv1/service_test.go
@@ -1264,7 +1264,7 @@ func getLastEvent(c <-chan apievents.AuditEvent) apievents.AuditEvent {
 	}
 }
 
-func TestResetPassword(t *testing.T) {
+func TestResetCredentials_ResetsPassword(t *testing.T) {
 	t.Parallel()
 
 	ctx := t.Context()
@@ -1282,7 +1282,7 @@ func TestResetPassword(t *testing.T) {
 	require.NoError(t, err)
 
 	// Reset password.
-	err = env.ResetPassword(ctx, "dave")
+	_, err = env.ResetCredentials(ctx, "dave")
 	require.NoError(t, err)
 
 	// Make sure that the password has been reset.
@@ -1293,6 +1293,6 @@ func TestResetPassword(t *testing.T) {
 
 	// Make sure that we can reset once again (i.e. we don't complain if there's
 	// no password).
-	err = env.ResetPassword(ctx, "dave")
+	_, err = env.ResetCredentials(ctx, "dave")
 	require.NoError(t, err)
 }
