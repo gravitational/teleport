31f1a6ccc386b2c590849a869237634217b9ef6f
Jake Alti <cenk.alti@goteleport.com>
2026-05-20 16:03:04 -0700

    kube: handle goaway test fixture conns concurrently

--- lib/kube/proxy/forwarder_test.go
+++ lib/kube/proxy/forwarder_test.go
@@ -2074,9 +2074,7 @@ func (g *goawayServer) Serve() error {
 			return err
 		}
 
-		if err := g.handleConn(conn); err != nil {
-			return err
-		}
+		go func() { _ = g.handleConn(conn) }()
 	}
 }
 
