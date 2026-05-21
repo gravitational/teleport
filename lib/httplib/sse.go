6f1e830b96ebc7ec4bc1524ea03c9f43150b7d62
Gabriel Corado <gabriel.oliveira@goteleport.com>
2026-05-21 17:37:46 -0300

    feat(httplib): read and write sse events helpers

---
+++ lib/httplib/sse.go
@@ -0,0 +1,339 @@
+// Teleport
+// Copyright (C) 2026 Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package httplib
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"io"
+	"iter"
+	"strings"
+
+	"github.com/gravitational/teleport"
+	"github.com/gravitational/trace"
+)
+
+var (
+	// ErrSSEEventTooLarge is the error returned when SSE event is larger than
+	// [MaxSSEEventSize].
+	ErrSSEEventTooLarge = errors.New("sse event exceeded max size")
+)
+
+// MaxSSEEventSize defines the max size of a SSE event.
+const MaxSSEEventSize = teleport.MaxHTTPResponseSize
+
+// SSEEvent is a server-sent event.
+type SSEEvent struct {
+	Event string
+	ID    string
+	Data  []byte
+	Retry string
+}
+
+// Empty reports whether the event is empty.
+func (e SSEEvent) Empty() bool {
+	return e.len() == 0
+}
+
+// Equal returns `true` if both events have the same contents.
+func (e SSEEvent) Equal(b SSEEvent) bool {
+	return e.Event == b.Event && e.ID == b.ID &&
+		bytes.Equal(e.Data, b.Data) && e.Retry == b.Retry
+}
+
+func (e SSEEvent) len() int {
+	return len(e.Event) + len(e.ID) + len(e.Data) + len(e.Retry)
+}
+
+// Validates the fields of the SSE event.
+func (e SSEEvent) validate() error {
+	switch {
+	case strings.ContainsAny(e.Event, "\r\n"):
+		return trace.BadParameter("Event field cannot contain line feed or carriage return")
+	case strings.ContainsAny(e.ID, "\r\n"):
+		return trace.BadParameter("ID field cannot contain line feed or carriage return")
+	case strings.ContainsAny(e.Retry, "\r\n"):
+		return trace.BadParameter("Retry field cannot contain line feed or carriage return")
+	case !onlyDigits(e.Retry):
+		return trace.BadParameter("Retry field can only contain digits")
+	}
+
+	return nil
+}
+
+// ReadSSEEvents reads SSE events from the provided reader.
+func ReadSSEEvents(r io.Reader) iter.Seq2[SSEEvent, error] {
+	return func(yield func(SSEEvent, error) bool) {
+		var currentEvent *SSEEvent
+		scanner := bufio.NewScanner(r)
+		scanner.Buffer(nil, MaxSSEEventSize+2)
+		scanner.Split(scanSSELines())
+
+		yieldEvent := func() bool {
+			// This handles cases where the stream has empty lines, so we
+			// consumed them, and there is no need to invoke `yield`.
+			if currentEvent == nil {
+				return true
+			}
+			if !yield(*currentEvent, nil) {
+				return false
+			}
+
+			// Reset for next event.
+			currentEvent = nil
+			return true
+		}
+
+		ensureRoom := func(addedSize int) bool {
+			if currentEvent == nil {
+				currentEvent = &SSEEvent{}
+			}
+			if currentEvent.len()+addedSize > MaxSSEEventSize {
+				return false
+			}
+			return true
+		}
+
+		for scanner.Scan() {
+			line := scanner.Bytes()
+
+			if len(line) == 0 {
+				if !yieldEvent() {
+					return
+				}
+				continue
+			}
+			if line[0] == ':' {
+				continue
+			}
+
+			// A field line without colon is considered valid with empty value.
+			//
+			// > Note: If a line doesn't contain a colon, the entire line is treated as the field name with an empty value string.
+			//
+			// From https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#fields
+			before, after, _ := bytes.Cut(line, []byte{':'})
+			// Per spec we must only trim the leading space from the field data.
+			// This avoids breaking the data sent by the server.
+			after = bytes.TrimPrefix(after, []byte{' '})
+			switch {
+			case bytes.Equal(before, sseEventKey):
+				if !ensureRoom(len(after)) {
+					yield(SSEEvent{}, trace.Wrap(ErrSSEEventTooLarge))
+					return
+				}
+				currentEvent.Event = string(after)
+			case bytes.Equal(before, sseIDKey):
+				if !ensureRoom(len(after)) {
+					yield(SSEEvent{}, trace.Wrap(ErrSSEEventTooLarge))
+					return
+				}
+				currentEvent.ID = string(after)
+			case bytes.Equal(before, sseRetryKey):
+				if !ensureRoom(len(after)) {
+					yield(SSEEvent{}, trace.Wrap(ErrSSEEventTooLarge))
+					return
+				}
+				currentEvent.Retry = string(after)
+			case bytes.Equal(before, sseDataKey):
+				// Account for the new line added after the `data` field.
+				if !ensureRoom(len(after) + 1) {
+					yield(SSEEvent{}, trace.Wrap(ErrSSEEventTooLarge))
+					return
+				}
+				// Cases where first data is empty, we cannot rely only on
+				// append as it would still return nil (so we wouldn't know a
+				// data field happened before). That's why we initialize the
+				// slice here.
+				if currentEvent.Data == nil {
+					currentEvent.Data = make([]byte, 0, len(after))
+				} else {
+					currentEvent.Data = append(currentEvent.Data, '\n')
+				}
+				currentEvent.Data = append(currentEvent.Data, after...)
+			default:
+				// Following the spec, unknown fields are just ignored.
+				continue
+			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			if errors.Is(err, bufio.ErrTooLong) {
+				yield(SSEEvent{}, trace.Wrap(ErrSSEEventTooLarge))
+				return
+			}
+
+			yield(SSEEvent{}, trace.Wrap(err))
+			return
+		}
+
+		yieldEvent()
+	}
+}
+
+// WriteSSEEvent writes an (non-empty) SSE event into the provided Writer.
+//
+// This function does not flush the writer. Callers using HTTP streaming should
+// flush their response after a successful write when events must be delivered
+// promptly.
+func WriteSSEEvent(w io.Writer, event SSEEvent) (int, error) {
+	if event.Empty() {
+		return 0, nil
+	}
+	if err := event.validate(); err != nil {
+		return 0, trace.Wrap(err)
+	}
+
+	// Use internal buffer so we only write once to the target writer.
+	var buf bytes.Buffer
+	if event.Event != "" {
+		writeField(&buf, sseEventKey, event.Event)
+	}
+	if event.ID != "" {
+		writeField(&buf, sseIDKey, event.ID)
+	}
+	for data := range splitSSEDataLines(event.Data) {
+		buf.Write(sseDataKey)
+		buf.WriteString(": ")
+		buf.Write(data)
+		buf.WriteByte('\n')
+	}
+	if event.Retry != "" {
+		writeField(&buf, sseRetryKey, event.Retry)
+	}
+	buf.WriteByte('\n')
+
+	n, err := buf.WriteTo(w)
+	return int(n), trace.Wrap(err)
+}
+
+// writeField internal helper that writes fields to buffer without allocating
+// additional strings.
+func writeField(b *bytes.Buffer, fieldName []byte, fieldData string) {
+	b.Write(fieldName)
+	b.WriteString(": ")
+	b.WriteString(fieldData)
+	b.WriteByte('\n')
+}
+
+// scanSSELines implements a custom scan line for bytes.Scanner that honors the
+// SSE spec in addition to the [MaxSSEEventSize].
+//
+// By the spec, the data can be split using \r (cr) \n (lf) following the end of
+// line definition:
+//
+//	end-of-line   = ( cr lf / cr / lf )
+//
+// https://html.spec.whatwg.org/multipage/server-sent-events.html#parsing-an-event-stream
+func scanSSELines() bufio.SplitFunc {
+	var skipLF bool
+
+	return func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		if skipLF {
+			if len(data) == 0 {
+				return 0, nil, nil
+			}
+			skipLF = false
+			if data[0] == '\n' {
+				return 1, nil, nil
+			}
+		}
+
+		for i, b := range data {
+			if i > MaxSSEEventSize {
+				return 0, nil, bufio.ErrTooLong
+			}
+
+			switch b {
+			case '\n':
+				return i + 1, data[:i], nil
+			case '\r':
+				// \r\n must count a single line break as per spec.
+				if i+1 < len(data) && data[i+1] == '\n' {
+					return i + 2, data[:i], nil
+				}
+				if i+1 == len(data) && !atEOF {
+					skipLF = true
+				}
+				return i + 1, data[:i], nil
+			}
+		}
+
+		if len(data) > MaxSSEEventSize {
+			return 0, nil, bufio.ErrTooLong
+		}
+
+		if atEOF && len(data) > 0 {
+			return len(data), data, nil
+		}
+
+		return 0, nil, nil
+	}
+}
+
+// splitSSEDataLines splits data into multiple event lines.
+//
+// By the spec, the data can be split using \r (cr) \n (lf) following the end of
+// line definition:
+//
+//	end-of-line   = ( cr lf / cr / lf )
+//
+// https://html.spec.whatwg.org/multipage/server-sent-events.html#parsing-an-event-stream
+func splitSSEDataLines(data []byte) iter.Seq[[]byte] {
+	return func(yield func([]byte) bool) {
+		// No data available, return nothing to the caller.
+		if len(data) == 0 {
+			return
+		}
+
+		for {
+			i := bytes.IndexAny(data, "\r\n")
+			if i < 0 {
+				yield(data)
+				return
+			}
+
+			if !yield(data[:i]) {
+				return
+			}
+
+			// \r\n must count a single line break as per spec.
+			if data[i] == '\r' && i+1 < len(data) && data[i+1] == '\n' {
+				data = data[i+2:]
+			} else {
+				data = data[i+1:]
+			}
+		}
+	}
+}
+
+func onlyDigits(str string) bool {
+	return strings.IndexFunc(str, func(r rune) bool {
+		return r < '0' || r > '9'
+	}) == -1
+}
+
+// SSE supported fields names.
+//
+// Ref: https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#fields.
+var (
+	sseEventKey = []byte("event")
+	sseIDKey    = []byte("id")
+	sseDataKey  = []byte("data")
+	sseRetryKey = []byte("retry")
+)
---
+++ lib/httplib/sse_test.go
@@ -0,0 +1,461 @@
+// Teleport
+// Copyright (C) 2026 Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package httplib
+
+import (
+	"bytes"
+	"io"
+	"strings"
+	"testing"
+	"testing/synctest"
+
+	"github.com/gravitational/teleport/lib/itertools/stream"
+	"github.com/gravitational/trace"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSSEEventsRead(t *testing.T) {
+	for name, tc := range map[string]struct {
+		input        string
+		expectError  require.ErrorAssertionFunc
+		expectEvents require.ValueAssertionFunc
+	}{
+		"data only": {
+			input:       dataOnlyExample,
+			expectError: require.NoError,
+			expectEvents: func(tt require.TestingT, i1 any, i2 ...any) {
+				require.Len(tt, i1, 2, i2...)
+				events := i1.([]SSEEvent)
+				requireEqualEvent(tt, SSEEvent{Data: []byte("some text")}, events[0])
+				requireEqualEvent(tt, SSEEvent{Data: []byte("another message\nwith two lines")}, events[1])
+			},
+		},
+		"named events": {
+			input:       namedEventsExample,
+			expectError: require.NoError,
+			expectEvents: func(tt require.TestingT, i1 any, i2 ...any) {
+				require.Len(tt, i1, 4, i2...)
+				events := i1.([]SSEEvent)
+				requireEqualEvent(tt, SSEEvent{Event: "userconnect", Data: []byte(`{"username": "bobby", "time": "02:33:48"}`)}, events[0])
+				requireEqualEvent(tt, SSEEvent{Event: "usermessage", Data: []byte(`{"username": "bobby", "time": "02:34:11", "text": "Hi everyone."}`)}, events[1])
+				requireEqualEvent(tt, SSEEvent{Event: "userdisconnect", Data: []byte(`{"username": "bobby", "time": "02:34:23"}`)}, events[2])
+				requireEqualEvent(tt, SSEEvent{Event: "usermessage", Data: []byte(`{"username": "sean", "time": "02:34:36", "text": "Bye, bobby."}`)}, events[3])
+			},
+		},
+		"mixed and matching events": {
+			input:       mixingAndMatchingExample,
+			expectError: require.NoError,
+			expectEvents: func(tt require.TestingT, i1 any, i2 ...any) {
+				require.Len(tt, i1, 3, i2...)
+				events := i1.([]SSEEvent)
+				requireEqualEvent(tt, SSEEvent{Event: "userconnect", Data: []byte(`{"username": "bobby", "time": "02:33:48"}`)}, events[0])
+				requireEqualEvent(tt, SSEEvent{Data: []byte("Here's a system message of some kind that will get used\nto accomplish some task.")}, events[1])
+				requireEqualEvent(tt, SSEEvent{Event: "usermessage", Data: []byte(`{"username": "bobby", "time": "02:34:11", "text": "Hi everyone."}`)}, events[2])
+			},
+		},
+		"field without colon": {
+			input:       "id\n\nevent\ndata: something\n\nevent: another event",
+			expectError: require.NoError,
+			expectEvents: func(tt require.TestingT, i1 any, i2 ...any) {
+				require.Len(tt, i1, 3, i2...)
+				events := i1.([]SSEEvent)
+				// ID event with no values.
+				requireEqualEvent(tt, SSEEvent{ID: ""}, events[0])
+				requireEqualEvent(tt, SSEEvent{Event: "", Data: []byte("something")}, events[1])
+				requireEqualEvent(tt, SSEEvent{Event: "another event"}, events[2])
+			},
+		},
+		"multiple data fields": {
+			input:       "event: hello\ndata: start of message\nid: 1\ndata: end of message\n",
+			expectError: require.NoError,
+			expectEvents: func(tt require.TestingT, i1 any, i2 ...any) {
+				require.Len(tt, i1, 1, i2...)
+				events := i1.([]SSEEvent)
+				requireEqualEvent(tt, SSEEvent{ID: "1", Event: "hello", Data: []byte("start of message\nend of message")}, events[0])
+			},
+		},
+		"carriage return line endings": {
+			input:       "event: hello\rdata: start of message\rdata: end of message\r",
+			expectError: require.NoError,
+			expectEvents: func(tt require.TestingT, i1 any, i2 ...any) {
+				require.Len(tt, i1, 1, i2...)
+				events := i1.([]SSEEvent)
+				requireEqualEvent(tt, SSEEvent{Event: "hello", Data: []byte("start of message\nend of message")}, events[0])
+			},
+		},
+		"carriage return and line feed line endings": {
+			input:       "event: hello\r\ndata: start of message\r\ndata: end of message\r\n",
+			expectError: require.NoError,
+			expectEvents: func(tt require.TestingT, i1 any, i2 ...any) {
+				require.Len(tt, i1, 1, i2...)
+				events := i1.([]SSEEvent)
+				requireEqualEvent(tt, SSEEvent{Event: "hello", Data: []byte("start of message\nend of message")}, events[0])
+			},
+		},
+		"preserve data whitespace": {
+			input:       "data:  token  \ndata:\ttab\t\n\n",
+			expectError: require.NoError,
+			expectEvents: func(tt require.TestingT, i1 any, i2 ...any) {
+				require.Len(tt, i1, 1, i2...)
+				events := i1.([]SSEEvent)
+				requireEqualEvent(tt, SSEEvent{Data: []byte(" token  \n\ttab\t")}, events[0])
+			},
+		},
+		"comment between fields": {
+			input:       "event: message\n: ping\ndata: body\n\n",
+			expectError: require.NoError,
+			expectEvents: func(tt require.TestingT, i1 any, i2 ...any) {
+				require.Len(tt, i1, 1, i2...)
+				events := i1.([]SSEEvent)
+				requireEqualEvent(tt, SSEEvent{Event: "message", Data: []byte("body")}, events[0])
+			},
+		},
+		"leading empty data line": {
+			input:       "data:\ndata: foo\n\n",
+			expectError: require.NoError,
+			expectEvents: func(tt require.TestingT, i1 any, i2 ...any) {
+				require.Len(tt, i1, 1, i2...)
+				events := i1.([]SSEEvent)
+				requireEqualEvent(tt, SSEEvent{Data: []byte("\nfoo")}, events[0])
+			},
+		},
+		"multiple empty data lines": {
+			input:       "data:\ndata:\ndata: foo\n\n",
+			expectError: require.NoError,
+			expectEvents: func(tt require.TestingT, i1 any, i2 ...any) {
+				require.Len(tt, i1, 1, i2...)
+				events := i1.([]SSEEvent)
+				requireEqualEvent(tt, SSEEvent{Data: []byte("\n\nfoo")}, events[0])
+			},
+		},
+		"single event data exceeds max size": {
+			input: "data: " + strings.Repeat("x", MaxSSEEventSize),
+			expectError: func(tt require.TestingT, err error, i ...any) {
+				require.ErrorIs(tt, err, ErrSSEEventTooLarge, i...)
+			},
+			expectEvents: require.Empty,
+		},
+		"single event field exceeds max size": {
+			input: "event: " + strings.Repeat("x", MaxSSEEventSize),
+			expectError: func(tt require.TestingT, err error, i ...any) {
+				require.ErrorIs(tt, err, ErrSSEEventTooLarge, i...)
+			},
+			expectEvents: require.Empty,
+		},
+		"single event exceeds max size multi lines": {
+			input: "data: " + strings.Repeat("x", MaxSSEEventSize/2) + "\ndata:" + strings.Repeat("y", MaxSSEEventSize/2),
+			expectError: func(tt require.TestingT, err error, i ...any) {
+				require.ErrorIs(tt, err, ErrSSEEventTooLarge, i...)
+			},
+			expectEvents: require.Empty,
+		},
+		"invalid event field is ignored": {
+			input:       "event: hello\nrandom: error\ndata: something",
+			expectError: require.NoError,
+			expectEvents: func(tt require.TestingT, i1 any, i2 ...any) {
+				require.Len(tt, i1, 1, i2...)
+				events := i1.([]SSEEvent)
+				requireEqualEvent(tt, SSEEvent{Event: "hello", Data: []byte("something")}, events[0])
+			},
+		},
+		"empty": {
+			input:        "",
+			expectError:  require.NoError,
+			expectEvents: require.Empty,
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			events, err := stream.Collect(ReadSSEEvents(strings.NewReader(tc.input)))
+			tc.expectError(t, err)
+			tc.expectEvents(t, events)
+		})
+	}
+}
+
+func TestSSEEventsReadBareCRLiveStream(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		chunks := make(chan []byte, 1)
+		reader := &chunkReader{chunks: chunks}
+
+		events := make(chan SSEEvent, 1)
+		errs := make(chan error, 1)
+		go func() {
+			for event, err := range ReadSSEEvents(reader) {
+				if err != nil {
+					errs <- err
+					return
+				}
+				events <- event
+				return
+			}
+		}()
+
+		chunks <- []byte("data: ok\r\r")
+		synctest.Wait()
+
+		select {
+		case event := <-events:
+			requireEqualEvent(t, SSEEvent{Data: []byte("ok")}, event)
+		case err := <-errs:
+			t.Fatalf("expected no errors but got %v", err)
+		}
+	})
+}
+
+func TestSSEEventsReadSplitCRLFLiveStream(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		chunks := make(chan []byte, 1)
+		reader := &chunkReader{chunks: chunks}
+
+		events := make(chan SSEEvent, 1)
+		errs := make(chan error, 1)
+		go func() {
+			for event, err := range ReadSSEEvents(reader) {
+				if err != nil {
+					errs <- err
+					return
+				}
+				events <- event
+				return
+			}
+		}()
+
+		chunks <- []byte("data: ok\r")
+		synctest.Wait()
+		requireNoSSEEvent(t, events, errs)
+
+		chunks <- []byte("\n")
+		synctest.Wait()
+		requireNoSSEEvent(t, events, errs)
+
+		chunks <- []byte("data: there\r\n\r\n")
+		synctest.Wait()
+
+		select {
+		case event := <-events:
+			requireEqualEvent(t, SSEEvent{Data: []byte("ok\nthere")}, event)
+		case err := <-errs:
+			t.Fatalf("expected no errors but got %v", err)
+		}
+	})
+}
+
+func TestSSEEventsWrite(t *testing.T) {
+	expectedString := func(str string) require.ValueAssertionFunc {
+		return func(tt require.TestingT, i1 any, i2 ...any) {
+			require.IsType(t, []byte{}, i1, "expected result to be `[]byte` but got %T", i1)
+			b := i1.([]byte)
+			require.Equal(tt, str, string(b), i2...)
+		}
+	}
+
+	for name, tc := range map[string]struct {
+		event        SSEEvent
+		expectError  require.ErrorAssertionFunc
+		expectOutput require.ValueAssertionFunc
+	}{
+		"no data event": {
+			event:        SSEEvent{ID: "1", Event: "hello"},
+			expectError:  require.NoError,
+			expectOutput: expectedString("event: hello\nid: 1\n\n"),
+		},
+		"data only": {
+			event:        SSEEvent{Data: []byte("some text")},
+			expectError:  require.NoError,
+			expectOutput: expectedString("data: some text\n\n"),
+		},
+		"multiline data": {
+			event:        SSEEvent{Data: []byte("another message\nwith two lines")},
+			expectError:  require.NoError,
+			expectOutput: expectedString("data: another message\ndata: with two lines\n\n"),
+		},
+		"multiline data escaped": {
+			event:        SSEEvent{Data: []byte("{\"response\": \"hello\\nworld\"}\n{\"response\": \"second\"}")},
+			expectError:  require.NoError,
+			expectOutput: expectedString("data: {\"response\": \"hello\\nworld\"}\ndata: {\"response\": \"second\"}\n\n"),
+		},
+		"carriage return data": {
+			event:        SSEEvent{Data: []byte("ok\revent: injected")},
+			expectError:  require.NoError,
+			expectOutput: expectedString("data: ok\ndata: event: injected\n\n"),
+		},
+		"carriage return newline data": {
+			event:        SSEEvent{Data: []byte("first\r\nsecond")},
+			expectError:  require.NoError,
+			expectOutput: expectedString("data: first\ndata: second\n\n"),
+		},
+		"named events": {
+			event:        SSEEvent{Event: "userconnect", Data: []byte(`{"username": "bobby", "time": "02:33:48"}`)},
+			expectError:  require.NoError,
+			expectOutput: expectedString("event: userconnect\ndata: {\"username\": \"bobby\", \"time\": \"02:33:48\"}\n\n"),
+		},
+		"all fields": {
+			event:        SSEEvent{ID: "1", Event: "hello", Data: []byte("start of message\nend of message"), Retry: "5"},
+			expectError:  require.NoError,
+			expectOutput: expectedString("event: hello\nid: 1\ndata: start of message\ndata: end of message\nretry: 5\n\n"),
+		},
+		"invalid fields": {
+			event: SSEEvent{ID: "1\n\r", Event: "hello", Data: []byte("start of message\nend of message"), Retry: "5"},
+			expectError: func(tt require.TestingT, err error, i ...any) {
+				require.True(tt, trace.IsBadParameter(err), "expected error %v to be BadParameter", err)
+			},
+			expectOutput: require.Empty,
+		},
+		"invalid retry field": {
+			event: SSEEvent{Event: "hello", Data: []byte("start of message\nend of message"), Retry: "non-digits"},
+			expectError: func(tt require.TestingT, err error, i ...any) {
+				require.True(tt, trace.IsBadParameter(err), "expected error %v to be BadParameter", err)
+			},
+			expectOutput: require.Empty,
+		},
+		"empty": {
+			event:        SSEEvent{},
+			expectError:  require.NoError,
+			expectOutput: require.Empty,
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			var buf bytes.Buffer
+			_, err := WriteSSEEvent(&buf, tc.event)
+			tc.expectError(t, err)
+			tc.expectOutput(t, buf.Bytes())
+		})
+	}
+}
+
+// TestSSEEventsWriteRead ensures both write and read SSE events functions
+// produce results that are compatible with each other.
+func TestSSEEventsWriteRead(t *testing.T) {
+	for name, input := range map[string]string{
+		// We use the version that doesn't include the comment as it is not read
+		// by the reader.
+		"data only":                 dataOnlyWithoutCommentExample,
+		"named events":              namedEventsExample,
+		"mixed and matching events": mixingAndMatchingExample,
+		"multiple data fields":      "event: hello\nid: 1\ndata: start of message\ndata: end of message\n\n",
+		"empty":                     "",
+	} {
+		t.Run(name, func(t *testing.T) {
+			inputEvents, err := stream.Collect(ReadSSEEvents(strings.NewReader(input)))
+			require.NoError(t, err)
+
+			var buf bytes.Buffer
+			for _, event := range inputEvents {
+				_, err := WriteSSEEvent(&buf, event)
+				require.NoError(t, err)
+			}
+
+			require.Equal(t, input, buf.String())
+		})
+	}
+}
+
+func FuzzReadSSEEvents(f *testing.F) {
+	f.Add("")
+	f.Add(dataOnlyExample)
+	f.Add(namedEventsExample)
+	f.Add(mixingAndMatchingExample)
+	f.Fuzz(func(t *testing.T, a string) {
+		reader := strings.NewReader(a)
+		require.NotPanics(t, func() {
+			for range ReadSSEEvents(reader) {
+			}
+		})
+	})
+}
+
+func requireEqualEvent(t require.TestingT, expected SSEEvent, target SSEEvent) {
+	require.Equal(t, expected.Event, target.Event)
+	require.Equal(t, expected.Data, target.Data)
+	require.Equal(t, expected.ID, target.ID)
+	require.Equal(t, expected.Retry, target.Retry)
+}
+
+func requireNoSSEEvent(t *testing.T, events <-chan SSEEvent, errs <-chan error) {
+	t.Helper()
+
+	select {
+	case event := <-events:
+		require.Failf(t, "unexpected SSE event", "event = %+v", event)
+	case err := <-errs:
+		t.Fatalf("expected no errors but got %v", err)
+	default:
+	}
+}
+
+type chunkReader struct {
+	chunks  <-chan []byte
+	pending []byte
+}
+
+func (r *chunkReader) Read(p []byte) (int, error) {
+	for len(r.pending) == 0 {
+		chunk, ok := <-r.chunks
+		if !ok {
+			return 0, io.EOF
+		}
+		r.pending = chunk
+	}
+
+	n := copy(p, r.pending)
+	r.pending = r.pending[n:]
+	return n, nil
+}
+
+// SSE stream examples from https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#examples
+//
+// Note all examples here contain two line breaks at the end to simulate the end
+// of message.
+const (
+	dataOnlyExample = `: this is a test stream
+
+data: some text
+
+data: another message
+data: with two lines
+
+`
+	namedEventsExample = `event: userconnect
+data: {"username": "bobby", "time": "02:33:48"}
+
+event: usermessage
+data: {"username": "bobby", "time": "02:34:11", "text": "Hi everyone."}
+
+event: userdisconnect
+data: {"username": "bobby", "time": "02:34:23"}
+
+event: usermessage
+data: {"username": "sean", "time": "02:34:36", "text": "Bye, bobby."}
+
+`
+	mixingAndMatchingExample = `event: userconnect
+data: {"username": "bobby", "time": "02:33:48"}
+
+data: Here's a system message of some kind that will get used
+data: to accomplish some task.
+
+event: usermessage
+data: {"username": "bobby", "time": "02:34:11", "text": "Hi everyone."}
+
+`
+	dataOnlyWithoutCommentExample = `data: some text
+
+data: another message
+data: with two lines
+
+`
+)
