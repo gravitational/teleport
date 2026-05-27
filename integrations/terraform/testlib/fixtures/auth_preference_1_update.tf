165b4966cddeab9ab4700281d9838cfc9b5a1677
Bernard Kim <bernard@goteleport.com>
2026-05-27 16:36:37 -0700

    Re-enable and update auth_preference test cases

--- integrations/terraform/testlib/fixtures/auth_preference_1_update.tf
+++ integrations/terraform/testlib/fixtures/auth_preference_1_update.tf
@@ -2,11 +2,12 @@ resource "teleport_auth_preference" "test" {
   version = "v2"
   metadata = {
     labels = {
+      "example"             = "yes"
       "teleport.dev/origin" = "dynamic"
     }
   }
 
   spec = {
-    disconnect_expired_cert = false
+    disconnect_expired_cert = true
   }
 }
