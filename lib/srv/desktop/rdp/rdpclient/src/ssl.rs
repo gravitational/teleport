5388c2813309df11291baa2bf0044cdeb13b11f3
Ryan Hammonds <ryan.hammonds@goteleport.com>
2026-05-29 12:51:58 -0400

    Make use of 'ironrdp_tls::extract_tls_server_public_key' for retrieving the server's public key from the TLS connection.

--- lib/srv/desktop/rdp/rdpclient/src/ssl.rs
+++ lib/srv/desktop/rdp/rdpclient/src/ssl.rs
@@ -65,20 +65,18 @@ pub(crate) async fn upgrade(
     ironrdp_tls::upgrade(initial_stream, server_name)
         .await
         .map_err(ClientError::from)
-        .map(|upgraded| {
+        .and_then(|upgraded| {
             // TODO (rhammonds): This is a temporary workaround to get our RDP client
             // working against the latest changes from IronRDP. We should consider changing
             // the return value of this function (and the calling code) to handle
             // a CertificateInner rather than just the raw public key.
-            (
+            Ok((
                 upgraded.0,
-                upgraded
-                    .1
-                    .tbs_certificate
-                    .subject_public_key_info
-                    .subject_public_key
-                    .raw_bytes()
+                ironrdp_tls::extract_tls_server_public_key(&upgraded.1)
+                    .ok_or(ClientError::InternalError(
+                        "failed to retrieve public key from TLS certificate".to_string(),
+                    ))?
                     .to_vec(),
-            )
+            ))
         })
 }
