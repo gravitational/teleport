5481586a57d5ba8a8673dce3ad1c5080dd0bf20f
Chris Thach <chris.thach@goteleport.com>
2026-05-29 19:43:16 -0400

    VNet SSH will dial direct with user cert before falling back to legacy per-session MFA cert (#66584)
    * Cherry-pick in-band MFA support changes for VNet that do no depend on x/crypto/ssh.
    
    Signed-off-by: Chris Thach <chris.thach@goteleport.com>
    
    * Use cached client.
    
    Signed-off-by: Chris Thach <chris.thach@goteleport.com>
    
    * Migrate to mfav2 after rebase.
    
    Signed-off-by: Chris Thach <chris.thach@goteleport.com>
    
    ---------
    
    Signed-off-by: Chris Thach <chris.thach@goteleport.com>

--- lib/client/cluster_client.go
+++ lib/client/cluster_client.go
@@ -35,6 +35,7 @@ import (
 	"github.com/gravitational/teleport/api/client/proto"
 	proxyclient "github.com/gravitational/teleport/api/client/proxy"
 	mfav1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/mfa/v1"
+	mfav2 "github.com/gravitational/teleport/api/gen/proto/go/teleport/mfa/v2"
 	"github.com/gravitational/teleport/api/mfa"
 	apissh "github.com/gravitational/teleport/api/ssh"
 	"github.com/gravitational/teleport/api/utils/keys"
@@ -101,6 +102,45 @@ func (c *ClusterClient) ConnectToCluster(ctx context.Context, clusterName string
 
 	authClient, err := authclient.NewClient(clientConfig)
 	return authClient, trace.Wrap(err)
+}
+
+// PerformSessionMFACeremony performs a session-bound MFA ceremony for a SSH session and returns the challenge name.
+func (c *ClusterClient) PerformSessionMFACeremony(ctx context.Context, sessionID []byte) (string, error) {
+	rootClient, err := c.ConnectToRootCluster(ctx)
+	if err != nil {
+		return "", trace.Wrap(err)
+	}
+	defer rootClient.Close()
+
+	mfaClient := rootClient.MFAServiceClientV2()
+	if mfaClient == nil {
+		return "", trace.BadParameter("MFA service client is not initialized (this is a bug)")
+	}
+
+	ceremony, err := mfa.NewSessionBoundCeremony(
+		mfa.SessionBoundCeremonyConfig{
+			CreateSessionChallenge:      mfaClient.CreateSessionChallenge,
+			ValidateSessionChallenge:    mfaClient.ValidateSessionChallenge,
+			PromptConstructor:           c.tc.NewMFAPrompt,
+			CallbackCeremonyConstructor: c.tc.NewRedirectorMFACeremony,
+			TargetCluster:               c.cluster,
+		},
+	)
+	if err != nil {
+		return "", trace.Wrap(err)
+	}
+
+	name, err := ceremony.Run(
+		ctx,
+		mfav2.SessionIdentifyingPayload_builder{
+			SshSessionId: sessionID,
+		}.Build(),
+	)
+	if err != nil {
+		return "", trace.Wrap(err)
+	}
+
+	return name, nil
 }
 
 // Close terminates the connections to Auth and Proxy.
