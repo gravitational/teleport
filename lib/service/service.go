c39b23106a94b10614900379a66ca12c2c84f88b
Erik Tate <erik.tate@goteleport.com>
2026-06-01 15:00:42 -0400

    passing AgentScope directly when starting kube service

--- lib/service/service.go
+++ lib/service/service.go
@@ -376,7 +376,7 @@ func newConnector(clientIdentity, serverIdentity *state.Identity) (*Connector, e
 	c := &Connector{
 		clusterName: clientIdentity.ClusterName,
 		hostID:      clientIdentity.ID.HostUUID,
-		scope:       clientIdentity.GetAgentScope(),
+		scope:       clientIdentity.AgentScope,
 		scopePin:    clientIdentity.ScopePin,
 		role:        clientIdentity.ID.Role,
 	}
