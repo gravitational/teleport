2e130a7ae6bbfda82b7bbe6a16be4fa365922ca5
Tiago Silva <tiago.silva@goteleport.com>
2026-06-03 11:12:04 +0100

    Move plugin CheckAndSetDefaults from marshal to write path
    CheckAndSetDefaults was being called inside MarshalPlugin as a side
    effect of serialization, which caused defaults (e.g. Namespace) to be
    applied to the in-memory object after roundtrip but not at construction
    time. Move the call to CreatePlugin and updateAndSwap where mutations
    belong, and call setStaticFields in NewPluginV1 so constructed objects
    are consistent from the start.
    
    Signed-off-by: Tiago Silva <tiago.silva@goteleport.com>

--- lib/services/local/plugins.go
+++ lib/services/local/plugins.go
@@ -49,6 +49,9 @@ func (s *PluginsService) CreatePlugin(ctx context.Context, plugin types.Plugin) 
 	if err := libplugin.Validate(plugin); err != nil {
 		return trace.Wrap(err)
 	}
+	if err := services.CheckAndSetDefaults(plugin); err != nil {
+		return trace.Wrap(err)
+	}
 	value, err := services.MarshalPlugin(plugin)
 	if err != nil {
 		return trace.Wrap(err)
@@ -246,6 +249,10 @@ func (s *PluginsService) updateAndSwap(ctx context.Context, name string, modify 
 
 	err = modify(newPlugin)
 	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	if err := services.CheckAndSetDefaults(newPlugin); err != nil {
 		return trace.Wrap(err)
 	}
 
