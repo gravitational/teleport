1493024657731f1c134f12c9cdd15f42649bc7fe
Bernard Kim <bernard@goteleport.com>
2026-06-04 11:28:53 -0700

    [terraform] Update auth_preference protoc-gen-terraform config (#67154)
    * Update protoc-gen-terraform-teleport.yaml
    
    The following attributes are marked as computed:
    - AuthPreferenceV2.Spec.IDP
    - AuthPreferenceV2.Spec.IDP.SAML
    - AuthPreferenceV2.Spec.Okta
    
    The following attributes are no longer marked as required:
    - AuthPreferenceV2.Metadata.Name

--- integrations/terraform/testlib/fixtures/auth_preference_0_set.tf
+++ integrations/terraform/testlib/fixtures/auth_preference_0_set.tf
@@ -1,13 +1,4 @@
 resource "teleport_auth_preference" "test" {
   version = "v2"
-  metadata = {
-    labels = {
-      "example"             = "yes"
-      "teleport.dev/origin" = "dynamic"
-    }
-  }
-
-  spec = {
-    disconnect_expired_cert = true
-  }
+  spec    = {}
 }
