38f8317090f1cba029079f181c10a87d9425011e
Chris Thach <chris.thach@goteleport.com>
2026-06-05 16:58:12 -0400

    Add MFA handling to WDS. Move auth check as early as possible to avoid expensive work.
    Signed-off-by: Chris Thach <chris.thach@goteleport.com>

--- lib/srv/desktop/mfa.go
+++ lib/srv/desktop/mfa.go
@@ -20,14 +20,20 @@ package desktop
 
 import (
 	"context"
+	"crypto/tls"
 
 	"github.com/gravitational/trace"
 	"google.golang.org/grpc"
 
 	tdpbv1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/desktop/v1"
 	mfav2 "github.com/gravitational/teleport/api/gen/proto/go/teleport/mfa/v2"
+	"github.com/gravitational/teleport/lib/srv/desktop/tdp"
+	"github.com/gravitational/teleport/lib/srv/desktop/tdp/protocol/tdpb"
 	"github.com/gravitational/teleport/lib/srv/mfa"
 )
+
+// ekmLabel is the label used for exporting keying material to derive the Session Identifying Payload.
+const ekmLabel = "EXPERIMENTAL-Teleport-MFA"
 
 // challengeVerifier verifies that a validated MFA challenge exists.
 type challengeVerifier interface {
@@ -87,4 +93,58 @@ func (pv *MFAPromptVerifier) VerifyResponse(ctx context.Context, resp *tdpbv1.MF
 	default:
 		return trace.BadParameter("missing or unknown MFAPromptResponse type: %T", r)
 	}
+}
+
+// ForceInBandMFAEnv is the environment variable that forces in-band MFA for all desktop connections.
+//
+// TODO(cthach): Move to common MFA package.
+const ForceInBandMFAEnv = "TELEPORT_UNSTABLE_FORCE_IN_BAND_MFA"
+
+// errInBandMFARequired is returned when in-band MFA is required but the client does not support it.
+var errInBandMFARequired = trace.AccessDenied(
+	"This connection requires in-band MFA, but your desktop client does not support it. " +
+		"Please update your Teleport desktop client to the latest version to connect.",
+)
+
+// HandleInBandMFA performs the in-band MFA exchange with the client. It computes the SIP from the TLS connection,
+// sends an MFA prompt, reads the client's response, and verifies the validated MFA challenge.
+func HandleInBandMFA(
+	ctx context.Context,
+	tlsConn *tls.Conn,
+	conn tdp.MessageReadWriteCloser,
+	cv challengeVerifier,
+	sourceCluster string,
+	username string,
+) error {
+	cs := tlsConn.ConnectionState()
+
+	sip, err := cs.ExportKeyingMaterial(ekmLabel, nil, 32)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	verifier, err := NewMFAPromptVerifier(cv, sourceCluster, username, sip)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	if err := conn.WriteMessage((*tdpb.AuthPrompt)(NewAuthPrompt())); err != nil {
+		return trace.Wrap(err)
+	}
+
+	msg, err := conn.ReadMessage()
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	resp, ok := msg.(*tdpb.MFAPromptResponse)
+	if !ok {
+		return trace.BadParameter("expected MFAPromptResponse, got %T", msg)
+	}
+
+	if err := verifier.VerifyResponse(ctx, (*tdpbv1.MFAPromptResponse)(resp)); err != nil {
+		return trace.Wrap(err)
+	}
+
+	return nil
 }
