a43fd71911b9d5479cf1a33d9e4a6f8d782cd6b9
Noah Stride <noah.stride@goteleport.com>
2026-06-11 13:44:50 +0100

    Migrate GetTunnelConnections/GetAllTunnelConnections to gRPC (#66066)
    * hack on migrating list RPC
    
    * Fix tests
    
    * Tidy up/fix tests
    
    * Use `clientutils.CollectWithFallback`
    
    * Use clearer c.APIClient rather than rely on embeding
    
    * Cover ListTunnelConnections in cache test
    
    * tweak logging
    
    * update deprecation version
    
    * Move fallback impls
    
    * fix span ctx prop
    
    * fix pagination bug when unmarshal bad
    
    * USe t.Context()
    
    * fix ctx propagation
    
    * Update tunn conns to use range pattern - thanks luke
    
    * fix unneeded dependency
    
    * fix deprecation
    
    * env: dev
    use opaque API

--- lib/auth/authclient/http_client.go
+++ lib/auth/authclient/http_client.go
@@ -315,51 +315,6 @@ func (c *HTTPClient) Delete(ctx context.Context, u string) (*roundtrip.Response,
 	return httplib.ConvertResponse(c.Client.Delete(ctx, u))
 }
 
-// GetTunnelConnections returns tunnel connections for a given cluster
-func (c *HTTPClient) GetTunnelConnections(clusterName string, opts ...services.MarshalOption) ([]types.TunnelConnection, error) {
-	if clusterName == "" {
-		return nil, trace.BadParameter("missing cluster name parameter")
-	}
-	out, err := c.Get(context.TODO(), c.Endpoint("tunnelconnections", clusterName), url.Values{})
-	if err != nil {
-		return nil, trace.Wrap(err)
-	}
-	var items []json.RawMessage
-	if err := json.Unmarshal(out.Bytes(), &items); err != nil {
-		return nil, trace.Wrap(err)
-	}
-	conns := make([]types.TunnelConnection, len(items))
-	for i, raw := range items {
-		conn, err := services.UnmarshalTunnelConnection(raw)
-		if err != nil {
-			return nil, trace.Wrap(err)
-		}
-		conns[i] = conn
-	}
-	return conns, nil
-}
-
-// GetAllTunnelConnections returns all tunnel connections
-func (c *HTTPClient) GetAllTunnelConnections(opts ...services.MarshalOption) ([]types.TunnelConnection, error) {
-	out, err := c.Get(context.TODO(), c.Endpoint("tunnelconnections"), url.Values{})
-	if err != nil {
-		return nil, trace.Wrap(err)
-	}
-	var items []json.RawMessage
-	if err := json.Unmarshal(out.Bytes(), &items); err != nil {
-		return nil, trace.Wrap(err)
-	}
-	conns := make([]types.TunnelConnection, len(items))
-	for i, raw := range items {
-		conn, err := services.UnmarshalTunnelConnection(raw)
-		if err != nil {
-			return nil, trace.Wrap(err)
-		}
-		conns[i] = conn
-	}
-	return conns, nil
-}
-
 type upsertServerRawReq struct {
 	Server json.RawMessage `json:"server"`
 	TTL    time.Duration   `json:"ttl"`
