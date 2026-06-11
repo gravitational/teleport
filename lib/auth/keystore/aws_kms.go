bd713a7b1878bdad6f98f133d7fbb1d757bf4229
Dustin Specker <dustin.specker@goteleport.com>
2026-06-11 10:07:15 -0500

    assume role with tags

--- lib/auth/keystore/aws_kms.go
+++ lib/auth/keystore/aws_kms.go
@@ -41,6 +41,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/resourcegroupstaggingapi"
 	rgttypes "github.com/aws/aws-sdk-go-v2/service/resourcegroupstaggingapi/types"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
+
+	ststypes "github.com/aws/aws-sdk-go-v2/service/sts/types"
 	"github.com/aws/smithy-go/tracing/smithyoteltracing"
 	"github.com/gravitational/trace"
 	"github.com/jonboulle/clockwork"
@@ -107,6 +109,13 @@ func newAWSKMSKeystore(ctx context.Context, cfg *servicecfg.AWSKMSConfig, opts *
 				cfg.RoleARN,
 				func(o *stscreds.AssumeRoleOptions) {
 					o.RoleSessionName = "teleport-aws-kms"
+					o.Tags = make([]ststypes.Tag, 0, len(cfg.Tags))
+					for key, value := range cfg.Tags {
+						o.Tags = append(o.Tags, ststypes.Tag{
+							Key:   aws.String(key),
+							Value: aws.String(value),
+						})
+					}
 					if cfg.ExternalID != "" {
 						o.ExternalID = aws.String(cfg.ExternalID)
 					}
