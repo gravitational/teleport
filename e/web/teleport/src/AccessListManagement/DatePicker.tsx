b8d2040fd75f3d3035f58d828c733bd1bc865617
Michael <michaelzmyers@gmail.com>
2026-06-15 05:27:03 -0500

    Update to react-day-picker v10 (#8882)
    Source-commit: gravitational/teleport.e@be023975c8e794e2314efdf74261a605ba145547
    Source-pull-requests: gravitational/teleport.e#8882

--- e/web/teleport/src/AccessListManagement/DatePicker.tsx
+++ e/web/teleport/src/AccessListManagement/DatePicker.tsx
@@ -1,5 +1,5 @@
 import { forwardRef } from 'react';
-import 'react-day-picker/dist/style.css';
+import 'react-day-picker/style.css';
 import { DayPicker } from 'react-day-picker';
 
 import { StyledDateRange } from 'design/DatePicker';
