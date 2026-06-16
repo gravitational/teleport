f973cae2d9d1534a9a98acac257f01b5bda1b93e
Gustavo Medori <mail@gustavo.sh>
2026-06-16 10:37:35 -0600

    Remove file creation header from VerifyApp.swift

--- mobile/Verify/Verify/VerifyApp.swift
+++ mobile/Verify/Verify/VerifyApp.swift
@@ -14,13 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-//
-//  VerifyApp.swift
-//  Verify
-//
-//  Created by Maja Cieślak on 2026-04-29.
-//
-
 import SwiftUI
 
 @main
