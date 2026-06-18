22ae8d9f417d6852232f399897316be142fe82cd
Bernard Kim <bernard@goteleport.com>
2026-06-17 17:06:00 -0700

    [terraform] Update cluster_networking_config protoc-gen-terraform config (#67216)

--- integrations/terraform/testlib/fixtures/networking_config_0_set.tf
+++ integrations/terraform/testlib/fixtures/networking_config_0_set.tf
@@ -1,13 +1,1 @@
-resource "teleport_cluster_networking_config" "test" {
-  version = "v2"
-  metadata = {
-    labels = {
-      "example"             = "yes"
-      "teleport.dev/origin" = "dynamic"
-    }
-  }
-
-  spec = {
-    client_idle_timeout = "30m"
-  }
-}
+resource "teleport_cluster_networking_config" "test" {}
