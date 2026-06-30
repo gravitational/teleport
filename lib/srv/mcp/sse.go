d4e192fd4003e64c8d320c7572b302c0196d5b07
Gabriel Corado <gabriel.oliveira@goteleport.com>
2026-06-30 11:52:18 -0300

    Implement app access managed client certificates (#67701)
    * feat(app): issue client certificates for downstream connections
    
    * chore(upstreamtls): code review suggestions
    
    * chore(upstreamtls): use opaque grpc api

--- lib/srv/mcp/sse.go
+++ lib/srv/mcp/sse.go
@@ -28,6 +28,7 @@ import (
 	"github.com/gravitational/trace"
 
 	"github.com/gravitational/teleport/api/types"
+	"github.com/gravitational/teleport/lib/authz"
 	"github.com/gravitational/teleport/lib/defaults"
 	"github.com/gravitational/teleport/lib/srv/app/upstreamtls"
 	logutils "github.com/gravitational/teleport/lib/utils/log"
@@ -145,11 +146,20 @@ func (s *Server) makeBasicHTTPTransport(ctx context.Context, app types.Applicati
 	// Note: For non-TLS apps (like `mcp+http`) this won't affect the
 	// connections as the transport won't make used of it.
 	tr.TLSClientConfig, err = upstreamtls.Configure(ctx, upstreamtls.Options{
-		Logger:       s.cfg.Log,
-		CAGetter:     s.cfg.AccessPoint,
-		ClusterName:  clusterName.GetClusterName(),
-		App:          app,
-		CipherSuites: s.cfg.CipherSuites,
+		Logger:                       s.cfg.Log,
+		AccessPoint:                  s.cfg.AccessPoint,
+		Clock:                        s.cfg.clock,
+		WorkloadIdentityClientGetter: s.cfg.AuthClient,
+		ClusterName:                  clusterName.GetClusterName(),
+		App:                          app,
+		CipherSuites:                 s.cfg.CipherSuites,
+		GetUserCertFunc: func() ([]byte, error) {
+			userCert, err := authz.UserCertificateFromContext(ctx)
+			if err != nil {
+				return nil, trace.Wrap(err)
+			}
+			return userCert.Raw, nil
+		},
 	})
 	if err != nil {
 		return nil, trace.Wrap(err)
