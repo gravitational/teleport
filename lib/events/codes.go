07ea24a4a06d1eb9a441d51cd3fe47b4a3c1c337
Alan Parra <alan.parra@goteleport.com>
2026-07-01 16:11:23 -0300

    Remove unused CertAuthorityOverride event codes

--- lib/events/codes.go
+++ lib/events/codes.go
@@ -945,15 +945,6 @@ const (
 	CertAuthOverrideUpsertCode = "TCO03I"
 	// CertAuthOverrideDeleteCode is the cert_auth_override delete event code.
 	CertAuthOverrideDeleteCode = "TCO04I"
-	// CertAuthOverrideCertificatesAddCode is the event code for specialized
-	// AddCertificateOverride operation.
-	CertAuthOverrideCertificatesAddCode = "TCO05I"
-	// CertAuthOverrideCertificatesUpdateCode is the event code for specialized
-	// UpdateCertificateOverride operation.
-	CertAuthOverrideCertificatesUpdateCode = "TCO06I"
-	// CertAuthOverrideCertificatesRemoveCode is the event code for specialized
-	// RemoveCertificateOverride operation.
-	CertAuthOverrideCertificatesRemoveCode = "TCO07I"
 
 	// BeamsConfigCreateCode is the Beams config create event code.
 	BeamsConfigCreateCode = "TBEAM001I"
