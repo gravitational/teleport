040a85405d0c5228562580c239a93db6c0706591
Roman Tkachenko <roman@goteleport.com>
2026-06-30 18:03:25 -0700

    Release 18.10.0-rc.1 (#68286)

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.9.2"
+const Version = "18.10.0-rc.1"
 
 const VersionMajor = 18
-const VersionMinor = 9
-const VersionPatch = 2
-const VersionPreRelease = ""
+const VersionMinor = 10
+const VersionPatch = 0
+const VersionPreRelease = "rc.1"
 const VersionMetadata = ""
