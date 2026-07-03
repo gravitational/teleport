ea2affed8a98943c1d79b7584ba03ecefde9ab52
Bernard Kim <bernard@goteleport.com>
2026-07-02 17:33:23 -0700

    Preserve unknown values during ModifyPlan

--- integrations/terraform/provider/resource_teleport_login_rule_plan_modifier.go
+++ integrations/terraform/provider/resource_teleport_login_rule_plan_modifier.go
@@ -51,7 +51,8 @@ func (r resourceTeleportLoginRule) ModifyPlan(ctx context.Context, req tfsdk.Mod
 		return
 	}
 
-	resp.Diagnostics.Append(schemav1.CopyLoginRuleToTerraform(ctx, loginRule, &config)...)
+	preserveUnknown := true
+	resp.Diagnostics.Append(schemav1.CopyLoginRuleToTerraformPreserveUnknown(ctx, loginRule, &config, preserveUnknown)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
