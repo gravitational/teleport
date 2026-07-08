ffa93df591770add460a5be3d91efb70d88159be
Gus Rivera <gus.rivera@goteleport.com>
2026-07-08 13:29:16 -0500

    Bump

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-dev.gus-design-system-fix.1"
+const Version = "19.0.0-dev.gus-design-system-fix.2"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev.gus-design-system-fix.1"
+const VersionPreRelease = "dev.gus-design-system-fix.2"
 const VersionMetadata = ""
