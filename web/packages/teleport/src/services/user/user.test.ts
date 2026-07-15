fefc2bd5d82ce5e8c5a899b07f15dc762e7d2854
Ryan Clark <ryan.clark@goteleport.com>
2026-07-15 13:18:27 +0200

    Add to more places

--- web/packages/teleport/src/services/user/user.test.ts
+++ web/packages/teleport/src/services/user/user.test.ts
@@ -390,6 +390,13 @@ test('undefined values in context response gives proper default values', async (
       create: false,
       remove: false,
     },
+    classifier: {
+      list: false,
+      read: false,
+      edit: false,
+      create: false,
+      remove: false,
+    },
     mobileDevice: {
       createEnrollToken: false,
     },
