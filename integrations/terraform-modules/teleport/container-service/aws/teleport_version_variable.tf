f90179a1ad472a7b33c8677e565b167603ca5e7a
Michael Myers <michael.myers@goteleport.com>
2026-07-15 11:21:40 -0500

    Release v19.0.0-dev.avatus-mcp.6

--- integrations/terraform-modules/teleport/container-service/aws/teleport_version_variable.tf
+++ integrations/terraform-modules/teleport/container-service/aws/teleport_version_variable.tf
@@ -1,5 +1,5 @@
 variable "teleport_version" {
-  default     = "19.0.0-dev.avatus-mcp.5"
+  default     = "19.0.0-dev.avatus-mcp.6"
   description = <<EOD
 The version of Teleport to deploy.
 Generally, the version of Teleport should be controlled by using the appropriate version of this module.
