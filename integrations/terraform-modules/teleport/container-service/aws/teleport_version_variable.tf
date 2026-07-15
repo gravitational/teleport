3f82e01ace86c61c584b00d19673588373ad696c
Gus Rivera <gus.rivera@goteleport.com>
2026-07-15 17:54:37 -0500

    bump

--- integrations/terraform-modules/teleport/container-service/aws/teleport_version_variable.tf
+++ integrations/terraform-modules/teleport/container-service/aws/teleport_version_variable.tf
@@ -1,5 +1,5 @@
 variable "teleport_version" {
-  default     = "19.0.0-prealpha.2"
+  default     = "19.0.0-dev.gus-test-all-workflow-dispatch.2"
   description = <<EOD
 The version of Teleport to deploy.
 Generally, the version of Teleport should be controlled by using the appropriate version of this module.
