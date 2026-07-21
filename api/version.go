6d62f1ae179dd7c66940077f714bd35c7bd43d10
Zac Bergquist <zac.bergquist@goteleport.com>
2026-07-21 08:48:45 -0600

    dev build 4

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-recordingexport.dev.3"
+const Version = "19.0.0-recordingexport.dev.4"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "recordingexport.dev.3"
+const VersionPreRelease = "recordingexport.dev.4"
 const VersionMetadata = ""
