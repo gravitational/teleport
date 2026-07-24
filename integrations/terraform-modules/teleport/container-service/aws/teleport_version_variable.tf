833366497442d9644c49807f8d20880e1233813d
Przemko Robakowski <przemko.robakowski@goteleport.com>
2026-07-24 02:56:34 +0200

    version

--- integrations/terraform-modules/teleport/container-service/aws/teleport_version_variable.tf
+++ integrations/terraform-modules/teleport/container-service/aws/teleport_version_variable.tf
@@ -1,5 +1,5 @@
 variable "teleport_version" {
-  default     = "19.0.0-dev.probakowski.webassets.2"
+  default     = "19.0.0-dev.probakowski.webassets.3"
   description = <<EOD
 The version of Teleport to deploy.
 Generally, the version of Teleport should be controlled by using the appropriate version of this module.
