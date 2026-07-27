29d476b468f1c2eecfe394981a0eb1017aa539ed
Pawel Kopiczko <pawel.kopiczko@goteleport.com>
2026-07-27 16:43:27 +0100

    Move to constants.go

--- lib/services/saml.go
+++ lib/services/saml.go
@@ -27,7 +27,6 @@ import (
 	"errors"
 	"log/slog"
 	"net/http"
-	"os"
 	"slices"
 	"strings"
 	"sync"
@@ -260,7 +259,7 @@ func getEntityDescriptor(ctx context.Context, params getEntityDescriptorParams) 
 	if url != "" && !params.Options.NoFollowURLs {
 		var checkRedirect func(req *http.Request, via []*http.Request) error
 		// TODO(kopiczko): Remove this env var after Jul 2027 (one year since introduced) if no issue is reported.
-		if disableCheckRedirect, _ := apiutils.ParseBool(os.Getenv("TELEPORT_UNSTABLE_DISABLE_SAML_REDIRECT_DOWNGRADE_CHECK")); disableCheckRedirect {
+		if disableCheckRedirect, _ := apiutils.ParseBool(teleport.EnvVarUnstableDisableSAMLRedirectDowngradeCheck); disableCheckRedirect {
 			log.DebugContext(ctx, "Redirect HTTPS downgrade check disabled with the unstable environment variable")
 		} else {
 			checkRedirect = func(req *http.Request, via []*http.Request) error {
