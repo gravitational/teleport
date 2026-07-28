27a39162c46b96dc945a551858a330aa1b716cf8
Krzysztof Skrzętnicki <krzysztof.skrzetnicki@goteleport.com>
2026-07-28 10:40:21 +0200

    Release v18.10.2-dev.pki-domain-db.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.10.0"
+const Version = "18.10.2-dev.pki-domain-db.1"
 
 const VersionMajor = 18
 const VersionMinor = 10
-const VersionPatch = 0
-const VersionPreRelease = ""
+const VersionPatch = 2
+const VersionPreRelease = "dev.pki-domain-db.1"
 const VersionMetadata = ""
