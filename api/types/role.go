27414d250f4354bee0a44c4f8c71fda06f5feafe
Przemko Robakowski <przemko.robakowski@goteleport.com>
2026-07-28 15:51:22 +0200

    Fix labels handling

--- api/types/role.go
+++ api/types/role.go
@@ -2346,6 +2346,10 @@ func (r *RoleV6) SetLabelMatchers(rct RoleConditionType, kind string, labelMatch
 		cond.DatabaseServiceLabels = labelMatchers.Labels
 		cond.DatabaseServiceLabelsExpression = labelMatchers.Expression
 		return nil
+	case KindLinuxDesktop:
+		cond.LinuxDesktopLabels = labelMatchers.Labels
+		cond.LinuxDesktopLabelsExpression = labelMatchers.Expression
+		return nil
 	case KindWindowsDesktop:
 		cond.WindowsDesktopLabels = labelMatchers.Labels
 		cond.WindowsDesktopLabelsExpression = labelMatchers.Expression
