e2d97d74671a0e2277811a55140000d6f7623056
Gavin Frazar <gavin.frazar@goteleport.com>
2026-07-29 11:05:23 -0700

    [v18] Teleport ECS Terraform module (#68686)
    Adds a Terraform module that deploys a Teleport instance to
    AWS ECS.

---
+++ integrations/terraform-modules/teleport/container-service/aws/teleport_version_variable.tf
@@ -0,0 +1,9 @@
+variable "teleport_version" {
+  default     = "18.10.0"
+  description = <<EOD
+The version of Teleport to deploy.
+Generally, the version of Teleport should be controlled by using the appropriate version of this module.
+This variable is intended for development usage.
+EOD
+  type        = string
+}
