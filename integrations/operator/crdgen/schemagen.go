90d10ba8d077d7a444c94b0a369d1cb221a34f35
Tim Buckley <tim@goteleport.com>
2026-07-31 22:30:07 -0600

    Fix google.protobuf.Struct handling in the Kubernetes operator
    This fixes a bug in the Kubernetes operator where nested fields in a
    `google.protobuf.Struct` object would be silently pruned by the
    validator. This matters to the `generic_oidc` join method where the
    `must_match_fields` field (which accepts structured fields to match
    against an incoming JWT) could get pruned and then silently ignored,
    bypassing join rules.
    
    Note that use of nested fields _usually_ results in an error, but only
    in strict mode. It's the default now, but could still plausibly be
    disabled causing silent loss of rules.

--- integrations/operator/crdgen/schemagen.go
+++ integrations/operator/crdgen/schemagen.go
@@ -524,9 +524,16 @@ func (generator *SchemaGenerator) singularProp(field *Field, prop *apiextv1.JSON
 		// JSON object. We can't know the structure ahead of time and there can
 		// be many levels of nesting within this.
 		prop.Type = "object"
-		prop.AdditionalProperties = &apiextv1.JSONSchemaPropsOrBool{
-			Allows: true,
-		}
+
+		// Structs have no defined schema and may be entirely user-defined /
+		// free-form, so set XPreserveUnknownFields to disable pruning for child
+		// fields to ensure Kubernetes doesn't prune them in the validator.
+		//
+		// Note that AdditionalProperties cannot be set: if it's non-nil, all
+		// child fields get pruned before the validator bothers to check
+		// XPreserveUnknownFields. In practice this means nested structs get
+		// pruned or rejected at runtime.
+		prop.XPreserveUnknownFields = new(true)
 	case field.IsMessage():
 		inner := field.TypeMessage()
 		if inner == nil {
