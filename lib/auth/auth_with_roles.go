c9f7370a8d4ff25c0c8b45477d89c79d1319eb27
Noah Stride <noah.stride@goteleport.com>
2026-08-03 21:37:31 +0100

    Extend WorkloadIdentity resource Watch/List to support scope filtering (#69132)
    * Extend WorkloadIdentity resource Watch/List to support scope filtering
    
    * Fix up codex's complaints
    
    * fix incorrect usage of t.Context()
    
    * Use correct t.Context90

--- lib/auth/auth_with_roles.go
+++ lib/auth/auth_with_roles.go
@@ -1307,7 +1307,8 @@ func supportedScopedWatchKind(kind string) bool {
 		types.KindAccessListReview,
 		scopedaccess.KindScopedRole,
 		scopedaccess.KindScopedRoleAssignment,
-		types.KindKubernetesCluster:
+		types.KindKubernetesCluster,
+		types.KindWorkloadIdentity:
 		return true
 	default:
 		return false
