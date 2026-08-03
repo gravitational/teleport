f853592d4fd1853e08f66f711f42b16c9c172366
Noah Stride <noah.stride@goteleport.com>
2026-08-03 13:10:11 +0000

    Avoid testify in authtest.go

--- lib/auth/authtest/authtest.go
+++ lib/auth/authtest/authtest.go
@@ -32,7 +32,6 @@ import (
 	"github.com/google/uuid"
 	"github.com/gravitational/trace"
 	"github.com/jonboulle/clockwork"
-	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
 	"golang.org/x/crypto/bcrypt"
 	"golang.org/x/crypto/ssh"
@@ -1182,7 +1181,9 @@ func TestBot(botName string, botInternal bool) TestIdentity {
 func TestScopedBot(t *testing.T, botName string, scope string, botInternal bool) TestIdentity {
 	// The cert username must match the User name the bot service persists.
 	userName, err := services.BotResourceName(scope, botName)
-	require.NoError(t, err)
+	if err != nil {
+		t.Fatalf("TestScopedBot: %s", err.Error())
+	}
 	return TestIdentity{
 		I: authz.LocalUser{
 			Username: userName,
