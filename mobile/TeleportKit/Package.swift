1a260aa2415be222096a69e578a656e68f41c186
Gustavo Medori <gustavo.medori@goteleport.com>
2026-08-04 13:54:08 -0600

    Add new SystemClients module to TeleportKit

--- mobile/TeleportKit/Package.swift
+++ mobile/TeleportKit/Package.swift
@@ -6,14 +6,24 @@ let package = Package(
     name: "TeleportKit",
     platforms: [.iOS(.v26)],
     products: [
-        .library(
-            name: "TeleportKit",
-            targets: ["TeleportKit"]
-        ),
+        .library(name: "TeleportKit", targets: ["TeleportKit"]),
+        .library(name: "SystemClients", targets: ["SystemClients"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", .upToNextMajor(from:  "1.14.0")),
+        .package(url: "https://github.com/pointfreeco/swift-sharing", .upToNextMajor(from:  "2.9.1")),
     ],
     targets: [
         .target(
             name: "TeleportKit",
+        ),
+        .target(
+            name: "SystemClients",
+            dependencies: [
+                .dependencies,
+                .dependenciesMacros,
+                .sharing,
+            ]
         ),
         .testTarget(
             name: "TeleportKitTests",
@@ -22,6 +32,16 @@ let package = Package(
     ],
     swiftLanguageModes: [.v6]
 )
+
+// MARK: - Custom Target.Dependency Values
+
+private extension Target.Dependency {
+    static let dependencies: Self = .product(name: "Dependencies", package: "swift-dependencies")
+    static let dependenciesMacros: Self = .product(name: "DependenciesMacros", package: "swift-dependencies")
+    static let sharing: Self = .product(name: "Sharing", package: "swift-sharing")
+}
+
+// MARK: - Build Settings
 
 // We enable these settings for all targets in this package so that the dialect of swift we're using is same across
 // all our code.
