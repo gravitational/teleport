b5ae2b2b780f20efe0b491b7cfae0590db5a346a
Gustavo Medori <gustavo.medori@goteleport.com>
2026-08-04 14:38:08 -0600

    Remove TeleportKit target

--- mobile/TeleportKit/Tests/TeleportKitTests/TeleportKitTests.swift
+++
@@ -1,8 +0,0 @@
-import Testing
-@testable import TeleportKit
-
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    // Swift Testing Documentation
-    // https://swiftpackageindex.com/swiftlang/swift-testing/documentation
-}
