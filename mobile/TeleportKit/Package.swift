e3966dc58f1320104621bc0bf2af4b9a199afbf8
Gustavo Medori <gustavo.medori@goteleport.com>
2026-08-04 13:25:03 -0600

    Update Package.swift to set swift settings all at once at the end

--- mobile/TeleportKit/Package.swift
+++ mobile/TeleportKit/Package.swift
@@ -2,16 +2,6 @@
 
 import PackageDescription
 
-// We enable these settings for all targets in this package so that the dialect of swift we're using is same across
-// all our code.
-let swiftSettings: [SwiftSetting] = [
-    .enableUpcomingFeature("InferIsolatedConformances"),
-    .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
-    .enableUpcomingFeature("ExistentialAny"),
-    .enableUpcomingFeature("InternalImportsByDefault"),
-    .enableUpcomingFeature("MemberImportVisibility"),
-]
-
 let package = Package(
     name: "TeleportKit",
     platforms: [.iOS(.v26)],
@@ -24,13 +14,27 @@ let package = Package(
     targets: [
         .target(
             name: "TeleportKit",
-            swiftSettings: swiftSettings,
         ),
         .testTarget(
             name: "TeleportKitTests",
             dependencies: ["TeleportKit"],
-            swiftSettings: swiftSettings,
         ),
     ],
     swiftLanguageModes: [.v6]
 )
+
+// We enable these settings for all targets in this package so that the dialect of swift we're using is same across
+// all our code.
+let swiftSettings: [SwiftSetting] = [
+    .enableUpcomingFeature("InferIsolatedConformances"),
+    .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
+    .enableUpcomingFeature("ExistentialAny"),
+    .enableUpcomingFeature("InternalImportsByDefault"),
+    .enableUpcomingFeature("MemberImportVisibility"),
+]
+
+for target in package.targets {
+	var settings = target.swiftSettings ?? []
+	settings.append(contentsOf: swiftSettings)
+	target.swiftSettings = settings
+}
