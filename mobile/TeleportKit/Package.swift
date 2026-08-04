c2a6373dee3094442c3165da589d1775043f94cc
Gustavo Medori <gustavo.medori@goteleport.com>
2026-08-04 16:07:58 -0600

    Add LogBackends module

--- mobile/TeleportKit/Package.swift
+++ mobile/TeleportKit/Package.swift
@@ -3,46 +3,66 @@
 import PackageDescription
 
 let package = Package(
-    name: "TeleportKit",
-    platforms: [.iOS(.v26)],
-    products: [
-        .library(name: "SystemClients", targets: ["SystemClients"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-dependencies", .upToNextMajor(from:  "1.14.0")),
-        .package(url: "https://github.com/pointfreeco/swift-sharing", .upToNextMajor(from:  "2.9.1")),
-    ],
-    targets: [
-        .target(
-            name: "SystemClients",
-            dependencies: [
-                .dependencies,
-                .dependenciesMacros,
-                .sharing,
-            ]
-        ),
-    ],
-    swiftLanguageModes: [.v6]
+	name: "TeleportKit",
+	platforms: [.iOS(.v26)],
+	products: [
+		.library(name: "SystemClients", targets: ["SystemClients"]),
+	],
+	dependencies: [
+		.package(url: "https://github.com/pointfreeco/swift-dependencies", .upToNextMajor(from: "1.14.0")),
+		.package(url: "https://github.com/pointfreeco/swift-sharing", .upToNextMajor(from: "2.9.1")),
+		.package(url: "https://github.com/apple/swift-log", .upToNextMajor(from: "1.14.0")),
+	],
+	targets: [
+		.target(
+			name: "SystemClients",
+			dependencies: [
+				.dependencies,
+				.dependenciesMacros,
+				.sharing,
+			],
+		),
+		.target(
+			name: "LogBackends",
+			dependencies: [
+				.logging,
+			],
+		),
+	],
+	swiftLanguageModes: [.v6],
 )
 
 // MARK: - Custom Target.Dependency Values
 
-private extension Target.Dependency {
-    static let dependencies: Self = .product(name: "Dependencies", package: "swift-dependencies")
-    static let dependenciesMacros: Self = .product(name: "DependenciesMacros", package: "swift-dependencies")
-    static let sharing: Self = .product(name: "Sharing", package: "swift-sharing")
+extension Target.Dependency {
+	fileprivate static let dependencies: Self = .product(
+		name: "Dependencies",
+		package: "swift-dependencies",
+	)
+	fileprivate static let dependenciesMacros: Self = .product(
+		name: "DependenciesMacros",
+		package: "swift-dependencies",
+	)
+	fileprivate static let sharing: Self = .product(
+		name: "Sharing",
+		package: "swift-sharing",
+	)
+	fileprivate static let logging: Self = .product(
+		name: "Logging",
+		package: "swift-log",
+	)
 }
 
 // MARK: - Build Settings
 
-// We enable these settings for all targets in this package so that the dialect of swift we're using is same across
-// all our code.
+/// We enable these settings for all targets in this package so that the dialect of swift we're using is same across
+/// all our code.
 let swiftSettings: [SwiftSetting] = [
-    .enableUpcomingFeature("InferIsolatedConformances"),
-    .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
-    .enableUpcomingFeature("ExistentialAny"),
-    .enableUpcomingFeature("InternalImportsByDefault"),
-    .enableUpcomingFeature("MemberImportVisibility"),
+	.enableUpcomingFeature("InferIsolatedConformances"),
+	.enableUpcomingFeature("NonisolatedNonsendingByDefault"),
+	.enableUpcomingFeature("ExistentialAny"),
+	.enableUpcomingFeature("InternalImportsByDefault"),
+	.enableUpcomingFeature("MemberImportVisibility"),
 ]
 
 for target in package.targets {
