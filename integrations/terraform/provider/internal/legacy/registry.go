c2d2b18c3c365a9a74e0b0be21d7eff3a90488b2
Tim Ross <tim.ross@goteleport.com>
2026-08-06 10:31:12 -0400

    Terraform: convert db object import rule to generic provider
    Contributes to https://github.com/gravitational/teleport/issues/69301.

--- integrations/terraform/provider/internal/legacy/registry.go
+++ integrations/terraform/provider/internal/legacy/registry.go
@@ -42,7 +42,6 @@ func DataSourceTypes() map[string]tfsdk.DataSourceType {
 		"teleport_health_check_config":        dataSourceTeleportHealthCheckConfigType{},
 		"teleport_vnet_config":                dataSourceTeleportVnetConfigType{},
 		"teleport_integration":                dataSourceTeleportIntegrationType{},
-		"teleport_db_object_import_rule":      dataSourceTeleportDatabaseObjectImportRuleType{},
 		"teleport_classifier":                 dataSourceTeleportClassifierType{},
 		// TODO(bl-nero): Add teleport_inference_* data sources after data sources
 		// are fixed. The current problems with data sources include:
@@ -88,7 +87,6 @@ func ResourceTypes() map[string]tfsdk.ResourceType {
 		"teleport_classifier":                 resourceTeleportClassifierType{},
 		"teleport_retrieval_model":            resourceTeleportRetrievalModelType{},
 		"teleport_workload_cluster":           resourceTeleportWorkloadClusterType{},
-		"teleport_db_object_import_rule":      resourceTeleportDatabaseObjectImportRuleType{},
 		"teleport_client_ip_restriction":      resourceTeleportClientIPRestrictionType{},
 	}
 }
---
+++ integrations/terraform/provider/internal/resources/db_object_import_rule.go
@@ -0,0 +1,66 @@
+// Teleport
+// Copyright (C) 2026 Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package resources
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+
+	dbobjectimportrulev1 "github.com/gravitational/teleport/api/gen/proto/go/teleport/dbobjectimportrule/v1"
+	"github.com/gravitational/teleport/api/types"
+
+	"github.com/gravitational/teleport/integrations/terraform/provider/internal/teleport"
+	"github.com/gravitational/teleport/integrations/terraform/provider/internal/tfdriver"
+	schemav1 "github.com/gravitational/teleport/integrations/terraform/tfschema/dbobjectimportrule/v1"
+)
+
+// NewDatabaseObjectImportRuleDataSourceType returns the database object import rule data source type.
+func NewDatabaseObjectImportRuleDataSourceType() tfdriver.DataSourceType[dbobjectimportrulev1.DatabaseObjectImportRule, tfdriver.NameIdentifier] {
+	return tfdriver.DataSourceType[dbobjectimportrulev1.DatabaseObjectImportRule, tfdriver.NameIdentifier]{
+		NewDataSourceClient: func(p tfsdk.Provider) tfdriver.DataSourceClient[dbobjectimportrulev1.DatabaseObjectImportRule, tfdriver.NameIdentifier] {
+			return teleport.NewDatabaseObjectImportRuleClient(clientFromProvider(p))
+		},
+		Kind: types.KindDatabaseObjectImportRule,
+		Codec: tfdriver.DataSourceCodecFuncs[dbobjectimportrulev1.DatabaseObjectImportRule]{
+			SchemaFunc:  schemav1.GenSchemaDatabaseObjectImportRule,
+			ToStateFunc: schemav1.CopyDatabaseObjectImportRuleToTerraform,
+		},
+		Identifier: tfdriver.NameIdentifierFromPath(path.Root("metadata").AtName("name")),
+	}
+}
+
+// NewDatabaseObjectImportRuleResourceType returns the database object import rule resource type.
+func NewDatabaseObjectImportRuleResourceType() tfdriver.ResourceType[dbobjectimportrulev1.DatabaseObjectImportRule, tfdriver.NameIdentifier] {
+	return tfdriver.ResourceType[dbobjectimportrulev1.DatabaseObjectImportRule, tfdriver.NameIdentifier]{
+		NewResourceClient: func(p tfsdk.Provider) tfdriver.ResourceClient[dbobjectimportrulev1.DatabaseObjectImportRule, tfdriver.NameIdentifier] {
+			return teleport.NewDatabaseObjectImportRuleClient(clientFromProvider(p))
+		},
+		Kind: types.KindDatabaseObjectImportRule,
+		Codec: tfdriver.ResourceCodecFuncs[dbobjectimportrulev1.DatabaseObjectImportRule]{
+			SchemaFunc:   schemav1.GenSchemaDatabaseObjectImportRule,
+			ToStateFunc:  schemav1.CopyDatabaseObjectImportRuleToTerraform,
+			FromPlanFunc: schemav1.CopyDatabaseObjectImportRuleFromTerraform,
+		},
+		Normalizer: tfdriver.ForceKind[dbobjectimportrulev1.DatabaseObjectImportRule](types.KindDatabaseObjectImportRule),
+		Identifier: tfdriver.NameIdentifierPolicy(path.Root("metadata").AtName("name"), func(importRule *dbobjectimportrulev1.DatabaseObjectImportRule) string {
+			return importRule.GetMetadata().GetName()
+		}),
+		ResourceRevision: func(st *dbobjectimportrulev1.DatabaseObjectImportRule) string {
+			return st.GetMetadata().GetRevision()
+		},
+	}
+}
