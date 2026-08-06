11b2f9756679a033facf8f71bf6ee6faa09a5cf5
Tim Ross <tim.ross@goteleport.com>
2026-08-05 20:19:24 -0400

    Terraform: Convert Lock to generic provider
    Contributes to https://github.com/gravitational/teleport/issues/69301.

--- integrations/terraform/gen/main.go
+++ integrations/terraform/gen/main.go
@@ -359,21 +359,6 @@ var (
 		HasCheckAndSetDefaults: true,
 	}
 
-	lock = payload{
-		Name:                   "Lock",
-		TypeName:               "LockV2",
-		VarName:                "lock",
-		GetMethod:              "GetLock",
-		CreateMethod:           "UpsertLock",
-		UpdateMethod:           "UpsertLock",
-		DeleteMethod:           "DeleteLock",
-		ID:                     `lock.Metadata.Name`,
-		Kind:                   "lock",
-		HasStaticID:            false,
-		TerraformResourceType:  "teleport_lock",
-		HasCheckAndSetDefaults: true,
-	}
-
 	oidcConnector = payload{
 		Name:                   "OIDCConnector",
 		TypeName:               "OIDCConnectorV3",
@@ -1133,8 +1118,6 @@ func genTFSchema() {
 	generateDataSource(dynamicWindowsDesktop, pluralDataSource)
 	generateResource(githubConnector, pluralResource)
 	generateDataSource(githubConnector, pluralDataSource)
-	generateResource(lock, pluralResource)
-	generateDataSource(lock, pluralDataSource)
 	generateResource(oidcConnector, pluralResource)
 	generateDataSource(oidcConnector, pluralDataSource)
 	generateResource(samlIdPServiceProvider, pluralResource)
