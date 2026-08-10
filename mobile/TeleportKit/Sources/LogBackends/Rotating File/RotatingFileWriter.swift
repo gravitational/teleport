93c1d727667fe7274eaf1831cac9195b86d8547d
Gustavo Medori <gustavo.medori@goteleport.com>
2026-08-10 16:29:12 -0600

    Add initial type definitions for implementing the RotatingFileWriter

---
+++ mobile/TeleportKit/Sources/LogBackends/Rotating File/RotatingFileWriter.swift
@@ -0,0 +1,82 @@
+// Teleport
+// Copyright (C) 2026 Gravitational, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/
+
+import Dependencies
+import Foundation
+import Synchronization
+import SystemClients
+
+public final class RotatingFileWriter: Sendable {
+	// MARK: Mutable State
+
+	private let inbox = Mutex(Inbox())
+	private let fileState = Mutex(FileState())
+
+	// MARK: Immutable State
+
+	private let activeFileURL: URL
+	private let configuration: Configuration
+	private let queue = DispatchQueue(label: "com.goteleport.teleportkit.rotating-file-writer")
+	private let fileSystemClient: FileSystemClient
+
+	init(activeFileURL: URL, configuration: Configuration = .live) {
+		self.activeFileURL = activeFileURL
+		self.configuration = configuration
+
+		// Retrieving the dependency once during init is safe because the file system implementation should not
+		// change mid-process.
+		@Dependency(\.fileSystemClient)
+		var fileSystemClient
+
+		self.fileSystemClient = fileSystemClient
+	}
+}
+
+// MARK: - Supporting Types
+
+extension RotatingFileWriter {
+	public struct Configuration: Sendable {
+		public static let live = Configuration(
+			maximumFileSize: 4 * 1024 * 1024, // 4 MB
+			maximumArchiveCount: 3, // At most 3 archived files. Including
+		)
+
+		/// The maximum size any individual file is allowed to be, in number of bytes
+		let maximumFileSize: Int
+
+		/// The maximum number of archived files. This does not include the file actively being written to.
+		///
+		/// For example, if the maximum archive count is 3, then at most the rotating file writer will govern 4 total
+		/// files: the 3 archive files and the one active file.
+		let maximumArchiveCount: Int
+	}
+
+	private struct Inbox {
+		var pendingItems: [Data] = []
+		var queuedBytes: Int {
+			pendingItems.map(\.count).reduce(0, +)
+		}
+	}
+
+	/// Contains mutable state that the worker needs in order to write to disk.
+	struct FileState {
+		var fileClient: WritableFileClient? = nil
+	}
+
+	private enum PendingItem {
+		case logMessage(Data)
+	}
+}
