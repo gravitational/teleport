42541eb876c0ec65a3df625ad16564e7d7505291
rosstimothy <39066650+rosstimothy@users.noreply.github.com>
2026-08-10 11:41:06 -0400

    Terraform: convert trusted cluster to generic provider (#69399)
    Contributes to https://github.com/gravitational/teleport/issues/69301.

--- integrations/terraform/gen/main.go
+++ integrations/terraform/gen/main.go
@@ -382,22 +382,6 @@ var (
 		HasCheckAndSetDefaults: true,
 	}
 
-	trustedCluster = payload{
-		Name:                   "TrustedCluster",
-		TypeName:               "TrustedClusterV2",
-		VarName:                "trustedCluster",
-		GetMethod:              "GetTrustedCluster",
-		CreateMethod:           "UpsertTrustedCluster",
-		UpdateMethod:           "UpsertTrustedCluster",
-		DeleteMethod:           "DeleteTrustedCluster",
-		UpsertMethodArity:      2,
-		ID:                     "trustedCluster.Metadata.Name",
-		Kind:                   "trusted_cluster",
-		HasStaticID:            false,
-		TerraformResourceType:  "teleport_trusted_cluster",
-		HasCheckAndSetDefaults: true,
-	}
-
 	uiConfig = payload{
 		Name:                   "UIConfig",
 		TypeName:               "UIConfigV1",
@@ -944,8 +928,6 @@ func genTFSchema() {
 	generateDataSource(samlIdPServiceProvider, pluralDataSource)
 	generateResource(provisionToken, pluralResource)
 	generateDataSource(provisionToken, pluralDataSource)
-	generateResource(trustedCluster, pluralResource)
-	generateDataSource(trustedCluster, pluralDataSource)
 	generateResource(sessionRecording, singularResource)
 	generateDataSource(sessionRecording, singularDataSource)
 	generateResource(uiConfig, singularResource)
