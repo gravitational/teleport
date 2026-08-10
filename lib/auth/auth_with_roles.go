19858459bd160ceb1f1cc4549fa655ea30fc66a9
Zac Bergquist <zac.bergquist@goteleport.com>
2026-08-10 02:05:22 -0600

    Allow deletion of a role named "admin" (#69486)
    This is a relic of the Teleport 6 days before we added RBAC to OSS
    Teleport. Back then, every local user had a role named "admin", and
    we had some protection in place preventing that role from being deleted.
    
    Today, no such role exists, but the deletion prevention remains. This
    means you can create your own custom role named "admin" and Teleport
    won't let you delete it.
    
    Closes #69222
    
    (cherry picked from commit 40e6fa0087089f8db54e60460b972166fdefc579)

--- lib/auth/auth_with_roles.go
+++ lib/auth/auth_with_roles.go
@@ -4944,14 +4944,6 @@ func (a *ServerWithRoles) DeleteRole(ctx context.Context, name string) error {
 		return trace.Wrap(err)
 	}
 
-	// DELETE IN (7.0)
-	// It's OK to delete this code alongside migrateOSS code in auth.
-	// It prevents 6.0 from migrating resources multiple times
-	// and the role is used for `tctl users add` code too.
-	if modules.GetModules().IsOSSBuild() && name == teleport.AdminRoleName {
-		return trace.AccessDenied("can not delete system role %q", name)
-	}
-
 	return a.authServer.DeleteRole(ctx, name)
 }
 
