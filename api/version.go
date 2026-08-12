872761bb1ca11dab886cdfd8ee896a20f09e0922
Angel Dionisio <angel.dionisio@goteleport.com>
2026-08-12 10:34:41 -0400

    Release v17.7.26-dev-aadc.signbckprt.1

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "17.7.26"
+const Version = "17.7.26-dev-aadc.signbckprt.1"
 
 const VersionMajor = 17
 const VersionMinor = 7
 const VersionPatch = 26
-const VersionPreRelease = ""
+const VersionPreRelease = "dev-aadc.signbckprt.1"
 const VersionMetadata = ""
