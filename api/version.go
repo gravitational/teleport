03bd765345667cd71722c84509337a76aa4fbd2f
Alan Parra <alan.parra@goteleport.com>
2026-08-12 14:04:36 -0300

    Release v18.10.2-dev.pki-domain-db.4

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "18.10.2-dev.pki-domain-db.3"
+const Version = "18.10.2-dev.pki-domain-db.4"
 
 const VersionMajor = 18
 const VersionMinor = 10
 const VersionPatch = 2
-const VersionPreRelease = "dev.pki-domain-db.3"
+const VersionPreRelease = "dev.pki-domain-db.4"
 const VersionMetadata = ""
