b445c5d40a476070f43e22a5e5cbfd2d0bd3b3b6
Angel Dionisio <angel.dionisio@goteleport.com>
2026-08-14 18:24:05 -0400

    Release v19.0.0-dev.aadc.tstsig.2

--- api/version.go
+++ api/version.go
@@ -2,10 +2,10 @@
 
 package api
 
-const Version = "19.0.0-dev.aadc.tstsig.1"
+const Version = "19.0.0-dev.aadc.tstsig.2"
 
 const VersionMajor = 19
 const VersionMinor = 0
 const VersionPatch = 0
-const VersionPreRelease = "dev.aadc.tstsig.1"
+const VersionPreRelease = "dev.aadc.tstsig.2"
 const VersionMetadata = ""
